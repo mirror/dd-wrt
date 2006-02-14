f1a3350653d00414c2e4a98786bb5abee843d96f
BrainSlayer <s.gottschall@dd-wrt.com>
2006-02-14 14:42:36 +0000

    gpl issues fixed
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/closed/openvpn.c
+++ src/router/rc/closed/openvpn.c
@@ -1,11 +1,24 @@
 /*
-this source is published under no warranty and may be published without any fee
-to third parties. 
-The use of this source is permitted without any charge for PRIVATE and NON COMERCIAL usage
-only unless other rights are granted by the firmware author only.
-
-DD-WRT v23 (c) 2004 - 2005 by Sebastian Gottschall / Blueline AG
-*/
+ * openvpn.c
+ *
+ * Copyright (C) 2005 - 2006 Sebastian Gottschall <sebastian.gottschall@blueline-ag.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $id
+ */
 
 
 #include <ctype.h>
