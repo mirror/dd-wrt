10a48d73942bae4aa2c0ce5be0873aaf2a2ad7af
BrainSlayer <s.gottschall@dd-wrt.com>
2006-02-16 12:19:35 +0000

    modularized
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@63 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/vfs/vfs.c
+++ src/router/vfs/vfs.c
@@ -4,117 +4,124 @@
 
 
 //typedef struct HEADER header;
-entry *vfsopen(char *name,char *mode)
+entry *
+vfsopen (char *name, char *mode)
 {
-header h;
-entry *current;
-int i,a;
-int r=0;
-FILE *in=fopen("/usr/lib/vfs.lib","rb");
-if (in==NULL)return NULL;
-r=fread(&h,10,1,in);
-h.entries=(entry*)malloc(sizeof(entry)*h.noe);
+  header h;
+  entry *current;
+  int i, a;
+  int r = 0;
+  FILE *in = fopen ("/usr/lib/vfs.lib", "rb");
+  if (in == NULL)
+    return NULL;
+  r = fread (&h, 10, 1, in);
+  h.entries = (entry *) malloc (sizeof (entry) * h.noe);
 //read entrytable
-for (i=0;i<h.noe;i++)
+  for (i = 0; i < h.noe; i++)
     {
-    h.entries[i].namelen=getc(in);
-    h.entries[i].name=(char*)malloc(h.entries[i].namelen+1);
-    fread(h.entries[i].name,h.entries[i].namelen,1,in);
-    h.entries[i].name[h.entries[i].namelen]=0;
-    if (strcmp(name,h.entries[i].name)==0)
+      h.entries[i].namelen = getc (in);
+      h.entries[i].name = (char *) malloc (h.entries[i].namelen + 1);
+      fread (h.entries[i].name, h.entries[i].namelen, 1, in);
+      h.entries[i].name[h.entries[i].namelen] = 0;
+      if (strcmp (name, h.entries[i].name) == 0)
 	{
-	current=&h.entries[i];
-	current->curpos=0;
-	current->offset=getw(in);
-	current->filelen=getw(in);
-	current->content=malloc(current->filelen);
-	fseek(in,current->offset,SEEK_SET);
-	fread(current->content,current->filelen,1,in);
-	fclose(in);
-	for (a=0;a<i;a++)
+	  current = &h.entries[i];
+	  current->curpos = 0;
+	  current->offset = getw (in);
+	  current->filelen = getw (in);
+	  current->content = malloc (current->filelen);
+	  fseek (in, current->offset, SEEK_SET);
+	  fread (current->content, current->filelen, 1, in);
+	  fclose (in);
+	  for (a = 0; a < i; a++)
 	    {
-	    free(h.entries[a].name);
-	    free(h.entries);
+	      free (h.entries[a].name);
+	      free (h.entries);
 	    }
-	return current;
+	  return current;
 	}
     }
-for (a=0;a<i;a++)
-	    {
-	    free(h.entries[a].name);
-	    free(h.entries);
-	    }
-return NULL;    
+  for (a = 0; a < i; a++)
+    {
+      free (h.entries[a].name);
+      free (h.entries);
+    }
+  return NULL;
 //if (r<10)return NULL;
 }
 
 
-int vfsseek(entry *in,int pos,int use)
+int
+vfsseek (entry * in, int pos, int use)
 {
-switch(use)
-{
-case SEEK_SET:
-in->curpos=pos;
-break;
-case SEEK_END:
-in->curpos=(in->filelen-1)-pos;
-if (in->curpos<0){
-    in->curpos=0;
-    return -1;
-    }
-break;
-case SEEK_CUR:
-in->curpos+=pos;
-if (in->curpos>=in->filelen)
+  switch (use)
     {
-    in->curpos=0;
-    return -1;
+    case SEEK_SET:
+      in->curpos = pos;
+      break;
+    case SEEK_END:
+      in->curpos = (in->filelen - 1) - pos;
+      if (in->curpos < 0)
+	{
+	  in->curpos = 0;
+	  return -1;
+	}
+      break;
+    case SEEK_CUR:
+      in->curpos += pos;
+      if (in->curpos >= in->filelen)
+	{
+	  in->curpos = 0;
+	  return -1;
+	}
+      break;
     }
-break;
-}
-return in->curpos;
+  return in->curpos;
 }
 
-int vfsgetc(entry *in)
+int
+vfsgetc (entry * in)
 {
-if (in->curpos==in->filelen)return EOF;
-unsigned char b=in->content[in->curpos];
-in->curpos++;
-return b;
+  if (in->curpos == in->filelen)
+    return EOF;
+  unsigned char b = in->content[in->curpos];
+  in->curpos++;
+  return b;
 }
 
-int vfsread(void *fill,int blocks,int blocksize,entry *in)
+int
+vfsread (void *fill, int blocks, int blocksize, entry * in)
 {
-int b;
-int i;
-int cnt=0;
-unsigned char *p=(unsigned char*)fill;
-for (i=0;i<blocks*blocksize;i++)
+  int b;
+  int i;
+  int cnt = 0;
+  unsigned char *p = (unsigned char *) fill;
+  for (i = 0; i < blocks * blocksize; i++)
     {
-    b =vfsgetc(in);
-    if (b==EOF)return cnt;
-    p[cnt++]=b;
+      b = vfsgetc (in);
+      if (b == EOF)
+	return cnt;
+      p[cnt++] = b;
     }
-return cnt;
+  return cnt;
 }
 
-int vfsrewind(entry *in)
+int
+vfsrewind (entry * in)
 {
-in->curpos=0;
-return 0;
+  in->curpos = 0;
+  return 0;
 }
 
-int vfsclose(entry *in)
+int
+vfsclose (entry * in)
 {
-if (in==NULL)return -1;
-if (in->name!=NULL)
-    free(in->name);
-if (in->content!=NULL)
-    free(in->content);
-free(in);
-return 0;
+  if (in == NULL)
+    return -1;
+  if (in->name != NULL)
+    free (in->name);
+  if (in->content != NULL)
+    free (in->content);
+  free (in);
+  return 0;
 }
-
-
-
-
