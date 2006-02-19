45d6bb21d5460691a34e193b7de72e8b24422594
BrainSlayer <s.gottschall@dd-wrt.com>
2006-02-19 19:14:01 +0000

    bugfix for memory leak in translation engine
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@84 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/cgi.c
+++ src/router/httpd/cgi.c
@@ -17,7 +17,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <signal.h>
-#include <shutils.h> //Added by Dainel(2004-07-26)
+#include <shutils.h>		//Added by Dainel(2004-07-26)
 #define assert(a)
 
 #if defined(linux)
@@ -31,104 +31,110 @@ static struct hsearch_data htab;
 static int htab_count;
 
 static void
-unescape(char *s)
+unescape (char *s)
 {
-	unsigned int c;
+  unsigned int c;
 
-	while ((s = strpbrk(s, "%+"))) {
-		/* Parse %xx */
-		if (*s == '%') {
-			sscanf(s + 1, "%02x", &c);
-			*s++ = (char) c;
-			strncpy(s, s + 2, strlen(s) + 1);
-		}
-		/* Space is special */
-		else if (*s == '+')
-			*s++ = ' ';
+  while ((s = strpbrk (s, "%+")))
+    {
+      /* Parse %xx */
+      if (*s == '%')
+	{
+	  sscanf (s + 1, "%02x", &c);
+	  *s++ = (char) c;
+	  strncpy (s, s + 2, strlen (s) + 1);
 	}
+      /* Space is special */
+      else if (*s == '+')
+	*s++ = ' ';
+    }
 }
 
 char *
-get_cgi(char *name)
+get_cgi (char *name)
 {
-	ENTRY e, *ep;
+  ENTRY e, *ep;
 
-	if (!htab.table)
-		return NULL;
+  if (!htab.table)
+    return NULL;
 
-	e.key = name;
-	hsearch_r(e, FIND, &ep, &htab);
+  e.key = name;
+  hsearch_r (e, FIND, &ep, &htab);
 
-	return ep ? ep->data : NULL;
+  return ep ? ep->data : NULL;
 }
 
 void
-set_cgi(char *name, char *value)
+set_cgi (char *name, char *value)
 {
-	ENTRY e, *ep;
+  ENTRY e, *ep;
 
-	//cprintf("\nIn set_cgi(), name = %s, value = %s\n", name, value);
+  //cprintf("\nIn set_cgi(), name = %s, value = %s\n", name, value);
 
-	if (!htab.table)
-		return;
+  if (!htab.table)
+    return;
 
-	e.key = name;
-	hsearch_r(e, FIND, &ep, &htab);
-	if (ep){
-		//cprintf("\nIn set_cgi(), ep = %s\n", ep);
-		ep->data = value;
-        }
-	else {
-		//cprintf("\nIn set_cgi(), ep = %s(NULL)\n", ep);
-		e.data = value;
-		hsearch_r(e, ENTER, &ep, &htab);
-		htab_count++;
-	}
-	assert(ep);
+  e.key = name;
+  hsearch_r (e, FIND, &ep, &htab);
+  if (ep)
+    {
+      //cprintf("\nIn set_cgi(), ep = %s\n", ep);
+      ep->data = value;
+    }
+  else
+    {
+      //cprintf("\nIn set_cgi(), ep = %s(NULL)\n", ep);
+      e.data = value;
+      hsearch_r (e, ENTER, &ep, &htab);
+      htab_count++;
+    }
+  assert (ep);
 }
 
 void
-init_cgi(char *query)
+init_cgi (char *query)
 {
-	int len, nel;
-	char *q, *name, *value;
+  int len, nel;
+  char *q, *name, *value;
 
-	htab_count = 0;
+  htab_count = 0;
 
-	//cprintf("\nIn init_cgi(), query = %s\n", query);
+  //cprintf("\nIn init_cgi(), query = %s\n", query);
 
-	/* Clear variables */
-	if (!query) {
-		hdestroy_r(&htab);
-		return;
-	}
+  /* Clear variables */
+  if (!query)
+    {
+      hdestroy_r (&htab);
+      return;
+    }
 
-	/* Parse into individual assignments */
-	q = query;
-	len = strlen(query);
-	nel = 1;
-	while (strsep(&q, "&;"))
-		nel++;
-	hcreate_r(nel, &htab);
-	//cprintf("\nIn init_cgi(), nel = %d\n", nel);
+  /* Parse into individual assignments */
+  q = query;
+  len = strlen (query);
+  nel = 1;
+  while (strsep (&q, "&;"))
+    nel++;
+  hcreate_r (nel, &htab);
+  //cprintf("\nIn init_cgi(), nel = %d\n", nel);
 
-	for (q = query; q < (query + len);) {
-		/* Unescape each assignment */
-		unescape(name = value = q);
+  for (q = query; q < (query + len);)
+    {
+      /* Unescape each assignment */
+      unescape (name = value = q);
 
-		/* Skip to next assignment */
-		for (q += strlen(q); q < (query + len) && !*q; q++);
+      /* Skip to next assignment */
+      for (q += strlen (q); q < (query + len) && !*q; q++);
 
-		/* Assign variable */
-		name = strsep(&value, "=");
-		if (value)
-			set_cgi(name, value);
-	}
-	//cprintf("\nIn init_cgi(), AFTER PROCESS query = %s\n", query);
+      /* Assign variable */
+      name = strsep (&value, "=");
+      if (value)
+	set_cgi (name, value);
+    }
+  //cprintf("\nIn init_cgi(), AFTER PROCESS query = %s\n", query);
 }
 
 int
-count_cgi()
+count_cgi ()
 {
-	return htab_count;
+  return htab_count;
 }
