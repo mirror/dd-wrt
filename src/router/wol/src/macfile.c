9435aa603195d9d5f6a4fcc85456a2a895c3bdb1
brainslayer <brainslayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-02-20 08:20:24 +0000

    useless
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@88 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/wol/src/macfile.c
+++ src/router/wol/src/macfile.c
@@ -1,25 +1,25 @@
 /*
- *	wol - wake on lan client
+ * wol - wake on lan client
  *
- *	parses a macfile and return its tokens
+ * parse a macfile and return its tokens
  * 
- *	$Id: macfile.c,v 1.1.2.1 2004/06/20 21:55:18 nikki Exp $
+ * $Id: macfile.c,v 1.8 2004/02/05 18:15:15 wol Exp $
  *
- *	Copyright (C) 2000-2002 Thomas Krennwallner <krennwallner@aon.at>
+ * Copyright (C) 2000,2001,2002,2004 Thomas Krennwallner <krennwallner@aon.at>
  *
- *	This program is free software; you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation; either version 2 of the License, or
- *	(at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *	You should have received a copy of the GNU General Public License
- *	along with this program; if not, write to the Free Software
- *	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 
@@ -28,7 +28,9 @@
 #include <config.h>
 #endif /* HAVE_CONFIG_H */
 
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif /* _GNU_SOURCE */
 
 #include <stdio.h>
 #include <unistd.h>
@@ -59,73 +61,89 @@
 
 
 static unsigned int
-get_tokens (const char *str, char *mac, char *host, unsigned int *port,
-						char *passwd)
+get_tokens (const char *str,
+	    char *mac,
+	    char *host,
+	    unsigned int *port,
+	    char *passwd)
 {
-	if (sscanf (str, " # %*s") == 1)
-		return 0;
+  if (sscanf (str, " # %*s") == 1)
+    {
+      return 0;
+    }
 
-	if (sscanf (str, " " MAC_FMT " " HOST_FMT PORT_FMT " " PASS_FMT,
-											mac, host, port, passwd) == 4)
-		{
-			return MAC | HOST | PORT | PASS;
-		}
+  if (sscanf (str, " " MAC_FMT " " HOST_FMT PORT_FMT " " PASS_FMT,
+	      mac, host, port, passwd) == 4)
+    {
+      return MAC | HOST | PORT | PASS;
+    }
 
-	if (sscanf (str, " " MAC_FMT " " HOST_FMT " " PASS_FMT,
-										mac, host, passwd) == 3)
-		{
-			return MAC | HOST | PASS;
-		}
+  if (sscanf (str, " " MAC_FMT " " HOST_FMT " " PASS_FMT,
+	      mac, host, passwd) == 3)
+    {
+      return MAC | HOST | PASS;
+    }
 
-	if (sscanf (str, " " MAC_FMT " " HOST_FMT PORT_FMT, mac, host, port) == 3)
-		{
-			return MAC | HOST | PORT;
-		}
+  if (sscanf (str, " " MAC_FMT " " HOST_FMT PORT_FMT, mac, host, port) == 3)
+    {
+      return MAC | HOST | PORT;
+    }
 
-	if (sscanf (str, " " MAC_FMT " " HOST_FMT, mac, host) == 2)
-		{
-			return MAC | HOST;
-		}
+  if (sscanf (str, " " MAC_FMT " " HOST_FMT, mac, host) == 2)
+    {
+      return MAC | HOST;
+    }
 
-	return 0;
+  return 0;
 }
 
 
 
 int
-macfile_parse (FILE *fp, char **mac_str, char **host_str, unsigned int *port,
-								char **passwd_str)
+macfile_parse (FILE *fp,
+	       char **mac_str,
+	       char **host_str,
+	       unsigned int *port,
+	       char **passwd_str)
 {
-	char *willy = NULL;
-	ssize_t ret;
-	size_t whale = 0;
-	char mac[18], host[255], passwd[18]; /* FIXME: host sufficient? */
-	unsigned int parsed_tokens = 0;
+  char *willy = NULL;
+  ssize_t ret;
+  size_t whale = 0;
+  char mac[18], host[255], passwd[18]; /* FIXME: host sufficient? */
+  unsigned int parsed_tokens = 0;
 
-	*port = 0;
+  *port = 0;
 
-	while (!feof (fp) && !ferror (fp))
-		{
-			if ((ret = getline (&willy, &whale, fp)) == -1)
-				return -1;
+  while (!feof (fp) && !ferror (fp))
+    {
+      if ((ret = getline (&willy, &whale, fp)) == -1)
+	{
+	  return -1;
+	}
+      
+      if ((parsed_tokens = get_tokens (willy, mac, host, port, passwd)) == 0)
+	continue;	
 
-			if ((parsed_tokens = get_tokens (willy, mac, host, port, passwd)) == 0)
-				continue;	
+      XFREE (willy);
+      break;
+    }
 
-			XFREE (willy);
-			break;
-		}
+  if (ferror (fp))
+    {
+      return -1;
+    }
 
-	if (ferror (fp))
-		return -1;
+  *mac_str = strdup (mac);
+  *host_str = strdup (host);
 
-	*mac_str = strdup (mac);
-	*host_str = strdup (host);
+  if (parsed_tokens & PASS)
+    {
+      *passwd_str = strdup (passwd);
+    }
+  else
+    {
+      *passwd_str = NULL;
+    }
 
-	if (parsed_tokens & PASS)
-		*passwd_str = strdup (passwd);
-	else
-		*passwd_str = NULL;
-
-	return 0;
+  return 0;
 }
--- src/router/wol/src/net.c
+++ src/router/wol/src/net.c
@@ -1,23 +1,23 @@
 /*
- *	wol - wake on lan client
+ * wol - wake on lan client
  *
- *	$Id: net.c,v 1.1.2.1 2004/06/20 21:55:18 nikki Exp $
+ * $Id: net.c,v 1.8 2004/04/20 19:51:18 wol Exp $
  *
- *	Copyright (C) 2000-2003 Thomas Krennwallner <krennwallner@aon.at>
+ * Copyright (C) 2000,2001,2002,2003,2004 Thomas Krennwallner <krennwallner@aon.at>
  *
- *	This program is free software; you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation; either version 2 of the License, or
- *	(at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *	You should have received a copy of the GNU General Public License
- *	along with this program; if not, write to the Free Software
- *	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 
@@ -42,92 +42,158 @@
 #include "wol.h"
 
 
-
 static int
 net_resolv (const char *hostname, struct in_addr *sin_addr)
 {
-	struct hostent *hent;
+  struct hostent *hent;
 
-	hent = gethostbyname (hostname);
-	if (hent == NULL)
-		{
-			errno = EINVAL;
-			return -1;
-		}
+  hent = gethostbyname (hostname);
+  if (hent == NULL)
+    {
+      errno = EINVAL;
+      return -1;
+    }
 
-	memcpy ((void *) sin_addr, (const void *) hent->h_addr,
-					(size_t) hent->h_length);
+  memcpy ((void *) sin_addr, (const void *) hent->h_addr, (size_t) hent->h_length);
 
-	return 0;
+  return 0;
 }
+
 
 
 int
 net_close (int socket)
 {
-	if (close (socket))
-		{
-			perror ("Couldn't close socket");
-			return -1;
-		}
+  if (close (socket))
+    {
+      perror ("Couldn't close socket");
+      return -1;
+    }
 
-	return 0;
+  return 0;
 }
-
 
 
 
 int
-net_open (void)
+udp_open (void)
 {
-	int optval;
-	int sockfd;
+  int optval;
+  int sockfd;
 
-	sockfd = socket (PF_INET, SOCK_DGRAM, 0);
-	if (sockfd < 0)
-		{
-			perror ("socket() failed");
-			return -1;
-		}
+  sockfd = socket (PF_INET, SOCK_DGRAM, 0);
+  if (sockfd < 0)
+    {
+      perror ("socket() failed");
+      return -1;
+    }
 
-	optval = 1;
+  optval = 1;
 
-	if (setsockopt (sockfd, SOL_SOCKET, SO_BROADCAST, &optval, sizeof (optval)))
-		{
-			perror ("setsockopt() failed");
-			close (sockfd);
-			return -1;
-		}
+  if (setsockopt (sockfd, SOL_SOCKET, SO_BROADCAST, &optval, sizeof (optval)))
+    {
+      perror ("setsockopt() failed");
+      close (sockfd);
+      return -1;
+    }
 
-	return sockfd;
+  return sockfd;
 }
 
 
 
-int 
-net_send (int socket, const char *ip_str, unsigned short int port,
-					const void *buf, size_t len)
+int
+tcp_open (const char *ip_str,
+	  unsigned int port)
 {
-	struct sockaddr_in toaddr;
-	int sendret;
+  int sockfd;
+  struct sockaddr_in toaddr;
 
-
-	if (ip_str == NULL || buf == NULL) return -1;
+  if (ip_str == NULL)
+    {
+      return -1;
+    }
 	
-	memset (&toaddr, 0, sizeof (struct sockaddr_in));
+  memset (&toaddr, 0, sizeof (struct sockaddr_in));
 
-	if (net_resolv (ip_str, &toaddr.sin_addr))
-		{
-			error (0, 0, _("Invalid IP address given: %s"), strerror (errno));
-			return -1;
-		}
+  if (net_resolv (ip_str, &toaddr.sin_addr))
+    {
+      error (0, 0, _("Invalid IP address given: %s"), strerror (errno));
+      return -1;
+    }
 
-	toaddr.sin_family = AF_INET;
-	toaddr.sin_port = htons (port);
+  toaddr.sin_family = AF_INET;
+  toaddr.sin_port = htons (port);
 
-	/* keep on sending and check for possible errors */
-	sendret = sendto (socket, buf, len, 0, (struct sockaddr *) &toaddr,
-										sizeof (struct sockaddr_in));
+  sockfd = socket (PF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0)
+    {
+      perror ("socket() failed");
+      return -1;
+    }
 
-	return sendret == -1 ? sendret : 0;
+  if (connect (sockfd, (const struct sockaddr *) &toaddr, sizeof (toaddr)))
+    {
+      error (0, 0, _("Couldn't connect to %s:%hu: %s"), ip_str, port, strerror (errno));
+      close (sockfd);
+      return -1;
+    }
+
+  return sockfd;
+}
+
+
+
+ssize_t 
+udp_send (int socket,
+	  const char *ip_str,
+	  unsigned short int port,
+	  const void *buf,
+	  size_t len)
+{
+  struct sockaddr_in toaddr;
+  ssize_t sendret;
+
+
+  if (ip_str == NULL || buf == NULL)
+    {
+      return -1;
+    }
+	
+  memset (&toaddr, 0, sizeof (struct sockaddr_in));
+
+  if (net_resolv (ip_str, &toaddr.sin_addr))
+    {
+      error (0, 0, _("Invalid IP address given: %s"), strerror (errno));
+      return -1;
+    }
+
+  toaddr.sin_family = AF_INET;
+  toaddr.sin_port = htons (port);
+
+  /* keep on sending and check for possible errors */
+  sendret = sendto (socket, buf, len, 0, (struct sockaddr *) &toaddr,
+		    sizeof (struct sockaddr_in));
+
+  return sendret == -1 ? sendret : 0;
+}
+
+
+
+ssize_t
+tcp_send (int sock,
+	  const void *buf,
+	  size_t len)
+{
+  return send (sock, buf, len, 0);
+}
+
+
+
+ssize_t
+tcp_recv (int sock,
+	  void *buf,
+	  size_t len)
+{
+  return recv (sock, buf, len, 0);
 }
