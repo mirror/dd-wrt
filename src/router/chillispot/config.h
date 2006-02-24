385ac0a3edb1e268abea7a1c792d80646a687060
brainslayer <brainslayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-02-24 15:37:45 +0000

    fon boottime fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@262 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/chillispot/config.h
+++ src/router/chillispot/config.h
@@ -27,7 +27,7 @@
 
 /* Define to 1 if your system has a GNU libc compatible `malloc' function, and
    to 0 otherwise. */
-#define HAVE_MALLOC 0
+#define HAVE_MALLOC 1
 
 /* Define to 1 if you have the <memory.h> header file. */
 #define HAVE_MEMORY_H 1
@@ -147,7 +147,6 @@
 /* #undef const */
 
 /* Define to rpl_malloc if the replacement function should be used. */
-#define malloc rpl_malloc
 
 /* Define to `int' if <sys/types.h> does not define. */
 /* #undef mode_t */
