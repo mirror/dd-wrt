bd5b9ea5e2c61a0b533812d2115cda7fa822d5c0
botho <USER@DOMAIN>
2006-03-03 18:54:47 +0000

    Add new function createBar to create progress bar
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@432 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/cisco_wrt54g_en/common.js
+++ src/router/kromo/cisco_wrt54g_en/common.js
@@ -420,3 +420,84 @@ function fix_cr(F) {
 return g;
 }
 
+// BEGIN xp_progressbar
+// Copyright 2004 Brian Gosselin of ScriptAsylum.com
+//
+// v1.0 - Initial release
+// v1.1 - Added ability to pause the scrolling action (requires you to assign
+//        the bar to a unique arbitrary variable).
+//      - Added ability to specify an action to perform after a x amount of
+//      - bar scrolls. This requires two added arguments.
+// v1.2 - Added ability to hide/show each bar (requires you to assign the bar
+//        to a unique arbitrary variable).
+// v.13 - Botho 03/03/06 : incorporate DD-WRT styles
+
+// var xyz = createBar(
+// total_width,
+// total_height,
+// background_color,       Botho 03/03/06 : incorporate DD-WRT styles
+// border_width,           Botho 03/03/06 : incorporate DD-WRT styles
+// border_color,           Botho 03/03/06 : incorporate DD-WRT styles
+// block_color,            Botho 03/03/06 : incorporate DD-WRT styles
+// scroll_speed,
+// block_count,
+// scroll_count,
+// action_to_perform_after_scrolled_n_times
+// )
+
+var w3c=(document.getElementById)?true:false;
+var ie=(document.all)?true:false;
+var N=-1;
+
+function createBar(w,h,speed,blocks,count,action){
+if(ie||w3c){
+var t='<div class="progressbar" id="_xpbar'+(++N)+'" style="width:'+w+'px; height:'+h+'px; border-style:solid; font-size:1px;">';
+t+='<span id="blocks'+N+'" style="left:-'+(h*2+1)+'px; position:absolute; font-size:1px">';
+for(i=0;i<blocks;i++){
+t+='<span class="progressbarblock" style="left:-'+((h*i)+i)+'px; font-size:1px; position:absolute; width:'+h+'px; height:'+h+'px; '
+t+=(ie)?'filter:alpha(opacity='+(100-i*(100/blocks))+')':'-Moz-opacity:'+((100-i*(100/blocks))/100);
+t+='"></span>';
+}
+t+='</span></div>';
+document.write(t);
+var bA=(ie)?document.all['blocks'+N]:document.getElementById('blocks'+N);
+bA.bar=(ie)?document.all['_xpbar'+N]:document.getElementById('_xpbar'+N);
+bA.blocks=blocks;
+bA.N=N;
+bA.w=w;
+bA.h=h;
+bA.speed=speed;
+bA.ctr=0;
+bA.count=count;
+bA.action=action;
+bA.togglePause=togglePause;
+bA.showBar=function(){
+this.bar.style.visibility="visible";
+}
+bA.hideBar=function(){
+this.bar.style.visibility="hidden";
+}
+bA.tid=setInterval('startBar('+N+')',speed);
+return bA;
+}}
+
+function startBar(bn){
+var t=(ie)?document.all['blocks'+bn]:document.getElementById('blocks'+bn);
+if(parseInt(t.style.left)+t.h+1-(t.blocks*t.h+t.blocks)>t.w){
+t.style.left=-(t.h*2+1)+'px';
+t.ctr++;
+if(t.ctr>=t.count){
+eval(t.action);
+t.ctr=0;
+}}else t.style.left=(parseInt(t.style.left)+t.h+1)+'px';
+}
+
+function togglePause(){
+if(this.tid==0){
+this.tid=setInterval('startBar('+this.N+')',this.speed);
+}else{
+clearInterval(this.tid);
+this.tid=0;
+}}
+
+// END xp_progressbar
