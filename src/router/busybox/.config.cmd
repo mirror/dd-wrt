cf5d0777aad0852c397519ebd3ddd6b9ef7856de
BrainSlayer <s.gottschall@dd-wrt.com>
2006-03-04 16:10:17 +0000

    busybox
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@460 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/busybox/.config.cmd
@@ -0,0 +1,22 @@
+deps_config := \
+	util-linux/Config.in \
+	sysklogd/Config.in \
+	shell/Config.in \
+	procps/Config.in \
+	networking/udhcp/Config.in \
+	networking/Config.in \
+	modutils/Config.in \
+	miscutils/Config.in \
+	loginutils/Config.in \
+	init/Config.in \
+	findutils/Config.in \
+	editors/Config.in \
+	debianutils/Config.in \
+	console-tools/Config.in \
+	coreutils/Config.in \
+	archival/Config.in \
+	/home/dd-wrt/DEV/src/router/busybox/sysdeps/linux/Config.in
+
+.config include/config.h: $(deps_config)
+
+$(deps_config):
