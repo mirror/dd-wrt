35226e787df5a21eaac8da38730615e46b73f850
steve <USER@DOMAIN>
2006-03-06 01:31:19 +0000

    sleep moved
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@523 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/broadcom.c
+++ src/router/httpd/modules/broadcom.c
@@ -3928,7 +3928,7 @@ tf_upnp (webs_t wp)
       if (strcmp (v, "all") == 0)
 	{
 	  nvram_set ("upnp_clear", "1");
-	  sleep (3); //wait until upnp is done clearing before continuing
+	  sleep (1); 
 	}
       else
 	{
@@ -3939,6 +3939,7 @@ tf_upnp (webs_t wp)
 
   // firewall + upnp service is restarted after this
   return 0;
+  sleep (3); //wait until upnp is done clearing before continuing
 }
 
 //      <% tf_upnp(); %>
