354282fc8d08d041800ff1c22f4f7170c3e01fb1
BrainSlayer <s.gottschall@dd-wrt.com>
2006-03-20 23:28:01 +0000

    ipp2p update, p2p catchall
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@680 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/busybox/libbb/login.c
+++ src/router/busybox/libbb/login.c
@@ -20,7 +20,7 @@
  * Optimize and correcting OCRNL by Vladimir Oleynik <dzo@simtreas.ru>
  */
 
-#include <sys/param.h>  /* MAXHOSTNAMELEN */
+#include <sys/param.h>		/* MAXHOSTNAMELEN */
 #include <stdio.h>
 #include <unistd.h>
 #include "libbb.h"
@@ -33,96 +33,115 @@
 static const char fmtstr_d[] = "%A, %d %B %Y";
 static const char fmtstr_t[] = "%H:%M:%S";
 
-void print_login_issue(const char *issue_file, const char *tty)
+void
+print_login_issue (const char *issue_file, const char *tty)
 {
-	FILE *fd;
-	int c;
-	char buf[256];
-	const char *outbuf;
-	time_t t;
-	struct utsname uts;
+  FILE *fd;
+  int c;
+  char buf[256];
+  const char *outbuf;
+  time_t t;
+  struct utsname uts;
 
-	time(&t);
-	uname(&uts);
+  time (&t);
+  uname (&uts);
 
-	puts("\r");	/* start a new line */
+  puts ("\r");			/* start a new line */
 
-	if ((fd = fopen(issue_file, "r"))) {
-		while ((c = fgetc(fd)) != EOF) {
-			outbuf = buf;
-			buf[0] = c;
-			if(c == '\n') {
-				buf[1] = '\r';
-				buf[2] = 0;
-			} else {
-				buf[1] = 0;
-			}
-			if (c == '\\' || c == '%') {
-				c = fgetc(fd);
-				switch (c) {
-					case 's':
-						outbuf = uts.sysname;
-						break;
+  if ((fd = fopen (issue_file, "r")))
+    {
+      while ((c = fgetc (fd)) != EOF)
+	{
+	  outbuf = buf;
+	  buf[0] = c;
+	  if (c == '\n')
+	    {
+	      buf[1] = '\r';
+	      buf[2] = 0;
+	    }
+	  else
+	    {
+	      buf[1] = 0;
+	    }
+	  if (c == '\\' || c == '%')
+	    {
+	      c = fgetc (fd);
+	      switch (c)
+		{
+		case 's':
+		  outbuf = uts.sysname;
+		  break;
 
-					case 'n':
-						outbuf = uts.nodename;
-						break;
+		case 'n':
+		  outbuf = uts.nodename;
+		  break;
 
-					case 'r':
-						outbuf = uts.release;
-						break;
+		case 'r':
+		  outbuf = uts.release;
+		  break;
 
-					case 'v':
-						outbuf = uts.version;
-						break;
+		case 'v':
+		  outbuf = uts.version;
+		  break;
 
-					case 'm':
-						outbuf = uts.machine;
-						break;
+		case 'm':
+		  outbuf = uts.machine;
+		  break;
 
-					case 'D':
-					case 'o':
-						getdomainname(buf, sizeof(buf));
-						buf[sizeof(buf) - 1] = '\0';
-						break;
+		case 'D':
+		case 'o':
+		  getdomainname (buf, sizeof (buf));
+		  buf[sizeof (buf) - 1] = '\0';
+		  break;
 
-					case 'd':
-						strftime(buf, sizeof(buf), fmtstr_d, localtime(&t));
-						break;
+		case 'd':
+		  strftime (buf, sizeof (buf), fmtstr_d, localtime (&t));
+		  break;
 
-					case 't':
-						strftime(buf, sizeof(buf), fmtstr_t, localtime(&t));
-						break;
+		case 't':
+		  strftime (buf, sizeof (buf), fmtstr_t, localtime (&t));
+		  break;
 
-					case 'h':
-						gethostname(buf, sizeof(buf) - 1);
-						break;
+		case 'h':
+		  gethostname (buf, sizeof (buf) - 1);
+		  break;
 
-					case 'l':
-						outbuf = tty;
-						break;
+		case 'l':
+		  outbuf = tty;
+		  break;
 
-					default:
-						buf[0] = c;
-				}
+		default:
+		  buf[0] = c;
 		}
-			fputs(outbuf, stdout);
-		}
-
-		fclose(fd);
-
-		fflush(stdout);
+	    }
+	  fputs (outbuf, stdout);
 	}
+
+      fclose (fd);
+
+      fflush (stdout);
+    }
 }
 
-void print_login_prompt(void)
+void
+print_login_prompt (void)
 {
-	char buf[MAXHOSTNAMELEN+1];
 
-	gethostname(buf, MAXHOSTNAMELEN);
-	fputs(buf, stdout);
+/* Login Prompt by BrainSlayer */
+  FILE *fp = fopen ("/tmp/loginprompt", "rb");
+  if (fp)
+    {    
+      while (!feof (fp))
+	{
+	  putc (getc (fp), stdout);
+	}
+      fclose (fp);
+    }
+  char buf[MAXHOSTNAMELEN + 1];
 
-	fputs(LOGIN, stdout);
-	fflush(stdout);
+  gethostname (buf, MAXHOSTNAMELEN);
+  fputs (buf, stdout);
+
+  fputs (LOGIN, stdout);
+  fflush (stdout);
 }
-
