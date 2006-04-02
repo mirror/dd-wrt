b8f0857e7e6b3d33501962b7535a9228ae6dabbe
brainslayer <brainslayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-04-02 15:13:14 +0000

    micro edition prepared
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@912 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/defaults.c
+++ src/router/shared/defaults.c
@@ -1236,8 +1236,9 @@ struct nvram_tuple router_defaults[] = {
 
 #endif
 #ifdef HAVE_SPUTNIK_APD
- {"sputnik_mjid_type","0",0},
+ {"sputnik_mjid_type","2",0},
  {"sputnik_mjid","",0},
+ {"apd_enable","0",0},
 #endif
 #ifdef HAVE_CHILLILOCAL
   {"fon_usernames", "0", 0},
