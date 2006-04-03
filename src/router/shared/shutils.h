9175661ae4b2c01e1ab05af40c3245e088b809c9
brainslayer <brainslayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-04-03 19:43:12 +0000

    watchdog fixed
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@941 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/shutils.h
+++ src/router/shared/shutils.h
@@ -142,6 +142,7 @@ strcat_r (const char *s1, const char *s2, char *buf)
 })
 /* Return NUL instead of NULL if undefined */
 #define safe_getenv(s) (getenv(s) ? : "")
+
 #define HAVE_SILENCE 1
 
 /* Print directly to the console */
@@ -180,22 +181,6 @@ strcat_r (const char *s1, const char *s2, char *buf)
 
 /* Return NUL instead of NULL if undefined */
 #define safe_getenv(s) (getenv(s) ? : "")
-
-#define HAVE_SILENCE 1
-
-/* Print directly to the console */
-#ifndef HAVE_SILENCE
-
-#define cprintf(fmt, args...) do { \
-	FILE *fp = fopen("/dev/console", "w"); \
-	if (fp) { \
-		fprintf(fp, fmt, ## args); \
-		fclose(fp); \
-	} \
-} while (0)
-#else
-#define cprintf(fmt, args...)
-#endif
 
 
 /* Debug print */
