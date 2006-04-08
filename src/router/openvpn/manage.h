5f90ff40f89bd6a91f4d2a58cf322eadefb58381
BrainSlayer <s.gottschall@dd-wrt.com>
2006-04-08 19:16:16 +0000

    openvpn update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@1032 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/openvpn/manage.h
+++ src/router/openvpn/manage.h
@@ -281,6 +281,7 @@ void management_clear_callback (struct management *man);
 
 bool management_query_user_pass (struct management *man, struct user_pass *up, const char *type, const bool password_only);
 
+bool management_should_daemonize (struct management *man);
 bool management_would_hold (struct management *man);
 bool management_hold (struct management *man);
 
