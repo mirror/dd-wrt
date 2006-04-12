1953aefe44c4edda12d0ca2cf4b7efa40a20051b
brainslayer <brainslayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-04-12 20:01:59 +0000

    new wireless tools
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@1094 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/wireless-tools/iwmulticall.c
+++ src/router/wireless-tools/iwmulticall.c
@@ -61,21 +61,27 @@ extern int
 /* Get iwconfig in there. Mandatory. */
 #define main(args...) main_iwconfig(args)
 #define iw_usage(args...) iwconfig_usage(args)
+#define find_command(args...) iwconfig_find_command(args)
 #include "iwconfig.c"
+#undef find_command
 #undef iw_usage
 #undef main
 
 /* Get iwlist in there. Scanning support is pretty sweet. */
 #define main(args...) main_iwlist(args)
 #define iw_usage(args...) iwlist_usage(args)
+#define find_command(args...) iwlist_find_command(args)
 #include "iwlist.c"
+#undef find_command
 #undef iw_usage
 #undef main
 
+#ifndef WE_ESSENTIAL
 /* Get iwspy in there, it's not that big. */
 #define main(args...) main_iwspy(args)
 #include "iwspy.c"
 #undef main
+#endif	/* WE_ESSENTIAL */
 
 /* Get iwpriv in there. Mandatory for HostAP and some other drivers. */
 #define main(args...) main_iwpriv(args)
@@ -122,8 +128,10 @@ main(int	argc,
     return(main_iwconfig(argc, argv));
   if(!strcmp(call_name, "iwlist"))
     return(main_iwlist(argc, argv));
+#ifndef WE_ESSENTIAL
   if(!strcmp(call_name, "iwspy"))
     return(main_iwspy(argc, argv));
+#endif	/* WE_ESSENTIAL */
   if(!strcmp(call_name, "iwpriv"))
     return(main_iwpriv(argc, argv));
   if(!strcmp(call_name, "iwgetid"))
