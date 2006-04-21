791bdd899915d58bc6ec4bcdc25e0c98bf344073
botho <USER@DOMAIN>
2006-04-21 14:24:53 +0000

    declare ej_js_include
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@1268 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/broadcom.h
+++ src/router/shared/broadcom.h
@@ -343,6 +343,10 @@ extern int traceroute_onload (webs_t wp, char *arg);
 
 /* Added by Botho 03.April.06 */
 extern void ej_dumpip_conntrack (int eid, webs_t wp, int argc, char_t ** argv);
+/* Added by Botho 21.April.06 */
+extern int ej_js_include (int eid, webs_t wp, int argc, char_t ** argv);
+
+
 
 
 
