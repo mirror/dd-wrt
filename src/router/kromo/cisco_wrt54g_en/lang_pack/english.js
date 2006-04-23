2dfbc8c4903c5ada76968122bdc992dcd8554fab
botho <USER@DOMAIN>
2006-04-23 18:45:44 +0000

    Test new Reboot & Success pages
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@1344 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/cisco_wrt54g_en/lang_pack/english.js
+++ src/router/kromo/cisco_wrt54g_en/lang_pack/english.js
@@ -459,7 +459,7 @@ success.mess1="Settings saved.";
 success.mess2="Upgrade successful.<br/>Unit is rebooting now. Please wait a few moments...";
 success.mess3="Settings are successful.";
 success.mess4=success.mess3 + "<br />Unit is rebooting now. Please wait a few moments...";
-success.mess5="The router IP Address seems to be modified. Please change your computer IP address to join the network and then connect again.";
+success.mess5="If you have changed your router?s IP address, please note that you must release/renew your client(s) address(s) on the network before connect again";
 
 
 
