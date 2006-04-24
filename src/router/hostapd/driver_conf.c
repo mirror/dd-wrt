2d2333515d65e940db424b66840f4ba701365c40
brainslayer <brainslayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-04-24 17:05:28 +0000

    replaced
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@1353 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/hostapd/driver_conf.c
@@ -0,0 +1,11 @@
+/* THIS FILE AUTOMATICALLY GENERATED, DO NOT EDIT! */
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include "hostapd.h"
+#include "driver.h"
+void madwifi_driver_register(void);
+void register_drivers(void) {
+madwifi_driver_register();
+}
