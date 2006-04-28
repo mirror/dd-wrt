9f5c316387f8a575c0559714d7ca0ce925619916
BrainSlayer <s.gottschall@dd-wrt.com>
2006-04-28 22:07:12 +0000

    rb500 linux 2.6 port update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@1458 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/net-tools/config.h
+++ src/router/net-tools/config.h
@@ -34,7 +34,7 @@
 #define HAVE_AFAX25 0
 #define HAVE_AFNETROM 0
 #define HAVE_AFROSE 0
-#define HAVE_AFX25 1
+#define HAVE_AFX25 0
 #define HAVE_AFECONET 0
 #define HAVE_AFDECnet 0
 #define HAVE_AFASH 0
