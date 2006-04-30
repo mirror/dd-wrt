60f0469f2ee36cb428063040c7dec0a1b7e94df9
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-04-30 09:09:55 +0000

    bug 1144: Add more fields to Filters.asp for URLs and keywords
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@1493 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/filters.c
+++ src/router/httpd/modules/filters.c
@@ -730,7 +730,7 @@ validate_filter_web (webs_t wp, char *value, struct variable *v)
   char filter_url[] = "filter_web_urlXXX";
   D ("validate_filter_web");
   /* Handle Website Blocking by URL Address */
-  for (i = 0; i < 4; i++)
+  for (i = 0; i < 6; i++)
     {
       char filter_host[] = "urlXXX";
       char *host;
@@ -753,7 +753,7 @@ validate_filter_web (webs_t wp, char *value, struct variable *v)
   nvram_set (filter_host, buf);
 
   /* Handle Website Blocking by Keyword */
-  for (i = 0; i < 6; i++)
+  for (i = 0; i < 8; i++)
     {
       char filter_url[] = "urlXXX";
       char *url;
