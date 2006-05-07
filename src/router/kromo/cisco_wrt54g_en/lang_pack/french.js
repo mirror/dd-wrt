9f5d7e62fc7dd0790ab88c869b347b7571ac3600
botho <botho@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-05-07 13:24:08 +0000

    DDNS messages translation TEST
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@1670 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/cisco_wrt54g_en/lang_pack/french.js
+++ src/router/kromo/cisco_wrt54g_en/lang_pack/french.js
@@ -1,2 +1,7 @@
 
 //bmenu.setup="TEST ...";
+// DDNS message TEST
+var ddnsm = new Object();
+ddnsm.dyn_good="DDNS a ?t? mis ? jour avec succes";
+ddnsm.dyn_noupdate="Aucune mise ? jour n?cessaire ? ce moment";
+ddnsm.dyn_nochg="Adresse IP inchang?e depuis la derni?re mise ? jour";
