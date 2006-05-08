d57a4867d558ba8d235c41f81cc716627fce7d84
botho <USER@DOMAIN>
2006-05-08 09:35:43 +0000

    test sleep
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@1724 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/upgrade.c
+++ src/router/httpd/modules/upgrade.c
@@ -48,7 +48,7 @@ do_upgrade_cgi (char *url, webs_t stream)	//jimmy, https, 8/6/2003
   /* Reboot if successful */
   if (upgrade_ret == 0)
     {
-      sleep (4);
+      sleep (10);
       sys_reboot ();
     }
 #else
