ee64e222fd4144d9ba9856b9d27f9dd204334dfd
steve <steve@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-05-09 19:35:45 +0000

    dnsmasq test
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@1737 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/dnsmasq/src/lease.c
+++ src/router/dnsmasq/src/lease.c
@@ -296,8 +296,8 @@ lease_update_file (struct daemon *daemon, int always, time_t now)
 /* DD-WRT udhcpd lease file compatibility */
 fprintf(stderr,"update lease file\n");
 
-  if (file_dirty != no)
-    {
+//  if (file_dirty != no)
+//    {
       rewind (daemon->lease_stream);
       ftruncate (fileno (daemon->lease_stream), 0);
 fprintf(stderr,"rewinding\n");
@@ -342,6 +342,6 @@ fprintf(stderr,"sync()\n");
       file_dirty = no;
 /* DD-WRT end */
 
-    }
+//    }
 }
 
