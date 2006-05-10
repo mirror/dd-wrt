44f5275e99c51a37221fd908b6fd2a124860be49
eko <USER@DOMAIN>
2006-05-10 05:49:47 +0000

    typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@1745 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/network.c
+++ src/router/services/network.c
@@ -943,7 +943,7 @@ start_wan (int status)
     case ROUTER_BUFFALO_WBR54G:
     case ROUTER_BUFFALO_WZRRSG54:
     case ROUTER_MOTOROLA_V1:
-		if ((!strcmp (nvram_safe_get ("pppoe_wan_ifname"), ""))
+		if (!strcmp (nvram_safe_get ("pppoe_wan_ifname"), ""))
 		    pppoe_wan_ifname = "eth1";
       break;
     }
