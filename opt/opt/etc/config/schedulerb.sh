2484fb761f0784a217834b516c13cdaf16377e86
BrainSlayer <s.gottschall@dd-wrt.com>
2006-05-16 21:36:47 +0000

    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@1961 52c4871e-980c-0410-b1e0-e73912ce01f8

--- opt/opt/etc/config/schedulerb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-SCHEDULE_TIME=$(nvram get schedule_time)
-
-sleep $SCHEDULE_TIME ; reboot &
--- opt/opt/make_kernel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#cat ../src/linux/linux/arch/mips/brcm-boards/bcm947xx/compressed/piggy | ./loader-0.02/lzma e -si -so > ../src/router/mipsel-uclibc/vmlinuz
-#cat ../src/linux/linux/arch/mips/brcm-boards/bcm947xx/compressed/piggy | 
-#./loader-0.02/lzma e ../src/linux/linux/arch/mips/bcm947xx/compressed/piggy ../src/router/mipsel-uclibc/vmlinuz
-./loader-0.02/lzma e ../src/linux/linux.v23/arch/mips/bcm947xx/compressed/piggy vmlinuz
-./loader-0.02/lzma e piggymicro vmlinuzmicro
