4811ffa0422b474a87e7d209fcc803a79c8f176d
BrainSlayer <s.gottschall@dd-wrt.com>
2006-05-16 21:59:14 +0000

    never compiled it sylvian?
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@1964 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/dd-wrt.c
+++ src/router/httpd/modules/dd-wrt.c
@@ -3801,7 +3801,7 @@ ej_port_vlan_table (int eid, webs_t wp, int argc, char_t ** argv)
   websWrite (wp, "<tr height=\"5\"><td>&nbsp;</td></tr>\n");
 
   websWrite (wp, "<tr>\n");
-  websWrite (wp, "<td><script type="text/javascript">Capture(vlan.aggregation)</script></td>\n");
+  websWrite (wp, "<td><script type=\"text/javascript\">Capture(vlan.aggregation)</script></td>\n");
 
   websWrite (wp,
 	     "<td colspan=\"6\"><select name=\"trunking\"><script type=\"text/javascript\">document.write(\"<option value=\\\"0\\\">\" + share.no + \"</option>\");</script><script type=\"text/javascript\">document.write(\"<option value=\\\"1\\\"");
