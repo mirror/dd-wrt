255875db8a6f6cc5dc776b0d57cb6657714f3225
brainslayer <brainslayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-05-16 21:36:47 +0000

    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@1961 52c4871e-980c-0410-b1e0-e73912ce01f8

--- opt/opt/translate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-./transtool/translate $1 ../src/router/mipsel-uclibc/target/www/*.asp
-./transtool/translate $1 ../src/router/mipsel-uclibc/target/www/*.htm
-#./transtool/translate lang/$1.lang ../src/router/mipsel-uclibc/target/www/*.html
-
-
-
