74b871dac78dfbaa1c9fbf1ed876a5dfce68809d
steve <USER@DOMAIN>
2006-05-22 03:29:57 +0000

    latest dnsmasq merged, gui leasetable broken until i rewrite it
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@2048 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/dnsmasq/src/isc.c
+++ src/router/dnsmasq/src/isc.c
@@ -1,4 +1,4 @@
-/* dnsmasq is Copyright (c) 2000 - 2004 by Simon Kelley
+/* dnsmasq is Copyright (c) 2000 - 2005 by Simon Kelley
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -12,21 +12,12 @@
 
 
 /* Code in this file is based on contributions by John Volpe. */
-/* DHCP altered to process udhcp format by W.J. van der Laan 2003 */
-/* Binary lease file code copied from Wifi-box WRT54G firmware source code */
-/* Binary lease file code merged into this file by Rod Whitby */
 
 #include "dnsmasq.h"
 
 #ifdef HAVE_ISC_READER
 
-struct lease_t {
-	unsigned char chaddr[16];
-	u_int32_t yiaddr;
-	u_int32_t expires;
-	char hostname[64];
-};
-#define EXPIRES_NEVER 0xFFFFFFFF
+#define MAXTOK 50
 
 struct isc_lease {
   char *name, *fqdn;
@@ -36,115 +27,195 @@ struct isc_lease {
 };
 
 static struct isc_lease *leases = NULL;
+static off_t lease_file_size = (off_t)0;
+static ino_t lease_file_inode = (ino_t)0;
 static int logged_lease = 0;
 
-//void load_dhcp(char *file, char *suffix, time_t now, char *hostname)
-//{
-FILE *load_dhcp(struct daemon *daemon, time_t now)
+static int next_token (char *token, int buffsize, FILE * fp)
+{
+  int c, count = 0;
+  char *cp = token;
+  
+  while((c = getc(fp)) != EOF)
+    {
+      if (c == '#')
+	do { c = getc(fp); } while (c != '\n' && c != EOF);
+      
+      if (c == ' ' || c == '\t' || c == '\n' || c == ';')
+	{
+	  if (count)
+	    break;
+	}
+      else if ((c != '"') && (count<buffsize-1))
+	{
+	  *cp++ = c;
+	  count++;
+	}
+    }
+  
+  *cp = 0;
+  return count ? 1 : 0;
+}
+
+void load_dhcp(struct daemon *daemon, time_t now)
 {
   char *hostname = daemon->namebuff;
-  char *file = daemon->lease_file;
-  char *suffix = daemon->domain_suffix;
-  char *dot;
+  char token[MAXTOK], *dot;
   struct in_addr host_address;
-  time_t ttd;
+  time_t ttd, tts;
   FILE *fp;
-  struct lease_t binlease;
   struct isc_lease *lease, *tmp, **up;
   struct stat statbuf;
 
+  if (stat(daemon->lease_file, &statbuf) == -1)
+    {
+      if (!logged_lease)
+	syslog(LOG_WARNING, _("failed to access %s: %m"), daemon->lease_file);
+      logged_lease = 1;
+      return;
+    }
+  
   logged_lease = 0;
   
+  if ((statbuf.st_size <= lease_file_size) &&
+      (statbuf.st_ino == lease_file_inode))
+    return;
   
-//  if (!(fp = fopen (file, "r+b")))
-//    {
-fprintf(stderr,"opening %s\n",file);
-      if (!(fp = fopen (file, "wb")))
-      {
-fprintf(stderr,"error while opening %s\n",file);
-      syslog (LOG_ERR, "failed to load %s: %m", file);
-      return NULL;
-      }
-//    }
-fprintf(stderr,"done()\n");
+  lease_file_size = statbuf.st_size;
+  lease_file_inode = statbuf.st_ino;
   
-  syslog (LOG_INFO, "reading %s", file);
-
-  while (fread(&binlease, sizeof(binlease), 1, fp))
+  if (!(fp = fopen (daemon->lease_file, "r")))
     {
-      /* Skip empty hostnames */
-      if(!binlease.hostname[0])
-	continue;
-  	
-      strncpy(hostname, binlease.hostname, MAXDNAME);
-      hostname[MAXDNAME-1] = 0;
-  	
-      if (!canonicalise(hostname))
-	{
-	  *hostname = 0;
-	  syslog(LOG_ERR, "bad name in %s", file); 
-	}
-      
-      if ((dot = strchr(hostname, '.')))
-	{ 
-	  if (!suffix || hostname_isequal(dot+1, suffix))
-	    {
-	      syslog(LOG_WARNING, 
-		     "Ignoring DHCP lease for %s because it has an illegal domain part", 
-		     hostname);
-	      continue;
-	    }
-	  *dot = 0;
-	}
+      syslog (LOG_ERR, _("failed to load %s: %m"), daemon->lease_file);
+      return;
+    }
+  
+  syslog (LOG_INFO, _("reading %s"), daemon->lease_file);
 
-      /* Address */
-      host_address.s_addr = binlease.yiaddr;
+  while ((next_token(token, MAXTOK, fp)))
+    {
+      if (strcmp(token, "lease") == 0)
+        {
+          hostname[0] = '\0';
+	  ttd = tts = (time_t)(-1);
+	  if (next_token(token, MAXTOK, fp) && 
+	      (host_address.s_addr = inet_addr(token)) != (in_addr_t) -1)
+            {
+              if (next_token(token, MAXTOK, fp) && *token == '{')
+                {
+                  while (next_token(token, MAXTOK, fp) && *token != '}')
+                    {
+                      if ((strcmp(token, "client-hostname") == 0) ||
+			  (strcmp(token, "hostname") == 0))
+			{
+			  if (next_token(hostname, MAXDNAME, fp))
+			    if (!canonicalise(hostname))
+			      {
+				*hostname = 0;
+				syslog(LOG_ERR, _("bad name in %s"), daemon->lease_file); 
+			      }
+			}
+                      else if ((strcmp(token, "ends") == 0) ||
+			       (strcmp(token, "starts") == 0))
+                        {
+                          struct tm lease_time;
+			  int is_ends = (strcmp(token, "ends") == 0);
+			  if (next_token(token, MAXTOK, fp) &&  /* skip weekday */
+			      next_token(token, MAXTOK, fp) &&  /* Get date from lease file */
+			      sscanf (token, "%d/%d/%d", 
+				      &lease_time.tm_year,
+				      &lease_time.tm_mon,
+				      &lease_time.tm_mday) == 3 &&
+			      next_token(token, MAXTOK, fp) &&
+			      sscanf (token, "%d:%d:%d:", 
+				      &lease_time.tm_hour,
+				      &lease_time.tm_min, 
+				      &lease_time.tm_sec) == 3)
+			    {
+			      /* There doesn't seem to be a universally available library function
+				 which converts broken-down _GMT_ time to seconds-in-epoch.
+				 The following was borrowed from ISC dhcpd sources, where
+                                 it is noted that it might not be entirely accurate for odd seconds.
+				 Since we're trying to get the same answer as dhcpd, that's just
+				 fine here. */
+			      static const int months [11] = { 31, 59, 90, 120, 151, 181,
+							       212, 243, 273, 304, 334 };
+			      time_t time = ((((((365 * (lease_time.tm_year - 1970) + /* Days in years since '70 */
+						  (lease_time.tm_year - 1969) / 4 +   /* Leap days since '70 */
+						  (lease_time.tm_mon > 1                /* Days in months this year */
+						   ? months [lease_time.tm_mon - 2]
+						   : 0) +
+						  (lease_time.tm_mon > 2 &&         /* Leap day this year */
+						   !((lease_time.tm_year - 1972) & 3)) +
+						  lease_time.tm_mday - 1) * 24) +   /* Day of month */
+						lease_time.tm_hour) * 60) +
+					      lease_time.tm_min) * 60) + lease_time.tm_sec;
+			      if (is_ends)
+				ttd = time;
+			      else
+				tts = time;			    }
+                        }
+		    }
+		  
+		  /* missing info? */
+		  if (!*hostname)
+		    continue;
+		  if (ttd == (time_t)(-1))
+		    continue;
+		  
+		  /* We use 0 as infinite in ttd */
+		  if ((tts != -1) && (ttd == tts - 1))
+		    ttd = (time_t)0;
+		  else if (difftime(now, ttd) > 0)
+		    continue;
 
-      /* Lease */
-      binlease.expires = ntohl(binlease.expires);
+		  if ((dot = strchr(hostname, '.')))
+		    { 
+		      if (!daemon->domain_suffix || hostname_isequal(dot+1, daemon->domain_suffix))
+			{
+			  syslog(LOG_WARNING, 
+				 _("Ignoring DHCP lease for %s because it has an illegal domain part"), 
+				 hostname);
+			  continue;
+			}
+		      *dot = 0;
+		    }
 
-      /* Calculate time to death */
-      ttd = (time_t)(0); /* infinite */
-      if (binlease.expires != EXPIRES_NEVER) {
-	/* WRT54G uses time remaining */
-	ttd = now + binlease.expires;
-	if (ttd < now)
-	  /* expired already */
-	  continue;
-      }
-      
-      syslog(LOG_INFO, "found lease for %s", hostname); 
+		  for (lease = leases; lease; lease = lease->next)
+		    if (hostname_isequal(lease->name, hostname))
+		      {
+			lease->expires = ttd;
+			lease->addr = host_address;
+			break;
+		      }
 
-      for (lease = leases; lease; lease = lease->next)
-	if (hostname_isequal(lease->name, hostname))
-	  {
-	    lease->expires = ttd;
-	    lease->addr = host_address;
-	    break;
-	  }
-      
-      if (!lease && (lease = malloc(sizeof(struct isc_lease))))
-	{
-	  lease->expires = ttd;
-	  lease->addr = host_address;
-	  lease->fqdn =  NULL;
-	  lease->next = leases;
-	  if (!(lease->name = malloc(strlen(hostname)+1)))
-	    free(lease);
-	  else
-	    {
-	      leases = lease;
-	      strcpy(lease->name, hostname);
-	      if (suffix && (lease->fqdn = malloc(strlen(hostname) + strlen(suffix) + 2)))
-		{
-		  strcpy(lease->fqdn, hostname);
-		  strcat(lease->fqdn, ".");
-		  strcat(lease->fqdn, suffix);
+		  if (!lease && (lease = malloc(sizeof(struct isc_lease))))
+		    {
+		      lease->expires = ttd;
+		      lease->addr = host_address;
+		      lease->fqdn =  NULL;
+		      lease->next = leases;
+		      if (!(lease->name = malloc(strlen(hostname)+1)))
+			free(lease);
+		      else
+			{
+			  leases = lease;
+			  strcpy(lease->name, hostname);
+			  if (daemon->domain_suffix && 
+			      (lease->fqdn = malloc(strlen(hostname) + strlen(daemon->domain_suffix) + 2)))
+			    {
+			      strcpy(lease->fqdn, hostname);
+			      strcat(lease->fqdn, ".");
+			      strcat(lease->fqdn, daemon->domain_suffix);
+			    }
+			}
+		    }
 		}
 	    }
 	}
     }
 
+  fclose(fp);
   
   /* prune expired leases */
   for (lease = leases, up = &leases; lease; lease = tmp)
@@ -152,7 +223,6 @@ fprintf(stderr,"done()\n");
        tmp = lease->next;
        if (lease->expires != (time_t)0 && difftime(now, lease->expires) > 0)
 	 {
-	   syslog(LOG_INFO, "expired lease for %s", lease->name); 
 	   *up = lease->next; /* unlink */
 	   free(lease->name);
 	   if (lease->fqdn)
@@ -169,18 +239,9 @@ fprintf(stderr,"done()\n");
 
   for (lease = leases; lease; lease = lease->next)
     {
-      if (lease->fqdn)
-	{
-	  cache_add_dhcp_entry(daemon,lease->fqdn, &lease->addr, lease->expires);
-	  cache_add_dhcp_entry(daemon,lease->name, &lease->addr, lease->expires);
-	}
-      else 
-	cache_add_dhcp_entry(daemon,lease->name, &lease->addr, lease->expires);
-
-      syslog(LOG_INFO, "stored lease for %s", lease->name); 
-
+      cache_add_dhcp_entry(daemon, lease->fqdn, &lease->addr, lease->expires);
+      cache_add_dhcp_entry(daemon, lease->name, &lease->addr, lease->expires);
     }
-return fp;
 }
 
 #endif
--- src/router/dnsmasq/src/netlink.c
+++ src/router/dnsmasq/src/netlink.c
@@ -1,4 +1,4 @@
-/* dnsmasq is Copyright (c) 2000-2006 Simon Kelley
+ /* dnsmasq is Copyright (c) 2000-2006 Simon Kelley
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -12,36 +12,52 @@
 
 #include "dnsmasq.h"
 
-#ifdef HAVE_RTNETLINK
+#ifdef HAVE_LINUX_NETWORK
 
 #include <linux/types.h>
 #include <linux/netlink.h>
 #include <linux/rtnetlink.h>
 
-static struct iovec iov[1];
+static struct iovec iov;
+
+static void nl_err(struct nlmsghdr *h);
+static void nl_routechange(struct daemon *daemon, struct nlmsghdr *h);
 
 void netlink_init(struct daemon *daemon)
 {
   struct sockaddr_nl addr;
-  daemon->netlinkfd = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
-  
-  if (daemon->netlinkfd < 0)
-    return; /* no kernel support */
 
   addr.nl_family = AF_NETLINK;
   addr.nl_pad = 0;
-  addr.nl_pid = getpid();
+  addr.nl_pid = 0; /* autobind */
 #ifdef HAVE_IPV6
-  addr.nl_groups = RTMGRP_IPV4_IFADDR | RTMGRP_IPV6_IFADDR | RTMGRP_IPV4_ROUTE | RTMGRP_IPV6_ROUTE;
+  addr.nl_groups = RTMGRP_IPV4_ROUTE | RTMGRP_IPV6_ROUTE;
 #else
-  addr.nl_groups = RTMGRP_IPV4_IFADDR | RTMGRP_IPV4_ROUTE;
+  addr.nl_groups = RTMGRP_IPV4_ROUTE;
 #endif
   
-  if (bind(daemon->netlinkfd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-    die2(_("cannot bind netlink socket: %s"), NULL);
+  /* May not be able to have permission to set multicast groups don't die in that case */
+  if ((daemon->netlinkfd = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE)) != -1)
+    {
+      if (bind(daemon->netlinkfd, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+	{
+	  addr.nl_groups = 0;
+	  if (errno != EPERM || bind(daemon->netlinkfd, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+	    daemon->netlinkfd = -1;
+	}
+    }
+  
+  if (daemon->netlinkfd == -1)
+    die2(_("cannot create netlink socket: %s"), NULL);
+  else
+    {
+      int flags = fcntl(daemon->netlinkfd, F_GETFD);
+      if (flags != -1)
+	fcntl(daemon->netlinkfd, F_SETFD, flags | FD_CLOEXEC); 
+    }
 
-  iov[0].iov_len = 200;
-  iov[0].iov_base = safe_malloc(iov[0].iov_len);
+  iov.iov_len = 200;
+  iov.iov_base = safe_malloc(iov.iov_len);
 }
 
 static ssize_t netlink_recv(struct daemon *daemon)
@@ -51,162 +67,179 @@ static ssize_t netlink_recv(struct daemon *daemon)
 
   msg.msg_control = NULL;
   msg.msg_controllen = 0;
-  msg.msg_flags = 0;
   msg.msg_name = NULL;
   msg.msg_namelen = 0;
-  msg.msg_iov = iov;
+  msg.msg_iov = &iov;
   msg.msg_iovlen = 1;
     
- retry:
-  while ((rc = recvmsg(daemon->netlinkfd, &msg, MSG_PEEK)) == -1 && retry_send());
-  
-  if (rc == -1)
-    return -1;
-
-  if (msg.msg_flags & MSG_TRUNC)
+  while (1)
     {
-      size_t newsz = iov[0].iov_len + 100;
-      void *new = realloc(iov[0].iov_base, newsz);
-      if (!new)
+      msg.msg_flags = 0;
+      while ((rc = recvmsg(daemon->netlinkfd, &msg, MSG_PEEK)) == -1 && errno == EINTR);
+      
+      /* 2.2.x doesn't suport MSG_PEEK at all, returning EOPNOTSUPP, so we just grab a 
+	 big buffer and pray in that case. */
+      if (rc == -1 && errno == EOPNOTSUPP)
+	{
+	  if (!expand_buf(&iov, 2000))
+	    return -1;
+	  break;
+	}
+      
+      if (rc == -1 || !(msg.msg_flags & MSG_TRUNC))
+        break;
+            
+      if (!expand_buf(&iov, iov.iov_len + 100))
 	return -1;
-      iov[0].iov_len = newsz;
-      iov[0].iov_base = new;
-      goto retry;
     }
-  
-  while ((rc = recvmsg(daemon->netlinkfd, &msg, 0)) == -1 && retry_send());
+
+  /* finally, read it for real */
+  while ((rc = recvmsg(daemon->netlinkfd, &msg, 0)) == -1 && errno == EINTR);
   
   return rc;
 }
   
-int netlink_process(struct daemon *daemon, int index, struct in_addr relay, 
-		    struct in_addr primary, struct dhcp_context **retp)
+int iface_enumerate(struct daemon *daemon, void *parm, int (*ipv4_callback)(), int (*ipv6_callback)())
 {
   struct sockaddr_nl addr;
   struct nlmsghdr *h;
-  int found_primary = 0;
   ssize_t len;
-  struct dhcp_context *ret = NULL;
   static unsigned int seq = 0;
+  int family = AF_INET;
 
   struct {
     struct nlmsghdr nlh;
     struct rtgenmsg g; 
   } req;
 
-  if (daemon->netlinkfd == -1)
-    return 0;
-
   addr.nl_family = AF_NETLINK;
   addr.nl_pad = 0;
   addr.nl_groups = 0;
   addr.nl_pid = 0; /* address to kernel */
 
+ again:
   req.nlh.nlmsg_len = sizeof(req);
   req.nlh.nlmsg_type = RTM_GETADDR;
-  req.nlh.nlmsg_flags = NLM_F_ROOT | NLM_F_MATCH | NLM_F_REQUEST;
+  req.nlh.nlmsg_flags = NLM_F_ROOT | NLM_F_MATCH | NLM_F_REQUEST | NLM_F_ACK; 
   req.nlh.nlmsg_pid = 0;
   req.nlh.nlmsg_seq = ++seq;
-  req.g.rtgen_family = AF_INET; 
+  req.g.rtgen_family = family; 
 
   /* Don't block in recvfrom if send fails */
   while((len = sendto(daemon->netlinkfd, (void *)&req, sizeof(req), 0, 
 		      (struct sockaddr *)&addr, sizeof(addr))) == -1 && retry_send());
-
-  if (len == -1)
-    {
-      /* if RTnetlink not configured in the kernel, don't keep trying. */
-      if (errno == ECONNREFUSED)
-	{
-	  close(daemon->netlinkfd);
-	  daemon->netlinkfd = -1;
-	}
-      return 0;
-    }
-
- get_next:
-  if ((len = netlink_recv(daemon)) == -1)
-    return 0;
   
-  for (h = (struct nlmsghdr *)iov[0].iov_base; NLMSG_OK(h, (unsigned int)len); h = NLMSG_NEXT(h, len))
+  if (len == -1)
+    return 0;
+    
+  while (1)
     {
-      if (h->nlmsg_seq != seq)
-	goto get_next;
-	
-      if (h->nlmsg_type == NLMSG_DONE)
-	break;
-
-      if (h->nlmsg_type == NLMSG_ERROR)
+      if ((len = netlink_recv(daemon)) == -1)
 	return 0;
-      
-      if (h->nlmsg_type == RTM_NEWADDR)
-	{
-	  struct ifaddrmsg *ifa = NLMSG_DATA(h);  
-	  if (ifa->ifa_index == index && ifa->ifa_family == AF_INET)
-	    {
-	      struct rtattr *rta = IFA_RTA(ifa);
-	      unsigned int len1 = h->nlmsg_len - NLMSG_LENGTH(sizeof(*ifa));
-	      struct in_addr netmask, addr, broadcast;
-	      
-	      netmask.s_addr = htonl(0xffffffff << (32 - ifa->ifa_prefixlen));
-	      addr.s_addr = 0;
-	      broadcast.s_addr = 0;
-	      
-	      while (RTA_OK(rta, len1))
-		{
-		  if (rta->rta_type == IFA_LOCAL)
-		    addr = *((struct in_addr *)(rta+1));
-		  else if (rta->rta_type == IFA_BROADCAST)
-		    broadcast = *((struct in_addr *)(rta+1));
-		  
-		  rta = RTA_NEXT(rta, len1);
-		}
-	      
-	      if (addr.s_addr)
-		{
-		  ret = complete_context(daemon, addr, ret, netmask, broadcast, relay, primary);	
-		  if (addr.s_addr == primary.s_addr)
-		    found_primary = 1;
-		}
-	    }
-	}
+  
+      for (h = (struct nlmsghdr *)iov.iov_base; NLMSG_OK(h, (size_t)len); h = NLMSG_NEXT(h, len))
+ 	if (h->nlmsg_type == NLMSG_ERROR)
+	  nl_err(h);
+	else if (h->nlmsg_seq != seq)
+	  nl_routechange(daemon, h); /* May be multicast arriving async */
+	else if (h->nlmsg_type == NLMSG_DONE)
+	  {
+#ifdef HAVE_IPV6
+	    if (family == AF_INET && ipv6_callback)
+	      {
+		family = AF_INET6;
+		goto again;
+	      }
+#endif
+	    return 1;
+	  }
+	else if (h->nlmsg_type == RTM_NEWADDR)
+	  {
+	    struct ifaddrmsg *ifa = NLMSG_DATA(h);  
+	    struct rtattr *rta = IFA_RTA(ifa);
+	    unsigned int len1 = h->nlmsg_len - NLMSG_LENGTH(sizeof(*ifa));
+	    
+	    if (ifa->ifa_family == AF_INET)
+	      {
+		struct in_addr netmask, addr, broadcast;
+		
+		netmask.s_addr = htonl(0xffffffff << (32 - ifa->ifa_prefixlen));
+		addr.s_addr = 0;
+		broadcast.s_addr = 0;
+		
+		while (RTA_OK(rta, len1))
+		  {
+		    if (rta->rta_type == IFA_LOCAL)
+		      addr = *((struct in_addr *)(rta+1));
+		    else if (rta->rta_type == IFA_BROADCAST)
+		      broadcast = *((struct in_addr *)(rta+1));
+		    
+		    rta = RTA_NEXT(rta, len1);
+		  }
+		
+		if (addr.s_addr && ipv4_callback)
+		  if (!((*ipv4_callback)(daemon, addr, ifa->ifa_index, netmask, broadcast, parm)))
+		    return 0;
+	      }
+#ifdef HAVE_IPV6
+	    else if (ifa->ifa_family == AF_INET6)
+	      {
+		struct in6_addr *addrp = NULL;
+		while (RTA_OK(rta, len1))
+		  {
+		    if (rta->rta_type == IFA_ADDRESS)
+		      addrp = ((struct in6_addr *)(rta+1)); 
+		    
+		    rta = RTA_NEXT(rta, len1);
+		  }
+		
+		if (addrp && ipv6_callback)
+		  if (!((*ipv6_callback)(daemon, addrp, ifa->ifa_index, ifa->ifa_index, parm)))
+		    return 0;
+	      }
+#endif
+	  }
     }
-
-  *retp = ret;
-  return found_primary;
 }
 
-
-/* We arrange to receive netlink multicast messages whenever the network config changes.
-   If this happens and we still have a DNS packet in the buffer, we re-send it.
-   This helps on DoD links, where frequently the packet which triggers dialling is
-   a DNS query, which then gets lost. By re-sending, we can avoid the lookup
-   failing. */ 
 void netlink_multicast(struct daemon *daemon)
 {
   ssize_t len;
   struct nlmsghdr *h;
   
-  if ((len = netlink_recv(daemon)) == -1)
-    return;
-  
-  if (!daemon->srv_save)
-    return;
-
-  for (h = (struct nlmsghdr *)iov[0].iov_base; NLMSG_OK(h, (unsigned int)len); h = NLMSG_NEXT(h, len))
+  if ((len = netlink_recv(daemon)) != -1)
     {
-      if (h->nlmsg_type == NLMSG_DONE || h->nlmsg_type == NLMSG_ERROR)
-	break;
-      
-      if (h->nlmsg_type == RTM_NEWADDR || h->nlmsg_type == RTM_NEWROUTE) 
-	{
-	  while(sendto(daemon->srv_save->sfd->fd, daemon->packet, daemon->packet_len, 0,
-		       &daemon->srv_save->addr.sa, sa_len(&daemon->srv_save->addr)) == -1 && retry_send()); 
-	  break;
-	}
+      for (h = (struct nlmsghdr *)iov.iov_base; NLMSG_OK(h, (size_t)len); h = NLMSG_NEXT(h, len))
+	if (h->nlmsg_type == NLMSG_ERROR)
+	  nl_err(h);
+	else
+	  nl_routechange(daemon, h);
     }
 }
 
+static void nl_err(struct nlmsghdr *h)
+{
+  struct nlmsgerr *err = NLMSG_DATA(h);
+  if (err->error != 0)
+    syslog(LOG_ERR, _("netlink returns error: %s"), strerror(-(err->error)));
+}
+
+/* We arrange to receive netlink multicast messages whenever the network route is added.
+   If this happens and we still have a DNS packet in the buffer, we re-send it.
+   This helps on DoD links, where frequently the packet which triggers dialling is
+   a DNS query, which then gets lost. By re-sending, we can avoid the lookup
+   failing. */ 
+static void nl_routechange(struct daemon *daemon, struct nlmsghdr *h)
+{
+  if (h->nlmsg_type == RTM_NEWROUTE && daemon->srv_save)
+    {
+      struct rtmsg *rtm = NLMSG_DATA(h);
+      if (rtm->rtm_type == RTN_UNICAST &&
+	  rtm->rtm_scope == RT_SCOPE_LINK) 
+	while(sendto(daemon->srv_save->sfd->fd, daemon->packet, daemon->packet_len, 0,
+		     &daemon->srv_save->addr.sa, sa_len(&daemon->srv_save->addr)) == -1 && retry_send()); 
+    }
+}
 #endif
 
       
