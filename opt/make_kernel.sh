a9451eb0b0ec58dabc358a8a54ab0d58cf2b9013
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-05-24 00:23:57 +0000

    refactoring
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@2133 52c4871e-980c-0410-b1e0-e73912ce01f8

--- opt/make_kernel.sh
+++ opt/make_kernel.sh
@@ -3,5 +3,5 @@
 #cat ../src/linux/linux/arch/mips/brcm-boards/bcm947xx/compressed/piggy | ./loader-0.02/lzma e -si -so > ../src/router/mipsel-uclibc/vmlinuz
 #cat ../src/linux/linux/arch/mips/brcm-boards/bcm947xx/compressed/piggy | 
 #./loader-0.02/lzma e ../src/linux/linux/arch/mips/bcm947xx/compressed/piggy ../src/router/mipsel-uclibc/vmlinuz
-./loader-0.02/lzma e ../src/linux/linux.v23/arch/mips/bcm947xx/compressed/piggy vmlinuz
+./loader-0.02/lzma e ../src/linux/brcm/linux.v23/arch/mips/bcm947xx/compressed/piggy vmlinuz
 ./loader-0.02/lzma e piggymicro vmlinuzmicro
