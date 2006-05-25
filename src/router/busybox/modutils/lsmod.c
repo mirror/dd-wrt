590b3b02a18af58c3a45256766ee144de319ebb7
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-05-25 17:05:57 +0000

    madwifi updates
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@2208 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/busybox/modutils/lsmod.c
+++ src/router/busybox/modutils/lsmod.c
@@ -164,11 +164,44 @@ extern int lsmod_main(int argc, char **argv)
 {
 	printf("Module                  Size  Used by");
 	check_tainted();
+//#if defined(CONFIG_FEATURE_LSMOD_PRETTY_2_6_OUTPUT)
+	{
+	  FILE *file;
+	  char line[4096];
 
-	if (bb_xprint_file_by_name("/proc/modules") < 0) {
-		return 0;
+	  file = bb_xfopen("/proc/modules", "r");
+
+	  while (fgets(line, sizeof(line), file)) {
+	    char *tok;
+
+	    tok = strtok(line, " \t");
+	    printf("%-19s", tok);
+	    tok = strtok(NULL, " \t\n");
+	    printf(" %8s", tok);
+	    tok = strtok(NULL, " \t\n");
+	    /* Null if no module unloading support. */
+	    if (tok) {
+	      printf("  %s", tok);
+	      tok = strtok(NULL, "\n");
+	      if (!tok)
+		tok = "";
+	      /* New-style has commas, or -.  If so,
+		 truncate (other fields might follow). */
+	      else if (strchr(tok, ',')) {
+		tok = strtok(tok, "\t ");
+		/* Strip trailing comma. */
+		if (tok[strlen(tok)-1] == ',')
+		  tok[strlen(tok)-1] = '\0';
+	      } else if (tok[0] == '-'
+			 && (tok[1] == '\0' || isspace(tok[1])))
+		tok = "";
+	      printf(" %s", tok);
+	    }
+	    printf("\n");
+	  }
+	  fclose(file);
 	}
-	return 1;
+	return 0;  /* Success  */
 }
 
 #endif /* CONFIG_FEATURE_QUERY_MODULE_INTERFACE */
