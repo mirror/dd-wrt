43152b25e9c84ce60bd3836eb9f3bf672ff3a54c
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-05-30 23:23:56 +0000

    Atheros support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@2326 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/radauth/wrt-radauth.c
+++ src/router/radauth/wrt-radauth.c
@@ -27,12 +27,8 @@
 #include <time.h>
 #include <unistd.h>
 #include "radius.h"
-#include <wlutils.h>
-#include <wlioctl.h>
-#include <bcmnvram.h>
 
 #define WAIT	300	/* Seconds until a STA expires */
-
 
 
 struct  sta {
@@ -47,6 +43,210 @@ char *server, *secret;
 short port;
 short mackey;
 short macfmt;
+
+
+/* Broadcom */
+#ifndef HAVE_MADWIFI
+#include <wlutils.h>
+#include <wlioctl.h>
+
+#include <bcmnvram.h>
+static void set_maclist(char *iface,char *buf)
+{
+	memset(buf,0,sizeof(buf));
+	wl_ioctl(iface, WLC_SET_MACLIST, buf, sizeof(buf));
+}
+static void security_disable(char *iface)
+{
+	int val;
+	val = WLC_MACMODE_DISABLED;
+	wl_ioctl(iface,WLC_SET_MACMODE, &val, sizeof(val));
+}
+static void security_deny(char *iface)
+{
+	int val;
+	val = WLC_MACMODE_DENY;
+	wl_ioctl(iface,WLC_SET_MACMODE, &val, sizeof(val));
+}
+static void kick_mac(char *iface,char *mac)
+{
+	wl_ioctl(iface, 0x8f, mac, 6);	/* Kick station off AP */
+}
+#else
+#include <sys/types.h>
+#include <sys/file.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <net/ethernet.h>
+#include <bcmnvram.h>
+
+#include "wireless.h"
+#include "net80211/ieee80211.h"
+#include "net80211/ieee80211_crypto.h"
+#include "net80211/ieee80211_ioctl.h"
+#define WLC_IOCTL_MAXLEN	8192
+
+
+/* Atheros */
+
+
+static int
+getsocket (void)
+{
+  static int s = -1;
+
+  if (s < 0)
+    {
+      s = socket (AF_INET, SOCK_DGRAM, 0);
+      if (s < 0)
+	err (1, "socket(SOCK_DGRAM)");
+    }
+  return s;
+}
+
+#define IOCTL_ERR(x) [x - SIOCIWFIRSTPRIV] "ioctl[" #x "]"
+static int
+set80211priv (struct iwreq *iwr, const char *ifname, int op, void *data,
+	     size_t len)
+{
+#define	N(a)	(sizeof(a)/sizeof(a[0]))
+
+  memset (iwr, 0, sizeof (struct iwreq));
+  strncpy (iwr->ifr_name, ifname, IFNAMSIZ);
+  if (len < IFNAMSIZ)
+    {
+      /*
+       * Argument data fits inline; put it there.
+       */
+      memcpy (iwr->u.name, data, len);
+    }
+  else
+    {
+      /*
+       * Argument data too big for inline transfer; setup a
+       * parameter block instead; the kernel will transfer
+       * the data for the driver.
+       */
+      iwr->u.data.pointer = data;
+      iwr->u.data.length = len;
+    }
+
+  if (ioctl (getsocket (), op, iwr) < 0)
+    {
+      static const char *opnames[] = {
+	IOCTL_ERR (IEEE80211_IOCTL_SETPARAM),
+	IOCTL_ERR (IEEE80211_IOCTL_GETPARAM),
+	IOCTL_ERR (IEEE80211_IOCTL_SETMODE),
+	IOCTL_ERR (IEEE80211_IOCTL_GETMODE),
+	IOCTL_ERR (IEEE80211_IOCTL_SETWMMPARAMS),
+	IOCTL_ERR (IEEE80211_IOCTL_GETWMMPARAMS),
+	IOCTL_ERR (IEEE80211_IOCTL_SETCHANLIST),
+	IOCTL_ERR (IEEE80211_IOCTL_GETCHANLIST),
+	IOCTL_ERR (IEEE80211_IOCTL_CHANSWITCH),
+	IOCTL_ERR (IEEE80211_IOCTL_GETCHANINFO),
+	IOCTL_ERR (IEEE80211_IOCTL_SETOPTIE),
+	IOCTL_ERR (IEEE80211_IOCTL_GETOPTIE),
+	IOCTL_ERR (IEEE80211_IOCTL_SETMLME),
+	IOCTL_ERR (IEEE80211_IOCTL_SETKEY),
+	IOCTL_ERR (IEEE80211_IOCTL_DELKEY),
+	IOCTL_ERR (IEEE80211_IOCTL_ADDMAC),
+	IOCTL_ERR (IEEE80211_IOCTL_DELMAC),
+	IOCTL_ERR (IEEE80211_IOCTL_WDSADDMAC),
+	IOCTL_ERR (IEEE80211_IOCTL_WDSDELMAC),
+      };
+      op -= SIOCIWFIRSTPRIV;
+      if (0 <= op && op < N (opnames))
+	perror (opnames[op]);
+      else
+	perror ("ioctl[unknown???]");
+      return -1;
+    }
+  return 0;
+#undef N
+}
+
+static int
+do80211priv (const char *ifname, int op, void *data, size_t len)
+{
+  struct iwreq iwr;
+
+  if (set80211priv (&iwr, ifname, op, data, len) < 0)
+    return -1;
+  if (len < IFNAMSIZ)
+    memcpy (data, iwr.u.name, len);
+  return iwr.u.data.length;
+}
+
+static int
+set80211param(char *iface, int op, int arg)
+{
+	struct iwreq iwr;
+
+	memset(&iwr, 0, sizeof(iwr));
+	strncpy(iwr.ifr_name, iface, IFNAMSIZ);
+	iwr.u.mode = op;
+	memcpy(iwr.u.name+sizeof(__u32), &arg, sizeof(arg));
+
+	if (ioctl(getsocket(), IEEE80211_IOCTL_SETPARAM, &iwr) < 0) {
+		perror("ioctl[IEEE80211_IOCTL_SETPARAM]");
+		return -1;
+	}
+	return 0;
+}
+
+
+
+struct maclist {
+	uint count;			/* number of MAC addresses */
+	struct ether_addr ea[1];	/* variable length array of MAC addresses */
+};
+
+static void set_maclist(char *iface,char *buf)
+{
+	struct ieee80211req_mlme mlme;
+	struct maclist *maclist = (struct maclist *) buf;
+
+//	if (authorized)
+//		mlme.im_op = IEEE80211_MLME_AUTHORIZE;
+//	else
+	uint i;
+	for (i=0;i<maclist->count;i++)
+	    {
+		mlme.im_op = IEEE80211_MLME_UNAUTHORIZE;
+		mlme.im_reason = 0;
+		memcpy(mlme.im_macaddr, &maclist->ea[i], IEEE80211_ADDR_LEN);
+		do80211priv(iface, IEEE80211_IOCTL_SETMLME, &mlme,sizeof(mlme));
+	    }
+}
+static void security_disable(char *iface)
+{
+#ifdef DEBUG
+	printf("Security Disable\n");
+#endif
+
+    set80211param(iface,IEEE80211_PARAM_MACCMD,IEEE80211_MACCMD_FLUSH);
+    set80211param(iface,IEEE80211_PARAM_MACCMD,IEEE80211_MACCMD_POLICY_OPEN);
+    
+}
+static void security_deny(char *iface)
+{
+#ifdef DEBUG
+	printf("Policy Deny\n");
+#endif
+    set80211param(iface,IEEE80211_PARAM_MACCMD,IEEE80211_MACCMD_POLICY_DENY);
+}
+static void kick_mac(char *iface,char *mac)
+{
+#ifdef DEBUG
+	printf("KickMac: %s\n",mac);
+#endif
+
+	struct ieee80211req_mlme mlme;
+	mlme.im_reason = IEEE80211_REASON_NOT_AUTHED;
+        ether_atoe (mac, mlme.im_macaddr);
+	do80211priv(iface, IEEE80211_IOCTL_SETMLME, &mlme, sizeof(mlme));
+}
+#endif
 
 int authmac(unsigned char *mac)
 {
@@ -107,7 +307,8 @@ int main(int argc, char** argv)
 	if ( argc < 2 )
 	{
 		fprintf(stderr,"wrt-radauth - A simple radius authenticator\n");
-		fprintf(stderr,"(C) 2005 Michael Gernoth, some enhancements by Sebastian Gottschall\n");
+		fprintf(stderr,"(C) 2005 Michael Gernoth\n");
+		fprintf(stderr,"(C) 2006 Atheros support Sebastian Gottschall\n");
 		fprintf(stderr,"Usage: %s [-n] interface\n",argv[0]);
 		fprintf(stderr,"\t-n1\tUse new MAC address format 'aabbcc-ddeeff' instead of 'AA-BB-CC-DD-EE-FF'\n");
 		fprintf(stderr,"\t-n2\tUse really new MAC address format 'aabbccddeeff' instead of 'AA-BB-CC-DD-EE-FF'\n");
@@ -167,10 +368,9 @@ int main(int argc, char** argv)
 	lastcnt=0;
 
 	/* Disable MAC security on card */
-	memset(buf,0,sizeof(buf));
-	wl_ioctl(iface, WLC_SET_MACLIST, buf, sizeof(buf));
-	val = WLC_MACMODE_DISABLED;
-	wl_ioctl(iface,WLC_SET_MACMODE, &val, sizeof(val));
+	set_maclist(iface,buf);
+	security_disable(iface);
+
 
 	/* No STAs in list */
 	first = NULL;
@@ -382,10 +582,10 @@ int main(int argc, char** argv)
 			if ( maclist->count )
 			{
 				unsigned char mac[6];
-
-				wl_ioctl(iface, WLC_SET_MACLIST, buf, sizeof(buf));
-				val = WLC_MACMODE_DENY;
-				wl_ioctl(iface,WLC_SET_MACMODE, &val, sizeof(val));
+				
+				
+				set_maclist(iface,buf);  //set maclist to deny
+				security_deny(iface); //deny macs
 
 				ea = maclist->ea;
 				for(i=0; i < maclist->count; i++)
@@ -402,7 +602,8 @@ int main(int argc, char** argv)
 					system(macbuild);
 					
 					}else
-					wl_ioctl(iface, 0x8f, mac, 6);	/* Kick station off AP */
+					kick_mac(iface,mac);
+					
 					
 					ea++;
 				}
@@ -414,8 +615,7 @@ int main(int argc, char** argv)
 #ifdef DEBUG
 					printf("Resetting security\n");
 #endif
-					val = WLC_MACMODE_DISABLED;
-					wl_ioctl(iface,WLC_SET_MACMODE, &val, sizeof(val));
+					security_disable(iface);
 				}
 			}
 			lastcnt = maclist->count;
