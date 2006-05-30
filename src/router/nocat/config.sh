8aacd71ae703c92e4fa1d4e2da9c0e0ff34e26bd
steve <USER@DOMAIN>
2006-05-30 23:46:16 +0000

    nocat-test
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@2327 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/nocat/config.sh
+++ src/router/nocat/config.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-CC="mipsel-linux-uclibc-gcc" CFLAGS="-Os -pipe -mips32 -mtune=mips32 -funit-at-a-time" \
+#CC="mipsel-linux-uclibc-gcc" 
+#CFLAGS="-Os -pipe -mips32 -mtune=mips32 -funit-at-a-time" \
 ./configure --with-remote-splash --prefix=/usr --localstatedir=/var --sysconfdir=/etc \
 --with-glib-prefix=${PWD}/../glib-1.2.10-install \
 --disable-glibtest --host=mipsel-linux
