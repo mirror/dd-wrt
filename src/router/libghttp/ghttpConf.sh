3b595ef88acec406b6ebd98f5fcc0c3c59575847
steve <USER@DOMAIN>
2006-06-02 18:12:30 +0000

    cleaup, old lib
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@2384 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libghttp/ghttpConf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the ghttp library in GNOME applications.
-#
-GHTTP_LIBDIR="-L/usr/local/lib"
-GHTTP_LIBS="-lghttp"
-GHTTP_INCLUDEDIR="-I/usr/local/include"
-MODULE_VERSION="ghttp-1.0.9"
-
