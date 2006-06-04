df050bf072969f6926e47c7e7543533577d980c3
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-06-04 15:50:53 +0000

    radauth format
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@2433 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/radauth/radius-client.c
+++ src/router/radauth/radius-client.c
@@ -20,34 +20,39 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
- 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include "radius.h"
 
-int main(int argc, char** argv)
+int
+main (int argc, char **argv)
 {
-int trycount = 10,i=1;
-	if ( argc < 6 )
+  int trycount = 10, i = 1;
+  if (argc < 6)
+    {
+      fprintf (stderr, "radius-client - A simple radius authenticator\n");
+      fprintf (stderr, "(C) 2004 Michael Gernoth,\n");
+      fprintf (stderr,
+	       "(c) 2005 some modifications by Sebastian Gottschall\n");
+      fprintf (stderr, "Usage: %s user pass server port shared-secret\n",
+	       argv[0]);
+      exit (1);
+    }
+  while ((trycount--) > 0)
+    {
+      if (radius (argv[3], atoi (argv[4]), argv[1], argv[2], argv[5]))
 	{
-		fprintf(stderr,"radius-client - A simple radius authenticator\n");
-		fprintf(stderr,"(C) 2004 Michael Gernoth,\n");
-		fprintf(stderr,"(c) 2005 some modifications by Sebastian Gottschall\n");
-		fprintf(stderr,"Usage: %s user pass server port shared-secret\n",argv[0]);
-		exit(1);
+	  printf ("Accept\n");
+	  break;
 	}
-	while((trycount--)>0)
+      else
 	{
-	    if (radius(argv[3],atoi(argv[4]),argv[1],argv[2],argv[5]))
-	    {
-	    	    printf("Accept\n");
-		    break;
-	    } else {
-		    sleep(1);
-		    printf("Reject on try %d\n",i++);
-	    }
+	  sleep (1);
+	  printf ("Reject on try %d\n", i++);
 	}
-	exit(0);
+    }
+  exit (0);
 }
