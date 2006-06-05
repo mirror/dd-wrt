226a810f8bcb397f3892a543c178fbe507a28d16
steve <USER@DOMAIN>
2006-06-05 05:33:49 +0000

    fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@2449 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/status.c
+++ src/router/httpd/modules/status.c
@@ -281,22 +281,21 @@ ej_show_status (int eid, webs_t wp, int argc, char_t ** argv)
   return;
 }
 
-void
+int
 ej_show_wan_domain (int eid, webs_t wp, int argc, char_t ** argv)
 {
   char *wan_domain, *lan_domain;
-  char *wan = "WAN:", *lan = "LAN:";
+  char *wan = "WAN: ", *lan = "LAN: ";
 
   if (strcmp (nvram_safe_get ("wan_domain"), ""))
     wan_domain = strcat (wan, nvram_safe_get ("wan_domain"));
-  else
+  else if (strcmp (nvram_safe_get ("wan_get_domain"), "")
     wan_domain = strcat (wan, nvram_safe_get ("wan_get_domain"));
 
   if (strcmp (nvram_safe_get ("lan_domain"), ""))
     lan_domain = strcat (lan, nvram_safe_get ("lan_domain"));
 
-  websWrite (wp, "%s %s", lan_domain, wan_domain);
-  return;
+  return websWrite (wp, "%s %s", lan_domain, wan_domain);
 }
 
 int
