570ecb230bd22afeb86dc15a39ea9e5bbab5e6d8
steve <steve@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-06-05 04:42:32 +0000

    fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@2448 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/status.c
+++ src/router/httpd/modules/status.c
@@ -284,9 +284,8 @@ ej_show_status (int eid, webs_t wp, int argc, char_t ** argv)
 void
 ej_show_wan_domain (int eid, webs_t wp, int argc, char_t ** argv)
 {
-  char wan_domain, lan_domain;
-  char wan ="WAN:";
-  char lan ="LAN:";
+  char *wan_domain, *lan_domain;
+  char *wan = "WAN:", *lan = "LAN:";
 
   if (strcmp (nvram_safe_get ("wan_domain"), ""))
     wan_domain = strcat (wan, nvram_safe_get ("wan_domain"));
