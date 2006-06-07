6b1ce64c500fb38e37d211ee14bfa53ff680bde7
BrainSlayer <s.gottschall@dd-wrt.com>
2006-06-07 14:28:07 +0000

    certificate fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@2489 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/httpd/gencert.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#SECS=$1
+
+# create the key and certificate request
+openssl req -new -out cert.csr -config openssl.cnf -keyout privkey.pem -newkey rsa:512 -passout pass:password
+# remove the passphrase from the key
+openssl rsa -in privkey.pem -out key.pem -passin pass:password
+# convert the certificate request into a signed certificate
+SECS="1145611923"
+openssl x509 -in cert.csr -out cert.pem -req -signkey key.pem -days 3650
+# Show human-readable format
+openssl x509 -in cert.pem -text -noout
+# Remove unused files
+#rm -f /tmp/cert.csr /tmp/privkey.pem
