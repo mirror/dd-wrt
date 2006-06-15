fac9a9af8f6df602dd2b855d7a1f8864d071e94f
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-06-15 02:21:47 +0000

    replace with new codeline
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@2596 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/rb500/linux-2.6/include/asm-sh/auxvec.h
+++
@@ -1,4 +0,0 @@
-#ifndef __ASM_SH_AUXVEC_H
-#define __ASM_SH_AUXVEC_H
-
-#endif /* __ASM_SH_AUXVEC_H */
--- src/linux/rb500/linux-2.6/include/asm-sh/setup.h
+++
@@ -1,8 +0,0 @@
-#ifdef __KERNEL__
-#ifndef _SH_SETUP_H
-#define _SH_SETUP_H
-
-#define COMMAND_LINE_SIZE 256
-
-#endif /* _SH_SETUP_H */
-#endif /* __KERNEL__ */
