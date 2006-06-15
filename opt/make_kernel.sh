4aa4ac2057d1ba3dc2c0f289abe8211d666e5677
steve <USER@DOMAIN>
2006-06-15 18:19:54 +0000

    updated micro shells
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@2609 52c4871e-980c-0410-b1e0-e73912ce01f8

--- opt/make_kernel.sh
+++ opt/make_kernel.sh
@@ -4,4 +4,5 @@
 #cat ../src/linux/linux/arch/mips/brcm-boards/bcm947xx/compressed/piggy | 
 #./loader-0.02/lzma e ../src/linux/linux/arch/mips/bcm947xx/compressed/piggy ../src/router/mipsel-uclibc/vmlinuz
 ./loader-0.02/lzma e ../src/linux/brcm/linux.v23/arch/mips/bcm947xx/compressed/piggy vmlinuz
+./loader-0.02/lzma e ../src/linux/brcm/linux.v23/arch/mips/bcm947xx/compressed/piggy vmlinuzmicro
 ./loader-0.02/lzma e piggymicro vmlinuzmicro
