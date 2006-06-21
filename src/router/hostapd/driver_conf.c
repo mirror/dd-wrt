093c33408fd11432c11c2c64ff882e485c425cde
BrainSlayer <s.gottschall@dd-wrt.com>
2006-06-21 13:22:33 +0000

    hostapd and suppplicant update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@2677 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/hostapd/driver_conf.c
@@ -0,0 +1,8 @@
+/* THIS FILE AUTOMATICALLY GENERATED, DO NOT EDIT! */
+#include "includes.h"
+#include "hostapd.h"
+#include "driver.h"
+void madwifi_driver_register(void);
+void register_drivers(void) {
+madwifi_driver_register();
+}
