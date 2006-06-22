4d070a6e075fbff74e6f87db33c6ff966452708c
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-06-22 14:56:17 +0000

    js formatter added
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@2689 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/tools/jsformat.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+void filter(char *name)
+{
+int len;
+FILE *in=fopen(name,"rb");
+fseek(in,0,SEEK_END);
+len=ftell(in);
+rewind(in);
+char *buf=(char*)malloc(len);
+fread(buf,len,1,in);
+fclose(in);
+in=fopen(name,"wb");
+int i;
+for (i=0;i<len;i++)
+{
+if (i==0 && buf[i]==0xa)i++;
+if (buf[i]=='/' && buf[i+1]=='/')
+    {
+    while(buf[i]!=0xa)i++;
+    continue;
+    }
+if (buf[i]==0xa && i<len-1)
+    {
+    while(buf[i+1]==0xa && i<len-1)i++;
+    }
+putc(buf[i],in);
+}
+fclose(in);
+}
+
+void main(int argc,char *argv[])
+{
+int i;
+for (i=1;i<argc;i++)
+    filter(argv[i]);
+}
