7f8f75380aea18d424078fc882b310064c728354
steve <steve@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-06-23 04:47:49 +0000

    now removed
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@2701 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/busybox/applets/version.c
+++ src/router/busybox/applets/version.c
@@ -1,9 +1,9 @@
 #include "busybox.h"
 
 #ifndef BB_EXTRA_VERSION
-#define BANNER "Now DD-WRT VeryBusyBox v" BB_VER " (" BB_BT ")"
+#define BANNER "DD-WRT VeryBusyBox v" BB_VER " (" BB_BT ")"
 #else
-#define BANNER "Now DD-WRT VeryBusyBox v" BB_VER " (" BB_EXTRA_VERSION ")"
+#define BANNER "DD-WRT VeryBusyBox v" BB_VER " (" BB_EXTRA_VERSION ")"
 #endif
 
 const char BB_BANNER[]=BANNER;
