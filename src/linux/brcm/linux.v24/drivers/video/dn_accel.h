7fbadaae029e754f3b1a7172584d839087b7477f
BrainSlayer <s.gottschall@dd-wrt.com>
2006-06-26 22:09:09 +0000

    v24 preparation
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@2725 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/brcm/linux.v24/drivers/video/dn_accel.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DN_ACCEL_H_
-#define _DN_ACCEL_H_
-
-#include <linux/fb.h>
-
-void dn_bitblt(struct display *p,int x_src,int y_src, int x_dest, int y_dest,
-	       int x_count, int y_count);
-
-#endif
--- src/linux/brcm/linux.v24/include/linux/vfs.h
+++
@@ -1,6 +0,0 @@
-#ifndef _LINUX_VFS_H
-#define _LINUX_VFS_H
-
-#include <asm/statfs.h>
-
-#endif
