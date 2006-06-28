ae0bab8d08f131280f1df1cff671fb21ff958d6e
BrainSlayer <s.gottschall@dd-wrt.com>
2006-06-28 08:23:48 +0000

    wpa/wpa2 for mssid
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@2763 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services.c
+++ src/router/services/services.c
@@ -1265,7 +1265,7 @@ getAuthMode (char *prefix)
 {
   char akm[32];
   sprintf (akm, "%s_akm", prefix);
-  if (nvram_match (akm, "disabled") || nvram_get(akm)==NULL)
+  if (nvram_match (akm, "disabled") || nvram_get(akm)==NULL || strlen(nvram_safe_get(akm))==0)
     return NULL;
   if (nvram_match (akm, "wpa") || nvram_match (akm, "radius"))
     return "2";
