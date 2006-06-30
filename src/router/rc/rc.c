68f75390e4d72cf80e04a9ecb7025d0c75db7672
BrainSlayer <s.gottschall@dd-wrt.com>
2006-06-30 22:44:13 +0000

    fix for bad code
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@2824 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/rc.c
+++ src/router/rc/rc.c
@@ -535,11 +535,11 @@ main (int argc, char **argv)
   else if (strstr (base, "ip-down"))
     return start_main ("ipdown", argc, argv);
   else if (strstr(base, "set-pppoepid")) //tallest 1219
-    return set_pppoepid_to_nv_main(argc, argv);
+    return start_main("set_pppoepid_to_nv",argc, argv);
   else if (strstr(base, "disconnected_pppoe")) //by tallest 0407
-    return disconnected_pppoe_main(argc, argv);
+    return start_main("disconnected_pppoe",argc, argv);
   else if (strstr(base, "ppp_event")) 
-    return pppevent_main(argc, argv);
+    return start_main("pppevent_main",argc, argv);
 
   /* udhcpc [ deconfig bound renew ] */
   else if (strstr (base, "udhcpc"))
