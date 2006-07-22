294db4092625b225b08c86fa3dff046be4dcdc81
BrainSlayer <s.gottschall@dd-wrt.com>
2006-07-22 02:38:32 +0000

    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@3151 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/xscale/linux-2.6.17/arch/powerpc/sysdev/fsl_soc.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PPC_FSL_SOC_H
-#define __PPC_FSL_SOC_H
-#ifdef __KERNEL__
-
-extern phys_addr_t get_immrbase(void);
-
-#endif
-#endif
--- src/linux/xscale/linux-2.6.17/include/asm-m32r/xor.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ASM_M32R_XOR_H
-#define _ASM_M32R_XOR_H
-
-/* $Id$ */
-
-#include <asm-generic/xor.h>
-
-#endif  /* _ASM_M32R_XOR_H */
