39c81e127f4cb5e3eb825c84ea44acf8665a1bb8
BrainSlayer <s.gottschall@dd-wrt.com>
2006-07-22 01:24:04 +0000

    fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@3149 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/misc/rootprep.sh
+++ src/router/misc/rootprep.sh
@@ -20,6 +20,7 @@ mkdir -p jffs
 mkdir -p mmc
 mkdir -p usr
 mkdir -p usr/local
+mkdir -p opt
 cd usr/local
 ln -sf /tmp/share share
 cd ../../
@@ -37,7 +38,7 @@ mkdir -p etc
 echo "/jffs/lib" > etc/ld.so.conf
 echo "/jffs/usr/lib" >> etc/ld.so.conf
 echo "/jffs/usr/local/lib" >> etc/ld.so.conf
-echo "/mmc/lib" > etc/ld.so.conf
+echo "/mmc/lib" >> etc/ld.so.conf
 echo "/mmc/usr/lib" >> etc/ld.so.conf
 echo "/mmc/usr/local/lib" >> etc/ld.so.conf
 echo "/lib" >> etc/ld.so.conf
