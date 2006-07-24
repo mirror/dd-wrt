d8a43bfe902aa492192d8d866b538f4feef4347b
eko <USER@DOMAIN>
2006-07-24 20:48:00 +0000

    mac change - Askey corp.
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@3202 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit.c
+++ src/router/services/sysinit.c
@@ -902,7 +902,7 @@ eval ("cp", "/etc/nvram/offsets.db", "/tmp/nvram");
       nvram_set ("wan_ifname", "eth1");	// fix for Belkin f5d7230 v1000 WAN problem.
       nvram_set ("wan_ifnames", "eth1");
       if (nvram_get ("et0macaddr")== NULL  || nvram_get ("et0macaddr") == "")
-      	nvram_set ("et0macaddr", "00:90:96:00:00:10"); //fix for missing cfe default = dead LAN ports.
+      	nvram_set ("et0macaddr", "00:16:E3:00:00:10"); //fix for missing cfe default = dead LAN ports.
       break;
       
 	case ROUTER_WRTSL54GS:
