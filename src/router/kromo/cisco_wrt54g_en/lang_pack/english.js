74760a79b89ba677fa8903e8aa28582c1c0462f6
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-07-30 12:21:49 +0000

    typos
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@3291 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/cisco_wrt54g_en/lang_pack/english.js
+++ src/router/kromo/cisco_wrt54g_en/lang_pack/english.js
@@ -1478,7 +1478,7 @@ wl_basic.b="B-Only";
 wl_basic.a="A-Only";
 wl_basic.g="G-Only";
 wl_basic.legend2="Radio Time Restrictions";
-wl_basic.radio="Radio Sheduling";
+wl_basic.radio="Radio Scheduling";
 wl_basic.radio_on="Radio is On";
 wl_basic.radio_off="Radio is Off";
 
