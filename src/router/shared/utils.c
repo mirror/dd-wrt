bb17a19ff84a0a7345f39a113e1e226dc516ac7a
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-08-03 00:26:55 +0000

    adhoc mode fixed
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@3369 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/utils.c
+++ src/router/shared/utils.c
@@ -902,7 +902,7 @@ get_wan_face (void)
 	strncpy (localwanface, nvram_safe_get ("pppd_pppifname"), IFNAMSIZ);
     }
 #ifndef HAVE_MADWIFI
-  else if (nvram_invmatch ("wl_mode", "ap"))
+  else if (nvram_match ("wl0_mode", "sta") || nvram_match("wl0_mode","apsta") || nvram_match("wl0_mode","wet"))
     {
 
       if (check_hw_type () == BCM4702_CHIP
