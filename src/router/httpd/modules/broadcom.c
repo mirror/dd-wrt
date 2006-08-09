cd65f968bfb9f87916899306654ab5f40715247a
botho <USER@DOMAIN>
2006-08-09 14:11:35 +0000

    delete radio icons
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@3428 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/broadcom.c
+++ src/router/httpd/modules/broadcom.c
@@ -4360,12 +4360,14 @@ wl_ioctl(get_wdev(), WLC_GET_RADIO, &radiooff, sizeof(int));
 switch (radiooff)
 	{	
 	case 0:
-		websWrite (wp, "On&nbsp;&nbsp;<img style=\"border-width: 0em;\" src=\"images/radio_on.gif\" width=\"35\" height=\"10\"> ");
+		//websWrite (wp, "On&nbsp;&nbsp;<img style=\"border-width: 0em;\" src=\"images/radio_on.gif\" width=\"35\" height=\"10\"> ");
+		websWrite (wp, "On"> ");
 		break;
 	case 1: // software disabled
 	case 2: // hardware disabled
 	case 3: // both are disabled
-		websWrite (wp, "Off&nbsp;&nbsp;<img style=\"border-width: 0em;\" src=\"images/radio_off.gif\" width=\"35\" height=\"10\"> ");
+		//websWrite (wp, "Off&nbsp;&nbsp;<img style=\"border-width: 0em;\" src=\"images/radio_off.gif\" width=\"35\" height=\"10\"> ");
+		websWrite (wp, "Off"> ");
 		break;
 	case -1:
 		websWrite (wp, "unknown");
