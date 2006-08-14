4a3f31e3948ddb2a7d7b439fe86fa0670ec06ce3
steve <steve@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-08-14 04:24:40 +0000

    nocat conf
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@3474 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/mkfiles.c
+++ src/router/services/mkfiles.c
@@ -145,73 +145,112 @@ start_mkfiles (void)
 }
 
 #ifdef HAVE_NOCAT
-int
-mk_nocat_conf (void)
+int mk_nocat_conf(void)
 {
-  FILE *fp;
-  /* BPsmythe: Write out a nocat.conf file */
-  if (!(fp = fopen (NOCAT_CONF, "w")))
-    {
-      perror (NOCAT_CONF);
-      return errno;
-    }
+	FILE *fp ;
+	/* BPsmythe: Write out a nocat.conf file */
+	if (!(fp = fopen(NOCAT_CONF, "w"))) {
+		perror(NOCAT_CONF);
+		return errno;
+	}
 
-  fprintf (fp, "#\n");
-  /* settings that need to be set based on router configurations */
-  fprintf (fp, "InternalDevice\t%s\n", nvram_safe_get ("lan_ifname"));
-  fprintf (fp, "ExternalDevice\t%s\n", nvram_safe_get ("wan_ifname"));
-  fprintf (fp, "LocalNetwork\t%s/%s\n",
-	   _get_network (nvram_safe_get ("lan_ipaddr"),
-			 nvram_safe_get ("lan_netmask")),
-	   nvram_safe_get ("lan_netmask"));
+	fprintf(fp, "#\n");
+	
+	/* settings that need to be set based on router configurations */
+        /* These are now autodetected on WRT54G via: lan_ifname and wan_ifname */
+	/*fprintf(fp, "InternalDevice\t%s\n", nvram_safe_get("lan_ifname")); */
+	/*fprintf(fp, "ExternalDevice\t%s\n", nvram_safe_get("wan_ifname")); */
+        /*
+	 * fprintf(fp, "InternalDevice\t%s\n", nvram_safe_get("NC_InternalDevice") ); 
+         * fprintf(fp, "ExternalDevice\t%s\n", nvram_safe_get("NC_ExternalDevice") ); 
+	 * // InsideIP is now depreciated, use GatewayAddr
+	 * fprintf(fp, "InsideIP\t%s\n", nvram_safe_get("lan_ipaddr")); 
+	 * fprintf(fp, "LocalNetwork\t%s/%s\n", 
+		get_network(nvram_safe_get("lan_ipaddr"), nvram_safe_get("lan_netmask")),
+		nvram_safe_get("lan_netmask") );
+	*/
+        /* These are now hardcoded as the defaults */
+	//fprintf(fp, "SplashForm\t%s\n", "splash.html");
+	//fprintf(fp, "StatusForm\t%s\n", "status.html");
 
-  /* Irving - Rework getting DNS */
-  struct dns_lists *dns_list = NULL;
-  dns_list = get_dns_list ();
-  if (!dns_list || dns_list->num_servers == 0)
-    {
-      fprintf (fp, "DNSAddr \t%s\n", nvram_safe_get ("lan_ipaddr"));
-    }
-  else
-    {
-      fprintf (fp, "DNSAddr \t%s %s %s\n", dns_list->dns_server[0],
-	       dns_list->dns_server[1], dns_list->dns_server[2]);
-    }
-  if (dns_list)
-    free (dns_list);
-  fflush (fp);
+	fprintf(fp, "RouteOnly\t%s\n", nvram_safe_get("NC_RouteOnly") );
 
-  /* These are user defined, eventually via the web page */
-  char *exclude = nvram_get ("NC_ExcludePorts");
-  if (exclude && strlen (exclude) > 0)
-    fprintf (fp, "ExcludePorts\t%s\n", nvram_safe_get ("NC_ExcludePorts"));
-  fprintf (fp, "Verbosity\t%s\n", nvram_safe_get ("NC_Verbosity"));
-  fprintf (fp, "GatewayName\t%s\n", nvram_safe_get ("NC_GatewayName"));
-  fprintf (fp, "GatewayAddr\t%s\n", nvram_safe_get ("lan_ipaddr"));
-  fprintf (fp, "GatewayPort\t5280\n");
-  fprintf (fp, "HomePage\t%s\n", nvram_safe_get ("NC_HomePage"));
-  fprintf (fp, "AllowedWebHosts\t%s %s\n", nvram_safe_get ("lan_ipaddr"),
-	   nvram_safe_get ("NC_AllowedWebHosts"));
-  fprintf (fp, "MACWhiteList\t%s\n", nvram_safe_get ("NC_MacWhiteList"));
-  fprintf (fp, "LoginTimeout\t%s\n", nvram_safe_get ("NC_LoginTimeout"));
-  fprintf (fp, "DocumentRoot\t%s\n", nvram_safe_get ("NC_DocumentRoot"));
-  if (nvram_invmatch ("NC_SplashURL", ""))
-    {
-      fprintf (fp, "SplashURL\t%s\n", nvram_safe_get ("NC_SplashURL"));
-      fprintf (fp, "SplashTimeout\t21600\n");
-    }
-  fprintf (fp, "RouteOnly\t%s\n", nvram_safe_get ("NC_RouteOnly"));
+	/* These are user defined, eventually via the web page */
+	fprintf(fp, "Verbosity\t%s\n", nvram_safe_get("NC_Verbosity") );
+	fprintf(fp, "GatewayName\t%s\n", nvram_safe_get("NC_GatewayName") );  
+	fprintf(fp, "GatewayAddr\t%s\n", nvram_safe_get("lan_ipaddr") );
+	fprintf(fp, "GatewayPort\t%s\n", nvram_safe_get("NC_GatewayPort") );
+	fprintf(fp, "GatewayMAC\t%s\n", nvram_safe_get("et0macaddr") );
+	fprintf(fp, "GatewayPassword\t%s\n", nvram_safe_get("NC_Password") );
+	fprintf(fp, "GatewayMode\t%s\n", nvram_safe_get("NC_GatewayMode") );
+	fprintf(fp, "DocumentRoot\t%s\n", nvram_safe_get("NC_DocumentRoot") );
+	fprintf(fp, "SplashURL\t%s\n", nvram_safe_get("NC_SplashURL") );
+	fprintf(fp, "SplashURLTimeout\t%s\n", nvram_safe_get("NC_SplashURLTimeout") );
+	fprintf(fp, "LeaseFile\t%s\n", nvram_safe_get("NC_LeaseFile") );
 
-  /* These should be hardcoded as the defaults */
-  fprintf (fp, "SplashForm\t%s\n", "splash.html");
-  fprintf (fp, "StatusForm\t%s\n", "status.html");
-  fprintf (fp, "GatewayMode\t%s\n", "Open");
-  fprintf (fp, "FirewallPath\t%s\n", "/usr/libexec/nocat");
-  fprintf (fp, "LogFacility\t%s\n,", "syslog");
+	/* Open-mode and common options */
+	fprintf(fp, "FirewallPath\t%s\n", "/usr/libexec/nocat/");
+	fprintf(fp, "ExcludePorts\t%s\n", nvram_safe_get("NC_ExcludePorts") ); 
+	fprintf(fp, "IncludePorts\t%s\n", nvram_safe_get("NC_IncludePorts") ); 
+	fprintf(fp, "AllowedWebHosts\t%s %s\n", nvram_safe_get("lan_ipaddr"), nvram_safe_get("NC_AllowedWebHosts") );  
+        /* TJaqua: Added MACWhiteList to ignore given machines or routers on the local net (e.g. routers with an alternate Auth). */
+        fprintf(fp, "MACWhiteList\t%s\n", nvram_safe_get("NC_MacWhiteList") );
+        /* TJaqua: Added AnyDNS to pass through any client-defined servers. */
+        if (!strcmp(nvram_safe_get("NC_AnyDNS"),"1")) {
+                fprintf(fp, "AnyDNS\t%s\n", nvram_safe_get("NC_AnyDNS") );
+        }
+        else {
+	        /* Irving - Rework getting DNS */
+	        struct dns_lists *dns_list = NULL;
+	        dns_list = get_dns_list(0);
+	        if(!dns_list || dns_list->num_servers == 0){
+		        fprintf(fp, "DNSAddr \t%s\n", nvram_safe_get("lan_ipaddr") );
+	        } else {
+		        fprintf(fp, "DNSAddr \t%s %s %s\n", dns_list->dns_server[0], dns_list->dns_server[1], dns_list->dns_server[2]);
+	        }
+        }
+	fprintf(fp, "HomePage\t%s\n", nvram_safe_get("NC_HomePage") );  
+	fprintf(fp, "ForcedRedirect\t%s\n", nvram_safe_get("NC_ForcedRedirect") );  
+	fprintf(fp, "PeerCheckTimeout\t%s\n", nvram_safe_get("NC_PeerChecktimeout") ); 
+	fprintf(fp, "IdleTimeout\t%s\n", nvram_safe_get("NC_IdleTimeout") );
+	fprintf(fp, "MaxMissedARP\t%s\n", nvram_safe_get("NC_MaxMissedARP") );
+	fprintf(fp, "LoginTimeout\t%s\n", nvram_safe_get("NC_LoginTimeout") ); 
+	fprintf(fp, "RenewTimeout\t%s\n", nvram_safe_get("NC_RenewTimeout") );
+	
+	/* defined for RADIUS 
+	fprintf(fp, "AuthServiceAddr\t%s\n", nvram_safe_get("NC_AuthServiceAddr") );
+	fprintf(fp, "LoginPage\t%s\n", nvram_safe_get("NC_LoginPage") );
+	fprintf(fp, "ConfirmPage\t%s\n", nvram_safe_get("NC_ConfirmPage") );
+	fprintf(fp, "LogoutPage\t%s\n", nvram_safe_get("NC_LogoutPage") );
+	fprintf(fp, "RADIUSAuthServer\t%s\n", nvram_safe_get("NC_RADIUSAuthServer") );
+	fprintf(fp, "RADIUSAuthPort\t%s\n", nvram_safe_get("NC_RADIUSAuthPort") );
+	fprintf(fp, "RADIUSAuthSecret\t%s\n", nvram_safe_get("NC_RADIUSAuthSecret") );
+	fprintf(fp, "RADIUSAuthNASIdentifier\t%s\n", nvram_safe_get("NC_RADIUSAuthNASIdentifier") );
+	fprintf(fp, "RADIUSAuthWait\t%s\n", nvram_safe_get("NC_RADIUSAuthWait") );
+	fprintf(fp, "RADIUSAuthRetries\t%s\n", nvram_safe_get("NC_RADIUSAuthRetries") );
+	fprintf(fp, "RADIUSAcctServer\t%s\n", nvram_safe_get("NC_RADIUSAcctServer") );
+	fprintf(fp, "RADIUSAcctPort\t%s\n", nvram_safe_get("NC_RADIUSAcctPort") );
+	fprintf(fp, "RADIUSAcctSecret\t%s\n", nvram_safe_get("NC_RADIUSAcctSecret") );
+	fprintf(fp, "RADIUSAcctNASIdentifier\t%s\n", nvram_safe_get("NC_RADIUSAcctNASIdentifier") );
+	fprintf(fp, "RADIUSAcctWait\t%s\n", nvram_safe_get("NC_RADIUSAcctWait") );
+	fprintf(fp, "RADIUSAcctRetries\t%s\n", nvram_safe_get("NC_RADIUSAcctRetries") );
+	*/
+	
+	/* defined for second radius server
+	fprintf(fp, "RADIUSAuth1Server\t%s\n", nvram_safe_get("NC_RADIUSAuth1Server") );
+	fprintf(fp, "RADIUSAuth1Port\t%s\n", nvram_safe_get("NC_RADIUSAuth1Port") );
+	fprintf(fp, "RADIUSAuth1Secret\t%s\n", nvram_safe_get("NC_RADIUSAuth1Secret") );
+	fprintf(fp, "RADIUSAuth1NASIdentifier\t%s\n", nvram_safe_get("NC_RADIUSAuth1NASIdentifier") );
+	fprintf(fp, "RADIUSAcct1Server\t%s\n", nvram_safe_get("NC_RADIUSAcct1Server") );
+	fprintf(fp, "RADIUSAcct1Port\t%s\n", nvram_safe_get("NC_RADIUSAcct1Port") );
+	fprintf(fp, "RADIUSAcct1Secret\t%s\n", nvram_safe_get("NC_RADIUSAcct1Secret") );
+	fprintf(fp, "RADIUSAcct1NASIdentifier\t%s\n", nvram_safe_get("NC_RADIUSAcct1NASIdentifier") );
+	*/
 
-  fclose (fp);
-  /* end BPsmythe */
-
-  return 0;
+	fclose(fp);
+	/* end BPsmythe */
+	fprintf(stderr,"Wrote: %s\n", NOCAT_CONF);
+	
+	return 0;
 }
 #endif
