eb1bce40db1e656690f5287cddb1c5d57d166221
steve <steve@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-08-15 01:10:48 +0000

    not sure why the compiler is so picky about this
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@3499 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/nocat/config.sh
+++ src/router/nocat/config.sh
@@ -4,4 +4,4 @@ CC="mipsel-linux-uclibc-gcc"
 CFLAGS="-Os -pipe -mips32 -mtune=mips32 -funit-at-a-time -I../libghttp" \
 ./configure --with-remote-splash --prefix=/usr --localstatedir=/var --sysconfdir=/etc \
 --with-glib-prefix=${PWD}/../glib-1.2.10-install \
---disable-glibtest --build=mipsel-linux
+--disable-glibtest --host=mipsel-linux
