e528e347032cb2f5e93be2808f5c0afe27b25871
eko <USER@DOMAIN>
2006-08-17 19:04:56 +0000

    itoa fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@3554 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/broadcom.c
+++ src/router/httpd/modules/broadcom.c
@@ -3960,26 +3960,27 @@ char timediffs[37][8] = {"-12","-11","-10","-09.5","-09","-08","-07","-06","-05"
 char timezones[37][8] = {"-12:00","-11:00","-10:00","-09:30","-09:00","-08:00","-07:00","-06:00","-05:00","-04:00","-03:30","-03:00","-02:00","-01:00","",
 								"+01:00","+02:00","+03:00","+03:30","+04:00","+05:00","+05:30","+05:45","+06:00","+06:30","+07:00","+08:00","+09:00","+09:30","+10:00","+10:30","+11:00","+11:30","+12:00","+12:45","+13:00","+14:00"};
 
-char diffeffect[4][29] = {"none","first Sun Apr - last Sun Oct","last Sun Mar - last Sun Oct","last Sun Oct - last Sun Mar"};
+char summertime[4][29] = {"none","first Sun Apr - last Sun Oct","last Sun Mar - last Sun Oct","last Sun Oct - last Sun Mar"};
+
+char timeoption[4][2] = {"1","2","3","4"};
 
 int i,j;
-char str[11], buf[2];
+char str[11];
 
-	for (i=0; i<37; i++)
-	{
-		for (j=0; j<4; j++)
+	  for (i=0; i<37; i++)
+		  {
+	  	for (j=0; j<4; j++)
 	  	{
 			strcpy (str, timediffs[i]);
 			strcat (str, " 1 ");
-  			itoa(j+1, buf, 10);
-			strcat (str, buf);
+			strcat (str, timeoption[j]);
 
 			websWrite (wp, "<option value=\"%s\" ", str);
 				if (nvram_match ("time_zone", str))
 				{
 				websWrite (wp, "selected=\"selected\"");
 				}
-			websWrite (wp, ">UTC%s / %s</option>\n", timezones[i], diffeffect[j]);
+			websWrite (wp, ">UTC%s / %s</option>\n", timezones[i], summertime[j]);
 
 		}
 	}
