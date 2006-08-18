3b7b98a376a0941fe91e6c199fe0f65078dfcc93
BrainSlayer <s.gottschall@dd-wrt.com>
2006-08-18 22:35:39 +0000

    another style fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@3593 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/broadcom.c
+++ src/router/httpd/modules/broadcom.c
@@ -3187,7 +3187,7 @@ ej_show_styles (int eid, webs_t wp, int argc, char_t ** argv)
   struct dirent *entry;
   while ((entry = readdir (directory)) != NULL)
     {
-      sprintf (buf, "/style/%s/style.css", entry->d_name);
+      sprintf (buf, "style/%s/style.css", entry->d_name);
       if (getWebsFile(buf)==NULL)
         {
 	sprintf (buf, "/www/style/%s/style.css", entry->d_name);
