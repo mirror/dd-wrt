5f9ebdec675321b464965d8fca316a2f042386ba
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-08-28 04:18:11 +0000

    extended wireless statistics for v24
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@3754 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/dd-wrt.c
+++ src/router/httpd/modules/dd-wrt.c
@@ -3288,6 +3288,8 @@ ej_active_wireless (int eid, webs_t wp, int argc, char_t ** argv)
 
   unlink (RSSI_TMP);
   int cnt = 0;
+  if (strcmp (iface, visible))
+    cnt = 1;
   mode = nvram_safe_get ("wl_mode");
   unsigned char buf[WLC_IOCTL_MAXLEN];
   memset (buf, 0, WLC_IOCTL_MAXLEN);
@@ -3374,7 +3376,7 @@ ej_active_wireless (int eid, webs_t wp, int argc, char_t ** argv)
 #else
 void
 ej_active_wireless_if (int eid, webs_t wp, int argc, char_t ** argv,
-		       char *iface,char *visible)
+		       char *iface, char *visible)
 {
   int rssi = 0, noise = 0;
   FILE *fp2;
@@ -3481,7 +3483,7 @@ ej_active_wireless_if (int eid, webs_t wp, int argc, char_t ** argv,
 void
 ej_active_wireless (int eid, webs_t wp, int argc, char_t ** argv)
 {
-  ej_active_wireless_if (eid, wp, argc, argv, get_wdev (),"wl0");
+  ej_active_wireless_if (eid, wp, argc, argv, get_wdev (), "wl0");
   char *next;
   char var[80];
   char *vifs = nvram_safe_get ("wl0_vifs");
@@ -3490,8 +3492,7 @@ ej_active_wireless (int eid, webs_t wp, int argc, char_t ** argv)
 
   foreach (var, vifs, next)
   {
-    websWrite (wp,","); 
-    ej_active_wireless_if (eid, wp, argc, argv, var,var);
+    ej_active_wireless_if (eid, wp, argc, argv, var, var);
   }
 }
 
