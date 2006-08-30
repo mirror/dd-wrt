a876c9ae39bb5e9551734a3a5c1b4d870234f2f3
BrainSlayer <s.gottschall@dd-wrt.com>
2006-08-30 20:24:03 +0000

    start for a new structure
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@3796 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/services/sysinit-rb532.c
@@ -0,0 +1,123 @@
+int
+start_sysinit (void)
+{
+  char buf[PATH_MAX];
+  struct utsname name;
+  struct stat tmp_stat;
+  time_t tm = 0;
+  unlink ("/etc/nvram/.lock");
+  cprintf ("sysinit() proc\n");
+  /* /proc */
+  mount ("proc", "/proc", "proc", MS_MGC_VAL, NULL);
+  cprintf ("sysinit() tmp\n");
+
+  /* /tmp */
+  mount ("ramfs", "/tmp", "ramfs", MS_MGC_VAL, NULL);
+  // fix for linux kernel 2.6
+  mount ("devpts", "/dev/pts", "devpts", MS_MGC_VAL, NULL);
+  eval ("mkdir", "/tmp/www");
+  
+  //load ext2 
+  // eval("insmod","jbd");
+  eval ("insmod", "ext2");
+#ifndef KERNEL_24
+  if (mount ("/dev/cf/card0/part3", "/usr/local", "ext2", MS_MGC_VAL, NULL))
+#else
+  if (mount
+      ("/dev/discs/disc0/part3", "/usr/local", "ext2", MS_MGC_VAL, NULL))
+#endif
+    {
+      //not created yet, create ext2 partition
+      eval ("/sbin/mke2fs", "-F", "-b", "1024", "/dev/cf/card0/part3");
+      //mount ext2 
+      mount ("/dev/cf/card0/part3", "/usr/local", "ext2", MS_MGC_VAL, NULL);
+      eval ("/bin/tar", "-xvvjf", "/etc/local.tar.bz2", "-C", "/");
+      mkdir ("/usr/local/nvram", 0777);
+//    eval("ln","-s","/etc/nvram","/usr/local/nvram");
+    }
+
+  unlink ("/tmp/nvram/.lock");
+  eval ("mkdir", "/tmp/nvram");
+  eval ("cp", "/etc/nvram/nvram.db", "/tmp/nvram");
+  eval ("cp", "/etc/nvram/offsets.db", "/tmp/nvram");
+  cprintf ("sysinit() var\n");
+
+  /* /var */
+  mkdir ("/tmp/var", 0777);
+  mkdir ("/var/lock", 0777);
+  mkdir ("/var/log", 0777);
+  mkdir ("/var/run", 0777);
+  mkdir ("/var/tmp", 0777);
+  cprintf ("sysinit() setup console\n");
+
+  /* Setup console */
+
+  cprintf ("sysinit() klogctl\n");
+  klogctl (8, NULL, atoi (nvram_safe_get ("console_loglevel")));
+  cprintf ("sysinit() get router\n");
+
+  int brand = getRouterBrand ();
+
+
+  /* Modules */
+  uname (&name);
+
+eval("insmod","md5");
+eval("insmod","aes");
+eval("insmod","blowfish");
+eval("insmod","deflate");
+eval("insmod","des");
+eval("insmod","michael_mic");
+eval("insmod","cast5");
+eval("insmod","crypto_null");
+
+system("/etc/kendin");
+eval("insmod","ixp400th");
+eval("insmod","ixp400");
+system("cat /usr/lib/firmware/IxNpeMicrocode.dat > /dev/IxNpe");
+eval("insmod","ixp400_eth");
+eval("insmod","ocf");
+eval("insmod","cryptodev");
+eval("insmod","ixp4xx","init_crypto=0");
+eval("ifconfig","ixp0","0.0.0.0","up");
+eval("vconfig","add","ixp0","1");
+eval("vconfig","add","ixp0","2");
+
+  eval ("insmod", "ath_hal");
+  eval ("insmod", "wlan");
+  eval ("insmod", "ath_rate_sample");
+  eval ("insmod", "ath_pci", "rfkill=0", "autocreate=none");
+
+
+  eval ("insmod", "wlan_acl");
+  eval ("insmod", "wlan_ccmp");
+  eval ("insmod", "wlan_tkip");
+  eval ("insmod", "wlan_wep");
+  eval ("insmod", "wlan_xauth");
+  eval ("insmod", "wlan_scan_ap");
+  eval ("insmod", "wlan_scan_sta");
+
+  eval ("ifconfig", "wifi0", "up");
+  eval ("ifconfig", "wifi1", "up");
+  eval ("ifconfig", "wifi2", "up");
+  eval ("ifconfig", "wifi3", "up");
+  eval ("ifconfig", "wifi4", "up");
+  eval ("ifconfig", "wifi5", "up");
+
+
+//  eval ("insmod", "mii");
+//  eval ("insmod", "korina");
+//  eval ("insmod", "via-rhine");
+  eval ("insmod", "ipv6");
+  /* Set a sane date */
+  stime (&tm);
+  if (brand == ROUTER_SIEMENS)
+    {
+      eval ("insmod", "led.o");	// Jerry Lee
+      powerled_ctrl (0);
+      led_ctrl (0);		// turn LED2 off
+    }
+
+  return 0;
+  cprintf ("done\n");
+}
