f8c9259e720fc4345089f9f06cadf747f970118e
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-09-08 12:00:02 +0000

    compile fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@3847 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/rb500/linux-2.6/include/asm-mips/rc32434/pci_regs.h
+++
@@ -1,8 +0,0 @@
-/* Override the default address space for this arch
-*/
-
-#include <linux/pci_regs.h>
-
-//#undef PCI_BASE_ADDRESS_SPACE
-//#define PCI_BASE_ADDRESS_SPACE PCI_BASE_ADDRESS_SPACE_MEMORY
-
--- src/linux/rb500/linux-2.6/include/asm-mips/sections.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ASM_SECTIONS_H
-#define _ASM_SECTIONS_H
-
-#include <asm-generic/sections.h>
-
-extern char _fdata;
-
-#endif /* _ASM_SECTIONS_H */
