79c36c2273c2ad7390d5cddd7334c0d29b4c3d20
BrainSlayer <s.gottschall@dd-wrt.com>
2006-09-11 00:38:10 +0000

    watchdog for magicbox added
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@3898 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rc/watchdog.c
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+
+
+static void watchdog (void)
+{
+	int fd=open("/dev/misc/watchdog",O_WRONLY);
+	if(fd==-1)
+	{
+		return;
+	}
+	while(1)
+	{
+		write(fd,"\0",1);
+		fsync(fd);
+		sleep(10);
+	}
+}
+
+int watchdog_main (int argc, char *argv[])
+{
+
+  /* Run it under background */
+  switch (fork ())
+    {
+    case -1:
+      exit (0);
+      break;
+    case 0:
+      /* child process */
+      watchdog();
+      exit(0);
+      break;
+    default:
+      /* parent process should just die */
+      _exit (0);
+    }
+return 0;
+}
