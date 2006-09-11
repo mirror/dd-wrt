319e87bfe7fc1d3664206b98fba5945019978cde
BrainSlayer <s.gottschall@dd-wrt.com>
2006-09-11 00:38:10 +0000

    watchdog for magicbox added
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@3898 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit-magicbox.c
+++ src/router/services/sysinit-magicbox.c
@@ -60,7 +60,7 @@ start_sysinit (void)
   mkdir ("/var/run", 0777);
   mkdir ("/var/tmp", 0777);
   cprintf ("sysinit() setup console\n");
-
+  eval("/sbin/watchdog");
   /* Setup console */
 
   cprintf ("sysinit() klogctl\n");
