3a156ad9cf02180b325f964e43b3156deb7f66a6
BrainSlayer <s.gottschall@dd-wrt.com>
2006-09-15 23:32:42 +0000

    wifidog added
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@3933 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/wifidog/src/centralserver.h
@@ -0,0 +1,46 @@
+/********************************************************************\
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+ *                                                                  *
+\********************************************************************/
+
+/* $Id: centralserver.h 901 2006-01-17 18:58:13Z mina $ */
+/** @file centralserver.h
+    @brief Functions to talk to the central server (auth/send stats/get rules/etc...)
+    @author Copyright (C) 2004 Philippe April <papril777@yahoo.com>
+*/
+
+#ifndef _CENTRALSERVER_H_
+#define _CENTRALSERVER_H_
+
+/** @brief Ask the central server to login a client */
+#define REQUEST_TYPE_LOGIN     "login"
+/** @brief Notify the the central server of a client logout */
+#define REQUEST_TYPE_LOGOUT    "logout"
+/** @brief Update the central server's traffic counters */
+#define REQUEST_TYPE_COUNTERS  "counters"
+
+/** @brief Initiates a transaction with the auth server */
+t_authcode auth_server_request(t_authresponse *authresponse, char *request_type, char *ip, char *mac, char *token, unsigned long long int incoming, unsigned long long int outgoing);
+
+/** @brief Tries really hard to connect to an auth server.  Returns a connected file descriptor or -1 on error */
+int connect_auth_server();
+
+/** @brief Helper function called by connect_auth_server() to do the actual work including recursion - do not call directly */
+int _connect_auth_server(int level);
+
+#endif /* _CENTRALSERVER_H_ */
---
+++ src/router/wifidog/src/ping_thread.c
@@ -0,0 +1,221 @@
+/********************************************************************\
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+ *                                                                  *
+\********************************************************************/
+
+/* $Id: ping_thread.c 901 2006-01-17 18:58:13Z mina $ */
+/** @file ping_thread.c
+    @brief Periodically checks in with the central auth server to make sure everything is running properly.
+    @author Copyright (C) 2004 Alexandre Carmel-Veilleux <acv@acv.ca>
+*/
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <signal.h>
+#include <errno.h>
+
+#include "../config.h"
+#include "safe.h"
+#include "common.h"
+#include "conf.h"
+#include "debug.h"
+#include "ping_thread.h"
+#include "util.h"
+
+static void ping(void);
+
+extern time_t started_time;
+
+/** Launches a thread that periodically checks in with the wifidog auth server to perform heartbeat function.
+@param arg NULL
+@todo This thread loops infinitely, need a watchdog to verify that it is still running?
+*/  
+void
+thread_ping(void *arg)
+{
+	pthread_cond_t		cond = PTHREAD_COND_INITIALIZER;
+	pthread_mutex_t		cond_mutex = PTHREAD_MUTEX_INITIALIZER;
+	struct	timespec	timeout;
+	
+	while (1) {
+		/* Make sure we check the servers at the very begining */
+		debug(LOG_DEBUG, "Running ping()");
+		ping();
+		
+		/* Sleep for config.checkinterval seconds... */
+		timeout.tv_sec = time(NULL) + config_get_config()->checkinterval;
+		timeout.tv_nsec = 0;
+
+		/* Mutex must be locked for pthread_cond_timedwait... */
+		pthread_mutex_lock(&cond_mutex);
+		
+		/* Thread safe "sleep" */
+		pthread_cond_timedwait(&cond, &cond_mutex, &timeout);
+
+		/* No longer needs to be locked */
+		pthread_mutex_unlock(&cond_mutex);
+	}
+}
+
+/** @internal
+ * This function does the actual request.
+ */
+static void
+ping(void)
+{
+	size_t			numbytes,
+				totalbytes;
+	int			sockfd, nfds, done;
+	char			request[MAX_BUF];
+	fd_set			readfds;
+	struct timeval		timeout;
+	FILE * fh;
+	unsigned long int sys_uptime  = 0;
+	unsigned int      sys_memfree = 0;
+	float             sys_load    = 0;
+
+
+	debug(LOG_DEBUG, "Entering ping()");
+	
+	sockfd = connect_auth_server();
+	if (sockfd == -1) {
+		/*
+		 * No auth servers for me to talk to
+		 */
+		return;
+	}
+
+	/*
+	 * Populate uptime, memfree and load
+	 */
+	if ((fh = fopen("/proc/uptime", "r"))) {
+		fscanf(fh, "%lu", &sys_uptime);
+		fclose(fh);
+	}
+	if ((fh = fopen("/proc/meminfo", "r"))) {
+		while (!feof(fh)) {
+			if (fscanf(fh, "MemFree: %u", &sys_memfree) == 0) {
+				/* Not on this line */
+				while (!feof(fh) && fgetc(fh) != '\n');
+			}
+			else {
+				/* Found it */
+				break;
+			}
+		}
+		fclose(fh);
+	}
+	if ((fh = fopen("/proc/loadavg", "r"))) {
+		fscanf(fh, "%f", &sys_load);
+		fclose(fh);
+	}
+
+	/*
+	 * Prep & send request
+	 */
+	snprintf(request, sizeof(request) - 1, "GET %sping/?gw_id=%s&sys_uptime=%lu&sys_memfree=%u&sys_load=%.2f&wifidog_uptime=%lu HTTP/1.0\r\n"
+			"User-Agent: WiFiDog %s\r\n"
+			"Host: %s\r\n"
+			"\r\n",
+			config_get_config()->auth_servers->authserv_path,
+			config_get_config()->gw_id,
+			sys_uptime,
+			sys_memfree,
+			sys_load,
+			(long unsigned int)((long unsigned int)time(NULL) - (long unsigned int)started_time),
+			VERSION,
+			config_get_config()->auth_servers->authserv_hostname);
+
+	debug(LOG_DEBUG, "HTTP Request to Server: [%s]", request);
+	
+	send(sockfd, request, strlen(request), 0);
+
+	debug(LOG_DEBUG, "Reading response");
+	
+	numbytes = totalbytes = 0;
+	done = 0;
+	do {
+		FD_ZERO(&readfds);
+		FD_SET(sockfd, &readfds);
+		timeout.tv_sec = 30; /* XXX magic... 30 second */
+		timeout.tv_usec = 0;
+		nfds = sockfd + 1;
+
+		nfds = select(nfds, &readfds, NULL, NULL, &timeout);
+
+		if (nfds > 0) {
+			/** We don't have to use FD_ISSET() because there
+			 *  was only one fd. */
+			numbytes = read(sockfd, request + totalbytes, MAX_BUF - (totalbytes + 1));
+			if (numbytes < 0) {
+				debug(LOG_ERR, "An error occurred while reading from auth server: %s", strerror(errno));
+				/* FIXME */
+				close(sockfd);
+				return;
+			}
+			else if (numbytes == 0) {
+				done = 1;
+			}
+			else {
+				totalbytes += numbytes;
+				debug(LOG_DEBUG, "Read %d bytes, total now %d", numbytes, totalbytes);
+			}
+		}
+		else if (nfds == 0) {
+			debug(LOG_ERR, "Timed out reading data via select() from auth server");
+			/* FIXME */
+			close(sockfd);
+			return;
+		}
+		else if (nfds < 0) {
+			debug(LOG_ERR, "Error reading data via select() from auth server: %s", strerror(errno));
+			/* FIXME */
+			close(sockfd);
+			return;
+		}
+	} while (!done);
+	close(sockfd);
+
+	debug(LOG_DEBUG, "Done reading reply, total %d bytes", totalbytes);
+
+	request[totalbytes] = '\0';
+
+	debug(LOG_DEBUG, "HTTP Response from Server: [%s]", request);
+	
+	if (strstr(request, "Pong") == 0) {
+		debug(LOG_WARNING, "Auth server did NOT say pong!");
+		/* FIXME */
+	}
+	else {
+		debug(LOG_DEBUG, "Auth Server Says: Pong");
+	}
+
+	return;	
+}
