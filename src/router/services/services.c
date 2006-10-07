d434507323e38bb0fc7c4b9d29bf34357c564e4f
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-10-07 21:47:38 +0000

    formatting
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@4000 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services.c
+++ src/router/services/services.c
@@ -1,7 +1,7 @@
 /*
  * services.c
  *
- * Copyright (C) 2006 Sebastian Gottschall <sebastian.gottschall@blueline-ag.de>
+ * Copyright (C) 2006 Sebastian Gottschall <gottschall@dd-wrt.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
