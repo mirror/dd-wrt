ef4d6be4dcf718604319d6e9a0a8eb8e8eedb8f9
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-10-08 16:49:22 +0000

    from Belkin 4.05.03 source, try to fix wan problem on some v1xxx units
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@4077 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/brcm/linux.v23/drivers/net/et/etc47xx.c
+++ src/linux/brcm/linux.v23/drivers/net/et/etc47xx.c
@@ -1083,14 +1083,13 @@ chipphyinit(struct bcm4xxx *ch, uint phyaddr)
 	phyid |=  chipphyrd( ch, phyaddr, 0x3) << 16;
 
 	if( phyid == 0x55210022) {
-		chipphywr( ch, phyaddr, 28, (uint16) (chipphyrd( ch, phyaddr, 28 ) & 0x0fff));
+		
+if (getRouterBrand () == ROUTER_RT210W)  //from Belkin 4.05.03 source, try to fix wan problem on some v1xxx units -Eko
+	{
 		chipphywr( ch, phyaddr, 30, (uint16) (chipphyrd( ch, phyaddr, 30 ) | 0x3000));
 		chipphywr( ch, phyaddr, 22, (uint16) (chipphyrd( ch, phyaddr, 22 ) & 0xffdf));
-		
-if (getRouterBrand () == ROUTER_RT210W)  //from Belkin sourece, try to fix wan problem on some v1xxx units
-	{
 		chipphyand(ch, phyaddr, 0, ~(1<<10)); // register0 10'bit reset,if system happen error,Wan breaking by ASKEY Colin 13/02/03 13:44:18,
-        chipphyand(ch, phyaddr, 16, (uint16) ~(1<<15));//  register16 15'bit reset, Jerry
+                chipphyand(ch, phyaddr, 16, (uint16) ~(1<<15));//  register16 15'bit reset, Jerry
 		chipphyand(ch, phyaddr, 0, ~(1<<11));//  register0's bit-11 reset
 		chipphyor(ch, phyaddr, 0, (1<<12));//  register0's bit-12 set
 		// the following set Register4's bit5-8
@@ -1099,11 +1098,18 @@ if (getRouterBrand () == ROUTER_RT210W)  //from Belkin sourece, try to fix wan p
 		chipphyor(ch, phyaddr, 4, (1<<7));
 		chipphyor(ch, phyaddr, 4, (1<<8));
 	}
+else  //default for all other routers
+	{
+		chipphywr( ch, phyaddr, 28, (uint16) (chipphyrd( ch, phyaddr, 28 ) & 0x0fff));
+		chipphywr( ch, phyaddr, 30, (uint16) (chipphyrd( ch, phyaddr, 30 ) | 0x3000));
+		chipphywr( ch, phyaddr, 22, (uint16) (chipphyrd( ch, phyaddr, 22 ) & 0xffdf));
+		
 		chipphywr( ch, phyaddr, 28, (uint16) ((chipphyrd( ch, phyaddr, 28 ) & 0x0fff) | 0x1000));
 		chipphywr( ch, phyaddr, 29, 1);
 		chipphywr( ch, phyaddr, 30, 4);
 
-		chipphywr( ch, phyaddr, 28, (uint16) (chipphyrd( ch, phyaddr, 28 ) & 0x0fff));					
+		chipphywr( ch, phyaddr, 28, (uint16) (chipphyrd( ch, phyaddr, 28 ) & 0x0fff));	
+	}				
 	}
 }
 
