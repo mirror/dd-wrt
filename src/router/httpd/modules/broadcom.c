86da81677390e02f06bd087ebc53a9ac192a7e31
botho <botho@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-10-08 16:33:17 +0000

    bug 1619
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@4075 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/broadcom.c
+++ src/router/httpd/modules/broadcom.c
@@ -3881,9 +3881,10 @@ ej_do_pagehead (int eid, webs_t wp, int argc, char_t ** argv)	//Eko
 {
 		char *style = nvram_get ("router_style");
 	
-			websWrite (wp,
+			/*websWrite (wp,
 				"<\?xml version=\"1.0\" encoding=\"%s\"\?>\n",
 				live_translate("lang_charset.set"));
+			IE Problem ...*/ 
 			websWrite (wp,
 				"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n");
 			websWrite (wp, "<html>\n");
