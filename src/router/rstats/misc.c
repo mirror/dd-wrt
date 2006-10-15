23c18599701e3170ca326714450e30a5d39561d1
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-10-15 14:51:28 +0000

    rstats from tomatoe
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@4157 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rstats/misc.c
@@ -0,0 +1,195 @@
+/*
+
+	Tomato Firmware
+	Copyright (C) 2006 Jonathan Zarate
+
+*/
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdarg.h>
+#include <syslog.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#include <bcmnvram.h>
+#include "shutils.h"
+#include "shared.h"
+#include "utils.h"
+
+
+int get_wan_proto(void)
+{
+	const char *names[] = {	// order must be synced with def at shared.h
+		"static",
+		"dhcp",
+		"heartbeat",
+		"l2tp",
+		"pppoe",
+		"pptp"
+	};
+	int i;
+	const char *p;
+	
+	p = nvram_safe_get("wan_proto");
+	for (i = 0; i < 6; ++i) {
+		if (strcmp(p, names[i]) == 0) return i + 1;
+	}
+	return WP_DISABLED;
+}
+
+int using_dhcpc(void)
+{
+	const char *proto = nvram_safe_get("wan_proto");
+	return (strcmp(proto, "dhcp") == 0) || (strcmp(proto, "l2tp") == 0) || (strcmp(proto, "heartbeat") == 0);
+}
+
+void notice_set(const char *path, const char *format, ...)
+{
+	char p[256];
+	char buf[2048];
+	va_list args;
+
+	mkdir("/var/notice", 0755);
+	snprintf(p, sizeof(p), "/var/notice/%s", path);
+	
+	va_start(args, format);
+	vsnprintf(buf, sizeof(buf), format, args);
+	va_end(args);
+	
+	f_write_string(p, buf, 0, 0);
+	if (buf[0]) syslog(LOG_INFO, "notice: %s", buf);
+}
+
+
+int check_wanup(void)
+{
+	int up = 0;
+	int proto;
+	char buf1[64];
+	char buf2[64];
+	const char *name;
+    int f;
+    struct ifreq ifr;
+	
+	proto = get_wan_proto();
+	if (proto == WP_DISABLED) return 0;
+	
+	if ((proto == WP_PPTP) || (proto == WP_L2TP) || (proto == WP_PPPOE) || (proto == WP_HEARTBEAT)) {
+		if (f_read_string("/tmp/ppp/link", buf1, sizeof(buf1)) > 0) {
+			if (proto == WP_HEARTBEAT) {
+				// contains pid of bpalogin
+				if (strcmp(psname(atoi(buf1), buf2, sizeof(buf2)), "bpalogin") == 0) {
+					up = 1;
+				}
+			}
+			else {
+				// contains the base name of a file in /var/run/ containing pid of a daemon
+				snprintf(buf2, sizeof(buf2), "/var/run/%s.pid", buf1);
+				if (f_read_string(buf2, buf1, sizeof(buf1)) > 0) {
+					name = psname(atoi(buf1), buf2, sizeof(buf2));
+					if (proto == WP_PPPOE) {
+						if (strcmp(name, "pppoecd") == 0) up = 1;
+					}
+					else {
+						if (strcmp(name, "pppd") == 0) up = 1;
+					}						
+				}
+			}
+			if (!up) {
+				unlink("/tmp/ppp/link");
+				cprintf("required daemon not found, assuming link is dead\n");
+			}
+		}
+	}
+	else if (!nvram_match("wan_ipaddr", "0.0.0.0")) {
+		up = 1;
+	}
+
+	if ((up) && ((f = socket(AF_INET, SOCK_DGRAM, 0)) >= 0)) {
+		strlcpy(ifr.ifr_name, nvram_safe_get("wan_iface"), sizeof(ifr.ifr_name));
+		if (ioctl(f, SIOCGIFFLAGS, &ifr) < 0) up = 0;
+		close(f);
+		if ((ifr.ifr_flags & IFF_UP) == 0) up = 0;
+	}
+
+	return up;
+}
+
+const dns_list_t *get_dns(void)
+{
+	static dns_list_t dns;
+	char s[512];
+	int n;
+	int i, j;
+	struct in_addr ia;
+	char d[4][16];
+
+	dns.count = 0;	
+	
+	strlcpy(s, nvram_safe_get("wan_dns"), sizeof(s));
+	if ((nvram_match("dns_addget", "1")) || (s[0] == 0)) {
+		n = strlen(s);
+		snprintf(s + n, sizeof(s) - n, " %s", nvram_safe_get("wan_get_dns"));
+	}
+
+	n = sscanf(s, "%15s %15s %15s %15s", d[0], d[1], d[2], d[3]);
+	for (i = 0; i < n; ++i) {
+		if (inet_pton(AF_INET, d[i], &ia) > 0) {
+			for (j = dns.count - 1; j >= 0; --j) {
+				if (dns.dns[j].s_addr == ia.s_addr) break;
+			}
+			if (j < 0) {
+				dns.dns[dns.count++].s_addr = ia.s_addr;
+				if (dns.count == 3) break;
+			}
+		}
+	}
+
+	return &dns;
+}
+
+// -----------------------------------------------------------------------------
+
+void set_action(int a)
+{
+	int r = 3;
+	while (f_write("/var/lock/action", &a, sizeof(a), 0, 0) != sizeof(a)) {
+		sleep(1);
+		if (--r == 0) return;
+	}
+	if (a != ACT_IDLE) sleep(2);
+}
+
+int check_action(void)
+{
+	int a;
+	int r = 3;
+	
+	while (f_read("/var/lock/action", &a, sizeof(a)) != sizeof(a)) {
+		sleep(1);
+		if (--r == 0) return ACT_UNKNOWN;
+	}
+	return a;
+}
+
+int wait_action_idle(int n)
+{
+	while (n-- > 0) {
+		if (check_action() == ACT_IDLE) return 1;
+		sleep(1);
+	}
+	return 0;
+}
+
+// -----------------------------------------------------------------------------
+
+int wl_client(void)
+{
+	return ((nvram_match("wl_mode", "sta")) || (nvram_match("wl_mode", "wet")));
+}
---
+++ src/router/rstats/shared.h
@@ -0,0 +1,94 @@
+#ifndef __SHARED_H__
+#define __SHARED_H__
+
+#include <netinet/in.h>
+#include <signal.h>
+
+
+#define Y2K			946684800UL		// seconds since 1970
+
+
+//version.c
+extern const char *tomato_version;
+extern const char *tomato_buildtime;
+
+
+// misc.c
+#define	WP_DISABLED		0		// order must be synced with def at misc.c
+#define	WP_STATIC		1
+#define WP_DHCP			2
+#define	WP_HEARTBEAT	3
+#define	WP_L2TP			4
+#define	WP_PPPOE		5
+#define	WP_PPTP			6
+
+
+typedef struct {
+	int count;
+	struct in_addr dns[3];
+} dns_list_t;
+
+extern int get_wan_proto(void);
+extern int using_dhcpc(void);
+extern void notice_set(const char *path, const char *format, ...);
+extern int check_wanup(void);
+extern const dns_list_t *get_dns(void);
+extern void set_action(int a);
+extern int check_action(void);
+extern int wait_action_idle(int n);
+extern int wl_client(void);
+
+
+// process.c
+extern char *psname(int pid, char *buffer, int maxlen);
+extern int pidof(const char *name);
+extern int killall(const char *name, int sig);
+
+
+// files.c
+#define FW_CREATE	0
+#define FW_APPEND	1
+#define FW_NEWLINE	2
+
+extern unsigned long f_size(const char *path);
+extern int f_exists(const char *file);
+extern int f_read(const char *file, void *buffer, int max);												// returns bytes read
+extern int f_write(const char *file, const void *buffer, int len, unsigned flags, unsigned cmode);		//
+extern int f_read_string(const char *file, char *buffer, int max);										// returns bytes read, not including term; max includes term
+extern int f_write_string(const char *file, const char *buffer, unsigned flags, unsigned cmode);		//
+extern int f_read_alloc(const char *path, char **buffer, int max);
+extern int f_read_alloc_string(const char *path, char **buffer, int max);
+
+
+// led.c
+#define LED_WLAN			0
+#define LED_DIAG			1
+#define LED_WHITE			2
+#define LED_AMBER			3
+#define LED_DMZ				4
+#define LED_USB				5	// SL
+#define LED_MAX				5
+
+#define	LED_OFF				0
+#define	LED_ON				1
+#define LED_BLINK			2	// (USB only)
+
+//	extern int gpio_outen(unsigned long bits, unsigned long mask);
+//	extern unsigned long rgpio(unsigned long bits);
+//	extern void wgpio(unsigned long bits, unsigned long mask);
+extern void led(int which, int mode);
+
+
+// base64.c
+extern const char base64_xlat[];
+extern int base64_encode(unsigned char *in, char *out, int inlen);			// returns amount of out buffer used
+extern int base64_decode(const char *in, unsigned char *out, int inlen);	// returns amount of out buffer used
+extern int base64_encoded_len(int len);
+extern int base64_decoded_len(int len);										// maximum possible, may not be actual
+
+
+// strings.c
+//	#define vstrsep(buf, sep, args...) _vstrsep(buf, sep, args, NULL)
+//	extern int _vstrsep(char *buf, const char *sep, ...);
+
+#endif
