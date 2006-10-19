06237f6b4aa899c51dbf4eef978359cde3444679
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-10-19 15:20:59 +0000

    snmp rssi values update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@4206 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/others/wl_snmpd.sh
+++ src/router/others/wl_snmpd.sh
@@ -2,44 +2,56 @@
 
 place=".1.3.6.1.4.1.2021.255"
 
-id=1
-lastid=0
+refresh() {
 
-for mac in $(wl assoclist | cut -d" " -f2)
-do
-  if test $lastid -eq 0
+  id=1
+  lastid=0
+  noise_reference=$(nvram get noise_reference)
+  
+  for mac in $(wl assoclist | cut -d" " -f2)
+  do
+    if test $lastid -eq 0
+    then
+      getnext_1361412021255="$place.3.54.1.3.32.1.1.1"
+      getnext_1361412021255354133211="$place.3.54.1.3.32.1.1.1"
+      getnext_1361412021255354133214="$place.3.54.1.3.32.1.4.1"
+      getnext_13614120212553541332126="$place.3.54.1.3.32.1.26.1"
+    else
+      eval getnext_1361412021255354133211${lastid}="$place.3.54.1.3.32.1.1.$id"
+      eval getnext_1361412021255354133214${lastid}="$place.3.54.1.3.32.1.4.$id"
+      eval getnext_13614120212553541332126${lastid}="$place.3.54.1.3.32.1.26.$id"
+    fi
+  
+    rssi=$(wl rssi $mac | cut -d" " -f3)
+    if test $rssi -eq 0
+    then
+      snr=0
+    else
+      let snr=-1*$noise_reference+$rssi
+    fi
+    mac=$(echo $mac | tr : ' ')
+  
+    eval value_1361412021255354133211${id}=$id;
+    eval type_1361412021255354133211${id}='integer';
+    eval value_1361412021255354133214${id}='$mac';
+    eval type_1361412021255354133214${id}='octet';
+    eval value_13614120212553541332126${id}=$snr;
+    eval type_13614120212553541332126${id}='integer';
+
+    lastid=$id
+    let id=$id+1
+  
+  done
+
+  if test $lastid -ne 0
   then
-    getnext_1361412021255="$place.3.54.1.3.32.1.1.1"
-    getnext_1361412021255354133211="$place.3.54.1.3.32.1.1.1"
-    getnext_1361412021255354133214="$place.3.54.1.3.32.1.4.1"
-    getnext_13614120212553541332126="$place.3.54.1.3.32.1.26.1"
-  else
-    eval getnext_1361412021255354133211${lastid}="$place.3.54.1.3.32.1.1.$id"
-    eval getnext_1361412021255354133214${lastid}="$place.3.54.1.3.32.1.4.$id"
-    eval getnext_13614120212553541332126${lastid}="$place.3.54.1.3.32.1.26.$id"
+    eval getnext_1361412021255354133211${lastid}="$place.3.54.1.3.32.1.4.1"
+    eval getnext_1361412021255354133214${lastid}="$place.3.54.1.3.32.1.26.1"
+    eval getnext_13614120212553541332126${lastid}="NONE"
   fi
+} 
 
-  let rssi=$(wl rssi $mac | cut -d" " -f3)-$(nvram get noise_reference)
-  mac=$(echo $mac | tr : ' ')
-
-  eval value_1361412021255354133211${id}=$id;
-  eval type_1361412021255354133211${id}='integer';
-  eval value_1361412021255354133214${id}='$mac';
-  eval type_1361412021255354133214${id}='octet';
-  eval value_13614120212553541332126${id}=$rssi;
-  eval type_13614120212553541332126${id}='integer';
-
-  lastid=$id
-  let id=$id+1
-
-done
-
-if test $lastid -ne 0
-then
-  eval getnext_1361412021255354133211${lastid}="$place.3.54.1.3.32.1.4.1"
-  eval getnext_1361412021255354133214${lastid}="$place.3.54.1.3.32.1.26.1"
-  eval getnext_13614120212553541332126${lastid}="NONE"
-fi
+LASTREFRESH=0
 
 while read CMD
 do
@@ -50,6 +62,13 @@ do
       ;;
     getnext)
       read REQ
+      let REFRESH=$(date +%s)-$LASTREFRESH
+      if test "x$REQ" = "x${place}.3.54.1.3.32.1.1" && test $REFRESH -gt 10
+      then
+        LASTREFRESH=$(date +%s)
+        refresh
+      fi
+      
       oid=$(echo $REQ | tr -d .) 
       eval ret=\$getnext_${oid}
       if test "x$ret" = "xNONE"
