304289a3d63554650b1caaa6c7937195900bf127
eko <USER@DOMAIN>
2006-10-21 08:10:16 +0000

    test to read phy
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@4254 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/brcm/linux.v23/drivers/net/b44.c
+++ src/linux/brcm/linux.v23/drivers/net/b44.c
@@ -508,10 +508,11 @@ static int b44_setup_phy(struct b44 *bp)
 //		printk(KERN_WARNING PFX "PHY: cannot reset MII transceiver isolate bit.\n");
 	}
 
-	if ((bp->pdev->device == PCI_DEVICE_ID_BCM4713) && (__b44_readphy(bp, 0, MII_BMCR, &val) == 0))
-			nvram_set ("altima_phy", "1");
-		else
-			nvram_set ("altima_phy", "0");
+	if ((bp->pdev->device == PCI_DEVICE_ID_BCM4713) && (__b44_readphy(bp, 3, MII_BMCR, &val) == 0))
+		{
+			char buffer[16];
+			nvram_set ("phy_type", itoa (val, buffer, 16));
+		}
 		
 	if (bp->phy_addr == B44_PHY_ADDR_NO_PHY)
 		return 0;
