5532c13fc772279e1b663e8f3133cfaa258296e0
BrainSlayer <s.gottschall@dd-wrt.com>
2006-10-23 11:14:30 +0000

    x86 fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@4312 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/network.c
+++ src/router/services/network.c
@@ -1150,6 +1150,14 @@ start_lan (void)
       nvram_set ("wl0_hwaddr", ether_etoa (ifr.ifr_hwaddr.sa_data, eabuf));
     }
 #endif
+#ifdef HAVE_X86
+  strncpy (ifr.ifr_name, "ath0", IFNAMSIZ);
+  if (ioctl (s, SIOCGIFHWADDR, &ifr) == 0)
+    {
+      char eabuf[32];
+      nvram_set ("wl0_hwaddr", ether_etoa (ifr.ifr_hwaddr.sa_data, eabuf));
+    }
+#endif
 #ifdef HAVE_XSCALE
   strncpy (ifr.ifr_name, "ath0", IFNAMSIZ);
   if (ioctl (s, SIOCGIFHWADDR, &ifr) == 0)
