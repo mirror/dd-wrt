f764f4190b43c2bbf74d465ebb003ac9c6de735c
botho <botho@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-10-28 17:00:57 +0000

    add ip_conntrack_table
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@4353 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/broadcom.h
+++ src/router/shared/broadcom.h
@@ -167,6 +167,8 @@ extern int dhcp_renew (webs_t wp);
 extern int dhcp_release (webs_t wp);
 extern int stop_ppp (webs_t wp);
 extern void ej_show_status_setting (int eid, webs_t wp, int argc, char_t ** argv);
+extern void ej_dumpip_conntrack (int eid, webs_t wp, int argc, char_t ** argv);
+extern void ej_ip_conntrack_table (int eid, webs_t wp, int argc, char_t ** argv);
 
 
 /*for dhcp */
@@ -315,8 +317,6 @@ extern int diag_ping_clear (webs_t wp);
 extern int ping_onload (webs_t wp, char *arg);
 //extern int traceroute_onload (webs_t wp, char *arg);
 
-/* Added by Botho 03.April.06 */
-extern void ej_dumpip_conntrack (int eid, webs_t wp, int argc, char_t ** argv);
 /* Added by Botho 21.April.06 */
 extern void ej_js_include (int eid, webs_t wp, int argc, char_t ** argv);
 extern void ej_css_include (int eid, webs_t wp, int argc, char_t ** argv);
