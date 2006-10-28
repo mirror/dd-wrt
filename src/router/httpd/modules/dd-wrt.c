d3ae6a613318a6071e511681ff24e9edd5a75ea3
botho <botho@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-10-28 18:05:42 +0000

    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@4364 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/dd-wrt.c
+++ src/router/httpd/modules/dd-wrt.c
@@ -5831,11 +5831,7 @@ ej_ip_conntrack_table (int eid, webs_t wp, int argc, char_t ** argv)
     
   while (fgets(line, sizeof(line), fp) != NULL)
   {
-  	protocol[] ="";
-  	srcip[] = "";
-  	dstip[] = "";
-  	dstport[] = "";
-  	
+  	  	
   	websWrite (wp, "<tr>\n");
   	
   	// Nb
