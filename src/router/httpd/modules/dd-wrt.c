0f3475de9cfaa3ebd6c43ceb63dfd4ca63b185a4
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-10-31 14:00:35 +0000

    unlink commented
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@4409 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/dd-wrt.c
+++ src/router/httpd/modules/dd-wrt.c
@@ -6128,7 +6128,7 @@ void ej_bandwidth(int eid, webs_t wp, int argc, char_t ** argv)
 		killall("rstats", sig);
 		wait_file_exists(name, 5, 0);
 		do_file(name,wp);
-		unlink(name);
+//		unlink(name);
 	}
 }
 #endif
