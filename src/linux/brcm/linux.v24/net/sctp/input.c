6104629bb0994a47283c85f86cc20ea6c53b1d25
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-11-07 22:29:57 +0000

    blahhhhh
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@4598 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/brcm/linux.v24/net/sctp/input.c
+++ src/linux/brcm/linux.v24/net/sctp/input.c
@@ -123,6 +123,9 @@ int sctp_rcv(struct sk_buff *skb)
 
 	SCTP_INC_STATS_BH(SctpInSCTPPacks);
 
+	if (skb_linearize(skb, GFP_ATOMIC) != 0)
+		goto discard_it;
+
 	sh = (struct sctphdr *) skb->h.raw;
 
 	/* Pull up the IP and SCTP headers. */
