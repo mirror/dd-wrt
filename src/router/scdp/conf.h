9bdeca6aa947dd31b387340ae0abc938fd33d6ac
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-11-09 14:41:30 +0000

    scdp added
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@4642 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/scdp/conf.h
@@ -0,0 +1,36 @@
+/* conf.h.  Generated automatically by configure.  */
+/* conf.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define to empty if the keyword does not work.  */
+/* #undef const */
+
+/* Define if you have the ANSI C header files.  */
+/* #undef STDC_HEADERS */
+
+/* Define if you have the gethostname function.  */
+/* #undef HAVE_GETHOSTNAME */
+
+/* Define if you have the getopt_long function.  */
+#define HAVE_GETOPT_LONG 1
+
+/* Define if you have the strspn function.  */
+/* #undef HAVE_STRSPN */
+
+/* Define if you have the uname function.  */
+/* #undef HAVE_UNAME */
+
+/* Define if you have the <getopt.h> header file.  */
+#define HAVE_GETOPT_H 1
+
+/* Define if you have the <sys/utsname.h> header file.  */
+#define HAVE_SYS_UTSNAME_H 1
+
+/* Define if you have the <unistd.h> header file.  */
+#define HAVE_UNISTD_H 1
+
+/* Name of package */
+#define PACKAGE "scdp"
+
+/* Version number of package */
+#define VERSION "1.0"
+
