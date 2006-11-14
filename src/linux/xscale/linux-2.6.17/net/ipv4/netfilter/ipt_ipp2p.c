82d784f76d0e3553c4131bea1bec0671afddec96
BrainSlayer <s.gottschall@dd-wrt.com>
2006-11-14 00:25:00 +0000

    ipp2p update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@4772 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/xscale/linux-2.6.17/net/ipv4/netfilter/ipt_ipp2p.c
+++ src/linux/xscale/linux-2.6.17/net/ipv4/netfilter/ipt_ipp2p.c
@@ -5,7 +5,7 @@
 #include <linux/netfilter_ipv4/ip_tables.h>
 #include <linux/version.h>
 //#include <linux/netfilter_ipv4/ipt_ipp2p.h>
-#include "ipt_ipp2p.h"
+#include <linux/netfilter_ipv4/ipt_ipp2p.h>
 #include <net/tcp.h>
 #include <net/udp.h>
 
