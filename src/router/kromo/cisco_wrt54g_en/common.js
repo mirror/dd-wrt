9ddeffed2e3b6548e6cbe78b4b53652466063447
eko <USER@DOMAIN>
2006-11-15 09:50:27 +0000

    js fix, botho
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@4802 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/cisco_wrt54g_en/common.js
+++ src/router/kromo/cisco_wrt54g_en/common.js
@@ -797,16 +797,10 @@ function StatusUpdate(_url, _frequency) {
 }
 
 // Gray all form when submitting
-function apply(form) {
-	/*
-	for (i = 0; i < form.elements.length; i++) {
-		if(defined(form.elements[i].disabled)) 
-			form.elements[i].disabled = true;
-	}
-	if (form.contents) document.getElementById("contents").style.color = '#999999';
-	*/
-	Dialog.info(errmsg.err100, {windowParameters: {className: "ddwrt", width:250, height:100}, showProgress: true});
-	form.submit();
+function apply(form, text) {
+    if(!text) text=errmsg.err100;
+    Dialog.info(text, {windowParameters: {className: "ddwrt", width:250, height:100}, showProgress: true});
+    setTimeout(function(){form.submit();}, 2000);
 }
 
 // Class for sending a request using ajax
@@ -1041,6 +1035,8 @@ function submitFooterButton(sub, res, reb, autoref, ref, clo) {
 	if(clo)
 		document.write("<input class=\"button\" type=\"button\" name=\"close_button\" value=\"" + sbutton.clos + "\" onclick=\"self.close();\" />");
 }
+
+
 
 
 
