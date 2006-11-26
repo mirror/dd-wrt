cced8bca2c7da586aa604af7cbf284a1329d1d7c
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-11-26 18:43:47 +0000

    wiviz stuff
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@4901 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/wiviz/wl_access.h
+++ src/router/wiviz/wl_access.h
@@ -13,52 +13,60 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Wi-viz; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+*/  
 //wl_access.h - functions for accessing broadcom crap
-
+  
 #define WL_DEVICE "eth1"
-
 typedef unsigned int uint32;
 typedef unsigned char uchar;
 typedef int bool;
-
-typedef struct ether_addr {
-	uchar addr[6];
+typedef struct ether_addr
+{
+  uchar addr[6];
 } ether_addr_t;
-
-typedef struct wlc_ssid {
-	uint32		SSID_len;
-	uchar		SSID[32];
+typedef struct wlc_ssid
+{
+  uint32 SSID_len;
+  uchar SSID[32];
 } wlc_ssid_t;
-/* For ioctls that take a list of MAC addresses */
-typedef struct maclist {
-	uint count;			/* number of MAC addresses */
-	struct ether_addr ea[1];	/* variable length array of MAC addresses */
-} maclist_t;
-/* Linux network driver ioctl encoding */
-typedef struct wl_ioctl {
-	uint cmd;	/* common ioctl definition */
-	void *buf;	/* pointer to user buffer */
-	uint len;	/* length of user buffer */
-	bool set;	/* get or set request (optional) */
-	uint used;	/* bytes read or written (optional) */
-	uint needed;	/* bytes needed (optional) */
-} wl_ioctl_t;
-/* channel encoding */
-typedef struct channel_info {
-	int hw_channel;
-	int target_channel;
-	int scan_channel;
-} channel_info_t;
-/* RSSI info for sta */
-typedef struct sta_rssi {
-  int RSSI;
-  char mac[6];
-  u_short zero_ex_forty_one;
-  } sta_rssi_t;
-/* check this magic number */
-#define WLC_IOCTL_MAGIC		0x14e46c77
 
+/* For ioctls that take a list of MAC addresses */ 
+  typedef struct maclist
+{
+  uint count;			/* number of MAC addresses */
+  struct ether_addr ea[1];	/* variable length array of MAC addresses */
+ } maclist_t;
+
+/* Linux network driver ioctl encoding */ 
+  typedef struct wl_ioctl
+{
+  uint cmd;			/* common ioctl definition */
+  void *buf;			/* pointer to user buffer */
+   uint len;			/* length of user buffer */
+   bool set;			/* get or set request (optional) */
+   uint used;			/* bytes read or written (optional) */
+   uint needed;		/* bytes needed (optional) */
+ } wl_ioctl_t;
+
+/* channel encoding */ 
+  typedef struct channel_info
+{
+  int hw_channel;
+   int target_channel;
+   int scan_channel;
+ } channel_info_t;
+
+/* RSSI info for sta */ 
+  typedef struct sta_rssi
+{
+  int RSSI;
+   char mac[6];
+   u_short zero_ex_forty_one;
+ } sta_rssi_t;
+
+/* check this magic number */ 
+#define WLC_IOCTL_MAGIC		0x14e46c77
+  
 #define WLC_GET_MAGIC				0
 #define WLC_GET_BSSID				23
 #define WLC_SET_BSSID				24
@@ -72,6 +80,5 @@ typedef struct sta_rssi {
 #define WLC_SET_AP				118
 #define WLC_GET_RSSI				127
 #define WLC_GET_ASSOCLIST			159
+int wl_ioctl (char *name, int cmd, void *buf, int len);
 
-
-int wl_ioctl(char *name, int cmd, void *buf, int len);
