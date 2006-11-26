104f4287c176d883310e13873161d159e7dcc11c
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-11-26 11:16:49 +0000

    still don't get the succ. page
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@4893 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/upgrade.c
+++ src/router/httpd/modules/upgrade.c
@@ -47,7 +47,7 @@ do_upgrade_cgi (char *url, webs_t stream)	//jimmy, https, 8/6/2003
 
   /* Reboot if successful */
   if (upgrade_ret == 0) {
-  	sleep (5);
+  	sleep (10);
   	sys_reboot ();
   }
   
