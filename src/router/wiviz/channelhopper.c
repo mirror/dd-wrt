dc833fc51ed5f5767ca2fb604d29961bb0b1f027
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-11-26 18:43:47 +0000

    wiviz stuff
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@4901 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/wiviz/channelhopper.c
+++ src/router/wiviz/channelhopper.c
@@ -13,7 +13,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Wi-viz; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+*/  
 #include <stdio.h>
 #include <pcap.h>
 #include <signal.h>
@@ -21,28 +21,34 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "wl_access.h"
 #include "channelhopper.h"
 #include "structs.h"
+  void
+ch_sig_handler (int i)
+{
+} void
 
-void ch_sig_handler(int i) {
-
-  }
-
-void channelHopper(wiviz_cfg * cfg) {
+channelHopper (wiviz_cfg * cfg)
+{
   int hopPos;
   int nc;
-
-  //Turn off signal handling from parent process
-  signal(SIGUSR1, &ch_sig_handler);
-  signal(SIGUSR2, &ch_sig_handler);
-
-  //Start hoppin'!
-  hopPos = 0;
-  while (1) {
-    nc = cfg->channelHopSeq[hopPos];
-    hopPos = (hopPos + 1) % cfg->channelHopSeqLen;
-    //Set the channel
-    fprintf(stderr, "It sets the channel to %i\n", nc);
-    wl_ioctl(WL_DEVICE, WLC_SET_CHANNEL, &nc, 4);
-    //Sleep
-    usleep(cfg->channelDwellTime * 1000);
+  
+    //Turn off signal handling from parent process
+    signal (SIGUSR1, &ch_sig_handler);
+  signal (SIGUSR2, &ch_sig_handler);
+  
+    //Start hoppin'!
+    hopPos = 0;
+  while (1)
+    {
+      nc = cfg->channelHopSeq[hopPos];
+      hopPos = (hopPos + 1) % cfg->channelHopSeqLen;
+      
+	//Set the channel
+	fprintf (stderr, "It sets the channel to %i\n", nc);
+      wl_ioctl (WL_DEVICE, WLC_SET_CHANNEL, &nc, 4);
+      
+	//Sleep
+	usleep (cfg->channelDwellTime * 1000);
     }
-  }
+}
+
+
