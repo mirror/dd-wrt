6de9e5291ac49e3ec7ad0e8146aee5e6136f81a2
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-11-29 17:29:07 +0000

    reboot code fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@4958 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rc/event.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <shutils.h>
+
+int
+event_main (int argc, char **argv)
+{
+  pid_t pid;
+  pid = fork ();
+  switch (pid)
+    {
+    case -1:
+      perror ("fork failed");
+      exit (1);
+      break;
+    case 0:
+      sleep(atoi(argv[1]));
+      kill(atoi(argv[2]),atoi(argv[3]));
+      exit (0);
+      break;
+    default:
+      _exit (0);
+      break;
+    }
+}
