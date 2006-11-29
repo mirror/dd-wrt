4370cab0f4d0d85025b69793b3c791a7c0ae95d6
BrainSlayer <s.gottschall@dd-wrt.com>
2006-11-29 15:26:43 +0000

    mac layers
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@4944 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/dd-wrt.c
+++ src/router/httpd/modules/dd-wrt.c
@@ -6360,7 +6360,7 @@ show_macfilter_if(wp,"wl");
 void
 do_filtertable (char *path, webs_t stream)
 {
-char *ifname = &path[indexof(path,".")+1];
+char *ifname = &path[indexof(path,'.')+1];
 char *webfile = getWebsFile ("WL_FilterTable.asp");
 char temp[4096];
 sprintf(temp,webfile,ifname,ifname,ifname);
