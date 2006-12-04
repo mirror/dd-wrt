e8a77a485f2f0e36b6f034c1a071c85533b94f4f
eko <USER@DOMAIN>
2006-12-04 10:04:06 +0000

    typo: image path
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@5051 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/wiviz/www/wiviz/wiviz.js
+++ src/router/wiviz/www/wiviz/wiviz.js
@@ -172,7 +172,7 @@ function repip() {
 			  y += 10;
 			  d = Math.sqrt(dx*dx+dy*dy);
 			  for (j = 0; j < d; j += 15) {
-			    nh += "<img src=images/wiviz/'"
+			    nh += "<img src='../images/wiviz/"
 					  + ((hs.age < idle_timeout) ? "pip" : "pip-idle")
 					  + (stupid ? ".gif" : ".png")
 						+ "' class='pip' style='top:"
@@ -247,7 +247,7 @@ function declump() {
 
 function genHTML(hs) {
 	var nh = '';
-  nh += "<center><img class='icon' src=images/wiviz/'"
+  nh += "<center><img class='icon' src='../images/wiviz/"
 	a = hs.descarr;
 	if (a[0] == 'ap' || a[0] == 'adhoc') {
 	  if (a[0] == 'ap') {
