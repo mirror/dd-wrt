602c8547cbab09809657ade3dbd7160a1d1c09ba
eko <USER@DOMAIN>
2006-12-04 17:52:24 +0000

    working, but try this too
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@5071 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/site_survey.c
+++ src/router/httpd/modules/site_survey.c
@@ -183,23 +183,23 @@ ej_dump_wiviz_plus_site_survey (int eid, webs_t wp, int argc, char_t ** argv)  /
 //	(site_survey_lists[i].capability & DOT11_CAP_PRIVACY) ? "No" : "Yes";
 	(site_survey_lists[i].capability & DOT11_CAP_PRIVACY) ? "unenc-na" : "enc-unknown";
 
-/*			websWrite (wp,
-			"new Array(\'%s\', %d, \'ap-channel-%s-ssid-",
+			websWrite (wp,
+			"  new Array(\'%s\', %d, \'ap-channel-%d-ssid-",
 			site_survey_lists[i].BSSID, site_survey_lists[i].RSSI, site_survey_lists[i].channel);
 		  
 			for (j = 0; j < strlen(site_survey_lists[i].SSID); j++)
 				websWrite (wp, "\\x%02X", *((char *) site_survey_lists[i].SSID + j) & 0xFF);
 			
-			websWrite (wp, "-%s\', 0),\n", open); */
+			websWrite (wp, "-%s\', 0),\n", open); 
 
 
-      websWrite (wp,
-		 "  new Array(\"%s\", \"%d\", \"ap-channel-%d-ssid-%s-%s\"),\n",
+ /*     websWrite (wp,
+		 "  new Array(\'%s\', %d, \'ap-channel-%d-ssid-%s-%s\', 0),\n",
 		 site_survey_lists[i].BSSID,
 		 site_survey_lists[i].RSSI, 
 		 site_survey_lists[i].channel,
 		 site_survey_lists[i].SSID[0]==0?"hidden":site_survey_lists[i].SSID,
-		 open);
+		 open); */
 	
     }
 		}
