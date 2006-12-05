c1ab4344ac08e6f20d78591b425ae5f862ead00c
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-12-05 11:56:37 +0000

    kill wiviz even before you run it, (strange, hehe)
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@5109 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rc/kill_wiviz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <shutils.h>
+
+int
+kill_wiviz_main (void)
+{
+  pid_t pid;
+  pid = fork ();
+  switch (pid)
+    {
+    case -1:
+      perror ("fork failed");
+      exit (1);
+      break;
+    case 0:
+    	sleep (3);
+		eval ("killall", "-USR1", "wiviz", ">/dev/null", "2>&1");	// then kill it to get data
+      exit (0);
+      break;
+    default:
+      _exit (0);
+      break;
+    }
+}
