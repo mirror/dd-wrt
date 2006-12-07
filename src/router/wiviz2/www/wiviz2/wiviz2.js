bbaf11672ef07440ed7055995fecc236a68bacb3
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-12-07 17:10:21 +0000

    typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@5165 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/wiviz2/www/wiviz2/wiviz2.js
+++ src/router/wiviz2/www/wiviz2/wiviz2.js
@@ -620,7 +620,7 @@ function wiviz_callback(hosts, cfgobj) {
 			}
 			el = document.createElement('img');
 			el.className = 'sizeimage';
-			el.src = "/images/wiviz/" + h.type + (h.encrypted == 'yes' ? "-enc" : "") + ".png";
+			el.src = "images/wiviz/" + h.type + (h.encrypted == 'yes' ? "-enc" : "") + ".png";
 			if (IEfix) {
 				var ne = document.createElement('a');
 				ne.style.width = '50px';
