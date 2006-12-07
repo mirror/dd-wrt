de11aa3ac7afab2d87985ab01c2e8849c9e98fcd
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2006-12-07 15:20:12 +0000

    wiviz2 added
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@5158 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/wiviz2/wl_access.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+
+#include "wl_access.h"
+
+int wl_ioctl(char *name, int cmd, void *buf, int len)
+{
+	struct ifreq ifr;
+	wl_ioctl_t ioc;
+	int ret = 0;
+ 	int s;
+
+	/* open socket to kernel */
+	if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		perror("socket");
+		return errno;
+	}
+
+	/* do it */
+	ioc.cmd = cmd;
+	ioc.buf = buf;
+	ioc.len = len;
+	strncpy(ifr.ifr_name, name, IFNAMSIZ);
+	ifr.ifr_data = (caddr_t) &ioc;
+	ret = ioctl(s, SIOCDEVPRIVATE, &ifr);
+
+	/* cleanup */
+	close(s);
+	return ret;
+}
+
+int get_mac(char *name, void *buf)
+{
+	struct ifreq ifr;
+	int ret = 0;
+ 	int s;
+
+	/* open socket to kernel */
+	if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		perror("socket");
+		return errno;
+	}
+
+	strncpy(ifr.ifr_name, name, IFNAMSIZ);
+	//ifr.ifr_data = (caddr_t) buf;
+	if ((ret = ioctl(s, SIOCGIFHWADDR, &ifr)) < 0)
+			perror(ifr.ifr_name);
+
+	/* cleanup */
+	close(s);
+	memcpy(buf, &ifr.ifr_hwaddr.sa_data, 6);
+	return ret;
+}
