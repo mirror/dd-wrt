4a89a27507161e4db186926ca48ca8ba43adda92
BrainSlayer <s.gottschall@dd-wrt.com>
2006-12-11 13:36:12 +0000

    latest stuff
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@5242 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/linux/x86/linux-2.6.17/include/linux/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 37 */
+#define UTS_MACHINE "i386"
+#define UTS_VERSION "#37 Mon Dec 11 02:59:38 CET 2006"
+#define LINUX_COMPILE_TIME "02:59:38"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "linux"
+#define LINUX_COMPILE_DOMAIN "site"
+#define LINUX_COMPILER "gcc version 4.2.0 20060709 (experimental) DD-WRT Platform"
