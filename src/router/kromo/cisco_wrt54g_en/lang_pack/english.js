68c23ad0454349363d6a05ec010941599170c27c
botho <USER@DOMAIN>
2007-01-24 09:22:30 +0000

    Translation
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@5646 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/cisco_wrt54g_en/lang_pack/english.js
+++ src/router/kromo/cisco_wrt54g_en/lang_pack/english.js
@@ -1205,6 +1205,8 @@ status_lan.h22="Dynamic Host Configuration Protocol";
 status_lan.legend2="DHCP Status";
 status_lan.legend3="DHCP Clients";
 status_lan.legend4="Active Clients";
+status_lan.concount="Conn. Count";
+status_lan.conratio="Ratio [<% nvram_get("ip_conntrack_max"); %>]";
 
 //help container
 var hstatus_lan = new Object();
