38cbc60f77d1fe4095daa5ff99be0b6f578d313d
BrainSlayer <s.gottschall@dd-wrt.com>
2007-01-29 16:06:16 +0000

    atheros buffer overflow fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@5706 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/utils/wl_atheros.c
+++ src/router/utils/wl_atheros.c
@@ -1,54 +1,141 @@
 #include <stdio.h>
 #include <wlutils.h>
+#include <shutils.h>
 
 
-
-int main(int argc,char *argv[])
+void
+showinterface (char *ifname)
 {
-if (argc<2)
+  char buf[8192];
+  int cnt = getassoclist (ifname, buf);
+  int count;
+  memcpy (&count, buf, 4);
+  unsigned char *p = &buf[4];
+  int a;
+  int pos = 0;
+  for (a = 0; a < cnt; a++)
     {
-    fprintf(stderr,"invalid argument\n");
-    return 0;
+      fprintf (stdout, "assoclist %2.2X:%2.2X:%2.2X:%2.2X:%2.2X:%2.2X\n",
+	       p[pos], p[pos + 1], p[pos + 2], p[pos + 3], p[pos + 4],
+	       p[pos + 5]);
+      pos += 6;
     }
-char *ifname="ath0";
-int i;
-for (i=1;i<argc;i++)
+
+
+}
+int
+main (int argc, char *argv[])
+{
+  if (argc < 2)
     {
-    if (!strcmp(argv[i],"-i"))
+      fprintf (stderr, "invalid argument\n");
+      return 0;
+    }
+  char *ifname = "ath0";
+  int i;
+  for (i = 1; i < argc; i++)
+    {
+      if (!strcmp (argv[i], "-i"))
 	{
-	ifname = argv[i++];
-	continue;
+	  ifname = argv[++i];
+	  continue;
 	}
-    if (!strcmp(argv[i],"assoclist"))
+      if (!strcmp (argv[i], "assoclist"))
 	{
-	char buf[8192];
-	getassoclist(ifname,buf);
-	int count;
-	memcpy(&count,buf,4);
-	unsigned char *p=&buf[4];
-	int a;
-	int pos=0;
-	for (a=0;a<count;a++)
+	  int ifcount = getifcount ("wifi");
+	  if (strcmp (argv[1], "-i") == 0)
+	    showinterface (ifname);
+	  else
 	    {
-	    fprintf(stdout,"assoclist %2.2X:%2.2X:%2.2X:%2.2X:%2.2X:%2.2X\n",p[pos],p[pos+1],p[pos+2],p[pos+3],p[pos+4],p[pos+5]);
-	    pos+=6;
+	      int c = 0;
+	      for (c = 0; c < ifcount; c++)
+		{
+		  char interface[32];
+		  sprintf (interface, "ath%d", c);
+		  showinterface (interface);
+		  char vif[32];
+		  sprintf (vif, "%s_vifs", interface);
+		  char var[80], *next;
+		  char *vifs = nvram_safe_get (vif);
+		  if (vifs != NULL)
+		    {
+		      foreach (var, vifs, next)
+		      {
+			showinterface (var);
+		      }
+		    }
+		}
 	    }
+
 	}
-    if (!strcmp(argv[i],"rssi"))
+      if (!strcmp (argv[i], "rssi"))
 	{
-	unsigned char rmac[6];
-	ether_atoe(argv[++i],rmac);
-	int rssi = getRssi(ifname,rmac);
-	fprintf(stdout,"rssi is %d\n",rssi);
+	  unsigned char rmac[6];
+	  ether_atoe (argv[++i], rmac);
+	    int ifcount = getifcount ("wifi");
+	  int rssi=-1;
+	  if (strcmp (argv[1], "-i") == 0)
+	    rssi = getRssi (ifname, rmac);
+	  else
+	    {
+	      int c = 0;
+	      for (c = 0; c < ifcount; c++)
+		{
+		  char interface[32];
+		  sprintf (interface, "ath%d", c);
+		      if (rssi==-1)
+		        rssi = getRssi(interface, rmac);
+		  char vif[32];
+		  sprintf (vif, "%s_vifs", interface);
+		  char var[80], *next;
+		  char *vifs = nvram_safe_get (vif);
+		  if (vifs != NULL)
+		    {
+		      foreach (var, vifs, next)
+		      {
+		      if (rssi==-1)
+		        rssi = getRssi(var, rmac);
+		      else break;
+		      }
+		    }
+		}
+	    }
+
+	  fprintf (stdout, "rssi is %d\n", rssi);
 	}
-    if (!strcmp(argv[i],"noise"))
+      if (!strcmp (argv[i], "noise"))
 	{
-	unsigned char rmac[6];
-	ether_atoe(argv[++i],rmac);
-	int rssi = getNoise(ifname,rmac);
-	fprintf(stdout,"noise is %d\n",rssi);
+	  unsigned char rmac[6];
+	  ether_atoe (argv[++i], rmac);
+	    int ifcount = getifcount ("wifi");
+	  int rssi=-1;
+	  if (strcmp (argv[1], "-i") == 0)
+	    rssi = getNoise (ifname, rmac);
+	  else
+	    {
+	      int c = 0;
+	      for (c = 0; c < ifcount; c++)
+		{
+		  char interface[32];
+		  sprintf (interface, "ath%d", c);
+		      if (rssi==-1)
+		        rssi = getNoise(interface, rmac);
+		  char vif[32];
+		  sprintf (vif, "%s_vifs", interface);
+		  char var[80], *next;
+		  char *vifs = nvram_safe_get (vif);
+		  if (vifs != NULL)
+		    {
+		      foreach (var, vifs, next)
+		      {
+		      if (rssi==-1)
+		        rssi = getNoise(var, rmac);
+		      else break;
+		      }
+		    }
+		}
+	    }
+	  fprintf (stdout, "noise is %d\n", rssi);
 	}
     }
 }
-
-
