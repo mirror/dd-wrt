d563b51d3048727c3ad255aaa592e2997f285d8b
BrainSlayer <s.gottschall@dd-wrt.com>
2007-02-04 23:17:47 +0000

    WHR-HP-AG108 support preparation
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@5779 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/ar531x/linux-2.6.19/include/asm-mips/mach-atheros/ar531x_platform.h
+++ src/linux/ar531x/linux-2.6.19/include/asm-mips/mach-atheros/ar531x_platform.h
@@ -1,6 +1,19 @@
 #ifndef __AR531X_PLATFORM_H
 #define __AR531X_PLATFORM_H
 
+/*
+ * Board support data.  The driver is required to locate
+ * and fill-in this information before passing a reference to
+ * this structure as the HAL_BUS_TAG parameter supplied to
+ * ath_hal_attach.
+ */
+struct ar531x_config {
+	const char  *board;	/* board config data */
+	const char	*radio;			/* radio config data */
+	int		unit;			/* unit number [0, 1] */
+	u32		tag;			/* used as devid for now */
+};
+
 struct ar531x_eth {
 	int phy;
 	int mac;
@@ -11,4 +24,3 @@ struct ar531x_eth {
 };
 
 #endif /* __AR531X_PLATFORM_H */
-
