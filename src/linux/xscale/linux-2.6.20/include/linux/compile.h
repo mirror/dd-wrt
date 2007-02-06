d04cc1aff17c01473459edd8246a4ef2adf71970
BrainSlayer <s.gottschall@dd-wrt.com>
2007-02-06 17:04:40 +0000

    squashfs, ixp4xx gpio support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@5784 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/linux/xscale/linux-2.6.20/include/linux/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 9 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#9 Tue Feb 6 18:34:10 CET 2007"
+#define LINUX_COMPILE_TIME "18:34:10"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "linux"
+#define LINUX_COMPILE_DOMAIN "site"
+#define LINUX_COMPILER "gcc version 4.1.1"
