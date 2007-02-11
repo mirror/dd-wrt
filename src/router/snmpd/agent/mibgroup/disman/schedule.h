d05907926cc44b3c0d718eccb8cfffcc9ef0ce33
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-02-11 15:41:24 +0000

    wrong naming
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@5826 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/snmpd/agent/mibgroup/disman/schedule.h
+++
@@ -1,3 +0,0 @@
-config_require(disman/schedule/schedCore)
-config_require(disman/schedule/schedConf)
-config_require(disman/schedule/schedTable)
--- src/router/snmpd/agent/mibgroup/notification-log-mib.h
+++
@@ -1,2 +0,0 @@
-config_require(notification-log-mib/notification_log)
-config_require(notification/snmpNotifyTable)
