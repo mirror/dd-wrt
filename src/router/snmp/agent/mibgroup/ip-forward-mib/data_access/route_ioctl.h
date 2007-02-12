3976073339bc9f3f8c41c0fbdec400c7e8ab973d
BrainSlayer <s.gottschall@dd-wrt.com>
2007-02-12 11:33:35 +0000

    rollback old snmp
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@5835 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/snmp/agent/mibgroup/ip-forward-mib/data_access/route_ioctl.h
+++
@@ -1,8 +0,0 @@
-/*
- * internal header, not for distribution
- */
-
-int _netsnmp_ioctl_route_set_v4(netsnmp_route_entry * entry);
-int _netsnmp_ioctl_route_remove_v4(netsnmp_route_entry * entry);
-int _netsnmp_ioctl_route_delete_v4(netsnmp_route_entry * entry);
-
--- src/router/snmp/agent/mibgroup/ip-mib/ipv4InterfaceTable.h
+++
@@ -1,5 +0,0 @@
-/*
- * module to include the modules
- */
-
-config_require(ip-mib/ipv4InterfaceTable/ipv4InterfaceTable)
