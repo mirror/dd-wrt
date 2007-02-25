e0a850c8640517e5d69957bb35fc9fbcf6cd1fc1
botho <botho@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-02-25 17:10:22 +0000

    change device name and try to resolve #0002101
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@6029 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/tools/upnp/upnp/device.c
+++ src/tools/upnp/upnp/device.c
@@ -148,6 +148,7 @@ void device_xml(PDevice pdev, UFILE *up)
     PFDEVXML func;
     char *friendlyname;
 	char *myip;
+	char *devicename;
 	int winmnp;
 
     // call the device's xml function, if defined.
@@ -157,47 +158,48 @@ void device_xml(PDevice pdev, UFILE *up)
     }
 
 	myip = nvram_safe_get("lan_ipaddr");
+	devicename = nvram_safe_get("DD_BOARD");
 	winmnp = (!nvram_match("upnpmnp", "0"));
 
     if (ISROOT(pdev)) {
 		uprintf(up, 
 			"<?xml version=\"1.0\"?>\r\n"
 			"<root xmlns=\"urn:schemas-upnp-org:device-1-0\">\r\n"
-			"<specVersion>\r\n"
-			"<major>1</major>\r\n"
-			"<minor>0</minor>\r\n"
-			"</specVersion>\r\n"
+			" <specVersion>\r\n"
+			"  <major>1</major>\r\n"
+			"  <minor>0</minor>\r\n"
+			" </specVersion>\r\n"
 			);
 
-		if (winmnp) uprintf(up, "<URLBase>http%s://%s:5431</URLBase>\r\n",
+		if (winmnp) uprintf(up, " <URLBase>http%s://%s:5431</URLBase>\r\n",
 			nvram_match("https_enable", "1") ? "s" : "", myip); //Botho : add https support
     }
 
     if (pdev->friendlyname) friendlyname = pdev->friendlyname;
         else friendlyname = pdev->template->type;
 
-    uprintf(up, "<device>\r\n");
-    uprintf(up, "<deviceType>%s</deviceType>\r\n", pdev->template->type);
-    uprintf(up, "<friendlyName>%s</friendlyName>\r\n", friendlyname);
-    uprintf(up, "<manufacturer>" DEV_MFR "</manufacturer>\r\n"
-				"<manufacturerURL>" DEV_MFR_URL "</manufacturerURL>\r\n"
-				"<modelDescription>" DEV_MODEL_DESCRIPTION "</modelDescription>\r\n"
-				"<modelName>" DEV_MODEL "</modelName>\r\n"
-				"<modelNumber>" DEV_MODEL_NO "</modelNumber>\r\n"
-				"<modelURL>" DEV_MODEL_URL "</modelURL>\r\n");
-	uprintf(up, "<UDN>%s</UDN>\r\n", pdev->udn);
-
-	if ((winmnp) && (ISROOT(pdev))) {
-		uprintf(up, "<presentationURL>http%s://%s/UPnP.asp</presentationURL>\r\n",
-			nvram_match("https_enable", "1") ? "s" : "", myip); //Botho : add https support
-	}
-
+    uprintf(up, " <device>\r\n");
+    uprintf(up, "  <deviceType>%s</deviceType>\r\n", pdev->template->type);
+    uprintf(up, "  <friendlyName>%s</friendlyName>\r\n", friendlyname);
+    uprintf(up, "  <manufacturer>" DEV_MFR "</manufacturer>\r\n"
+				"  <manufacturerURL>" DEV_MFR_URL "</manufacturerURL>\r\n"
+				"  <modelDescription>" DEV_MODEL_DESCRIPTION "</modelDescription>\r\n");
+	uprintf(up, "  <modelName>%s</modelName>\r\n", devicename);
+	uprintf(up, "  <modelNumber>" DEV_MODEL_NO "</modelNumber>\r\n"
+				"  <modelURL>" DEV_MODEL_URL "</modelURL>\r\n"
+				"  <serialNumber>how obtain that ?</serialNumber>\r\n");
+	uprintf(up, "  <UDN>%s</UDN>\r\n", pdev->udn);
 
     // generate XML for any services in this device.
     device_servicelist(pdev, up);
 
     // generate XML for any subdevices in this device.
     device_devicelist(pdev, up);
+	
+    if ((winmnp) && (ISROOT(pdev))) {
+		uprintf(up, "<presentationURL>/UPnP.asp</presentationURL>\r\n",
+			nvram_match("https_enable", "1") ? "s" : "", myip); //Botho : add https support
+	}
 	
     uprintf(up, "</device>\r\n");
 
@@ -213,10 +215,10 @@ void device_devicelist(PDevice pdev, UFILE *up)
 
     // generate XML for any subdevices in this device.
     if (pdev->subdevs) {
-	uprintf(up, "<deviceList>\r\n");
+	uprintf(up, "  <deviceList>\r\n");
 	for (psubdev = pdev->subdevs; psubdev; psubdev = psubdev->next) 
 	    device_xml(psubdev, up);
-	uprintf(up, "</deviceList>\r\n");
+	uprintf(up, "  </deviceList>\r\n");
     }
 }
 
@@ -228,34 +230,34 @@ void device_servicelist(PDevice pdev, UFILE *up)
 
     // generate XML for any services in this device.
     if (pdev->services) {
-	uprintf(up, "<serviceList>\r\n");
+	uprintf(up, "  <serviceList>\r\n");
 	forall_services(pdev, psvc) {
 	    snprintf(svcurl, sizeof(svcurl), "/%s/%s", pdev->udn, psvc->template->name);
 
-	    uprintf(up, "<service>\r\n");
-	    uprintf(up, "<serviceType>urn:%s:service:%s</serviceType>\r\n", 
+	    uprintf(up, "  <service>\r\n");
+	    uprintf(up, "   <serviceType>urn:%s:service:%s</serviceType>\r\n", 
 		    psvc->template->schema, psvc->template->name);
 	    if (psvc->template->serviceid) {
 #if 0	// test alt serviceid - tofu
 			if ((strcmp(psvc->template->serviceid, "urn:upnp-org:serviceId:WANCommonInterfaceConfig") == 0) ||
 				(strcmp(psvc->template->serviceid, "urn:upnp-org:serviceId:WANIPConnection") == 0) ||
 				(strcmp(psvc->template->serviceid, "urn:upnp-org:serviceId:WANPPPConnection") == 0)) {
-				uprintf(up, "<serviceId>%s</serviceId>\r\n", psvc->template->serviceid);
+				uprintf(up, "   <serviceId>%s</serviceId>\r\n", psvc->template->serviceid);
 			}
 			else
 #endif
-			uprintf(up, "<serviceId>%s%d</serviceId>\r\n", 
+			uprintf(up, "   <serviceId>%s%d</serviceId>\r\n", 
 					psvc->template->serviceid, psvc->instance);
 	    } else {
-		uprintf(up, "<serviceId>urn:upnp-org:serviceId:%s%d</serviceId>\r\n", 
+		uprintf(up, "   <serviceId>urn:upnp-org:serviceId:%s%d</serviceId>\r\n", 
 			psvc->template->name, psvc->instance);
 	    }
-	    uprintf(up, "<controlURL>/%s/%s</controlURL>\r\n", pdev->udn, psvc->template->name);
-	    uprintf(up, "<eventSubURL>/%s/%s</eventSubURL>\r\n", pdev->udn, psvc->template->name);
-	    uprintf(up, "<SCPDURL>/dynsvc/%s.xml</SCPDURL>\r\n", psvc->template->name);
-	    uprintf(up, "</service>\r\n");
+	    uprintf(up, "   <SCPDURL>/dynsvc/%s.xml</SCPDURL>\r\n", psvc->template->name);
+	    uprintf(up, "   <controlURL>/%s/%s</controlURL>\r\n", pdev->udn, psvc->template->name);
+	    uprintf(up, "   <eventSubURL>/%s/%s</eventSubURL>\r\n", pdev->udn, psvc->template->name);
+	    uprintf(up, "   </service>\r\n");
 	}
-	uprintf(up, "</serviceList>\r\n");
+	uprintf(up, "  </serviceList>\r\n");
     }
 }
 
