043d4d87e560a079bd6aac8b45385b270d57d05a
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-02-26 20:12:25 +0000

    debuginfos removed again
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@6043 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/servicemanager.c
+++ src/router/rc/servicemanager.c
@@ -26,17 +26,17 @@
 //#include <shutils.h>
 
 #define SERVICE_MODULE "/lib/services.so"
-//#define cprintf(fmt, args...)
+#define cprintf(fmt, args...)
 
 
-#define cprintf(fmt, args...) do { \
+/*#define cprintf(fmt, args...) do { \
 	FILE *fp = fopen("/dev/console", "w"); \
 	if (fp) { \
 		fprintf(fp, fmt, ## args); \
 		fclose(fp); \
 	} \
 } while (0)
-
+*/
 
 
 void *
