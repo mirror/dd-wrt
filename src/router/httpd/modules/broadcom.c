5261b3dbdc7fc059db95f81bffc056ca09f5a5b5
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-03-07 13:24:09 +0000

    status bandwidth in shown in micro fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@6138 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/broadcom.c
+++ src/router/httpd/modules/broadcom.c
@@ -3826,6 +3826,7 @@ ej_do_menu (  webs_t wp, int argc, char_t ** argv)
 
   int sipgate = nvram_match ("sipgate", "1");
   int vlan_supp = check_vlan_support ();
+  int micro = nvram_match ("dist_type", "micro");
 #ifdef HAVE_SPUTNIK_APD
   int sputnik = nvram_match ("apd_enable", "1");
 #else
@@ -3940,10 +3941,8 @@ ej_do_menu (  webs_t wp, int argc, char_t ** argv)
 		j++;
 	      if ((!openvpn) && !strcmp (menu[i][j], "Status_OpenVPN.asp"))	//jump over OpenVPN
 		j++;
-#ifdef HAVE_MICRO
-	      if (!strcmp (menu[i][j], "Status_Bandwidth.asp"))	//jump over bandtwidth in micro build
+	      if ((micro) && !strcmp (menu[i][j], "Status_Bandwidth.asp"))	//jump over bandtwidth in micro build
 		j++;
-#endif
 	      if ((!auth) && !strcmp (menu[i][j], "Info.htm"))	//jump over Sys-Info
 		j++;
 		
