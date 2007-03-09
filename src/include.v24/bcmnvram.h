eeaf0b9e1c60616ae9a0de95aa7b83e756063e8d
BrainSlayer <s.gottschall@dd-wrt.com>
2007-03-09 19:19:37 +0000

    this is missing eko
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@6160 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/include.v24/bcmnvram.h
+++ src/include.v24/bcmnvram.h
@@ -106,6 +106,8 @@ extern int nvram_prefix_match(const char *name, const char *prefix,char *match);
 
 extern int nvram_default_match (char *var, char *match, char *def);
 
+extern char *nvram_default_get (char *var, char *def);
+
 /*
  * Set the value of an NVRAM variable. The name and value strings are
  * copied into private storage. Pointers to previously set values
