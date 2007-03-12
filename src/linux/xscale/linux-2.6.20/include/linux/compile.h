9c7894b603dd7a5c4e852cc01dacfadec0799e64
BrainSlayer <s.gottschall@dd-wrt.com>
2007-03-12 22:00:12 +0000

    support for extended channels by using frequencies instead of channels. this change requires also a wireless extension change to support private calls with bigger data arrays
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@6213 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/xscale/linux-2.6.20/include/linux/compile.h
+++ src/linux/xscale/linux-2.6.20/include/linux/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 18 */
+/* This file is auto generated, version 32 */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#18 Tue Feb 27 02:48:22 CET 2007"
-#define LINUX_COMPILE_TIME "02:48:22"
+#define UTS_VERSION "#32 Mon Mar 12 21:07:01 CET 2007"
+#define LINUX_COMPILE_TIME "21:07:01"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "linux"
 #define LINUX_COMPILE_DOMAIN "site"
