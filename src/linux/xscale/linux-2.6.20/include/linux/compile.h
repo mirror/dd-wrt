b50192525ca21ec0d0957c50095a63f7c816fb40
BrainSlayer <s.gottschall@dd-wrt.com>
2007-03-21 19:19:00 +0000

    arpnat stuff
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@6303 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/xscale/linux-2.6.20/include/linux/compile.h
+++ src/linux/xscale/linux-2.6.20/include/linux/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 33 */
+/* This file is auto generated, version 37 */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#33 Mon Mar 12 23:23:36 CET 2007"
-#define LINUX_COMPILE_TIME "23:23:36"
+#define UTS_VERSION "#37 Wed Mar 21 18:34:09 CET 2007"
+#define LINUX_COMPILE_TIME "18:34:09"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "linux"
 #define LINUX_COMPILE_DOMAIN "site"
