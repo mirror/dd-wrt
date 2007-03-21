e2b5a487ee8809b8c70a0cc1b7a866e3ca9820e6
BrainSlayer <s.gottschall@dd-wrt.com>
2007-03-21 19:26:25 +0000

    arpnat stuff
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@6306 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/magicbox/linux-2.6.19/net/ipv4/devinet.c
+++ src/linux/magicbox/linux-2.6.19/net/ipv4/devinet.c
@@ -1636,6 +1636,7 @@ void __init devinet_init(void)
 
 EXPORT_SYMBOL(in_dev_finish_destroy);
 EXPORT_SYMBOL(inet_select_addr);
+EXPORT_SYMBOL(inet_confirm_addr);
 EXPORT_SYMBOL(inetdev_by_index);
 EXPORT_SYMBOL(register_inetaddr_notifier);
 EXPORT_SYMBOL(unregister_inetaddr_notifier);
