21887cadbf3baaa42a59c52ce8b73d48df1273bf
BrainSlayer <s.gottschall@dd-wrt.com>
2007-03-21 19:28:04 +0000

    arpnat dummy for broadcom
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@6307 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/brcm/linux.v23/include/pcmcia/version.h
+++ src/linux/brcm/linux.v23/include/pcmcia/version.h
@@ -1,4 +1,7 @@
 /* version.h 1.94 2000/10/03 17:55:48 (David Hinds) */
 
+#define CS_PKG_RELEASE		"3.2.8"
+#define CS_PKG_RELEASE_CODE	0x3208
+
 #define CS_RELEASE "3.1.22"
 #define CS_RELEASE_CODE 0x3116
