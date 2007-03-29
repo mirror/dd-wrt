7f58c1946a4bee4f5f1cf6184bd0d98627881536
BrainSlayer <s.gottschall@dd-wrt.com>
2007-03-29 09:02:13 +0000

    nvram moved
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@6387 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rc/nvram.c
@@ -0,0 +1,87 @@
+/*
+ * Frontend command-line utility for Linux NVRAM layer
+ *
+ * Copyright 2001-2003, Broadcom Corporation
+ * All Rights Reserved.
+ * 
+ * THIS SOFTWARE IS OFFERED "AS IS", AND BROADCOM GRANTS NO WARRANTIES OF ANY
+ * KIND, EXPRESS OR IMPLIED, BY STATUTE, COMMUNICATION OR OTHERWISE. BROADCOM
+ * SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A SPECIFIC PURPOSE OR NONINFRINGEMENT CONCERNING THIS SOFTWARE.
+ *
+ * $Id: main.c,v 1.1.1.5 2003/10/29 03:06:28 honor Exp $
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <typedefs.h>
+#include <bcmnvram.h>
+
+static void
+usage (void)
+{
+  fprintf (stderr,
+	   "usage: nvram [get name] [set name=value] [unset name] [show]\n");
+  exit (0);
+}
+
+/* NVRAM utility */
+int
+nvram_main (int argc, char **argv)
+{
+  char *name, *value, buf[NVRAM_SPACE];
+  int size;
+
+  /* Skip program name */
+  --argc;
+  ++argv;
+
+  if (!*argv)
+    usage ();
+
+  /* Process the remaining arguments. */
+  for (; *argv; argv++)
+    {
+      if (!strncmp (*argv, "get", 3))
+	{
+	  if (*++argv)
+	    {
+	      if ((value = nvram_get (*argv)))
+		puts (value);
+	    }
+	}
+      else if (!strncmp (*argv, "set", 3))
+	{
+	  if (*++argv)
+	    {
+	      strncpy (value = buf, *argv, sizeof (buf));
+	      name = strsep (&value, "=");
+	      nvram_set (name, value);
+	    }
+	}
+      else if (!strncmp (*argv, "unset", 5))
+	{
+	  if (*++argv)
+	    nvram_unset (*argv);
+	}
+      else if (!strncmp (*argv, "commit", 5))
+	{
+	  nvram_commit ();
+	}
+      else if (!strncmp (*argv, "show", 4) || !strncmp (*argv, "getall", 6))
+	{
+	  nvram_getall (buf, sizeof (buf));
+	  for (name = buf; *name; name += strlen (name) + 1)
+	    puts (name);
+	  size = sizeof (struct nvram_header) + (int) name - (int) buf;
+	  fprintf (stderr, "size: %d bytes (%d left)\n", size,
+		   NVRAM_SPACE - size);
+	}
+      if (!*argv)
+	break;
+    }
+
+  return 0;
+}
