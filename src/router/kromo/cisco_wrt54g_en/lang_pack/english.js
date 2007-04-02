cdd34b12c0617fda61c9a9e9989c6064a7fe4608
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-04-02 23:47:44 +0000

    fast framing support (optional)
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@6427 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/cisco_wrt54g_en/lang_pack/english.js
+++ src/router/kromo/cisco_wrt54g_en/lang_pack/english.js
@@ -1629,6 +1629,7 @@ wl_basic.bridged="Bridged";
 wl_basic.turbo="Turbo Mode";
 wl_basic.extrange="Extended Range";
 wl_basic.supergcomp="Super G Compression";
+wl_basic.supergff="Super G Fast Framing";
 wl_basic.extchannel="Extended Channel Mode";
 wl_basic.outband="Outdoor Band";
 wl_basic.channel_width="Channel Width";
