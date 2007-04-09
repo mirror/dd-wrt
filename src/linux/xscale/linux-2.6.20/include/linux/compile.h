f2267ce52a5b9634fa3619ac9fbc78ca46d1820f
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-04-09 11:00:28 +0000

    new nvram driver. this should speed up bootup time and webinterface speed on avila gateworks, fonera and whr-hp-ag108 devices
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@6512 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/xscale/linux-2.6.20/include/linux/compile.h
+++ src/linux/xscale/linux-2.6.20/include/linux/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 43 */
+/* This file is auto generated, version 67 */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#43 Thu Apr 5 20:22:53 CEST 2007"
-#define LINUX_COMPILE_TIME "20:22:53"
+#define UTS_VERSION "#67 Mon Apr 9 12:04:11 CEST 2007"
+#define LINUX_COMPILE_TIME "12:04:11"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "dd-wrt"
 #define LINUX_COMPILE_DOMAIN "FBNRIE"
--- src/linux/xscale/linux-2.6.20/include/linux/utsrelease.h
+++ src/linux/xscale/linux-2.6.20/include/linux/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "2.6.20.4"
+#define UTS_RELEASE "2.6.20.6"
