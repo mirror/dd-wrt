26fac6e42465c512bac82424935eee1ec9300a86
eko <USER@DOMAIN>
2007-04-20 05:47:50 +0000

    moved to chaps.c
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@6723 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/chaps.c
+++ src/router/httpd/modules/chaps.c
@@ -174,3 +174,55 @@ show_chaps_table (webs_t wp, char *type, int which)
 }
 
 
+
+static void
+ej_show_chaps (webs_t wp, int argc, char_t ** argv)
+
+{
+  int i;
+  char buffer[1024], *count;
+  int c = 0;
+  count = nvram_safe_get ("pppoeserver_chapsnum");
+  if (count == NULL || strlen (count) == 0)
+    {
+      websWrite (wp, "<tr>\n");
+      websWrite (wp,
+		 "<td colspan=\"4\" align=\"center\" valign=\"middle\">- <script type=\"text/javascript\">Capture(share.none)</script> -</td>\n");
+      websWrite (wp, "</tr>\n");
+    }
+  c = atoi (count);
+  if (c <= 0)
+    {
+      websWrite (wp, "<tr>\n");
+      websWrite (wp,
+		 "<td colspan=\"4\" align=\"center\" valign=\"middle\">- <script type=\"text/javascript\">Capture(share.none)</script> -</td>\n");
+      websWrite (wp, "</tr>\n");
+    }
+  for (i = 0; i < c; i++)
+    {
+      websWrite (wp, "<tr><td>\n");
+      FWSHOW1
+	("<input maxlength=\"30\" size=\"30\" name=\"user%d\" onblur=\"valid_name(this,'Name')\" value=\"",
+	 i);
+      show_chaps_table (wp, "user", i);
+      websWrite (wp, "\" /></td>\n");
+      websWrite (wp, "<td>\n");
+      FWSHOW1
+	("<input maxlength=\"30\" size=\"30\" name=\"pass%d\" onblur=\"valid_name(this,'Name')\" value=\"",
+	 i);
+      show_chaps_table (wp, "pass", i);
+      websWrite (wp, "\" /></td>\n");
+      websWrite (wp, "<td>\n");
+      FWSHOW1
+	("<input class=\"num\" maxlength=\"15\" size=\"24\" name=\"ip%d\" value=\"",
+	 i);
+      show_chaps_table (wp, "ip", i);
+      websWrite (wp, "\" /></td>\n");
+      websWrite (wp, "<td>\n");
+      FWSHOW1 ("<input type=\"checkbox\" value=\"on\" name=\"enable%d\" ", i);
+      show_chaps_table (wp, "enable", i);
+      websWrite (wp, " /></td>\n");
+      websWrite (wp, "</tr>\n");
+    }
+  return;
+}
