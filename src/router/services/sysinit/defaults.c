70b95881b0be8cf4e13430e29530d70a62971267
BrainSlayer <s.gottschall@dd-wrt.com>
2007-04-24 17:40:26 +0000

    changes to allow buggy straightcore units to connect with pppoe and mppe encryption
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@6805 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/defaults.c
+++ src/router/services/sysinit/defaults.c
@@ -479,6 +479,7 @@ struct nvram_tuple srouter_defaults[] = {
 
 #ifdef HAVE_MADWIFI
 #ifdef HAVE_MAKSAT
+  {"show_hidden", "1", 0},
   {"ath_regulatory", "0", 0},
   {"wl0_ssid", "maksat", 0},	/* Service set ID (network name) */
   {"ath0_ssid", "maksat", 0},	/* Service set ID (network name) */
