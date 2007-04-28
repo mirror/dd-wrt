77751ad018f1e47076c67beaf50ceb49ba41695a
BrainSlayer <s.gottschall@dd-wrt.com>
2007-04-28 17:37:41 +0000

    puh i hope that is the last one
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@6852 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/www.mk
+++ src/router/rules/www.mk
@@ -1,11 +1,22 @@
-www www-%: 
+www-install:
+	rm -rf $(INSTALLDIR)/www
+	install -d $(INSTALLDIR)/www
+	install -d $(INSTALLDIR)/www/www
+ifeq ($(KROMOGUI),y)
+	$(MAKE) -C kromo/$(WEB_PAGE) install INSTALLDIR=$(INSTALLDIR)/www
+else
+	$(MAKE) -C www/$(WEB_PAGE) install INSTALLDIR=$(INSTALLDIR)/www
+endif
+
+www: 
+	rm -rf $(INSTALLDIR)/www
 ifeq ($(CONFIG_EOP_TUNNEL),y)
-	$(MAKE) -C eop-tunnel install
+	$(MAKE) -C eop-tunnel www-install INSTALLDIR=$(INSTALLDIR)/www
 endif
 ifeq ($(KROMOGUI),y)
-	$(MAKE) -C kromo/$(WEB_PAGE) $* INSTALLDIR=$(INSTALLDIR)/www
+	$(MAKE) -C kromo/$(WEB_PAGE) install INSTALLDIR=$(INSTALLDIR)/www
 else
-	$(MAKE) -C www/$(WEB_PAGE) $* INSTALLDIR=$(INSTALLDIR)/www
+	$(MAKE) -C www/$(WEB_PAGE) install INSTALLDIR=$(INSTALLDIR)/www
 endif
 
 www-distclean www-clean:
