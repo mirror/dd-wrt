089bee292b37d4c5944dfc5952e3aa3eb2c12915
BrainSlayer <s.gottschall@dd-wrt.com>
2007-04-29 21:44:07 +0000

    sputnik makefile changes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@6884 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/sputnik.mk
+++ src/router/rules/sputnik.mk
@@ -1,12 +1,11 @@
 sputnik:
-	make -C sputnik
+	if test -e "sputnik/Makefile"; then make -C sputnik; fi
 
 sputnik-clean:
-	make -C sputnik clean
+	if test -e "sputnik/Makefile"; then make -C sputnik clean; fi
 
 sputnik-install:
 	mkdir -p $(INSTALLDIR)/sputnik/etc/config
 	cp -fpR sputnik/config/* $(INSTALLDIR)/sputnik/etc/config
 	install -D sputnik/$(ARCH)/sputnik $(INSTALLDIR)/sputnik/usr/sbin/sputnik
 	install -D sputnik/$(ARCH)/libiksemel.so $(INSTALLDIR)/sputnik/usr/lib/libiksemel.so
-
