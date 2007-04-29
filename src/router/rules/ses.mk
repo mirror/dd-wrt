020e2e8bc529a3cffc5dee34d6f734c8a0cba560
BrainSlayer <s.gottschall@dd-wrt.com>
2007-04-29 03:46:36 +0000

    fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@6856 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/ses.mk
+++ src/router/rules/ses.mk
@@ -2,5 +2,7 @@ ses:
 	[ ! -f ses/Makefile ] || $(MAKE) -C ses
 
 ses-install:
-	install -D ses/ses/ses $(INSTALLDIR)/ses/usr/sbin/ses
-	$(STRIP) $(INSTALLDIR)/ses/usr/sbin/ses
+	install -D ses/config/ses.webservices $(INSTALLDIR)/ses/etc/config/ses.webservices
+	install -D ses/config/ses.nvramconfig $(INSTALLDIR)/ses/etc/config/ses.nvramconfig	
+#	install -D ses/ses/ses $(INSTALLDIR)/ses/usr/sbin/ses
+##	$(STRIP) $(INSTALLDIR)/ses/usr/sbin/ses
