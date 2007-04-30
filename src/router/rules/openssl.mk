c6702e1c9c4fac8da25ffe4d01ebf1d83ba81801
BrainSlayer <s.gottschall@dd-wrt.com>
2007-04-30 14:53:32 +0000

    broadcom brick fix, rebuild is in progress
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@6888 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/openssl.mk
+++ src/router/rules/openssl.mk
@@ -1,16 +1,18 @@
 openssl:
 	$(MAKE) -C openssl CC="$(ARCH)-linux-uclibc-gcc -I$(TOP)/zlib" MAKEDEPPROG=$(ARCH)-linux-uclibc-gcc 
-#	$(MAKE) -C openssl CC="$(ARCH)-linux-uclibc-gcc -I$(TOP)/zlib" MAKEDEPPROG=$(ARCH)-linux-uclibc-gcc
 	$(MAKE) -C openssl build-shared CC="$(ARCH)-linux-uclibc-gcc -I$(TOP)/zlib" MAKEDEPPROG=$(ARCH)-linux-uclibc-gcc
 	$(MAKE) -C openssl build_apps CC="$(ARCH)-linux-uclibc-gcc -I$(TOP)/zlib" MAKEDEPPROG=$(ARCH)-linux-uclibc-gcc
-	
+ifeq ($(ARCH),mipsel)
+	rm -f openssl/libcrypto.so.0.9.8
+	rm -f openssl/libssl.so.0.9.8
+endif
 
 openssl-install:
+ifneq ($(ARCH),mipsel)
 	install -D openssl/libcrypto.so.0.9.8 $(INSTALLDIR)/openssl/usr/lib/libcrypto.so.0.9.8
-#	$(STRIP) $(INSTALLDIR)/openssl/usr/lib/libcrypto.so
 	install -D openssl/libssl.so.0.9.8 $(INSTALLDIR)/openssl/usr/lib/libssl.so.0.9.8
 	install -D openssl/apps/openssl $(INSTALLDIR)/openssl/usr/sbin/openssl
-#	$(STRIP) $(INSTALLDIR)/openssl/usr/lib/libssl.so
+endif
 	@true
 
 openssl-clean:
