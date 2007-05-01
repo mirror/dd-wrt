d63b59e4ac94b1ffdabb40b36eb3ba164dd2e47c
BrainSlayer <s.gottschall@dd-wrt.com>
2007-05-01 01:11:59 +0000

    other rb532 related fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@6891 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/openssl.mk
+++ src/router/rules/openssl.mk
@@ -3,12 +3,19 @@ openssl:
 	$(MAKE) -C openssl build-shared CC="$(ARCH)-linux-uclibc-gcc -I$(TOP)/zlib" MAKEDEPPROG=$(ARCH)-linux-uclibc-gcc
 	$(MAKE) -C openssl build_apps CC="$(ARCH)-linux-uclibc-gcc -I$(TOP)/zlib" MAKEDEPPROG=$(ARCH)-linux-uclibc-gcc
 ifeq ($(ARCH),mipsel)
+ifneq ($(ARCHITECTURE),rb532)
 	rm -f openssl/libcrypto.so.0.9.8
 	rm -f openssl/libssl.so.0.9.8
+endif
 endif
 
 openssl-install:
 ifneq ($(ARCH),mipsel)
+	install -D openssl/libcrypto.so.0.9.8 $(INSTALLDIR)/openssl/usr/lib/libcrypto.so.0.9.8
+	install -D openssl/libssl.so.0.9.8 $(INSTALLDIR)/openssl/usr/lib/libssl.so.0.9.8
+	install -D openssl/apps/openssl $(INSTALLDIR)/openssl/usr/sbin/openssl
+endif
+ifeq ($(ARCHITECTURE),rb532)
 	install -D openssl/libcrypto.so.0.9.8 $(INSTALLDIR)/openssl/usr/lib/libcrypto.so.0.9.8
 	install -D openssl/libssl.so.0.9.8 $(INSTALLDIR)/openssl/usr/lib/libssl.so.0.9.8
 	install -D openssl/apps/openssl $(INSTALLDIR)/openssl/usr/sbin/openssl
