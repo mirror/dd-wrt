7cf61cce711e6fdbf77285a0590e436018e2a60a
eko <USER@DOMAIN>
2007-05-04 14:48:55 +0000

    fix (dropbear gui in micro)
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@6932 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/ses.mk
+++ src/router/rules/ses.mk
@@ -2,7 +2,9 @@ ses:
 	[ ! -f ses/Makefile ] || $(MAKE) -C ses
 
 ses-install:
+ifeq ($(CONFIG_SES),y)
 	install -D ses/config/ses.webservices $(INSTALLDIR)/ses/etc/config/ses.webservices
 	install -D ses/config/ses.nvramconfig $(INSTALLDIR)/ses/etc/config/ses.nvramconfig	
 #	install -D ses/ses/ses $(INSTALLDIR)/ses/usr/sbin/ses
 ##	$(STRIP) $(INSTALLDIR)/ses/usr/sbin/ses
+endif
