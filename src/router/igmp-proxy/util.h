95219495a6485d15f345791c8f339a1cd9883a8d
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-05-10 19:55:13 +0000

    unix formating
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@7048 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/igmp-proxy/util.h
+++ src/router/igmp-proxy/util.h
@@ -1,16 +1,15 @@
 #ifndef __UTIL_H__
 #define __UTIL_H__
-
-typedef struct _set_t {
-	void *next;
-} set_t;
-
-void list_add(set_t**,set_t*);
-set_t* list_remove(set_t**);
-set_t* list_get_head(set_t**);
-set_t* list_get_next(set_t*);
-int list_remove_node(set_t**,set_t*);
-void list_cat(set_t**,set_t**);
-unsigned short in_cksum(unsigned short*,int);
-
-#endif
+ typedef struct _set_t
+{
+  void *next;
+ } set_t;
+void list_add (set_t **, set_t *);
+set_t * list_remove (set_t **);
+set_t * list_get_head (set_t **);
+set_t * list_get_next (set_t *);
+int list_remove_node (set_t **, set_t *);
+void list_cat (set_t **, set_t **);
+unsigned short in_cksum (unsigned short *, int);
+
+#endif	/*  */
