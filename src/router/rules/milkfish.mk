7f63f1e83f717bfbb232473ab0a5dff69b3015d6
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-05-24 14:22:38 +0000

    c/p typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@7118 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/milkfish.mk
+++ src/router/rules/milkfish.mk
@@ -24,6 +24,7 @@ milkfish-install:
 	install -m0755 milkfish/usr/sbin/mf_sip_tracer.sh $(INSTALLDIR)/milkfish/usr/sbin
 	install -d -m0755 $(INSTALLDIR)/milkfish/usr/lib
 	install -m0644 milkfish/usr/lib/milkfish_functions.sh $(INSTALLDIR)/milkfish/usr/lib/
-	install -D  milkfish/etc/config/mmc.nvramconfig $(INSTALLDIR)/milkfish/etc/config/mmc.nvramconfig
+	install -D  milkfish/etc/config/milkfish.nvramconfig $(INSTALLDIR)/milkfish/etc/config/milkfish.nvramconfig
+	
 milkfish-clean:
 	rm -rf $(INSTALLDIR)/milkfish/*
