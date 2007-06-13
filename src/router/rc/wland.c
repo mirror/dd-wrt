da645389730f57e0310615d1205232dd4a9d984c
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-06-13 00:18:35 +0000

    more aqos fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@7232 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/wland.c
+++ src/router/rc/wland.c
@@ -216,7 +216,7 @@ do_aqos_check (void)
 	{
 	
 	  char addition[128];
-	  sprintf(addition,"echo "%s" >>/tmp/aqos_macs",mac_buf);
+	  sprintf(addition,"echo \"%s\" >>/tmp/aqos_macs",mac_buf);
 	  system2(addition);
 	  //create default rule for mac
 	  add_usermac(mac_buf,qosidx,defaulup,defauldown);
@@ -226,7 +226,7 @@ do_aqos_check (void)
       if (!cip && strlen(ip_buf)>0)
 	{
 	  char addition[128];
-	  sprintf(addition,"echo "%s" >>/tmp/aqos_ips",ip_buf);
+	  sprintf(addition,"echo \"%s\" >>/tmp/aqos_ips",ip_buf);
 	  system2(addition);
 	  //create default rule for ip
 	  add_userip(ip_buf,qosidx,defaulup,defauldown);
