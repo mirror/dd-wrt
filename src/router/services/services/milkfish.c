f0081fdf5f461929255ec6f683f1047a60e594c0
BrainSlayer <s.gottschall@dd-wrt.com>
2007-06-18 18:14:35 +0000

    filter out bad characters (\r)
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@7270 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/milkfish.c
+++ src/router/services/services/milkfish.c
@@ -11,7 +11,7 @@ stop_milkfish (void)
 {
   if (pidof ("rtpproxy") > 0 || pidof ("openserctl") > 0)
     syslog (LOG_INFO, "Milkfish service successfully stopped\n");
-    
+
   killall ("rtpproxy", SIGTERM);
   killall ("openserctl", SIGTERM);
 }
@@ -19,13 +19,13 @@ stop_milkfish (void)
 void
 start_milkfish (void)
 {
-	if (nvram_match ("milkfish_enabled", "1"))
+  if (nvram_match ("milkfish_enabled", "1"))
     {
-		eval ("/etc/config/milkfish.startup");
-		eval ("/etc/config/milkfish.netup");  //start rtpproxy and openserctl
-    
-		syslog (LOG_INFO, "Milkfish service successfully started\n");
-	}
+      eval ("/etc/config/milkfish.startup");
+      eval ("/etc/config/milkfish.netup");	//start rtpproxy and openserctl
+
+      syslog (LOG_INFO, "Milkfish service successfully started\n");
+    }
 }
 
 #endif
