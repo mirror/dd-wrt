a5d85790094a3b636ad6740a5fe867304cdc4f0b
steve <steve@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-06-20 23:27:32 +0000

    dhcp_release and dhcp_lease_time should be copied to /usr/sbin. They are used by the GUI frontend. Why were these not being compiled or copied?
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@7288 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/dnsmasq.mk
+++ src/router/rules/dnsmasq.mk
@@ -1,8 +1,11 @@
 dnsmasq:
 	$(MAKE) -C dnsmasq COPTS=-DHAVE_BROKEN_RTC CFLAGS="$(COPTS)"
+	$(MAKE) -C dnsmasq/contrib/wrt CFLAGS="$(COPTS)"
 
 dnsmasq-install:
 	install -D dnsmasq/contrib/wrt/lease_update.sh $(INSTALLDIR)/dnsmasq/etc/lease_update.sh
+	install -D dnsmasq/contrib/wrt/dhcp_release $(INSTALLDIR)/dnsmasq/usr/sbin/dhcp_release
+	install -D dnsmasq/contrib/wrt/dhcp_lease_time $(INSTALLDIR)/dnsmasq/usr/sbin/dhcp_lease_time
 	install -D dnsmasq/src/dnsmasq $(INSTALLDIR)/dnsmasq/usr/sbin/dnsmasq
 
 
