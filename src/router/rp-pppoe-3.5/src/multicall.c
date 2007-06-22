709df7de7850f02cb8c5f0a24fe9191cc86df40e
BrainSlayer <s.gottschall@dd-wrt.com>
2007-06-22 00:42:19 +0000

    other size reducing option
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@7305 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rp-pppoe-3.5/src/multicall.c
+++ src/router/rp-pppoe-3.5/src/multicall.c
@@ -1,5 +1,66 @@
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/errno.h>
+#include "pppoe.h"
 
+#define control_exit() (void) 0
+
+extern  void
+printErr(char const *str);
+
+/**********************************************************************
+*%FUNCTION: fatalSys
+*%ARGUMENTS:
+* str -- error message
+*%RETURNS:
+* Nothing
+*%DESCRIPTION:
+* Prints a message plus the errno value to stderr and syslog and exits.
+***********************************************************************/
+void
+fatalSys(char const *str)
+{
+    char buf[SMALLBUF];
+    snprintf(buf, SMALLBUF, "%s: %s", str, strerror(errno));
+    printErr(buf);
+    control_exit();
+    exit(EXIT_FAILURE);
+}
+
+/**********************************************************************
+*%FUNCTION: sysErr
+*%ARGUMENTS:
+* str -- error message
+*%RETURNS:
+* Nothing
+*%DESCRIPTION:
+* Prints a message plus the errno value to syslog.
+***********************************************************************/
+void
+sysErr(char const *str)
+{
+    char buf[1024];
+    sprintf(buf, "%.256s: %.256s", str, strerror(errno));
+    printErr(buf);
+}
+
+/**********************************************************************
+*%FUNCTION: rp_fatal
+*%ARGUMENTS:
+* str -- error message
+*%RETURNS:
+* Nothing
+*%DESCRIPTION:
+* Prints a message to stderr and syslog and exits.
+***********************************************************************/
+void
+rp_fatal(char const *str)
+{
+    printErr(str);
+    control_exit();
+    exit(EXIT_FAILURE);
+}
 
 #ifdef HAVE_PPPOERELAY
 extern int pppoerelay_main(int argc, char *argv[]);
