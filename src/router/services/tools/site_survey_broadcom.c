79bc6cdf2caa0afe286aac4d7859620c1f1e2ed7
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-06-27 20:21:39 +0000

    fixes for the GW2345 switch
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@7325 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/tools/site_survey_broadcom.c
+++ src/router/services/tools/site_survey_broadcom.c
@@ -298,11 +298,11 @@ site_survey_main (int argc, char *argv[])
   params.home_time = -1;
 
   /* can only scan in STA mode */
-#ifndef HAVE_MSSID
+//#ifndef HAVE_MSSID
   wl_ioctl (dev, WLC_GET_AP, &oldap, sizeof (oldap));
   if (oldap > 0)
     eval ("wl", "ap", "0");
-#endif
+//#endif
   if (wl_ioctl (dev, WLC_SCAN, &params, 64) < 0)
     {
       fprintf (stderr, "scan failed\n");
@@ -366,10 +366,10 @@ site_survey_main (int argc, char *argv[])
     }
 
 endss:
-#ifndef HAVE_MSSID
+//#ifndef HAVE_MSSID
   if (oldap > 0)
     eval ("wl", "ap", "1");
-#endif
+//#endif
 
   C_led (0);
 #ifdef HAVE_MSSID
