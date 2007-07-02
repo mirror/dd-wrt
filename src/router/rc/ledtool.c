dde7ea99ca71ff8b66a6dbffe4d54f820e3c2ff1
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-07-02 18:57:53 +0000

    led trigger tool
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@7354 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rc/ledtool.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+
+#include <bcmnvram.h>
+#include <netconf.h>
+#include <shutils.h>
+#include <wlutils.h>
+#include <wlioctl.h>
+#include <rc.h>
+
+#include <cy_conf.h>
+#include <bcmutils.h>
+#include <utils.h>
+#include <nvparse.h>
+
+
+
+int
+ledtool_main (int argc, char **argv)
+{
+  /* Run it in the background */
+  switch (fork ())
+    {
+    case -1:
+      // can't fork
+      exit (0);
+      break;
+    case 0:
+      /* child process */
+      // fork ok
+      (void) setsid ();
+      break;
+    default:
+      /* parent process should just die */
+      _exit (0);
+    }
+int times = atoi(argv[1]);
+while(times >0)
+{
+int count=3;
+  while (count>0)
+    {
+    led_control(LED_DIAG,LED_ON);
+    usleep(500000);
+    led_control(LED_DIAG,LED_OFF);
+    usleep(500000);
+    count--;
+    }
+    sleep(3);
+    times--;
+}
+
+  return 0;
+}				// end main
