d0e8b92316f14d14094cdd7b6601021dc650460d
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-07-08 19:47:52 +0000

    bmoN
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@7405 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/bmon.mk
+++ src/router/rules/bmon.mk
@@ -1,8 +1,11 @@
 bmon:
-	cd bmon && ./configure --host=$(ARCH)-linux --disable-rrd --disable-asound --disable-dbi --disable-curses
+	cd bmon/libnl && ./configure --host $(ARCH)-linux --prefix=/usr
+	make -C bmon/libnl
+	cd bmon && ./configure --host=$(ARCH)-linux --disable-rrd --disable-asound --disable-dbi --disable-curses DEPFLAGS="$(COPTS) -I$(TOP)/bmon/libnl/include"  CFLAGS="$(COPTS) -I$(TOP)/bmon/libnl/include" LDFLAGS="-L$(TOP)/bmon/libnl/lib"
 	make -C bmon
 
 bmon-clean:
 	make -C bmon/src clean
+	make -C bmon/libnl clean
 
 
