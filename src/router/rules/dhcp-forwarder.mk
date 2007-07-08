73b8e20092fbcc5a910804c21e1d8d681a8f93bf
BrainSlayer <s.gottschall@dd-wrt.com>
2007-07-08 19:47:52 +0000

    bmoN
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@7405 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/dhcp-forwarder.mk
+++ src/router/rules/dhcp-forwarder.mk
@@ -1,5 +1,5 @@
 dhcpforwarder:
-	cd dhcpforwarder && ./configure --host=$(ARCH)-linux-elf --sysconfdir=/tmp/dhcp-fwd CC=$(ARCH)-linux-uclibc-gcc CFLAGS="$(COPTS) -DHAVE_MALLOC=1 -Drpl_malloc=malloc" 
+	cd dhcpforwarder && ./configure --host=$(ARCH)-linux-elf --sysconfdir=/tmp/dhcp-fwd CC=$(ARCH)-linux-uclibc-gcc CFLAGS="$(COPTS) -DHAVE_MALLOC=1 -Drpl_malloc=malloc -DNEED_PRINTF" 
 	$(MAKE) -C dhcpforwarder
 
 dhcpforwarder-install:
