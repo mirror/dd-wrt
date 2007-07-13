0c75f5bf1f314f292456ab7f7ada15d02901a0d2
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-07-13 22:50:07 +0000

    final gw2345 fix for 2.6.22 and new ethernet driver
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@7476 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/utils.c
+++ src/router/shared/utils.c
@@ -176,12 +176,12 @@ internal_getRouterBrand ()
       setRouter ("Avila GW2347");
       return ROUTER_BOARD_GATEWORX_SWAP;
     }
-  else if (reg1 = 0x13 && reg2 == 0x7a11)
+  else if (reg1 == 0x13 && reg2 == 0x7a11)
     {
       setRouter ("Avila GW2348-4/2");
       return ROUTER_BOARD_GATEWORX;
     }
-  else if (reg1 = 0x22 && reg2 == 0x1450)	//kendin switch 
+  else if (reg1 == 0x22 && reg2 == 0x1450)	//kendin switch 
     {
       setRouter ("Avila GW2345");
       return ROUTER_BOARD_GATEWORX_GW2345;
