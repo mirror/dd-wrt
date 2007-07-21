cb56a9b99bcf77791374478fa141ee2aa0ff846e
chris <chris@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-07-21 14:23:53 +0000

    quagga Extra configure option
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@7519 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/quagga.mk
+++ src/router/rules/quagga.mk
@@ -1,5 +1,5 @@
 quagga:
-	cd quagga && ./configure --host=$(ARCH)-uclibc-linux --enable-opaque-lsa --enable-ospf-te --disable-ospfclient --enable-multipath=32  --disable-ipv6 --prefix=/usr  --disable-ospf6d  --enable-user=root --enable-group=root --disable-ospfapi
+	cd quagga && ./configure --host=$(ARCH)-uclibc-linux $(CONFIG_QUAGGA_EXTRA) --enable-opaque-lsa --enable-ospf-te --disable-ospfclient --enable-multipath=32  --disable-ipv6 --prefix=/usr  --disable-ospf6d  --enable-user=root --enable-group=root --disable-ospfapi
 	$(MAKE) -C quagga
 
 quagga-install:
