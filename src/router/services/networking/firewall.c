1ef4356fe4d88057a5469b39ee6f8bc055bb4dbc
BrainSlayer <s.gottschall@dd-wrt.com>
2007-08-06 20:23:28 +0000

    add gpl license
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@7631 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/firewall.c
+++ src/router/services/networking/firewall.c
@@ -1,12 +1,23 @@
-
-/* The macro 'DEVELOPE_ENV' is for developement only. If it is defined, you
- * can compile it in your (x86) host machine and run it locally. This speeds
- * up the developing.
+/*
+ * firewall.c
  *
- * Tips :
- * shell> gcc firewall.c -o fw
- * shell> ./fw
- * shell> iptables-restore /tmp/.ipt
+ * Copyright (C) 2007 Sebastian Gottschall <gottschall@dd-wrt.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id:
  */
 
 //#define DEVELOPE_ENV
