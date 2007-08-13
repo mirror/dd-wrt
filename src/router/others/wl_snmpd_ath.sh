a9a5396dea8c2da5aa37d1b76671865727710bc7
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-08-13 11:38:33 +0000

    noise fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@7646 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/others/wl_snmpd_ath.sh
+++ src/router/others/wl_snmpd_ath.sh
@@ -14,13 +14,13 @@ refresh() {
       getnext_1361412021255="$place.3.54.1.3.32.1.1.1"
       getnext_1361412021255354133211="$place.3.54.1.3.32.1.1.1"
       getnext_1361412021255354133214="$place.3.54.1.3.32.1.4.1"
-      getnext_13614120212553541332126="$place.3.54.1.3.32.1.26.1"
       getnext_13614120212553541332113="$place.3.54.1.3.32.1.13.1"
+      getnext_13614120212553541332126="$place.3.54.1.3.32.1.26.1"
     else
       eval getnext_1361412021255354133211${lastid}="$place.3.54.1.3.32.1.1.$id"
       eval getnext_1361412021255354133214${lastid}="$place.3.54.1.3.32.1.4.$id"
-      eval getnext_13614120212553541332126${lastid}="$place.3.54.1.3.32.1.26.$id"
       eval getnext_13614120212553541332113${lastid}="$place.3.54.1.3.32.1.13.$id"
+      eval getnext_13614120212553541332126${lastid}="$place.3.54.1.3.32.1.26.$id"
     fi
   
     rssi=$(wl_atheros rssi $mac | cut -d" " -f3)
@@ -37,10 +37,10 @@ refresh() {
     eval type_1361412021255354133211${id}='integer';
     eval value_1361412021255354133214${id}='$mac';
     eval type_1361412021255354133214${id}='octet';
-    eval value_13614120212553541332126${id}=$snr;
-    eval type_13614120212553541332126${id}='integer';
     eval value_13614120212553541332113${id}=$noise_reference;
     eval type_13614120212553541332113${id}='integer';
+    eval value_13614120212553541332126${id}=$snr;
+    eval type_13614120212553541332126${id}='integer';
 
     lastid=$id
     let id=$id+1
@@ -50,9 +50,9 @@ refresh() {
   if test $lastid -ne 0
   then
     eval getnext_1361412021255354133211${lastid}="$place.3.54.1.3.32.1.4.1"
-    eval getnext_1361412021255354133214${lastid}="$place.3.54.1.3.32.1.26.1"
+    eval getnext_1361412021255354133214${lastid}="$place.3.54.1.3.32.1.13.1"
+    eval getnext_13614120212553541332113${lastid}="$place.3.54.1.3.32.1.26.1"
     eval getnext_13614120212553541332126${lastid}="NONE"
-    eval getnext_13614120212553541332113${lastid}="NONE"
   fi
 } 
 
