4ddb3ee48c0bc60e282f369e1786712c735b9a93
BrainSlayer <s.gottschall@dd-wrt.com>
2007-08-21 17:10:17 +0000

    check only for station on the first interface
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@7724 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/dd-wrt.c
+++ src/router/httpd/modules/dd-wrt.c
@@ -1574,7 +1574,7 @@ selmatch (char *var, char *is, char *ret)
 }
 
 static void
-show_security_prefix (webs_t wp, int argc, char_t ** argv, char *prefix)
+show_security_prefix (webs_t wp, int argc, char_t ** argv, char *prefix,int primary)
 {
   char var[80];
   char sta[80];
@@ -1596,7 +1596,7 @@ show_security_prefix (webs_t wp, int argc, char_t ** argv, char *prefix)
   websWrite (wp, "<option value=\"psk\" %s>WPA Personal</option>\n",
 	     selmatch (var, "psk", "selected=\"selected\""));
   sprintf (sta, "%s_mode", prefix);
-  if (nvram_match (sta, "ap"))
+  if (!primary || nvram_match (sta, "ap"))
     {
       websWrite (wp, "<option value=\"wpa\" %s>WPA Enterprise</option>\n",
 		 selmatch (var, "wpa", "selected=\"selected\""));
@@ -1604,7 +1604,7 @@ show_security_prefix (webs_t wp, int argc, char_t ** argv, char *prefix)
   websWrite (wp,
 	     "<option value=\"psk2\" %s>WPA2 Personal</option>\n",
 	     selmatch (var, "psk2", "selected=\"selected\""));
-  if (nvram_match (sta, "ap"))
+  if (!primary || nvram_match (sta, "ap"))
     {
       websWrite (wp, "<option value=\"wpa2\" %s>WPA2 Enterprise</option>\n",
 		 selmatch (var, "wpa2", "selected=\"selected\""));
@@ -1620,7 +1620,7 @@ show_security_prefix (webs_t wp, int argc, char_t ** argv, char *prefix)
   websWrite (wp, "<option value=\"wep\" %s>WEP</option>\n",
 	     selmatch (var, "wep", "selected=\"selected\""));
 #ifdef HAVE_WPA_SUPPLICANT
-  if (nvram_match (sta, "sta") || nvram_match (sta, "apsta") || nvram_match (sta, "wet"))
+  if (!primary || nvram_match (sta, "sta") || nvram_match (sta, "apsta") || nvram_match (sta, "wet"))
     {
       websWrite (wp, "<option value=\"8021X\" %s>802.1x</option>\n",
 		 selmatch (var, "8021X", "selected=\"selected\""));
@@ -1656,7 +1656,7 @@ ej_show_security_single (webs_t wp, int argc, char_t ** argv, char *prefix)
   websWrite (wp,
 	     "<legend><script type=\"text/javascript\">Capture(share.pintrface)</script> %s SSID [%s] HWAddr [%s]</legend>\n",
 	     prefix, nvram_safe_get (ssid), nvram_safe_get (mac));
-  show_security_prefix (wp, argc, argv, prefix);
+  show_security_prefix (wp, argc, argv, prefix, 1);
   websWrite (wp, "</fieldset>\n<br />\n");
   foreach (var, vifs, next)
   {
@@ -1667,7 +1667,7 @@ ej_show_security_single (webs_t wp, int argc, char_t ** argv, char *prefix)
 	       "<legend><script type=\"text/javascript\">Capture(share.vintrface)</script> %s SSID [%s]</legend>\n",
 	       var, nvram_get (ssid));
     rep (var, '.', 'X');
-    show_security_prefix (wp, argc, argv, var);
+    show_security_prefix (wp, argc, argv, var, 0);
     websWrite (wp, "</fieldset>\n<br />\n");
   }
 
