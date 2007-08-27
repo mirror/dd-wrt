1f09ffd5c4f3b516631459af3a19cab9565e5598
eko <USER@DOMAIN>
2007-08-27 19:11:15 +0000

    force-reconect: cron: must be newline terminated to work.
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@7758 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/services.c
+++ src/router/services/services/services.c
@@ -361,7 +361,7 @@ start_cron (void)
     {
       FILE *fp;
       fp = fopen ("/tmp/cron.d/pppoe_reconnect", "w");
-      fprintf (fp, "%s %s * * * root /usr/bin/killall pppd",
+      fprintf (fp, "%s %s * * * root /usr/bin/killall pppd\n",
 	       nvram_safe_get ("reconnect_minutes"),
 	       nvram_safe_get ("reconnect_hours"));
       fclose (fp);
