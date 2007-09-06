216381cd0a2b38bdbd6c7b83375e81dec6006dd1
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-09-06 13:41:13 +0000

    allow 5 and 10 mhz channels for fonera
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@7837 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/ej.c
+++ src/router/httpd/ej.c
@@ -284,9 +284,9 @@ do_ej_buffer (char *buffer, webs_t stream)	// jimmy, https, 8/4/2003
 		  *end++ = '\0';
 
 		  /* Call function */
-		  cdebug (func);
+		  cprintf ("calling %s\n",func);
 		  call (func, stream);
-		  cdebug (func);
+		  cprintf ("leaving %s\n",func);
 		}
 	      asp = NULL;
 	      len = 0;
