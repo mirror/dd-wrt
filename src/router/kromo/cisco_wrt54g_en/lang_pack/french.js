5f462e6de82f8e75f9cdc3270c9fdd5dd71147f2
botho <botho@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-09-09 18:25:26 +0000

    bandwidth monitoring translation
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@7903 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/cisco_wrt54g_en/lang_pack/french.js
+++ src/router/kromo/cisco_wrt54g_en/lang_pack/french.js
@@ -1406,7 +1406,6 @@ wl_adv.shrt="Court";
 //help container
 
 hwl_adv.right2="La valeur <em>Auto</em> définie par défaut vous permet de choisir entre une authentification <em>Système ouvert</em> ou <em>Clé partagée</em>. En mode d'authentification <em>Système ouvert</em>, l'expéditeur et le destinataire n'utilisent pas de clé WEP pour l'authentification. En revanche, ils utilisent ce type de clé en mode <em>Clé partagée</em>.";
-hwl_basic.right6="Sélectionnez les heures pendant lesquelles le signal radio sans fil sera activé (<em>vert</em> indique que le signal sans fil est activé, et <em>rouge</em> indique que le signal est désactivé).";
 
 // ** Wireless_Basic.asp **//
 wl_basic.titl="Sans Fil";
@@ -1468,7 +1467,7 @@ wl_basic.preambletime="OFDM Preamble Time";
 hwl_basic.right2="Si vous disposez à la fois des périphériques 802.11g et 802.11b sur votre réseau, conservez le paramètre par défaut <em>Mixte</em>. Si vous utilisez uniquement des périphériques 802.11g, sélectionnez <em>G-uniquement</em>. Si vous travaillez uniquement avec des périphériques 802.11b, sélectionnez <em>B-uniquement</em>. Si votre réseau n'est muni d'aucun périphérique sans fil, sélectionnez l'option <em>Désactiver</em>.</br><b>Remarque :</b> quand vous changez de mode, certains paramètres avancés sont succeptibles d'être modifiés ("+wl_adv.label16+", "+wl_adv.label2+" ou "+wl_adv.label5+").";
 hwl_basic.right3="Sensibilité: ";
 hwl_basic.right4="Permet de régler le <em>ack timing</em>. 0 désactive complètement cette fonctionnalité.";
-
+hwl_basic.right6="Sélectionnez les heures pendant lesquelles le signal radio sans fil sera activé (<em>vert</em> indique que le signal sans fil est activé, et <em>rouge</em> indique que le signal est désactivé).";
 
 // ** Fail_s.asp / Fail_u_s.asp / Fail.asp **//
 
