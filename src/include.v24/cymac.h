9a1f25bbe023ddf40073b5c38e1e09e036a92436
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-09-20 14:05:49 +0000

    quirk
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@7991 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/include.v24/cymac.h
+++ src/include.v24/cymac.h
@@ -75,4 +75,23 @@ NOT_CNULL(unsigned char *var, int m, unsigned char c, int len) {
                 } \
                 sprintf(mac,"%02X:%02X:%02X:%02X:%02X:%02X",m[0],m[1],m[2],m[3],m[4],m[5]); \
 })
+#define MAC_SUB(mac) ({\
+                int i,j; \
+                unsigned char m[6]; \
+                /* sscanf(mac,"%x:%x:%x:%x:%x:%x",&m[0],&m[1],&m[2],&m[3],&m[4],&m[5]);   will error */ \
+                for(j=0,i=0 ; i<PER_MAC_LEN ; i+=3,j++) { \
+                        if(mac[i] >= 'A' && mac[i] <= 'F')              mac[i] = mac[i] - 55;\
+                        if(mac[i+1] >= 'A' && mac[i+1] <= 'F')  mac[i+1] = mac[i+1] - 55;\
+                        if(mac[i] >= 'a' && mac[i] <= 'f')              mac[i] = mac[i] - 87;\
+                        if(mac[i+1] >= 'a' && mac[i+1] <= 'f')  mac[i+1] = mac[i+1] - 87;\
+                        if(mac[i] >= '0' && mac[i] <= '9')              mac[i] = mac[i] - 48;\
+                        if(mac[i+1] >= '0' && mac[i+1] <= '9')  mac[i+1] = mac[i+1] - 48;\
+                        m[j] = mac[i]*16 + mac[i+1]; \
+                } \
+                for(i=5 ; i>=3 ; i--){ \
+                        if( m[i] == 0xFF)       { m[i] = 0x0; continue; } \
+                        else                    { m[i] = m[i] - 1; break; } \
+                } \
+                sprintf(mac,"%02X:%02X:%02X:%02X:%02X:%02X",m[0],m[1],m[2],m[3],m[4],m[5]); \
+})
 
