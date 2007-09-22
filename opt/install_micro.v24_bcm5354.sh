a4f5ce845864a1b56e44985d3f62c2839384acd3
tornado <tornado@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-09-22 04:41:47 +0000

    Commented out - not used
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@8013 52c4871e-980c-0410-b1e0-e73912ce01f8

--- opt/install_micro.v24_bcm5354.sh
+++ opt/install_micro.v24_bcm5354.sh
@@ -67,7 +67,7 @@ export TARGETDIR=../src/router/mipsel-uclibc/target
 
 
 ../src/linux/brcm/linux.v23/scripts/squashfs/mksquashfs-lzma ../src/router/mipsel-uclibc/target target.squashfs -noappend -root-owned -le
-./make_kernel.v24.sh
+#./make_kernel.v24.sh
 ../tools/trx -o dd-wrt.v23.trx ./loader-0.02/loader.gz ../src/router/mipsel-uclibc/vmlinuz target.squashfs
 ../tools/trx_gs -o dd-wrt.v23_gs.trx ./loader-0.02/loader.gz ../src/router/mipsel-uclibc/vmlinuz target.squashfs
 #./asus/asustrx -p WL500gx -v 1.9.2.7 -o dd-wrt.v23_asus.trx ./loader-0.02/loader.gz ../src/router/mipsel-uclibc/vmlinuz target.squashfs
