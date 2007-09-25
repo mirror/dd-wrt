0ff8c36406f5b84d9a0672e6af83d31b94610e16
botho <USER@DOMAIN>
2007-09-25 19:51:35 +0000

    translation
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@8061 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/cisco_wrt54g_en/lang_pack/french.js
+++ src/router/kromo/cisco_wrt54g_en/lang_pack/french.js
@@ -1135,7 +1135,7 @@ hstatus_lan.right10="En cliquant sur n'importe quelle adresse Mac, vous obtiendr
 // ** Status_Bandwidth.asp **//
 status_band.titl="Statut de la Bande Passante";
 status_band.h2="Etat de la Bande Passante";
-status_band.chg_unit="Echelle en ";
+status_band.chg_unit="Basculer en ";
 status_band.chg_scale="Echelle auto";
 status_band.chg_error="Impossible d'obtenir les interfaces réseaux";
 status_band.chg_collect_initial="Mise en cache des données, veuillez patienter SVP...";
@@ -1145,8 +1145,8 @@ status_band.follow="Suivre";
 status_band.up="Haut";
 
 //help container
-hstatus_band.right1="Cliquez sur l'étiquette \"<em>Echelle en ...</em>\" pour changer d'unité ("+share.bytes+"/s ou bits/s).";
-hstatus_band.right2="Cliquer sur \"<em>Echelle auto ...</em>\" permet de modifier le type d'échelle (<em>"+status_band.follow+"</em> ajuste l'echelle en permanence tandis que <em>"+status_band.up+"</em> ajuste l'echelle sur le pic maximum constaté).";
+hstatus_band.right1="Cliquez sur l'étiquette \"<em>"+status_band.chg_unit+" ...</em>\" pour changer d'unité ("+share.bytes+"/s ou bits/s).";
+hstatus_band.right2="Cliquer sur \"<em>"+status_band.chg_scale+" ...</em>\" permet de modifier le type d'échelle (<em>"+status_band.follow+"</em> ajuste l'echelle en permanence tandis que <em>"+status_band.up+"</em> ajuste l'echelle sur le pic maximum constaté).";
 
 // ** Status_Router.asp **//
 status_router.titl="Statut du Routeur";
