0ce598db0bc43de89aaf703e5a70d1bff6f4fea9
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-10-04 01:34:44 +0000

    make it possible to set username and password from commandline
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@8103 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/services/tools/setuserpw.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <wait.h>
+
+#include <bcmnvram.h>
+#include <netconf.h>
+#include <shutils.h>
+#include <utils.h>
+#include <cy_conf.h>
+#include <code_pattern.h>
+#include <rc.h>
+
+int
+setuserpasswd_main (int argc, char **argv)
+{
+
+
+  if (!argv[1] || !argv[2])
+    {
+      fprintf (stderr, "usage: setuserpasswd [username] [password]\n");
+      return EINVAL;
+    }
+  nvram_set ("http_username", zencrypt (argv[1]));
+  nvram_set ("http_passwd", zencrypt (argv[2]));
+  nvram_commit ();
+}
