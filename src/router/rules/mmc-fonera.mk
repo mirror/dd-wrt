4944bb54ef59d2dd31652e912d2618ae0eb78d1d
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-10-06 17:44:33 +0000

    fonera mmc driver integration
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@8141 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/mmc-fonera.mk
+++ src/router/rules/mmc-fonera.mk
@@ -3,9 +3,9 @@ mmc-fonera:
 
 mmc-fonera-install:
 	install -D  mmc-fonera/mmc.ko $(INSTALLDIR)/mmc-fonera/lib/modules/$(KERNELRELEASE)/mmc.ko
-	install -D  mmc/config/mmc.webconfig $(INSTALLDIR)/mmc/etc/config/mmc.webconfig
-	install -D  mmc/config/mmc.startup $(INSTALLDIR)/mmc/etc/config/mmc.startup
-	install -D  mmc/config/mmc.nvramconfig $(INSTALLDIR)/mmc/etc/config/mmc.nvramconfig
+	install -D  mmc-fonera/config/mmc.webconfig $(INSTALLDIR)/mmc-fonera/etc/config/mmc.webconfig
+	install -D  mmc-fonera/config/mmc.startup $(INSTALLDIR)/mmc-fonera/etc/config/mmc.startup
+	install -D  mmc-fonera/config/mmc.nvramconfig $(INSTALLDIR)/mmc-fonera/etc/config/mmc.nvramconfig
 
 mmc-fonera-clean:
 	$(MAKE) -C $(LINUXDIR) M=$(TOP)/mmc-fonera clean	
