551f6ea827fe07ca067973906ad84c8ba8fee779
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2007-10-29 03:24:38 +0000

    reinitialize routing if a pptp user connects
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@8281 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/pptp.c
+++ src/router/services/services/pptp.c
@@ -170,6 +170,7 @@ start_pptpd (void)
 
   fp = fopen ("/tmp/pptpd/ip-up", "w");
   fprintf (fp, "#!/bin/sh\n"
+	   "/sbin/startservice set_routes\n"  // reinitialize routing, just in case that a target route exists
 	   "/usr/sbin/iptables -I FORWARD -i $1 -p tcp --tcp-flags SYN,RST SYN -m tcpmss --mss %d: -j TCPMSS --set-mss %d\n"
 	   "/usr/sbin/iptables -I INPUT -i $1 -j ACCEPT\n"
 	   "/usr/sbin/iptables -I FORWARD -i $1 -j ACCEPT\n"
