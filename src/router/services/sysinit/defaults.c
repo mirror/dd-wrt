f16ab157f3fc6831e6bf1904ce71416ba591d019
milkfish <USER@DOMAIN>
2007-11-04 01:23:08 +0000

    new phonebook variables
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@8324 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/defaults.c
+++ src/router/services/sysinit/defaults.c
@@ -2108,6 +2108,8 @@ struct nvram_tuple srouter_defaults[] = {
   {"milkfish_ddsubscribersnum", "0", 0},      /* MILKFISH SETTING */
   {"milkfish_ddaliases", "", 0},      /* MILKFISH SETTING */
   {"milkfish_ddaliasesnum", "0", 0},      /* MILKFISH SETTING */
+  {"milkfish_ddactive", "", 0},      /* MILKFISH SETTING */
+  {"milkfish_ddactivenum", "0", 0},      /* MILKFISH SETTING */
 #endif
 #ifdef HAVE_OLSRD
   {"olsrd_pollsize", "0.1", 0},
