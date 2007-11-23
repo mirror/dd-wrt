e62a853686cec55bb67397134e7463d0d23e2a57
BrainSlayer <s.gottschall@dd-wrt.com>
2007-11-23 16:39:15 +0000

    update strip lib script
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@8427 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/strip_libs.sh
@@ -0,0 +1,19 @@
+
+export ARCH=$1
+# strip executables
+./sstrip/sstrip $ARCH-uclibc/target/bin/*
+./sstrip/sstrip $ARCH-uclibc/target/sbin/rc
+./sstrip/sstrip $ARCH-uclibc/target/usr/sbin/*
+./sstrip/sstrip $ARCH-uclibc/target/usr/bin/*
+./sstrip/sstrip $ARCH-uclibc/target/sbin/*
+# strip libraries
+./sstrip/sstrip $ARCH-uclibc/target/usr/lib/*
+./sstrip/sstrip $ARCH-uclibc/target/usr/lib/iptables/*
+./sstrip/sstrip $ARCH-uclibc/target/usr/lib/l2tp/*
+./sstrip/sstrip $ARCH-uclibc/target/usr/lib/ser/modules/*
+./sstrip/sstrip $ARCH-uclibc/target/usr/lib/l2tp/*
+./sstrip/sstrip $ARCH-uclibc/target/lib/*.so
+./sstrip/sstrip $ARCH-uclibc/target/lib/*.so.1
+./sstrip/sstrip $ARCH-uclibc/target/lib/*.so.0
+./sstrip/sstrip $ARCH-uclibc/target/lib/*.so.6
+./sstrip/sstrip $ARCH-uclibc/target/lib/*.so.29
