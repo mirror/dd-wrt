1b8ff7848da3496ef21431d483b69ac470acac9f
BrainSlayer <s.gottschall@dd-wrt.com>
2007-11-29 14:10:32 +0000

    another header folder
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@8454 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/include.bcm/sbhndpio.h
@@ -0,0 +1,50 @@
+/*
+ * Generic Broadcom Home Networking Division (HND) PIO engine HW interface
+ * This supports the following chips: BCM42xx, 44xx, 47xx .
+ *
+ * Copyright 2007, Broadcom Corporation
+ * All Rights Reserved.
+ * 
+ * THIS SOFTWARE IS OFFERED "AS IS", AND BROADCOM GRANTS NO WARRANTIES OF ANY
+ * KIND, EXPRESS OR IMPLIED, BY STATUTE, COMMUNICATION OR OTHERWISE. BROADCOM
+ * SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A SPECIFIC PURPOSE OR NONINFRINGEMENT CONCERNING THIS SOFTWARE.
+ * $Id$
+ */
+
+#ifndef	_sbhndpio_h_
+#define	_sbhndpio_h_
+
+/* PIO structure,
+ *  support two PIO format: 2 bytes access and 4 bytes access
+ *  basic FIFO register set is per channel(transmit or receive)
+ *  a pair of channels is defined for convenience
+ */
+
+/* 2byte-wide pio register set per channel(xmt or rcv) */
+typedef volatile struct {
+	uint16	fifocontrol;
+	uint16	fifodata;
+	uint16	fifofree;	/* only valid in xmt channel, not in rcv channel */
+	uint16	PAD;
+} pio2regs_t;
+
+/* a pair of pio channels(tx and rx) */
+typedef volatile struct {
+	pio2regs_t	tx;
+	pio2regs_t	rx;
+} pio2regp_t;
+
+/* 4byte-wide pio register set per channel(xmt or rcv) */
+typedef volatile struct {
+	uint32	fifocontrol;
+	uint32	fifodata;
+} pio4regs_t;
+
+/* a pair of pio channels(tx and rx) */
+typedef volatile struct {
+	pio4regs_t	tx;
+	pio4regs_t	rx;
+} pio4regp_t;
+
+#endif /* _sbhndpio_h_ */
