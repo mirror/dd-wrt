ae87fea0c059718d0d890346356209ba4e232801
milkfish <USER@DOMAIN>
2007-12-01 00:30:00 +0000

    new phonebook function added
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@8461 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/milkfish.c
+++ src/router/httpd/modules/milkfish.c
@@ -372,3 +372,86 @@ fclose(fp);
   return;
 }
 
+void
+show_registrations_table (webs_t wp, char *type, int which)
+{
+
+  static char word[256];
+  char *next, *wordlist;
+  char *user, *contact, *agent;
+  static char new_user[200], new_contact[200], new_agent[200];
+  int temp;
+  wordlist = nvram_safe_get ("milkfish_ddactive");
+  temp = which;
+
+  foreach (word, wordlist, next)
+  {
+    if (which-- == 0)
+      {
+        contact = word;
+        user = strsep (&contact, ":");
+        if (!user || !contact)
+          continue;
+
+        agent = contact;
+        contact = strsep (&agent, ":");
+        if (!contact || !agent)
+          continue;
+       
+
+        if (!strcmp (type, "user"))
+          {
+            httpd_filter_name (user, new_user, sizeof (new_user), GET);
+            websWrite (wp, "%s", new_user);
+          }
+        else if (!strcmp (type, "contact"))
+          {
+            httpd_filter_name (contact, new_contact, sizeof (new_contact), GET);
+            websWrite (wp, "%s", new_contact);
+          }
+        else if (!strcmp (type, "agent"))
+          {
+            httpd_filter_name (agent, new_agent, sizeof (new_agent), GET);
+            websWrite (wp, "%s", new_agent);
+          }
+
+        return;
+      }
+   }
+}
+
+
+void
+ej_show_registrations (webs_t wp, int argc, char_t ** argv)
+{
+  int i;
+  char *count;
+  int c;
+  count = nvram_safe_get ("milkfish_ddactivenum");
+  if (count == NULL || strlen (count) == 0 || (c=atoi(count))<=0)
+  {
+      websWrite (wp, "<tr>\n");
+      websWrite (wp,
+                 "<td colspan=\"4\" align=\"center\" valign=\"middle\">- <script type=\"text/javascript\">Capture(share.none)</script> -</td>\n");
+      websWrite (wp, "</tr>\n");
+  }
+  for (i = 0; i < c; i++)
+    {
+      websWrite (wp, "<tr><td>\n");
+      websWrite (wp, "<input maxlength=\"20\" size=\"20\" name=\"user%d\" onblur=\"valid_name(this,'Name')\" value=\"",i);
+      show_registrations_table (wp, "user", i);
+      websWrite (wp, "\" readonly=\"readonly\" /></td>\n");
+      websWrite (wp, "<td>\n");
+      websWrite (wp, "<input maxlength=\"50\" size=\"50\" name=\"contact%d\" onblur=\"valid_name(this,'Name')\" value=\"",i);
+      show_registrations_table (wp, "contact", i);
+      websWrite (wp, "\" readonly=\"readonly\" /></td>\n");
+      websWrite (wp, "<td>\n");
+      websWrite (wp, "<input maxlength=\"50\" size=\"50\" name=\"agent%d\" onblur=\"valid_name(this,'Name')\" value=\"",i);
+      show_registrations_table (wp, "agent", i);
+      websWrite (wp, "\" readonly=\"readonly\" /></td>\n");
+      websWrite (wp, "</tr>\n");
+    }
+  return;
+}
+
+
