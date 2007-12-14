304932c7c92b58b1b5c34f58a4ea2b86b555d0b3
BrainSlayer <s.gottschall@dd-wrt.com>
2007-12-14 17:58:08 +0000

    not required
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@8609 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/include.v23/proto/ethernet.h
+++ src/include.v23/proto/ethernet.h
@@ -1,14 +1,16 @@
-/*******************************************************************************
- * $Id: ethernet.h,v 1.1.1.10 2004/11/09 06:36:21 honor Exp $
- * Copyright 2004, Broadcom Corporation      
- * All Rights Reserved.      
- *       
- * THIS SOFTWARE IS OFFERED "AS IS", AND BROADCOM GRANTS NO WARRANTIES OF ANY      
- * KIND, EXPRESS OR IMPLIED, BY STATUTE, COMMUNICATION OR OTHERWISE. BROADCOM      
- * SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS      
- * FOR A SPECIFIC PURPOSE OR NONINFRINGEMENT CONCERNING THIS SOFTWARE.      
+/*
  * From FreeBSD 2.2.7: Fundamental constants relating to ethernet.
- ******************************************************************************/
+ *
+ * Copyright 2006, Broadcom Corporation
+ * All Rights Reserved.
+ * 
+ * THIS SOFTWARE IS OFFERED "AS IS", AND BROADCOM GRANTS NO WARRANTIES OF ANY
+ * KIND, EXPRESS OR IMPLIED, BY STATUTE, COMMUNICATION OR OTHERWISE. BROADCOM
+ * SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A SPECIFIC PURPOSE OR NONINFRINGEMENT CONCERNING THIS SOFTWARE.
+ *
+ * $Id: ethernet.h,v 1.1.1.14 2006/02/27 03:43:16 honor Exp $
+ */
 
 #ifndef _NET_ETHERNET_H_	    /* use native BSD ethernet.h when available */
 #define _NET_ETHERNET_H_
@@ -71,7 +73,9 @@
 #define ETHER_TYPE_8021Q	0x8100		/* 802.1Q */
 #define	ETHER_TYPE_BRCM		0x886c		/* Broadcom Corp. */
 #define	ETHER_TYPE_802_1X	0x888e		/* 802.1x */
-#define	ETHER_TYPE_802_1X_PREAUTH	0x88c7	/* 802.1x preauthentication*/
+#ifdef BCMWPA2
+#define	ETHER_TYPE_802_1X_PREAUTH 0x88c7	/* 802.1x preauthentication */
+#endif
 
 /* Broadcom subtype follows ethertype;  First 2 bytes are reserved; Next 2 are subtype; */
 #define	ETHER_BRCM_SUBTYPE_LEN	4		/* Broadcom 4 byte subtype */
@@ -105,7 +109,13 @@ struct	ether_header {
 struct	ether_addr {
 	uint8 octet[ETHER_ADDR_LEN];
 } PACKED;
-#endif
+#endif	/* !__INCif_etherh Quick and ugly hack for VxWorks */
+
+/*
+ * Takes a pointer, sets locally admininistered
+ * address bit in the 48-bit Ethernet address.
+ */
+#define ETHER_SET_LOCALADDR(ea)	(((uint8 *)(ea))[0] = (((uint8 *)(ea))[0] | 2))
 
 /*
  * Takes a pointer, returns true if a 48-bit multicast address
@@ -113,17 +123,17 @@ struct	ether_addr {
  */
 #define ETHER_ISMULTI(ea) (((uint8 *)(ea))[0] & 1)
 
+
 /* compare two ethernet addresses - assumes the pointers can be referenced as shorts */
-#define	ether_cmp(a, b)	( \
-	!(((short*)a)[0] == ((short*)b)[0]) | \
-	!(((short*)a)[1] == ((short*)b)[1]) | \
-	!(((short*)a)[2] == ((short*)b)[2]))
+#define	ether_cmp(a, b)	(!(((short*)a)[0] == ((short*)b)[0]) | \
+			 !(((short*)a)[1] == ((short*)b)[1]) | \
+			 !(((short*)a)[2] == ((short*)b)[2]))
 
 /* copy an ethernet address - assumes the pointers can be referenced as shorts */
 #define	ether_copy(s, d) { \
-	((short*)d)[0] = ((short*)s)[0]; \
-	((short*)d)[1] = ((short*)s)[1]; \
-	((short*)d)[2] = ((short*)s)[2]; }
+		((short*)d)[0] = ((short*)s)[0]; \
+		((short*)d)[1] = ((short*)s)[1]; \
+		((short*)d)[2] = ((short*)s)[2]; }
 
 /*
  * Takes a pointer, returns true if a 48-bit broadcast (all ones)
@@ -146,12 +156,6 @@ static const struct ether_addr ether_bcast = {{255, 255, 255, 255, 255, 255}};
 			    ((uint8 *)(ea))[3] |		\
 			    ((uint8 *)(ea))[4] |		\
 			    ((uint8 *)(ea))[5]) == 0)
-
-/* Differentiated Services Codepoint - upper 6 bits of tos in iphdr */
-#define	DSCP_MASK		0xFC		/* upper 6 bits */
-#define	DSCP_SHIFT		2
-#define	DSCP_WME_PRI_MASK	0xE0		/* upper 3 bits */
-#define	DSCP_WME_PRI_SHIFT	5
 
 #undef PACKED
 #if !defined(__GNUC__)
