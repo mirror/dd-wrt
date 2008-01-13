660e005585d99e9d14e3906016ad4a7e92fbf99f
eko <USER@DOMAIN>
2008-01-13 11:16:24 +0000

    typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@8766 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-broadcom.c
+++ src/router/services/sysinit/sysinit-broadcom.c
@@ -238,7 +238,7 @@ loadWlModule (void)		//set wled params, get boardflags, set afterburner bit, loa
     case ROUTER_WRT150N:
     case ROUTER_WRT150NV11:
     case ROUTER_WRT160N:
-    case ROUTER_WRT310N
+    case ROUTER_WRT310N:
     case ROUTER_WRT300N:
     case ROUTER_WRT300NV11:
     case ROUTER_WRT350N:
