7183dd96a184ba6837ea32b891789dc3d3009d7b
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-01-16 14:06:42 +0000

    WZR res. to def. fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@8782 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/resetbutton.c
+++ src/router/rc/resetbutton.c
@@ -429,10 +429,6 @@ period_check (int sig)
 		led_control (LED_DIAG, LED_ON);  //turn diag led on, so we know reset was pressed and we're restoring defaults.
 #endif
 		eval ("erase", "nvram");
-		if (brand == ROUTER_BUFFALO_WZRRSG54)
-		{ 
-			nvram_unset_all ();
-		}
 #endif
 
 
