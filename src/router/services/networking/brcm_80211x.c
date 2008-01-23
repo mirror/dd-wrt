801df6fe90f22da75f76a4d0c8dda486f2c13d5c
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-01-23 00:29:18 +0000

    not required anymore
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@8833 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/brcm_80211x.c
+++ src/router/services/networking/brcm_80211x.c
@@ -70,8 +70,8 @@ setupSupplicant (char *prefix)
 #endif
       fprintf (fp, "fast_reauth=1\n");
       fprintf (fp, "eapol_version=1\n");
-      fprintf (fp, "ctrl_interface_group=0\n");
-      fprintf (fp, "ctrl_interface=/var/run/wpa_supplicant\n");
+     // fprintf (fp, "ctrl_interface_group=0\n");
+     // fprintf (fp, "ctrl_interface=/var/run/wpa_supplicant\n");
 
       fprintf (fp, "network={\n");
       sprintf (psk, "%s_ssid", prefix);
@@ -132,8 +132,8 @@ setupSupplicant (char *prefix)
 #endif
       fprintf (fp, "fast_reauth=1\n");
       fprintf (fp, "eapol_version=2\n");
-      fprintf (fp, "ctrl_interface_group=0\n");
-      fprintf (fp, "ctrl_interface=/var/run/wpa_supplicant\n");
+      //fprintf (fp, "ctrl_interface_group=0\n");
+      //fprintf (fp, "ctrl_interface=/var/run/wpa_supplicant\n");
       fprintf (fp, "network={\n");
       sprintf (psk, "%s_ssid", prefix);
       fprintf (fp, "\tssid=\"%s\"\n", nvram_safe_get (psk));
