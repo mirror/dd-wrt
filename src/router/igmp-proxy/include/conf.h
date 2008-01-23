2f6d03de187c399e64a7f21cf8fd1b08fe712e44
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-01-23 19:11:10 +0000

    new igmp code
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@8841 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/igmp-proxy/include/conf.h
@@ -0,0 +1,10 @@
+#ifndef CONF_H__
+#define CONF_H__
+
+#define DEFAULT_CONF_FILE_PATH "/tmp/igmprt.conf"
+#define DEFAULT_PID_FILE_NAME "/tmp/igmprt.pid"
+
+#define DEFAULT_VERSION 34
+#define DEFAULT_ISQUERIER 1
+
+#endif
