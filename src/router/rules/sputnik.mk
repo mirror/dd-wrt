84549ab39cde4f76b0cbeb9a1041d6aa1e59751a
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-01-31 18:46:49 +0000

    sputnik pro fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@8916 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/sputnik.mk
+++ src/router/rules/sputnik.mk
@@ -6,6 +6,10 @@ sputnik-clean:
 
 sputnik-install:
 	mkdir -p $(INSTALLDIR)/sputnik/etc/config
-	cp -fpR sputnik/config/* $(INSTALLDIR)/sputnik/etc/config
+ifeq ($(CONFIG_SPUTNIK_PRO),y)
+	cp -fpR sputnik/config/1sputnik.webhotspot_pro $(INSTALLDIR)/sputnik/etc/config/1sputnik.webhotspot
+else
+	cp -fpR sputnik/config/1sputnik.webhotspot $(INSTALLDIR)/sputnik/etc/config/1sputnik.webhotspot
+endif
 	install -D sputnik/$(ARCH)/sputnik $(INSTALLDIR)/sputnik/usr/sbin/sputnik
 	install -D sputnik/$(ARCH)/libiksemel.so $(INSTALLDIR)/sputnik/usr/lib/libiksemel.so
