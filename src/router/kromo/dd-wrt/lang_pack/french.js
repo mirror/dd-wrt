e08fed080617020459f6d0c9d5d9959757bf3fb9
botho <botho@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-02-10 17:38:11 +0000

    Status_Internet.asp will comming... great !
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@8988 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/dd-wrt/lang_pack/french.js
+++ src/router/kromo/dd-wrt/lang_pack/french.js
@@ -1198,8 +1198,8 @@ status_inet.leasetime="Renouvellement du bail dans";
 status_inet.traff="Trafic";
 status_inet.ttraffin="Total Incoming Trafic (MBytes)";
 status_inet.ttraffout="Total Outgoing Trafic (MBytes)";
-status_inet.previous="Previous Month";
-status_inet.next="Next Month";
+status_inet.previous="Mois précédent";
+status_inet.next="Mois suivant";
 
 //help container
 
