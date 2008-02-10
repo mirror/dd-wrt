21cee2e126def43301d97e4efc0e11ebb5ecbc20
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-02-10 15:23:13 +0000

    traff graph GUI mostly finished
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@8982 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/dd-wrt/lang_pack/japanese.js
+++ src/router/kromo/dd-wrt/lang_pack/japanese.js
@@ -1307,6 +1307,8 @@ status_inet.leasetime="残りリース時間";
 status_inet.traff="Traffic";
 status_inet.ttraffin="Total Incoming Traffic (MBytes)";
 status_inet.ttraffout="Total Outgoing Traffic (MBytes)";
+status_inet.previous="Previous Month";
+status_inet.next="Next Month";
 
 //help container
 
