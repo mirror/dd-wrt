ee47367a549924d79979efe746d34d77326a2d41
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-03-01 12:56:09 +0000

    Netgear WGR614L basic support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9178 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/utils.c
+++ src/router/shared/utils.c
@@ -1088,7 +1088,15 @@ internal_getRouterBrand ()
       cprintf ("router is Asus WL-520GU/GC\n");
       setRouter ("Asus WL-520GU/GC");
       return ROUTER_ASUS_WL520GUGC;
-    }        
+    }   
+    
+  if (boardnum == 83258 &&
+      nvram_match ("boardtype", "0x48E") && nvram_match ("boardrev", "0x10"))
+    {
+      cprintf ("router is Netgear WGR614L\n");
+      setRouter ("Netgear WGR614L");
+      return ROUTER_NETGEAR_WGR614L;
+    }      
 
   if (boardnum == 56 &&
       nvram_match ("boardtype", "0x456") && nvram_match ("boardrev", "0x10"))
