ecd2d9e64ff79d7562a73e84438ae399bfb33ab4
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-03-07 13:55:52 +0000

    ext3 replaced with ext2
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9261 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-broadcom.c
+++ src/router/services/sysinit/sysinit-broadcom.c
@@ -910,10 +910,12 @@ start_sysinit (void)
      	 
          if (nvram_match ("usb_fs_ext3", "1"))
          {
-         	cprintf ("loading jbd\n");
-         	eval ("insmod", "jbd");
-         	cprintf ("loading ext3\n");
-         	eval ("insmod", "ext3");
+         	cprintf ("loading ext2\n");
+         	eval ("insmod", "ext2");	         
+//         	cprintf ("loading jbd\n");
+//         	eval ("insmod", "jbd");
+//         	cprintf ("loading ext3\n");
+//         	eval ("insmod", "ext3");
      	 }
      	  
          if (nvram_match ("usb_fs_fat", "1"))
