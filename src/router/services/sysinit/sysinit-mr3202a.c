e7542dc906b8e3bdf3618e8cc534b2f7d07af551
BrainSlayer <s.gottschall@dd-wrt.com>
2008-03-12 17:00:53 +0000

    fix unresolved symbols and reenable watchdog for mr3202a
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9303 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-mr3202a.c
+++ src/router/services/sysinit/sysinit-mr3202a.c
@@ -91,7 +91,7 @@ start_sysinit (void)
   mkdir ("/var/run", 0777);
   mkdir ("/var/tmp", 0777);
   cprintf ("sysinit() setup console\n");
-//  eval ("/sbin/watchdog");
+  eval ("/sbin/watchdog");
   /* Setup console */
 
   cprintf ("sysinit() klogctl\n");
