e9c009711eada3dbd8d3d1b94e6e77934139d76a
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-03-29 19:51:04 +0000

    ROUTER_NETGEAR_WG602_V4 wlan led
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9358 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-broadcom.c
+++ src/router/services/sysinit/sysinit-broadcom.c
@@ -178,6 +178,7 @@ loadWlModule (void)		//set wled params, get boardflags, set afterburner bit, loa
     case ROUTER_WRT300N:
     case ROUTER_WRT600N:
     case ROUTER_WRT350N:
+    case ROUTER_NETGEAR_WG602_V4:
       nvram_set ("wl0gpio0", "8");
       break;
     case ROUTER_BUFFALO_WHRG54S:
