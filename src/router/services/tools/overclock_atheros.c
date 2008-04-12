40c3bedb6426d7dcf417cbde9a2a4a06364dc154
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-04-12 15:10:28 +0000

    supports now 180,200,220 and 240 mhz clock selections by nvram var "cpuclk"
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9409 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/tools/overclock_atheros.c
+++ src/router/services/tools/overclock_atheros.c
@@ -18,7 +18,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  * desc:
- * overclocks 2313 and 2316/17 or compatible wisoc boards with redboot or wistron zLoader to 220mhz/200mhz
+ * overclocks 2313 and 2316/17 or compatible wisoc boards with redboot or wistron zLoader to any 200 - 240 mhz (you must select it by i.e. nvram set cpuclk=220) 
  * just a dirty hack i found while playing with code 
  */
 #include <unistd.h>
@@ -28,20 +28,19 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <shutils.h>
+#include <bcmnvram.h>
 
 
 /*
 to prevent bricks and other troubles you should use this tool only manually. it will not be implemented in the webgui
 */
 
-void
-start_overclock
- (void)		// hidden feature. must be called with "startservice overlock". then reboot the unit
+void start_overclock (void)	// hidden feature. must be called with "startservice overlock". then reboot the unit
 {
   long len;
   long i;
-#ifdef HAVE_CA8  
-  FILE *in = fopen ("/dev/mtdblock/2", "rb");  // zloader Board Data access. the board data section contains also the cpu programming code
+#ifdef HAVE_CA8
+  FILE *in = fopen ("/dev/mtdblock/2", "rb");	// zLoader Board Data access. the board data section contains also the cpu programming code
 #else
   FILE *in = fopen ("/dev/mtdblock/0", "rb");
 #endif
@@ -53,57 +52,122 @@ start_overclock
     putc (getc (in), out);
   fclose (in);
   fclose (out);
+  int clk = atoi (nvram_default_get ("cpuclk", "180"));
+
   in = fopen ("/tmp/boot", "r+b");
-  fseek (in, 0xe64b, SEEK_SET);				
-  int zmul = getc(in);
-  fseek (in, 0xcb, SEEK_SET);				
+  fseek (in, 0xe64b, SEEK_SET);
+  int zmul = getc (in);
+  fseek (in, 0xcb, SEEK_SET);
   int vipermul = getc (in);
   fseek (in, 0x1e3, SEEK_SET);
   int div = getc (in);
   fseek (in, 0x1ef, SEEK_SET);
   int mul = getc (in);
- // fprintf(stderr,"vipermul %X, div %X, mul %X\n",vipermul,div,mul);
+  // fprintf(stderr,"vipermul %X, div %X, mul %X\n",vipermul,div,mul);
   if (div == 0x3 && mul == 0x5c)
     {
       fprintf (stderr, "ap51/ap61 (ar2315 or ar2317) found\n");
       fseek (in, 0x1e3, SEEK_SET);
       putc (0x1, in);
       fseek (in, 0x1ef, SEEK_SET);
-      putc (0x28, in); //0x2c for 220 mhz 0x30 for 240 mhz
+      if (clk == 200)
+	putc (0x28, in);	//0x2c for 220 mhz 0x30 for 240 mhz
+      else if (clk == 220)
+	putc (0x2c, in);	//0x2c for 220 mhz 0x30 for 240 mhz
+      else if (clk == 240)
+	putc (0x30, in);	//0x2c for 220 mhz 0x30 for 240 mhz
+      else
+	{
+	  nvram_set ("cpuclk", "200");
+	  nvram_commit ();
+	  putc (0x28, in);	//0x2c for 220 mhz 0x30 for 240 mhz
+	}
+
       fclose (in);
       eval ("mtd", "-f", "write", "/tmp/boot", "RedBoot");
       fprintf (stderr, "board is now clocked at 200 mhz, please reboot\n");
     }
-  else if (div == 0x01 && mul == 0x28)
+  if (div == 0x1 && (mul == 0x28 || mul == 0x2c || mul == 0x30))
     {
-      fprintf (stderr, "board already clocked to 200mhz\n");
+      fprintf (stderr, "ap51/ap61 (ar2315 or ar2317) found\n");
+      if (clk == 200 && mul == 0x28)
+	{
+	  fprintf (stderr, "board already clocked to 200mhz\n");
+	  fclose (in);
+	  return;
+	}
+      if (clk == 220 && mul == 0x2c)
+	{
+	  fprintf (stderr, "board already clocked to 220mhz\n");
+	  fclose (in);
+	  return;
+	}
+      if (clk == 240 && mul == 0x30)
+	{
+	  fprintf (stderr, "board already clocked to 240mhz\n");
+	  fclose (in);
+	  return;
+	}
+      fseek (in, 0x1e3, SEEK_SET);
+      putc (0x1, in);
+      fseek (in, 0x1ef, SEEK_SET);
+      if (clk == 200)
+	putc (0x28, in);	//0x2c for 220 mhz 0x30 for 240 mhz
+      else if (clk == 220)
+	putc (0x2c, in);	//0x2c for 220 mhz 0x30 for 240 mhz
+      else if (clk == 240)
+	putc (0x30, in);	//0x2c for 220 mhz 0x30 for 240 mhz
+      else
+	{
+	  nvram_set ("cpuclk", "200");
+	  nvram_commit ();
+	  putc (0x28, in);	//0x2c for 220 mhz 0x30 for 240 mhz
+	}
       fclose (in);
+      eval ("mtd", "-f", "write", "/tmp/boot", "RedBoot");
+      fprintf (stderr, "board is now clocked at 200 mhz, please reboot\n");
     }
-  else if (div == 0x01 && mul == 0x2c)
-    {
-      fprintf (stderr, "board already clocked to 220mhz\n");
-      fclose (in);
-    }
-  else if (div == 0x01 && mul == 0x30)
-    {
-      fprintf (stderr, "board already clocked to 220mhz\n");
-      fclose (in);
-    }
-  else if (vipermul == 0xb)
-    {
-      fprintf (stderr, "board already clocked to 220mhz\n");
-      fclose (in);
-    }
-  else if (vipermul == 0xc)
-    {
-      fprintf (stderr, "board already clocked to 240mhz\n");
-      fclose (in);
-    }
-  else if (vipermul == 0x9)
+  else if (vipermul == 0x9 || vipermul == 0xa || vipermul == 0xb
+	   || vipermul == 0xc)
     {
       fprintf (stderr, "viper (ar2313) found\n");
+      if (clk == 180 && vipermul == 0x9)
+	{
+	  fprintf (stderr, "board already clocked to 180mhz\n");
+	  fclose (in);
+	  return;
+	}
+      if (clk == 200 && vipermul == 0xa)
+	{
+	  fprintf (stderr, "board already clocked to 200mhz\n");
+	  fclose (in);
+	  return;
+	}
+      if (clk == 220 && vipermul == 0xb)
+	{
+	  fprintf (stderr, "board already clocked to 220mhz\n");
+	  fclose (in);
+	  return;
+	}
+      if (clk == 240 && vipermul == 0xc)
+	{
+	  fprintf (stderr, "board already clocked to 240mhz\n");
+	  fclose (in);
+	  return;
+	}
       fseek (in, 0xcb, SEEK_SET);
-      putc (0xb, in);
+      if (clk == 200)
+	putc (0xa, in);		//0x2c for 220 mhz 0x30 for 240 mhz
+      else if (clk == 220)
+	putc (0xb, in);		//0x2c for 220 mhz 0x30 for 240 mhz
+      else if (clk == 240)
+	putc (0xc, in);		//0x2c for 220 mhz 0x30 for 240 mhz
+      else
+	{
+	  nvram_set ("cpuclk", "220");
+	  nvram_commit ();
+	  putc (0xb, in);	//0x2c for 220 mhz 0x30 for 240 mhz
+	}
       fclose (in);
       eval ("mtd", "-f", "write", "/tmp/boot", "RedBoot");
       fprintf (stderr, "board is now clocked at 220 mhz, please reboot\n");
@@ -118,11 +182,46 @@ start_overclock
       fprintf (stderr, "board already clocked to 240mhz\n");
       fclose (in);
     }
-  else if (zmul == 0x9) // special handling for zLoader based boards
+  else if (zmul == 0x9 || zmul == 0xa || zmul == 0xb || zmul == 0xc)	// special handling for zLoader based boards
     {
       fprintf (stderr, "viper (ar2313) found (zLoader)\n");
-      fseek (in, 0xe64b, SEEK_SET);				
-      putc (0xb, in);
+      if (clk == 180 && zmul == 0x9)
+	{
+	  fprintf (stderr, "board already clocked to 180mhz\n");
+	  fclose (in);
+	  return;
+	}
+      if (clk == 200 && zmul == 0xa)
+	{
+	  fprintf (stderr, "board already clocked to 200mhz\n");
+	  fclose (in);
+	  return;
+	}
+      if (clk == 220 && zmul == 0xb)
+	{
+	  fprintf (stderr, "board already clocked to 220mhz\n");
+	  fclose (in);
+	  return;
+	}
+      if (clk == 240 && zmul == 0xc)
+	{
+	  fprintf (stderr, "board already clocked to 240mhz\n");
+	  fclose (in);
+	  return;
+	}
+      fseek (in, 0xe64b, SEEK_SET);
+      if (clk == 200)
+	putc (0xa, in);		//0x2c for 220 mhz 0x30 for 240 mhz
+      else if (clk == 220)
+	putc (0xb, in);		//0x2c for 220 mhz 0x30 for 240 mhz
+      else if (clk == 240)
+	putc (0xc, in);		//0x2c for 220 mhz 0x30 for 240 mhz
+      else
+	{
+	  nvram_set ("cpuclk", "220");
+	  nvram_commit ();
+	  putc (0xb, in);	//0x2c for 220 mhz 0x30 for 240 mhz
+	}
       fclose (in);
       eval ("mtd", "-f", "write", "/tmp/boot", "bdata");
       fprintf (stderr, "board is now clocked at 220 mhz, please reboot\n");
