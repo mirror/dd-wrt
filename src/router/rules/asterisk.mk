21f258cf22ebca25cba4020ebccc894353fd33d0
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-04-13 22:45:28 +0000

    asterisk checkin
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9422 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rules/asterisk.mk
@@ -0,0 +1,41 @@
+asterisk-configure:
+	cd asterisk && ./configure --host=$(ARCH)-linux-uclibc CFLAGS="$(COPTS) -DNEED_PRINTF" -without-curl \
+	--without-curses \
+	--without-gsm \
+	--without-imap \
+	--without-isdnnet \
+	--without-kde \
+	--without-misdn \
+	--without-nbs \
+	--with-ncurses="$(TOP)/ncurses" \
+	--without-netsnmp \
+	--without-newt \
+	--without-odbc \
+	--without-ogg \
+	--without-osptk \
+	--without-oss \
+	--without-pri \
+	--without-qt \
+	--without-radius \
+	--without-spandsp \
+	--without-suppserv \
+	--without-tds \
+	--without-termcap \
+	--without-tinfo \
+	--without-tonezone \
+	--without-vorbis \
+	--without-vpb \
+	--without-zaptel \
+
+#	--with-popt="$(STAGING_DIR)/usr" \
+
+asterisk:
+	make -C asterisk
+
+asterisk-install:
+	make -C asterisk install DESTDIR=$(INSTALLDIR)/asterisk
+	make -C asterisk adsi DESTDIR=$(INSTALLDIR)/asterisk
+#	make -C ncurses install.libs DESTDIR=$(INSTALLDIR)/ncurses
+#	rm -rf $(INSTALLDIR)/ncurses/usr/include
+#	rm -f $(INSTALLDIR)/ncurses/usr/lib/*.a
+
