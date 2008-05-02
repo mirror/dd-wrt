53f3c940d8068b14217ca40c639df557e3c3eace
BrainSlayer <s.gottschall@dd-wrt.com>
2008-05-02 15:11:46 +0000

    fix wifidog
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9477 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/wifidog.c
+++ src/router/services/services/wifidog.c
@@ -32,6 +32,10 @@ start_wifidog (void)
 {
   if (nvram_match ("wd_enable", "1"))
     {
+    eval ("insmod", "ipt_mark");
+    eval ("insmod", "ipt_mac");
+    eval ("insmod", "xt_mark");
+    eval ("insmod", "xt_mac");
       mkdir ("/tmp/etc/", 0744);
       FILE *fp = fopen ("/tmp/etc/wifidog.conf", "wb");
       if (!strlen (nvram_safe_get ("wd_gwid")))
