cc35d9c651b1d310e7a7e28232e4d9bd296d9f4a
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-06-04 11:50:04 +0000

    the final working WRT600n v1.1 fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9562 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/utils.c
+++ src/router/shared/utils.c
@@ -997,6 +997,13 @@ internal_getRouterBrand ()
     }
 
   if (nvram_match("boardnum","20070615") &&
+      nvram_match ("boardtype", "0x478") && nvram_match ("cardbus", "0") && nvram_match("switch_type","5395"))
+    {
+      cprintf ("router is Linksys WRT600N v1.1\n");
+      setRouter ("Linksys WRT600N v1.1");
+      return ROUTER_WRT600N;
+    }
+  if (nvram_match("boardnum","20070615") &&
       nvram_match ("boardtype", "0x478") && nvram_match ("cardbus", "0"))
     {
       cprintf ("router is Linksys WRT600N\n");
