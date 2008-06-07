0450790dbb0cda147b456aa386f824e3aa6e148c
BrainSlayer <s.gottschall@dd-wrt.com>
2008-06-07 12:05:41 +0000

    gpio lib
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9594 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/shared/gpio.c
@@ -0,0 +1,193 @@
+/*
+ */
+#include <string.h>
+#include <unistd.h>
+
+#include <typedefs.h>
+#include <bcmutils.h>
+#include <wlutils.h>
+#include <shutils.h>
+#include <utils.h>
+#include <bcmnvram.h>
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+
+#ifdef HAVE_AR531X
+
+void
+set_gpio (int gpio, int value)
+{
+  FILE *in;
+  char buf[64];
+  sprintf (buf, "/proc/gpio/%d_dir", gpio);
+  in = fopen (buf, "wb");
+  fprintf (in, "1");
+  fclose (in);
+  sprintf (buf, "/proc/gpio/%d_out", gpio);
+  in = fopen (buf, "wb");
+  fprintf (in, "%d", value);
+  fclose (in);
+}
+
+int
+get_gpio (int gpio)
+{
+  FILE *in;
+  int ret;
+  char buf[64];
+  sprintf (buf, "/proc/gpio/%d_dir", gpio);
+  in = fopen (buf, "wb");
+  fprintf (in, "0");
+  fclose (in);
+  sprintf (buf, "/proc/gpio/%d_in", gpio);
+  in = fopen (buf, "rb");
+  fscanf (in, "%d", &ret);
+  fclose (in);
+  return ret;
+}
+
+#elif HAVE_XSCALE
+#define u8 unsigned char
+#define u32 unsigned long
+
+//#include <linux/ixp425-gpio.h>
+
+#include <asm/hardware.h>
+#include <asm-arm/arch-ixp4xx/ixp4xx-regs.h>
+
+#define IXP4XX_GPIO_OUT 		0x1
+#define IXP4XX_GPIO_IN  		0x2
+
+struct gpio_bit
+{
+  unsigned char bit;
+  unsigned char state;
+};
+
+
+
+char *filename = "/dev/gpio";
+
+
+
+
+
+void
+set_gpio (int gpio, int value)
+{
+  int file;
+  struct gpio_bit _bit;
+
+  /* open device */
+  if ((file = open (filename, O_RDWR)) == -1)
+    {
+      /* ERROR HANDLING; you can check errno to see what went wrong */
+//      fprintf (stderr, "Error: could not open %s (%d)\n", filename, errno);
+      return;
+    }
+
+  /* Config bit as output */
+  _bit.bit = gpio;
+  _bit.state = IXP4XX_GPIO_OUT;
+  if (ioctl (file, GPIO_SET_CONFIG, (long) &_bit) < 0)
+    {
+      /* ERROR HANDLING; you can check errno to see what went wrong */
+//      fprintf (stderr, "Error: ioctl failed: %s (%d)\n", strerror (errno),
+//             errno);
+      return;
+    }
+
+  /* Write data */
+  _bit.bit = gpio;
+  _bit.state = value;
+  if (ioctl (file, GPIO_SET_BIT, (unsigned long) &_bit) < 0)
+    {
+      /* ERROR HANDLING; you can check errno to see what went wrong */
+//      fprintf (stderr, "Error: ioctl failed: %s (%d)\n", strerror (errno),
+//             errno);
+      return;
+    }
+
+  close (file);
+
+}
+
+int
+get_gpio (int gpio)
+{
+  int file;
+  struct gpio_bit _bit;
+
+  /* open device */
+  if ((file = open (filename, O_RDONLY)) == -1)
+    {
+      /* ERROR HANDLING; you can check errno to see what went wrong */
+      return 1;
+    }
+
+  /* Config pin as input */
+  _bit.bit = gpio;
+  _bit.state = IXP4XX_GPIO_IN;
+  if (ioctl (file, GPIO_SET_CONFIG, (long) &_bit) < 0)
+    {
+      /* ERROR HANDLING; you can check errno to see what went wrong */
+      return 1;
+    }
+
+  /* Read data */
+  _bit.bit = gpio;
+  if (ioctl (file, GPIO_GET_BIT, (long) &_bit) < 0)
+    {
+      /* ERROR HANDLING; you can check errno to see what went wrong */
+      return 1;
+    }
+
+  close (file);
+  return _bit.state;
+}
+
+
+#else
+
+void
+set_gpio (int gpio, int value)
+{
+  int gpioout = open ("/dev/gpio/out", O_RDWR);
+
+  read (gpioouten, &gpio, sizeof (gpio));
+  gpio |= pin;
+  write (gpioouten, &gpio, sizeof (gpio));
+
+  read (gpioout, &gpio, sizeof (gpio));
+  if (value)
+    {
+      gpio |= gpio;
+    }
+  else
+    {
+      gpio &= ~gpio;
+    }
+  write (gpioout, &gpio, sizeof (gpio));
+  close (gpioout);
+}
+
+int
+get_gpio (int gpio)
+{
+  int gpioin = open ("/dev/gpio/in", O_RDWR);
+  read (gpioouten, &gpio, sizeof (gpio));
+  gpio &= ~pin;
+  write (gpioouten, &gpio, sizeof (gpio));
+  read (gpioin, &gpio, sizeof (gpio));
+  gpio = (gpio & gpio) ? 1 : 0;
+  return gpio;
+  close (gpioin);
+}
+
+#endif
