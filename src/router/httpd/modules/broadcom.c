385b5e9116db752c73a133ffd3a8d5d2290d7f22
BrainSlayer <s.gottschall@dd-wrt.com>
2008-06-07 15:15:03 +0000

    crash fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9601 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/broadcom.c
+++ src/router/httpd/modules/broadcom.c
@@ -5501,7 +5501,7 @@ ej_dumparptable (webs_t wp, int argc, char_t ** argv)
 	  if ((strlen (mac) != 17)
 	      || (strcmp (mac, "00:00:00:00:00:00") == 0))
 	    continue;
-	  if (strcmp (landev, nvram_get ("wan_iface")) == 0)
+	  if (strcmp (landev, nvram_safe_get ("wan_iface")) == 0)
 	    continue;		//skip all but LAN arp entries
 	  strcpy (hostname, "*");	//set name to *
 
