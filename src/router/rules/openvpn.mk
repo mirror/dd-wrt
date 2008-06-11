f0aae9be7004287a2a5ea99365986f60c76153f6
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-06-11 19:41:58 +0000

    include server config
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9650 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/openvpn.mk
+++ src/router/rules/openvpn.mk
@@ -1,16 +1,18 @@
 openvpn-configure: openssl
 	cd lzo && ./configure --host=$(ARCH)-linux CFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections"
 	make -C lzo
-ifeq ($(CONFIG_NEWMEDIA),y)
-	cd openvpn && ./configure --host=$(ARCH)-linux CPPFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections -I../lzo/include -I../openssl/include -L../lzo -L../openssl -L../lzo/src/.libs" --enable-static --disable-shared --disable-pthread --disable-plugins --disable-debug --enable-password-save --enable-management --enable-lzo --enable-small --enable-server CFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="-L../openssl -L../lzo -L../lzo/src/.libs  -ffunction-sections -fdata-sections -Wl,--gc-sections"
-else
-	cd openvpn && ./configure --host=$(ARCH)-linux CPPFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections -I../lzo/include -I../openssl/include -L../lzo -L../openssl -L../lzo/src/.libs" --enable-static --disable-shared --disable-pthread --disable-plugins --disable-debug --disable-management --disable-socks --enable-lzo --enable-small --enable-server --enable-http --enable-password-save CFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="-L../openssl -L../lzo -L../lzo/src/.libs  -ffunction-sections -fdata-sections -Wl,--gc-sections"
-endif
 
 openvpn:
+#ifeq ($(CONFIG_NEWMEDIA),y)
+	cd openvpn && ./configure --host=$(ARCH)-linux CPPFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections -I../lzo/include -I../openssl/include -L../lzo -L../openssl -L../lzo/src/.libs" --enable-static --disable-shared --disable-pthread --disable-plugins --disable-debug --enable-password-save --enable-management --enable-lzo --enable-small --enable-server CFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="-L../openssl -L../lzo -L../lzo/src/.libs  -ffunction-sections -fdata-sections -Wl,--gc-sections"
+#else
+#	cd openvpn && ./configure --host=$(ARCH)-linux CPPFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections -I../lzo/include -I../openssl/include -L../lzo -L../openssl -L../lzo/src/.libs" --enable-static --disable-shared --disable-pthread --disable-plugins --disable-debug --disable-management --disable-socks --enable-lzo --enable-small --enable-server --enable-http --enable-password-save CFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="-L../openssl -L../lzo -L../lzo/src/.libs  -ffunction-sections -fdata-sections -Wl,--gc-sections"
+#endif
 	make -C lzo clean
 	make -C lzo
-#	rm -f openssl/*.so*
+ifneq ($(CONFIG_MADWIFI),y)
+	rm -f openssl/*.so*
+endif
 ifeq ($(CONFIG_NEWMEDIA),y)
 	make -C openvpn clean
 else
@@ -20,13 +22,13 @@ endif
 
 openvpn-install:
 	install -D openvpn/openvpn $(INSTALLDIR)/openvpn/usr/sbin/openvpn
-ifneq ($(CONFIG_NEWMEDIA),y)	
-	install -D openvpn/config/openvpn.nvramconfig $(INSTALLDIR)/openvpn/etc/config/openvpn.nvramconfig
-	install -D openvpn/config/openvpn.webservices $(INSTALLDIR)/openvpn/etc/config/openvpn.webservices
-else
+#ifneq ($(CONFIG_NEWMEDIA),y)	
+	install -D openvpn/config/openvpncl.nvramconfig $(INSTALLDIR)/openvpn/etc/config/openvpncl.nvramconfig
+	install -D openvpn/config/openvpncl.webservices $(INSTALLDIR)/openvpn/etc/config/openvpncl.webservices
+#else
 	install -D openvpn/config2/openvpn.nvramconfig $(INSTALLDIR)/openvpn/etc/config/openvpn.nvramconfig
 	install -D openvpn/config2/openvpn.webservices $(INSTALLDIR)/openvpn/etc/config/openvpn.webservices
-endif
+#endif
 
 openvpn-clean:
 	if test -e "openvpn/Makefile"; then make -C openvpn clean; fi
