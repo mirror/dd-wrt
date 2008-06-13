5e136d2527dc2ec1dfadb7f72c4dac00b6cfba48
BrainSlayer <s.gottschall@dd-wrt.com>
2008-06-13 14:35:28 +0000

    fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9689 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/openssl.mk
+++ src/router/rules/openssl.mk
@@ -10,8 +10,10 @@ openssl:
 #endif
 
 openssl-install:
+ifeq ($(CONFIG_MADWIFI),y)
 	install -D openssl/libcrypto.so.0.9.8 $(INSTALLDIR)/openssl/usr/lib/libcrypto.so.0.9.8
 	install -D openssl/libssl.so.0.9.8 $(INSTALLDIR)/openssl/usr/lib/libssl.so.0.9.8
+endif
 ifneq ($(ARCH),mipsel)
 	install -D openssl/apps/openssl $(INSTALLDIR)/openssl/usr/sbin/openssl
 endif
