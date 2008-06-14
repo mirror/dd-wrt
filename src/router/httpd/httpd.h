06d7867128d0f08f775a03c659b739403e9cc078
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-06-14 20:36:38 +0000

    formating
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9710 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/httpd.h
+++ src/router/httpd/httpd.h
@@ -32,16 +32,16 @@ extern BIO *bio_err;
 #endif
 
 
-typedef FILE * webs_t;
-extern char * wfgets(char *buf, int len, FILE *fp);
-extern int wfputc(char c, FILE *fp);
-extern int wfputs(char *buf, FILE *fp);
-extern int wfprintf(FILE *fp, char *fmt,...);
-extern size_t wfwrite(char *buf, int size, int n, FILE *fp);
-extern size_t wfread(char *buf, int size, int n, FILE *fp);
-extern int wfflush(FILE *fp);
-extern int wfclose(FILE *fp);
-#ifdef HAVE_HTTPS  
+typedef FILE *webs_t;
+extern char *wfgets (char *buf, int len, FILE * fp);
+extern int wfputc (char c, FILE * fp);
+extern int wfputs (char *buf, FILE * fp);
+extern int wfprintf (FILE * fp, char *fmt, ...);
+extern size_t wfwrite (char *buf, int size, int n, FILE * fp);
+extern size_t wfread (char *buf, int size, int n, FILE * fp);
+extern int wfflush (FILE * fp);
+extern int wfclose (FILE * fp);
+#ifdef HAVE_HTTPS
 extern int do_ssl;
 #endif
 /* Basic authorization userid and passwd limit */
@@ -51,33 +51,34 @@ extern char auth_realm[AUTH_MAX];
 extern void send_authenticate (char *realm);
 
 /* Generic MIME type handler */
-struct mime_handler {
-	char *pattern;
-	char *mime_type;
-	char *extra_header;
-	void (*input)(char *path, FILE *stream, int len, char *boundary);
-	void (*output)(char *path, FILE *stream,char *query);
-	int (*auth)(char *userid, char *passwd, char *realm);
+struct mime_handler
+{
+  char *pattern;
+  char *mime_type;
+  char *extra_header;
+  void (*input) (char *path, FILE * stream, int len, char *boundary);
+  void (*output) (char *path, FILE * stream, char *query);
+  int (*auth) (char *userid, char *passwd, char *realm);
 };
 typedef struct
 {
   char *path;			/* Web page URL path */
   unsigned long offset;		/* Web page data */
   int size;			/* Size of web page in bytes */
-//  int csize;			/* Size of web page in bytes */
+//  int csize;                  /* Size of web page in bytes */
 } websRomPageIndexType;
 
 
 extern struct mime_handler mime_handlers[];
 
 /* CGI helper functions */
-extern void init_cgi(char *query);
-extern char * get_cgi(char *name);
-extern void set_cgi(char *name, char *value);
-extern int count_cgi();
+extern void init_cgi (char *query);
+extern char *get_cgi (char *name);
+extern void set_cgi (char *name, char *value);
+extern int count_cgi ();
 
 /* Regular file handler */
-extern void do_file(char *path, webs_t stream,char *query);
+extern void do_file (char *path, webs_t stream, char *query);
 
 /* GoAhead 2.1 compatibility */
 //typedef FILE * webs_t;
@@ -88,7 +89,7 @@ typedef char char_t;
 #define TMPVAR _TMPVAR(__LINE__)
 
 //#define websWrite(wp, fmt, args...) ({ int TMPVAR = wfprintf(wp, fmt, ## args); wfflush(wp); TMPVAR; })
-int websWrite(webs_t wp, char *fmt, ...);
+int websWrite (webs_t wp, char *fmt, ...);
 #define websDebugWrite(wp, fmt, args...)
 //#define websDebugWrite(wp, fmt, args...) ({ error_value = 1; wfputs("<!--", wp); int TMPVAR = wfprintf(wp, fmt, ## args); wfputs("-->", wp); wfflush(wp); TMPVAR; })
 #define websError(wp, code, msg, args...)
@@ -99,7 +100,7 @@ int websWrite(webs_t wp, char *fmt, ...);
 #define websDone(wp, code) wfflush(wp)
 
 
-extern char *websGetVar(webs_t wp, char *var, char *d);
+extern char *websGetVar (webs_t wp, char *var, char *d);
 
 #define websSetVar(wp, var, value) set_cgi(var, value)
 #define websDefaultHandler(wp, urlPrefix, webDir, arg, url, path, query) ({ do_ej(path, wp,""); fflush(wp); 1; })
@@ -107,26 +108,27 @@ extern char *websGetVar(webs_t wp, char *var, char *d);
 #define websWriteDataNonBlock websWriteData
 #define a_assert(a)
 
-extern int ejArgs(int argc, char_t **argv, char_t *fmt, ...);
+extern int ejArgs (int argc, char_t ** argv, char_t * fmt, ...);
 
 /* GoAhead 2.1 Embedded JavaScript compatibility */
-extern FILE *getWebsFile(char *path);
-extern int getWebsFileLen(char *path);
+extern FILE *getWebsFile (char *path);
+extern int getWebsFileLen (char *path);
 
-extern void do_ej(char *path, webs_t stream,char *query);
+extern void do_ej (char *path, webs_t stream, char *query);
 extern char *zencrypt (char *passwd);
 
-extern void do_filtertable(char *path, webs_t stream,char *query);
-extern void do_wds(char *path, webs_t stream,char *query);
-extern void do_ej_buffer(char *buffer, webs_t stream);
+extern void do_filtertable (char *path, webs_t stream, char *query);
+extern void do_wds (char *path, webs_t stream, char *query);
+extern void do_ej_buffer (char *buffer, webs_t stream);
 int do_auth (char *userid, char *passwd, char *realm);
 void Initnvramtab (void);
 
 
 
-struct ej_handler {
-	char *pattern;
-	void (*output)(webs_t wp, int argc, char_t **argv);
+struct ej_handler
+{
+  char *pattern;
+  void (*output) (webs_t wp, int argc, char_t ** argv);
 };
 extern struct ej_handler ej_handlers[];
 
