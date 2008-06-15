a87826c5f16ae4bca955e574a0a079187cda9958
BrainSlayer <s.gottschall@dd-wrt.com>
2008-06-15 20:01:56 +0000

    fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9715 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/openssl.mk
+++ src/router/rules/openssl.mk
@@ -17,12 +17,7 @@ endif
 ifneq ($(ARCH),mipsel)
 	install -D openssl/apps/openssl $(INSTALLDIR)/openssl/usr/sbin/openssl
 endif
-#ifeq ($(ARCHITECTURE),rb532)
-#	install -D openssl/libcrypto.so.0.9.8 $(INSTALLDIR)/openssl/usr/lib/libcrypto.so.0.9.8
-#	install -D openssl/libssl.so.0.9.8 $(INSTALLDIR)/openssl/usr/lib/libssl.so.0.9.8
-#	install -D openssl/apps/openssl $(INSTALLDIR)/openssl/usr/sbin/openssl
-#endif
-#	@true
+	@true
 
 openssl-clean:
 	$(MAKE) -C openssl clean
