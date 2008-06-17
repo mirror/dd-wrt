f3bb94cc3358eb7e090f4083f1fb6ca1deee801f
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-06-17 17:52:44 +0000

    bridged = 0
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9753 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/utils.c
+++ src/router/shared/utils.c
@@ -4328,7 +4328,7 @@ doMultiCast (void)
     }
   foreach (name, nvram_safe_get ("lan_ifnames"), next)
   {
-    if (nvram_nmatch ("1", "%s_multicast", name))
+    if (nvram_nmatch ("1", "%s_multicast", name) && nvram_nmatch ("0", "%s_bridged", name))
       {
 	ifcount++;
       }
