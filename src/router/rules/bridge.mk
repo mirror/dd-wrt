9732c77249d0a85c8cad154cebf8b8f1030f6b95
BrainSlayer <s.gottschall@dd-wrt.com>
2008-06-17 01:58:31 +0000

    not required anymore
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9729 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/bridge.mk
+++ src/router/rules/bridge.mk
@@ -1,16 +1,17 @@
 bridge:
-	$(MAKE) -C bridge brctl/brctl
-bridge:
-	$(MAKE) -C bridge brctl/brctl
+	@true
+#	$(MAKE) -C bridge brctl/brctl
 
 bridge-clean:
-	@echo "Cleaning bridge"
-	@if [ -e bridge/libbridge/libbridge.a ]; then\
-		rm bridge/brctl/*.o bridge/libbridge/*.a bridge/libbridge/*.o;\
-	else echo "Nothing to clean";\
-	fi;
+	@true
+#	@echo "Cleaning bridge"
+##	@if [ -e bridge/libbridge/libbridge.a ]; then\
+#		rm bridge/brctl/*.o bridge/libbridge/*.a bridge/libbridge/*.o;\
+#	else echo "Nothing to clean";\
+#	fi;
 
 bridge-install:
-	install -D bridge/brctl/brctl $(INSTALLDIR)/bridge/usr/sbin/brctl
-	$(STRIP) $(INSTALLDIR)/bridge/usr/sbin/brctl
+	@true
+#	install -D bridge/brctl/brctl $(INSTALLDIR)/bridge/usr/sbin/brctl
+#	$(STRIP) $(INSTALLDIR)/bridge/usr/sbin/brctl
 
