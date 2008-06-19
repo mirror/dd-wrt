713954bd79f64d33b4a53d9ada7021038c13279f
BrainSlayer <s.gottschall@dd-wrt.com>
2008-06-19 04:10:13 +0000

    WRT300N v1.1 problem finally solved
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9762 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/gpio.c
+++ src/router/shared/gpio.c
@@ -166,7 +166,6 @@ get_gpio (int gpio)
 void
 set_gpio (int pin, int value)
 {
-  cprintf("set GPIO %d = %d\n",pin,value);
   int gpioouten = open ("/dev/gpio/outen", O_RDWR);
   int gpioout = open ("/dev/gpio/out", O_RDWR);
   unsigned int gpio;
@@ -192,7 +191,6 @@ set_gpio (int pin, int value)
 int
 get_gpio (int pin)
 {
-  cprintf("get GPIO %d\n",pin);
   unsigned int gpio;
   int gpioouten = open ("/dev/gpio/outen", O_RDWR);
   int gpioin = open ("/dev/gpio/in", O_RDWR);
