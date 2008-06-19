8215c51ec4870960f52c61a5097f3d7b0c03b351
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-06-19 12:15:59 +0000

    revert this for now until the busybox brctl is ready to use
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9766 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/bridge.mk
+++ src/router/rules/bridge.mk
@@ -1,17 +1,14 @@
 bridge:
-	@true
-#	$(MAKE) -C bridge brctl/brctl
+	$(MAKE) -C bridge brctl/brctl
 
 bridge-clean:
-	@true
-#	@echo "Cleaning bridge"
-##	@if [ -e bridge/libbridge/libbridge.a ]; then\
-#		rm bridge/brctl/*.o bridge/libbridge/*.a bridge/libbridge/*.o;\
-#	else echo "Nothing to clean";\
-#	fi;
+	@echo "Cleaning bridge"
+	@if [ -e bridge/libbridge/libbridge.a ]; then\
+		rm bridge/brctl/*.o bridge/libbridge/*.a bridge/libbridge/*.o;\
+	else echo "Nothing to clean";\
+	fi;
 
 bridge-install:
-	@true
-#	install -D bridge/brctl/brctl $(INSTALLDIR)/bridge/usr/sbin/brctl
-#	$(STRIP) $(INSTALLDIR)/bridge/usr/sbin/brctl
+	install -D bridge/brctl/brctl $(INSTALLDIR)/bridge/usr/sbin/brctl
+	$(STRIP) $(INSTALLDIR)/bridge/usr/sbin/brctl
 
