3e37463fcda95a90a42488f49b99420d6a77cc52
BrainSlayer <s.gottschall@dd-wrt.com>
2008-06-23 12:43:12 +0000

    correct restart procedure
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9787 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit.c
+++ src/router/services/sysinit/sysinit.c
@@ -1134,7 +1134,6 @@ start_restore_defaults (void)
       nvram_set ("block_multicast", "1");
     }
   nvram_set("generate_key","0");
-  nvram_set("clone_wan_mac","0");
 
   cprintf ("check CFE nv\n");
   if (check_now_boot () == PMON_BOOT)
