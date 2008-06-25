b38835ad4481a6ecb8652f72ac2d34af5a6497ee
tornado <tornado@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-06-25 04:33:30 +0000

    ProftpD setup
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9799 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/proftpd.mk
+++ src/router/rules/proftpd.mk
@@ -1,5 +1,15 @@
+proftpd-configure:
+	cd proftpd && ./configure --host=mipsel-linux --prefix=/tmp/proftpd 
+	
 proftpd:
 	$(MAKE) -C proftpd
+
+proftpd-clean:
+	$(MAKE) -C proftpd clean
+
 proftpd-install:
 	install -D proftpd/proftpd $(INSTALLDIR)/proftpd/usr/sbin/proftpd
+	install -D proftpd/proftpd.conf $(INSTALLDIR)/proftpd/etc/config/proftpd.conf
 	$(STRIP) $(INSTALLDIR)/proftpd/usr/sbin/proftpd
+
+
