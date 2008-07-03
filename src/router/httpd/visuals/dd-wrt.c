9083f99e8d445050903dbfa2e5f780c372f6cd57
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-07-03 21:28:02 +0000

    boundary is 2345 (2346 means off)
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@9871 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/dd-wrt.c
+++ src/router/httpd/visuals/dd-wrt.c
@@ -2931,8 +2931,8 @@ showrtssettings (webs_t wp, char *var)
   char ip[32];
   sprintf (ip, "%s_rtsvalue", var);
   websWrite (wp,
-	     "<input class=\"num\" maxlength=\"4\" size=\"4\" onblur=\"valid_range(this,1,2346,share.ip)\" name=\"%s_rtsvalue\" value=\"%s\" />",
-	     var, nvram_default_get (ip, "2346"));
+	     "<input class=\"num\" maxlength=\"4\" size=\"4\" onblur=\"valid_range(this,1,2345,share.ip)\" name=\"%s_rtsvalue\" value=\"%s\" />",
+	     var, nvram_default_get (ip, "2345"));
   websWrite (wp, "</div>\n");
   websWrite (wp, "</div>\n");
 
