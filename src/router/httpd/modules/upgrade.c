c37ff6d4810f7fb2c3b69b64a0ef54eaa00e147d
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-07-26 17:20:42 +0000

    code pattern update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@10006 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/upgrade.c
+++ src/router/httpd/modules/upgrade.c
@@ -277,6 +277,7 @@ sys_upgrade (char *url, webs_t stream, int *total, int type)	//jimmy, https, 8/6
 	      && memcmp (&buf[0], &CODE_PATTERN_WRTSL54GS, 4)
 	      && memcmp (&buf[0], &CODE_PATTERN_WRT54G3G, 4)
 	      && memcmp (&buf[0], &CODE_PATTERN_WRT54G3GV, 4)
+	      && memcmp (&buf[0], &CODE_PATTERN_WRT610N, 4)
 	      && memcmp (&buf[0], &CODE_PATTERN_WRT54GSV4, 4))
 	    {
 	      cprintf ("code pattern error!\n");
