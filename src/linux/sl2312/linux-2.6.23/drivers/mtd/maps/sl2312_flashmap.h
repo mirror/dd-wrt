b3294318ec11b6d8a63ad82c516ad8ae98503ae4
BrainSlayer <s.gottschall@dd-wrt.com>
2008-07-29 16:01:09 +0000

    correct flashmap and kernel config
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@10035 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/sl2312/linux-2.6.23/drivers/mtd/maps/sl2312_flashmap.h
+++ src/linux/sl2312/linux-2.6.23/drivers/mtd/maps/sl2312_flashmap.h
@@ -12,10 +12,9 @@
 
 static struct mtd_partition sl2312_partitions[] = {
 	{ name: "RedBoot", 	 offset: 0x00000000, size: 0x00020000, },
-	{ name: "kernel", 	 offset: 0x00020000, size: 0x00100000, },
-	{ name: "rootfs", 	 offset: 0x00120000, size: 0x00500000, },
-	{ name: "rootfs_data", 	 offset: 0x00620000, size: 0x001A0000, },
-	{ name: "VCTL", 	 offset: 0x007C0000, size: 0x00010000, },
-	{ name: "cfg", 	 	 offset: 0x007D0000, size: 0x00020000, },
+	{ name: "linux", 	 offset: 0x00020000, size: 0x00760000, },
+	{ name: "rootfs", 	 offset: 0x00000000, size: 0x00000000, }, // variable size
+	{ name: "ddwrt", 	 offset: 0x00000000, size: 0x00000000, }, // variable size
+	{ name: "nvram", 	 offset: 0x007E0000, size: 0x00010000, },
 	{ name: "FIS directory", offset: 0x007F0000, size: 0x00010000, }
 };
