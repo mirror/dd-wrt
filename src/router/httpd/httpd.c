cb59b2c8b08a2f1a156fd2d8ea72a364bf372951
BrainSlayer <s.gottschall@dd-wrt.com>
2008-08-27 14:37:38 +0000

    another try to get around the crash problem (could be stack related)
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@10226 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/httpd.c
+++ src/router/httpd/httpd.c
@@ -1038,17 +1038,11 @@ get_client_ip_mac (int conn_fp)
   getpeername (conn_fp, (struct sockaddr *) &sa, &len);
   char *peer = inet_ntoa (sa.sin_addr);
   char *client = malloc(strlen(peer)+1);
-  
+  strcpy(client,peer);
   nvram_set ("http_client_ip", client);
-  free(client);
-  //if(strcmp(nvram_safe_get("http_client_ip"), inet_ntoa(sa.sin_addr)))
-  //      nvram_set("http_client_ip", inet_ntoa(sa.sin_addr));
-  m = get_mac_from_ip (inet_ntoa (sa.sin_addr));
+  m = get_mac_from_ip (client);
   nvram_set ("http_client_mac", m);
-  //if(m){
-  //      if(strcmp(nvram_safe_get("http_client_mac"), m))
-  //              nvram_set("http_client_mac", m);
-  //}
+  free(client);
 }
 
 static void
