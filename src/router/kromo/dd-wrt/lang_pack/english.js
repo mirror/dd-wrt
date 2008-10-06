278d39588339fbe2012b828cfbecd05ba29e3cc4
eko <USER@DOMAIN>
2008-10-06 12:58:02 +0000

    DST for Argentina
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@10455 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/dd-wrt/lang_pack/english.js
+++ src/router/kromo/dd-wrt/lang_pack/english.js
@@ -833,6 +833,7 @@ idx.summt_opt4="last Sun Oct - last Sun Mar";
 idx.summt_opt5="2nd Sun Mar - first Sun Nov";
 idx.summt_opt6="first Sun Oct - 3rd Sun Mar";
 idx.summt_opt7="last Sun Sep - first Sun Apr";
+idx.summt_opt8="3rd Sun Oct - 3rd Sun Mar";
 idx.portsetup="Port Setup";
 idx.wanport="WAN Port Assignment";
 idx.ntp_client="NTP Client";
