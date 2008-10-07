237b756abd55d3f8da33e1def8cd015d78523ba2
BrainSlayer <s.gottschall@dd-wrt.com>
2008-10-07 23:10:52 +0000

    fix wrong path
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@10477 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/olsrd/src/defs.h
+++ src/router/olsrd/src/defs.h
@@ -63,7 +63,7 @@ extern const char build_host[];
 
 #ifndef OLSRD_GLOBAL_CONF_FILE
 #define OLSRD_CONF_FILE_NAME	"olsrd.conf"
-#define OLSRD_GLOBAL_CONF_FILE	"/etc/" OLSRD_CONF_FILE_NAME
+#define OLSRD_GLOBAL_CONF_FILE	"/tmp/" OLSRD_CONF_FILE_NAME
 #endif
 
 #define	MAXMESSAGESIZE		1500	/* max broadcast size */
