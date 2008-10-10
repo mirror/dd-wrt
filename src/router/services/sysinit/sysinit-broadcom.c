7b5cfda4346199685645448df44d6d4d24ffe94d
eko <USER@DOMAIN>
2008-10-10 09:48:13 +0000

    Netgear WGR614x gpio
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@10487 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-broadcom.c
+++ src/router/services/sysinit/sysinit-broadcom.c
@@ -1027,8 +1027,6 @@ int start_sysinit( void )
 	    nvram_set ("sromrev", "3");    // This is a fix for WGR614L NA - which has a wrong sromrev
 	    need_reboot = 1;
         }
-        eval( "gpio", "enable", "7" );	// turn off power led /fix gpio 7 for reset
-        eval( "gpio", "init", "7" );	// enable gpio 7 for reset
 	    break;	    
 	 
 	case ROUTER_ALLNET01:
