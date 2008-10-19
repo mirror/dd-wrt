373b91409bf56ad5f365b122d71f6daf677fb105
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-10-19 20:30:36 +0000

    updated rules for eoc2610 special hal
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@10542 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/busybox.mk
+++ src/router/rules/busybox.mk
@@ -81,6 +81,10 @@ ifeq ($(ARCHITECTURE),ls2)
 	cp busybox/.config_fonera busybox/.config
 	echo "# CONFIG_MKE2FS is not set" >> busybox/.config
 else
+ifeq ($(ARCHITECTURE),eoc2610)
+	cp busybox/.config_fonera busybox/.config
+	echo "# CONFIG_MKE2FS is not set" >> busybox/.config
+else
 ifeq ($(ARCHITECTURE),lsx)
 	cp busybox/.config_fonera busybox/.config
 	echo "# CONFIG_MKE2FS is not set" >> busybox/.config
@@ -90,6 +94,7 @@ ifeq ($(ARCHITECTURE),danube)
 	echo "# CONFIG_MKE2FS is not set" >> busybox/.config
 else
 	cp busybox/.config_3com busybox/.config
+endif
 endif
 endif
 endif
