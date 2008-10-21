980c6b9241d172c5f84add429cf0b03a9a05e642
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-10-21 23:08:51 +0000

    fixes alot of issues with braindead isp's
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@10556 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/pppd.new/pppd/ccp.c
+++ src/router/pppd.new/pppd/ccp.c
@@ -1364,6 +1364,7 @@ ccp_reqci(f, p, lenp, dont_nak)
     int *lenp;
     int dont_nak;
 {
+    int mtu;
     int ret, newret, res;
     u_char *p0, *retp, p2, p5;
     int len, clen, type, nb;
@@ -1655,7 +1656,7 @@ ccp_reqci(f, p, lenp, dont_nak)
 		 * because MPPE frames **grow**.  The kernel [must]
 		 * allocate MPPE_PAD extra bytes in xmit buffers.
 		 */
-/*
+
 		mtu = netif_get_mtu(f->unit);
 		if (mtu) {
 		    netif_set_mtu(f->unit, mtu - MPPE_PAD);
@@ -1666,7 +1667,7 @@ ccp_reqci(f, p, lenp, dont_nak)
 			lcp_close(f->unit, "Cannot adjust MTU needed by MPPE.");
 		    }
 		}
-*/
+
 		break;
 #endif /* MPPE */
 
