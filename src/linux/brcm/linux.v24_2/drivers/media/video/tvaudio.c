87034bd3197590223a878c916373136ba9249d54
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-10-22 20:01:50 +0000

    update to 2.4.36.8
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@10569 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/brcm/linux.v24_2/drivers/media/video/tvaudio.c
+++ src/linux/brcm/linux.v24_2/drivers/media/video/tvaudio.c
@@ -1635,7 +1635,7 @@ static int chip_command(struct i2c_client *client,
 	case VIDIOCSFREQ:
 	{
 	    	chip->mode = 0; /* automatic */
-		if (desc->checkmode) {
+		if (desc->checkmode && desc->setmode) {
 			desc->setmode(chip,VIDEO_SOUND_MONO);
 		    	if (chip->prevmode != VIDEO_SOUND_MONO)
 		    		chip->prevmode = -1; /* reset previous mode */
