b849e722f276d838c3a0bbfc1111f4e59871f77b
BrainSlayer <s.gottschall@dd-wrt.com>
2008-10-23 20:55:18 +0000

    make broadcast relay optional
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@10583 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/defaults.c
+++ src/router/services/sysinit/defaults.c
@@ -1423,6 +1423,7 @@ struct nvram_tuple srouter_defaults[] = {
     {"ntp_enable", "1", 0},
     // #endif
     {"pptpd_enable", "0", 0},
+    {"pptpd_bcrelay", "1", 0},
     {"pptpd_lip", "", 0},
     {"pptpd_rip", "", 0},
     {"pptpd_auth", "", 0},
