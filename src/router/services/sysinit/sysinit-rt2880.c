a91232c3fc1182a13540ce9dc78e23fbc9acc9c7
BrainSlayer <s.gottschall@dd-wrt.com>
2008-11-05 01:55:27 +0000

    radauth support for rt2880
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@10757 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/services/sysinit/sysinit-rt2880.c
@@ -0,0 +1,165 @@
+/*
+ * sysinit-adm5120.c
+ *
+ * Copyright (C) 2008 Sebastian Gottschall <gottschall@dd-wrt.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id:
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <time.h>
+#include <unistd.h>
+#include <errno.h>
+#include <syslog.h>
+#include <signal.h>
+#include <string.h>
+#include <termios.h>
+#include <sys/klog.h>
+#include <sys/types.h>
+#include <sys/mount.h>
+#include <sys/reboot.h>
+#include <sys/stat.h>
+#include <sys/sysmacros.h>
+#include <sys/time.h>
+#include <sys/utsname.h>
+#include <sys/wait.h>
+#include <linux/if_ether.h>
+#include <linux/mii.h>
+#include <linux/sockios.h>
+#include <net/if.h>
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <linux/sockios.h>
+#include <linux/mii.h>
+
+#include <bcmnvram.h>
+#include <shutils.h>
+#include <utils.h>
+#include <cymac.h>
+
+
+#define sys_reboot() eval("sync"); eval("event","3","1","15")
+
+void start_sysinit( void )
+{
+    char buf[PATH_MAX];
+    struct utsname name;
+    struct stat tmp_stat;
+    time_t tm = 0;
+
+    cprintf( "sysinit() proc\n" );
+    /*
+     * /proc 
+     */
+    mount( "proc", "/proc", "proc", MS_MGC_VAL, NULL );
+    mount( "sysfs", "/sys", "sysfs", MS_MGC_VAL, NULL );
+    cprintf( "sysinit() tmp\n" );
+
+    /*
+     * /tmp 
+     */
+    mount( "ramfs", "/tmp", "ramfs", MS_MGC_VAL, NULL );
+    // fix for linux kernel 2.6
+    mount( "devpts", "/dev/pts", "devpts", MS_MGC_VAL, NULL );
+    eval( "mkdir", "/tmp/www" );
+    eval( "mknod", "/dev/nvram", "c", "229", "0" );
+    eval( "mknod", "/dev/ppp", "c", "108", "0" );
+    eval( "mknod", "-m", "0660", "/dev/mmc", "b", "126", "0" );
+    eval( "mknod", "-m", "0660", "/dev/mmc0", "b", "126", "1" );
+    eval( "mknod", "-m", "0660", "/dev/mmc1", "b", "126", "2" );
+    eval( "mknod", "-m", "0660", "/dev/mmc2", "b", "126", "3" );
+    eval( "mknod", "-m", "0660", "/dev/mmc3", "b", "126", "4" );
+
+    eval( "mkdir", "/dev/mtd" );
+    eval( "mknod", "/dev/mtd/0", "c", "90", "0" );
+    eval( "mknod", "/dev/mtd/0ro", "c", "90", "1" );
+    eval( "mknod", "/dev/mtd/1", "c", "90", "2" );
+    eval( "mknod", "/dev/mtd/1ro", "c", "90", "3" );
+    eval( "mknod", "/dev/mtd/2", "c", "90", "4" );
+    eval( "mknod", "/dev/mtd/2ro", "c", "90", "5" );
+    eval( "mknod", "/dev/mtd/3", "c", "90", "6" );
+    eval( "mknod", "/dev/mtd/3ro", "c", "90", "7" );
+    eval( "mknod", "/dev/mtd/4", "c", "90", "8" );
+    eval( "mknod", "/dev/mtd/4ro", "c", "90", "9" );
+
+    eval( "mknod", "/dev/gpio", "c", "252", "0" );
+
+    cprintf( "sysinit() var\n" );
+
+    /*
+     * /var 
+     */
+    mkdir( "/tmp/var", 0777 );
+    mkdir( "/var/lock", 0777 );
+    mkdir( "/var/log", 0777 );
+    mkdir( "/var/run", 0777 );
+    mkdir( "/var/tmp", 0777 );
+    cprintf( "sysinit() setup console\n" );
+    /*
+     * Setup console 
+     */
+
+    cprintf( "sysinit() klogctl\n" );
+    klogctl( 8, NULL, atoi( nvram_safe_get( "console_loglevel" ) ) );
+    cprintf( "sysinit() get router\n" );
+
+    /*
+     * Modules 
+     */
+    uname( &name );
+    /*
+     * load some netfilter stuff 
+     */
+
+
+//    eval( "watchdog" );
+    /*
+     * Set a sane date 
+     */
+
+    stime( &tm );
+    nvram_set( "wl0_ifname", "ra0" );
+
+    eval( "ifconfig","eth2","up");
+    eval( "vconfig", "set_name_type", "VLAN_PLUS_VID_NO_PAD" );
+    eval( "vconfig", "add", "eth2", "1" ); //LAN 
+    eval( "vconfig", "add", "eth2", "2" ); //WAN
+
+    insmod("rt2860v2_ap");
+    return;
+}
+
+int check_cfe_nv( void )
+{
+    nvram_set( "portprio_support", "0" );
+    return 0;
+}
+
+int check_pmon_nv( void )
+{
+    return 0;
+}
+
+void start_overclocking( void )
+{
+}
+void enable_dtag_vlan( int enable )
+{
+
+}
