486d6c10943dcd2826ce5b78582ac574eb9af980
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-11-08 08:24:29 +0000

    etherip addition
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@10815 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/iproute2/include/utils.h
+++ src/router/iproute2/include/utils.h
@@ -22,6 +22,9 @@ extern char * _SL_;
 #ifndef IPPROTO_AH
 #define IPPROTO_AH	51
 #endif
+#ifndef IPPROTO_ETHERIP
+#define IPPROTO_ETHERIP	97
+#endif
 #ifndef IPPROTO_COMP
 #define IPPROTO_COMP	108
 #endif
