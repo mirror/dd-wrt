8c05dc464a64fd0a96a6c44d66fecb5806517ce8
BrainSlayer <s.gottschall@dd-wrt.com>
2008-11-10 17:16:46 +0000

    ethernet over ip tunnel
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@10844 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/pb42/linux-2.6.22/net/core/dev.c
+++ src/linux/pb42/linux-2.6.22/net/core/dev.c
@@ -1715,6 +1715,18 @@ int netif_rx(struct sk_buff *skb)
 	queue = &__get_cpu_var(softnet_data);
 
 	__get_cpu_var(netdev_rx_stat).total++;
+
+#ifdef CONFIG_BRIDGE
+	/* Optimisation for framebursting (allow interleaving of pkts by
+	 * immediately processing the rx pkt instead of Qing the pkt and deferring
+	 * the processing). Only optimise for bridging and guard against non
+	 * TASKLET based netif_rx calls.
+	 */
+	if (!in_irq() && (skb->dev->br_port != NULL) && br_handle_frame_hook != NULL) {
+		local_irq_restore(flags);
+		return netif_receive_skb(skb);
+	}
+#endif		
 	if (queue->input_pkt_queue.qlen <= netdev_max_backlog) {
 		if (queue->input_pkt_queue.qlen) {
 enqueue:
