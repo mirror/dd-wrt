2c2b95c16daf7929c43b1b65519dce0d42be5fbc
BrainSlayer <s.gottschall@dd-wrt.com>
2008-11-12 23:08:56 +0000

    new bonding driver
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@10887 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/pb42/linux-2.6.22/net/core/dev.c
+++ src/linux/pb42/linux-2.6.22/net/core/dev.c
@@ -2878,6 +2878,7 @@ static int dev_ifsioc(struct ifreq *ifr, unsigned int cmd)
 			    cmd <= SIOCDEVPRIVATE + 15) ||
 			    cmd == SIOCBONDENSLAVE ||
 			    cmd == SIOCBONDRELEASE ||
+			    cmd == SIOCBONDSETWEIGHT ||
 			    cmd == SIOCBONDSETHWADDR ||
 			    cmd == SIOCBONDSLAVEINFOQUERY ||
 			    cmd == SIOCBONDINFOQUERY ||
@@ -3039,6 +3040,7 @@ int dev_ioctl(unsigned int cmd, void __user *arg)
 		case SIOCBONDRELEASE:
 		case SIOCBONDSETHWADDR:
 		case SIOCBONDCHANGEACTIVE:
+		case SIOCBONDSETWEIGHT:
 		case SIOCBRADDIF:
 		case SIOCBRDELIF:
 			if (!capable(CAP_NET_ADMIN))
