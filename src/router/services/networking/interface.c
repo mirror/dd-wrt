ca9dc3143ad0f6bfd7dd24ecfedfa9b45c991af4
BrainSlayer <s.gottschall@dd-wrt.com>
2008-11-21 10:11:02 +0000

    add compatibility for mikrotik ie's
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@10974 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/interface.c
+++ src/router/services/networking/interface.c
@@ -563,6 +563,10 @@ int flush_interfaces( void )
     snprintf( all_ifnames, 255, "%s %s %s", "eth0",
 	      nvram_safe_get( "lan_ifnames" ),
 	      nvram_safe_get( "wan_ifnames" ) );
+#elif HAVE_RCAA01
+    snprintf( all_ifnames, 255, "%s %s %s", "eth0 vlan0 vlan1",
+	      nvram_safe_get( "lan_ifnames" ),
+	      nvram_safe_get( "wan_ifnames" ) );
 #elif HAVE_CA8PRO
     snprintf( all_ifnames, 255, "%s %s %s", "vlan0 vlan1",
 	      nvram_safe_get( "lan_ifnames" ),
