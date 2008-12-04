54d126c74f97ec66df222a7635a055f94deec892
eko <USER@DOMAIN>
2008-12-04 11:43:15 +0000

    formating
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11066 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/usb_hotplug.c
+++ src/router/rc/usb_hotplug.c
@@ -1,10 +1,10 @@
 /* 
- *  USB hotplug service  Copyright 2007, Broadcom Corporation All Rights
- * Reserved.  THIS SOFTWARE IS OFFERED "AS IS", AND BROADCOM GRANTS NO
- * WARRANTIES OF ANY KIND, EXPRESS OR IMPLIED, BY STATUTE, COMMUNICATION OR
- * OTHERWISE. BROADCOM SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A SPECIFIC PURPOSE OR NONINFRINGEMENT
- * CONCERNING THIS SOFTWARE.  $Id$ 
+ * USB hotplug service    Copyright 2007, Broadcom Corporation  All Rights
+ * Reserved.    THIS SOFTWARE IS OFFERED "AS IS", AND BROADCOM GRANTS NO
+ * WARRANTIES OF ANY  KIND, EXPRESS OR IMPLIED, BY STATUTE, COMMUNICATION OR
+ * OTHERWISE. BROADCOM  SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS  FOR A SPECIFIC PURPOSE OR NONINFRINGEMENT
+ * CONCERNING THIS SOFTWARE.    $Id$  
  */  
     
 #include <stdio.h>
@@ -16,113 +16,113 @@
     
 #include <typedefs.h>
 #include <shutils.h>
-static bool usb_ufd_connected( char *str );
-static int usb_process_path( char *path );
-static int usb_add_ufd( void );
+ static bool usb_ufd_connected( char *str );
+ static int usb_process_path( char *path );
+ static int usb_add_ufd( void );
 
-
+ 
     // int hotplug_usb(void)
 int main( int argc, char **argv ) 
 {
-    char *device, *interface;
-    char *action;
-    int class, subclass, protocol;
+     char *device, *interface;
+     char *action;
+     int class, subclass, protocol;
 
-    if( !( action = getenv( "ACTION" ) )
-	  || !( device = getenv( "TYPE" ) ) )
-	return EINVAL;
-    sscanf( device, "%d/%d/%d", &class, &subclass, &protocol );
-    if( class == 0 )
+      if( !( action = getenv( "ACTION" ) )
+	  ||  !( device = getenv( "TYPE" ) ) )
+	 return EINVAL;
+      sscanf( device, "%d/%d/%d", &class, &subclass, &protocol );
+     if( class == 0 )
     {
-	if( !( interface = getenv( "INTERFACE" ) ) )
-	    return EINVAL;
-	sscanf( interface, "%d/%d/%d", &class, &subclass, &protocol );
-    }
-    
+	 if( !( interface = getenv( "INTERFACE" ) ) )
+	     return EINVAL;
+	 sscanf( interface, "%d/%d/%d", &class, &subclass, &protocol );
+     }
+     
 	/* 
 	 * If a new USB device is added and it is of storage class 
 	 */ 
-	if( class == 8 && subclass == 6  &&!strcmp( action, "add" ) )
-	return ( usb_add_ufd(  ) );
-    return 0;
-}
+	if( class == 8 && subclass == 6   &&!strcmp( action, "add" ) )
+	 return ( usb_add_ufd(  ) );
+      return 0;
+ }
 
-
+  
     /* 
-     *  Check if the UFD is still connected because the links created in
-     * /dev/discs are not removed when the UFD is unplugged. 
+     *   Check if the UFD is still connected because the links  created in
+     * /dev/discs are not removed when the UFD is  unplugged.  
      */ 
-static bool  usb_ufd_connected( char *str ) 
+static bool   usb_ufd_connected( char *str ) 
 {
-    uint host_no;
-    char proc_file[128];
+     uint host_no;
+     char proc_file[128];
 
-    
+     
 	/* 
 	 * Host no. assigned by scsi driver for this UFD 
 	 */ 
 	host_no = atoi( str );
-    sprintf( proc_file, "/proc/scsi/usb-storage-%d/%d", host_no, host_no );
-    if( eval( "/bin/grep", "-q", "Attached: Yes", proc_file ) == 0 )
-	return TRUE;
+      sprintf( proc_file, "/proc/scsi/usb-storage-%d/%d", host_no, host_no );
+      if( eval( "/bin/grep", "-q", "Attached: Yes", proc_file ) == 0 )
+	 return TRUE;
     
     else
-	return FALSE;
-}
+	 return FALSE;
+ }
 
-
+  
     /* 
-     *  Mount the path and look for the WCN configuration file. If it
-     * exists launch wcnparse to process the configuration. 
+     *   Mount the path and look for the WCN configuration file.  If it
+     * exists launch wcnparse to process the configuration.  
      */ 
-static int usb_process_path( char *path ) 
+ static int  usb_process_path( char *path ) 
 {
-    int ret = ENOENT;
-    struct stat tmp_stat;
+     int ret = ENOENT;
+     struct stat tmp_stat;
 
-    eval( "/bin/mount", "-t", "vfat", path, "/mnt" );
+      eval( "/bin/mount", "-t", "vfat", path, "/mnt" );
     
 	/* 
 	 * if (stat("/mnt/SMRTNTKY/WSETTING.WFC", &tmp_stat) == 0) {
 	 * eval("/usr/sbin/wcnparse", "-C", "/mnt",
-	 * "SMRTNTKY/WSETTING.WFC"); ret = 0; } eval("/bin/umount",
-	 * "/mnt"); 
+	 * "SMRTNTKY/WSETTING.WFC");  ret = 0;  }  eval("/bin/umount",
+	 * "/mnt");  
 	 */ 
 	return ret;
-}
+ }
 
-
+  
     /* 
      * Handle hotplugging of UFD 
      */ 
-static int usb_add_ufd(  ) 
+static int  usb_add_ufd(  ) 
 {
-    DIR * dir;
-    struct dirent *entry;
-    char path[128];
+     DIR * dir;
+     struct dirent *entry;
+     char path[128];
 
-    if( ( dir = opendir( "/dev/discs" ) ) == NULL )
-	return EINVAL;
-    
+      if( ( dir = opendir( "/dev/discs" ) ) == NULL )
+	 return EINVAL;
+     
 	/* 
 	 * Scan through entries in the directories 
 	 */ 
 	while( ( entry = readdir( dir ) ) != NULL )
     {
-	if( ( strncmp( entry->d_name, "disc", 4 ) ) )
-	    continue;
-	
+	 if( ( strncmp( entry->d_name, "disc", 4 ) ) )
+	     continue;
+	 
 	    /* 
-	     * Files created when the UFD is inserted are not removed when
-	     * it is removed. Verify the device is still inserted. Strip
-	     * the "disc" and pass the rest of the string. 
+	     * Files created when the UFD is inserted are not  removed when
+	     * it is removed. Verify the device  is still inserted.  Strip
+	     * the "disc" and pass the rest of the string.  
 	     */ 
 	    if( usb_ufd_connected( entry->d_name + 4 ) == FALSE )
-	    continue;
-	sprintf( path, "/dev/discs/%s/disc", entry->d_name );
-	eval( "/usr/sbin/disktype", path, ">/tmp/disktype.dump" );
-	eval( "/usr/sbin/disktype", path, ">/tmp/disktype.dump" );
-	
+	     continue;
+	  sprintf( path, "/dev/discs/%s/disc", entry->d_name );
+	  eval( "/usr/sbin/disktype", path, ">/tmp/disktype.dump" );
+	 eval( "/usr/sbin/disktype", path, ">/tmp/disktype.dump" );
+	  
 	    /* 
 	     * Check if it has FAT file system 
 	     */ 
@@ -137,28 +137,28 @@ static int usb_add_ufd(  )
 		      "/tmp/disktype.dump" ) == 0 )
 		
 	    {
-		char part[10], *partitions, *next;
-		struct stat tmp_stat;
+		 char part[10], *partitions, *next;
+		 struct stat tmp_stat;
 
-		partitions = "part1 part2 part3 part4";
-		foreach( part, partitions, next )
+		  partitions = "part1 part2 part3 part4";
+		 foreach( part, partitions, next )
 		{
-		    sprintf( path, "/dev/discs/%s/%s", entry->d_name, part );
-		    if( stat( path, &tmp_stat ) )
-			continue;
-		    if( usb_process_path( path ) == 0 )
-			return 0;
-		}
-	    }
+		     sprintf( path, "/dev/discs/%s/%s", entry->d_name, part );
+		     if( stat( path, &tmp_stat ) )
+			 continue;
+		     if( usb_process_path( path ) == 0 )
+			 return 0;
+		 }
+	     }
 	    
 	    else
 	    {
-		if( usb_process_path( path ) == 0 )
-		    return 0;
-	    }
-	}
-    }
-    return 0;
-}
+		 if( usb_process_path( path ) == 0 )
+		     return 0;
+	     }
+	 }
+     }
+      return 0;
+ }
 
 
