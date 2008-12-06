94eb93b339e5689a25607377d3254dbf1c7e22f6
eko <USER@DOMAIN>
2008-12-06 07:48:31 +0000

    usb auto mount
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11091 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/defaults.c
+++ src/router/services/sysinit/defaults.c
@@ -2486,7 +2486,8 @@ struct nvram_tuple srouter_defaults[] = {
     {"usb_printer", "0", 0},
     {"usb_fs_ext3", "0", 0},
     {"usb_fs_fat", "0", 0},
-    {"usb_automnt", "1", 0},   
+    {"usb_automnt", "0", 0},
+    {"usb_mntpoint", "mnt", 0},   
 #endif
     {"ttraff_enable", "1", 0},
 #ifdef HAVE_PPPOESERVER
