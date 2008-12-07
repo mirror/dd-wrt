5ac002135c5ada999022401a77ea11b47cea080b
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-12-07 08:25:49 +0000

    proftp changes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11123 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/proftpd.mk
+++ src/router/rules/proftpd.mk
@@ -9,8 +9,8 @@ proftpd-clean:
 
 proftpd-install:
 	install -D proftpd/proftpd $(INSTALLDIR)/proftpd/usr/sbin/proftpd
-	install -D proftpd/proftpd.conf $(INSTALLDIR)/proftpd/etc/config/proftpd.conf
-	install -D proftpd/proftpd.startup $(INSTALLDIR)/proftpd/etc/config/proftpd.startup
+#	install -D proftpd/proftpd.conf $(INSTALLDIR)/proftpd/etc/config/proftpd.conf
+#	install -D proftpd/proftpd.startup $(INSTALLDIR)/proftpd/etc/config/proftpd.startup
 	install -D proftpd/config/ftp.webftp $(INSTALLDIR)/proftpd/etc/config/ftp.webftp
 	install -D proftpd/config/proftpd.nvramconfig $(INSTALLDIR)/proftpd/etc/config/proftpd.nvramconfig
 	$(STRIP) $(INSTALLDIR)/proftpd/usr/sbin/proftpd
