d9c931081aa760a28629aed492972259faaef187
eko <USER@DOMAIN>
2008-12-08 16:19:36 +0000

    cosmetic
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11150 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/proftp.c
+++ src/router/services/services/proftp.c
@@ -74,15 +74,15 @@ void start_ftpsrv( void )
 		 "<Directory      /%s/*>\n"
 		 "  AllowOverwrite  on\n"
 		 "   <Limit WRITE>\n"
-      	 "     %s"
-     	 "   </Limit>\n"
+		 "%s"
+		 "   </Limit>\n"
 		 "</Directory>\n",
 		 nvram_safe_get( "lan_ipaddr" ),
 		 nvram_invmatch( "proftpd_passw", "" ) ? "/tmp/proftpd/etc/passwd" : "/tmp/etc/passwd",
 		 nvram_safe_get( "proftpd_port" ),
 		 nvram_safe_get( "proftpd_dir" ),
 		 nvram_safe_get( "proftpd_dir" ),
-		 nvram_match( "proftpd_writeen", "on" ) ? "" : "DenyAll\n" );
+		 nvram_match( "proftpd_writeen", "on" ) ? "" : "    DenyAll\n" );
 
 		 
 	fclose( fp );
