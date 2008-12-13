4ed1242329616e1319a3c66fcf141e2502abee89
BrainSlayer <s.gottschall@dd-wrt.com>
2008-12-13 20:03:31 +0000

    fix pound makefile
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11184 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/pound.mk
+++ src/router/rules/pound.mk
@@ -2,7 +2,7 @@ obj-$(CONFIG_POUND) += pound
 
 
 pound-configure:
-	cd pound && ./configure --with-ssl=$(TOP)/openssl --host=$(ARCH)-linux CC=$(ARCH)-linux-uclibc-gcc LDFLAGS="-Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/openssl" CFLAGS="-Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections $(COPTS) -L$(TOP)/openssl"
+	cd pound && ./configure --prefix=/usr --with-ssl=$(TOP)/openssl --host=$(ARCH)-linux CC=$(ARCH)-linux-uclibc-gcc LDFLAGS="-Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/openssl" CFLAGS="-Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections $(COPTS) -L$(TOP)/openssl"
 
 pound-clean:
 	make -C pound clean
@@ -11,4 +11,4 @@ pound:
 	make -C pound
 
 pound-install:
-	@true
+	make -C pound install DESTDIR=$(INSTALLDIR)/pound
