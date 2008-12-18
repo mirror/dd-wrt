c0ccd261796d548bbb7e76fc9a4043334c3f5f79
BrainSlayer <s.gottschall@dd-wrt.com>
2008-12-18 00:51:00 +0000

    remove debug
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11232 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/usb_hotplug.c
+++ src/router/services/services/usb_hotplug.c
@@ -31,27 +31,27 @@ void start_hotplug_usb( void )
     fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
     if( !(nvram_match ("usb_automnt", "1") ) )
     return;
-    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
+//    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
 
     if( !( action = getenv( "ACTION" ) ) || !( device = getenv( "TYPE" ) ) )
 	return;
-    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
+//    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
     sscanf( device, "%d/%d/%d", &class, &subclass, &protocol );
-    fprintf(stderr,"%s:%d %s\n",__FILE__,__LINE__,device);
+//    fprintf(stderr,"%s:%d %s\n",__FILE__,__LINE__,device);
     if( class == 0 )
     {
 	if( !( interface = getenv( "INTERFACE" ) ) )
 	    return;
 	sscanf( interface, "%d/%d/%d", &class, &subclass, &protocol );
     }
-    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
+//    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
 
     /* 
      * If a new USB device is added and it is of storage class 
      */
     if( class == 8 && subclass == 6 && !strcmp( action, "add" ) )
 	usb_add_ufd(  );
-    fprintf(stderr,"%s:%d %s\n",__FILE__,__LINE__,device);
+//    fprintf(stderr,"%s:%d %s\n",__FILE__,__LINE__,device);
     return;
 }
 
@@ -77,7 +77,7 @@ static bool usb_ufd_connected( char *str )
 	{
 	if (strstr( line, "Attached: Yes" ) )
 	{
-	fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
+//	fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
 	fclose( fp );
 	return TRUE;
 	}
@@ -88,11 +88,11 @@ static bool usb_ufd_connected( char *str )
 	sprintf( proc_file, "/proc/scsi/usb-storage/%d",host_no );
 	if( ( fp = fopen( proc_file, "r" ) ) )
 	    {
-    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
+//    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
 	    fclose(fp);
 	    return TRUE;
 	    }
-    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
+//    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
 	return FALSE;
     
 }
@@ -105,7 +105,7 @@ static int usb_process_path( char *path, char *fs)
 {
     int ret = ENOENT;
     char mount_point[32];
-    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
+//    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
     
     sprintf( mount_point, "/%s", nvram_default_get( "usb_mntpoint", "mnt" ) ); 
 
@@ -139,7 +139,7 @@ static int usb_add_ufd(  )
     /* 
      * Scan through entries in the directories 
      */
-    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
+//    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
     while( ( entry = readdir( dir ) ) != NULL )
     {
 	if( ( strncmp( entry->d_name, "disc", 4 ) ) )
@@ -150,12 +150,12 @@ static int usb_add_ufd(  )
 	 * it is removed. Verify the device  is still inserted.  Strip
 	 * the "disc" and pass the rest of the string.  
 	 */
-    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
+//    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
 	if( usb_ufd_connected( entry->d_name + 4 ) == FALSE )
 	    continue;
-    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
+//    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
 	sprintf( path, "/dev/discs/%s/disc", entry->d_name );
-    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
+//    fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);
 	sysprintf ("/usr/sbin/disktype %s > %s", path, DUMPFILE);
 
 	/* 
