435b7930a487b4e9c2b60675e7cc3fd82f3b5cc9
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-12-19 14:11:21 +0000

    ead support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11251 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libpcap.mk
+++ src/router/rules/libpcap.mk
@@ -1,6 +1,6 @@
 libpcap:
-	$(MAKE) -C libpcap CC=$(CC) AR=$(AR) RANLIB=$(RANLIB) libpcap.so
-	$(MAKE) -C libpcap_noring CC=$(CC) AR=$(AR) RANLIB=$(RANLIB) libpcap.a
+	$(MAKE) -C libpcap CC=$(CC) AR=$(AR) RANLIB=$(RANLIB) libpcap.so 
+	$(MAKE) -C libpcap_noring CC=$(CC) AR=$(AR) RANLIB=$(RANLIB) libpcap.a 
 
 libpcap-install:
 	install -d $(INSTALLDIR)/libpcap/usr/lib
