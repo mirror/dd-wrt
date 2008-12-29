8a20e1ff8c5f105f4a9b1f5be3130fe885d3123d
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2008-12-29 18:44:04 +0000

    enable console output for pound
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11290 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/pound.mk
+++ src/router/rules/pound.mk
@@ -1,7 +1,7 @@
 
 
 pound-configure:
-	cd pound && ./configure --prefix=/usr --with-ssl=$(TOP)/openssl --host=$(ARCH)-linux CC=$(ARCH)-linux-uclibc-gcc LDFLAGS="-Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/openssl" CFLAGS="-Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections $(COPTS) -L$(TOP)/openssl"
+	cd pound && ./configure --prefix=/usr --with-ssl=$(TOP)/openssl --host=$(ARCH)-linux CC=$(ARCH)-linux-uclibc-gcc LDFLAGS="-Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/openssl" CFLAGS="-Drpl_malloc=malloc -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections $(COPTS) -L$(TOP)/openssl"
 
 pound-clean:
 	make -C pound clean
