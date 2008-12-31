375ee63e79d3e25bf6a3fc3ce8d679e98b0e1d2a
BrainSlayer <s.gottschall@dd-wrt.com>
2008-12-31 17:14:23 +0000

    no a good idea to set different macs, this will trigger the ethernet driver mac filter if promisc is not onfigured
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11292 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-ca8.c
+++ src/router/services/sysinit/sysinit-ca8.c
@@ -145,7 +145,7 @@ void start_sysinit( void )
 		    ether_etoa( ( unsigned char * )ifr.ifr_hwaddr.sa_data,
 				eabuf ) );
 	    nvram_set( "et0macaddr", macaddr );
-	    MAC_ADD( macaddr );
+//	    MAC_ADD( macaddr );
 	    ether_atoe( macaddr, ( unsigned char * )ifr.ifr_hwaddr.sa_data );
 	    strncpy( ifr.ifr_name, "vlan1", IFNAMSIZ );
 	    ioctl( s, SIOCSIFHWADDR, &ifr );
