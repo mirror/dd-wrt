696279976bfd703bab310bb2ae2bd4eb2c46587a
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2009-01-08 10:06:01 +0000

    len of mac table increased
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11336 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/wl.c
+++ src/router/libutils/wl.c
@@ -267,7 +267,7 @@ typedef struct _RT_802_11_MAC_ENTRY
 typedef struct _RT_802_11_MAC_TABLE
 {
     unsigned long Num;
-    RT_802_11_MAC_ENTRY Entry[32];	//MAX_LEN_OF_MAC_TABLE = 32
+    RT_802_11_MAC_ENTRY Entry[128];	//MAX_LEN_OF_MAC_TABLE = 32
 } RT_802_11_MAC_TABLE;
 
 typedef struct STAINFO
