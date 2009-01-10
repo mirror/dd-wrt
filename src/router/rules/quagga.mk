24040612cecd0810b7dbd631efd4c938e7d7811a
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2009-01-10 14:32:46 +0000

    fix that typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11358 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/quagga.mk
+++ src/router/rules/quagga.mk
@@ -1,12 +1,12 @@
 quagga-configure:
 ifeq ($(CONFIG_QUAGGA_STABLE),y)
 	cd quagga-stable/readline && ./configure --host=$(ARCH)-uclibc-linux --prefix=/usr CFLAGS="$(COPTS)"
-	$(MAKE) -C quagga/readline
+	$(MAKE) -C quagga-stable/readline
 	cd quagga-stable && ./configure --host=$(ARCH)-uclibc-linux $(CONFIG_QUAGGA_EXTRA) --enable-opaque-lsa --enable-ospf-te --disable-ospfclient --enable-multipath=32  --disable-ipv6 --prefix=/usr  --disable-ospf6d  --enable-vtysh --enable-user=root --enable-group=root --disable-ospfapi --with-libreadline=$(TOP)/quagga/readline CFLAGS="-I$(TOP)/quagga -Drpl_malloc=malloc -Drpl_realloc=realloc $(COPTS)" LDFLAGS="-L$(TOP)/quagga/readline -L$(TOP)/ncurses/lib -lncurses" 
 else
 	cd quagga/readline && ./configure --host=$(ARCH)-uclibc-linux --prefix=/usr CFLAGS="$(COPTS)"
 	$(MAKE) -C quagga/readline
-	cd quagga && ./configure --host=$(ARCH)-uclibc-linux $(CONFIG_QUAGGA_EXTRA) --enable-opaque-lsa --enable-ospf-te --disable-ospfclient --enable-multipath=32  --disable-ipv6 --prefix=/usr  --disable-ospf6d  --enable-vtysh --enable-user=root --enable-group=root --disable-ospfapi --with-libreadline=$(TOP)/quagga/readline CFLAGS="-I$(TOP)/quagga -Drpl_malloc=malloc -Drpl_realloc=realloc $(COPTS)" LDFLAGS="-L$(TOP)/quagga/readline -L$(TOP)/ncurses/lib -lncurses" 
+	cd quagga && ./configure --host=$(ARCH)-uclibc-linux $(CONFIG_QUAGGA_EXTRA) --enable-opaque-lsa --enable-ospf-te --disable-ospfclient --enable-multipath=32  --disable-ipv6 --prefix=/usr  --disable-ospf6d  --enable-vtysh --enable-user=root --enable-group=root --disable-ospfapi --disable-isisd --with-libreadline=$(TOP)/quagga/readline CFLAGS="-I$(TOP)/quagga -Drpl_malloc=malloc -Drpl_realloc=realloc $(COPTS)" LDFLAGS="-L$(TOP)/quagga/readline -L$(TOP)/ncurses/lib -lncurses" 
 endif
 
 quagga: ncurses
