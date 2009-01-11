4d998c1de1f3a2b75f4afb5b990cd74ffb023abe
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2009-01-11 17:09:57 +0000

    alignment fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11382 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/rt2880/linux-2.6.23/net/core/dev.c
+++ src/linux/rt2880/linux-2.6.23/net/core/dev.c
@@ -1781,6 +1781,19 @@ int netif_rx(struct sk_buff *skb)
 	queue = &__get_cpu_var(softnet_data);
 
 	__get_cpu_var(netdev_rx_stat).total++;
+
+#ifdef CONFIG_BRIDGE
+	/* Optimisation for framebursting (allow interleaving of pkts by
+	 * immediately processing the rx pkt instead of Qing the pkt and deferring
+	 * the processing). Only optimise for bridging and guard against non
+	 * TASKLET based netif_rx calls.
+	 */
+	if (!in_irq() && (skb->dev->br_port != NULL) && br_handle_frame_hook != NULL) {
+		local_irq_restore(flags);
+		return netif_receive_skb(skb);
+	}
+#endif		
+
 	if (queue->input_pkt_queue.qlen <= netdev_max_backlog) {
 		if (queue->input_pkt_queue.qlen) {
 enqueue:
