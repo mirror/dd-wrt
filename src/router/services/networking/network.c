2cca3aebbe3afee7f0b4959afebeeee52f90c6bc
BrainSlayer <s.gottschall@dd-wrt.com>
2009-01-21 17:37:13 +0000

    bullet 2 and loco 2 support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11495 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/network.c
+++ src/router/services/networking/network.c
@@ -1065,7 +1065,7 @@ void start_lan( void )
 #ifdef HAVE_LS2
     if( getSTA(  ) || getWET(  ) || nvram_match( "wan_proto", "disabled" ) )
     {
-#ifdef HAVE_NS2
+#if defined(HAVE_NS2) || defined(HAVE_BS2) || defined(HAVE_LC2)
 	nvram_set( "lan_ifname", "br0" );
 	nvram_set( "lan_ifnames", "eth0 vlan2 ath0" );
 	PORTSETUPWAN( "" );
@@ -1077,7 +1077,7 @@ void start_lan( void )
     }
     else
     {
-#ifdef HAVE_NS2
+#if defined(HAVE_NS2) || defined(HAVE_BS2) || defined(HAVE_LC2)
 	nvram_set( "lan_ifname", "br0" );
 	nvram_set( "lan_ifnames", "eth0 ath0" );
 	PORTSETUPWAN( "eth0" );
@@ -2599,6 +2599,14 @@ void start_wan( int status )
 			    "" ) ? nvram_safe_get( "pppoe_wan_ifname" ) :
 	    "eth0";
 #elif HAVE_NS2
+    char *pppoe_wan_ifname = nvram_invmatch( "pppoe_wan_ifname",
+					     "" ) ?
+	nvram_safe_get( "pppoe_wan_ifname" ) : "eth0";
+#elif HAVE_BS2
+    char *pppoe_wan_ifname = nvram_invmatch( "pppoe_wan_ifname",
+					     "" ) ?
+	nvram_safe_get( "pppoe_wan_ifname" ) : "eth0";
+#elif HAVE_LC2
     char *pppoe_wan_ifname = nvram_invmatch( "pppoe_wan_ifname",
 					     "" ) ?
 	nvram_safe_get( "pppoe_wan_ifname" ) : "eth0";
