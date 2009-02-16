cc2fd88b26e8213d201af50e0bb3fbbb195c42da
BrainSlayer <s.gottschall@dd-wrt.com>
2009-02-16 15:43:35 +0000

    add relay bridge support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11616 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/wshaper.c
+++ src/router/services/networking/wshaper.c
@@ -683,6 +683,7 @@ void stop_wshaper( void )
     ret = eval( script_name, "stop", "XX", "vlan1" );
     ret = eval( script_name, "stop", "XX", "vlan2" );
     ret = eval( script_name, "stop", "XX", "ra0" );
+    ret = eval( script_name, "stop", "XX", "apcli0" );
 #elif HAVE_FONERA
     ret = eval( script_name, "stop", "XX", "eth0" );
     ret = eval( script_name, "stop", "XX", "vlan0" );
