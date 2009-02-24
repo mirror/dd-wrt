5b83715d5bc81405ecc8e6551f2bab2dcdd4bd1f
BrainSlayer <s.gottschall@dd-wrt.com>
2009-02-24 16:14:51 +0000

    rpl_malloc added
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11678 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/wol.mk
+++ src/router/rules/wol.mk
@@ -1,5 +1,5 @@
 wol-configure:
-	cd wol && ./configure --disable-nls --host=$(ARCH)-linux CC=$(CC) CFLAGS="$(COPTS)"
+	cd wol && ./configure --disable-nls --host=$(ARCH)-linux CC=$(CC) CFLAGS="$(COPTS) -Drpl_malloc=malloc" 
 
 wol:
 	$(MAKE) -C wol
