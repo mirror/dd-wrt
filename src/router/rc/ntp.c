1a7124de03fa70b9bac0001090b351356ae6d7ac
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2009-03-03 10:37:36 +0000

    daylight time saving for some states in australia
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11713 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/ntp.c
+++ src/router/rc/ntp.c
@@ -110,7 +110,7 @@ int do_ntp( void )		// called from ntp_main and
     tv.tv_sec += ( int )( fofs * 3600 );	// <-- cast it or this will
     // be off (?)
 
-    if( ( dst >= 1 ) && ( dst <= 8 ) )
+    if( ( dst >= 1 ) && ( dst <= 9 ) )
     {
 	struct tm *tm = localtime( &tv.tv_sec );
 	int mon = tm->tm_mon + 1;
@@ -126,18 +126,6 @@ int do_ntp( void )		// called from ntp_main and
 	    ( ( mbeg < mend ) && ( mon > mbeg ) && ( mon < mend ) ) ||
 	    ( ( mbeg > mend ) && ( ( mon > mbeg ) || ( mon < mend ) ) ) )
 	{
-
-	    // ...in dst...
-
-	    // if this is the end day, don't undo dst until we're past 1am to 
-	    // 
-	    // 
-	    // 
-	    // 
-	    // 
-	    // 
-	    // 
-	    // avoid the day from going back to yesterday
 	    if( ( mon != mend ) || ( day != dend ) || ( tm->tm_hour <= 1 ) )
 		tv.tv_sec += dstEntry[dst].dstBias;
 	}
