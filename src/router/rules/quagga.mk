a766829f8adca38a3e297511fbc1c13779d9df34
BrainSlayer <s.gottschall@dd-wrt.com>
2009-03-18 15:11:28 +0000

    quagga fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11769 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/quagga.mk
+++ src/router/rules/quagga.mk
@@ -2,7 +2,7 @@ quagga-configure:
 ifeq ($(CONFIG_QUAGGA_STABLE),y)
 	cd quagga-stable/readline && ./configure --host=$(ARCH)-uclibc-linux --prefix=/usr CFLAGS="$(COPTS)"
 	$(MAKE) -C quagga-stable/readline
-	cd quagga-stable && ./configure --host=$(ARCH)-uclibc-linux $(CONFIG_QUAGGA_EXTRA) --enable-opaque-lsa --enable-ospf-te --disable-ospfclient --enable-multipath=32  --disable-ipv6 --prefix=/usr  --disable-ospf6d  --enable-vtysh --enable-user=root --enable-group=root --disable-ospfapi --with-libreadline=$(TOP)/quagga-stable/readline CFLAGS="-I$(TOP)/quagga -Drpl_malloc=malloc -Drpl_realloc=realloc $(COPTS)" LDFLAGS="-L$(TOP)/quagga-stable/readline -L$(TOP)/ncurses/lib -lncurses" 
+	cd quagga-stable && ./configure --host=$(ARCH)-uclibc-linux $(CONFIG_QUAGGA_EXTRA) --enable-opaque-lsa --enable-ospf-te --disable-ospfclient --enable-multipath=32  --disable-ipv6 --prefix=/usr  --disable-ospf6d  --enable-vtysh --enable-user=root --enable-group=root --disable-ospfapi --with-libreadline=$(TOP)/quagga-stable/readline CFLAGS="-I$(TOP)/quagga-stable -Drpl_malloc=malloc -Drpl_realloc=realloc $(COPTS)" LDFLAGS="-L$(TOP)/quagga-stable/readline -L$(TOP)/ncurses/lib -lncurses" 
 else
 	cd quagga/readline && ./configure --host=$(ARCH)-uclibc-linux --prefix=/usr CFLAGS="$(COPTS)"
 	$(MAKE) -C quagga/readline
