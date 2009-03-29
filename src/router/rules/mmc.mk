3101ae730080c40484afef34c6269ff827c07a77
eko <USER@DOMAIN>
2009-03-29 08:02:50 +0000

    makes duplicate mmc.o; done in main Makefile
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11799 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/mmc.mk
+++ src/router/rules/mmc.mk
@@ -2,11 +2,6 @@ mmc:
 	$(MAKE) -C mmc
 
 mmc-install:
-ifeq ($(CONFIG_BCM5354),y)
-	install -D  mmc/mmc.o $(INSTALLDIR)/mmc/lib/modules/2.4.36/mmc.o
-else
-	install -D  mmc/mmc.o $(INSTALLDIR)/mmc/lib/modules/2.4.35/mmc.o
-endif
 	install -D  mmc/config/mmc.webconfig $(INSTALLDIR)/mmc/etc/config/mmc.webconfig
 	install -D  mmc/config/mmc.startup $(INSTALLDIR)/mmc/etc/config/mmc.startup
 	install -D  mmc/config/mmc.nvramconfig $(INSTALLDIR)/mmc/etc/config/mmc.nvramconfig
