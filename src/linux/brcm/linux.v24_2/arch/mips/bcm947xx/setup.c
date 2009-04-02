8aa30d67754cb4210dded0c0a7a94f113135a040
eko <USER@DOMAIN>
2009-04-02 06:15:41 +0000

    should fix wrt600n leds
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11805 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/brcm/linux.v24_2/arch/mips/bcm947xx/setup.c
+++ src/linux/brcm/linux.v24_2/arch/mips/bcm947xx/setup.c
@@ -211,7 +211,9 @@ iswrt300n11=1;
 char *boardtype = nvram_get("boardtype");
 char *boothwmodel = nvram_get("boot_hw_model");
 char *boothwver = nvram_get("boot_hw_ver");
+char *cardbus = nvram_get("cardbus");
 if (boardtype==NULL || strcmp(boardtype,"0x478"))iswrt350n=0;
+if (cardbus!=NULL && !strcmp(cardbus, "0") && boardtype!=NULL && !strcmp(boardtype,"0x478"))iswrt350n=0;
 if (boothwmodel==NULL || strcmp(boothwmodel,"WRT300N"))iswrt300n11=0;
 if (boothwmodel!=NULL && !strcmp(boothwmodel,"WRT610N"))
     {
