952505bb3cdb3c6ef566f118e4e6c373bef7d893
BrainSlayer <s.gottschall@dd-wrt.com>
2009-04-04 14:31:27 +0000

    handled by option driver
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11830 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/x86/linux-2.6.23/drivers/usb/storage/initializers.c
+++ src/linux/x86/linux-2.6.23/drivers/usb/storage/initializers.c
@@ -90,3 +90,16 @@ int usb_stor_ucr61s2b_init(struct us_data *us)
 
 	return (res ? -1 : 0);
 }
+
+/* This places the HUAWEI E220 devices in multi-port mode */
+int usb_stor_huawei_e220_init(struct us_data *us)
+{
+	int result;
+
+	result = usb_stor_control_msg(us, us->send_ctrl_pipe,
+				      USB_REQ_SET_FEATURE,
+				      USB_TYPE_STANDARD | USB_RECIP_DEVICE,
+				      0x01, 0x0, NULL, 0x0, 1000);
+	US_DEBUGP("usb_control_msg performing result is %d\n", result);
+	return (result ? 0 : -1);
+}
