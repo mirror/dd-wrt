224981674998ec00adcb71a8ad6d7fcb87b3002d
BrainSlayer <s.gottschall@dd-wrt.com>
2009-04-04 00:55:46 +0000

    better detection handling for multiple usb ports
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11823 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/lib3g.c
+++ src/router/libutils/lib3g.c
@@ -3,41 +3,65 @@
 
 
 
+static int scanFor(int Vendor,int Product)
+{
+int count=1;
+while (1)
+    {
+    char sysfs[64];
+    sprintf(sysfs,"/sys/bus/usb/devices/%d-0:1.0/bInterfaceNumber",count);
+    FILE *probe =fopen(sysfs,"rb");
+    if (!probe)
+	return 0;
+    fclose(probe);
+    sprintf(sysfs,"/sys/bus/usb/devices/%d-1/idProduct",count);
+    FILE *modem =fopen(sysfs,"rb");
+    if (!modem)
+	{
+	count++;
+	continue;
+	}
+    int idProduct;
+    int idVendor;
+    fscanf(modem,"%X",&idProduct);
+    fclose(modem);    
+    sprintf(sysfs,"/sys/bus/usb/devices/%d-1/idVendor",count);
+    modem =fopen(sysfs,"rb");
+    if (!modem)
+	{
+	count++;
+	continue;
+	}
+    fscanf(modem,"%X",&idVendor);
+    fclose(modem);
+    if (idVendor==Vendor && idProduct==Product)
+	return 1;
+    count++;
+    }
 
+}
 
 
 char *get3GControlDevice(void)
 {
 	char *ttsdevice="/dev/usb/tts/0";
-	FILE *modem =fopen("/sys/bus/usb/devices/1-1/idProduct","rb");
-	if (!modem)
-	    return ttsdevice;
-	int idProduct;
-	int idVendor;
-	fscanf(modem,"%X",&idProduct);
-	fclose(modem);    
-	modem =fopen("/sys/bus/usb/devices/1-1/idVendor","rb");
-	if (!modem)
-	    return ttsdevice;
-	fscanf(modem,"%X",&idVendor);
-	fclose(modem);    
-	if (idVendor==0x1199 && idProduct==0x6880)
+	if (scanFor(0x1199,0x6880))
 	    {
 	    //sierra wireless 
 	    fprintf(stderr,"Sierra Wireless Compass 885 deteted\n");
-	    ttsdevice="/dev/usb/tts/3";
+	    return "/dev/usb/tts/3";
 	    }
-	if (idVendor==0x12d1 && idProduct==0x1003)
+	if (scanFor(0x12d1,0x1003))
 	    {
 	    //huawei
 	    fprintf(stderr,"HUAWEI/Option E172 detected\n");
-	    ttsdevice="/dev/usb/tts/0";
+	    return "/dev/usb/tts/0";
 	    }
-	if (idVendor==0x0af0 && idProduct==0x7011)
+	if (scanFor(0x0af0,0x7011))
 	    {
 	    //huawei
 	    fprintf(stderr,"HUAWEI/Option E301 HSUPA detected\n");
-	    ttsdevice="/dev/usb/tts/0";
+	    return "/dev/usb/tts/0";
 	    }
 return ttsdevice;
 }
