a94f2d31c6ebd12e0003d198c131b82aa2886e7d
eko <USER@DOMAIN>
2009-04-07 13:12:14 +0000

    wrt600n leds test
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11870 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/utils.c
+++ src/router/libutils/utils.c
@@ -2714,12 +2714,12 @@ int led_control( int type, int act )
 	    // usb_gpio = 0x04; 
 	    break;
 	case ROUTER_WRT600N:
-	    connected_gpio = 0x13;
-	    power_gpio = 0x02;
+	    // connected_gpio = 0x13;
+	    power_gpio = 0x12;
 	    diag_gpio = 0x02;
 	    usb_gpio = 0x13;
-	    ses_gpio = 0x18;
-	    ses2_gpio = 0x1a;
+	    //ses_gpio = 0x18;
+	    //ses2_gpio = 0x1a;
 	    break;
 	case ROUTER_LINKSYS_WRT55AG:
 	    connected_gpio = 0x13;
