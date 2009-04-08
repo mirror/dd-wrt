ac9d0e4101a26e2a3c81005577a313abf27c2115
eko <USER@DOMAIN>
2009-04-08 07:02:42 +0000

    security leds for wrt600n ....
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11884 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/utils.h
+++ src/router/shared/utils.h
@@ -450,7 +450,7 @@ extern void getWANMac( char *newmac );
 extern int led_control( int type, int act );
 enum
 { LED_POWER, LED_DIAG, LED_DMZ, LED_CONNECTED, LED_BRIDGE, LED_VPN,
-    LED_SES, LED_SES2, LED_WLAN, LED_USB
+    LED_SES, LED_SES2, LED_WLAN, LED_USB, LED_SEC0, LED_SEC1
 };
 enum
 { LED_ON, LED_OFF, LED_FLASH };
