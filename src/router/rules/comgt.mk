711df4d7bfaab66b59f1ddee7ee4931fb23a5739
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2009-04-13 17:43:00 +0000

    reset and wakeup scripts
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11911 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/comgt.mk
+++ src/router/rules/comgt.mk
@@ -7,5 +7,7 @@ comgt-install:
 	install -D comgt/comgt $(INSTALLDIR)/comgt/usr/sbin/comgt
 	install -D comgt/scripts/dial.comgt $(INSTALLDIR)/comgt/etc/comgt/dial.comgt
 	install -D comgt/scripts/setmode.comgt $(INSTALLDIR)/comgt/etc/comgt/setmode.comgt
+	install -D comgt/scripts/reset.comgt $(INSTALLDIR)/comgt/etc/comgt/reset.comgt
+	install -D comgt/scripts/wakeup.comgt $(INSTALLDIR)/comgt/etc/comgt/wakeup.comgt
 #	install -D comgt/usb_modeswitch $(INSTALLDIR)/comgt/usr/sbin/usb_modeswitch
 
