29d7da390d1c1c5c1ad6faaef4a84921bf325a9a
BrainSlayer <s.gottschall@dd-wrt.com>
2009-04-14 02:39:55 +0000

    revert
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11937 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/asterisk.mk
+++ src/router/rules/asterisk.mk
@@ -1,14 +1,12 @@
 asterisk-configure:
-	cd asterisk && ./configure --host=$(ARCH)-linux-uclibc CFLAGS="$(COPTS) -DNEED_PRINTF -I$(TOP)/zlib -L$(TOP)" -without-curl \
+	cd asterisk && ./configure --host=$(ARCH)-linux-uclibc CFLAGS="$(COPTS) -DNEED_PRINTF" -without-curl \
 	--without-curses \
-	--without-gtk \
-	--without-gtk2 \
 	--without-gsm \
+	--without-imap \
 	--without-isdnnet \
 	--without-kde \
 	--without-misdn \
 	--without-nbs \
-	--without-x11 \
 	--with-ncurses="$(TOP)/ncurses" \
 	--without-netsnmp \
 	--without-newt \
@@ -19,7 +17,6 @@ asterisk-configure:
 	--without-pri \
 	--without-qt \
 	--without-radius \
-	--without-sdl \
 	--without-spandsp \
 	--without-suppserv \
 	--without-tds \
@@ -29,7 +26,6 @@ asterisk-configure:
 	--without-vorbis \
 	--without-vpb \
 	--without-zaptel \
-	--with-z="$(TOP)/zlib" 
 
 #	--with-popt="$(STAGING_DIR)/usr" \
 
@@ -39,9 +35,6 @@ asterisk:
 asterisk-install:
 	make -C asterisk install DESTDIR=$(INSTALLDIR)/asterisk
 	make -C asterisk adsi DESTDIR=$(INSTALLDIR)/asterisk
-	rm -rf $(INSTALLDIR)/asterisk/var
-	rm -rf $(INSTALLDIR)/asterisk/usr/share
-	rm -rf $(INSTALLDIR)/asterisk/usr/include
 #	make -C ncurses install.libs DESTDIR=$(INSTALLDIR)/ncurses
 #	rm -rf $(INSTALLDIR)/ncurses/usr/include
 #	rm -f $(INSTALLDIR)/ncurses/usr/lib/*.a
