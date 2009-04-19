313aceb8d7d93eb95035dd064308ba28359a88ec
BrainSlayer <s.gottschall@dd-wrt.com>
2009-04-19 23:45:23 +0000

    new olsrd version
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@11962 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/olsrd/contrib/netsimpcap/src/config.h
@@ -0,0 +1,34 @@
+
+/*
+ * NetsimPcap - a userspace network bridge with simulated packet loss
+ *             Copyright 2008 H. Rogge (rogge@fgan.de)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CONFIG_H_
+
+#define CONFIG_H_
+
+#define GRID(x,y,width) ((x) + (y) * (width))
+
+int readConnectionMatrix(float *connectionMatrix, char *filename, int len);
+#endif /*CONFIG_H_ */
+
+/*
+ * Local Variables:
+ * c-basic-offset: 2
+ * indent-tabs-mode: nil
+ * End:
+ */
