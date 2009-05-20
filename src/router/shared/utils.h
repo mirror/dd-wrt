67116f2937859a04d17dc79a6e10df5168b1f9a4
eko <USER@DOMAIN>
2009-05-20 06:40:11 +0000

    some more dual radio stuff
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@12176 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/utils.h
+++ src/router/shared/utils.h
@@ -483,7 +483,7 @@ extern unsigned long get_register_value( unsigned short id,
 int ct_openlog( const char *ident, int option, int facility, char *log_name );
 void ct_syslog( int level, int enable, const char *fmt, ... );
 void ct_logger( int level, const char *fmt, ... );
-struct wl_assoc_mac *get_wl_assoc_mac( int *c );
+struct wl_assoc_mac *get_wl_assoc_mac( int instance, int *c );
 
 extern struct detect_wans *detect_protocol( char *wan_face, char *lan_face,
 					    char *type );
