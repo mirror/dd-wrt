67aea10dd37702e01897e6efb086cd723295be5f
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2009-05-22 14:14:46 +0000

    now MAC filter works for both radios
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@12205 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/validate/validators.c
+++ src/router/httpd/validate/validators.c
@@ -1418,13 +1418,11 @@ void validate_wl_hwaddrs( webs_t wp, char *value, struct variable *v )
 	char mlist[32];
 	sprintf( mlist, "%s_maclist", ifname );
 	nvram_set( mlist, buf );
+	char acmac[32];
+	sprintf( acmac, "%s_active_mac", ifname );
+	nvram_set( acmac, "" );	
 	if( !strcmp( ifname, "wl0" ) )
-	{
-	nvram_set( "wl_active_mac", "" );
-	nvram_set( "wl0_active_mac", "" );
-	}
-	else
-	nvram_set( "wl1_active_mac", "" );
+		nvram_set( "wl_active_mac", "" );
     }
     free(buf);
 }
