6bc94b354729f40bae7918f0cdc22ef86c7a0ca0
BrainSlayer <s.gottschall@dd-wrt.com>
2009-05-25 21:22:10 +0000

    nprobe update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@12209 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/nprobe.mk
+++ src/router/rules/nprobe.mk
@@ -1,10 +1,16 @@
 nprobe-configure:
-	cd nprobe && ./configure --host=$(ARCH)-linux CC=$(ARCH)-linux-uclibc-gcc CPPFLAGS="-I../libpcap_noring $(COPTS) -DNEED_PRINTF" CFLAGS="-I../libpcap_noring $(COPTS) -DNEED_PRINTF" LDFLAGS="-L../libpcap_noring" --with-only-ipv4 PCAP_ROOT="$(TOP)/libpcap_noring" \
+	chmod +x nprobe/configure
+	chmod +x nprobe/config.guess
+	cd nprobe && ./configure --prefix=/usr --host=$(ARCH)-linux CC=$(ARCH)-linux-uclibc-gcc CPPFLAGS="-I../libpcap_noring $(COPTS) -DNEED_PRINTF" CFLAGS="-I../libpcap_noring $(COPTS) -DNEED_PRINTF" LDFLAGS="-L../libpcap_noring" --with-only-ipv4 PCAP_ROOT="$(TOP)/libpcap_noring" \
 
 nprobe:
-	make -j 4 -C nprobe
+	make -C nprobe all
+
+
+nprobe-clean:
+	make -j 4 -C nprobe clean
 
 
 nprobe-install:
-	install -D nprobe/nprobe $(INSTALLDIR)/nprobe/usr/sbin/nprobe
+	make -C nprobe install DESTDIR=$(INSTALLDIR)/nprobe
 
