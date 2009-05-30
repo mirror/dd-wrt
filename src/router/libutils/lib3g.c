3171dc35d4ebb6ef7795af9aa5a34b25acb4dc40
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2009-05-30 21:10:25 +0000

    code formating
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@12225 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/lib3g.c
+++ src/router/libutils/lib3g.c
@@ -3,192 +3,173 @@
 #include <shutils.h>
 #include <bcmnvram.h>
 
-
-
-static int scanFor(int Vendor,int Product)
+static int scanFor(int Vendor, int Product)
 {
 #ifdef ARCH_broadcom
-char grepstr[128];
-sprintf(grepstr,"grep Vendor=%x ProdID=%x /tmp/usb/devices|wc -l",Vendor,Product);
-FILE *check=popen(grepstr,"rb");
-if (check)
-    {
-    int count=0;
-    fscanf(check,"%d",&count);
-    fclose(check);
-    if (count>0)
-	{
-	eval("umount /tmp/usb");
-	return 1;
+	char grepstr[128];
+	sprintf(grepstr, "grep Vendor=%x ProdID=%x /tmp/usb/devices|wc -l",
+		Vendor, Product);
+	FILE *check = popen(grepstr, "rb");
+	if (check) {
+		int count = 0;
+		fscanf(check, "%d", &count);
+		fclose(check);
+		if (count > 0) {
+			eval("umount /tmp/usb");
+			return 1;
+		}
 	}
-    }
-return 0;
-#else
-int count=1;
-while (1)
-    {
-    char sysfs[64];
-    sprintf(sysfs,"/sys/bus/usb/devices/%d-0:1.0/bInterfaceNumber",count);
-    FILE *probe =fopen(sysfs,"rb");
-    if (!probe)
 	return 0;
-    fclose(probe);
-    sprintf(sysfs,"/sys/bus/usb/devices/%d-1/idProduct",count);
-    FILE *modem =fopen(sysfs,"rb");
-    if (!modem)
-	{
-	count++;
-	continue;
+#else
+	int count = 1;
+	while (1) {
+		char sysfs[64];
+		sprintf(sysfs, "/sys/bus/usb/devices/%d-0:1.0/bInterfaceNumber",
+			count);
+		FILE *probe = fopen(sysfs, "rb");
+		if (!probe)
+			return 0;
+		fclose(probe);
+		sprintf(sysfs, "/sys/bus/usb/devices/%d-1/idProduct", count);
+		FILE *modem = fopen(sysfs, "rb");
+		if (!modem) {
+			count++;
+			continue;
+		}
+		int idProduct;
+		int idVendor;
+		fscanf(modem, "%X", &idProduct);
+		fclose(modem);
+		sprintf(sysfs, "/sys/bus/usb/devices/%d-1/idVendor", count);
+		modem = fopen(sysfs, "rb");
+		if (!modem) {
+			count++;
+			continue;
+		}
+		fscanf(modem, "%X", &idVendor);
+		fclose(modem);
+		if (idVendor == Vendor && idProduct == Product)
+			return 1;
+		count++;
 	}
-    int idProduct;
-    int idVendor;
-    fscanf(modem,"%X",&idProduct);
-    fclose(modem);    
-    sprintf(sysfs,"/sys/bus/usb/devices/%d-1/idVendor",count);
-    modem =fopen(sysfs,"rb");
-    if (!modem)
-	{
-	count++;
-	continue;
-	}
-    fscanf(modem,"%X",&idVendor);
-    fclose(modem);
-    if (idVendor==Vendor && idProduct==Product)
-	return 1;
-    count++;
-    }
 #endif
 }
 
 void checkreset(char *tty)
 {
 #ifdef HAVE_CAMBRIA
-	eval("comgt","-d",tty,"-s","/etc/comgt/reset.comgt");
-	FILE *check=NULL;
-	int count=0;
+	eval("comgt", "-d", tty, "-s", "/etc/comgt/reset.comgt");
+	FILE *check = NULL;
+	int count = 0;
 	sleep(1);
-	while (!(check=fopen(tty,"rb")) && count<10)
-	    {
-	    sleep(1);
-	    count++;
-	    }
+	while (!(check = fopen(tty, "rb")) && count < 10) {
+		sleep(1);
+		count++;
+	}
 	if (check)
-	    fclose(check);
+		fclose(check);
 	else
-	    fprintf(stderr,"reset error\n");
-	fprintf(stderr,"wakeup card\n");
-	eval("comgt","-d",tty,"-s","/etc/comgt/wakeup.comgt");
-	sleep(5);//give extra delay for registering
+		fprintf(stderr, "reset error\n");
+	fprintf(stderr, "wakeup card\n");
+	eval("comgt", "-d", tty, "-s", "/etc/comgt/wakeup.comgt");
+	sleep(5);		//give extra delay for registering
 #endif
 }
 
 char *get3GControlDevice(void)
 {
 #ifdef ARCH_broadcom
-mkdir("/tmp/usb");
-eval("mount","-t","usbfs","usb","/tmp/usb");
-insmod("usbserial");
+	mkdir("/tmp/usb");
+	eval("mount", "-t", "usbfs", "usb", "/tmp/usb");
+	insmod("usbserial");
 //insmod("sierra");  //further investigation required (compass problem)
-insmod("option");
-insmod("ipw");
-insmod("pl2303");
+	insmod("option");
+	insmod("ipw");
+	insmod("pl2303");
 #endif
 
-	char *ttsdevice="/dev/usb/tts/0";
+	char *ttsdevice = "/dev/usb/tts/0";
 #ifdef HAVE_CAMBRIA
-	    eval("gpio","enable","26");
-	    eval("gpio","disable","27");
-	    if (nvram_match("wan_select","1"))
-		{
-		eval("gpio","enable","26");
-		eval("gpio","disable","27");
-		}
-	    if (nvram_match("wan_select","2"))
-		{
-		eval("gpio","disable","26");
-		eval("gpio","enable","27");
-		}
-	    if (nvram_match("wan_select","3"))
-		{
-		eval("gpio","enable","26");
-		eval("gpio","enable","27");
-		}
+	eval("gpio", "enable", "26");
+	eval("gpio", "disable", "27");
+	if (nvram_match("wan_select", "1")) {
+		eval("gpio", "enable", "26");
+		eval("gpio", "disable", "27");
+	}
+	if (nvram_match("wan_select", "2")) {
+		eval("gpio", "disable", "26");
+		eval("gpio", "enable", "27");
+	}
+	if (nvram_match("wan_select", "3")) {
+		eval("gpio", "enable", "26");
+		eval("gpio", "enable", "27");
+	}
 #endif
-	nvram_set("3gdata","/dev/usb/tts/0");
-	if (scanFor(0x1199,0x6880))
-	    {
-	    //sierra wireless 
-	    fprintf(stderr,"Sierra Wireless Compass 885 deteted\n");
-	    nvram_set("3gdata","/dev/usb/tts/4");
-	    return "/dev/usb/tts/3";
-	    }
-	if (scanFor(0x1199,0x6890))
-	    {
-	    //sierra wireless 
-	    fprintf(stderr,"Sierra Wireless Compass 888 deteted\n");
-	    nvram_set("3gdata","/dev/usb/tts/4");
-	    return "/dev/usb/tts/3";
-	    }
-	if (scanFor(0x1199,0x683C))
-	    {
-	    //sierra wireless 
-	    fprintf(stderr,"Sierra Wireless MC8790\n");
-	    nvram_set("3gdata","/dev/usb/tts/4");
-	    checkreset("/dev/usb/tts/3");
-	    return "/dev/usb/tts/3";
-	    }
-	if (scanFor(0x1199,0x683D))
-	    {
-	    //sierra wireless 
-	    fprintf(stderr,"Sierra Wireless MC8790\n");
-	    nvram_set("3gdata","/dev/usb/tts/4");
-	    checkreset("/dev/usb/tts/3");
-	    return "/dev/usb/tts/3";
-	    }
-	if (scanFor(0x1199,0x683E))
-	    {
-	    //sierra wireless 
-	    fprintf(stderr,"Sierra Wireless MC8790\n");
-	    nvram_set("3gdata","/dev/usb/tts/4");
-	    checkreset("/dev/usb/tts/3");
-	    return "/dev/usb/tts/3";
-	    }
-	if (scanFor(0x12d1,0x1003))
-	    {
-	    //huawei
-	    fprintf(stderr,"HUAWEI/Option E172 detected\n");
-	    return "/dev/usb/tts/0";
-	    }
-	if (scanFor(0x0af0,0x7011))
-	    {
-	    //huawei
-	    fprintf(stderr,"HUAWEI/Option E301 HSUPA detected\n");
-	    return "/dev/usb/tts/0";
-	    }
-	if (scanFor(0x12d1,0x1001))
-	    {
-	    //huawei
-	    fprintf(stderr,"HUAWEI/Option E600 detected\n");
-	    return "/dev/usb/tts/0";
-	    }
-	if (scanFor(0x12d1,0x1003))
-	    {
-	    //huawei
-	    fprintf(stderr,"HUAWEI/Option EC270 detected\n");
-	    return "/dev/usb/tts/0";
-	    }
-	if (scanFor(0x12d1,0x1412))
-	    {
-	    //huawei
-	    fprintf(stderr,"HUAWEI/Option EC168 detected\n");
-	    return "/dev/usb/tts/0";
-	    }
-	if (scanFor(0x1199,0x6832))
-	    { 
-	    //sierra wireless mc 8780
-	    fprintf(stderr,"Sierra Wireless MC 8780 detected\nreset card\n");
-	    checkreset("/dev/usb/tts/2");
-	    return "/dev/usb/tts/2";
-	    }
-return ttsdevice;
+	nvram_set("3gdata", "/dev/usb/tts/0");
+	if (scanFor(0x1199, 0x6880)) {
+		//sierra wireless 
+		fprintf(stderr, "Sierra Wireless Compass 885 deteted\n");
+		nvram_set("3gdata", "/dev/usb/tts/4");
+		return "/dev/usb/tts/3";
+	}
+	if (scanFor(0x1199, 0x6890)) {
+		//sierra wireless 
+		fprintf(stderr, "Sierra Wireless Compass 888 deteted\n");
+		nvram_set("3gdata", "/dev/usb/tts/4");
+		return "/dev/usb/tts/3";
+	}
+	if (scanFor(0x1199, 0x683C)) {
+		//sierra wireless 
+		fprintf(stderr, "Sierra Wireless MC8790\n");
+		nvram_set("3gdata", "/dev/usb/tts/4");
+		checkreset("/dev/usb/tts/3");
+		return "/dev/usb/tts/3";
+	}
+	if (scanFor(0x1199, 0x683D)) {
+		//sierra wireless 
+		fprintf(stderr, "Sierra Wireless MC8790\n");
+		nvram_set("3gdata", "/dev/usb/tts/4");
+		checkreset("/dev/usb/tts/3");
+		return "/dev/usb/tts/3";
+	}
+	if (scanFor(0x1199, 0x683E)) {
+		//sierra wireless 
+		fprintf(stderr, "Sierra Wireless MC8790\n");
+		nvram_set("3gdata", "/dev/usb/tts/4");
+		checkreset("/dev/usb/tts/3");
+		return "/dev/usb/tts/3";
+	}
+	if (scanFor(0x12d1, 0x1003)) {
+		//huawei
+		fprintf(stderr, "HUAWEI/Option E172 detected\n");
+		return "/dev/usb/tts/0";
+	}
+	if (scanFor(0x0af0, 0x7011)) {
+		//huawei
+		fprintf(stderr, "HUAWEI/Option E301 HSUPA detected\n");
+		return "/dev/usb/tts/0";
+	}
+	if (scanFor(0x12d1, 0x1001)) {
+		//huawei
+		fprintf(stderr, "HUAWEI/Option E600 detected\n");
+		return "/dev/usb/tts/0";
+	}
+	if (scanFor(0x12d1, 0x1003)) {
+		//huawei
+		fprintf(stderr, "HUAWEI/Option EC270 detected\n");
+		return "/dev/usb/tts/0";
+	}
+	if (scanFor(0x12d1, 0x1412)) {
+		//huawei
+		fprintf(stderr, "HUAWEI/Option EC168 detected\n");
+		return "/dev/usb/tts/0";
+	}
+	if (scanFor(0x1199, 0x6832)) {
+		//sierra wireless mc 8780
+		fprintf(stderr,
+			"Sierra Wireless MC 8780 detected\nreset card\n");
+		checkreset("/dev/usb/tts/2");
+		return "/dev/usb/tts/2";
+	}
+	return ttsdevice;
 }
