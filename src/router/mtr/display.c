508f0e5d0ac123099c42f49d16a415cb7b122fa2
BrainSlayer <s.gottschall@dd-wrt.com>
2009-06-03 13:30:31 +0000

    mtr added
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@12257 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/mtr/display.c
@@ -0,0 +1,244 @@
+/*
+    mtr  --  a network diagnostic tool
+    Copyright (C) 1997,1998  Matt Kimball
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include <config.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+#include "mtr.h"
+#include "display.h"
+#include "mtr-curses.h"
+#include "mtr-gtk.h"
+#include "report.h"
+#include "select.h"
+#include "raw.h"
+#include "dns.h"
+
+extern int DisplayMode;
+
+#ifdef NO_CURSES
+#define mtr_curses_open()
+#define mtr_curses_close()
+#define mtr_curses_redraw()
+#define mtr_curses_keyaction()
+#define mtr_curses_clear()
+#else
+#include "mtr-curses.h"
+#endif
+
+#ifdef NO_GTK
+#define gtk_open()
+#define gtk_close()
+#define gtk_redraw()
+#define gtk_keyaction() 0
+#define gtk_loop() {fprintf (stderr, "No GTK support. Sorry.\n"); exit (1); } 
+#else
+#include "mtr-gtk.h"
+#endif
+
+#ifdef NO_SPLIT
+#define split_open()
+#define split_close()
+#define split_redraw()
+#define split_keyaction() 0
+#else
+#include "split.h"
+#endif
+
+void display_detect(int *argc, char ***argv) {
+  DisplayMode = DisplayReport;
+
+#ifndef NO_CURSES
+  DisplayMode = DisplayCurses;
+#endif
+
+#ifndef NO_GTK
+  if(gtk_detect(argc, argv)) {
+    DisplayMode = DisplayGTK;
+  }
+#endif
+}
+
+
+void display_open(void)
+{
+  switch(DisplayMode) {
+
+  case DisplayReport:
+    report_open();
+    break;
+  case DisplayTXT:
+    txt_open();
+    break;
+  case DisplayXML:
+    xml_open();
+    break;
+  case DisplayCSV:
+    csv_open();
+    break;
+  case DisplayCurses:
+    mtr_curses_open();  
+    break;
+  case DisplaySplit:            /* BL */
+    split_open();
+    break;
+  case DisplayGTK:
+    gtk_open();
+    break;
+  }
+}
+
+
+void display_close(void)
+{
+  switch(DisplayMode) {
+  case DisplayReport:
+    report_close();
+    break;
+  case DisplayTXT:
+    txt_close();
+    break;
+  case DisplayXML:
+    xml_close();
+    break;
+  case DisplayCSV:
+    csv_close();
+    break;
+  case DisplayCurses:
+    mtr_curses_close();
+    break;
+  case DisplaySplit:            /* BL */
+    split_close();
+    break;
+  case DisplayGTK:
+    gtk_close();
+    break;
+  }
+}
+
+
+void display_redraw(void)
+{
+  switch(DisplayMode) {
+
+  case DisplayCurses:
+    mtr_curses_redraw();
+    break;
+
+  case DisplaySplit:            /* BL */
+    split_redraw();
+    break;
+
+  case DisplayGTK:
+    gtk_redraw();
+    break;
+  }
+}
+
+
+int display_keyaction(void)
+{
+  switch(DisplayMode) {
+  case DisplayCurses:
+    return mtr_curses_keyaction();
+
+  case DisplaySplit:		/* BL */
+    return split_keyaction();
+
+  case DisplayGTK:
+    return gtk_keyaction();
+  }
+  return 0;
+}
+
+
+void display_rawping(int host, int msec) 
+{
+  switch(DisplayMode) {
+  case DisplayReport:
+  case DisplayTXT:
+  case DisplayXML:
+  case DisplayCSV:
+  case DisplaySplit:            /* BL */
+  case DisplayCurses:
+  case DisplayGTK:
+    break;
+  case DisplayRaw:
+    raw_rawping (host, msec);
+    break;
+  }
+}
+
+
+void display_rawhost(int host, ip_t *ip_addr) 
+{
+  switch(DisplayMode) {
+  case DisplayReport:
+  case DisplayTXT:
+  case DisplayXML:
+  case DisplayCSV:
+  case DisplaySplit:            /* BL */
+  case DisplayCurses:
+  case DisplayGTK:
+    break;
+  case DisplayRaw:
+    raw_rawhost (host, ip_addr);
+    break;
+  }
+}
+
+
+void display_loop(void)
+{
+  switch(DisplayMode) {
+  case DisplayReport:
+  case DisplayTXT:
+  case DisplayXML:
+  case DisplayCSV:
+  case DisplaySplit:            /* BL */
+  case DisplayCurses:
+  case DisplayRaw:
+    select_loop();
+    break;
+  case DisplayGTK:
+    gtk_loop();
+    break;
+  }
+}
+
+
+void display_clear(void)
+{
+  switch(DisplayMode) {
+  case DisplayCurses:
+    mtr_curses_clear();
+    break;
+  case DisplayReport:
+  case DisplayTXT:
+  case DisplayXML:
+  case DisplayCSV:
+  case DisplaySplit:            /* BL */
+  case DisplayRaw:
+    break;
+
+  case DisplayGTK:
+    break;
+  }
+}
---
+++ src/router/mtr/dns.h
@@ -0,0 +1,35 @@
+/*
+    mtr  --  a network diagnostic tool
+    Copyright (C) 1997,1998  Matt Kimball
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include <netinet/in.h>
+
+
+/*  Prototypes for dns.c  */
+
+void dns_open(void);
+int dns_waitfd(void);
+void dns_ack(void);
+void dns_events(double *sinterval);
+char *dns_lookup(ip_t * address);
+char *dns_lookup2(ip_t * address);
+struct hostent * dns_forward(const char *name);
+char *strlongip(ip_t * ip);
+
+void addr2ip6arpa( ip_t * ip, char * buf );
+struct hostent *addr2host( const char *addr, int type );
