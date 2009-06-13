1c4094409d8a340b4a8f3ba2e386760f7ca9d746
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2009-06-13 15:20:15 +0000

    consider this eoc2610/eoc1650 and eoc5610 change as experimental, it seems to increae signal strength on some devices
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@12290 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/madwifi.c
+++ src/router/services/networking/madwifi.c
@@ -1201,15 +1201,18 @@ static void configure_single(int count)
 	int tx = 1;
 	int diva = 0;		//atoi( nvram_default_get( diversity, "0" ) );
 #elif defined(HAVE_EOC5610)
-	int rx = atoi(nvram_default_get(txantenna, "1"));
-	int tx = atoi(nvram_default_get(txantenna, "1"));
-	int diva = 0;		//atoi( nvram_default_get( diversity, "0" ) );
+//	int rx = atoi(nvram_default_get(txantenna, "1"));
+//	int tx = atoi(nvram_default_get(txantenna, "1"));
+//	int diva = 0;		//atoi( nvram_default_get( diversity, "0" ) );
+    int rx = 1;
+    int tx = 0; // fix to internal path, since both antennas use the same connector. so only the switch matters
+	int diva = 1;		//1;// atoi( nvram_default_get( diversity, "0" ) );
 #elif defined(HAVE_EOC1650) || defined(HAVE_EOC2610)
-	int rx = 2;		//atoi( nvram_default_get( txantenna, "2" ) ); // secondary antenna output is the internal antenna and should be used as default value
-	int tx = 2;		//atoi( nvram_default_get( txantenna, "2" ) );
-//    int rx = 1;
-//    int tx = 0; // fix to internal path, since both antennas use the same connector. so only the switch matters
-	int diva = 0;		//1;// atoi( nvram_default_get( diversity, "0" ) );
+//	int rx = 2;		//atoi( nvram_default_get( txantenna, "2" ) ); // secondary antenna output is the internal antenna and should be used as default value
+//	int tx = 2;		//atoi( nvram_default_get( txantenna, "2" ) );
+    int rx = 1;
+    int tx = 0; // fix to internal path, since both antennas use the same connector. so only the switch matters
+	int diva = 1;		//1;// atoi( nvram_default_get( diversity, "0" ) );
 #else
 	int rx = atoi(nvram_default_get(rxantenna, "1"));
 	int tx = atoi(nvram_default_get(txantenna, "1"));
