816dd94bcea0ad1c6be08d54c46a93df3d1a0b3e
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2009-06-29 14:32:25 +0000

    take out this set from the static ram
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@12396 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/services/bin/defaults.c
@@ -0,0 +1,29 @@
+#define STORE_DEFAULTS
+
+#include "../sysinit/defaults.c"
+
+
+int main(int argc,char *argv[])
+{
+FILE *out;
+out=fopen("defaults.bin","wb");
+int i;
+int len = sizeof(srouter_defaults)/sizeof(struct nvram_tuple);
+fwrite(&len,4,1,out);
+for (i=0;i<sizeof(srouter_defaults)/sizeof(struct nvram_tuple);i++)
+    {
+    if (srouter_defaults[i].name)
+	{
+    putc(strlen(srouter_defaults[i].name),out);
+    fwrite(srouter_defaults[i].name,strlen(srouter_defaults[i].name),1,out);
+    putc(strlen(srouter_defaults[i].value),out);
+    fwrite(srouter_defaults[i].value,strlen(srouter_defaults[i].value),1,out);
+	}else{
+	putc(0,out);
+	putc(0,out);
+	}
+	
+    }
+fclose(out);
+return 0;
+}
