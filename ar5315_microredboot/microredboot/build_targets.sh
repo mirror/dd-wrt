36217a67f8a3610a074c109257eaea0cc168b6a0
BrainSlayer <s.gottschall@dd-wrt.com>
2009-07-06 11:58:28 +0000

    current builds (reset/reboot fix for redboot)
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@12441 52c4871e-980c-0410-b1e0-e73912ce01f8

--- ar5315_microredboot/microredboot/build_targets.sh
+++ ar5315_microredboot/microredboot/build_targets.sh
@@ -9,8 +9,27 @@ make ap61 RESETBUTTON=0x06 FIS=1
 mkdir images_dir300
 cp images/*.rom images_dir300 
 
+
+rm -f images/*
+make ap61 RESETBUTTON=0x06 FIS=0 LED1_PIN=0 LED2_PIN=1 LED3_PIN=3 LED4_PIN=4
+mkdir images_ubnt
+cp images/redboot_ap61_16M_4M_kendin.rom images_ubnt/BS2_LC2.rom
+cp images/redboot_ap61_32M_8M_kendin.rom images_ubnt/PICO2.rom
+
+rm -f images/*
+make ap51 RESETBUTTON=0x06 FIS=0 LED1_PIN=0 LED2_PIN=1 LED3_PIN=3 LED4_PIN=4
+mkdir images_ubnt
+cp images/redboot_ap51_16M_4M_icplus.rom images_ubnt/LS2_PS2.rom
+cp images/redboot_ap51_16M_4M_kendin.rom images_ubnt/NS2.rom
+
+rm -f images/*
+make ap48 RESETBUTTON=0x06 FIS=0 LED1_PIN=7 LED2_PIN=5 LED3_PIN=4 LED4_PIN=3
+mkdir images_ubnt
+cp images/redboot_ap48_16M_4M_kendin.rom images_ubnt/LS5_NS5_PS5_BS5_LC5_PICO5.rom
+
 #senao like inverted gpio 5
-make ap51 RESETBUTTON=0x15 FIS=2
+rm -f images/*
+make ap51 RESETBUTTON=0x15 FIS=2  LED1_PIN=4 LED2_PIN=6 LED3_PIN=7
 mkdir images_senao
 cp images/*.rom images_senao 
 
@@ -21,7 +40,7 @@ mkdir images_wrt54g2
 cp images/*.rom images_wrt54g2
 
 rm -f images/*
-make ap48 RESETBUTTON=0x16 FIS=0
+make ap48 RESETBUTTON=0x16 FIS=0 LED1_PIN=0 LED2_PIN=2 LED3_PIN=1
 mkdir images_senao
 cp images/*.rom images_senao
 
@@ -34,4 +53,5 @@ cp images/*.rom images_default
 cp -rv images_senao /GruppenLW/releases/images
 cp -rv images_default /GruppenLW/releases/images
 cp -rv images_dir300 /GruppenLW/releases/images
+cp -rv images_ubnt /GruppenLW/releases/images
 cp -rv images_wrt54g2 /GruppenLW/releases/images
