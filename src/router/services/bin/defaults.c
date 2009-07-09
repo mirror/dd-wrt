ce209267f29c62f5f121209a6c2446c3f1b377cb
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2009-07-09 16:50:48 +0000

    formated
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@12469 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/bin/defaults.c
+++ src/router/services/bin/defaults.c
@@ -2,37 +2,36 @@
 
 #include "../sysinit/defaults.c"
 
-
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-FILE *out;
-out=fopen("defaults.bin","wb");
-int i;
-int len = sizeof(srouter_defaults)/sizeof(struct nvram_tuple);
-fwrite(&len,4,1,out);
-for (i=0;i<sizeof(srouter_defaults)/sizeof(struct nvram_tuple);i++)
-    {
-    if (srouter_defaults[i].name)
-	{
-    putc(strlen(srouter_defaults[i].name),out);
-    fwrite(srouter_defaults[i].name,strlen(srouter_defaults[i].name),1,out);
-    len =  strlen(srouter_defaults[i].value);
-    if (len>127)
-	{
-	len|=128;
-	putc(len&0xff,out);
-	putc(strlen(srouter_defaults[i].value)>>7,out);
-	}else
-	{
-	putc(len,out);	
+	FILE *out;
+	out = fopen("defaults.bin", "wb");
+	int i;
+	int len = sizeof(srouter_defaults) / sizeof(struct nvram_tuple);
+	fwrite(&len, 4, 1, out);
+	for (i = 0; i < sizeof(srouter_defaults) / sizeof(struct nvram_tuple);
+	     i++) {
+		if (srouter_defaults[i].name) {
+			putc(strlen(srouter_defaults[i].name), out);
+			fwrite(srouter_defaults[i].name,
+			       strlen(srouter_defaults[i].name), 1, out);
+			len = strlen(srouter_defaults[i].value);
+			if (len > 127) {
+				len |= 128;
+				putc(len & 0xff, out);
+				putc(strlen(srouter_defaults[i].value) >> 7,
+				     out);
+			} else {
+				putc(len, out);
+			}
+			fwrite(srouter_defaults[i].value,
+			       strlen(srouter_defaults[i].value), 1, out);
+		} else {
+			putc(0, out);
+			putc(0, out);
+		}
+
 	}
-    fwrite(srouter_defaults[i].value,strlen(srouter_defaults[i].value),1,out);
-	}else{
-	putc(0,out);
-	putc(0,out);
-	}
-	
-    }
-fclose(out);
-return 0;
+	fclose(out);
+	return 0;
 }
