e3468cbc191604cd10823ccc871bcb35da607c3f
BrainSlayer <s.gottschall@dd-wrt.com>
2009-07-14 17:04:16 +0000

    correct power offset for dlm105
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@12501 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/dd-wrt.c
+++ src/router/httpd/visuals/dd-wrt.c
@@ -4123,8 +4123,8 @@ void ej_show_wireless_single(webs_t wp, char *prefix)
 	websWrite(wp, "//]]>\n</script>\n");
 	websWrite(wp, "</select>\n");
 	websWrite(wp, "</div>\n");
-#if defined(HAVE_EOC5610)
-/*	websWrite(wp,
+/*#if defined(HAVE_EOC5610)
+	websWrite(wp,
 		  "<div class=\"setting\"><div class=\"label\"><script type=\"text/javascript\">Capture(wl_adv.label25)</script></div><select name=\"%s\" >\n",
 		  wl_txantenna);
 	websWrite(wp, "<script type=\"text/javascript\">\n//<![CDATA[\n");
@@ -4140,7 +4140,7 @@ void ej_show_wireless_single(webs_t wp, char *prefix)
 
 	websWrite(wp, "</select>\n");
 	websWrite(wp, "</div>\n");*/
-#elif ( ! defined(HAVE_TRIMAX) && defined(HAVE_EOC2610)) || defined(HAVE_PICO2) || defined(HAVE_PICO2HP) || defined(HAVE_PICO5)
+#if ( defined(HAVE_PICO2) || defined(HAVE_PICO2HP) || defined(HAVE_PICO5)
 
 /*    websWrite( wp,
 	       "<div class=\"setting\"><div class=\"label\"><script type=\"text/javascript\">Capture(wl_adv.label25)</script></div><select name=\"%s\" >\n",
