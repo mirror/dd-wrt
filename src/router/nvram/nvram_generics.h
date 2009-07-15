b5d6640ccc45d08bb739bbc890b49760db22739d
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2009-07-15 15:56:58 +0000

    format source
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@12511 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/nvram/nvram_generics.h
+++ src/router/nvram/nvram_generics.h
@@ -13,73 +13,76 @@
 } while (0)
 */
 
-void nvram_store_collection(char *name,char *buf)
+void nvram_store_collection(char *name, char *buf)
 {
-char *chain;
-char *n;
-int size=strlen(buf);
-int chaincount=size/1024;
-int c=0;
-int i,offset;
-offset=0;
-cprintf("chaincount = %d\n",chaincount);
-for (i=0;i<chaincount;i++)
-    {
-    n=malloc(strlen(name)+16);
-    sprintf(n,"%s%d",name,c++);
-    cprintf("get chain name %s\n",n);
-    chain=malloc(1025);
-    memcpy(chain,&buf[offset],1024);
-    chain[1024]=0;
-    cprintf("story chain %s\n",chain);
-    nvram_set(n,chain);
-    offset+=1024;
-    free(n);
-    free(chain);
-    }
-int rest=size%1024;
-if (rest)
-{
-n=malloc(strlen(name)+16);
-sprintf(n,"%s%d",name,c);
-cprintf("chainname = %s, malloc = %d\n",n,rest+16);
-chain=malloc(rest+1);
-memcpy(chain,&buf[offset],rest);
-chain[rest]=0;
-nvram_set(n,chain);
-cprintf("free mem\n");
-free(n);
-free(chain);
+	char *chain;
+	char *n;
+	int size = strlen(buf);
+	int chaincount = size / 1024;
+	int c = 0;
+	int i, offset;
+	offset = 0;
+	cprintf("chaincount = %d\n", chaincount);
+	for (i = 0; i < chaincount; i++) {
+		n = malloc(strlen(name) + 16);
+		sprintf(n, "%s%d", name, c++);
+		cprintf("get chain name %s\n", n);
+		chain = malloc(1025);
+		memcpy(chain, &buf[offset], 1024);
+		chain[1024] = 0;
+		cprintf("story chain %s\n", chain);
+		nvram_set(n, chain);
+		offset += 1024;
+		free(n);
+		free(chain);
+	}
+	int rest = size % 1024;
+	if (rest) {
+		n = malloc(strlen(name) + 16);
+		sprintf(n, "%s%d", name, c);
+		cprintf("chainname = %s, malloc = %d\n", n, rest + 16);
+		chain = malloc(rest + 1);
+		memcpy(chain, &buf[offset], rest);
+		chain[rest] = 0;
+		nvram_set(n, chain);
+		cprintf("free mem\n");
+		free(n);
+		free(chain);
+	}
 }
-}
+
 /*
     do not forget to free the returned result
 */
 char *nvram_get_collection(char *name)
 {
-char *chains=NULL;
-int offset=0;
-int c=0;
-char n[65];
+	char *chains = NULL;
+	int offset = 0;
+	int c = 0;
+	char n[65];
 
-sprintf(n,"%s%d",name,c++);
-cprintf("name = %s\n",n);
-while(nvram_get(n)!=NULL)
-    {
-    char *chain=nvram_get(n);
-    cprintf("chain = %s\n",chain);
-    if (chains==NULL)
-	chains=malloc(strlen(chain)+1);
-    chains=(char*)realloc(chains,chains!=NULL?strlen(chains)+strlen(chain)+1:strlen(chain)+1);
-    cprintf("alloc okay\n");
-    memcpy(&chains[offset],chain,strlen(chain));
-    offset+=strlen(chain);
-    chains[offset]=0;
-    cprintf("copy %s\n",chains);
-    sprintf(n,"%s%d",name,c++);
-    }
-return chains;
+	sprintf(n, "%s%d", name, c++);
+	cprintf("name = %s\n", n);
+	while (nvram_get(n) != NULL) {
+		char *chain = nvram_get(n);
+		cprintf("chain = %s\n", chain);
+		if (chains == NULL)
+			chains = malloc(strlen(chain) + 1);
+		chains =
+		    (char *)realloc(chains,
+				    chains !=
+				    NULL ? strlen(chains) + strlen(chain) +
+				    1 : strlen(chain) + 1);
+		cprintf("alloc okay\n");
+		memcpy(&chains[offset], chain, strlen(chain));
+		offset += strlen(chain);
+		chains[offset] = 0;
+		cprintf("copy %s\n", chains);
+		sprintf(n, "%s%d", name, c++);
+	}
+	return chains;
 }
+
 /*
  * Match an NVRAM variable.
  * @param	name	name of variable to match
@@ -87,8 +90,8 @@ return chains;
  * @return	TRUE if variable is defined and its value is string equal
  *		to match or FALSE otherwise
  */
-int
-nvram_match(char *name, char *match) {
+int nvram_match(char *name, char *match)
+{
 	const char *value = nvram_get(name);
 	return (value && !strcmp(value, match));
 }
@@ -100,108 +103,110 @@ nvram_match(char *name, char *match) {
  * @return	TRUE if variable is defined and its value is not string
  *		equal to invmatch or FALSE otherwise
  */
-int
-nvram_invmatch(char *name, char *invmatch) {
+int nvram_invmatch(char *name, char *invmatch)
+{
 	const char *value = nvram_get(name);
 	return (value && strcmp(value, invmatch));
 }
 
-
 char *nvram_prefix_get(const char *name, const char *prefix)
 {
-char p[64];
-sprintf(p,"%s_%s",prefix,name);
-return nvram_safe_get(p);
-}
-int nvram_prefix_match(const char *name, const char *prefix,char *match)
-{
-char p[64];
-sprintf(p,"%s_%s",prefix,name);
-return nvram_match(p,match);
+	char p[64];
+	sprintf(p, "%s_%s", prefix, name);
+	return nvram_safe_get(p);
 }
 
-int nvram_nmatch(char *match,const char *fmt,...)
+int nvram_prefix_match(const char *name, const char *prefix, char *match)
 {
-char varbuf[64];
-va_list args;
-va_start(args, (char*)fmt);
-vsnprintf(varbuf, sizeof(varbuf), fmt, args);
-va_end(args);
-return nvram_match(varbuf,match);
+	char p[64];
+	sprintf(p, "%s_%s", prefix, name);
+	return nvram_match(p, match);
 }
 
-char *nvram_nget(const char *fmt,...)
+int nvram_nmatch(char *match, const char *fmt, ...)
 {
-char varbuf[64];
-va_list args;
-va_start(args, (char*)fmt);
-vsnprintf(varbuf, sizeof(varbuf), fmt, args);
-va_end(args);
-return nvram_safe_get(varbuf);
+	char varbuf[64];
+	va_list args;
+	va_start(args, (char *)fmt);
+	vsnprintf(varbuf, sizeof(varbuf), fmt, args);
+	va_end(args);
+	return nvram_match(varbuf, match);
 }
-char *nvram_nset(char *value,const char *fmt,...)
+
+char *nvram_nget(const char *fmt, ...)
 {
-char varbuf[64];
-va_list args;
-va_start(args, (char*)fmt);
-vsnprintf(varbuf, sizeof(varbuf), fmt, args);
-va_end(args);
-return nvram_set(varbuf,value);
+	char varbuf[64];
+	va_list args;
+	va_start(args, (char *)fmt);
+	vsnprintf(varbuf, sizeof(varbuf), fmt, args);
+	va_end(args);
+	return nvram_safe_get(varbuf);
+}
+
+char *nvram_nset(char *value, const char *fmt, ...)
+{
+	char varbuf[64];
+	va_list args;
+	va_start(args, (char *)fmt);
+	vsnprintf(varbuf, sizeof(varbuf), fmt, args);
+	va_end(args);
+	return nvram_set(varbuf, value);
 }
 
 char *nvram_safe_get(const char *name)
 {
-return nvram_get(name)?:"";
-} 
+	return nvram_get(name) ? : "";
+}
 
 void nvram_safe_unset(const char *name)
 {
-if(nvram_get(name)) 
-	nvram_unset(name); 
+	if (nvram_get(name))
+		nvram_unset(name);
 }
 
 void nvram_safe_set(const char *name, char *value)
 {
-if(!nvram_get(name) || strcmp(nvram_safe_get(name), value))
-	nvram_set(name, value); 
-}
-int nvram_default_match (char *var, char *match, char *def)
-{
-  char *v = nvram_get (var);
-  if (v == NULL || strlen (v) == 0)
-    {
-      nvram_set (var, def);
-      return !strcmp(match,def);
-    }
-  return nvram_match (var, match);
-}
-char *nvram_default_get (char *var, char *def)
-{
-  char *v = nvram_get (var);
-  if (v == NULL || strlen (v) == 0)
-    {
-      nvram_set (var, def);
-      return def;
-    }
-  return nvram_safe_get (var);
+	if (!nvram_get(name) || strcmp(nvram_safe_get(name), value))
+		nvram_set(name, value);
 }
 
-void fwritenvram(char *var,FILE *fp)
+int nvram_default_match(char *var, char *match, char *def)
 {
-int i;
-if (fp==NULL)
-    return;
-      char *host_key = nvram_safe_get (var);
-      int len = strlen(host_key);
-      for (i=0;i<len;i++)
-     	  if (host_key[i] != 0x0D)
-	    fprintf (fp, "%c", host_key[i]);
+	char *v = nvram_get(var);
+	if (v == NULL || strlen(v) == 0) {
+		nvram_set(var, def);
+		return !strcmp(match, def);
+	}
+	return nvram_match(var, match);
 }
-void writenvram(char *var,char *file)
+
+char *nvram_default_get(char *var, char *def)
 {
-FILE *fp=fopen(file,"wb");
-if (fp==NULL)
-    return;
-fwritenvram(var,fp);
-fclose(fp);
+	char *v = nvram_get(var);
+	if (v == NULL || strlen(v) == 0) {
+		nvram_set(var, def);
+		return def;
+	}
+	return nvram_safe_get(var);
+}
+
+void fwritenvram(char *var, FILE * fp)
+{
+	int i;
+	if (fp == NULL)
+		return;
+	char *host_key = nvram_safe_get(var);
+	int len = strlen(host_key);
+	for (i = 0; i < len; i++)
+		if (host_key[i] != 0x0D)
+			fprintf(fp, "%c", host_key[i]);
+}
+
+void writenvram(char *var, char *file)
+{
+	FILE *fp = fopen(file, "wb");
+	if (fp == NULL)
+		return;
+	fwritenvram(var, fp);
+	fclose(fp);
 }
