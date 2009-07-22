c43bde89ddfcb987b4712ebee317dac80280b719
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2009-07-22 14:55:20 +0000

    detect fis directory partition
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@12551 52c4871e-980c-0410-b1e0-e73912ce01f8

--- ar5315_microredboot/microredboot/boot/src/lib/lib.c
+++ ar5315_microredboot/microredboot/boot/src/lib/lib.c
@@ -25,29 +25,22 @@ size_t strlen(const char *s)
 	return sc - s;
 }
 
-void *memmove( void *s1, const void *s2, size_t n )
+void *memmove(void *s1, const void *s2, size_t n)
 {
-    char *dst = (char *)s1;
-    const char *src = (const char *)s2;
-    if ((src < dst) && (dst < (src + n)))
-    {
-        // Have to copy backwards
-        src += n;
-        dst += n;
-        while (n--)
-        {
-            *--dst = *--src;
-        }
-    }
-    else
-    {
-        while (n--)
-        {
-            *dst++ = *src++;
-        }
-    }
+	char *dst = (char *)s1;
+	const char *src = (const char *)s2;
+	if ((src < dst) && (dst < (src + n))) {
+		// Have to copy backwards
+		src += n;
+		dst += n;
+		while (n--) {
+			*--dst = *--src;
+		}
+	} else {
+		while (n--) {
+			*dst++ = *src++;
+		}
+	}
 
-    
-    return s1;
-} // __memmove()
-
+	return s1;
+}				// __memmove()
