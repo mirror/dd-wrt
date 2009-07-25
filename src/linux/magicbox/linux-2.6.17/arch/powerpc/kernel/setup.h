9e2df16ee302c4829ab28838cae8c8c1716bf8af
BrainSlayer <s.gottschall@dd-wrt.com>
2009-07-25 16:46:29 +0000

    remove obsolete kernels to reduce repository size
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@12565 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/magicbox/linux-2.6.17/arch/powerpc/kernel/setup.h
+++
@@ -1,6 +0,0 @@
-#ifndef _POWERPC_KERNEL_SETUP_H
-#define _POWERPC_KERNEL_SETUP_H
-
-void check_for_initrd(void);
-
-#endif /* _POWERPC_KERNEL_SETUP_H */
--- src/linux/magicbox/linux-2.6.19/arch/powerpc/platforms/pasemi/pasemi.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PASEMI_PASEMI_H
-#define _PASEMI_PASEMI_H
-
-extern unsigned long pas_get_boot_time(void);
-extern void pas_pci_init(void);
-extern void pas_pcibios_fixup(void);
-
-#endif /* _PASEMI_PASEMI_H */
