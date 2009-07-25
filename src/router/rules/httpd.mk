9ae86f9f7d43b61f2832faae7bf530aad403fd9e
BrainSlayer <s.gottschall@dd-wrt.com>
2009-07-25 14:08:38 +0000

    private svn
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@12559 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/httpd.mk
+++ src/router/rules/httpd.mk
@@ -1,3 +1,11 @@
+httpd-checkout:
+	rm -rf $(TOP)/register
+	svn co svn://svn.dd-wrt.com/private/register $(TOP)/register
+
+httpd-update:
+	svn update $(TOP)/register
+
+
 httpd: nvram shared hsiab matrixssl www wireless-tools
 #	$(MAKE) -C httpd/axTLS
 #	$(MAKE) www
