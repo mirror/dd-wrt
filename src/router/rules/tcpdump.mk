dc677de477eb0f04dce8274bb5fc02273c562ba4
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2009-08-06 15:25:57 +0000

    consider dependencies
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@12619 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/tcpdump.mk
+++ src/router/rules/tcpdump.mk
@@ -1,4 +1,4 @@
-tcpdump-configure:
+tcpdump-configure: libpcap
 	cd tcpdump && ./configure --host=$(ARCH)-linux --enable-shared --disable-static --without-crypto CC=$(ARCH)-linux-uclibc-gcc ac_cv_linux_vers=2 CPPFLAGS="-I../libpcap_noring $(COPTS) -DNEED_PRINTF" CFLAGS="-I../libpcap_noring $(COPTS) -DNEED_PRINTF" LDFLAGS="-L../libpcap_noring"
 
 tcpdump:
