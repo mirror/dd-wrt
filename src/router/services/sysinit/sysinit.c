edd4f06606efbe709a202dfc7208000ef0cd2889
BrainSlayer <s.gottschall@dd-wrt.com>
2009-08-27 11:38:33 +0000

    more on WHRG300NH
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@12737 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit.c
+++ src/router/services/sysinit/sysinit.c
@@ -392,6 +392,16 @@ void start_restore_defaults(void)
 		{"wan_default", "eth0", 0},
 		{0, 0, 0}
 	};
+#elif HAVE_AP83
+	struct nvram_tuple generic[] = {
+		{"lan_ifname", "br0", 0},
+		{"lan_ifnames", "eth0 ath0", 0},
+		{"wan_ifname", "eth1", 0},
+		{"wan_ifname2", "eth1", 0},
+		{"wan_ifnames", "eth1", 0},
+		{"wan_default", "eth1", 0},
+		{0, 0, 0}
+	};
 #elif HAVE_LSX
 	struct nvram_tuple generic[] = {
 		{"lan_ifname", "br0", 0},
