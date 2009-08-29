47fb682f4fa097328d670a10d49a3669623df4f3
BrainSlayer <s.gottschall@dd-wrt.com>
2009-08-29 12:55:56 +0000

    to be replaced
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@12751 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/freeradius/src/modules/rlm_example/other.h
+++
@@ -1,8 +0,0 @@
-#ifndef _OTHER_H
-#define _OTHER_H
-
-/* define the function */
-
-void other_function(void);
-
-#endif /*_OTHER_H*/
--- src/router/freeradius/src/tests/eapsim-02/check.gdb
+++
@@ -1,3 +0,0 @@
-# $Id: check.gdb,v 1.1 2003/10/31 22:36:47 mcr Exp $
-file ./eapsimlibtest 
-set args <eapsim-in.txt
