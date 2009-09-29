0d07f7335a6a339395588fa325024d178a689777
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2009-09-29 10:19:57 +0000

    safer
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@12995 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/brcm/linux-2.6.23/arch/mips/brcm-boards/bcm947xx/setup.c
+++ src/linux/brcm/linux-2.6.23/arch/mips/brcm-boards/bcm947xx/setup.c
@@ -177,8 +177,8 @@ char *boothwmodel = nvram_get("boot_hw_model");
 char *boothwver = nvram_get("boot_hw_ver");
 char *cardbus = nvram_get("cardbus");
 char *boardrev = nvram_get("boardrev");
-if (boardrev!=NULL && (!strcmp(boardrev,"0x1213") || !strcmp(boardrev,"02")) && !strcmp(boardtype,"0x04CF"))iswnr3500v2=1;
-if (boardrev!=NULL && (!strcmp(boardrev,"0x1304") || !strcmp(boardrev,"0x1305")) && !strcmp(boardtype,"0x04EF"))iswrt320n=1;
+if (boardrev!=NULL && (!strcmp(boardrev,"0x1213") || !strcmp(boardrev,"02")) && boardtype!=NULL && !strcmp(boardtype,"0x04CF"))iswnr3500v2=1;
+if (boardrev!=NULL && (!strcmp(boardrev,"0x1304") || !strcmp(boardrev,"0x1305")) && boardtype!=NULL && !strcmp(boardtype,"0x04EF"))iswrt320n=1;
 if (boardtype==NULL || strcmp(boardtype,"0x478"))iswrt350n=0;
 if (cardbus!=NULL && !strcmp(cardbus, "0") && boardtype!=NULL && !strcmp(boardtype,"0x478"))
     {
