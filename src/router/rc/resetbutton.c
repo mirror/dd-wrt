8edc82ebb95694642fc59ba2984e492e2a6a27fd
BrainSlayer <s.gottschall@dd-wrt.com>
2009-10-12 17:24:21 +0000

    reset button support for EAP9550
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@13072 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/resetbutton.c
+++ src/router/rc/resetbutton.c
@@ -386,7 +386,7 @@ void period_check(int sig)
 	} else if (brand == ROUTER_BOARD_ESR6650) {
 		val = get_gpio(10) << 10;
 	} else if (brand == ROUTER_BOARD_EAP9550) {
-		val = get_gpio(10) << 10;
+		val = get_gpio(0);
 	} else if (brand == ROUTER_BOARD_ESR9752) {
 		val = get_gpio(0);
 	} else if (brand == ROUTER_BOARD_AR670W) {
