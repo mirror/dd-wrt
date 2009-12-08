2f32cef122e240ce75993ee3c75d50c02ce1c994
BrainSlayer <s.gottschall@dd-wrt.com>
2009-12-08 17:30:55 +0000

    more modules
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@13414 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/services/services/redial.c
@@ -0,0 +1,90 @@
+/*
+ * redial.c
+ *
+ * Copyright (C) 2009 Sebastian Gottschall <gottschall@dd-wrt.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id:
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>		/* AhMan March 18 2005 */
+#include <sys/socket.h>
+#include <sys/mount.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <wait.h>
+#include <net/route.h>		/* AhMan March 18 2005 */
+#include <sys/types.h>
+#include <signal.h>
+
+#include <bcmnvram.h>
+#include <bcmconfig.h>
+#include <netconf.h>
+#include <shutils.h>
+#include <utils.h>
+#include <cy_conf.h>
+#include <code_pattern.h>
+#include <rc.h>
+#include <wlutils.h>
+#include <nvparse.h>
+#include <syslog.h>
+#include <services.h>
+
+void stop_redial(void)
+{
+
+	if (pidof("redial") > 0) {
+		dd_syslog(LOG_INFO,
+			  "ppp_redial : redial daemon successfully stopped\n");
+		// ret = killps("redial","-9");
+		killall("redial", SIGKILL);
+	}
+	cprintf("done\n");
+	return;
+}
+
+void start_redial(void)
+{
+	int ret;
+	pid_t pid;
+	char *redial_argv[] = { "/tmp/ppp/redial",
+		nvram_safe_get("ppp_redialperiod"),
+		NULL
+	};
+	if (pidof("redial") > 0) {
+		return;		// not required, already running
+	}
+
+	symlink("/sbin/rc", "/tmp/ppp/redial");
+
+	ret = _evalpid(redial_argv, NULL, 0, &pid);
+	dd_syslog(LOG_INFO,
+		  "ppp_redial : redial process successfully started\n");
+
+	cprintf("done\n");
+	return;
+}
