2cc8d68d4e91d4ee6c1a3d42ff2ef283a310b9a5
BrainSlayer <s.gottschall@dd-wrt.com>
2009-12-09 02:39:45 +0000

    and one again more
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@13422 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/services/services/radiotimer.c
@@ -0,0 +1,94 @@
+/*
+ * radiotimer.c
+ *
+ * Copyright (C) 2009 Sebastian Gottschall <gottschall@dd-wrt.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id:
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>		/* AhMan March 18 2005 */
+#include <sys/socket.h>
+#include <sys/mount.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <wait.h>
+#include <net/route.h>		/* AhMan March 18 2005 */
+#include <sys/types.h>
+#include <signal.h>
+
+#include <bcmnvram.h>
+#include <bcmconfig.h>
+#include <netconf.h>
+#include <shutils.h>
+#include <utils.h>
+#include <cy_conf.h>
+#include <code_pattern.h>
+#include <rc.h>
+#include <wlutils.h>
+#include <nvparse.h>
+#include <syslog.h>
+#include <services.h>
+
+void start_radio_timer(void)
+{
+	if (nvram_match("radio0_timer_enable", "0")
+	    && nvram_match("radio1_timer_enable", "0"))
+		return;
+#ifdef HAVE_MADWIFI
+	if (nvram_match("ath0_net_mode", "disabled"))
+#else
+	if (nvram_match("wl0_net_mode", "disabled")
+	    && nvram_match("wl1_net_mode", "disabled"))
+#endif
+		return;
+
+	pid_t pid;
+
+	char *argv[] = { "radio_timer", NULL };
+	int ret = _evalpid(argv, NULL, 0, &pid);
+
+	dd_syslog(LOG_INFO,
+		  "radio_timer : radio timer daemon successfully started\n");
+
+	cprintf("done");
+
+	return;
+}
+
+void stop_radio_timer(void)
+{
+
+	if (pidof("radio_timer") > 0) {
+		dd_syslog(LOG_INFO,
+			  "radio_timer : radio timer daemon successfully stopped\n");
+		killall("radio_timer", SIGKILL);
+	}
+	cprintf("done\n");
+
+	return;
+}
