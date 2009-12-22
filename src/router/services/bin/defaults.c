33473a55a0742cbff37f8ebb0ca168f19fa5df18
BrainSlayer <s.gottschall@dd-wrt.com>
2009-12-22 11:02:44 +0000

    add license
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@13500 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/bin/defaults.c
+++ src/router/services/bin/defaults.c
@@ -1,3 +1,27 @@
+/*
+ * defaults.c
+ *
+ * Copyright (C) 2009 Sebastian Gottschall <gottschall@dd-wrt.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * small helper utility to generate a defaults database file in /etc
+ * $Id:
+ */
+
+
 #define STORE_DEFAULTS
 
 #include "../sysinit/defaults.c"
