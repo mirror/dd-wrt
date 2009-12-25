d1e21c13804e8c80cc98c61eb8f9d79c48465927
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2009-12-25 06:03:39 +0000

    Dlink DIR-825 support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@13516 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/services/sysinit/sysinit-dir825.c
@@ -0,0 +1,179 @@
+/*
+ * sysinit-pb42.c
+ *
+ * Copyright (C) 2006 Sebastian Gottschall <gottschall@dd-wrt.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id:
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <time.h>
+#include <unistd.h>
+#include <errno.h>
+#include <syslog.h>
+#include <signal.h>
+#include <string.h>
+#include <termios.h>
+#include <sys/klog.h>
+#include <sys/types.h>
+#include <sys/mount.h>
+#include <sys/reboot.h>
+#include <sys/stat.h>
+#include <sys/sysmacros.h>
+#include <sys/time.h>
+#include <sys/utsname.h>
+#include <sys/wait.h>
+#include <sys/ioctl.h>
+
+#include <bcmnvram.h>
+#include <shutils.h>
+#include <utils.h>
+#include <cymac.h>
+#define SIOCGMIIREG	0x8948	/* Read MII PHY register.  */
+#define SIOCSMIIREG	0x8949	/* Write MII PHY register.  */
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <linux/if.h>
+#include <linux/sockios.h>
+#include <linux/mii.h>
+#include "devices/wireless.c"
+
+void start_sysinit(void)
+{
+	struct utsname name;
+	time_t tm = 0;
+
+	unlink("/etc/nvram/.lock");
+	cprintf("sysinit() proc\n");
+	/*
+	 * /proc 
+	 */
+	mount("proc", "/proc", "proc", MS_MGC_VAL, NULL);
+	mount("sysfs", "/sys", "sysfs", MS_MGC_VAL, NULL);
+	cprintf("sysinit() tmp\n");
+
+	/*
+	 * /tmp 
+	 */
+	mount("ramfs", "/tmp", "ramfs", MS_MGC_VAL, NULL);
+	// fix for linux kernel 2.6
+	mount("devpts", "/dev/pts", "devpts", MS_MGC_VAL, NULL);
+	mount("devpts", "/proc/bus/usb", "usbfs", MS_MGC_VAL, NULL);
+	eval("mkdir", "/tmp/www");
+	eval("mknod", "/dev/nvram", "c", "229", "0");
+	eval("mknod", "/dev/ppp", "c", "108", "0");
+
+	unlink("/tmp/nvram/.lock");
+	eval("mkdir", "/tmp/nvram");
+
+	/*
+	 * /var 
+	 */
+	mkdir("/tmp/var", 0777);
+	mkdir("/var/lock", 0777);
+	mkdir("/var/log", 0777);
+	mkdir("/var/run", 0777);
+	mkdir("/var/tmp", 0777);
+	cprintf("sysinit() setup console\n");
+	if (!nvram_match("disable_watchdog", "1"))
+		eval("watchdog");
+	/*
+	 * Setup console 
+	 */
+
+	cprintf("sysinit() klogctl\n");
+	klogctl(8, NULL, atoi(nvram_safe_get("console_loglevel")));
+	cprintf("sysinit() get router\n");
+
+	/*
+	 * Modules 
+	 */
+	uname(&name);
+
+	/*
+	 * network drivers 
+	 */
+	fprintf(stderr, "load ATH Ethernet Driver\n");
+	insmod("ag7100_mod");
+	eval("ifconfig", "eth0", "hw", "ether", "00:11:22:33:44:55");
+	eval("ifconfig", "eth1", "hw", "ether", "00:11:22:33:44:66");
+
+
+	eval("ifconfig", "eth0", "up");
+	eval("ifconfig", "eth1", "up");
+	struct ifreq ifr;
+	int s;
+
+	if ((s = socket(AF_INET, SOCK_RAW, IPPROTO_RAW))) {
+		char eabuf[32];
+
+		strncpy(ifr.ifr_name, "eth0", IFNAMSIZ);
+		ioctl(s, SIOCGIFHWADDR, &ifr);
+		nvram_set("et0macaddr",
+			  ether_etoa((unsigned char *)ifr.ifr_hwaddr.sa_data,
+				     eabuf));
+		nvram_set("et0macaddr_safe",
+			  ether_etoa((unsigned char *)ifr.ifr_hwaddr.sa_data,
+				     eabuf));
+		close(s);
+	}
+	detect_wireless_devices();
+
+/*#ifdef HAVE_RS
+	system2("echo 2 >/proc/sys/dev/wifi0/ledpin");
+	system2("echo 1 >/proc/sys/dev/wifi0/softled");
+	system2("echo 2 >/proc/sys/dev/wifi1/ledpin");
+	system2("echo 1 >/proc/sys/dev/wifi1/softled");
+	system2("echo 2 >/proc/sys/dev/wifi2/ledpin");
+	system2("echo 1 >/proc/sys/dev/wifi2/softled");
+#else
+	system2("echo 2 >/proc/sys/dev/wifi0/ledpin");
+	system2("echo 1 >/proc/sys/dev/wifi0/softled");
+#endif*/
+	insmod("ipv6");
+
+
+	/*
+	 * Set a sane date 
+	 */
+	stime(&tm);
+	nvram_set("wl0_ifname", "ath0");
+
+	return;
+	cprintf("done\n");
+}
+
+int check_cfe_nv(void)
+{
+	nvram_set("portprio_support", "0");
+	return 0;
+}
+
+int check_pmon_nv(void)
+{
+	return 0;
+}
+
+void start_overclocking(void)
+{
+}
+
+char *enable_dtag_vlan(int enable)
+{
+	return "eth0";
+}
