8051539454bcc11ca05ee5a5be7ccd19349ba7ac
BrainSlayer <s.gottschall@dd-wrt.com>
2010-01-29 21:40:10 +0000

    radio turnoff/on
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@13769 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/radiofunctions.c
+++ src/router/services/services/radiofunctions.c
@@ -1,13 +1,33 @@
 
-extern void radio_off(void);
-extern void radio_on(void);
+extern void radio_off(int idx);
+extern void radio_on(int idx);
 
 void start_radio_off(void)
 {
-	radio_off();
+	radio_off(-1);
 }
 
 void start_radio_on(void)
 {
-	radio_on();
+	radio_on(-1);
+}
+
+void start_radio_off_0(void)
+{
+	radio_off(0);
+}
+
+void start_radio_on_0(void)
+{
+	radio_on(0);
+}
+
+void start_radio_off_1(void)
+{
+	radio_off(1);
+}
+
+void start_radio_on_1(void)
+{
+	radio_on(1);
 }
