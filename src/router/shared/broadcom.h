d148140c5c094ceb77e44ba3bc9e30235f7ad597
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-01-30 07:54:42 +0000

    show default tx power
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@13779 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/broadcom.h
+++ src/router/shared/broadcom.h
@@ -457,6 +457,7 @@ extern void ej_get_qosmacs(webs_t wp, int argc, char_t ** argv);
 extern void ej_get_qossvcs2(webs_t wp, int argc, char_t ** argv);
 extern void ej_get_qosips2(webs_t wp, int argc, char_t ** argv);
 extern void ej_get_qosmacs2(webs_t wp, int argc, char_t ** argv);
+extern void ej_show_defwpower(webs_t wp, int argc, char_t ** argv);
 
 // extern void ej_get_services_options (webs_t wp, int argc, char_t ** argv);
 extern void ej_show_infopage(webs_t wp, int argc, char_t ** argv);
