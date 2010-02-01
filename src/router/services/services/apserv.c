bda1921c56f95821b05a77634d615816fbda4ff3
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-02-01 16:06:43 +0000

    ap serv implementation (test)
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@13795 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/services/services/apserv.c
@@ -0,0 +1,64 @@
+/*
+ * upnp.c
+ *
+ * Copyright (C) 2007 Sebastian Gottschall <gottschall@dd-wrt.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id:
+ */
+#ifdef HAVE_AP_SERV
+#include <stdlib.h>
+#include <bcmnvram.h>
+#include <shutils.h>
+#include <utils.h>
+#include <syslog.h>
+#include <signal.h>
+
+void stop_apserv(void);
+
+void start_apserv(void)
+{
+	char *wan_ifname = get_wan_face();
+	int ret;
+
+	if (nvram_match("apserv_enable", "0")) {
+		stop_apserv();
+		return;
+	}
+	/*
+	 * Make sure its not running first 
+	 */
+	ret = killall("ap_serv", SIGUSR1);
+	ret = eval("ap_serv");
+	dd_syslog(LOG_INFO,"ap_serv : ap_serv daemon successfully started\n");
+	cprintf("done\n");
+	return;
+}
+
+void stop_apserv(void)
+{
+	if (pidof("ap_serv") > 0) {
+		dd_syslog(LOG_INFO,
+			  "ap_serv : ap_serv daemon successfully stopped\n");
+		killall("ap_serv", SIGUSR1);
+		killall("ap_serv", SIGTERM);
+		cprintf("done\n");
+	}
+	return;
+}
+
+
+#endif
