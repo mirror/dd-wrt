db2a206dd19aa4516dcebb74a139348f171a104f
BrainSlayer <s.gottschall@dd-wrt.com>
2010-02-02 01:13:34 +0000

    typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@13805 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/ap-serv.mk
+++ src/router/rules/ap-serv.mk
@@ -14,6 +14,6 @@ ap-serv-clean:
 	@true
 
 ap-serv-install:
-	install -D ap-serv/ap_serv-$(ARCH)-(ARCHITECTURE) $(INSTALLDIR)/ap-serv/usr/sbin/ap_serv
+	install -D ap-serv/ap_serv-$(ARCH)-$(ARCHITECTURE) $(INSTALLDIR)/ap-serv/usr/sbin/ap_serv
 	install -D ap-serv/config/apserv.webservices $(INSTALLDIR)/ap-serv/etc/config/apserv.webservices
 	install -D ap-serv/config/apserv.nvramconfig $(INSTALLDIR)/ap-serv/etc/config/apserv.nvramconfig
