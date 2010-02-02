cf8a6ccfd34073139442eb54d5b63c4bb1b5540f
BrainSlayer <s.gottschall@dd-wrt.com>
2010-02-02 00:51:14 +0000

    todo entries
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@13799 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rules/aoss.mk
@@ -0,0 +1,17 @@
+aoss-checkout:
+	rm -rf $(TOP)/aoss
+	svn co svn://svn.dd-wrt.com/private/aoss $(TOP)/aoss
+
+aoss-update:
+	svn update $(TOP)/aoss
+
+
+aoss: 
+	if test -e "aoss/Makefile"; then make -C aoss; fi
+	@true
+aoss-clean:
+	if test -e "aoss/Makefile"; then make -C aoss clean; fi
+	@true
+
+aoss-install:
+	@true
