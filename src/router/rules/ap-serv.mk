78b4a525ba0b0ec6d746c1f7de684bfbf023a012
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-02-02 01:00:06 +0000

    todo entries
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@13804 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/ap-serv.mk
+++ src/router/rules/ap-serv.mk
@@ -1,19 +1,19 @@
 ap-serv-checkout:
-	rm -rf $(TOP)/ap-serv
-	svn co svn://svn.dd-wrt.com/private/ap_serv $(TOP)/ap-serv
+	rm -rf $(TOP)/ap-serv/src
+	svn co svn://svn.dd-wrt.com/private/ap_serv/src $(TOP)/ap-serv/src
 
 ap-serv-update:
-	svn update $(TOP)/ap-serv
+	svn update $(TOP)/ap-serv/src
 
 
 ap-serv: 
-	if test -e "ap-serv/Makefile"; then make -C ap-serv; fi
+	if test -e "ap-serv/src/Makefile"; then make -C ap-serv/src; fi
 	@true
 ap-serv-clean:
-	if test -e "ap-serv/Makefile"; then make -C ap-serv clean; fi
+	if test -e "ap-serv/src/Makefile"; then make -C ap-serv/src clean; fi
 	@true
 
 ap-serv-install:
-	install -D ap-serv/src/ap_serv $(INSTALLDIR)/ap-serv/usr/sbin/ap_serv
+	install -D ap-serv/ap_serv-$(ARCH)-(ARCHITECTURE) $(INSTALLDIR)/ap-serv/usr/sbin/ap_serv
 	install -D ap-serv/config/apserv.webservices $(INSTALLDIR)/ap-serv/etc/config/apserv.webservices
 	install -D ap-serv/config/apserv.nvramconfig $(INSTALLDIR)/ap-serv/etc/config/apserv.nvramconfig
