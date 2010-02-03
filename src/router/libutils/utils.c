6e2efc0d843fa0499b82b2b56bc2c6e05ca1229b
BrainSlayer <s.gottschall@dd-wrt.com>
2010-02-03 10:11:52 +0000

    trendnet TEW-652BRP v1.0R support (identical to TEW-632BRP)
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@13828 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/utils.c
+++ src/router/libutils/utils.c
@@ -999,6 +999,9 @@ int internal_getRouterBrand()
 #elif HAVE_DIR825
 	setRouter("Dlink DIR-825");
 	return ROUTER_BOARD_PB42;
+#elif HAVE_TEW652BRP
+	setRouter("Trendnet TEW-652BRP");
+	return ROUTER_BOARD_PB42;
 #elif HAVE_TEW632BRP
 	setRouter("Trendnet TEW-632BRP");
 	return ROUTER_BOARD_PB42;
