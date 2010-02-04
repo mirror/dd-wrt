5f3d4a6a4e5197532eb22ea86354ead85fabb800
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-02-04 14:24:35 +0000

    assign different wan mac
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@13843 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-ap83.c
+++ src/router/services/sysinit/sysinit-ap83.c
@@ -169,8 +169,10 @@ void start_sysinit(void)
 			copy[4], copy[5]);
 		fprintf(stderr, "configure eth0 to %s\n", mac);
 		eval("ifconfig", "eth0", "hw", "ether", mac);
+		MAC_ADD(mac);
 		fprintf(stderr, "configure eth1 to %s\n", mac);
 		eval("ifconfig", "eth1", "hw", "ether", mac);
+		MAC_SUB(mac);
 	}
 #endif
 #ifdef HAVE_TEW632BRP
