c8bbe87969ba9f8894fd917105763ea042e20db3
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-02-04 00:48:44 +0000

    adjust chainmasks
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@13833 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/utils.c
+++ src/router/libutils/utils.c
@@ -894,8 +894,8 @@ int internal_getRouterBrand()
 		{"Ubiquiti AP 1000N", 0xe402, "3", "3", ROUTER_BOARD_R2M},	//
 		{"Ubiquiti Simple AP", 0xe4a2, "3", "3", ROUTER_BOARD_R2M},	//
 		{"Ubiquiti PowerBridge M5", 0xe1a5, "3", "3", ROUTER_BOARD_R5M},	//
-		{"Ubiquiti NanoBridge M5", 0xe235, "1", "1", ROUTER_BOARD_BS5M},	//
-		{"Ubiquiti NanoBridge M2", 0xe232, "1", "1", ROUTER_BOARD_BS2M},	//
+		{"Ubiquiti NanoBridge M5", 0xe235, "3", "3", ROUTER_BOARD_BS5M},	//
+		{"Ubiquiti NanoBridge M2", 0xe232, "3", "3", ROUTER_BOARD_BS2M},	//
 		{"Ubiquiti Rocket M3", 0xe1b3, "3", "3", ROUTER_BOARD_R5M},	//
 		{"Ubiquiti Rocket M900", 0xe1b9, "3", "3", ROUTER_BOARD_R2M},	//
 		{NULL, 0, NULL, NULL, 0},	//
