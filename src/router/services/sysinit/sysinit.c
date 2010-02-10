aa0f68f5608687bf35555014e89360671e667e54
BrainSlayer <s.gottschall@dd-wrt.com>
2010-02-10 00:47:51 +0000

    typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@13877 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit.c
+++ src/router/services/sysinit/sysinit.c
@@ -592,7 +592,7 @@ void start_restore_defaults(void)
 #elif HAVE_DIR615E
 	struct nvram_tuple generic[] = {
 		{"lan_ifname", "br0", 0},
-		{"lan_ifnames", "eth1 eth1 ath0", 0},
+		{"lan_ifnames", "eth0 eth1 ath0", 0},
 		{"wan_ifname", "", 0},
 		{"wan_ifname2", "", 0},
 		{"wan_ifnames", "", 0},
