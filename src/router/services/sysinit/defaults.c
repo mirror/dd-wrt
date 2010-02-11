eb877c1cc73f882dc665153402897c594339405a
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-02-11 15:07:34 +0000

    different default
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@13895 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/defaults.c
+++ src/router/services/sysinit/defaults.c
@@ -653,14 +653,16 @@ struct nvram_tuple srouter_defaults[] = {
 #ifdef HAVE_MADWIFI
 #ifdef HAVE_MAKSAT
 	{"show_hidden", "1", 0},
-	{"ath_regulatory", "0", 0},
+	{"ath0_regulatory", "0", 0},
+	{"ath1_regulatory", "0", 0},
+	{"ath2_regulatory", "0", 0},
+	{"ath3_regulatory", "0", 0},
 #ifdef HAVE_MAKSAT_BLANK
 	{"ath0_ssid", "default", 0},	/* Service set ID (network name) */
 #else
 	{"ath0_ssid", "maksat", 0},	/* Service set ID (network name) */
 #endif
 #else
-	{"ath_regulatory", "1", 0},
 #ifdef HAVE_BUFFALO
 	{"wl0_ssid", "BUFFALO", 0},	/* Service set ID (network name) */
 	{"ath0_ssid", "BUFFALO", 0},	/* Service set ID (network name) */
