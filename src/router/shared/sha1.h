86cb98e6b53b6872cd5af96ff654e939c868c9a0
BrainSlayer <s.gottschall@dd-wrt.com>
2010-02-16 06:04:32 +0000

    move sha1 to shared functions
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@13911 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/shared/sha1.h
@@ -0,0 +1,17 @@
+
+typedef unsigned int uint32_t;
+typedef unsigned char uint8_t;
+
+typedef struct sha1_ctx_t {
+	uint32_t count[2];
+	uint32_t hash[5];
+	uint32_t wbuf[16];
+} sha1_ctx_t;
+
+
+void sha1_begin(sha1_ctx_t *ctx);
+void sha1_hash(const void *data, unsigned int length, sha1_ctx_t *ctx);
+
+void* sha1_end(void *resbuf, sha1_ctx_t *ctx);
+
+#define SHA1_DIGEST_SIZE 20
