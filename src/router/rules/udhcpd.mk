f33a0d2616edf51a1a42eca1ed4d3a20598fe50d
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-02-26 04:33:28 +0000

    obsolete for now
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@13993 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/udhcpd.mk
+++ src/router/rules/udhcpd.mk
@@ -10,11 +10,11 @@ udhcpd-install:
 ifeq ($(CONFIG_UDHCPD),y)
 	install -D udhcpd/udhcpd $(INSTALLDIR)/udhcpd/usr/sbin/udhcpd
 	install -D udhcpd/dumpleases $(INSTALLDIR)/udhcpd/usr/sbin/dumpleases
-ifeq ($(CONFIG_BUFFALO),y)
-	install -D udhcpd/config/dhcpd.webservices.buffalo $(INSTALLDIR)/udhcpd/etc/config/dhcpd.webservices
-else
+#ifeq ($(CONFIG_BUFFALO),y)
+#	install -D udhcpd/config/dhcpd.webservices.buffalo $(INSTALLDIR)/udhcpd/etc/config/dhcpd.webservices
+#else
 	install -D udhcpd/config/dhcpd.webservices $(INSTALLDIR)/udhcpd/etc/config/dhcpd.webservices
-endif
+#endif
 	install -D udhcpd/config/dhcpd.startup $(INSTALLDIR)/udhcpd/etc/config/dhcpd.startup
 	$(STRIP) $(INSTALLDIR)/udhcpd/usr/sbin/udhcpd
 	$(STRIP) $(INSTALLDIR)/udhcpd/usr/sbin/dumpleases
