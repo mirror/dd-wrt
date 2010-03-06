82a176171e08bde6dbfc694f7c2ef1475edfd8c7
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-03-06 01:30:02 +0000

    do not compile in ipv6 support if it isnt selected
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@14041 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/dnsmasq.mk
+++ src/router/rules/dnsmasq.mk
@@ -1,14 +1,18 @@
+ifneq ($(CONFIG_IPV6),y)
+export DNSMASQ_MAKEFLAGS:=-DNO_IPV6
+endif
+
 dnsmasq:
 ifeq ($(CONFIG_DNSMASQ_TFTP),y)
 	$(MAKE) -j 4 -C dnsmasq COPTS=-DHAVE_BROKEN_RTC CFLAGS="$(COPTS) -DNO_LOG -ffunction-sections -fdata-sections -Wl,--gc-sections"
 else
 ifeq ($(CONFIG_DIST),"micro")
-	$(MAKE) -j 4 -C dnsmasq "COPTS=-DHAVE_BROKEN_RTC -DNO_TFTP" CFLAGS="$(COPTS) -DNO_LOG -ffunction-sections -fdata-sections -Wl,--gc-sections"
+	$(MAKE) -j 4 -C dnsmasq "COPTS=-DHAVE_BROKEN_RTC -DNO_TFTP" CFLAGS="$(COPTS) -DNO_LOG $(DNSMASQ_MAKEFLAGS) -ffunction-sections -fdata-sections -Wl,--gc-sections"
 else
 ifeq ($(CONFIG_DIST),"micro-special")
-	$(MAKE) -j 4 -C dnsmasq "COPTS=-DHAVE_BROKEN_RTC -DNO_TFTP" CFLAGS="$(COPTS) -DNO_LOG -ffunction-sections -fdata-sections -Wl,--gc-sections"
+	$(MAKE) -j 4 -C dnsmasq "COPTS=-DHAVE_BROKEN_RTC -DNO_TFTP" CFLAGS="$(COPTS) -DNO_LOG $(DNSMASQ_MAKEFLAGS) -ffunction-sections -fdata-sections -Wl,--gc-sections"
 else
-	$(MAKE) -j 4 -C dnsmasq "COPTS=-DHAVE_BROKEN_RTC -DNO_TFTP" CFLAGS="$(COPTS) -ffunction-sections -fdata-sections -Wl,--gc-sections" 
+	$(MAKE) -j 4 -C dnsmasq "COPTS=-DHAVE_BROKEN_RTC -DNO_TFTP" CFLAGS="$(COPTS) $(DNSMASQ_MAKEFLAGS) -ffunction-sections -fdata-sections -Wl,--gc-sections" 
 endif
 endif
 endif
