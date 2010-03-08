594bde1f6f17fdbc7269b33b78deedf6fe041ba6
BrainSlayer <s.gottschall@dd-wrt.com>
2010-03-08 23:12:37 +0000

    more fixes for rb600
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@14072 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/services/sysinit/sysinit-rb600.c
@@ -0,0 +1,169 @@
+/*
+ * sysinit-rb600.c
+ *
+ * Copyright (C) 2010 Sebastian Gottschall <gottschall@dd-wrt.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id:
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <time.h>
+#include <unistd.h>
+#include <errno.h>
+#include <syslog.h>
+#include <signal.h>
+#include <string.h>
+#include <termios.h>
+#include <sys/klog.h>
+#include <sys/types.h>
+#include <sys/mount.h>
+#include <sys/reboot.h>
+#include <sys/stat.h>
+#include <sys/sysmacros.h>
+#include <sys/time.h>
+#include <sys/utsname.h>
+#include <sys/wait.h>
+#include <linux/if_ether.h>
+#include <linux/mii.h>
+#include <linux/sockios.h>
+#include <net/if.h>
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <linux/sockios.h>
+#include <linux/mii.h>
+
+#include <bcmnvram.h>
+#include <shutils.h>
+#include <utils.h>
+#include <cymac.h>
+#include "devices/wireless.c"
+
+void start_sysinit(void)
+{
+	char buf[PATH_MAX];
+	struct utsname name;
+	struct stat tmp_stat;
+	time_t tm = 0;
+
+	unlink("/etc/nvram/.lock");
+	cprintf("sysinit() proc\n");
+	/*
+	 * /proc 
+	 */
+	mount("proc", "/proc", "proc", MS_MGC_VAL, NULL);
+	mount("sysfs", "/sys", "sysfs", MS_MGC_VAL, NULL);
+	cprintf("sysinit() tmp\n");
+
+	/*
+	 * /tmp 
+	 */
+	mount("ramfs", "/tmp", "ramfs", MS_MGC_VAL, NULL);
+	// fix for linux kernel 2.6
+	mount("devpts", "/dev/pts", "devpts", MS_MGC_VAL, NULL);
+	mount("devpts", "/proc/bus/usb", "usbfs", MS_MGC_VAL, NULL);
+	eval("mkdir", "-p", "/usr/local/nvram");
+	eval("mknod", "/dev/nvram", "c", "229", "0");
+	eval("mknod", "/dev/ppp", "c", "108", "0");
+	eval("mkdir", "/tmp/www");
+	eval("mount","-o","remount,rw","/dev/root");
+	unlink("/tmp/nvram/.lock");
+	eval("mkdir", "/tmp/nvram");
+	eval("/bin/tar", "-xzf", "/dev/mtdblock/2", "-C", "/");
+	FILE *in = fopen("/tmp/nvram/nvram.db", "rb");
+
+	if (in != NULL) {
+		fclose(in);
+		eval("/usr/sbin/convertnvram");
+		eval("/sbin/mtd", "erase", "nvram");
+		nvram_commit();
+	}
+	cprintf("sysinit() var\n");
+
+	/*
+	 * /var 
+	 */
+	mkdir("/tmp/var", 0777);
+	mkdir("/var/lock", 0777);
+	mkdir("/var/log", 0777);
+	mkdir("/var/run", 0777);
+	mkdir("/var/tmp", 0777);
+	cprintf("sysinit() setup console\n");
+	if (!nvram_match("disable_watchdog", "1"))
+		eval("watchdog");
+	/*
+	 * Setup console 
+	 */
+
+	cprintf("sysinit() klogctl\n");
+	klogctl(8, NULL, atoi(nvram_safe_get("console_loglevel")));
+	cprintf("sysinit() get router\n");
+
+	int brand = getRouterBrand();
+
+	/*
+	 * Modules 
+	 */
+	uname(&name);
+	/*
+	 * network drivers 
+	 */
+	detect_wireless_devices();
+
+	struct ifreq ifr;
+	int s;
+
+	if ((s = socket(AF_INET, SOCK_RAW, IPPROTO_RAW))) {
+		char eabuf[32];
+
+		strncpy(ifr.ifr_name, "eth0", IFNAMSIZ);
+		ioctl(s, SIOCGIFHWADDR, &ifr);
+		nvram_set("et0macaddr_safe",
+			  ether_etoa((unsigned char *)ifr.ifr_hwaddr.sa_data,
+				     eabuf));
+		close(s);
+	}
+
+	/*
+	 * Set a sane date 
+	 */
+	stime(&tm);
+	nvram_set("wl0_ifname", "ath0");
+
+	return;
+}
+
+int check_cfe_nv(void)
+{
+	nvram_set("portprio_support", "0");
+	return 0;
+}
+
+int check_pmon_nv(void)
+{
+	return 0;
+}
+
+void start_overclocking(void)
+{
+}
+
+void enable_dtag_vlan(int enable)
+{
+
+}
