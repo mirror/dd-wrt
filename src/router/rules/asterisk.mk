e28bd4119caa5759f94a15fcef86fe069cb55457
BrainSlayer <s.gottschall@dd-wrt.com>
2010-03-14 23:52:22 +0000

    asterisk update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@14111 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/asterisk.mk
+++ src/router/rules/asterisk.mk
@@ -1,8 +1,12 @@
+
+
 asterisk-configure:
-	cd asterisk && ./configure --host=$(ARCH)-linux-uclibc CFLAGS="$(COPTS) -DNEED_PRINTF" -without-curl \
+	cd asterisk && ./configure --host=$(ARCH)-linux-uclibc \
+	--without-curl \
 	--without-curses \
-	--without-gsm \
-	--without-imap \
+	--with-gsm=internal \
+	--without-gtk \
+	--without-gtk2 \
 	--without-isdnnet \
 	--without-kde \
 	--without-misdn \
@@ -13,29 +17,66 @@ asterisk-configure:
 	--without-odbc \
 	--without-ogg \
 	--without-osptk \
-	--without-oss \
 	--without-pri \
 	--without-qt \
 	--without-radius \
+	--without-sdl \
 	--without-spandsp \
 	--without-suppserv \
 	--without-tds \
 	--without-termcap \
 	--without-tinfo \
-	--without-tonezone \
 	--without-vorbis \
 	--without-vpb \
-	--without-zaptel \
-
-#	--with-popt="$(STAGING_DIR)/usr" \
+	--with-z="$(TOP)/zlib" \
+	--disable-xmldoc \
+	--without-dahdi \
+	--without-gnutls \
+	--without-iksemel
 
 asterisk:
-	make -j 4 -C asterisk
+	-rm asterisk/menuselect.makeopts
+	$(MAKE) -C asterisk \
+		include/asterisk/version.h \
+		include/asterisk/buildopts.h defaults.h \
+		makeopts.embed_rules
+	ASTCFLAGS="$(COPTS) -DLOW_MEMORY -fPIC" \
+	ASTLDFLAGS="$(COPTS) -DLOW_MEMORY -fPIC" \
+	$(MAKE) -C asterisk \
+		ASTVARLIBDIR="/usr/lib/asterisk" \
+		NOISY_BUILD="1" \
+		DEBUG="" \
+		OPTIMIZE="" \
+		all
+	make -C asterisk
 
 asterisk-install:
-	make -C asterisk install DESTDIR=$(INSTALLDIR)/asterisk
-	make -C asterisk adsi DESTDIR=$(INSTALLDIR)/asterisk
-#	make -C ncurses install.libs DESTDIR=$(INSTALLDIR)/ncurses
-#	rm -rf $(INSTALLDIR)/ncurses/usr/include
-#	rm -f $(INSTALLDIR)/ncurses/usr/lib/*.a
+	ASTCFLAGS="$(COPTS) -DLOW_MEMORY -fPIC" \
+	ASTLDFLAGS="$(COPTS) -DLOW_MEMORY -fPIC" \
+	$(MAKE) -C asterisk \
+		ASTVARLIBDIR="/usr/lib/asterisk" \
+		NOISY_BUILD="1" \
+		DEBUG="" \
+		OPTIMIZE="" \
+		DESTDIR=$(INSTALLDIR)/asterisk \
+		install
+	ASTCFLAGS="$(COPTS) -DLOW_MEMORY -fPIC" \
+	ASTLDFLAGS="$(COPTS) -DLOW_MEMORY -fPIC" \
+	$(MAKE) -C asterisk \
+		ASTVARLIBDIR="/usr/lib/asterisk" \
+		NOISY_BUILD="1" \
+		DEBUG="" \
+		OPTIMIZE="" \
+		DESTDIR=$(INSTALLDIR)/asterisk \
+		adsi
+#	sed 's|/var/lib/asterisk|/usr/lib/asterisk|g' $(INSTALLDIR)/asterisk/etc/asterisk/musiconhold.conf
+	rm -rf $(INSTALLDIR)/asterisk/usr/include
+	rm -rf $(INSTALLDIR)/asterisk/usr/share
+	rm -rf $(INSTALLDIR)/asterisk/usr/lib/static-http
+	rm -rf $(INSTALLDIR)/asterisk/usr/lib/sounds
+	rm -rf $(INSTALLDIR)/asterisk/usr/lib/images
+	rm -rf $(INSTALLDIR)/asterisk/usr/lib/documentation
+	rm -rf $(INSTALLDIR)/asterisk/usr/lib/agi-bin
+	rm -rf $(INSTALLDIR)/asterisk/var
+	rm -f $(INSTALLDIR)/asterisk/usr/lib/*.a
 
