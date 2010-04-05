d4975c26e31263ab98afa600f196065bb40d6ceb
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-04-05 10:30:59 +0000

    add tool (process_langfile) to reduce size of language files by removing duplicates (not translated items). Works kinda slow, but reduces size of firmware by 54k. Probably could be done faster....
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@14173 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ tools/process_langfile.c
@@ -0,0 +1,91 @@
+/* Language js file processing */
+/* this program removes duplicate entries in not completly translated language files to reduce overall size */
+
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+
+int filter (char *m, char *source, char *dest, int len);
+
+int
+main (int argc, char *argv[])
+{
+    int i;
+    char *m;
+    int a;
+    FILE *inFile;
+    FILE *refFile;
+    char linebuff[32762];
+
+    if (argc <= 2)
+        return 0;
+
+    printf ("Language files processing...\n");
+
+    for (a = 2; a < argc; a++)
+    {
+        if (!strcmp(argv[a], argv[1]))
+            continue;
+
+        printf ("Processing %s\n", argv[a]);
+        inFile = fopen (argv[a], "rb");
+        if (inFile == NULL)
+            return 0;
+        fseek (inFile, 0, SEEK_END);
+        int len = ftell (inFile);
+        m = (char *) malloc (len);
+        fseek (inFile, 0, SEEK_SET);
+        for (i = 0; i < len; i++)
+            m[i] = getc (inFile);
+
+        fclose (inFile);
+
+        FILE *refFile = fopen (argv[1], "rb");
+        while (fgets(linebuff, sizeof(linebuff), refFile))
+        {
+            // some sanity checks
+            if (linebuff[0]=='\r' || linebuff[0]=='\n')
+                continue;
+            if (!strstr(linebuff,"=\""))
+                continue;
+            if (!strstr(linebuff,"\";"))
+                continue;
+
+            len = filter (m, linebuff, "", len);
+        }
+        fclose (refFile);
+
+        printf ("Writing \n");
+
+        inFile = fopen (argv[a], "wb");
+        fwrite (m, len, 1, inFile);
+        free (m);
+
+        fclose (inFile);
+
+    }
+    return 0;
+}
+
+
+int
+filter (char *m, char *source, char *dest, int len)
+{
+    int i;
+
+    for (i = 0; i < len - strlen (source); i++)
+    {
+        if (strncmp ((char *) &m[i], source, strlen (source)) == 0)
+        {
+            memcpy (&m[i], dest, strlen (dest));
+            int delta = strlen (source) - strlen (dest);
+            memcpy (&m[i + strlen (dest)], &m[i + strlen (source)],
+                    len - (i + strlen (source)));
+            len -= delta;
+            i += strlen (dest);
+        }
+    }
+    return len;
+}
+
+
