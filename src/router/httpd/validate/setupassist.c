b0dfd207bb2db71bb00f6cea04fcdf5dd7ce619a
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-04-12 20:56:24 +0000

    changes for setupassistant
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@14225 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/httpd/validate/setupassist.c
@@ -0,0 +1,59 @@
+#ifdef HAVE_BUFFALO
+
+#define VALIDSOURCE 1
+
+#ifdef WEBS
+#include <webs.h>
+#include <uemf.h>
+#include <ej.h>
+#else                           /* !WEBS */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <httpd.h>
+#include <errno.h>
+#endif                          /* WEBS */
+
+#include <proto/ethernet.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/klog.h>
+#include <sys/wait.h>
+#include <cyutils.h>
+#include <support.h>
+#include <cy_conf.h>
+// #ifdef EZC_SUPPORT
+#include <ezc.h>
+// #endif
+#include <broadcom.h>
+#include <wlutils.h>
+#include <netdb.h>
+#include <utils.h>
+#include <stdarg.h>
+#include <sha1.h>
+
+struct variable **variables;
+
+void setupassistant_save(webs_t wp)
+{
+        fprintf(stderr, "[SetupAssistant Save]");
+	
+	// wireless
+	wireless_save(wp);
+	
+	// security
+	security_save(wp);
+		
+	// all other vars
+	validate_cgi(wp);
+}
+#endif
