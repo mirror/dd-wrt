0561a70ff64bd5dba38a90c544f91c8811953556
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-04-28 19:50:45 +0000

    sata/pata support and new cleaner rtc driver
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@14351 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/sl2312/linux-2.6.23/arch/arm/mach-sl2312/sl2312-otg.c
+++ src/linux/sl2312/linux-2.6.23/arch/arm/mach-sl2312/sl2312-otg.c
@@ -75,7 +75,98 @@ static struct platform_device ehci_device_2 = {
 	.resource       = sl2312_otg_resources_2,
 };
 
+
+static struct resource gemini_rtc_resources[] = {
+	[0] = {
+		.start  = SL2312_RTC_BASE,
+		.end    = SL2312_RTC_BASE + 0x24,
+		.flags  = IORESOURCE_MEM,
+	},
+	[1] = {
+		.start  = IRQ_RTC,
+		.end    = IRQ_RTC,
+		.flags  = IORESOURCE_IRQ,
+	},
+};
+
+static struct platform_device gemini_rtc_device = {
+	.name		= "rtc-gemini",
+	.id		= 0,
+	.num_resources	= ARRAY_SIZE(gemini_rtc_resources),
+	.resource	= gemini_rtc_resources,
+};
+
+/*
+* PATA
+*/
+
+static u64 gemini_pata_dmamask0 = 0xffffffffUL;
+static u64 gemini_pata_dmamask1 = 0xffffffffUL;
+
+static struct resource gemini_pata_resources0[] = {
+	[0] = {
+		.start  = SL2312_IDE0_BASE,
+		.end    = SL2312_IDE0_BASE + 0x40,
+		.flags  = IORESOURCE_MEM,
+	},
+	[1] = {
+		.start  = IRQ_IDE0,
+		.end    = IRQ_IDE0,
+		.flags  = IORESOURCE_IRQ,
+	},
+};
+
+static struct resource gemini_pata_resources1[] = {
+	[0] = {
+		.start  = SL2312_IDE1_BASE,
+		.end    = SL2312_IDE1_BASE + 0x40,
+		.flags  = IORESOURCE_MEM,
+	},
+	[1] = {
+		.start  = IRQ_IDE1,
+		.end    = IRQ_IDE1,
+		.flags  = IORESOURCE_IRQ,
+	},
+};
+
+static struct platform_device gemini_pata_devices[] = {
+	{
+		.name		= "pata_gemini",
+		.id		= 0,
+		.dev		= {
+			.dma_mask = &gemini_pata_dmamask0,
+			.coherent_dma_mask = 0xffffffff,
+		},
+		.num_resources  = ARRAY_SIZE(gemini_pata_resources0),
+		.resource       = gemini_pata_resources0,
+	},
+	{
+		.name		= "pata_gemini",
+		.id		= 1,
+		.dev		= {
+			.dma_mask = &gemini_pata_dmamask1,
+			.coherent_dma_mask = 0xffffffff,
+		},
+		.num_resources  = ARRAY_SIZE(gemini_pata_resources1),
+		.resource       = gemini_pata_resources1,
+	},
+};
+
+int __init platform_register_pata(unsigned int i)
+{
+	switch (i) {
+	case 0:
+		return platform_device_register(&gemini_pata_devices[0]);
+	case 1:
+		return platform_device_register(&gemini_pata_devices[1]);
+	default:
+		return -EINVAL;
+	}
+}
+
+
 static struct platform_device *devices[] __initdata = {
+	&gemini_rtc_device,
 	&ehci_device_1,
 #ifdef CONFIG_MACH_WBD222
 	&ehci_device_2,
@@ -84,6 +175,9 @@ static struct platform_device *devices[] __initdata = {
 
 static int __init sl2312_init(void)
 {
+#ifdef CONFIG_MACH_WBD222
+	platform_register_pata(0);
+#endif
 	return platform_add_devices(devices, ARRAY_SIZE(devices));
 }
 
