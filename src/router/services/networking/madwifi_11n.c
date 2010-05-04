c1fdd3013058d8c9e9fba806debf066b35db7317
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-05-04 09:19:22 +0000

    increase delay
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@14383 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/madwifi_11n.c
+++ src/router/services/networking/madwifi_11n.c
@@ -1095,7 +1095,7 @@ void configure_single_11n(int count)
 				  nvram_nget("%s_netmask", dev));
 		}
 		if (nvram_match(channel,"0"))
-		    sleep(4);
+		    sleep(10);
 	} else {
 		char bridged[32];
 
