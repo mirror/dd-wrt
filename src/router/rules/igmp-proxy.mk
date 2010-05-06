3826f1696b1f09a50ddf32b987c8f6b7268f7478
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-05-06 14:24:21 +0000

    igmp
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@14397 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/igmp-proxy.mk
+++ src/router/rules/igmp-proxy.mk
@@ -1,6 +1,12 @@
+igmp-proxy-configure:
+	cd igmp-proxy && ./configure --host=$(ARCH)-linux-elf CFLAGS="$(COPTS) -O2 -ffunction-sections -fdata-sections -Wl,--gc-sections -std=gnu99"
+
 igmp-proxy-clean:
 	make -C igmp-proxy clean
 
 igmp-proxy:
 	make -C igmp-proxy
+
+igmp-install:
+	install -D igmp-proxy/src/igmp-proxy $(INSTALLDIR)/igmp-proxy/usr/sbin
 
