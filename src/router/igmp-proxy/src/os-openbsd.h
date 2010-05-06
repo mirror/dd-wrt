84d6eda04511eb65491ef9ab5d547118096ddedf
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-05-06 14:11:53 +0000

    replace with new version
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@14395 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/igmp-proxy/src/os-openbsd.h
@@ -0,0 +1,21 @@
+#include <netinet/in_systm.h>
+#include <netinet/ip_mroute.h>
+#include <netinet/ip.h>
+#include <netinet/igmp.h>
+
+#define IGMP_MEMBERSHIP_QUERY IGMP_HOST_MEMBERSHIP_QUERY
+#define IGMP_V1_MEMBERSHIP_REPORT IGMP_v1_HOST_MEMBERSHIP_REPORT
+#define IGMP_V2_MEMBERSHIP_REPORT IGMP_v2_HOST_MEMBERSHIP_REPORT
+#define IGMP_V2_LEAVE_GROUP IGMP_HOST_LEAVE_MESSAGE
+
+#define INADDR_ALLRTRS_GROUP INADDR_ALLROUTERS_GROUP
+
+static inline u_short ip_data_len(const struct ip *ip)
+{
+	return ntohs(ip->ip_len) - (ip->ip_hl << 2);
+}
+
+static inline void ip_set_len(struct ip *ip, u_short len)
+{
+	ip->ip_len = htons(len);
+}
