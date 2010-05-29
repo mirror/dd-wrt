fb69fabf73ab82ae08e97173b734a094a74b2f91
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-05-29 15:01:42 +0000

    start support Netgear WNR2000v2
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@14521 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/netgear/ambitCfg-wnr2000v2.h
@@ -0,0 +1,48 @@
+/***************************************************************************
+#***
+#***    Copyright 2005  Hon Hai Precision Ind. Co. Ltd.
+#***    All Rights Reserved.
+#***    No portions of this material shall be reproduced in any form without the
+#***    written permission of Hon Hai Precision Ind. Co. Ltd.
+#***
+#***    All information contained in this document is Hon Hai Precision Ind.  
+#***    Co. Ltd. company private, proprietary, and trade secret property and 
+#***    are protected by international intellectual property laws and treaties.
+#***
+#****************************************************************************
+***
+***    Filename: ambitCfg.h
+***
+***    Description:
+***         This file is specific to each project. Every project should have a
+***    different copy of this file.
+***        Included from ambit.h which is shared by every project.
+***
+***    History:
+***
+***	   Modify Reason		                                        Author		         Date		                Search Flag(Option)
+***	--------------------------------------------------------------------------------------
+***    File Creation                                            Jasmine Yang    11/02/2005
+*******************************************************************************/
+
+
+#ifndef _AMBITCFG_H
+#define _AMBITCFG_H
+
+#define WW_VERSION           1 /* WW SKUs */
+#define NA_VERSION           2 /* NA SKUs */
+#define JP_VERSION           3
+#define GR_VERSION           4
+#define PR_VERSION           5
+#define KO_VERSION           6
+
+#define WLAN_REGION          WW_VERSION
+#define FW_REGION            WW_VERSION   /* true f/w region */
+
+/*formal version control*/
+#define AMBIT_HARDWARE_VERSION     "U12H11400"
+#define AMBIT_SOFTWARE_VERSION     "V1.0.0.35"
+#define AMBIT_UI_VERSION           "1.0.51NA"
+#define AMBIT_PRODUCT_NAME          "WNR2000V2"
+
+#endif /*_AMBITCFG_H*/
