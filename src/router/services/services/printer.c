2fe6bdf72ba48f6d15d978b01e7c4e4cf4f7c2e9
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-06-16 21:53:22 +0000

    start only if printer support is enabled
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@14615 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/services/services/printer.c
@@ -0,0 +1,52 @@
+/*
+ * rstats.c
+ *
+ * Copyright (C) 2007 Sebastian Gottschall <gottschall@dd-wrt.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id:
+ */
+#ifdef HAVE_P910ND
+#include <stdlib.h>
+#include <bcmnvram.h>
+#include <shutils.h>
+#include <utils.h>
+#include <syslog.h>
+#include <signal.h>
+#include <services.h>
+
+void stop_printer(void)
+{
+	stop_process("p9100d", "P910ND printer daemon");
+}
+
+void start_printer(void)
+{
+	if (nvram_match("usb_printer", "1")) {
+		FILE *test = fopen("/dev/usb/lp0", "rb");
+		if (!test) {
+			eval("mknod", "/dev/lp0", "c", "180", "0");
+			eval("mknod", "/dev/lp1", "c", "180", "1");
+			eval("mknod", "/dev/lp2", "c", "180", "2");
+			eval("p910nd", "-f", "/dev/lp0", "0");
+		} else {
+			fclose(test);
+			eval("p910nd", "-f", "/dev/usb/lp0", "0");
+		}
+	}
+}
+
+#endif
