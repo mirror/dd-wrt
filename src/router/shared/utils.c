89dc6e1fbc3b2f263e637608f5a360d788eb1bc4
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-06-23 15:18:49 +0000

    move functions to correct location if not used in different libraries
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@14636 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/utils.c
+++ src/router/shared/utils.c
@@ -1792,3 +1792,32 @@ int searchfor(FILE * fp, char *str, int scansize)
 	free(buffer);
 	return -1;
 }
+
+void addAction(char *action)
+{
+	char *actionstack = "";
+	char *next;
+	char service[80];
+	if (action == NULL || strlen(action) == 0)
+		return;
+	char *services = nvram_safe_get("action_service");
+
+	foreach(service, services, next) {
+		if (!strcmp(service, action)) {
+			return;
+		}
+	}
+	if (strlen(services) > 0) {
+		actionstack = safe_malloc(strlen(services) + strlen(action) + 2);
+		memset(actionstack, 0, strlen(services) + strlen(action) + 2);
+		strcpy(actionstack, action);
+		strcat(actionstack, " ");
+		strcat(actionstack, nvram_safe_get("action_service"));
+		nvram_set("action_service", actionstack);
+		free(actionstack);
+	} else {
+		nvram_set("action_service", action);
+	}
+
+}
+
