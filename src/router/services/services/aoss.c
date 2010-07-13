a0ef51e213143c1bcb26065979945a26169ca9e9
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-07-13 14:10:00 +0000

    switch interface
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@14788 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/aoss.c
+++ src/router/services/services/aoss.c
@@ -39,7 +39,7 @@ void start_aoss(void)
 	}
 	if (pidof("aoss") > 0)
 	    return;
-	ret = eval("aoss", "-i", nvram_safe_get("lan_ifname"), "-m", "ap");
+	ret = eval("aoss", "-i", "aoss", "-m", "ap");
 	dd_syslog(LOG_INFO, "aoss : aoss daemon successfully started\n");
 	cprintf("done\n");
 	return;
