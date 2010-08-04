81d7dd7653104f74d2bb5e43b326ec768c2e7b55
BrainSlayer <s.gottschall@dd-wrt.com>
2010-08-04 12:28:17 +0000

    more gw2388 related patches
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@14884 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/interface.c
+++ src/router/services/networking/interface.c
@@ -232,7 +232,7 @@ void start_config_vlan(void)
 
 void start_setup_vlans(void)
 {
-#if defined(HAVE_RB500) || defined(HAVE_XSCALE) || defined(HAVE_MAGICBOX) || defined(HAVE_RB600) || defined(HAVE_FONERA) || defined(HAVE_WHRAG108) || defined(HAVE_LS2) || defined(HAVE_CA8) || defined(HAVE_TW6600) || defined(HAVE_PB42) || defined(HAVE_LS5) || defined(HAVE_LSX) || defined(HAVE_DANUBE) || defined(HAVE_STORM) || defined(HAVE_ADM5120) || defined(HAVE_RT2880) || defined(HAVE_SOLO51) || defined(HAVE_OPENRISC)
+#if defined(HAVE_RB500) || defined(HAVE_XSCALE) || defined(HAVE_LAGUNA) || defined(HAVE_MAGICBOX) || defined(HAVE_RB600) || defined(HAVE_FONERA) || defined(HAVE_WHRAG108) || defined(HAVE_LS2) || defined(HAVE_CA8) || defined(HAVE_TW6600) || defined(HAVE_PB42) || defined(HAVE_LS5) || defined(HAVE_LSX) || defined(HAVE_DANUBE) || defined(HAVE_STORM) || defined(HAVE_ADM5120) || defined(HAVE_RT2880) || defined(HAVE_SOLO51) || defined(HAVE_OPENRISC)
 	return;
 #else
 	/*
