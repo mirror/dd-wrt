526164a1f2d85c25a87d7c19e49ee28122cb10b2
chris <chris@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-08-05 12:10:15 +0000

    parprouted update to 0.7 change path of ip command debug to syslog fix compile flags
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@14892 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/parprouted/parprouted.c
+++ src/router/parprouted/parprouted.c
@@ -1,5 +1,5 @@
 /* parprouted: ProxyARP routing daemon. 
- * (C) 2004 Vladimir Ivaschenko <vi@maks.net>
+ * (C) 2008 Vladimir Ivaschenko <vi@maks.net>
  *
  * This application is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -22,6 +22,7 @@
 
 char *progname;
 int debug=0;
+int verbose=0;
 int option_arpperm=0;
 static int perform_shutdown=0;
 
@@ -36,17 +37,19 @@ int last_iface_idx=-1;
 ARPTAB_ENTRY **arptab;
 pthread_mutex_t arptab_mutex;
 
-ARPTAB_ENTRY * replace_entry(struct in_addr ipaddr) 
+ARPTAB_ENTRY * replace_entry(struct in_addr ipaddr, char *dev) 
 {
     ARPTAB_ENTRY * cur_entry=*arptab;
     ARPTAB_ENTRY * prev_entry=NULL;
-        
-    while (cur_entry != NULL && ipaddr.s_addr != cur_entry->ipaddr_ia.s_addr) {
+
+    while (cur_entry != NULL && ( ipaddr.s_addr != cur_entry->ipaddr_ia.s_addr || ( strncmp(cur_entry->ifname,dev,strlen(dev)) != 0 ) ) ) {
 	prev_entry = cur_entry;
 	cur_entry = cur_entry->next;
     };
 
     if (cur_entry == NULL) {
+	if (debug) syslog(LOG_NOTICE,"Creating new arptab entry %s(%s)\n", inet_ntoa(ipaddr), dev);
+
 	if ((cur_entry = (ARPTAB_ENTRY *) malloc(sizeof(ARPTAB_ENTRY))) == NULL) {
 	    errstr = strerror(errno);
 	    syslog(LOG_INFO, "No memory: %s", errstr);
@@ -55,7 +58,8 @@ ARPTAB_ENTRY * replace_entry(struct in_addr ipaddr)
 	    else { prev_entry->next = cur_entry; }
 	    cur_entry->next = NULL;
 	    cur_entry->ifname[0] = '\0';
-	    cur_entry->route_added=0;
+	    cur_entry->route_added = 0;
+	    cur_entry->want_route = 1;
 	}
     }
     
@@ -78,63 +82,130 @@ int findentry(struct in_addr ipaddr)
 	return 1;
 }
 
-void processarp(int cleanup) 
+/* Remove all entires in arptab where ipaddr is NOT on interface dev */
+int remove_other_routes(struct in_addr ipaddr, const char* dev)
+{
+    ARPTAB_ENTRY * cur_entry;
+    int removed = 0;
+        
+    for (cur_entry=*arptab; cur_entry != NULL; cur_entry = cur_entry->next) {
+        if (ipaddr.s_addr == cur_entry->ipaddr_ia.s_addr && strcmp(dev, cur_entry->ifname) != 0)  {
+            if (debug && cur_entry->want_route) syslog(LOG_NOTICE,"Marking entry %s(%s) for removal\n", inet_ntoa(ipaddr), cur_entry->ifname);
+            cur_entry->want_route = 0;
+            ++removed;
+        }
+    }
+    return removed;
+}
+
+
+/* Remove route from kernel */
+int route_remove(ARPTAB_ENTRY* cur_entry)
+{
+    char routecmd_str[ROUTE_CMD_LEN];
+    int success = 1;
+    
+    if (snprintf(routecmd_str, ROUTE_CMD_LEN-1, 
+	    "/usr/sbin/ip route del %s/32 metric 50 dev %s scope link",
+	    inet_ntoa(cur_entry->ipaddr_ia), cur_entry->ifname) > ROUTE_CMD_LEN-1) 
+    {
+	syslog(LOG_INFO, "ip route command too large to fit in buffer!");
+    } else {
+	if (system(routecmd_str) != 0)
+	{
+	    syslog(LOG_INFO, "'%s' unsuccessful!", routecmd_str);
+	    if (debug) syslog(LOG_NOTICE,"%s failed\n", routecmd_str);
+	    success = 0;
+	}
+	else 
+	{
+	    if (debug) syslog(LOG_NOTICE,"%s success\n", routecmd_str);
+	    success = 1;
+	}
+    }
+    if (success)
+	cur_entry->route_added = 0;
+    
+    return success;
+}
+
+/* Add route into kernel */
+int route_add(ARPTAB_ENTRY* cur_entry)
+{
+    char routecmd_str[ROUTE_CMD_LEN];
+    int success = 1;
+
+    if (snprintf(routecmd_str, ROUTE_CMD_LEN-1, 
+	    "/usr/sbin/ip route add %s/32 metric 50 dev %s scope link",
+	    inet_ntoa(cur_entry->ipaddr_ia), cur_entry->ifname) > ROUTE_CMD_LEN-1) 
+    {
+	syslog(LOG_INFO, "ip route command too large to fit in buffer!");
+    } else {
+	if (system(routecmd_str) != 0)
+	{ 
+	    syslog(LOG_INFO, "'%s' unsuccessful, will try to remove!", routecmd_str); 
+	    if (debug) syslog(LOG_NOTICE,"%s failed\n", routecmd_str);
+	    route_remove(cur_entry);
+	    success = 0;
+	}
+	else
+	{
+	    if (debug) syslog(LOG_NOTICE,"%s success\n", routecmd_str);
+	    success = 1;
+	}
+    }
+    if (success)
+	cur_entry->route_added = 1;
+
+    return success;
+}
+
+
+void processarp(int in_cleanup) 
 {
     ARPTAB_ENTRY *cur_entry=*arptab, *prev_entry=NULL;
-    char routecmd_str[ROUTE_CMD_LEN];
 
+    /* First loop to remove unwanted routes */
     while (cur_entry != NULL) {
+	if (debug && verbose) syslog(LOG_NOTICE,"Working on route %s(%s) tstamp %u want_route %d\n", inet_ntoa(cur_entry->ipaddr_ia), cur_entry->ifname, (int) cur_entry->tstamp, cur_entry->want_route);
 
-	if (cur_entry->tstamp - time(NULL) <= ARP_TABLE_ENTRY_TIMEOUT 
-	    && !cur_entry->route_added 
-	    && !cur_entry->incomplete
-	    && !cleanup) 
-	{
+	if ( !cur_entry->want_route
+	    || time(NULL) - cur_entry->tstamp > ARP_TABLE_ENTRY_TIMEOUT 
+	    || in_cleanup)  {
+	    
+	    if (cur_entry->route_added)
+		route_remove(cur_entry);
 
-	    /* added route to the kernel */
-	    if (snprintf(routecmd_str, ROUTE_CMD_LEN-1, 
-		     "/sbin/ip route add %s/32 metric 50 dev %s scope link",
-		     inet_ntoa(cur_entry->ipaddr_ia), cur_entry->ifname) > ROUTE_CMD_LEN-1) 
-	    {
-		syslog(LOG_INFO, "ip route command too large to fit in buffer!");
-	    } else {
-		if (system(routecmd_str) != 0)
-		    { syslog(LOG_INFO, "'%s' unsuccessful!", routecmd_str); }
-	    }
-
-	    cur_entry->route_added = 1;
-	    cur_entry = cur_entry->next;
-
-	} else if (!cur_entry->incomplete && (
-		     cur_entry->tstamp - time(NULL) > ARP_TABLE_ENTRY_TIMEOUT 
-		     || cleanup
-		    )) {
-
-	    /* remove entry from arp table and remove route from kernel */
-	    if (snprintf(routecmd_str, ROUTE_CMD_LEN-1, 
-		     "/sbin/ip route del %s/32 metric 50 dev %s scope link",
-		     inet_ntoa(cur_entry->ipaddr_ia), cur_entry->ifname) > ROUTE_CMD_LEN-1) 
-	    {
-		syslog(LOG_INFO, "ip route command too large to fit in buffer!");
-	    } else {
-		if (system(routecmd_str) != 0)
-		    syslog(LOG_INFO, "'%s' unsuccessful!", routecmd_str);
-	    }
-
-	    if (prev_entry != NULL) {
-		prev_entry->next = cur_entry->next;
-		free(cur_entry);
-		cur_entry=prev_entry->next;
-	    } else {
-		*arptab = cur_entry->next;
-		free(cur_entry);
-		cur_entry=*arptab;
-	    }
+	    /* remove from arp list */
+	    if (debug) syslog(LOG_NOTICE,"Delete arp %s(%s)\n", inet_ntoa(cur_entry->ipaddr_ia), cur_entry->ifname);
 		
+	    if (prev_entry != NULL) {
+	        prev_entry->next = cur_entry->next;
+	        free(cur_entry);
+	        cur_entry=prev_entry->next;
+	    } else {
+	        *arptab = cur_entry->next;
+	        free(cur_entry);
+        	cur_entry=*arptab;
+            }
 	} else {
+    	    prev_entry = cur_entry;
 	    cur_entry = cur_entry->next;
-	} /* if */
+	}	
+    } /* while loop */
 
+    /* Now loop to add new routes */
+    cur_entry=*arptab;
+    while (cur_entry != NULL) {
+	if (time(NULL) - cur_entry->tstamp <= ARP_TABLE_ENTRY_TIMEOUT 
+		&& cur_entry->want_route
+		&& !cur_entry->route_added 
+		&& !in_cleanup) 
+	{
+	    /* add route to the kernel */
+	    route_add(cur_entry);
+	}
+	cur_entry = cur_entry->next;
     } /* while loop */
 
 }	
@@ -145,10 +216,10 @@ void parseproc()
     int firstline;
     ARPTAB_ENTRY *entry;
     char line[ARP_LINE_LEN];
-    char *item;
     struct in_addr ipaddr;
     int incomplete=0;
     int i;
+    char *ip, *mac, *dev, *hw, *flags, *mask;
     
     /* Parse /proc/net/arp table */
         
@@ -170,6 +241,7 @@ void parseproc()
 	    }
 	} else {
 	    if (firstline) { firstline=0; continue; }
+	    if (debug && verbose) syslog(LOG_NOTICE,"read ARP line %s", line);
 
 	    incomplete=0;
 	    	    
@@ -181,62 +253,74 @@ void parseproc()
 	    if (strstr(line, "0x0") != NULL)
 		incomplete=1;
 	    
-	    item=strtok(line, " ");
+	    ip=strtok(line, " ");
 
-	    if ((inet_aton(item, &ipaddr)) == -1)
-		    syslog(LOG_INFO, "Error parsing IP address %s", item);
-	    
+	    if ((inet_aton(ip, &ipaddr)) == -1)
+		    syslog(LOG_INFO, "Error parsing IP address %s", ip);
+			
 	    /* if IP address is marked as undiscovered and does not exist in arptab,
 	       send ARP request to all ifaces */
 
 	    if (incomplete &! findentry(ipaddr) ) {
+	    	if (debug)  syslog(LOG_NOTICE,"incomplete entry %s found, request on all interfaces\n", inet_ntoa(ipaddr));
 		for (i=0; i <= last_iface_idx; i++)
-		    arp_req(ifaces[i], ipaddr);
+		    arp_req(ifaces[i], ipaddr, 0);
 	    }
 
-	    entry=replace_entry(ipaddr);
-	    entry->incomplete = incomplete;
-	    
-	    entry->ipaddr_ia.s_addr = ipaddr.s_addr;
-	    
 	    /* Hardware type */
-	    item=strtok(NULL, " "); 
+	    hw=strtok(NULL, " "); 
 	    
 	    /* flags */
-	    item=strtok(NULL, " "); 
+	    flags=strtok(NULL, " "); 
 
 	    /* MAC address */	    
-	    item=strtok(NULL, " ");
-
-	    if (strlen(item) < ARP_TABLE_ENTRY_LEN)
-		strncpy(entry->hwaddr, item, ARP_TABLE_ENTRY_LEN);
-	    else 
-		syslog(LOG_INFO, "Error during ARP table parsing");
+	    mac=strtok(NULL, " ");
 
 	    /* Mask */
-	    item=strtok(NULL, " "); 
+	    mask=strtok(NULL, " "); 
 
 	    /* Device */
-	    item=strtok(NULL, " ");
-	    if (item[strlen(item)-1] == '\n') { item[strlen(item)-1] = '\0'; }
-	    if (strlen(item) < ARP_TABLE_ENTRY_LEN) {
-		if (entry->route_added && !strcmp(item, entry->ifname))
-		    /* Remove route from kernel if it already exists through
-		       a different interface */
-		    entry->tstamp=0;
-		else 
-		    strncpy(entry->ifname, item, ARP_TABLE_ENTRY_LEN);
-	    } else {
-		    syslog(LOG_INFO, "Error during ARP table parsing");
-	    }
+	    dev=strtok(NULL, " ");
+
+	    if (dev[strlen(dev)-1] == '\n') { dev[strlen(dev)-1] = '\0'; }
+
+	    entry=replace_entry(ipaddr, dev);
+	    
+	    if (entry->incomplete != incomplete && debug)
+	    	syslog(LOG_NOTICE,"change entry %s(%s) to incomplete=%d\n", ip, dev, incomplete);
+
+	    entry->ipaddr_ia.s_addr = ipaddr.s_addr;
+	    entry->incomplete       = incomplete;
+	    
+	    if (strlen(mac) < ARP_TABLE_ENTRY_LEN)
+		strncpy(entry->hwaddr, mac, ARP_TABLE_ENTRY_LEN);
+	    else 
+		syslog(LOG_INFO, "Error during ARP table parsing");
+	    
+	    if (strlen(dev) < ARP_TABLE_ENTRY_LEN) 
+		strncpy(entry->ifname, dev, ARP_TABLE_ENTRY_LEN);
+	    else
+		syslog(LOG_INFO, "Error during ARP table parsing");
+
+	    /* do not add routes for incomplete entries */
+	    if (debug && entry->want_route != !incomplete) 
+		syslog(LOG_NOTICE,"%s(%s): set want_route %d\n", inet_ntoa(entry->ipaddr_ia), entry->ifname, !incomplete);
+	    entry->want_route   = !incomplete; 
+
+            /* Remove route from kernel if it already exists through
+               a different interface */
+            if (entry->want_route)
+            {
+                if (remove_other_routes(entry->ipaddr_ia, entry->ifname) > 0)
+                    if (debug) syslog(LOG_NOTICE,"Found ARP entry %s(%s), removed entries via other interfaces\n", inet_ntoa(entry->ipaddr_ia), entry->ifname);
+            }
 
 	    time(&entry->tstamp);
 	    
-	    if (debug &! entry->route_added && !incomplete) {
-	        printf("add entry: IPAddr: '%s' HWAddr: '%s' Dev: '%s'\n", 
-		    inet_ntoa(entry->ipaddr_ia), entry->hwaddr, entry->ifname);
+	    if (debug && !entry->route_added && entry->want_route) {
+	        syslog(LOG_NOTICE,"arptab entry: '%s' HWAddr: '%s' Dev: '%s' route_added:%d want_route:%d\n", 
+		    inet_ntoa(entry->ipaddr_ia), entry->hwaddr, entry->ifname, entry->route_added, entry->want_route);
 	    }
-
 	}
     }
 
@@ -290,7 +374,9 @@ void *main_thread()
 	pthread_mutex_unlock(&arptab_mutex);
 	usleep(SLEEPTIME);
 	if (!option_arpperm && time(NULL)-last_refresh > REFRESHTIME) {
+	    pthread_mutex_lock(&arptab_mutex);
 	    refresharp(*arptab);
+	    pthread_mutex_unlock(&arptab_mutex);
 	    time(&last_refresh);
 	}
     }
@@ -326,7 +412,7 @@ int main (int argc, char **argv)
 
     if (help || last_iface_idx <= -1) {
 	    printf("parprouted: proxy ARP routing daemon, version %s.\n", VERSION);
-    	    printf("(C) 2002 Vladimir Ivaschenko <vi@maks.net>, GPL2 license.\n");
+    	    printf("(C) 2007 Vladimir Ivaschenko <vi@maks.net>, GPL2 license.\n");
 	    printf("Usage: parprouted [-d] [-p] interface [interface]\n");
 	    exit(1);
     }
@@ -389,7 +475,7 @@ int main (int argc, char **argv)
 	    syslog(LOG_ERR, "Error creating ARP thread for %s.",ifaces[i]);
 	    abort();
 	}
-	if (debug) printf("Created ARP thread for %s.\n",ifaces[i]);
+	if (debug) syslog(LOG_NOTICE,"Created ARP thread for %s.\n",ifaces[i]);
     }
         
     if (pthread_join(my_threads[0], NULL)) {
