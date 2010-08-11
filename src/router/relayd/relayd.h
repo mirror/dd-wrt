e498e1d038e074ee613019a4b23e097089a1931e
ChristianScheele <scheele@newmedia-net.de>
2010-08-11 22:53:01 +0000

    relayd policy-routing
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@14926 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/relayd/relayd.h
@@ -0,0 +1,132 @@
+/*
+ *   Copyright (C) 2010 Felix Fietkau <nbd@openwrt.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License v2 as published by
+ *   the Free Software Foundation.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ *
+ */
+#ifndef __RELAYD_H
+#define __RELAYD_H
+
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <net/ethernet.h>
+#include <netinet/if_ether.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+
+#include <linux/if_packet.h>
+#include <linux/rtnetlink.h>
+#include <linux/neighbour.h>
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "uloop.h"
+#include "list.h"
+
+#define DEBUG
+#ifdef DEBUG
+#define DPRINTF(level, ...) if (debug >= level) fprintf(stderr, __VA_ARGS__);
+#else
+#define DPRINTF(...) do {} while(0)
+#endif
+
+#ifndef __packed
+#define __packed __attribute__((packed))
+#endif
+
+#define __uc(c) ((unsigned char *)(c))
+
+#define MAC_FMT	"%02x:%02x:%02x:%02x:%02x:%02x"
+#define MAC_BUF(_c) __uc(_c)[0], __uc(_c)[1], __uc(_c)[2], __uc(_c)[3], __uc(_c)[4], __uc(_c)[5]
+
+#define IP_FMT	"%d.%d.%d.%d"
+#define IP_BUF(_c) __uc(_c)[0], __uc(_c)[1], __uc(_c)[2], __uc(_c)[3]
+
+#define DUMMY_IP ((uint8_t *) "\x01\x01\x01\x01")
+
+#define DHCP_FLAG_BROADCAST	(1 << 15)
+
+struct relayd_interface {
+	struct list_head list;
+	struct uloop_fd fd;
+	struct uloop_fd bcast_fd;
+	struct sockaddr_ll sll;
+	struct sockaddr_ll bcast_sll;
+	char ifname[IFNAMSIZ];
+	struct list_head hosts;
+	uint8_t src_ip[4];
+	bool managed;
+	int rt_table;
+};
+
+struct relayd_host {
+	struct list_head list;
+	struct relayd_interface *rif;
+	uint8_t lladdr[ETH_ALEN];
+	uint8_t ipaddr[4];
+	struct uloop_timeout timeout;
+	int cleanup_pending;
+};
+
+struct arp_packet {
+	struct ether_header eth;
+	struct ether_arp arp;
+} __packed;
+
+struct ip_packet {
+	struct ether_header eth;
+	struct iphdr iph;
+} __packed;
+
+struct dhcp_header {
+	uint8_t op, htype, hlen, hops;
+	uint32_t xit;
+	uint16_t secs, flags;
+	struct in_addr ciaddr, yiaddr, siaddr, giaddr;
+	unsigned char chaddr[16];
+	unsigned char sname[64];
+	unsigned char file[128];
+} __packed;
+
+struct rtnl_req {
+	struct nlmsghdr nl;
+	struct rtmsg rt;
+} __packed;
+
+extern struct list_head interfaces;
+extern int debug;
+extern int route_table;
+
+void rtnl_route_set(struct relayd_host *host, bool add);
+
+static inline void relayd_add_route(struct relayd_host *host)
+{
+	rtnl_route_set(host, true);
+}
+
+static inline void relayd_del_route(struct relayd_host *host)
+{
+	rtnl_route_set(host, false);
+}
+
+void relayd_add_interface_routes(struct relayd_interface *rif);
+void relayd_del_interface_routes(struct relayd_interface *rif);
+
+int relayd_rtnl_init(void);
+void relayd_rtnl_done(void);
+
+struct relayd_host *relayd_refresh_host(struct relayd_interface *rif, const uint8_t *lladdr, const uint8_t *ipaddr);
+
+#endif
---
+++ src/router/relayd/route.c
@@ -0,0 +1,328 @@
+/*
+ *   Copyright (C) 2010 Felix Fietkau <nbd@openwrt.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License v2 as published by
+ *   the Free Software Foundation.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <sys/socket.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <linux/fib_rules.h>
+
+#include "relayd.h"
+
+static struct uloop_fd rtnl_sock;
+static unsigned int rtnl_seq, rtnl_dump_seq;
+int route_table = 16800;
+
+static void rtnl_flush(void)
+{
+	int fd;
+
+	fd = open("/proc/sys/net/ipv4/route/flush", O_WRONLY);
+	if (fd < 0)
+		return;
+
+	write(fd, "-1", 2);
+	close(fd);
+}
+
+static void rtnl_route_request(struct relayd_interface *rif, struct relayd_host *host, bool add)
+{
+	static struct {
+		struct nlmsghdr nl;
+		struct rtmsg rt;
+		struct {
+			struct rtattr rta;
+			uint8_t ipaddr[4];
+		} __packed dst;
+		struct {
+			struct rtattr rta;
+			int table;
+		} __packed table;
+		struct {
+			struct rtattr rta;
+			int ifindex;
+		} __packed dev;
+	} __packed req = {
+		.nl = {
+			.nlmsg_len = sizeof(req),
+		},
+		.rt = {
+			.rtm_family = AF_INET,
+			.rtm_dst_len = 32,
+			.rtm_table = RT_TABLE_MAIN,
+		},
+		.table.rta = {
+			.rta_type = RTA_TABLE,
+			.rta_len = sizeof(req.table),
+		},
+		.dst.rta = {
+			.rta_type = RTA_DST,
+			.rta_len = sizeof(req.dst),
+		},
+		.dev.rta = {
+			.rta_type = RTA_OIF,
+			.rta_len = sizeof(req.dev),
+		},
+	};
+
+	memcpy(req.dst.ipaddr, host->ipaddr, sizeof(req.dst.ipaddr));
+	req.dev.ifindex = host->rif->sll.sll_ifindex;
+	req.table.table = rif->rt_table;
+
+	req.nl.nlmsg_flags = NLM_F_REQUEST;
+	if (add) {
+		req.nl.nlmsg_type = RTM_NEWROUTE;
+		req.nl.nlmsg_flags |= NLM_F_CREATE | NLM_F_REPLACE;
+
+		req.rt.rtm_protocol = RTPROT_BOOT;
+		req.rt.rtm_scope = RT_SCOPE_LINK;
+		req.rt.rtm_type = RTN_UNICAST;
+	} else {
+		req.nl.nlmsg_type = RTM_DELROUTE;
+		req.rt.rtm_scope = RT_SCOPE_NOWHERE;
+	}
+
+	send(rtnl_sock.fd, &req, sizeof(req), 0);
+	rtnl_flush();
+}
+
+static void rtnl_rule_request(struct relayd_interface *rif, bool add)
+{
+	static struct {
+		struct nlmsghdr nl;
+		struct rtmsg rt;
+		struct {
+			struct rtattr rta;
+			int table;
+		} __packed table;
+		struct {
+			struct rtattr rta;
+			char ifname[IFNAMSIZ + 1];
+		} __packed dev;
+	} __packed req = {
+		.rt = {
+			.rtm_family = AF_INET,
+			.rtm_table = RT_TABLE_UNSPEC,
+			.rtm_scope = RT_SCOPE_UNIVERSE,
+			.rtm_protocol = RTPROT_BOOT,
+		},
+		.table.rta = {
+			.rta_type = FRA_TABLE,
+			.rta_len = sizeof(req.table),
+		},
+		.dev.rta = {
+			.rta_type = FRA_IFNAME,
+		},
+	};
+
+	int padding = sizeof(req.dev.ifname);
+	padding -= strlen(rif->ifname) + 1;
+
+	strcpy(req.dev.ifname, rif->ifname);
+	req.dev.rta.rta_len = sizeof(req.dev.rta) + strlen(rif->ifname) + 1;
+	req.table.table = rif->rt_table;
+	req.nl.nlmsg_len = sizeof(req) - padding;
+
+	req.nl.nlmsg_flags = NLM_F_REQUEST;
+	if (add) {
+		req.nl.nlmsg_type = RTM_NEWRULE;
+		req.nl.nlmsg_flags |= NLM_F_CREATE | NLM_F_EXCL;
+
+		req.rt.rtm_type = RTN_UNICAST;
+	} else {
+		req.nl.nlmsg_type = RTM_DELRULE;
+		req.rt.rtm_type = RTN_UNSPEC;
+	}
+
+	send(rtnl_sock.fd, &req, req.nl.nlmsg_len, 0);
+	rtnl_flush();
+}
+
+void rtnl_route_set(struct relayd_host *host, bool add)
+{
+	struct relayd_interface *rif;
+
+	list_for_each_entry(rif, &interfaces, list) {
+		if (rif == host->rif)
+			continue;
+
+		rtnl_route_request(rif, host, add);
+	}
+}
+
+void relayd_add_interface_routes(struct relayd_interface *rif)
+{
+	rif->rt_table = route_table++;
+	rtnl_rule_request(rif, true);
+}
+
+void relayd_del_interface_routes(struct relayd_interface *rif)
+{
+	rtnl_rule_request(rif, false);
+}
+
+#ifndef NDA_RTA
+#define NDA_RTA(r) \
+    ((struct rtattr*)(((char*)(r)) + NLMSG_ALIGN(sizeof(struct ndmsg))))
+#endif
+
+static void rtnl_parse_newneigh(struct nlmsghdr *h)
+{
+	struct relayd_interface *rif = NULL;
+	struct ndmsg *r = NLMSG_DATA(h);
+	const uint8_t *lladdr = NULL;
+	const uint8_t *ipaddr = NULL;
+	struct rtattr *rta;
+	int len;
+
+	if (r->ndm_family != AF_INET)
+		return;
+
+	list_for_each_entry(rif, &interfaces, list) {
+		if (rif->sll.sll_ifindex == r->ndm_ifindex)
+			goto found_interface;
+	}
+	return;
+
+found_interface:
+	len = h->nlmsg_len - NLMSG_LENGTH(sizeof(*r));
+	for (rta = NDA_RTA(r); RTA_OK(rta, len); rta = RTA_NEXT(rta, len)) {
+		switch(rta->rta_type) {
+		case NDA_LLADDR:
+			lladdr = RTA_DATA(rta);
+			break;
+		case NDA_DST:
+			ipaddr = RTA_DATA(rta);
+			break;
+		default:
+			break;
+		}
+	}
+
+	if (!lladdr || !ipaddr || (r->ndm_state & (NUD_INCOMPLETE|NUD_FAILED)))
+		return;
+
+	if (!memcmp(lladdr, "\x00\x00\x00\x00\x00\x00", ETH_ALEN))
+		return;
+
+	DPRINTF(1, "%s: Found ARP cache entry for host "IP_FMT" ("MAC_FMT")\n",
+		rif->ifname, IP_BUF(ipaddr), MAC_BUF(lladdr));
+	relayd_refresh_host(rif, lladdr, ipaddr);
+}
+
+static void rtnl_parse_packet(void *data, int len)
+{
+	struct nlmsghdr *h;
+
+	for (h = data; NLMSG_OK(h, len); h = NLMSG_NEXT(h, len)) {
+		if (h->nlmsg_type == NLMSG_DONE ||
+		    h->nlmsg_type == NLMSG_ERROR)
+			return;
+
+		if (h->nlmsg_seq != rtnl_dump_seq)
+			continue;
+
+		if (h->nlmsg_type == RTM_NEWNEIGH)
+			rtnl_parse_newneigh(h);
+	}
+}
+
+static void rtnl_cb(struct uloop_fd *fd, unsigned int events)
+{
+	struct sockaddr_nl nladdr;
+	static uint8_t buf[16384];
+	struct iovec iov = {
+		.iov_base = buf,
+		.iov_len = sizeof(buf),
+	};
+	struct msghdr msg = {
+		.msg_name = &nladdr,
+		.msg_namelen = sizeof(nladdr),
+		.msg_iov = &iov,
+		.msg_iovlen = 1,
+	};
+
+	do {
+		int len;
+
+		len = recvmsg(rtnl_sock.fd, &msg, 0);
+		if (len < 0) {
+			if (errno == EINTR)
+				continue;
+
+			return;
+		}
+
+		if (!len)
+			break;
+
+		if (nladdr.nl_pid != 0)
+			continue;
+
+		rtnl_parse_packet(buf, len);
+	} while (1);
+}
+
+int relayd_rtnl_init(void)
+{
+	struct sockaddr_nl snl_local;
+	static struct {
+		struct nlmsghdr nlh;
+		struct rtgenmsg g;
+	} req = {
+		.nlh = {
+			.nlmsg_len = sizeof(req),
+			.nlmsg_type = RTM_GETNEIGH,
+			.nlmsg_flags = NLM_F_ROOT|NLM_F_MATCH|NLM_F_REQUEST,
+			.nlmsg_pid = 0,
+		},
+		.g.rtgen_family = AF_INET,
+	};
+
+	rtnl_sock.fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
+	if (rtnl_sock.fd < 0) {
+		perror("socket(AF_NETLINK)");
+		return -1;
+	}
+
+	snl_local.nl_family = AF_NETLINK;
+
+	if (bind(rtnl_sock.fd, (struct sockaddr *) &snl_local, sizeof(struct sockaddr_nl)) < 0) {
+		perror("bind");
+		close(rtnl_sock.fd);
+		return -1;
+	}
+
+	rtnl_sock.cb = rtnl_cb;
+	uloop_fd_add(&rtnl_sock, ULOOP_READ | ULOOP_EDGE_TRIGGER);
+
+	rtnl_seq = time(NULL);
+	rtnl_dump_seq = rtnl_seq;
+	req.nlh.nlmsg_seq = rtnl_seq;
+	send(rtnl_sock.fd, &req, sizeof(req), 0);
+
+	return 0;
+}
+
+void relayd_rtnl_done(void)
+{
+	uloop_fd_delete(&rtnl_sock);
+	close(rtnl_sock.fd);
+}
