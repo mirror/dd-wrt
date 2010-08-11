480f41c47a36a6d07256533d699264e8bca2c335
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-08-11 05:21:03 +0000

    L7 bt3 is too restrictive; blocks some games too. Remove for now...
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@14919 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/firewall.c
+++ src/router/services/networking/firewall.c
@@ -1514,9 +1514,9 @@ static void advgrp_chain(int seq, unsigned int mark, int urlenable)
 					save2file
 					    ("-A advgrp_%d -m layer7 --l7proto bt2 -j %s\n",
 					     seq, log_drop);
-					save2file
-					    ("-A advgrp_%d -m layer7 --l7proto bt3 -j %s\n",
-					     seq, log_drop);
+//					save2file
+//					    ("-A advgrp_%d -m layer7 --l7proto bt3 -j %s\n",
+//					     seq, log_drop);
 #ifndef HAVE_MICRO
 //                                      save2file
 //                                          ("-A advgrp_%d -p tcp -m length ! --length 50:51 -m datalen --offset 4 --byte 4 --add 10 -m layer7 --l7proto bt3 -j %s\n",
