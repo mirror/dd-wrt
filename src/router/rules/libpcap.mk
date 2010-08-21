7dc35ecf1b8fce1c082b9f3a4db5d2caf2abb0d4
BrainSlayer <s.gottschall@dd-wrt.com>
2010-08-21 11:48:56 +0000

    allow to link as shared too
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15002 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libpcap.mk
+++ src/router/rules/libpcap.mk
@@ -1,8 +1,18 @@
 libpcap:
-	$(MAKE) -C libpcap CC=$(CC) AR=$(AR) RANLIB=$(RANLIB) libpcap.so 
+#	$(MAKE) -C libpcap CC=$(CC) AR=$(AR) RANLIB=$(RANLIB) libpcap.so 
+ifeq ($(CONFIG_LIBPCAP_SHARED),y)
+	$(MAKE) -C libpcap_noring CC=$(CC) AR=$(AR) RANLIB=$(RANLIB) libpcap.so 
+else
 	$(MAKE) -C libpcap_noring CC=$(CC) AR=$(AR) RANLIB=$(RANLIB) libpcap.a 
+endif
 
 libpcap-install:
+	@true
+ifeq ($(CONFIG_LIBPCAP_SHARED),y)
 	install -d $(INSTALLDIR)/libpcap/usr/lib
-	install libpcap/libpcap.so $(INSTALLDIR)/libpcap/usr/lib
-	$(STRIP) $(INSTALLDIR)/libpcap/usr/lib/libpcap.so
+	install libpcap_noring/libpcap.so.1.0.0 $(INSTALLDIR)/libpcap/usr/lib
+endif
+
+libpcap-clean:
+	$(MAKE) -C libpcap_noring clean
+
