56046a0b4f3fb507e9565cc9c28c5a0235e88866
eko <USER@DOMAIN>
2010-08-29 08:04:29 +0000

    fix relayd gui
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15024 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/dd-wrt.c
+++ src/router/httpd/visuals/dd-wrt.c
@@ -3797,6 +3797,9 @@ void ej_show_wireless_single(webs_t wp, char *prefix)
 			websWrite(wp,
 				  "document.write(\"<option value=\\\"wet\\\" %s >\" + wl_basic.clientBridge + \"</option>\");\n",
 #endif
+					  nvram_match(wl_mode,
+						       "wet") ? 
+					  "selected=\\\"selected\\\"" : "");
 #endif
 			if (!cpeonly)
 				websWrite(wp,
