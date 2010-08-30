588ec0d4d1acc8e3febea1a0d6678a6306de09c6
eko <USER@DOMAIN>
2010-08-30 06:11:12 +0000

    Belkin Play support (f7d4302v1)
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15041 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/resetbutton.c
+++ src/router/rc/resetbutton.c
@@ -704,6 +704,7 @@ void period_check(int sig)
 	case ROUTER_WRT610N:
 	case ROUTER_ASUS_RTN16:
 	case ROUTER_BELKIN_F7D4301:
+	case ROUTER_BELKIN_F7D4302:
 		sesgpio = 0x108;	// gpio 8, inversed
 		break;
 	case ROUTER_ASUS_WL500W:
