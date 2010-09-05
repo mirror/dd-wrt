29018916355afbe46bb45b0a757a9a2ef853228b
ChristianScheele <scheele@newmedia-net.de>
2010-09-05 17:16:15 +0000

    hostapd-wps rule change
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15075 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/hostapd-wps.mk
+++ src/router/rules/hostapd-wps.mk
@@ -1,3 +1,10 @@
+ifeq ($(CONFIG_ATH9K),y)
+ATH9K_CFLAGS=-I$(TOP)/libnl-tiny/include \
+	-DCONFIG_LIBNL20 \
+	-D_GNU_SOURCE
+ATH9K_LDFLAGS=-L$(TOP)/libnl-tiny/ -lm -lnl-tiny
+endif
+
 hostapd2: 
 	$(MAKE) -C hostapd-wps/hostapd clean
 	$(MAKE) -C hostapd-wps/wpa_supplicant clean
@@ -7,12 +14,13 @@ hostapd2:
 	` > hostapd-wps/.cflags
 
 	
-	$(MAKE) -C hostapd-wps/hostapd CFLAGS="$$(cat hostapd-wps/.cflags)"  MULTICALL=1 hostapd_cli hostapd_multi.a
-	$(MAKE) -C hostapd-wps/wpa_supplicant CFLAGS="$$(cat hostapd-wps/.cflags)" MULTICALL=1 wpa_cli wpa_supplicant_multi.a
+
+	$(MAKE) -C hostapd-wps/hostapd CFLAGS="$$(cat hostapd-wps/.cflags) $(ATH9K_CFLAGS)" CONFIG_ATH9K=$(CONFIG_ATH9K) MULTICALL=1 hostapd_cli hostapd_multi.a
+	$(MAKE) -C hostapd-wps/wpa_supplicant CFLAGS="$$(cat hostapd-wps/.cflags) $(ATH9K_CFLAGS)" CONFIG_ATH9K=$(CONFIG_ATH9K) MULTICALL=1 wpa_cli wpa_supplicant_multi.a
 	$(CC) $(COPTS) -L$(TOP)/nvram  -L$(TOP)/libutils -Wall -ffunction-sections -fdata-sections -Wl,--gc-sections -o hostapd-wps/wpad hostapd-wps/multicall/multicall.c \
 		hostapd-wps/hostapd/hostapd_multi.a \
 		hostapd-wps/wpa_supplicant/wpa_supplicant_multi.a \
-		-lutils -lnvram
+		$(ATH9K_LDFLAGS) -lutils -lnvram
 
 hostapd2-clean:
 	$(MAKE) -C hostapd-wps/hostapd clean
