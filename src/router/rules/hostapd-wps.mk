a152ab6aec606111977779a45a71fb488f9d7b6a
BrainSlayer <s.gottschall@dd-wrt.com>
2010-09-06 11:01:11 +0000

    use correct config
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15080 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/hostapd-wps.mk
+++ src/router/rules/hostapd-wps.mk
@@ -15,8 +15,8 @@ hostapd2:
 
 	
 
-	$(MAKE) -C hostapd-wps/hostapd CFLAGS="$$(cat hostapd-wps/.cflags) $(ATH9K_CFLAGS)" CONFIG_ATH9K=$(CONFIG_ATH9K) CONFIG_DD_DIST=$(CONFIG_DIST) MULTICALL=1 hostapd_cli hostapd_multi.a
-	$(MAKE) -C hostapd-wps/wpa_supplicant CFLAGS="$$(cat hostapd-wps/.cflags) $(ATH9K_CFLAGS)" CONFIG_ATH9K=$(CONFIG_ATH9K) CONFIG_DD_DIST=$(CONFIG_DIST) MULTICALL=1 wpa_cli wpa_supplicant_multi.a
+	$(MAKE) -C hostapd-wps/hostapd CFLAGS="$$(cat hostapd-wps/.cflags) $(ATH9K_CFLAGS)" CONFIG_ATH9K=$(CONFIG_ATH9K) MULTICALL=1 hostapd_cli hostapd_multi.a
+	$(MAKE) -C hostapd-wps/wpa_supplicant CFLAGS="$$(cat hostapd-wps/.cflags) $(ATH9K_CFLAGS)" CONFIG_ATH9K=$(CONFIG_ATH9K) MULTICALL=1 wpa_cli wpa_supplicant_multi.a
 	$(CC) $(COPTS) -L$(TOP)/nvram  -L$(TOP)/libutils -Wall -ffunction-sections -fdata-sections -Wl,--gc-sections -o hostapd-wps/wpad hostapd-wps/multicall/multicall.c \
 		hostapd-wps/hostapd/hostapd_multi.a \
 		hostapd-wps/wpa_supplicant/wpa_supplicant_multi.a \
--- src/router/rules/madwifi_mimo.mk
+++ src/router/rules/madwifi_mimo.mk
@@ -1,145 +1,38 @@
-ifeq ($(ARCH),mipsel)
+madwifi_mimo-checkout:
+	rm -rf $(TOP)/madwifi.dev/madwifi_mimo.dev
+	svn co svn://svn.dd-wrt.com/private/madwifi_mimo $(TOP)/madwifi.dev/madwifi_mimo.dev
+
+madwifi_mimo-update:
+	svn update $(TOP)/madwifi.dev/madwifi_mimo.dev
+
+ifeq ($(CONFIG_AHB),y)
 madwifi_mimo:
-	make -C madwifi.dev/madwifi.dev KERNELPATH=$(LINUXDIR) TARGET=mipsisa32-le-elf
-	make -C madwifi.dev/madwifi.dev/tools TARGET=mipsisa32-le-elf BINDIR=$(INSTALLDIR)/madwifi/usr/sbin
+	make -C madwifi.dev/madwifi_mimo.dev/core/hal/linux TARGET=$(ARCHITECTURE)-elf AR9100=1 AR5416_G_MODE=1 GSOAP_WITH_LEANER=1
+	make -C madwifi.dev/madwifi_mimo.dev KERNELPATH=$(LINUXDIR) TARGET=$(ARCHITECTURE)-elf  AR9100=1 AR5416_G_MODE=1 GSOAP_WITH_LEANER=1 BUS=AHB
+	make -C madwifi.dev/madwifi_mimo.dev/tools KERNELPATH=$(LINUXDIR) TARGET=$(ARCHITECTURE)-elf BINDIR=$(INSTALLDIR)/madwifi/usr/sbin  AR9100=1 AR5416_G_MODE=1 GSOAP_WITH_LEANER=1 BUS=AHB
 
 madwifi_mimo-clean:
-ifeq ($(CONFIG_MADWIFI),y)
-	make -C madwifi.dev/madwifi.dev clean KERNELPATH=$(LINUXDIR) TARGET=mipsisa32-le-elf
-	make -C madwifi.dev/madwifi.dev/tools BINDIR=$(INSTALLDIR)/madwifi/usr/sbin clean
-else
-	@true
-endif
+	make -C madwifi.dev/madwifi_mimo.dev/core/hal/linux TARGET=$(ARCHITECTURE)-elf clean
+	make -C madwifi.dev/madwifi_mimo.dev clean KERNELPATH=$(LINUXDIR) TARGET=$(ARCHITECTURE)-elf
+	make -C madwifi.dev/madwifi_mimo.dev/tools KERNELPATH=$(LINUXDIR) BINDIR=$(INSTALLDIR)/madwifi/usr/sbin clean
 
 madwifi_mimo-install:
 	mkdir -p $(INSTALLDIR)/madwifi/usr/sbin
-	make -C madwifi.dev/madwifi.dev/tools BINDIR=$(INSTALLDIR)/madwifi/usr/sbin install
-	make -C madwifi.dev/madwifi.dev KERNELPATH=$(LINUXDIR) BINDIR=/usr/sbin DESTDIR=$(INSTALLDIR)/madwifi TARGET=mipsisa32-le-elf install
-endif
-
-ifeq ($(ARCH),armeb)
-madwifi_mimo:
-ifeq ($(CONFIG_BOESE),y)
-	make -C madwifi_mimo KERNELPATH=$(LINUXDIR) TARGET=xscale-boese-be-elf  
-	make -C madwifi_mimo/tools CFLAGS="-DBOESE=1 $(COPTS)" TARGET=xscale-boese-be-elf BINDIR=$(INSTALLDIR)/madwifi/usr/sbin
+	make -C madwifi.dev/madwifi_mimo.dev/tools KERNELPATH=$(LINUXDIR) BINDIR=/madwifi/usr/sbin DESTDIR=$(INSTALLDIR) install   AR9100=1 AR5416_G_MODE=1 GSOAP_WITH_LEANER=1 BUS=AHB
+	make -C madwifi.dev/madwifi_mimo.dev KERNELPATH=$(LINUXDIR) BINDIR=/usr/sbin DESTDIR=$(INSTALLDIR)/madwifi MODULEPATH=/lib/80211n  TARGET=mipsisa32-be-elf install   AR9100=1 AR5416_G_MODE=1 GSOAP_WITH_LEANER=1 BUS=AHB
 else
-	make -C madwifi_mimo KERNELPATH=$(LINUXDIR) TARGET=xscale-$(MADFLAG)be-elf  
-	make -C madwifi_mimo/tools CFLAGS="$(CONFIG_MADWIFIFLAGS) $(COPTS) -DNEED_PRINTF" TARGET=xscale-$(MADFLAG)be-elf BINDIR=$(INSTALLDIR)/madwifi/usr/sbin 
-endif
-madwifi_mimo-clean:
-	make -C madwifi_mimo clean KERNELPATH=$(LINUXDIR) TARGET=xscale-$(MADFLAG)be-elf
-	make -C madwifi_mimo/tools BINDIR=$(INSTALLDIR)/madwifi/usr/sbin clean
-
-madwifi_mimo-install:
-	mkdir -p $(INSTALLDIR)/madwifi_mimo/usr/sbin
-#	make -C madwifi_mimo/tools BINDIR=$(INSTALLDIR)/madwifi_mimo/usr/ath/sbin install
-	make -C madwifi_mimo KERNELPATH=$(LINUXDIR) BINDIR=/usr/ath/sbin DESTDIR=$(INSTALLDIR)/madwifi_mimo TARGET=xscale-$(MADFLAG)be-elf install
-endif
-
-
-ifeq ($(ARCH),mips)
-ifeq ($(ARCHITECTURE),fonera)
 madwifi_mimo:
-	make -C madwifi.dev/madwifi.dev KERNELPATH=$(LINUXDIR) TARGET=ap51 BUS=AHB TOOLPATH=$(LINUXDIR) LDOPTS="--no-warn-mismatch"
-	make -C madwifi.dev/madwifi.dev/tools TARGET=ap51 BINDIR=$(INSTALLDIR)/madwifi/usr/sbin BUS=AHB TOOLPATH=$(LINUXDIR)
+	make -C madwifi.dev/madwifi_mimo.dev/core/hal/linux TARGET=$(ARCHITECTURE)-elf AR5416_G_MODE=1 GSOAP_WITH_LEANER=1
+	make -C madwifi.dev/madwifi_mimo.dev KERNELPATH=$(LINUXDIR) TARGET=$(ARCHITECTURE)-elf AR5416_G_MODE=1 GSOAP_WITH_LEANER=1
+	make -C madwifi.dev/madwifi_mimo.dev/tools KERNELPATH=$(LINUXDIR) TARGET=$(ARCHITECTURE)-elf BINDIR=$(INSTALLDIR)/madwifi/usr/sbin AR5416_G_MODE=1 GSOAP_WITH_LEANER=1
 
 madwifi_mimo-clean:
-	make -C madwifi.dev/madwifi.dev clean KERNELPATH=$(LINUXDIR) TARGET=ap51 BUS=AHB TOOLPATH=$(LINUXDIR)
-	make -C madwifi.dev/madwifi.dev/tools BINDIR=$(INSTALLDIR)/madwifi/usr/sbin clean BUS=AHB TOOLPATH=$(LINUXDIR)
+	make -C madwifi.dev/madwifi_mimo.dev/core/hal/linux TARGET=$(ARCHITECTURE)-elf clean
+	make -C madwifi.dev/madwifi_mimo.dev clean KERNELPATH=$(LINUXDIR) TARGET=$(ARCHITECTURE)-elf
+	make -C madwifi.dev/madwifi_mimo.dev/tools KERNELPATH=$(LINUXDIR) BINDIR=$(INSTALLDIR)/madwifi/usr/sbin clean
 
 madwifi_mimo-install:
 	mkdir -p $(INSTALLDIR)/madwifi/usr/sbin
-	make -C madwifi.dev/madwifi.dev/tools BINDIR=$(INSTALLDIR)/madwifi/usr/sbin BUS=AHB TOOLPATH=$(LINUXDIR) install
-	make -C madwifi.dev/madwifi.dev/ KERNELPATH=$(LINUXDIR) BINDIR=/usr/sbin BUS=AHB TOOLPATH=$(LINUXDIR) DESTDIR=$(INSTALLDIR)/madwifi TARGET=ap51 install
-else
-ifeq ($(ARCHITECTURE),whrag108)
-madwifi_mimo:
-#	make -C madwifi.dev/madwifi.dev/diag TARGET=xscale-$(MADFLAG)be-elf BINDIR=$(INSTALLDIR)/madwifi/usr/sbin 
-ifeq ($(CONFIG_BOESE),y)
-	make -C madwifi.dev/madwifi.dev KERNELPATH=$(LINUXDIR) TARGET=ap30-boese-be-elf  BUS=AHB   TOOLPATH=$(LINUXDIR)
-	make -C madwifi.dev/madwifi.dev/tools CFLAGS="-DBOESE=1 $(COPTS)" TARGET=ap30-boese-be-elf BUS=AHB  BINDIR=$(INSTALLDIR)/madwifi/usr/sbin  TOOLPATH=$(LINUXDIR)
-else
-	make -C madwifi.dev/madwifi.dev KERNELPATH=$(LINUXDIR) TARGET=ap30-$(MADFLAG)  BUS=AHB   TOOLPATH=$(LINUXDIR)
-	make -C madwifi.dev/madwifi.dev/tools CFLAGS="$(CONFIG_MADWIFIFLAGS) $(COPTS) -DNEED_PRINTF" TARGET=ap30-$(MADFLAG)elf  TOOLPATH=$(LINUXDIR) BUS=AHB BINDIR=$(INSTALLDIR)/madwifi/usr/sbin 
-endif
-
-madwifi_mimo-clean:
-#	make -C madwifi.dev/madwifi.dev/diag TARGET=xscale-$(MADFLAG)be-elf BINDIR=$(INSTALLDIR)/madwifi/usr/sbin clean
-ifeq ($(CONFIG_BOESE),y)
-	make -C madwifi.dev/madwifi.dev clean KERNELPATH=$(LINUXDIR) TARGET=ap30-boese-be-elf  BUS=AHB  TOOLPATH=$(LINUXDIR)
-else
-	make -C madwifi.dev/madwifi.dev clean KERNELPATH=$(LINUXDIR) TARGET=ap30-$(MADFLAG) BUS=AHB  TOOLPATH=$(LINUXDIR)
-endif
-	make -C madwifi.dev/madwifi.dev/tools BINDIR=$(INSTALLDIR)/madwifi/usr/sbin clean 
-
-madwifi_mimo-install:
-	mkdir -p $(INSTALLDIR)/madwifi/usr/sbin
-	make -C madwifi.dev/madwifi.dev/tools BINDIR=$(INSTALLDIR)/madwifi/usr/sbin install
-#	make -C madwifi.dev/madwifi.dev/diag TARGET=xscale-$(MADFLAG)be-elf BINDIR=$(INSTALLDIR)/madwifi/usr/sbin install
-ifeq ($(CONFIG_BOESE),y)
-	make -C madwifi.dev/madwifi.dev KERNELPATH=$(LINUXDIR) BINDIR=/usr/sbin BUS=AHB TOOLPATH=$(LINUXDIR)  DESTDIR=$(INSTALLDIR)/madwifi TARGET=ap30-boese-be-elf install
-else
-	make -C madwifi.dev/madwifi.dev KERNELPATH=$(LINUXDIR) BINDIR=/usr/sbin BUS=AHB TOOLPATH=$(LINUXDIR)  DESTDIR=$(INSTALLDIR)/madwifi TARGET=ap30-$(MADFLAG) install
-endif
-else
-ifeq ($(ARCHITECTURE),ca8)
-madwifi_mimo:
-#	make -C madwifi.dev/madwifi.dev/diag TARGET=xscale-$(MADFLAG)be-elf BINDIR=$(INSTALLDIR)/madwifi/usr/sbin 
-ifeq ($(CONFIG_BOESE),y)
-	make -C madwifi.dev/madwifi.dev KERNELPATH=$(LINUXDIR) TARGET=ap43-boese-be-elf  BUS=AHB   TOOLPATH=$(LINUXDIR)
-	make -C madwifi.dev/madwifi.dev/tools CFLAGS="-DBOESE=1 $(COPTS)" TARGET=ap43-boese-be-elf BUS=AHB  BINDIR=$(INSTALLDIR)/madwifi/usr/sbin  TOOLPATH=$(LINUXDIR)
-else
-	make -C madwifi.dev/madwifi.dev KERNELPATH=$(LINUXDIR) TARGET=ap43-$(MADFLAG)  BUS=AHB   TOOLPATH=$(LINUXDIR)
-	make -C madwifi.dev/madwifi.dev/tools CFLAGS="$(CONFIG_MADWIFIFLAGS) $(COPTS) -DNEED_PRINTF" TARGET=ap43-$(MADFLAG)elf  TOOLPATH=$(LINUXDIR) BUS=AHB BINDIR=$(INSTALLDIR)/madwifi/usr/sbin 
-endif
-
-madwifi_mimo-clean:
-#	make -C madwifi.dev/madwifi.dev/diag TARGET=xscale-$(MADFLAG)be-elf BINDIR=$(INSTALLDIR)/madwifi/usr/sbin clean
-ifeq ($(CONFIG_BOESE),y)
-	make -C madwifi.dev/madwifi.dev clean KERNELPATH=$(LINUXDIR) TARGET=ap43-boese-be-elf  BUS=AHB  TOOLPATH=$(LINUXDIR)
-else
-	make -C madwifi.dev/madwifi.dev clean KERNELPATH=$(LINUXDIR) TARGET=ap43-$(MADFLAG) BUS=AHB  TOOLPATH=$(LINUXDIR)
-endif
-	make -C madwifi.dev/madwifi.dev/tools BINDIR=$(INSTALLDIR)/madwifi/usr/sbin clean 
-
-madwifi_mimo-install:
-	mkdir -p $(INSTALLDIR)/madwifi/usr/sbin
-	make -C madwifi.dev/madwifi.dev/tools BINDIR=$(INSTALLDIR)/madwifi/usr/sbin install
-#	make -C madwifi.dev/madwifi.dev/diag TARGET=xscale-$(MADFLAG)be-elf BINDIR=$(INSTALLDIR)/madwifi/usr/sbin install
-ifeq ($(CONFIG_BOESE),y)
-	make -C madwifi.dev/madwifi.dev KERNELPATH=$(LINUXDIR) BINDIR=/usr/sbin BUS=AHB TOOLPATH=$(LINUXDIR)  DESTDIR=$(INSTALLDIR)/madwifi TARGET=ap43-boese-be-elf install
-else
-	make -C madwifi.dev/madwifi.dev KERNELPATH=$(LINUXDIR) BINDIR=/usr/sbin BUS=AHB TOOLPATH=$(LINUXDIR)  DESTDIR=$(INSTALLDIR)/madwifi TARGET=ap43-$(MADFLAG) install
-endif
-
-else
-madwifi_mimo:
-	make -C madwifi_mimo/ KERNELPATH=$(LINUXDIR) TARGET=mipsisa32-be-elf
-	make -C madwifi_mimo/tools TARGET=mipsisa32-be-elf BINDIR=$(INSTALLDIR)/madwifi/usr/sbin
-
-madwifi_mimo-clean:
-	make -C madwifi_mimo/ clean KERNELPATH=$(LINUXDIR) TARGET=mipsisa32-be-elf
-	make -C madwifi_mimo/tools BINDIR=$(INSTALLDIR)/madwifi/usr/sbin clean
-
-madwifi_mimo-install:
-	mkdir -p $(INSTALLDIR)/madwifi/usr/sbin
-	make -C madwifi_mimo KERNELPATH=$(LINUXDIR) BINDIR=/usr/sbin DESTDIR=$(INSTALLDIR)/madwifi TARGET=mipsisa32-be-elf install
-endif
-endif
-endif
-endif
-
-ifeq ($(ARCH),i386)
-madwifi_mimo:
-	make -C madwifi_mimo KERNELPATH=$(LINUXDIR) TARGET=i386$(MADFLAG)-elf  
-	make -C madwifi_mimo/tools CFLAGS="$(CONFIG_MADWIFIFLAGS) $(COPTS) -DNEED_PRINTF" TARGET=i386$(MADFLAG)-elf BINDIR=$(INSTALLDIR)/madwifi/usr/sbin 
-
-madwifi_mimo-clean:
-	make -C madwifi_mimo clean KERNELPATH=$(LINUXDIR) TARGET=i386$(MADFLAG)-elf
-	make -C madwifi_mimo/tools BINDIR=$(INSTALLDIR)/madwifi/usr/sbin clean
-
-madwifi_mimo-install:
-	mkdir -p $(INSTALLDIR)/madwifi_mimo/usr/sbin
-#	make -C madwifi_mimo/tools BINDIR=$(INSTALLDIR)/madwifi_mimo/usr/ath/sbin install
-	make -C madwifi_mimo KERNELPATH=$(LINUXDIR) BINDIR=/usr/ath/sbin DESTDIR=$(INSTALLDIR)/madwifi_mimo TARGET=i386$(MADFLAG)-elf install
+	make -C madwifi.dev/madwifi_mimo.dev/tools KERNELPATH=$(LINUXDIR) BINDIR=/madwifi/usr/sbin DESTDIR=$(INSTALLDIR) install AR5416_G_MODE=1 GSOAP_WITH_LEANER=1
+	make -C madwifi.dev/madwifi_mimo.dev KERNELPATH=$(LINUXDIR) BINDIR=/usr/sbin DESTDIR=$(INSTALLDIR)/madwifi MODULEPATH=/lib/80211n  TARGET=$(ARCHITECTURE)-elf install AR5416_G_MODE=1 GSOAP_WITH_LEANER=1
 endif
