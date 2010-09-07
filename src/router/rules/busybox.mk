c768e344a69911488e74d4adc024f330695dac8d
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-09-07 11:49:55 +0000

    more missing arch
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15100 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/busybox.mk
+++ src/router/rules/busybox.mk
@@ -121,6 +121,14 @@ ifeq ($(ARCHITECTURE),wrt54g2v11)
 	cp busybox/.config_fonera busybox/.config
 	echo "# CONFIG_MKE2FS is not set" >> busybox/.config
 else
+ifeq ($(ARCHITECTURE),bs2)
+	cp busybox/.config_fonera busybox/.config
+	echo "# CONFIG_MKE2FS is not set" >> busybox/.config
+else
+ifeq ($(ARCHITECTURE),bwrg1000)
+	cp busybox/.config_fonera busybox/.config
+	echo "# CONFIG_MKE2FS is not set" >> busybox/.config
+else
 ifeq ($(ARCHITECTURE),eoc2610)
 	cp busybox/.config_fonera busybox/.config
 	echo "# CONFIG_MKE2FS is not set" >> busybox/.config
@@ -170,6 +178,8 @@ ifeq ($(ARCHITECTURE),danube)
 	echo "# CONFIG_MKE2FS is not set" >> busybox/.config
 else
 	cp busybox/.config_3com busybox/.config
+endif
+endif
 endif
 endif
 endif
