91c0d5aff61b80be9faf7427a1931486e29db48a
BrainSlayer <s.gottschall@dd-wrt.com>
2010-09-08 09:01:11 +0000

    remove debug message
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15108 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/hotplug.c
+++ src/router/rc/hotplug.c
@@ -38,7 +38,6 @@
 int main(int argc, char **argv)
 {
 	if (argc >= 2) {
-		fprintf(stderr, "hotplug %s\n", argv[1]);
 		if (!strcmp(argv[1], "net")) {
 			start_service("hotplug_net");
 			return 0;
