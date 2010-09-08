ff745cab6f0cc1a12253082a094f281e5ee001dc
BrainSlayer <s.gottschall@dd-wrt.com>
2010-09-08 12:47:50 +0000

    prepare
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15118 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/aoss.c
+++ src/router/services/services/aoss.c
@@ -48,45 +48,54 @@ void start_aoss(void)
 	strcpy(copy, vifbak);
 
 
-#if 0 //def HAVE_WZRHPAG300NH
+#ifdef HAVE_WZRHPAG300NH
 	char *vifbak2 = nvram_safe_get("ath1_vifs");
 	char copy2[256];
 	strcpy(copy2, vifbak2);
 #endif
 	sysprintf("startservice deconfigurewifi");
 	nvram_unset("ath0_vifs");
-#if 0//def HAVE_WZRHPAG300NH
+#ifdef HAVE_WZRHPAG300NH
 	nvram_unset("ath1_vifs");
 #endif
 	sysprintf("startservice configurewifi");
 	nvram_set("ath0_vifs", copy);
-#if 0//def HAVE_WZRHPAG300NH
+#ifdef HAVE_WZRHPAG300NH
 	nvram_set("ath1_vifs", copy2);
 #endif
 	nvram_commit();
 
-#if 0//def HAVE_WZRHPAG300NH
-	sysprintf("80211n_wlanconfig aoss2 create wlandev wifi1 wlanmode ap");
-	sysprintf("iwconfig aoss2 essid ESSID-AOSS");
-	sysprintf("iwpriv aoss2 authmode 4");
-	sysprintf("iwconfig aoss2 key [1] 4D454C434F");
-	sysprintf("iwconfig aoss2 key [1]");
-	sysprintf("ifconfig aoss2 0.0.0.0 up");
-	sysprintf("iptables -I OUTPUT -o aoss2 -j ACCEPT"); 
-	sysprintf("iptables -I INPUT -i aoss2 -j ACCEPT"); 
-#endif
+#ifdef HAVE_WZRHPAG300NH
+	sysprintf("80211n_wlanconfig aossa create wlandev wifi1 wlanmode ap");
+	sysprintf("iwconfig aossa essid ESSID-AOSS");
+	sysprintf("iwpriv aossa authmode 4");
+	sysprintf("iwconfig aossa key [1] 4D454C434F");
+	sysprintf("iwconfig aossa key [1]");
+	sysprintf("ifconfig aossa 0.0.0.0 up");
+
+	sysprintf("80211n_wlanconfig aossg create wlandev wifi0 wlanmode ap");
+	sysprintf("iwconfig aossg essid ESSID-AOSS");
+	sysprintf("iwpriv aossg authmode 4");
+	sysprintf("iwconfig aossg key [1] 4D454C434F");
+	sysprintf("iwconfig aossg key [1]");
+	sysprintf("ifconfig aossg 0.0.0.0 up");
+	//create aoss bridge
+	sysprintf("brctl addbr aoss");
+	sysprintf("ifconfig aoss 0.0.0.0 up");
+	sysprintf("brctl addif aoss aossa");
+	sysprintf("brctl addif aoss aossg");
+
+
+#else
 	sysprintf("80211n_wlanconfig aoss create wlandev wifi0 wlanmode ap");
 	sysprintf("iwconfig aoss essid ESSID-AOSS");
 	sysprintf("iwpriv aoss authmode 4");
 	sysprintf("iwconfig aoss key [1] 4D454C434F");
 	sysprintf("iwconfig aoss key [1]");
 	sysprintf("ifconfig aoss 0.0.0.0 up");
+#endif
 	sysprintf("iptables -I OUTPUT -o aoss -j ACCEPT"); 
 	sysprintf("iptables -I INPUT -i aoss -j ACCEPT"); 
-
-#if 0//def HAVE_WZRHPAG300NH
-	ret = eval("aoss", "-i", "aoss2", "-m", "ap");
-#endif
 	ret = eval("aoss", "-i", "aoss", "-m", "ap");
 	dd_syslog(LOG_INFO, "aoss : aoss daemon successfully started\n");
 	cprintf("done\n");
@@ -96,10 +105,6 @@ void start_aoss(void)
 void stop_aoss(void)
 {
 	stop_process("aoss", "buffalo aoss daemon");
-#if 0//def HAVE_WZRHPAG300NH
-	sysprintf("iptables -D OUTPUT -o aoss2 -j ACCEPT"); 
-	sysprintf("iptables -D INPUT -i aoss2 -j ACCEPT"); 
-#endif
 	sysprintf("iptables -D OUTPUT -o aoss -j ACCEPT"); 
 	sysprintf("iptables -D INPUT -i aoss -j ACCEPT"); 
 	return;
