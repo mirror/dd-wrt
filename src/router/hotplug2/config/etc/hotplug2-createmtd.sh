6913681187259048022d76ca50ff8c0142808043
ChristianScheele <scheele@newmedia-net.de>
2010-09-14 13:43:24 +0000

    hotplug2 fork after event might fix missiong devicenodes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15184 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/hotplug2/config/etc/hotplug2-createmtd.sh
+++ src/router/hotplug2/config/etc/hotplug2-createmtd.sh
@@ -6,11 +6,11 @@ then
         mkdir -p /dev/mtdblock >/dev/null 2>&1
         ln -s /dev/$DEVICENAME /dev/mtdblock/$MINDEV
 #boeshack
-	if [ ! -e /dev/$DEVICENAME ]
-	then
-		echo mknod /dev/$DEVICENAME b $MAJOR $MINOR >>/tmp/hotplug-missing-stuff.txt
-		mknod /dev/$DEVICENAME b $MAJOR $MINOR
-	fi
+#	if [ ! -e /dev/$DEVICENAME ]
+#	then
+#		echo mknod /dev/$DEVICENAME b $MAJOR $MINOR >>/tmp/hotplug-missing-stuff.txt
+#		mknod /dev/$DEVICENAME b $MAJOR $MINOR
+#	fi
 else
         MINDEV=`echo $DEVICENAME | cut -c4-`
         mkdir -p /dev/mtd >/dev/null 2>&1
