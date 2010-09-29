d88b6ede7a88832ab26f7ec1db01eefbd60f0186
BrainSlayer <s.gottschall@dd-wrt.com>
2010-09-29 16:40:01 +0000

    use private symlink way for better maintainance
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15312 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/httpd.mk
+++ src/router/rules/httpd.mk
@@ -1,9 +1,3 @@
-httpd-checkout:
-	rm -rf $(TOP)/register
-	svn co svn://svn.dd-wrt.com/private/register $(TOP)/register
-
-httpd-update:
-	svn update $(TOP)/register
 
 
 httpd: nvram shared hsiab matrixssl www wireless-tools
