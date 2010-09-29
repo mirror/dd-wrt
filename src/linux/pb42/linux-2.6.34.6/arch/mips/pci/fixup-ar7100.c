5806b296c18219fdaed9f65380a5528ad0f9deec
BrainSlayer <s.gottschall@dd-wrt.com>
2010-09-29 11:02:19 +0000

    auto detect calibration data
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15309 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/pb42/linux-2.6.34.6/arch/mips/pci/fixup-ar7100.c
+++ src/linux/pb42/linux-2.6.34.6/arch/mips/pci/fixup-ar7100.c
@@ -1,6 +1,5 @@
 #include <linux/init.h>
 #include <linux/pci.h>
-#include <linux/ath9k_platform.h>
 #include "ar7100.h"
 
 /*
@@ -28,33 +27,11 @@ int __init pcibios_map_irq(const struct pci_dev *dev, u8 slot, u8 pin)
     }
 #endif
 }
-static struct ath9k_platform_data ap94_wmac0_data;
-static struct ath9k_platform_data ap94_wmac1_data;
-
-#define CALDATA0_OFFSET		0x1000
-#define CALDATA1_OFFSET		0x5000
  
 
 int 
 pcibios_plat_dev_init(struct pci_dev *dev)
 {
-	u8 *art = (u8 *) KSEG1ADDR(0x1fff0000);
-
-	ap94_wmac0_data.slot = 0;
-	ap94_wmac1_data.slot = 1;
-
-	printk(KERN_EMERG "init ATH9k WMAC %d\n",PCI_SLOT(dev->devfn));
-	switch(PCI_SLOT(dev->devfn)) {
-	case 0:
-		memcpy(ap94_wmac0_data.eeprom_data, art + CALDATA0_OFFSET,sizeof(ap94_wmac0_data.eeprom_data));
-		dev->dev.platform_data = &ap94_wmac0_data;
-		break;
-
-	case 1:
-		memcpy(ap94_wmac1_data.eeprom_data, art + CALDATA1_OFFSET,sizeof(ap94_wmac1_data.eeprom_data));
-		dev->dev.platform_data = &ap94_wmac1_data;
-		break;
-	}
         return 0;
 }
 
--- src/linux/pb42/linux-2.6.34.6/arch/mips/pci/fixup-ar7240.c
+++ src/linux/pb42/linux-2.6.34.6/arch/mips/pci/fixup-ar7240.c
@@ -1,6 +1,5 @@
 #include <linux/init.h>
 #include <linux/pci.h>
-#include <linux/ath9k_platform.h>
 #include "ar7240.h"
 
 /*
@@ -29,24 +28,9 @@ int __init pcibios_map_irq(const struct pci_dev *dev, u8 slot, u8 pin)
 #endif
 }
 
-static struct ath9k_platform_data ap91_wmac0_data;
-
-#define CALDATA0_OFFSET		0x1000
-
 int 
 pcibios_plat_dev_init(struct pci_dev *dev)
 {
-	u8 *art = (u8 *) KSEG1ADDR(0x1fff0000);
-
-	ap91_wmac0_data.slot = 0;
-
-	printk(KERN_EMERG "init ATH9k WMAC %d\n",PCI_SLOT(dev->devfn));
-	switch(PCI_SLOT(dev->devfn)) {
-	case 0:
-		memcpy(ap91_wmac0_data.eeprom_data, art + CALDATA0_OFFSET,sizeof(ap91_wmac0_data.eeprom_data));
-		dev->dev.platform_data = &ap91_wmac0_data;
-		break;
-	}
         return 0;
 }
 
