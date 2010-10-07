00b6072523cbf684dfb546572868362f51f026bd
BrainSlayer <s.gottschall@dd-wrt.com>
2010-10-07 11:43:26 +0000

    swap utilities
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15385 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/madwifi_mimo.mk
+++ src/router/rules/madwifi_mimo.mk
@@ -17,7 +17,7 @@ endif
 madwifi_mimo-install:
 	mkdir -p $(INSTALLDIR)/madwifi/usr/sbin
 	make -C madwifi.dev/madwifi_mimo.dev/tools KERNELPATH=$(LINUXDIR) BINDIR=/madwifi/usr/sbin DESTDIR=$(INSTALLDIR) install   AR9100=1 AR5416_G_MODE=1 GSOAP_WITH_LEANER=1 BUS=AHB
-	make -C madwifi.dev/madwifi_mimo.dev KERNELPATH=$(LINUXDIR) BINDIR=/usr/sbin DESTDIR=$(INSTALLDIR)/madwifi MODULEPATH=/lib/80211n  TARGET=mipsisa32-be-elf install   AR9100=1 AR5416_G_MODE=1 GSOAP_WITH_LEANER=1 BUS=AHB
+	make -C madwifi.dev/madwifi_mimo.dev KERNELPATH=$(LINUXDIR) BINDIR=/usr/sbin DESTDIR=$(INSTALLDIR)/madwifi MODULEPATH=/lib/80211n  TARGET=$(ARCHITECTURE)-elf install   AR9100=1 AR5416_G_MODE=1 GSOAP_WITH_LEANER=1 BUS=AHB
 else
 madwifi_mimo:
 	make -C madwifi.dev/madwifi_mimo.dev/core/hal/linux TARGET=$(ARCHITECTURE)-elf AR5416_G_MODE=1 GSOAP_WITH_LEANER=1
