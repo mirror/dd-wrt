f48bd6e1ca3c6343a43d5006e67c8000ea0f7cbf
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-10-09 11:11:56 +0000

    update usb modeswitch
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15408 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/usb_modeswitch/usb_modeswitch.h
+++ src/router/usb_modeswitch/usb_modeswitch.h
@@ -2,9 +2,8 @@
   This file is part of usb_modeswitch, a mode switching tool for controlling
   flip flop (multiple device) USB gear
 
-  Copyright (C) 2007, 2008  Josua Dietze
-
-  Created with help from usbsnoop2libusb.pl (http://iki.fi/lindi/usb/usbsnoop2libusb.pl)
+  Version 1.1.4, 2010/08/17
+  Copyright (C) 2007, 2008, 2009, 2010  Josua Dietze
 
   Config file parsing stuff borrowed from Guillaume Dargaud
   (http://www.gdargaud.net/Hack/SourceCode.html)
@@ -26,6 +25,37 @@
 
 #include <stdlib.h>
 #include <usb.h>
+
+void readConfigFile(const char *configFilename);
+void printConfig();
+int switchSendMessage();
+int switchConfiguration();
+int switchAltSetting();
+void switchHuaweiMode();
+void switchSierraMode();
+void switchGCTMode();
+void switchAVMMode();
+int switchSonyMode();
+int detachDriver();
+int checkSuccess();
+int sendMessage(char* message, int count);
+int write_bulk(int endpoint, char *message, int length);
+int read_bulk(int endpoint, char *buffer, int length);
+void release_usb_device(int dummy);
+struct usb_device* search_devices( int *numFound, int vendor, int product, char* productList, int targetClass, int mode);
+int find_first_bulk_output_endpoint(struct usb_device *dev);
+int find_first_bulk_input_endpoint(struct usb_device *dev);
+char* ReadParseParam(const char* FileName, char *VariableName);
+int hex2num(char c);
+int hex2byte(const char *hex);
+int hexstr2bin(const char *hex, char *buffer, int len);
+void printVersion();
+void printHelp();
+int readArguments(int argc, char **argv);
+void deviceDescription();
+int deviceInquire();
+void resetUSB();
+void release_usb_device(int dummy);
 
 // Boolean
 #define  and     &&
@@ -68,9 +98,3 @@ extern char *TempPP;
 	if ((TempPP=ReadParseParam((ParamFileName), #B))!=NULL) \
 		B=(toupper(TempPP[0])=='Y' || toupper(TempPP[0])=='T'|| TempPP[0]=='1'); else B=0
 
-
-void release_usb_device(int dummy);
-
-struct usb_device* search_devices(int *numFound, int vendor, int product, int targetClass);
-
-int hexstr2bin(const char *hex, char *buf, int len);
