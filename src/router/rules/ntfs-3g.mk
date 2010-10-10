1e357106bf00435c49466ff1ddf1c6867fa9efce
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-10-10 11:30:30 +0000

    add ntfs-3g
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15421 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/ntfs-3g.mk
+++ src/router/rules/ntfs-3g.mk
@@ -8,12 +8,12 @@ ntfs-3g:
 
 ntfs-3g-install:
 	install -D ntfs-3g/src/ntfs-3g $(INSTALLDIR)/ntfs-3g/usr/sbin/ntfs-3g
-	install -D ntfs-3g/src/ntfs-3g.probe $(INSTALLDIR)/ntfs-3g/usr/sbin/ntfs-3g.probe
+#	install -D ntfs-3g/src/ntfs-3g.probe $(INSTALLDIR)/ntfs-3g/usr/sbin/ntfs-3g.probe
 	$(STRIP) $(INSTALLDIR)/ntfs-3g/usr/sbin/ntfs-3g
-	$(STRIP) $(INSTALLDIR)/ntfs-3g/usr/sbin/ntfs-3g.probe
+#	$(STRIP) $(INSTALLDIR)/ntfs-3g/usr/sbin/ntfs-3g.probe
 
 ntfs-3g-clean:
 	$(MAKE) -C ntfs-3g clean
 
 ntfs-3g-configure:
-	cd ntfs-3g && ./configure --prefix=/usr --with-fuse=$(TOP)/ntfs-3g/fuse --target=$(ARCH)-linux --host=$(ARCH) CC=$(ARCH)-linux-uclibc-gcc
+	cd ntfs-3g && ./configure --prefix=/usr --with-fuse=internal --target=$(ARCH)-linux --host=$(ARCH) CC=$(ARCH)-linux-uclibc-gcc
