9c3fbd7ff15ca4bb58682743172db4663e2a4313
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-10-25 23:18:05 +0000

    clear dns cache to override dyndns bug, where one of the multiple ipcheck servers isnt reachable
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15506 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/inadyn/src/ip.c
+++ src/router/inadyn/src/ip.c
@@ -18,6 +18,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define MODULE_TAG "INADYN:IP: "
 #include <stdlib.h>
 #include <string.h>
+#include <resolv.h>
 
 #include "debug_if.h"
 #include "ip.h"
