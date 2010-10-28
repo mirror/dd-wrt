7d0e43d5305ed30f810efd702f370a7c352bcacf
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-10-28 14:58:06 +0000

    add wps file
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15538 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/httpd/validate/wps.c
@@ -0,0 +1,53 @@
+#ifdef HAVE_WPS
+#define VALIDSOURCE 1
+
+#ifdef WEBS
+#include <webs.h>
+#include <uemf.h>
+#include <ej.h>
+#else				/* !WEBS */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <httpd.h>
+#include <errno.h>
+#endif				/* WEBS */
+
+#include <proto/ethernet.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/klog.h>
+#include <sys/wait.h>
+#include <cyutils.h>
+#include <support.h>
+#include <cy_conf.h>
+// #ifdef EZC_SUPPORT
+#include <ezc.h>
+// #endif
+#include <broadcom.h>
+#include <wlutils.h>
+#include <netdb.h>
+#include <utils.h>
+#include <stdarg.h>
+#include <sha1.h>
+
+struct variable **variables;
+
+void wps_register(webs_t wp)
+{
+	char *pin = websGetVar(wp, "wps_pin", NULL);
+	if (pin) {
+		sysprintf("hostapd_cli -i ath0 wps_pin any %s", pin);
+		sysprintf("hostapd_cli -i ath1 wps_pin any %s", pin);
+	}
+}
+#endif
