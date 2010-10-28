a671228150e3b1e6f171ce4aca5487e4ee4ae7c5
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-10-28 18:27:07 +0000

    set timeout to 300 seconds
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15554 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/validate/wps.c
+++ src/router/httpd/validate/wps.c
@@ -46,9 +46,9 @@ void wps_register(webs_t wp)
 {
 	char *pin = websGetVar(wp, "wps_pin", NULL);
 	if (pin) {
-		sysprintf("hostapd_cli -i ath0 wps_pin any %s", pin);
+		sysprintf("hostapd_cli -i ath0 wps_pin any %s 300", pin);
 #ifdef HAVE_WZRHPAG300NH
-		sysprintf("hostapd_cli -i ath1 wps_pin any %s", pin);
+		sysprintf("hostapd_cli -i ath1 wps_pin any %s 300", pin);
 #endif
 	}
 }
