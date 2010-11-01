3b5a35b46ce27c445edc7279d7b872b95c00b971
sash <sash@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-11-01 02:35:43 +0000

    ovpn:  gui status format
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15612 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/openvpn/config/openvpnstate.sh
+++ src/router/openvpn/config/openvpnstate.sh
@@ -1,3 +1,3 @@
-echo "`/bin/echo "state" | /usr/bin/nc 127.0.0.1 5001 |awk -F"," '{ print $2}'`: `/bin/echo "state" | /usr/bin/nc 127.0.0.1 5001 |awk -F"," '{print $3}'`"
-echo "Local Address: `/bin/echo "state" | /usr/bin/nc 127.0.0.1 5001 |awk -F"," '{print $4}'`"
+echo "`/bin/echo "state" | /usr/bin/nc 127.0.0.1 5001 |awk -F"," '{ print $2}'`: `/bin/echo "state" | /usr/bin/nc 127.0.0.1 5001 |awk -F"," '{print $3}'` - "
+echo "Local Address: `/bin/echo "state" | /usr/bin/nc 127.0.0.1 5001 |awk -F"," '{print $4}'` - "
 echo "Remote Address: `ifconfig tun0 |awk '{print $3}'|grep P-t-P|awk -F ":" '{print $2}'`"
