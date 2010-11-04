c2bebceb576d9cf58f95e2f5804662f6ba0b49ae
sash <sash@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-11-04 00:30:51 +0000

    ovpn: gui log update. not really beautiful but ok for now
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15674 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/openvpn/config/openvpnlog.sh
+++ src/router/openvpn/config/openvpnlog.sh
@@ -1,3 +1,17 @@
+#!/bin/sh
+/bin/echo -e "Serverlog \n"
+/bin/echo "log 500" | /usr/bin/nc 127.0.0.1 5002 | /bin/grep -v "^>" | /usr/bin/awk -F "," '{
+	printf strftime("%Y%m%d %H:%M:%S ",$1);
+	for (i=2;i<=NF;i++)
+		printf $i" "
+	printf "<br>\n";
+	}'
+
+/bin/echo -e "\n"
+/bin/echo -e "\n"
+/bin/echo -e "\n"
+
+/bin/echo -e "Clientlog \n"
 /bin/echo "log 500" | /usr/bin/nc 127.0.0.1 5001 | /bin/grep -v "^>" | /usr/bin/awk -F "," '{
 	printf strftime("%Y%m%d %H:%M:%S ",$1);
 	for (i=2;i<=NF;i++)
--- src/router/openvpn/config/openvpnstate.sh
+++ src/router/openvpn/config/openvpnstate.sh
@@ -1,3 +1,10 @@
-echo "`/bin/echo "state" | /usr/bin/nc 127.0.0.1 5001 |awk -F"," '{ print $2}'`: `/bin/echo "state" | /usr/bin/nc 127.0.0.1 5001 |awk -F"," '{print $3}'` - "
-echo "Local Address: `/bin/echo "state" | /usr/bin/nc 127.0.0.1 5001 |awk -F"," '{print $4}'` - "
-echo "Remote Address: `ifconfig tun0 |awk '{print $3}'|grep P-t-P|awk -F ":" '{print $2}'`"
+#!/bin/sh
+echo -e "Server: \n"
+echo -e "`/bin/echo "state" | /usr/bin/nc 127.0.0.1 5002 |awk -F"," '{ print $2}'`: `/bin/echo "state" | /usr/bin/nc 127.0.0.1 5001 |awk -F"," '{print $3}'` \n"
+echo -e "Local Address: `/bin/echo "state" | /usr/bin/nc 127.0.0.1 5001 |awk -F"," '{print $4}'` \n"
+echo -e "Remote Address: `ifconfig tun0 |awk '{print $3}'|grep P-t-P|awk -F ":" '{print $2}'`"
+
+echo -e "Client: \n"
+echo -e "`/bin/echo "state" | /usr/bin/nc 127.0.0.1 5001 |awk -F"," '{ print $2}'`: `/bin/echo "state" | /usr/bin/nc 127.0.0.1 5001 |awk -F"," '{print $3}'` \n"
+echo -e "Local Address: `/bin/echo "state" | /usr/bin/nc 127.0.0.1 5001 |awk -F"," '{print $4}'` \n"
+echo -e "Remote Address: `ifconfig tun0 |awk '{print $3}'|grep P-t-P|awk -F ":" '{print $2}'` \n" 
