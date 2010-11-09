cb9ba1de55279dc1e9ee68ed1b7c5b8a83ab5174
chris <chris@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-11-09 16:28:51 +0000

    ath9k list_channel changes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15708 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/wl_ath9k.c
+++ src/router/libutils/wl_ath9k.c
@@ -44,11 +44,6 @@
 
 #include <linux/nl80211.h>
 
-struct wifi_channels {
-	int channel;
-	int freq;
-	int noise;
-};
 
 // dummy TBD 
 int getassoclist_ath9k(char *ifname, unsigned char *list)
@@ -82,7 +77,7 @@ int getNoise_ath9k(char *ifname, unsigned char *mac)
 
 
 // dummy TBD  erstmal alles zum spielen
-static struct wifi_channels *list_channelsext_ath9k(const char *ifname, int allchans)
+static struct wifi_channels *list_channelsext_ath9k(const char *ifname, char country[2],int max_bandwidth_khz, unsigned char band)
 {
 	// get_ath9k_phy_idx
 	int i;
@@ -92,28 +87,79 @@ static struct wifi_channels *list_channelsext_ath9k(const char *ifname, int allc
 	struct wifi_channels *list =
 	    (struct wifi_channels *)safe_malloc(sizeof(struct wifi_channels) *
 					   (13+1+166-36+1));
+	(void)memset(list, 0, (sizeof(struct wifi_channels)*(3+1+166-36+1)));
 	for (i=0;i<13;i++) {
 		list[count].channel = i+1;
 		list[count].freq = 5*(i+1)+2407;
-		list[count].noise = -95;	// achans.ic_chans[i].ic_noise;
+		list[count].noise = -95;
+		if (i>3)
+			list[count].ht40minus = 1;
+		if (i<10)
+			list[count].ht40plus = 1;
+		list[count].outdoor = 1;
+		list[count].dfs = 0;
+		if (!strcmp(country,"DE"))
+			list[count].max_eirp = 20;
+		else
+			list[count].max_eirp = 27;
+		list[count].no_ofdm = 0;
 		count++;
-	}
+		}
 	list[count].channel = 14;
 	list[count].freq = 2484;
-	list[count].noise = -95;	// achans.ic_chans[i].ic_noise;
+	list[count].noise = -95;
+	list[count].no_ofdm = 1;
+	list[count].outdoor = 1;
+	list[count].no_ofdm = 1;
 	count++;
-	for (i=36;i<166;i++) {
-		list[count].channel = i+1;
-		list[count].freq = (i+1)*5+5000;
-		list[count].noise = -95;	// achans.ic_chans[i].ic_noise;
-		count++;
-	}
+	if (!strcmp(country,"DE"))
+		{
+		for (i=36;i<140;i++) {
+			list[count].channel = i+1;
+			list[count].freq = (i+1)*5+5000;
+			list[count].noise = -95;
+			if (i>38)
+				list[count].ht40minus = 1;
+			if (i<138)
+				list[count].ht40plus = 1;
+			if (i<=60)
+				list[count].outdoor = 0;
+			else
+				list[count].outdoor = 1;
+			if (i>=52)
+				list[count].dfs = 1;
+			list[count].max_eirp = 27;
+			list[count].no_ofdm = 0;
+			count++;
+			}
+		}
+	else
+		{
+		for (i=36;i<166;i++) {
+			list[count].channel = i+1;
+			list[count].freq = (i+1)*5+5000;
+			list[count].noise = -95;
+			if (i>38)
+				list[count].ht40minus = 1;
+			if (i<138)
+				list[count].ht40plus = 1;
+			if (i<=60)
+				list[count].outdoor = 0;
+			else
+				list[count].outdoor = 1;
+			if (i>=52)
+				list[count].dfs = 1;
+			list[count].max_eirp = 27;
+			list[count].no_ofdm = 0;
+			count++;
+			}
+		}
 
 	list[count++].freq = -1;
 	return list;
 }
 
-struct wifi_channels *list_channels_ath9k(char *devnr)
+struct wifi_channels *list_channels_ath9k(char *devnr, char country[2],int max_bandwidth_khz, unsigned char band)
 {
-	return list_channelsext_ath9k(devnr, 1);
+	return list_channelsext_ath9k(devnr, country,max_bandwidth_khz,band);
 }
