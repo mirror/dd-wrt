896e5803e23c8a1b2084a1614478c374fc4dcebf
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-11-20 00:30:29 +0000

    configure vlans for g450
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15769 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/interface.c
+++ src/router/services/networking/interface.c
@@ -597,6 +597,9 @@ int flush_interfaces(void)
 #elif HAVE_WR1043
 	snprintf(all_ifnames, 255, "%s %s %s", "vlan1 vlan2",
 		 nvram_safe_get("lan_ifnames"), nvram_safe_get("wan_ifnames"));
+#elif HAVE_WZRG450
+	snprintf(all_ifnames, 255, "%s %s %s", "vlan1 vlan2",
+		 nvram_safe_get("lan_ifnames"), nvram_safe_get("wan_ifnames"));
 #elif HAVE_AP83
 	snprintf(all_ifnames, 255, "%s %s %s", "eth0 eth1",
 		 nvram_safe_get("lan_ifnames"), nvram_safe_get("wan_ifnames"));
