8bd91fedeb39b92a81352eb88abbcc012d160a22
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-11-30 14:00:29 +0000

    replace with newer version
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15844 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/asterisk/doc/tex/asterisk/labels.pl
@@ -0,0 +1,53 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/field-table/;
+$external_labels{$key} = "$URL/" . q|node250.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/voicemail/;
+$external_labels{$key} = "$URL/" . q|node150.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/event-table/;
+$external_labels{$key} = "$URL/" . q|node250.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/odbcstorage/;
+$external_labels{$key} = "$URL/" . q|node272.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/trunks/;
+$external_labels{$key} = "$URL/" . q|node146.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2002-2-1 (1.71)
+# labels from external_latex_labels array.
+
+
+$key = q/field-table/;
+$external_latex_labels{$key} = q|11.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/voicemail/;
+$external_latex_labels{$key} = q|5.3.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/event-table/;
+$external_latex_labels{$key} = q|11.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/odbcstorage/;
+$external_latex_labels{$key} = q|12.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/trunks/;
+$external_latex_labels{$key} = q|5.2.3|; 
+$noresave{$key} = "$nosave";
+
+1;
+
