d1230f890e1dbb62e0826bcf7b274e50818d1fe7
BrainSlayer <s.gottschall@dd-wrt.com>
2010-12-02 16:10:42 +0000

    extended flags
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15869 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/madwifi_mimo.mk
+++ src/router/rules/madwifi_mimo.mk
@@ -2,6 +2,10 @@
 ifeq ($(CONFIG_BUFFALO),y)
 export EXTRA_VERSION=-buffalo
 endif
+ifeq ($(CONFIG_TX99),y)
+export EXTRA_CAPS="ATH_CAP_TX99=1"
+
+endif
 ifeq ($(CONFIG_NEXTMEDIA),y)
 export EXTRA_VERSION=-nextmedia
 endif
@@ -9,9 +13,9 @@ endif
 
 ifeq ($(CONFIG_AHB),y)
 madwifi_mimo:
-	make -C madwifi.dev/madwifi_mimo.dev/core/hal/linux TARGET=$(ARCHITECTURE)$(EXTRA_VERSION)-elf AR9100=1 AR5416_G_MODE=1 GSOAP_WITH_LEANER=1
-	make -C madwifi.dev/madwifi_mimo.dev KERNELPATH=$(LINUXDIR) TARGET=$(ARCHITECTURE)$(EXTRA_VERSION)-elf  AR9100=1 AR5416_G_MODE=1 GSOAP_WITH_LEANER=1 BUS=AHB
-	make -C madwifi.dev/madwifi_mimo.dev/tools KERNELPATH=$(LINUXDIR) TARGET=$(ARCHITECTURE)$(EXTRA_VERSION)-elf BINDIR=$(INSTALLDIR)/madwifi/usr/sbin  AR9100=1 AR5416_G_MODE=1 GSOAP_WITH_LEANER=1 BUS=AHB
+	make -C madwifi.dev/madwifi_mimo.dev/core/hal/linux TARGET=$(ARCHITECTURE)$(EXTRA_VERSION)-elf AR9100=1 AR5416_G_MODE=1 GSOAP_WITH_LEANER=1 $(EXTRA_CAPS)
+	make -C madwifi.dev/madwifi_mimo.dev KERNELPATH=$(LINUXDIR) TARGET=$(ARCHITECTURE)$(EXTRA_VERSION)-elf  AR9100=1 AR5416_G_MODE=1 GSOAP_WITH_LEANER=1 BUS=AHB $(EXTRA_CAPS)
+	make -C madwifi.dev/madwifi_mimo.dev/tools KERNELPATH=$(LINUXDIR) TARGET=$(ARCHITECTURE)$(EXTRA_VERSION)-elf BINDIR=$(INSTALLDIR)/madwifi/usr/sbin  AR9100=1 AR5416_G_MODE=1 GSOAP_WITH_LEANER=1 BUS=AHB $(EXTRA_CAPS)
 
 ifeq ($(CONFIG_MADWIFI),y)
 madwifi_mimo-clean:
@@ -24,13 +28,13 @@ endif
 
 madwifi_mimo-install:
 	mkdir -p $(INSTALLDIR)/madwifi/usr/sbin
-	make -C madwifi.dev/madwifi_mimo.dev/tools KERNELPATH=$(LINUXDIR) BINDIR=/madwifi/usr/sbin DESTDIR=$(INSTALLDIR) install   AR9100=1 AR5416_G_MODE=1 GSOAP_WITH_LEANER=1 BUS=AHB
-	make -C madwifi.dev/madwifi_mimo.dev KERNELPATH=$(LINUXDIR) BINDIR=/usr/sbin DESTDIR=$(INSTALLDIR)/madwifi MODULEPATH=/lib/80211n  TARGET=$(ARCHITECTURE)$(EXTRA_VERSION)-elf install   AR9100=1 AR5416_G_MODE=1 GSOAP_WITH_LEANER=1 BUS=AHB
+	make -C madwifi.dev/madwifi_mimo.dev/tools KERNELPATH=$(LINUXDIR) BINDIR=/madwifi/usr/sbin DESTDIR=$(INSTALLDIR) install   AR9100=1 AR5416_G_MODE=1 GSOAP_WITH_LEANER=1 BUS=AHB  $(EXTRA_CAPS)
+	make -C madwifi.dev/madwifi_mimo.dev KERNELPATH=$(LINUXDIR) BINDIR=/usr/sbin DESTDIR=$(INSTALLDIR)/madwifi MODULEPATH=/lib/80211n  TARGET=$(ARCHITECTURE)$(EXTRA_VERSION)-elf install   AR9100=1 AR5416_G_MODE=1 GSOAP_WITH_LEANER=1 BUS=AHB  $(EXTRA_CAPS)
 else
 madwifi_mimo:
-	make -C madwifi.dev/madwifi_mimo.dev/core/hal/linux TARGET=$(ARCHITECTURE)$(EXTRA_VERSION)-elf AR5416_G_MODE=1 GSOAP_WITH_LEANER=1
-	make -C madwifi.dev/madwifi_mimo.dev KERNELPATH=$(LINUXDIR) TARGET=$(ARCHITECTURE)$(EXTRA_VERSION)-elf AR5416_G_MODE=1 GSOAP_WITH_LEANER=1
-	make -C madwifi.dev/madwifi_mimo.dev/tools KERNELPATH=$(LINUXDIR) TARGET=$(ARCHITECTURE)$(EXTRA_VERSION)-elf BINDIR=$(INSTALLDIR)/madwifi/usr/sbin AR5416_G_MODE=1 GSOAP_WITH_LEANER=1
+	make -C madwifi.dev/madwifi_mimo.dev/core/hal/linux TARGET=$(ARCHITECTURE)$(EXTRA_VERSION)-elf AR5416_G_MODE=1 GSOAP_WITH_LEANER=1 $(EXTRA_CAPS)
+	make -C madwifi.dev/madwifi_mimo.dev KERNELPATH=$(LINUXDIR) TARGET=$(ARCHITECTURE)$(EXTRA_VERSION)-elf AR5416_G_MODE=1 GSOAP_WITH_LEANER=1 $(EXTRA_CAPS)
+	make -C madwifi.dev/madwifi_mimo.dev/tools KERNELPATH=$(LINUXDIR) TARGET=$(ARCHITECTURE)$(EXTRA_VERSION)-elf BINDIR=$(INSTALLDIR)/madwifi/usr/sbin AR5416_G_MODE=1 GSOAP_WITH_LEANER=1 $(EXTRA_CAPS)
 
 ifeq ($(CONFIG_MADWIFI),y)	
 madwifi_mimo-clean:
@@ -43,6 +47,6 @@ endif
 
 madwifi_mimo-install:
 	mkdir -p $(INSTALLDIR)/madwifi/usr/sbin
-	make -C madwifi.dev/madwifi_mimo.dev/tools KERNELPATH=$(LINUXDIR) BINDIR=/madwifi/usr/sbin DESTDIR=$(INSTALLDIR) install AR5416_G_MODE=1 GSOAP_WITH_LEANER=1
-	make -C madwifi.dev/madwifi_mimo.dev KERNELPATH=$(LINUXDIR) BINDIR=/usr/sbin DESTDIR=$(INSTALLDIR)/madwifi MODULEPATH=/lib/80211n  TARGET=$(ARCHITECTURE)$(EXTRA_VERSION)-elf install AR5416_G_MODE=1 GSOAP_WITH_LEANER=1
+	make -C madwifi.dev/madwifi_mimo.dev/tools KERNELPATH=$(LINUXDIR) BINDIR=/madwifi/usr/sbin DESTDIR=$(INSTALLDIR) install AR5416_G_MODE=1 GSOAP_WITH_LEANER=1  $(EXTRA_CAPS)
+	make -C madwifi.dev/madwifi_mimo.dev KERNELPATH=$(LINUXDIR) BINDIR=/usr/sbin DESTDIR=$(INSTALLDIR)/madwifi MODULEPATH=/lib/80211n  TARGET=$(ARCHITECTURE)$(EXTRA_VERSION)-elf install AR5416_G_MODE=1 GSOAP_WITH_LEANER=1  $(EXTRA_CAPS)
 endif
