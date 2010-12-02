f5f195f532513b36b209edd4ba06a91e1d4ef1da
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-12-02 16:13:30 +0000

    for micro
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15870 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/pptp-client.mk
+++ src/router/rules/pptp-client.mk
@@ -5,8 +5,10 @@ pptp-client-clean:
 	$(MAKE) -C pptp-client clean
 
 pptp-client-install:
-ifneq ($(CONFIG_NEWMEDIA),y)
 	install -D pptp-client/pptp $(INSTALLDIR)/pptp-client/usr/sbin/pptp
+ifneq ($(CONFIG_DIST),"micro")
+ifneq ($(CONFIG_DIST),"micro-special")
+ifneq ($(CONFIG_NEWMEDIA),y)
 	install -D pptp-client/config/pptpd_client.ip-up $(INSTALLDIR)/pptp-client/etc/config/pptpd_client.ip-up
 	install -D pptp-client/config/pptpd_client.ip-down $(INSTALLDIR)/pptp-client/etc/config/pptpd_client.ip-down
 	install -D pptp-client/config/pptpd_client.nvramconfig $(INSTALLDIR)/pptp-client/etc/config/pptpd_client.nvramconfig
@@ -14,9 +16,7 @@ ifneq ($(CONFIG_NEWMEDIA),y)
 	install -D pptp-client/config/pptpd_client.sh $(INSTALLDIR)/pptp-client/etc/config/pptpd_client.sh
 	install -D pptp-client/config/pptpd_client.startup $(INSTALLDIR)/pptp-client/etc/config/pptpd_client.startup
 	install -D pptp-client/config/pptpd_client.vpn $(INSTALLDIR)/pptp-client/etc/config/pptpd_client.vpn
-	install -D pptp-client/pptp $(INSTALLDIR)/pptp-client/usr/sbin/pptp
 else
-	install -D pptp-client/pptp $(INSTALLDIR)/pptp-client/usr/sbin/pptp
 	install -D pptp-client/config2/pptpd_client.ip-up $(INSTALLDIR)/pptp-client/etc/config/pptpd_client.ip-up
 	install -D pptp-client/config2/pptpd_client.ip-down $(INSTALLDIR)/pptp-client/etc/config/pptpd_client.ip-down
 	install -D pptp-client/config2/pptpd_client.nvramconfig $(INSTALLDIR)/pptp-client/etc/config/pptpd_client.nvramconfig
@@ -24,6 +24,7 @@ else
 	install -D pptp-client/config2/pptpd_client.sh $(INSTALLDIR)/pptp-client/etc/config/pptpd_client.sh
 	install -D pptp-client/config2/pptpd_client.wanup $(INSTALLDIR)/pptp-client/etc/config/pptpd_client.wanup
 	install -D pptp-client/config2/pptpd_client.vpn $(INSTALLDIR)/pptp-client/etc/config/pptpd_client.vpn
-	install -D pptp-client/pptp $(INSTALLDIR)/pptp-client/usr/sbin/pptp
 endif
-
+endif
+endif
+endif
