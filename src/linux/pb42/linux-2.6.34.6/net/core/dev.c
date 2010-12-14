277f17515d3fd9740df137ed390ddfd464411a7d
chris <chris@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-12-14 18:09:15 +0000

    net: fix tx queue selection for bridged devices implementing select_queue
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15919 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/pb42/linux-2.6.34.6/net/core/dev.c
+++ src/linux/pb42/linux-2.6.34.6/net/core/dev.c
@@ -1980,16 +1980,16 @@ static inline u16 dev_cap_txqueue(struct net_device *dev, u16 queue_index)
 struct netdev_queue *dev_pick_tx(struct net_device *dev, struct sk_buff *skb)
 {
 	int queue_index;
-	struct sock *sk = skb->sk;
+	const struct net_device_ops *ops = dev->netdev_ops;
 
-	queue_index = sk_tx_queue_get(sk);
-	if (queue_index < 0) {
-		const struct net_device_ops *ops = dev->netdev_ops;
+	if (ops->ndo_select_queue) {
+		queue_index = ops->ndo_select_queue(dev, skb);
+		queue_index = dev_cap_txqueue(dev, queue_index);
+	} else {
+		struct sock *sk = skb->sk;
+		queue_index = sk_tx_queue_get(sk);
+		if (queue_index < 0) {
 
-		if (ops->ndo_select_queue) {
-			queue_index = ops->ndo_select_queue(dev, skb);
-			queue_index = dev_cap_txqueue(dev, queue_index);
-		} else {
 			queue_index = 0;
 			if (dev->real_num_tx_queues > 1)
 				queue_index = skb_tx_hash(dev, skb);
@@ -5282,7 +5282,7 @@ static void netdev_wait_allrefs(struct net_device *dev)
 			rebroadcast_time = jiffies;
 		}
 
-		msleep(250);
+		msleep(1);
 
 		if (time_after(jiffies, warning_time + 10 * HZ)) {
 			printk(KERN_EMERG "unregister_netdevice: "
