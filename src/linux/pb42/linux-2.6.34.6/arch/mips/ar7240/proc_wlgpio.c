4d8b755c615d475f13654de4f2949c9ceeffe27c
BrainSlayer <s.gottschall@dd-wrt.com>
2010-12-16 14:03:13 +0000

    extended gpios
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15930 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/linux/pb42/linux-2.6.34.6/arch/mips/ar7240/proc_wlgpio.c
@@ -0,0 +1,176 @@
+/*
+ * proc_gpio: AR5315 GPIO pins in /proc/gpio/
+ * by olg 
+ * modification for AR7240 support by Sebastian Gottschall <s.gottschall@newmedia-net.de>
+ * GPL'ed
+ * some code stolen from Yoshinori Sato <ysato@users.sourceforge.jp>
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License version 2 as published
+ *  by the Free Software Foundation.
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/types.h>
+#include <linux/stddef.h>
+#include <linux/proc_fs.h>
+#include <linux/kernel.h>
+#include <linux/string.h>
+#include <linux/fs.h>
+#include <asm/uaccess.h>	/* for copy_from_user */
+
+#include "ar7240.h"
+
+
+typedef	u32					gpio_words;
+
+#define	GPIO_WL0_ADDR		KSEG1ADDR(AR7240_PCI_MEM_BASE + 0x4048)				//AR9220 GPIO IN/OUT REGISTER	--> PCI MAP 0xB0000000 + OFFSET [0x4048]
+
+static void set_wl0_gpio(int gpio,int val)
+{
+	register	gpio_words	wl0	= (gpio_words)ar7240_reg_rd(GPIO_WL0_ADDR);	//ar9280 register [0x4048]
+	if (val)
+	    wl0|=1<<gpio;
+	else
+	    wl0&=(~(1<<gpio));
+
+	ar7240_reg_wr(GPIO_WL0_ADDR, wl0);	//ar9283 register [0x4048]
+	ar7240_reg_rd(GPIO_WL0_ADDR);	//ar9283 register [0x4048]
+}
+
+static int get_wl0_gpio(int gpio)
+{
+	register	gpio_words	wl0	= (gpio_words)ar7240_reg_rd(GPIO_WL0_ADDR);	//ar9280 register [0x4048]
+	if (wl0 & (1<<gpio))
+	    return 1;
+	else
+	    return 0;
+}
+
+
+
+#define PROCFS_MAX_SIZE 64
+extern const char *get_arch_type (void);
+static struct proc_dir_entry *proc_gpio, *gpio_dir;
+
+//Masks for data exchange through "void *data" pointer
+
+static void cleanup_proc (void);
+
+//The buffer used to store the data returned by the proc file
+static char procfs_buffer[PROCFS_MAX_SIZE];
+static unsigned long procfs_buffer_size = 0;
+
+
+
+static int
+gpio_proc_read (char *buf, char **start, off_t offset,
+		int len, int *eof, void *data)
+{
+  u32 reg = 0;
+    reg = get_wl0_gpio((unsigned int)data);
+
+  if (reg)
+    buf[0] = '1';
+  else
+    buf[0] = '0';
+  buf[1] = 0;
+
+  *eof = 1;
+
+  return (2);
+
+}
+
+static int
+gpio_proc_write (struct file *file, const char *buffer, unsigned long count,
+		 void *data)
+{
+  u32 reg = 0;
+
+  /* get buffer size */
+  procfs_buffer_size = count;
+  if (procfs_buffer_size > PROCFS_MAX_SIZE)
+    {
+      procfs_buffer_size = PROCFS_MAX_SIZE;
+    }
+  /* write data to the buffer */
+  if (copy_from_user (procfs_buffer, buffer, procfs_buffer_size))
+    {
+      return -EFAULT;
+    }
+
+  procfs_buffer[procfs_buffer_size] = 0;
+
+
+  if (procfs_buffer[0] == '0' || procfs_buffer[0] == 'i')
+    reg = 0;
+  if (procfs_buffer[0] == '1' || procfs_buffer[0] == 'o')
+    reg = 1;
+    set_wl0_gpio(((unsigned int) data),reg);
+  return procfs_buffer_size;
+}
+
+
+
+
+
+static __init int
+register_proc (void)
+{
+  unsigned char i, flag = 0;
+  char proc_name[64];
+  int gpiocount = 16;
+
+  /* create directory gpio */
+  gpio_dir = proc_mkdir ("wl0gpio", NULL);
+  if (gpio_dir == NULL)
+    goto fault;
+
+  for (i = 0; i < gpiocount; i++)	//create for every GPIO "x_in"," x_out" and "x_dir"
+    {
+      sprintf (proc_name, "%i_out", i);
+      proc_gpio = create_proc_entry (proc_name, S_IRUGO, gpio_dir);
+      if (proc_gpio)
+	{
+	  proc_gpio->read_proc = gpio_proc_read;
+	  proc_gpio->write_proc = gpio_proc_write;
+	  proc_gpio->data = i;
+	}
+      else
+	goto fault;
+    }
+
+  printk (KERN_NOTICE "wl0gpio_proc: module loaded and /proc/wl0gpio/ created\n");
+  return 0;
+
+fault:
+  cleanup_proc ();
+  return -EFAULT;
+}
+
+static void
+cleanup_proc (void)
+{
+  unsigned char i;
+  char proc_name[64];
+  int gpiocount=16;
+
+  for (i = 0; i < gpiocount; i++)
+    {
+      sprintf (proc_name, "%i_out", i);
+      remove_proc_entry (proc_name, gpio_dir);
+    }
+  remove_proc_entry ("wl0gpio", NULL);
+  printk (KERN_INFO "wl0gpio_proc: unloaded and /proc/wl0gpio/ removed\n");
+
+}
+
+
+module_init (register_proc);
+module_exit (cleanup_proc);
+
+MODULE_AUTHOR ("Sebastian Gottschall");
+MODULE_DESCRIPTION ("AR7240 GPIO pins in /proc/wl0gpio/");
+MODULE_LICENSE ("GPL");
