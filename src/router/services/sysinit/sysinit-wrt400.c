63900b83fe517de8fdd09c84646dd1bbb6927762
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2010-12-17 02:07:29 +0000

    correct gpio pins for ses gpio
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15935 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-wrt400.c
+++ src/router/services/sysinit/sysinit-wrt400.c
@@ -245,6 +245,7 @@ void start_sysinit(void)
 
 	led_control(LED_POWER, LED_ON);
 	led_control(LED_SES, LED_OFF);
+	led_control(LED_SES2, LED_OFF);
 	led_control(LED_DIAG, LED_OFF);
 	led_control(LED_BRIDGE, LED_OFF);
 	led_control(LED_WLAN, LED_OFF);
