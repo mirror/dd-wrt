ae969644be33a0fbd82ee4e0933e8fbe92176413
BrainSlayer <s.gottschall@dd-wrt.com>
2010-12-27 13:34:15 +0000

    advanced version
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15974 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/ntfs-3g/libntfs-3g/volume.c
+++ src/router/ntfs-3g/libntfs-3g/volume.c
@@ -868,6 +868,7 @@ ntfs_volume *ntfs_device_mount(struct ntfs_device *dev, unsigned long flags)
 	VOLUME_INFORMATION *vinf;
 	ntfschar *vname;
 	int i, j, eo;
+	unsigned int k;
 	u32 u;
 
 	vol = ntfs_volume_startup(dev, flags);
@@ -1024,6 +1025,17 @@ ntfs_volume *ntfs_device_mount(struct ntfs_device *dev, unsigned long flags)
 	if (ntfs_inode_close(ni)) {
 		ntfs_log_perror("Failed to close $UpCase");
 		goto error_exit;
+	}
+	/* Consistency check of $UpCase, restricted to plain ASCII chars */
+	k = 0x20;
+	while ((k < vol->upcase_len)
+	    && (k < 0x7f)
+	    && (le16_to_cpu(vol->upcase[k])
+			== ((k < 'a') || (k > 'z') ? k : k + 'A' - 'a')))
+		k++;
+	if (k < 0x7f) {
+		ntfs_log_error("Corrupted file $UpCase\n");
+		goto io_error_exit;
 	}
 
 	/*
