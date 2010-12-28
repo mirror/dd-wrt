133ba9358770d076ce08eb176a24cf82b9e58cb6
BrainSlayer <s.gottschall@dd-wrt.com>
2010-12-28 00:24:06 +0000

    experimental danube port
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@15978 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/mtd.c
+++ src/router/rc/mtd.c
@@ -338,6 +338,10 @@ int mtd_write(const char *path, const char *mtd)
 	trx.magic = STORE32_LE(trx.magic);
 	trx.len = STORE32_LE(trx.len);
 	trx.crc32 = STORE32_LE(trx.crc32);
+#elif HAVE_DANUBE
+	trx.magic = STORE32_LE(trx.magic);
+	trx.len = STORE32_LE(trx.len);
+	trx.crc32 = STORE32_LE(trx.crc32);
 #elif HAVE_XSCALE
 	trx.magic = STORE32_LE(trx.magic);
 	trx.len = STORE32_LE(trx.len);
