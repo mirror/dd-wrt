bf319b074f7acb6d97971302991599d73ca02406
BrainSlayer <s.gottschall@dd-wrt.com>
2011-01-27 13:02:50 +0000

    if ip is empty, use asterisk
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@16113 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/pppoeserver.c
+++ src/router/services/services/pppoeserver.c
@@ -237,6 +237,8 @@ void start_pppoeserver(void)
 				if (!ip || !enable)
 					continue;
 
+				if (!strcmp(ip,"0.0.0.0"))
+				    ip = "*";
 				if (!strcmp(enable, "on"))
 					fprintf(fp, "%s * %s %s\n", user, pass,
 						ip);
