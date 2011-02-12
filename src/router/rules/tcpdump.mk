26af86227f50f2ce1b2b7d2eed86160aa6ea4d23
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-02-12 12:27:33 +0000

    fix ipv6 for tcpdump
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@16193 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/tcpdump.mk
+++ src/router/rules/tcpdump.mk
@@ -1,5 +1,11 @@
+ifeq ($(CONFIG_IPV6),y)
+export EXTRA:=--enable-ipv6
+endif
+
+
+
 tcpdump-configure: libpcap
-	cd tcpdump && ./configure --host=$(ARCH)-linux --enable-shared --disable-static --without-crypto CC=$(ARCH)-linux-uclibc-gcc ac_cv_linux_vers=2 CPPFLAGS="-I../libpcap_noring $(COPTS) -DNEED_PRINTF" CFLAGS="-I../libpcap_noring $(COPTS) -DNEED_PRINTF -DHAVE_BPF_DUMP" LDFLAGS="-L../libpcap_noring"
+	cd tcpdump && ./configure --host=$(ARCH)-linux --enable-shared --disable-static --without-crypto $(EXTRA) CC=$(ARCH)-linux-uclibc-gcc ac_cv_linux_vers=2 td_cv_buggygetaddrinfo=no CPPFLAGS="-I../libpcap_noring $(COPTS) -DNEED_PRINTF" CFLAGS="-I../libpcap_noring $(COPTS) -DNEED_PRINTF -DHAVE_BPF_DUMP" LDFLAGS="-L../libpcap_noring"
 
 tcpdump:
 	make -j 4 -C tcpdump
