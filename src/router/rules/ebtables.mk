b8eabf643a198c57696885d3df16cbba433f44a3
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-03-20 23:26:54 +0000

    ebtables for newer kernel versions
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@16514 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/ebtables.mk
+++ src/router/rules/ebtables.mk
@@ -1,13 +1,26 @@
 ebtables:
+ifeq ($(CONFIG_HOTPLUG2),y)
+	$(MAKE) -C ebtables-2.0.9 static BINDIR=/usr/sbin LIBDIR=/usr/lib KERNEL_DIR=$(LINUXDIR) KERNEL_INCLUDES=$(LINUXDIR)/include
+else
 	$(MAKE) -C ebtables static BINDIR=/usr/sbin LIBDIR=/usr/lib KERNEL_DIR=$(LINUXDIR) KERNEL_INCLUDES=$(LINUXDIR)/include
+endif
 
 ebtables-install:
 ifeq ($(CONFIG_EBTABLES),y)
+ifeq ($(CONFIG_HOTPLUG2),y)
+	install -D ebtables-2.0.9/static $(INSTALLDIR)/ebtables/usr/sbin/ebtables
+else
 	install -D ebtables/ebtables $(INSTALLDIR)/ebtables/usr/sbin/ebtables
+
+endif
 else
         # So that generic rule does not take precedence
 	@true
 endif
 
 ebtables-clean:
+ifeq ($(CONFIG_HOTPLUG2),y)
+	-$(MAKE) -C ebtables-2.0.9 KERNEL_DIR=$(LINUXDIR) clean
+else
 	-$(MAKE) -C ebtables KERNEL_DIR=$(LINUXDIR) clean
+endif
