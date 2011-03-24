58ccc8e2f3998c7ce4c067f6c932fa2a0b1d9f47
BrainSlayer <s.gottschall@dd-wrt.com>
2011-03-24 18:34:36 +0000

    gui fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@16596 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/ejwps.c
+++ src/router/httpd/visuals/ejwps.c
@@ -16,12 +16,17 @@ void ej_get_wpsstatus(webs_t wp, int argc, char_t ** argv)
 {
 	if (nvram_match("wps_status","0"))
 	    websWrite(wp,"Unconfigured");
+	else
 	if (nvram_match("wps_status","1"))
 	    websWrite(wp,"Configured");
+	else
 	if (nvram_match("wps_status","2"))
 	    websWrite(wp,"Waiting for Enrollee");
+	else
 	if (nvram_match("wps_status","3"))
 	    websWrite(wp,"Waiting for Registrar");
+	else
+	    websWrite(wp,"Configured");
 	return;
 }
 #endif
