0e9003f7bbcbd180b4beb5d64674d6ce16e80bb2
ChristianScheele <scheele@newmedia-net.de>
2011-03-27 23:27:09 +0000

    batman-adv copy-script,batctl -DNEED_PRINTF is necessary
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@16626 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/batman-adv.mk
+++ src/router/rules/batman-adv.mk
@@ -1,6 +1,6 @@
 batman-adv:
 	$(MAKE) -C batman-adv KERNELPATH="$(LINUXDIR)" all
-	$(MAKE) -C batctl all CC=$(CC) CFLAGS="$(COPTS) -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="$(COPTS) -ffunction-sections -fdata-sections -Wl,--gc-sections -lm"
+	$(MAKE) -C batctl all CC=$(CC) CFLAGS="$(COPTS) -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="$(COPTS) -ffunction-sections -fdata-sections -Wl,--gc-sections -lm"
 
 
 batman-adv-clean:
@@ -12,3 +12,4 @@ batman-adv-install:
 	install -D batman-adv/batman-adv.ko $(INSTALLDIR)/batman-adv/lib/batman-adv/batman-adv.ko
 	strip --strip-unneeded --remove-section=.comment --remove-section=.pdr --remove-section=.mdebug.abi32 $(INSTALLDIR)/batman-adv/lib/batman-adv/*
 	install -D batctl/batctl $(INSTALLDIR)/batman-adv/usr/sbin/batctl
+	install -D batctl/config/batstart.sh $(INSTALLDIR)/batman-adv/usr/sbin/batstart.sh
