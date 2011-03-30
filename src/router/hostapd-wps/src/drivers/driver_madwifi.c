028b89ece4fe476e4aa0c27b8c8d6ed3f00619d7
chris <chris@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-03-30 13:08:35 +0000

    hostapd - fix intel client disassoc issues
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@16638 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/hostapd-wps/src/drivers/driver_madwifi.c
+++ src/router/hostapd-wps/src/drivers/driver_madwifi.c
@@ -1097,7 +1097,7 @@ madwifi_wireless_event_init(struct madwifi_driver_data *drv)
 
 static int
 madwifi_send_eapol(void *priv, const u8 *addr, const u8 *data, size_t data_len,
-		   int encrypt, const u8 *own_addr)
+		   int encrypt, const u8 *own_addr, u32 flags)
 {
 	struct madwifi_driver_data *drv = priv;
 	unsigned char buf[3000];
