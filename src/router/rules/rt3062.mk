9d2a8ad4df69d12a830898eba98f4f0d4b1029c8
BrainSlayer <s.gottschall@dd-wrt.com>
2011-03-31 21:17:51 +0000

    rt3062ap
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@16651 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rules/rt3062.mk
@@ -0,0 +1,9 @@
+rt3062:
+	make -C rt3062 CHIPSET=2860
+	make -C rt3062 CHIPSET=3062
+
+rt3062-install:
+	install -D rt3062/os/linux/rt2860ap.ko $(INSTALLDIR)/rt3062/lib/rt3062/rt2860ap.ko
+	install -D rt3062/os/linux/rt3062ap.ko $(INSTALLDIR)/rt3062/lib/rt3062/rt3062ap.ko
+	
+
