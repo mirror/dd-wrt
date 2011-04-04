600419e51b0a8b1118d671890d7cf18f332c2083
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-04-04 01:16:23 +0000

    play with opendpi
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@16680 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/lib.c
+++ src/router/httpd/visuals/lib.c
@@ -140,6 +140,10 @@ int protocol_to_num(char *proto)
 		return 99;
 	else if (!strcmp(proto, "p2p"))
 		return 100;
+#ifdef HAVE_OPENDPI
+	else if (!strcmp(proto, "dpi"))
+		return 101;
+#endif
 	else
 		return 0;
 }
