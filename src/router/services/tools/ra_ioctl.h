973400cc7b200af1b3924450908c0f8df4ca8fb9
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-04-11 23:15:55 +0000

    experimental rt3352 device
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@16776 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/tools/ra_ioctl.h
+++ src/router/services/tools/ra_ioctl.h
@@ -6,6 +6,9 @@
 #define RAETH_ESW_REG_WRITE		0x89F2
 #define RAETH_MII_READ			0x89F3
 #define RAETH_MII_WRITE			0x89F4
+#define RAETH_ESW_INGRESS_RATE		0x89F5
+#define RAETH_ESW_EGRESS_RATE		0x89F6
+
 
 /* rt3052 embedded ethernet switch registers */
 #define REG_ESW_VLAN_ID_BASE		0x50
@@ -18,7 +21,12 @@
 #define REG_ESW_WT_MAC_AD1		0x38
 #define REG_ESW_WT_MAC_AD2		0x3C
 
+#if defined(HAVE_RT3352) || defined (CONFIG_RALINK_RT5350)
+#define REG_ESW_MAX			0x16C
+#else //RT305x, RT3350
 #define REG_ESW_MAX			0xFC
+#endif
+
 
 typedef struct rt3052_esw_reg {
 	unsigned int off;
@@ -26,10 +34,17 @@ typedef struct rt3052_esw_reg {
 } esw_reg;
 
 typedef struct ralink_mii_ioctl_data {
-	__u32 phy_id;
-	__u32 reg_num;
-	__u32 val_in;
-	__u32 val_out;
+	__u32	phy_id;
+	__u32	reg_num;
+	__u32	val_in;
+	__u32	val_out;
 } ra_mii_ioctl_data;
+
+typedef struct rt335x_esw_reg {
+	unsigned int on_off;
+	unsigned int port;
+	unsigned int bw;/*Mbps*/
+} esw_rate;
+
 
 #endif
