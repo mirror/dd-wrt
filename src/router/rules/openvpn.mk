80b9a4eba3414679f3923f17f8d1167e72738638
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-04-22 21:04:32 +0000

    openvpn rules update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@16863 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/openvpn.mk
+++ src/router/rules/openvpn.mk
@@ -1,10 +1,9 @@
 openvpn-configure: openssl
-	cd lzo && ./configure --host=$(ARCH)-linux CFLAGS="$(COPTS)"
-	make -C lzo clean
+	if ! test -e "lzo/Makefile"; then cd lzo && ./configure --host=$(ARCH)-linux CFLAGS="$(COPTS)"; fi
 	make -j 4 -C lzo
-	cd openvpn && ./configure --host=$(ARCH)-linux CPPFLAGS="-I../lzo/include -I../openssl/include -L../lzo -L../openssl -L../lzo/src/.libs" --enable-static --disable-shared --disable-pthread --disable-plugins --disable-debug --enable-password-save --enable-management --enable-lzo --enable-small --enable-server --enable-multihome CFLAGS="$(COPTS)" LDFLAGS="-L../openssl -L../lzo -L../lzo/src/.libs -ldl"
+	if ! test -e "openvpn/Makefile"; then cd openvpn && ./configure --host=$(ARCH)-linux CPPFLAGS="-I../lzo/include -I../openssl/include -L../lzo -L../openssl -L../lzo/src/.libs" --enable-pthread --disable-plugins --disable-debug --enable-password-save --enable-management --enable-lzo --enable-server --enable-multihome CFLAGS="$(COPTS)" LDFLAGS="-L../openssl -L../lzo -L../lzo/src/.libs -ldl"; fi
 
-openvpn: openssl
+openvpn: openssl openvpn-configure
 #ifeq ($(CONFIG_NEWMEDIA),y)
 #else
 #	cd openvpn && ./configure --host=$(ARCH)-linux CPPFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections -I../lzo/include -I../openssl/include -L../lzo -L../openssl -L../lzo/src/.libs" --enable-static --disable-shared --disable-pthread --disable-plugins --disable-debug --disable-management --disable-socks --enable-lzo --enable-small --enable-server --enable-http --enable-password-save CFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="-L../openssl -L../lzo -L../lzo/src/.libs  -ffunction-sections -fdata-sections -Wl,--gc-sections"
