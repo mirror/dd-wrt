5b6d8097a24a401fc192d10674fab67f7b6de7bc
BrainSlayer <s.gottschall@dd-wrt.com>
2011-04-25 21:16:01 +0000

    any sub interface should be considered too
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@16880 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/madwifi_ath9k.c
+++ src/router/services/networking/madwifi_ath9k.c
@@ -551,9 +551,9 @@ void setupHostAP_ath9k(char *maininterface, int isfirst, int vapid, int aoss)
 		if (nvram_nmatch("1", "%s_bridged", ifname))
 			fprintf(fp, "bridge=%s\n", getBridge(ifname));
 		if (!aoss) {
-			if (!strcmp(ifname, "ath0"))
+			if (!strncmp(ifname, "ath0",4))
 				led_control(LED_SEC0, LED_ON);
-			if (!strcmp(ifname, "ath1"))
+			if (!strncmp(ifname, "ath1",4))
 				led_control(LED_SEC1, LED_ON);
 
 		}
@@ -598,9 +598,9 @@ void setupHostAP_ath9k(char *maininterface, int isfirst, int vapid, int aoss)
 		   nvram_match(akm, "psk psk2") ||
 		   nvram_match(akm, "wpa") || nvram_match(akm, "wpa2")
 		   || nvram_match(akm, "wpa wpa2")) {
-		if (!strcmp(ifname, "ath0"))
+		if (!strncmp(ifname, "ath0",4))
 			led_control(LED_SEC0, LED_ON);
-		if (!strcmp(ifname, "ath1"))
+		if (!strncmp(ifname, "ath1",4))
 			led_control(LED_SEC1, LED_ON);
 
 		// sprintf(buf, "rsn_preauth_interfaces=%s\n", "br0");
