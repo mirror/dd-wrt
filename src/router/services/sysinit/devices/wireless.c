f6c934f3a769298cf24ab646b4c1ef4c41fffec1
BrainSlayer <s.gottschall@dd-wrt.com>
2011-04-26 23:48:00 +0000

    can't be static
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@16916 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/devices/wireless.c
+++ src/router/services/sysinit/devices/wireless.c
@@ -23,7 +23,7 @@
  */
 
 // extern int getath9kdevicecount(void);
-extern void delete_ath9k_devices(char *physical_iface)
+extern void delete_ath9k_devices(char *physical_iface);
 
 static void detect_wireless_devices(void)
 {
