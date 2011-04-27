9eaa95deaca37915c7d42abaae51e8690d7a7874
BrainSlayer <s.gottschall@dd-wrt.com>
2011-04-27 00:56:33 +0000

    fix samba config code
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@16919 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/samba3.c
+++ src/router/services/services/samba3.c
@@ -42,6 +42,7 @@ void start_samba3(void)
 	struct samba3_shareuser *csu, *csunext;
 	struct samba3_user *samba3users, *cu, *cunext;
 	struct samba3_share *samba3shares;
+	FILE *fp;
 	if (!nvram_match("samba3_enable", "1"))
 		return;
 
@@ -52,17 +53,18 @@ void start_samba3(void)
 
 	samba3users = getsamba3users();
 	for (cu = samba3users; cu; cu = cunext) {
-		sysprintf
-		    ("grep -q \"%s\" /etc/passwd || echo \"%s\"\":*:1000:1000:\"%s\":/var:/bin/false\" >> /etc/passwd",
-		     cu->username, cu->username, cu->username);
-		sysprintf("smbpasswd \"%s\" \"%s\"", cu->username,
-			  cu->password);
+		if (strlen(cu->username)) {
+			sysprintf
+			    ("grep -q \"%s\" /etc/passwd || echo \"%s\"\":*:1000:1000:\"%s\":/var:/bin/false\" >> /etc/passwd",
+			     cu->username, cu->username, cu->username);
+			sysprintf("smbpasswd \"%s\" \"%s\"", cu->username,
+				  cu->password);
+		}
 		cunext = cu->next;
 		free(cu);
 
 	}
-	free(samba3users);
-	FILE *fp = fopen("/tmp/smb.conf", "wb");
+	fp = fopen("/tmp/smb.conf", "wb");
 	fprintf(fp,
 		"[global]\n"
 		"server string = %s\n"
@@ -82,33 +84,35 @@ void start_samba3(void)
 		nvram_safe_get("samba3_workgrp"));
 
 	samba3shares = getsamba3shares();
-
 	for (cs = samba3shares; cs; cs = csnext) {
-		fprintf(stderr,"add share %s\n",cs->label);
-		fprintf(fp, "[%s]\n", cs->label);
-		fprintf(fp, "comment = %s\n", cs->label);
-		fprintf(fp, "path = %s\n", cs->mp);
-		fprintf(fp, "read only = %s\n",
-			!strcmp(cs->access_perms, "ro") ? "Yes" : "No");
-		fprintf(fp, "guest ok = %s\n", cs->public == 1 ? "Yes" : "No");
-		fprintf(fp, "valid users =");
-		int first = 0;
-		for (csu = cs->users; csu; csu = csunext) {
-			fprintf(stderr,"permitt user %s\n",csu->username);
-			if (first)
-				fprintf(fp, ",");
-			first = 1;
-			fprintf(fp, " %s", csu->username);
-			csunext = csu->next;
-			free(csu);
+		fprintf(stderr, "add share %s\n", cs->label);
+		if (strlen(cs->label)) {
+			fprintf(fp, "[%s]\n", cs->label);
+			fprintf(fp, "comment = %s\n", cs->label);
+			fprintf(fp, "path = %s\n", cs->mp);
+			fprintf(fp, "read only = %s\n",
+				!strcmp(cs->access_perms, "ro") ? "Yes" : "No");
+			fprintf(fp, "guest ok = %s\n",
+				cs->public == 1 ? "Yes" : "No");
+			fprintf(fp, "valid users =");
+			int first = 0;
+			for (csu = cs->users; csu; csu = csunext) {
+				fprintf(stderr, "permitt user %s\n",
+					csu->username);
+				if (first)
+					fprintf(fp, ",");
+				first = 1;
+				fprintf(fp, " %s", csu->username);
+				csunext = csu->next;
+				free(csu);
+			}
+			free(cs->users);
+			fprintf(fp, "\n");
+			fprintf(fp, "force user = root\n");
 		}
-		free(cs->users);
-		fprintf(fp, "\n");
-		fprintf(fp, "force user = root\n");
 		csnext = cs->next;
 		free(cs);
 	}
-	free(samba3shares);
 	fclose(fp);
 	sysprintf("chmod 777 /jffs\n");
 
@@ -122,5 +126,12 @@ void stop_samba3(void)
 {
 	stop_process("smbd", "samba");
 	stop_process("nmbd", "nmbd");
+}
+#endif
+
+#ifdef TEST
+void main(int argc, char *argv[])
+{
+	start_samba3();
 }
 #endif
