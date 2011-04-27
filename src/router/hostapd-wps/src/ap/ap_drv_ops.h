33b805fab50232cea3ac1709673e40e5615b94a1
BrainSlayer <s.gottschall@dd-wrt.com>
2011-04-27 16:08:13 +0000

    fix PS3/PSP authentication issue
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@16925 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/hostapd-wps/src/ap/ap_drv_ops.h
+++ src/router/hostapd-wps/src/ap/ap_drv_ops.h
@@ -20,6 +20,7 @@ struct wpa_bss_params;
 struct wpa_driver_scan_params;
 struct ieee80211_ht_capabilities;
 
+u32 hostapd_sta_flags_to_drv(u32 flags);
 int hostapd_set_ap_wps_ie(struct hostapd_data *hapd);
 int hostapd_set_authorized(struct hostapd_data *hapd,
 			   struct sta_info *sta, int authorized);
