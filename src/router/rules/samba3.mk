5f65e66fb9cf8db5d75ef8cc65669a497030f679
BrainSlayer <s.gottschall@dd-wrt.com>
2011-04-28 00:36:37 +0000

    new samba
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@16933 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/samba3.mk
+++ src/router/rules/samba3.mk
@@ -1,15 +1,14 @@
-
 samba3:
 	CC="$(ARCH)-linux-uclibc-gcc" \
-	CFLAGS="$(COPTS)" \
-	CPPFLAGS="$(COPTS)" \
-	LDFLAGS="-fPIC $(COPTS)" \
-	$(MAKE) -C samba3/source all bin/smbpasswd  
+	CFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections $(LTO)" \
+	CPPFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections $(LTO)" \
+	LDFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections $(LTO)" \
+	$(MAKE) -C samba3/source all bin/smbpasswd WITH_LFS=yes
 
 samba3-install:
 	mkdir -p $(INSTALLDIR)/samba3
 	install -D samba3/source/bin/smbd $(INSTALLDIR)/samba3/usr/sbin/smbd
-	install -D samba3/source/bin/nmbd $(INSTALLDIR)/samba3/usr/sbin/nmbd
+#	install -D samba3/source/bin/nmbd $(INSTALLDIR)/samba3/usr/sbin/nmbd
 	install -D samba3/source/bin/smbpasswd $(INSTALLDIR)/samba3/usr/sbin/smbpasswd
 	install -D samba3/config/samba3.webnas $(INSTALLDIR)/samba3/etc/config/samba3.webnas
 #	install -D samba3/config/samba3.startup $(INSTALLDIR)/samba3/etc/config/samba3.startup
