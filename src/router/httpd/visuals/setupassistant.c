5617cdf8ceeede43ddf51fcda73504e150c4e006
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-05-01 19:29:28 +0000

    relayd for setup assistent
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@16952 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/setupassistant.c
+++ src/router/httpd/visuals/setupassistant.c
@@ -539,6 +539,9 @@ void ej_sas_show_wireless_single(webs_t wp, char *prefix)
 			websWrite(wp,
 				  "document.write(\"<option value=\\\"wet\\\" %s >\" + wl_basic.clientBridge + \"</option>\");\n",
 #endif
+				  nvram_match(wl_mode,
+					      "wet") ?
+				  "selected=\\\"selected\\\"" : "");
 #endif
 	if (!cpeonly)
 		websWrite(wp,
