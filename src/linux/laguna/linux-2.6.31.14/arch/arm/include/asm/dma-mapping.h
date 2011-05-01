6adae73798b8d1e4c27070af90dec2739388eb8c
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-05-01 16:00:34 +0000

    fix cache sync problem
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@16949 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/laguna/linux-2.6.31.14/arch/arm/include/asm/dma-mapping.h
+++ src/linux/laguna/linux-2.6.31.14/arch/arm/include/asm/dma-mapping.h
@@ -350,7 +350,8 @@ static inline dma_addr_t dma_map_page(struct device *dev, struct page *page,
 static inline void dma_unmap_single(struct device *dev, dma_addr_t handle,
 		size_t size, enum dma_data_direction dir)
 {
-	/* nothing to do */
+	if (dir != DMA_TO_DEVICE)
+		dma_cache_maint(dma_to_virt(dev, handle), size, DMA_FROM_DEVICE);
 }
 #endif /* CONFIG_DMABOUNCE */
 
@@ -398,6 +399,8 @@ static inline void dma_sync_single_range_for_cpu(struct device *dev,
 {
 	BUG_ON(!valid_dma_direction(dir));
 
+	if (dir != DMA_TO_DEVICE)
+		dma_cache_maint(dma_to_virt(dev, handle) + offset, size, DMA_FROM_DEVICE);
 	dmabounce_sync_for_cpu(dev, handle, offset, size, dir);
 }
 
