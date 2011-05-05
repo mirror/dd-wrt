63cc90e326f9f3e6192fd083916bc8f19c05777f
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-05-05 01:34:22 +0000

    Remoteengeneer x86 build
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@16977 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/broadcom.c
+++ src/router/httpd/modules/broadcom.c
@@ -1647,6 +1647,12 @@ apply_cgi(webs_t wp, char_t * urlPrefix, char_t * webDir, int arg,
 		killall("udhcpc", SIGKILL);
 		sys_commit();
 #ifdef HAVE_X86
+#ifdef HAVE_ERC
+		eval("mount", "/usr/local", "-o", "remount,rw");
+		eval("nvram","restore","/etc/defaults/x86ree.backup");
+		eval("reboot");
+		eval("event", "5", "1", "15");
+#endif
 		char drive[64];
 		sprintf(drive, "/dev/discs/disc%d/disc", getdiscindex());
 		FILE *in = fopen64(drive, "r+b");
