dabb2fe998bf677e600b41cc4d883384e41a2042
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-05-15 19:40:41 +0000

    optimize again
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17067 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/laguna/linux-2.6.31.14/arch/arm/mach-cns3xxx/pm.c
+++ src/linux/laguna/linux-2.6.31.14/arch/arm/mach-cns3xxx/pm.c
@@ -308,7 +308,7 @@ void enter_wfi(void)
             "mov r0, #0\n"
             "mcr p15, 0, r0, c7, c10, 5\n"
             "mcr p15, 0, r0, c7, c10, 4\n"
-            "mcr p15, 0, r0, c7, c0, 4\n"
+            "mcr p15, 0, r0, c7, c0, 4\n" : "r0"
             );
 
 }
@@ -519,7 +519,7 @@ void cns3xxx_wfi(void)
 	asm volatile(
 			"mov r0, #0\n"
 			"mcr p15, 0, r0, c7, c10, 4\n"
-			"mcr p15, 0, r0, c7, c0, 4\n"
+			"mcr p15, 0, r0, c7, c0, 4\n" : "r0"
 			);
 }
 
