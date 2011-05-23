a3f361c2553a81b64a5687f5866ee3a9406c37cf
eko <eko@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-05-23 08:05:07 +0000

    update sample bcm K26 script for new toolchain (that fixes missing script problem.....)
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17104 52c4871e-980c-0410-b1e0-e73912ce01f8

--- opt/install_mini_v24-K26.sh
+++ opt/install_mini_v24-K26.sh
@@ -2,17 +2,16 @@ echo "#define BUILD_DATE \"$(date +%D)\"" > build.h
 
 REV=`svn info |grep Revision | cut -f 2 -d ' '`
 
-export PATH=/xfs/toolchains/toolchain-mipsel_gcc4.1.2/bin/:$PATH
+export PATH=/xfs/toolchains/toolchain-mipsel_gcc-linaro_uClibc-0.9.32/bin:$PATH
 
 
-cd ../src
+cd ../src/router
 
-cd router
 
-## use your desired config file here
+## use your desired config file here... 
 cp ./configs/broadcom_K26/.config_mini.v24-K26 .config
 
-## uncomment next line for 60K nvram builds, e.g. e2000, e3000...
+## uncomment next line for 60K nvram builds, e.g. e2000, e3000, e3200, e4200...
 # echo CONFIG_NVRAM_60K=y >> .config
 
 
@@ -21,7 +20,8 @@ make -f Makefile.brcm26 clean all install
 
 cd mipsel-uclibc
 
+## uncomment next line for normal (32k nvram) builds
 cp dd-wrt.v24-K26.trx ~/dd-wrt/GruppenLW/dd-wrt.v24-${REV}_NEWD-2_K2.6_mini.bin
 
-## uncomment next line for 60K nvram builds, e.g. e2000, e3000...
-# cp dd-wrt.v24-K26.trx ~/dd-wrt/GruppenLW/dd-wrt.v24-${REV}_NEWD-2_K2.6_mini-e2k-e3k.bin
+## uncomment next line for 60K nvram builds, e.g. e2000, e3000, e3200, e4200...
+# cp dd-wrt.v24-K26_nv60k.bin ~/dd-wrt/GruppenLW/dd-wrt.v24-${REV}_NEWD-2_K2.6_mini-nv60k.bin
