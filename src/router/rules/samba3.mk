1b89e9c74a1f4d4c15cb4c890414cb1ee9e840cd
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-05-25 13:27:47 +0000

    add nmbd
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17129 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/samba3.mk
+++ src/router/rules/samba3.mk
@@ -8,16 +8,22 @@ samba3:
 	CFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections $(LTO) $(SAMBA3_EXTRA)" \
 	CPPFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections $(LTO) $(SAMBA3_EXTRA)" \
 	LDFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections $(LTO) $(SAMBA3_EXTRA)" \
-	$(MAKE) -C samba3/source all bin/nmbd bin/smbpasswd WITH_LFS=yes
+	$(MAKE) -C samba3/source all bin/smbpasswd WITH_LFS=yes
+
+	CC="$(ARCH)-linux-uclibc-gcc" \
+	CFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections -fPIC $(LTO) $(SAMBA3_EXTRA)" \
+	CPPFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections -fPIC $(LTO) $(SAMBA3_EXTRA)" \
+	LDFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections -fPIC $(LTO) $(SAMBA3_EXTRA)" \
+	$(MAKE) -C samba3/source_nmbd all bin/nmbd WITH_LFS=yes
 
 samba3-install:
 	mkdir -p $(INSTALLDIR)/samba3
 	install -D samba3/source/bin/smbd $(INSTALLDIR)/samba3/usr/sbin/smbd
 ifeq ($(CONFIG_BUFFALO),y)
-	install -D samba3/source/bin/nmbd $(INSTALLDIR)/samba3/usr/sbin/nmbd
+	install -D samba3/source_nmbd/bin/nmbd $(INSTALLDIR)/samba3/usr/sbin/nmbd
 endif
 ifeq ($(CONFIG_NMBD),y)
-	install -D samba3/source/bin/nmbd $(INSTALLDIR)/samba3/usr/sbin/nmbd
+	install -D samba3/source_nmbd/bin/nmbd $(INSTALLDIR)/samba3/usr/sbin/nmbd
 endif
 	install -D samba3/source/bin/smbpasswd $(INSTALLDIR)/samba3/usr/sbin/smbpasswd
 	install -D samba3/config/samba3.webnas $(INSTALLDIR)/samba3/etc/config/samba3.webnas
@@ -27,3 +33,4 @@ endif
 
 samba3-clean:
 	$(MAKE) -C samba3/source clean
+	$(MAKE) -C samba3/source_nmbd clean
