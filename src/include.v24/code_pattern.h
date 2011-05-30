6c39b2030590acfa3685fa28362df026a805a67a
eko <USER@DOMAIN>
2011-05-30 05:44:57 +0000

    prep for 64k nvram routers
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17142 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/include.v24/code_pattern.h
+++ src/include.v24/code_pattern.h
@@ -67,6 +67,7 @@
 #define CODE_PATTERN_E3200   "3200"
 #define CODE_PATTERN_E4200   "4200"
 #define CODE_PATTERN_NV60K   "NV60"  //used for flashing nvram=60k routers
+#define CODE_PATTERN_NV60K   "NV64"  //used for flashing nvram=64k routers
 
 
 #ifdef HAVE_OMNI
