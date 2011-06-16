2c3d67324c97159a8062f0088c364e6f7be2d7a1
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-06-16 17:43:47 +0000

    doesnt belong to here
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17213 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/x86/linux-2.6.39/crypto/ocf/ep80579/tolapai/Acceleration/library/icp_utils/OSAL/lib/EP805XX/linux/linuxle/platforms/EP805XX/os/linux/src/IxOsalOsOemSymbols.d
+++
@@ -1,4 +0,0 @@
-lib/EP805XX/linux/linuxleplatforms/EP805XX/os/linux/src/IxOsalOsOemSymbols.d lib/EP805XX/linux/linuxle/platforms/EP805XX/os/linux/src/IxOsalOsOemSymbols.o:  \
- platforms/EP805XX/os/linux/src/IxOsalOsOemSymbols.c \
-  /home/seg/DEV/x86/src/linux/x86/linux-2.6.23/include/linux/autoconf.h \
-  /home/seg/DEV/x86/src/linux/x86/linux-2.6.23/include/linux/utsrelease.h platforms/EP805XX/os/linux/src/component.mk Makefile
