f6b206e7701e425d84db638fdfcde9ac44f4205c
BrainSlayer <s.gottschall@dd-wrt.com>
2011-06-16 17:43:47 +0000

    doesnt belong to here
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17213 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/x86/linux-2.6.39/crypto/ocf/ep80579/tolapai/Acceleration/library/icp_utils/OSAL/lib/EP805XX/linux/linuxle/common/os/linux/src/modules/ddk/IxOsalOsDdkSymbols.d
+++
@@ -1,4 +0,0 @@
-lib/EP805XX/linux/linuxlecommon/os/linux/src/modules/ddk/IxOsalOsDdkSymbols.d lib/EP805XX/linux/linuxle/common/os/linux/src/modules/ddk/IxOsalOsDdkSymbols.o:  \
- common/os/linux/src/modules/ddk/IxOsalOsDdkSymbols.c \
-  /home/seg/DEV/x86/src/linux/x86/linux-2.6.23/include/linux/autoconf.h \
-  /home/seg/DEV/x86/src/linux/x86/linux-2.6.23/include/linux/utsrelease.h common/os/linux/src/modules/ddk/component.mk Makefile
