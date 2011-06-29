96ddbaa1507ce39a0e36ca9455a3ff16e3c61690
BrainSlayer <s.gottschall@dd-wrt.com>
2011-06-29 00:07:29 +0000

    2.6.34.10
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17248 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/pb42/linux-2.6.34.6/include/linux/sched.h
+++ src/linux/pb42/linux-2.6.34.6/include/linux/sched.h
@@ -672,7 +672,7 @@ struct user_struct {
 	atomic_t inotify_devs;	/* How many inotify devs does this user have opened? */
 #endif
 #ifdef CONFIG_EPOLL
-	atomic_t epoll_watches;	/* The number of file descriptors currently watched */
+	atomic_long_t epoll_watches; /* The number of file descriptors currently watched */
 #endif
 #ifdef CONFIG_POSIX_MQUEUE
 	/* protected by mq_lock	*/
@@ -1071,7 +1071,7 @@ struct sched_class {
 					 struct task_struct *task);
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
-	void (*moved_group) (struct task_struct *p, int on_rq);
+	void (*task_move_group) (struct task_struct *p, int on_rq);
 #endif
 };
 
