50e52ad3b693f483592ba0a1a7cb4b2ee5e58b5c
BrainSlayer <s.gottschall@dd-wrt.com>
2011-07-08 14:42:09 +0000

    fix pppoe client bug
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17310 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/pppoeserver.c
+++ src/router/services/services/pppoeserver.c
@@ -80,8 +80,7 @@ static void makeipup(void)
 	FILE *fp = fopen("/tmp/pppoeserver/ip-up", "w");
 
 	fprintf(fp, "#!/bin/sh\n" "startservice set_routes\n"	// reinitialize 
-		"echo \"$PPPD_PID $1 $5 $PEERNAME date +%s\" >> /tmp/pppoe_connected\n"	//
-//->use something like $(( ($(date +%s) - $(date -d "$dates" +%s)) / (60*60*24*31) )) for computing uptime in the gui
+		"echo \"$PPPD_PID $1 $5 $PEERNAME\" >> /tmp/pppoe_connected\n"	//
 		"iptables -I FORWARD -i $1 -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu\n"	//
 		"iptables -I INPUT -i $1 -j ACCEPT\n"	//
 		"iptables -I FORWARD -i $1 -j ACCEPT\n"	//
