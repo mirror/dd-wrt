1485f943dddbb45420f2ad9e1a84ec9389ea34c4
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-07-10 16:38:15 +0000

    fix self made assert bug
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17328 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/openvpn/error.h
+++ src/router/openvpn/error.h
@@ -205,7 +205,7 @@ const char *msg_flags_string (const unsigned int flags, struct gc_arena *gc);
 FILE *msg_fp(const unsigned int flags);
 
 /* Fatal logic errors */
-#define ASSERT(x) 
+#define ASSERT(x) (x)
 //do { if (!(x)) assert_failed(__FILE__, __LINE__); } while (false)
 
 void assert_failed (const char *filename, int line);
