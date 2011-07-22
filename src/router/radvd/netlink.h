254dbcccfd72903b99381507662777a10f6760ce
BrainSlayer <s.gottschall@dd-wrt.com>
2011-07-22 21:36:44 +0000

    missing header
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17379 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/radvd/netlink.h
@@ -0,0 +1,21 @@
+/*
+ *   $Id: netlink.h,v 1.1 2011/02/25 04:17:23 reubenhwk Exp $
+ *
+ *   Authors:
+ *    Lars Fenneberg		<lf@elemental.net>	 
+ *    Reuben Hawkins		<reubenhwk@gmail.com>
+ *
+ *   This software is Copyright 1996,1997 by the above mentioned author(s), 
+ *   All Rights Reserved.
+ *
+ *   The license which is distributed with this software in the file COPYRIGHT
+ *   applies to this software. If your distribution is missing this file, you
+ *   may request it from <pekkas@netcore.fi>.
+ *
+ */
+
+#pragma once
+
+void process_netlink_msg(int sock);
+int netlink_socket(void);
+
