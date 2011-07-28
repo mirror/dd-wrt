baa2ea2a644dcfcb38b6b1a80332e5be04836b8d
ChristianScheele <scheele@newmedia-net.de>
2011-07-28 20:50:52 +0000

    l2tpv3tun rule file typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17402 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/l2tpv3tun.mk
+++ src/router/rules/l2tpv3tun.mk
@@ -5,7 +5,7 @@ l2tpv3tun:
 	$(MAKE) -C l2tpv3tun NL2FOUND=Y NL1FOUND= NLLIBNAME="libnl-tiny" CFLAGS="$(COPTS) -I$(TOP)/libnl-tiny/include -DCONFIG_LIBNL20 -D_GNU_SOURCE  -DNEED_PRINTF" LDFLAGS="-L$(TOP)/libnl-tiny/ -lnl-tiny" LIBS="-lm -lnl-tiny"
 
 l2tpv3tun-clean:
-	$(MAKE) -C l2tpv3tun clean NL2FOUND=Y NL1FOUND= NLLIBNAME="libnl-tiny" CFLAGS="$(IW_CFLAGS)" LDFLAGS="$(IW_LDFLAGS)" LIBS="-lm -lnl-tiny"
+	$(MAKE) -C l2tpv3tun clean NL2FOUND=Y NL1FOUND= NLLIBNAME="libnl-tiny" CFLAGS="$(COPTS) -I$(TOP)/libnl-tiny/include -DCONFIG_LIBNL20 -D_GNU_SOURCE  -DNEED_PRINTF" LDFLAGS="-L$(TOP)/libnl-tiny/ -lnl-tiny" LIBS="-lm -lnl-tiny"
 
 l2tpv3tun-install:
 	install -D l2tpv3tun/l2tpv3tun $(INSTALLDIR)/l2tpv3tun/usr/sbin/l2tpv3tun
