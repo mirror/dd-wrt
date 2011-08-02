c6d2ed09cf24f0e638f1640adc2d8f912efb1472
BrainSlayer <s.gottschall@dd-wrt.com>
2011-08-02 17:11:35 +0000

    fix usb hotplug for slow reacting devices like cdrom or dvd drives (tested with a external bluray writer)
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17412 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/hotplug.c
+++ src/router/rc/hotplug.c
@@ -38,7 +38,7 @@
 int main(int argc, char **argv)
 {
 	if (argc >= 2) {
-		sysprintf("echo received %s >> /tmp/hotplugs",argv[1]);
+//		sysprintf("echo received %s >> /tmp/hotplugs",argv[1]);
 		if (!strcmp(argv[1], "net")) {
 			start_service("hotplug_net");
 			return 0;
