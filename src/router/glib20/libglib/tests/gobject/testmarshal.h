8290d08a71d6b74c309dffbb1918b1496599b6c4
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-08-07 14:37:14 +0000

    moved
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17444 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/glib20/libglib/tests/gobject/testmarshal.h
@@ -0,0 +1,28 @@
+
+#ifndef __test_marshal_MARSHAL_H__
+#define __test_marshal_MARSHAL_H__
+
+#include	<glib-object.h>
+
+G_BEGIN_DECLS
+
+/* BOOLEAN:INT (./testmarshal.list:2) */
+extern void test_marshal_BOOLEAN__INT (GClosure     *closure,
+                                       GValue       *return_value,
+                                       guint         n_param_values,
+                                       const GValue *param_values,
+                                       gpointer      invocation_hint,
+                                       gpointer      marshal_data);
+
+/* STRING:INT (./testmarshal.list:3) */
+extern void test_marshal_STRING__INT (GClosure     *closure,
+                                      GValue       *return_value,
+                                      guint         n_param_values,
+                                      const GValue *param_values,
+                                      gpointer      invocation_hint,
+                                      gpointer      marshal_data);
+
+G_END_DECLS
+
+#endif /* __test_marshal_MARSHAL_H__ */
+
