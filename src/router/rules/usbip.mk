7543b16105678700f133b1f669586664e088c98e
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-08-07 14:54:12 +0000

    moved
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17445 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/usbip.mk
+++ src/router/rules/usbip.mk
@@ -1,7 +1,7 @@
 usbip-configure:
 	cd usbip/libsysfs && ./configure --host=$(ARCH)-linux CC=$(CC) CFLAGS="$(COPTS) -DNEED_PRINTF -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_malloc=malloc"
 	make -C usbip/libsysfs clean all
-	cd usbip && ./configure --host=$(ARCH)-linux CC=$(CC) CFLAGS="$(COPTS) -DNEED_PRINTF -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_realloc=realloc -Drpl_malloc=malloc -I$(TOP)/usbip/libsysfs/include -I$(TOP)/glib20/libglib/glib -L$(TOP)/usbip/libsysfs/lib/.libs -L$(TOP)/glib20/libglib/glib/.libs"
+	cd usbip && ./configure --disable-static --enable-shared --host=$(ARCH)-linux CC=$(CC) CFLAGS="$(COPTS) -DNEED_PRINTF -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_realloc=realloc -Drpl_malloc=malloc -I$(TOP)/usbip/libsysfs/include -I$(TOP)/glib20/libglib/glib -L$(TOP)/usbip/libsysfs/lib/.libs -L$(TOP)/glib20/libglib/glib/.libs"
 
 usbip:
 	$(MAKE) -C usbip
@@ -14,5 +14,6 @@ usbip-install:
 	install -D usbip/cmd/.libs/bind_driver $(INSTALLDIR)/usbip/usr/sbin/bind_driver
 	install -D usbip/cmd/.libs/usbip $(INSTALLDIR)/usbip/usr/sbin/usbip
 	install -D usbip/cmd/.libs/usbipd $(INSTALLDIR)/usbip/usr/sbin/usbipd
-	install -D usbip/lib/.libs/libusbip.so.0 $(INSTALLDIR)/usbip/usr/lib/libusbip.so.0
+	install -D usbip/libsrc/.libs/libusbip.so.0 $(INSTALLDIR)/usbip/usr/lib/libusbip.so.0
+	install -D usbip/libsysfs/lib/.libs/libsysfs.so.2 $(INSTALLDIR)/usbip/usr/lib/libsysfs.so.2
 	install -D usbip/usb.ids $(INSTALLDIR)/usbip/usr/share/hwdata/usb.ids
