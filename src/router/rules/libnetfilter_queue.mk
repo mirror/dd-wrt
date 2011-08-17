82526cba8b71c48f20fc42499972c29f5b190a48
BrainSlayer <s.gottschall@dd-wrt.com>
2011-08-17 14:26:40 +0000

    not yet finished but works somehow
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17496 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libnetfilter_queue.mk
+++ src/router/rules/libnetfilter_queue.mk
@@ -3,12 +3,16 @@ libnetfilter_queue-configure: libnfnetlink
 	export LIBNFNETLINK_LIBS="-L$(TOP)/libnfnetlink/src/.libs" ; \
 	cd libnetfilter_queue && ./configure \
 		--build=$(ARCH)-linux \
-		--host=$(ARCH)-linux-gnu  
+		--host=$(ARCH)-linux-gnu \
+		--prefix=/usr \
+		--disable-shared \
+		--enable-static
 		#CFLAGS="$(COPTS) -fPIC -DNEED_PRINTF -I$(TOP)/libnfnetlink/include/libnfnetlink" LDFLAGS="-L$(TOP)/libnfnetlink/src/.libs"
 
 
 libnetfilter_queue: libnfnetlink
 	$(MAKE) -C libnetfilter_queue CFLAGS="$(COPTS) -DNEED_PRINTF -I$(TOP)/libnfnetlink/include" LDFLAGS="-L$(TOP)/libnfnetlink/src/.libs"
 
-libdnet-install:
-	@true
+libnetfilter_queue-install:
+	@true	
+#	install -D libnetfilter_queue/src/.libs/libnetfilter_queue.so.1 $(INSTALLDIR)/libnetfilter_queue/usr/lib/libnetfilter_queue.so.1
