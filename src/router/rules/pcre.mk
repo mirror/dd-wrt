feadb1c5a1a7b10443da8e41e66cda06694ea5f1
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-08-17 14:26:40 +0000

    not yet finished but works somehow
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17496 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/pcre.mk
+++ src/router/rules/pcre.mk
@@ -1,11 +1,13 @@
 pcre-configure:
-	cd pcre && ./configure --host=$(ARCH)-linux-uclibc --disable-xmldoc --enable-utf8 --enable-unicode-properties --disable-pcretest-libreadline
+	cd pcre && ./configure --host=$(ARCH)-linux-uclibc CFLAGS="$(COPTS) -DNEED_PRINTF" --prefix=/usr --disable-xmldoc --enable-utf8 --enable-unicode-properties --disable-pcretest-libreadline
 
 pcre:
-	$(MAKE) -C pcre CFLAGS="$(COPTS) -DNEED_PRINTF"
+	$(MAKE) -C pcre CFLAGS="$(COPTS) -DNEED_PRINTF" CXXFLAGS="$(COPTS) -DNEED_PRINTF" CPPFLAGS="$(COPTS) -DNEED_PRINTF"
 
 pcre-clean:
 	$(MAKE) -C pcre clean CFLAGS="$(COPTS) -DNEED_PRINTF"
 
 pcre-install:
-	install -D l2tpv3tun/l2tpv3tun $(INSTALLDIR)/l2tpv3tun/usr/sbin/l2tpv3tun
+	install -D pcre/.libs/libpcre.so.0 $(INSTALLDIR)/pcre/usr/lib/libpcre.so.0
+	install -D pcre/.libs/libpcrecpp.so.0 $(INSTALLDIR)/pcre/usr/lib/libpcrecpp.so.0
+	install -D pcre/.libs/libpcreposix.so.0 $(INSTALLDIR)/pcre/usr/lib/libpcreposix.so.0
