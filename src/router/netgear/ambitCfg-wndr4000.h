3ad63d7bd5dcd8013f14927559747044137d50aa
eko <USER@DOMAIN>
2011-08-30 07:08:35 +0000

    netgear wndr3400
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17533 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/netgear/ambitCfg-wndr4000.h
@@ -0,0 +1,33 @@
+
+#ifndef _AMBITCFG_H
+#define _AMBITCFG_H
+
+#define WW_VERSION           1 /* WW SKUs */
+#define NA_VERSION           2 /* NA SKUs */
+#define JP_VERSION           3
+#define GR_VERSION           4
+#define PR_VERSION           5
+#define KO_VERSION           6
+#define RU_VERSION           7
+#define SS_VERSION           8
+#define PT_VERSION           9
+#define TWC_VERSION          10
+#define BRIC_VERSION         11
+#define SK_VERSION           12
+
+
+#define WLAN_REGION          WW_VERSION
+#define FW_REGION            WW_VERSION   /* true f/w region */
+
+/*formal version control*/
+#define AMBIT_HARDWARE_VERSION     "U12H18100"
+#define AMBIT_SOFTWARE_VERSION     "V1.0.0.60"
+#define AMBIT_UI_VERSION           "8.0.49"
+#define STRING_TBL_VERSION         "1.0.0.60_2.1.13.1"
+
+#define AMBIT_PRODUCT_NAME          "WNDR4000"
+#define AMBIT_PRODUCT_DESCRIPTION   "Netgear Wireless Router WNDR4000"
+#define UPnP_MODEL_URL              "WNDR4000.aspx"
+#define UPnP_MODEL_DESCRIPTION      "NETGEAR WNDR4000 N750 Wireless Dual Band Gigabit Router"
+
+#endif /*_AMBITCFG_H*/
