1be3a8d7747c7872d5dfbaf999025192b6fed9dd
sash <USER@DOMAIN>
2011-09-14 17:08:54 +0000

    ppX_connected: peers dont get reliably deleted. maybe we should seek for the pid instead of the interface. the pid is more unique at runtime.
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17611 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/pptp.c
+++ src/router/services/services/pptp.c
@@ -239,8 +239,8 @@ void start_pptpd(void)
 		nvram_get("pptpd_ipup_script") : "");
 	fclose(fp);
 	fp = fopen("/tmp/pptpd/ip-down", "w");
-	fprintf(fp, "#!/bin/sh\n" "grep -v $1  /tmp/pptp_connected > /tmp/pptp_connected.new\n"	//
-		"mv /tmp/pptp_connected.new /tmp/pptp_connected\n"	//
+	fprintf(fp, "#!/bin/sh\n" "grep -v $PPPD_PID /tmp/pptp_connected > /tmp/pptp_connected.tmp\n"	//
+		"mv /tmp/pptp_connected.tmp /tmp/pptp_connected\n"	//
 		"iptables -D FORWARD -i $1 -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu\n"	//
 		"iptables -D INPUT -i $1 -j ACCEPT\n" "iptables -D FORWARD -i $1 -j ACCEPT\n"	//
 		"iptables -t nat -D PREROUTING -i $1 -p udp -m udp --sport 9 -j DNAT --to-destination %s\n"	// rule for wake on lan over pptp tunnel
