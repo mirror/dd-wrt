87e320735e99d83cccef88800d6767ffdf5d2014
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-09-18 19:11:17 +0000

    remove orphan files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17630 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/busybox/coreutils/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_coreutils/built-in.o :=  rm -f coreutils/built-in.o; arm-linux-uclibc-ar rcs coreutils/built-in.o
--- src/router/busybox/procps/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_procps/built-in.o :=  rm -f procps/built-in.o; arm-linux-uclibc-ar rcs procps/built-in.o
