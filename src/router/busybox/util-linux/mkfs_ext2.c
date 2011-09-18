7710ec1269720dc8381240255c959bd675f67dea
BrainSlayer <s.gottschall@dd-wrt.com>
2011-09-18 19:30:11 +0000

    ext2fs header fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17633 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/busybox/util-linux/mkfs_ext2.c
+++ src/router/busybox/util-linux/mkfs_ext2.c
@@ -47,7 +47,7 @@
 /* //usage:  "\n	-v		Verbose" */
 
 #include "libbb.h"
-#include <linux/fs.h>
+//#include <linux/fs.h>
 #include <linux/ext2_fs.h>
 
 #define ENABLE_FEATURE_MKFS_EXT2_RESERVED_GDT 0
