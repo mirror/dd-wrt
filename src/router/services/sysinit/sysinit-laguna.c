b4aeb2942e88efb07361b8c81fdc1fc649303724
chris <chris@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-10-22 13:48:04 +0000

    gateworks laguna hardware detection ; only run gsp_updater if we are really on 2388
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17801 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-laguna.c
+++ src/router/services/sysinit/sysinit-laguna.c
@@ -125,7 +125,7 @@ void start_sysinit(void)
 	stime(&tm);
 	nvram_set("wl0_ifname", "ath0");
 	eval("hwclock", "-s");
-	if (getRouterBrand() == ROUTER_BOARD_GW2388)
+	if (!strcmp(nvram_safe_get("DD_BOARD"), "Gateworks Laguna GW2388") || !strcmp(nvram_safe_get("DD_BOARD2"), "Gateworks Laguna GW2388") )
 		sysprintf("gsp_updater -f /etc/gsp_update.txt");
 
 	return;
