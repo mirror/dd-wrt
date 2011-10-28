0cf0b0ce4a3dfe876e145214ffaff81a047a8c89
BrainSlayer <s.gottschall@dd-wrt.com>
2011-10-28 09:55:34 +0000

    new usbip daemon version
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17816 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/usbip/libsrc/usbip_host_driver.h
+++ src/router/usbip/libsrc/usbip_host_driver.h
@@ -16,9 +16,33 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef __UTILS_H
-#define __UTILS_H
+#ifndef __USBIP_HOST_DRIVER_H
+#define __USBIP_HOST_DRIVER_H
 
-int modify_match_busid(char *busid, int add);
+#include <stdint.h>
+#include "usbip_common.h"
 
-#endif /* __UTILS_H */
+struct usbip_host_driver {
+	int ndevs;
+	struct sysfs_driver *sysfs_driver;
+	/* list of exported device */
+	struct dlist *edev_list;
+};
+
+struct usbip_exported_device {
+	struct sysfs_device *sudev;
+	int32_t status;
+	struct usbip_usb_device udev;
+	struct usbip_usb_interface uinf[];
+};
+
+extern struct usbip_host_driver *host_driver;
+
+int usbip_host_driver_open(void);
+void usbip_host_driver_close(void);
+
+int usbip_host_refresh_device_list(void);
+int usbip_host_export_device(struct usbip_exported_device *edev, int sockfd);
+struct usbip_exported_device *usbip_host_get_device(int num);
+
+#endif /* __USBIP_HOST_DRIVER_H */
