27a0963dd5f0625af574ab3bac19502183592283
BrainSlayer <s.gottschall@dd-wrt.com>
2011-11-11 17:48:15 +0000

    fixup for another toolchain variant
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17885 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/proftpd/lib/glibc-glob.c
+++ src/router/proftpd/lib/glibc-glob.c
@@ -237,7 +237,7 @@ extern void bcopy ();
 #if !defined HAVE_MEMPCPY && defined __GLIBC__ && __GLIBC__ - 0 == 2 && __GLIBC_MINOR__ >= 1
 # define HAVE_MEMPCPY	1
 # undef  mempcpy
-# define mempcpy(Dest, Src, Len) __mempcpy (Dest, Src, Len)
+# define mempcpy(Dest, Src, Len) __mempcpy2 (Dest, Src, Len)
 #endif
 #endif
 
