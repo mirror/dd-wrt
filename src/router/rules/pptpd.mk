6104e78b4c8f4ead24e1c83e42d0c513f705e9c6
BrainSlayer <s.gottschall@dd-wrt.com>
2011-11-16 13:53:09 +0000

    pptp accel
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17905 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/pptpd.mk
+++ src/router/rules/pptpd.mk
@@ -1,18 +1,40 @@
+pptpd-configure:
+ifeq ($(CONFIG_PPTP_ACCEL),y)
+	cd pptpd-accel && ./configure CC="$(CC)" CFLAGS="$(COPTS) -DNEED_PRINTF" --prefix=/usr --host=$(ARCH)-linux
+
+endif
+	@true
+
 pptpd:
 ifeq ($(CONFIG_PPTPD),y)
+ifeq ($(CONFIG_PPTP_ACCEL),y)
+	$(MAKE) -C pptpd-accel
+else
 	$(MAKE) -C pptpd
+endif
 else
 	@true
 endif
 
 pptpd-clean:
+ifeq ($(CONFIG_PPTP_ACCEL),y)
+	$(MAKE) -C pptpd-accel clean
+else
 	$(MAKE)  -C pptpd clean
+endif
 
 pptpd-install:
 ifeq ($(CONFIG_PPTPD),y)
+ifeq ($(CONFIG_PPTP_ACCEL),y)
+	install -D pptpd-accel/pptpd $(INSTALLDIR)/pptpd/usr/sbin/pptpd
+	install -D pptpd-accel/pptpctrl $(INSTALLDIR)/pptpd/usr/sbin/pptpctrl
+	install -D pptpd-accel/bcrelay $(INSTALLDIR)/pptpd/usr/sbin/bcrelay
+	@true
+else
 	install -D pptpd/pptpd $(INSTALLDIR)/pptpd/usr/sbin/pptpd
 	install -D pptpd/pptpctrl $(INSTALLDIR)/pptpd/usr/sbin/pptpctrl
 	install -D pptpd/bcrelay $(INSTALLDIR)/pptpd/usr/sbin/bcrelay
         # So that generic rule does not take precedence
 	@true
+endif
 endif
