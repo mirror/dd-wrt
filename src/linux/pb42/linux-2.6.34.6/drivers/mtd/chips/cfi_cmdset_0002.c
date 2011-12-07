76e131264c1eb20d9d1a54a4406af783627fd06f
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2011-12-07 19:31:36 +0000

    seem to make troubles with some ap83 devices
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@17974 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/pb42/linux-2.6.34.6/drivers/mtd/chips/cfi_cmdset_0002.c
+++ src/linux/pb42/linux-2.6.34.6/drivers/mtd/chips/cfi_cmdset_0002.c
@@ -620,8 +620,7 @@ static int get_chip(struct map_info *map, struct flchip *chip, unsigned long adr
 		return 0;
 
 	case FL_ERASING:
-//		if (1 /* no suspend */ || !cfip || !(cfip->EraseSuspend & (0x1|0x2)) ||
-		if (!cfip || !(cfip->EraseSuspend & (0x1|0x2)) ||
+		if (1 /* no suspend */ || !cfip || !(cfip->EraseSuspend & (0x1|0x2)) ||
 		    !(mode == FL_READY || mode == FL_POINT ||
 		    (mode == FL_WRITING && (cfip->EraseSuspend & 0x2))))
 			goto sleep;
