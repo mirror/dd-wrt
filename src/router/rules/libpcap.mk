2cd4e9d938d6ab125e74ad7738d674e78f21f703
BrainSlayer <s.gottschall@dd-wrt.com>
2011-12-15 15:12:52 +0000

    fix outstanding cc variables
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18013 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libpcap.mk
+++ src/router/rules/libpcap.mk
@@ -1,5 +1,5 @@
 libpcap:
-#	$(MAKE) -C libpcap CC=$(CC) AR=$(AR) RANLIB=$(RANLIB) libpcap.so 
+#	$(MAKE) -C libpcap CC="$(CC)" AR=$(AR) RANLIB=$(RANLIB) libpcap.so 
 	$(MAKE) -C libpcap_noring CC="$(CC)" AR=$(AR) RANLIB=$(RANLIB) version.h
 ifeq ($(CONFIG_LIBPCAP_SHARED),y)
 	$(MAKE) -C libpcap_noring CC="$(CC)" AR=$(AR) RANLIB=$(RANLIB) libpcap.so 
