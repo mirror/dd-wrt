38296611b6a830944f5b21c45e4e2412fb9d6d2a
chris <chris@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-01-05 16:29:59 +0000

    swconfig rule file fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18082 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/swconfig.mk
+++ src/router/rules/swconfig.mk
@@ -8,4 +8,4 @@ swconfig-clean:
 	$(MAKE) -C swconfig clean CFLAGS="$(COPTS) -I$(TOP)/libnl-tiny/include -DCONFIG_LIBNL20 -D_GNU_SOURCE  -DNEED_PRINTF" LDFLAGS="-L$(TOP)/libnl-tiny/ -lnl-tiny" LIBS="-lm -lnl-tiny"
 
 swconfig-install:
-	install -D swconfig $(INSTALLDIR)/usr/sbin/swconfig
+	install -D swconfig/swconfig $(INSTALLDIR)/swconfig/usr/sbin/swconfig
