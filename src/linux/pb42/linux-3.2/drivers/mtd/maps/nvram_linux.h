98fc6ccf017a1a7cc07a7b4c7ccf402e03738b85
BrainSlayer <s.gottschall@dd-wrt.com>
2012-01-11 19:46:28 +0000

    for that device we are using a bigger space
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18150 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/pb42/linux-3.2/drivers/mtd/maps/nvram_linux.h
+++ src/linux/pb42/linux-3.2/drivers/mtd/maps/nvram_linux.h
@@ -163,7 +163,11 @@ extern int nvram_getall(char *buf, int count);
 #define NVRAM_MAGIC			0x48534C46	/* 'NVFL' */
 #define NVRAM_VERSION		1
 #define NVRAM_HEADER_SIZE	sizeof(struct nvram_header)
+#ifdef CONFIG_WZR_HP_G300NH
+#define NVRAM_SPACE			0x20000
+#else
 #define NVRAM_SPACE			0x10000
+#endif
 
 #define	NVRAM_CRC_MASK		0x000000ff
 #define	NVRAM_CRC_SHIFT		0
