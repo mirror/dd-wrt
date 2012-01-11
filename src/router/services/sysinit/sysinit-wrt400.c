858c62b38dc399e55c4b7ad395b37a36b597c823
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-01-11 18:57:44 +0000

    test fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18149 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-wrt400.c
+++ src/router/services/sysinit/sysinit-wrt400.c
@@ -118,7 +118,7 @@ void start_sysinit(void)
 			copy[1], copy[2], copy[3], copy[4], copy[5]);
 #elif HAVE_WZRHPAG300NH
 		system("swconfig dev eth0 set reset 1");
-		system("swconfig dev eth0 set enable_vlan 1");
+		system("swconfig dev eth0 set enable_vlan 0");
 		system("swconfig dev eth0 vlan 1 set ports \"0 1 2 3 4\"");
 		system("swconfig dev eth0 set apply");
 		fseek(fp, 0x5120C, SEEK_SET);
