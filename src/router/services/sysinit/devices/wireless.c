e12e59cff6b680e4e6340e322a9fa59212ff18d8
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-01-21 17:09:05 +0000

    save memory if these devices arent used. so just do not load the drivers if they arent needed
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18257 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/devices/wireless.c
+++ src/router/services/sysinit/devices/wireless.c
@@ -67,36 +67,145 @@ static void setWirelessLed(int phynum, int ledpin)
 #define setWirelessLedPhy0(b) setWirelessLed(0,b+32)
 #define setWirelessLedPhy1(b) setWirelessLed(1,b+48)
 
+static char *has_device(char *dev)
+{
+	char path[64];
+	static char devstr[32];
+	sprintf(path, "/sys/bus/pci/devices/0000:00:%s/device", dev);
+	FILE *fp = fopen(path, "rb");
+	if (fp) {
+		fscanf(fp, "%s", devstr);
+		fclose(fp);
+		return devstr;
+	}
+
+	return "";
+}
+
+#define AR5416_DEVID_PCI	0x0023
+#define AR5416_DEVID_PCIE	0x0024
+#define AR9160_DEVID_PCI	0x0027
+#define AR9280_DEVID_PCI	0x0029
+#define AR9280_DEVID_PCIE	0x002a
+#define AR9285_DEVID_PCIE	0x002b
+#define AR2427_DEVID_PCIE	0x002c
+#define AR9287_DEVID_PCI	0x002d
+#define AR9287_DEVID_PCIE	0x002e
+#define AR9300_DEVID_PCIE	0x0030
+#define AR9300_DEVID_AR9340	0x0031
+#define AR9300_DEVID_AR9485_PCIE 0x0032
+#define AR9300_DEVID_AR9580	0x0033
+#define AR9300_DEVID_AR9462	0x0034
+#define AR9300_DEVID_AR9330	0x0035
+
+#define AR5416_AR9100_DEVID	0x000b
+
 static void detect_wireless_devices(void)
 {
+	int loadath9k = 1;
+	int loadlegacy = 1;
 #ifdef HAVE_RT61
-	FILE *fp = fopen("/sys/bus/pci/devices/0000:00:0e.0/device", "rb");
-	if (fp) {
-		char str[32];
-		fscanf(fp, "%s", str);
-		fclose(fp);
-		if (!strcmp(str, "0x3592"))
-			nvram_set("rtchip", "3062");
-		else
-			nvram_set("rtchip", "2860");
-	} else
+	if (!strcmp(has_device("0e.00"), "0x3592"))
+		nvram_set("rtchip", "3062");
+	else
 		nvram_set("rtchip", "2860");
-
+#endif
+#ifdef HAVE_XSCALE
+	loadath9k = 0;
+	loadlegacy = 0;
+	char path[32];
+	int i = 0;
+	for (i = 0; i < 5; i++) {
+		sprintf(path, "0%d.00", i);
+		if (!strcmp(has_device(path), "0x0023"))
+			loadath9k = 1;
+		if (!strcmp(has_device(path), "0x0024"))
+			loadath9k = 1;
+		if (!strcmp(has_device(path), "0x0027"))
+			loadath9k = 1;
+		if (!strcmp(has_device(path), "0x0029"))
+			loadath9k = 1;
+		if (!strcmp(has_device(path), "0x002a"))
+			loadath9k = 1;
+		if (!strcmp(has_device(path), "0x002b"))
+			loadath9k = 1;
+		if (!strcmp(has_device(path), "0x002c"))
+			loadath9k = 1;
+		if (!strcmp(has_device(path), "0x002d"))
+			loadath9k = 1;
+		if (!strcmp(has_device(path), "0x002e"))
+			loadath9k = 1;
+		if (!strcmp(has_device(path), "0x0030"))
+			loadath9k = 1;
+		if (!strcmp(has_device(path), "0x0031"))
+			loadath9k = 1;
+		if (!strcmp(has_device(path), "0x0032"))
+			loadath9k = 1;
+		if (!strcmp(has_device(path), "0x0034"))
+			loadath9k = 1;
+		if (!strcmp(has_device(path), "0x0035"))
+			loadath9k = 1;
+		if (!strcmp(has_device(path), "0x000b"))
+			loadath9k = 1;
+		if (!strcmp(has_device(path), "0x001b"))
+			loadlegacy = 1;
+		if (!strcmp(has_device(path), "0x0013"))
+			loadlegacy = 1;
+		if (!strcmp(has_device(path), "0x0207"))
+			loadlegacy = 1;
+		if (!strcmp(has_device(path), "0x0007"))
+			loadlegacy = 1;
+		if (!strcmp(has_device(path), "0xff16"))
+			loadlegacy = 1;
+		if (!strcmp(has_device(path), "0x0012"))
+			loadlegacy = 1;
+		if (!strcmp(has_device(path), "0x1014"))
+			loadlegacy = 1;
+		if (!strcmp(has_device(path), "0x101a"))
+			loadlegacy = 1;
+		if (!strcmp(has_device(path), "0x0015"))
+			loadlegacy = 1;
+		if (!strcmp(has_device(path), "0x0016"))
+			loadlegacy = 1;
+		if (!strcmp(has_device(path), "0x0017"))
+			loadlegacy = 1;
+		if (!strcmp(has_device(path), "0x0018"))
+			loadlegacy = 1;
+		if (!strcmp(has_device(path), "0x0019"))
+			loadlegacy = 1;
+		if (!strcmp(has_device(path), "0x001a"))
+			loadlegacy = 1;
+		if (!strcmp(has_device(path), "0x001b"))
+			loadlegacy = 1;
+		if (!strcmp(has_device(path), "0x018a"))
+			loadlegacy = 1;
+		if (!strcmp(has_device(path), "0x001c"))
+			loadlegacy = 1;
+		if (!strcmp(has_device(path), "0x001d"))
+			loadlegacy = 1;
+		if (!strcmp(has_device(path), "0x9013"))
+			loadlegacy = 1;
+		if (!strcmp(has_device(path), "0xff1a"))
+			loadlegacy = 1;
+	}
 #endif
 #ifndef HAVE_NOWIFI
 	nvram_default_get("rate_control", "minstrel");
 #ifdef HAVE_MADWIFI
-	fprintf(stderr, "load ATH 802.11 a/b/g Driver\n");
-	insmod("ath_hal");
-	if (nvram_get("rate_control") != NULL) {
-		char rate[64];
+	if (loadlegacy) {
+		fprintf(stderr, "load ATH 802.11 a/b/g Driver\n");
+		insmod("ath_hal");
+		if (nvram_get("rate_control") != NULL) {
+			char rate[64];
 
-		sprintf(rate, "ratectl=%s", nvram_safe_get("rate_control"));
-		eval("insmod", "ath_pci", rate);
-		eval("insmod", "ath_ahb", rate);
-	} else {
-		insmod("ath_pci");
-		insmod("ath_ahb");
+			sprintf(rate, "ratectl=%s",
+				nvram_safe_get("rate_control"));
+			eval("insmod", "ath_pci", rate);
+			eval("insmod", "ath_ahb", rate);
+		} else {
+			insmod("ath_pci");
+			insmod("ath_ahb");
+		}
 	}
 #ifdef HAVE_ATH9K
 #ifndef HAVE_MADWIFI_MIMO
@@ -105,36 +214,40 @@ static void detect_wireless_devices(void)
 	if (nvram_match("mimo_driver", "ath9k"))
 #endif
 	{
-		fprintf(stderr, "load ATH9K 802.11n Driver\n");
-		// some are just for future use and not (yet) there
-		insmod("/lib/ath9k/compat.ko");
-		insmod("/lib/ath9k/compat_firmware_class.ko");
-		insmod("/lib/ath9k/cfg80211.ko");
-		insmod("/lib/ath9k/mac80211.ko");
-		insmod("/lib/ath9k/ath.ko");
-		insmod("/lib/ath9k/ath9k_hw.ko");
-		insmod("/lib/ath9k/ath9k_common.ko");
+		if (loadath9k) {
+			fprintf(stderr, "load ATH9K 802.11n Driver\n");
+			// some are just for future use and not (yet) there
+			insmod("/lib/ath9k/compat.ko");
+			insmod("/lib/ath9k/compat_firmware_class.ko");
+			insmod("/lib/ath9k/cfg80211.ko");
+			insmod("/lib/ath9k/mac80211.ko");
+			insmod("/lib/ath9k/ath.ko");
+			insmod("/lib/ath9k/ath9k_hw.ko");
+			insmod("/lib/ath9k/ath9k_common.ko");
 #ifdef HAVE_WZRG450
-		system("/sbin/insmod /lib/ath9k/ath9k.ko blink=1");
+			system("/sbin/insmod /lib/ath9k/ath9k.ko blink=1");
 #else
-		insmod("/lib/ath9k/ath9k.ko");
+			insmod("/lib/ath9k/ath9k.ko");
 #endif
-		delete_ath9k_devices(NULL);
+			delete_ath9k_devices(NULL);
+		}
 	} else {
 #endif
 #ifdef HAVE_MADWIFI_MIMO
-		fprintf(stderr, "load ATH 802.11n Driver\n");
-		insmod("/lib/80211n/ath_mimo_hal.ko");
-		if (nvram_get("rate_control") != NULL) {
-			char rate[64];
+		if (loadath9k) {
+			fprintf(stderr, "load ATH 802.11n Driver\n");
+			insmod("/lib/80211n/ath_mimo_hal.ko");
+			if (nvram_get("rate_control") != NULL) {
+				char rate[64];
 
-			sprintf(rate, "ratectl=%s",
-				nvram_safe_get("rate_control"));
-			insmod("/lib/80211n/ath_mimo_pci.ko");
-			insmod("/lib/80211n/ath_mimo_ahb.ko");
-		} else {
-			insmod("/lib/80211n/ath_mimo_pci.ko");
-			insmod("/lib/80211n/ath_mimo_ahb.ko");
+				sprintf(rate, "ratectl=%s",
+					nvram_safe_get("rate_control"));
+				insmod("/lib/80211n/ath_mimo_pci.ko");
+				insmod("/lib/80211n/ath_mimo_ahb.ko");
+			} else {
+				insmod("/lib/80211n/ath_mimo_pci.ko");
+				insmod("/lib/80211n/ath_mimo_ahb.ko");
+			}
 		}
 #endif
 #ifdef HAVE_ATH9K
