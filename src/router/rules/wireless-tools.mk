65b16bd4472a0e8bf20d1e458d52829e2d4a23e7
BrainSlayer <s.gottschall@dd-wrt.com>
2012-01-22 19:11:03 +0000

    newer midnight commander and wireless tools multicall
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18280 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/wireless-tools.mk
+++ src/router/rules/wireless-tools.mk
@@ -5,7 +5,7 @@ wireless-tools:
 	make -C wireless-tools CC="$(CC)" CFLAGS="$(COPTS)" all iwmulticall
 
 wireless-tools-install:
-	make -C wireless-tools INSTALL_DIR=$(INSTALLDIR)/wireless-tools/usr/sbin install-bin
+	make -C wireless-tools INSTALL_DIR=$(INSTALLDIR)/wireless-tools/usr/sbin install-iwmulticall
 	make -C wireless-tools INSTALL_LIB=$(INSTALLDIR)/wireless-tools/lib install-dynamic
 	
 
