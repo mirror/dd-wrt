168e147348fea75850114ce5526ae129bdd449cc
BrainSlayer <s.gottschall@dd-wrt.com>
2012-01-22 19:17:21 +0000

    do not strip before postprocess
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18282 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/inadyn.mk
+++ src/router/rules/inadyn.mk
@@ -3,7 +3,7 @@ inadyn:
 
 inadyn-install:
 	install -D inadyn/bin/linux/inadyn $(INSTALLDIR)/inadyn/usr/sbin/inadyn
-	$(STRIP) $(INSTALLDIR)/inadyn/usr/sbin/inadyn
+#	$(STRIP) $(INSTALLDIR)/inadyn/usr/sbin/inadyn
 
 inadyn-clean:
 	$(MAKE) -C inadyn clean
