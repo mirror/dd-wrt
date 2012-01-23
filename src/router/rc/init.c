598f5f1320baadd28a35a43ff801a47dafd3b32c
BrainSlayer <s.gottschall@dd-wrt.com>
2012-01-23 10:42:23 +0000

    branding update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18290 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/init.c
+++ src/router/rc/init.c
@@ -546,6 +546,7 @@ int main(int argc, char **argv)
 	FILE *fp = fopen("/tmp/loginprompt", "wb");
 
 #ifndef HAVE_MAKSAT
+#ifndef HAVE_TRIMAX
 #ifndef HAVE_WIKINGS
 #ifndef HAVE_ESPOD
 #ifndef HAVE_NEXTMEDIA
@@ -569,6 +570,7 @@ int main(int argc, char **argv)
 	fprintf(fp,
 		"DD-WRT v24-sp2 custom (c) 2012 NewMedia-NET GmbH\nRelease: "
 		BUILD_DATE " (SVN revision: %s)\n", SVN_REVISION);
+#endif
 #endif
 #endif
 #endif
