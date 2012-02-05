755ab625d65a2f219979bdf3e916a5d31abde997
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-02-05 23:07:14 +0000

    nornet and wasp
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18374 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/network.c
+++ src/router/services/networking/network.c
@@ -1078,6 +1078,20 @@ void start_lan(void)
 		nvram_set("et0macaddr",
 			  ether_etoa(ifr.ifr_hwaddr.sa_data, eabuf));
 	strcpy(mac, nvram_safe_get("et0macaddr"));
+#elif HAVE_HORNET
+	if (getSTA() || getWET() || CANBRIDGE()) {
+		nvram_setz(lan_ifnames, "eth0 eth1 ath0");
+		PORTSETUPWAN("");
+	} else {
+		nvram_setz(lan_ifnames, "eth0 eth1 ath0");
+		PORTSETUPWAN("eth1");
+	}
+	strncpy(ifr.ifr_name, "eth1", IFNAMSIZ);
+	ioctl(s, SIOCGIFHWADDR, &ifr);
+	if (nvram_match("et0macaddr", ""))
+		nvram_set("et0macaddr",
+			  ether_etoa(ifr.ifr_hwaddr.sa_data, eabuf));
+	strcpy(mac, nvram_safe_get("et0macaddr"));
 #elif HAVE_WHRHPGN
 	if (getSTA() || getWET() || CANBRIDGE()) {
 		nvram_setz(lan_ifnames, "eth0 eth1 ath0");
