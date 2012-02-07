3f3a918284456d4f9fcbdbd660ea52bc4780d133
markus <markus@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-02-07 08:10:22 +0000

    implement gratuitous arp, thx @ Daniel Danzberger
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18388 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/utils.c
+++ src/router/libutils/utils.c
@@ -4452,3 +4452,177 @@ double HTTxRate40_400(unsigned int index)
 	}
 	return vHTTxRate40_400[index];
 }
+
+/* gartarp */
+
+struct arph {
+	uint16_t hw_type;
+
+	#define ARPHDR_ETHER            1
+
+	uint16_t proto_type;
+
+	char ha_len;
+	char pa_len;
+
+	#define ARPOP_BROADCAST         1
+	#define ARPOP_REPLY		        2
+	uint16_t opcode;
+	char source_add[ETH_ALEN];
+	char source_ip[IP_ALEN];
+	char dest_add[ETH_ALEN];
+	char dest_ip[IP_ALEN];
+
+
+} __attribute__((packed));
+
+#define ARP_HLEN	sizeof(struct arph) + ETH_HLEN
+#define ARPHDR_ETHER            1
+#define BCAST		"\xff\xff\xff\xff\xff\xff"
+
+static inline int get_iface_attr(int sk, char *iface, char *hw, char *paddr)
+{
+	int ret;
+        struct ifreq ifr;
+
+        strcpy(ifr.ifr_name, iface);
+
+        ret = ioctl(sk, SIOCGIFHWADDR, &ifr);
+	if (unlikely(ret == -1)) {
+		perror("ioctl SIOCGIFHWADDR");
+		return ret;
+	}
+	memcpy(hw, ifr.ifr_hwaddr.sa_data, ETH_ALEN);
+
+        ret = ioctl(sk, SIOCGIFADDR, &ifr);
+	if (unlikely(ret == -1)) {
+		perror("ioctl SIOCGIFADDR");
+		return ret;
+	}
+	memcpy(paddr, ifr.ifr_addr.sa_data+2, IP_ALEN);
+
+	return ret;
+}
+
+static inline void setup_eth(struct ether_header *eth, char *hw_addr)
+{
+	memcpy(eth->ether_shost, hw_addr, ETH_ALEN);
+	memcpy(eth->ether_dhost, BCAST, ETH_ALEN);
+	eth->ether_type = htons(ETH_P_ARP);
+}
+
+static inline void setup_garp(struct arph *arp, char *hw_addr, char *paddr)
+{
+        arp->hw_type          = htons(ARPHDR_ETHER);
+        arp->proto_type       = htons(ETH_P_IP);
+        arp->ha_len           = ETH_ALEN;
+        arp->pa_len           = IP_ALEN;
+
+	memcpy(arp->source_add, hw_addr, ETH_ALEN);
+        memcpy(arp->source_ip, paddr, IP_ALEN);
+}
+
+static inline void setup_garp_broadcast(struct arph *arp, char *paddr)
+{
+        arp->opcode = htons(ARPOP_BROADCAST);
+
+	memset(arp->dest_add, 0, ETH_ALEN);
+        memcpy(arp->dest_ip, paddr, IP_ALEN);
+}
+
+static inline void setup_garp_reply(struct arph *arp, char *hw_addr, char *paddr)
+{
+        arp->opcode = htons(ARPOP_REPLY);
+
+	memcpy(arp->dest_add, hw_addr, ETH_ALEN);
+        memcpy(arp->dest_ip, paddr, IP_ALEN);
+}
+
+/*
+ * send_garp 
+ *
+ * - sends 20 gartuitous arps 
+ * in a 200 millisec interval.
+ * One as braadcast and one as reply.
+ * 
+ * 
+ * parameter iface: sending interface name
+ *
+ * returns false on failure
+ *         true on success 
+ */
+static int send_garp(char *iface)
+{
+
+		
+	usleep(500000);
+	
+	char pkt[ARP_HLEN];
+	char iface_hw[ETH_ALEN];
+	char iface_paddr[IP_ALEN];
+	struct sockaddr addr;
+	struct ether_header *eth;
+	struct arph *arp;
+	int rc;
+	int sk;
+	int n_garps=10;
+
+	eth = (struct ether_header *)pkt;
+	arp = (struct arph *)(pkt + ETH_HLEN);
+
+	sk = socket(AF_INET, SOCK_PACKET, htons(ETH_P_ARP));
+	if (unlikely(sk == -1)) {
+		perror("socket");
+		return sk;
+	}
+
+	rc = get_iface_attr(sk, iface, iface_hw, iface_paddr);
+	if (unlikely(rc == -1))
+		goto out;
+
+	addr.sa_family = AF_INET;
+	strcpy(addr.sa_data, iface);
+
+	setup_eth(eth, iface_hw);
+	setup_garp(arp, iface_hw, iface_paddr);
+
+	while (n_garps--) {
+		setup_garp_broadcast(arp, iface_paddr);
+		rc = sendto(sk,
+				pkt,
+				ARP_HLEN,
+				0,
+				&addr,
+				sizeof(struct sockaddr)
+				);
+	        if (unlikely(rc == -1)) {
+	                perror("sendto");
+			goto out;
+		}
+
+		setup_garp_reply(arp, iface_hw, iface_paddr);
+		rc = sendto(sk,
+				pkt,
+				ARP_HLEN,
+				0,
+				&addr, 
+				sizeof(struct sockaddr)
+				); 
+	        if (unlikely(rc == -1)) {
+	                perror("sendto");
+			goto out;
+		}
+		//usleep(500000);
+	}
+
+out:
+	close(sk);
+	return rc;
+}
+
+int gratarp_main(char *iface)
+{
+	if (iface)
+		send_garp(iface);
+	return 0;
+}
