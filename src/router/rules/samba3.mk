c149023bbc51ba2ab67232b7c40eb35f1b7abaf9
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-02-13 15:54:04 +0000

    samba3.6 optimized by nbd
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18451 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/samba3.mk
+++ src/router/rules/samba3.mk
@@ -1,36 +1,67 @@
 ifeq ($(CONFIG_HOTPLUG2),y)
 export SAMBA3_EXTRA:= -DHAVE_DDINOTIFY
 endif
+CONFIGURE_VARS += \
+	ac_cv_file__proc_sys_kernel_core_pattern=yes \
+	libreplace_cv_HAVE_C99_VSNPRINTF=yes \
+	libreplace_cv_HAVE_GETADDRINFO=yes \
+	libreplace_cv_HAVE_IFACE_IFCONF=yes \
+	LINUX_LFS_SUPPORT=yes \
+	samba_cv_CC_NEGATIVE_ENUM_VALUES=yes \
+	samba_cv_HAVE_GETTIMEOFDAY_TZ=yes \
+	samba_cv_HAVE_IFACE_IFCONF=yes \
+	samba_cv_HAVE_KERNEL_OPLOCKS_LINUX=yes \
+	samba_cv_HAVE_SECURE_MKSTEMP=yes \
+	samba_cv_HAVE_WRFILE_KEYTAB=no \
+	samba_cv_USE_SETREUID=yes \
+	samba_cv_USE_SETRESUID=yes \
+	samba_cv_have_setreuid=yes \
+	samba_cv_have_setresuid=yes
 
+CONFIGURE_ARGS += \
+	--host=$(ARCH)-linux \
+	--exec-prefix=/usr \
+	--prefix=/ \
+	--disable-avahi \
+	--disable-cups \
+	--disable-pie \
+	--disable-relro \
+	--disable-static \
+	--disable-swat \
+	--with-codepagedir=/etc/samba \
+	--with-configdir=/etc/samba \
+	--with-included-iniparser \
+	--with-included-popt \
+	--with-lockdir=/var/lock \
+	--with-logfilebase=/var/log \
+	--with-nmbdsocketdir=/var/nmbd \
+	--with-piddir=/var/run \
+	--with-privatedir=/etc/samba \
+	--with-sendfile-support \
+	--without-cluster-support \
+	--without-ads \
+	--without-krb5 \
+	--without-ldap \
+	--without-pam \
+	--without-winbind
+	
+
+
+samba3-configure:
+	cd samba36/source3 && ./configure $(CONFIGURE_VARS) $(CONFIGURE_ARGS) CFLAGS="$(COPTS) -DMAX_DEBUG_LEVEL=2  -ffunction-sections -fdata-sections -Wl,--gc-sections $(LTO) $(SAMBA3_EXTRA)" LDFLAGS="$(COPTS) -DMAX_DEBUG_LEVEL=2  -ffunction-sections -fdata-sections -Wl,--gc-sections $(LTO) $(SAMBA3_EXTRA)"
 
 samba3:
-	CC="$(CC)" \
-	CFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections $(LTO) $(SAMBA3_EXTRA)" \
-	CPPFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections $(LTO) $(SAMBA3_EXTRA)" \
-	LDFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections $(LTO) $(SAMBA3_EXTRA)" \
-	$(MAKE) -C samba3/source all bin/smbpasswd WITH_LFS=yes
-
-	CC="$(CC)" \
-	CFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections -fPIC $(LTO) $(SAMBA3_EXTRA)" \
-	CPPFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections -fPIC $(LTO) $(SAMBA3_EXTRA)" \
-	LDFLAGS="$(COPTS)  -ffunction-sections -fdata-sections -Wl,--gc-sections -fPIC $(LTO) $(SAMBA3_EXTRA)" \
-	$(MAKE) -C samba3/source_nmbd all bin/nmbd WITH_LFS=yes
+	$(MAKE) -C samba36/source3 all WITH_LFS=yes DYNEXP= 
 
 samba3-install:
 	mkdir -p $(INSTALLDIR)/samba3
-	install -D samba3/source/bin/smbd $(INSTALLDIR)/samba3/usr/sbin/smbd
-ifeq ($(CONFIG_BUFFALO),y)
-	install -D samba3/source_nmbd/bin/nmbd $(INSTALLDIR)/samba3/usr/sbin/nmbd
-endif
-ifeq ($(CONFIG_NMBD),y)
-	install -D samba3/source_nmbd/bin/nmbd $(INSTALLDIR)/samba3/usr/sbin/nmbd
-endif
-	install -D samba3/source/bin/smbpasswd $(INSTALLDIR)/samba3/usr/sbin/smbpasswd
-	install -D samba3/config/samba3.webnas $(INSTALLDIR)/samba3/etc/config/samba3.webnas
-#	install -D samba3/config/samba3.startup $(INSTALLDIR)/samba3/etc/config/samba3.startup
-	install -D samba3/config/samba3.nvramconfig $(INSTALLDIR)/samba3/etc/config/samba3.nvramconfig
+	install -D samba36/source3/bin/samba_multicall $(INSTALLDIR)/samba3/usr/sbin/samba_multicall
+	install -D samba36/source3/bin/smbpasswd $(INSTALLDIR)/samba3/usr/sbin/smbpasswd
+	install -D samba36/config/samba3.webnas $(INSTALLDIR)/samba3/etc/config/samba3.webnas
+	install -D samba36/config/samba3.nvramconfig $(INSTALLDIR)/samba3/etc/config/samba3.nvramconfig
+	cd  $(INSTALLDIR)/samba3/usr/sbin && ln -sf samba_multicall smbd
+	cd  $(INSTALLDIR)/samba3/usr/sbin && ln -sf samba_multicall nmbd
 
 
 samba3-clean:
-	$(MAKE) -C samba3/source clean
-	$(MAKE) -C samba3/source_nmbd clean
+	$(MAKE) -C samba36/source3 clean
