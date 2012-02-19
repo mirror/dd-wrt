b1d7f88083473699be1dafdcb9b1fb560b73f7c7
BrainSlayer <s.gottschall@dd-wrt.com>
2012-02-19 12:50:15 +0000

    working
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18501 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-rcaa01.c
+++ src/router/services/sysinit/sysinit-rcaa01.c
@@ -86,7 +86,11 @@ void start_sysinit(void)
 	/*
 	 * network drivers 
 	 */
+#ifdef HAVE_HOTPLUG2
+	insmod("ar231x");
+#else
 	insmod("ar2313");
+#endif
 	detect_wireless_devices();
 	int s;
 	struct ifreq ifr;
