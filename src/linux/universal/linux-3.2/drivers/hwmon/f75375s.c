700909972fe5b38339c47520a80066d49bdb6709
BrainSlayer <s.gottschall@dd-wrt.com>
2012-02-21 02:26:57 +0000

    3.2.7
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18520 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.2/drivers/hwmon/f75375s.c
+++ src/linux/universal/linux-3.2/drivers/hwmon/f75375s.c
@@ -159,7 +159,7 @@ static inline void f75375_write8(struct i2c_client *client, u8 reg,
 static inline void f75375_write16(struct i2c_client *client, u8 reg,
 		u16 value)
 {
-	int err = i2c_smbus_write_byte_data(client, reg, (value << 8));
+	int err = i2c_smbus_write_byte_data(client, reg, (value >> 8));
 	if (err)
 		return;
 	i2c_smbus_write_byte_data(client, reg + 1, (value & 0xFF));
@@ -311,7 +311,7 @@ static int set_pwm_enable_direct(struct i2c_client *client, int nr, int val)
 		fanmode  |= (3 << FAN_CTRL_MODE(nr));
 		break;
 	case 2: /* AUTOMATIC*/
-		fanmode  |= (2 << FAN_CTRL_MODE(nr));
+		fanmode  |= (1 << FAN_CTRL_MODE(nr));
 		break;
 	case 3: /* fan speed */
 		break;
