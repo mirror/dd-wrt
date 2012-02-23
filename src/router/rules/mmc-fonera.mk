c7825f795c795000f79409ceb38b6275ac453eee
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-02-23 00:42:39 +0000

    cannot be used on newer kernels right now
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18530 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/mmc-fonera.mk
+++ src/router/rules/mmc-fonera.mk
@@ -1,11 +1,20 @@
 mmc-fonera:
+ifneq ($(CONFIG_HOTPLUG2),y)
 	$(MAKE) -C $(LINUXDIR) M=$(TOP)/mmc-fonera modules
+endif
+	@true	
 
 mmc-fonera-install:
+ifneq ($(CONFIG_HOTPLUG2),y)
 	install -D  mmc-fonera/mmc.ko $(INSTALLDIR)/mmc-fonera/lib/modules/$(KERNELRELEASE)/mmc.ko
 	install -D  mmc-fonera/config/mmc.webconfig $(INSTALLDIR)/mmc-fonera/etc/config/mmc.webconfig
 	install -D  mmc-fonera/config/mmc.startup $(INSTALLDIR)/mmc-fonera/etc/config/mmc.startup
 	install -D  mmc-fonera/config/mmc.nvramconfig $(INSTALLDIR)/mmc-fonera/etc/config/mmc.nvramconfig
+endif
+	@true	
 
 mmc-fonera-clean:
+ifneq ($(CONFIG_HOTPLUG2),y)
 	$(MAKE) -C $(LINUXDIR) M=$(TOP)/mmc-fonera clean	
+endif
+	@true	
