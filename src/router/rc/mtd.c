9bb23588d8d6855802878e389f2d6f62644c15ed
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-02-25 13:44:03 +0000

    more budged
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18557 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/mtd.c
+++ src/router/rc/mtd.c
@@ -383,7 +383,7 @@ int mtd_write(const char *path, const char *mtd)
 	int mul = 1;		// temporarily use 1 instead of 4 until we
 
 	// found a a solution
-	if ((info.freeram + info.bufferram) >= (trx.len + 1 * 1024 * 1024)) {
+	if ((info.freeram + info.bufferram) >= (trx.len + 4 * 1024 * 1024)) {
 		fprintf(stderr,
 			"The free memory is enough, writing image once.\n");
 		/* 
