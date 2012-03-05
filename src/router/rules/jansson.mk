5aa2389024bf49f0de39d7adbed3c6741843d702
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-03-05 03:15:35 +0000

    fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18661 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/jansson.mk
+++ src/router/rules/jansson.mk
@@ -1,5 +1,5 @@
 jansson-configure:
-	cd jansson && ./configure --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS) -ffunction-sections -fdata-sections -Wl,--gc-sections -fPIC"; fi
+	cd jansson && ./configure --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS) -ffunction-sections -fdata-sections -Wl,--gc-sections -fPIC"
 
 jansson: jansson-configure
 	$(MAKE) -C jansson
