91cd7af1d8600b26758c118aed90987333da6af6
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-03-05 14:27:33 +0000

    fix wr741 mac
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18667 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.2/arch/mips/ar7240/platform.c
+++ src/linux/universal/linux-3.2/arch/mips/ar7240/platform.c
@@ -321,7 +321,7 @@ int __init ar7240_platform_init(void)
 	ap91_pci_init(NULL, NULL);
 #else
 	ee = getCalData(0);
-	if (ee) {
+	if (ee && !mac) {
 	    if (!memcmp(((u8 *)ee)+0x20c,"\xff\xff\xff\xff\xff\xff",6) || !memcmp(((u8 *)ee)+0x20c,"\x00\x00\x00\x00\x00\x00",6)) {
 		printk("Found empty mac address in calibration dataset, leave the responsibility to the driver to use the correct one\n");
 		mac = ((u8 *)ee)-0x1000;
