8a2116944403a1e77051b5d82ad1544db495279f
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-03-13 17:23:11 +0000

    okay, the way is different
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18727 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/network.c
+++ src/router/services/networking/network.c
@@ -1260,20 +1260,6 @@ void start_lan(void)
 		nvram_set("et0macaddr",
 			  ether_etoa(ifr.ifr_hwaddr.sa_data, eabuf));
 	strcpy(mac, nvram_safe_get("et0macaddr"));
-#elif HAVE_WR741V4
-	if (getSTA() || getWET() || CANBRIDGE()) {
-		nvram_setz(lan_ifnames, "vlan1 vlan2 ath0");
-		PORTSETUPWAN("");
-	} else {
-		nvram_setz(lan_ifnames, "vlan1 vlan2 ath0");
-		PORTSETUPWAN("vlan2");
-	}
-	strncpy(ifr.ifr_name, "eth1", IFNAMSIZ);
-	ioctl(s, SIOCGIFHWADDR, &ifr);
-	if (nvram_match("et0macaddr", ""))
-		nvram_set("et0macaddr",
-			  ether_etoa(ifr.ifr_hwaddr.sa_data, eabuf));
-	strcpy(mac, nvram_safe_get("et0macaddr"));
 #elif HAVE_WR741
 	if (getSTA() || getWET() || CANBRIDGE()) {
 		nvram_setz(lan_ifnames, "eth0 eth1 ath0");
@@ -2821,10 +2807,6 @@ void start_wan(int status)
 	char *pppoe_wan_ifname = nvram_invmatch("pppoe_wan_ifname",
 						"") ?
 	    nvram_safe_get("pppoe_wan_ifname") : "eth1";
-#elif HAVE_WR741V4
-	char *pppoe_wan_ifname = nvram_invmatch("pppoe_wan_ifname",
-						"") ?
-	    nvram_safe_get("pppoe_wan_ifname") : "vlan2";
 #elif HAVE_WR741
 	char *pppoe_wan_ifname = nvram_invmatch("pppoe_wan_ifname",
 						"") ?
