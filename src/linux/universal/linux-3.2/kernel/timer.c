164f84dc8ebfcf94c1106b06d5ec84bed6ac42a6
BrainSlayer <s.gottschall@dd-wrt.com>
2012-03-17 22:05:11 +0000

    reduce icache load
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18767 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.2/kernel/timer.c
+++ src/linux/universal/linux-3.2/kernel/timer.c
@@ -763,7 +763,7 @@ unsigned long apply_slack(struct timer_list *timer, unsigned long expires)
 	if (mask == 0)
 		return expires;
 
-	bit = find_last_bit(&mask, BITS_PER_LONG);
+	bit = __fls(mask);
 
 	mask = (1 << bit) - 1;
 
