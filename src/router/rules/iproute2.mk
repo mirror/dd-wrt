3e641c841578d60e925d6864ff7a31172e6dc340
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-03-18 13:34:36 +0000

    no for micro
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18768 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/iproute2.mk
+++ src/router/rules/iproute2.mk
@@ -4,8 +4,12 @@ iproute2:
 iproute2-install:
 	chmod 0644 iproute2/tc/tc
 	install -D iproute2/tc/tc $(INSTALLDIR)/iproute2/usr/sbin/tc
+ifneq ($(CONFIG_DIST),"micro")
+ifneq ($(CONFIG_DIST),"micro-special")
 	chmod 0644 iproute2/ip/ip
 	install -D iproute2/ip/ip $(INSTALLDIR)/iproute2/usr/sbin/ip
+endif
+endif
 ifeq ($(CONFIG_WSHAPER),y)
 	chmod 0644 iproute2/wshaper.htb
 	install -D iproute2/wshaper.htb $(INSTALLDIR)/iproute2/usr/sbin/wshaper
