b5f795804a46f0d334d624f5b2251d3b31c6b7fe
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-03-21 18:01:42 +0000

    might affect this kernel too under some conditions
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18796 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.2/arch/mips/kernel/jump_label.c
+++ src/linux/universal/linux-3.2/arch/mips/kernel/jump_label.c
@@ -43,7 +43,7 @@ void arch_jump_label_transform(struct jump_entry *e,
 	get_online_cpus();
 	mutex_lock(&text_mutex);
 	*insn_p = insn;
-
+	if (flush_icache_range)
 	flush_icache_range((unsigned long)insn_p,
 			   (unsigned long)insn_p + sizeof(*insn_p));
 
