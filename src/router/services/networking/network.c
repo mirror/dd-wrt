32d102914ff1e34d007245037e8cc91d6d3477e2
BrainSlayer <s.gottschall@dd-wrt.com>
2012-03-22 12:05:04 +0000

    wasp vlan config
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18799 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/network.c
+++ src/router/services/networking/network.c
@@ -1106,6 +1106,20 @@ void start_lan(void)
 		nvram_set("et0macaddr",
 			  ether_etoa(ifr.ifr_hwaddr.sa_data, eabuf));
 	strcpy(mac, nvram_safe_get("et0macaddr"));
+#elif HAVE_WASP
+	if (getSTA() || getWET() || CANBRIDGE()) {
+		nvram_setz(lan_ifnames, "vlan1 vlan2 ath0");
+		PORTSETUPWAN("");
+	} else {
+		nvram_setz(lan_ifnames, "vlan1 vlan2 ath0");
+		PORTSETUPWAN("vlan2");
+	}
+	strncpy(ifr.ifr_name, "eth0", IFNAMSIZ);
+	ioctl(s, SIOCGIFHWADDR, &ifr);
+	if (nvram_match("et0macaddr", ""))
+		nvram_set("et0macaddr",
+			  ether_etoa(ifr.ifr_hwaddr.sa_data, eabuf));
+	strcpy(mac, nvram_safe_get("et0macaddr"));
 #elif HAVE_WHRHPGN
 	if (getSTA() || getWET() || CANBRIDGE()) {
 		nvram_setz(lan_ifnames, "eth0 eth1 ath0");
@@ -2850,6 +2864,10 @@ void start_wan(int status)
 	char *pppoe_wan_ifname = nvram_invmatch("pppoe_wan_ifname",
 						"") ?
 	    nvram_safe_get("pppoe_wan_ifname") : "eth0";
+#elif HAVE_WASP
+	char *pppoe_wan_ifname = nvram_invmatch("pppoe_wan_ifname",
+						"") ?
+	    nvram_safe_get("pppoe_wan_ifname") : "vlan2";
 #elif HAVE_WHRHPGN
 	char *pppoe_wan_ifname = nvram_invmatch("pppoe_wan_ifname",
 						"") ?
--- src/router/services/sysinit/sysinit-wasp.c
+++ src/router/services/sysinit/sysinit-wasp.c
@@ -92,8 +92,15 @@ void start_sysinit(void)
 		fprintf(stderr, "configure eth1 to %s\n", mac);
 		eval("ifconfig", "eth1", "hw", "ether", mac);
 	}*/
+	system("swconfig dev eth0 set reset 1");
+	system("swconfig dev eth0 set enable_vlan 1");
+	system("swconfig dev eth0 vlan 1 set ports \"0t 1 2 3 4\"");
+	system("swconfig dev eth0 vlan 2 set ports \"0t 5\"");
+	system("swconfig dev eth0 set apply");
 	eval("ifconfig", "eth0", "up");
-	eval("ifconfig", "eth1", "up");
+	eval("vconfig", "set_name_type", "VLAN_PLUS_VID_NO_PAD");
+	eval("vconfig", "add", "eth0", "1");
+	eval("vconfig", "add", "eth0", "2");
 	struct ifreq ifr;
 	int s;
 
