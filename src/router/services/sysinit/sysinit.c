0afb95eaf78f9971a447a18a4df81ce7f4d70ef3
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-03-29 11:51:33 +0000

    DIR825-C1 in progress
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18853 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit.c
+++ src/router/services/sysinit/sysinit.c
@@ -933,7 +933,7 @@ void start_restore_defaults(void)
 #elif HAVE_WASP
 	struct nvram_tuple generic[] = {
 		{"lan_ifname", "br0", 0},
-		{"lan_ifnames", "vlan1 vlan2 ath0", 0},
+		{"lan_ifnames", "vlan1 vlan2 ath0 ath1", 0},
 		{"wan_ifname", "", 0},
 		{"wan_ifname2", "", 0},
 		{"wan_ifnames", "", 0},
