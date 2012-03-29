b4aed85c2172b4d856e66c45828c1e727005a423
BrainSlayer <s.gottschall@dd-wrt.com>
2012-03-29 23:12:40 +0000

    das hat hier nix zu suchen
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18869 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/upgrade.c
+++ src/router/httpd/modules/upgrade.c
@@ -476,10 +476,12 @@ do_upgrade_post(char *url, webs_t stream, int len, char *boundary)	// jimmy,
 	if (nvram_match("sv_restore_defaults", "1")) {
 		eval("erase", "nvram");
 	}
-	nvram_set("sv_restore_defaults", "1");
 #ifdef HAVE_BUFFALO_SA
 		if(region_sa)
+		{
+			nvram_set("sv_restore_defaults", "1");
 			nvram_set("region", "SA");
+		}
 #endif
 	sys_commit();
 	
