9ca3484a14dcceef67fd4b7f8bbe8da94a644879
BrainSlayer <s.gottschall@dd-wrt.com>
2012-03-31 15:28:34 +0000

    interim fix until the switch driver is fixed
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18885 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-hornet.c
+++ src/router/services/sysinit/sysinit-hornet.c
@@ -122,7 +122,7 @@ void start_sysinit(void)
 #ifdef HAVE_WR741V4
 #ifdef HAVE_SWCONFIG
 		system("swconfig dev eth1 set reset 1");
-		system("swconfig dev eth1 set enable_vlan 0");
+		system("swconfig dev eth1 set enable_vlan 1");
 		system("swconfig dev eth1 vlan 1 set ports \"0 1 2 3 4\"");
 		system("swconfig dev eth1 set apply");
 #endif
