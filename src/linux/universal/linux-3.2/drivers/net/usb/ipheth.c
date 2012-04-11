b9b39ca2c32ebaec14587f7c5cb5d3abb9ae4b51
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-04-11 02:56:26 +0000

    need dedicate name
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@18974 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.2/drivers/net/usb/ipheth.c
+++ src/linux/universal/linux-3.2/drivers/net/usb/ipheth.c
@@ -453,7 +453,7 @@ static int ipheth_probe(struct usb_interface *intf,
 
 	netdev->netdev_ops = &ipheth_netdev_ops;
 	netdev->watchdog_timeo = IPHETH_TX_TIMEOUT;
-	strcpy(netdev->name, "eth%d");
+	strcpy(netdev->name, "iph%d");
 
 	dev = netdev_priv(netdev);
 	dev->udev = udev;
