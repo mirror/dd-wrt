13689ed9c61273671ec87d95313d5433f786fe23
eko <USER@DOMAIN>
2012-04-20 07:33:36 +0000

    missing define
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@19068 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/include.bcm/wlioctl.h
+++ src/include.bcm/wlioctl.h
@@ -642,6 +642,7 @@ typedef struct wlc_iov_trx_s {
 
 #define	WLC_IOCTL_MAXLEN	8192		/* max length ioctl buffer required */
 #define	WLC_IOCTL_SMLEN		256		/* "small" length ioctl buffer required */
+#define WLC_IOCTL_MEDLEN	1536	/* "med" length ioctl buffer required */
 
 /* common ioctl definitions */
 #define WLC_GET_MAGIC				0
