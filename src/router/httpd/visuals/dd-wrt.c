0c6dd51d30995837da5cb394da7b6f57b1ddad1a
BrainSlayer <s.gottschall@dd-wrt.com>
2012-04-20 10:54:29 +0000

    better
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@19076 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/dd-wrt.c
+++ src/router/httpd/visuals/dd-wrt.c
@@ -3632,7 +3632,7 @@ static int show_virtualssid(webs_t wp, char *prefix)
 #else
 		showbridgesettings(wp, var, 1, 0);
 #endif
-#if defined(HAVE_EASY_WIRELESS_CONFIG) && !defined(HAVE_MODERN)
+#if defined(HAVE_EASY_WIRELESS_CONFIG) && !defined(HAVE_BCMMODERN)
 		websWrite(wp, "</div>\n");
 #endif
 		websWrite(wp, "</fieldset><br />\n");
