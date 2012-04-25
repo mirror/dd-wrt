bb63e60dbcc60063ce62dbfdbc8011ee402ac29c
BrainSlayer <s.gottschall@dd-wrt.com>
2012-04-25 12:27:01 +0000

    guessed values
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@19117 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/netgear/ambitCfg-wndr3700v3.h
@@ -0,0 +1,59 @@
+/***************************************************************************
+#***
+#***    Copyright 2005  Hon Hai Precision Ind. Co. Ltd.
+#***    All Rights Reserved.
+#***    No portions of this material shall be reproduced in any form without the
+#***    written permission of Hon Hai Precision Ind. Co. Ltd.
+#***
+#***    All information contained in this document is Hon Hai Precision Ind.  
+#***    Co. Ltd. company private, proprietary, and trade secret property and 
+#***    are protected by international intellectual property laws and treaties.
+#***
+#****************************************************************************
+***
+***    Filename: ambitCfg.h
+***
+***    Description:
+***         This file is specific to each project. Every project should have a
+***    different copy of this file.
+***        Included from ambit.h which is shared by every project.
+***
+***    History:
+***
+***	   Modify Reason		                                        Author		         Date		                Search Flag(Option)
+***	--------------------------------------------------------------------------------------
+***    File Creation                                            Jasmine Yang    11/02/2005
+*******************************************************************************/
+
+
+#ifndef _AMBITCFG_H
+#define _AMBITCFG_H
+
+#define WW_VERSION           1 /* WW SKUs */
+#define NA_VERSION           2 /* NA SKUs */
+#define JP_VERSION           3
+#define GR_VERSION           4
+#define PR_VERSION           5
+#define KO_VERSION           6
+#define RU_VERSION           7
+#define SS_VERSION           8
+#define PT_VERSION           9
+#define TWC_VERSION          10
+#define BRIC_VERSION         11
+#define SK_VERSION           12
+
+#define WLAN_REGION          WW_VERSION
+#define FW_REGION            WW_VERSION   /* true f/w region */
+
+/*formal version control*/
+#define AMBIT_HARDWARE_VERSION     "U12H194T00"
+#define AMBIT_SOFTWARE_VERSION     "V1.0.0.22"
+#define AMBIT_UI_VERSION           "1.0.17"
+#define STRING_TBL_VERSION         "1.0.0.50_2.1.15.7"
+
+#define AMBIT_PRODUCT_NAME          "WNDR3700v3"
+#define AMBIT_PRODUCT_DESCRIPTION   "Netgear Wireless Router WNDR3700v3"
+#define UPnP_MODEL_URL              "WNDR3700v3.aspx"
+#define UPnP_MODEL_DESCRIPTION      "N900"
+
+#endif /*_AMBITCFG_H*/
