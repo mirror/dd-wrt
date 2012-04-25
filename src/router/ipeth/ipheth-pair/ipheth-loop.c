384c439432f08f30eb063d3077e1540b7e32d960
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-04-25 21:40:47 +0000

    loop daemon
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@19133 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/ipeth/ipheth-pair/ipheth-loop.c
@@ -0,0 +1,25 @@
+#include <signal.h>
+#include <stdlib.h>
+
+
+
+int main(int argc,char *argv[])
+{
+	pid_t pid;
+
+	pid = fork();
+	switch (pid) {
+	case -1:
+		perror("fork failed");
+		exit(1);
+	case 0:
+		for (;;) {
+		    system("ipheth-pair");
+		}
+		break;
+	default:
+		_exit(0);
+		break;
+	}
+
+}
