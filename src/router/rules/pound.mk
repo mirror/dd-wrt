1ccb5b7e516c4982f8d323c25e9699c6b6ed6897
BrainSlayer <s.gottschall@dd-wrt.com>
2012-04-29 19:46:18 +0000

    redo if makefile does not exist
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@19166 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/pound.mk
+++ src/router/rules/pound.mk
@@ -7,6 +7,7 @@ pound-clean:
 	make -C pound clean
 
 pound:
+	if ! test -e "pound/Makefile"; then cd pound && ./configure --prefix=/usr --with-ssl=$(TOP)/openssl --host=$(ARCH)-linux CC="$(ARCH)-linux-uclibc-gcc" LDFLAGS="-Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/openssl" CFLAGS="-Drpl_malloc=malloc -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections $(COPTS) -L$(TOP)/openssl"; fi
 	make -C pound CC="$(CC)"
 
 pound-install:
