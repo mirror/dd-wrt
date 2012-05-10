9fec7350c07d94d13c15901543a38b29e6c9b0a1
sash <sash@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-05-10 22:40:08 +0000

    pppoesrv: better handling
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@19234 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/pppoeserver.c
+++ src/router/services/services/pppoeserver.c
@@ -100,21 +100,23 @@ static void makeipup(void)
 		"echo \"`date +%%s`\t$PEERNAME\" >> /tmp/pppoe_uptime\n"	//
 		//->use something like $(( ($(date +%s) - $(date -d "$dates" +%s)) / (60*60*24*31) )) for computing uptime in the gui
 		"iptables -I INPUT -i $1 -j ACCEPT\n"	//
-		"iptables -I FORWARD -i $1 -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu\n"	//
+		"iptables -I FORWARD -i $1 -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu\n"
 		"iptables -I FORWARD -i $1 -j ACCEPT\n"	//
 		"echo 1 > /proc/sys/net/ipv4/conf/br0/proxy_arp\n"		
 		"echo 1 > /proc/sys/net/ipv4/conf/$1/proxy_arp\n"
 		"startservice set_routes\n");	// reinitialize 
 		//	per peer shaping
 	if (nvram_match("pppoeradius_enabled", "1")) {
-		fprintf(fp, "IN=`grep -i RP-Upstream-Speed-Limit /var/run/radattr.$1 | awk '{print $2}'`\n"	//
-			"OUT=`grep -i RP-Downstream-Speed-Limit /var/run/radattr.$1 | awk '{print $2}'`\n"	//
-			"if [ ! -z $IN ] && [ ! -z $OUT ] && [ $IN -gt 0 ] && [ $OUT -gt 0 ]\n"	//only if Speed limit !0 and !empty
-			"then	tc qdisc del root dev $1\n"	//
-			"\t tc qdisc del dev $1 ingress\n"	//
-			"\t tc qdisc add dev $1 root tbf rate \"$OUT\"kbit latency 50ms burst \"$OUT\"kbit\n"	//
+		fprintf(fp, "IN=`grep -i RP-Upstream-Speed-Limit /var/run/radattr.$1 | awk '{print $2}'`\n"
+			"OUT=`grep -i RP-Downstream-Speed-Limit /var/run/radattr.$1 | awk '{print $2}'`\n"
+			"if [ ! -z $IN ] &&  [ $IN -gt 0 ]\n"
+			"then tc qdisc del dev $1 ingress\n"
 			"\t tc qdisc add dev $1 handle ffff: ingress\n"	//
 			"\t tc filter add dev $1 parent ffff: protocol ip prio 50 u32 match ip src 0.0.0.0/0 police rate \"$IN\"kbit burst \"$IN\"kbit drop flowid :1\n"
+			"fi\n"
+			"if [ ! -z $OUT ] && [ $OUT -gt 0 ]\n"	//only if Speed limit !0 and !empty
+			"then	tc qdisc del root dev $1\n"
+			"\t tc qdisc add dev $1 root tbf rate \"$OUT\"kbit latency 50ms burst \"$OUT\"kbit\n"
 			"fi\n");
 		}
 //tc qdisc add dev $1 root red min 150KB max 450KB limit 600KB burst 200 avpkt 1000 probability 0.02 bandwidth 100Mbit
