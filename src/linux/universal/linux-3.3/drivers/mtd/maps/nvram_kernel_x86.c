538117a110271d450c937d9799e7cc71d743e5a3
BrainSlayer <s.gottschall@dd-wrt.com>
2012-05-14 11:00:47 +0000

    on some platforms kmalloc isnt a good idea
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@19252 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.3/drivers/mtd/maps/nvram_kernel_x86.c
+++ src/linux/universal/linux-3.3/drivers/mtd/maps/nvram_kernel_x86.c
@@ -179,7 +179,7 @@ _nvram_realloc(struct nvram_tuple *t, const char *name, const char *value)
 		return NULL;
 
 	if (!t) {
-		if (!(t = kmalloc(sizeof(struct nvram_tuple) + strlen(name) + 1, GFP_ATOMIC)))
+		if (!(t = vmalloc(sizeof(struct nvram_tuple) + strlen(name) + 1)))
 			return NULL;
 
 		/* Copy name */
@@ -205,7 +205,7 @@ _nvram_free(struct nvram_tuple *t)
 	if (!t)
 		nvram_offset = 0;
 	else
-		kfree(t);
+		vfree(t);
 }
 
 int
@@ -218,10 +218,10 @@ nvram_set(const char *name, const char *value)
 	spin_lock_irqsave(&nvram_lock, flags);
 	if ((ret = _nvram_set(name, value))) {
 		/* Consolidate space and try again */
-		if ((header = kmalloc(NVRAM_SPACE, GFP_ATOMIC))) {
+		if ((header = vmalloc(NVRAM_SPACE))) {
 			if (_nvram_commit(header) == 0)
 				ret = _nvram_set(name, value);
-			kfree(header);
+			vfree(header);
 		}
 	}
 	spin_unlock_irqrestore(&nvram_lock, flags);
@@ -367,7 +367,7 @@ dev_nvram_read(struct file *file, char *buf, size_t count, loff_t *ppos)
 	unsigned long off;
 
 	if (count > sizeof(tmp)) {
-		if (!(name = kmalloc(count, GFP_KERNEL)))
+		if (!(name = vmalloc(count)))
 			return -ENOMEM;
 	}
 
@@ -407,7 +407,7 @@ dev_nvram_read(struct file *file, char *buf, size_t count, loff_t *ppos)
  
 done:
 	if (name != tmp)
-		kfree(name);
+		vfree(name);
 
 	return ret;
 }
@@ -419,7 +419,7 @@ dev_nvram_write(struct file *file, const char *buf, size_t count, loff_t *ppos)
 	ssize_t ret;
 
 	if (count > sizeof(tmp)) {
-		if (!(name = kmalloc(count, GFP_KERNEL)))
+		if (!(name = vmalloc(count)))
 			return -ENOMEM;
 	}
 
@@ -437,7 +437,7 @@ dev_nvram_write(struct file *file, const char *buf, size_t count, loff_t *ppos)
 
  done:
 	if (name != tmp)
-		kfree(name);
+		vfree(name);
 
 	return ret;
 }	
