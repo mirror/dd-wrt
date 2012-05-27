f276adfc2dc17aa87696319e07fca5876ce9d1e1
BrainSlayer <s.gottschall@dd-wrt.com>
2012-05-27 21:42:45 +0000

    update configs.mk
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@19304 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/configs.mk
+++ src/router/rules/configs.mk
@@ -243,6 +243,9 @@ configs-checkout:
 configs-update:
 #	svn commit -m "faster hand optimized mksquashfs-lzma tool" $(LINUXDIR)
 	svn update $(LINUXDIR)
+	svn update $(LINUXDIR)/../linux-3.3
+	svn update $(LINUXDIR)/../linux-3.4
+	svn update $(LINUXDIR)/../linux-3.2
 	svn update $(TOP)/private
 	$(TOP)/private/symlinks.sh $(TOP)
 
