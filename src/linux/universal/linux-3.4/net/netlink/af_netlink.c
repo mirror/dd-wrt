5dcf3fb1925d03603561829d9cf8bd118742db15
BrainSlayer <s.gottschall@dd-wrt.com>
2012-05-27 15:49:53 +0000

    remove unwanted reallocs
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@19299 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.4/net/netlink/af_netlink.c
+++ src/linux/universal/linux-3.4/net/netlink/af_netlink.c
@@ -854,25 +854,7 @@ void netlink_detachskb(struct sock *sk, struct sk_buff *skb)
 
 static struct sk_buff *netlink_trim(struct sk_buff *skb, gfp_t allocation)
 {
-	int delta;
-
 	skb_orphan(skb);
-
-	delta = skb->end - skb->tail;
-	if (delta * 2 < skb->truesize)
-		return skb;
-
-	if (skb_shared(skb)) {
-		struct sk_buff *nskb = skb_clone(skb, allocation);
-		if (!nskb)
-			return skb;
-		kfree_skb(skb);
-		skb = nskb;
-	}
-
-	if (!pskb_expand_head(skb, 0, -delta, allocation))
-		skb->truesize -= delta;
-
 	return skb;
 }
 
