39e0ba557b358f96bd61127b3f99400c460aa143
BrainSlayer <s.gottschall@dd-wrt.com>
2012-07-06 21:50:40 +0000

    update mtr
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@19446 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/mtr/mtr.c
+++ src/router/mtr/mtr.c
@@ -3,9 +3,8 @@
     Copyright (C) 1997,1998  Matt Kimball
 
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+    it under the terms of the GNU General Public License version 2 as 
+    published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -61,6 +60,7 @@ char *Hostname = NULL;
 char *InterfaceAddress = NULL;
 char  LocalHostname[128];
 int   dns = 1;
+int   enablempls = 0;
 int   cpacketsize = 64;          /* default packet size */
 int   bitpattern = 0;
 int   tos = 0;
@@ -142,6 +142,7 @@ void parse_arg (int argc, char **argv)
 				   overload psize<0, ->rand(min,max) */
     { "bitpattern", 1, 0, 'b' },/* overload b>255, ->rand(0,255) */
     { "tos", 1, 0, 'Q' },	/* typeof service (0,255) */
+    { "mpls", 0, 0, 'e' },
     { "no-dns", 0, 0, 'n' },
     { "address", 1, 0, 'a' },
     { "first-ttl", 1, 0, 'f' },	/* -f & -m are borrowed from traceroute */
@@ -156,7 +157,7 @@ void parse_arg (int argc, char **argv)
   while(1) {
     /* added f:m:o: byMin */
     opt = getopt_long(argc, argv,
-		      "vhrwxtglpo:i:c:s:b:Q:na:f:m:u46", long_options, NULL);
+		      "vhrwxtglpo:i:c:s:b:Q:ena:f:m:u46", long_options, NULL);
     if(opt == -1)
       break;
 
@@ -199,6 +200,9 @@ void parse_arg (int argc, char **argv)
       break;
     case 'a':
       InterfaceAddress = optarg;
+      break;
+    case 'e':
+      enablempls = 1;
       break;
     case 'n':
       dns = 0;
@@ -378,8 +382,9 @@ int main(int argc, char **argv)
   if (PrintHelp) {
     printf("usage: %s [-hvrwctglspniu46] [--help] [--version] [--report]\n"
 	   "\t\t[--report-wide] [--report-cycles=COUNT] [--curses] [--gtk]\n"
-           "\t\t[--raw] [--split] [--no-dns] [--address interface]\n" /* BL */
+           "\t\t[--raw] [--split] [--mpls] [--no-dns] [--address interface]\n" /* BL */
            "\t\t[--psize=bytes/-s bytes]\n"            /* ok */
+           "\t\t[--report-wide|-w] [-u]\n"            /* rew */
 	   "\t\t[--interval=SECONDS] HOSTNAME [PACKETSIZE]\n", argv[0]);
     exit(0);
   }
@@ -402,7 +407,10 @@ int main(int argc, char **argv)
   hints.ai_socktype = SOCK_DGRAM;
   error = getaddrinfo( Hostname, NULL, &hints, &res );
   if ( error ) {
-    perror( gai_strerror(error) );
+    if (error == EAI_SYSTEM)
+       perror ("Failed to resolve host");
+    else
+       fprintf (stderr, "Failed to resolve host: %s\n", gai_strerror(error));
     exit( EXIT_FAILURE );
   }
   /* Convert the first addrinfo into a hostent. */
