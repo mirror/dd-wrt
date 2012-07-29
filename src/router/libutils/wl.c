cef9789b59bcf7aba7212165e870dc397ab9f111
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-07-29 22:38:34 +0000

    enabled flag
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@19572 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/wl.c
+++ src/router/libutils/wl.c
@@ -586,7 +586,6 @@ static int getcenterchannel(chanspec_t chspec)
 #else
 
 #ifndef CHSPEC_IS40
-
 #define WL_CHANSPEC_CTL_SB_MASK		0x0300
 #define WL_CHANSPEC_CTL_SB_LOWER	0x0100
 #define WL_CHANSPEC_CTL_SB_UPPER	0x0200
@@ -716,7 +715,9 @@ int getchannels(unsigned int *retlist, char *ifname)
 		c = list->element[i];
 		int cspec = c & 0x700;
 		int cbw = c & 0x3800;
+//		fprintf(stderr,"wl%d: %X spec %d, cbw %d\n",wl,c,cbw,cspec);
 		if ((cbw == mask) && (cspec == spec)) {
+//		fprintf(stderr,"take wl%d: %X spec %d, cbw %d\n",wl,c,cbw,cspec);
 
 			int channel = getcenterchannel(c);
 
