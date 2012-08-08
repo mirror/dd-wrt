229fa1e45d54a3a58010fb4d6aaf911852555e5a
BrainSlayer <s.gottschall@dd-wrt.com>
2012-08-08 15:20:52 +0000

    wdr3400 support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@19638 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/resetbutton.c
+++ src/router/rc/resetbutton.c
@@ -140,6 +140,15 @@ int getbuttonstate()
 {
 	return 0;
 }
+#elif defined(HAVE_WDR4300)
+int getbuttonstate()
+{
+//	int ret = get_gpio(17);
+//
+//	if (ret == 0)
+//		return 1;
+	return 0;
+}
 #elif defined(HAVE_DIR825C1)
 int getbuttonstate()
 {
@@ -776,6 +785,9 @@ void period_check(int sig)
 	sesgpio = 0x00b;
 	val |= get_gpio(11) << 11;	//aoss pushbutton
 #elif defined(HAVE_RB2011)
+//	sesgpio = 0x110;
+//	val |= get_gpio(16) << 16;	//aoss pushbutton
+#elif defined(HAVE_WDR4300)
 //	sesgpio = 0x110;
 //	val |= get_gpio(16) << 16;	//aoss pushbutton
 #elif defined(HAVE_DIR825C1)
