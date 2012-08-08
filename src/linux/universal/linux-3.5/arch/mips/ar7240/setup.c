cf7c5a81c7e62272e1e8c1022ad567ba1f7f9e15
BrainSlayer <s.gottschall@dd-wrt.com>
2012-08-08 22:01:15 +0000

    more revisions
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@19653 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.5/arch/mips/ar7240/setup.c
+++ src/linux/universal/linux-3.5/arch/mips/ar7240/setup.c
@@ -184,13 +184,25 @@ switch (id) {
 		chip = "9344";
 		rev = 1;
 		break;
+	case AR9344_REV_1_2:
+		chip = "9344";
+		rev = 2;
+		break;
 	case AR9342_REV_1_1:
 		chip = "9342";
 		rev = 1;
 		break;
+	case AR9342_REV_1_2:
+		chip = "9342";
+		rev = 2;
+		break;
 	case AR9341_REV_1_1:
 		chip = "9341";
 		rev = 1;
+		break;
+	case AR9341_REV_1_2:
+		chip = "9341";
+		rev = 2;
 		break;
 	default:
 		chip = "724x";
