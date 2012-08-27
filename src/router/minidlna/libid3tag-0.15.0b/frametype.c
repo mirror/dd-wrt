dac4f5404ab7ffd2a847d30e1de30448dd733266
BrainSlayer <s.gottschall@dd-wrt.com>
2012-08-27 13:08:29 +0000

    fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@19731 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/minidlna/libid3tag-0.15.0b/frametype.c
+++ src/router/minidlna/libid3tag-0.15.0b/frametype.c
@@ -1,4 +1,4 @@
-/* C code produced by gperf version 3.0.3 */
+/* C code produced by gperf version 3.0.4 */
 /* Command-line: gperf -tCcTonD -K id -N id3_frametype_lookup -s -3 -k '*' frametype.gperf  */
 
 #if !((' ' == 32) && ('!' == 33) && ('"' == 34) && ('#' == 35) \
@@ -353,7 +353,7 @@ hash (str, len)
 
 #ifdef __GNUC__
 __inline
-#ifdef __GNUC_STDC_INLINE__
+#if defined __GNUC_STDC_INLINE__ || defined __GNUC_GNU_INLINE__
 __attribute__ ((__gnu_inline__))
 #endif
 #endif
