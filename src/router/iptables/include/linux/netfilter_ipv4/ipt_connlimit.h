451938ee82da34bfd54dc26a6fffa1aaea7cb8aa
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-08-27 11:05:06 +0000

    for compatiblity with different versions
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@19725 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/iptables/include/linux/netfilter_ipv4/ipt_connlimit.h
+++ src/router/iptables/include/linux/netfilter_ipv4/ipt_connlimit.h
@@ -6,7 +6,7 @@ struct ipt_connlimit_data;
 struct ipt_connlimit_info {
 	int limit;
 	int inverse;
-	u_int32_t mask;
+	u_int32_t v4_mask;
 	struct ipt_connlimit_data *data;
 };
 #endif /* _IPT_CONNLIMIT_H */
