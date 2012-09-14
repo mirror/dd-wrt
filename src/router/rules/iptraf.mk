6547cfcc27dd2cfe1a388d8713914d57fe90981b
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-09-14 16:33:53 +0000

    Update iptraf
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@19944 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/iptraf.mk
+++ src/router/rules/iptraf.mk
@@ -1,9 +1,12 @@
+iptraf-configure:
+	cd iptraf && ./configure --prefix=/usr --host=$(ARCH)-linux CFLAGS="$(COPTS) -I$(TOP)/ncurses/include -L$(TOP)/ncurses/lib"
+
 iptraf:
-	make -C iptraf/src
+	make -C iptraf
 
 iptraf-clean:
-	make -C iptraf/src clean
+	make -C iptraf clean
 
 iptraf-install:
-	make -C iptraf/src install
+	make -C iptraf install DESTDIR=$(INSTALLDIR)/iptraf
 
