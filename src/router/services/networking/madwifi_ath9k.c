54feff3be1115c4fdd5481e3a30ede843fca1b42
BrainSlayer <s.gottschall@dd-wrt.com>
2012-09-20 14:12:43 +0000

    disassoc station in case of excessive retries
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@19992 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/madwifi_ath9k.c
+++ src/router/services/networking/madwifi_ath9k.c
@@ -549,7 +549,7 @@ void setupHostAP_ath9k(char *maininterface, int isfirst, int vapid, int aoss)
 		fprintf(stderr, "setup vap %d bss %s\n", vapid, ifname);
 		fprintf(fp, "bss=%s\n", ifname);
 	}
-
+	fprintf(fp,"disassoc_low_ack=1\n");
 	char *mode = nvram_nget("%s_mode", ifname);
 	if (!strcmp(mode, "wdsap"))
 		fprintf(fp, "wds_sta=1\n");
