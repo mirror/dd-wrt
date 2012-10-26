af9da90776b5307847b081c67e46ffc525e466bd
BrainSlayer <s.gottschall@dd-wrt.com>
2012-10-26 17:09:54 +0000

    prepare for hostapd update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@20181 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/hostapd-wps.mk
+++ src/router/rules/hostapd-wps.mk
@@ -5,31 +5,33 @@ ATH9K_CFLAGS=-I$(TOP)/libnl-tiny/include\
 ATH9K_LDFLAGS=-L$(TOP)/libnl-tiny/ -lm -lnl-tiny
 endif
 
+HOSTAPDVERSION=wps
+
 hostapd2: libnltiny
-	$(MAKE) -C hostapd-wps/hostapd clean
-	$(MAKE) -C hostapd-wps/wpa_supplicant clean
+	$(MAKE) -C hostapd-$(HOSTAPDVERSION)/hostapd clean
+	$(MAKE) -C hostapd-$(HOSTAPDVERSION)/wpa_supplicant clean
 	echo ` \
-		$(MAKE) -s -C hostapd-wps/hostapd MULTICALL=1 dump_cflags; \
-		$(MAKE) -s -C hostapd-wps/wpa_supplicant MULTICALL=1 dump_cflags \
-	` > hostapd-wps/.cflags
+		$(MAKE) -s -C hostapd-$(HOSTAPDVERSION)/hostapd MULTICALL=1 dump_cflags; \
+		$(MAKE) -s -C hostapd-$(HOSTAPDVERSION)/wpa_supplicant MULTICALL=1 dump_cflags \
+	` > hostapd-$(HOSTAPDVERSION)/.cflags
 
 	
 
-	$(MAKE) -C hostapd-wps/hostapd CFLAGS="$$(cat hostapd-wps/.cflags) $(ATH9K_CFLAGS)" CONFIG_ATH9K=$(CONFIG_ATH9K) MULTICALL=1 hostapd_cli hostapd_multi.a
-	$(MAKE) -C hostapd-wps/wpa_supplicant CFLAGS="$$(cat hostapd-wps/.cflags) $(ATH9K_CFLAGS)" CONFIG_ATH9K=$(CONFIG_ATH9K) MULTICALL=1 wpa_cli wpa_supplicant_multi.a
-	$(CC) $(COPTS) -L$(TOP)/nvram  -L$(TOP)/libutils -Wall -ffunction-sections -fdata-sections -Wl,--gc-sections -o hostapd-wps/wpad hostapd-wps/multicall/multicall.c \
-		hostapd-wps/hostapd/hostapd_multi.a \
-		hostapd-wps/wpa_supplicant/wpa_supplicant_multi.a \
+	$(MAKE) -C hostapd-$(HOSTAPDVERSION)/hostapd CFLAGS="$$(cat hostapd-$(HOSTAPDVERSION)/.cflags) $(ATH9K_CFLAGS)" CONFIG_ATH9K=$(CONFIG_ATH9K) MULTICALL=1 hostapd_cli hostapd_multi.a
+	$(MAKE) -C hostapd-$(HOSTAPDVERSION)/wpa_supplicant CFLAGS="$$(cat hostapd-$(HOSTAPDVERSION)/.cflags) $(ATH9K_CFLAGS)" CONFIG_ATH9K=$(CONFIG_ATH9K) MULTICALL=1 wpa_cli wpa_supplicant_multi.a
+	$(CC) $(COPTS) -L$(TOP)/nvram  -L$(TOP)/libutils -Wall -ffunction-sections -fdata-sections -Wl,--gc-sections -o hostapd-$(HOSTAPDVERSION)/wpad hostapd-$(HOSTAPDVERSION)/multicall/multicall.c \
+		hostapd-$(HOSTAPDVERSION)/hostapd/hostapd_multi.a \
+		hostapd-$(HOSTAPDVERSION)/wpa_supplicant/wpa_supplicant_multi.a \
 		$(ATH9K_LDFLAGS) -lutils -lnvram
 
 hostapd2-clean:
-	$(MAKE) -C hostapd-wps/hostapd clean
-	$(MAKE) -C hostapd-wps/wpa_supplicant clean
+	$(MAKE) -C hostapd-$(HOSTAPDVERSION)/hostapd clean
+	$(MAKE) -C hostapd-$(HOSTAPDVERSION)/wpa_supplicant clean
 
 hostapd2-install:
-	install -D hostapd-wps/wpad $(INSTALLDIR)/hostapd2/usr/sbin/wpad
+	install -D hostapd-$(HOSTAPDVERSION)/wpad $(INSTALLDIR)/hostapd2/usr/sbin/wpad
 ifeq ($(CONFIG_WPS),y)
-	install -D hostapd-wps/hostapd/hostapd_cli $(INSTALLDIR)/hostapd2/usr/sbin/hostapd_cli
+	install -D hostapd-$(HOSTAPDVERSION)/hostapd/hostapd_cli $(INSTALLDIR)/hostapd2/usr/sbin/hostapd_cli
 endif
 	cd $(INSTALLDIR)/hostapd2/usr/sbin && ln -sf wpad hostapd
 	cd $(INSTALLDIR)/hostapd2/usr/sbin && ln -sf wpad wpa_supplicant
