c4a692c04a28cfb4d4add2e738ff3e5c8e24b46a
chris <chris@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-10-28 17:31:17 +0000

    nrpe 2.13
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@20183 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/nrpe/src/check_nrpe.c
+++ src/router/nrpe/src/check_nrpe.c
@@ -4,7 +4,7 @@
  * Copyright (c) 1999-2008 Ethan Galstad (nagios@nagios.org)
  * License: GPL
  *
- * Last Modified: 03-10-2008
+ * Last Modified: 11-11-2011
  *
  * Command line: CHECK_NRPE -H <host_address> [-p port] [-c command] [-to to_sec]
  *
