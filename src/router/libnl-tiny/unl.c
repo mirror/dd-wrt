f5d7b65237444cd1723d3790d46e16897632c619
ChristianScheele <scheele@newmedia-net.de>
2012-11-05 10:10:51 +0000

    libnl-tiny update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@20221 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libnl-tiny/unl.c
+++ src/router/libnl-tiny/unl.c
@@ -173,7 +173,7 @@ void unl_genl_loop(struct unl *unl, unl_cb handler, void *arg)
 	nl_cb_put(cb);
 }
 
-static int unl_genl_multicast_id(struct unl *unl, const char *name)
+int unl_genl_multicast_id(struct unl *unl, const char *name)
 {
 	struct nlattr *tb[CTRL_ATTR_MCAST_GRP_MAX + 1];
 	struct nlattr *groups, *group;
@@ -191,11 +191,11 @@ static int unl_genl_multicast_id(struct unl *unl, const char *name)
 	NLA_PUT_STRING(msg, CTRL_ATTR_FAMILY_NAME, unl->family_name);
 	unl_genl_request_single(unl, msg, &msg);
 	if (!msg)
-		goto nla_put_failure;
+		return -1;
 
 	groups = unl_find_attr(unl, msg, CTRL_ATTR_MCAST_GROUPS);
 	if (!groups)
-		goto fail;
+		goto nla_put_failure;
 
 	nla_for_each_nested(group, groups, rem) {
 		const char *gn;
@@ -215,9 +215,8 @@ static int unl_genl_multicast_id(struct unl *unl, const char *name)
 		break;
 	}
 
-fail:
-	nlmsg_free(msg);
 nla_put_failure:
+	nlmsg_free(msg);
 	return ret;
 }
 
