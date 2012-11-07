cdbffc059c8a1263a9fa4333ef7554782bc17126
BrainSlayer <s.gottschall@dd-wrt.com>
2012-11-07 17:19:58 +0000

    fix reboot race condition
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@20239 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.7/arch/arm/mach-cns3xxx/pm.c
+++ src/linux/universal/linux-3.7/arch/arm/mach-cns3xxx/pm.c
@@ -120,7 +120,7 @@ void cns3xxx_restart(char mode, const char *cmd)
 	 * To reset, we hit the on-board reset register
 	 * in the system FPGA.
 	 */
-	cns3xxx_pwr_soft_rst(CNS3XXX_PWR_SOFTWARE_RST(GLOBAL));
+	cns3xxx_pwr_soft_rst_force(CNS3XXX_PWR_SOFTWARE_RST(GLOBAL));
 }
 
 /*
