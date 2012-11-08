a11eb21dec2e01328b9e35c61c8f78e5d753350b
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-11-08 02:39:43 +0000

    add latest code
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@20254 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/iptables/extensions/libipt_opendpi.c
+++ src/router/iptables/extensions/libipt_opendpi.c
@@ -1,8 +1,9 @@
 /* 
- * libxt_opendpi.c
- * Copyright (C) 2010 Gerardo E. Gidoni <gerel@gnu.org>
+ * libxt_ndpi.c
+ * Copyright (C) 2010-2012 G. Elian Gidoni <geg@gnu.org>
+ *               2012 Ed Wildgoose <lists@wildgooses.com>
  * 
- * This file is part of OpenDPI, an open source deep packet inspection
+ * This file is part of nDPI, an open source deep packet inspection
  * library based on the PACE technology by ipoque GmbH
  * 
  * This program is free software; you can redistribute it and/or
@@ -24,54 +25,64 @@
 #include <getopt.h>
 #include <arpa/inet.h>
 #include <iptables.h>
-#include "xt_opendpi.h"
+#include "ndpi_define.h"
+#include "ndpi_macros.h"
+#include "ndpi_protocols_osdpi.h"
+#include "ndpi_api.h"
+#include "ndpi_protocol_history.h"
+#include "ndpi_structs.h"
+
+//#include <linux/version.h>
+
+#include "xt_ndpi.h"
+
 
 #define true 1
 #define false 0
 
-static char *prot_long_str[] = { IPOQUE_PROTOCOL_LONG_STRING };
-static char *prot_short_str[] = { IPOQUE_PROTOCOL_SHORT_STRING };
+static char *prot_long_str[] = { NDPI_PROTOCOL_LONG_STRING };
+static char *prot_short_str[] = { NDPI_PROTOCOL_SHORT_STRING };
 
 static void 
-opendpi_mt4_save(const struct ipt_ip *ip, const struct xt_entry_match *match)
+ndpi_mt4_save(const struct ipt_ip *entry, const struct ipt_entry_match *match)
 {
-	const struct xt_opendpi_mtinfo *info = (const void *)match->data;
+	const struct xt_ndpi_mtinfo *info = (const void *)match->data;
         int i;
 
-        for (i = 1; i <= IPOQUE_MAX_SUPPORTED_PROTOCOLS; i++){
-                if (IPOQUE_COMPARE_PROTOCOL_TO_BITMASK(info->flags, i) != 0){
+        for (i = 1; i <= NDPI_LAST_IMPLEMENTED_PROTOCOL; i++){
+                if (NDPI_COMPARE_PROTOCOL_TO_BITMASK(info->flags, i) != 0){
                         printf("--%s ", prot_short_str[i]);
                 }
         }
 }
 
 
-static void opendpi_mt4_print(const struct ipt_ip *ip, const struct ipt_entry_match *match, int numeric)
+static void 
+ndpi_mt4_print(const struct ipt_ip *entry, const struct ipt_entry_match *match,
+                  int numeric)
 {
-	const struct xt_opendpi_mtinfo *info = (const void *)match->data;
+	const struct xt_ndpi_mtinfo *info = (const void *)match->data;
 	int i;
 
-        for (i = 1; i <= IPOQUE_MAX_SUPPORTED_PROTOCOLS; i++){
-                if (IPOQUE_COMPARE_PROTOCOL_TO_BITMASK(info->flags, i) != 0){
+        for (i = 1; i <= NDPI_LAST_IMPLEMENTED_PROTOCOL; i++){
+                if (NDPI_COMPARE_PROTOCOL_TO_BITMASK(info->flags, i) != 0){
                         printf("protocol %s ", prot_long_str[i]);
                 }
         }
 }
 
 
-static int
-opendpi_mt4_parse(int c, char **argv, int invert, unsigned int *flags,
-      const struct ipt_entry *entry,
-      unsigned int *nfcache,
-      struct ipt_entry_match **match)
+static int 
+ndpi_mt4_parse(int c, char **argv, int invert, unsigned int *flags,
+                  const struct ipt_entry *entry,unsigned int *nfcache, struct ipt_entry_match **match)
 {
-	struct xt_opendpi_mtinfo *info = (void *)(*match)->data;
+	struct xt_ndpi_mtinfo *info = (void *)(*match)->data;
         int i;
 
         *flags = 0;
-        for (i = 1; i <= IPOQUE_MAX_SUPPORTED_PROTOCOLS; i++){
+        for (i = 1; i <= NDPI_LAST_IMPLEMENTED_PROTOCOL; i++){
                 if (c == i){
-                        IPOQUE_ADD_PROTOCOL_TO_BITMASK(info->flags, i);
+                        NDPI_ADD_PROTOCOL_TO_BITMASK(info->flags, i);
                         /*printf("Parameter detected as protocol %s.\n",
                           prot_long_str[i]);*/
                         *flags = 1;
@@ -87,22 +98,22 @@ opendpi_mt4_parse(int c, char **argv, int invert, unsigned int *flags,
 #endif
 
 static void
-opendpi_mt_check (unsigned int flags)
+ndpi_mt_check (unsigned int flags)
 {
 	if (flags == 0){
-		xtables_error(PARAMETER_PROBLEM, "xt_opendpi: You need to "
+		xtables_error(PARAMETER_PROBLEM, "xt_ndpi: You need to "
                               "specify at least one protocol");
 	}
 }
 
 
 static void
-opendpi_mt_help(void)
+ndpi_mt_help(void)
 {
         int i;
 
-	printf("opendpi match options:\n");
-        for (i = 1; i <= IPOQUE_MAX_SUPPORTED_PROTOCOLS; i++){
+	printf("ndpi match options:\n");
+        for (i = 1; i <= NDPI_LAST_IMPLEMENTED_PROTOCOL; i++){
                 printf("--%s Match for %s protocol packets.\n",
                        prot_short_str[i], prot_long_str[i]);
         }
@@ -110,45 +121,49 @@ opendpi_mt_help(void)
 
 
 static void 
-opendpi_mt_init (struct ipt_entry_match *match)
+ndpi_mt_init(struct ipt_entry_match *m, unsigned int *nfcache)
 {
-	struct xt_opendpi_mtinfo *info = (void *)match->data;
+	struct xt_ndpi_mtinfo *info = (void *)m->data;
 	/* inet_pton(PF_INET, "192.0.2.137", &info->dst.in); */
 }
 
 
-static struct option opendpi_mt_opts[IPOQUE_MAX_SUPPORTED_PROTOCOLS+1];
+static struct option ndpi_mt_opts[NDPI_LAST_IMPLEMENTED_PROTOCOL+1];
 
 static struct iptables_match
-opendpi_mt4_reg = {
+ndpi_mt4_reg = {
 	.version = IPTABLES_VERSION,
-	.name = "opendpi",
+	.name = "ndpi",
 	.revision = 0,
+//#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,28)
 //	.family = AF_INET,
-	.size = IPT_ALIGN(sizeof(struct xt_opendpi_mtinfo)),
-	.userspacesize = IPT_ALIGN(sizeof(struct xt_opendpi_mtinfo)),
-	//.help = opendpi_mt_help,
-	.init = opendpi_mt_init,
-	.parse = opendpi_mt4_parse,
-	.final_check = opendpi_mt_check,
-	.print = opendpi_mt4_print,
-	.save = opendpi_mt4_save,
-	.extra_opts = opendpi_mt_opts,
+//#else
+//	.family = NFPROTO_IPV4,
+//#endif
+	.size = IPT_ALIGN(sizeof(struct xt_ndpi_mtinfo)),
+	.userspacesize = IPT_ALIGN(sizeof(struct xt_ndpi_mtinfo)),
+//	.help = ndpi_mt_help,
+	.init = ndpi_mt_init,
+	.parse = ndpi_mt4_parse,
+	.final_check = ndpi_mt_check,
+	.print = ndpi_mt4_print,
+	.save = ndpi_mt4_save,
+	.extra_opts = ndpi_mt_opts,
 };
 
 void _init(void)
 {
         int i;
 
-        for (i = 0; i < IPOQUE_MAX_SUPPORTED_PROTOCOLS; i++){
-                opendpi_mt_opts[i].name = prot_short_str[i+1];
-                opendpi_mt_opts[i].has_arg = false;
-                opendpi_mt_opts[i].val = i+1;
+        for (i = 0; i < NDPI_LAST_IMPLEMENTED_PROTOCOL; i++){
+                ndpi_mt_opts[i].name = prot_short_str[i+1];
+                ndpi_mt_opts[i].has_arg = false;
+                ndpi_mt_opts[i].val = i+1;
         }
-        opendpi_mt_opts[i].name = NULL;
-        opendpi_mt_opts[i].flag = NULL;
-        opendpi_mt_opts[i].has_arg = 0;
-        opendpi_mt_opts[i].val = 0;
+        ndpi_mt_opts[i].name = NULL;
+        ndpi_mt_opts[i].flag = NULL;
+        ndpi_mt_opts[i].has_arg = 0;
+        ndpi_mt_opts[i].val = 0;
 
-	register_match(&opendpi_mt4_reg);
+	register_match(&ndpi_mt4_reg);
 }
