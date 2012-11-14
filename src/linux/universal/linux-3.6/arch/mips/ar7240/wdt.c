d5bd9cb5e696f53379491a3fb4344f1fea010bcb
BrainSlayer <s.gottschall@dd-wrt.com>
2012-11-14 13:03:02 +0000

    original LSDK code, does not belong to OpenWrt
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@20300 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.6/arch/mips/ar7240/wdt.c
+++ src/linux/universal/linux-3.6/arch/mips/ar7240/wdt.c
@@ -2,7 +2,6 @@
  * wdt.c
  *
  * Copyright (C) 2012 Sebastian Gottschall <gottschall@dd-wrt.com>
- * mainly based on Atheros LSDK Code, some code taken from OpenWrt and ATH79 tree
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
