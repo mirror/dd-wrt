25b4c9a72c4e17c979ec6d343f121db48e885fa1
BrainSlayer <s.gottschall@dd-wrt.com>
2012-11-21 00:12:34 +0000

    update radvd
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@20320 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/radvd.mk
+++ src/router/rules/radvd.mk
@@ -1,14 +1,13 @@
 radvd-configure:
-	if test ! -e "radvd/Makefile"; then \
-	    cd radvd/flex && ./configure --disable-nls --prefix=/usr --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS)"; \
-	    cd .. && ./configure --host=$(ARCH)-linux CFLAGS="$(COPTS) -DNEED_PRINTF -I$(TOP)/radvd/flex" LDFLAGS="-L$(TOP)/radvd/flex"  --with-flex=$(TOP)/radvd/flex; \
-	fi
+	cd radvd/libdaemon && ./configure --disable-nls --disable-shared --enable-static --disable-lynx --prefix=/usr --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS)"  ac_cv_func_setpgrp_void=yes ; make
+	cd radvd/flex && ./configure --disable-nls --prefix=/usr --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS)"; \
+	cd .. && ./configure --host=$(ARCH)-linux CFLAGS="$(COPTS) -DNEED_PRINTF -I$(TOP)/radvd/flex" DAEMON_CFLAGS="-I$(TOP)/radvd/libdaemon" DAEMON_LIBS="-L$(TOP)/radvd/libdaemon/libdaemon/.libs  -ldaemon" LDFLAGS="-L$(TOP)/radvd/flex"  --with-flex=$(TOP)/radvd/flex; \
 	
 radvd-clean:
-	if test -e "radvd/Makefile"; then make -C make -C radvd/flex clean; make -C radvd clean; fi
+	if test -e "radvd/Makefile"; then make -C radvd/flex clean; make -C radvd clean; fi
 		
 
-radvd: radvd-configure
+radvd:
 	make -C radvd/flex libfl.a
 	make -C radvd
 
