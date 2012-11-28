4a4038afca417d2a4cfb2120853c3504eaaff6b0
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2012-11-28 13:52:20 +0000

    missing commits
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@20343 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rules/_cmake.mk
@@ -0,0 +1,34 @@
+define CMakeConfigure 
+	(cd $(strip $(1)); \
+		CFLAGS="$(CFLAGS) $(EXTRA_CFLAGS)" \
+		CXXFLAGS="$(CFLAGS) $(EXTRA_CFLAGS)" \
+		cmake \
+			-DCMAKE_SYSTEM_NAME=Linux \
+			-DCMAKE_SYSTEM_VERSION=1 \
+			-DCMAKE_SYSTEM_PROCESSOR=$(ARCH) \
+			-DCMAKE_BUILD_TYPE=Release \
+			-DCMAKE_C_FLAGS_RELEASE="-DNDEBUG" \
+			-DCMAKE_CXX_FLAGS_RELEASE="-DNDEBUG" \
+			-DCMAKE_C_COMPILER=$(CROSS_COMPILE)gcc \
+			-DCMAKE_CXX_COMPILER=$(CROSS_COMPILE)g++ \
+			-DCMAKE_EXE_LINKER_FLAGS="$(LDFLAGS)" \
+			-DCMAKE_MODULE_LINKER_FLAGS="$(LDFLAGS)" \
+			-DCMAKE_SHARED_LINKER_FLAGS="$(LDFLAGS)" \
+			-DCMAKE_FIND_ROOT_PATH=$(2) \
+			-DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=BOTH \
+			-DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY \
+			-DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY \
+			-DCMAKE_STRIP=: \
+			-DCMAKE_INSTALL_PREFIX=$(2) \
+			-DDL_LIBRARY=$(2) \
+			$(3) \
+		. \
+	)
+endef
+
+define CMakeClean
+	( rm -rf $(strip $(1))/CMakeCache.txt ;\
+	rm -rf $(strip $(1))/cmake_install.cmake ;\
+	rm -rf $(strip $(1))/CMakeFiles ;\
+	)	
+endef
