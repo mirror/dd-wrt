8f9dc992789f474e38bf2e9c5b3647eccecb8bba
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-01-14 13:35:04 +0000

    nm extra
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@20508 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/configs.mk
+++ src/router/rules/configs.mk
@@ -228,6 +228,7 @@ obj-$(CONFIG_MINIDLNA) += minidlna
 obj-$(CONFIG_NRPE) += nrpe
 obj-$(CONFIG_LINKS) += links
 obj-$(CONFIG_SOFTFLOWD) += softflowd
+obj-$(CONFIG_NEXTMEDIAEXTRA) += nextemdiaextra
 
 #obj-y+=anchorfree
 obj-y+=ttraff
