1a00aa65c8a7bea05e341094478335f9b1f95d68
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-01-26 09:09:38 +0000

    update glib
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@20560 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/glib20/libglib/glib/tests/mappedfile.c
@@ -0,0 +1,183 @@
+#define GLIB_DISABLE_DEPRECATION_WARNINGS
+
+#include <config.h>
+#include <glib.h>
+#include <string.h>
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+#include <glib/gstdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+static void
+test_basic (void)
+{
+  GMappedFile *file;
+  GError *error;
+
+  error = NULL;
+  file = g_mapped_file_new (SRCDIR "/empty", FALSE, &error);
+  g_assert_no_error (error);
+
+  g_mapped_file_ref (file);
+  g_mapped_file_unref (file);
+
+  g_mapped_file_unref (file);
+}
+
+static void
+test_empty (void)
+{
+  GMappedFile *file;
+  GError *error;
+
+  error = NULL;
+  file = g_mapped_file_new (SRCDIR "/empty", FALSE, &error);
+  g_assert_no_error (error);
+
+  g_assert (g_mapped_file_get_contents (file) == NULL);
+
+  g_mapped_file_free (file);
+}
+
+static void
+test_device (void)
+{
+  GError *error = NULL;
+  GMappedFile *file;
+
+  file = g_mapped_file_new ("/dev/null", FALSE, &error);
+  g_assert_error (error, G_FILE_ERROR, G_FILE_ERROR_INVAL);
+  g_assert (file == NULL);
+  g_error_free (error);
+}
+
+static void
+test_nonexisting (void)
+{
+  GMappedFile *file;
+  GError *error;
+
+  error = NULL;
+  file = g_mapped_file_new ("no-such-file", FALSE, &error);
+  g_assert_error (error, G_FILE_ERROR, G_FILE_ERROR_NOENT);
+  g_clear_error (&error);
+  g_assert (file == NULL);
+}
+
+static void
+test_writable (void)
+{
+  GMappedFile *file;
+  GError *error;
+  gchar *contents;
+  const gchar *old = "MMMMMMMMMMMMMMMMMMMMMMMMM";
+  const gchar *new = "abcdefghijklmnopqrstuvxyz";
+
+  if (access (SRCDIR "/4096-random-bytes", W_OK) != 0)
+    {
+      g_test_message ("Skipping writable mapping test");
+      return;
+    }
+
+  error = NULL;
+  file = g_mapped_file_new (SRCDIR "/4096-random-bytes", TRUE, &error);
+  g_assert_no_error (error);
+
+  contents = g_mapped_file_get_contents (file);
+  g_assert (strncmp (contents, old, strlen (old)) == 0);
+
+  memcpy (contents, new, strlen (new));
+  g_assert (strncmp (contents, new, strlen (new)) == 0);
+
+  g_mapped_file_free (file);
+
+  error = NULL;
+  file = g_mapped_file_new (SRCDIR "/4096-random-bytes", TRUE, &error);
+  g_assert_no_error (error);
+
+  contents = g_mapped_file_get_contents (file);
+  g_assert (strncmp (contents, old, strlen (old)) == 0);
+
+  g_mapped_file_free (file);
+}
+
+static void
+test_writable_fd (void)
+{
+  GMappedFile *file;
+  GError *error;
+  gchar *contents;
+  const gchar *old = "MMMMMMMMMMMMMMMMMMMMMMMMM";
+  const gchar *new = "abcdefghijklmnopqrstuvxyz";
+  int fd;
+
+  if (access (SRCDIR "/4096-random-bytes", W_OK) != 0)
+    {
+      g_test_message ("Skipping writable mapping test");
+      return;
+    }
+
+  error = NULL;
+  fd = g_open (SRCDIR "/4096-random-bytes", O_RDWR, 0);
+  g_assert (fd != -1);
+  file = g_mapped_file_new_from_fd (fd, TRUE, &error);
+  g_assert_no_error (error);
+
+  contents = g_mapped_file_get_contents (file);
+  g_assert (strncmp (contents, old, strlen (old)) == 0);
+
+  memcpy (contents, new, strlen (new));
+  g_assert (strncmp (contents, new, strlen (new)) == 0);
+
+  g_mapped_file_free (file);
+  close (fd);
+
+  error = NULL;
+  fd = g_open (SRCDIR "/4096-random-bytes", O_RDWR, 0);
+  g_assert (fd != -1);
+  file = g_mapped_file_new_from_fd (fd, TRUE, &error);
+  g_assert_no_error (error);
+
+  contents = g_mapped_file_get_contents (file);
+  g_assert (strncmp (contents, old, strlen (old)) == 0);
+
+  g_mapped_file_free (file);
+
+}
+
+static void
+test_gbytes (void)
+{
+  GMappedFile *file;
+  GBytes *bytes;
+  GError *error;
+
+  error = NULL;
+  file = g_mapped_file_new (SRCDIR "/empty", FALSE, &error);
+  g_assert_no_error (error);
+
+  bytes = g_mapped_file_get_bytes (file);
+  g_mapped_file_unref (file);
+
+  g_assert_cmpint (g_bytes_get_size (bytes), ==, 0);
+  g_bytes_unref (bytes);
+}
+
+int
+main (int argc, char *argv[])
+{
+  g_test_init (&argc, &argv, NULL);
+
+  g_test_add_func ("/mappedfile/basic", test_basic);
+  g_test_add_func ("/mappedfile/empty", test_empty);
+  g_test_add_func ("/mappedfile/device", test_device);
+  g_test_add_func ("/mappedfile/nonexisting", test_nonexisting);
+  g_test_add_func ("/mappedfile/writable", test_writable);
+  g_test_add_func ("/mappedfile/writable_fd", test_writable_fd);
+  g_test_add_func ("/mappedfile/gbytes", test_gbytes);
+
+  return g_test_run ();
+}
---
+++ src/router/glib20/libglib/glib/tests/unix.c
@@ -0,0 +1,164 @@
+/* 
+ * Copyright (C) 2011 Red Hat, Inc.
+ *
+ * This work is provided "as is"; redistribution and modification
+ * in whole or in part, in any medium, physical or electronic is
+ * permitted without restriction.
+ *
+ * This work is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * In no event shall the authors or contributors be liable for any
+ * direct, indirect, incidental, special, exemplary, or consequential
+ * damages (including, but not limited to, procurement of substitute
+ * goods or services; loss of use, data, or profits; or business
+ * interruption) however caused and on any theory of liability, whether
+ * in contract, strict liability, or tort (including negligence or
+ * otherwise) arising in any way out of the use of this software, even
+ * if advised of the possibility of such damage.
+ *
+ * Author: Colin Walters <walters@verbum.org> 
+ */
+
+#include "config.h"
+
+#include "glib-unix.h"
+#include <string.h>
+
+static void
+test_pipe (void)
+{
+  GError *error = NULL;
+  int pipefd[2];
+  char buf[1024];
+  ssize_t bytes_read;
+  gboolean res;
+
+  res = g_unix_open_pipe (pipefd, FD_CLOEXEC, &error);
+  g_assert (res);
+  g_assert_no_error (error);
+
+  write (pipefd[1], "hello", sizeof ("hello"));
+  memset (buf, 0, sizeof (buf));
+  bytes_read = read (pipefd[0], buf, sizeof(buf) - 1);
+  g_assert_cmpint (bytes_read, >, 0);
+  buf[bytes_read] = '\0';
+
+  close (pipefd[0]);
+  close (pipefd[1]);
+
+  g_assert (g_str_has_prefix (buf, "hello"));
+}
+
+static void
+test_error (void)
+{
+  GError *error = NULL;
+  gboolean res;
+
+  res = g_unix_set_fd_nonblocking (123456, TRUE, &error);
+  g_assert_cmpint (errno, ==, EBADF);
+  g_assert (!res);
+  g_assert_error (error, G_UNIX_ERROR, 0);
+  g_clear_error (&error);
+}
+
+static gboolean sig_received = FALSE;
+
+static gboolean
+on_sig_received (gpointer user_data)
+{
+  GMainLoop *loop = user_data;
+  g_main_loop_quit (loop);
+  sig_received = TRUE;
+  return G_SOURCE_REMOVE;
+}
+
+static gboolean
+sig_not_received (gpointer data)
+{
+  GMainLoop *loop = data;
+  (void) loop;
+  g_error ("Timed out waiting for signal");
+  return G_SOURCE_REMOVE;
+}
+
+static gboolean
+exit_mainloop (gpointer data)
+{
+  GMainLoop *loop = data;
+  g_main_loop_quit (loop);
+  return G_SOURCE_REMOVE;
+}
+
+static void
+test_signal (int signum)
+{
+  GMainLoop *mainloop;
+  int id;
+
+  mainloop = g_main_loop_new (NULL, FALSE);
+
+  sig_received = FALSE;
+  g_unix_signal_add (signum, on_sig_received, mainloop);
+  kill (getpid (), signum);
+  g_assert (!sig_received);
+  id = g_timeout_add (5000, sig_not_received, mainloop);
+  g_main_loop_run (mainloop);
+  g_assert (sig_received);
+  sig_received = FALSE;
+  g_source_remove (id);
+
+  /* Ensure we don't get double delivery */
+  g_timeout_add (500, exit_mainloop, mainloop);
+  g_main_loop_run (mainloop);
+  g_assert (!sig_received);
+  g_main_loop_unref (mainloop);
+
+}
+
+static void
+test_sighup (void)
+{
+  test_signal (SIGHUP);
+}
+
+static void
+test_sigterm (void)
+{
+  test_signal (SIGTERM);
+}
+
+static void
+test_sighup_add_remove (void)
+{
+  GMainLoop *mainloop;
+  guint id;
+
+  mainloop = g_main_loop_new (NULL, FALSE);
+
+  sig_received = FALSE;
+  id = g_unix_signal_add (SIGHUP, on_sig_received, mainloop);
+  g_source_remove (id);
+  kill (getpid (), SIGHUP);
+  g_assert (!sig_received);
+  g_main_loop_unref (mainloop);
+
+}
+
+int
+main (int   argc,
+      char *argv[])
+{
+  g_test_init (&argc, &argv, NULL);
+
+  g_test_add_func ("/glib-unix/pipe", test_pipe);
+  g_test_add_func ("/glib-unix/error", test_error);
+  g_test_add_func ("/glib-unix/sighup", test_sighup);
+  g_test_add_func ("/glib-unix/sigterm", test_sigterm);
+  g_test_add_func ("/glib-unix/sighup_again", test_sighup);
+  g_test_add_func ("/glib-unix/sighup_add_remove", test_sighup_add_remove);
+
+  return g_test_run();
+}
