025607ff9d3971809d842cd80e3e57aef587f272
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-02-21 22:55:00 +0000

    wndr3700v4
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@20751 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/utils.c
+++ src/router/libutils/utils.c
@@ -1684,6 +1684,13 @@ int internal_getRouterBrand()
 	nvram_default_get("ath1_rxantenna", "7");
 	nvram_default_get("ath1_txantenna", "7");
 	return ROUTER_BOARD_WHRHPGN;
+#elif HAVE_WNDR3700V4
+	setRouter("Netgear WNDR3700 V4");
+	nvram_default_get("ath0_rxantenna", "3");
+	nvram_default_get("ath0_txantenna", "3");
+	nvram_default_get("ath1_rxantenna", "3");
+	nvram_default_get("ath1_txantenna", "3");
+	return ROUTER_BOARD_WHRHPGN;
 #elif HAVE_DIR825C1
 	setRouter("Dlink DIR825-C1");
 	nvram_default_get("ath0_rxantenna", "3");
