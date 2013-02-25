07c3f55f1864767c5a8937a7f8c1664ead4cf867
sash <sash@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-02-25 21:33:00 +0000

    ovpn: change log ports here too
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@20788 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/openvpn/config/openvpnlog.sh
+++ src/router/openvpn/config/openvpnlog.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 /bin/echo -e "Serverlog \n"
-/bin/echo "log 500" | /usr/bin/nc 127.0.0.1 5002 | /bin/grep -v "^>" | /usr/bin/awk -F "," '{
+/bin/echo "log 500" | /usr/bin/nc 127.0.0.1 14 | /bin/grep -v "^>" | /usr/bin/awk -F "," '{
 	printf strftime("%Y%m%d %H:%M:%S ",$1);
 	for (i=2;i<=NF;i++)
 		printf $i" "
@@ -15,7 +15,7 @@
 /bin/echo -e " \n"
 
 /bin/echo -e "Clientlog \n"
-/bin/echo "log 500" | /usr/bin/nc 127.0.0.1 5001 | /bin/grep -v "^>" | /usr/bin/awk -F "," '{
+/bin/echo "log 500" | /usr/bin/nc 127.0.0.1 16 | /bin/grep -v "^>" | /usr/bin/awk -F "," '{
 	printf strftime("%Y%m%d %H:%M:%S ",$1);
 	for (i=2;i<=NF;i++)
 		printf $i" "
