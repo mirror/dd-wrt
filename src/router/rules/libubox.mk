32af27508b3c3cb402bd5622d5d8e489acfff052
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-03-03 19:37:07 +0000

    strip down
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@20832 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libubox.mk
+++ src/router/rules/libubox.mk
@@ -1,26 +1,28 @@
 PKG_BUILD_DIR_LIBUBOX=$(TOP)/libubox
 STAGING_DIR=$(TOP)/_staging
 PKG_INSTALL:=1
+CMAKE_C_COMPILER:=mips-linux-uclibc-gcc
 
-MAKE_FLAGS+=VERBOSE=1
+MAKE_FLAGS+=VERBOSE=0
+EXTRA_CFLAGS:=-I$(TOP)/_staging/usr/include
+LDFLAGS+=-L$(TOP)/_staging/usr/lib
 
 
 libubox-configure: 
-	$(call CMakeConfigure,$(PKG_BUILD_DIR_LIBUBOX),$(STAGING_DIR),$(CMAKE_OPTIONS))
+	$(call CMakeConfigure,$(PKG_BUILD_DIR_LIBUBOX),$(STAGING_DIR),$(CMAKE_OPTIONS) -DBUILD_LUA=OFF)
 
-libubox: json-c libubox-configure
-#	if [ ! -e "$(PKG_BUILD_DIR)/Makefile" ]; then $(MAKE) -C libubox clean ; fi
+libubox: 
 	$(MAKE) -C libubox
 	-mkdir -p $(TOP)/_staging
 	-mkdir -p $(STAGING_DIR)/usr/include/libubox
 	-install -D libubox/libubox.so $(STAGING_DIR)/usr/lib/libubox.so
-	-install -D libubox/libblobmsg_json.so $(STAGING_DIR)/usr/lib/libblobmsg_json.so
+#	-install -D libubox/libjson_script.so $(STAGING_DIR)/usr/lib/libjson_script.so
+#	-install -D libubox/libblobmsg_json.so $(STAGING_DIR)/usr/lib/libblobmsg_json.so
 	-cp libubox/*.h $(STAGING_DIR)/usr/include/libubox
-	#$(MAKE) -C libubox install
 
 libubox-install:
 	install -D libubox/libubox.so $(INSTALLDIR)/libubox/usr/lib/libubox.so
-	install -D libubox/libblobmsg_json.so $(INSTALLDIR)/libubox/usr/lib/libblobmsg_json.so 
+#	install -D libubox/libblobmsg_json.so $(INSTALLDIR)/libubox/usr/lib/libblobmsg_json.so 
 
 libubox-clean:
 	if [ -e "$(PKG_BUILD_DIR_LIBUBOX)/Makefile" ]; then $(MAKE) -C libubox clean ; fi
