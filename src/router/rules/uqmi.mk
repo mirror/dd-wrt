be5d7d96a8bf7dd032979b16b4d1aa1cc416425b
BrainSlayer <s.gottschall@dd-wrt.com>
2013-03-03 19:37:21 +0000

    strip down
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@20833 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rules/uqmi.mk
@@ -0,0 +1,21 @@
+PKG_BUILD_DIR_UQMI=$(TOP)/uqmi
+STAGING_DIR=$(TOP)/_staging
+PKG_INSTALL:=1
+CMAKE_C_COMPILER:=mips-linux-uclibc-gcc
+
+MAKE_FLAGS+=VERBOSE=0
+EXTRA_CFLAGS:=-I$(TOP)/_staging/usr/include -L$(TOP)/_staging/usr/lib -DNEED_PRINTF
+LDFLAGS+=-L$(TOP)/_staging/usr/lib
+
+
+uqmi-configure: 
+	$(call CMakeConfigure,$(PKG_BUILD_DIR_UQMI),$(STAGING_DIR),$(CMAKE_OPTIONS))
+
+uqmi: 
+	$(MAKE) -C uqmi
+
+uqmi-install:
+	install -D uqmi/uqmi $(INSTALLDIR)/uqmi/usr/sbin/uqmi
+
+uqmi-clean:
+	$(MAKE) -C libuqmi
