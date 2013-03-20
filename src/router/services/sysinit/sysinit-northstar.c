da220354a35d552bd9f9c81ce6d0c93847690ed7
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-03-20 14:38:38 +0000

    Asus RT-AC67U support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21018 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-northstar.c
+++ src/router/services/sysinit/sysinit-northstar.c
@@ -139,6 +139,7 @@ void start_sysinit(void)
 
 	switch (getRouterBrand()) {
 	case ROUTER_ASUS_AC56U:
+	case ROUTER_ASUS_AC67U:
 		if (nvram_get("productid") != NULL
 		    || nvram_match("http_username", "admin")) {
 			int deadcount = 10;
