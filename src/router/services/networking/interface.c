6492ec3269de3f7f364a29382d52b37bcd8a7699
BrainSlayer <s.gottschall@dd-wrt.com>
2013-03-24 18:02:46 +0000

    fix typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21052 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/interface.c
+++ src/router/services/networking/interface.c
@@ -1,24 +1,25 @@
-^				/*
-				 * interface.c
-				 *
-				 * Copyright (C) 2007 Sebastian Gottschall <gottschall@dd-wrt.com>
-				 *
-				 * This program is free software; you can redistribute it and/or
-				 * modify it under the terms of the GNU General Public License
-				 * as published by the Free Software Foundation; either version 2
-				 * of the License.
-				 *
-				 * This program is distributed in the hope that it will be useful,
-				 * but WITHOUT ANY WARRANTY; without even the implied warranty of
-				 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-				 * GNU General Public License for more details.
-				 *
-				 * You should have received a copy of the GNU General Public License
-				 * along with this program; if not, write to the Free Software
-				 * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-				 *
-				 * $Id:
-				 */
+/*
+ * interface.c
+ *
+ * Copyright (C) 2007 Sebastian Gottschall <gottschall@dd-wrt.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id:
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
