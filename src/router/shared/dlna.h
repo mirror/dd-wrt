a29d7c8912b7e24caf49c5709c4987ae323e2053
BrainSlayer <s.gottschall@dd-wrt.com>
2013-03-26 15:31:08 +0000

    prepare dlna gui
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21067 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/shared/dlna.h
@@ -0,0 +1,20 @@
+#ifndef _dlna_h_
+#define _dlna_h_
+#include <jansson.h>
+
+#define TYPE_AUDIO 0x1
+#define TYPE_VIDEO 0x2
+#define TYPE_IMAGES 0x4
+
+struct dlna_share {
+	char mp[32];
+	char label[64];
+	int types;
+	struct dlna_share *next;
+};
+
+
+struct dlna_share *getdlnashares(void);
+struct dlna_share *getdlnashare(char *mp, char *label, int types);
+
+#endif
