be16479dda75410f6cfe07e2cf08404a30163909
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-03-29 13:43:35 +0000

    update kernels
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21129 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.4/include/linux/skbuff.h
+++ src/linux/universal/linux-3.4/include/linux/skbuff.h
@@ -492,7 +492,7 @@ struct sk_buff {
 	union {
 		__u32		mark;
 		__u32		dropcount;
-		__u32		avail_size;
+		__u32		reserved_tailroom;
 	};
 
 	sk_buff_data_t		transport_header;
@@ -1392,7 +1392,10 @@ static inline int skb_tailroom(const struct sk_buff *skb)
  */
 static inline int skb_availroom(const struct sk_buff *skb)
 {
-	return skb_is_nonlinear(skb) ? 0 : skb->avail_size - skb->len;
+	if (skb_is_nonlinear(skb))
+		return 0;
+
+	return skb->end - skb->tail - skb->reserved_tailroom;
 }
 
 /**
