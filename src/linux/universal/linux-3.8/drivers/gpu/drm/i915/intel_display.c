1d5a9949436ada426237b06eb365ff992e22bd59
BrainSlayer <s.gottschall@dd-wrt.com>
2013-04-11 19:04:42 +0000

    update 3.8.7-rc1
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21231 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.8/drivers/gpu/drm/i915/intel_display.c
+++ src/linux/universal/linux-3.8/drivers/gpu/drm/i915/intel_display.c
@@ -8901,6 +8901,15 @@ static struct intel_quirk intel_quirks[] = {
 
 	/* Acer Aspire 4736Z */
 	{ 0x2a42, 0x1025, 0x0260, quirk_invert_brightness },
+
+	/* Acer/eMachines G725 */
+	{ 0x2a42, 0x1025, 0x0210, quirk_invert_brightness },
+
+	/* Acer/eMachines e725 */
+	{ 0x2a42, 0x1025, 0x0212, quirk_invert_brightness },
+
+	/* Acer/Packard Bell NCL20 */
+	{ 0x2a42, 0x1025, 0x034b, quirk_invert_brightness },
 };
 
 static void intel_init_quirks(struct drm_device *dev)
