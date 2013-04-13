728901a1a0cd9e510b71a843f8f06e8ec5b3fe66
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-04-13 16:32:55 +0000

    use different kernel entry
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21261 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.9/arch/mips/ar7240/prom.c
+++ src/linux/universal/linux-3.9/arch/mips/ar7240/prom.c
@@ -38,18 +38,11 @@
 #include <asm/mach-ar71xx/ar71xx.h>
 #include <asm/mach-ar71xx/ar933x_uart.h>
 
-int __ath_flash_size;
 
 
 void __init prom_init(void)
 {
-
-    printk ("flash_size passed from bootloader = %d\n", (int)fw_arg3);
-    __ath_flash_size = fw_arg3;
-
-
     mips_machtype  = MACH_ATHEROS_AR7240;
-
 }
 
 void __init prom_free_prom_memory(void)
