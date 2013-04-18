7ae1f9bd35781b5ed75e7cc3db8d78977765ddaa
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-04-18 10:31:00 +0000

    mod secure
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21325 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/olsrd.mk
+++ src/router/rules/olsrd.mk
@@ -12,5 +12,6 @@ ifneq ($(CONFIG_DIST),"micro")
 	install -D olsrd/lib/dyn_gw/olsrd_dyn_gw.so.0.5 $(INSTALLDIR)/olsrd/usr/lib/olsrd_dyn_gw.so
 	install -D olsrd/lib/httpinfo/olsrd_httpinfo.so.0.1 $(INSTALLDIR)/olsrd/usr/lib/olsrd_httpinfo.so
 	install -D olsrd/lib/nameservice/olsrd_nameservice.so.0.3 $(INSTALLDIR)/olsrd/usr/lib/olsrd_nameservice.so
+	install -D olsrd/lib/secure/olsrd_secure.so.0.6 $(INSTALLDIR)/olsrd/usr/lib/olsrd_secure.so.0.6
 endif
 	install -D olsrd/config/olsrd.nvramconfig $(INSTALLDIR)/olsrd/etc/config/olsrd.nvramconfig
