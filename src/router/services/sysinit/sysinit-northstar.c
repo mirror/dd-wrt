b679a00f801ac44a1d16ea4b895ee40353d9359a
BrainSlayer <s.gottschall@dd-wrt.com>
2013-04-20 16:15:32 +0000

    sane default state
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21373 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-northstar.c
+++ src/router/services/sysinit/sysinit-northstar.c
@@ -245,6 +245,15 @@ void start_sysinit(void)
 	stime(&tm);
 	nvram_set("wl0_ifname", "ath0");
 
+	led_control(LED_POWER, LED_ON);
+	led_control(LED_DIAG, LED_OFF);
+	led_control(LED_SES, LED_OFF);
+	led_control(LED_SES2, LED_OFF);
+	led_control(LED_BRIDGE, LED_OFF);
+	led_control(LED_WLAN0, LED_OFF);
+	led_control(LED_WLAN1, LED_OFF);
+	led_control(LED_CONNECTED, LED_OFF);
+
 	return;
 }
 
