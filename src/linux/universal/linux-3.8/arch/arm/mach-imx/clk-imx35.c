042a6e37e7f8e7c09a394f7b546b046fbcbab5a1
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-04-24 08:38:23 +0000

    update to 3.8.9-rc1
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21392 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.8/arch/arm/mach-imx/clk-imx35.c
+++ src/linux/universal/linux-3.8/arch/arm/mach-imx/clk-imx35.c
@@ -265,6 +265,8 @@ int __init mx35_clocks_init()
 	clk_prepare_enable(clk[gpio3_gate]);
 	clk_prepare_enable(clk[iim_gate]);
 	clk_prepare_enable(clk[emi_gate]);
+	clk_prepare_enable(clk[max_gate]);
+	clk_prepare_enable(clk[iomuxc_gate]);
 
 	/*
 	 * SCC is needed to boot via mmc after a watchdog reset. The clock code
