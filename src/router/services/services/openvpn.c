b13a5cf637ba08f0b4417c5ca4f6a798dc51ba95
sash <sash@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-04-26 20:31:38 +0000

    ovpncl: change order
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21412 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/openvpn.c
+++ src/router/services/services/openvpn.c
@@ -496,13 +496,13 @@ void start_openvpn(void)
 	if (strlen(nvram_safe_get("openvpncl_route")) > 0) {	//policy based routing
 		write_nvram("/tmp/openvpncl/policy_ips", "openvpncl_route");
 //		fprintf(fp, "ip route flush table 10\n");
-		if (nvram_match("openvpncl_tuntap", "tap"))
-			fprintf(fp, "ip route add default via $route_vpn_gateway table 10\n");
-		else {
-			fprintf(fp, "ip route add default via $ifconfig_remote table 10\n");
-			}
 		fprintf(fp, "for IP in `cat /tmp/openvpncl/policy_ips` ; do\n"
 			"\t ip rule add from $IP table 10\n" "done\n");
+		if (nvram_match("openvpncl_tuntap", "tap"))
+			fprintf(fp, "ip route add default via $route_vpn_gateway table 10\n");
+		else
+			fprintf(fp, "ip route add default via $ifconfig_remote table 10\n");
+		fprintf(fp, "ip route flush cache\n");
 	}
 	if (nvram_match("block_multicast", "0") //block multicast on bridged vpns
 		&& nvram_match("openvpncl_tuntap", "tap")
