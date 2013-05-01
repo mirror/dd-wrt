7d1e0f4107fd424c0a0b1263bf4d0143e365e160
BrainSlayer <s.gottschall@dd-wrt.com>
2013-05-01 18:06:49 +0000

    fix compiling
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21442 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.9/net/bridge/br_if.c
+++ src/linux/universal/linux-3.9/net/bridge/br_if.c
@@ -67,8 +67,8 @@ void br_port_carrier_check(struct net_bridge_port *p)
 	struct net_device *dev = p->dev;
 	struct net_bridge *br = p->br;
 
-	if (!(p->flags & BR_ADMIN_COST) &&
-	if (netif_running(dev) && netif_carrier_ok(dev))
+	if (!(p->flags & BR_ADMIN_COST) && 
+		netif_running(dev) && netif_carrier_ok(dev))
 		p->path_cost = port_cost(dev);
 
 	if (!netif_running(br->dev))
