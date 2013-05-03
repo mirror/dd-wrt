42df79f6ba0603610381f98a728e716d3b95254c
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-05-03 12:31:38 +0000

    even higher performance. up to 3 times faster than before
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21460 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/usb_hotplug.c
+++ src/router/services/services/usb_hotplug.c
@@ -268,7 +268,7 @@ static int usb_process_path(char *path, char *fs, char *target)
 		mkdir("/tmp/mnt", 0700);
 #ifdef HAVE_NTFS3G
 	if (!strcmp(fs, "ntfs")) {
-		ret = eval("ntfs-3g", "-o", "compression,direct_io", path, mount_point);
+		ret = eval("ntfs-3g", "-o", "compression,direct_io,big_writes", path, mount_point);
 	} else
 #endif
 	if (!strcmp(fs, "vfat"))
