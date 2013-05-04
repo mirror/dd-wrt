9dd8b0f80f6364672d5e44a2f05520aae1c103fd
chris <chris@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-05-04 11:29:31 +0000

    chillispot enable Reverse Path Filtering to prevent double packages (typo)
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21467 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/chillispot.c
+++ src/router/services/services/chillispot.c
@@ -188,10 +188,10 @@ void main_config(void)
 	fprintf(fp, "iptables -I FORWARD -o tun0 -j %s\n", log_accept);
 	// enable Reverse Path Filtering to prevent double outgoing packages
 	if (nvram_match("chilli_enable", "1") && nvram_match("hotss_enable", "0")) {
-		fprintf(fp, "echo 1 > proc/sys/net/ipv4/conf/%s/rp_filter\n", nvram_safe_get("chilli_interface"));
+		fprintf(fp, "echo 1 > /proc/sys/net/ipv4/conf/%s/rp_filter\n", nvram_safe_get("chilli_interface"));
 	}
 	if (nvram_match("chilli_enable", "1") && nvram_match("hotss_enable", "1")) {
-		fprintf(fp, "echo 1 > proc/sys/net/ipv4/conf/%s/rp_filter\n", nvram_safe_get("hotss_interface"));
+		fprintf(fp, "echo 1 > /proc/sys/net/ipv4/conf/%s/rp_filter\n", nvram_safe_get("hotss_interface"));
 	}
 	//      secure chilli interface, only usefull if ! br0
 	if (nvram_match("chilli_enable", "1")
