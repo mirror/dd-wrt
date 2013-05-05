a6fdefa38a4e4c5562513780ab7c2d35d9ae41f3
ChristianScheele <scheele@newmedia-net.de>
2013-05-05 13:02:23 +0000

    coova-chilli rule files changes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21473 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/chillispot.mk
+++ src/router/rules/chillispot.mk
@@ -1,27 +1,50 @@
+ifeq ($(CONFIG_COOVA_CHILLI),y)
+CHILLIDIR=coova-chilli
+CHILLIEXTRAFLAGS=--enable-uamdomainfile \
+	--prefix=/usr \
+	--datadir=/usr/share \
+	--localstatedir=/var \
+	--sysconfdir=/etc \
+	--disable-miniportal \
+	--sbindir=/usr/sbin \
+	--enable-shared \
+	--disable-static \
+	--disable-debug \
+	--enable-binstatusfile 
+else
+CHILLIDIR=chillispot
+endif
+
 chillispot-configure:
-	cd chillispot && ./configure --host=$(ARCH)-linux-elf CFLAGS="$(COPTS) -DHAVE_MALLOC=1 -Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections"
+	cd $(CHILLIDIR) && ./configure $(CHILLIEXTRAFLAGS) --host=$(ARCH)-linux-elf CFLAGS="$(COPTS) -DHAVE_MALLOC=1 -Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections"
 
 chillispot:
-	$(MAKE) -j 4 -C chillispot
+	$(MAKE) -j 4 -C $(CHILLIDIR)
 
 chillispot-install:
 ifneq ($(CONFIG_FON),y)
-	install -D chillispot/config/chillispot.nvramconfig $(INSTALLDIR)/chillispot/etc/config/chillispot.nvramconfig
-	install -D chillispot/config/chillispot.webhotspot $(INSTALLDIR)/chillispot/etc/config/chillispot.webhotspot
+	install -D $(CHILLIDIR)/config/chillispot.nvramconfig $(INSTALLDIR)/chillispot/etc/config/chillispot.nvramconfig
+	install -D $(CHILLIDIR)/config/chillispot.webhotspot $(INSTALLDIR)/chillispot/etc/config/chillispot.webhotspot
 endif
 ifeq ($(CONFIG_TIEXTRA1),y)
 	install -D private/telkom/mchillispot.webhotspot $(INSTALLDIR)/chillispot/etc/config/chillispotm.webhotspot
 endif
 ifeq ($(CONFIG_CHILLILOCAL),y)
-	install -D chillispot/config/fon.nvramconfig $(INSTALLDIR)/chillispot/etc/config/fon.nvramconfig
-	install -D chillispot/config/fon.webhotspot $(INSTALLDIR)/chillispot/etc/config/fon.webhotspot
+	install -D $(CHILLIDIR)/config/fon.nvramconfig $(INSTALLDIR)/chillispot/etc/config/fon.nvramconfig
+	install -D $(CHILLIDIR)/config/fon.webhotspot $(INSTALLDIR)/chillispot/etc/config/fon.webhotspot
 endif
 ifeq ($(CONFIG_HOTSPOT),y)
-	install -D chillispot/config/hotss.nvramconfig $(INSTALLDIR)/chillispot/etc/config/hotss.nvramconfig
-	install -D chillispot/config/3hotss.webhotspot $(INSTALLDIR)/chillispot/etc/config/3hotss.webhotspot
+	install -D $(CHILLIDIR)/config/hotss.nvramconfig $(INSTALLDIR)/chillispot/etc/config/hotss.nvramconfig
+	install -D $(CHILLIDIR)/config/3hotss.webhotspot $(INSTALLDIR)/chillispot/etc/config/3hotss.webhotspot
 endif
-	install -D chillispot/src/chilli $(INSTALLDIR)/chillispot/usr/sbin/chilli
-	$(STRIP) $(INSTALLDIR)/chillispot/usr/sbin/chilli
+	install -D $(CHILLIDIR)/src/chilli $(INSTALLDIR)/chillispot/usr/sbin/chilli
+ifeq ($(CONFIG_COOVA_CHILLI),y)
+	-install -D $(CHILLIDIR)/src/chilli_opt $(INSTALLDIR)/chillispot/usr/sbin/chilli_opt
+	-install -D $(CHILLIDIR)/src/chilli_query $(INSTALLDIR)/chillispot/usr/sbin/chilli_query
+	-install -D $(CHILLIDIR)/src/chilli_radconfig $(INSTALLDIR)/chillispot/usr/sbin/chilli_radconfig
+	-install -D $(CHILLIDIR)/src/chilli_response $(INSTALLDIR)/chillispot/usr/sbin/chilli_response
+endif
+	$(STRIP) $(INSTALLDIR)/chillispot/usr/sbin/chilli*
 
 chillispot-clean:
-	$(MAKE) -C chillispot clean
+	$(MAKE) -C $(CHILLIDIR) clean
