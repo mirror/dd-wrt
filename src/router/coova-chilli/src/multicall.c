fbc98b913ba3cb4bc86abc5d4e00eeb95a1c43e8
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-05-05 19:34:14 +0000

    coova multicall
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21482 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/coova-chilli/src/multicall.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include "../config.h"
+
+
+extern int mchilli_main(int argc, char **argv);
+extern int mopt_main(int argc, char **argv);
+extern int mproxy_main(int argc, char **argv);
+extern int mquery_main(int argc, char **argv);
+extern int mradconfig_main(int argc, char **argv);
+extern int mresponse_main(int argc, char **argv);
+extern int mradsec_main(int argc, char **argv);
+
+int main(int argc, char **argv)
+{
+	bool restart = false;
+	const char *prog = argv[0];
+
+restart:
+	if (strstr(prog, "chilli"))
+		return mchilli_main(argc, argv);
+	else if (strstr(prog, "chilli_opt"))
+		return mopt_main(argc, argv);
+	else if (strstr(prog, "chilli_query"))
+		return mquery_main(argc, argv);
+	else if (strstr(prog, "chilli_radconfig"))
+		return mradconfig_main(argc, argv);
+	else if (strstr(prog, "chilli_response"))
+		return mresponse_main(argc, argv);
+
+	#if defined(ENABLE_CHILLIPROXY)
+	else if (strstr(prog, "chilli_proxy"))
+		return mproxy_main(argc, argv);
+	#endif
+	#if defined(ENABLE_CHILLIRADSEC)
+	else if (strstr(prog, "chilli_radsec"))
+		return mradsec_main(argc, argv);
+	#endif
+	#if defined(ENABLE_CHILLIREDIR)
+	else if (strstr(prog, "chilli_redir"))
+		return mredir_main(argc, argv);
+	#endif
+	#if defined(ENABLE_MULTIROUTE)
+	else if (strstr(prog, "chilli_rtmon"))
+		return mrtmon_main(argc, argv);
+	#endif
+	#if defined(ENABLE_CHILLISCRIPT)
+	else if (strstr(prog, "chilli_script"))
+		return mscript_main(argc, argv);
+	#endif
+	if (!restart && argc > 1) {
+		argv++;
+		argc--;
+		restart = true;
+		goto restart;
+	}
+
+	fprintf(stderr, "Invalid command.\nUsage: %s wpa_supplicant|hostapd [<arguments>]\n", prog);
+	return 255;
+}
