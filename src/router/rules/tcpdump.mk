299d81466ddbb70b5ed4cc804a5cca28056f5a99
BrainSlayer <s.gottschall@dd-wrt.com>
2013-05-11 14:01:40 +0000

    fix it
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21585 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/tcpdump.mk
+++ src/router/rules/tcpdump.mk
@@ -5,7 +5,7 @@ endif
 
 
 tcpdump-configure: libpcap
-	cd tcpdump && ./configure --host=$(ARCH)-linux --enable-shared --disable-static --without-crypto $(EXTRA) CC="$(CC)" ac_cv_linux_vers=2 ac_cv_ssleay_path=no td_cv_buggygetaddrinfo=no CPPFLAGS="-I../libpcap_noring $(COPTS) $(MIPS16_OPT) -DNEED_PRINTF" CFLAGS="-I../libpcap_noring $(COPTS) $(MIPS16_OPT)-DNEED_PRINTF -DHAVE_BPF_DUMP" LDFLAGS="-L../libpcap_noring"
+	cd tcpdump && ./configure --host=$(ARCH)-linux --enable-shared --disable-static --without-crypto $(EXTRA) CC="$(CC)" ac_cv_linux_vers=2 ac_cv_ssleay_path=no td_cv_buggygetaddrinfo=no CPPFLAGS="-I../libpcap_noring $(COPTS) $(MIPS16_OPT) -DNEED_PRINTF" CFLAGS="-I../libpcap_noring $(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -DHAVE_BPF_DUMP" LDFLAGS="-L../libpcap_noring"
 
 tcpdump:
 	make -j 4 -C tcpdump
