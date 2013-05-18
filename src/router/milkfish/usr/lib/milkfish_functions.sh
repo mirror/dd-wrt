f679d2fc7419efdeb4add1a2ccdaf1e7cd7ce294
BrainSlayer <s.gottschall@dd-wrt.com>
2013-05-18 09:02:10 +0000

    fix script
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21619 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/milkfish/usr/lib/milkfish_functions.sh
+++ src/router/milkfish/usr/lib/milkfish_functions.sh
@@ -506,8 +506,9 @@ mf_sipdb_restorenvdd () {
     [ -d /var/openser/dbtext ] &&\
     mf_feedback "Restoring SIP ddsubscriber database from NVRAM..."
     if [ ! -z "$(nvram get milkfish_ddsubscribers)" ]; then 
+	echo "#!/bin/sh" > /tmp/restorenvdd.sh
 	nvram get milkfish_ddsubscribers | tr ' ' '\n' | awk -F : '{print "dbtextctl add " $1 " " $2 " " "nomail"}'
-	nvram get milkfish_ddsubscribers | tr ' ' '\n' | awk -F : '{print "dbtextctl add " $1 " " $2 " " "nomail"}' > /tmp/restorenvdd.sh
+	nvram get milkfish_ddsubscribers | tr ' ' '\n' | awk -F : '{print "dbtextctl add " $1 " " $2 " " "nomail"}' >> /tmp/restorenvdd.sh
 	[ -e /tmp/restorenvdd.sh ] && chmod +x /tmp/restorenvdd.sh && /tmp/restorenvdd.sh && rm /tmp/restorenvdd.sh
 	echo "Done."
     else
@@ -515,8 +516,9 @@ mf_sipdb_restorenvdd () {
     fi
     mf_feedback "Restoring SIP ddaliases database from NVRAM..."
     if [ ! -z "$(nvram get milkfish_ddaliases)" ]; then 
+	echo "#!/bin/sh" > /tmp/restorenvdd.sh
 	[ -e /var/openser/dbtext/aliases ] && nvram get milkfish_ddaliases | tr ' ' '\n' | awk -F : '{print "[ -z \"$(grep " $1 ": /var/openser/dbtext/aliases)\" ] && echo \"" $1 "::sip\\:" $2 "::0:1.00:Milkfish-Alias:42::0:0:128:Milkfish-Router:\" >> /var/openser/dbtext/aliases" }'
-	[ -e /var/openser/dbtext/aliases ] && nvram get milkfish_ddaliases | tr ' ' '\n' | awk -F : '{print "[ -z \"$(grep " $1 ": /var/openser/dbtext/aliases)\" ] && echo \"" $1 "::sip\\:" $2 "::0:1.00:Milkfish-Alias:42::0:0:128:Milkfish-Router:\" >> /var/openser/dbtext/aliases" }' > /tmp/restorenvdd.sh
+	[ -e /var/openser/dbtext/aliases ] && nvram get milkfish_ddaliases | tr ' ' '\n' | awk -F : '{print "[ -z \"$(grep " $1 ": /var/openser/dbtext/aliases)\" ] && echo \"" $1 "::sip\\:" $2 "::0:1.00:Milkfish-Alias:42::0:0:128:Milkfish-Router:\" >> /var/openser/dbtext/aliases" }' >> /tmp/restorenvdd.sh
 	[ -e /tmp/restorenvdd.sh ] && chmod +x /tmp/restorenvdd.sh && /tmp/restorenvdd.sh && rm /tmp/restorenvdd.sh
 	echo "Done."
     else
