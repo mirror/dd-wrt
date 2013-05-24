454e43dd920df4a08e6bd3bbfa5ac56f00039de2
BrainSlayer <s.gottschall@dd-wrt.com>
2013-05-24 12:46:34 +0000

    set only if parameter doesnt exist
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21662 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-broadcom.c
+++ src/router/services/sysinit/sysinit-broadcom.c
@@ -749,7 +749,7 @@ void start_sysinit(void)
 	case ROUTER_NETGEAR_WNDR4500:
 	case ROUTER_NETGEAR_WNDR4500V2:
 	case ROUTER_NETGEAR_R6300:
-		nvram_set("clkfreq","600");
+		check_nv("clkfreq","600");
 		nvram_set("vlan1hwname", "et0");
 		nvram_set("vlan2hwname", "et0");
 		nvram_set("vlan1ports", "0 1 2 3 8*");
