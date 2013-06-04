2b21b9d1a2e74a073988f4b46c1f928e0cafdd10
chris <chris@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-06-04 18:13:43 +0000

    fix _cmake.mk / libubox / uqmi (cmake does not like to be called again if CMakeCache.txt is already there)
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21780 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/_cmake.mk
+++ src/router/rules/_cmake.mk
@@ -1,8 +1,11 @@
+export TMPDIR=/tmp
 define CMakeConfigure 
 	(cd $(strip $(1)); \
-		CFLAGS="$(CFLAGS) $(EXTRA_CFLAGS)" \
+		CFLAGS="$(CFLAGS) $(EXTRA_CFLAGS) $(4)" \
+		LDFLAGS="$(5)" \
 		CXXFLAGS="$(CFLAGS) $(EXTRA_CFLAGS)" \
 		cmake \
+			--debug-output \
 			-DCMAKE_SYSTEM_NAME=Linux \
 			-DCMAKE_SYSTEM_VERSION=1 \
 			-DCMAKE_SYSTEM_PROCESSOR=$(ARCH) \
@@ -11,18 +14,17 @@ define CMakeConfigure
 			-DCMAKE_CXX_FLAGS_RELEASE="-DNDEBUG" \
 			-DCMAKE_C_COMPILER=$(CROSS_COMPILE)gcc \
 			-DCMAKE_CXX_COMPILER=$(CROSS_COMPILE)g++ \
-			-DCMAKE_EXE_LINKER_FLAGS="$(LDFLAGS)" \
-			-DCMAKE_MODULE_LINKER_FLAGS="$(LDFLAGS)" \
-			-DCMAKE_SHARED_LINKER_FLAGS="$(LDFLAGS)" \
+			-DCMAKE_EXE_LINKER_FLAGS="$(5)" \
+			-DCMAKE_MODULE_LINKER_FLAGS="$(5)" \
+			-DCMAKE_SHARED_LINKER_FLAGS="$(5)" \
 			-DCMAKE_FIND_ROOT_PATH=$(2) \
-			-DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=BOTH \
-			-DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY \
-			-DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY \
+			-DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=$(STAGING_DIR_HOST) \
+			-DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=$(2) \
+			-DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=$(2) \
 			-DCMAKE_STRIP=: \
-			-DCMAKE_INSTALL_PREFIX=$(2) \
-			-DDL_LIBRARY=$(2) \
+			-DCMAKE_INSTALL_PREFIX=/usr \
 			$(3) \
-		. \
+			. \
 	)
 endef
 
@@ -30,5 +32,6 @@ define CMakeClean
 	( rm -rf $(strip $(1))/CMakeCache.txt ;\
 	rm -rf $(strip $(1))/cmake_install.cmake ;\
 	rm -rf $(strip $(1))/CMakeFiles ;\
+	rm -rf $(strip $(1))/Makefile ;\
 	)	
 endef
