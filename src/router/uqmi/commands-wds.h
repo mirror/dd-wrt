ed36d8403e2ece333d8545c2fb19183b583a6591
chris <chris@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-06-11 07:29:50 +0000

    uqmi add set_roaming
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21823 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/uqmi/commands-wds.h
+++ src/router/uqmi/commands-wds.h
@@ -3,6 +3,7 @@
 	__uqmi_command(wds_set_auth, auth-type, required, CMD_TYPE_OPTION), \
 	__uqmi_command(wds_set_username, username, required, CMD_TYPE_OPTION), \
 	__uqmi_command(wds_set_password, password, required, CMD_TYPE_OPTION), \
+	__uqmi_command(wds_set_autoconnect, autoconnect, no, CMD_TYPE_OPTION), \
 	__uqmi_command(wds_get_packet_service_status, get-data-status, no, QMI_SERVICE_WDS), \
 	__uqmi_command(wds_reset, reset-wds, no, QMI_SERVICE_WDS) \
 
@@ -12,5 +13,6 @@
 		"    --auth-type pap|chap|both|none: Use network authentication type\n" \
 		"    --username <name>:              Use network username\n" \
 		"    --password <password>:          Use network password\n" \
+		"    --autconnect:                   Enable automatic connect/reconnect\n" \
 		"  --get-data-status:                Get current data access status\n" \
 
