ed2950d34e03c522b23daa7c6695ab6bdfbafed7
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-06-26 20:15:02 +0000

    cd
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21924 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-northstar.c
+++ src/router/services/sysinit/sysinit-northstar.c
@@ -231,8 +231,7 @@ void start_sysinit(void)
 			buf[26] = 0;
 			fprintf(stderr, "set 5g mac %s\n", &buf[9]);
 			nvram_set("pci/2/0/macaddr", &buf[9]);
-			nvram_set("pci/2/1/macaddr", &buf[9]);
-
+			nvram_unset("pci/2/1/macaddr");
 			fp = popen("cat /dev/mtdblock0|grep wlan24mac", "rb");
 			fread(buf, 1, 27, fp);
 			fclose(fp);
@@ -433,12 +432,14 @@ void start_sysinit(void)
 			t = dir868_1_1params;
 			while (t->name) {
 				fprintf(stderr, "set pci/1/1/%s to %s\n", t->name, t->value);
+				usleep(10);
 				nvram_nset(t->value, "pci/1/1/%s", t->name);
 				t++;
 			}
 			t = dir868_2_1params;
 			while (t->name) {
 				fprintf(stderr, "set pci/2/1/%s to %s\n", t->name, t->value);
+				usleep(10);
 				nvram_nset(t->value, "pci/2/1/%s", t->name);
 				t++;
 			}
