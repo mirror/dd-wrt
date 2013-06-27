693ba9673bbe5ccd1a55c952cdeb6e856a0a4e24
ChristianScheele <scheele@newmedia-net.de>
2013-06-27 14:17:56 +0000

    libpcap allow (re)-configure (and recompiling), fix lib name for shared lib
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@21933 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libpcap.mk
+++ src/router/rules/libpcap.mk
@@ -1,8 +1,21 @@
+libpcap-configure:
+	cd libpcap_noring ; ac_cv_linux_vers=2 ./configure \
+		--target=$(ARCH)-openwrt-linux \
+		--host=$(ARCH)-linux-uclibc  \
+		--program-prefix= --program-suffix= --prefix=/usr \
+		--exec-prefix=/usr --bindir=/usr/bin --sbindir=/usr/sbin \
+		--libexecdir=/usr/lib --sysconfdir=/etc --datadir=/usr/share \
+		--localstatedir=/var --mandir=/usr/man --infodir=/usr/info \
+		--disable-nls --enable-shared --enable-static \
+		--disable-yydebug --enable-ipv6 --with-build-cc=gcc \
+		--with-pcap=linux --without-septel --without-dag \
+		CC="$(CC)" CFLAGS="$(COPTS)"
 libpcap:
 #	$(MAKE) -C libpcap CC="$(CC)" AR=$(AR) RANLIB=$(RANLIB) libpcap.so 
 	$(MAKE) -C libpcap_noring CC="$(CC)" AR=$(AR) RANLIB=$(RANLIB) version.h
 ifeq ($(CONFIG_LIBPCAP_SHARED),y)
 	$(MAKE) -C libpcap_noring CC="$(CC)" AR=$(AR) RANLIB=$(RANLIB) libpcap.so 
+	-cd libpcap_noring ; ln -s libpcap.so.1.0.0 libpcap.so
 else
 	$(MAKE) -C libpcap_noring CC="$(CC)" AR=$(AR) RANLIB=$(RANLIB) libpcap.a 
 endif
