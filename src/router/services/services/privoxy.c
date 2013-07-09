2ca95871395f44ce951849acc6e3f614c42af068
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-07-09 22:24:08 +0000

    c translation
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22009 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/privoxy.c
+++ src/router/services/services/privoxy.c
@@ -1,4 +1,4 @@
- 
+
 #ifdef HAVE_PRIVOXY
 #include <unistd.h>
 #include <stdio.h>
@@ -13,13 +13,44 @@
 #include <utils.h>
 #include <bcmnvram.h>
 #include <shutils.h>
+#include <services.h>
 
 void start_privoxy(void)
 {
 	if (!nvram_match("privoxy_enable", "1"))
 		return;
 
-	eval("sh" , "/etc/config/privoxy.startup");;
+	int mode = 0;
+	char *ip = nvram_safe_get("lan_ipaddr");
+
+	sysprintf("grep -q nobody /etc/passwd || echo \"nobody:*:65534:65534:nobody:/var:/bin/false\" >> /etc/passwd");
+	mkdir("/var/log/privoxy", 0777);
+
+	if (nvram_match("privoxy_transp_enable", "1"))
+		mode = 1;
+
+	FILE *fp = fopen("/tmp/privoxy.conf", "wb");
+
+	if (nvram_match("privoxy_advanced", "1") && nvram_invmatch("privoxy_conf", "")) {
+		fprintf(fp, "%s", nvram_safe_get("privoxy_conf"));
+	} else {
+		fprintf(fp, "confdir /etc/privoxy\n"
+			"logdir /var/log/privoxy\n"
+			"actionsfile match-all.action\n"
+			"actionsfile default.action\n"
+			"actionsfile user.action\n"
+			"filterfile default.filter\n"
+			"logfile logfile\n"
+			"listen-address  %s:8118\n"
+			"toggle  1\n"
+			"enable-remote-toggle  0\n"
+			"enable-remote-http-toggle  0\n"
+			"enable-edit-actions 0\n"
+			"buffer-limit 4096\n"
+			"accept-intercepted-requests %d\n" "split-large-forms 0\n" "keep-alive-timeout 5\n" "socket-timeout 300\n" "max-client-connections 64\n" "handle-as-empty-doc-returns-ok 1\n", ip, mode);
+	}
+	fclose(fp);
+	eval("privoxy", "/tmp/privoxy.conf");
 	syslog(LOG_INFO, "Privoxy : privoxy started\n");
 	return;
 }
