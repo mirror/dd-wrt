475f4b66f040d6b73fd35ade89bdd835356bd2dd
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-07-10 10:18:53 +0000

    optimize
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22014 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/privoxy.mk
+++ src/router/rules/privoxy.mk
@@ -1,9 +1,9 @@
 
 privoxy:
 	CC="$(ARCH)-linux-uclibc-gcc" \
-	CFLAGS="$(COPTS) -I$(TOP)/zlib -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	CPPFLAGS="$(COPTS) -I$(TOP)/zlib -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	LDFLAGS="$(COPTS) -L$(TOP)/zlib -lz -fPIC" \
+	CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	LDFLAGS="$(COPTS) $(MIPS16_OPT) -L$(TOP)/zlib -ffunction-sections -fdata-sections -Wl,--gc-sections -lz -fPIC" \
 	$(MAKE) -C privoxy
 
 privoxy-install:
@@ -24,4 +24,4 @@ privoxy-clean:
 privoxy-configure:
 	cd privoxy && rm -rf config.{cache,status} \
 	&& autoheader && autoconf \
-	&& ./configure ac_cv_func_setpgrp_void=yes --prefix=/usr --sysconfdir=/etc/privoxy --target=$(ARCH)-linux --host=$(ARCH) CC=$(ARCH)-linux-uclibc-gcc LDFLAGS="$(COPTS) -L$(TOP)/zlib -lz -fPIC" 
+	&& ./configure ac_cv_func_setpgrp_void=yes --prefix=/usr --sysconfdir=/etc/privoxy --target=$(ARCH)-linux --host=$(ARCH) CC=$(ARCH)-linux-uclibc-gcc LDFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/zlib -lz -fPIC" 
