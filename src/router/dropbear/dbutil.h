83817c75082837e30bfdaed7b1152022ece488fc
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-07-11 16:15:07 +0000

    fixup
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22039 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/dropbear/dbutil.h
+++ src/router/dropbear/dbutil.h
@@ -92,6 +92,7 @@ int m_str_to_uint(const char* str, unsigned int *val);
 #define DEF_MP_INT(X) mp_int X = {0, 0, 0, NULL}
 
 /* Dropbear assertion */
-#define dropbear_assert(X) do { if (!(X)) { fail_assert(#X, __FILE__, __LINE__); } } while (0)
+#define dropbear_assert(X) do {} while(0)
+//do { if (!(X)) { fail_assert(#X, __FILE__, __LINE__); } } while (0)
 
 #endif /* _DBUTIL_H_ */
