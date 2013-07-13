7a2a5057efb91068f9fdf3f275c6d1dbfadab75d
BrainSlayer <s.gottschall@dd-wrt.com>
2013-07-13 14:52:48 +0000

    nbd's patch to shrink metadata
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22052 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.9/kernel/module.c
+++ src/linux/universal/linux-3.9/kernel/module.c
@@ -2692,6 +2692,7 @@ static struct module *setup_load_info(struct load_info *info, int flags)
 
 static int check_modinfo(struct module *mod, struct load_info *info, int flags)
 {
+#ifndef CONFIG_MODULE_STRIPPED
 	const char *modmagic = get_modinfo(info, "vermagic");
 	int err;
 
@@ -2718,6 +2719,7 @@ static int check_modinfo(struct module *mod, struct load_info *info, int flags)
 		       " the quality is unknown, you have been warned.\n",
 		       mod->name);
 	}
+#endif
 
 	/* Set up license info based on the info section */
 	set_license(mod, get_modinfo(info, "license"));
