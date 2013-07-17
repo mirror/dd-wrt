d330be45ee52c52cb9ab6b4d193e95c33540b327
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-07-17 11:38:13 +0000

    carambola changes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22071 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit.c
+++ src/router/services/sysinit/sysinit.c
@@ -1062,6 +1062,16 @@ void start_restore_defaults(void)
 		{"wan_default", "eth1", 0},
 		{0, 0, 0}
 	};
+#elif HAVE_CARAMBOLA
+	struct nvram_tuple generic[] = {
+		{"lan_ifname", "br0", 0},
+		{"lan_ifnames", "eth0 eth1 ath0", 0},
+		{"wan_ifname", "eth1", 0},
+		{"wan_ifname2", "eth1", 0},
+		{"wan_ifnames", "eth1", 0},
+		{"wan_default", "eth1", 0},
+		{0, 0, 0}
+	};
 #elif HAVE_WR703
 	struct nvram_tuple generic[] = {
 		{"lan_ifname", "br0", 0},
