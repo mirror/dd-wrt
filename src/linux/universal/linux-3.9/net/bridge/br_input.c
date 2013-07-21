a562295ff6a7cfbd221e70df5ea36a78fc1fb7ed
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-07-21 12:29:29 +0000

    fix bridging local check, or make it more sane for the compiler
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22098 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.9/net/bridge/br_input.c
+++ src/linux/universal/linux-3.9/net/bridge/br_input.c
@@ -114,8 +114,8 @@ int br_handle_frame_finish(struct sk_buff *skb)
 			skb2 = skb;
 
 		br->dev->stats.multicast++;
-	} else if ((p->flags & BR_ISOLATE_MODE) ||(dst = __br_fdb_get(br, dest, vid)) &&
-			dst->is_local) {
+	} else if ((p->flags & BR_ISOLATE_MODE) || ((dst = __br_fdb_get(br, dest, vid)) &&
+			dst->is_local)) {
 		skb2 = skb;
 		/* Do not forward the packet since it's local. */
 		skb = NULL;
