069943599bb155550542958d684549be92b2be30
kong <kong@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-07-23 18:52:51 +0000

    monitor temperature and wireless clients on both wl0 and wl1
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22118 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/zabbix.mk
+++ src/router/rules/zabbix.mk
@@ -1,9 +1,9 @@
 
 zabbix:
 	CC="$(ARCH)-linux-uclibc-gcc" \
-	CFLAGS="$(COPTS) -Os -I$(TOP)/zlib -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	CPPFLAGS="$(COPTS) -Os -I$(TOP)/zlib -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	LDFLAGS="$(COPTS) -Os -L$(TOP)/zlib -lz -fPIC" \
+	CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	LDFLAGS="$(COPTS) $(MIPS16_OPT) -L$(TOP)/zlib -lz -fPIC" \
 	$(MAKE) -C zabbix
 
 zabbix-install:
@@ -12,8 +12,8 @@ zabbix-install:
 	install -D zabbix/config/zabbix.webservices $(INSTALLDIR)/zabbix/etc/config/zabbix.webservices
 	install -D zabbix/scripts/wclients $(INSTALLDIR)/zabbix/usr/sbin/wclients
 	install -D zabbix/scripts/topcpu $(INSTALLDIR)/zabbix/usr/sbin/topcpu
+	install -D zabbix/scripts/temps $(INSTALLDIR)/zabbix/usr/sbin/temps
 	install -D zabbix/src/zabbix_agent/zabbix_agentd $(INSTALLDIR)/zabbix/usr/sbin/zabbix_agentd
-#	install -D zabbix/src/zabbix_agent/zabbix_agent $(INSTALLDIR)/zabbix/usr/sbin/zabbix_agent
 	$(STRIP) $(INSTALLDIR)/zabbix/usr/sbin/zabbix_agentd
 
 zabbix-clean:
@@ -23,4 +23,4 @@ zabbix-configure:
 	cd zabbix && ./configure ac_cv_host=$(ARCH)-uclibc-linux --target=$(ARCH)-linux --host=$(ARCH) CC=$(ARCH)-linux-uclibc-gcc \
 	--disable-server --disable-proxy --disable-java --enable-agent \
 	-with-iconv-include=$(TOP)/glib20/libiconv/include --with-iconv-lib=$(TOP)/glib20/libiconv/lib/.libs \
-	CFLAGS="-Os -DOLD_LIBC_MODE -ffunction-sections -fdata-sections -Wl,--gc-section"
+	CFLAGS="$(COPTS) $(MIPS16_OPT) -DOLD_LIBC_MODE -ffunction-sections -fdata-sections -Wl,--gc-section"
