1883d7b8920d89652929af17acf681aeaad4065c
kong <USER@DOMAIN>
2013-07-31 20:01:31 +0000

    update make rules and fix compile error in client.c
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22139 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/bird.mk
+++ src/router/rules/bird.mk
@@ -1,57 +1,4 @@
-bird-symlinks:
-	$(MAKE) -C bird/obj .dir-stamp
-	mkdir -p bird/obj/conf bird/obj/lib
-	- (cd bird/obj/conf ; \
-	ln -s ../../conf/cf-lex.l cf-lex.l ; \
-	ln -s ../../conf/conf.c conf.c ; \
-	ln -s ../../conf/conf.h conf.h ; \
-	ln -s ../../sysdep/unix/config.Y config.Y ; \
-	ln -s ../../sysdep/unix/krt.Y krt.Y ; \
-	ln -s ../../sysdep/linux/netlink.Y netlink.Y ; \
-	)
-	- (cd bird/obj/lib ; \
-	ln -s ../../lib/birdlib.h birdlib.h ; \
-	ln -s ../../lib/bitops.c bitops.c ; \
-	ln -s ../../lib/bitops.h bitops.h ; \
-	ln -s ../../lib/checksum.c checksum.c ; \
-	ln -s ../../lib/checksum.h checksum.h ; \
-	ln -s ../../sysdep/unix/endian.h endian.h ; \
-	ln -s ../../lib/event.c event.c ; \
-	ln -s ../../lib/event.h event.h ; \
-	ln -s ../../sysdep/unix/io.c io.c ; \
-	ln -s ../../lib/ip.c ip.c ; \
-	ln -s ../../lib/ip.h ip.h ; \
-	ln -s ../../lib/ipv4.c ipv4.c ; \
-	ln -s ../../lib/ipv4.h ipv4.h ; \
-	ln -s ../../sysdep/unix/krt.c krt.c ; \
-	ln -s ../../sysdep/unix/krt.h krt.h ; \
-	ln -s ../../sysdep/linux/krt-sys.h krt-sys.h ; \
-	ln -s ../../lib/lists.c lists.c ; \
-	ln -s ../../lib/lists.h lists.h ; \
-	ln -s ../../sysdep/unix/log.c log.c ; \
-	ln -s ../../sysdep/unix/main.c main.c ; \
-	ln -s ../../lib/md5.c md5.c ; \
-	ln -s ../../lib/md5.h md5.h ; \
-	ln -s ../../lib/mempool.c mempool.c ; \
-	ln -s ../../sysdep/linux/netlink.c netlink.c ; \
-	ln -s ../../lib/patmatch.c patmatch.c ; \
-	ln -s ../../lib/printf.c printf.c ; \
-	ln -s ../../sysdep/unix/random.c random.c ; \
-	ln -s ../../lib/resource.c resource.c ; \
-	ln -s ../../lib/resource.h resource.h ; \
-	ln -s ../../lib/slab.c slab.c ; \
-	ln -s ../../lib/slists.c slists.c ; \
-	ln -s ../../lib/slists.h slists.h ; \
-	ln -s ../../lib/socket.h socket.h ; \
-	ln -s ../../lib/string.h string.h ; \
-	ln -s ../../sysdep/linux/sysio.h sysio.h ; \
-	ln -s ../../sysdep/unix/timer.h timer.h ; \
-	ln -s ../../lib/unaligned.h unaligned.h ; \
-	ln -s ../../sysdep/unix/unix.h unix.h ; \
-	ln -s ../../lib/xmalloc.c xmalloc.c ; \
-	)
-
-bird: bird-symlinks
+bird: 
 	$(MAKE) -C bird
 
 bird-install:
@@ -59,8 +6,8 @@ bird-install:
 	install -D bird/bird $(INSTALLDIR)/bird/usr/sbin/bird
 	$(STRIP) $(INSTALLDIR)/bird/usr/sbin/bird
 
-#bird-configure:
-#	cd bird && ./configure --build=$(ARCH)-linux --host=$(ARCH)-linux --prefix=/usr --disable-client --with-sysconfig=sysdep/cf/linux-22.h --localstatedir=/tmp/bird --sysconfdir=/tmp/bird --no-create --no-recursion CC=$(ARCH)-linux-uclibc-gcc bird_cv_c_endian=big-endian
+bird-configure:
+	cd bird && ./configure --target=$(ARCH)-linux --host=$(ARCH) CC=$(ARCH)-linux-uclibc-gcc --prefix=/usr --disable-client --with-sysconfig=sysdep/cf/linux.h --localstatedir=/tmp/bird --sysconfdir=/tmp/bird
 
 bird-clean:
 	$(MAKE) -C bird clean
