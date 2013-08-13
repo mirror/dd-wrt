e007d21b78a2787ad6a5c76e6539a5f9a10b3481
BrainSlayer <s.gottschall@dd-wrt.com>
2013-08-13 09:33:28 +0000

    fix dma api backport
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22194 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.5/arch/mips/include/asm/dma-mapping.h
+++ src/linux/universal/linux-3.5/arch/mips/include/asm/dma-mapping.h
@@ -350,21 +350,6 @@ int
 dma_common_get_sgtable(struct device *dev, struct sg_table *sgt,
 		       void *cpu_addr, dma_addr_t dma_addr, size_t size);
 
-static inline int
-dma_get_sgtable_attrs(struct device *dev, struct sg_table *sgt, void *cpu_addr,
-		      dma_addr_t dma_addr, size_t size, struct dma_attrs *attrs)
-{
-	struct dma_map_ops *ops = get_dma_ops(dev);
-	BUG_ON(!ops);
-	if (ops && ops->get_sgtable)
-		return ops->get_sgtable(dev, sgt, cpu_addr, dma_addr, size,
-					attrs);
-	return dma_common_get_sgtable(dev, sgt, cpu_addr, dma_addr, size);
-}
-
-#define dma_get_sgtable(d, t, v, h, s) dma_get_sgtable_attrs(d, t, v, h, s, NULL)
-
-
 static inline int dma_supported(struct device *dev, u64 mask)
 {
 	struct dma_map_ops *ops = get_dma_ops(dev);
