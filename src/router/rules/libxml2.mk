4b82e6d8071ce45f8c84ab54f9d0beb1b4bceb01
kong <USER@DOMAIN>
2013-09-02 20:18:59 +0000

    add mk files for libxml lighttpd
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22255 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rules/libxml2.mk
@@ -0,0 +1,21 @@
+
+libxml2:
+	CC="$(ARCH)-linux-uclibc-gcc" \
+	CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	LDFLAGS="$(COPTS) $(MIPS16_OPT) -L$(TOP)/zlib -lz -fPIC" \
+	$(MAKE) -C libxml2
+
+libxml2-install:
+	mkdir -p $(INSTALLDIR)/lighttpd/usr/lib ; true
+	cp -av libxml2/.libs/libxml2.so.* $(INSTALLDIR)/lighttpd/usr/lib/ ; true
+	
+#	$(STRIP) $(INSTALLDIR)/zabbix/usr/sbin/zabbix_agentd
+
+libxml2-clean:
+	$(MAKE) -C libxml2 clean
+
+libxml2-configure:
+	cd libxml2 && ./configure ac_cv_host=$(ARCH)-uclibc-linux --target=$(ARCH)-linux --host=$(ARCH) CC=$(ARCH)-linux-uclibc-gcc \
+	--without-python \
+	CFLAGS="$(COPTS) $(MIPS16_OPT) -DOLD_LIBC_MODE -ffunction-sections -fdata-sections -Wl,--gc-section"
