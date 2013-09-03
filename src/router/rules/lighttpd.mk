1309a03a6d8435d541919775e32dc14f08c39911
BrainSlayer <s.gottschall@dd-wrt.com>
2013-09-03 15:58:26 +0000

    fix lighttpd
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22264 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/lighttpd.mk
+++ src/router/rules/lighttpd.mk
@@ -1,31 +1,9 @@
-
-lighttpd:
-	CC="$(ARCH)-linux-uclibc-gcc" \
-	CFLAGS="$(COPTS) $(MIPS16_OPT)   -I$(TOP)/zlib -I$(TOP)/openssl/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib -I$(TOP)/openssl/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	LDFLAGS="$(COPTS) $(MIPS16_OPT)  -L$(TOP)/zlib -ffunction-sections -fdata-sections -Wl,--gc-sections -lz -fPIC" \
-	$(MAKE) -C lighttpd
-
-lighttpd-install:
-	install -D lighttpd/src/lighttpd $(INSTALLDIR)/lighttpd/usr/sbin/lighttpd
-	mkdir -p $(INSTALLDIR)/lighttpd/usr/lib/lighttpd ; true
-	mkdir -p $(INSTALLDIR)/lighttpd/etc/lighttpd ; true
-	cp -av lighttpd/src/.libs/*.so $(INSTALLDIR)/lighttpd/usr/lib/lighttpd/ ; true
-	cp -av $(TOP)/pcre/.libs/libpcre.so.* $(INSTALLDIR)/lighttpd/usr/lib/ ; true
-#	install -D lighttpd/configs/lighttpd.nvramconfig $(INSTALLDIR)/lighttpd/etc/config/lighttpd.nvramconfig
-	install -D lighttpd/configs/lighttpd.conf $(INSTALLDIR)/lighttpd/etc/lighttpd.conf
-#	cp -rf privoxy/templates $(INSTALLDIR)/privoxy/etc/privoxy/
-#	$(STRIP) $(INSTALLDIR)/privoxy/usr/sbin/privoxy
-
-lighttpd-clean:
-	$(MAKE) -C lighttpd
-
-	
-LIGHTTPD_CONFIGURE_ARGS+= \
+CONFIGURE_ARGS+= \
         --libdir=/usr/lib/lighttpd \
         --sysconfdir=/tmp/lighttpd \
         --enable-shared \
         --enable-static \
+        --disable-rpath \
         --without-attr \
         --without-bzip2 \
         --without-fam \
@@ -34,12 +12,24 @@ LIGHTTPD_CONFIGURE_ARGS+= \
         --without-lua \
         --without-memcache \
         --without-mysql \
+	--with-libpcre-includes="$(TOP)/pcre" \
+	--with-libpcre_libraries="$(TOP)/pcre/.libs" \
         --with-openssl="$(TOP)/openssl" \
-        --with-openssl-includes="$(TOP)/openssl/include" \
-        --with-openssl-libs="$(TOP)/openssl" \
+	--with-openssl-includes="$(TOP)/openssl/include" \
+	--with-openssl-libs="$(TOP)/openssl" \
         --with-pcre \
-        --without-valgrind \
-        --with-zlib
-	
-lighttpd-configure:
-	cd lighttpd && ./configure  $(LIGHTTPD_CONFIGURE_ARGS) --prefix=/usr --sysconfdir=/etc/lighttpd --target=$(ARCH)-linux --host=$(ARCH)-linux-uclibc CC=$(ARCH)-linux-uclibc-gcc CFLAGS="-fPIC -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/pcre -I$(TOP)/zlib" LDFLAGS="$(COPTS) $(MIPS16_OPT) -L$(TOP)/pcre/.libs -lpthread $(LDFLAGS) -L$(TOP)/zlib -lz -fPIC" 
+        --without-valgrind 
+
+lighttpd-configure: pcre-configure pcre openssl
+	cd lighttpd && ./configure --host=$(ARCH)-linux $(CONFIGURE_ARGS) CFLAGS="-fPIC -DNEED_PRINTF $(COPTS) -I$(TOP)/pcre -I$(TOP)/zlib" CPPFLAGS="$(COPTS)" LDFLAGS="-L$(TOP)/pcre/.libs -lpthread -lpcre -L$(TOP)/zlib $(LDFLAGS)"
+
+lighttpd: openssl
+	make -C lighttpd 
+
+lighttpd-clean:
+	make -C lighttpd clean
+
+lighttpd-install:
+	install -D lighttpd/src/.libs/lighttpd $(INSTALLDIR)/lighttpd/usr/sbin/lighttpd
+	mkdir -p $(INSTALLDIR)/lighttpd/usr/lib/lighttpd ; true
+	cp -av lighttpd/src/.libs/mod_*.so $(INSTALLDIR)/lighttpd/usr/lib/lighttpd/ ; true
