96c872f063ac70756f61376b4596a22a4641a1d8
BrainSlayer <s.gottschall@dd-wrt.com>
2013-09-09 18:21:37 +0000

    fix build process
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22303 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libxml2.mk
+++ src/router/rules/libxml2.mk
@@ -19,3 +19,8 @@ libxml2-configure:
 	cd libxml2 && ./configure ac_cv_host=$(ARCH)-uclibc-linux --target=$(ARCH)-linux --host=$(ARCH) CC=$(ARCH)-linux-uclibc-gcc \
 	--without-python \
 	CFLAGS="$(COPTS) $(MIPS16_OPT) -DOLD_LIBC_MODE -ffunction-sections -fdata-sections -Wl,--gc-section"
+	CC="$(ARCH)-linux-uclibc-gcc" \
+	CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	LDFLAGS="$(COPTS) $(MIPS16_OPT) -L$(TOP)/zlib -lz -fPIC" \
+	$(MAKE) -C libxml2
