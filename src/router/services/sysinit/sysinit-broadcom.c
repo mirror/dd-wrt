ce40f60b95db8001644a90d6c69fd6b6e3717bbb
BrainSlayer <s.gottschall@dd-wrt.com>
2013-09-12 12:49:49 +0000

    fix boot partition to 0
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22337 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-broadcom.c
+++ src/router/services/sysinit/sysinit-broadcom.c
@@ -297,6 +297,7 @@ static void loadWlModule(void)	// set wled params, get boardflags,
 			MAC_ADD(mac);
 			nvram_set("pci/2/1/macaddr", mac);
 		}
+		nvram_set("bootpartition","0");
 		insmod("wl");	// load module
 		break;
 	case ROUTER_WRT600N:
