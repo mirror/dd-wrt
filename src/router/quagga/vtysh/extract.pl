908f530f81ff4753e6247d3575a12faaa8789ead
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-09-19 12:09:26 +0000

    clean source
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22397 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/quagga/vtysh/extract.pl
+++ src/router/quagga/vtysh/extract.pl
@@ -63,7 +63,7 @@ $ignore{'"show history"'} = "ignore";
 foreach (@ARGV) {
     $file = $_;
 
-    open (FH, "ccache arm-linux-uclibc-gcc -E -DHAVE_CONFIG_H -DVTYSH_EXTRACT_PL -DHAVE_IPV6 -I.. -I./ -I./.. -I../lib -I../lib -I../isisd/topology  $file |");
+    open (FH, "gcc -E -DHAVE_CONFIG_H -DVTYSH_EXTRACT_PL -DHAVE_IPV6 -I.. -I./ -I./.. -I../lib -I../lib -I../isisd/topology  $file |");
     local $/; undef $/;
     $line = <FH>;
     close (FH);
