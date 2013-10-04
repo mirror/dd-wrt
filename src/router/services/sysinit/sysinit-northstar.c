77f854080685b85680e2fbed662528dc22787316
BrainSlayer <s.gottschall@dd-wrt.com>
2013-10-04 08:52:12 +0000

    country settings
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22450 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-northstar.c
+++ src/router/services/sysinit/sysinit-northstar.c
@@ -444,6 +444,79 @@ void start_sysinit(void)
 			nvram_set("pci/1/1/vendid", "0x14E4");
 
 		}
+	case ROUTER_BUFFALO_WZR1750:
+	case ROUTER_BUFFALO_WZR900DHP:
+	case ROUTER_BUFFALO_WZR600DHP2:
+	nvram_default_get("wl_country_code", "US");
+	nvram_default_get("wl0_country_code", "US");
+	nvram_default_get("wl1_country_code", "US");
+	nvram_default_get("wl_country_rev", "0");
+	nvram_default_get("wl0_country_rev", "0");
+	nvram_default_get("wl1_country_rev", "0");
+	#ifdef HAVE_BUFFALO
+	char *cname = nvram_safe_get("region");
+	nvram_set("wl0_country_code","Q1");
+	nvram_set("wl0_country_rev","27");
+	nvram_set("wl1_country_code","Q1");
+	nvram_set("wl1_country_rev","27");
+	if (!strcmp(cname,"JP")) {
+	    nvram_set("wl0_country_code","JP");
+	    nvram_set("wl0_country_rev","45");
+	    nvram_set("wl1_country_code","JP");
+	    nvram_set("wl1_country_rev","45");
+	}
+
+	if (!strcmp(cname,"US")) {
+	    nvram_set("wl0_country_code","Q2");
+	    nvram_set("wl0_country_rev","41");
+	    nvram_set("wl1_country_code","Q2");
+	    nvram_set("wl1_country_rev","41");
+	}
+
+	if (!strcmp(cname,"EU")) {
+	    nvram_set("wl0_country_code","EU");
+	    nvram_set("wl0_country_rev","61");
+	    nvram_set("wl1_country_code","EU");
+	    nvram_set("wl1_country_rev","61");
+	}
+
+	if (!strcmp(cname,"AP")) {
+	    nvram_set("wl0_country_code","CN");
+	    nvram_set("wl0_country_rev","34");
+	    nvram_set("wl1_country_code","Q2");
+	    nvram_set("wl1_country_rev","41");
+	}
+
+	if (!strcmp(cname,"KR")) {
+	    nvram_set("wl0_country_code","KR");
+	    nvram_set("wl0_country_rev","55");
+	    nvram_set("wl1_country_code","Q2");
+	    nvram_set("wl1_country_rev","41");
+	}
+
+	if (!strcmp(cname,"CH")) {
+	    nvram_set("wl0_country_code","CH");
+	    nvram_set("wl0_country_rev","34");
+	    nvram_set("wl1_country_code","Q2");
+	    nvram_set("wl1_country_rev","41");
+	}
+
+	if (!strcmp(cname,"TW")) {
+	    nvram_set("wl0_country_code","TW");
+	    nvram_set("wl0_country_rev","34");
+	    nvram_set("wl1_country_code","Q2");
+	    nvram_set("wl1_country_rev","41");
+	}
+
+	if (!strcmp(cname,"RU")) {
+	    nvram_set("wl0_country_code","RU");
+	    nvram_set("wl0_country_rev","37");
+	    nvram_set("wl1_country_code","Q2");
+	    nvram_set("wl1_country_rev","41");
+	}	
+	#endif
+	break;
+	
 	default:
 		nvram_set("bootpartition","0");
 		nvram_default_get("wl_country_code", "US");
