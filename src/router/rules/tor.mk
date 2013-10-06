c04ee59266c5332d1f4e7b7869a8014a4ebe5472
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-10-06 17:32:16 +0000

    tor config
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22464 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/tor.mk
+++ src/router/rules/tor.mk
@@ -1,8 +1,29 @@
-tor-configure:
-	cd tor && ./configure --host=$(ARCH)-linux-uclibc
+miniupnpc:
+	make -C miniupnpc OS="Linux" OS_STRING="DD-WRT" PREFIX="/usr" CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -fPIC"
+
+miniupnpc-clean:
+	make -C miniupnpc clean
+
+miniupnpc-configure:
+
+
+miniupnpc-install:
+
+
+tor-configure: libevent miniupnpc
+	cd tor && ./configure  --prefix=/usr ac_cv_host=$(ARCH)-uclibc-linux --target=$(ARCH)-linux --with-libminiupnpc-dir=$(TOP)/libminiupnpc --host=$(ARCH) CC="ccache $(ARCH)-linux-uclibc-gcc" \
+	--enable-upnp \
+	--disable-asciidoc \
+	--disable-gcc-hardening \
+	CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib -I$(TOP) -I$(TOP)/miniupnpc  -I$(TOP)/openssl/include -I$(TOP)/libevent -I$(TOP)/libevent/include" \
+	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib -I$(TOP) -I$(TOP)/miniupnpc -I$(TOP)/openssl/include -I$(TOP)/libevent  -I$(TOP)/libevent/include" \
+	LDFLAGS="$(COPTS) $(MIPS16_OPT)  -L$(TOP)/zlib   -L$(TOP)/openssl -L$(TOP)/libevent/.libs -L$(TOP)/miniupnpc" 
 
 tor:
+	make -C tor
 
 tor-clean:
+	make -C tor clean
 
 tor-install:
+	make -C tor DESTDIR=$(INSTALLDIR)/tor install
