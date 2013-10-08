417ac004f6101cc427ce21e7a95159b6ebf5e230
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-10-08 15:00:24 +0000

    format
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22488 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/tor.c
+++ src/router/services/services/tor.c
@@ -43,7 +43,7 @@ void start_tor(void)
 {
 	int ret;
 	pid_t pid;
-	char *tor_argv[] = { "tor","--defaults-torrc", "/tmp/torrc",
+	char *tor_argv[] = { "tor", "--defaults-torrc", "/tmp/torrc",
 		NULL
 	};
 
@@ -51,38 +51,37 @@ void start_tor(void)
 
 	if (nvram_match("tor_enable", "0"))
 		return;
-	
-	FILE *fp=fopen("/tmp/torrc","wb");
-	fprintf(fp,"Log notice syslog\n");
-	if (nvram_match("tor_relayonly","1"))
-	    fprintf(fp,"SocksPort 0\n");
+
+	FILE *fp = fopen("/tmp/torrc", "wb");
+	fprintf(fp, "Log notice syslog\n");
+	if (nvram_match("tor_relayonly", "1"))
+		fprintf(fp, "SocksPort 0\n");
 	else {
-	    fprintf(fp,"SocksPort 9050\n");
-	    fprintf(fp,"SocksPort %s:9050\n",nvram_safe_get("lan_ipaddr"));
+		fprintf(fp, "SocksPort 9050\n");
+		fprintf(fp, "SocksPort %s:9050\n", nvram_safe_get("lan_ipaddr"));
 	}
-	fprintf(fp,"RunAsDaemon 1\n");
-//	fprintf(fp,"ControlPort 9051\n");
-	if (nvram_match("tor_relay","1"));
-	    fprintf(fp,"ORPort 9001\n");
-	if (nvram_match("tor_dir","1"))
-	    fprintf(fp,"DirPort 9030\n");
-	if (nvram_match("tor_bridge","1"))
-	    fprintf(fp,"BridgeRelay 1\n");
-	if (nvram_match("tor_transparent","1")) {
-	    fprintf(fp,"VirtualAddrNetwork 10.192.0.0/10\n");
-	    fprintf(fp,"AutomapHostsOnResolve 1\n");
-	    fprintf(fp,"TransPort 9040\n");
-	    fprintf(fp,"DNSPort 53\n");
-	    fprintf(fp,"TransListenAddress %s\n",nvram_safe_get("lan_ipaddr"));
-	    fprintf(fp,"DNSListenAddress %s\n",nvram_safe_get("lan_ipaddr"));
-	    sysprintf("iptables -t nat -A PREROUTING -i br0 -p udp --dport 53 -j REDIRECT --to-ports 53");
-	    sysprintf("iptables -t nat -A PREROUTING -i br0 -p tcp --syn -j REDIRECT --to-ports 9040");
+	fprintf(fp, "RunAsDaemon 1\n");
+//      fprintf(fp,"ControlPort 9051\n");
+	if (nvram_match("tor_relay", "1")) ;
+	fprintf(fp, "ORPort 9001\n");
+	if (nvram_match("tor_dir", "1"))
+		fprintf(fp, "DirPort 9030\n");
+	if (nvram_match("tor_bridge", "1"))
+		fprintf(fp, "BridgeRelay 1\n");
+	if (nvram_match("tor_transparent", "1")) {
+		fprintf(fp, "VirtualAddrNetwork 10.192.0.0/10\n");
+		fprintf(fp, "AutomapHostsOnResolve 1\n");
+		fprintf(fp, "TransPort 9040\n");
+		fprintf(fp, "DNSPort 53\n");
+		fprintf(fp, "TransListenAddress %s\n", nvram_safe_get("lan_ipaddr"));
+		fprintf(fp, "DNSListenAddress %s\n", nvram_safe_get("lan_ipaddr"));
+		sysprintf("iptables -t nat -A PREROUTING -i br0 -p udp --dport 53 -j REDIRECT --to-ports 53");
+		sysprintf("iptables -t nat -A PREROUTING -i br0 -p tcp --syn -j REDIRECT --to-ports 9040");
 
 	}
-	    
-	fclose(fp);    	
+
+	fclose(fp);
 	ret = _evalpid(tor_argv, NULL, 0, &pid);
-
 
 }
 
