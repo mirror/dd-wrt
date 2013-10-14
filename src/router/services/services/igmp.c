5678ebf484b4d9376e495436b0234552a69ec671
markus <markus@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-10-14 12:28:38 +0000

    some iptv related changes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22528 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/igmp.c
+++ src/router/services/services/igmp.c
@@ -33,19 +33,22 @@ void start_igmp_proxy(void)
 {
 	int ret = 0;
 	pid_t pid;
-	char name[80], *next, *svbuf;
+	char name[80], *next, *svbuf, *tvnic;
 	char *argv[] = { "igmprt", "/tmp/igmpproxy.conf", NULL };
 
 	int ifcount = 0;
+	
+	if (!nvram_match("igmp_enable", "1")) 
+		return;
 
 	FILE *fp = fopen("/tmp/igmpproxy.conf", "wb");
 
-	if (nvram_match("dtag_vlan8", "1") && nvram_match("wan_vdsl", "1")) {
+	if (strcmp(nvram_safe_get("tvnicfrom"), "")) {
 		fprintf(fp, "quickleave\nphyint %s upstream  ratelimit 0  threshold 1\n", nvram_safe_get("tvnicfrom"));
 		fprintf(fp, "phyint %s disabled\n", get_wan_face());
-	} else {
+	} else
 		fprintf(fp, "quickleave\nphyint %s upstream  ratelimit 0  threshold 1\n", get_wan_face());
-	}
+
 	if (nvram_match("block_multicast", "0")) {
 		fprintf(fp, "phyint %s downstream  ratelimit 0  threshold 1\n", nvram_safe_get("lan_ifname"));
 		ifcount++;
