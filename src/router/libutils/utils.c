3dfa8ae2e69171f23af8c671107baae3d84fa87f
BrainSlayer <s.gottschall@dd-wrt.com>
2013-10-21 14:28:09 +0000

    fix wrong gpio
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22616 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/utils.c
+++ src/router/libutils/utils.c
@@ -4824,7 +4824,7 @@ int led_control(int type, int act)
 		break;
 	case ROUTER_BUFFALO_WZR900DHP:
 	case ROUTER_BUFFALO_WZR600DHP2:
-		usb_power = 0x009;	// USB 2.0 ehci port
+//		usb_power = 0x009;	// USB 2.0 ehci port
 		usb_power1 = 0x10a;	// USB 3.0 xhci port
 //              wlan0_gpio = 0x028; // wireless orange
 //              wlan1_gpio = 0x029; // wireless blue
