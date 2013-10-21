9e5690b29728bfc7a7e8e2f159ea6150f171a7c2
BrainSlayer <s.gottschall@dd-wrt.com>
2013-10-21 12:12:48 +0000

    wrong location
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22609 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-northstar.c
+++ src/router/services/sysinit/sysinit-northstar.c
@@ -363,14 +363,6 @@ void start_sysinit(void)
 		}
 		break;
 	case ROUTER_NETGEAR_R7000:
-		nvram_set("vlan1hwname", "et0");
-		nvram_set("vlan2hwname", "et0");
-		nvram_set("vlan0ports", "");
-		nvram_set("vlan0hwname", "");
-		nvram_set("vlan1ports", "4 3 2 1 5*");
-		nvram_set("vlan2ports", "0 5u");
-		nvram_set("lan_ifnames", "vlan1 eth1 eth2");
-		nvram_set("wan_ifname", "vlan2");
 
 		if (nvram_get("pci/1/1/vendid") == NULL) {
 			if (!sv_valid_hwaddr(nvram_safe_get("pci/1/1/macaddr"))
