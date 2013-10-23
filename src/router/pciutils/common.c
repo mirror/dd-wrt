e3581a9e3bf13ccabce15eaee29dbb43b70aa22e
BrainSlayer <s.gottschall@dd-wrt.com>
2013-10-23 21:18:12 +0000

    Update pciutils
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22628 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/pciutils/common.c
+++ src/router/pciutils/common.c
@@ -1,9 +1,7 @@
 /*
- *	$Id: common.c,v 1.2 2002/03/30 15:39:24 mj Exp $
+ *	The PCI Utilities -- Common Functions
  *
- *	Linux PCI Utilities -- Common Functions
- *
- *	Copyright (c) 1997--1999 Martin Mares <mj@ucw.cz>
+ *	Copyright (c) 1997--2008 Martin Mares <mj@ucw.cz>
  *
  *	Can be freely distributed and used under the terms of the GNU GPL.
  */
@@ -16,13 +14,13 @@
 
 #include "pciutils.h"
 
-void __attribute__((noreturn))
+void NONRET
 die(char *msg, ...)
 {
   va_list args;
 
   va_start(args, msg);
-  fputs("lspci: ", stderr);
+  fprintf(stderr, "%s: ", program_name);
   vfprintf(stderr, msg, args);
   fputc('\n', stderr);
   exit(1);
@@ -37,18 +35,75 @@ xmalloc(unsigned int howmuch)
   return p;
 }
 
+void *
+xrealloc(void *ptr, unsigned int howmuch)
+{
+  void *p = realloc(ptr, howmuch);
+  if (!p)
+    die("Unable to allocate %d bytes of memory", howmuch);
+  return p;
+}
+
+char *
+xstrdup(char *str)
+{
+  int len = strlen(str) + 1;
+  char *copy = xmalloc(len);
+  memcpy(copy, str, len);
+  return copy;
+}
+
+static void
+set_pci_method(struct pci_access *pacc, char *arg)
+{
+  char *name;
+  int i;
+
+  if (!strcmp(arg, "help"))
+    {
+      printf("Known PCI access methods:\n\n");
+      for (i=0; name = pci_get_method_name(i); i++)
+	if (name[0])
+	  printf("%s\n", name);
+      exit(0);
+    }
+  else
+    {
+      i = pci_lookup_method(arg);
+      if (i < 0)
+	die("No such PCI access method: %s (see `-A help' for a list)", arg);
+      pacc->method = i;
+    }
+}
+
+static void
+set_pci_option(struct pci_access *pacc, char *arg)
+{
+  if (!strcmp(arg, "help"))
+    {
+      struct pci_param *p;
+      printf("Known PCI access parameters:\n\n");
+      for (p=NULL; p=pci_walk_params(pacc, p);)
+	printf("%-20s %s (%s)\n", p->param, p->help, p->value);
+      exit(0);
+    }
+  else
+    {
+      char *sep = strchr(arg, '=');
+      if (!sep)
+	die("Invalid PCI access parameter syntax: %s", arg);
+      *sep++ = 0;
+      if (pci_set_param(pacc, arg, sep) < 0)
+	die("Unrecognized PCI access parameter: %s (see `-O help' for a list)", arg);
+    }
+}
+
 int
 parse_generic_option(int i, struct pci_access *pacc, char *optarg)
 {
   switch (i)
     {
-#ifdef HAVE_PM_LINUX_PROC
-    case 'P':
-      pacc->method_params[PCI_ACCESS_PROC_BUS_PCI] = optarg;
-      pacc->method = PCI_ACCESS_PROC_BUS_PCI;
-      break;
-#endif
-#ifdef HAVE_PM_INTEL_CONF
+#ifdef PCI_HAVE_PM_INTEL_CONF
     case 'H':
       if (!strcmp(optarg, "1"))
 	pacc->method = PCI_ACCESS_I386_TYPE1;
@@ -58,19 +113,20 @@ parse_generic_option(int i, struct pci_access *pacc, char *optarg)
 	die("Unknown hardware configuration type %s", optarg);
       break;
 #endif
-#ifdef HAVE_PM_SYSCALLS
-    case 'S':
-      pacc->method = PCI_ACCESS_SYSCALLS;
-      break;
-#endif
-#ifdef HAVE_PM_DUMP
+#ifdef PCI_HAVE_PM_DUMP
     case 'F':
-      pacc->method_params[PCI_ACCESS_DUMP] = optarg;
+      pci_set_param(pacc, "dump.name", optarg);
       pacc->method = PCI_ACCESS_DUMP;
       break;
 #endif
+    case 'A':
+      set_pci_method(pacc, optarg);
+      break;
     case 'G':
       pacc->debugging++;
+      break;
+    case 'O':
+      set_pci_option(pacc, optarg);
       break;
     default:
       return 0;
--- src/router/pciutils/lib/access.c
+++ src/router/pciutils/lib/access.c
@@ -1,9 +1,7 @@
 /*
- *	$Id: access.c,v 1.8 2002/03/30 15:39:25 mj Exp $
- *
  *	The PCI Library -- User Access
  *
- *	Copyright (c) 1997--1999 Martin Mares <mj@ucw.cz>
+ *	Copyright (c) 1997--2008 Martin Mares <mj@ucw.cz>
  *
  *	Can be freely distributed and used under the terms of the GNU GPL.
  */
@@ -14,168 +12,6 @@
 #include <string.h>
 
 #include "internal.h"
-
-static struct pci_methods *pci_methods[PCI_ACCESS_MAX] = {
-  NULL,
-#ifdef HAVE_PM_LINUX_PROC
-  &pm_linux_proc,
-#else
-  NULL,
-#endif
-#ifdef HAVE_PM_SYSCALLS
-  &pm_syscalls,
-#else
-  NULL,
-#endif
-#ifdef HAVE_PM_INTEL_CONF
-  &pm_intel_conf1,
-  &pm_intel_conf2,
-#else
-  NULL,
-  NULL,
-#endif
-#ifdef HAVE_PM_FBSD_DEVICE
-  &pm_fbsd_device,
-#else
-  NULL,
-#endif
-#ifdef HAVE_PM_AIX_DEVICE
-  &pm_aix_device,
-#else
-  NULL,
-#endif
-#ifdef HAVE_PM_DUMP
-  &pm_dump,
-#else
-  NULL,
-#endif
-};
-
-struct pci_access *
-pci_alloc(void)
-{
-  struct pci_access *a = malloc(sizeof(struct pci_access));
-  int i;
-
-  bzero(a, sizeof(*a));
-  a->id_file_name = PATH_PCI_IDS;
-  for(i=0; i<PCI_ACCESS_MAX; i++)
-    if (pci_methods[i] && pci_methods[i]->config)
-      pci_methods[i]->config(a);
-  return a;
-}
-
-void *
-pci_malloc(struct pci_access *a, int size)
-{
-  void *x = malloc(size);
-
-  if (!x)
-    a->error("Out of memory (allocation of %d bytes failed)", size);
-  return x;
-}
-
-void
-pci_mfree(void *x)
-{
-  if (x)
-    free(x);
-}
-
-static void
-pci_generic_error(char *msg, ...)
-{
-  va_list args;
-
-  va_start(args, msg);
-  fputs("pcilib: ", stderr);
-  vfprintf(stderr, msg, args);
-  fputc('\n', stderr);
-  exit(1);
-}
-
-static void
-pci_generic_warn(char *msg, ...)
-{
-  va_list args;
-
-  va_start(args, msg);
-  fputs("pcilib: ", stderr);
-  vfprintf(stderr, msg, args);
-  fputc('\n', stderr);
-}
-
-static void
-pci_generic_debug(char *msg, ...)
-{
-  va_list args;
-
-  va_start(args, msg);
-  vfprintf(stdout, msg, args);
-  va_end(args);
-}
-
-static void
-pci_null_debug(char * UNUSED msg, ...)
-{
-}
-
-void
-pci_init(struct pci_access *a)
-{
-  if (!a->error)
-    a->error = pci_generic_error;
-  if (!a->warning)
-    a->warning = pci_generic_warn;
-  if (!a->debug)
-    a->debug = pci_generic_debug;
-  if (!a->debugging)
-    a->debug = pci_null_debug;
-
-  if (a->method)
-    {
-      if (a->method >= PCI_ACCESS_MAX || !pci_methods[a->method])
-	a->error("This access method is not supported.");
-      a->methods = pci_methods[a->method];
-    }
-  else
-    {
-      unsigned int i;
-      for(i=0; i<PCI_ACCESS_MAX; i++)
-	if (pci_methods[i])
-	  {
-	    a->debug("Trying method %d...", i);
-	    if (pci_methods[i]->detect(a))
-	      {
-		a->debug("...OK\n");
-		a->methods = pci_methods[i];
-		a->method = i;
-		break;
-	      }
-	    a->debug("...No.\n");
-	  }
-      if (!a->methods)
-	a->error("Cannot find any working access method.");
-    }
-  a->debug("Decided to use %s\n", a->methods->name);
-  a->methods->init(a);
-}
-
-void
-pci_cleanup(struct pci_access *a)
-{
-  struct pci_dev *d, *e;
-
-  for(d=a->devices; d; d=e)
-    {
-      e = d->next;
-      pci_free_dev(d);
-    }
-  if (a->methods)
-    a->methods->cleanup(a);
-  pci_free_name_list(a);
-  pci_mfree(a);
-}
 
 void
 pci_scan_bus(struct pci_access *a)
@@ -188,9 +24,10 @@ pci_alloc_dev(struct pci_access *a)
 {
   struct pci_dev *d = pci_malloc(a, sizeof(struct pci_dev));
 
-  bzero(d, sizeof(*d));
+  memset(d, 0, sizeof(*d));
   d->access = a;
   d->methods = a->methods;
+  d->hdrtype = -1;
   if (d->methods->init_dev)
     d->methods->init_dev(d);
   return d;
@@ -206,10 +43,11 @@ pci_link_dev(struct pci_access *a, struct pci_dev *d)
 }
 
 struct pci_dev *
-pci_get_dev(struct pci_access *a, int bus, int dev, int func)
+pci_get_dev(struct pci_access *a, int domain, int bus, int dev, int func)
 {
   struct pci_dev *d = pci_alloc_dev(a);
 
+  d->domain = domain;
   d->bus = bus;
   d->dev = dev;
   d->func = func;
@@ -220,6 +58,8 @@ void pci_free_dev(struct pci_dev *d)
 {
   if (d->methods->cleanup_dev)
     d->methods->cleanup_dev(d);
+  pci_free_caps(d);
+  pci_mfree(d->phy_slot);
   pci_mfree(d);
 }
 
@@ -264,6 +104,12 @@ pci_read_block(struct pci_dev *d, int pos, byte *buf, int len)
   return d->methods->read(d, pos, buf, len);
 }
 
+int
+pci_read_vpd(struct pci_dev *d, int pos, byte *buf, int len)
+{
+  return d->methods->read_vpd ? d->methods->read_vpd(d, pos, buf, len) : 0;
+}
+
 static inline int
 pci_write_data(struct pci_dev *d, void *buf, int pos, int len)
 {
@@ -306,17 +152,24 @@ pci_write_block(struct pci_dev *d, int pos, byte *buf, int len)
 }
 
 int
-pci_fill_info(struct pci_dev *d, int flags)
+pci_fill_info_v31(struct pci_dev *d, int flags)
 {
   if (flags & PCI_FILL_RESCAN)
     {
       flags &= ~PCI_FILL_RESCAN;
       d->known_fields = 0;
+      pci_free_caps(d);
     }
   if (flags & ~d->known_fields)
     d->known_fields |= d->methods->fill_info(d, flags & ~d->known_fields);
   return d->known_fields;
 }
+
+/* In version 3.1, pci_fill_info got new flags => versioned alias */
+STATIC_ALIAS(int pci_fill_info(struct pci_dev *d, int flags), pci_fill_info_v31(d,flags));
+DEFINE_ALIAS(int pci_fill_info_v30(struct pci_dev *d, int flags), pci_fill_info_v31);
+SYMBOL_VERSION(pci_fill_info_v30, pci_fill_info@LIBPCI_3.0);
+SYMBOL_VERSION(pci_fill_info_v31, pci_fill_info@@LIBPCI_3.1);
 
 void
 pci_setup_cache(struct pci_dev *d, byte *cache, int len)
