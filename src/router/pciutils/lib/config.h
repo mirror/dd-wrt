7a558fba82b67841294121ade4aadcd03551ad33
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-10-23 22:12:24 +0000

    works now
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22640 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/pciutils/lib/config.h
+++ src/router/pciutils/lib/config.h
@@ -1,9 +1,16 @@
-#define ARCH_MIPSEL
-#define OS_LINUX
-#define HAVE_PM_LINUX_PROC
-#define HAVE_LINUX_BYTEORDER_H
-#define PATH_PROC_BUS_PCI "/proc/bus/pci"
-#define HAVE_PM_DUMP
-#define PATH_PCI_IDS "/usr/share/pci.ids"
-#define HAVE_OWN_HEADER_H
-#define PCILIB_VERSION "2.1.10"
+#define PCI_CONFIG_H
+#define PCI_ARCH_MIPSEL
+#define PCI_OS_LINUX
+#define PCI_HAVE_PM_LINUX_SYSFS
+#define PCI_HAVE_PM_LINUX_PROC
+#define PCI_HAVE_LINUX_BYTEORDER_H
+#define PCI_PATH_PROC_BUS_PCI "/proc/bus/pci"
+#define PCI_PATH_SYS_BUS_PCI "/sys/bus/pci"
+#define PCI_HAVE_PM_INTEL_CONF
+#define PCI_HAVE_64BIT_ADDRESS
+#define PCI_HAVE_PM_DUMP
+#define PCI_IDS "pci.ids"
+#define PCI_PATH_IDS_DIR "/usr/share"
+#define PCI_USE_DNS
+#define PCI_ID_DOMAIN "pci.id.ucw.cz"
+#define PCILIB_VERSION "3.2.0"
