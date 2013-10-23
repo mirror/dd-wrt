fc8fd08e8767a1a79fbab79a88d3a3528bebb9b3
BrainSlayer <s.gottschall@dd-wrt.com>
2013-10-23 21:57:40 +0000

    update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22638 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/pciutils/lib/config.h
@@ -0,0 +1,9 @@
+#define ARCH_MIPSEL
+#define OS_LINUX
+#define HAVE_PM_LINUX_PROC
+#define HAVE_LINUX_BYTEORDER_H
+#define PATH_PROC_BUS_PCI "/proc/bus/pci"
+#define HAVE_PM_DUMP
+#define PATH_PCI_IDS "/usr/share/pci.ids"
+#define HAVE_OWN_HEADER_H
+#define PCILIB_VERSION "2.1.10"
