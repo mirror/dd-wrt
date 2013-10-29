5b1d778ca05754869122eed37f9e63ee9af047b4
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-10-29 14:53:08 +0000

    obsolete
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22684 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/mv78xx/linux-2.6.23/drivers/scsi/thor/core/core_xor.h
+++
@@ -1,8 +0,0 @@
-#ifndef CORE_XOR_H
-#define CORE_XOR_H
-
-void Core_ModuleSendXORRequest(MV_PVOID This, PMV_XOR_Request pXORReq);
-
-#endif
-
-
--- src/linux/mv78xx/linux-2.6.23/include/asm-arm/siginfo.h
+++
@@ -1,7 +0,0 @@
-#ifndef _ASMARM_SIGINFO_H
-#define _ASMARM_SIGINFO_H
-
-#include <asm-generic/siginfo.h>
-#define	FPE_FLTISN	(__SI_FAULT|9)	/* Input Subnormal (VFPv2) */
-
-#endif
