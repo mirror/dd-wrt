d740c7ab0d8c171ba2073606379027f065387695
BrainSlayer <s.gottschall@dd-wrt.com>
2013-11-03 11:48:10 +0000

    update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22719 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.5/include/linux/usb_usual.h
+++ src/linux/universal/linux-3.5/include/linux/usb_usual.h
@@ -64,7 +64,9 @@
 	US_FLAG(NO_READ_CAPACITY_16,	0x00080000)		\
 		/* cannot handle READ_CAPACITY_16 */		\
 	US_FLAG(INITIAL_READ10,	0x00100000)			\
-		/* Initial READ(10) (and others) must be retried */
+		/* Initial READ(10) (and others) must be retried */	\
+	US_FLAG(NEEDS_CAP16,	0x00400000)
+		/* cannot handle READ_CAPACITY_10 */
 
 #define US_FLAG(name, value)	US_FL_##name = value ,
 enum { US_DO_ALL_FLAGS };
