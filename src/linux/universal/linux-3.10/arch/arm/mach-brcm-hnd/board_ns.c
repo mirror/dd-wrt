d4075d72708ebbd62aeaa5cf2f5a79638af50628
kong <USER@DOMAIN>
2013-11-06 15:29:23 +0000

    move to another position
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22760 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.10/arch/arm/mach-brcm-hnd/board_ns.c
+++ src/linux/universal/linux-3.10/arch/arm/mach-brcm-hnd/board_ns.c
@@ -833,10 +833,10 @@ struct mtd_partition *init_nflash_mtd_partitions(hndnand_t * nfl, struct mtd_inf
 			else
 				bcm947xx_nflash_parts[nparts].size = nparts ? (NFL_BOOT_OS_SIZE - NFL_BOOT_SIZE) : NFL_BOOT_OS_SIZE;
 		}
-		/* prevent overwriting board_data Netgear R6300V2*/
+		/* fix linux offset for this unit */
 		if (nvram_match("boardnum","679") && nvram_match("boardtype", "0x0646") && (nvram_match("boardrev", "0x1110"))) {
-			offset += 0x20000;
-			bcm947xx_nflash_parts[nparts].size -= 0x20000;
+			offset += 0x180000;
+			bcm947xx_nflash_parts[nparts].size -= 0x180000;
 		}
 		
 		bcm947xx_nflash_parts[nparts].offset = offset;
