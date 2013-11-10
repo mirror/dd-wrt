1a1ce4df985a66060efd2ab788dda5d9278725c8
BrainSlayer <s.gottschall@dd-wrt.com>
2013-11-10 16:34:19 +0000

    clock patch from asus
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22796 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-northstar.c
+++ src/router/services/sysinit/sysinit-northstar.c
@@ -945,6 +945,8 @@ void start_sysinit(void)
 		
 		break;
 	case ROUTER_ASUS_AC67U:
+		if (!nvram_match("bl_version", "1.0.1.1"))
+			nvram_set("clkfreq", "800,666");
 		if (nvram_get("productid") != NULL || nvram_match("http_username", "admin")) {
 			int deadcount = 10;
 			while (deadcount--) {
@@ -1006,6 +1008,7 @@ void start_sysinit(void)
 		nvram_set("1:mcsbw1605ghpo", "0");
 		break;
 	case ROUTER_ASUS_AC56U:
+		nvram_set("clkfreq", "800,666");
 		if (nvram_get("productid") != NULL || nvram_match("http_username", "admin")) {
 			int deadcount = 10;
 			while (deadcount--) {
@@ -1379,6 +1382,7 @@ void start_sysinit(void)
 			writeproc("/proc/irq/163/smp_affinity", "2");
 			writeproc("/proc/irq/169/smp_affinity", "2");
 		}
+		writeproc("/proc/irq/111/smp_affinity", "2");
 		writeproc("/proc/irq/112/smp_affinity", "2");
 	}
 
