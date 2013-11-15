f375ec3d199d223b4333ff67a3140cc05df09177
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-11-15 12:00:04 +0000

    fix wrong flags
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22850 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.12/drivers/mtd/mtdpart.c
+++ src/linux/universal/linux-3.12/drivers/mtd/mtdpart.c
@@ -759,6 +759,7 @@ static int split_rootfs_data(struct mtd_info *master, struct mtd_info *rpart, co
 
 	dpart->size = rpart->size - (split_offset - spart->offset);
 	dpart->offset = split_offset;
+	dpart->mask_flags = 0;
 
 	if (dpart == NULL)
 		return 1;
@@ -888,6 +889,7 @@ int add_mtd_partitions(struct mtd_info *master,
 				    	part.name = "rootfs";
 				    	part.offset = offset;
 				    	part.size = (master->size - nvramsize) - offset; 
+				    	part.mask_flags = 0;
 				    	add_mtd_partitions(master,&part,1);
 					break;
 				    } 
