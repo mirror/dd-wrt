0494eb361fc4f00113e75b67afa439a147d11775
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-11-16 11:13:37 +0000

    fix unit
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22858 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/ejs.c
+++ src/router/httpd/visuals/ejs.c
@@ -2427,7 +2427,7 @@ void ej_get_cputemp(webs_t wp, int argc, char_t ** argv)
 	int TEMP_MUL = 10;
 	FILE *fp = fopen("/sys/bus/i2c/devices/0-0029/temp0_input", "rb");
 #elif HAVE_UNIWIP
-	int TEMP_MUL = 100;
+	int TEMP_MUL = 1000;
 	FILE *fp = fopen("/sys/bus/i2c/devices/0-0049/temp1_input", "rb");
 #elif HAVE_VENTANA
 	int TEMP_MUL = 10;
