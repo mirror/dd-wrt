30cf6b1e29fc09c7f8abc7800abcacf68c50d4e0
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-11-18 15:34:26 +0000

    gpsd
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22880 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/gpsd/setup.py
+++
@@ -1,8 +0,0 @@
-# $Id: setup.py 4076 2006-12-05 02:00:19Z esr $
-# Creates build/lib.linux-${arch}-${pyvers}/gpspacket.so,
-# where ${arch} is an architecture and ${pyvers} is a Python version.
-from distutils.core import setup, Extension
-setup(name="gpspacket", version="1.0",
-      ext_modules=[Extension("gpspacket",
-                             ["gpspacket.c", "packet.c",
-                              "isgps.c", "rtcm.c", "strl.c", "hex.c"])])
