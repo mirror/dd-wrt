161f6e17abbd841aeb2b9e3eae44c8e8108df8b2
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-11-19 11:33:30 +0000

    reset button support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22898 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/resetbutton.c
+++ src/router/rc/resetbutton.c
@@ -130,8 +130,8 @@ int getbuttonstate()
 {
 	int ret = get_gpio(232);
 	if (ret == 1)
-		return 0;;
-	return 0;
+		return 0;
+	return 1;
 }
 #elif defined(HAVE_WDR4900)
 int getbuttonstate()
@@ -800,7 +800,7 @@ void period_check(int sig)
 
 	int state = 0;
 
-#if defined(HAVE_XSCALE) || defined(HAVE_MAGICBOX) || defined(HAVE_FONERA) || defined(HAVE_WHRAG108) || defined(HAVE_GATEWORX) || defined(HAVE_STORM) || defined(HAVE_LS2) || defined(HAVE_CA8) || defined(HAVE_TW6600)  || defined(HAVE_LS5) || defined(HAVE_LSX) || defined(HAVE_WP54G) || defined(HAVE_NP28G) || defined(HAVE_SOLO51) || defined(HAVE_OPENRISC) || defined(HAVE_DANUBE)
+#if defined(HAVE_XSCALE) || defined(HAVE_MAGICBOX) || defined(HAVE_FONERA) || defined(HAVE_WHRAG108) || defined(HAVE_GATEWORX) || defined(HAVE_STORM) || defined(HAVE_LS2) || defined(HAVE_CA8) || defined(HAVE_TW6600)  || defined(HAVE_LS5) || defined(HAVE_LSX) || defined(HAVE_WP54G) || defined(HAVE_NP28G) || defined(HAVE_SOLO51) || defined(HAVE_OPENRISC) || defined(HAVE_DANUBE) || defined(HAVE_UNIWIP)
 	state = val;
 	int sesgpio = 0xfff;
 	int wifigpio = 0xfff;
