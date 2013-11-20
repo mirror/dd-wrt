03f222bb4c5ec08a284cbbb2e1b9523596c1dcac
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-11-20 17:38:37 +0000

    gps math
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22924 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/httpd/visuals/gps.c
@@ -0,0 +1,101 @@
+#ifdef HAVE_GPSI
+
+#define VISUALSOURCE 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <math.h>
+
+#include <broadcom.h>
+#include <cymac.h>
+
+void ej_getlongitude(webs_t wp, int argc, char_t ** argv)
+{
+    char *lon = nvram_safe_get("gps_lon");
+    char lon_deg[4];
+    char lon_min1[3];
+    char lon_min2[32];
+    char lon_min3[32];
+    if (strlen(lon)==0) {
+	websWrite(wp,"N/A");
+	return;
+    }
+    strncpy(lon_deg,lon,3);
+    strncpy(lon_min1,lon+3,2);
+    strcpy(lon_min2,lon+6);
+    sprintf(lon_min3,"0.%s%s",lon_min1,lon_min2);
+    float lon_dec = atof(lon_min3) / .6f;
+    float lon_val = atof(lon_deg) + lon_dec;
+    if (nvram_invmatch("gps_lon_e","E"))
+	lon_val *= -1;
+    websWrite(wp,"%f",lon_val);
+	return;
+}
+
+void ej_getlatidude(webs_t wp, int argc, char_t ** argv)
+{
+    char *lat = nvram_safe_get("gps_lat");
+    char lat_deg[4];
+    char lat_min1[3];
+    char lat_min2[32];
+    char lat_min3[32];
+    if (strlen(lat)==0) {
+	websWrite(wp,"N/A");
+	return;
+    }
+    strncpy(lat_deg,lat,2);
+    strncpy(lat_min1,lat+2,2);
+    strcpy(lat_min2,lat+5);
+    sprintf(lat_min3,"0.%s%s",lat_min1,lat_min2);
+    float lat_dec = atof(lat_min3) / .6f;
+    float lat_val = atof(lat_deg) + lat_dec;
+    if (nvram_invmatch("gps_lat_e","N"))
+	lat_val *= -1;
+
+	websWrite(wp,"%f",lat_val);
+	return;
+}
+void ej_gpslink(webs_t wp, int argc, char_t ** argv)
+{
+    char *lon = nvram_safe_get("gps_lon");
+    char lon_deg[4];
+    char lon_min1[3];
+    char lon_min2[32];
+    char lon_min3[32];
+    if (strlen(lon)==0) {
+	websWrite(wp,"N/A");
+	return;
+    }
+    strncpy(lon_deg,lon,3);
+    strncpy(lon_min1,lon+3,2);
+    strcpy(lon_min2,lon+6);
+    sprintf(lon_min3,"0.%s%s",lon_min1,lon_min2);
+    float lon_dec = atof(lon_min3) / .6f;
+    float lon_val = atof(lon_deg) + lon_dec;
+    if (nvram_invmatch("gps_lon_e","E"))
+	lon_val *= -1;
+
+    char *lat = nvram_safe_get("gps_lat");
+    char lat_deg[4];
+    char lat_min1[3];
+    char lat_min2[32];
+    char lat_min3[32];
+    if (strlen(lat)==0) {
+	websWrite(wp,"N/A");
+	return;
+    }
+    strncpy(lat_deg,lat,2);
+    strncpy(lat_min1,lat+2,2);
+    strcpy(lat_min2,lat+5);
+    sprintf(lat_min3,"0.%s%s",lat_min1,lat_min2);
+    float lat_dec = atof(lat_min3) / .6f;
+    float lat_val = atof(lat_deg) + lat_dec;
+    if (nvram_invmatch("gps_lat_e","N"))
+	lat_val *= -1;
+
+    websWrite(wp,"<a href=\"https://maps.google.com/maps?q=%f,%f\">Google Maps</a>",lat_val,lon_val);
+}
+#endif
