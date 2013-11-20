f17c319718cdcb598dca2634eeee81a2c6add2b3
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2013-11-20 19:19:15 +0000

    reset button for ventana
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@22929 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/resetbutton.c
+++ src/router/rc/resetbutton.c
@@ -125,6 +125,14 @@ int getbuttonstate()
 	return ret;
 #endif
 }
+#elif defined(HAVE_VENTANA)
+int getbuttonstate()
+{
+	int ret = get_gpio(240);
+	if (ret == 1)
+		return 0;
+	return 1;
+}
 #elif defined(HAVE_UNIWIP)
 int getbuttonstate()
 {
@@ -800,7 +808,7 @@ void period_check(int sig)
 
 	int state = 0;
 
-#if defined(HAVE_XSCALE) || defined(HAVE_MAGICBOX) || defined(HAVE_FONERA) || defined(HAVE_WHRAG108) || defined(HAVE_GATEWORX) || defined(HAVE_STORM) || defined(HAVE_LS2) || defined(HAVE_CA8) || defined(HAVE_TW6600)  || defined(HAVE_LS5) || defined(HAVE_LSX) || defined(HAVE_WP54G) || defined(HAVE_NP28G) || defined(HAVE_SOLO51) || defined(HAVE_OPENRISC) || defined(HAVE_DANUBE) || defined(HAVE_UNIWIP)
+#if defined(HAVE_XSCALE) || defined(HAVE_MAGICBOX) || defined(HAVE_FONERA) || defined(HAVE_WHRAG108) || defined(HAVE_GATEWORX) || defined(HAVE_STORM) || defined(HAVE_LS2) || defined(HAVE_CA8) || defined(HAVE_TW6600)  || defined(HAVE_LS5) || defined(HAVE_LSX) || defined(HAVE_WP54G) || defined(HAVE_NP28G) || defined(HAVE_SOLO51) || defined(HAVE_OPENRISC) || defined(HAVE_DANUBE) || defined(HAVE_UNIWIP) || defined(HAVE_VENTANA)
 	state = val;
 	int sesgpio = 0xfff;
 	int wifigpio = 0xfff;
