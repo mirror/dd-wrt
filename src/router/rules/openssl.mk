9ad2561530e662c6144979350f3c5ee8c8c48f22
BrainSlayer <s.gottschall@dd-wrt.com>
2013-12-19 20:13:04 +0000

    eliptic curve support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@23177 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/openssl.mk
+++ src/router/rules/openssl.mk
@@ -72,10 +72,10 @@ openssl-clean:
 OPENSSL_NO_CIPHERS:= no-idea no-md2 no-mdc2 no-rc5 no-sha0 no-smime \
 					no-rmd160 no-aes192 no-ripemd no-camellia no-ans1 no-krb5
 ifeq ($(CONFIG_XSCALE),y)
-OPENSSL_OPTIONS:= no-ec no-err no-hw threads zlib-dynamic \
+OPENSSL_OPTIONS:= no-err no-hw threads zlib-dynamic \
 					no-sse2 no-perlasm -DHAVE_CRYPTODEV
 else
-OPENSSL_OPTIONS:= no-ec no-err no-hw threads zlib-dynamic \
+OPENSSL_OPTIONS:= no-err no-hw threads zlib-dynamic \
 					no-engines no-perlasm
 endif
 
