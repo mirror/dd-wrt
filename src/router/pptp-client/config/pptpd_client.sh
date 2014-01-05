03d47bf24efecc4f204b22c2679d4b57a14888b2
BrainSlayer <s.gottschall@dd-wrt.com>
2014-01-05 16:44:47 +0000

    additional pptp options. if defined, they will replace the default option set
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@23290 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/pptp-client/config/pptpd_client.sh
+++ src/router/pptp-client/config/pptpd_client.sh
@@ -4,11 +4,16 @@ PASS=$(/usr/sbin/nvram get pptpd_client_srvpass)
 SEC=$(/usr/sbin/nvram get pptpd_client_srvsec)
 MTU=$(/usr/sbin/nvram get pptpd_client_srvmtu)
 MRU=$(/usr/sbin/nvram get pptpd_client_srvmru)
+OPTIONS=$(/usr/sbin/nvram get pptpd_client_options)
 sleep 10
 rm /tmp/pptpd_client/ -R
 mkdir /tmp/pptpd_client
 mkdir /tmp/ppp
+if [ -z "$OPTIONS" ]; then
 cp /etc/config/pptpd_client.options /tmp/pptpd_client/options.vpn
+else
+echo "$OPTIONS" > /tmp/pptpd_client/options.vpn
+fi
 echo "$SEC"  >> /tmp/pptpd_client/options.vpn
 echo -n "mtu " >> /tmp/pptpd_client/options.vpn
 echo "$MTU"  >> /tmp/pptpd_client/options.vpn
