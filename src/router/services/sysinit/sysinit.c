0c680d2ee7cd574ff0b628bb4e568769adfc22f6
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-01-15 18:28:14 +0000

    add interfac
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@23372 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit.c
+++ src/router/services/sysinit/sysinit.c
@@ -941,7 +941,7 @@ void start_restore_defaults(void)
 #elif HAVE_RT2880
 	struct nvram_tuple generic[] = {
 		{"lan_ifname", "br0", 0},
-		{"lan_ifnames", "vlan1 vlan2 ra0",
+		{"lan_ifnames", "vlan1 vlan2 ra0 ba0",
 		 0},
 		{"wan_ifname2", "vlan2", 0},
 		{"wan_ifname", "vlan2", 0},
