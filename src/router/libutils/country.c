c1d4f33b375f0b9318bece48d9a609cb1022406f
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-01-31 17:43:57 +0000

    add serbia
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@23459 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/country.c
+++ src/router/libutils/country.c
@@ -334,6 +334,7 @@ enum CountryCode {
 	CTRY_ROMANIA = 642,	/* Romania */
 	CTRY_RUSSIA = 643,	/* Russia */
 	CTRY_SAUDI_ARABIA = 682,	/* Saudi Arabia */
+	CTRY_SERBIA = 688,     /* Republic of Serbia */
 	CTRY_SERBIA_MONTENEGRO = 891,	/* Serbia and Montenegro */
 	CTRY_SINGAPORE = 702,	/* Singapore */
 	CTRY_SLOVAKIA = 703,	/* Slovak Republic */
@@ -598,6 +599,7 @@ static COUNTRY_CODE_TO_ENUM_RD allCountries[] = {
 	{CTRY_RUSSIA, NULL1_WORLD, "RU", "RUSSIA", YES, NO, YES, 7000},
 	{CTRY_SAUDI_ARABIA, NULL1_WORLD, "SA", "SAUDI_ARABIA", YES, NO, YES,
 	 7000},
+	{CTRY_SERBIA, ETSI1_WORLD,   "RS", "REPUBLIC OF SERBIA", YES,  NO, YES, 7000 },
 //#ifdef HAVE_BUFFALO
 //      {CTRY_SINGAPORE, APL6_FCCA, "SG", "SINGAPORE", YES, YES, YES, 7000},
 //#else
