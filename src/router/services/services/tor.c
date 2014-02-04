95ddf401866dd42ee1d8520b9535ba177027189d
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-02-04 16:50:56 +0000

    thats wrong markus. long and int are identical on 32 bit systems, but differ on 64 bit systems
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@23511 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/tor.c
+++ src/router/services/services/tor.c
@@ -64,7 +64,7 @@ void start_tor(void)
 	}
 	fprintf(fp, "RunAsDaemon 1\n");
 	fprintf(fp, "Address %s\n",get_wan_ipaddr());
-//      fprintf(fp,"ControlPort 9051\n");
+//      fprintf(fp, "ControlPort 9051\n");
 	if (nvram_match("tor_relay", "1"))
 		fprintf(fp, "ORPort 9001\n");
 	if (nvram_match("tor_dir", "1"))
