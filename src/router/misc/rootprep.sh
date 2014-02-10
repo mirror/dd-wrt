cb683832dd76e57688f2b350a2f57a1c7606693c
kong <USER@DOMAIN>
2014-02-10 22:25:45 +0000

    optware compat - link to iptables extension for optware
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@23565 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/misc/rootprep.sh
+++ src/router/misc/rootprep.sh
@@ -50,6 +50,9 @@ echo "/jffs/usr/local/lib" >> etc/ld.so.conf
 echo "/mmc/lib" >> etc/ld.so.conf
 echo "/mmc/usr/lib" >> etc/ld.so.conf
 echo "/mmc/usr/local/lib" >> etc/ld.so.conf
+echo "/opt/lib" >> etc/ld.so.conf
+echo "/opt/usr/lib" >> etc/ld.so.conf
+echo "/opt/usr/local/lib" >> etc/ld.so.conf
 echo "/lib" >> etc/ld.so.conf
 echo "/usr/lib" >> etc/ld.so.conf
 /sbin/ldconfig -r $ROOTDIR
@@ -57,3 +60,5 @@ echo "/usr/lib" >> etc/ld.so.conf
 # miscellaneous
 ln -sf tmp/mnt mnt
 mkdir -p proc
+mkdir -p opt/lib/iptables
+ln -sf /opt/usr/lib/iptables usr/lib/iptables
