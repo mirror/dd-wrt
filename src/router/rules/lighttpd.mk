277bcfb4d9403a23452d310ca82b104f748c2b91
kong <USER@DOMAIN>
2014-02-20 09:48:45 +0000

    update pcre - 8.34
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@23608 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/lighttpd.mk
+++ src/router/rules/lighttpd.mk
@@ -32,7 +32,7 @@ lighttpd-clean:
 
 lighttpd-install:
 	install -D lighttpd/src/.libs/lighttpd $(INSTALLDIR)/lighttpd/usr/sbin/lighttpd
-	install -D $(TOP)/pcre/.libs/libpcre.so.0 $(INSTALLDIR)/lighttpd/usr/lib/libpcre.so.0
+	install -D $(TOP)/pcre/.libs/libpcre.so.1 $(INSTALLDIR)/lighttpd/usr/lib/libpcre.so.1
 	mkdir -p $(INSTALLDIR)/lighttpd/usr/lib/lighttpd ; true
 	cp -av lighttpd/src/.libs/mod_*.so $(INSTALLDIR)/lighttpd/usr/lib/lighttpd/ ; true
 	mkdir -p $(INSTALLDIR)/lighttpd/etc ; true
