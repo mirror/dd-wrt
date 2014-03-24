d54f9a7b659794a1dbf64a5e35a8f6611f057da5
BrainSlayer <s.gottschall@dd-wrt.com>
2014-03-24 16:10:14 +0000

    revert those changes in newer kernels as well
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@23803 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.12/arch/arm/plat-brcm/bcm5301x_pcie.c
+++ src/linux/universal/linux-3.12/arch/arm/plat-brcm/bcm5301x_pcie.c
@@ -812,7 +812,7 @@ static void __init soc_pcie_map_init(struct soc_pcie_port * port)
 		port->reg_base + SOC_PCIE_SYS_IARR(1));
 
 #ifdef CONFIG_SPARSEMEM
-	addr = PHYS_OFFSET2;
+	addr = PHYS_OFFSET2 + SZ_128M;
 	__raw_writel(addr | 0x1,
 		port->reg_base + SOC_PCIE_SYS_IMAP2(0));
 	__raw_writel(addr | size,
