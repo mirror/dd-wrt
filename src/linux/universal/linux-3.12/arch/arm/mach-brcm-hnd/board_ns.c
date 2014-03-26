3f0c6a0aaa82cb07079aae44cf092edfae74d68c
BrainSlayer <s.gottschall@dd-wrt.com>
2014-03-26 18:09:24 +0000

    more ids
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@23806 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.12/arch/arm/mach-brcm-hnd/board_ns.c
+++ src/linux/universal/linux-3.12/arch/arm/mach-brcm-hnd/board_ns.c
@@ -764,7 +764,7 @@ struct mtd_partition *init_nflash_mtd_partitions(hndnand_t * nfl, struct mtd_inf
 	uint32 bootossz = NFL_BOOT_OS_SIZE;
 	int isbufdual = 0;
 	uint boardnum = bcm_strtoul(nvram_safe_get("boardnum"), NULL, 0);
-	if ((boardnum == 2013012401 || boardnum == 2013083001 || boardnum == 2013032101) && nvram_match("boardtype", "0x0646")
+	if ((boardnum == 2013012401 || boardnum == 2013083001 || boardnum == 2013032101 || boardnum == 2013013101) && nvram_match("boardtype", "0x0646")
 	    && nvram_match("boardrev", "0x1110")) {
 		printk(KERN_EMERG "Buffalo WZR-900DHP dualboot\n");
 		isbufdual = 1;
