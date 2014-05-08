eb413dac8cee89846ca8a92ed70362248018ebea
BrainSlayer <s.gottschall@dd-wrt.com>
2014-05-08 18:34:24 +0000

    wdr3500 fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24015 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.13/arch/mips/ar7240/dev-ar9xxx-wmac.c
+++ src/linux/universal/linux-3.13/arch/mips/ar7240/dev-ar9xxx-wmac.c
@@ -117,7 +117,7 @@ static void ar934x_wmac_init(void)
 	ar9xxx_wmac_resources[0].start = AR934X_WMAC_BASE;
 	ar9xxx_wmac_resources[0].end = AR934X_WMAC_BASE + AR934X_WMAC_SIZE - 1;
 	ar9xxx_wmac_resources[1].start = AR934X_IP2_IRQ_WMAC;
-	ar9xxx_wmac_resources[1].start = AR934X_IP2_IRQ_WMAC;
+	ar9xxx_wmac_resources[1].end = AR934X_IP2_IRQ_WMAC;
 	if (ar71xx_ref_freq == MHZ_25) {
 		printk(KERN_EMERG "25MHZ ref freq\n");
 		ar9xxx_wmac_data.is_clk_25mhz = true;
