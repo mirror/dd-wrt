982b5ad61c5d9bbff2c8e06b0d2f00b6ee52f3de
kong <USER@DOMAIN>
2014-05-11 17:34:11 +0000

    hfilter object already exists, put filter tran to correct location
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24030 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/dd-wrt/lang_pack/english.js
+++ src/router/kromo/dd-wrt/lang_pack/english.js
@@ -651,6 +651,10 @@ filter.legend5="Website Blocking by Keyword";
 filter.mess1="Delete the Policy?";
 filter.mess2="You must at least select a day.";
 filter.mess3="The end time must be bigger than start time.";
+filter.nat="Filter WAN NAT Redirection";
+filter.port113="Filter IDENT (Port 113)";
+filter.snmp="Block WAN SNMP access";
+filter.none="";
 
 //help container
 var hfilter=new Object();
@@ -662,6 +666,9 @@ hfilter.right10="Enter the time of the day you would like your policy to apply."
 hfilter.right12="You may choose to block access to certain services. Click <em>" + sbutton.filterSer + "</em> to modify these settings.";
 hfilter.right14="You can block access to certain websites by entering their URL.";
 hfilter.right16="You can block access to certain website by the keywords contained in their webpage.";
+hfilter.page1="<dd>This feature allows you to customize up to ten different Internet Access Policies for particular PCs, which are identified by their IP or MAC addresses. For each policy designated PCs, during the days and time periods specified.<br /><br />To create or edit a policy, follow these instructions:<ol class=\"wide\"><li>Select the policy number (1-10) in the drop-down menu.</li><li>Enter a name in the Enter Profile Name field.</li><li>Click the <i>Edit List of PCs</i> button.</li><li>On the <i>List of PCs</i> screen, specify PCs by IP address or MAC address. Enter the appropriate IP addresses into the <i>IP</i> fields. If you have a range of IP addresses to filter, complete the appropriate <i>IP Range</i> fields. Enter the appropriate MAC addresses into the <i>MAC</i> fields.</li><li>Click the <i>Apply</i> button to save your changes. Click the <i>Cancel</i> button to cancel your unsaved changes. Click the <i>Close</i> button to return to the Filters screen.</li><li>If you want to block the listed PCs from Internet access during the designated days and time, then keep the default setting, <i>Deny</i>. If you want the listed PCs to have Internet filtered during the designated days and time, then click the radio button next to <i>Filter</i>.</li><li>Set the days when access will be filtered. Select <i>Everyday</i> or the appropriate days of the week.</li><li>Set the time when access will be filtered. Select <i>24 Hours</i>, or check the box next to <i>From</i> and use the drop-down boxes to designate a specific time period.</li><li>Click the <i>Add to Policy</i> button to save your changes and active it.</li><li>To create or edit additional policies, repeat steps 1-9.</li></ol><br />To delete an Internet Access Policy, select the policy number, and click the <i>Delete</i> button.</dd>";
+hfilter.page2="<dd>To see a summary of all the policies, click the <i>Summary</i> button. The Internet Policy Summary screen will show each policy's number, Policy Name, Days, and Time of Day. To delete a policy, click its box, and then click the <i>Delete</i> button. Click the <i>Close</i> button to return to the Filters screen.</dd>";
+
 
 
 // ** FilterSummary.asp **//
@@ -691,9 +698,6 @@ firewall.ssh="Limit SSH Access";
 firewall.telnet="Limit Telnet Access";
 firewall.pptp="Limit PPTP Server Access";
 firewall.ftp="Limit FTP Server Access";
-filter.nat="Filter WAN NAT Redirection";
-filter.port113="Filter IDENT (Port 113)";
-filter.snmp="Block WAN SNMP access";
 
 //help container
 var hfirewall=new Object();
@@ -2291,11 +2295,6 @@ var wl_filter=new Object();
 wl_filter.titl="MAC Address Filter List";
 wl_filter.h2="MAC Address Filter List";
 wl_filter.h3="Enter MAC Address in this format&nbsp;:&nbsp;&nbsp;&nbsp;xx:xx:xx:xx:xx:xx";
-
-//help page
-var hfilter=new Object();
-hfilter.page1="<dd>This feature allows you to customize up to ten different Internet Access Policies for particular PCs, which are identified by their IP or MAC addresses. For each policy designated PCs, during the days and time periods specified.<br /><br />To create or edit a policy, follow these instructions:<ol class=\"wide\"><li>Select the policy number (1-10) in the drop-down menu.</li><li>Enter a name in the Enter Profile Name field.</li><li>Click the <i>Edit List of PCs</i> button.</li><li>On the <i>List of PCs</i> screen, specify PCs by IP address or MAC address. Enter the appropriate IP addresses into the <i>IP</i> fields. If you have a range of IP addresses to filter, complete the appropriate <i>IP Range</i> fields. Enter the appropriate MAC addresses into the <i>MAC</i> fields.</li><li>Click the <i>Apply</i> button to save your changes. Click the <i>Cancel</i> button to cancel your unsaved changes. Click the <i>Close</i> button to return to the Filters screen.</li><li>If you want to block the listed PCs from Internet access during the designated days and time, then keep the default setting, <i>Deny</i>. If you want the listed PCs to have Internet filtered during the designated days and time, then click the radio button next to <i>Filter</i>.</li><li>Set the days when access will be filtered. Select <i>Everyday</i> or the appropriate days of the week.</li><li>Set the time when access will be filtered. Select <i>24 Hours</i>, or check the box next to <i>From</i> and use the drop-down boxes to designate a specific time period.</li><li>Click the <i>Add to Policy</i> button to save your changes and active it.</li><li>To create or edit additional policies, repeat steps 1-9.</li></ol><br />To delete an Internet Access Policy, select the policy number, and click the <i>Delete</i> button.</dd>";
-hfilter.page2="<dd>To see a summary of all the policies, click the <i>Summary</i> button. The Internet Policy Summary screen will show each policy's number, Policy Name, Days, and Time of Day. To delete a policy, click its box, and then click the <i>Delete</i> button. Click the <i>Close</i> button to return to the Filters screen.</dd>";
 
 // ** WL_ActiveTable.asp **//
 var wl_active=new Object();
