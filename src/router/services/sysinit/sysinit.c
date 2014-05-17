bc2947b6f872a19f25e89c6ecf1a1daa74bc0af8
BrainSlayer <s.gottschall@dd-wrt.com>
2014-05-17 11:22:07 +0000

    asus rtn18u support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24077 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit.c
+++ src/router/services/sysinit/sysinit.c
@@ -2242,6 +2242,7 @@ void start_restore_defaults(void)
 	case ROUTER_LINKSYS_EA6900:
 	case ROUTER_LINKSYS_EA6700:
 	case ROUTER_LINKSYS_EA6500V2:
+	case ROUTER_ASUS_RTN18U:
 	case ROUTER_ASUS_AC67U:
 		if (!nvram_get("vlan1ports") || nvram_match("vlan1ports", "")
 		    || !nvram_get("vlan2ports")
