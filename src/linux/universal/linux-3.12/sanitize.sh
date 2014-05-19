70249a66c1f239e0a9b87281b8fd579296a6a9cd
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-05-19 19:18:15 +0000

    config updates
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24089 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.12/sanitize.sh
+++ src/linux/universal/linux-3.12/sanitize.sh
@@ -7,7 +7,13 @@ do
 	then 
 	    echo COPY $i
 	    cp $i .config
-	    make oldconfig ARCH=i386
+	    grep "CONFIG_X86_64=y" $i
+	    if [ $? -eq 0 ] 
+		then 
+		    make oldconfig ARCH=x86_64
+		else
+		    make oldconfig ARCH=i386
+		fi
 	    cp .config $i
     fi
 
@@ -38,19 +44,31 @@ do
 	    echo CONFIG_WPE72=y >> .config
 	    echo CONFIG_WA901=y >> .config
 	    echo CONFIG_WDR4300=y >> .config
+	    echo CONFIG_WDR3500=y >> .config
 	    echo CONFIG_WDR2543=y >> .config
 	    echo CONFIG_WR841V8=y >> .config
+	    echo CONFIG_WR841V9=y >> .config
 	    echo CONFIG_NVRAM_64K=y >> .config
 	    echo CONFIG_NVRAM_60K=y >> .config
+	    echo CONFIG_ALFANX=y >> .config
+	    echo CONFIG_AP135=y >> .config
+	    echo CONFIG_WR1043V2=y >> .config
+	    echo CONFIG_ARCHERC7=y >> .config
 	    make oldconfig ARCH=mips
 	    sed -i 's/\CONFIG_WR841V8=y/ /g' .config	    
+	    sed -i 's/\CONFIG_WR841V9=y/ /g' .config	    
 	    sed -i 's/\CONFIG_WPE72=y/ /g' .config	    
 	    sed -i 's/\CONFIG_DIR615I=y/ /g' .config	    
 	    sed -i 's/\CONFIG_WA901=y/ /g' .config	    
 	    sed -i 's/\CONFIG_WDR4300=y/ /g' .config	    
+	    sed -i 's/\CONFIG_WDR3500=y/ /g' .config	    
 	    sed -i 's/\CONFIG_WDR2543=y/ /g' .config	    
 	    sed -i 's/\CONFIG_NVRAM_64K=y/ /g' .config	    
 	    sed -i 's/\CONFIG_NVRAM_60K=y/ /g' .config	    
+	    sed -i 's/\CONFIG_ALFANX=y/ /g' .config	    
+	    sed -i 's/\CONFIG_AP135=y/ /g' .config	    
+	    sed -i 's/\CONFIG_WR1043V2=y/ /g' .config	    
+	    sed -i 's/\CONFIG_ARCHERC7=y/ /g' .config	    
 	    cp .config $i
     fi
 done
