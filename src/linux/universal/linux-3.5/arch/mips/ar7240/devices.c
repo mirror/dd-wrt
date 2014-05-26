0e55f3259cae35c34d2454839c15cf58b521db1c
BrainSlayer <s.gottschall@dd-wrt.com>
2014-05-26 12:22:49 +0000

    fix frame len
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24157 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.5/arch/mips/ar7240/devices.c
+++ src/linux/universal/linux-3.5/arch/mips/ar7240/devices.c
@@ -798,10 +798,11 @@ void __init ar71xx_add_device_eth(unsigned int id)
 	else
 		pdev = &ar71xx_eth1_device;
 
+	pdata = pdev->dev.platform_data;
+
 	pdata->max_frame_len = 1540;
 	pdata->desc_pktlen_mask = 0xfff;
 
-	pdata = pdev->dev.platform_data;
 
 	err = ar71xx_setup_phy_if_mode(id, pdata);
 	if (err) {
