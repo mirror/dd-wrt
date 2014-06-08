3cd23dee588be5367e73effc208f98cc50644a23
BrainSlayer <s.gottschall@dd-wrt.com>
2014-06-08 09:01:01 +0000

    musl fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24244 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/xfsprogs/libdisk/xvm.h
+++ src/router/xfsprogs/libdisk/xvm.h
@@ -20,6 +20,30 @@
 #define DIOCGETVOLDEV    _DIOC_(36) /* subvolume devices */
 #define DIOCGETVOLSTRIPE _DIOC_(47) /* subvolume stripe info */
 
+#ifndef __UCLIBC__
+/* Convenience types.  */
+typedef unsigned char __u_char;
+typedef unsigned short int __u_short;
+typedef unsigned int __u_int;
+typedef unsigned long int __u_long;
+
+/* Fixed-size types, underlying types depend on word size and compiler.  */
+typedef signed char __int8_t;
+typedef unsigned char __uint8_t;
+typedef signed short int __int16_t;
+typedef unsigned short int __uint16_t;
+typedef signed int __int32_t;
+typedef unsigned int __uint32_t;
+#if __WORDSIZE == 64
+typedef signed long int __int64_t;
+typedef unsigned long int __uint64_t;
+#elif defined(__GNUC__)
+__extension__ typedef signed long long int __int64_t;
+__extension__ typedef unsigned long long int __uint64_t;
+#endif
+
+#endif
+
 /*
  * Structure returned by the DIOCGETVOLDEV ioctl to list the
  * subvolume device nodes in a volume.  These are external device
