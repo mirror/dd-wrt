bbd4490d3490eb404aa7b73ac67afedb44bd6421
BrainSlayer <s.gottschall@dd-wrt.com>
2014-06-08 09:01:30 +0000

    musl fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24246 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/xfsprogs/include/linux.h
+++ src/router/xfsprogs/include/linux.h
@@ -28,6 +28,30 @@
 #include <getopt.h>
 #include <endian.h>
 
+#ifndef __UCLIBC__
+/* Convenience types.  */
+typedef unsigned char __u_char;
+typedef unsigned short int __u_short;
+typedef unsigned int __u_int;
+typedef unsigned long int __u_long;
+
+/* Fixed-size types, underlying types depend on word size and compiler.  */
+typedef signed char __int8_t;
+typedef unsigned char __uint8_t;
+typedef signed short int __int16_t;
+typedef unsigned short int __uint16_t;
+typedef signed int __int32_t;
+typedef unsigned int __uint32_t;
+#if __WORDSIZE == 64
+typedef signed long int __int64_t;
+typedef unsigned long int __uint64_t;
+#elif defined(__GNUC__)
+__extension__ typedef signed long long int __int64_t;
+__extension__ typedef unsigned long long int __uint64_t;
+#endif
+
+#endif
+
 static __inline__ int xfsctl(const char *path, int fd, int cmd, void *p)
 {
 	return ioctl(fd, cmd, p);
