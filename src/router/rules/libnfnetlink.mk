a8d486b92b6d96c2b215867c2b197b71a97d2282
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-06-08 11:42:14 +0000

    musl fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24251 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libnfnetlink.mk
+++ src/router/rules/libnfnetlink.mk
@@ -3,13 +3,15 @@ libnfnetlink-configure:
 		--build=$(ARCH)-linux \
 		--host=$(ARCH)-linux-gnu \
 		--prefix=/usr \
+		--enable-shared \
+		--disable-static \
 		--libdir=$(TOP)/libnfnetlink/src/.libs
 		
-		#CFLAGS="$(COPTS) -fPIC -DNEED_PRINTF -I$(TOP)/iptables/include/libipq/" LDFLAGS="-L$(TOP)/iptables/libipq"
+		#CFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE fPIC -DNEED_PRINTF -I$(TOP)/iptables/include/libipq/" LDFLAGS="-L$(TOP)/iptables/libipq"
 
 
 libnfnetlink:
-	$(MAKE) -C libnfnetlink CFLAGS="$(COPTS) -DNEED_PRINTF"
+	$(MAKE) -C libnfnetlink CFLAGS="$(COPTS)  $(MIPS16_OPT) -D_GNU_SOURCE -DNEED_PRINTF"
 
 libnfnetlink-install:
 	install -D libnfnetlink/src/.libs/libnfnetlink.so.0 $(INSTALLDIR)/libnfnetlink/usr/lib/libnfnetlink.so.0
