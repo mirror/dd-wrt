9406661b2b8607641699d27484acff6dc585c475
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-06-09 12:33:09 +0000

    musl fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24285 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/transmission/third-party/miniupnp/portlistingparse.h
+++ src/router/transmission/third-party/miniupnp/portlistingparse.h
@@ -11,7 +11,7 @@
 /* for the definition of UNSIGNED_INTEGER */
 #include "miniupnpctypes.h"
 
-#if defined(NO_SYS_QUEUE_H) || defined(_WIN32) || defined(__HAIKU__)
+#ifndef __UCLIBC__
 #include "bsdqueue.h"
 #else
 #include <sys/queue.h>
