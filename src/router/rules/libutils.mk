0ae7108e83585d46b2180720e66dcce7c4cc84cd
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-06-17 10:36:24 +0000

    libnltiny isnt used on old architecures without ath9k
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24364 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libutils.mk
+++ src/router/rules/libutils.mk
@@ -7,12 +7,15 @@ endif
 ifeq ($(CONFIG_MINIDLNA),y)
 	JANSSON=jansson
 endif
+ifeq ($(CONFIG_ATH9K),y)
+	TINY=libnltiny
+endif
 
 
 libutils-clean:
 	make -C libutils clean
 
-libutils: nvram libnltiny  $(JANSSON)
+libutils: nvram $(TINY)  $(JANSSON)
 	make -C libutils
 
 libutils-install:
