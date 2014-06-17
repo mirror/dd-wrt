c0b7fc9ac30bce7e924dbdb2a69e581eeb7b6aed
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-06-17 22:52:08 +0000

    gpios for wxr
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24375 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-northstar.c
+++ src/router/services/sysinit/sysinit-northstar.c
@@ -2322,9 +2322,10 @@ void start_sysinit(void)
 		set_gpio(12, 1);	// fixup ses button
 		break;
 	case ROUTER_BUFFALO_WXR1900DHP:
-		nvram_set("0:ledbh12", "7");
-		nvram_set("1:ledbh10", "7");
-		set_gpio(12, 1);	// fixup ses button
+		nvram_set("0:ledbh10", "7");
+		nvram_set("1:ledbh12", "7");
+		set_gpio(16, 1);	// fixup ses button
+		set_gpio(15, 1);	// fixup ses button
 		break;
 	case ROUTER_BUFFALO_WZR900DHP:
 	case ROUTER_BUFFALO_WZR600DHP2:
