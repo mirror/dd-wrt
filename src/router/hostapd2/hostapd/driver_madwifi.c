4176a9c3817a827fc7a409643e3a9f8b4c9469fd
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-06-18 15:27:43 +0000

    fix for musl
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24391 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/hostapd2/hostapd/driver_madwifi.c
+++ src/router/hostapd2/hostapd/driver_madwifi.c
@@ -43,6 +43,21 @@
 /* Assume this is built against madwifi-ng */
 #define MADWIFI_NG
 #endif /* IEEE80211_IOCTL_SETWMMPARAMS */
+#ifndef __UCLIBC__
+/* Convenience types.  */
+typedef unsigned char __u_char;
+typedef unsigned short int __u_short;
+typedef unsigned int __u_int;
+typedef unsigned long int __u_long;
+
+/* Fixed-size types, underlying types depend on word size and compiler.  */
+typedef signed char __int8_t;
+typedef unsigned char __uint8_t;
+typedef signed short int __int16_t;
+typedef unsigned short int __uint16_t;
+typedef signed int __int32_t;
+typedef unsigned int __uint32_t;
+#endif
 
 #include "wireless_copy.h"
 #include <linux/if_vlan.h>
