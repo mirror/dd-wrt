72b2346698d7f11910085b4272c40055506d117a
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-06-18 13:50:44 +0000

    add mfdb
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24387 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/igmp-proxy.mk
+++ src/router/rules/igmp-proxy.mk
@@ -1,5 +1,5 @@
 igmp-proxy-configure:
-	cd igmp-proxy && ./configure --host=$(ARCH)-linux-elf CFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE  -I$(TOP)/kernel_headers/$(KERNELRELEASE)/include -ffunction-sections -fdata-sections -Wl,--gc-sections -std=gnu99"
+	cd igmp-proxy && ./configure --host=$(ARCH)-linux-elf CFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE  -I$(TOP)/kernel_headers/$(KERNELRELEASE)/include -ffunction-sections -fdata-sections -Wl,--gc-sections -std=gnu99" LDFLAGS="-L$(TOP)/libutils -lutils"
 
 igmp-proxy-clean:
 	make -C igmp-proxy clean
