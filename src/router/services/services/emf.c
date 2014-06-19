1d920ffefb938dff92bc7adcdfad6b0135a0e15f
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-06-19 08:50:02 +0000

    take care if its enabled or disabled
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24403 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/emf.c
+++ src/router/services/services/emf.c
@@ -24,6 +24,7 @@
 #include <bcmnvram.h>
 #include <shutils.h>
 #include <utils.h>
+#include <wlutils.h>
 #include <syslog.h>
 #include <signal.h>
 #include <services.h>
@@ -33,7 +34,10 @@ void start_emf(void)
 	int i;
 	int cnt = get_wl_instances();
 	for (i = 0; i < cnt; i++) {
-		eval("emf", "start", getBridge(get_wl_instance_name(i)));
+		if (nvram_nmatch("1","wl%d_wmf_bss_enable",i))
+		    eval("emf", "start", getBridge(get_wl_instance_name(i)));
+		else
+		    eval("emf", "stop", getBridge(get_wl_instance_name(i)));
 	}
 	return;
 }
