2abc6d348592add10fcec1a1b0c18c5014f817c3
BrainSlayer <s.gottschall@dd-wrt.com>
2014-06-19 08:46:21 +0000

    emf handling
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24402 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/services/services/emf.c
@@ -0,0 +1,50 @@
+/*
+ * emf.c
+ *
+ * Copyright (C) 2014 Sebastian Gottschall <gottschall@dd-wrt.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id:
+ */
+#ifdef HAVE_EMF
+#include <stdlib.h>
+#include <bcmnvram.h>
+#include <shutils.h>
+#include <utils.h>
+#include <syslog.h>
+#include <signal.h>
+#include <services.h>
+
+void start_emf(void)
+{
+	int i;
+	int cnt = get_wl_instances();
+	for (i = 0; i < cnt; i++) {
+		eval("emf", "start", getBridge(get_wl_instance_name(i)));
+	}
+	return;
+}
+
+void stop_emf(void)
+{
+	int i;
+	int cnt = get_wl_instances();
+	for (i = 0; i < cnt; i++) {
+		eval("emf", "stop", getBridge(get_wl_instance_name(i)));
+	}
+	return;
+}
+#endif
