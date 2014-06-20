f5d412339ec33305a2d3591ba25b85700b4ccd8a
BrainSlayer <s.gottschall@dd-wrt.com>
2014-06-20 23:38:47 +0000

    wmf should work for vap's as well
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24417 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/emf.c
+++ src/router/services/services/emf.c
@@ -34,10 +34,23 @@ void start_emf(void)
 	int i;
 	int cnt = get_wl_instances();
 	for (i = 0; i < cnt; i++) {
-		if (nvram_nmatch("1","wl%d_wmf_bss_enable",i))
-		    eval("emf", "start", getBridge(get_wl_instance_name(i)));
+		if (nvram_nmatch("1", "wl%d_wmf_bss_enable", i))
+			eval("emf", "start", getBridge(get_wl_instance_name(i)));
 		else
-		    eval("emf", "stop", getBridge(get_wl_instance_name(i)));
+			eval("emf", "stop", getBridge(get_wl_instance_name(i)));
+
+		char *next;
+		char var[80];
+		char *vifs = nvram_nget("wl%d_vifs", i);
+		if (vifs != NULL) {
+			foreach(var, vifs, next) {
+				if (nvram_nmatch("1", "%s_wmf_bss_enable", var))
+					eval("emf", "start", getBridge(var));
+				else
+					eval("emf", "stop", getBridge(var));
+
+			}
+		}
 	}
 	return;
 }
@@ -47,6 +60,15 @@ void stop_emf(void)
 	int i;
 	int cnt = get_wl_instances();
 	for (i = 0; i < cnt; i++) {
+		char *next;
+		char var[80];
+		char *vifs = nvram_nget("wl%d_vifs", i);
+		if (vifs != NULL) {
+			foreach(var, vifs, next) {
+				eval("emf", "stop", getBridge(var));
+			}
+		}
+
 		eval("emf", "stop", getBridge(get_wl_instance_name(i)));
 	}
 	return;
