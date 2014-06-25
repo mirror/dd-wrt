7eb059b19d72410793706ac5f031ae29baca8155
kong <USER@DOMAIN>
2014-06-25 15:06:38 +0000

    forgot to commit this for micro builds
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24474 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/misc/rootprep_micro.sh
+++ src/router/misc/rootprep_micro.sh
@@ -121,6 +121,7 @@ echo "/opt/usr/lib" >> etc/ld.so.conf
 echo "/opt/usr/local/lib" >> etc/ld.so.conf
 echo "/lib" >> etc/ld.so.conf
 echo "/usr/lib" >> etc/ld.so.conf
+echo "" > tmp/TZ
 /sbin/ldconfig -r $ROOTDIR
 
 # miscellaneous
@@ -128,3 +129,4 @@ mkdir -p mnt
 mkdir -p proc
 mkdir -p opt/lib/iptables
 ln -sf /opt/usr/lib/iptables usr/lib/iptables
+ln -sf /tmp/TZ etc/TZ
