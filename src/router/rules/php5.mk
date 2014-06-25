d2a0a89c0c75ff1f57c12132fff3a0caf95eda6a
BrainSlayer <s.gottschall@dd-wrt.com>
2014-06-25 19:30:01 +0000

    another dirty workaround
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24477 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/php5.mk
+++ src/router/rules/php5.mk
@@ -6,6 +6,8 @@ libpng:
 	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	LDFLAGS="$(COPTS) $(MIPS16_OPT)  -L$(TOP)/zlib -fPIC -v -Wl,--verbose" \
 	$(MAKE) -C libpng
+	-mkdir -p $(TOP)/libgd/libpng/.libs/include
+	-cp $(TOP)/libgd/libpng/*.h $(TOP)/libgd/libpng/.libs/include
 	
 libpng-clean:
 	cd libgd && \
