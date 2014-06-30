1b4eb342701c1202e32606b3403a71876a49e9c4
BrainSlayer <s.gottschall@dd-wrt.com>
2014-06-30 11:37:05 +0000

    sysinit for e100/edgerouter
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24518 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/services/sysinit/sysinit-octeon.c
@@ -0,0 +1,135 @@
+/*
+ * sysinit-pb42.c
+ *
+ * Copyright (C) 2006 Sebastian Gottschall <gottschall@dd-wrt.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id:
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <time.h>
+#include <unistd.h>
+#include <errno.h>
+#include <syslog.h>
+#include <signal.h>
+#include <string.h>
+#include <termios.h>
+#include <sys/klog.h>
+#include <sys/types.h>
+#include <sys/mount.h>
+#include <sys/reboot.h>
+#include <sys/stat.h>
+#include <sys/sysmacros.h>
+#include <sys/time.h>
+#include <sys/utsname.h>
+#include <sys/wait.h>
+#include <sys/ioctl.h>
+
+#include <bcmnvram.h>
+#include <shutils.h>
+#include <utils.h>
+
+#define SIOCGMIIREG	0x8948	/* Read MII PHY register.  */
+#define SIOCSMIIREG	0x8949	/* Write MII PHY register.  */
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <linux/if.h>
+#include <linux/sockios.h>
+#include <linux/mii.h>
+#include "devices/wireless.c"
+
+void start_sysinit(void)
+{
+	char buf[PATH_MAX];
+	struct stat tmp_stat;
+	time_t tm = 0;
+
+	if (!nvram_match("disable_watchdog", "1"))
+		eval("watchdog");
+	/*
+	 * Setup console 
+	 */
+
+	cprintf("sysinit() klogctl\n");
+	klogctl(8, NULL, atoi(nvram_safe_get("console_loglevel")));
+	cprintf("sysinit() get router\n");
+
+	insmod("mbcache");
+	insmod("jbd2");
+	insmod("ext4");
+
+	if (mount("/dev/sda3", "/jffs", "ext2", MS_MGC_VAL, NULL)) {
+		eval("/sbin/mkfs.ext2", "-F", "-b", "1024", "/dev/sda3");
+		mount("/dev/sda3", "/jffs", "ext2", MS_MGC_VAL, NULL);
+		eval("mount","--bind","/jffs","/usr/local");
+	}
+
+	int brand = getRouterBrand();
+
+	/*
+	 * network drivers 
+	 */
+	eval("ifconfig", "eth0", "up");
+	eval("ifconfig", "eth1", "up");
+	eval("ifconfig", "eth2", "up");
+	struct ifreq ifr;
+	int s;
+
+	if ((s = socket(AF_INET, SOCK_RAW, IPPROTO_RAW))) {
+		char eabuf[32];
+
+		strncpy(ifr.ifr_name, "eth0", IFNAMSIZ);
+		ioctl(s, SIOCGIFHWADDR, &ifr);
+		nvram_set("et0macaddr", ether_etoa((unsigned char *)ifr.ifr_hwaddr.sa_data, eabuf));
+		nvram_set("et0macaddr_safe", ether_etoa((unsigned char *)ifr.ifr_hwaddr.sa_data, eabuf));
+		close(s);
+	}
+
+	detect_wireless_devices();
+
+	/*
+	 * Set a sane date 
+	 */
+	stime(&tm);
+	nvram_set("wl0_ifname", "ath0");
+
+	eval("hwclock", "-s");
+
+	return;
+	cprintf("done\n");
+}
+
+int check_cfe_nv(void)
+{
+	nvram_set("portprio_support", "0");
+	return 0;
+}
+
+int check_pmon_nv(void)
+{
+	return 0;
+}
+
+void start_overclocking(void)
+{
+}
+
+void enable_dtag_vlan(int enable)
+{
+
+}
