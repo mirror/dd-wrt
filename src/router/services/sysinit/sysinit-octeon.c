1d6b64776cab8cb13b127f6fdd8dc1cfb0544f5e
brainslayer <brainslayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-06-30 11:37:15 +0000

    sysinit for e100/edgerouter
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24519 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-octeon.c
+++ src/router/services/sysinit/sysinit-octeon.c
@@ -1,7 +1,7 @@
 /*
- * sysinit-pb42.c
+ * sysinit-octeon.c
  *
- * Copyright (C) 2006 Sebastian Gottschall <gottschall@dd-wrt.com>
+ * Copyright (C) 2014 Sebastian Gottschall <gottschall@dd-wrt.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
