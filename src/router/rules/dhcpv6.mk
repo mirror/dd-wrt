bf27f77bb654c484bf9a7a781b5ce28677e59099
kong <USER@DOMAIN>
2014-07-12 11:17:02 +0000

    rule file
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24616 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rules/dhcpv6.mk
@@ -0,0 +1,24 @@
+dhcpv6:
+	CC="$(CC)" \
+	CFLAGS="$(COPTS)  $(MIPS16_OPT) -DNEED_PRINTF -D_GNU_SOURCE  -DUSE_DHCP6SRV -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -D_GNU_SOURCE  -DUSE_DHCP6SRV -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	CXXFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -D_GNU_SOURCE  -DUSE_DHCP6SRV -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	LDFLAGS="$(COPTS) $(MIPS16_OPT) -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	$(MAKE) -C dhcpv6 all
+	
+dhcpv6-install:
+	install -D dhcpv6/dhcp6c $(INSTALLDIR)/dhcpv6/usr/bin/dhcp6c
+	install -D dhcpv6/dhcp6s $(INSTALLDIR)/dhcpv6/usr/bin/dhcp6s
+
+dhcpv6-clean:
+	$(MAKE) -C dhcpv6 clean
+
+dhcpv6-configure:
+	cd dhcpv6 && ./configure --prefix= --with-localdbdir=/var --with-sysconfdir=/etc \
+			--target=$(ARCH)-linux \
+			--host=$(ARCH) \
+			ac_cv_func_setpgrp_void=yes \
+			CC="$(CC)" \
+			CXXFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -D_GNU_SOURCE -DUSE_DHCP6SRV -ffunction-sections -fdata-sections -Wl,--gc-sections"  \
+			CFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -D_GNU_SOURCE  -DUSE_DHCP6SRV -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+			LDFLAGS="$(COPTS) $(MIPS16_OPT) -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections"
