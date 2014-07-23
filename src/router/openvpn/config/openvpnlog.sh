2857254fca3402b70bcdd3c4b8e43105834343a2
kong <kong@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-07-23 17:22:57 +0000

    fix openvpn status page
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24707 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/openvpn/config/openvpnlog.sh
+++ src/router/openvpn/config/openvpnlog.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-/bin/echo -e "Serverlog \n"
+if [ "$(nvram get openvpn_enable)" = "1" ]; then
+/bin/echo -e "Serverlog: <br>"
 /bin/echo "log 500" | /usr/bin/nc 127.0.0.1 14 | /bin/grep -v "^>" | /usr/bin/awk -F "," '{
 	printf strftime("%Y%m%d %H:%M:%S ",$1);
 	for (i=2;i<=NF;i++)
@@ -7,14 +8,13 @@
 	printf "<br>\n";
 	}'
 
-/bin/echo -e " \n"
+/bin/echo -e "<br>"
 /bin/cat /tmp/openvpn/openvpn.conf
 
-/bin/echo -e " \n"
-/bin/echo -e " \n"
-/bin/echo -e " \n"
-
-/bin/echo -e "Clientlog \n"
+/bin/echo -e "<br><br><br>"
+fi
+if [ "$(nvram get openvpncl_enable)" = "1" ]; then
+/bin/echo -e "Clientlog: <br>"
 /bin/echo "log 500" | /usr/bin/nc 127.0.0.1 16 | /bin/grep -v "^>" | /usr/bin/awk -F "," '{
 	printf strftime("%Y%m%d %H:%M:%S ",$1);
 	for (i=2;i<=NF;i++)
@@ -22,5 +22,6 @@
 	printf "<br>\n";
 	}'
 
-/bin/echo -e " \n"
+/bin/echo -e "<br>"
 /bin/cat /tmp/openvpncl/openvpn.conf
+fi
--- src/router/openvpn/config/openvpnstate.sh
+++ src/router/openvpn/config/openvpnstate.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-echo -e "Server: \n"
-echo -e "`/bin/echo "state" | /usr/bin/nc 127.0.0.1 14 |awk -F"," '{ print $2}'`: `/bin/echo "state" | /usr/bin/nc 127.0.0.1 14 |awk -F"," '{print $3}'` \n"
-echo -e "Local Address: `/bin/echo "state" | /usr/bin/nc 127.0.0.1 14 |awk -F"," '{print $4}'` \n"
-echo -e "Remote Address: `ifconfig tun2 |awk '{print $3}'|grep P-t-P|awk -F ":" '{print $2}'`"
 
-echo -e "Client: \n"
-echo -e "`/bin/echo "state" | /usr/bin/nc 127.0.0.1 16 |awk -F"," '{ print $2}'`: `/bin/echo "state" | /usr/bin/nc 127.0.0.1 16 |awk -F"," '{print $3}'` \n"
-echo -e "Local Address: `/bin/echo "state" | /usr/bin/nc 127.0.0.1 16 |awk -F"," '{print $4}'` \n"
-echo -e "Remote Address: `ifconfig tun1 |awk '{print $3}'|grep P-t-P|awk -F ":" '{print $2}'` \n"
+if [ "$(nvram get openvpn_enable)" = "1" ]; then
+echo -e "Server: "
+echo -e "`/bin/echo "state" | /usr/bin/nc 127.0.0.1 14 |awk -F"," '{ print $2}'` `/bin/echo "state" | /usr/bin/nc 127.0.0.1 14 |awk -F"," '{print $3}'` <br><br>"
+echo -e "Local Address: `/bin/echo "state" | /usr/bin/nc 127.0.0.1 14 |awk -F"," '{print $4}'` <br>"
+echo -e "Remote Address: `ifconfig tun2 |awk '{print $3}'|grep P-t-P|awk -F ":" '{print $2}'` <br><br><br>"
+fi
+
+if [ "$(nvram get openvpncl_enable)" = "1" ]; then
+echo -e "Client: "
+echo -e "`/bin/echo "state" | /usr/bin/nc 127.0.0.1 16 |awk -F"," '{ print $2}'` `/bin/echo "state" | /usr/bin/nc 127.0.0.1 16 |awk -F"," '{print $3}'` <br><br>"
+echo -e "Local Address: `/bin/echo "state" | /usr/bin/nc 127.0.0.1 16 |awk -F"," '{print $4}'` <br>"
+echo -e "Remote Address: `ifconfig tun1 |awk '{print $3}'|grep P-t-P|awk -F ":" '{print $2}'` <br>"
+fi
