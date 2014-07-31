a85d1059e2adf13f53c31ed13751a248eddf8173
BrainSlayer <s.gottschall@dd-wrt.com>
2014-07-31 20:13:10 +0000

    for debugging
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24769 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/ejs.c
+++ src/router/httpd/visuals/ejs.c
@@ -2717,6 +2717,7 @@ void ej_get_radio_state(webs_t wp, int argc, char_t ** argv)
 		return;
 		}
 		qcsapi_interface_get_status("wifi0", status);
+		fprintf(stderr,"status %s\n",status);
 		if (!strcmp(status, "up"))
 			websWrite(wp, "%s", live_translate("wl_basic.radio_on"));
 		else
