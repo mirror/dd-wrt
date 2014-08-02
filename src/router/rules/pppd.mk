a6532b3788fa7e3903c0de801bd3eb71e27106a9
BrainSlayer <s.gottschall@dd-wrt.com>
2014-08-02 10:01:09 +0000

    move to new dir
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24791 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/pppd.mk
+++ src/router/rules/pppd.mk
@@ -1,7 +1,7 @@
 pppd-symlinks:
-	- (cd pppd.new ; \
+	- (cd pppd ; \
 	ln -s linux/Makefile.top Makefile ; \
-	ln -s Makefile.linux pppd.new/Makefile ; \
+	ln -s Makefile.linux pppd/Makefile ; \
 	ln -s Makefile.linux chat/Makefile ; \
 	ln -s Makefile.linux pppd/plugins/Makefile ; \
 	ln -s Makefile.linux pppd/plugins/radius/Makefile ; \
@@ -17,71 +17,71 @@ else
 pppd: pppd-symlinks
 endif
 ifeq ($(CONFIG_IPV6),y)
-	$(MAKE) HAVE_INET6=y -j 4 -C pppd.new/pppd
+	$(MAKE) HAVE_INET6=y -j 4 -C pppd/pppd
 else
-	$(MAKE) -j 4 -C pppd.new/pppd
+	$(MAKE) -j 4 -C pppd/pppd
 endif
 ifeq ($(CONFIG_3G),y)
-	$(MAKE) -j 4 -C pppd.new/chat
+	$(MAKE) -j 4 -C pppd/chat
 endif
 ifeq ($(CONFIG_PPPSTATS),y)
-	$(MAKE) -j 4 -C pppd.new/pppstats
+	$(MAKE) -j 4 -C pppd/pppstats
 endif
-	$(MAKE) -j 4 -C pppd.new/pppd/plugins/rp-pppoe
+	$(MAKE) -j 4 -C pppd/pppd/plugins/rp-pppoe
 ifeq ($(CONFIG_PPTP_ACCEL),y)
-	$(MAKE) -j 4 -C pppd.new/pppd/plugins/pptp
+	$(MAKE) -j 4 -C pppd/pppd/plugins/pptp
 endif
 ifeq ($(CONFIG_RADIUSPLUGIN),y)
-	$(MAKE) -j 4 -C pppd.new/pppd/plugins/radius
+	$(MAKE) -j 4 -C pppd/pppd/plugins/radius
 endif
 ifeq ($(CONFIG_PPPOATM),y)
-	$(MAKE) -j 4 -C pppd.new/pppd/plugins/pppoatm
+	$(MAKE) -j 4 -C pppd/pppd/plugins/pppoatm
 endif
 ifeq ($(CONFIG_L2TP),y)
-	$(MAKE) -j 4 -C pppd.new/pppd/plugins/pppol2tp
+	$(MAKE) -j 4 -C pppd/pppd/plugins/pppol2tp
 endif
 
 
 pppd-clean pppd-distclean: pppd-symlinks
-	$(MAKE) -C pppd.new/pppd/plugins/rp-pppoe clean
-	$(MAKE) -C pppd.new/pppd/plugins/pppoatm clean
-	$(MAKE) -C pppd.new/pppd/plugins/radius clean
-	$(MAKE) -C pppd.new/pppd/plugins/pppol2tp clean
-	$(MAKE) -C pppd.new/chat clean
-	$(MAKE) HAVE_INET6=y  -C pppd.new/pppd clean
+	$(MAKE) -C pppd/pppd/plugins/rp-pppoe clean
+	$(MAKE) -C pppd/pppd/plugins/pppoatm clean
+	$(MAKE) -C pppd/pppd/plugins/radius clean
+	$(MAKE) -C pppd/pppd/plugins/pppol2tp clean
+	$(MAKE) -C pppd/chat clean
+	$(MAKE) HAVE_INET6=y  -C pppd/pppd clean
 
 pppd-install:
-	install -D pppd.new/pppd/pppd $(INSTALLDIR)/pppd/usr/sbin/pppd
+	install -D pppd/pppd/pppd $(INSTALLDIR)/pppd/usr/sbin/pppd
 ifeq ($(CONFIG_3G),y)
-	install -D pppd.new/chat/chat $(INSTALLDIR)/pppd/usr/sbin/chat
+	install -D pppd/chat/chat $(INSTALLDIR)/pppd/usr/sbin/chat
 endif
-	install -D pppd.new/pppd/plugins/rp-pppoe/rp-pppoe.so $(INSTALLDIR)/pppd/usr/lib/rp-pppoe.so
+	install -D pppd/pppd/plugins/rp-pppoe/rp-pppoe.so $(INSTALLDIR)/pppd/usr/lib/rp-pppoe.so
 ifeq ($(CONFIG_PPTP_ACCEL),y)
-	install -D pppd.new/pppd/plugins/pptp/pptp.so $(INSTALLDIR)/pppd/usr/lib/pptp.so
+	install -D pppd/pppd/plugins/pptp/pptp.so $(INSTALLDIR)/pppd/usr/lib/pptp.so
 endif
 
 
 ifeq ($(CONFIG_PPPOATM),y)
-	install -D pppd.new/pppd/plugins/pppoatm/pppoatm.so $(INSTALLDIR)/pppd/usr/lib/pppoatm.so
+	install -D pppd/pppd/plugins/pppoatm/pppoatm.so $(INSTALLDIR)/pppd/usr/lib/pppoatm.so
 endif
 ifeq ($(CONFIG_L2TP),y)
-	install -D pppd.new/pppd/plugins/pppol2tp/pppol2tp.so $(INSTALLDIR)/pppd/usr/lib/pppol2tp.so
+	install -D pppd/pppd/plugins/pppol2tp/pppol2tp.so $(INSTALLDIR)/pppd/usr/lib/pppol2tp.so
 endif
 
 ifeq ($(CONFIG_PPPSTATS),y)
-	install -D pppd.new/pppstats/pppstats $(INSTALLDIR)/pppd/usr/sbin/pppstats
+	install -D pppd/pppstats/pppstats $(INSTALLDIR)/pppd/usr/sbin/pppstats
 else
 	rm -rf $(INSTALLDIR)/pppd/usr/sbin/pppstats
 endif
 ifeq ($(CONFIG_RADIUSPLUGIN),y)
-	install -D pppd.new/pppd/plugins/radius/radius.so $(INSTALLDIR)/pppd/usr/lib/radius.so
-	install -D pppd.new/pppd/plugins/radius/radattr.so $(INSTALLDIR)/pppd/usr/lib/radattr.so
-	install -D pppd.new/pppd/plugins/radius/radrealms.so $(INSTALLDIR)/pppd/usr/lib/radrealms.so
+	install -D pppd/pppd/plugins/radius/radius.so $(INSTALLDIR)/pppd/usr/lib/radius.so
+	install -D pppd/pppd/plugins/radius/radattr.so $(INSTALLDIR)/pppd/usr/lib/radattr.so
+	install -D pppd/pppd/plugins/radius/radrealms.so $(INSTALLDIR)/pppd/usr/lib/radrealms.so
 	install -d $(INSTALLDIR)/pppd/etc
-	cp pppd.new/pppd/plugins/radius/etc/issue $(INSTALLDIR)/pppd/etc/issue
-	cp pppd.new/pppd/plugins/radius/etc/port-id-map $(INSTALLDIR)/pppd/etc/port-id-map
-	cp pppd.new/pppd/plugins/radius/etc/dictionary $(INSTALLDIR)/pppd/etc/dictionary
-	cp pppd.new/pppd/plugins/radius/etc/dictionary.microsoft $(INSTALLDIR)/pppd/etc/dictionary.microsoft
+	cp pppd/pppd/plugins/radius/etc/issue $(INSTALLDIR)/pppd/etc/issue
+	cp pppd/pppd/plugins/radius/etc/port-id-map $(INSTALLDIR)/pppd/etc/port-id-map
+	cp pppd/pppd/plugins/radius/etc/dictionary $(INSTALLDIR)/pppd/etc/dictionary
+	cp pppd/pppd/plugins/radius/etc/dictionary.microsoft $(INSTALLDIR)/pppd/etc/dictionary.microsoft
 	chmod 0644 $(INSTALLDIR)/pppd/etc/*
 else
 	rm -rf $(INSTALLDIR)/pppd/usr/lib/pppd/rad*.so
