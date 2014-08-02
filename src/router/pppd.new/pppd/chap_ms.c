943da4db4069e0891e86e2464426c04f80637753
BrainSlayer <s.gottschall@dd-wrt.com>
2014-08-02 09:57:38 +0000

    fix buffer overrun
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24788 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/pppd.new/pppd/chap_ms.c
+++ src/router/pppd.new/pppd/chap_ms.c
@@ -382,7 +382,7 @@ chapms2_make_response(unsigned char *response, int id, char *our_name,
 		      unsigned char *private)
 {
 	const struct chapms2_response_cache_entry *cache_entry;
-	unsigned char auth_response[MS_AUTH_RESPONSE_LENGTH];
+	unsigned char auth_response[MS_AUTH_RESPONSE_LENGTH+1];
 
 	challenge++;	/* skip length, should be 16 */
 	*response++ = MS_CHAP2_RESPONSE_LEN;
