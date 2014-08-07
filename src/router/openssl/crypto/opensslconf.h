86c7ed11a353c729fd159226b1fe14bb6109768e
BrainSlayer <s.gottschall@dd-wrt.com>
2014-08-07 17:36:01 +0000

    openssl update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24811 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/openssl/crypto/opensslconf.h
+++ src/router/openssl/crypto/opensslconf.h
@@ -80,6 +80,9 @@
 #ifndef OPENSSL_NO_STORE
 # define OPENSSL_NO_STORE
 #endif
+#ifndef OPENSSL_NO_UNIT_TEST
+# define OPENSSL_NO_UNIT_TEST
+#endif
 
 #endif /* OPENSSL_DOING_MAKEDEPEND */
 
@@ -175,6 +178,9 @@
 # endif
 # if defined(OPENSSL_NO_STORE) && !defined(NO_STORE)
 #  define NO_STORE
+# endif
+# if defined(OPENSSL_NO_UNIT_TEST) && !defined(NO_UNIT_TEST)
+#  define NO_UNIT_TEST
 # endif
 #endif
 
