c613cf8af67a5f943051f4ce4b149d130d76ad18
BrainSlayer <s.gottschall@dd-wrt.com>
2014-08-19 15:33:59 +0000

    fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24879 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/dhcpv6.mk
+++ src/router/rules/dhcpv6.mk
@@ -1,9 +1,28 @@
+ifeq ($(CONFIG_SAMBA3),y)
+  JFLAGS = -I$(TOP)/jansson/src -L$(TOP)/jansson/src/.libs -ljansson -lm
+endif
+ifeq ($(CONFIG_FTP),y)
+  JFLAGS = -I$(TOP)/jansson/src -L$(TOP)/jansson/src/.libs -ljansson -lm
+endif
+ifeq ($(CONFIG_MINIDLNA),y)
+  JFLAGS = -I$(TOP)/jansson/src -L$(TOP)/jansson/src/.libs -ljansson -lm
+endif
+
+ifeq ($(CONFIG_TIEXTRA1),y)
+  JFLAGS = -I$(TOP)/jansson/src -I$(TOP)/private/telkom -L$(TOP)/jansson/src/.libs -ljansson -lm
+endif
+ifeq ($(CONFIG_TIEXTRA2),y)
+  JFLAGS = -I$(TOP)/jansson/src -I$(TOP)/private/telkom -L$(TOP)/jansson/src/.libs -ljansson -lm
+endif
+
+
+
 dhcpv6:
 	CC="$(CC)" \
-	CFLAGS="$(COPTS)  $(MIPS16_OPT) -DNEED_PRINTF -D_GNU_SOURCE -I$(TOP)/shared -DUSE_DHCP6SRV -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -D_GNU_SOURCE -I$(TOP)/shared  -DUSE_DHCP6SRV -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	CXXFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -D_GNU_SOURCE -I$(TOP)/shared  -DUSE_DHCP6SRV -L$(TOP)/nvram  -L$(TOP)/libutils -lutils -lnvram -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	LDFLAGS="$(COPTS) $(MIPS16_OPT) -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	CFLAGS="$(COPTS)  $(MIPS16_OPT) $(JFLAGS) -DNEED_PRINTF -D_GNU_SOURCE -I$(TOP)/shared -DUSE_DHCP6SRV -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	CPPFLAGS="$(COPTS) $(MIPS16_OPT) $(JFLAGS) -DNEED_PRINTF -D_GNU_SOURCE -I$(TOP)/shared  -DUSE_DHCP6SRV -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	CXXFLAGS="$(COPTS) $(MIPS16_OPT) $(JFLAGS) -DNEED_PRINTF -D_GNU_SOURCE -I$(TOP)/shared  -DUSE_DHCP6SRV -L$(TOP)/nvram  -L$(TOP)/libutils -lutils -lnvram -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	LDFLAGS="$(COPTS) $(MIPS16_OPT) $(JFLAGS) -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	$(MAKE) -C dhcpv6 all
 	
 dhcpv6-install:
@@ -14,11 +33,13 @@ dhcpv6-clean:
 	$(MAKE) -C dhcpv6 clean
 
 dhcpv6-configure:
+
+
 	cd dhcpv6 && ./configure --prefix= --with-localdbdir=/var --with-sysconfdir=/etc \
 			--target=$(ARCH)-linux \
 			--host=$(ARCH) \
 			ac_cv_func_setpgrp_void=yes \
 			CC="$(CC)" \
-			CXXFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/shared -DNEED_PRINTF -D_GNU_SOURCE -DUSE_DHCP6SRV -ffunction-sections -fdata-sections -Wl,--gc-sections"  \
-			CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/shared -DNEED_PRINTF -D_GNU_SOURCE  -DUSE_DHCP6SRV -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-			LDFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/shared -fPIC -L$(TOP)/nvram -L$(TOP)/libutils -lutils -lnvram -ffunction-sections -fdata-sections -Wl,--gc-sections"
+			CXXFLAGS="$(COPTS) $(MIPS16_OPT) $(JFLAGS) -I$(TOP)/shared -DNEED_PRINTF -D_GNU_SOURCE -DUSE_DHCP6SRV -ffunction-sections -fdata-sections -Wl,--gc-sections"  \
+			CFLAGS="$(COPTS) $(MIPS16_OPT) $(JFLAGS) -I$(TOP)/shared -DNEED_PRINTF -D_GNU_SOURCE  -DUSE_DHCP6SRV -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+			LDFLAGS="$(COPTS) $(MIPS16_OPT) $(JFLAGS) -I$(TOP)/shared -fPIC -L$(TOP)/nvram -L$(TOP)/libutils -lutils -lnvram -ffunction-sections -fdata-sections -Wl,--gc-sections"
