b61e3893d99a6017e869bf4176d2c693a1d05087
BrainSlayer <s.gottschall@dd-wrt.com>
2014-09-08 22:06:57 +0000

    more sysprintf replacements
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@24996 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/quantenna.c
+++ src/router/services/networking/quantenna.c
@@ -157,7 +157,7 @@ int gen_stateless_conf(void)
 
 	char *country = getIsoName(nvram_safe_get("wl_regdomain"));
 	if (!country)
-	    country="DE";
+		country = "DE";
 	char lower[32];
 	int i;
 	for (i = 0; i < strlen(country); i++)
