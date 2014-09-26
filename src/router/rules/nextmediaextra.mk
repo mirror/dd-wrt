1df3a50a43d9aa37fd80adad077bfd9276368386
BrainSlayer <s.gottschall@dd-wrt.com>
2014-09-26 00:24:11 +0000

    missing
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@25059 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/nextmediaextra.mk
+++ src/router/rules/nextmediaextra.mk
@@ -3,4 +3,8 @@ nextmediaextra-configure:
 nextmediaextra:
 	@true
 nextmediaextra-install:
-	-nextmediaextra/install.sh $(TOP) $(INSTALLDIR) $(PLATFORM)
+ifeq ($(CONFIG_MUSL),y)
+	-nextmediaextra/install.sh $(TOP) $(INSTALLDIR) $(PLATFORM)-musl 
+else
+	-nextmediaextra/install.sh $(TOP) $(INSTALLDIR) $(PLATFORM) 
+endif
