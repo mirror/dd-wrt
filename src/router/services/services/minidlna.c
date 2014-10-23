b9a034e54ea336fefb71b4cf0472ff96325d2c9a
brainslayer <brainslayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-10-23 23:30:23 +0000

    subdirectory handling for minidlna as requested by users
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@25168 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/minidlna.c
+++ src/router/services/services/minidlna.c
@@ -65,11 +65,11 @@ void start_dlna(void)
 		if (strlen(cs->mp)) {
 			if ((cs->types & (TYPE_VIDEO | TYPE_AUDIO | TYPE_IMAGES))) {
 				if (cs->types & TYPE_VIDEO)
-					fprintf(fp, "media_dir=V,%s\n", cs->mp);
+					fprintf(fp, "media_dir=V,%s%s%s\n", cs->mp, (cs->sd[0] != '/' && cs->sd[0] != 0) ? "/" : "", cs->sd);
 				if (cs->types & TYPE_AUDIO)
-					fprintf(fp, "media_dir=A,%s\n", cs->mp);
+					fprintf(fp, "media_dir=A,%s%s%s\n", cs->mp, (cs->sd[0] != '/' && cs->sd[0] != 0) ? "/" : "", cs->sd);
 				if (cs->types & TYPE_IMAGES)
-					fprintf(fp, "media_dir=P,%s\n", cs->mp);
+					fprintf(fp, "media_dir=P,%s%s%s\n", cs->mp, (cs->sd[0] != '/' && cs->sd[0] != 0) ? "/" : "", cs->sd);
 			}
 
 		}
