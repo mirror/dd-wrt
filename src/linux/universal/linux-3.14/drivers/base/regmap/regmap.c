7f6b963ff0009f70a9e1cf29ee0291f438a51a2c
BrainSlayer <s.gottschall@dd-wrt.com>
2014-10-31 20:55:16 +0000

    Update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@25221 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.14/drivers/base/regmap/regmap.c
+++ src/linux/universal/linux-3.14/drivers/base/regmap/regmap.c
@@ -1309,7 +1309,7 @@ int _regmap_write(struct regmap *map, unsigned int reg,
 	}
 
 #ifdef LOG_DEVICE
-	if (strcmp(dev_name(map->dev), LOG_DEVICE) == 0)
+	if (map->dev && strcmp(dev_name(map->dev), LOG_DEVICE) == 0)
 		dev_info(map->dev, "%x <= %x\n", reg, val);
 #endif
 
@@ -1558,6 +1558,9 @@ int regmap_bulk_write(struct regmap *map, unsigned int reg, const void *val,
 	} else {
 		void *wval;
 
+		if (!val_count)
+			return -EINVAL;
+
 		wval = kmemdup(val, val_count * val_bytes, GFP_KERNEL);
 		if (!wval) {
 			ret = -ENOMEM;
@@ -1740,7 +1743,7 @@ static int _regmap_read(struct regmap *map, unsigned int reg,
 	ret = map->reg_read(context, reg, val);
 	if (ret == 0) {
 #ifdef LOG_DEVICE
-		if (strcmp(dev_name(map->dev), LOG_DEVICE) == 0)
+		if (map->dev && strcmp(dev_name(map->dev), LOG_DEVICE) == 0)
 			dev_info(map->dev, "%x => %x\n", reg, *val);
 #endif
 
