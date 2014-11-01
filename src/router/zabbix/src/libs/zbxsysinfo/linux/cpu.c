dbc1030468f229b9613bd433f6f8983515c2cfb6
BrainSlayer <s.gottschall@dd-wrt.com>
2014-11-01 02:03:09 +0000

    getloadavg is not supported by uclibc, so we need a alternate function here
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@25223 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/zabbix/src/libs/zbxsysinfo/linux/cpu.c
+++ src/router/zabbix/src/libs/zbxsysinfo/linux/cpu.c
@@ -99,6 +99,22 @@ int	SYSTEM_CPU_UTIL(AGENT_REQUEST *request, AGENT_RESULT *result)
 	return get_cpustat(result, cpu_num, state, mode);
 }
 
+#if !defined(HAVE_GETLOADAVG)
+int getloadavg(double *a, int n)
+{
+	int i;
+	double b[3];
+	FILE *f = fopen("/proc/loadavg", "rbe");
+	if (!f) return -1;
+	i = fscanf(f, "%lf %lf %lf", b, b+1, b+2);
+	fclose(f);
+	if (n > i) n = i;
+	if (n < 0) return -1;
+	memcpy(a, b, n * sizeof *a);
+	return n;
+}
+#endif
+
 int	SYSTEM_CPU_LOAD(AGENT_REQUEST *request, AGENT_RESULT *result)
 {
 	char	*tmp;
