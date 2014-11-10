7b19e75996174d1b51b77621f6031ebfa786582c
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-11-10 23:22:16 +0000

    port config for whr1166d
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@25309 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-rt2880.c
+++ src/router/services/sysinit/sysinit-rt2880.c
@@ -120,42 +120,48 @@ void start_sysinit(void)
 
 	//LAN/WAN ports as security mode
 	if (getRouterBrand() == ROUTER_BOARD_E1700) {
-//	sysprintf("switch reg w 3600 5ee3a");
-//	sysprintf("switch reg w 7000 717fc3");
-//	sysprintf("switch reg w 7804 1015e9f");
-//	sysprintf("switch reg w 3600 5ee3b");
-	}  else {
+//      sysprintf("switch reg w 3600 5ee3a");
+//      sysprintf("switch reg w 7000 717fc3");
+//      sysprintf("switch reg w 7804 1015e9f");
+//      sysprintf("switch reg w 3600 5ee3b");
+	} else {
 #ifndef HAVE_WHR1166D
-	sysprintf("switch reg w 2004 ff0003");
-	sysprintf("switch reg w 2104 ff0003");
-	sysprintf("switch reg w 2204 ff0003");
-	sysprintf("switch reg w 2304 ff0003");
-	sysprintf("switch reg w 2404 ff0003");
-	sysprintf("switch reg w 2504 ff0003");
-	//LAN/WAN ports as transparent port
-	sysprintf("switch reg w 2010 810000c0");
-	sysprintf("switch reg w 2110 810000c0");
-	sysprintf("switch reg w 2210 810000c0");
-	sysprintf("switch reg w 2310 810000c0");
-	sysprintf("switch reg w 2410 810000c0");
-	sysprintf("switch reg w 2510 810000c0");
-	//set CPU/P7 port as user port
-	sysprintf("switch reg w 2610 81000000");
-	sysprintf("switch reg w 2710 81000000");
+		sysprintf("switch reg w 2004 ff0003");
+		sysprintf("switch reg w 2104 ff0003");
+		sysprintf("switch reg w 2204 ff0003");
+		sysprintf("switch reg w 2304 ff0003");
+		sysprintf("switch reg w 2404 ff0003");
+		sysprintf("switch reg w 2504 ff0003");
+		//LAN/WAN ports as transparent port
+		sysprintf("switch reg w 2010 810000c0");
+		sysprintf("switch reg w 2110 810000c0");
+		sysprintf("switch reg w 2210 810000c0");
+		sysprintf("switch reg w 2310 810000c0");
+		sysprintf("switch reg w 2410 810000c0");
+		sysprintf("switch reg w 2510 810000c0");
+		//set CPU/P7 port as user port
+		sysprintf("switch reg w 2610 81000000");
+		sysprintf("switch reg w 2710 81000000");
 
-	sysprintf("switch reg w 2604 20ff0003");	// #port6, Egress VLAN Tag Attribution=tagged
-	sysprintf("switch reg w 2704 20ff0003");	// #port7, Egress VLAN Tag Attribution=tagged
+		sysprintf("switch reg w 2604 20ff0003");	// #port6, Egress VLAN Tag Attribution=tagged
+		sysprintf("switch reg w 2704 20ff0003");	// #port7, Egress VLAN Tag Attribution=tagged
 
-	sysprintf("switch reg w 2014 10001");
-	sysprintf("switch reg w 2114 10001");
-	sysprintf("switch reg w 2214 10001");
-	sysprintf("switch reg w 2314 10001");
-	sysprintf("switch reg w 2414 10002");
-	sysprintf("switch reg w 2514 10001");
-	//VLAN member port
-	sysprintf("switch vlan set 0 1 11110111");
-	sysprintf("switch vlan set 1 2 00001011");
-	sysprintf("switch clear");
+		sysprintf("switch reg w 2014 10001");
+		sysprintf("switch reg w 2114 10001");
+		sysprintf("switch reg w 2214 10001");
+		sysprintf("switch reg w 2314 10001");
+		sysprintf("switch reg w 2414 10002");
+		sysprintf("switch reg w 2514 10001");
+		//VLAN member port
+		sysprintf("switch vlan set 0 1 11110111");
+		sysprintf("switch vlan set 1 2 00001011");
+		sysprintf("switch clear");
+#else
+		eval("swconfig", "dev", "eth0", "set", "reset", "1");
+		eval("swconfig", "dev", "eth0", "set", "enable_vlan", "1");
+		eval("swconfig", "dev", "eth0", "vlan", "1", "set", "ports", "0 1 2 3 6t");
+		eval("swconfig", "dev", "eth0", "vlan", "2", "set", "ports", "5 6t");
+		eval("swconfig", "dev", "eth0", "set", "apply");
 #endif
 	}
 	eval("ifconfig", "eth0", "up");
