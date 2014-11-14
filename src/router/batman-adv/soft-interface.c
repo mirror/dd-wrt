29361c461f6e93e3942dfd63250ede3de5f648ea
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-11-14 23:18:05 +0000

    compat
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@25372 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: f166ab3c4126b15ab4ab327d0b8fbaf455280b0c

--- src/router/batman-adv/soft-interface.c
+++ src/router/batman-adv/soft-interface.c
@@ -614,12 +614,19 @@ static void batadv_softif_init_early(struct net_device *dev)
 	memset(priv, 0, sizeof(*priv));
 }
 
+#if LINUX_VERSION_CODE > KERNEL_VERSION(3,15,0)
+typedef struct ctl_table ctl_table;
+#define m_alloc_netdev(a,b,c) alloc_netdev(a,b,NET_NAME_UNKNOWN, c)
+#else
+#define m_alloc_netdev alloc_netdev
+#endif
+
 struct net_device *batadv_softif_create(const char *name)
 {
 	struct net_device *soft_iface;
 	int ret;
 
-	soft_iface = alloc_netdev(sizeof(struct batadv_priv), name,
+	soft_iface = m_alloc_netdev(sizeof(struct batadv_priv), name,
 				  batadv_softif_init_early);
 	if (!soft_iface)
 		return NULL;
