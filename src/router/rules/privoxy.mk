7bc41465d917d0a41ba861d9c79020ed6d1643b4
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-11-17 11:11:09 +0000

    install libpcre
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@25387 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 70a0af434ea54238fca2c7e9b22d2ffe2e915674

--- src/router/rules/privoxy.mk
+++ src/router/rules/privoxy.mk
@@ -1,5 +1,5 @@
 
-privoxy:
+privoxy: pcre
 	CC="$(ARCH)-linux-uclibc-gcc" \
 	CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib -I$(TOP)/pcre -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib -ffunction-sections -fdata-sections -Wl,--gc-sections" \
@@ -21,7 +21,7 @@ privoxy-install:
 privoxy-clean:
 	$(MAKE) -C privoxy clean
 
-privoxy-configure:
+privoxy-configure: pcre-configure pcre
 	cd privoxy && rm -rf config.{cache,status} \
 	&& autoheader && autoconf \
 	&& ./configure ac_cv_func_setpgrp_void=yes --prefix=/usr --enable-compression --sysconfdir=/etc/privoxy --target=$(ARCH)-linux --host=$(ARCH) CC=$(ARCH)-linux-uclibc-gcc CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib -I$(TOP)/pcre -ffunction-sections -fdata-sections -Wl,--gc-sections" CPPFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib -I$(TOP)/pcre -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/zlib -L$(TOP)/pcre/.libs -lz -fPIC" 
