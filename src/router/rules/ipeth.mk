b2ab53b4a928348e4b1cb869f73eb11ee9be5bc7
BrainSlayer <s.gottschall@dd-wrt.com>
2014-11-26 13:31:05 +0000

    new rules for new libs
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@25449 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: df084d2023886e617d488cc6f677f7a6a8e83f2b

--- src/router/rules/ipeth.mk
+++ src/router/rules/ipeth.mk
@@ -2,7 +2,7 @@ LIBPLIST_PKG_BUILD_DIR=$(TOP)/ipeth/libplist
 
 ipeth-configure: 
 	cd $(TOP)/ipeth/libxml2 && ./configure  --host=$(ARCH)-linux CFLAGS="$(COPTS) $(MIPS16_OPT) -fPIC  -ffunction-sections -fdata-sections -Wl,--gc-sections" --without-python \
-	--enable-shared \
+	--disable-shared \
 	--enable-static \
 	--with-c14n \
 	--without-catalog \
@@ -35,81 +35,49 @@ ipeth-configure:
 
 	cd $(TOP)/ipeth/libxml2 && make
 	
-	rm -f $(TOP)/ipeth/libplist/CMakeCache.txt
-	$(call CMakeClean,$(LIBPLIST_PKG_BUILD_DIR))
-	(cd  $(TOP)/ipeth/libplist; \
-		CFLAGS="$(TARGET_CFLAGS) $(EXTRA_CFLAGS) $(COPTS) $(MIPS16_OPT) -I$(TOP)/ipeth/libplist/include -fPIC  -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	cd $(TOP)/ipeth/libplist && ./autogen.sh
+	cd $(TOP)/ipeth/libplist && ./configure --host=$(ARCH)-linux --without-cython CFLAGS="$(TARGET_CFLAGS) $(EXTRA_CFLAGS) $(COPTS) $(MIPS16_OPT) -I$(TOP)/ipeth/libxml2/include -I$(TOP)/ipeth/libplist/include -fPIC  -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 		CXXFLAGS="$(TARGET_CFLAGS) $(EXTRA_CFLAGS) $(COPTS) $(MIPS16_OPT) -I$(TOP)/ipeth/libplist -fPIC  -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-		cmake \
-			-DCMAKE_SYSTEM_NAME=Linux \
-			-DCMAKE_SYSTEM_VERSION=1 \
-			-DCMAKE_SYSTEM_PROCESSOR=$(ARCH) \
-			-DCMAKE_BUILD_TYPE=Release \
-			-DCMAKE_C_FLAGS_RELEASE="-DNDEBUG" \
-			-DCMAKE_CXX_FLAGS_RELEASE="-DNDEBUG" \
-			-DCMAKE_C_COMPILER=$(CROSS_COMPILE)gcc \
-			-DCMAKE_CXX_COMPILER=$(CROSS_COMPILE)g++ \
-			-DCMAKE_EXE_LINKER_FLAGS="$(TARGET_LDFLAGS) -lm" \
-			-DCMAKE_MODULE_LINKER_FLAGS="$(TARGET_LDFLAGS) -lm" \
-			-DCMAKE_SHARED_LINKER_FLAGS="$(TARGET_LDFLAGS) -lm" \
-			-DCMAKE_FIND_ROOT_PATH=$(STAGING_DIR) \
-			-DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=$(STAGING_DIR_HOST) \
-			-DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=$(STAGING_DIR) \
-			-DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=$(STAGING_DIR) \
-			-DCMAKE_STRIP=: \
-			-DCMAKE_INSTALL_PREFIX=/usr \
-			-DLIBXML2_INCLUDE_DIR=$(TOP)/ipeth/libxml2/include \
-			-DLIBXML2_LIBRARIES=$(TOP)/ipeth/libxml2/.libs/libxml2.a \
-		. \
-	)
-
+		LDFLAGS="-L$(TOP)/ipeth/libxml2/.libs -ffunction-sections -fdata-sections -Wl,--gc-sections"
 	cd $(TOP)/ipeth/libplist && make
 
 
-	rm -f $(TOP)/ipeth/libusbmuxd/CMakeCache.txt
-	$(call CMakeClean,$(TOP)/ipeth/libusbmuxd)
-	(cd  $(TOP)/ipeth/libusbmuxd; \
-		CFLAGS="$(TARGET_CFLAGS) $(EXTRA_CFLAGS) $(COPTS) $(MIPS16_OPT) -I$(TOP)/ipeth/libusbmuxd/include  -ffunction-sections -fdata-sections -Wl,--gc-sections " \
-		CXXFLAGS="$(TARGET_CFLAGS) $(EXTRA_CFLAGS) $(COPTS) $(MIPS16_OPT) -I$(TOP)/ipeth/libusbmuxd  -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-		cmake \
-			-DCMAKE_SYSTEM_NAME=Linux \
-			-DCMAKE_SYSTEM_VERSION=1 \
-			-DCMAKE_SYSTEM_PROCESSOR=$(ARCH) \
-			-DCMAKE_BUILD_TYPE=Release \
-			-DCMAKE_C_FLAGS_RELEASE="-DNDEBUG" \
-			-DCMAKE_CXX_FLAGS_RELEASE="-DNDEBUG" \
-			-DCMAKE_C_COMPILER=$(CROSS_COMPILE)gcc \
-			-DCMAKE_CXX_COMPILER=$(CROSS_COMPILE)g++ \
-			-DCMAKE_EXE_LINKER_FLAGS="$(TARGET_LDFLAGS) -lpthread" \
-			-DCMAKE_MODULE_LINKER_FLAGS="$(TARGET_LDFLAGS) -lpthread" \
-			-DCMAKE_SHARED_LINKER_FLAGS="$(TARGET_LDFLAGS) -lpthread" \
-			-DCMAKE_FIND_ROOT_PATH=$(STAGING_DIR) \
-			-DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=$(STAGING_DIR_HOST) \
-			-DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=$(STAGING_DIR) \
-			-DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=$(STAGING_DIR) \
-			-DCMAKE_STRIP=: \
-			-DCMAKE_INSTALL_PREFIX=/usr \
-			-DUSB_INCLUDE_DIR=$(TOP)/usb_modeswitch/libusb/libusb \
-			-DUSB_LIBRARY=$(TOP)/usb_modeswitch/libusb/libusb/.libs/libusb-1.0.a \
-			-DPLIST_INCLUDE_DIR=$(TOP)/ipeth/libplist/include \
-			-DPLIST_LIBRARY=$(TOP)/ipeth/libplist/src/libplist.so \
-		. \
-	)
+	cd $(TOP)/ipeth/libusbmuxd && ./autogen.sh
+	cd $(TOP)/ipeth/libusbmuxd && ./configure --host=$(ARCH)-linux --without-cython CFLAGS="$(TARGET_CFLAGS) $(EXTRA_CFLAGS) $(COPTS) $(MIPS16_OPT) -I$(TOP)/ipeth/libxml2/include -I$(TOP)/ipeth/libplist/include -I$(TOP)/usb_modeswitch/libusb/libusb -fPIC  -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+		CXXFLAGS="$(TARGET_CFLAGS) $(EXTRA_CFLAGS) $(COPTS) $(MIPS16_OPT) -I$(TOP)/ipeth/libplist -fPIC  -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+		LDFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections" \
+		libplist_CFLAGS="-I$(TOP)/ipeth/libplist/include" \
+		libplist_LIBS="-L$(TOP)/ipeth/libplist/src/.libs -lplist" ac_cv_func_malloc_0_nonnull=yes  ac_cv_func_realloc_0_nonnull=yes
+		
 	cd $(TOP)/ipeth/libusbmuxd && make
 
+	cd $(TOP)/ipeth/libimobiledevice && ./autogen.sh
 	cd $(TOP)/ipeth/libimobiledevice && ./configure --without-cython --host=$(ARCH)-linux \
 		ac_cv_sys_file_offset_bits=64 \
 		CFLAGS="$(COPTS) $(MIPS16_OPT)  -ffunction-sections -fdata-sections -Wl,--gc-sections -fPIC -I$(TOP)/ipeth  -Drpl_localtime=localtime -I$(TOP)/openssl/include -Drpl_malloc=malloc -Drpl_realloc=realloc" \
-		LDFLAGS="-L$(TOP)/ipeth/nettle -L$(TOP)/openssl -L$(TOP)/ipeth/libusbmuxd/libusbmuxd -L$(TOP)/zlib" \
-		libusbmuxd_CFLAGS="-I$(TOP)/usb_modeswitch/libusb/libusb -I$(TOP)/ipeth/libusbmuxd/libusbmuxd" \
+		LDFLAGS="-L$(TOP)/ipeth/nettle -L$(TOP)/openssl -L$(TOP)/ipeth/libusbmuxd/src/.libs -L$(TOP)/zlib" \
+		libusbmuxd_CFLAGS="-I$(TOP)/usb_modeswitch/libusb/libusb -I$(TOP)/ipeth/libusbmuxd/include" \
 		libusbmuxd_LIBS="$(TOP)/usb_modeswitch/libusb/libusb/.libs/libusb-1.0.a -lusbmuxd" \
 		libplist_CFLAGS="-I$(TOP)/ipeth/libplist/include" \
-		libplist_LIBS="-L$(TOP)/ipeth/libplist/src -lplist" \
+		libplist_LIBS="-L$(TOP)/ipeth/libplist/src/.libs -lplist" \
 		libplistmm_CFLAGS="-I$(TOP)/ipeth/libplist/include" \
-		libplistmm_LIBS="-L$(TOP)/ipeth/libplist/src -lplist"
+		libplistmm_LIBS="-L$(TOP)/ipeth/libplist/src/.libs -lplist"
 	cd $(TOP)/ipeth/libimobiledevice && make
 
+	cd $(TOP)/ipeth/usbmuxd && ./autogen.sh
+	cd $(TOP)/ipeth/usbmuxd && ./configure --host=$(ARCH)-linux --without-cython CFLAGS="$(TARGET_CFLAGS) $(EXTRA_CFLAGS) $(COPTS) $(MIPS16_OPT) -I$(TOP)/ipeth/libxml2/include -I$(TOP)/ipeth/libplist/include -I$(TOP)/usb_modeswitch/libusb/libusb -I$(TOP)/ipeth/libimobiledevice/include -fPIC  -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+		CXXFLAGS="$(TARGET_CFLAGS) $(EXTRA_CFLAGS) $(COPTS) $(MIPS16_OPT) -I$(TOP)/ipeth/libplist -fPIC  -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+		LDFLAGS="-L$(TOP)/openssl -lssl -lcrypto -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+		libusbmuxd_CFLAGS="-I$(TOP)/ipeth/libusbmuxd/include" \
+		libusbmuxd_LIBS="$(TOP)/ipeth/libusbmuxd/src/.libs/libusbmuxd.a" \
+		libplist_CFLAGS="-I$(TOP)/ipeth/libplist/include" \
+		libplist_LIBS="-L$(TOP)/ipeth/libplist/src/.libs -lplist" ac_cv_func_malloc_0_nonnull=yes  ac_cv_func_realloc_0_nonnull=yes \
+		libimobiledevice_CFLAGS="-I$(TOP)/ipeth/libimobiledevice/include" \
+		libimobiledevice_LIBS="$(TOP)/ipeth/libimobiledevice/src/.libs/libimobiledevice.a $(TOP)/ipeth/libusbmuxd/src/.libs/libusbmuxd.a" \
+		libusb_CFLAGS="-I$(TOP)/ipeth/libusbmuxd/include -I$(TOP)/usb_modeswitch/libusb/libusb" \
+		libusb_LIBS="$(TOP)/usb_modeswitch/libusb/libusb/.libs/libusb-1.0.a"
 
+	cd $(TOP)/ipeth/usbmuxd && make
 
 
 ipeth: comgt
@@ -128,6 +96,7 @@ endif
 	$(MAKE) -C $(TOP)/ipeth/libplist
 	$(MAKE) -C $(TOP)/ipeth/libusbmuxd 
 	$(MAKE) -C $(TOP)/ipeth/libimobiledevice
+	$(MAKE) -C $(TOP)/ipeth/usbmuxd 
 	$(MAKE) -C $(TOP)/ipeth/ipheth-pair clean
 	$(MAKE) -C $(TOP)/ipeth/ipheth-pair
 	
@@ -136,12 +105,13 @@ ipeth-clean:
 	$(MAKE) -C $(TOP)/ipeth/libplist clean
 	$(MAKE) -C $(TOP)/ipeth/libusbmuxd clean
 	$(MAKE) -C $(TOP)/ipeth/libimobiledevice clean
+	$(MAKE) -C $(TOP)/ipeth/usbmuxd clean
 	$(MAKE) -C $(TOP)/ipeth/ipheth-pair clean
 
 ipeth-install:
-	install -D $(TOP)/ipeth/libplist/src/libplist.so.1 $(INSTALLDIR)/ipeth/usr/lib/libplist.so.1
+	install -D $(TOP)/ipeth/libplist/src/.libs/libplist.so.3 $(INSTALLDIR)/ipeth/usr/lib/libplist.so.3
 #	install -D $(TOP)/ipeth/libusbmuxd/libusbmuxd/libusbmuxd.so.2 $(INSTALLDIR)/ipeth/usr/lib/libusbmuxd.so.2
-	install -D $(TOP)/ipeth/libusbmuxd/daemon/usbmuxd $(INSTALLDIR)/ipeth/usr/sbin/usbmuxd
+	install -D $(TOP)/ipeth/usbmuxd/src/.libs/usbmuxd $(INSTALLDIR)/ipeth/usr/sbin/usbmuxd
 #	install -D $(TOP)/ipeth/libimobiledevice/src/.libs/libimobiledevice.so.3 $(INSTALLDIR)/ipeth/usr/lib/libimobiledevice.so.3
 	install -D $(TOP)/ipeth/ipheth-pair/ipheth-pair $(INSTALLDIR)/ipeth/usr/sbin/ipheth-pair
 	install -D $(TOP)/ipeth/ipheth-pair/ipheth-loop $(INSTALLDIR)/ipeth/usr/sbin/ipheth-loop
