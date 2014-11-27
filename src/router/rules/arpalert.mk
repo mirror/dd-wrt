3d4f1566065cbe60d6066de532a86a3b16eebe4d
ChristianScheele <scheele@newmedia-net.de>
2014-11-27 16:40:30 +0000

    arpalert ac_cv_header_sys_sysctl_h=yes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@25494 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/arpalert.mk
+++ src/router/rules/arpalert.mk
@@ -1,5 +1,5 @@
 arpalert-configure:
-	cd arpalert && ./configure --prefix=/usr sysconfdir=/etc --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS) -fPIC -DNEED_PRINTF -I../libpcap_noring -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="-L$(TOP)/libpcap_noring -lpcap"
+	cd arpalert && ac_cv_header_sys_sysctl_h=yes ./configure --prefix=/usr sysconfdir=/etc --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS) -fPIC -DNEED_PRINTF -I../libpcap_noring -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="-L$(TOP)/libpcap_noring -lpcap"
 
 arpalert: 
 	$(MAKE) -C arpalert LDFLAGS+="-L$(TOP)/libpcap_noring"
