44d58b292a1a342efa3fe303d4fd16f4d6c8c504
ChristianScheele <scheele@newmedia-net.de>
2014-11-27 15:26:46 +0000

    arpalert rule file ec
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@25492 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 51cb26cdd97fa1e215936e726ed254b97fa7cacd

---
+++ src/router/rules/arpalert.mk
@@ -0,0 +1,19 @@
+arpalert-configure:
+	cd arpalert && ./configure --prefix=/usr sysconfdir=/etc --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS) -fPIC -DNEED_PRINTF -I../libpcap_noring -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="-L$(TOP)/libpcap_noring -lpcap"
+
+arpalert: 
+	$(MAKE) -C arpalert LDFLAGS+="-L$(TOP)/libpcap_noring"
+
+arpalert-clean: 
+	if test -e "nmap/Makefile"; then $(MAKE) -C arpalert clean ; fi
+
+arpalert-install: 
+	$(MAKE) -C arpalert install DESTDIR=$(INSTALLDIR)/arpalert
+	rm -rf $(INSTALLDIR)/arpalert/usr/share
+	rm -rf $(INSTALLDIR)/arpalert/usr/include
+	rm -rf $(INSTALLDIR)/arpalert/usr/var
+	rm -rf $(INSTALLDIR)/arpalert/etc/arpalert/arpalert.conf*
+	rm -rf $(INSTALLDIR)/arpalert/etc/arpalert/maclist.*
+	ln -sf /tmp/arpalert.conf $(INSTALLDIR)/arpalert/etc/arpalert/arpalert.conf
+	ln -sf /tmp/maclist.allow $(INSTALLDIR)/arpalert/etc/arpalert/maclist.allow
+	ln -sf /tmp/maclist.deny $(INSTALLDIR)/arpalert/etc/arpalert/maclist.deny
