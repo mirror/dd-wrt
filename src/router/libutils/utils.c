1ef351224609641783ce44d3c7f8f06cfbb19e36
BrainSlayer <s.gottschall@dd-wrt.com>
2014-12-10 19:50:56 +0000

    archer c5, same as c7 v2
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@25617 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/utils.c
+++ src/router/libutils/utils.c
@@ -1869,6 +1869,13 @@ int internal_getRouterBrand()
 	nvram_default_get("ath1_rxantenna", "7");
 	nvram_default_get("ath1_txantenna", "7");
 	return ROUTER_BOARD_WHRHPGN;
+#elif HAVE_ARCHERC5
+	setRouter("TPLINK ARCHER-C5 v1");
+	nvram_default_get("ath0_rxantenna", "7");
+	nvram_default_get("ath0_txantenna", "7");
+	nvram_default_get("ath1_rxantenna", "7");
+	nvram_default_get("ath1_txantenna", "7");
+	return ROUTER_BOARD_WHRHPGN;
 #elif HAVE_ARCHERC7
 	setRouter("TPLINK ARCHER-C7 v2");
 	nvram_default_get("ath0_rxantenna", "7");
