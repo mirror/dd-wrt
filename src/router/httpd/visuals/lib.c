d8497a204c2a0285d65b0c2373888672542fb106
markusquint <USER@DOMAIN>
2014-12-15 08:14:32 +0000

    Branding
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@25649 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 14ad01ab70ce6891f909451ba9864b1962241302

--- src/router/httpd/visuals/lib.c
+++ src/router/httpd/visuals/lib.c
@@ -144,6 +144,8 @@ void ej_get_firmware_version(webs_t wp, int argc, char_t ** argv)
 		}
 #elif HAVE_KORENRON
 		websWrite(wp, "KORENRON %s %s%s", MINOR_VERSION, nvram_safe_get("dist_type"), DIST_OPT);
+#elif HAVE_TESTEM
+		websWrite(wp, "TESTEM %s %s%s", MINOR_VERSION, nvram_safe_get("dist_type"), DIST_OPT);
 #elif HAVE_SANSFIL
 		websWrite(wp, "SANSFIL %s %s%s", MINOR_VERSION, nvram_safe_get("dist_type"), DIST_OPT);
 #else
