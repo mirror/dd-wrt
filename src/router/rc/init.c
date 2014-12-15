bb900b5746d9146eebfdb467a9be67423a52881f
markus <markus@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-12-15 08:14:32 +0000

    Branding
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@25649 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 82e59c4c1940c72b34e560ca4b73c3f2aa4fab47

--- src/router/rc/init.c
+++ src/router/rc/init.c
@@ -563,6 +563,8 @@ int main(int argc, char **argv)
 	fprintf(fp, "SANSFIL %s (c) 2014 NewMedia-NET GmbH\nRelease: " BUILD_DATE " (SVN revision: %s)\n", DIST, SVN_REVISION);
 #elif HAVE_KORENRON
 	fprintf(fp, "KORENRON %s (c) 2014 NewMedia-NET GmbH\nRelease: " BUILD_DATE " (SVN revision: %s)\n", DIST, SVN_REVISION);
+#elif HAVE_TESTEM
+	fprintf(fp, "TESTEM %s (c) 2014 NewMedia-NET GmbH\nRelease: " BUILD_DATE " (SVN revision: %s)\n", DIST, SVN_REVISION);
 #elif HAVE_ONNET
 #ifdef HAVE_ONNET_BLANK
 	fprintf(fp, "Enterprise AP %s (c) 2014 NewMedia-NET GmbH\nRelease: " BUILD_DATE " (SVN revision: %s)\n", DIST, SVN_REVISION);
