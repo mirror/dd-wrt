c81a93a6b3d3b49f6a83be1c8f3a965822901fa4
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-12-20 09:54:15 +0000

    adm5120 is old as well
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@25685 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 64bb0f64b0c9492acbc2d7d1ad9770aafc06a386

--- src/router/rules/radauth.mk
+++ src/router/rules/radauth.mk
@@ -3,10 +3,10 @@ radauth: nvram shared
 
 radauth-install:
 	install -D radauth/wrt-radauth $(INSTALLDIR)/radauth/usr/sbin/wrt-radauth
-	install -D radauth/radius-client $(INSTALLDIR)/radauth/usr/sbin/radius-client
 	install -D radauth/radiusallow $(INSTALLDIR)/radauth/usr/sbin/radiusallow
 	install -D radauth/radiusdisallow $(INSTALLDIR)/radauth/usr/sbin/radiusdisallow
 ifeq ($(CONFIG_RFLOW),y)
+	install -D radauth/radius-client $(INSTALLDIR)/radauth/usr/sbin/radius-client
 	install -D radauth/macupd $(INSTALLDIR)/radauth/usr/sbin/macupd
 endif
 
