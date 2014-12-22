1c10b05985dc9fb00ff13abb636f24a2866c0768
kong <kong@52c4871e-980c-0410-b1e0-e73912ce01f8>
2014-12-22 21:18:52 +0000

    zlib is used all over the place, static just wastes flash space
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@25699 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: ede1246eff909a1c35c51fafb78306b66eccb317

--- src/router/rules/zlib.mk
+++ src/router/rules/zlib.mk
@@ -1,5 +1,5 @@
 zlib-configure:
-	cd zlib && ./configure --static
+	cd zlib && ./configure
 
 zlib-clean:
 	make -C zlib clean
@@ -8,5 +8,7 @@ zlib:
 	make -j 4 -C zlib
 
 zlib-install:
-	@true
+	install -D zlib/libz.so.1.2.8 $(INSTALLDIR)/zlib/usr/lib/libz.so.1.2.8
+	cd $(INSTALLDIR)/zlib/usr/lib ; ln -s libz.so.1.2.8 libz.so.1  ; true
+	cd $(INSTALLDIR)/zlib/usr/lib ; ln -s libz.so.1.2.8 libz.so  ; true
 
