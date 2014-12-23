86835cb4a2ccc4cb837c6cd1a5e0601156ce324e
BrainSlayer <s.gottschall@dd-wrt.com>
2014-12-23 02:08:30 +0000

    use mips16 if available
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@25715 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/zlib.mk
+++ src/router/rules/zlib.mk
@@ -1,11 +1,12 @@
 zlib-configure:
-	cd zlib && ./configure
+	cd zlib && CFLAGS="$(COPTS) $(MIPS16_OPT)" && ./configure
 
 zlib-clean:
 	make -C zlib clean
 
 zlib:
-	make -j 4 -C zlib
+	make -C zlib
+	rm -f zlib/libz.a
 
 zlib-install:
 	install -D zlib/libz.so.1.2.8 $(INSTALLDIR)/zlib/usr/lib/libz.so.1.2.8
