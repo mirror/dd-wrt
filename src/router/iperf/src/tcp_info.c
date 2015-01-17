dfd20bfb084a0c1d1538e3f70333e639ea9dd6f1
BrainSlayer <s.gottschall@dd-wrt.com>
2015-01-17 13:03:27 +0000

    fix various musl compat issues
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@25950 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: b0c0bdb4cb298c9cb220f80c811f9ea93a08af95

--- src/router/iperf/src/tcp_info.c
+++ src/router/iperf/src/tcp_info.c
@@ -46,7 +46,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/param.h>
-#include <sys/queue.h>
+//#include <sys/queue.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/tcp.h>
