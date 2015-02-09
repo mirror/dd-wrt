714f6ceceb4c6adaf385ebf72d1438c6c88050bf
BrainSlayer <s.gottschall@dd-wrt.com>
2015-02-09 00:06:13 +0000

    dir880l support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26197 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.10/arch/arm/mach-brcm-hnd/board_ns.c
+++ src/linux/universal/linux-3.10/arch/arm/mach-brcm-hnd/board_ns.c
@@ -508,9 +508,15 @@ struct mtd_partition *init_mtd_partitions(hndsflash_t * sfl_info, struct mtd_inf
 
 	if (nvram_match("boardnum", "24") && nvram_match("boardtype", "0x0646")
 	    && nvram_match("boardrev", "0x1110")
-	    && nvram_match("gpio7", "wps_button")) {
+	    && nvram_match("gpio7", "wps_button") && !nvram_match("gpio6", "wps_led")) {
 		maxsize = 0x200000;
 		size = maxsize;
+	}
+
+	if (nvram_match("boardnum", "24") && nvram_match("boardtype", "0x0646")
+	    && nvram_match("boardrev", "0x1110")
+	    && nvram_match("gpio7", "wps_button") && nvram_match("gpio6", "wps_led")) {
+		bootsz = 0x200000;
 	}
 
 	if (nvram_match("boardnum", "24") && nvram_match("boardtype", "0x072F")
