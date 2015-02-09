9b06719e221cc60f353047793e4b69751440c227
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2015-02-09 11:13:54 +0000

    led fixups
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26205 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: ffd1ca11910c8712f133bbd9db1786853dd4b2bf

--- src/router/services/sysinit/sysinit-northstar.c
+++ src/router/services/sysinit/sysinit-northstar.c
@@ -1888,7 +1888,7 @@ void start_sysinit(void)
 				{"measpower1", "0x7f", 0},
 				{"measpower2", "0x7f", 0},
 				{"tssiposslope2g", "1", 0},
-				{"ledbh13","7"},
+				{"ledbh13", "7"},
 				{0, 0, 0}
 			};
 			struct nvram_tuple dir880_1params[] = {
@@ -1984,10 +1984,16 @@ void start_sysinit(void)
 				{"mcsbw1605gmpo", "0", 0},
 				{"mcslr5gmpo", "0", 0},
 				{"pa5ga2", "0xff3d,0x1aee,0xfcc1,0xff33,0x1a56,0xfcc4,0xff3f,0x1b04,0xfcc1,0xff47,0x1b48,0xfcc2", 0},
-				{"ledbh14","7"},
+				{"ledbh14", "7"},
 				{0, 0, 0}
 			};
 
+			nvram_set("0:ledbh1", "11");
+			nvram_set("1:ledbh1", "11");
+			nvram_set("0:ledbh2", "11");
+			nvram_set("1:ledbh2", "11");
+			nvram_set("0:ledbh0", "11");
+			nvram_set("1:ledbh0", "11");
 
 			struct nvram_tuple *t;
 			t = dir880_0params;
