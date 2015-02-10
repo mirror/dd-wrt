9ad1e13bb8aa8513f87776dd0d33a224e6980320
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2015-02-10 11:30:53 +0000

    Update lzo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26228 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 79a2fb9d5d77edf734b4b0718bbd101a14275a4e

--- src/router/lzo/src/lzo_crc.c
+++ src/router/lzo/src/lzo_crc.c
@@ -2,7 +2,7 @@
 
    This file is part of the LZO real-time data compression library.
 
-   Copyright (C) 1996-2014 Markus Franz Xaver Johannes Oberhumer
+   Copyright (C) 1996-2015 Markus Franz Xaver Johannes Oberhumer
    All Rights Reserved.
 
    The LZO library is free software; you can redistribute it and/or
@@ -150,6 +150,4 @@ lzo_crc32(lzo_uint32_t c, const lzo_bytep buf, lzo_uint len)
 #undef LZO_DO16
 
 
-/*
-vi:ts=4:et
-*/
+/* vim:set ts=4 sw=4 et: */
