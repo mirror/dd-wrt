1d1546484dd27833462ce482d22da586a30a7306
BrainSlayer <s.gottschall@dd-wrt.com>
2015-02-11 07:59:49 +0000

    must be little bit more dedicated
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26239 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: b80c1d4bc3576c16de9aab343ec422fca4423ecb

--- tools/removewhitespace.sh
+++ tools/removewhitespace.sh
@@ -3,8 +3,6 @@
 for i in $1/*.asp $1/*.svg $1/*.htm $1/*.html $1/*.js $1/*.css; do
 	if test -e $i; then
 		echo $i
-		cp $i $i.tmp
-		tr -d '\t\n\r\f' < $i.tmp > $i
-		rm $i.tmp
+		./removeshitespace $i
 	fi
 done
