9dd57fa68573d0f3f9a4e614ac9d8226c0b65a4c
BrainSlayer <s.gottschall@dd-wrt.com>
2015-02-11 08:02:38 +0000

    fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26240 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: b3097b877d53dc3b456a0e77a732fa76438fc28b

--- tools/removewhitespace.sh
+++ tools/removewhitespace.sh
@@ -3,6 +3,6 @@
 for i in $1/*.asp $1/*.svg $1/*.htm $1/*.html $1/*.js $1/*.css; do
 	if test -e $i; then
 		echo $i
-		./removeshitespace $i
+		`pwd`/../../../../tools/removewhitespace $i
 	fi
 done
