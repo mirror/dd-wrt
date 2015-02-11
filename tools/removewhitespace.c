007bbad1c24439a2938326ab6851c6bdece7afec
BrainSlayer <s.gottschall@dd-wrt.com>
2015-02-11 07:59:49 +0000

    must be little bit more dedicated
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26239 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: b80c1d4bc3576c16de9aab343ec422fca4423ecb

---
+++ tools/removewhitespace.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+#define FILTER(a) if (!strncmp(&mem[i],a,strlen(a))) \
+		{ \
+		fwrite(&mem[i],strlen(a),1,fp); \
+		i+=strlen(a); \
+		while(mem[i]!=0xa) { \
+		    putc(mem[i++],fp); \
+		} \
+		putc(mem[i],fp); \
+		continue; \
+		} \
+
+#define FILTER_pre(a) if (!strncmp(&mem[i],a,strlen(a))) \
+		{ \
+		putc(0xa,fp); \
+		fwrite(&mem[i],strlen(a),1,fp); \
+		i+=strlen(a); \
+		while(mem[i]!=0xa) { \
+		    putc(mem[i++],fp); \
+		} \
+		putc(mem[i],fp); \
+		continue; \
+		} \
+
+
+
+void main(int argc,char *argv[])
+{
+FILE *fp;
+char *mem;
+fp=fopen(argv[1],"rb");
+fseek(fp,0,SEEK_END);
+long l;
+l = ftell(fp);
+rewind(fp);
+mem=malloc(l);
+fread(mem,l,1,fp);
+fclose(fp);
+fp = fopen(argv[1],"wb");
+int i;
+for (i=0;i<l;i++)
+{
+    FILTER_pre("//<![CDATA[");
+    FILTER_pre("//]]>");
+    if (mem[i]!='\r' &&  mem[i]!='\n' && mem[i]!='\t' && mem[i]!='\f')
+	putc(mem[i],fp);
+}
+fclose(fp);
+}
