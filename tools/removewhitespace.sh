80bd7b3ff91b33c8af30ffc843214b11cd30e49a
BrainSlayer <s.gottschall@dd-wrt.com>
2015-02-12 06:49:38 +0000

    exclude svg
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26252 52c4871e-980c-0410-b1e0-e73912ce01f8

--- tools/removewhitespace.sh
+++ tools/removewhitespace.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in $1/*.asp $1/*.svg $1/*.htm $1/*.html $1/*.js $1/*.css; do
+for i in $1/*.asp $1/*.htm $1/*.html $1/*.js $1/*.css; do
 	if test -e $i; then
 		echo $i
 		`pwd`/../../../../tools/removewhitespace $i
