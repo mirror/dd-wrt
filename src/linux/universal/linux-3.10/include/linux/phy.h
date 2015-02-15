6ca2e2677eccd2b15936397490b03099f3847938
BrainSlayer <s.gottschall@dd-wrt.com>
2015-02-15 16:05:20 +0000

    disable slub for that target. it crashes kernel
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26288 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: ce461a371afd8ba992159b7a2b99460bad430ec9

--- src/linux/universal/linux-3.10/include/linux/phy.h
+++ src/linux/universal/linux-3.10/include/linux/phy.h
@@ -537,8 +537,6 @@ static inline int phy_write(struct phy_device *phydev, u32 regnum, u16 val)
 {
 	return mdiobus_write(phydev->bus, phydev->addr, regnum, val);
 }
-int get_phy_id(struct mii_bus *bus, int addr, u32 *phy_id,
-		      bool is_c45, struct phy_c45_device_ids *c45_ids);
 struct phy_device *phy_device_create(struct mii_bus *bus, int addr, int phy_id,
 		bool is_c45, struct phy_c45_device_ids *c45_ids);
 struct phy_device *get_phy_device(struct mii_bus *bus, int addr, bool is_c45);
