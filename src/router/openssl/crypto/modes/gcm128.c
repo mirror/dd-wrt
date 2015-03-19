46ccd36258f210553de4c1a4b5ed843859dd44c4
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2015-03-19 18:56:46 +0000

    openssl update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26491 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 3c50673ae77d4019e0e47a9d644833a027ae2385

--- src/router/openssl/crypto/modes/gcm128.c
+++ src/router/openssl/crypto/modes/gcm128.c
@@ -852,7 +852,11 @@ void CRYPTO_gcm128_init(GCM128_CONTEXT *ctx, void *key, block128_f block)
     {
         gcm_init_4bit(ctx->Htable, ctx->H.u);
         ctx->gmult = gcm_gmult_4bit;
+#  if defined(GHASH)
         ctx->ghash = gcm_ghash_4bit;
+#  else
+        ctx->ghash = NULL;
+#  endif
     }
 # elif  defined(GHASH_ASM_SPARC)
     if (OPENSSL_sparcv9cap_P[0] & SPARCV9_VIS3) {
@@ -862,7 +866,11 @@ void CRYPTO_gcm128_init(GCM128_CONTEXT *ctx, void *key, block128_f block)
     } else {
         gcm_init_4bit(ctx->Htable, ctx->H.u);
         ctx->gmult = gcm_gmult_4bit;
+#  if defined(GHASH)
         ctx->ghash = gcm_ghash_4bit;
+#  else
+        ctx->ghash = NULL;
+#  endif
     }
 # elif  defined(GHASH_ASM_PPC)
     gcm_init_4bit(ctx->Htable, ctx->H.u);
