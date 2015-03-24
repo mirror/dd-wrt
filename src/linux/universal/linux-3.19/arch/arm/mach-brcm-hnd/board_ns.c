a216ee42af91c033f437449488696877fc301c59
BrainSlayer <s.gottschall@dd-wrt.com>
2015-03-24 14:53:05 +0000

    detection fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26533 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 150b71242f0dece0de589ca3ad4ebfaefe4a77b3

--- src/linux/universal/linux-3.19/arch/arm/mach-brcm-hnd/board_ns.c
+++ src/linux/universal/linux-3.19/arch/arm/mach-brcm-hnd/board_ns.c
@@ -518,6 +518,12 @@ struct mtd_partition *init_mtd_partitions(hndsflash_t * sfl_info, struct mtd_inf
 		bootsz = 0x200000;
 	}
 
+	if (nvram_match("boardnum", "24") && nvram_match("boardtype", "0x0646")
+	    && nvram_match("boardrev", "0x1100")
+	    && nvram_match("gpio8","wps_button")) {
+		bootsz = 0x200000;
+	}
+
 	if (nvram_match("boardnum", "24") && nvram_match("boardtype", "0x072F")
 	    && nvram_match("boardrev", "0x1101")
 	    && nvram_match("gpio7", "wps_button")) {
@@ -758,6 +764,14 @@ static uint lookup_nflash_rootfs_offset(hndnand_t * nfl, struct mtd_info *mtd, i
 	    && nvram_match("boardrev", "0x1110")
 	    && nvram_match("gpio7", "wps_button")) {
 		printk(KERN_INFO "DIR-686L Hack for detecting filesystems\n");
+		blocksize = 65536;
+	}
+
+
+	if (nvram_match("boardnum", "24") && nvram_match("boardtype", "0x0646")
+	    && nvram_match("boardrev", "0x1100")
+	    && nvram_match("gpio8","wps_button")) {
+		printk(KERN_INFO "DIR-660L Hack for detecting filesystems\n");
 		blocksize = 65536;
 	}
 
