53d798dfab07042ba7044abad7a9d69de8f563bc
BrainSlayer <s.gottschall@dd-wrt.com>
2015-03-25 03:53:23 +0000

    almost working so far, just switch is not working yet
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26547 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: e8f09173c88f2066cf1cd96bafb931c32a42b741

--- src/linux/universal/linux-3.10/sanitize.sh
+++ src/linux/universal/linux-3.10/sanitize.sh
@@ -49,6 +49,7 @@ do
 	    echo CONFIG_DAP3310=y >> .config
 	    echo CONFIG_WR1043V2=y >> .config
 	    echo CONFIG_ARCHERC7=y >> .config
+	    echo CONFIG_DIR859=y >> .config
 	    echo CONFIG_MMS344=y >> .config
 	    make oldconfig ARCH=mips
 	    sed -i 's/\CONFIG_WR841V8=y/ /g' .config	    
@@ -66,6 +67,7 @@ do
 	    sed -i 's/\CONFIG_DAP3310=y/ /g' .config	    
 	    sed -i 's/\CONFIG_WR1043V2=y/ /g' .config	    
 	    sed -i 's/\CONFIG_ARCHERC7=y/ /g' .config	    
+	    sed -i 's/\CONFIG_DIR859=y/ /g' .config	    
 	    sed -i 's/\CONFIG_MMS344=y/ /g' .config	    
 	    cp .config $i
     fi
