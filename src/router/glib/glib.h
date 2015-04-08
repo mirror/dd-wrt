bf195b155ad4c6991db38a90661c660e0dd8e7ef
BrainSlayer <s.gottschall@dd-wrt.com>
2015-04-08 22:52:46 +0000

    use internal functions on musl as well,but musl names it different than gnu libc and uclibc
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26652 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 5046d1609a7e3f9e6078edf6303a7170096d93f5

--- src/router/glib/glib.h
+++ src/router/glib/glib.h
@@ -151,6 +151,8 @@ extern "C" {
 
 #ifdef __va_copy
 #  define G_VA_COPY(ap1, ap2)	  __va_copy(ap1, ap2)
+#elif va_copy
+#  define G_VA_COPY(ap1, ap2)	  va_copy(ap1, ap2)
 #else
 #  define G_VA_COPY(ap1, ap2)	  ((ap1) = (ap2))
 #endif
