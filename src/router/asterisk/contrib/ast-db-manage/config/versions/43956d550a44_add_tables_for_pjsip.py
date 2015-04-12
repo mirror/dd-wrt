041ffbdbba81191e23a9e7fa4d61a84d17333822
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2015-04-12 13:43:32 +0000

    asterisk update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26680 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 48f23a08e9d8b2dbc38ee581934ade69c79bc7b7

---
+++ src/router/asterisk/contrib/ast-db-manage/config/versions/43956d550a44_add_tables_for_pjsip.py
@@ -0,0 +1,189 @@
+"""Add tables for pjsip
+
+Revision ID: 43956d550a44
+Revises: 4da0c5f79a9c
+Create Date: 2013-09-30 13:23:59.676690
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '43956d550a44'
+down_revision = '4da0c5f79a9c'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+YESNO_VALUES = ['yes', 'no']
+PJSIP_CID_PRIVACY_VALUES = ['allowed_not_screened', 'allowed_passed_screened',
+                            'allowed_failed_screened', 'allowed',
+                            'prohib_not_screened', 'prohib_passed_screened',
+                            'prohib_failed_screened', 'prohib', 'unavailable']
+PJSIP_100REL_VALUES = ['no', 'required', 'yes']
+PJSIP_CONNECTED_LINE_METHOD_VALUES = ['invite', 'reinvite', 'update']
+PJSIP_DIRECT_MEDIA_GLARE_MITIGATION_VALUES = ['none', 'outgoing', 'incoming']
+PJSIP_DTMF_MODE_VALUES = ['rfc4733', 'inband', 'info']
+PJSIP_IDENTIFY_BY_VALUES = ['username']
+PJSIP_TIMERS_VALUES = ['forced', 'no', 'required', 'yes']
+PJSIP_MEDIA_ENCRYPTION_VALUES = ['no', 'sdes', 'dtls']
+PJSIP_T38UDPTL_EC_VALUES = ['none', 'fec', 'redundancy']
+PJSIP_DTLS_SETUP_VALUES = ['active', 'passive', 'actpass']
+PJSIP_AUTH_TYPE_VALUES = ['md5', 'userpass']
+PJSIP_TRANSPORT_METHOD_VALUES = ['default', 'unspecified', 'tlsv1', 'sslv2',
+                                 'sslv3', 'sslv23']
+PJSIP_TRANSPORT_PROTOCOL_VALUES = ['udp', 'tcp', 'tls', 'ws', 'wss']
+
+
+def upgrade():
+    op.create_table(
+        'ps_endpoints',
+        sa.Column('id', sa.String(40), nullable=False, unique=True),
+        sa.Column('transport', sa.String(40)),
+        sa.Column('aors', sa.String(200)),
+        sa.Column('auth', sa.String(40)),
+        sa.Column('context', sa.String(40)),
+        sa.Column('disallow', sa.String(200)),
+        sa.Column('allow', sa.String(200)),
+        sa.Column('direct_media', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('connected_line_method',
+                  sa.Enum(*PJSIP_CONNECTED_LINE_METHOD_VALUES, name='pjsip_connected_line_method_values')),
+        sa.Column('direct_media_method',
+                  sa.Enum(*PJSIP_CONNECTED_LINE_METHOD_VALUES, name='pjsip_connected_line_method_values')),
+        sa.Column('direct_media_glare_mitigation',
+                  sa.Enum(*PJSIP_DIRECT_MEDIA_GLARE_MITIGATION_VALUES, name='pjsip_direct_media_glare_mitigation_values')),
+        sa.Column('disable_direct_media_on_nat', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('dtmf_mode', sa.Enum(*PJSIP_DTMF_MODE_VALUES, name='pjsip_dtmf_mode_values')),
+        sa.Column('external_media_address', sa.String(40)),
+        sa.Column('force_rport', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('ice_support', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('identify_by', sa.Enum(*PJSIP_IDENTIFY_BY_VALUES, name='pjsip_identify_by_values')),
+        sa.Column('mailboxes', sa.String(40)),
+        sa.Column('moh_suggest', sa.String(40)),
+        sa.Column('outbound_auth', sa.String(40)),
+        sa.Column('outbound_proxy', sa.String(40)),
+        sa.Column('rewrite_contact', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('rtp_ipv6', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('rtp_symmetric', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('send_diversion', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('send_pai', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('send_rpid', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('timers_min_se', sa.Integer),
+        sa.Column('timers', sa.Enum(*PJSIP_TIMERS_VALUES, name='pjsip_timer_values')),
+        sa.Column('timers_sess_expires', sa.Integer),
+        sa.Column('callerid', sa.String(40)),
+        sa.Column('callerid_privacy', sa.Enum(*PJSIP_CID_PRIVACY_VALUES, name='pjsip_cid_privacy_values')),
+        sa.Column('callerid_tag', sa.String(40)),
+        sa.Column('100rel', sa.Enum(*PJSIP_100REL_VALUES, name='pjsip_100rel_values')),
+        sa.Column('aggregate_mwi', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('trust_id_inbound', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('trust_id_outbound', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('use_ptime', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('use_avpf', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('media_encryption', sa.Enum(*PJSIP_MEDIA_ENCRYPTION_VALUES, name='pjsip_media_encryption_values')),
+        sa.Column('inband_progress', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('call_group', sa.String(40)),
+        sa.Column('pickup_group', sa.String(40)),
+        sa.Column('named_call_group', sa.String(40)),
+        sa.Column('named_pickup_group', sa.String(40)),
+        sa.Column('device_state_busy_at', sa.Integer),
+        sa.Column('fax_detect', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('t38_udptl', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('t38_udptl_ec', sa.Enum(*PJSIP_T38UDPTL_EC_VALUES, name='pjsip_t38udptl_ec_values')),
+        sa.Column('t38_udptl_maxdatagram', sa.Integer),
+        sa.Column('t38_udptl_nat', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('t38_udptl_ipv6', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('tone_zone', sa.String(40)),
+        sa.Column('language', sa.String(40)),
+        sa.Column('one_touch_recording', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('record_on_feature', sa.String(40)),
+        sa.Column('record_off_feature', sa.String(40)),
+        sa.Column('rtp_engine', sa.String(40)),
+        sa.Column('allow_transfer', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('allow_subscribe', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('sdp_owner', sa.String(40)),
+        sa.Column('sdp_session', sa.String(40)),
+        sa.Column('tos_audio', sa.Integer),
+        sa.Column('tos_video', sa.Integer),
+        sa.Column('cos_audio', sa.Integer),
+        sa.Column('cos_video', sa.Integer),
+        sa.Column('sub_min_expiry', sa.Integer),
+        sa.Column('from_domain', sa.String(40)),
+        sa.Column('from_user', sa.String(40)),
+        sa.Column('mwi_fromuser', sa.String(40)),
+        sa.Column('dtls_verify', sa.String(40)),
+        sa.Column('dtls_rekey', sa.String(40)),
+        sa.Column('dtls_cert_file', sa.String(200)),
+        sa.Column('dtls_private_key', sa.String(200)),
+        sa.Column('dtls_cipher', sa.String(200)),
+        sa.Column('dtls_ca_file', sa.String(200)),
+        sa.Column('dtls_ca_path', sa.String(200)),
+        sa.Column('dtls_setup', sa.Enum(*PJSIP_DTLS_SETUP_VALUES, name='pjsip_dtls_setup_values')),
+        sa.Column('srtp_tag_32', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+    )
+
+    op.create_index('ps_endpoints_id', 'ps_endpoints', ['id'])
+
+    op.create_table(
+        'ps_auths',
+        sa.Column('id', sa.String(40), nullable=False, unique=True),
+        sa.Column('auth_type', sa.Enum(*PJSIP_AUTH_TYPE_VALUES, name='pjsip_auth_type_values')),
+        sa.Column('nonce_lifetime', sa.Integer),
+        sa.Column('md5_cred', sa.String(40)),
+        sa.Column('password', sa.String(80)),
+        sa.Column('realm', sa.String(40)),
+        sa.Column('username', sa.String(40)),
+    )
+
+    op.create_index('ps_auths_id', 'ps_auths', ['id'])
+
+    op.create_table(
+        'ps_aors',
+        sa.Column('id', sa.String(40), nullable=False, unique=True),
+        sa.Column('contact', sa.String(40)),
+        sa.Column('default_expiration', sa.Integer),
+        sa.Column('mailboxes', sa.String(80)),
+        sa.Column('max_contacts', sa.Integer),
+        sa.Column('minimum_expiration', sa.Integer),
+        sa.Column('remove_existing', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+        sa.Column('qualify_frequency', sa.Integer),
+        sa.Column('authenticate_qualify', sa.Enum(*YESNO_VALUES, name='yesno_values')),
+    )
+
+    op.create_index('ps_aors_id', 'ps_aors', ['id'])
+
+    op.create_table(
+        'ps_contacts',
+        sa.Column('id', sa.String(40), nullable=False, unique=True),
+        sa.Column('uri', sa.String(40)),
+        sa.Column('expiration_time', sa.String(40)),
+        sa.Column('qualify_frequency', sa.Integer),
+    )
+
+    op.create_index('ps_contacts_id', 'ps_contacts', ['id'])
+
+    op.create_table(
+        'ps_domain_aliases',
+        sa.Column('id', sa.String(40), nullable=False, unique=True),
+        sa.Column('domain', sa.String(80)),
+    )
+
+    op.create_index('ps_domain_aliases_id', 'ps_domain_aliases', ['id'])
+
+    op.create_table(
+        'ps_endpoint_id_ips',
+        sa.Column('id', sa.String(40), nullable=False, unique=True),
+        sa.Column('endpoint', sa.String(40)),
+        sa.Column('match', sa.String(80)),
+    )
+
+    op.create_index('ps_endpoint_id_ips_id',
+                    'ps_endpoint_id_ips', ['id'])
+
+
+def downgrade():
+    op.drop_table('ps_endpoints')
+    op.drop_table('ps_auths')
+    op.drop_table('ps_aors')
+    op.drop_table('ps_contacts')
+    op.drop_table('ps_domain_aliases')
+    op.drop_table('ps_endpoint_id_ips')
---
+++ src/router/asterisk/contrib/scripts/refcounter.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python
+"""Process a ref debug log
+
+ This file will process a log file created by the REF_DEBUG
+ build option in Asterisk.
+
+ See http://www.asterisk.org for more information about
+ the Asterisk project. Please do not directly contact
+ any of the maintainers of this project for assistance;
+ the project provides a web site, mailing lists and IRC
+ channels for your use.
+
+ This program is free software, distributed under the terms of
+ the GNU General Public License Version 2. See the LICENSE file
+ at the top of the source tree.
+
+ Copyright (C) 2014, Digium, Inc.
+ Matt Jordan <mjordan@digium.com>
+"""
+
+import sys
+import os
+
+from optparse import OptionParser
+
+
+def parse_line(line):
+    """Parse out a line into its constituent parts.
+
+    Keyword Arguments:
+    line The line from a ref debug log to parse out
+
+    Returns:
+    A dictionary containing the options, or None
+    """
+    tokens = line.strip().split(',', 7)
+    if len(tokens) < 8:
+        print "ERROR: ref debug line '%s' contains fewer tokens than " \
+              "expected: %d" % (line.strip(), len(tokens))
+        return None
+
+    processed_line = {'addr': tokens[0],
+                      'delta': tokens[1],
+                      'thread_id': tokens[2],
+                      'file': tokens[3],
+                      'line': tokens[4],
+                      'function': tokens[5],
+                      'state': tokens[6],
+                      'tag': tokens[7],
+                      }
+    return processed_line
+
+
+def process_file(options):
+    """The routine that kicks off processing a ref file
+
+    Keyword Arguments:
+    filename The full path to the file to process
+
+    Returns:
+    A tuple containing:
+        - A list of objects whose lifetimes were completed
+            (i.e., finished objects)
+        - A list of objects referenced after destruction
+            (i.e., invalid objects)
+        - A list of objects whose lifetimes were not completed
+            (i.e., leaked objects)
+        - A list of objects whose lifetimes are skewed
+            (i.e., Object history starting with an unusual ref count)
+    """
+
+    finished_objects = []
+    invalid_objects = []
+    leaked_objects = []
+    skewed_objects = []
+    current_objects = {}
+    filename = options.filepath
+
+    with open(filename, 'r') as ref_file:
+        for line in ref_file:
+            parsed_line = parse_line(line)
+            if not parsed_line:
+                continue
+
+            invalid = False
+            obj = parsed_line['addr']
+
+            if obj not in current_objects:
+                current_objects[obj] = {'log': [], 'curcount': 1}
+                if 'constructor' in parsed_line['state']:
+                    # This is the normal expected case
+                    pass
+                elif 'invalid' in parsed_line['state']:
+                    invalid = True
+                    current_objects[obj]['curcount'] = 0
+                    if options.invalid:
+                        invalid_objects.append((obj, current_objects[obj]))
+                elif 'destructor' in parsed_line['state']:
+                    current_objects[obj]['curcount'] = 0
+                    if options.skewed:
+                        skewed_objects.append((obj, current_objects[obj]))
+                else:
+                    current_objects[obj]['curcount'] = int(
+                        parsed_line['state'])
+                    if options.skewed:
+                        skewed_objects.append((obj, current_objects[obj]))
+            else:
+                current_objects[obj]['curcount'] += int(parsed_line['delta'])
+
+            current_objects[obj]['log'].append(
+                "[%s] %s:%s %s: %s %s - [%s]" % (
+                    parsed_line['thread_id'],
+                    parsed_line['file'],
+                    parsed_line['line'],
+                    parsed_line['function'],
+                    parsed_line['delta'],
+                    parsed_line['tag'],
+                    parsed_line['state']))
+
+            # It is possible for curcount to go below zero if someone
+            # unrefs an object by two or more when there aren't that
+            # many refs remaining.  This condition abnormally finishes
+            # the object.
+            if current_objects[obj]['curcount'] <= 0:
+                if current_objects[obj]['curcount'] < 0:
+                    current_objects[obj]['log'].append(
+                        "[%s] %s:%s %s: %s %s - [%s]" % (
+                            parsed_line['thread_id'],
+                            parsed_line['file'],
+                            parsed_line['line'],
+                            parsed_line['function'],
+                            "+0",
+                            "Object abnormally finalized",
+                            "**implied destructor**"))
+                    # Highlight the abnormally finished object in the
+                    # invalid section as well as reporting it in the normal
+                    # finished section.
+                    if options.invalid:
+                        invalid_objects.append((obj, current_objects[obj]))
+                if not invalid and options.normal:
+                    finished_objects.append((obj, current_objects[obj]))
+                del current_objects[obj]
+
+    if options.leaks:
+        for key, lines in current_objects.iteritems():
+            leaked_objects.append((key, lines))
+    return (finished_objects, invalid_objects, leaked_objects, skewed_objects)
+
+
+def print_objects(objects, prefix=""):
+    """Prints out the objects that were processed
+
+    Keyword Arguments:
+    objects A list of objects to print
+    prefix  A prefix to print that specifies something about
+            this object
+    """
+
+    print "======== %s Objects ========" % prefix
+    print "\n"
+    for obj in objects:
+        print "==== %s Object %s history ====" % (prefix, obj[0])
+        for line in obj[1]['log']:
+            print line
+        print "\n"
+
+
+def main(argv=None):
+    """Main entry point for the script"""
+
+    ret_code = 0
+
+    if argv is None:
+        argv = sys.argv
+
+    parser = OptionParser()
+
+    parser.add_option("-f", "--file", action="store", type="string",
+                      dest="filepath", default="/var/log/asterisk/refs",
+                      help="The full path to the refs file to process")
+    parser.add_option("-i", "--suppress-invalid", action="store_false",
+                      dest="invalid", default=True,
+                      help="If specified, don't output invalid object "
+                           "references")
+    parser.add_option("-l", "--suppress-leaks", action="store_false",
+                      dest="leaks", default=True,
+                      help="If specified, don't output leaked objects")
+    parser.add_option("-n", "--suppress-normal", action="store_false",
+                      dest="normal", default=True,
+                      help="If specified, don't output objects with a "
+                           "complete lifetime")
+    parser.add_option("-s", "--suppress-skewed", action="store_false",
+                      dest="skewed", default=True,
+                      help="If specified, don't output objects with a "
+                           "skewed lifetime")
+
+    (options, args) = parser.parse_args(argv)
+
+    if not options.invalid and not options.leaks and not options.normal \
+            and not options.skewed:
+        print >>sys.stderr, "All options disabled"
+        return -1
+
+    if not os.path.isfile(options.filepath):
+        print >>sys.stderr, "File not found: %s" % options.filepath
+        return -1
+
+    try:
+        (finished_objects,
+         invalid_objects,
+         leaked_objects,
+         skewed_objects) = process_file(options)
+
+        if options.invalid and len(invalid_objects):
+            print_objects(invalid_objects, "Invalid Referenced")
+            ret_code |= 4
+
+        if options.leaks and len(leaked_objects):
+            print_objects(leaked_objects, "Leaked")
+            ret_code |= 1
+
+        if options.skewed and len(skewed_objects):
+            print_objects(skewed_objects, "Skewed")
+            ret_code |= 2
+
+        if options.normal:
+            print_objects(finished_objects, "Finalized")
+
+    except (KeyboardInterrupt, SystemExit, IOError):
+        print >>sys.stderr, "File processing cancelled"
+        return -1
+
+    return ret_code
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
