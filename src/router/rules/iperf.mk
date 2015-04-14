0736486f75f7c60d9bb9cf839be1ca652126f46d
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2015-04-14 12:26:42 +0000

    use different libdir
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26717 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 5c9bf3cd000f2be147a57a9af549812d577fac2d

--- src/router/rules/iperf.mk
+++ src/router/rules/iperf.mk
@@ -1,5 +1,5 @@
 iperf-configure:
-	cd iperf && ./configure --host=$(ARCH)-linux --prefix=/usr CC="$(CC)" CFLAGS="$(COPTS) -DNEED_PRINTF -D_GNU_SOURCE -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_malloc=malloc" CPPFLAGS="$(COPTS) -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_malloc=malloc"
+	cd iperf && ./configure --host=$(ARCH)-linux --prefix=/usr --libdir=/usr/lib CC="$(CC)" CFLAGS="$(COPTS) -DNEED_PRINTF -D_GNU_SOURCE -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_malloc=malloc" CPPFLAGS="$(COPTS) -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_malloc=malloc"
 
 iperf:
 	$(MAKE) -C iperf
