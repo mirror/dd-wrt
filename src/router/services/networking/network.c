5e90e6f4108ddb9fab09ca76182d0fb31bde0ab9
BrainSlayer <s.gottschall@dd-wrt.com>
2015-04-25 08:44:34 +0000

    swap it
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26753 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: a889c1d2d48286ef93c16ab098c4dda2f9c81154

--- src/router/services/networking/network.c
+++ src/router/services/networking/network.c
@@ -1419,9 +1419,9 @@ void start_lan(void)
 		PORTSETUPWAN("");
 	} else {
 		nvram_setz(lan_ifnames, "eth0 eth1 ath0");
-		PORTSETUPWAN("eth1");
+		PORTSETUPWAN("eth0");
 	}
-	strncpy(ifr.ifr_name, "eth0", IFNAMSIZ);
+	strncpy(ifr.ifr_name, "eth1", IFNAMSIZ);
 	ioctl(s, SIOCGIFHWADDR, &ifr);
 	if (nvram_match("et0macaddr", ""))
 		nvram_set("et0macaddr", ether_etoa(ifr.ifr_hwaddr.sa_data, eabuf));
