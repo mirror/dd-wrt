19e2751818c830660a472a81e5cb0a6e28572637
BrainSlayer <s.gottschall@dd-wrt.com>
2015-04-25 04:10:34 +0000

    wr710 support which is simply a 703 with no change
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26749 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/network.c
+++ src/router/services/networking/network.c
@@ -1413,6 +1413,19 @@ void start_lan(void)
 	if (nvram_match("et0macaddr", ""))
 		nvram_set("et0macaddr", ether_etoa(ifr.ifr_hwaddr.sa_data, eabuf));
 	strcpy(mac, nvram_safe_get("et0macaddr"));
+#elif HAVE_WR710
+	if (getSTA() || getWET() || CANBRIDGE()) {
+		nvram_setz(lan_ifnames, "eth0 eth1 ath0");
+		PORTSETUPWAN("");
+	} else {
+		nvram_setz(lan_ifnames, "eth0 eth1 ath0");
+		PORTSETUPWAN("eth1");
+	}
+	strncpy(ifr.ifr_name, "eth0", IFNAMSIZ);
+	ioctl(s, SIOCGIFHWADDR, &ifr);
+	if (nvram_match("et0macaddr", ""))
+		nvram_set("et0macaddr", ether_etoa(ifr.ifr_hwaddr.sa_data, eabuf));
+	strcpy(mac, nvram_safe_get("et0macaddr"));
 #elif HAVE_WR703
 	if (getSTA() || getWET() || CANBRIDGE()) {
 		nvram_setz(lan_ifnames, "eth1 ath0");
