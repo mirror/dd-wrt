0b606415201cb7994bbc4454c202c63c212b3139
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2015-04-26 10:23:41 +0000

    different phy config
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26759 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 5cac16c68df4b45a1f9037f0cf66fd697d68a21e

--- src/router/services/sysinit/sysinit-hornet.c
+++ src/router/services/sysinit/sysinit-hornet.c
@@ -144,16 +144,7 @@ void start_sysinit(void)
 
 #ifdef HAVE_WR741V4
 #ifdef HAVE_SWCONFIG
-#ifdef HAVE_WR710
-	eval("swconfig", "dev", "eth1", "set", "reset", "1");
-	eval("swconfig", "dev", "eth1", "set", "enable_vlan", "1");
-	eval("swconfig", "dev", "eth1", "vlan", "1", "set", "ports", "0t 3");
-	eval("swconfig", "dev", "eth1", "vlan", "2", "set", "ports", "0t 1");
-	eval("swconfig", "dev", "eth1", "set", "apply");
-	eval("vconfig", "set_name_type", "VLAN_PLUS_VID_NO_PAD");
-	eval("vconfig", "add", "eth1", "1");
-	eval("vconfig", "add", "eth1", "2");
-#else
+#ifndef HAVE_WR710
 	eval("swconfig", "dev", "eth1", "set", "reset", "1");
 	eval("swconfig", "dev", "eth1", "set", "enable_vlan", "1");
 	eval("swconfig", "dev", "eth1", "vlan", "1", "set", "ports", "0 1 2 3 4");
