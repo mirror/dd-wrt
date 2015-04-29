7673331008f042336e19a112e1242bd7629e0725
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2015-04-29 10:39:54 +0000

    revert
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26781 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: cbafb5395a1f48ae0629e611e9db8a266e8348eb

---
+++ src/router/freeradius/src/modules/rlm_example/other.h
@@ -0,0 +1,13 @@
+/* Copyright 2006 The FreeRADIUS server project */
+
+#ifndef _OTHER_H
+#define _OTHER_H
+
+#include <freeradius-devel/ident.h>
+RCSIDH(other_h, "$Id: c41a72698c188b6a4ba09fe2bcf70404146e6d90 $")
+
+/* define the function */
+
+void other_function(void);
+
+#endif /*_OTHER_H*/
