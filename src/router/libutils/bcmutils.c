b5ee086a3683e9acdfc6c903b1d3a7970cba97bf
BrainSlayer <s.gottschall@dd-wrt.com>
2015-05-05 14:53:28 +0000

    allow to shape bandwidth per device and matching protocol. multiple entries are possible
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26829 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: cc2b888d94ab19f47702880f054fb6b28b3fa580

--- src/router/libutils/bcmutils.c
+++ src/router/libutils/bcmutils.c
@@ -1793,6 +1793,91 @@ char *get_filter_services(void)
 	return services;
 }
 
+filters *get_filters_list(void)
+{
+	char word[1024], *next;
+	char protocol[64], ports[64];
+	char delim[] = "<&nbsp;>";
+	char *services;
+	// services = nvram_safe_get("filter_services");
+	services = get_filter_services();
+	int count = 0;
+	split(word, services, next, delim) {
+		count++;
+	}
+	filters *s_filters = (filters *) malloc(sizeof(filters) * count + 1);
+	count = 0;
+	split(word, services, next, delim) {
+		int len = 0;
+		char *name, *prot, *port;
+		int from = 0, to = 0;
+
+		if ((name = strstr(word, "$NAME:")) == NULL || (prot = strstr(word, "$PROT:")) == NULL || (port = strstr(word, "$PORT:")) == NULL)
+			continue;
+
+		/*
+		 * $NAME 
+		 */
+		if (sscanf(name, "$NAME:%3d:", &len) != 1) {
+			free(services);
+			free(s_filters);
+			return NULL;
+		}
+
+		strncpy(name, name + sizeof("$NAME:nnn:") - 1, len);
+		name[len] = '\0';
+		s_filters[count].name = strdup(name);
+
+		/*
+		 * $PROT 
+		 */
+		if (sscanf(prot, "$PROT:%3d:", &len) != 1) {
+			free(services);
+			free(s_filters);
+			return NULL;
+		}
+
+		strncpy(protocol, prot + sizeof("$PROT:nnn:") - 1, len);
+		protocol[len] = '\0';
+		if (!strcmp(protocol, "tcp"))
+			s_filters[count].proto = 1;
+		if (!strcmp(protocol, "udp"))
+			s_filters[count].proto = 2;
+		if (!strcmp(protocol, "both"))
+			s_filters[count].proto = 3;
+		if (!strcmp(protocol, "l7"))
+			s_filters[count].proto = 4;
+		if (!strcmp(protocol, "dpi"))
+			s_filters[count].proto = 5;
+		if (!strcmp(protocol, "p2p"))
+			s_filters[count].proto = 6;
+
+		/*
+		 * $PORT 
+		 */
+		if (sscanf(port, "$PORT:%3d:", &len) != 1) {
+			free(services);
+			free(s_filters);
+			return NULL;
+		}
+
+		strncpy(ports, port + sizeof("$PORT:nnn:") - 1, len);
+		ports[len] = '\0';
+
+		if (sscanf(ports, "%d:%d", &from, &to) != 2) {
+			free(services);
+			free(s_filters);
+			return NULL;
+		}
+		s_filters[count].portfrom = from;
+		s_filters[count].portto = to;
+		count++;
+	}
+	s_filters[count].name = NULL;
+	free(services);
+	return s_filters;
+}
+
 int endswith(char *str, char *cmp)
 {
 	int cmp_len, str_len, i;
