a4e2b7cdef6aebe5391d98e6aac97a014928401c
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2015-05-24 09:31:08 +0000

    conntrack custom, required for new npdi code
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26988 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: fae42871c1fa589a57b794eb8f76224b35d432b7

--- src/linux/universal/linux-3.18/net/netfilter/nf_conntrack_standalone.c
+++ src/linux/universal/linux-3.18/net/netfilter/nf_conntrack_standalone.c
@@ -206,7 +206,7 @@ static int ct_seq_show(struct seq_file *s, void *v)
 			l3proto, l4proto))
 		goto release;
 
-	if (seq_print_acct(s, ct, IP_CT_DIR_ORIGINAL))
+	if (nf_ct_ext_seq_print(s,ct,IP_CT_DIR_ORIGINAL))
 		goto release;
 
 	if (!(test_bit(IPS_SEEN_REPLY_BIT, &ct->status)))
@@ -217,7 +217,7 @@ static int ct_seq_show(struct seq_file *s, void *v)
 			l3proto, l4proto))
 		goto release;
 
-	if (seq_print_acct(s, ct, IP_CT_DIR_REPLY))
+	if (nf_ct_ext_seq_print(s,ct,IP_CT_DIR_REPLY))
 		goto release;
 
 	if (test_bit(IPS_ASSURED_BIT, &ct->status))
