e8cabcbc753902ff439315fb2632fe39d3b3d1a1
brainslayer <brainslayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2015-05-24 12:10:09 +0000

    more protocols
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@26992 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 847c253050dde1beecfa47f81912ea48ea63813c

---
+++ src/router/opendpi/config.h
@@ -0,0 +1,85 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* The JSON-C library is present */
+/* #undef HAVE_JSON_C */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <netinet/in.h> header file. */
+#define HAVE_NETINET_IN_H 1
+
+/* Define if you have POSIX threads libraries and header files. */
+#define HAVE_PTHREAD 1
+
+/* Have PTHREAD_PRIO_INHERIT. */
+#define HAVE_PTHREAD_PRIO_INHERIT 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* SVN Release */
+#define NDPI_GIT_RELEASE "r1.5.2"
+
+/* Last SVN change */
+#define NDPI_SVN_DATE "Sun May 24 11:52:21 CEST 2015"
+
+/* Name of package */
+#define PACKAGE "libndpi"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "libndpi"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libndpi 1.5.2"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "libndpi"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.5.2"
+
+/* Define to necessary symbol if this constant uses a non-standard name on
+   your system. */
+/* #undef PTHREAD_CREATE_JOINABLE */
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "1.5.2"
