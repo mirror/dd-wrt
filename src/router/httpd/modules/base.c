022800d7e60a772011fd62dbd1d3f8853c6dadc9
BrainSlayer <s.gottschall@dd-wrt.com>
2015-05-30 15:48:54 +0000

    rename source file for basic httpd operation, since the file name has nothing todo anymore with its original source
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@27121 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: b03f9c3a6a48c49efba08a622a50460ebc5c6166

--- src/router/httpd/modules/base.c
+++ src/router/httpd/modules/base.c
@@ -1,18 +1,25 @@
-
 /*
- * Broadcom Home Gateway Reference Design
- * Web Page Configuration Support Routines
+ * DD-WRT base.c (derived originally from broadcom.c WRT54G linksys gpl source. but honestly, there is nothing left of it)
  *
- * Copyright 2001-2003, Broadcom Corporation
- * All Rights Reserved.
+ * Copyright (C) 2005 - 2015 Sebastian Gottschall <sebastian.gottschall@newmedia-net.de>
  *
- * THIS SOFTWARE IS OFFERED "AS IS", AND BROADCOM GRANTS NO WARRANTIES OF ANY
- * KIND, EXPRESS OR IMPLIED, BY STATUTE, COMMUNICATION OR OTHERWISE. BROADCOM
- * SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A SPECIFIC PURPOSE OR NONINFRINGEMENT CONCERNING THIS SOFTWARE.
- * $Id: broadcom.c,v 1.9 2005/11/30 11:53:42 seg Exp $
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id:
  */
-
+ 
 #ifdef WEBS
 #include <webs.h>
 #include <uemf.h>
