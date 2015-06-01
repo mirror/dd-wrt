5e1a9a4a5266abfa0aca0cdd3cb3412fee1d9088
BrainSlayer <s.gottschall@dd-wrt.com>
2015-06-01 16:06:39 +0000

    fix configuration for DAP3662
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@27170 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 56a02fa181859a3e42be1c80d42ee926c450e580

--- src/router/services/sysinit/sysinit-wasp.c
+++ src/router/services/sysinit/sysinit-wasp.c
@@ -87,6 +87,11 @@ void start_sysinit(void)
 	eval("swconfig", "dev", "eth0", "set", "enable_vlan", "1");
 	eval("swconfig", "dev", "eth0", "vlan", "1", "set", "ports", "0t 2 3 4 5");
 	eval("swconfig", "dev", "eth0", "vlan", "2", "set", "ports", "0t 1");
+#elif defined (HAVE_DAP3662)
+	eval("swconfig", "dev", "eth0", "set", "reset", "1");
+	eval("swconfig", "dev", "eth0", "set", "enable_vlan", "0");
+	eval("swconfig", "dev", "eth0", "vlan", "1", "set", "ports", "0 2 3 4 5");
+	eval("swconfig", "dev", "eth0", "vlan", "2", "set", "ports", "1 6");
 #elif defined (HAVE_DIR862)
 	eval("swconfig", "dev", "eth0", "set", "reset", "1");
 	eval("swconfig", "dev", "eth0", "set", "enable_vlan", "0");
@@ -130,7 +135,7 @@ void start_sysinit(void)
 		eval("ifconfig", "eth0", "hw", "ether", mac);
 	}
 #endif
-#ifdef HAVE_MMS344
+#if defined(HAVE_MMS344) && !defined(HAVE_DAP3662)
 	FILE *fp = fopen("/dev/mtdblock/6", "rb");
 	if (fp) {
 		unsigned char buf2[256];
@@ -170,35 +175,7 @@ void start_sysinit(void)
 		nvram_set("et0macaddr_safe", ether_etoa((unsigned char *)ifr.ifr_hwaddr.sa_data, eabuf));
 		close(s);
 	}
-#if defined(HAVE_ARCHERC7) || defined(HAVE_DIR859)
-	FILE *fp = fopen("/dev/mtdblock/5", "rb");
-	FILE *out = fopen("/tmp/archerc7-board.bin", "wb");
-	if (fp) {
-		fseek(fp, 20480, SEEK_SET);
-		int i;
-		for (i = 0; i < 6; i++)
-			putc(getc(fp), out);
-		char *mac = "\x00\x01\x02\x03\x04\x05";
-		if ((s = socket(AF_INET, SOCK_RAW, IPPROTO_RAW))) {
-			char eabuf[32];
-
-			strncpy(ifr.ifr_name, "eth0", IFNAMSIZ);
-			ioctl(s, SIOCGIFHWADDR, &ifr);
-			mac = (unsigned char *)ifr.ifr_hwaddr.sa_data;
-			close(s);
-		}
-		for (i = 0; i < 6; i++)
-			putc(mac[i], out);
-		fseek(fp, 20492, SEEK_SET);
-		for (i = 0; i < 2104; i++)
-			putc(getc(fp), out);
-		fclose(fp);
-		fclose(out);
-		eval("rm", "-f", "/tmp/ath10k-board.bin");
-		eval("ln", "-s", "/tmp/archerc7-board.bin", "/tmp/ath10k-board.bin");
-	}
-#endif
-#if defined(HAVE_DAP3662)
+#if defined(HAVE_ARCHERC7) || defined(HAVE_DIR859) || defined(HAVE_DAP3662)
 	FILE *fp = fopen("/dev/mtdblock/5", "rb");
 	FILE *out = fopen("/tmp/archerc7-board.bin", "wb");
 	if (fp) {
