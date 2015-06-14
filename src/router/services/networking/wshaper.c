680365f556864b3eaa12d2b53e7cb344ad8fe97c
BrainSlayer <s.gottschall@dd-wrt.com>
2015-06-14 21:49:18 +0000

    fix wrong var
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@27297 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: f0436deb39ecef04ec15cb593f4e171044b7dade

--- src/router/services/networking/wshaper.c
+++ src/router/services/networking/wshaper.c
@@ -379,7 +379,7 @@ static void aqos_tables(void)
 			break;
 		if (!strcmp(proto, "|") || !strcmp(proto, "none")) {
 			/* mark interface interface as priority / bandwidth limits based without any service filter */
-			    addIF(data);
+			addIF(data);
 		}
 		char chainname_in[32];
 		sprintf(chainname_in, "FILTER_%s_IN", data);
@@ -399,7 +399,7 @@ static void aqos_tables(void)
 
 		if (nvram_match("wshaper_dev", "LAN")) {
 			if (nvram_nmatch("1", "%s_bridged", data)) {
-				if (!is_ath9k(word))
+				if (!is_ath9k(data))
 					eval("ifconfig", data, "down");
 				down_addIF(data);
 				eval("iptables", "-t", "mangle", "-D", "INPUT", "-m", "physdev", "--physdev-in", data, "-j", "IMQ", "--todev", "0");
@@ -418,7 +418,7 @@ static void aqos_tables(void)
 //                      eval("iptables", "-t", "mangle", "-I", "FILTER_IN", "2", "-m", "mark", "--mark", nullmask, "-m", "physdev", "--physdev-in", data, "-j", chainname_in);
 //                      eval("iptables", "-t", "mangle", "-I", "FILTER_OUT", "2", "-m", "mark", "--mark", nullmask, "-m", "physdev", "--physdev-is-bridged", "--physdev-out", data, "-j", chainname_out);
 
-			if (!is_ath9k(word))
+			if (!is_ath9k(data))
 				eval("ifconfig", data, "down");
 			down_addIF(data);
 			eval("iptables", "-t", "mangle", "-D", "FILTER_IN", "-m", "physdev", "--physdev-in", data, "-j", chainname_in);
@@ -771,7 +771,7 @@ static int svqos_iptables(void)
 			sprintf(s_level, "%d", atoi(level) / 10);
 			/* outgoing data */
 			if (is_in_bridge(data)) {
-				if (!is_ath9k(word))
+				if (!is_ath9k(data))
 					eval("ifconfig", data, "down");
 				down_addIF(data);
 				eval("iptables", "-t", "mangle", "-I", "VPN_OUT", "1", "-m", "physdev", "--physdev-is-bridged", "--physdev-out", data, "-j", "DSCP", "--set-dscp", s_level);
