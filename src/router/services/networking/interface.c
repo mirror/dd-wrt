449e6e3ca25ea6c61b31bcfa02e0f17e0997deca
BrainSlayer <s.gottschall@dd-wrt.com>
2015-06-17 19:50:21 +0000

    mms344 wan port
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@27351 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 03df8ee26864863e18755af1f05df6322926a062

--- src/router/services/networking/interface.c
+++ src/router/services/networking/interface.c
@@ -449,6 +449,8 @@ int flush_interfaces(void)
 	snprintf(all_ifnames, 255, "%s %s %s", "eth1", nvram_safe_get("lan_ifnames"), nvram_safe_get("wan_ifnames"));
 #elif HAVE_WNR2000
 	snprintf(all_ifnames, 255, "%s %s %s", "eth0 eth1", nvram_safe_get("lan_ifnames"), nvram_safe_get("wan_ifnames"));
+#elif HAVE_MMS344
+	snprintf(all_ifnames, 255, "%s %s %s", "vlan1 vlan2", nvram_safe_get("lan_ifnames"), nvram_safe_get("wan_ifnames"));
 #elif HAVE_WZR450HP2
 	snprintf(all_ifnames, 255, "%s %s %s", "eth0 eth1", nvram_safe_get("lan_ifnames"), nvram_safe_get("wan_ifnames"));
 #elif HAVE_WDR3500
