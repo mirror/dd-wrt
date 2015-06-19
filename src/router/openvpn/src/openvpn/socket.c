27a23de7521d020e2d78c78e85d8aee0dc76d8da
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2015-06-19 09:09:02 +0000

    openvpn update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@27381 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 4fdcfd12dfc59fad3ba9e6394878d0e896c2299e

--- src/router/openvpn/src/openvpn/socket.c
+++ src/router/openvpn/src/openvpn/socket.c
@@ -168,10 +168,6 @@ openvpn_getaddrinfo (unsigned int flags,
 
   ASSERT(res);
 
-#if defined(HAVE_RES_INIT)
-  res_init ();
-#endif
-
   if (!hostname)
     hostname = "::";
 
@@ -228,6 +224,9 @@ openvpn_getaddrinfo (unsigned int flags,
        */
       while (true)
         {
+#ifndef WIN32
+	  res_init ();
+#endif
           /* try hostname lookup */
           hints.ai_flags = 0;
           dmsg (D_SOCKET_DEBUG, "GETADDRINFO flags=0x%04x ai_family=%d ai_socktype=%d",
@@ -246,10 +245,13 @@ openvpn_getaddrinfo (unsigned int flags,
                     }
                   else
                     {
+		      /* turn success into failure (interrupted syscall) */
                       if (0 == status) {
                           ASSERT(res);
                           freeaddrinfo(*res);
-                          res = NULL;
+                          *res = NULL;
+                          status = EAI_AGAIN;	/* = temporary failure */
+                          errno = EINTR;
                       }
                       goto done;
                     }
