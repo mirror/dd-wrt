066f342db10def0ac19ceee90e00d75347dcf22c
BrainSlayer <s.gottschall@dd-wrt.com>
2015-06-21 14:12:08 +0000

    fix rate display, adjust center frequency for 40 mhz
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@27409 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 40c887541d60f490de58848937846406cb5fa601

--- src/router/libutils/mac80211info.c
+++ src/router/libutils/mac80211info.c
@@ -1147,7 +1147,8 @@ struct wifi_interface *mac80211_get_interface(char *dev)
 				break;
 			case NL80211_CHAN_WIDTH_40:
 				interface->width = 40;
-				interface->center1 += 10;				
+				if (interface->center1 != -1)
+					interface->center1 += 10;				
 				break;
 			case NL80211_CHAN_WIDTH_80:
 				interface->width = 80;
