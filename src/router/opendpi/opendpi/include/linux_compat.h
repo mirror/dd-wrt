a30e593a7ee9107817a829120b9bb90573e42106
BrainSlayer <s.gottschall@dd-wrt.com>
2015-07-06 06:44:32 +0000

    reformat code
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@27493 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/opendpi/opendpi/include/linux_compat.h
+++ src/router/opendpi/opendpi/include/linux_compat.h
@@ -22,7 +22,6 @@
  *
  */
 
-
 #ifndef __NDPI_LINUX_COMPAT_H__
 #define __NDPI_LINUX_COMPAT_H__
 
@@ -42,66 +41,62 @@
 #endif
 #endif
 
-#pragma pack(push, 1)  /* push current alignment to stack */
-#pragma pack(1)     /* set alignment to 1 byte boundary */
+#pragma pack(push, 1)		/* push current alignment to stack */
+#pragma pack(1)			/* set alignment to 1 byte boundary */
 
-#pragma pack(pop)   /* restore original alignment from stack */
+#pragma pack(pop)		/* restore original alignment from stack */
 
 struct ndpi_ethhdr {
-  u_char h_dest[6];       /* destination eth addr */
-  u_char h_source[6];     /* source ether addr    */
-  u_int16_t h_proto;      /* packet type ID field */
+	u_char h_dest[6];	/* destination eth addr */
+	u_char h_source[6];	/* source ether addr    */
+	u_int16_t h_proto;	/* packet type ID field */
 };
 
 struct ndpi_80211q {
-  u_int16_t vlanId;
-  u_int16_t protoType;
+	u_int16_t vlanId;
+	u_int16_t protoType;
 };
 
 struct ndpi_iphdr {
-#if defined(__LITTLE_ENDIAN__) 
-  u_int8_t ihl:4, version:4;
+#if defined(__LITTLE_ENDIAN__)
+	u_int8_t ihl:4, version:4;
 #elif defined(__BIG_ENDIAN__)
-  u_int8_t version:4, ihl:4;
+	u_int8_t version:4, ihl:4;
 #else
 # error "Byte order must be defined"
 #endif
-  u_int8_t tos;
-  u_int16_t tot_len;
-  u_int16_t id;
-  u_int16_t frag_off;
-  u_int8_t ttl;
-  u_int8_t protocol;
-  u_int16_t check;
-  u_int32_t saddr;
-  u_int32_t daddr;
+	u_int8_t tos;
+	u_int16_t tot_len;
+	u_int16_t id;
+	u_int16_t frag_off;
+	u_int8_t ttl;
+	u_int8_t protocol;
+	u_int16_t check;
+	u_int32_t saddr;
+	u_int32_t daddr;
 };
-
 
 #ifdef WIN32
 
-typedef unsigned char  u_char;
+typedef unsigned char u_char;
 typedef unsigned short u_short;
-typedef unsigned int   uint;
-typedef unsigned long  u_long;
-typedef u_char  u_int8_t;
+typedef unsigned int uint;
+typedef unsigned long u_long;
+typedef u_char u_int8_t;
 typedef u_short u_int16_t;
-typedef uint   u_int32_t;
+typedef uint u_int32_t;
 
-#define _WS2TCPIP_H_ /* Avoid compilation problems */
+#define _WS2TCPIP_H_		/* Avoid compilation problems */
 #define HAVE_SIN6_LEN
-
 
 /* IPv6 address */
 /* Already defined in WS2tcpip.h */
-struct ndpi_win_in6_addr
-{
-  union
-  {
-    u_int8_t u6_addr8[16];
-    u_int16_t u6_addr16[8];
-    u_int32_t u6_addr32[4];
-  } in6_u;
+struct ndpi_win_in6_addr {
+	union {
+		u_int8_t u6_addr8[16];
+		u_int16_t u6_addr16[8];
+		u_int32_t u6_addr32[4];
+	} in6_u;
 #ifdef s6_addr
 #undef s6_addr
 #endif
@@ -115,18 +110,17 @@ struct ndpi_win_in6_addr
 #endif
 
 #define s6_addr                 in6_u.u6_addr8
-  // #define s6_addr16               in6_u.u6_addr16
-  // #define s6_addr32               in6_u.u6_addr32
+	// #define s6_addr16               in6_u.u6_addr16
+	// #define s6_addr32               in6_u.u6_addr32
 
 };
 
 #define in6_addr win_in6_addr
 
 /* Generic extension header.  */
-struct ndpi_ip6_ext
-{
-  u_int8_t  ip6e_nxt;		/* next header.  */
-  u_int8_t  ip6e_len;		/* length in units of 8 octets.  */
+struct ndpi_ip6_ext {
+	u_int8_t ip6e_nxt;	/* next header.  */
+	u_int8_t ip6e_len;	/* length in units of 8 octets.  */
 };
 
 #define s6_addr16		__u6_addr.__u6_addr16
@@ -137,52 +131,50 @@ struct ndpi_ip6_ext
 #endif
 #endif
 
-
-
 struct ndpi_in6_addr {
-  union {
-    u_int8_t   __u6_addr8[16];
-    u_int16_t  __u6_addr16[8];
-    u_int32_t  __u6_addr32[4];
-  } __u6_addr;                    /* 128-bit IP6 address */
+	union {
+		u_int8_t __u6_addr8[16];
+		u_int16_t __u6_addr16[8];
+		u_int32_t __u6_addr32[4];
+	} __u6_addr;		/* 128-bit IP6 address */
 };
 
 struct ndpi_ip6_hdr {
-  union {
-    struct ndpi_ip6_hdrctl {
-      u_int32_t ip6_un1_flow;
-      u_int16_t ip6_un1_plen;
-      u_int8_t ip6_un1_nxt;
-      u_int8_t ip6_un1_hlim;
-    } ip6_un1;
-    u_int8_t ip6_un2_vfc;
-  } ip6_ctlun;
-  struct ndpi_in6_addr ip6_src;
-  struct ndpi_in6_addr ip6_dst;
+	union {
+		struct ndpi_ip6_hdrctl {
+			u_int32_t ip6_un1_flow;
+			u_int16_t ip6_un1_plen;
+			u_int8_t ip6_un1_nxt;
+			u_int8_t ip6_un1_hlim;
+		} ip6_un1;
+		u_int8_t ip6_un2_vfc;
+	} ip6_ctlun;
+	struct ndpi_in6_addr ip6_src;
+	struct ndpi_in6_addr ip6_dst;
 };
 
 struct ndpi_tcphdr {
-  u_int16_t source;
-  u_int16_t dest;
-  u_int32_t seq;
-  u_int32_t ack_seq;
+	u_int16_t source;
+	u_int16_t dest;
+	u_int32_t seq;
+	u_int32_t ack_seq;
 #if defined(__LITTLE_ENDIAN__)
-  u_int16_t res1:4, doff:4, fin:1, syn:1, rst:1, psh:1, ack:1, urg:1, ece:1, cwr:1;
+	u_int16_t res1:4, doff:4, fin:1, syn:1, rst:1, psh:1, ack:1, urg:1, ece:1, cwr:1;
 #elif defined(__BIG_ENDIAN__)
-  u_int16_t doff:4, res1:4, cwr:1, ece:1, urg:1, ack:1, psh:1, rst:1, syn:1, fin:1;
+	u_int16_t doff:4, res1:4, cwr:1, ece:1, urg:1, ack:1, psh:1, rst:1, syn:1, fin:1;
 #else
 # error "Byte order must be defined"
-#endif  
-  u_int16_t window;
-  u_int16_t check;
-  u_int16_t urg_ptr;
+#endif
+	u_int16_t window;
+	u_int16_t check;
+	u_int16_t urg_ptr;
 };
 
 struct ndpi_udphdr {
-  u_int16_t source;
-  u_int16_t dest;
-  u_int16_t len;
-  u_int16_t check;
+	u_int16_t source;
+	u_int16_t dest;
+	u_int16_t len;
+	u_int16_t check;
 };
 
 #endif
