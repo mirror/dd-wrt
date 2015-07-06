422a9a95b9ff5c720e99b47158564452e98c015d
BrainSlayer <s.gottschall@dd-wrt.com>
2015-07-06 06:42:54 +0000

    add snapchat
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@27491 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: bf943b31f814f19f2ded8ac5248580bfcfe513d0

--- src/router/opendpi/src/xt_ndpi.h
+++ src/router/opendpi/src/xt_ndpi.h
@@ -82,9 +82,9 @@ struct xt_ndpi_tginfo {
 "megaco",          "redis",                "pando",            "vhua",          "telegram", \
 "vevo",            "pandora",              "quic",    "popo",           "manolito", "feidian", \
 "gadugadu", "i23v5", "secondlife", "whatsapp_voice", "eaq", "meu", "torcedor", "kakaotalk", \
-"kakaotalk_voice" , "twitch", "quickplay", "tim", "mpegts"
+"kakaotalk_voice" , "twitch", "quickplay", "tim", "mpegts", "snapchat"
 
-#define NDPI_PROTOCOL_MAXNUM 204
+#define NDPI_PROTOCOL_MAXNUM 205
 #endif
 
 #endif /* _LINUX_NETFILTER_XT_NDPI_H */
