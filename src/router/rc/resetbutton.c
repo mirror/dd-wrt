9d80443d034ba1bd047b7b3a06b2f8df1d4eb13c
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2015-07-08 18:41:32 +0000

    fix typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@27505 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 510c46d766889e44a133023c5feb6e522f22aca4

--- src/router/rc/resetbutton.c
+++ src/router/rc/resetbutton.c
@@ -214,7 +214,6 @@ int getbuttonstate()
 #elif defined(HAVE_DIR862)
 int getbuttonstate()
 {
-	return 0;
 	int ret = get_gpio(17);
 
 	if (ret == 0)
@@ -342,6 +341,14 @@ int getbuttonstate()
 int getbuttonstate()
 {
 	int ret = get_gpio(12);
+	if (ret == 0)
+		return 1;
+	return 0;
+}
+#elif defined(HAVE_DAP2330)
+int getbuttonstate()
+{
+	int ret = get_gpio(17);
 	if (ret == 0)
 		return 1;
 	return 0;
