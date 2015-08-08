bff9385b304cc998c82f88560c9e72323621be82
kong <USER@DOMAIN>
2015-08-08 20:27:36 +0000

    nvram space for mvebu
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@27575 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/bcmutils.c
+++ src/router/libutils/bcmutils.c
@@ -1956,6 +1956,8 @@ void addAction(char *action)
 
 #ifdef NVRAM_SPACE_256
 #define NVRAMSPACE NVRAM_SPACE_256
+#elif HAVE_MVEBU
+#define NVRAMSPACE 0x10000
 #else
 #define NVRAMSPACE NVRAM_SPACE
 #endif
