dbfe0d5b7e8671e060b1d949acd5b33c66b53cd6
BrainSlayer <s.gottschall@dd-wrt.com>
2015-08-11 22:16:37 +0000

    not for 3.18
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@27603 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 69a2856b8864bc294b6569a6474da1d4c11492e8

--- src/linux/universal/linux-3.18/drivers/leds/trigger/ledtrig-morse.c
+++ src/linux/universal/linux-3.18/drivers/leds/trigger/ledtrig-morse.c
@@ -26,7 +26,6 @@
 #include <linux/list.h>
 #include <linux/spinlock.h>
 #include <linux/device.h>
-#include <linux/sysdev.h>
 #include <linux/timer.h>
 #include <linux/ctype.h>
 #include <linux/leds.h>
