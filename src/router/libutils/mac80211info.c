e53d7f5a78c1f8b98924c34eb1714628f9a50087
BrainSlayer <s.gottschall@dd-wrt.com>
2015-08-17 17:43:28 +0000

    for debugging only
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@27658 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/mac80211info.c
+++ src/router/libutils/mac80211info.c
@@ -248,7 +248,6 @@ unsigned int get_ath10kreg(char *ifname, unsigned int reg)
 void set_ath10kreg(char *ifname, unsigned int reg, unsigned int value)
 {
 	char file[64];
-	fprintf(stderr, "write reg %X value %X\n", reg, value);
 	int phy = get_ath9k_phy_ifname(ifname);
 	sprintf(file, "/sys/kernel/debug/ieee80211/phy%d/ath10k/reg_addr", phy);
 	FILE *fp = fopen(file, "wb");
