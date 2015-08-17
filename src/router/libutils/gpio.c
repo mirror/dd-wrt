a5a8ffb307f85a562c18941a6466c2f1b89a389f
brainslayer <brainslayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2015-08-17 14:10:39 +0000

    gpios
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@27646 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/gpio.c
+++ src/router/libutils/gpio.c
@@ -40,7 +40,7 @@
 #include <fcntl.h>
 #include <errno.h>
 
-#if defined(HAVE_UNIWIP) || defined(HAVE_OCTEON)
+#if defined(HAVE_UNIWIP) || defined(HAVE_OCTEON)  || defined(HAVE_MVEBU)
 void set_gpio(int pin, int value)
 {
 	char str[32];
