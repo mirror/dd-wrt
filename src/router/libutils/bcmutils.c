ba539efc00b863f4ac574a6e9b061df7fea22dcc
BrainSlayer <s.gottschall@dd-wrt.com>
2015-08-20 22:12:27 +0000

    introduce is_mvebu for special handling and remove always true case in is_ath10k
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@27708 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: a053e123b392311d89faa42f97a4456f95e90685

--- src/router/libutils/bcmutils.c
+++ src/router/libutils/bcmutils.c
@@ -377,7 +377,7 @@ int has_mimo(char *prefix)
 int has_ac(char *prefix)
 {
 #ifdef HAVE_ATH10K
-	return is_ath10k(prefix);
+	return is_ath10k(prefix) || is_mvebu(prefix);
 #elif HAVE_ATH9K
 	return 0;
 #else
