b0974b22d557ad0bc953eb8748bd511e221ff079
BrainSlayer <s.gottschall@dd-wrt.com>
2015-08-20 22:17:51 +0000

    no runtime detection here yet
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@27710 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 290c7e1407b653cefee50663ff8009a6ba769440

--- src/router/rc/wland.c
+++ src/router/rc/wland.c
@@ -680,8 +680,7 @@ int main(int argc, char **argv)
 	while (1) {
 		sleep(WLAND_INTERVAL);
 		do_wlan_check();
-#ifdef HAVE_ATH10K
-		if (!is_mvebu(prefix)) {
+#if defined(HAVE_ATH10K) && !defined(HAVE_MVEBU)
 			int c = getdevicecount();
 			char dev[32];
 			int i;
@@ -693,7 +692,6 @@ int main(int argc, char **argv)
 					set_ath10kdistance(dev, atoi(nvram_safe_get(dst)));
 				}
 			}
-		}
 #endif
 
 	}
