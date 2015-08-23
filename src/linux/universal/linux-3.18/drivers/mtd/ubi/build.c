ce188e74b22906b010ad1e8b5371cafe64fda67e
BrainSlayer <s.gottschall@dd-wrt.com>
2015-08-23 13:55:16 +0000

    boot from ubi based flash
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@27724 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 4678f82672faf875d28fcf99739726a549348de9

--- src/linux/universal/linux-3.18/drivers/mtd/ubi/build.c
+++ src/linux/universal/linux-3.18/drivers/mtd/ubi/build.c
@@ -1209,6 +1209,49 @@ static struct mtd_info * __init open_mtd_device(const char *mtd_dev)
 	return mtd;
 }
 
+/*
+ * This function tries attaching mtd partitions named either "ubi" or "data"
+ * during boot.
+ */
+static void __init ubi_auto_attach(void)
+{
+	int err;
+	struct mtd_info *mtd;
+
+	/* try attaching mtd device named "ubi" or "data" */
+	mtd = open_mtd_device("ubi");
+	if (IS_ERR(mtd))
+		mtd = open_mtd_device("data");
+
+	if (!IS_ERR(mtd)) {
+		size_t len;
+		char magic[4];
+
+		/* check for a valid ubi magic */
+		err = mtd_read(mtd, 0, 4, &len, (void *) magic);
+		if (!err && len == 4 && strncmp(magic, "UBI#", 4)) {
+			pr_err("UBI error: no valid UBI magic found inside mtd%d", mtd->index);
+			put_mtd_device(mtd);
+			return;
+		}
+
+		/* auto-add only media types where UBI makes sense */
+		if (mtd->type == MTD_NANDFLASH ||
+		    mtd->type == MTD_NORFLASH ||
+		    mtd->type == MTD_DATAFLASH ||
+		    mtd->type == MTD_MLCNANDFLASH) {
+			mutex_lock(&ubi_devices_mutex);
+			pr_notice("UBI: auto-attach mtd%d", mtd->index);
+			err = ubi_attach_mtd_dev(mtd, UBI_DEV_NUM_AUTO, 0, 0);
+			mutex_unlock(&ubi_devices_mutex);
+			if (err < 0) {
+				pr_err("UBI error: cannot attach mtd%d", mtd->index);
+				put_mtd_device(mtd);
+			}
+		}
+	}
+}
+
 static int __init ubi_init(void)
 {
 	int err, i, k;
@@ -1297,6 +1340,12 @@ static int __init ubi_init(void)
 				goto out_detach;
 		}
 	}
+
+	/* auto-attach mtd devices only if built-in to the kernel and no ubi.mtd
+	 * parameter was given */
+	if (config_enabled(CONFIG_MTD_ROOTFS_ROOT_DEV) &&
+	    !ubi_is_module() && !mtd_devs)
+		ubi_auto_attach();
 
 	err = ubiblock_init();
 	if (err) {
