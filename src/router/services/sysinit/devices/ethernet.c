dd6fb59955821c69da34f425c9bbc6260495438d
BrainSlayer <s.gottschall@dd-wrt.com>
2015-09-27 17:03:06 +0000

    format list
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@27856 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 6a07b7f68b279f615235d1ac71444373568993cc

--- src/router/services/sysinit/devices/ethernet.c
+++ src/router/services/sysinit/devices/ethernet.c
@@ -27,11 +27,17 @@
 #include <bcmnvram.h>
 
 static char pcidrivers[] = {
-	"dmfe de4x5 de2104x tulip xircom_cb winbond-840 uli526x hp100 mlx4_en mlx4_core starfire w5300 w5100 sungem cassini niu sunhme cxgb cxgb4 cxgb4vf s2io vxge myri10ge fealnx enc28j60 netxen_nic qlge qla3xxx qlcnic ks8851_mll ks8851 ksz884x via-velocity via-rhine fmvj18x_cs enic tg3 cnic bnx2x jme stmmac sfc dnet bna ns83820 natsemi ipg tlan r6040 forcedeth sky2 skge epic100 smsc9420 smsc911x smc91c92_cs be2net xirc2ps_cs amd8111e pcnet32 nmclan_cs et131x 8139too 8139cp r8169 sundance dl2k tehuti ixgbe e1000e igbvf e1000gcu e1000gbe e100 ixgb igb e1000 sc92031 sis190 sis900 axnet_cs 8390 ne2k-pci pcnet_cs 3c59x 3c589_cs 3c574_cs typhoon ethoc acenic yellowfin hamachi samsung-sxgbe atl1c atl1e atl1 atl2 alx"
+	"dmfe de4x5 de2104x tulip xircom_cb winbond-840 uli526x hp100 mlx4_en mlx4_core starfire w5300 w5100 sungem cassini niu"
+	" sunhme cxgb cxgb4 cxgb4vf s2io vxge myri10ge fealnx enc28j60 netxen_nic qlge qla3xxx qlcnic ks8851_mll ks8851 ksz884x"
+	" via-velocity via-rhine fmvj18x_cs enic tg3 cnic bnx2x jme stmmac sfc dnet bna ns83820 natsemi ipg tlan r6040 forcedeth"
+	" sky2 skge epic100 smsc9420 smsc911x smc91c92_cs be2net xirc2ps_cs amd8111e pcnet32 nmclan_cs et131x 8139too 8139cp r8169"
+	" sundance dl2k tehuti ixgbe e1000e igbvf e1000gcu e1000gbe e100 ixgb igb e1000 sc92031 sis190 sis900 axnet_cs 8390 ne2k-pci"
+	" pcnet_cs 3c59x 3c589_cs 3c574_cs typhoon ethoc acenic yellowfin hamachi samsung-sxgbe atl1c atl1e atl1 atl2 alx"
 };
 
 static char usbdrivers[] = {
-	"r8152 dm9601 gl620a smsc95xx ax88179_178a cdc_mbim asix rtl8150 pegasus net1080 sr9700 kaweth cx82310_eth usbnet lg-vl600 hso zaurus int51x1 mcs7830 catc smsc75xx plusb kalmia"
+	"r8152 dm9601 gl620a smsc95xx ax88179_178a asix rtl8150 pegasus net1080 sr9700 kaweth cx82310_eth usbnet"
+	" lg-vl600 hso zaurus int51x1 mcs7830 catc smsc75xx plusb kalmia"
 };
 
 static int detect_driver(char *drivers, char *list)
