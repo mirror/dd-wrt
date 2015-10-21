be35693b261e18f1df31e34ed94035fad8746009
BrainSlayer <s.gottschall@dd-wrt.com>
2015-10-21 15:25:18 +0000

    update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28003 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/squid/src/dns/LookupDetails.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 1996-2015 The Squid Software Foundation and contributors
+ *
+ * Squid software is distributed under GPLv2+ license and includes
+ * contributions from numerous individuals and organizations.
+ * Please see the COPYING and CONTRIBUTORS files for details.
+ */
+
+/* DEBUG: section 78    DNS lookups */
+
+#ifndef SQUID_DNS_LOOKUPDETAILS_H
+#define SQUID_DNS_LOOKUPDETAILS_H
+
+#include "SquidString.h"
+
+namespace Dns
+{
+
+/// encapsulates DNS lookup results
+class LookupDetails
+{
+public:
+    LookupDetails() : wait(-1) {} ///< no error, no lookup delay (i.e., no lookup)
+    LookupDetails(const String &anError, int aWait) : error(anError), wait(aWait) {}
+
+    std::ostream &print(std::ostream &os) const;
+
+public:
+    String error; ///< error message for unsuccessful lookups; empty otherwise
+    int wait; ///< msecs spent waiting for the lookup (if any) or -1 (if none)
+};
+
+} // namespace Dns
+
+inline std::ostream &
+operator <<(std::ostream &os, const Dns::LookupDetails &dns)
+{
+    return dns.print(os);
+}
+
+#endif /* SQUID_DNS_LOOKUPDETAILS_H */
+
---
+++ src/router/squid/src/dns/forward.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 1996-2015 The Squid Software Foundation and contributors
+ *
+ * Squid software is distributed under GPLv2+ license and includes
+ * contributions from numerous individuals and organizations.
+ * Please see the COPYING and CONTRIBUTORS files for details.
+ */
+
+#ifndef _SQUID_SRC_DNS_FORWARD_H
+#define _SQUID_SRC_DNS_FORWARD_H
+
+namespace Ip
+{
+class Address;
+}
+
+class rfc1035_rr;
+
+typedef void IDNSCB(void *, const rfc1035_rr *, int, const char *);
+
+// generic DNS API
+namespace Dns
+{
+
+class LookupDetails;
+
+void Init(void);
+void Shutdown(void);
+
+} // namespace Dns
+
+// internal DNS client API
+void idnsALookup(const char *, IDNSCB *, void *);
+void idnsPTRLookup(const Ip::Address &, IDNSCB *, void *);
+
+#endif /* _SQUID_SRC_DNS_FORWARD_H */
+
