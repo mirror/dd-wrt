aab38d487de62ff20c0bf581aff225eeb201c4d3
BrainSlayer <s.gottschall@dd-wrt.com>
2015-11-01 22:21:09 +0000

    update minidlna
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28071 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 46f0b14db520bc214ae797733ef995cf32506c5c

--- src/router/minidlna/minidlna/upnpdescgen.c
+++ src/router/minidlna/minidlna/upnpdescgen.c
@@ -30,7 +30,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "upnpdescstrings.h"
+#include "config.h"
 #include "getifaddr.h"
 #include "upnpdescgen.h"
 #include "minidlnapath.h"
--- src/router/minidlna/minidlna/upnphttp.h
+++ src/router/minidlna/minidlna/upnphttp.h
@@ -50,7 +50,7 @@
 #define __UPNPHTTP_H__
 
 #include <netinet/in.h>
-#include "queue.h"
+#include <sys/queue.h>
 
 #include "minidlnatypes.h"
 #include "config.h"
