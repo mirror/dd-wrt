81fea04a05df3516e5ede050b613d9e8ce445592
BrainSlayer <s.gottschall@dd-wrt.com>
2015-11-08 13:48:53 +0000

    fix support list
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28101 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 791b29e8084e20d604ae6a260a621016af637881

--- src/router/tools/archerc9/mktplinkfw3.c
+++ src/router/tools/archerc9/mktplinkfw3.c
@@ -406,7 +406,7 @@ void * generate_sysupgrade_image(const struct flash_partition_entry *flash_parts
 
 
 /** Generates an image for CPE210/220/510/520 and writes it to a file */
-static void do_cpe510(const char *support_list,const char *cfe_name, const char *output, const char *kernel_image, const char *rootfs_image, bool add_jffs2_eof, bool sysupgrade) {
+static void do_cpe510(const char *support_list,int size,const char *cfe_name, const char *output, const char *kernel_image, const char *rootfs_image, bool add_jffs2_eof, bool sysupgrade) {
 	struct image_partition_entry parts[7] = {};
 
 	
@@ -415,7 +415,7 @@ static void do_cpe510(const char *support_list,const char *cfe_name, const char 
 	parts[2] = read_file("os-image", kernel_image, false);
 	parts[3] = read_file("file-system", rootfs_image, add_jffs2_eof);
 	parts[4] = make_softversion(softversion, sizeof(softversion)-1);
-	parts[5] = make_support_list(support_list, strlen(support_list));
+	parts[5] = make_support_list(support_list, size - 1);
 	size_t len;
 	void *image;
 	if (sysupgrade)
@@ -514,13 +514,13 @@ int main(int argc, char *argv[]) {
 		error(1, 0, "no output filename has been specified");
 
 	if (strcmp(board, "ARCHERC9") == 0)
-		do_cpe510(archerc9_support_list, "archerc9_cfe.bin", output, kernel_image, rootfs_image, add_jffs2_eof, sysupgrade);
+		do_cpe510(archerc9_support_list,sizeof(archerc9_support_list), "archerc9_cfe.bin", output, kernel_image, rootfs_image, add_jffs2_eof, sysupgrade);
 	else if (strcmp(board, "ARCHERC9v2") == 0)
-		do_cpe510(archerc9v2_support_list, "archerc9v2_cfe.bin", output, kernel_image, rootfs_image, add_jffs2_eof, sysupgrade);
+		do_cpe510(archerc9v2_support_list,sizeof(archerc9v2_support_list), "archerc9v2_cfe.bin", output, kernel_image, rootfs_image, add_jffs2_eof, sysupgrade);
 	else if (strcmp(board, "ARCHERC8") == 0)
-		do_cpe510(archerc8_support_list, "archerc8_cfe.bin", output, kernel_image, rootfs_image, add_jffs2_eof, sysupgrade);
+		do_cpe510(archerc8_support_list,sizeof(archerc8_support_list), "archerc8_cfe.bin", output, kernel_image, rootfs_image, add_jffs2_eof, sysupgrade);
 	else if (strcmp(board, "ARCHERC8v2") == 0)
-		do_cpe510(archerc8v2_support_list, "archerc8v2_cfe.bin", output, kernel_image, rootfs_image, add_jffs2_eof, sysupgrade);
+		do_cpe510(archerc8v2_support_list,sizeof(archerc8v2_support_list), "archerc8v2_cfe.bin", output, kernel_image, rootfs_image, add_jffs2_eof, sysupgrade);
 	else
 		error(1, 0, "unsupported board %s", board);
 
