5fd8a98f4e7f0077024c1ac93c3c56d26e410c91
BrainSlayer <s.gottschall@dd-wrt.com>
2015-11-09 18:17:46 +0000

    fix dir885
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28105 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/utils/utils.c
+++ src/router/services/utils/utils.c
@@ -49,6 +49,7 @@ static void getMac(char *newmac)
 		break;
 	case ROUTER_NETGEAR_R8000:
 	case ROUTER_TRENDNET_TEW828:
+	case ROUTER_DLINK_DIR885:
 		strcpy(newmac, nvram_safe_get("et2macaddr"));
 		break;
 	default:
