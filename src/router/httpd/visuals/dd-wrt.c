95f3b9bda817d694de841f1f1257085ca44989a7
kong <USER@DOMAIN>
2015-11-13 20:28:19 +0000

    clk rate
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28163 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: eec4bfbe59846994a0511a8c32c78e8cecde4c14

--- src/router/httpd/visuals/dd-wrt.c
+++ src/router/httpd/visuals/dd-wrt.c
@@ -337,6 +337,21 @@ void ej_get_clkfreq(webs_t wp, int argc, char_t ** argv)
 	}
 	return;
 }
+#elif HAVE_IPQ806X
+void ej_get_clkfreq(webs_t wp, int argc, char_t ** argv)
+{
+	FILE *fp = fopen("/sys/kernel/debug/clk/krait0_pri_mux/clk_rate", "rb");
+	if (fp) {
+		int freq;
+		fscanf(fp, "%d", &freq);
+		fclose(fp);
+		websWrite(wp, "%d", freq / 1000000);
+	} else {
+		websWrite(wp, "1400");
+
+	}
+	return;
+}
 #elif HAVE_X86
 void ej_get_clkfreq(webs_t wp, int argc, char_t ** argv)
 {
