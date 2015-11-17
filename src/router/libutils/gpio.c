d8aa288a32df9565b3c5b5f81daa73605fcca0cf
kong <USER@DOMAIN>
2015-11-17 23:15:41 +0000

    gpios
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28219 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/gpio.c
+++ src/router/libutils/gpio.c
@@ -456,6 +456,44 @@ void set_gpio(int gpio, int value)
 			break;
 		}
 	}
+	
+	if (brand == ROUTER_LINKSYS_EA8500) {
+		switch (gpio) {
+		case 0:
+			sysprintf("echo %d > /sys/class/leds/ea8500\\:white\\:power/brightness", value);
+			break;
+		case 1:
+			sysprintf("echo %d > /sys/class/leds/ea8500\\:white\\:wifi/brightness", value);
+			break;
+		case 2:	
+			sysprintf("echo %d > /sys/class/leds/ea8500\\:white\\:esata/brightness", value);
+			break;
+		case 3:
+			sysprintf("echo %d > /sys/class/leds/ea8500\\:amber\\:usb1/brightness", value);
+			break;
+		case 4:
+			sysprintf("echo %d > /sys/class/leds/ea8500\\:amber\\:usb3/brightness", value);
+			break;
+		case 5:
+			sysprintf("echo %d > /sys/class/leds/ea8500\\:red\\:wan/brightness", value);
+			break;
+		case 6:
+			sysprintf("echo %d > /sys/class/leds/ea8500\\:white\\:internet/brightness", value);
+			break;
+		case 7:
+			sysprintf("echo %d > /sys/class/leds/ea8500\\:white\\:rfkill/brightness", value);
+			break;
+		case 8:
+			sysprintf("echo %d > /sys/class/leds/ea8500\\:white\\:wps/brightness", value);
+			break;
+		case 9:
+			sysprintf("echo %d > /sys/class/leds/ea8500\\:amber\\:status/brightness", value);
+			break;
+		default:
+			i_set_gpio(gpio, value);
+			break;
+		}
+	}
 
 }
 
