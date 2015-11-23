80345ee2912053478cecc7aef922d81d66ac4b17
brainslayer <brainslayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2015-11-23 14:14:50 +0000

    update nmap
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28294 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/nmap/FPModel.h
@@ -0,0 +1,10 @@
+#ifndef _FPMODEL_H_
+#define _FPMODEL_H_
+
+extern struct model FPModel;
+extern double FPscale[][2];
+extern double FPmean[][695];
+extern double FPvariance[][695];
+extern FingerMatch FPmatches[];
+
+#endif
