10dcd8b047db9d27eab75e4d74c302937fc6ac06
BrainSlayer <s.gottschall@dd-wrt.com>
2015-11-25 15:19:00 +0000

    detect bootdevice by reading BOOTCONFIG, fall back to default if not found
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28324 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/drivers/mtd/ubi/build.c
+++ src/linux/universal/linux-3.18/drivers/mtd/ubi/build.c
@@ -1209,6 +1209,100 @@ static struct mtd_info * __init open_mtd_device(const char *mtd_dev)
 	return mtd;
 }
 
+#define ALT_PART_NAME_LENGTH 16
+struct per_part_info {
+	char name[ALT_PART_NAME_LENGTH];
+	uint32_t primaryboot;
+	uint32_t upgraded;
+};
+
+#define NUM_ALT_PARTITION 3
+typedef struct {
+#define _SMEM_DUAL_BOOTINFO_MAGIC       0xA5A3A1A0
+	/* Magic number for identification when reading from flash */
+	uint32_t magic;
+	/* upgradeinprogress indicates to attempting the upgrade */
+	uint32_t upgradeinprogress;
+	/* numaltpart indicate number of alt partitions */
+	uint32_t numaltpart;
+
+	struct per_part_info per_part_entry[NUM_ALT_PARTITION];
+} ipq_smem_bootconfig_info_t;
+
+/* version 2 */
+#define SMEM_DUAL_BOOTINFO_MAGIC_START 0xA3A2A1A0
+#define SMEM_DUAL_BOOTINFO_MAGIC_END 0xB3B2B1B0
+
+typedef struct {
+	uint32_t magic_start;
+	uint32_t upgradeinprogress;
+	uint32_t age;
+	uint32_t numaltpart;
+	struct per_part_info per_part_entry[NUM_ALT_PARTITION];
+	uint32_t magic_end;
+} ipq_smem_bootconfig_v2_info_t;
+
+
+
+
+static int getbootdevice(void)
+{
+	struct mtd_info *mtd;
+	size_t len;
+	int i;
+	ipq_smem_bootconfig_info_t *ipq_smem_bootconfig_info = NULL;
+	ipq_smem_bootconfig_v2_info_t *ipq_smem_bootconfig_v2_info = NULL;
+	unsigned int *smem, *p;
+	mtd = open_mtd_device("BOOTCONFIG");
+	if (!mtd)
+	    return -1;
+
+	smem = (unsigned int *)vmalloc(0x60000);
+	memset(smem, 0, 0x60000);
+	mtd_read(mtd, 0, 0x60000, &len, (void *)smem);
+	put_mtd_device(mtd);
+	if (len != 0x60000)
+	    return -1;
+	p = smem;
+	for (i = 0; i < 0x60000 - sizeof(ipq_smem_bootconfig_v2_info); i += 4) {
+		if (*p == SMEM_DUAL_BOOTINFO_MAGIC_START) {
+			ipq_smem_bootconfig_v2_info = (ipq_smem_bootconfig_v2_info_t *)p;
+			break;
+		}
+		if (*p == _SMEM_DUAL_BOOTINFO_MAGIC) {
+			ipq_smem_bootconfig_info = (ipq_smem_bootconfig_info_t *)p;
+			break;
+		}
+		p++;
+	}
+
+	if (ipq_smem_bootconfig_v2_info) {
+		for (i = 0; i < ipq_smem_bootconfig_v2_info->numaltpart; i++) {
+			if (!strncmp(ipq_smem_bootconfig_v2_info->per_part_entry[i].name, "rootfs", 6)) {
+				vfree(smem);
+				if (ipq_smem_bootconfig_v2_info->per_part_entry[i].primaryboot)
+					return 1;
+				else
+					return 0;
+			}
+		}
+	}
+	if (ipq_smem_bootconfig_info) {
+		for (i = 0; i < ipq_smem_bootconfig_info->numaltpart; i++) {
+			if (!strncmp(ipq_smem_bootconfig_info->per_part_entry[i].name, "rootfs", 6)) {
+				vfree(smem);
+				if (ipq_smem_bootconfig_info->per_part_entry[i].primaryboot)
+					return 1;
+				else
+					return 0;
+			}
+		}
+	}
+	vfree(smem);
+	printk(KERN_INFO "no valid bootconfig found, use default\n");
+	return -1;
+}
+
 /*
  * This function tries attaching mtd partitions named either "ubi" or "data"
  * during boot.
@@ -1219,10 +1313,16 @@ static void __init ubi_auto_attach(void)
 	struct mtd_info *mtd;
 
 	/* try attaching mtd device named "ubi" or "data" */
+	int bootdevice = getbootdevice();
+	if (bootdevice == 1)
+	    mtd = open_mtd_device("linux2");
+	else if (bootdevice == 0)
+	    mtd = open_mtd_device("linux");
+	else { // default behaviour
 	mtd = open_mtd_device("ubi");
 	if (IS_ERR(mtd))
 		mtd = open_mtd_device("data");
-
+	}
 	if (!IS_ERR(mtd)) {
 		size_t len;
 		char magic[4];
