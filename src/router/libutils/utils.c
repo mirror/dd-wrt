d61752c2b7bb7bb9e4e12498083f9bc35c0917c8
BrainSlayer <s.gottschall@dd-wrt.com>
2015-11-27 04:51:23 +0000

    always the same crap. new version, no other significant change
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28368 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: a168e68283e55450f2ce11ecea7eb1d817010efa

--- src/router/libutils/utils.c
+++ src/router/libutils/utils.c
@@ -2663,6 +2663,11 @@ int internal_getRouterBrand()
 	nvram_default_get("ath0_txantenna", "3");
 	setRouter("Dlink DAP-2230");
 	return ROUTER_BOARD_PB42;
+#elif HAVE_WR841V10
+	nvram_default_get("ath0_rxantenna", "3");
+	nvram_default_get("ath0_txantenna", "3");
+	setRouter("TP-Link TL-WR841ND v10");
+	return ROUTER_BOARD_PB42;
 #elif HAVE_WR841V9
 	nvram_default_get("ath0_rxantenna", "3");
 	nvram_default_get("ath0_txantenna", "3");
