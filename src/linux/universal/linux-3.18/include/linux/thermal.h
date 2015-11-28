67e16a22c333f8acc782165fb3636428d38c30ce
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2015-11-28 01:57:11 +0000

    bad typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28374 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/include/linux/thermal.h
+++ src/linux/universal/linux-3.18/include/linux/thermal.h
@@ -323,7 +323,7 @@ void thermal_zone_of_sensor_unregister(struct device *dev,
 static inline struct thermal_zone_device *
 thermal_zone_of_sensor_register(struct device *dev, int id,
 				void *data, int (*get_temp)(void *, long *),
-				int (*get_trend)(void *, long *));
+				int (*get_trend)(void *, long *))
 {
 	return NULL;
 }
