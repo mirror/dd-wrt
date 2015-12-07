af9f27b52f6ebc2f8f1f51ba5465c3378d90a941
BrainSlayer <s.gottschall@dd-wrt.com>
2015-12-07 11:10:46 +0000

    it seems that kong had weekend in his head :-)
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28470 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 0781b02beadfe72acfcacc9653ccea98902c2a1a

--- src/router/services/sysinit/sysinit.c
+++ src/router/services/sysinit/sysinit.c
@@ -1233,7 +1233,7 @@ void start_restore_defaults(void)
 	else
 		generic = wrt1200;
 #elif HAVE_IPQ806X
-	struct nvram_param generic[] = {
+	struct nvram_param ipq806x[] = {
 		{"lan_ifname", "br0"},
 		{"lan_ifnames", "eth0 eth1 ath0 ath1",},
 		{"wan_ifname", "eth0"},
@@ -1251,6 +1251,14 @@ void start_restore_defaults(void)
 		{"wan_default", "vlan2"},
 		{0, 0}
 	};
+
+	struct nvram_param *generic = NULL;
+
+	if (wrt_brand == ROUTER_LINKSYS_EA8500)
+		generic = ea8500;
+	else
+		generic = ipq806x;
+
 #elif HAVE_WDR4900
 	struct nvram_param generic[] = {
 		{"lan_ifname", "br0"},
@@ -2011,8 +2019,6 @@ void start_restore_defaults(void)
     || defined(HAVE_STORM) || defined(HAVE_ADM5120) || defined(HAVE_CA8)  || defined(HAVE_OCTEON)
 	int brand = getRouterBrand();
 	linux_overrides = generic;
-	if( brand == ROUTER_LINKSYS_EA8500 )
-		linux_overrides = ea8500;
 
 	if (nvram_invmatch("sv_restore_defaults", "0"))	// ||
 		// nvram_invmatch("os_name", 
