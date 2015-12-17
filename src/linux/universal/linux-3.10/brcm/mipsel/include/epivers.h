ca769b9e99191c40bf18afa6adc7a538133eb13b
BrainSlayer <s.gottschall@dd-wrt.com>
2015-12-17 01:48:24 +0000

    Update driver
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28534 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 3af7b89e4e829caa1a84dd81e08540369bbf9645

--- src/linux/universal/linux-3.10/brcm/mipsel/include/epivers.h
+++ src/linux/universal/linux-3.10/brcm/mipsel/include/epivers.h
@@ -24,23 +24,23 @@
 
 #define	EPI_MINOR_VERSION	14
 
-#define	EPI_RC_NUMBER		89
+#define	EPI_RC_NUMBER		131
 
-#define	EPI_INCREMENTAL_NUMBER	21
+#define	EPI_INCREMENTAL_NUMBER	1608
 
 #define	EPI_BUILD_NUMBER	0
 
-#define	EPI_VERSION		7, 14, 89, 21
+#define	EPI_VERSION		7, 14, 131, 1608
 
-#define	EPI_VERSION_NUM		0x070e5915
+#define	EPI_VERSION_NUM		0x070e8364
 
-#define EPI_VERSION_DEV		7.14.89
+#define EPI_VERSION_DEV		7.14.131
 
 /* Driver Version String, ASCII, 32 chars max */
 #ifdef WLTEST
-#define	EPI_VERSION_STR		"7.14.89.21 (r524987 WLTEST)"
+#define	EPI_VERSION_STR		"7.14.131.1608 (r589038 WLTEST)"
 #else
-#define	EPI_VERSION_STR		"7.14.89.21 (r524987)"
+#define	EPI_VERSION_STR		"7.14.131.1608 (r589038)"
 #endif
 
 #endif /* _epivers_h_ */
