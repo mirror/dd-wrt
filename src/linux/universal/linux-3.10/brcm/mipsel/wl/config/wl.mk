c4a6b9966614fa91e664e097c44fb2a2b43371e2
BrainSlayer <s.gottschall@dd-wrt.com>
2015-12-24 01:10:24 +0000

    rollback driver
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28596 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 5278881c412871c92454c6d0976359d5570213be

--- src/linux/universal/linux-3.10/brcm/mipsel/wl/config/wl.mk
+++ src/linux/universal/linux-3.10/brcm/mipsel/wl/config/wl.mk
@@ -14,11 +14,8 @@
 # WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
 # OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
 # CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-# $Id: wl.mk 569486 2015-07-08 08:34:51Z $
+# $Id: wl.mk 524987 2015-01-08 14:34:41Z $
 
-
-WLFLAGS += -DBCM943217ROUTER_ACI_SCANMORECH
-WLFLAGS += -DBPHY_DESENSE
 
 
 ifeq ($(NO_BCMDBG_ASSERT), 1)
@@ -337,16 +334,8 @@ endif
 	WLFILES_SRC_HI += src/wl/sys/wlc_stf.c
 	WLFILES_SRC_HI += src/wl/sys/wlc_lq.c
 	ifeq ($(WL_PROT_OBSS),1)
-		WL_MODESW := 1
 		WLFLAGS += -DWL_PROT_OBSS
 		WLFILES_SRC_HI += src/wl/sys/wlc_prot_obss.c
-	endif
-	ifeq ($(WL_MODESW),1)
-		WLFLAGS += -DWL_MODESW
-		WLFILES_SRC_HI += src/wl/sys/wlc_modesw.c
-		ifeq ($(DEBUG),1)
-			WLFLAGS += -DWL_MODESW_TIMECAL
-		endif
 	endif
 	WLFILES_SRC_HI += src/wl/sys/wlc_pm.c
 	WLFILES_SRC_HI += src/wl/sys/wlc_btcx.c
@@ -1020,9 +1009,6 @@ endif
 ifeq ($(UCAST_UPNP), 1)
 	WLFLAGS += -DWL_UCAST_UPNP
 endif
-ifeq ($(IGMPQ_FILTER), 1)
-	WLFLAGS += -DWL_WMF_IGMP_QUERY_FILTER
-endif
 #endif
 
 #ifdef MCAST_REGEN
@@ -1596,10 +1582,6 @@ endif
 ifeq ($(WLP2P),1)
 	WLFLAGS += -DWLP2P
 	WLFILES_SRC_HI += src/wl/sys/wlc_p2p.c
-	## Defining WAR4360_UCODE only for NIC driver
-	ifneq ($(WL_SPLIT),1)
-		WLFLAGS += -DWAR4360_UCODE
-	endif
 	WLFLAGS += -DWL_BSSCFG_TX_SUPR -DWIFI_ACT_FRAME
 	WLMCNX := 1
 ifndef WLMCHAN
@@ -1805,14 +1787,6 @@ else
 endif
 
 
-
-
-ifeq ($(STBLINUX),1)
-	WLFLAGS += -DSTB
-	ifeq ($(BCMEXTNVM),1)
-		WLFLAGS += -DBCMEXTNVM -DBCM47XX -DNVRAM_TARGET_DIR="\"$(NVRAM_TARGET_DIR)\""
-	endif
-endif
 
 #ifndef LINUX_HYBRID
 # AP/ROUTER with SDSTD
@@ -2533,6 +2507,12 @@ ifeq ($(PKTC_DONGLE),1)
 	WLFLAGS += -DPKTC_DONGLE
 endif
 
+#ifdef WLAIBSS
+ifeq ($(WLAIBSS), 1)
+        WLFLAGS += -DWLAIBSS
+        WLFILES_SRC_HI += src/wl/sys/wlc_aibss.c
+endif
+#endif
 
 #ifdef WLIPFO
 ifeq ($(WLIPFO), 1)
@@ -2573,18 +2553,7 @@ endif
 ifeq ($(ARMV7L),1)
 	ifeq ($(STBLINUX),1)
 		WLFLAGS += -DSTBLINUX
-		WLFLAGS += -DSTB
-		ifneq ($(BCM_SECURE_DMA),1)
-                        WLFLAGS += -DBCM47XX
-                endif
-		ifeq ($(BCM_SECURE_DMA),1)
-			WLFILES_SRC_LO += src/shared/stbutils.c
-		endif
 	endif
-endif
-
-ifeq ($(STBLINUX),1)
-	WLFLAGS += -DSTBLINUX
 endif
 
 # Legacy WLFILES pathless definition, please use new src relative path
@@ -2615,14 +2584,8 @@ ifeq ($(WLROAMPROF),1)
 endif
 
 # enabling secure DMA feature
-
 ifeq ($(BCM_SECURE_DMA),1)
 	WLFLAGS += -DBCM_SECURE_DMA
-endif
-
-#Support for STBC
-ifeq ($(WL11N_STBC_RX_ENABLED),1)
-	WLFLAGS += -DWL11N_STBC_RX_ENABLED
 endif
 
 # Work-arounds for ROM compatibility - relocate struct fields that were excluded in ROMs,
