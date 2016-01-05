8dc4123bba16aa4736a2df81d63c8d865745b8ac
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2016-01-05 21:21:38 +0000

    jwap606 related patches
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28670 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/interface.c
+++ src/router/services/networking/interface.c
@@ -461,6 +461,8 @@ int flush_interfaces(void)
 	snprintf(all_ifnames, 255, "%s %s %s", "eth0 eth1", nvram_safe_get("lan_ifnames"), nvram_safe_get("wan_ifnames"));
 #elif HAVE_WDR3500
 	snprintf(all_ifnames, 255, "%s %s %s", "eth0 eth1", nvram_safe_get("lan_ifnames"), nvram_safe_get("wan_ifnames"));
+#elif HAVE_JWAP606
+	snprintf(all_ifnames, 255, "%s %s %s", "eth0", nvram_safe_get("lan_ifnames"), nvram_safe_get("wan_ifnames"));
 #elif HAVE_WASP
 	snprintf(all_ifnames, 255, "%s %s %s", "vlan1 vlan2", nvram_safe_get("lan_ifnames"), nvram_safe_get("wan_ifnames"));
 #elif HAVE_WDR2543
