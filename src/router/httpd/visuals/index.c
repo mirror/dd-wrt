ec7654f04f246ffe360c0cd4f550297abcc2cbc3
kong <USER@DOMAIN>
2016-01-11 21:55:25 +0000

    add it here as well
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28764 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: f247e8c491be3288152989a396e6f6916f929079

--- src/router/httpd/visuals/index.c
+++ src/router/httpd/visuals/index.c
@@ -81,6 +81,8 @@ void ej_get_clone_mac(webs_t wp, int argc, char_t ** argv)
 					c = strdup(nvram_safe_get("et1macaddr"));
 				else if (getRouterBrand() == ROUTER_NETGEAR_R8000)
 					c = strdup(nvram_safe_get("et2macaddr"));
+				else if (getRouterBrand() == ROUTER_NETGEAR_R8500)
+					c = strdup(nvram_safe_get("et2macaddr"));
 				else
 					c = strdup(nvram_safe_get("et0macaddr"));
 			}
