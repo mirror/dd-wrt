d42249ecfbf208633a0483fa5813bf932d7c8dba
kong <USER@DOMAIN>
2016-01-11 14:03:15 +0000

    add define
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28745 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/bcmutils.c
+++ src/router/libutils/bcmutils.c
@@ -1956,6 +1956,8 @@ void addAction(char *action)
 
 #ifdef NVRAM_SPACE_256
 #define NVRAMSPACE NVRAM_SPACE_256
+#elif HAVE_NVRAM_128
+#define NVRAMSPACE 0x20000
 #elif HAVE_MVEBU
 #define NVRAMSPACE 0x10000
 #elif HAVE_IPQ806X
