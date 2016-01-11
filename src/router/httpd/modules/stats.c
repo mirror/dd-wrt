5d61fc3f853a902a7c3baf5eda1232461cfa6402
kong <kong@52c4871e-980c-0410-b1e0-e73912ce01f8>
2016-01-11 09:36:37 +0000

    extend definition
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28717 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/stats.c
+++ src/router/httpd/modules/stats.c
@@ -31,7 +31,11 @@ extern int http_post(const char *server, char *buf, size_t count);
 
 #ifdef NVRAM_SPACE_256
 #define NVRAMSPACE NVRAM_SPACE_256
+#elif HAVE_NVRAM_128
+#define NVRAMSPACE 0x20000
 #elif HAVE_MVEBU
+#define NVRAMSPACE 0x10000
+#elif HAVE_IPQ806X
 #define NVRAMSPACE 0x10000
 #else
 #define NVRAMSPACE NVRAM_SPACE
