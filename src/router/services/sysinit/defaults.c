459b3454ea9c8db068608445a275f921a52521ff
kong <USER@DOMAIN>
2016-01-11 09:52:27 +0000

    for autopower webif setting
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28725 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 6dd80d3f50a5ba5bf64500a4d6d8d26971e343fd

--- src/router/services/sysinit/defaults.c
+++ src/router/services/sysinit/defaults.c
@@ -2073,6 +2073,9 @@ struct nvram_param srouter_defaults[] = {
 	{"wl0_txpwr", "71"},
 	{"wl1_txpwr", "71"},
 	{"wl2_txpwr", "71"},
+	{"wl0_txpwrusr", "1"},
+	{"wl1_txpwrusr", "1"},
+	{"wl2_txpwrusr", "1"},
 #endif
 #endif
 
@@ -2180,6 +2183,7 @@ struct nvram_param srouter_defaults[] = {
 	{"ipv6_pf_len", "64"},
 	{"ipv6_mtu", ""},
 	{"ipv6_tun_client_addr_pref", "64"},
+	{"ipv6_tun_upd_url", "See tunnelbroker account"},
 	{"radvd_enable", "1"},
 	{"radvd_custom", "0"},
 	{"radvd_conf", ""},
