12ffec6ae89412e0687ab68c5f0e02f7b3b0a79c
BrainSlayer <s.gottschall@dd-wrt.com>
2016-01-12 05:23:03 +0000

    bus pci bus number bug
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28775 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 0bde528c0281f1cdb5da3bb7b0ba1ab6b3b60762

--- src/linux/universal/linux-4.4/drivers/pci/probe.c
+++ src/linux/universal/linux-4.4/drivers/pci/probe.c
@@ -2023,11 +2023,6 @@ unsigned int pci_scan_child_bus(struct pci_bus *bus)
 	/* Reserve buses for SR-IOV capability. */
 	max += pci_iov_bus_range(bus);
 
-#ifdef CONFIG_BCM47XX
-	if (pci_domain_nr(bus) && pci_is_root_bus(bus))
-		max += pci_domain_nr(bus) - 1;
-#endif
-
 	/*
 	 * After performing arch-dependent fixup of the bus, look behind
 	 * all PCI-to-PCI bridges on this bus.
