3c476b0219a9a0fa50efeb6e136cfb7a1d9361d3
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2016-01-20 11:31:38 +0000

    this adds support for newer binutils, but needs to be carefully tested first. not sure yet if it works
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28893 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.10/arch/mips/math-emu/cp1emu.c
+++ src/linux/universal/linux-3.10/arch/mips/math-emu/cp1emu.c
@@ -807,7 +807,7 @@ static int isBranchInstr(struct pt_regs *regs, struct mm_decoded_insn dec_insn,
 		if (insn.i_format.rs == bc_op) {
 			preempt_disable();
 			if (is_fpu_owner())
-				asm volatile("cfc1\t%0,$31" : "=r" (fcr31));
+			        fcr31 = read_32bit_cp1_register(CP1_STATUS);
 			else
 				fcr31 = current->thread.fpu.fcr31;
 			preempt_enable();
