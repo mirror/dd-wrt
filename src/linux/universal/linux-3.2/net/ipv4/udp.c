2c9c79795ababcdb5e7dc7818eeefcb287b5f75b
BrainSlayer <s.gottschall@dd-wrt.com>
2016-01-23 07:54:09 +0000

    update kernel
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28925 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 62238f13f029cebaa5ec20675654e253e4649101

--- src/linux/universal/linux-3.2/net/ipv4/udp.c
+++ src/linux/universal/linux-3.2/net/ipv4/udp.c
@@ -1182,6 +1182,7 @@ int udp_recvmsg(struct kiocb *iocb, struct sock *sk, struct msghdr *msg,
 	int peeked;
 	int err;
 	int is_udplite = IS_UDPLITE(sk);
+	bool checksum_valid = false;
 	bool slow;
 
 	if (flags & MSG_ERRQUEUE)
@@ -1207,17 +1208,18 @@ try_again:
 	 */
 
 	if (copied < ulen || UDP_SKB_CB(skb)->partial_cov) {
-		if (udp_lib_checksum_complete(skb))
+		checksum_valid = !udp_lib_checksum_complete(skb);
+		if (!checksum_valid)
 			goto csum_copy_err;
 	}
 
-	if (skb_csum_unnecessary(skb))
+	if (checksum_valid || skb_csum_unnecessary(skb))
 		err = skb_copy_datagram_iovec(skb, sizeof(struct udphdr),
 					      msg->msg_iov, copied);
 	else {
 		err = skb_copy_and_csum_datagram_iovec(skb,
 						       sizeof(struct udphdr),
-						       msg->msg_iov, copied);
+						       msg->msg_iov);
 
 		if (err == -EINVAL)
 			goto csum_copy_err;
