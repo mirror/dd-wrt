25333ab4dd13ad1ce2b3c90923d6953da14e3e81
BrainSlayer <s.gottschall@dd-wrt.com>
2016-01-25 21:15:24 +0000

    e2fsprogs update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28950 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: d698484356d7666f2f206827164acbcab008a27f

--- src/router/rules/e2fsprogs.mk
+++ src/router/rules/e2fsprogs.mk
@@ -1,31 +1,49 @@
-e2fsprogs-configure:
-	cd e2fsprogs && ./configure --host=$(ARCH)-linux CC="$(CC) $(COPTS) -D_GNU_SOURCE  -DNEED_PRINTF -std=gnu89" --disable-static --enable-shared --with-gnu-ld --disable-rpath --enable-elf-shlibs --enable-compression --enable-htree --enable-symlink-install --disable-tls --libdir=/lib root_prefix=$(INSTALLDIR)/e2fsprogs
-	make -C e2fsprogs
-	cd xfsprogs && ./configure --host=$(ARCH)-linux CFLAGS="$(COPTS)  -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -I$(TOP)/e2fsprogs/lib -DNEED_PRINTF" LDFLAGS="-L$(TOP)/e2fsprogs/lib/uuid" CC="$(CC) $(COPTS)" --disable-static --enable-shared --with-gnu-ld --disable-rpath --enable-gettext=no --enable-lib64=no root_prefix=$(INSTALLDIR)/e2fsprogs
+xfsprogs-configure:
+	cd xfsprogs && ./configure --host=$(ARCH)-linux CFLAGS="$(COPTS)  -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -I$(TOP)/e2fsprogs/lib -DNEED_PRINTF" LDFLAGS="-L$(TOP)/e2fsprogs/lib/uuid" CC="$(CC) $(COPTS)" --disable-static --enable-shared --with-gnu-ld --disable-rpath --enable-gettext=no --enable-lib64=no root_prefix=$(INSTALLDIR)/xfsprogs
 
-e2fsprogs:
-	make -C e2fsprogs
-ifeq ($(CONFIG_E2FSPROGS_ADV),y)
+xfsprogs:
 	make -C xfsprogs DEBUG= Q= 
-	make -C btrfsprogs CC="$(CC)" CFLAGS="$(COPTS) -I$(TOP)/e2fsprogs/lib  -D_GNU_SOURCE -DNEED_PRINTF  -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="-L$(TOP)/e2fsprogs/lib/uuid  -ffunction-sections -fdata-sections -Wl,--gc-sections" prefix=/usr
-endif
 
-e2fsprogs-clean:
-	make -C e2fsprogs clean
-ifeq ($(CONFIG_E2FSPROGS_ADV),y)
+
+xfsprogs-clean:
 	make -C xfsprogs DEBUG= Q= clean
-	make -C btrfsprogs CC="$(CC)" CFLAGS="$(COPTS) -I$(TOP)/e2fsprogs/lib  -D_GNU_SOURCE -DNEED_PRINTF  -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="-L$(TOP)/e2fsprogs/lib/uuid  -ffunction-sections -fdata-sections -Wl,--gc-sections" prefix=/usr clean
-endif
 
-e2fsprogs-install:
-	-make -C e2fsprogs install DESTDIR=$(INSTALLDIR)/e2fsprogs
-ifeq ($(CONFIG_E2FSPROGS_ADV),y)
+xfsprogs-install:
 	-make -C xfsprogs install DESTDIR=$(INSTALLDIR)/e2fsprogs PKG_SBIN_DIR=$(INSTALLDIR)/e2fsprogs/usr/sbin PKG_ROOT_SBIN_DIR=$(INSTALLDIR)/e2fsprogs/sbin PKG_ROOT_LIB_DIR=$(INSTALLDIR)/e2fsprogs/lib PKG_LIB_DIR=$(INSTALLDIR)/e2fsprogs/usr/lib	\
 		PKG_MAN_DIR=$(INSTALLDIR)/e2fsprogs/usr/man \
 		PKG_LOCALE_DIR=$(INSTALLDIR)/e2fsprogs/usr/share/locale \
 		PKG_DOC_DIR=$(INSTALLDIR)/e2fsprogs/usr/share/doc/xfsprogs
-	-make -C btrfsprogs install DESTDIR=$(INSTALLDIR)/e2fsprogs prefix=/usr
-else
+
+btrfsprogs-configure:
+	cd btrfsprogs && ./autogen.sh
+	cd btrfsprogs && ./configure --host=$(ARCH)-linux CFLAGS="$(COPTS) -I$(TOP)/e2fsprogs/lib -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -I$(TOP)/zlib -I$(TOP)/lzo/include -I$(TOP)/$(ARCH)-uclibc/install/util-linux/usr/include -DNEED_PRINTF" LDFLAGS="-L$(TOP)/zlib -L$(TOP)/$(ARCH)-uclibc/install/util-linux/usr/lib  -L$(TOP)/e2fsprogs/lib -L$(TOP)/lzo/src/.libs -lz" CC="$(CC) $(COPTS)" --disable-backtrace root_prefix=$(INSTALLDIR)/btrfsprogs  ac_cv_env_ZLIB_CFLAGS="-lz"
+
+btrfsprogs:
+	make -C btrfsprogs 
+
+btrfsprogs-clean:
+	make -C btrfsprogs clean
+
+btrfsprogs-install:	
+	-make -C btrfsprogs install DESTDIR=$(INSTALLDIR)/btrfsprogs prefix=/usr
+	-rm -rf $(INSTALLDIR)/btrfsprogs/usr/include
+	-rm -rf $(INSTALLDIR)/btrfsprogs/usr/lib
+	-rm -rf $(INSTALLDIR)/btrfsprogs/usr/share
+	
+e2fsprogs-configure:
+	cd e2fsprogs && ./configure --host=$(ARCH)-linux CC="$(CC) $(COPTS) -D_GNU_SOURCE  -DNEED_PRINTF -std=gnu89" --disable-static --enable-shared --with-gnu-ld --disable-rpath --enable-elf-shlibs --enable-compression --enable-htree --enable-symlink-install --disable-tls --libdir=/lib root_prefix=$(INSTALLDIR)/e2fsprogs
+	make -C e2fsprogs
+
+e2fsprogs:
+	make -C e2fsprogs
+
+e2fsprogs-clean:
+	make -C e2fsprogs clean
+
+e2fsprogs-install:
+	-make -C e2fsprogs install DESTDIR=$(INSTALLDIR)/e2fsprogs
+
+ifneq ($(CONFIG_E2FSPROGS_ADV),y)
 	-rm -f $(INSTALLDIR)/e2fsprogs/sbin/*fsck*
 	rm -rf $(INSTALLDIR)/e2fsprogs/usr/bin
 	rm -rf $(INSTALLDIR)/e2fsprogs/usr/sbin
