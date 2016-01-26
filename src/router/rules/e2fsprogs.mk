8069825601bf8ea5ca979f4fb54b0e43967ff732
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2016-01-26 00:24:49 +0000

    change to static blkid variant. dynamic doesnt want
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28959 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/e2fsprogs.mk
+++ src/router/rules/e2fsprogs.mk
@@ -16,16 +16,16 @@ xfsprogs-install:
 
 btrfsprogs-configure:
 	cd btrfsprogs && ./autogen.sh
-	cd btrfsprogs && ./configure --host=$(ARCH)-linux CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -I$(TOP)/$(ARCH)-uclibc/install/util-linux/usr/include -I$(TOP)/e2fsprogs/lib -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -I$(TOP)/zlib -I$(TOP)/lzo/include -DNEED_PRINTF" LDFLAGS="-L$(TOP)/zlib -L$(TOP)/$(ARCH)-uclibc/install/util-linux/usr/lib  -L$(TOP)/e2fsprogs/lib -L$(TOP)/lzo/src/.libs -lz  -ffunction-sections -fdata-sections -Wl,--gc-sections" CC="$(CC) $(COPTS)" --disable-backtrace --disable-documentation root_prefix=$(INSTALLDIR)/btrfsprogs  ac_cv_env_ZLIB_CFLAGS="-lz" EXT2FS_CFLAGS=" " EXT2FS_LIBS="-lext2fs" COM_ERR_CFLAGS=" " COM_ERR_LIBS="-lcom_err" BLKID_CFLAGS=" " BLKID_LIBS="-lblkid"
+	cd btrfsprogs && ./configure --host=$(ARCH)-linux CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -I$(TOP)/$(ARCH)-uclibc/install/util-linux/usr/include -I$(TOP)/e2fsprogs/lib -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -I$(TOP)/zlib -I$(TOP)/lzo/include -DNEED_PRINTF" LDFLAGS="-L$(TOP)/zlib -L$(TOP)/$(ARCH)-uclibc/install/util-linux/usr/lib  -L$(TOP)/e2fsprogs/lib -L$(TOP)/lzo/src/.libs -lz  -ffunction-sections -fdata-sections -Wl,--gc-sections" CC="$(CC) $(COPTS)" --disable-backtrace --disable-documentation root_prefix=$(INSTALLDIR)/btrfsprogs  ZLIB_CFLAGS=" " ZLIB_LIBS="$(TOP)/zlib/libz.a" EXT2FS_CFLAGS=" " EXT2FS_LIBS="-lext2fs" COM_ERR_CFLAGS=" " COM_ERR_LIBS="-lcom_err" BLKID_CFLAGS=" " BLKID_LIBS="-lblkid"
 
 btrfsprogs: util-linux
 	make -C util-linux clean
 	make -C util-linux
 	make -C util-linux install DESTDIR=$(INSTALLDIR)/util-linux
 	rm -f $(INSTALLDIR)/util-linux/usr/lib/libuuid.a
-	rm -f $(INSTALLDIR)/util-linux/usr/lib/libblkid.a
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libblkid.so*
 	rm -f $(TOP)/util-linux/.libs/libuuid.a
-	rm -f $(TOP)/util-linux/.libs/libblkid.a
+	rm -f $(TOP)/util-linux/.libs/libblkid.so*
 	make -C btrfsprogs 
 
 btrfsprogs-clean:
