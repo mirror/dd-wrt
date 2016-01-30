43b69ffdc1e39c569c43b56c115b73816386956a
BrainSlayer <s.gottschall@dd-wrt.com>
2016-01-30 10:18:28 +0000

    affects only AR7100 and AR9100
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@28990 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4/include/linux/types.h
+++ src/linux/universal/linux-4.4/include/linux/types.h
@@ -232,20 +232,16 @@ typedef void (*call_rcu_func_t)(struct rcu_head *head, rcu_callback_t func);
 /* clocksource cycle base type */
 typedef u64 cycle_t;
 
-#ifdef CONFIG_MIPS
+#ifdef CONFIG_MACH_AR7100
 struct net_hdr_word {
        u32 words[1];
 } __attribute__((packed, aligned(2)));
 
 #define net_hdr_word(_p) (((struct net_hdr_word *) (_p))->words[0])
-#else
-#define net_hdr_word(_p) (*((u32 *)_p))
-#endif
-
-#ifdef CONFIG_MACH_AR7100
 #define MIPS_ENABLED(a) a
 #else
 #define MIPS_ENABLED(a)
+#define net_hdr_word(_p) (*((u32 *)_p))
 #endif
 
 #endif /*  __ASSEMBLY__ */
