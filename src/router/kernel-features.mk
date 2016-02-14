8d1e7258b272bf0814e4ceac4b9f215be9a056dd
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2016-02-14 15:33:25 +0000

    move to central point
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@29079 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kernel-features.mk
+++ src/router/kernel-features.mk
@@ -60,6 +60,9 @@ define kernelfeatures
 		sed -i 's/\CONFIG_NETWORK_FILESYSTEMS=y/# CONFIG_NETWORK_FILESYSTEMS is not set/g' $(LINUXDIR)/.config; \
 		sed -i 's/\CONFIG_NLS=y/# CONFIG_NLS is not set/g' $(LINUXDIR)/.config; \
 	fi	
+	if [ "$(CONFIG_BONDING)" != "y" ]; then \
+		sed -i 's/\CONFIG_BONDING=m/# CONFIG_CIFS is not set/g' $(LINUXDIR)/.config; \
+	fi	
 	if [ "$(CONFIG_USBIP)" = "y" ]; then \
 		sed -i 's/\# CONFIG_USBIP_CORE is not set/CONFIG_USBIP_CORE=m/g' $(LINUXDIR)/.config; \
 		echo "CONFIG_USBIP_VHCI_HCD=m" >> $(LINUXDIR)/.config; \
