d13456f3730b236a067383cc2a0bd225cb3e7a1c
BrainSlayer <s.gottschall@dd-wrt.com>
2016-02-29 10:21:12 +0000

    fix for usb / ventana
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@29174 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit.c
+++ src/router/services/sysinit/sysinit.c
@@ -2868,7 +2868,7 @@ void start_drivers(void)
 		led_control(LED_USB1, LED_ON);
 
 		insmod
-		    ("nls_base usb-common usbcore ehci-hcd ehci-platform ehci-pci usb-uhci uhci-hcd usb-ohci ohci-hcd xhci-hcd xhci-pci xhci-plat-hcd dwc_otg usb-libusual fsl-mph-dr-of phy-mxs-usb ci_hdrc ci13xxx_imx usbmisc_imx ci_hdrc_imx dwc3 dwc3-qcom phy-qcom-hsusb phy-qcom-ssusb");
+		    ("nls_base usb-common usbcore ehci-hcd ehci-platform ehci-pci usb-uhci uhci-hcd usb-ohci ohci-hcd xhci-hcd xhci-pci xhci-plat-hcd dwc_otg usb-libusual fsl-mph-dr-of phy-mxs-usb extcon ci_hdrc ci13xxx_imx usbmisc_imx ci_hdrc_imx dwc3 dwc3-qcom phy-qcom-hsusb phy-qcom-ssusb");
 
 #ifdef HAVE_IPQ806X
 		sleep(5);
@@ -2916,6 +2916,7 @@ void start_drivers(void)
 		rmmod("usbmisc_imx");
 		rmmod("ci13xxx_imx");
 		rmmod("ci_hdrc");
+		rmmod("extcon");
 		rmmod("phy-mxs-usb");
 		rmmod("fsl-mph-dr-of");
 
