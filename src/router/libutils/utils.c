fc4d78e8160b4513e4556040127939bcc74a71c0
BrainSlayer <s.gottschall@dd-wrt.com>
2016-03-15 23:50:49 +0000

    some new device specs
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@29248 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: c1073380d11b174ddf3406c254a179bd152abfd7

--- src/router/libutils/utils.c
+++ src/router/libutils/utils.c
@@ -2448,6 +2448,8 @@ int internal_getRouterBrand()
 		{"UniFi UAP v2", 0xe572, 3, 3, ROUTER_BOARD_UNIFI, 0, 10},	//
 		{"UniFi UAP-LR v2", 0xe582, 3, 3, ROUTER_BOARD_UNIFI, 0, 10},	//
 		{"UniFi UAP-AC-LR", 0xe527, 3, 3, ROUTER_UBNT_UAPAC, 0, 10},	//
+		{"UniFi UAP-AC-Pro-Gen2", 0xe537, 3, 3, ROUTER_UBNT_UAPAC, 0, 10},	//
+		{"UniFi UAP-AC-Lite", 0xe517, 3, 3, ROUTER_UBNT_UAPAC, 0, 10},	//
 		{NULL, 0, 0, 0, 0, 0, 0},	//
 	};
 
