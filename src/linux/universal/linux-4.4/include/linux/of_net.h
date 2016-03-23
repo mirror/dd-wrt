561d4d09f9a1a2477db94564910ba8a9d1138988
BrainSlayer <s.gottschall@dd-wrt.com>
2016-03-23 14:49:19 +0000

    wdr4900 4.4 kernel port
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@29326 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4/include/linux/of_net.h
+++ src/linux/universal/linux-4.4/include/linux/of_net.h
@@ -13,7 +13,6 @@
 struct net_device;
 extern int of_get_phy_mode(struct device_node *np);
 extern const void *of_get_mac_address(struct device_node *np);
-extern int of_get_mac_address_mtd(struct device_node *np, void *mac);
 extern struct net_device *of_find_net_device_by_node(struct device_node *np);
 #else
 static inline int of_get_phy_mode(struct device_node *np)
