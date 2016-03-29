854b1dc218ec0beaf419070c771a93fb580d3f8d
kong <USER@DOMAIN>
2016-03-29 23:37:47 +0000

    fix tunnel
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@29377 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 7736a251b82bb981fce030e7f8603feaf6dcf908

--- src/router/services/networking/network.c
+++ src/router/services/networking/network.c
@@ -4761,6 +4761,10 @@ void start_wan_done(char *wan_ifname)
 #else
 		eval("wget", nvram_safe_get("ipv6_tun_upd_url"), "-O", "/tmp/tunnelstat");
 #endif
+		if (nvram_match("wshaper_enable", "1")){
+			stop_ipv6_tunnel(wan_ifname);
+			start_ipv6_tunnel(wan_ifname);
+		}
 	}
 }
 
