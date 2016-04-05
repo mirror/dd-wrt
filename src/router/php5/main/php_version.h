2a147c7b016795b4213368f252a26989b41e672b
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2016-04-05 19:53:55 +0000

    update php
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@29410 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/php5/main/php_version.h
+++ src/router/php5/main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 5
-#define PHP_RELEASE_VERSION 32
+#define PHP_RELEASE_VERSION 34
 #define PHP_EXTRA_VERSION ""
-#define PHP_VERSION "5.5.32"
-#define PHP_VERSION_ID 50532
+#define PHP_VERSION "5.5.34"
+#define PHP_VERSION_ID 50534
