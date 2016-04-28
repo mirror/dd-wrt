b786d04feeefe553504a0df8c5ed1a18148bef51
BrainSlayer <s.gottschall@dd-wrt.com>
2016-04-28 03:05:33 +0000

    remove useless data
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@29537 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 2fb7d34bfb5e37a3fd3bc21519946be2cbb0e9a1

--- src/router/rules/libffi.mk
+++ src/router/rules/libffi.mk
@@ -7,3 +7,5 @@ libffi:
 libffi-install:
 	make -C libffi install DESTDIR=$(INSTALLDIR)/libffi
 	-rm -rf $(INSTALLDIR)/libffi/usr/share
+	-rm -rf $(INSTALLDIR)/libffi/usr/lib/libffi-3.2.1
+	-rm -rf $(INSTALLDIR)/libffi/usr/lib/pkgconfig
