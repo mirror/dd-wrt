427f8803ee3296c2b5fbfff23326c9728771abcf
BrainSlayer <s.gottschall@dd-wrt.com>
2016-05-05 21:39:11 +0000

    some init code for ac1200 to avoid switch reset
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@29582 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 11ca48ace65fbe604f8c7ee40b78c773ac32e413

--- src/linux/universal/linux-4.4/arch/arm/mach-brcm-hnd/gpio.c
+++ src/linux/universal/linux-4.4/arch/arm/mach-brcm-hnd/gpio.c
@@ -259,7 +259,10 @@ static int __init gpio_init(void)
 		printk(KERN_EMERG "Netgear R8500 init\n");
 		gpios = 1<<0 | 1<<1 | 1<<2 | 1<<3 | 1<<4 | 1<<5 | 1<<6 | 1<<7 | 1<<8 | 1<<9 | 1<<10 | 1<<11 | 1<<12 | 1<<13 | 1<<14 | 1<<15 | 1<<16 | 1<<17 | 1<<18 | 1<<19 | 1<<20 | 1<<21 | 1<<22 | 1<<23  | 1<<24 ;
 	}
-	
+	if (nvram_match("model","RT-AC1200G+")) {
+		printk(KERN_EMERG " Init Asus RT-AC1200G+\n");
+		isdefault = 1;
+	}	
 	
 
 	for (i = 0; i < 32; i++) {
