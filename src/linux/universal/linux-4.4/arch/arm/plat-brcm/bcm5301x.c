1603295c1ffcd263bf055cdb29bb773bef2534a7
BrainSlayer <s.gottschall@dd-wrt.com>
2016-05-05 13:10:06 +0000

    current state
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@29574 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 8f037e14ee01894e11ed3524d1a4baba1101da62

--- src/linux/universal/linux-4.4/arch/arm/plat-brcm/bcm5301x.c
+++ src/linux/universal/linux-4.4/arch/arm/plat-brcm/bcm5301x.c
@@ -14,25 +14,99 @@
 #include <linux/delay.h>
 #include <linux/serial_8250.h>
 #include <linux/proc_fs.h>
-#include <linux/module.h>
+#include <linux/spi/spi.h>
+#include <linux/irq.h>
+#include <linux/spinlock.h>
 #include <asm/hardware/cache-l2x0.h>
 #include <asm/mach/map.h>
 #include <asm/clkdev.h>
+#include <asm/uaccess.h>
+#include <asm/ptrace.h>
 #include <mach/clkdev.h>
 #include <mach/io_map.h>
 #include <plat/bsp.h>
 #include <plat/mpcore.h>
 #include <plat/plat-bcm5301x.h>
 
-#ifdef	CONFIG_SMP
-#include <asm/spinlock.h>
-#endif
+#include <typedefs.h>
+#include <sbchipc.h>
 #include <bcmutils.h>
 #include <siutils.h>
 #include <bcmdefs.h>
 #include <bcmdevs.h>
 
-#define ACP_WAR_ENAB() 0
+#ifdef	CONFIG_SMP
+#include <asm/spinlock.h>
+#endif
+
+#define SOC_CHIPCOMON_A_REG_VA(offset)		(SOC_CHIPCOMON_A_BASE_VA + (offset))
+#define SOC_CHIPCOMON_A_INTSTATUS_VA		SOC_CHIPCOMON_A_REG_VA(CC_INTSTATUS)
+#define SOC_CHIPCOMON_A_INTMASK_VA		SOC_CHIPCOMON_A_REG_VA(CC_INTMASK)
+#define SOC_CHIPCOMON_A_GPIOINPUT_VA		SOC_CHIPCOMON_A_REG_VA(CC_GPIOIN)
+#define SOC_CHIPCOMON_A_GPIOINTPOLARITY_VA	SOC_CHIPCOMON_A_REG_VA(CC_GPIOPOL)
+#define SOC_CHIPCOMON_A_GPIOINTMASK_VA		SOC_CHIPCOMON_A_REG_VA(CC_GPIOINTM)
+#define SOC_CHIPCOMON_A_GPIOEVENT_VA		SOC_CHIPCOMON_A_REG_VA(CC_GPIOEVENT)
+#define SOC_CHIPCOMON_A_GPIOEVENTINTMASK_VA	SOC_CHIPCOMON_A_REG_VA(CC_GPIOEVENTMASK)
+#define SOC_CHIPCOMON_A_GPIOEVENTINTPOLARITY_VA	SOC_CHIPCOMON_A_REG_VA(CC_GPIOEVENTPOL)
+#define SOC_CHIPCOMON_A_CORECAP			SOC_CHIPCOMON_A_REG_VA(CC_CAPABILITIES)
+#define SOC_CHIPCOMON_A_CORECTRL		SOC_CHIPCOMON_A_REG_VA(0x08)
+#define SOC_CHIPCOMON_A_CLKDIV			SOC_CHIPCOMON_A_REG_VA(CC_CLKDIV)
+
+#define SOC_CHIPCOMON_A_INTMASK_UART		(1 << 6)
+#define SOC_CHIPCOMON_A_INTMASK_GPIO		(1 << 0)
+
+#define SOC_CHIPCOMON_A_CORECAP_UARTCLKSEL_M	0x3
+#define SOC_CHIPCOMON_A_CORECAP_UARTCLKSEL_S	3
+
+#define SOC_CHIPCOMON_A_CORECTRL_UARTCLKOVR_M	0x1
+#define SOC_CHIPCOMON_A_CORECTRL_UARTCLKOVR_S	0
+
+#define SOC_CHIPCOMON_A_CLKDIV_UARTCLKDIV_M	0xFF
+#define SOC_CHIPCOMON_A_CLKDIV_UARTCLKDIV_S	0
+
+#define SOC_APBX_IDM_IDM_IO_CONTROL_DIRECT	(SOC_IDM_BASE_VA + 0x21408)
+
+#define SOC_APBX_IDM_IDM_IO_CONTROL_DIRECT_UARTCLKSEL_M	0x1
+#define SOC_APBX_IDM_IDM_IO_CONTROL_DIRECT_UARTCLKSEL_S	17
+
+#define PLAT_SM_CLR(val, field)			((val) & ~(field##_M << field##_S))
+#define PLAT_SM_SET(val, field, field_val)	((val) | (((field_val) & field##_M) << field##_S))
+#define PLAT_SM_GET(val, field)			(((val) >> field##_S) & field##_M)
+#define PLAT_SM_ASSIGN(val, field, field_val)	PLAT_SM_SET(PLAT_SM_CLR(val, field), field, field_val)
+
+static struct clk * _soc_refclk = NULL;
+
+#ifdef CONFIG_PLAT_UART_CLOCKS
+
+#if CONFIG_PLAT_CCA_UART0_DIVIDER <= 0xFFFF
+# define CONFIG_PLAT_CCA_UART0_DIVIDER_FLAG	UPF_SPD_CUST
+#else
+# define CONFIG_PLAT_CCA_UART0_DIVIDER_FLAG	0
+#endif
+
+#if CONFIG_PLAT_CCA_UART1_DIVIDER <= 0xFFFF
+# define CONFIG_PLAT_CCA_UART1_DIVIDER_FLAG	UPF_SPD_CUST
+#else
+# define CONFIG_PLAT_CCA_UART1_DIVIDER_FLAG	0
+#endif
+
+#if CONFIG_PLAT_CCB_UART0_DIVIDER <= 0xFFFF
+# define CONFIG_PLAT_CCB_UART0_DIVIDER_FLAG	UPF_SPD_CUST
+#else
+# define CONFIG_PLAT_CCB_UART0_DIVIDER_FLAG	0
+#endif
+
+#else
+
+#define CONFIG_PLAT_CCA_UART0_DIVIDER		0
+#define CONFIG_PLAT_CCA_UART1_DIVIDER		0
+#define CONFIG_PLAT_CCB_UART0_DIVIDER		0
+
+#define CONFIG_PLAT_CCA_UART0_DIVIDER_FLAG	0
+#define CONFIG_PLAT_CCA_UART1_DIVIDER_FLAG	0
+#define CONFIG_PLAT_CCB_UART0_DIVIDER_FLAG	0
+
+#endif /* PLAT_UART_CLOCKS */
 
 /* Global SB handle */
 extern si_t *bcm947xx_sih;
@@ -40,27 +114,39 @@ extern si_t *bcm947xx_sih;
 
 extern int _chipid;
 
-static struct clk * _soc_refclk = NULL;
-
 static struct plat_serial8250_port uart_ports[] = {
 	{
 	.type       = PORT_16550,
-	.flags      = UPF_FIXED_TYPE | UPF_SHARE_IRQ,
+	.flags      = UPF_FIXED_TYPE | UPF_SHARE_IRQ | CONFIG_PLAT_CCA_UART0_DIVIDER_FLAG,
 	.regshift   = 0,
-	.iotype     = UPIO_MEM,	
+	.iotype     = UPIO_MEM,
 	.mapbase    = (resource_size_t)(PLAT_UART1_PA),
 	.membase    = (void __iomem *) PLAT_UART1_VA,
-	.irq        = IRQ_CCA,
+	.irq        = IRQ_CCA_UART,
+	.custom_divisor = CONFIG_PLAT_CCA_UART0_DIVIDER,
 	},
 	{
 	.type       = PORT_16550,
-	.flags      = UPF_FIXED_TYPE | UPF_SHARE_IRQ,
+	.flags      = UPF_FIXED_TYPE | UPF_SHARE_IRQ | CONFIG_PLAT_CCA_UART1_DIVIDER_FLAG,
 	.regshift   = 0,
-	.iotype     = UPIO_MEM,	
+	.iotype     = UPIO_MEM,
 	.mapbase    = (resource_size_t)(PLAT_UART2_PA),
 	.membase    = (void __iomem *) PLAT_UART2_VA,
-	.irq        = IRQ_CCA,
+	.irq        = IRQ_CCA_UART,
+	.custom_divisor = CONFIG_PLAT_CCA_UART1_DIVIDER,
 	},
+#ifdef CONFIG_PLAT_MUX_CONSOLE_CCB
+	{
+	.type       = PORT_16550,
+	.flags      = UPF_FIXED_TYPE | CONFIG_PLAT_CCB_UART0_DIVIDER_FLAG,
+	.regshift   = 2,	/* Chipcommon B regs are 32-bit aligned */
+	.iotype     = UPIO_MEM,
+	.mapbase    = (resource_size_t)(PLAT_UART0_PA),
+	.membase    = (void __iomem *) PLAT_UART0_VA,
+	.irq        = IRQ_CCB_UART0,
+	.custom_divisor = CONFIG_PLAT_CCB_UART0_DIVIDER,
+	},
+#endif /* CONFIG_PLAT_MUX_CONSOLE_CCB */
 	{ .flags = 0, },	/* List terminatoir */
 };
 
@@ -72,12 +158,20 @@ static struct platform_device platform_serial_devices = {
 	},
 };
 
+static struct platform_device platform_spi_master_device = {
+	.name = "bcm5301x-spi-master",
+	.id = 1, /* Bus number */
+	.dev = {
+		.platform_data = NULL, /* Passed to driver */
+	},
+};
+
 /*
  * Map fix-mapped I/O that is needed before full MMU operation
  */
-void __init soc_map_io( struct clk * refclk )
+void __init soc_map_io(struct clk * refclk)
 {
-	struct map_desc desc[2] ;
+	struct map_desc desc[2];
 
 	/*
 	* Map Chipcommon A & B to a fixed virtual location
@@ -88,19 +182,21 @@ void __init soc_map_io( struct clk * refclk )
 	*/
 
 	desc[0].virtual = IO_BASE_VA;
-	desc[0].pfn = __phys_to_pfn( IO_BASE_PA );
+	desc[0].pfn = __phys_to_pfn(IO_BASE_PA);
 	if (BCM53573_CHIP(_chipid)) {
 		desc[0].length = SZ_2M;
 	} else {
 		desc[0].length = SZ_16M;	/* CCA+CCB: 0x18000000-0x18ffffff */
 	}
-	desc[0].type = MT_DEVICE ;
 
-	iotable_init( desc, 1);
+	desc[0].type = MT_DEVICE;
 
-	mpcore_map_io( );
+	iotable_init(desc, 1);
+
+	mpcore_map_io();
+	printk(KERN_INFO "set refclk to %p\n", refclk);
 	/* Save refclk for later use */
-	_soc_refclk = refclk ;
+	_soc_refclk = refclk;
 }
 
 static int soc_abort_handler(unsigned long addr, unsigned int fsr,
@@ -110,16 +206,16 @@ static int soc_abort_handler(unsigned long addr, unsigned int fsr,
 	 * These happen for no good reason
 	 * possibly left over from CFE
 	 */
-	printk( KERN_WARNING 
+	printk(KERN_WARNING
 		"External imprecise Data abort at "
-		"addr=%#lx, fsr=%#x ignored.\n", 
-		addr, fsr );
+		"addr=%#lx, fsr=%#x, pc=%#lx lr=%#lx ignored.\n",
+		addr, fsr, regs->ARM_pc, regs->ARM_lr);
 
 	/* Returning non-zero causes fault display and panic */
         return 0;
 }
 
-static void soc_aborts_enable(void)
+static void __init soc_aborts_enable(void)
 {
 	u32 x;
 
@@ -139,6 +235,202 @@ static void soc_aborts_enable(void)
 
 }
 
+#ifdef CONFIG_PLAT_CCA_GPIO_IRQ
+
+#include <asm/gpio.h>
+
+struct soc_cca_gpio_irq_data {
+	unsigned int type;
+};
+
+static void soc_cca_gpio_handle(void)
+{
+	uint32 mask = readl(SOC_CHIPCOMON_A_GPIOINTMASK_VA);
+	uint32 polarity = readl(SOC_CHIPCOMON_A_GPIOINTPOLARITY_VA);
+	uint32 input = readl(SOC_CHIPCOMON_A_GPIOINPUT_VA);
+	uint32 event = readl(SOC_CHIPCOMON_A_GPIOEVENT_VA);
+	uint32 eventmask = readl(SOC_CHIPCOMON_A_GPIOEVENTINTMASK_VA);
+	int gpio;
+
+	for (gpio = 0; gpio < IRQ_CCA_GPIO_N; ++gpio) {
+		uint32 gpio_bit = (1 << gpio);
+		uint32 level_triggered = ((input ^ polarity) & mask & gpio_bit);
+		uint32 edge_triggered = (event & eventmask & gpio_bit);
+
+		if (level_triggered || edge_triggered) {
+			generic_handle_irq(IRQ_CCA_GPIO(gpio));
+		}
+	}
+}
+
+static void soc_cca_irq_handler(unsigned int irq, struct irq_desc *desc)
+{
+	uint32 status = readl(SOC_CHIPCOMON_A_INTSTATUS_VA);
+
+	desc->chip->mask(irq);
+	desc->chip->ack(irq);
+
+	if (status & SOC_CHIPCOMON_A_INTMASK_UART) {
+		generic_handle_irq(IRQ_CCA_UART);
+	}
+
+	if (status & SOC_CHIPCOMON_A_INTMASK_GPIO) {
+		soc_cca_gpio_handle();
+	}
+
+	desc->chip->unmask(irq);
+}
+
+static void soc_cca_gpio_irq_update_type(unsigned int irq)
+{
+	int gpio = irq_to_gpio(irq);
+	struct soc_cca_gpio_irq_data *pdata;
+	uint32 gpio_bit;
+	unsigned int type;
+
+	if (gpio < 0) {
+		return;
+	}
+
+	pdata = get_irq_data(irq);
+	type = pdata->type;
+	gpio_bit = (1 << gpio);
+
+	if (type & IRQ_TYPE_LEVEL_LOW) {
+		writel(readl(SOC_CHIPCOMON_A_GPIOINTPOLARITY_VA) | gpio_bit,
+			SOC_CHIPCOMON_A_GPIOINTPOLARITY_VA);
+	} else if (type & IRQ_TYPE_LEVEL_HIGH) {
+		writel(readl(SOC_CHIPCOMON_A_GPIOINTPOLARITY_VA) & ~gpio_bit,
+			SOC_CHIPCOMON_A_GPIOINTPOLARITY_VA);
+	} else if (type & IRQ_TYPE_EDGE_FALLING) {
+		writel(readl(SOC_CHIPCOMON_A_GPIOEVENTINTPOLARITY_VA) | gpio_bit,
+			SOC_CHIPCOMON_A_GPIOEVENTINTPOLARITY_VA);
+	} else if (type & IRQ_TYPE_EDGE_RISING) {
+		writel(readl(SOC_CHIPCOMON_A_GPIOEVENTINTPOLARITY_VA) & ~gpio_bit,
+			SOC_CHIPCOMON_A_GPIOEVENTINTPOLARITY_VA);
+	}
+}
+
+static void soc_cca_gpio_irq_ack(unsigned int irq)
+{
+	int gpio = irq_to_gpio(irq);
+
+	if (gpio < 0) {
+		return;
+	}
+
+	writel((1 << gpio), SOC_CHIPCOMON_A_GPIOEVENT_VA);
+}
+
+static void soc_cca_gpio_irq_maskunmask(unsigned int irq, bool mask)
+{
+	int gpio = irq_to_gpio(irq);
+	struct soc_cca_gpio_irq_data *data;
+	uint32 mask_addr;
+	uint32 gpio_bit;
+	uint32 val;
+
+	if (gpio < 0) {
+		return;
+	}
+
+	data = get_irq_data(irq);
+	gpio_bit = (1 << gpio);
+
+	if (data->type & IRQ_TYPE_EDGE_BOTH) {
+		mask_addr = SOC_CHIPCOMON_A_GPIOEVENTINTMASK_VA;
+	} else {
+		mask_addr = SOC_CHIPCOMON_A_GPIOINTMASK_VA;
+	}
+
+	val = readl(mask_addr);
+	if (mask) {
+		val &= ~gpio_bit;
+	} else {
+		val |= gpio_bit;
+	}
+	writel(val, mask_addr);
+}
+
+static void soc_cca_gpio_irq_mask(unsigned int irq)
+{
+	soc_cca_gpio_irq_maskunmask(irq, true);
+}
+
+static void soc_cca_gpio_irq_unmask(unsigned int irq)
+{
+	soc_cca_gpio_irq_maskunmask(irq, false);
+}
+
+static void soc_cca_gpio_irq_enable(unsigned int irq)
+{
+	soc_cca_gpio_irq_update_type(irq);
+	soc_cca_gpio_irq_ack(irq);
+	soc_cca_gpio_irq_unmask(irq);
+}
+
+static void soc_cca_gpio_irq_disable(unsigned int irq)
+{
+	soc_cca_gpio_irq_mask(irq);
+}
+
+static int soc_cca_gpio_irq_set_type(unsigned int irq, unsigned int type)
+{
+	struct soc_cca_gpio_irq_data *pdata;
+
+	if (irq_to_gpio(irq) < 0) {
+		return -EINVAL;
+	}
+
+	if ((type & IRQ_TYPE_SENSE_MASK) == 0) {
+		return -EINVAL;
+	}
+
+	pdata = get_irq_data(irq);
+	pdata->type = type & IRQ_TYPE_SENSE_MASK;
+
+	return 0;
+}
+
+static struct irq_chip cca_gpio_irq_chip = {
+	.name		= "CCA_GPIO",
+	.ack		= soc_cca_gpio_irq_ack,
+	.mask		= soc_cca_gpio_irq_mask,
+	.unmask		= soc_cca_gpio_irq_unmask,
+	.enable		= soc_cca_gpio_irq_enable,
+	.disable	= soc_cca_gpio_irq_disable,
+	.set_type	= soc_cca_gpio_irq_set_type,
+};
+
+static void __init soc_cca_irq_enable(void)
+{
+	static struct soc_cca_gpio_irq_data irq_data[IRQ_CCA_GPIO_N];
+	int irq;
+
+	for (irq = IRQ_CCA_FIRST; irq <= IRQ_CCA_LAST; ++irq) {
+		if (irq == IRQ_CCA_UART) {
+			set_irq_chip(irq, NULL);
+		} else {
+			set_irq_chip(irq, &cca_gpio_irq_chip);
+			set_irq_data(irq, &irq_data[irq_to_gpio(irq)]);
+		}
+		set_irq_handler(irq, handle_level_irq);
+		set_irq_flags(irq, IRQF_VALID);
+	}
+
+	set_irq_chained_handler(IRQ_CCA, soc_cca_irq_handler);
+
+	printk(KERN_INFO"%d IRQ chained: UART, GPIOs\n", IRQ_CCA);
+}
+
+#else
+
+static void __init soc_cca_irq_enable(void)
+{
+}
+
+#endif /* CONFIG_PLAT_CCA_GPIO_IRQ */
+
 /*
  * This SoC relies on MPCORE GIC interrupt controller
  */
@@ -146,7 +438,7 @@ void __init soc_init_irq( void )
 {
 	mpcore_init_gic();
 	soc_aborts_enable();
-
+	soc_cca_irq_enable();
 }
 
 /*
@@ -195,11 +487,10 @@ static u32 __init soc_timerclk_calibration(void)
 /*
  * Initialize SoC timers
  */
-void __init soc_init_timer( void )
+void __init soc_init_timer(void)
 {
 	unsigned long periphclk_freq;
-	struct clk * clk ;
-
+	struct clk * clk;
 	if (BCM53573_CHIP(sih->chip)) {
 		soc_pmu_clk_init(_soc_refclk);
 		periphclk_freq = (unsigned long)soc_timerclk_calibration();
@@ -214,13 +505,12 @@ void __init soc_init_timer( void )
 		periphclk_freq = clk_get_rate(clk);
 	}
 
-	BUG_ON( !periphclk_freq );
+	BUG_ON(!periphclk_freq);
 
 	/* Fire up the global MPCORE timer */
-	mpcore_init_timer( periphclk_freq );
+	mpcore_init_timer(periphclk_freq);
 
 }
-
 
 /*
  * Install all other SoC device drivers
@@ -236,21 +526,25 @@ void __init soc_add_devices( void )
 	i = readl( SOC_CHIPCOMON_A_BASE_VA + 0x04 );	
 	/* UARTClkSel ChipcommonA_CoreCapabilities bit 4..3 */
 	UARTClkSel = ( i >> 3 ) & 0x3 ;
+	printk(KERN_INFO "corecaps 0x%08X UARTClkSel %d\n",i, UARTClkSel);
 	/* UARTClkOvr ChipcommonA_CoreCtrl bit 0 */
 	UARTClkOvr = 1 &  readl( SOC_CHIPCOMON_A_BASE_VA + 0x08 );
 	/* UARTClkDiv: ChipcommonA_ClkDiv bits 0..7 */
 	UARTClkDiv =  0xff & readl( SOC_CHIPCOMON_A_BASE_VA + 0xa4 );
 	if( UARTClkDiv == 0 ) UARTClkDiv = 0x100 ;
+	
 
-	if( UARTClkSel == 0 )
+	if ( UARTClkSel == 0 || BCM53573_CHIP(sih->chip))
 		{
+		printk(KERN_INFO "use refclk\n");
 		/* UARTClkSel = 0 -> external reference clock source */
 		clk = _soc_refclk ;
 		BUG_ON( !clk );
 		clk_rate = clk_get_rate(clk);
-		}
-	else if( UARTClkSel == 1 )
+		} 
+		else if( UARTClkSel == 1 )
 		{
+		printk(KERN_INFO "use slow clk\n");
 		/* UARTClkSel = 1 -> Internal clock optionally divided */
 		clk = clk_get_sys( NULL, "iproc_slow_clk");
 		BUG_ON( !clk );
@@ -258,10 +552,10 @@ void __init soc_add_devices( void )
 		if( ! UARTClkOvr )
 			clk_rate /= UARTClkDiv;
 		}
-
 	printk( KERN_INFO "CCA UART Clock Config: Sel=%d Ovr=%d Div=%d\n",
 		UARTClkSel, UARTClkOvr, UARTClkDiv );
 	printk( KERN_INFO "CCA UART Clock rate %uHz\n", clk_rate );
+
 
 	/* fixup UART port structure */
 	for(i = 0; i < ARRAY_SIZE(uart_ports); i++ )
@@ -316,17 +610,16 @@ void plat_wake_secondary_cpu( unsigned cpu, void (* _sec_entry_va)(void) )
 }
 
 #ifdef CONFIG_CACHE_L310
-extern struct outer_cache_fns outer_cache;
-
 /*
  * SoC initialization that need to be done early,
  * e.g. L2 cache, clock, I/O pin mux, power management
  */
-static int  __init bcm5301_pl310_init( void )
+static int  __init bcm5301_pl310_init(void)
 {
 	void __iomem *l2cache_base;
-	u32 auxctl_val, auxctl_msk ;
-
+	u32 auxctl_val, auxctl_msk;
+	extern void __init l310_init(void __iomem *, u32, u32, int);
+	/* Return immediately since no L2 cache for BCM53573 */
 	if (BCM53573_CHIP(sih->chip)) {
 		outer_cache.inv_range = NULL;
 		outer_cache.clean_range = NULL;
@@ -338,7 +631,7 @@ static int  __init bcm5301_pl310_init( void )
 	}
 	/* Default AUXCTL modifiers */
 	auxctl_val = 0UL;
-	auxctl_msk = ~0UL ;
+	auxctl_msk = ~0UL;
 
 	/* Customize AUXCTL */
 	auxctl_val |= 0 << 0 ;	/* Full line of zero Enable - requires A9 cfg */
@@ -346,16 +639,15 @@ static int  __init bcm5301_pl310_init( void )
 	auxctl_val |= 1 << 29;	/* Instruction prefetch enable */
 	auxctl_val |= 1 << 28;	/* Data prefetch enable */
 	auxctl_val |= 1 << 30;	/* Early BRESP enable */
-	auxctl_val |= 1 << 22;	/* for dma coherency */
-	
-	if (ACP_WAR_ENAB())
+
+	if (ACP_WAR_ENAB() || arch_is_coherent())
 		auxctl_val |= 1 << 11; /* Store buffer device limitation enable */
 
-	l2cache_base = ioremap( L2CC_BASE_PA, SZ_4K );
+	l2cache_base = ioremap(L2CC_BASE_PA, SZ_4K);
 
 	/* Configure using default aux control value */
-	if( l2cache_base != NULL )
-		l2x0_init( l2cache_base, auxctl_val, auxctl_msk);
+	if (l2cache_base != NULL)
+		l310_init(l2cache_base, auxctl_val, auxctl_msk, 32);
 
 	return 0;
 }
