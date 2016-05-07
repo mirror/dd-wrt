243c7c4b4000fd6457b4709aec7a823ff360b1bb
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2016-05-07 10:40:09 +0000

    Comfast WR650AC support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@29598 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4/sanitize.sh
+++ src/linux/universal/linux-4.4/sanitize.sh
@@ -77,6 +77,7 @@ do
 	    echo CONFIG_XWLOCO=y >> .config
 	    echo CONFIG_WR710=y >> .config
 	    echo CONFIG_GL150=y >> .config
+	    echo CONFIG_WR650AC=y >> .config
 	    make oldconfig ARCH=mips
 	    sed -i 's/\CONFIG_WR841V8=y/ /g' .config	    
 	    sed -i 's/\CONFIG_WR710=y/ /g' .config	    
@@ -105,6 +106,7 @@ do
 	    sed -i 's/\CONFIG_UAPAC=y/ /g' .config	    
 	    sed -i 's/\CONFIG_XWLOCO=y/ /g' .config	    
 	    sed -i 's/\CONFIG_GL150=y/ /g' .config	    
+	    sed -i 's/\CONFIG_WR650AC=y/ /g' .config	    
 	    cp .config $i
     fi
 done
