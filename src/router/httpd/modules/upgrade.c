be27942e63b2c1c4d251cd71c448a472c258d335
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2016-05-10 14:59:15 +0000

    missing define
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@29627 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/upgrade.c
+++ src/router/httpd/modules/upgrade.c
@@ -400,7 +400,7 @@ sys_upgrade(char *url, webs_t stream, int *total, int type)	// jimmy,
 			if (!strncmp(buf, "bgn", 3) || !strncmp(buf, "WZR", 3)
 			    || !strncmp(buf, "WHR", 3)
 			    || !strncmp(buf, "WLA", 3)
-#ifdef HAVE_IDEXX
+#if defined(HAVE_IDEXX) || defined(HAVE_IDEXX_SIGNATUR)
 			    || !strncmp(buf, "HDR0", 4)
 #endif
 				) {
