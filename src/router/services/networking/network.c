af9e74e5f40d2e683119e052077b952ff7e140db
BrainSlayer <s.gottschall@dd-wrt.com>
2016-05-18 12:26:39 +0000

    wr615N support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@29725 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 64ae224951d7c98bd9d4f5ec0e7cdefd2a00ad8d

--- src/router/services/networking/network.c
+++ src/router/services/networking/network.c
@@ -1299,6 +1299,18 @@ void start_lan(void)
 	if (nvram_match("et0macaddr", ""))
 		nvram_set("et0macaddr", ether_etoa(ifr.ifr_hwaddr.sa_data, eabuf));
 	strcpy(mac, nvram_safe_get("et0macaddr"));
+#elif HAVE_WR615N
+	nvram_setz(lan_ifnames, "eth0 eth1 ath0 ath1");
+	if (getSTA() || getWET() || CANBRIDGE()) {
+		PORTSETUPWAN("");
+	} else {
+		PORTSETUPWAN("eth1");
+	}
+	strncpy(ifr.ifr_name, "eth0", IFNAMSIZ);
+	ioctl(s, SIOCGIFHWADDR, &ifr);
+	if (nvram_match("et0macaddr", ""))
+		nvram_set("et0macaddr", ether_etoa(ifr.ifr_hwaddr.sa_data, eabuf));
+	strcpy(mac, nvram_safe_get("et0macaddr"));
 #elif HAVE_E355AC
 	nvram_setz(lan_ifnames, "eth0 eth1 ath0 ath1");
 	if (getSTA() || getWET() || CANBRIDGE()) {
@@ -3166,6 +3178,9 @@ void start_wan(int status)
 #elif HAVE_E380AC
 	char *pppoe_wan_ifname = nvram_invmatch("pppoe_wan_ifname",
 						"") ? nvram_safe_get("pppoe_wan_ifname") : "eth0";
+#elif HAVE_WR615N
+	char *pppoe_wan_ifname = nvram_invmatch("pppoe_wan_ifname",
+						"") ? nvram_safe_get("pppoe_wan_ifname") : "eth1";
 #elif HAVE_E325N
 	char *pppoe_wan_ifname = nvram_invmatch("pppoe_wan_ifname",
 						"") ? nvram_safe_get("pppoe_wan_ifname") : "eth1";
