4cc1c416f2fe3f974918d7d296e0bda1046621c1
brainslayer <brainslayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2016-05-30 22:25:07 +0000

    try cacao
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@29789 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/java.mk
+++ src/router/rules/java.mk
@@ -16,28 +16,47 @@ java-configure:
 	--disable-examples \
 	--with-antlr-jar=$(TOP)/java/antlr/antlr-3.4-complete.jar
 	
-	cd $(TOP)/java/jamvm && ./autogen.sh 
-	cd $(TOP)/java/jamvm && ./configure \
+#	cd $(TOP)/java/jamvm && ./autogen.sh 
+#	cd $(TOP)/java/jamvm && ./configure \
+#	--host=$(ARCH)-linux CC="$(ARCH)-linux-uclibc-gcc" \
+#	CFLAGS="$(COPTS) -I$(TOP)/zlib -L$(TOP)/zlib -DNEED_PRINTF" \
+#	CCASFLAGS="$(COPTS) -DNEED_PRINTF" \
+#	--with-java-runtime-library=gnuclasspath \
+#	--with-classpath-install-dir=/usr \
+#	--prefix=/usr \
+#	--libdir=/usr/lib \
+#	--disable-int-inlining \
+#	--disable-shared \
+#	--without-pic
+
+	cd $(TOP)/java/cacao && ./configure \
 	--host=$(ARCH)-linux CC="$(ARCH)-linux-uclibc-gcc" \
-	CFLAGS="$(COPTS) -I$(TOP)/zlib -L$(TOP)/zlib -DNEED_PRINTF" \
+	CFLAGS="$(COPTS) -I$(TOP)/zlib -L$(TOP)/zlib -DNEED_PRINTF -fPIC" \
+	CXXFLAGS="$(COPTS) -I$(TOP)/zlib -L$(TOP)/zlib -DNEED_PRINTF -fPIC" \
+	CPPFLAGS="$(COPTS) -I$(TOP)/zlib -L$(TOP)/zlib -DNEED_PRINTF -fPIC" \
 	CCASFLAGS="$(COPTS) -DNEED_PRINTF" \
 	--with-java-runtime-library=gnuclasspath \
+	--with-java-runtime-library-classes=$(TOP)/java/classpath/lib/glibj.zip \
 	--with-classpath-install-dir=/usr \
 	--prefix=/usr \
 	--libdir=/usr/lib \
 	--disable-int-inlining \
 	--disable-shared \
-	--without-pic
+	--without-pic \
+	--with-jasmin-jar=$(TOP)/java/jasmin/cup.jar:$(TOP)/java/jasmin/jasmin-sable.jar \
+	--with-junit-jar=$(TOP)/java/junit/junit4.jar
 
 java:
 	-make -C $(TOP)/java/classpath
 	-make -C $(TOP)/java/classpath
-	make -C $(TOP)/java/jamvm GLIBJ_ZIP=$(TOP)/java/classpath/lib/glibj.zip
+	#make -C $(TOP)/java/jamvm GLIBJ_ZIP=$(TOP)/java/classpath/lib/glibj.zip
+	make -C $(TOP)/java/cacao
 
 
 java-clean:
 	make -C $(TOP)/java/classpath clean
-	make -C $(TOP)/java/jamvm clean
+#	make -C $(TOP)/java/jamvm clean
+	make -C $(TOP)/java/cacao clean
 	
 	
 java-install:
@@ -48,10 +67,21 @@ java-install:
 	rm -rf $(INSTALLDIR)/java/usr/bin
 	rm -f $(INSTALLDIR)/java/usr/share/classpath/tools.zip
 	rm -f $(INSTALLDIR)/java/usr/lib/classpath/*.la
-	make -C $(TOP)/java/jamvm install DESTDIR=$(INSTALLDIR)/java
+#	make -C $(TOP)/java/jamvm install DESTDIR=$(INSTALLDIR)/java
+#	rm -f $(INSTALLDIR)/java/usr/lib/*.a
+#	rm -f $(INSTALLDIR)/java/usr/lib/*.la
+#	rm -rf $(INSTALLDIR)/java/usr/include
+
+	make -C $(TOP)/java/cacao install DESTDIR=$(INSTALLDIR)/java
+	rm -rf $(INSTALLDIR)/java/usr/include
 	rm -f $(INSTALLDIR)/java/usr/lib/*.a
 	rm -f $(INSTALLDIR)/java/usr/lib/*.la
-	rm -rf $(INSTALLDIR)/java/usr/include
+	rm -rf $(INSTALLDIR)/java/usr/lib/pkgconfig
+	rm -rf $(INSTALLDIR)/java/usr/share/gc
+	rm -rf $(INSTALLDIR)/java/usr/share/libatomic_ops
+	rm -rf $(INSTALLDIR)/java/usr/share/man
+
+
 	
 #	mkdir -p $(INSTALLDIR)/java/usr/lib/classpath
 #	mkdir -p $(INSTALLDIR)/java/usr/share/classpath
