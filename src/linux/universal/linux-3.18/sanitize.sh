3fc6b41af7c3608fd62fc02ba398d389b3de3547
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2016-06-01 13:36:22 +0000

    xd3200
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@29814 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/sanitize.sh
+++ src/linux/universal/linux-3.18/sanitize.sh
@@ -73,6 +73,7 @@ do
 	    echo CONFIG_E325N=y >> .config
 	    echo CONFIG_WR615N=y >> .config
 	    echo CONFIG_E380AC=y >> .config
+	    echo CONFIG_XD3200=y >> .config
 	    make oldconfig ARCH=mips
 	    sed -i 's/\CONFIG_WR841V8=y/ /g' .config	    
 	    sed -i 's/\CONFIG_WR710=y/ /g' .config	    
@@ -107,6 +108,7 @@ do
 	    sed -i 's/\CONFIG_WR615N=y/ /g' .config	    
 	    sed -i 's/\CONFIG_WR941V6=y/ /g' .config	    
 	    sed -i 's/\CONFIG_E380AC=y/ /g' .config	    
+	    sed -i 's/\CONFIG_XD3200=y/ /g' .config	    
 	    cp .config $i
     fi
 done
