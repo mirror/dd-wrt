23f558cf7a56f879f2fb6b436b955c553088aa2f
BrainSlayer <s.gottschall@dd-wrt.com>
2016-06-10 01:09:34 +0000

    for greenfield support, if supported by driver
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@29860 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 285e13410fcf8027bbdc3ec6318ae8879cc70dd2

--- src/router/services/networking/madwifi_ath9k.c
+++ src/router/services/networking/madwifi_ath9k.c
@@ -502,7 +502,9 @@ void setupHostAP_generic_ath9k(char *prefix, FILE * fp, int isrepeater, int aoss
 	if (!isath5k) {
 		char shortgi[32];
 		sprintf(shortgi, "%s_shortgi", prefix);
-		caps = mac80211_get_caps(prefix, nvram_default_match(shortgi, "1", "1") ? 1 : 0);
+		char greenfield[32];
+		sprintf(shortgi, "%s_gf", prefix);
+		caps = mac80211_get_caps(prefix, nvram_default_match(shortgi, "1", "1") ? 1 : 0, nvram_default_match(greenfield, "1", "0") ? 1 : 0);
 		if (strlen(ht) > 0) {
 			fprintf(fp, "ht_capab=[%s]%s\n", ht, caps);
 		} else {
