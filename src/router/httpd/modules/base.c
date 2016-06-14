edc0fc525d0d9328400b365d5104418de6a07596
BrainSlayer <s.gottschall@dd-wrt.com>
2016-06-14 23:28:09 +0000

    allow to configure spectral config
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@29949 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: bd3fb410cd028dd9ab5a8862ce6e9d35d9cb61b8

--- src/router/httpd/modules/base.c
+++ src/router/httpd/modules/base.c
@@ -660,7 +660,7 @@ void do_spectral_scan(struct mime_handler *handler, char *p, webs_t stream, char
 #endif
 		asprintf(&path, "/sys/kernel/debug/ieee80211/phy%d/ath9k", phy);
 
-	sysprintf("echo 64 > %s/spectral_count", path);
+	sysprintf("echo %s > %s/spectral_count", nvram_default_get("spectral_count", "64"), path);
 	sysprintf("cat %s/spectral_scan0 > /dev/null", path);
 #ifdef HAVE_ATH10K
 	if (is_ath10k(ifname)) {
@@ -691,7 +691,7 @@ void do_spectral_scan(struct mime_handler *handler, char *p, webs_t stream, char
 		buffer[result] = 0;
 		websWrite(stream, "%s", buffer);
 	}
-	fclose(fp);
+	pclose(fp);
 	sysprintf("echo disable > %s/spectral_scan_ctl", path);
 	free(path);
 
