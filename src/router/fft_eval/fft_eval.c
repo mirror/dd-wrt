661cf6f326a9aa359c5092a06921f93271b38795
BrainSlayer <BrainSlayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2016-06-14 19:13:49 +0000

    update copyright
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@29940 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/fft_eval/fft_eval.c
+++ src/router/fft_eval/fft_eval.c
@@ -2,6 +2,7 @@
  * Copyright (C) 2012 Simon Wunderlich <siwu@hrz.tu-chemnitz.de>
  * Copyright (C) 2012 Fraunhofer-Gesellschaft zur Foerderung der angewandten Forschung e.V.
  * Copyright (C) 2013 Gui Iribarren <gui@altermundi.net>
+ * Copyright (C) 2016 Sebastian Gottschall <s.gottschall@dd-wrt.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of version 2 of the GNU General Public
