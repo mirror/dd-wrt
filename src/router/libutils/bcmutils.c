1073124448a6c024bfe9becb40d92f149c0ce113
brainslayer <brainslayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2016-06-23 12:10:34 +0000

    cannot work here
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@30013 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/bcmutils.c
+++ src/router/libutils/bcmutils.c
@@ -391,7 +391,7 @@ int has_mimo(char *prefix)
 int has_ac(char *prefix)
 {
 #ifdef HAVE_ATH10K
-	return (is_ath10k(prefix) || is_mvebu(prefix)) && has_5ghz(prefix);
+	return (is_ath10k(prefix) || is_mvebu(prefix));
 #elif HAVE_ATH9K
 	return 0;
 #else
