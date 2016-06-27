fa2e95f807366cceaea6dba007a89283397d9fa2
BrainSlayer <s.gottschall@dd-wrt.com>
2016-06-27 14:02:31 +0000

    latest driver
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@30048 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.10/drivers/net/ethernet/ralink/mdio_rt2880.h
+++ src/linux/universal/linux-3.10/drivers/net/ethernet/ralink/mdio_rt2880.h
@@ -1,5 +1,4 @@
-/*
- *   This program is free software; you can redistribute it and/or modify
+/*   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation; version 2 of the License
  *
@@ -8,11 +7,9 @@
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.
  *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, write to the Free Software
- *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
- *
- *   Copyright (C) 2009-2013 John Crispin <blogic@openwrt.org>
+ *   Copyright (C) 2009-2015 John Crispin <blogic@openwrt.org>
+ *   Copyright (C) 2009-2015 Felix Fietkau <nbd@nbd.name>
+ *   Copyright (C) 2013-2015 Michael Lee <igvtee@gmail.com>
  */
 
 #ifndef _RALINK_MDIO_RT2880_H__
