8a5d106fcc1d06127f528886bce27dc5cddbc53f
BrainSlayer <s.gottschall@dd-wrt.com>
2016-06-30 08:19:08 +0000

    try it with 64
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@30070 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: e05dd26bba66aca521050921d14093eb45850029

--- src/router/httpd/modules/base.c
+++ src/router/httpd/modules/base.c
@@ -664,7 +664,7 @@ void do_spectral_scan(struct mime_handler *handler, char *p, webs_t stream, char
 	sysprintf("cat %s/spectral_scan0 > /dev/null", path);
 #ifdef HAVE_ATH10K
 	if (is_ath10k(ifname)) {
-		sysprintf("echo 256 > %s/spectral_bins", path);
+		sysprintf("echo 64 > %s/spectral_bins", path);
 		sysprintf("echo manual > %s/spectral_scan_ctl", path);
 		sysprintf("echo trigger > %s/spectral_scan_ctl", path);
 	} else
