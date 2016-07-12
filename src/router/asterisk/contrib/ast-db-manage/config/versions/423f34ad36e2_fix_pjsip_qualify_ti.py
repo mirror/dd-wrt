133733b6bdf36372f40b0d733b536593f1d858f6
BrainSlayer <s.gottschall@dd-wrt.com>
2016-07-12 20:12:20 +0000

    update asterisk
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@30195 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 9f5d5c61dcfc6adc81eefdb0f9062d328d86eb68

---
+++ src/router/asterisk/contrib/ast-db-manage/config/versions/423f34ad36e2_fix_pjsip_qualify_ti.py
@@ -0,0 +1,22 @@
+"""fix pjsip qualify timeout
+
+Revision ID: 423f34ad36e2
+Revises: 136885b81223
+Create Date: 2016-01-13 21:49:21.557734
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '423f34ad36e2'
+down_revision = '136885b81223'
+
+from alembic import op
+import sqlalchemy as sa
+
+def upgrade():
+    op.alter_column('ps_aors', 'qualify_timeout', type_=sa.Float)
+    op.alter_column('ps_contacts', 'qualify_timeout', type_=sa.Float)
+
+def downgrade():
+    op.alter_column('ps_aors', 'qualify_timeout', type_=sa.Integer)
+    op.alter_column('ps_contacts', 'qualify_timeout', type_=sa.Integer)
