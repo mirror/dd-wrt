10b47d33161190c230ca2a3c83a72ab7d7dfbd00
BrainSlayer <s.gottschall@dd-wrt.com>
2016-07-18 07:54:55 +0000

    fix libgd build
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@30252 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 5c6d77d21948cc6d2966e4cdbb2bba9a0f6786b7

--- src/router/rules/php5.mk
+++ src/router/rules/php5.mk
@@ -36,11 +36,29 @@ libgd-clean:
 	make -C libgd clean
 	
 libgd-configure:
-	cd libgd && ./configure --host=$(ARCH)-linux-uclibc  --with-jpeg=$(TOP)/minidlna/jpeg-8 --without-xpm --without-x --without-tiff --without-freetype --without-fontconfig --without-x --disable-shared --enable-static --with-zlib CC="ccache $(ARCH)-linux-uclibc-gcc" CFLAGS="-fPIC -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/minidlna/jpeg-8 -I$(TOP)/zlib" 'LDFLAGS=-L$(TOP)/minidlna/lib -L$(TOP)/zlib -L$(TOP)/libgd/libpng/.libs'
+	cd libgd && ./configure --host=$(ARCH)-linux-uclibc  \
+	--with-jpeg=$(TOP)/minidlna/jpeg-8 \
+	--without-xpm \
+	--without-x \
+	--without-tiff \
+	--without-freetype \
+	--without-fontconfig \
+	--without-x \
+	--disable-shared \
+	--enable-static \
+	--with-zlib \
+	CC="ccache $(ARCH)-linux-uclibc-gcc" \
+	CFLAGS="-fPIC -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/minidlna/jpeg-8 -I$(TOP)/zlib" \
+	LDFLAGS="-L$(TOP)/minidlna/lib -L$(TOP)/zlib -L$(TOP)/libgd/libpng/.libs -lpng12" \
+	LIBPNG_CFLAGS="-I$(TOP)/libgd/libpng" \
+	LIBPNG_LIBS="-L$(TOP)/libgd/libpng/.libs -lpng12"
+
 	CC="ccache $(ARCH)-linux-uclibc-gcc" \
 	CFLAGS="$(COPTS) $(MIPS16_OPT)   -I$(TOP)/minidlna/jpeg-8 -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/minidlna/jpeg-8 -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	LDFLAGS="$(COPTS) $(MIPS16_OPT)  -lm -L$(TOP)/zlib -L$(TOP)/libgd/libpng/.libs -lpng12 -L$(TOP)/minidlna/lib -ljpeg -fPIC -v -Wl,--verbose" \
+	LIBPNG_CFLAGS="-I$(TOP)/libgd/libpng" \
+	LIBPNG_LIBS="-L$(TOP)/libgd/libpng/.libs -lpng12" \
 	$(MAKE) -C libgd
 
 libgd-install:
