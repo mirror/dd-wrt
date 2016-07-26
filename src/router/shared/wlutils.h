7107314ab31282786196613e8f28e783576e4640
BrainSlayer <s.gottschall@dd-wrt.com>
2016-07-26 10:57:29 +0000

    several fixes for broadcasting vht160 in vht80 mode etc and experimental 80p80 support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@30294 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/wlutils.h
+++ src/router/shared/wlutils.h
@@ -159,7 +159,7 @@ extern int has_greenfield(char *interface);
 #ifdef HAVE_ATH10K
 extern int has_vht160(char *interface);
 extern int has_vht80plus80(char *interface);
-extern char *mac80211_get_vhtcaps(char *interface, int shortgi);
+extern char *mac80211_get_vhtcaps(char *interface, int shortgi, int vht80, int vht160, int vht8080);
 extern unsigned int get_ath10kreg(char *ifname, unsigned int reg);
 extern void set_ath10kreg(char *ifname, unsigned int reg, unsigned int value);
 extern void set_ath10kdistance(char *ifname, unsigned int distance);
