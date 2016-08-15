ad6a04489a9ef04b5b1bea4eb14444162e4b6014
brainslayer <brainslayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2016-08-15 22:36:43 +0000

    fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@30432 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.5/net/ipv4/tcp_input.c
+++ src/linux/universal/linux-3.5/net/ipv4/tcp_input.c
@@ -3773,7 +3773,7 @@ static bool tcp_process_frto(struct sock *sk, int flag)
 
 static inline u32 prandom_u32_max(u32 ep_ro)
 {
-	return (u32)(((u64) prandom_u32() * ep_ro) >> 32);
+	return (u32)(((u64) random32() * ep_ro) >> 32);
 }
 
 /* RFC 5961 7 [ACK Throttling] */
