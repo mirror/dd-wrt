b2782cc64828341d15556aa572b1696b0bdee2bf
brainslayer <brainslayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2016-08-17 10:32:50 +0000

    musl does not support this syntax
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@30435 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/timezones.c
+++ src/router/libutils/timezones.c
@@ -800,57 +800,57 @@ TIMEZONE_TO_TZSTRING allTimezones[] = {
 	{"Eire", "GMT0IST,M3.5.0/1,M10.5.0"}
 	,
 
-	{"Etc/GMT+1", "<GMT-1>-1"}
+	{"Etc/GMT+1", "GMT-1"}
 	,
-	{"Etc/GMT+10", "<GMT-10>-10"}
+	{"Etc/GMT+10", "GMT-10"}
 	,
-	{"Etc/GMT+11", "<GMT-11>-11"}
+	{"Etc/GMT+11", "GMT-11"}
 	,
-	{"Etc/GMT+12", "<GMT-12>-12"}
+	{"Etc/GMT+12", "GMT-12"}
 	,
-	{"Etc/GMT+2", "<GMT-2>-2"}
+	{"Etc/GMT+2", "GMT-2"}
 	,
-	{"Etc/GMT+3", "<GMT-3>-3"}
+	{"Etc/GMT+3", "GMT-3"}
 	,
-	{"Etc/GMT+4", "<GMT-4>-4"}
+	{"Etc/GMT+4", "GMT-4"}
 	,
-	{"Etc/GMT+5", "<GMT-5>-5"}
+	{"Etc/GMT+5", "GMT-5"}
 	,
-	{"Etc/GMT+6", "<GMT-6>-6"}
+	{"Etc/GMT+6", "GMT-6"}
 	,
-	{"Etc/GMT+7", "<GMT-7>-7"}
+	{"Etc/GMT+7", "GMT-7"}
 	,
-	{"Etc/GMT+8", "<GMT-8>-8"}
+	{"Etc/GMT+8", "GMT-8"}
 	,
-	{"Etc/GMT+9", "<GMT-9>-9"}
+	{"Etc/GMT+9", "GMT-9"}
 	,
-	{"Etc/GMT-1", "<GMT-1>-1"}
+	{"Etc/GMT-1", "GMT-1"}
 	,
-	{"Etc/GMT-10", "<GMT+10>10"}
+	{"Etc/GMT-10", "GMT+10"}
 	,
-	{"Etc/GMT-11", "<GMT+11>11"}
+	{"Etc/GMT-11", "GMT+11"}
 	,
-	{"Etc/GMT-12", "<GMT+12>12"}
+	{"Etc/GMT-12", "GMT+12"}
 	,
-	{"Etc/GMT-13", "<GMT+13>13"}
+	{"Etc/GMT-13", "GMT+13"}
 	,
-	{"Etc/GMT-14", "<GMT+14>14"}
+	{"Etc/GMT-14", "GMT+14"}
 	,
-	{"Etc/GMT-2", "<GMT+2>2"}
+	{"Etc/GMT-2", "GMT+2"}
 	,
-	{"Etc/GMT-3", "<GMT+3>3"}
+	{"Etc/GMT-3", "GMT+3"}
 	,
-	{"Etc/GMT-4", "<GMT+4>4"}
+	{"Etc/GMT-4", "GMT+4"}
 	,
-	{"Etc/GMT-5", "<GMT+5>5"}
+	{"Etc/GMT-5", "GMT+5"}
 	,
-	{"Etc/GMT-6", "<GMT+6>6"}
+	{"Etc/GMT-6", "GMT+6"}
 	,
-	{"Etc/GMT-7", "<GMT+7>7"}
+	{"Etc/GMT-7", "GMT+7"}
 	,
-	{"Etc/GMT-8", "<GMT+8>8"}
+	{"Etc/GMT-8", "GMT+8"}
 	,
-	{"Etc/GMT-9", "<GMT+9>9"}
+	{"Etc/GMT-9", "GMT+9"}
 	,
 	{"Etc/Greenwich", "GMT0"}
 	,
