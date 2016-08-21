0f3083d54e7e432e668aeede7af54c58c3940d67
BrainSlayer <s.gottschall@dd-wrt.com>
2016-08-21 16:18:44 +0000

    fix flags
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@30461 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/softether.mk
+++ src/router/rules/softether.mk
@@ -1,11 +1,17 @@
 softether-configure:
 	@true
 
-softether:
+softether: zlib quagga ncurses
+	-rm -f softether/bin/vpnserver/hamcore.se2
+	-rm -f softether/bin/vpnclient/hamcore.se2
+	-rm -f softether/bin/vpnbridge/hamcore.se2
+	-rm -f softether/bin/vpncmd/hamcore.se2
+	-rm -f softether/tmp/hamcorebuilder
+	-rm -f softether/src/bin/BuiltHamcoreFiles/unix/hamcore.se2
 	$(MAKE) -C softether clean
-	$(MAKE) -C softether CC=gcc FLAGS="-O2 -DCPU_64" src/bin/BuiltHamcoreFiles/unix/hamcore.se2
+	$(MAKE) -C softether CC="ccache gcc" FLAGS="-O2 -DCPU_64" src/bin/BuiltHamcoreFiles/unix/hamcore.se2
 	$(MAKE) -C softether clean
-	$(MAKE) -C softether FLAGS="$(MIPS16_OPT) -I$(TOP)/quagga -L$(TOP)/quagga/readline -I$(TOP)/openssl/include -L$(TOP)/openssl -I$(TOP)/zlib -L$(TOP)/zlib -I$(TOP)/ncurses/include -L$(TOP)/ncurses/lib"
+	$(MAKE) -C softether FLAGS="$(MIPS16_OPT) -I$(TOP)/quagga -L$(TOP)/quagga/readline -I$(TOP)/openssl/include -L$(TOP)/openssl -I$(TOP)/zlib -L$(TOP)/zlib -I$(TOP)/ncurses/include -L$(TOP)/ncurses/lib  -ffunction-sections -fdata-sections -Wl,--gc-sections"
 
 softether-clean:
 	$(MAKE) -C softether clean
