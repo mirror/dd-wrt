34b13daf0105620e7fc87eab579ac766e34ecb03
BrainSlayer <s.gottschall@dd-wrt.com>
2016-09-02 20:43:34 +0000

    softirq: let ksoftirqd do its job
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@30543 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 06e90e2e67f1455e56f8edd362ab761df1e36892

--- src/linux/universal/linux-3.5/kernel/softirq.c
+++ src/linux/universal/linux-3.5/kernel/softirq.c
@@ -84,6 +84,17 @@ static void wakeup_softirqd(void)
 }
 
 /*
+ * If ksoftirqd is scheduled, we do not want to process pending softirqs
+ * right now. Let ksoftirqd handle this at its own rate, to get fairness.
+ */
+static bool ksoftirqd_running(void)
+{
+	struct task_struct *tsk = __this_cpu_read(ksoftirqd);
+
+	return tsk && (tsk->state == TASK_RUNNING);
+}
+
+/*
  * preempt_count and SOFTIRQ_OFFSET usage:
  * - preempt_count is changed by SOFTIRQ_OFFSET on entering or leaving
  *   softirq processing.
@@ -288,7 +299,7 @@ asmlinkage void do_softirq(void)
 
 	pending = local_softirq_pending();
 
-	if (pending)
+	if (pending && !ksoftirqd_running())
 		__do_softirq();
 
 	local_irq_restore(flags);
@@ -319,6 +330,11 @@ void irq_enter(void)
 
 static inline void invoke_softirq(void)
 {
+#ifndef __ARCH_HAS_DO_SOFTIRQ
+	if (ksoftirqd_running())
+		return;
+#endif
+
 	if (!force_irqthreads) {
 #ifdef __ARCH_IRQ_EXIT_IRQS_DISABLED
 		__do_softirq();
