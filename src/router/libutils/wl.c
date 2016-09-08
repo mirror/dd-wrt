6a8cb24e1fb9ae2424d1548f88c0c9af6579b6e7
BrainSlayer <s.gottschall@dd-wrt.com>
2016-09-08 14:05:49 +0000

    fix channel boundaries
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@30597 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 05495ea0c5b8fb0b9c3efcbdefd5667223d876de

--- src/router/libutils/wl.c
+++ src/router/libutils/wl.c
@@ -47,7 +47,7 @@ int ieee80211_mhz2ieee(int freq)
 	if (freq < 4990 && freq > 4940)
 		return ((freq * 10) + (((freq % 5) == 2) ? 5 : 0) - 49400) / 5;
 	// 5000 will become  channel 200
-	if (freq > 4910 && freq < 5005)
+	if (freq >= 4800 && freq < 5005)
 		return (freq - 4000) / 5;
 	if (freq < 5000)
 		return 15 + ((freq - 2512) / 20);
