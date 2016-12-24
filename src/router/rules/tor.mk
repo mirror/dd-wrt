a4a368a22d3ed78327c565eb2394ce035117082e
BrainSlayer <s.gottschall@dd-wrt.com>
2016-12-24 11:39:49 +0000

    configure seem to spit out bogus results with some toolchains
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31006 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 5b7f62a3118496817e3f34062c1041eb945255b8

--- src/router/rules/tor.mk
+++ src/router/rules/tor.mk
@@ -17,6 +17,7 @@ miniupnpc-install:
 tor-configure: libevent
 	cd tor && ./configure  --prefix=/usr ac_cv_host=$(ARCH)-uclibc-linux --target=$(ARCH)-linux --disable-systemd --host=$(ARCH) CC="ccache $(ARCH)-linux-uclibc-gcc" \
 	--disable-asciidoc \
+	--disable-tool-name-check \
 	--disable-gcc-hardening \
 	CFLAGS="$(COPTS) $(MIPS16_OPT) -std=gnu99 -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib -I$(TOP) -I$(TOP)/openssl/include -I$(TOP)/libevent -I$(TOP)/libevent/include" \
 	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -std=gnu99 -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib -I$(TOP) -I$(TOP)/openssl/include -I$(TOP)/libevent  -I$(TOP)/libevent/include" \
