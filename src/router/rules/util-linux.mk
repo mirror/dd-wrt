fe63d72d7df566fbd5be5e492e29a596d9a856c5
BrainSlayer <s.gottschall@dd-wrt.com>
2016-12-29 19:59:08 +0000

    required for new glib
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31033 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 0df3eb432129a0c8b7d5c7ef3c4d7e6983b1580a

---
+++ src/router/rules/util-linux.mk
@@ -0,0 +1,60 @@
+util-linux-configure:
+	make -C util-linux clean
+	cd util-linux && ./configure --host=$(ARCH)-linux-uclibc --prefix=/usr --libdir=/usr/lib CFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF" PKG_CONFIG="/tmp" NCURSES_CFLAGS="-I$(TOP)/ncurses/include" NCURSES_LIBS="-L$(TOP)/ncurses/lib -lncurses" \
+	--disable-rpath \
+	--enable-new-mount	\
+	--disable-tls		\
+	--disable-sulogin	\
+	--without-python	\
+	--without-udev		\
+	--with-ncurses
+	make -C util-linux
+
+util-linux-clean:
+	make -C util-linux clean
+
+util-linux:
+	make -C util-linux clean
+	make -C util-linux
+
+util-linux-install:
+	make -C util-linux clean
+	make -C util-linux
+	make -C util-linux install DESTDIR=$(INSTALLDIR)/util-linux
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libuuid.a
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libblkid.a
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libmount.la
+	rm -f $(TOP)/util-linux/.libmount.la
+	rm -f $(TOP)/util-linux/.libs/libuuid.a
+	rm -f $(TOP)/util-linux/.libs/libblkid.a
+	rm -f $(TOP)/util-linux/.libs/libmount.la
+ifneq ($(CONFIG_ASTERISK),y)
+	rm -rf $(INSTALLDIR)/util-linux/usr/sbin
+	rm -rf $(INSTALLDIR)/util-linux/usr/bin
+	rm -rf $(INSTALLDIR)/util-linux/usr/share
+	rm -rf $(INSTALLDIR)/util-linux/usr/include
+	rm -rf $(INSTALLDIR)/util-linux/usr/lib/pkgconfig
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libmount*
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libfdisk*
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libsmartcols*
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libblkid.a
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libblkid.la
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libuuid.a
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libuuid.la
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libblkid.so*
+	rm -rf $(INSTALLDIR)/util-linux/bin
+	rm -rf $(INSTALLDIR)/util-linux/sbin
+	rm -f $(INSTALLDIR)/util-linux/lib/libfdisk.so*
+	rm -f $(INSTALLDIR)/util-linux/lib/libsmartcols.so*
+endif
+ifneq ($(CONFIG_ASTERISK),y)
+ifneq ($(CONFIG_ZABBIX),y)
+ifneq ($(CONFIG_MC),y)
+ifneq ($(CONFIG_LIBQMI),y)
+ifneq ($(CONFIG_WEBSERVER),y)
+	rm -f $(INSTALLDIR)/util-linux/lib/libmount.so*
+endif
+endif
+endif
+endif
+endif
