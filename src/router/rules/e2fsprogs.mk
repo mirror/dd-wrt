691e949075c589b68d9e4a049ec7ce350552052e
BrainSlayer <s.gottschall@dd-wrt.com>
2016-12-30 08:55:47 +0000

    this increases build speed, remove unnessecary rebuilds
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31044 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/e2fsprogs.mk
+++ src/router/rules/e2fsprogs.mk
@@ -19,13 +19,10 @@ btrfsprogs-configure: lzo
 	cd btrfsprogs && ./configure --host=$(ARCH)-linux CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -I$(TOP)/btrfsprogs -I$(TOP)/$(ARCH)-uclibc/install/util-linux/usr/include -I$(TOP)/e2fsprogs/lib -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -I$(TOP)/zlib -I$(TOP)/lzo/include -DNEED_PRINTF" LDFLAGS="-L$(TOP)/zlib -L$(TOP)/$(ARCH)-uclibc/install/util-linux/usr/lib  -L$(TOP)/e2fsprogs/lib -L$(TOP)/lzo/src/.libs -lz  -ffunction-sections -fdata-sections -Wl,--gc-sections" CC="$(CC) $(COPTS)" --disable-backtrace --disable-documentation root_prefix=$(INSTALLDIR)/btrfsprogs  ZLIB_CFLAGS=" " ZLIB_LIBS="$(TOP)/zlib/libz.a" EXT2FS_CFLAGS=" " EXT2FS_LIBS="-lext2fs" COM_ERR_CFLAGS=" " COM_ERR_LIBS="-lcom_err" BLKID_CFLAGS=" " BLKID_LIBS="-lblkid"
 
 btrfsprogs: lzo util-linux
-	make -C util-linux clean
 	make -C util-linux
 	make -C util-linux install DESTDIR=$(INSTALLDIR)/util-linux
 	rm -f $(INSTALLDIR)/util-linux/usr/lib/libuuid.a
 	rm -f $(INSTALLDIR)/util-linux/usr/lib/libblkid.so*
-	rm -f $(TOP)/util-linux/.libs/libuuid.a
-	rm -f $(TOP)/util-linux/.libs/libblkid.so*
 	make -C btrfsprogs 
 
 btrfsprogs-clean:
@@ -34,8 +31,6 @@ btrfsprogs-clean:
 btrfsprogs-install:	
 	rm -f $(INSTALLDIR)/util-linux/usr/lib/libuuid.a
 	rm -f $(INSTALLDIR)/util-linux/usr/lib/libblkid.so*
-	rm -f $(TOP)/util-linux/.libs/libuuid.a
-	rm -f $(TOP)/util-linux/.libs/libblkid.so*
 	-make -C btrfsprogs install DESTDIR=$(INSTALLDIR)/btrfsprogs prefix=/usr
 	-rm -f $(INSTALLDIR)/btrfsprogs/usr/bin/btrfs*
 	-rm -rf $(INSTALLDIR)/btrfsprogs/usr/include
