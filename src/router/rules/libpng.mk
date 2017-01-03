01fbd7f36098108a6233e769f9da2ef025ed6e06
BrainSlayer <s.gottschall@dd-wrt.com>
2017-01-03 23:42:36 +0000

    update libs
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31084 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libpng.mk
+++ src/router/rules/libpng.mk
@@ -1,23 +1,17 @@
 libpng:
-	cd libgd && \
+	cd libpng && \
 	CC="ccache $(ARCH)-linux-uclibc-gcc" \
 	CFLAGS="$(COPTS) $(MIPS16_OPT)   -I$(TOP)/zlib/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	LDFLAGS="$(COPTS) $(MIPS16_OPT)  -L$(TOP)/zlib -fPIC -v -Wl,--verbose" \
+	LDFLAGS="$(COPTS) $(MIPS16_OPT)  -L$(TOP)/zlib -fPIC -v -Wl,--verbose"
 	$(MAKE) -C libpng
-	-mkdir -p $(TOP)/libgd/libpng/.libs/include
-	-cp $(TOP)/libgd/libpng/*.h $(TOP)/libgd/libpng/.libs/include
-	
+	-mkdir -p $(TOP)/libpng/.libs/include
+	-cp $(TOP)/libpng/*.h $(TOP)/libpng/.libs/include
+	-cp $(TOP)/libpng/.libs/libpng16.a $(TOP)/libpng/.libs/libpng.a
+
 libpng-clean:
-	cd libgd && \
 	make -C libpng clean
 	
 libpng-configure:
-	cd libgd && \
-	cd libpng &&   ./configure --host=$(ARCH)-linux-uclibc  --disable-shared --enable-static CC="ccache $(ARCH)-linux-uclibc-gcc" CFLAGS="-fPIC -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib/include" CPPFLAGS="-fPIC -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib/include" 'LDFLAGS=-L$(TOP)/zlib'	
-	cd libgd && \
-	CC="ccache $(ARCH)-linux-uclibc-gcc" \
-	CFLAGS="$(COPTS) $(MIPS16_OPT)   -I$(TOP)/zlib/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	LDFLAGS="$(COPTS) $(MIPS16_OPT)  -L$(TOP)/zlib -fPIC -v -Wl,--verbose" \
+	cd libpng &&   ./configure --host=$(ARCH)-linux-uclibc  --disable-shared --enable-static CC="ccache $(ARCH)-linux-uclibc-gcc" CFLAGS="-fPIC $(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib/include" CPPFLAGS="-fPIC $(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib/include" 'LDFLAGS=-L$(TOP)/zlib'
 	$(MAKE) -C libpng
