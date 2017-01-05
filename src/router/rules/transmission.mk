188e78e028826d88875302030ca2392674a74e81
BrainSlayer <s.gottschall@dd-wrt.com>
2017-01-05 21:24:04 +0000

    split another makefile
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31097 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: cdd8291116514b78fa9940b1b3b0df72f5db24e5

--- src/router/rules/transmission.mk
+++ src/router/rules/transmission.mk
@@ -1,32 +1,3 @@
-curl:
-	CC="ccache $(ARCH)-linux-uclibc-gcc" \
-	CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib -I$(TOP)/openssl/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib -I$(TOP)/openssl/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	LDFLAGS="$(COPTS) $(MIPS16_OPT) -L$(TOP)/zlib -L$(TOP)/openssl -lcrypo -lssl -ldl" \
-	$(MAKE) -C curl
-
-curl-install:
-	$(MAKE) -C curl install DESTDIR=$(INSTALLDIR)/curl
-	rm -f $(INSTALLDIR)/curl/usr/bin/curl-config
-	rm -rf $(INSTALLDIR)/curl/usr/include
-	rm -rf $(INSTALLDIR)/curl/usr/lib/pkgconfig
-	rm -f $(INSTALLDIR)/curl/usr/lib/*.a
-	rm -f $(INSTALLDIR)/curl/usr/lib/*.la
-	rm -rf $(INSTALLDIR)/curl/usr/share
-
-curl-clean:
-	$(MAKE) -C curl clean
-
-curl-configure:
-	cd curl && aclocal && automake && ./configure --prefix=/usr ac_cv_host=$(ARCH)-uclibc-linux --libdir=/usr/lib --target=$(ARCH)-linux --host=$(ARCH) CC="ccache $(ARCH)-linux-uclibc-gcc" \
-	CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib  -I$(TOP)/openssl/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	LDFLAGS="$(COPTS) $(MIPS16_OPT) -L$(TOP)/zlib -L$(TOP)/openssl -lcrypto -lssl -ldl" 
-	CC="ccache $(ARCH)-linux-uclibc-gcc" \
-	CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib -I$(TOP)/openssl/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib -I$(TOP)/openssl/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	LDFLAGS="$(COPTS) $(MIPS16_OPT) -L$(TOP)/zlib -L$(TOP)/openssl -lcrypo -lssl -ldl" \
-	$(MAKE) -C curl
-
 transmission: libevent curl
 	CC="ccache $(ARCH)-linux-uclibc-gcc" \
 	CFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib  -I$(TOP)/curl/include -I$(TOP)/libevent/include" \
