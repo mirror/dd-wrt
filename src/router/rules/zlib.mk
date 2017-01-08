675b3ad818bb024c06da2e6e31b579b312008160
brainslayer <brainslayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2017-01-08 12:39:01 +0000

    upgrade zlib
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31108 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/zlib.mk
+++ src/router/rules/zlib.mk
@@ -9,7 +9,7 @@ zlib:
 	rm -f zlib/libz.a
 
 zlib-install:
-	install -D zlib/libz.so.1.2.8 $(INSTALLDIR)/zlib/usr/lib/libz.so.1.2.8
-	cd $(INSTALLDIR)/zlib/usr/lib ; ln -s libz.so.1.2.8 libz.so.1  ; true
-	cd $(INSTALLDIR)/zlib/usr/lib ; ln -s libz.so.1.2.8 libz.so  ; true
+	install -D zlib/libz.so.1.2.10 $(INSTALLDIR)/zlib/usr/lib/libz.so.1.2.10
+	cd $(INSTALLDIR)/zlib/usr/lib ; ln -s libz.so.1.2.10 libz.so.1  ; true
+	cd $(INSTALLDIR)/zlib/usr/lib ; ln -s libz.so.1.2.10 libz.so  ; true
 
