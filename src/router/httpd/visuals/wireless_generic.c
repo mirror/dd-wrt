507820eacdf993dd938ce220fbf7c55bdd9a0344
brainslayer <brainslayer@52c4871e-980c-0410-b1e0-e73912ce01f8>
2017-01-28 12:18:52 +0000

    fix some newly introduced musl foo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31198 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/wireless_generic.c
+++ src/router/httpd/visuals/wireless_generic.c
@@ -22,14 +22,16 @@
 static char *UPTIME(int uptime)
 {
 	int days, minutes;
-	static char str[64] = { 0 };
+	char str[64] = { 0 };
+	char str2[64] = { 0 };
 	memset(str, 0, 64);
+	memset(str2, 0, 64);
 	days = uptime / (60 * 60 * 24);
 	if (days)
-		sprintf(str, "%d day%s, ", days, (days == 1 ? "" : "s"));
+		sprintf(str2, "%d day%s, ", days, (days == 1 ? "" : "s"));
 	minutes = uptime / 60;
-	if (strlen(str) > 0)
-		sprintf(str, "%s %d:%02d:%02d", str, (minutes / 60) % 24, minutes % 60, uptime % 60);
+	if (strlen(str2) > 0)
+		sprintf(str, "%s %d:%02d:%02d", str2, (minutes / 60) % 24, minutes % 60, uptime % 60);
 	else
 		sprintf(str, "%d:%02d:%02d", (minutes / 60) % 24, minutes % 60, uptime % 60);
 	return str;
