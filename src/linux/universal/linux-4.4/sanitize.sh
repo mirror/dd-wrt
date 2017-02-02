998928ebc1ff2f8e666b548d05a94c0f4c5b9fa3
BrainSlayer <s.gottschall@dd-wrt.com>
2017-02-02 19:44:14 +0000

    wallystech dr342-nas
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31223 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 7e0c84cc824450e567b2a237d432abe70f24e9b9

--- src/linux/universal/linux-4.4/sanitize.sh
+++ src/linux/universal/linux-4.4/sanitize.sh
@@ -75,6 +75,7 @@ do
 	    echo CONFIG_E380AC=y >> .config
 	    echo CONFIG_XD3200=y >> .config
 	    echo CONFIG_AP120C=y >> .config
+	    echo CONFIG_WILLY=y >> .config
 	    make oldconfig ARCH=mips
 	    sed -i 's/\CONFIG_WR841V8=y/ /g' .config	    
 	    sed -i 's/\CONFIG_WR710=y/ /g' .config	    
@@ -111,6 +112,7 @@ do
 	    sed -i 's/\CONFIG_E380AC=y/ /g' .config	    
 	    sed -i 's/\CONFIG_XD3200=y/ /g' .config	    
 	    sed -i 's/\CONFIG_AP120C=y/ /g' .config	    
+	    sed -i 's/\CONFIG_WILLY=y/ /g' .config	    
     fi
     sed -i 's/\# CONFIG_EXFAT_FS is not set/ /g' .config	    
     cp .config $i
