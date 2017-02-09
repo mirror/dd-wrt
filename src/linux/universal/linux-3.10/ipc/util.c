bde35762a054b0ca4fc7cfc2af82063850bc94d7
BrainSlayer <s.gottschall@dd-wrt.com>
2017-02-09 07:39:48 +0000

    update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31295 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 27b816a252a146a5b19dcfea3d84d0b482a73451

--- src/linux/universal/linux-3.10/ipc/util.c
+++ src/linux/universal/linux-3.10/ipc/util.c
@@ -907,10 +907,8 @@ static int sysvipc_proc_show(struct seq_file *s, void *it)
 	struct ipc_proc_iter *iter = s->private;
 	struct ipc_proc_iface *iface = iter->iface;
 
-	if (it == SEQ_START_TOKEN) {
-		seq_puts(s, iface->header);
-		return 0;
-	}
+	if (it == SEQ_START_TOKEN)
+		return seq_puts(s, iface->header);
 
 	return iface->show(s, it);
 }
