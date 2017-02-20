0699754060dd4bfeb256db0ea020538bed8c7bc5
BrainSlayer <s.gottschall@dd-wrt.com>
2017-02-20 18:36:31 +0000

    fix headers
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31477 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: b4482238494e157d5b2d78fb9191cda0e1bbe786

--- src/router/rules/usbip.mk
+++ src/router/rules/usbip.mk
@@ -12,7 +12,7 @@ usbip-configure: util-linux
 	rm -f $(INSTALLDIR)/util-linux/usr/lib/libuuid.la
 	rm -f $(INSTALLDIR)/util-linux/usr/lib/libblkid.a
 	rm -f $(INSTALLDIR)/util-linux/usr/lib/libblkid.la
-	cd usbip/eudev && ./configure --host=$(ARCH)-linux CC="$(CC)" BLKID_CFLAGS=" " BLKID_LIBS="-lblkid -luuid" CFLAGS="$(COPTS) $(MIPS16_OPT)  -L$(TOP)/$(ARCH)-uclibc/install/util-linux/usr/lib -DNEED_PRINTF -D_GNU_SOURCE -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_realloc=realloc -Drpl_malloc=malloc"  --disable-nls --prefix=/usr --disable-hwdb --disable-introspection --disable-manpages --disable-selinux --enable-blkid --disable-kmod
+	cd usbip/eudev && ./configure --host=$(ARCH)-linux CC="$(CC)" BLKID_CFLAGS=" " BLKID_LIBS="-lblkid -luuid" CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/kernel_headers/$(KERNELRELEASE)/include  -L$(TOP)/$(ARCH)-uclibc/install/util-linux/usr/lib -DNEED_PRINTF -D_GNU_SOURCE -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_realloc=realloc -Drpl_malloc=malloc"  --disable-nls --prefix=/usr --disable-hwdb --disable-introspection --disable-manpages --disable-selinux --enable-blkid --disable-kmod
 	touch usbip/eudev/*
 	make -C usbip/eudev
 	cd usbip && ./autogen.sh
