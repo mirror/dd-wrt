0a4879aa9164ec4bc1ad01799cb14ee1c47ca8f8
BrainSlayer <s.gottschall@dd-wrt.com>
2017-03-08 21:55:23 +0000

    fix wrong argument
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31609 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: e6d8b88b3a0f8d717fc7d068fbbe5b04daf39434

--- src/linux/universal/linux-4.9/include/linux/fs.h
+++ src/linux/universal/linux-4.9/include/linux/fs.h
@@ -2873,7 +2873,7 @@ static inline void dio_end_io(struct bio *bio, int error)
 {
 }
 static ssize_t __blockdev_direct_IO(struct kiocb *iocb, struct inode *inode,
-	struct block_device *bdev, struct iov_iter *iter, loff_t offset,
+	struct block_device *bdev, struct iov_iter *iter,
 	get_block_t get_block, dio_iodone_t end_io,
 	dio_submit_t submit_io,	int flags)
 {
