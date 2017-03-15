11ef835ee78c984ffb7b5aa0a7e5a20fd3ba828a
BrainSlayer <s.gottschall@dd-wrt.com>
2017-03-15 02:38:03 +0000

    fix typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31670 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 79b3c863451f7d81b989d9b24ce94897b8b3b52d

--- src/linux/universal/linux-4.4/drivers/mtd/mtdpart.c
+++ src/linux/universal/linux-4.4/drivers/mtd/mtdpart.c
@@ -736,7 +736,7 @@ static int split_squashfs(struct mtd_info *master, int offset, int *split_offset
 		return -EINVAL;
 	}
 	printk(KERN_EMERG " magic %X vs %X\n",sb.s_magic, SQUASHFS_MAGIC);
-	if (SQUASHFS_MAGIC != le32_to_cpu(sb.s_magic) {
+	if (SQUASHFS_MAGIC != le32_to_cpu(sb.s_magic)) {
 		printk(KERN_ALERT "split_squashfs: no squashfs found in \"%s\"\n",
 			master->name);
 		*split_offset = 0;
