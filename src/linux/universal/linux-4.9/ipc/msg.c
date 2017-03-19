be3886a23f417ffd1284e033f39164365eb92081
BrainSlayer <s.gottschall@dd-wrt.com>
2017-03-19 17:33:47 +0000

    update debloat patch
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31711 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.9/ipc/msg.c
+++ src/linux/universal/linux-4.9/ipc/msg.c
@@ -1058,6 +1058,9 @@ void __init msg_init(void)
 {
 	msg_init_ns(&init_ipc_ns);
 
+	if (IS_ENABLED(CONFIG_PROC_STRIPPED))
+		return;
+
 	ipc_init_proc_interface("sysvipc/msg",
 				"       key      msqid perms      cbytes       qnum lspid lrpid   uid   gid  cuid  cgid      stime      rtime      ctime\n",
 				IPC_MSG_IDS, sysvipc_msg_proc_show);
