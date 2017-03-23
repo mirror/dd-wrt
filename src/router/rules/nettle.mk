8afbbac8fae606e060fa1062abeb1452c46eb2c1
kong <USER@DOMAIN>
2017-03-23 21:03:58 +0000

    add nettle make rule
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31746 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rules/nettle.mk
@@ -0,0 +1,19 @@
+NETTLE_CONFIGURE_ARGS+= \
+	--disable-documentation \
+	--with-include-path="$(TOP)/openssl/include" \
+	--with-lib-path="$(TOP)/openssl , $(TOP)/gmp"
+
+nettle-configure: openssl
+	cd nettle && ./configure --host=$(ARCH)-linux --prefix=/usr CC="ccache $(CC)" $(NETTLE_CONFIGURE_ARGS) CFLAGS="-fPIC -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/pcre -I$(TOP)/gmp -I$(TOP)/zlib" CPPFLAGS="$(COPTS) $(MIPS16_OPT)" LDFLAGS="-L$(TOP)/pcre/.libs -L$(TOP)/gmp/.libs -lpthread -lpcre -L$(TOP)/zlib $(LDFLAGS) -lz"
+
+nettle: openssl
+	make -C nettle 
+
+nettle-clean:
+	-make -C nettle clean
+
+nettle-install:
+	mkdir -p $(INSTALLDIR)/nettle/usr/lib
+ifeq ($(CONFIG_DNSSEC),y)
+	install -D nettle/.lib/* $(INSTALLDIR)/nettle/usr/lib
+endif
