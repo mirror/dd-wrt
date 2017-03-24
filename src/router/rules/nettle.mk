0c086cff8e8222a25f758aeb9bbde1045959a2d9
BrainSlayer <s.gottschall@dd-wrt.com>
2017-03-24 06:24:25 +0000

    should not be required here
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31768 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: a21acf04f45a5ebc1f22e265d1854c95c9bae1ca

--- src/router/rules/nettle.mk
+++ src/router/rules/nettle.mk
@@ -4,7 +4,7 @@ NETTLE_CONFIGURE_ARGS+= \
 	--with-lib-path="$(TOP)/openssl , $(TOP)/gmp"
 
 nettle-configure: openssl gmp
-	cd nettle && ./configure --host=$(ARCH)-linux --prefix=/usr CC="ccache $(CC)" $(NETTLE_CONFIGURE_ARGS) CFLAGS="-fPIC -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/pcre -I$(TOP)/gmp -I$(TOP)/zlib" CPPFLAGS="$(COPTS) $(MIPS16_OPT)" LDFLAGS="-L$(TOP)/pcre/.libs -L$(TOP)/gmp/.libs -lpthread -lpcre -L$(TOP)/zlib $(LDFLAGS) -lz"
+	cd nettle && ./configure --host=$(ARCH)-linux --prefix=/usr CC="ccache $(CC)" $(NETTLE_CONFIGURE_ARGS) CFLAGS="-fPIC $(COPTS) $(MIPS16_OPT) -I$(TOP)/pcre -I$(TOP)/gmp -I$(TOP)/zlib" CPPFLAGS="$(COPTS) $(MIPS16_OPT)" LDFLAGS="-L$(TOP)/pcre/.libs -L$(TOP)/gmp/.libs -lpthread -lpcre -L$(TOP)/zlib $(LDFLAGS) -lz"
 
 nettle: openssl gmp
 	make -C nettle 
