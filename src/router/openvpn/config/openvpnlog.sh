d52663486f46b1c85315350c15969aaac862adca
BrainSlayer <s.gottschall@dd-wrt.com>
2017-04-04 15:17:24 +0000

    openvpn use port for status that is configured, and not fixed ones
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31814 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: eed93c4b9a29d0cfcaaef4dc6426dd9cbb1f80ef

--- src/router/openvpn/config/openvpnlog.sh
+++ src/router/openvpn/config/openvpnlog.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 if [ "$(nvram get openvpn_enable)" = "1" ]; then
+PORT=`grep "^management " /tmp/openvpn/openvpn.conf | awk '{print $3}'`
+if [ x${PORT} = x ]
+then
+	PORT=14
+fi
 /bin/echo -e "Serverlog: <br>"
-/bin/echo "log 500" | /usr/bin/nc 127.0.0.1 14 | /bin/grep -v "^>" | /usr/bin/awk -F "," '{
+/bin/echo "log 500" | /usr/bin/nc 127.0.0.1 ${PORT }| /bin/grep -v "^>" | /usr/bin/awk -F "," '{
 	printf strftime("%Y%m%d %H:%M:%S ",$1);
 	for (i=2;i<=NF;i++)
 		printf $i" "
@@ -9,13 +14,18 @@ if [ "$(nvram get openvpn_enable)" = "1" ]; then
 	}'
 
 /bin/echo -e "<br>"
-/bin/cat /tmp/openvpn/openvpn.conf
+#/bin/cat /tmp/openvpn/openvpn.conf
 
 /bin/echo -e "<br><br><br>"
 fi
 if [ "$(nvram get openvpncl_enable)" = "1" ]; then
+PORT=`grep "^management " /tmp/openvpncl/openvpn.conf | awk '{print $3}'`
+if [ x${PORT} = x ]
+then
+	PORT=16
+fi
 /bin/echo -e "Clientlog: <br>"
-/bin/echo "log 500" | /usr/bin/nc 127.0.0.1 16 | /bin/grep -v "^>" | /usr/bin/awk -F "," '{
+/bin/echo "log 500" | /usr/bin/nc 127.0.0.1 ${PORT} | /bin/grep -v "^>" | /usr/bin/awk -F "," '{
 	printf strftime("%Y%m%d %H:%M:%S ",$1);
 	for (i=2;i<=NF;i++)
 		printf $i" "
@@ -23,5 +33,5 @@ if [ "$(nvram get openvpncl_enable)" = "1" ]; then
 	}'
 
 /bin/echo -e "<br>"
-/bin/cat /tmp/openvpncl/openvpn.conf
+#/bin/cat /tmp/openvpncl/openvpn.conf
 fi
