608fbd18352ed6e1140a0c8ee6ec7c17a31d0c19
BrainSlayer <s.gottschall@dd-wrt.com>
2017-04-23 17:43:18 +0000

    sd8887 id
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31894 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: d218aabac138cdc1be7a15d2945e1f08ec5a8227

--- src/router/libutils/utils.c
+++ src/router/libutils/utils.c
@@ -7422,6 +7422,8 @@ static struct wifidevices wdevices[] = {
 	{"88W8864 802.11ac", 0x11ab, 0x2a55},
 	{"88W8897 802.11ac", 0x11ab, 0x2b38},
 	{"WIL6210 802.11ad", 0x1ae9, 0x0310},
+	{"SD8887 802.11ac", 0x02df, 0x9135},
+
 
 };
 
