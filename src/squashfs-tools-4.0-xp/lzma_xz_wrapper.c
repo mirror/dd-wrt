657b0449cc71ff3891ab6c99053316eb1a2822e6
BrainSlayer <s.gottschall@dd-wrt.com>
2017-04-27 21:37:37 +0000

    special squashfs variant based on squashfs 4.0, much slower but higher compression ratio. its compatible to standard squashfs by the way and only xz compression has been tested, lzma hasnt been finished here
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31918 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/squashfs-tools-4.0-xp/lzma_xz_wrapper.c
@@ -0,0 +1,394 @@
+/*
+ * Copyright (c) 2010, 2013
+ * Phillip Lougher <phillip@squashfs.org.uk>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * lzma_xz_wrapper.c
+ *
+ * Support for LZMA1 compression using XZ Utils liblzma http://tukaani.org/xz/
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <lzma.h>
+
+#include "squashfs_fs.h"
+#include "compressor.h"
+#include "lzma_xz_options.h"
+
+#define LZMA_PROPS_SIZE 5
+#define LZMA_UNCOMP_SIZE 8
+#define LZMA_HEADER_SIZE (LZMA_PROPS_SIZE + LZMA_UNCOMP_SIZE)
+
+#define LZMA_OPTIONS 5
+#define MEMLIMIT (32 * 1024 * 1024)
+
+static int lzma_compress_2(void *dest, void *src,  int size,
+	int block_size, int *error,int lc, int lp, int pb)
+{
+	uint32_t preset;
+	unsigned char *d = (unsigned char *) dest;
+	struct lzma_xz_options *opts = lzma_xz_get_options();
+
+	lzma_options_lzma opt;
+	lzma_stream strm = LZMA_STREAM_INIT;
+	int res;
+
+	preset = opts->preset;
+
+	preset |= LZMA_PRESET_EXTREME;
+
+	lzma_lzma_preset(&opt, opts->preset);
+	opt.lc = lc;
+	opt.lp = lp;
+	opt.pb = pb;
+//	opt.nice_len = 64;
+	opt.dict_size = opts->dict_size;
+
+	res = lzma_alone_encoder(&strm, &opt);
+	if(res != LZMA_OK) {
+		lzma_end(&strm);
+		goto failed;
+	}
+
+	strm.next_out = dest;
+	strm.avail_out = block_size;
+	strm.next_in = src;
+	strm.avail_in = size;
+
+	res = lzma_code(&strm, LZMA_FINISH);
+	lzma_end(&strm);
+
+	if(res == LZMA_STREAM_END) {
+		/*
+	 	 * Fill in the 8 byte little endian uncompressed size field in
+		 * the LZMA header.  8 bytes is excessively large for squashfs
+		 * but this is the standard LZMA header and which is expected by
+		 * the kernel code
+	 	 */
+
+		d[LZMA_PROPS_SIZE] = size & 255;
+		d[LZMA_PROPS_SIZE + 1] = (size >> 8) & 255;
+		d[LZMA_PROPS_SIZE + 2] = (size >> 16) & 255;
+		d[LZMA_PROPS_SIZE + 3] = (size >> 24) & 255;
+		d[LZMA_PROPS_SIZE + 4] = lc;
+		d[LZMA_PROPS_SIZE + 5] = lp;
+		d[LZMA_PROPS_SIZE + 6] = pb;
+		d[LZMA_PROPS_SIZE + 7] = opt.nice_len;
+
+		return (int) strm.total_out;
+	}
+
+	if(res == LZMA_OK)
+		/*
+	 	 * Output buffer overflow.  Return out of buffer space
+	 	 */
+		return 0;
+
+failed:
+	/*
+	 * All other errors return failure, with the compressor
+	 * specific error code in *error
+	 */
+	*error = res;
+	return -1;
+}
+
+
+
+#include <malloc.h>
+#include <stdio.h>
+#include <pthread.h>
+
+unsigned char pbmatrix[3] = { 0, 1, 2 };
+unsigned char lcmatrix[4] = { 0, 1, 2, 3 };
+unsigned char lpmatrix[4] = { 0, 1, 2, 3 };
+
+struct MATRIXENTRY {
+	int pb;
+	int lc;
+	int lp;
+};
+
+struct MATRIXENTRY matrix[] = {
+	{2, 0, 0},
+	{2, 0, 1},
+	{2, 0, 2},
+	{2, 1, 0},
+	{2, 1, 2},
+	{2, 2, 0},
+	{2, 3, 0},
+	{0, 2, 0},
+	{0, 2, 1},
+	{0, 3, 0},
+	{0, 0, 0},
+	{0, 0, 2},
+	{1, 0, 1},
+	{1, 2, 0},
+	{1, 3, 0}
+};
+
+int pbsave = -1;
+int lcsave = -1;
+int lpsave = -1;
+int s_avail;
+
+int testlevel;
+//int testfb;
+static pthread_spinlock_t pos_mutex;
+static pthread_spinlock_t p_mutex;
+unsigned char *test1;
+unsigned char *testsource;
+
+
+typedef unsigned long uLongf;
+
+uLongf test2len;
+uLongf test1len;
+uLongf testsourcelen;
+int running = 0;
+void *brute(void *arg)
+{
+	int oldstate;
+	uLongf test3len = test2len;
+	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, &oldstate);
+	pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, &oldstate);
+	int *count = (int *)arg;
+	int testcount = count[0];
+	int takelcvalue = matrix[testcount].lc;
+	int takepbvalue = matrix[testcount].pb;
+	int takelpvalue = matrix[testcount].lp;
+	unsigned char *test2 = (unsigned char *) malloc(test2len * 2);
+//fprintf(stderr,"try method [pb:%d lc:%d lp:%d]\n",takepbvalue,takelcvalue,takelpvalue);
+
+	//static int lzma_compress_2(void *dest, void *src,  int size,int block_size, int *error,int lc, int lp, int pb)
+
+	int error;
+	test3len = lzma_compress_2(test2, testsource, testsourcelen, s_avail, &error, takelcvalue, takelpvalue, takepbvalue);
+//fprintf(stderr,"test return %d\n",test3len);
+	pthread_spin_lock(&pos_mutex);
+	if (test3len < test1len) {
+		test1len = test3len;
+		memcpy(test1, test2, test3len);
+		pbsave = takepbvalue;
+		lcsave = takelcvalue;
+		lpsave = takelpvalue;
+	}
+	pthread_spin_unlock(&pos_mutex);
+//fprintf(stderr,"finished %d running\n",running);
+	free(test2);
+	running = 0;
+	return NULL;
+}
+int count=0;
+
+//int (*compress)(void *, void *, void *, int, int, int *);
+///	int (*uncompress)(void *, void *, int, int, int *);
+//static int lzma_compress(char *dummy, void *dest, void *src,  int size,
+//	int block_size, int *error)
+static int lzma_compress(void *dummy, void *dst,
+			      void *src, int sourceLen,
+			      int block_size, int *error)
+{
+//printf("in %s %d\n",__func__,count);
+
+	pthread_spin_lock(&p_mutex);
+	count++;
+	unsigned char *source = (unsigned char*)src;
+	unsigned char *dest = (unsigned char*)dst;
+	int i, a;
+	pthread_t *thread;
+	cpu_set_t cpuset[4];
+	test1 = (unsigned char *) malloc(block_size);
+	test1len = block_size*2;
+	test2len = block_size;
+	testsource = source;
+	//testfb = fb;
+	testsourcelen = sourceLen;
+	//testlevel = level;
+	s_avail = block_size;
+	
+	pthread_spin_init(&pos_mutex, 0);
+	if ((thread = (pthread_t *) malloc((16) * sizeof(pthread_t))) == NULL)
+		fprintf(stderr,
+			"Out of memory allocating thread descriptors\n");
+//for (a=0;a<2;a++)
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_attr_setstacksize(&attr, 64 * 1024);
+
+	int threadcount = 0;
+#define MAX_THREADS 8
+	int *argument = (int *)malloc(16 * 4);
+	for (a = 0; a < 15 / MAX_THREADS; a++) {
+		for (i = 0; i < MAX_THREADS; i++) {
+			argument[i] = threadcount++;
+//              fprintf(stderr,"start thread %d\n",threadcount);
+			if (pthread_create
+			    (&thread[i], &attr, brute, &argument[i]) != 0)
+				fprintf(stderr, "Failed to create thread\n");
+		}
+		for (i = 0; i < MAX_THREADS; i++) {
+			pthread_join(thread[i], NULL);
+		}
+	}
+
+	for (i = 0; i < 15 % MAX_THREADS; i++) {
+		argument[i] = threadcount++;
+//              fprintf(stderr,"start thread %d\n",threadcount);
+		if (pthread_create(&thread[i], NULL, brute, &argument[i]) != 0)
+			fprintf(stderr, "Failed to create thread\n");
+	}
+	for (i = 0; i < 15 % MAX_THREADS; i++) {
+		pthread_join(thread[i], NULL);
+	}
+	free(argument);
+	pthread_attr_destroy(&attr);
+//	fprintf(stderr, "use method [pb:%d lc:%d lp:%d] (len %d/%d)\n",
+//		pbsave, lcsave, lpsave, test1len,block_size);
+	memcpy(dest, test1, test1len);
+	//dest[0] = pbsave;
+	//dest[1] = lcsave;
+	//dest[2] = lpsave;
+	//dest[3] = fb;
+	
+	//*destLen = test1len //+ 4;
+	free(thread);
+	pthread_spin_destroy(&pos_mutex);
+	free(test1);
+	count--;
+	pthread_spin_unlock(&p_mutex);
+//printf("out %s %d\n",__func__,count);
+	return test1len;
+}
+
+
+
+static int lzma_uncompress(void *dest, void *src, int size, int outsize,
+	int *error)
+{
+	lzma_stream strm = LZMA_STREAM_INIT;
+	int uncompressed_size = 0, res;
+	unsigned char lzma_header[LZMA_HEADER_SIZE];
+	fprintf(stderr, "uncompress\n");
+	res = lzma_alone_decoder(&strm, MEMLIMIT);
+	if(res != LZMA_OK) {
+		lzma_end(&strm);
+		goto failed;
+	}
+
+	memcpy(lzma_header, src, LZMA_HEADER_SIZE);
+	uncompressed_size = lzma_header[LZMA_PROPS_SIZE] |
+		(lzma_header[LZMA_PROPS_SIZE + 1] << 8) |
+		(lzma_header[LZMA_PROPS_SIZE + 2] << 16) |
+		(lzma_header[LZMA_PROPS_SIZE + 3] << 24);
+
+	if(uncompressed_size > outsize) {
+		res = 0;
+		goto failed;
+	}
+
+	memset(lzma_header + LZMA_PROPS_SIZE, 255, LZMA_UNCOMP_SIZE);
+
+	strm.next_out = dest;
+	strm.avail_out = outsize;
+	strm.next_in = lzma_header;
+	strm.avail_in = LZMA_HEADER_SIZE;
+
+	res = lzma_code(&strm, LZMA_RUN);
+
+	if(res != LZMA_OK || strm.avail_in != 0) {
+		lzma_end(&strm);
+		goto failed;
+	}
+
+	strm.next_in = src + LZMA_HEADER_SIZE;
+	strm.avail_in = size - LZMA_HEADER_SIZE;
+
+	res = lzma_code(&strm, LZMA_FINISH);
+	lzma_end(&strm);
+
+	if(res == LZMA_STREAM_END || (res == LZMA_OK &&
+		strm.total_out >= uncompressed_size && strm.avail_in == 0))
+		return uncompressed_size;
+
+failed:
+	*error = res;
+	return -1;
+}
+
+static int lzma_options(char *argv[], int argc)
+{
+	return lzma_xz_options(argv, argc, LZMA_OPT_LZMA);
+}
+
+
+static int lzma_options_post(int block_size)
+{
+	return lzma_xz_options_post(block_size, LZMA_OPT_LZMA);
+}
+
+
+static void *lzma_dump_options(int block_size, int *size)
+{
+	return lzma_xz_dump_options(block_size, size, 0);
+}
+
+
+static int lzma_extract_options(int block_size, void *buffer, int size)
+{
+	return lzma_xz_extract_options(block_size, buffer, size, LZMA_OPT_LZMA);
+}
+
+
+void lzma_usage()
+{
+	lzma_xz_usage(LZMA_OPT_LZMA);
+}
+
+
+int lzma_init(void **dummy, int a, int b)
+{
+	static int hasinit = 0;
+	if (hasinit)
+	    return 0;
+	hasinit=1;
+	
+	pthread_spin_init(&p_mutex, 0);
+	return 0;
+}
+
+int lzma_deinit(void)
+{
+	pthread_spin_destroy(&p_mutex);
+	return 0;
+}
+
+struct compressor lzma_comp_ops = {
+	.init = lzma_init,
+	.deinit = lzma_deinit,
+	.compress = lzma_compress,
+	.uncompress = lzma_uncompress,
+	.options = lzma_options,
+	.options_post = lzma_options_post,
+	.dump_options = lzma_dump_options,
+	.extract_options = lzma_extract_options,
+	.usage = lzma_usage,
+	.id = LZMA_COMPRESSION,
+	.name = "lzma",
+	.supported = 1
+};
+
---
+++ src/squashfs-tools-4.0-xp/xz_wrapper.c
@@ -0,0 +1,457 @@
+/*
+ * Copyright (c) 2010, 2011, 2012, 2013
+ * Phillip Lougher <phillip@squashfs.org.uk>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * xz_wrapper.c
+ *
+ * Support for XZ (LZMA2) compression using XZ Utils liblzma
+ * http://tukaani.org/xz/
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <lzma.h>
+
+#include "squashfs_fs.h"
+#include "xz_wrapper.h"
+#include "compressor.h"
+#include "lzma_xz_options.h"
+
+static struct bcj bcj[] = {
+	{"x86", LZMA_FILTER_X86, 0},
+	{"powerpc", LZMA_FILTER_POWERPC, 0},
+	{"ia64", LZMA_FILTER_IA64, 0},
+	{"arm", LZMA_FILTER_ARM, 0},
+	{"armthumb", LZMA_FILTER_ARMTHUMB, 0},
+	{"sparc", LZMA_FILTER_SPARC, 0},
+	{NULL, LZMA_VLI_UNKNOWN, 0}
+};
+
+static int filter_count = 1;
+
+static int xz_options(char *argv[], int argc)
+{
+	if (strcmp(argv[0], "-Xbcj") == 0) {
+		int i;
+		char *name;
+
+		if (argc < 2) {
+			fprintf(stderr, "xz: -Xbcj missing filter\n");
+			return -2;
+		}
+
+		name = argv[1];
+		while (name[0] != '\0') {
+			for (i = 0; bcj[i].name; i++) {
+				int n = strlen(bcj[i].name);
+				if ((strncmp(name, bcj[i].name, n) == 0) && (name[n] == '\0' || name[n] == ',')) {
+					if (bcj[i].selected == 0) {
+						bcj[i].selected = 1;
+						filter_count++;
+					}
+					name += name[n] == ',' ? n + 1 : n;
+					break;
+				}
+			}
+			if (bcj[i].name == NULL) {
+				fprintf(stderr, "xz: -Xbcj unrecognised " "filter\n");
+				return -2;
+			}
+		}
+		return 1;
+	} else {
+		return lzma_xz_options(argv, argc, LZMA_OPT_XZ);
+	}
+}
+
+/*
+ * This function is called after all options have been parsed.
+ * It is used to do post-processing on the compressor options using
+ * values that were not expected to be known at option parse time.
+ *
+ * In this case block_size may not be known until after -Xdict-size has
+ * been processed (in the case where -b is specified after -Xdict-size)
+ *
+ * This function returns 0 on successful post processing, or
+ *			-1 on error
+ */
+static int xz_options_post(int block_size)
+{
+	return lzma_xz_options_post(block_size, LZMA_OPT_XZ);
+}
+
+static void *xz_dump_options(int block_size, int *size)
+{
+	int i, flags = 0;
+
+	for (i = 0; bcj[i].name; i++)
+		flags |= bcj[i].selected << i;
+
+	return lzma_xz_dump_options(block_size, size, flags);
+}
+
+/*
+ * This function is a helper specifically for the append mode of
+ * mksquashfs.  Its purpose is to set the internal compressor state
+ * to the stored compressor options in the passed compressor options
+ * structure.
+ *
+ * In effect this function sets up the compressor options
+ * to the same state they were when the filesystem was originally
+ * generated, this is to ensure on appending, the compressor uses
+ * the same compression options that were used to generate the
+ * original filesystem.
+ *
+ * Note, even if there are no compressor options, this function is still
+ * called with an empty compressor structure (size == 0), to explicitly
+ * set the default options, this is to ensure any user supplied
+ * -X options on the appending mksquashfs command line are over-ridden
+ *
+ * This function returns 0 on sucessful extraction of options, and
+ *			-1 on error
+ */
+static int xz_extract_options(int block_size, void *buffer, int size)
+{
+	int ret = lzma_xz_extract_options(block_size, buffer, size, LZMA_OPT_XZ);
+
+	if (!ret) {
+		int i;
+		struct lzma_xz_options *opts = lzma_xz_get_options();
+		for (i = 0; bcj[i].name; i++) {
+			if ((opts->flags >> i) & 1) {
+				bcj[i].selected = 1;
+				filter_count++;
+			} else
+				bcj[i].selected = 0;
+		}
+	}
+	return ret;
+}
+
+#include <malloc.h>
+#include <stdio.h>
+#if 0
+void xz_display_options(void *buffer, int size)
+{
+	struct comp_opts *comp_opts = buffer;
+	int dictionary_size, flags, printed;
+	int i, n;
+
+	/* check passed comp opts struct is of the correct length */
+	if (size != sizeof(struct comp_opts))
+		goto failed;
+
+	SQUASHFS_INSWAP_COMP_OPTS(comp_opts);
+
+	dictionary_size = comp_opts->dictionary_size;
+	flags = comp_opts->flags;
+
+	/*
+	 * check that the dictionary size seems correct - the dictionary
+	 * size should 2^n or 2^n+2^(n+1)
+	 */
+	n = ffs(dictionary_size) - 1;
+	if (dictionary_size != (1 << n) && dictionary_size != ((1 << n) + (1 << (n + 1))))
+		goto failed;
+
+	printf("\tDictionary size %d\n", dictionary_size);
+
+	printed = 0;
+	for (i = 0; bcj[i].name; i++) {
+		if ((flags >> i) & 1) {
+			if (printed)
+				printf(", ");
+			else
+				printf("\tFilters selected: ");
+			printf("%s", bcj[i].name);
+			printed = 1;
+		}
+	}
+
+	if (!printed)
+		printf("\tNo filters specified\n");
+	else
+		printf("\n");
+
+	return;
+
+failed:
+	fprintf(stderr, "xz: error reading stored compressor options from " "filesystem!\n");
+}
+#endif
+
+struct MATRIXENTRY {
+	int pb;
+	int lc;
+	int lp;
+	int used;
+};
+
+
+
+static struct MATRIXENTRY matrix[] = {
+{0,0,0},
+{0,0,1},
+{0,0,2},
+{0,1,0},
+{0,1,1},
+{0,2,0},
+{0,2,1},
+{0,3,0},
+{0,3,1},
+{1,0,0},
+{1,0,1},
+{1,0,2},
+{1,0,3},
+{1,1,0},
+{1,1,1},
+{1,1,2},
+{1,2,1},
+{1,3,0},
+{1,3,1},
+{2,0,0},
+{2,0,1},
+{2,0,2},
+{2,0,3},
+{2,1,2},
+{2,2,2},
+{3,0,1},
+{3,0,2},
+};
+
+//static struct MATRIXENTRY matrix[4*4*4];
+
+
+/*
+ * This function is called by mksquashfs to initialise the
+ * compressor, before compress() is called.
+ *
+ * This function returns 0 on success, and
+ *			-1 on error
+ */
+static int xz_init(void **strm, int block_size, int datablock)
+{
+
+	int i, j, filters = datablock ? filter_count : 1;
+	struct filter *filter = malloc(filters * sizeof(struct filter));
+	struct xz_stream *stream;
+	struct lzma_xz_options *opts = lzma_xz_get_options();
+/*	int a,b,c,cnt=0;
+	for (a=0;a<4;a++)
+	for (b=0;b<4;b++)
+	for (c=0;c<4;c++)  {
+	    matrix[cnt].pb = a;
+	    matrix[cnt].lc = b;
+	    matrix[cnt++].lp = c;
+	}*/
+
+	if (filter == NULL)
+		goto failed;
+
+	stream = *strm = malloc(sizeof(struct xz_stream));
+	if (stream == NULL)
+		goto failed2;
+
+	stream->filter = filter;
+	stream->filters = filters;
+
+	memset(filter, 0, filters * sizeof(struct filter));
+
+	stream->dictionary_size = datablock ? opts->dict_size : SQUASHFS_METADATA_SIZE;
+
+	filter[0].filter[0].id = LZMA_FILTER_LZMA2;
+	filter[0].filter[0].options = &stream->opt;
+	filter[0].filter[1].id = LZMA_VLI_UNKNOWN;
+
+	for (i = 0, j = 1; datablock && bcj[i].name; i++) {
+		if (bcj[i].selected) {
+			filter[j].buffer = malloc(block_size*4);
+			if (filter[j].buffer == NULL)
+				goto failed3;
+			filter[j].filter[0].id = bcj[i].id;
+			filter[j].filter[1].id = LZMA_FILTER_LZMA2;
+			filter[j].filter[1].options = &stream->opt;
+			filter[j].filter[2].id = LZMA_VLI_UNKNOWN;
+			j++;
+		}
+	}
+
+	return 0;
+
+failed3:
+	for (i = 1; i < filters; i++)
+		free(filter[i].buffer);
+	free(stream);
+
+failed2:
+	free(filter);
+
+failed:
+	return -1;
+}
+
+static int xz_compress2(void *strm, unsigned char *dest, void *src, int size, int block_size, int *error, int lc, int lp, int pb)
+{
+	int i;
+	lzma_ret res = 0;
+	struct xz_stream *stream = strm;
+	struct filter *selected = NULL;
+	struct lzma_xz_options *opts = lzma_xz_get_options();
+
+	stream->filter[0].buffer = dest;
+
+	for (i = 0; i < stream->filters; i++) {
+		uint32_t preset = opts->preset;
+		struct filter *filter = &stream->filter[i];
+		filter->length = 0;
+		preset |= LZMA_PRESET_EXTREME;
+
+		if (lzma_lzma_preset(&stream->opt, preset))
+			goto failed;
+
+		stream->opt.lc = lc;
+		stream->opt.lp = lp;
+		stream->opt.pb = pb;
+
+		if (opts->fb)
+			stream->opt.nice_len = opts->fb;
+
+		stream->opt.dict_size = stream->dictionary_size;
+
+		filter->length = 0;
+		res = lzma_stream_buffer_encode(filter->filter, LZMA_CHECK_CRC32, NULL, src, size, filter->buffer, &filter->length, block_size);
+		if (res == LZMA_OK) {
+			if (!selected || selected->length > filter->length)
+				selected = filter;
+		} else if (res != LZMA_BUF_ERROR)
+			goto failed;
+	}
+
+	if (!selected) {
+		/*
+		 * Output buffer overflow.  Return out of buffer space
+		 */
+		return 0;
+	}
+
+	if (selected->buffer != dest) {
+		memcpy(dest, selected->buffer, selected->length);
+		/*      dest[selected->length++] = lc;
+		   dest[selected->length++] = lp;
+		   dest[selected->length++] = pb;
+		   dest[selected->length++] = opts->fb; */
+	}else {
+	}
+	*error = 0;
+	return (int)selected->length;
+
+failed:
+	/*
+	 * All other errors return failure, with the compressor
+	 * specific error code in *error
+	 */
+	*error = res;
+	return -1;
+}
+
+
+typedef unsigned long uLongf;
+
+static int xz_compress(void *s_strm, void *dst, void *src, int sourceLen, int block_size, int *error)
+{
+	int test1len,test3len;
+	int s_fail;
+	int i, a;
+	test1len = block_size * 2;
+	int oldstate;
+	int testcount;
+	unsigned char *test2 = (unsigned char *)malloc(block_size * 4);
+	s_fail = 1;
+	for (testcount = 0; testcount < sizeof(matrix) / sizeof(struct MATRIXENTRY); testcount++) {
+		int takelcvalue = matrix[testcount].lc;
+		int takepbvalue = matrix[testcount].pb;
+		int takelpvalue = matrix[testcount].lp;
+		int error=0;
+		test3len = xz_compress2(s_strm, test2, src, sourceLen, block_size, &error, takelcvalue, takelpvalue, takepbvalue);
+		if (!error && test3len > 0 && test3len < test1len) {
+			test1len = test3len;
+			memcpy(dst, test2, test3len);
+			s_fail = 0;
+			matrix[testcount].used = 1;
+		}
+	}
+	free(test2);
+	if (s_fail)
+		test1len = 0;
+	return test1len;
+}
+
+static int xz_uncompress(void *dest, void *src, int size, int block_size, int *error)
+{
+	size_t src_pos = 0;
+	size_t dest_pos = 0;
+	uint64_t memlimit = MEMLIMIT;
+	lzma_ret res = lzma_stream_buffer_decode(&memlimit, 0, NULL,
+						 src, &src_pos, size, dest, &dest_pos, block_size);
+	if (res == LZMA_OK && size == (int)src_pos)
+		return (int)dest_pos;
+	else {
+		*error = res;
+		return -1;
+	}
+	return res == LZMA_OK && size == (int)src_pos ? (int)dest_pos : -1;
+}
+
+void xz_usage()
+{
+	lzma_xz_usage(LZMA_OPT_XZ);
+	fprintf(stderr, "\t  -Xbcj filter1,filter2,...,filterN\n");
+	fprintf(stderr, "\t\tCompress using filter1,filter2,...,filterN in");
+	fprintf(stderr, " turn\n\t\t(in addition to no filter), and choose");
+	fprintf(stderr, " the best compression.\n");
+	fprintf(stderr, "\t\tAvailable filters: x86, arm, armthumb,");
+	fprintf(stderr, " powerpc, sparc, ia64\n");
+}
+
+int xz_deinit(void)
+{
+	int testcount;
+	for (testcount = 0; testcount < sizeof(matrix) / sizeof(struct MATRIXENTRY); testcount++) {
+		if (matrix[testcount].used)
+			printf("{%d,%d,%d},\n",matrix[testcount].pb,matrix[testcount].lc,matrix[testcount].lp);
+	}
+	
+	
+	return 0;
+}
+
+struct compressor xz_comp_ops = {
+	.init = xz_init,
+	.deinit = xz_deinit,
+	.compress = xz_compress,
+	.uncompress = xz_uncompress,
+	.options = xz_options,
+	.options_post = xz_options_post,
+	.dump_options = xz_dump_options,
+	.extract_options = xz_extract_options,
+//	.display_options = xz_display_options,
+	.usage = xz_usage,
+	.id = XZ_COMPRESSION,
+	.name = "xz",
+	.supported = 1
+};
