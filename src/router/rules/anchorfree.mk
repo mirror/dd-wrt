e8176b6b15074c507b01db7958b5cc679cff8904
BrainSlayer <s.gottschall@dd-wrt.com>
2017-05-05 11:03:58 +0000

    add hydra
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31949 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rules/anchorfree.mk
@@ -0,0 +1,68 @@
+libevent-af:
+	CC="ccache $(ARCH)-linux-uclibc-gcc" \
+	CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/openssl/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/openssl/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	LDFLAGS="$(COPTS) $(MIPS16_OPT) -L$(TOP)/openssl" \
+	$(MAKE) -C libevent2-anchorfree
+
+libevent-af-install:
+	@true
+
+
+libevent-af-clean:
+	$(MAKE) -C libevent2-anchorfree clean
+
+
+libevent-af-configure:
+	cd libevent2-anchorfree && ./autogen.sh
+	cd libevent2-anchorfree && ./configure  --enable-static --disable-debug-mode --prefix=/usr ac_cv_host=$(ARCH)-uclibc-linux --libdir=/usr/lib --target=$(ARCH)-linux --host=$(ARCH) CC="ccache $(ARCH)-linux-uclibc-gcc" \
+	CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/openssl/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	LDFLAGS="$(COPTS) $(MIPS16_OPT) -L$(TOP)/openssl" 
+	CC="ccache $(ARCH)-linux-uclibc-gcc" \
+	CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/openssl/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/openssl/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	LDFLAGS="$(COPTS) $(MIPS16_OPT) -L$(TOP)/openssl" \
+	$(MAKE) -C libevent2-anchorfree
+
+
+
+
+HYDRA_OPTS:= \
+	CC="ccache $(ARCH)-linux-uclibc-gcc" \
+	LD="$(ARCH)-linux-uclibc-ld" \
+	EXTRA_CFLAGS+="-ffunction-sections -fdata-sections -DOPENWRT -DDISABLE_VIPER_BSS_RESET $(COPTS) -DNEED_PRINTF" \
+	EXTRA_CFLAGS+="-I$(TOP)/jansson/src" \
+	EXTRA_CFLAGS+="-I$(TOP)/libevent2-anchorfree/include" \
+	EXTRA_CFLAGS+="-I$(TOP)/openssl/include" \
+	EXTRA_CFLAGS+="-I$(TOP)/libevent2-anchorfree/compat" \
+	EXTRA_CFLAGS+="-I$(TOP)/zlib/include" \
+	EXTRA_LDFLAGS+="$(TOP)/libevent2-anchorfree/.libs/libevent.a" \
+	EXTRA_LDFLAGS+="$(TOP)/libevent2-anchorfree/.libs/libevent_pthreads.a" \
+	EXTRA_LDFLAGS+="$(TOP)/jansson/src/.libs/libjansson.a" \
+	EXTRA_LDFLAGS+="-L$(TOP)/zlib -lz" \
+	EXTRA_LDFLAGS+="-L$(TOP)/openssl -lssl -lcrypto" \
+	EXTRA_LDFLAGS+="-Wl,--gc-sections" \
+	OPENWRT=yes \
+	PLATFORM=openwrt 
+
+
+hydra: 
+	$(MAKE) -C hydra/tranceport $(HYDRA_OPTS)
+	$(MAKE) -C hydra/viper $(HYDRA_OPTS)
+	$(MAKE) -C hydra/vpr_stats $(HYDRA_OPTS)
+	$(MAKE) -C hydra/proxy_plugin $(HYDRA_OPTS)
+	$(MAKE) -C hydra/ecc-ser $(HYDRA_OPTS)
+	$(MAKE) -C hydra/afvpn $(HYDRA_OPTS)
+
+	
+hydra-clean:
+	$(MAKE) -C hydra/tranceport clean $(HYDRA_OPTS)
+	$(MAKE) -C hydra/viper clean $(HYDRA_OPTS)
+	$(MAKE) -C hydra/vpr_stats clean $(HYDRA_OPTS)
+	$(MAKE) -C hydra/proxy_plugin clean $(HYDRA_OPTS)
+	$(MAKE) -C hydra/ecc-ser clean $(HYDRA_OPTS)
+	$(MAKE) -C hydra/afvpn clean $(HYDRA_OPTS)
+
+hydra-install:
+	mkdir -p $(INSTALLDIR)/hydra/usr/sbin
+	cp hydra/afvpn/client.openwrt $(INSTALLDIR)/hydra/usr/sbin
