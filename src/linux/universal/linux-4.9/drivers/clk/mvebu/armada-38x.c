5499d76c4a1debd2418ff0aeac12b6e2e722e062
BrainSlayer <s.gottschall@dd-wrt.com>
2017-05-07 19:25:25 +0000

    fix wrong clocks
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@31975 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.9/drivers/clk/mvebu/armada-38x.c
+++ src/linux/universal/linux-4.9/drivers/clk/mvebu/armada-38x.c
@@ -46,10 +46,11 @@ static u32 __init armada_38x_get_tclk_freq(void __iomem *sar)
 }
 
 static const u32 armada_38x_cpu_frequencies[] __initconst = {
-	0, 0, 0, 0,
-	1066 * 1000 * 1000, 0, 0, 0,
+	666 * 1000 * 1000, 0, 800 * 1000 * 1000, 0,
+	1066 * 1000 * 1000, 0, 1200 * 1000 * 1000, 0,
 	1332 * 1000 * 1000, 0, 0, 0,
-	1600 * 1000 * 1000,
+	1600 * 1000 * 1000, 0, 0, 0,
+	1866 * 1000 * 1000, 0, 0, 2000 * 1000 * 1000,
 };
 
 static u32 __init armada_38x_get_cpu_freq(void __iomem *sar)
