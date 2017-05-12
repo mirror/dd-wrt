91d6a393372e792e76a3532ba6f2a758a881e878
BrainSlayer <s.gottschall@dd-wrt.com>
2017-05-12 07:47:36 +0000

    its not required to pass the string through all layers. we can just parse it one time and use a definition for later checks
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32016 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: b4683ad512188e160157837957f70f4181a57541

--- src/router/httpd/modules/upgrade_rb500.c
+++ src/router/httpd/modules/upgrade_rb500.c
@@ -32,16 +32,16 @@ static int upgrade_ret;
 
 void
 // do_upgrade_cgi(char *url, FILE *stream)
-do_upgrade_cgi(char *method, struct mime_handler *handler, char *url, webs_t streamm, char *query)	// jimmy,
+do_upgrade_cgi(unsigned char method, struct mime_handler *handler, char *url, webs_t streamm, char *query)	// jimmy,
 								// https,
 								// 8/6/2003
 {
 #ifndef ANTI_FLASH
 	fprintf(stderr, "do post\n");
 	if (upgrade_ret)
-		do_ej(NULL, handler, "Fail_u_s.asp", stream, NULL);
+		do_ej(METHOD_GET, handler, "Fail_u_s.asp", stream, NULL);
 	else
-		do_ej(NULL, handler, "Success_u_s.asp", stream, NULL);
+		do_ej(METHOD_GET, handler, "Success_u_s.asp", stream, NULL);
 	fprintf(stderr, "websdone\n");
 
 	websDone(stream, 200);
@@ -55,7 +55,7 @@ do_upgrade_cgi(char *method, struct mime_handler *handler, char *url, webs_t str
 		sys_reboot();
 	}
 #else
-	do_ej(NULL, handler, "Fail_u_s.asp", stream, NULL);
+	do_ej(METHOD_GET, handler, "Fail_u_s.asp", stream, NULL);
 	websDone(stream, 200);
 #endif
 }
