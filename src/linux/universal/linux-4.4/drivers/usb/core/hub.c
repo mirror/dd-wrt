964ac38d3e5be053742b37db230aad31859c345a
BrainSlayer <s.gottschall@dd-wrt.com>
2017-05-19 07:24:41 +0000

    update kernels
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32101 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 36c168d3589ba7427cf5ad6ee1b5ee3bb1ddb489

--- src/linux/universal/linux-4.4/drivers/usb/core/hub.c
+++ src/linux/universal/linux-4.4/drivers/usb/core/hub.c
@@ -1051,6 +1051,9 @@ static void hub_activate(struct usb_hub *hub, enum hub_activation_type type)
 
 		portstatus = portchange = 0;
 		status = hub_port_status(hub, port1, &portstatus, &portchange);
+		if (status)
+			goto abort;
+
 		if (udev || (portstatus & USB_PORT_STAT_CONNECTION))
 			dev_dbg(&port_dev->dev, "status %04x change %04x\n",
 					portstatus, portchange);
@@ -1183,7 +1186,7 @@ static void hub_activate(struct usb_hub *hub, enum hub_activation_type type)
 
 	/* Scan all ports that need attention */
 	kick_hub_wq(hub);
-
+ abort:
 	if (type == HUB_INIT2 || type == HUB_INIT3) {
 		/* Allow autosuspend if it was suppressed */
  disconnected:
@@ -2080,6 +2083,12 @@ void usb_disconnect(struct usb_device **pdev)
 	if(udev->parent == udev->bus->root_hub)
 		ap_usb_led_off();
 #endif
+
+	/*
+	 * Ensure that the pm runtime code knows that the USB device
+	 * is in the process of being disconnected.
+	 */
+	pm_runtime_barrier(&udev->dev);
 
 	usb_lock_device(udev);
 
