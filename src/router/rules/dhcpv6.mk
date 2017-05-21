c56c1966ed27141fbd606cc25c4f084e3aaf62aa
BrainSlayer <s.gottschall@dd-wrt.com>
2017-05-21 12:03:09 +0000

    lib location fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32112 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 8c6575191047884e83eac2d836f055122588b084

--- src/router/rules/dhcpv6.mk
+++ src/router/rules/dhcpv6.mk
@@ -4,7 +4,7 @@ dhcpv6:
 	CFLAGS="$(COPTS)  $(MIPS16_OPT) $(JFLAGS) -DNEED_PRINTF -D_GNU_SOURCE -I$(TOP)/shared -DUSE_DHCP6SRV -DNOCONFIG_DEBUG -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	CPPFLAGS="$(COPTS) $(MIPS16_OPT) $(JFLAGS) -DNEED_PRINTF -D_GNU_SOURCE -I$(TOP)/shared  -DUSE_DHCP6SRV -DNOCONFIG_DEBUG -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	CXXFLAGS="$(COPTS) $(MIPS16_OPT) $(JFLAGS) -DNEED_PRINTF -D_GNU_SOURCE -I$(TOP)/shared  -DUSE_DHCP6SRV -DNOCONFIG_DEBUG -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	LDFLAGS="$(COPTS) $(MIPS16_OPT) $(JFLAGS) -ffunction-sections -fdata-sections -Wl,--gc-sections  $(TOP)/libutils/getifaddrs.o -L$(TOP)/libutils/ -lutils -L$(TOP)/nvram -lnvram" \
+	LDFLAGS="$(COPTS) $(MIPS16_OPT) $(JFLAGS) -ffunction-sections -fdata-sections -Wl,--gc-sections  $(TOP)/libutils/getifaddrs.o -L$(TOP)/libutils/ -lutils -L$(TOP)/nvram -lnvram -Wl,-rpath,$(TOP)/jansson/src/.libs" \
 	$(MAKE) -C dhcpv6 all
 	
 dhcpv6-install:
@@ -24,4 +24,4 @@ dhcpv6-configure:
 			CC="$(CC)" \
 			CXXFLAGS="$(COPTS) $(MIPS16_OPT) $(JFLAGS) -I$(TOP)/shared -DNEED_PRINTF -D_GNU_SOURCE -DUSE_DHCP6SRV -DNOCONFIG_DEBUG -ffunction-sections -fdata-sections -Wl,--gc-sections"  \
 			CFLAGS="$(COPTS) $(MIPS16_OPT) $(JFLAGS) -I$(TOP)/shared -DNEED_PRINTF -D_GNU_SOURCE  -DUSE_DHCP6SRV -DNOCONFIG_DEBUG -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-			LDFLAGS="$(COPTS) $(MIPS16_OPT) $(JFLAGS) -I$(TOP)/shared -ffunction-sections -fdata-sections -Wl,--gc-sections $(TOP)/libutils/getifaddrs.o -L$(TOP)/libutils/ -lutils -L$(TOP)/nvram -lnvram"
+			LDFLAGS="$(COPTS) $(MIPS16_OPT) $(JFLAGS) -I$(TOP)/shared -ffunction-sections -fdata-sections -Wl,--gc-sections $(TOP)/libutils/getifaddrs.o -L$(TOP)/libutils/ -lutils -L$(TOP)/nvram -lnvram -Wl,-rpath,$(TOP)/jansson/src/.libs"
