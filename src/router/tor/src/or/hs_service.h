93cd58a47cdcb2e739a194ad4e247833bbf84503
BrainSlayer <s.gottschall@dd-wrt.com>
2017-05-26 18:51:21 +0000

    update tor
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32141 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 7879aef43155844386efdc6735fa00a5e2225b35

---
+++ src/router/tor/src/or/hs_service.h
@@ -0,0 +1,27 @@
+/* Copyright (c) 2016, The Tor Project, Inc. */
+/* See LICENSE for licensing information */
+
+/**
+ * \file hs_service.h
+ * \brief Header file for hs_service.c.
+ **/
+
+#ifndef TOR_HS_SERVICE_H
+#define TOR_HS_SERVICE_H
+
+#include "or.h"
+#include "hs/cell_establish_intro.h"
+
+/* These functions are only used by unit tests and we need to expose them else
+ * hs_service.o ends up with no symbols in libor.a which makes clang throw a
+ * warning at compile time. See #21825. */
+
+hs_cell_establish_intro_t *
+generate_establish_intro_cell(const uint8_t *circuit_key_material,
+                              size_t circuit_key_material_len);
+ssize_t
+get_establish_intro_payload(uint8_t *buf, size_t buf_len,
+                            const hs_cell_establish_intro_t *cell);
+
+#endif /* TOR_HS_SERVICE_H */
+
