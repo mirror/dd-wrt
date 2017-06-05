71bed98565fb5f53afbc592769d0ce2366a8596a
BrainSlayer <s.gottschall@dd-wrt.com>
2017-06-05 17:23:46 +0000

    update kernel
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32235 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: c6f437a35ae7ae74c57fd44621b99688fba9a45a

--- src/linux/universal/linux-4.9/net/ipv6/tcp_ipv6.c
+++ src/linux/universal/linux-4.9/net/ipv6/tcp_ipv6.c
@@ -1050,6 +1050,7 @@ static struct sock *tcp_v6_syn_recv_sock(const struct sock *sk, struct sk_buff *
 		newtp->af_specific = &tcp_sock_ipv6_mapped_specific;
 #endif
 
+		newnp->ipv6_mc_list = NULL;
 		newnp->ipv6_ac_list = NULL;
 		newnp->ipv6_fl_list = NULL;
 		newnp->pktoptions  = NULL;
@@ -1119,6 +1120,7 @@ static struct sock *tcp_v6_syn_recv_sock(const struct sock *sk, struct sk_buff *
 	   First: no IPv4 options.
 	 */
 	newinet->inet_opt = NULL;
+	newnp->ipv6_mc_list = NULL;
 	newnp->ipv6_ac_list = NULL;
 	newnp->ipv6_fl_list = NULL;
 
