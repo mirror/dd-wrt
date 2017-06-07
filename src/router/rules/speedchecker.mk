902a5139efc474c4a31fe3f43ce2cc413d936539
BrainSlayer <s.gottschall@dd-wrt.com>
2017-06-07 17:05:40 +0000

    speedchecker changes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32259 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: c416573a76d5b50bcdc7befb92667c9210b793d5

--- src/router/rules/speedchecker.mk
+++ src/router/rules/speedchecker.mk
@@ -6,7 +6,7 @@ speedchecker-clean:
 
 speedchecker-install:
 	install -D speedchecker/configs/speedchecker.nvramconfig $(INSTALLDIR)/speedchecker/etc/config/speedchecker.nvramconfig
-	install -D speedchecker/configs/speedchecker.webservices $(INSTALLDIR)/speedchecker/etc/config/speedchecker.webservices
+	install -D speedchecker/configs/speedchecker.startup $(INSTALLDIR)/speedchecker/etc/config/speedchecker.startup
 	install -D speedchecker/scc $(INSTALLDIR)/speedchecker/usr/sbin/scc
 	install -D speedchecker/scripts/ping $(INSTALLDIR)/speedchecker/usr/bin/speedchecker/ping
 	install -D speedchecker/scripts/traceroute $(INSTALLDIR)/speedchecker/usr/bin/speedchecker/traceroute
