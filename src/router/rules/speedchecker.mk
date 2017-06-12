01decaf70d43719e62fb6cdcd57ff24a9be67ffc
BrainSlayer <s.gottschall@dd-wrt.com>
2017-06-12 06:58:26 +0000

    speedchecker changes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32313 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 5bf0aed2ff7b187921da7886e6d559af9a61cbaf

--- src/router/rules/speedchecker.mk
+++ src/router/rules/speedchecker.mk
@@ -10,3 +10,4 @@ speedchecker-install:
 	install -D speedchecker/scc $(INSTALLDIR)/speedchecker/usr/sbin/scc
 	install -D speedchecker/scripts/ping $(INSTALLDIR)/speedchecker/usr/bin/speedchecker/ping
 	install -D speedchecker/scripts/traceroute $(INSTALLDIR)/speedchecker/usr/bin/speedchecker/traceroute
+	svn log $(TOP)//private/speedchecker/scc.c | head -n2 | tail -n1 | cut -d \| -f 1 |cut -c2- | awk '{print "PSVNVERSION="$$1}' >$(INSTALLDIR)/speedchecker/usr/bin/speedchecker/sccversion
