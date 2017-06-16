3ea0a13e1b94248f85fa26a1d94bbd3b58b0159a
BrainSlayer <s.gottschall@dd-wrt.com>
2017-06-16 06:52:58 +0000

    init with default values
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32395 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 018c578b423998df6ec91394a7c52ca693513236

--- src/router/httpd/visuals/dd-wrt.c
+++ src/router/httpd/visuals/dd-wrt.c
@@ -4321,8 +4321,10 @@ void ej_gen_init_timer(webs_t wp, int argc, char_t ** argv)
 	int c = getdevicecount();
 	int i;
 	for (i = 0; i < c; i++) {
+		char radio[32];
+		sprintf(radio,"radio%d_on_time",i);
 		websWrite(wp, "setRadioTable(%d);\n", i);
-		websWrite(wp, "initWlTimer('%s',%d);\n", nvram_nget("radio%d_on_time", i), i);
+		websWrite(wp, "initWlTimer('%s',%d);\n", nvram_default_get(radio, "111111111111111111111111");
 		websWrite(wp, "show_layer_ext(document.wireless.radio%d_timer_enable, 'radio%d', %d);", i, i, nvram_nmatch("1", "radio%d_timer_enable", i) ? 1 : 0);
 	}
 #endif
