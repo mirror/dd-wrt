532ca22d70db86926324ca5c080bb241db90159c
BrainSlayer <s.gottschall@dd-wrt.com>
2017-06-28 03:57:13 +0000

    some new properties
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32478 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 2d650c87687e9821aa108585638aac60c5e08000

--- src/router/services/networking/rt2880.c
+++ src/router/services/networking/rt2880.c
@@ -1150,6 +1150,9 @@ void configure_wifi_single(int idx)	// madwifi implementation for atheros based
 		fprintf(fp, "HT_LDPC=1\n");
 		fprintf(fp, "VHT_STBC=1\n");
 		fprintf(fp, "VHT_LDPC=1\n");
+		fprintf(fp, "VHT_SGI=%d\n", nvram_nmatch("1", "wl%d_shortgi", idx) ? 1 : 0);
+		fprintf(fp, "G_BAND_256QAM=%d\n", ? nvram_nmatch("1", "wl%d_turbo_qam", idx) ? 1 : 0);
+
 	}
 	fclose(fp);
 
