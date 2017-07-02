4328132112b4c27c3cf209403e40af4d9cd6c189
BrainSlayer <s.gottschall@dd-wrt.com>
2017-07-02 11:01:26 +0000

    merge
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32551 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 99e12edb037d2ccc1ad475add49ccfa01dc0ba0b

--- src/linux/universal/linux-4.9/arch/mips/include/asm/string.h
+++ src/linux/universal/linux-4.9/arch/mips/include/asm/string.h
@@ -131,6 +131,8 @@ strncmp(__const__ char *__cs, __const__ char *__ct, size_t __count)
 }
 #endif /* CONFIG_32BIT */
 
+#define __HAVE_ARCH_MEMSET
+extern void *memset(void *__s, int __c, size_t __count);
 
 #define __HAVE_ARCH_MEMCPY
 extern void *memcpy(void *__to, __const__ void *__from, size_t __n);
@@ -139,7 +141,6 @@ extern void *memcpy(void *__to, __const__ void *__from, size_t __n);
 extern void *memmove(void *__dest, __const__ void *__src, size_t __n);
 
 #ifndef __ZBOOT__
-#define __HAVE_ARCH_MEMSET
 #define memset(__s, __c, len)					\
 ({								\
 	size_t __len = (len);					\
@@ -151,7 +152,6 @@ extern void *memmove(void *__dest, __const__ void *__src, size_t __n);
 	__ret;							\
 })
 
-#define __HAVE_ARCH_MEMCPY
 #define memcpy(dst, src, len)					\
 ({								\
 	size_t __len = (len);					\
@@ -163,7 +163,6 @@ extern void *memmove(void *__dest, __const__ void *__src, size_t __n);
 	__ret;							\
 })
 
-#define __HAVE_ARCH_MEMMOVE
 #define memmove(dst, src, len)					\
 ({								\
 	size_t __len = (len);					\
@@ -178,4 +177,5 @@ extern void *memmove(void *__dest, __const__ void *__src, size_t __n);
 #define __HAVE_ARCH_MEMCMP
 #define memcmp(src1, src2, len) __builtin_memcmp((src1), (src2), (len))
 #endif
+
 #endif /* _ASM_STRING_H */
