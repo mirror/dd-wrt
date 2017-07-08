3c4b5c43257c5fecbf4d4f113d2c6c314bdabb9e
BrainSlayer <s.gottschall@dd-wrt.com>
2017-07-08 11:48:48 +0000

    remove obsolete files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32605 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 876d550ccd20bfd9fed51c7a62ae3bfc91c022a1

--- src/router/rules/speedchecker.mk
+++ src/router/rules/speedchecker.mk
@@ -7,10 +7,7 @@ speedchecker-clean:
 
 speedchecker-install:
 	install -D speedchecker/configs/speedchecker.nvramconfig $(INSTALLDIR)/speedchecker/etc/config/speedchecker.nvramconfig
-	install -D speedchecker/configs/speedchecker.startup $(INSTALLDIR)/speedchecker/etc/config/speedchecker.startup
 	install -D speedchecker/scc $(INSTALLDIR)/speedchecker/usr/sbin/scc
 	install -D speedchecker/scripts/ping $(INSTALLDIR)/speedchecker/usr/bin/speedchecker/ping
 	install -D speedchecker/scripts/traceroute $(INSTALLDIR)/speedchecker/usr/bin/speedchecker/traceroute
-	svn log $(TOP)/private/speedchecker/scc.c | head -n2 | tail -n1 | cut -d \| -f 1 |cut -c2- | awk '{print "PSVNVERSION="$$1}' >$(INSTALLDIR)/speedchecker/usr/bin/speedchecker/sccversion
-	cat $(TOP)/private/speedchecker/speedchecker/VERSION >>$(INSTALLDIR)/speedchecker/usr/bin/speedchecker/sccversion
 	install -D speedchecker/configs/01-sc.shownf $(INSTALLDIR)/speedchecker/etc/config/01-sc.shownf
