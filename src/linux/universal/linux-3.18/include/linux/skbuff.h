44ac78779259da21ada1be2f50feaeda9518a3a2
BrainSlayer <s.gottschall@dd-wrt.com>
2017-07-09 10:03:10 +0000

    disable gro for foreighn mac addresses
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32622 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 513b3af52d078fa24a8d0b616adf37b03f39e4ba

--- src/linux/universal/linux-3.18/include/linux/skbuff.h
+++ src/linux/universal/linux-3.18/include/linux/skbuff.h
@@ -608,7 +608,8 @@ struct sk_buff {
 #endif
 	__u8			ipvs_property:1;
 	__u8			inner_protocol_type:1;
-	/* 4 or 6 bit hole */
+	__u8			gro_skip:1;
+	/* 3 or 5 bit hole */
 
 #ifdef CONFIG_NET_SCHED
 	__u16			tc_index;	/* traffic control index */
