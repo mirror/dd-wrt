8f8ddd59108a10da8d370583d0f114738eaab711
kong <USER@DOMAIN>
2017-07-17 20:41:02 +0000

    changed busybox paths
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32734 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 91621cf580526fe51efeb0a5c27bd9488a1661a2

--- src/router/rules/openvpn.mk
+++ src/router/rules/openvpn.mk
@@ -44,7 +44,10 @@ CONFIGURE_ARGS_OVPN += \
 	$(SSL_ADDOPT) \
 	CFLAGS="$(COPTS) -I$(SSLPATH)/include  -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	LDFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections -L$(SSL_LIB_PATH) -L$(TOP)/lzo -L$(TOP)/lzo/src/.libs -ldl" \
-	ac_cv_func_epoll_create=yes
+	ac_cv_func_epoll_create=yes \
+	ac_cv_path_IFCONFIG=/sbin/ifconfig \
+	ac_cv_path_ROUTE=/sbin/route \
+	ac_cv_path_IPROUTE=/usr/sbin/ip
 
 openvpn-conf-prep:
 	-rm -f openvpn/Makefile
