b1b17e7dff6ff826fffb04994a41260c2f68c3e7
BrainSlayer <s.gottschall@dd-wrt.com>
2017-07-18 16:18:52 +0000

    fix wrong offset
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32741 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/httpd.c
+++ src/router/httpd/httpd.c
@@ -496,7 +496,7 @@ static void do_file_2(struct mime_handler *handler, char *path, webs_t stream, c
 #endif
 	{
 		wfflush(stream);
-		wfsendfile(fileno(web), 0, len, stream);
+		wfsendfile(fileno(web), ftell(web), len, stream);
 	}
 	fclose(web);
 }
