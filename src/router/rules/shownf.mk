3e2ee3c0ebfaf535a5d7e60b64ca81dabc5ee498
BrainSlayer <s.gottschall@dd-wrt.com>
2017-07-25 21:33:41 +0000

    install only if dnscrypt is available
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32834 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: c3da95d5eae1bd999d221c4c42646f7ce34f01dd

--- src/router/rules/shownf.mk
+++ src/router/rules/shownf.mk
@@ -9,5 +9,6 @@ shownf-install:
 	install -D shownf/configs/shownf.webconfig $(INSTALLDIR)/shownf/etc/config/shownf.webconfig
 	install -D shownf/configs/00-shownf-head.shownf $(INSTALLDIR)/shownf/etc/config/00-shownf-head.shownf
 	install -D shownf/configs/99-shownf.shownf $(INSTALLDIR)/shownf/etc/config/99-shownf.shownf
-#
+ifeq ($(CONFIG_DNSCRYPT),y)
 	-install -D shownf/snippets/02-dnscrypt.shownf $(INSTALLDIR)/shownf/etc/config/02-dnscrypt.shownf
+endif
