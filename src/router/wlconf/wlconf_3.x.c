433197429ac78ee15c79a07dbc1af136b41a80b1
BrainSlayer <s.gottschall@dd-wrt.com>
2017-07-25 18:47:13 +0000

    in that way it doesnt matter in which order dhd and wl is loaded
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32827 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: d94d903d8a26a94a2f24ad8ea98005c9706c4a6b

--- src/router/wlconf/wlconf_3.x.c
+++ src/router/wlconf/wlconf_3.x.c
@@ -1716,8 +1716,12 @@ cprintf("get wl addr %s\n",ether_etoa((uchar *)vif_addr, eaddr));
 	/* Get instance */
 cprintf("get instance\n");
 	int offset = 0;
-	if (!strcmp(name, "eth2") && !dhd_probe("eth1") && dhd_probe("eth2"))
-	    offset = 1;
+	if (!strcmp(name, "eth2")) {
+		if (!dhd_probe("eth1") && dhd_probe("eth2") && !wl_probe("eth2"))
+			offset = 1;
+		else if (!dhd_probe("eth2") && dhd_probe("eth1") && !wl_probe("eth1"))
+			offset = 1;
+	}
 	WL_IOCTL(name, WLC_GET_INSTANCE, &unit, sizeof(unit));
 	
 	unit+=offset;
