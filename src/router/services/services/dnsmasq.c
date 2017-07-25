75bd1a0dfb12688f3097d9299c8cf46da40726e4
BrainSlayer <s.gottschall@dd-wrt.com>
2017-07-25 21:56:55 +0000

    for dnssec
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32837 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 174466f249e790c255d288121f4269239764e513

--- src/router/services/services/dnsmasq.c
+++ src/router/services/services/dnsmasq.c
@@ -387,6 +387,23 @@ void start_dnsmasq(void)
 			free(cp);
 		}
 	}
+	fprintf(fp, "bogus-priv\n");
+	fprintf(fp, "conf-file /etc/rfc6761.conf\n");
+#ifdef HAVE_DNSSEC
+	if (nvram_matchi("dnssec", 1)) {
+		fprintf(fp, "conf-file /etc/trust-anchors.conf\n");
+		fprintf(fp, "dnssec\n");
+		if (!nvram_matchi("ntp_enable", 1)) {
+			fprintf(fp, "dnssec-no-timecheck\n");
+		}
+		if (nvram_matchi("dnssec_cu", 1)) {
+			fprintf(fp, "dnssec-check-unsigned\n");
+		}
+	}
+#endif
+	if (nvram_matchi("proxy_dnssec", 1)) {
+		fprintf(fp, "proxy-dnssec\n");
+	}
 	/* stop dns rebinding for private addresses */
 	if (nvram_matchi("dnsmasq_no_dns_rebind", 1)) {
 		fprintf(fp, "stop-dns-rebind\n");
