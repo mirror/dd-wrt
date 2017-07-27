c7ef8bba2bb66d505cf540700c4841dbf1cd22f0
BrainSlayer <s.gottschall@dd-wrt.com>
2017-07-27 18:40:35 +0000

    for rpc
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32876 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 4cf8497b4f17a95d376c3e089543c918dba66c70

--- src/router/rules/lsof.mk
+++ src/router/rules/lsof.mk
@@ -3,8 +3,8 @@ lsof-configure:
 		LINUX_CLIB="-DGLIBCV=2" \
 		LSOF_CC="$(CC)" \
 		LSOF_VSTR="$(KERNELRELEASE)" \
-		LSOF_CFGF="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF -DHASNORPC_H" \
-		LSOF_CFGL="-ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF" \
+		LSOF_CFGF="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF -D_GNU_SOURCE -I$(TOP)/lsof/librpc" \
+		LSOF_CFGL="-ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF -L$(TOP)/lsof/librpc -lrpc" \
 		LSOF_AR="$(CROSS_COMPILE)ar cr" \
 		LSOF_RANLIB="$(CROSS_COMPILE)ranlib" \
 		./Configure -n linux
@@ -12,7 +12,7 @@ lsof-configure:
 #	cd lsof && ./configure --prefix=/usr --libdir=/usr/lib --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF"
 
 lsof:
-	$(MAKE) -C lsof
+	$(MAKE) -C lsof all
 
 lsof-clean:
 	if test -e "lsof/Makefile"; then make -C lsof clean; fi
