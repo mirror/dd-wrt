88ed4f277b1945ab58d3ba5e4628855c3d17fd48
BrainSlayer <s.gottschall@dd-wrt.com>
2017-07-27 23:04:12 +0000

    update licenses
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32882 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/usb_hotplug_adv.c
+++ src/router/services/services/usb_hotplug_adv.c
@@ -1,10 +1,12 @@
-/* 
+/*
+ * usb_hotplug_adv.c
+ *
  * Copyright (C) 2013 Richard Schneidt
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
- * of the License.
+ * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,7 +15,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id:
  */
 
 #include <stdio.h>
