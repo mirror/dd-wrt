5384771b4f3e8dd9e17271a2fac4766d3a378a9d
BrainSlayer <s.gottschall@dd-wrt.com>
2017-07-30 20:09:40 +0000

    update aes libs
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32926 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: c25a77a275472b21475ac2e1fa8834a060f575c8

--- src/router/openssl/crypto/cryptlib.c
+++ src/router/openssl/crypto/cryptlib.c
@@ -1015,16 +1015,3 @@ void *OPENSSL_stderr(void)
 {
     return stderr;
 }
-
-int CRYPTO_memcmp(const volatile void *in_a, const volatile void *in_b, size_t len)
-{
-    size_t i;
-    const volatile unsigned char *a = in_a;
-    const volatile unsigned char *b = in_b;
-    unsigned char x = 0;
-
-    for (i = 0; i < len; i++)
-        x |= a[i] ^ b[i];
-
-    return x;
-}
