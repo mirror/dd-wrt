82d554d18f6c0010efc98aebb0de5f4d3eca8189
BrainSlayer <s.gottschall@dd-wrt.com>
2017-07-30 20:12:58 +0000

    update asm
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32928 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: daa0e6d190565689de70b0ea141850ba6df8e1f8

--- src/router/openssl/crypto/sha/asm/sha512p8-ppc.pl
+++ src/router/openssl/crypto/sha/asm/sha512p8-ppc.pl
@@ -1,4 +1,11 @@
-#!/usr/bin/env perl
+#! /usr/bin/env perl
+# Copyright 2014-2016 The OpenSSL Project Authors. All Rights Reserved.
+#
+# Licensed under the OpenSSL license (the "License").  You may not use
+# this file except in compliance with the License.  You can obtain a copy
+# in the file LICENSE in the source distribution or at
+# https://www.openssl.org/source/license.html
+
 
 # ====================================================================
 # Written by Andy Polyakov <appro@openssl.org> for the OpenSSL
@@ -23,6 +30,15 @@
 # for sha1-ppc.pl - 73%. 100% means that multi-process result equals
 # to single-process one, given that all threads end up on the same
 # physical core.
+#
+######################################################################
+# Believed-to-be-accurate results in cycles per processed byte [on
+# little-endian system]. Numbers in square brackets are for 64-bit
+# build of sha512-ppc.pl, presented for reference.
+#
+#		POWER8
+# SHA256	9.9 [15.8]
+# SHA512	6.3 [10.3]
 
 $flavour=shift;
 $output =shift;
