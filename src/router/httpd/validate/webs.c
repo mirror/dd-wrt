07dbff75517b99ced15706674d2f4b5174fe8c58
kong <USER@DOMAIN>
2017-08-02 18:53:03 +0000

    set defaults
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@32995 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: f0c42033f483158cc1ed07fd1ff87917634952ff

--- src/router/httpd/validate/webs.c
+++ src/router/httpd/validate/webs.c
@@ -1453,11 +1453,11 @@ void qos_save(webs_t wp)
 	nvram_seti("default_lanlevel", websGetVari(wp, "default_lanlevel", 0));
 	nvram_seti("wshaper_downlink", websGetVari(wp, "wshaper_downlink", 0));
 	nvram_seti("wshaper_uplink", websGetVari(wp, "wshaper_uplink", 0));
-	nvram_set("wshaper_dev", websGetVar(wp, "wshaper_dev"));
+	nvram_set("wshaper_dev", websGetVar(wp, "wshaper_dev", "WAN"));
 	nvram_seti("qos_type", websGetVari(wp, "qos_type", 0));
 
 #if defined(HAVE_CODEL) || defined(HAVE_FQ_CODEL) || defined(HAVE_PIE)
-	nvram_set("svqos_aqd", websGetVar(wp, "qos_aqd"));
+	nvram_set("svqos_aqd", websGetVar(wp, "qos_aqd", "sfq"));
 #endif
 
 	// nvram_commit ();
