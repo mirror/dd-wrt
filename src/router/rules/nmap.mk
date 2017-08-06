e3388c9f77dea0988de5f5cf4d4f782e58dda89e
BrainSlayer <s.gottschall@dd-wrt.com>
2017-08-06 21:25:10 +0000

    enable libssh2
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33035 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 92f9fe5740cc88cf4834e7288afd1c89c614ccc0

--- src/router/rules/nmap.mk
+++ src/router/rules/nmap.mk
@@ -9,13 +9,19 @@ nmap-configure:
 		--with-libpcre=included \
 		--with-libpcap="../libpcap" \
 		--with-liblua=included \
+		--with-libssh2=included \
 		--without-zenmap \
+		--without-ncat \
+		--without-nmap-update \
+		--without-ndiff \
+		--without-nping \
 		CPPFLAGS="-I$(TOP)/libpcap -I$(TOP)/openssl/include $(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 		CFLAGS="-I$(TOP)/libpcap -I$(TOP)/openssl/include $(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 		CXXFLAGS="-I$(TOP)/libpcap -I$(TOP)/openssl/include $(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 		LDFLAGS="-L$(TOP)/libpcap -L$(TOP)/openssl $(COPTS) -lssl -lcrypto" PCAP_ROOT="$(TOP)/libpcap  -ffunction-sections -fdata-sections -Wl,--gc-sections"
 
 nmap:
+	cd nmap && find . -name makefile.dep -delete	
 	make -C nmap $(NMAP_EXTRAFLAGS) clean
 	make -C nmap $(NMAP_EXTRAFLAGS)
 
