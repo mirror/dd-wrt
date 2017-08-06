77b05ac32869420f6c02e6c2bcd16f77fb1de167
BrainSlayer <s.gottschall@dd-wrt.com>
2017-08-06 14:11:47 +0000

    openssl 1.1 fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33023 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: e236682896663de676f86ec09d70b3708027f87f

--- src/router/aircrack-ng/src/crypto.c
+++ src/router/aircrack-ng/src/crypto.c
@@ -288,7 +288,7 @@ void calc_pmk( char *key, char *essid_pre, unsigned char pmk[40] )
 void calc_mic (struct AP_info *ap, unsigned char pmk[32], unsigned char ptk[80], unsigned char mic[20]) {
 	int i;
 	unsigned char pke[100];
-	HMAC_CTX ctx;
+	HMAC_CTX *ctx;
 
 	memcpy( pke, "Pairwise key expansion", 23 );
 
@@ -314,17 +314,17 @@ void calc_mic (struct AP_info *ap, unsigned char pmk[32], unsigned char ptk[80],
 		memcpy( pke + 67, ap->wpa.snonce, 32 );
 	}
 
-	HMAC_CTX_init(&ctx);
-	HMAC_Init_ex(&ctx, pmk, 32, EVP_sha1(), NULL);
+	ctx = HMAC_CTX_new();
+	HMAC_Init_ex(ctx, pmk, 32, EVP_sha1(), NULL);
 	for(i = 0; i < 4; i++ )
 	{
 		pke[99] = i;
 		//HMAC(EVP_sha1(), values[0], 32, pke, 100, ptk + i * 20, NULL);
-		HMAC_Init_ex(&ctx, 0, 0, 0, 0);
-		HMAC_Update(&ctx, pke, 100);
-		HMAC_Final(&ctx, ptk + i*20, NULL);
+		HMAC_Init_ex(ctx, 0, 0, 0, 0);
+		HMAC_Update(ctx, pke, 100);
+		HMAC_Final(ctx, ptk + i*20, NULL);
 	}
-	HMAC_CTX_cleanup(&ctx);
+	HMAC_CTX_free(ctx);
 
 	if( ap->wpa.keyver == 1 )
 	{
