e5dd93de32bbb3aee49e42fe9c4c63e35523d1c8
BrainSlayer <s.gottschall@dd-wrt.com>
2017-08-06 21:45:18 +0000

    update asterisk
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33037 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/asterisk/contrib/ast-db-manage/config/versions/f638dbe2eb23_symmetric_transport.py
@@ -0,0 +1,32 @@
+"""symmetric_transport
+
+Revision ID: f638dbe2eb23
+Revises: 15db7b91a97a
+Create Date: 2017-03-09 09:38:59.513479
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'f638dbe2eb23'
+down_revision = '15db7b91a97a'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import ENUM
+
+YESNO_NAME = 'yesno_values'
+YESNO_VALUES = ['yes', 'no']
+
+def upgrade():
+    ############################# Enums ##############################
+
+    # yesno_values have already been created, so use postgres enum object
+    # type to get around "already created" issue - works okay with mysql
+    yesno_values = ENUM(*YESNO_VALUES, name=YESNO_NAME, create_type=False)
+
+    op.add_column('ps_transports', sa.Column('symmetric_transport', yesno_values))
+    op.add_column('ps_subscription_persistence', sa.Column('contact_uri', sa.String(256)))
+
+def downgrade():
+    op.drop_column('ps_subscription_persistence', 'contact_uri')
+    op.drop_column('ps_transports', 'symmetric_transport')
