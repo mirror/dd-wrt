cb0fe2bfdbadb43b6eee42ed89192acac34f826f
BrainSlayer <s.gottschall@dd-wrt.com>
2017-08-10 08:34:50 +0000

    disable sslv3 and gost
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33089 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/openssl.mk
+++ src/router/rules/openssl.mk
@@ -88,9 +88,9 @@ openssl-clean:
 
 
 
-OPENSSL_NO_CIPHERS:= no-idea no-md2 no-mdc2 no-rc5 no-camellia no-whirlpool no-seed
+OPENSSL_NO_CIPHERS:= no-idea no-md2 no-mdc2 no-rc5 no-camellia no-whirlpool no-seed -no-gost
 
-OPENSSL_OPTIONS:= no-err threads no-ssl2 enable-ssl3 enable-ssl3-method zlib-dynamic no-ec2m no-heartbeats no-async no-egd
+OPENSSL_OPTIONS:= no-err threads no-ssl2 enable-ssl3-method zlib-dynamic no-ec2m no-heartbeats no-async no-egd
 
 ifeq ($(CONFIG_XSCALE),y)
 OPENSSL_OPTIONS += -DHAVE_CRYPTODEV
