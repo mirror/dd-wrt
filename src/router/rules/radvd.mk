f83999d2c307fe1fcdade424b0f21c87a0c68c0c
BrainSlayer <s.gottschall@dd-wrt.com>
2017-08-11 14:09:16 +0000

    fix dep
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33122 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 28b279a463368843fb6bf1e87328a271b862850e

--- src/router/rules/radvd.mk
+++ src/router/rules/radvd.mk
@@ -3,7 +3,7 @@ radvd-configure: nvram libutils
 	cd radvd/libdaemon && ./configure --disable-nls --disable-shared --enable-static --disable-lynx --prefix=/usr --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS) -ffunction-sections -fdata-sections -Wl,--gc-sections "  ac_cv_func_setpgrp_void=yes ; make
 	-cd radvd && aclocal && autoconf && automake -a && cd .. 
 	cd radvd/flex && ./configure --disable-nls --prefix=/usr --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS) -ffunction-sections -fdata-sections -Wl,--gc-sections"; \
-	cd .. && ./configure --host=$(ARCH)-linux CHECK_CFLAGS=no CHECK_LIBS=no CFLAGS="$(COPTS) -DNEED_PRINTF -I$(TOP)/radvd/flex -ffunction-sections -fdata-sections -Wl,--gc-sections " DAEMON_CFLAGS="-I$(TOP)/radvd/libdaemon" DAEMON_LIBS="-L$(TOP)/radvd/libdaemon/libdaemon/.libs  -ldaemon" LDFLAGS="-L$(TOP)/radvd/flex -L$(TOP)/libutils -lutils -L$(TOP)/nvram -lnvram -Wl,-rpath,$(TOP)/jansson/src/.libs  -ffunction-sections -fdata-sections -Wl,--gc-sections"  --with-flex=$(TOP)/radvd/flex; \
+	cd .. && ./configure --host=$(ARCH)-linux CHECK_CFLAGS=no CHECK_LIBS=no CFLAGS="$(COPTS) -DNEED_PRINTF -I$(TOP)/radvd/flex -ffunction-sections -fdata-sections -Wl,--gc-sections " DAEMON_CFLAGS="-I$(TOP)/radvd/libdaemon" DAEMON_LIBS="-L$(TOP)/radvd/libdaemon/libdaemon/.libs  -ldaemon" LDFLAGS="-L$(TOP)/radvd/flex -L$(TOP)/libutils -lutils -lshutils -L$(TOP)/nvram -lnvram -Wl,-rpath,$(TOP)/jansson/src/.libs  -ffunction-sections -fdata-sections -Wl,--gc-sections"  --with-flex=$(TOP)/radvd/flex; \
 	
 radvd-clean:
 	if test -e "radvd/Makefile"; then make -C radvd/flex clean; make -C radvd clean; make -C radvd/libdaemon clean; fi
