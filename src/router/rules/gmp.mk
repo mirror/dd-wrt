52adf83fef3903dfc38eab8b78f27833f78205b6
BrainSlayer <s.gottschall@dd-wrt.com>
2017-08-13 22:39:53 +0000

    for some reason ppc crashes, so lets link them together
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33158 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: ea1ad655005d4352e50b71fef5a2024895093fd3

--- src/router/rules/gmp.mk
+++ src/router/rules/gmp.mk
@@ -3,21 +3,22 @@ gmp-configure:
 		--build=$(ARCH)-linux \
 		--host=$(ARCH)-linux-gnu \
 		--prefix=/usr \
-		CFLAGS="$(COPTS) $(MIPS16_OPT) -fPIC -I$(TOP)/iptables/include/libipq/" LDFLAGS="-L$(TOP)/iptables/libipq"
+		--enable-assembly \
+		CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/iptables/include/libipq/" LDFLAGS="-L$(TOP)/iptables/libipq"
 
 
 gmp:
-	$(MAKE) -C gmp CFLAGS="$(COPTS) -fPIC"
+	$(MAKE) -C gmp CFLAGS="$(COPTS)"
 
 gmp-install:
-	$(MAKE) -C gmp install DESTDIR=$(INSTALLDIR)/gmp
-	rm -rf $(INSTALLDIR)/gmp/usr/share
-	rm -rf $(INSTALLDIR)/gmp/usr/include
-	mkdir -p $(INSTALLDIR)/gmp/usr/lib
-	-mv $(INSTALLDIR)/gmp/usr/lib64/* $(INSTALLDIR)/gmp/usr/lib  
-	rm -f $(INSTALLDIR)/gmp/usr/lib/libgmp.a
-	rm -f $(INSTALLDIR)/gmp/usr/lib/libgmp.la
-	rm -rf $(INSTALLDIR)/gmp/usr/lib64
+#	$(MAKE) -C gmp install DESTDIR=$(INSTALLDIR)/gmp
+#	rm -rf $(INSTALLDIR)/gmp/usr/share
+#	rm -rf $(INSTALLDIR)/gmp/usr/include
+#	mkdir -p $(INSTALLDIR)/gmp/usr/lib
+#	-mv $(INSTALLDIR)/gmp/usr/lib64/* $(INSTALLDIR)/gmp/usr/lib  
+#	rm -f $(INSTALLDIR)/gmp/usr/lib/libgmp.a
+#	rm -f $(INSTALLDIR)/gmp/usr/lib/libgmp.la
+#	rm -rf $(INSTALLDIR)/gmp/usr/lib64
 
 
 
--- src/router/rules/nettle.mk
+++ src/router/rules/nettle.mk
@@ -4,7 +4,7 @@ NETTLE_CONFIGURE_ARGS+= \
 	--with-lib-path="$(TOP)/openssl , $(TOP)/gmp"
 
 nettle-configure: pcre zlib openssl gmp
-	cd nettle && ./configure --host=$(ARCH)-linux --prefix=/usr CC="ccache $(CC)" $(NETTLE_CONFIGURE_ARGS) CFLAGS="-fPIC $(COPTS) $(MIPS16_OPT) -I$(TOP)/pcre -I$(TOP)/gmp -I$(TOP)/zlib" CPPFLAGS="$(COPTS) $(MIPS16_OPT)" LDFLAGS="-L$(TOP)/pcre/.libs -L$(TOP)/gmp/.libs -lpthread -lpcre -L$(TOP)/zlib $(LDFLAGS) -lz"
+	cd nettle && ./configure --host=$(ARCH)-linux --disable-pic --prefix=/usr CC="ccache $(CC)" $(NETTLE_CONFIGURE_ARGS) CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/pcre -I$(TOP)/gmp -I$(TOP)/zlib" CPPFLAGS="$(COPTS) $(MIPS16_OPT)" LDFLAGS="-L$(TOP)/pcre/.libs -L$(TOP)/gmp/.libs -lpthread -lpcre -L$(TOP)/zlib $(LDFLAGS) -lz"
 
 nettle: openssl gmp
 	make -C nettle 
@@ -13,9 +13,9 @@ nettle-clean:
 	-make -C nettle clean
 
 nettle-install:
-	make -C nettle install DESTDIR=$(INSTALLDIR)/nettle
-	-mv $(INSTALLDIR)/nettle/usr/lib64 $(INSTALLDIR)/nettle/usr/lib
-	rm -rf $(INSTALLDIR)/nettle/usr/lib/pkgconfig
-	rm -rf $(INSTALLDIR)/nettle/usr/include
-	rm -rf $(INSTALLDIR)/nettle/usr/bin
-	rm -f $(INSTALLDIR)/nettle/usr/lib/*.a
+#	make -C nettle install DESTDIR=$(INSTALLDIR)/nettle
+#	-mv $(INSTALLDIR)/nettle/usr/lib64 $(INSTALLDIR)/nettle/usr/lib
+#	rm -rf $(INSTALLDIR)/nettle/usr/lib/pkgconfig
+#	rm -rf $(INSTALLDIR)/nettle/usr/include
+#	rm -rf $(INSTALLDIR)/nettle/usr/bin
+#	rm -f $(INSTALLDIR)/nettle/usr/lib/*.a
