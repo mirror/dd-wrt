f45c1799f959212b49cea4d4a767db063ec927e2
BrainSlayer <s.gottschall@dd-wrt.com>
2017-08-24 22:43:53 +0000

    do not install cron for micro
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33212 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 71ae47fc1b8d00d5c01faa0b59644788c067543d

--- src/router/rules/cron.mk
+++ src/router/rules/cron.mk
@@ -1,5 +1,8 @@
 cron-install:
+ifneq ($(CONFIG_DIST),"micro")
+ifneq ($(CONFIG_DIST),"micro-special")
 	#install -d $(INSTALLDIR)/cron/etc/cron.d
 	install -D cron/cron $(INSTALLDIR)/cron/usr/sbin/cron
 	$(STRIP) $(INSTALLDIR)/cron/usr/sbin/cron
-
+endif
+endif
