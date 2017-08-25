5d953e216e6f1be3bd3389f47297d60289b380d0
BrainSlayer <s.gottschall@dd-wrt.com>
2017-08-25 09:20:11 +0000

    fix build
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33222 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/iperf.mk
+++ src/router/rules/iperf.mk
@@ -1,8 +1,9 @@
 iperf-configure:
+	cd iperf && ./bootstrap.sh
 	cd iperf && ./configure --host=$(ARCH)-linux --disable-shared --prefix=/usr --libdir=/usr/lib CC="$(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -D_GNU_SOURCE -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_malloc=malloc" CPPFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_malloc=malloc"
 
 iperf:
-	$(MAKE) -C iperf
+	$(MAKE) -C iperf/src iperf3
 
 iperf-clean:
 	if test -e "iperf/Makefile"; then make -C iperf clean; fi
