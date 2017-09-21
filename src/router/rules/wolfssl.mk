d5208f4d859a7b31161dc00214b7aa54843be9fb
BrainSlayer <s.gottschall@dd-wrt.com>
2017-09-21 20:05:03 +0000

    smaller
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33386 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/wolfssl.mk
+++ src/router/rules/wolfssl.mk
@@ -1,5 +1,5 @@
 wolfssl-configure:
-	cd wolfssl && ./configure --prefix=/usr --host=$(ARCH)-linux --enable-opensslextra --disable-shared --enable-static --disable-oldtls --enable-ecc --disable-sslv3 CC="ccache $(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections"
+	cd wolfssl && ./configure --prefix=/usr --host=$(ARCH)-linux --enable-opensslextra --disable-shared --enable-static -disable-errorstrings --disable-oldtls --disable-poly1305 --disable-chacha --enable-ecc --disable-sslv3 CC="ccache $(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections"
 
 wolfssl:
 	$(MAKE) -j 4 -C wolfssl
