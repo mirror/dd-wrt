6ac7e2cce860511f5eb5f14d7aed3d41e7a1cd18
BrainSlayer <s.gottschall@dd-wrt.com>
2017-10-01 13:44:24 +0000

    apu2 reboot fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33421 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 7a00ae50da636e5ecbb3a89d435f899c0f7da6cd

--- src/linux/universal/linux-4.9/arch/x86/kernel/reboot.c
+++ src/linux/universal/linux-4.9/arch/x86/kernel/reboot.c
@@ -443,6 +443,16 @@ static struct dmi_system_id __initdata reboot_dmi_table[] = {
 		},
 	},
 
+	/* PC Engines */
+	{	/* Handle problems with rebooting on PC Engines apu2 */
+		.callback = set_pci_reboot,
+		.ident = "PC Engines apu2",
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "PC Engines"),
+			DMI_MATCH(DMI_BOARD_NAME, "apu2"),
+		},
+	},
+
 	/* Sony */
 	{	/* Handle problems with rebooting on Sony VGN-Z540N */
 		.callback = set_bios_reboot,
