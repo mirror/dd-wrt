72990924d3e1e0ccc3cd29c469169707e5f6280e
BrainSlayer <s.gottschall@dd-wrt.com>
2017-10-08 11:51:44 +0000

    update kernels
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33472 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 5b809bb88058bc0994fa43cbbd095566a5ad0eaa

--- src/linux/universal/linux-3.18/net/netfilter/nf_conntrack_ecache.c
+++ src/linux/universal/linux-3.18/net/netfilter/nf_conntrack_ecache.c
@@ -205,6 +205,8 @@ void nf_ct_expect_unregister_notifier(struct net *net,
 	BUG_ON(notify != new);
 	RCU_INIT_POINTER(net->ct.nf_expect_event_cb, NULL);
 	mutex_unlock(&nf_ct_ecache_mutex);
+	/* synchronize_rcu() is called from ctnetlink_exit. */
+	/* synchronize_rcu() is called from ctnetlink_exit. */
 }
 EXPORT_SYMBOL_GPL(nf_ct_expect_unregister_notifier);
 
