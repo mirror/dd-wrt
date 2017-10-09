1af0f755fb7506a4eae73a379a7136b6feda9b91
BrainSlayer <s.gottschall@dd-wrt.com>
2017-10-09 21:57:38 +0000

    backport tcp performance patches to 4.4
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33481 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4/net/ipv4/tcp_timer.c
+++ src/linux/universal/linux-4.4/net/ipv4/tcp_timer.c
@@ -275,7 +275,7 @@ static void tcp_delack_timer(unsigned long data)
 		inet_csk(sk)->icsk_ack.blocked = 1;
 		NET_INC_STATS_BH(sock_net(sk), LINUX_MIB_DELAYEDACKLOCKED);
 		/* deleguate our work to tcp_release_cb() */
-		if (!test_and_set_bit(TCP_DELACK_TIMER_DEFERRED, &tcp_sk(sk)->tsq_flags))
+		if (!test_and_set_bit(TCP_DELACK_TIMER_DEFERRED, &sk->sk_tsq_flags))
 			sock_hold(sk);
 	}
 	bh_unlock_sock(sk);
@@ -546,7 +546,7 @@ static void tcp_write_timer(unsigned long data)
 		tcp_write_timer_handler(sk);
 	} else {
 		/* deleguate our work to tcp_release_cb() */
-		if (!test_and_set_bit(TCP_WRITE_TIMER_DEFERRED, &tcp_sk(sk)->tsq_flags))
+		if (!test_and_set_bit(TCP_WRITE_TIMER_DEFERRED, &sk->sk_tsq_flags))
 			sock_hold(sk);
 	}
 	bh_unlock_sock(sk);
