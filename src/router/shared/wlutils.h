b8eac4730a3f42b6a164a16107de7d61496f3794
BrainSlayer <s.gottschall@dd-wrt.com>
2017-10-11 20:28:36 +0000

    ath9k supports airtime fairness which is enabled by default. allow to disable it
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33511 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 848ab3df8ecaaeb9f8b41b9362df6759579b3527

--- src/router/shared/wlutils.h
+++ src/router/shared/wlutils.h
@@ -185,6 +185,7 @@ extern void set_ath10kdistance(char *ifname, unsigned int distance);
 extern unsigned int get_ath10kack(char *ifname);
 extern unsigned int get_ath10kdistance(char *ifname);
 #endif
+extern int has_airtime_fairness(char *prefix);
 extern int mac80211_check_band(char *interface, int checkband);
 struct wifi_channels *mac80211_get_channels(char *interface, const char *country, int max_bandwidth_khz, unsigned char checkband);
 #define AUTO_FORCEHT40 1
