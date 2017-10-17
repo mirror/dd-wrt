a1c41c948d8dc731376bb3b210d33304b8c0a214
BrainSlayer <s.gottschall@dd-wrt.com>
2017-10-17 20:07:29 +0000

    fix for nomessage
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33532 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/hostapd-2016-09-05/src/utils/wpa_debug.h
+++ src/router/hostapd-2016-09-05/src/utils/wpa_debug.h
@@ -383,17 +383,7 @@ enum hostapd_logger_level {
 void wpa_debug_open_syslog(void);
 void wpa_debug_close_syslog(void);
 
-#else /* CONFIG_DEBUG_SYSLOG */
-
-static inline void wpa_debug_open_syslog(void)
-{
-}
-
-static inline void wpa_debug_close_syslog(void)
-{
-}
-
-#endif /* CONFIG_DEBUG_SYSLOG */
+#endif
 
 #ifdef CONFIG_DEBUG_LINUX_TRACING
 
