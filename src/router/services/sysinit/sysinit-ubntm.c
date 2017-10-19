d584f7c156e5f540f44b8009344cd0ec842c4716
BrainSlayer <s.gottschall@dd-wrt.com>
2017-10-19 15:01:50 +0000

    use different mac on vlans
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33554 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 051ae62ccc0ee53ad28b3e615f8e82be41ff2213

--- src/router/services/sysinit/sysinit-ubntm.c
+++ src/router/services/sysinit/sysinit-ubntm.c
@@ -181,6 +181,18 @@ void start_sysinit(void)
 		eval("vconfig", "set_name_type", "VLAN_PLUS_VID_NO_PAD");
 		eval("vconfig", "add", "eth0", "1");
 		eval("vconfig", "add", "eth0", "2");
+		int s;
+		if ((s = socket(AF_INET, SOCK_RAW, IPPROTO_RAW))) {
+			char eabuf[32];
+
+			strncpy(ifr.ifr_name, "eth0", IFNAMSIZ);
+			ioctl(s, SIOCGIFHWADDR, &ifr);
+			strcpy(mac, ether_etoa((char *)ifr.ifr_hwaddr.sa_data, eabuf));
+			eval("ifconfig", "vlan1", "hw", "ether", mac);
+			MAC_ADD(mac);
+			eval("ifconfig", "vlan2", "hw", "ether", mac);
+			close(s);
+		}
 
 	}
 
