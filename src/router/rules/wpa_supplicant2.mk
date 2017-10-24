a7b2561dcae089b7629bf428a479092dd8b30fd3
BrainSlayer <s.gottschall@dd-wrt.com>
2017-10-24 22:08:45 +0000

    try newer supplicant for broadcom
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33606 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/wpa_supplicant2.mk
+++ src/router/rules/wpa_supplicant2.mk
@@ -1,17 +1,17 @@
 wpa_supplicant2: hostapd2 
 ifeq ($(CONFIG_WPA_SUPPLICANT2),y)
-	$(MAKE) -C hostapd2/wpa_supplicant -j1 clean
-	$(MAKE) -C hostapd2/wpa_supplicant -j1 
+	$(MAKE) -C hostapd-2017-08-24/wpa_supplicant -j1 clean
+	$(MAKE) -C hostapd-2017-08-24/wpa_supplicant -j1 
 else
 	@true
 endif
 
 wpa_supplicant2-clean:
-	$(MAKE) -C hostapd2/wpa_supplicant clean
+	$(MAKE) -C hostapd-2017-08-24/wpa_supplicant clean
 
 wpa_supplicant2-install:
 ifeq ($(CONFIG_WPA_SUPPLICANT2),y)
-	install -D hostapd2/wpa_supplicant/wpa_supplicant $(INSTALLDIR)/wpa_supplicant2/usr/sbin/wpa_supplicant
+	install -D hostapd-2017-08-24/wpa_supplicant/wpa_supplicant $(INSTALLDIR)/wpa_supplicant2/usr/sbin/wpa_supplicant
 	$(STRIP) $(INSTALLDIR)/wpa_supplicant2/usr/sbin/wpa_supplicant
 else
 	# So that generic rule does not take precedence
