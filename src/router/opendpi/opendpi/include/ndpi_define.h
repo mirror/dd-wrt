8b16856cb91b17c9dcb0279b6befcda9b2baf314
BrainSlayer <s.gottschall@dd-wrt.com>
2017-11-03 09:46:39 +0000

    fix array length
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33669 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/opendpi/opendpi/include/ndpi_define.h
+++ src/router/opendpi/opendpi/include/ndpi_define.h
@@ -220,7 +220,7 @@
   ((((u_int64_t *)(x))[0]) < (((u_int64_t *)(y))[0]) || ( (((u_int64_t *)(x))[0]) == (((u_int64_t *)(y))[0]) && (((u_int64_t *)(x))[1]) < (((u_int64_t *)(y))[1])) )
 
 //#if !defined(__KERNEL__) && !defined(NDPI_IPTABLES_EXT)
-#define NDPI_NUM_BITS              256
+#define NDPI_NUM_BITS              272
 //#else
 /* custom protocols not supported */
 //#define NDPI_NUM_BITS              192
