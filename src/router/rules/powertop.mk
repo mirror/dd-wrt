c0f402c7800a318c62b5359063b6de3be4ea10d5
BrainSlayer <s.gottschall@dd-wrt.com>
2017-11-05 20:30:21 +0000

    rules
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33696 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: ae771650baf0e82f392da47ee3977ea4924e304e

---
+++ src/router/rules/powertop.mk
@@ -0,0 +1,22 @@
+powertop-configure:
+	cd powertop && ./configure --disable-nls --prefix=/usr --host=$(ARCH)-linux \
+	NCURSES_CFLAGS="-I$(TOP)/ncurses/include" \
+	NCURSES_LIBS="-L$(TOP)/ncurses/lib -lncurses" \
+	LIBNL_CFLAGS="-I$(TOP)/libnl-tiny/include" \
+	LIBNL_LIBS="-L$(TOP)/libnl-tiny -lnl-tiny" \
+	PCIUTILS_CFLAGS="-I$(TOP)/pciutils/lib" \
+	PCIUTILS_LIBS="-L$(TOP)/pciutils/lib -lpci" \
+	CC="$(CC)" \
+	CFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -D_GNU_SOURCE -Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	CXXFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -D_GNU_SOURCE -Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections"
+
+powertop:
+	$(MAKE) -j 4 -C powertop
+
+powertop-clean:
+	if test -e "powertop/Makefile"; then make -C powertop clean; fi
+	@true
+
+powertop-install:
+	$(MAKE) -C powertop install DESTDIR=$(INSTALLDIR)/powertop
+	rm -rf $(INSTALLDIR)/powertop/usr/share
