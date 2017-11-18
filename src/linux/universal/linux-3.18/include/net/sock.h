1f6a3c0eb0b9739ca5597b0b80904231658ea846
BrainSlayer <s.gottschall@dd-wrt.com>
2017-11-18 09:41:42 +0000

    fix tsq and backport some tso logic
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33774 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/include/net/sock.h
+++ src/linux/universal/linux-3.18/include/net/sock.h
@@ -252,6 +252,7 @@ struct cg_proto;
   *	@sk_gso_type: GSO type (e.g. %SKB_GSO_TCPV4)
   *	@sk_gso_max_size: Maximum GSO segment size to build
   *	@sk_gso_max_segs: Maximum number of GSO segments
+  *	@sk_pacing_shift: scaling factor for TCP Small Queues
   *	@sk_lingertime: %SO_LINGER l_linger setting
   *	@sk_backlog: always used with the per-socket spinlock held
   *	@sk_callback_lock: used with the callbacks in the end of this struct
@@ -405,6 +406,8 @@ struct sock {
 				sk_type      : 16;
 	kmemcheck_bitfield_end(flags);
 	u16			sk_gso_max_segs;
+#define sk_pacing_shift sk_pacing_shift /* for backport checks */
+	u8			sk_pacing_shift;
 	unsigned long	        sk_lingertime;
 	struct proto		*sk_prot_creator;
 	rwlock_t		sk_callback_lock;
