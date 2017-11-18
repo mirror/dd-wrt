490fdb371c77c75f5b1e741c8b5bdf1b1b06d892
BrainSlayer <s.gottschall@dd-wrt.com>
2017-11-18 21:28:55 +0000

    update kernels
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33779 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: a101f5810f627231dd3f627737544b4b44a7546b

--- src/linux/universal/linux-4.9/net/core/dev.c
+++ src/linux/universal/linux-4.9/net/core/dev.c
@@ -1122,9 +1122,8 @@ static int dev_alloc_name_ns(struct net *net,
 	return ret;
 }
 
-static int dev_get_valid_name(struct net *net,
-			      struct net_device *dev,
-			      const char *name)
+int dev_get_valid_name(struct net *net, struct net_device *dev,
+		       const char *name)
 {
 	BUG_ON(!net);
 
@@ -1140,6 +1139,7 @@ static int dev_get_valid_name(struct net *net,
 
 	return 0;
 }
+EXPORT_SYMBOL(dev_get_valid_name);
 
 /**
  *	dev_change_name - change name of a device
