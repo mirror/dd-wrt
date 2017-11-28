0e25f17a75d0014074a68e4c1a2508060900c887
BrainSlayer <s.gottschall@dd-wrt.com>
2017-11-28 01:44:45 +0000

    optimize cflags
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33875 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 15dc87f69d36a903427934811d4c729dd13ab9d2

--- src/router/rules/libnetfilter_log.mk
+++ src/router/rules/libnetfilter_log.mk
@@ -7,14 +7,11 @@ libnetfilter_log-configure: libnfnetlink
 		--host=$(ARCH)-linux-gnu \
 		--prefix=/usr \
 		--libdir=$(TOP)/libnfnetlink/src/.libs
-		
-		#CFLAGS="$(COPTS) -fPIC -DNEED_PRINTF -I$(TOP)/iptables/include/libipq/" LDFLAGS="-L$(TOP)/iptables/libipq"
-
 
 libnetfilter_log: libnfnetlink
-	$(MAKE) -C libnetfilter_log CFLAGS="$(COPTS) -DNEED_PRINTF"
-	$(MAKE) -C libnetfilter_log/utils nfulnl_test CFLAGS="$(COPTS) -DNEED_PRINTF"
-	#$(MAKE) -C libnetfilter_log/utils CFLAGS="$(COPTS) -DNEED_PRINTF"
+	$(MAKE) -C libnetfilter_log CFLAGS="$(COPTS) $(MIPS16_OPT)"
+	$(MAKE) -C libnetfilter_log/utils nfulnl_test CFLAGS="$(COPTS) $(MIPS16_OPT)"
+	#$(MAKE) -C libnetfilter_log/utils CFLAGS="$(COPTS) $(MIPS16_OPT)"
 
 libnetfilter_log-install:
 	install -D libnetfilter_log/src/.libs/libnetfilter_log.so.1 $(INSTALLDIR)/libnetfilter_log/usr/lib/libnetfilter_log.so.1
