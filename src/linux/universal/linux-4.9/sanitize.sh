86ee53979274f110cb4ef163fff4c76246236975
BrainSlayer <s.gottschall@dd-wrt.com>
2017-12-01 07:25:48 +0000

    still in progress. wr810n support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33952 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 987eb1fbfa25b3fe383abac9fad6c739dd3e816d

--- src/linux/universal/linux-4.9/sanitize.sh
+++ src/linux/universal/linux-4.9/sanitize.sh
@@ -113,6 +113,7 @@ do
 	    echo CONFIG_XWM400=y >> .config
 	    echo CONFIG_ARCHERC7V4=y >> .config
 	    echo CONFIG_UBNTFIX=y >> .config
+	    echo CONFIG_WR810N=y >> .config
 	    make oldconfig ARCH=mips
 	    sed -i 's/\CONFIG_WR841V8=y/ /g' .config	    
 	    sed -i 's/\CONFIG_WR710=y/ /g' .config	    
@@ -153,6 +154,7 @@ do
 	    sed -i 's/\CONFIG_XWM400=y/ /g' .config	    
 	    sed -i 's/\CONFIG_ARCHERC7V4=y/ /g' .config	    
 	    sed -i 's/\CONFIG_UBNTFIX=y/ /g' .config	    
+	    sed -i 's/\CONFIG_WR810N=y/ /g' .config	    
     fi
     cp .config $i
 done
