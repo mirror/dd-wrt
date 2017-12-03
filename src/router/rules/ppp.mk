925deee1a9e9094ddc66b28f10c032af6af60000
BrainSlayer <s.gottschall@dd-wrt.com>
2017-12-03 11:41:23 +0000

    rename rp-pppoe dir
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33968 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 3020bb0a904c5ac55b45126f1d9f67aa87cbe2b2

--- src/router/rules/ppp.mk
+++ src/router/rules/ppp.mk
@@ -1,34 +1,34 @@
 ppp:
-	$(MAKE) -C rp-pppoe-3.5/src
+	$(MAKE) -C rp-pppoe/src
 
 ppp-clean:
-	$(MAKE) -C rp-pppoe-3.5/src clean
+	$(MAKE) -C rp-pppoe/src clean
 
 ppp-install:
-#	install -D rp-pppoe-3.5/src/pppoe $(INSTALLDIR)/ppp/usr/sbin/pppoe
+#	install -D rp-pppoe/src/pppoe $(INSTALLDIR)/ppp/usr/sbin/pppoe
 #	$(STRIP) $(INSTALLDIR)/ppp/usr/sbin/pppoe
 ifeq ($(CONFIG_PPPOERELAY),y)
-	install -D rp-pppoe-3.5/src/pppoe-multi $(INSTALLDIR)/ppp/usr/sbin/pppoe-multi
+	install -D rp-pppoe/src/pppoe-multi $(INSTALLDIR)/ppp/usr/sbin/pppoe-multi
 	cd $(INSTALLDIR)/ppp/usr/sbin && ln -sf pppoe-multi pppoe-relay
-	install -D rp-pppoe-3.5/config/pppoe-relay.nvramconfig $(INSTALLDIR)/ppp/etc/config/pppoe-relay.nvramconfig
-	install -D rp-pppoe-3.5/config/pppoe-relay.webservices $(INSTALLDIR)/ppp/etc/config/pppoe-relay.webservices
+	install -D rp-pppoe/config/pppoe-relay.nvramconfig $(INSTALLDIR)/ppp/etc/config/pppoe-relay.nvramconfig
+	install -D rp-pppoe/config/pppoe-relay.webservices $(INSTALLDIR)/ppp/etc/config/pppoe-relay.webservices
 else
 	rm -f $(INSTALLDIR)/ppp/usr/sbin/pppoe-relay
 endif
 ifeq ($(CONFIG_PPPOESERVER),y)
-	install -D rp-pppoe-3.5/src/pppoe-multi $(INSTALLDIR)/ppp/usr/sbin/pppoe-multi
+	install -D rp-pppoe/src/pppoe-multi $(INSTALLDIR)/ppp/usr/sbin/pppoe-multi
 	$(STRIP) $(INSTALLDIR)/ppp/usr/sbin/pppoe-multi
 	cd $(INSTALLDIR)/ppp/usr/sbin && ln -sf pppoe-multi pppoe-server
-	install -D rp-pppoe-3.5/config/pppoe-server.nvramconfig $(INSTALLDIR)/ppp/etc/config/pppoe-server.nvramconfig
+	install -D rp-pppoe/config/pppoe-server.nvramconfig $(INSTALLDIR)/ppp/etc/config/pppoe-server.nvramconfig
 endif
 ifeq ($(CONFIG_PPPOESNIFF),y)
-	install -D rp-pppoe-3.5/src/pppoe-sniff $(INSTALLDIR)/ppp/usr/sbin/pppoe-sniff
+	install -D rp-pppoe/src/pppoe-sniff $(INSTALLDIR)/ppp/usr/sbin/pppoe-sniff
 	$(STRIP) $(INSTALLDIR)/ppp/usr/sbin/pppoe-sniff
 else
 	rm -f $(INSTALLDIR)/ppp/usr/sbin/pppoe-sniff
 endif
 ifeq ($(CONFIG_PPPOEDISCOVERY),y)
-	install -D rp-pppoe-3.5/src/pppoe $(INSTALLDIR)/ppp/usr/sbin/pppoe
+	install -D rp-pppoe/src/pppoe $(INSTALLDIR)/ppp/usr/sbin/pppoe
 	$(STRIP) $(INSTALLDIR)/ppp/usr/sbin/pppoe
 else
 	rm -f $(INSTALLDIR)/ppp/usr/sbin/pppoe
