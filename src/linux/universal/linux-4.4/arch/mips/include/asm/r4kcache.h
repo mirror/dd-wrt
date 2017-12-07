3bd54e046e2caa1adbbfef7ea1c745fb71810a9a
BrainSlayer <s.gottschall@dd-wrt.com>
2017-12-07 11:00:02 +0000

    unroll mipsr4k cache blast function
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@33991 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: c802e6ddeb1ae36e13cc16926e3caeca347d5803

--- src/linux/universal/linux-4.4/arch/mips/include/asm/r4kcache.h
+++ src/linux/universal/linux-4.4/arch/mips/include/asm/r4kcache.h
@@ -743,18 +743,64 @@ static inline void prot##extra##blast_##pfx##cache##_range(unsigned long start, 
 						    unsigned long end)	\
 {									\
 	unsigned long lsize = cpu_##desc##_line_size();			\
+	unsigned long lsize_2 = lsize * 2;				\
+	unsigned long lsize_3 = lsize * 3;				\
+	unsigned long lsize_4 = lsize * 4;				\
+	unsigned long lsize_5 = lsize * 5;				\
+	unsigned long lsize_6 = lsize * 6;				\
+	unsigned long lsize_7 = lsize * 7;				\
+	unsigned long lsize_8 = lsize * 8;				\
 	unsigned long addr = start & ~(lsize - 1);			\
-	unsigned long aend = (end - 1) & ~(lsize - 1);			\
+	unsigned long aend = (end + lsize - 1) & ~(lsize - 1);		\
+	int lines = (aend - addr) / lsize;				\
 	war								\
 									\
 	__##pfx##flush_prologue						\
 									\
-	while (1) {							\
+	while (lines >= 8) {						\
 		war2						\
 		prot##cache_op(hitop, addr);				\
-		if (addr == aend)					\
-			break;						\
-		addr += lsize;						\
+		war2						\
+		prot##cache_op(hitop, addr + lsize);			\
+		war2						\
+		prot##cache_op(hitop, addr + lsize_2);			\
+		war2						\
+		prot##cache_op(hitop, addr + lsize_3);			\
+		war2						\
+		prot##cache_op(hitop, addr + lsize_4);			\
+		war2						\
+		prot##cache_op(hitop, addr + lsize_5);			\
+		war2						\
+		prot##cache_op(hitop, addr + lsize_6);			\
+		war2						\
+		prot##cache_op(hitop, addr + lsize_7);			\
+		addr += lsize_8;					\
+		lines -= 8;						\
+	}								\
+									\
+	if (lines & 0x4) {						\
+		war2						\
+		prot##cache_op(hitop, addr);				\
+		war2						\
+		prot##cache_op(hitop, addr + lsize);			\
+		war2						\
+		prot##cache_op(hitop, addr + lsize_2);			\
+		war2						\
+		prot##cache_op(hitop, addr + lsize_3);			\
+		addr += lsize_4;					\
+	}								\
+									\
+	if (lines & 0x2) {						\
+		war2						\
+		prot##cache_op(hitop, addr);				\
+		war2						\
+		prot##cache_op(hitop, addr + lsize);			\
+		addr += lsize_2;					\
+	}								\
+									\
+	if (lines & 0x1) {						\
+		war2						\
+		prot##cache_op(hitop, addr);				\
 	}								\
 									\
 	__##pfx##flush_epilogue						\
