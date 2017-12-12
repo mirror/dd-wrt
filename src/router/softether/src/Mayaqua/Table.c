8c69f0a5d28702e30d41bdfd22c904ee9e82295d
BrainSlayer <s.gottschall@dd-wrt.com>
2017-12-12 20:19:14 +0000

    update softether
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@34064 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: cd0a9b1e9878d7131b22de8d1879522dbcc1a460

--- src/router/softether/src/Mayaqua/Table.c
+++ src/router/softether/src/Mayaqua/Table.c
@@ -1,17 +1,17 @@
-// SoftEther VPN Source Code
+// SoftEther VPN Source Code - Stable Edition Repository
 // Mayaqua Kernel
 // 
 // SoftEther VPN Server, Client and Bridge are free software under GPLv2.
 // 
-// Copyright (c) 2012-2016 Daiyuu Nobori.
-// Copyright (c) 2012-2016 SoftEther VPN Project, University of Tsukuba, Japan.
-// Copyright (c) 2012-2016 SoftEther Corporation.
+// Copyright (c) Daiyuu Nobori.
+// Copyright (c) SoftEther VPN Project, University of Tsukuba, Japan.
+// Copyright (c) SoftEther Corporation.
 // 
 // All Rights Reserved.
 // 
 // http://www.softether.org/
 // 
-// Author: Daiyuu Nobori
+// Author: Daiyuu Nobori, Ph.D.
 // Comments: Tetsuo Sugiyama, Ph.D.
 // 
 // This program is free software; you can redistribute it and/or
@@ -1004,7 +1004,3 @@ bool LoadTableW(wchar_t *filename)
 }
 
 
-
-// Developed by SoftEther VPN Project at University of Tsukuba in Japan.
-// Department of Computer Science has dozens of overly-enthusiastic geeks.
-// Join us: http://www.tsukuba.ac.jp/english/admission/
