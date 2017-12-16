667c6729174c825340979d9b8f937336e1353b5c
kong <USER@DOMAIN>
2017-12-16 10:56:31 +0000

    add offset
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@34106 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 1231ee844cdd59d415401fa335bb3c907c67ee1b

--- src/linux/universal/linux-4.4/arch/arm/mach-brcm-hnd/gpio.c
+++ src/linux/universal/linux-4.4/arch/arm/mach-brcm-hnd/gpio.c
@@ -238,7 +238,7 @@ static int __init gpio_init(void)
 		gpios = 1<<0 | 1<<1 | 1<<2 | 1<<3 | 1<<4 | 1<<5 | 1<<6 | 1<<7 | 1<<8 | 1<<9 | 1<<10 | 1<<11 | 1<<15;
 	}
 	
-	if ((boardnum == 32) && nvram_match("boardtype", "0x0646") && (nvram_match("boardrev", "0x1601"))) {
+	if ((boardnum == 32) && nvram_match("boardtype", "0x0646") && (nvram_match("boardrev", "0x1601")&& !nvram_match("et2phyaddr", "30"))) {
 		printk(KERN_EMERG "Netgear R6400 init\n");
 		gpios = 1<<0 | 1<<1 | 1<<2 | 1<<3 | 1<<4 | 1<<5 | 1<<6 | 1<<7 | 1<<8 | 1<<9 | 1<<10 | 1<<11 | 1<<15;
 	}
@@ -257,6 +257,12 @@ static int __init gpio_init(void)
 		printk(KERN_EMERG "Netgear R8500 init\n");
 		gpios = 1<<0 | 1<<1 | 1<<2 | 1<<3 | 1<<4 | 1<<5 | 1<<6 | 1<<7 | 1<<8 | 1<<9 | 1<<10 | 1<<11 | 1<<12 | 1<<13 | 1<<14 | 1<<15 | 1<<16 | 1<<17 | 1<<18 | 1<<19 | 1<<20 | 1<<21 | 1<<22 | 1<<23  | 1<<24 ;
 	}
+	
+	if ((boardnum == 32) && nvram_match("boardtype", "0x0646") && (nvram_match("boardrev", "0x1601") && nvram_match("et2phyaddr", "30"))) {
+		printk(KERN_EMERG "Netgear R7000P init\n");
+		gpios = 1<<0 | 1<<1 | 1<<2 | 1<<3 | 1<<4 | 1<<5 | 1<<6 | 1<<7 | 1<<8 | 1<<9 | 1<<10 | 1<<11 | 1<<12 | 1<<13 | 1<<14 | 1<<15 | 1<<16 | 1<<17 | 1<<18 | 1<<19 | 1<<20 | 1<<21 | 1<<22 | 1<<23  | 1<<24 ;
+	}
+	
 	if (nvram_match("model","RT-AC1200G+")) {
 		printk(KERN_EMERG " Init Asus RT-AC1200G+\n");
 		isdefault = 1;
