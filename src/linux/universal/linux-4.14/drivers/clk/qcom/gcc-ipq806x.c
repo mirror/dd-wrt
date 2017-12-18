e8c7a46fc7e3be59a77e2c2b8355dc29691d84c1
BrainSlayer <s.gottschall@dd-wrt.com>
2017-12-18 19:45:17 +0000

    rollback
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@34130 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 5ead0ce89b7cb41c8557134dfd5ec4c441efab63

--- src/linux/universal/linux-4.14/drivers/clk/qcom/gcc-ipq806x.c
+++ src/linux/universal/linux-4.14/drivers/clk/qcom/gcc-ipq806x.c
@@ -30,7 +30,6 @@
 #include "clk-pll.h"
 #include "clk-rcg.h"
 #include "clk-branch.h"
-#include "clk-hfpll.h"
 #include "reset.h"
 
 static struct clk_pll pll0 = {
@@ -112,84 +111,6 @@ static struct clk_regmap pll8_vote = {
 		.num_parents = 1,
 		.ops = &clk_pll_vote_ops,
 	},
-};
-
-static struct hfpll_data hfpll0_data = {
-	.mode_reg = 0x3200,
-	.l_reg = 0x3208,
-	.m_reg = 0x320c,
-	.n_reg = 0x3210,
-	.config_reg = 0x3204,
-	.status_reg = 0x321c,
-	.config_val = 0x7845c665,
-	.droop_reg = 0x3214,
-	.droop_val = 0x0108c000,
-	.min_rate = 600000000UL,
-	.max_rate = 1800000000UL,
-};
-
-static struct clk_hfpll hfpll0 = {
-	.d = &hfpll0_data,
-	.clkr.hw.init = &(struct clk_init_data){
-		.parent_names = (const char *[]){ "pxo" },
-		.num_parents = 1,
-		.name = "hfpll0",
-		.ops = &clk_ops_hfpll,
-		.flags = CLK_IGNORE_UNUSED,
-	},
-	.lock = __SPIN_LOCK_UNLOCKED(hfpll0.lock),
-};
-
-static struct hfpll_data hfpll1_data = {
-	.mode_reg = 0x3240,
-	.l_reg = 0x3248,
-	.m_reg = 0x324c,
-	.n_reg = 0x3250,
-	.config_reg = 0x3244,
-	.status_reg = 0x325c,
-	.config_val = 0x7845c665,
-	.droop_reg = 0x3314,
-	.droop_val = 0x0108c000,
-	.min_rate = 600000000UL,
-	.max_rate = 1800000000UL,
-};
-
-static struct clk_hfpll hfpll1 = {
-	.d = &hfpll1_data,
-	.clkr.hw.init = &(struct clk_init_data){
-		.parent_names = (const char *[]){ "pxo" },
-		.num_parents = 1,
-		.name = "hfpll1",
-		.ops = &clk_ops_hfpll,
-		.flags = CLK_IGNORE_UNUSED,
-	},
-	.lock = __SPIN_LOCK_UNLOCKED(hfpll1.lock),
-};
-
-static struct hfpll_data hfpll_l2_data = {
-	.mode_reg = 0x3300,
-	.l_reg = 0x3308,
-	.m_reg = 0x330c,
-	.n_reg = 0x3310,
-	.config_reg = 0x3304,
-	.status_reg = 0x331c,
-	.config_val = 0x7845c665,
-	.droop_reg = 0x3314,
-	.droop_val = 0x0108c000,
-	.min_rate = 600000000UL,
-	.max_rate = 1800000000UL,
-};
-
-static struct clk_hfpll hfpll_l2 = {
-	.d = &hfpll_l2_data,
-	.clkr.hw.init = &(struct clk_init_data){
-		.parent_names = (const char *[]){ "pxo" },
-		.num_parents = 1,
-		.name = "hfpll_l2",
-		.ops = &clk_ops_hfpll,
-		.flags = CLK_IGNORE_UNUSED,
-	},
-	.lock = __SPIN_LOCK_UNLOCKED(hfpll_l2.lock),
 };
 
 static struct clk_pll pll14 = {
@@ -373,7 +294,7 @@ static struct clk_rcg gsbi1_uart_src = {
 			.parent_names = gcc_pxo_pll8,
 			.num_parents = 2,
 			.ops = &clk_rcg_ops,
-			.flags = CLK_SET_PARENT_GATE | CLK_IGNORE_UNUSED,
+			.flags = CLK_SET_PARENT_GATE,
 		},
 	},
 };
@@ -391,7 +312,7 @@ static struct clk_branch gsbi1_uart_clk = {
 			},
 			.num_parents = 1,
 			.ops = &clk_branch_ops,
-			.flags = CLK_SET_RATE_PARENT | CLK_IGNORE_UNUSED,
+			.flags = CLK_SET_RATE_PARENT,
 		},
 	},
 };
@@ -969,7 +890,6 @@ static struct clk_branch gsbi1_h_clk = {
 		.hw.init = &(struct clk_init_data){
 			.name = "gsbi1_h_clk",
 			.ops = &clk_branch_ops,
-			.flags = CLK_IGNORE_UNUSED,
 		},
 	},
 };
@@ -1233,8 +1153,6 @@ static struct clk_rcg prng_src = {
 		.parent_map = gcc_pxo_pll8_map,
 	},
 	.clkr = {
-		.enable_reg = 0x2e80,
-		.enable_mask = BIT(11),
 		.hw.init = &(struct clk_init_data){
 			.name = "prng_src",
 			.parent_names = gcc_pxo_pll8,
@@ -2882,9 +2800,6 @@ static struct clk_regmap *gcc_ipq806x_clks[] = {
 	[UBI32_CORE2_CLK_SRC] = &ubi32_core2_src_clk.clkr,
 	[NSSTCM_CLK_SRC] = &nss_tcm_src.clkr,
 	[NSSTCM_CLK] = &nss_tcm_clk.clkr,
-	[PLL9] = &hfpll0.clkr,
-	[PLL10] = &hfpll1.clkr,
-	[PLL12] = &hfpll_l2.clkr,
 };
 
 static const struct qcom_reset_map gcc_ipq806x_resets[] = {
