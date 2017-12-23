434c6fb714cdb168bb8caa960f5efd748980de0f
BrainSlayer <s.gottschall@dd-wrt.com>
2017-12-23 00:34:00 +0000

    mvebu fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@34188 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: f10e33047cd6d1b2518a71da67b7020c2c3ac0c5

--- src/linux/universal/linux-4.14/crypto/cryptodev/ioctl.c
+++ src/linux/universal/linux-4.14/crypto/cryptodev/ioctl.c
@@ -1124,7 +1124,7 @@ static struct ctl_table verbosity_ctl_dir[] = {
 		.mode           = 0644,
 		.proc_handler   = proc_dointvec,
 	},
-	{0, },
+	{}
 };
 
 static struct ctl_table verbosity_ctl_root[] = {
@@ -1133,7 +1133,8 @@ static struct ctl_table verbosity_ctl_root[] = {
 		.mode           = 0555,
 		.child          = verbosity_ctl_dir,
 	},
-	{0, },
+	{}
+
 };
 static struct ctl_table_header *verbosity_sysctl_header;
 static int __init init_cryptodev(void)
