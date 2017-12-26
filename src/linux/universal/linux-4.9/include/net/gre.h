ec9bb2090f4f836ed0fb17dcb6ac157a8442d1a8
BrainSlayer <s.gottschall@dd-wrt.com>
2017-12-26 10:58:53 +0000

    mtik eoip tunnel
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@34252 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 0cbe3e8f2e5c9bd3bbe853de3491ac4f2bd716b7

--- src/linux/universal/linux-4.9/include/net/gre.h
+++ src/linux/universal/linux-4.9/include/net/gre.h
@@ -23,6 +23,13 @@ struct gre_full_hdr {
 #define GREPROTO_MAX		2
 #define GRE_IP_PROTO_MAX	2
 
+/* handle protocols with non-standard GRE header by ids that do not overlap
+ * with possible standard GRE protocol versions (0x00 - 0x7f)
+ */
+#define GREPROTO_NONSTD_BASE       0x80
+#define GREPROTO_NONSTD_EOIP       (0 + GREPROTO_NONSTD_BASE)
+#define GREPROTO_NONSTD_MAX        (1 + GREPROTO_NONSTD_BASE)
+
 struct gre_protocol {
 	int  (*handler)(struct sk_buff *skb);
 	void (*err_handler)(struct sk_buff *skb, u32 info);
