84cfe8f899e12728ff653e1bfa42b8147818be7c
BrainSlayer <s.gottschall@dd-wrt.com>
2017-12-28 00:12:03 +0000

    mtik eoip for 3.2
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@34297 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.2/include/net/gre.h
+++ src/linux/universal/linux-3.2/include/net/gre.h
@@ -7,6 +7,14 @@
 #define GREPROTO_PPTP		1
 #define GREPROTO_MAX		2
 
+/* handle protocols with non-standard GRE header by ids that do not overlap
+ * with possible standard GRE protocol versions (0x00 - 0x7f)
+ */
+#define GREPROTO_NONSTD_BASE		0x80
+#define GREPROTO_NONSTD_EOIP		(0 + GREPROTO_NONSTD_BASE)
+#define GREPROTO_NONSTD_MAX		(1 + GREPROTO_NONSTD_BASE)
+
+
 struct gre_protocol {
 	int  (*handler)(struct sk_buff *skb);
 	void (*err_handler)(struct sk_buff *skb, u32 info);
