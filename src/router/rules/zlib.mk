dc0f040bffd80a556531ffbac696a2288712d962
BrainSlayer <s.gottschall@dd-wrt.com>
2018-01-04 06:03:59 +0000

    use cmake
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@34362 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/zlib.mk
+++ src/router/rules/zlib.mk
@@ -1,11 +1,21 @@
-zlib-configure:
-	cd zlib && CFLAGS="$(COPTS) $(MIPS16_OPT)" && ./configure
+PKG_INSTALL:=1
 
-zlib-clean:
-	make -C zlib clean
+MAKE_FLAGS+=VERBOSE=0
+
+ZLIB_PKG_BUILD_DIR=$(TOP)/zlib
+ZLIB_CMAKE_OPTIONS=
+ZLIB_STAGING_DIR=$(TOP)/_staging/usr
+ZLIB_EXTRA_CFLAGS=-I$(TOP)/_staging/usr/include $(COPTS) $(MIPS16_OPT)
+ZLIB_EXTRA_LDFLAGS=-L$(TOP)/_staging/usr/lib
+
+ZLIB_CMAKE_OPTIONS=
+
+zlib-configure:
+	$(call CMakeClean,$(ZLIB_PKG_BUILD_DIR))
+	$(call CMakeConfigure,$(ZLIB_PKG_BUILD_DIR),$(ZLIB_STAGING_DIR),$(ZLIB_CMAKE_OPTIONS),$(ZLIB_EXTRA_CFLAGS),$(ZLIB_EXTRA_LDFLAGS)) 
 
 zlib:
-	make -C zlib
+	$(MAKE) -C zlib
 	rm -f zlib/libz.a
 
 zlib-install:
@@ -13,3 +23,6 @@ zlib-install:
 	cd $(INSTALLDIR)/zlib/usr/lib ; ln -s libz.so.1.2.11 libz.so.1  ; true
 	cd $(INSTALLDIR)/zlib/usr/lib ; ln -s libz.so.1.2.11 libz.so  ; true
 
+zlib-clean:
+	if [ -e "$(ZLIB_PKG_BUILD_DIR)/Makefile" ]; then $(MAKE) -C zlib clean ; fi
+	$(call CMakeClean,$(ZLIB_PKG_BUILD_DIR))
