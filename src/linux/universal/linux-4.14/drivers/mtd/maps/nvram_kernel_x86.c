91125aa4cc9c807e0cc9818d71023d77e2376d80
BrainSlayer <s.gottschall@dd-wrt.com>
2018-01-05 11:32:19 +0000

    fix api change
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@34391 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: addb5114789d4712f3c5b0898dbd76bfb77b9c18

--- src/linux/universal/linux-4.14/drivers/mtd/maps/nvram_kernel_x86.c
+++ src/linux/universal/linux-4.14/drivers/mtd/maps/nvram_kernel_x86.c
@@ -166,7 +166,8 @@ int _nvram_read(char *buf)
 	wr = buf;
 	offs = 0;
 	for (i = 0; i < (NVRAM_SPACE / PAGE_SIZE); i++) {
-		len = kernel_read(srcf,  srcf->f_pos + offs, wr, PAGE_SIZE);
+		loff_t offset = srcf->f_pos + offs;
+		len = kernel_read(srcf, wr, PAGE_SIZE, &offset);
 		offs += len;
 		wr += PAGE_SIZE;
 	}
