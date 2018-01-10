ca3559c8aa956e6a4eb86238a47879e3ca7b9a6a
ChristianScheele <scheele@newmedia-net.de>
2018-01-10 14:03:21 +0000

    Wireguard missing module
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@34422 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 40a956786ccce8d14bf5cad2d701f682d126ba3a

--- src/router/rules/wireguard.mk
+++ src/router/rules/wireguard.mk
@@ -4,18 +4,13 @@ export KERNELDIR:=$(LINUX_DIR)
 wireguard: 
 	cd $(WG_DIR)/src ; $(MAKE) -C $(LINUXDIR) M="$(TOP)/$(WG_DIR)/src"
 	cd $(WG_DIR)/src ; $(MAKE) LIBMNL_CFLAGS="-I$(TOP)/libmnl/include" LIBMNL_LDLIBS="-L$(TOP)/libmnl/src/.libs -lmnl" tools
-#	$(MAKE) M="$(WG_DIR)/src" tools
 
 wireguard-install:
-	@true
-	install -D libmnl/src/.libs/libmnl.so.0.2.0 $(INSTALLDIR)/libmnl/usr/lib/libmnl.so.0.2.0
-	cd $(INSTALLDIR)/libmnl/usr/lib ; ln -s libmnl.so.0.2.0 libmnl.so  ; true
-	cd $(INSTALLDIR)/libmnl/usr/lib ; ln -s libmnl.so.0.2.0 libmnl.so.0  ; true
+	install -D $(WG_DIR)/src/wireguard.ko $(INSTALLDIR)/wireguard/lib/modules/$(KERNELRELEASE)/wireguard.ko
+	install -D $(WG_DIR)/src/tools/wg $(INSTALLDIR)/wireguard/usr/bin/wg
 
 wireguard-configure:
-	#cd libmnl && ./configure --target=$(ARCH)-linux --host=$(ARCH) CC=$(ARCH)-linux-gcc 
-	true
-	#cd libmnl && ./configure --host=$(ARCH)-linux
+	@true
 
 wireguard-clean:
 	cd $(WG_DIR)/src ; $(MAKE) -C $(LINUXDIR) M="$(TOP)/$(WG_DIR)/src" clean
