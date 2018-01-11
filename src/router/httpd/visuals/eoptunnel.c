93bbead446ea4179e2f7734bef593ccfef1b8443
BrainSlayer <s.gottschall@dd-wrt.com>
2018-01-11 07:41:54 +0000

    typos
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@34452 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/eoptunnel.c
+++ src/router/httpd/visuals/eoptunnel.c
@@ -81,15 +81,15 @@ void ej_show_eop_tunnels(webs_t wp, int argc, char_t ** argv)
 		//public key show
 		sprintf(temp, "oet%d_public", tun);
 		websWrite(wp, "<div class=\"setting\">\n");
-		websWrite(wp, "<div class=\"label\"><script type=\"text/javascript\">Capture(eoip.wireguard_localkey)</script>:%s</div>\n",nvram_safe_get(temp));
-		websWrite(wp, "<input size=\"32\" maxlength=\"32\" name=\"%s\" value=\"%s\ disabled=\"true\"/>\n", temp, nvram_safe_get(temp));
+		websWrite(wp, "<div class=\"label\"><script type=\"text/javascript\">Capture(eoip.wireguard_localkey)</script></div>\n");
+		websWrite(wp, "<input size=\"32\" maxlength=\"32\" name=\"%s\" value=\"%s\" disabled=\"true\"/>\n", temp, nvram_safe_get(temp));
 		websWrite(wp, "</div>\n");
 
 		//public key peer input
 		sprintf(temp, "oet%d_peerkey", tun);
 		websWrite(wp, "<div class=\"setting\">\n");
-		websWrite(wp, "<div class=\"label\"><script type=\"text/javascript\">Capture(eoip.wireguard_peerkey)</script>:%s</div>\n",nvram_safe_get(temp));
-		websWrite(wp, "<input size=\"32\" maxlength=\"32\" name=\"%s\" value=\"%s\ disabled=\"true\"/>\n", temp, nvram_safe_get(temp));
+		websWrite(wp, "<div class=\"label\"><script type=\"text/javascript\">Capture(eoip.wireguard_peerkey)</script></div>\n");
+		websWrite(wp, "<input size=\"32\" maxlength=\"32\" name=\"%s\" value=\"%s\" disabled=\"true\"/>\n", temp, nvram_safe_get(temp));
 		websWrite(wp, "</div>\n");
 #endif
 		websWrite(wp, "</div>\n");
