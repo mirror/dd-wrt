6d9588fc4cd3c0b870ffaf3ae3d6eb4b5a7e18e4
BrainSlayer <s.gottschall@dd-wrt.com>
2018-01-11 01:29:54 +0000

    fix rules file. this is a versioning system. no need tor crazy useless dir names
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@34425 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/wireguard.mk
+++ src/router/rules/wireguard.mk
@@ -1,18 +1,15 @@
-WG_DIR=WireGuard-0.0.20171221
-export KERNELDIR:=$(LINUX_DIR)
-
 wireguard: 
-	cd $(WG_DIR)/src ; $(MAKE) -C $(LINUXDIR) M="$(TOP)/$(WG_DIR)/src"
-	cd $(WG_DIR)/src ; $(MAKE) LIBMNL_CFLAGS="-I$(TOP)/libmnl/include" LIBMNL_LDLIBS="-L$(TOP)/libmnl/src/.libs -lmnl" tools
+	cd wireguard/src ; $(MAKE) -C $(LINUXDIR) M="$(TOP)/wireguard/src" KERNELDIR=$(LINUX_DIR)
+	cd wireguard/src ; $(MAKE) LIBMNL_CFLAGS="-I$(TOP)/libmnl/include" LIBMNL_LDLIBS="-L$(TOP)/libmnl/src/.libs -lmnl" tools
 
 wireguard-install:
-	install -D $(WG_DIR)/src/wireguard.ko $(INSTALLDIR)/wireguard/lib/modules/$(KERNELRELEASE)/wireguard.ko
-	install -D $(WG_DIR)/src/tools/wg $(INSTALLDIR)/wireguard/usr/bin/wg
+	install -D wireguard/src/wireguard.ko $(INSTALLDIR)/wireguard/lib/modules/$(KERNELRELEASE)/wireguard.ko
+	install -D wireguard/src/tools/wg $(INSTALLDIR)/wireguard/usr/bin/wg
 
 wireguard-configure:
 	@true
 
 wireguard-clean:
-	cd $(WG_DIR)/src ; $(MAKE) -C $(LINUXDIR) M="$(TOP)/$(WG_DIR)/src" clean
-	cd $(WG_DIR)/src/tools ; $(MAKE) M="$(TOP)/$(WG_DIR)/src" clean
+	cd wireguard/src ; $(MAKE) -C $(LINUXDIR) M="$(TOP)/wireguard/src" KERNELDIR=$(LINUX_DIR) clean
+	cd wireguard/src/tools ; $(MAKE) M="$(TOP)/wireguard/src" clean
 
