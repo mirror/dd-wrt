475b14e2eed8e75fa252dbdba83f89fbe75b2bff
BrainSlayer <s.gottschall@dd-wrt.com>
2018-01-12 07:14:21 +0000

    format fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@34469 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 502fe2336b3aefd32adac945f8a60997df919dae

--- src/router/httpd/visuals/eoptunnel.c
+++ src/router/httpd/visuals/eoptunnel.c
@@ -89,7 +89,7 @@ void ej_show_eop_tunnels(webs_t wp, int argc, char_t ** argv)
 				{
 					websWrite(wp, "<div class=\"setting\">\n");
 					websWrite(wp, "<div class=\"label\"><script type=\"text/javascript\">Capture(eoip.wireguard_localport)</script></div>\n");
-					websWrite(wp, "<input type=\"number\" size=\"6\" maxlength=\"6\" class=\"num\" value=\"%d\" disabled=\"disabled\"/>\n", tun + 51819);
+					websWrite(wp, "<input size=\"5\" maxlength=\"5\" class=\"num\" value=\"%d\" disabled=\"disabled\"/>\n", tun + 51819);
 					websWrite(wp, "</div>\n");
 				}
 				//public key show
@@ -97,7 +97,7 @@ void ej_show_eop_tunnels(webs_t wp, int argc, char_t ** argv)
 				{
 					websWrite(wp, "<div class=\"setting\">\n");
 					websWrite(wp, "<div class=\"label\"><script type=\"text/javascript\">Capture(eoip.wireguard_localkey)</script></div>\n");
-					websWrite(wp, "<input type=\"text\" size=\"44\" maxlength=\"44\" name=\"%s\" value=\"%s\" disabled=\"disabled\"/>\n", temp, nvram_safe_get(temp));
+					websWrite(wp, "<input size=\"44\" maxlength=\"44\" name=\"%s\" value=\"%s\" disabled=\"disabled\"/>\n", temp, nvram_safe_get(temp));
 					websWrite(wp, "</div>\n");
 				}
 				//public key peer input
@@ -105,7 +105,7 @@ void ej_show_eop_tunnels(webs_t wp, int argc, char_t ** argv)
 				{
 					websWrite(wp, "<div class=\"setting\">\n");
 					websWrite(wp, "<div class=\"label\"><script type=\"text/javascript\">Capture(eoip.wireguard_peerkey)</script></div>\n");
-					websWrite(wp, "<input type=\"text\" size=\"44\" maxlength=\"44\" name=\"%s\" value=\"%s\" />\n", temp, nvram_safe_get(temp));
+					websWrite(wp, "<input size=\"44\" maxlength=\"44\" name=\"%s\" value=\"%s\" />\n", temp, nvram_safe_get(temp));
 					websWrite(wp, "</div>\n");
 				}
 #endif
