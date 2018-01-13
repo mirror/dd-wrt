f5a9d5dee30aa0661e3ed03d81e9a9a76106539f
BrainSlayer <s.gottschall@dd-wrt.com>
2018-01-13 00:56:17 +0000

    update iperf3
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@34492 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 671f57bb7102e6938c9eeca1da99b505fe9287e7

--- src/router/rules/iperf.mk
+++ src/router/rules/iperf.mk
@@ -1,6 +1,6 @@
 iperf-configure:
 	cd iperf && ./bootstrap.sh
-	cd iperf && ./configure --host=$(ARCH)-linux --disable-shared --prefix=/usr --libdir=/usr/lib CC="$(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -D_GNU_SOURCE -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_malloc=malloc" CPPFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_malloc=malloc"
+	cd iperf && ./configure --host=$(ARCH)-linux --disable-shared --without-openssl --prefix=/usr --libdir=/usr/lib CC="$(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -D_GNU_SOURCE -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_malloc=malloc" CPPFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_malloc=malloc"
 
 iperf:
 	$(MAKE) -C iperf/src iperf3
