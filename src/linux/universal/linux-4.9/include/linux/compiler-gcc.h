f9729a3909827ec6e486dbcee0a24b55e1eef7c2
BrainSlayer <s.gottschall@dd-wrt.com>
2018-01-25 11:35:37 +0000

    backport patch to older kernels
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@34646 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 0d39bd715496a72830554c60449c76c4e086f0b7

--- src/linux/universal/linux-4.9/include/linux/compiler-gcc.h
+++ src/linux/universal/linux-4.9/include/linux/compiler-gcc.h
@@ -68,8 +68,7 @@
  * Force always-inline if the user requests it so via the .config,
  * or if gcc is too old:
  */
-#if !defined(CONFIG_ARCH_SUPPORTS_OPTIMIZED_INLINING) ||		\
-    !defined(CONFIG_OPTIMIZE_INLINING) || (__GNUC__ < 4)
+#if !defined(CONFIG_OPTIMIZE_INLINING) || (__GNUC__ < 4)
 #define inline		inline		__attribute__((always_inline)) notrace
 #define __inline__	__inline__	__attribute__((always_inline)) notrace
 #define __inline	__inline	__attribute__((always_inline)) notrace
