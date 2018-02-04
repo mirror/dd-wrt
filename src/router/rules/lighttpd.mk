da3d035fea627990cc6e8430eede0880085bf3ae
BrainSlayer <s.gottschall@dd-wrt.com>
2018-02-04 19:17:57 +0000

    some aarch64 relevant changes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@34800 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 05b272681f61fd44b426bb79e3742dd6a4b5d639

--- src/router/rules/lighttpd.mk
+++ src/router/rules/lighttpd.mk
@@ -23,7 +23,7 @@ CONFIGURE_ARGS+= \
         --with-zlib
 
 lighttpd-configure: pcre-configure pcre openssl
-	cd lighttpd && ./configure --host=$(ARCH)-linux CC="ccache $(CC)" $(CONFIGURE_ARGS) CFLAGS="-fPIC -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/pcre -I$(TOP)/zlib" CPPFLAGS="$(COPTS) $(MIPS16_OPT)" LDFLAGS="-L$(TOP)/pcre/.libs -lpthread -lpcre -L$(TOP)/zlib $(LDFLAGS) -lz" PCRE_LIB="-lpcre" PCRECONFIG="true"
+	cd lighttpd && ./configure --host=$(ARCH)-linux $(CONFIGURE_ARGS) CFLAGS="-fPIC -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/pcre -I$(TOP)/zlib" CPPFLAGS="$(COPTS) $(MIPS16_OPT)" LDFLAGS="-L$(TOP)/pcre/.libs -lpthread -lpcre -L$(TOP)/zlib $(LDFLAGS) -lz" PCRE_LIB="-lpcre" PCRECONFIG="true"
 
 lighttpd: openssl
 	make -C lighttpd 
