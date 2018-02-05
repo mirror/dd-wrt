eaaf90b15731d5e5a445cc855f37e6e710c85834
BrainSlayer <s.gottschall@dd-wrt.com>
2018-02-05 21:56:29 +0000

    update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@34838 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: ef0a692cf3f1464efe8119a8ad223be3d69ff76b

--- src/router/rules/xfsprogs.mk
+++ src/router/rules/xfsprogs.mk
@@ -1,7 +1,8 @@
 xfsprogs-configure:
-	cd xfsprogs && ./configure --host=$(ARCH)-linux CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections  -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -I$(TOP)/e2fsprogs/lib -DNEED_PRINTF" LDFLAGS="-L$(TOP)/e2fsprogs/lib/uuid  -ffunction-sections -fdata-sections -Wl,--gc-sections" CC="$(CC) $(COPTS)" --disable-static --enable-shared --with-gnu-ld --disable-rpath --enable-gettext=no --enable-lib64=no root_prefix=$(INSTALLDIR)/xfsprogs
+	cd xfsprogs && ./configure --host=$(ARCH)-linux CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections  -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -I$(TOP)/e2fsprogs/lib -DNEED_PRINTF" LDFLAGS="-L$(TOP)/e2fsprogs/lib/uuid -luuid -ffunction-sections -fdata-sections -Wl,--gc-sections" CC="$(CC) $(COPTS)" --disable-static --enable-shared --with-gnu-ld --disable-rpath --enable-gettext=no --disable-blkid --enable-lib64=no root_prefix=$(INSTALLDIR)/xfsprogs
 
 xfsprogs:
+	make -C xfsprogs/libxfs crc32table.h
 	make -C xfsprogs DEBUG= Q= 
 
 
@@ -9,9 +10,10 @@ xfsprogs-clean:
 	make -C xfsprogs DEBUG= Q= clean
 
 xfsprogs-install:
-	-make -C xfsprogs install DESTDIR=$(INSTALLDIR)/e2fsprogs PKG_SBIN_DIR=$(INSTALLDIR)/e2fsprogs/usr/sbin PKG_ROOT_SBIN_DIR=$(INSTALLDIR)/e2fsprogs/sbin PKG_ROOT_LIB_DIR=$(INSTALLDIR)/e2fsprogs/lib PKG_LIB_DIR=$(INSTALLDIR)/e2fsprogs/usr/lib	\
-		PKG_MAN_DIR=$(INSTALLDIR)/xfsprogs/usr/man \
-		PKG_LOCALE_DIR=$(INSTALLDIR)/xfsprogs/usr/share/locale \
-		PKG_DOC_DIR=$(INSTALLDIR)/xfsprogs/usr/share/doc/xfsprogs
+	-make -C xfsprogs install DESTDIR=$(INSTALLDIR)/xfsprogs PKG_SBIN_DIR=/usr/sbin PKG_ROOT_SBIN_DIR=/sbin PKG_ROOT_LIB_DIR=/lib PKG_LIB_DIR=/usr/lib	\
+		PKG_MAN_DIR=/usr/man \
+		PKG_LOCALE_DIR=/usr/share/locale \
+		PKG_DOC_DIR=/usr/share/doc/xfsprogs
 	rm -rf $(INSTALLDIR)/xfsprogs/usr/share
 	rm -rf $(INSTALLDIR)/xfsprogs/usr/man
+	rm -rf $(INSTALLDIR)/xfsprogs/usr/include
