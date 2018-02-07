2f927c03ce09b330cf2035031b3ef319097c15f8
BrainSlayer <s.gottschall@dd-wrt.com>
2018-02-07 21:54:01 +0000

    strip put unwanted debug stuff
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@34873 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/dhcpv6/debug.h
+++ src/router/dhcpv6/debug.h
@@ -8,8 +8,16 @@
 /* First include stdio.h, which may contain the prototype for the external dprintf.
  * We do not want that. We redefine dprintf to our local implementation. */
 #include <stdio.h>
+
+#if 0 //def NEED_PRINTF
+
 #define dprintf(level, args...) \
 	( { if ((level) <= MAX_DEBUG_LEVEL) my_dprintf((level), args); } )
+
+
+#else
+#define dprintf(level, args...) do {} while(0)
+#endif
 
 #ifndef __P
 # define __P(x) x
