06b4f9a5b01613faa37cabfc696f2cc0fd84e59d
BrainSlayer <s.gottschall@dd-wrt.com>
2018-02-11 18:39:59 +0000

    reduce size
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@34923 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/opendpi/opendpi/lib/protocols/apple_push.c
+++ src/router/opendpi/opendpi/lib/protocols/apple_push.c
@@ -53,7 +53,7 @@ static void ndpi_check_apple_push(struct ndpi_detection_module_struct *ndpi_stru
 	NDPI_ADD_PROTOCOL_TO_BITMASK(flow->excluded_protocol_bitmask, NDPI_PROTOCOL_APPLE_PUSH);
 }
 
-void ndpi_search_apple_push(struct ndpi_detection_module_struct *ndpi_struct, struct ndpi_flow_struct *flow)
+static void ndpi_search_apple_push(struct ndpi_detection_module_struct *ndpi_struct, struct ndpi_flow_struct *flow)
 {
 	struct ndpi_packet_struct *packet = &flow->packet;
 
@@ -64,7 +64,7 @@ void ndpi_search_apple_push(struct ndpi_detection_module_struct *ndpi_struct, st
 		ndpi_check_apple_push(ndpi_struct, flow);
 }
 
-void init_apple_push_dissector(struct ndpi_detection_module_struct *ndpi_struct, u_int32_t *id, NDPI_PROTOCOL_BITMASK * detection_bitmask)
+static void init_apple_push_dissector(struct ndpi_detection_module_struct *ndpi_struct, u_int32_t *id, NDPI_PROTOCOL_BITMASK * detection_bitmask)
 {
 	ndpi_set_bitmask_protocol_detection("APPLE_PUSH", ndpi_struct, detection_bitmask, *id,
 					    NDPI_PROTOCOL_APPLE_PUSH, ndpi_search_apple_push, NDPI_SELECTION_BITMASK_PROTOCOL_TCP, SAVE_DETECTION_BITMASK_AS_UNKNOWN, ADD_TO_DETECTION_BITMASK);
