8bd191ef54fb9634c122ac47f60e134fcc814fef
BrainSlayer <s.gottschall@dd-wrt.com>
2018-02-16 13:09:57 +0000

    fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@35028 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 21cb730f6a9d905121983ba1b194b621b99301ed

--- src/linux/universal/linux-4.4/include/net/netfilter/nf_queue.h
+++ src/linux/universal/linux-4.4/include/net/netfilter/nf_queue.h
@@ -28,8 +28,8 @@ struct nf_queue_handler {
 						struct nf_hook_ops *ops);
 };
 
-void nf_register_queue_handler(const struct nf_queue_handler *qh);
-void nf_unregister_queue_handler(void);
+void nf_register_queue_handler(struct *net, const struct nf_queue_handler *qh);
+void nf_unregister_queue_handler(struct *net);
 void nf_reinject(struct nf_queue_entry *entry, unsigned int verdict);
 extern void nf_queue_entry_release_refs(struct nf_queue_entry *entry);
 
