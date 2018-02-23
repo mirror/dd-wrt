d4796adb442e2b7d8553c1cc7e5dd036a89b64e0
ChristianScheele <scheele@newmedia-net.de>
2018-02-23 02:11:17 +0000

    first shot of working web and file update for Ventana
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@35087 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rc/update-prepare.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+#
+# Working update script vor Ventana for use with web and file
+# for now only for MUSL
+#
+#
+if [ x$3 = xusefile ]
+then
+	UPDATEFILE=`basename $1`
+	FIFO=${UPDATEFILE}
+	USEFILE=1
+else
+	FIFO=`basename $1`
+	USEFILE=0
+fi
+
+MTDPART=$2
+
+copylibs(){
+P=$2
+# for musl, needs to be changed if not musl!!
+# uclibc we should use ldd() { LD_TRACE_LOADED_OBJECTS=1 $*; }
+#
+/tmp/ldd $1 | awk '{if (NF == 4) print $1" "$3}' | while read link file
+do
+		DIR=`dirname $file`
+		if [ ! -d ${DIR} ]
+		then
+			mkdir -p ${P}/${DIR}
+		fi
+		cp -a $file ${P}/${DIR}/
+		cp -a ${DIR}/${link} ${P}/${DIR}/
+done
+}
+R=/tmp/new_root/
+# for musl... fix me for other c-library
+ln -s /lib/libc.so /tmp/ldd
+# for pivot we need a new root, folder is not enough
+if [ x$3 != xnomount ]
+then
+	mkdir -p ${R}
+	mount -n -t tmpfs none $R
+	mkdir -p ${R}/tmp
+fi
+
+B=${R}/bin
+for i in etc bin lib usr/lib sbin proc dev sys usr/sbin oldroot
+do
+	mkdir -p ${R}/$i
+done
+#cp -a /lib/ld-musl-armhf.so.1 ${R}/lib
+for i in /bin/busybox /sbin/mtd /sbin/ledtool /usr/sbin/httpd
+do
+	cp $i $B
+	copylibs $i $R
+done
+#for httpd:
+for i in validate.so visuals.so
+do
+		cp /usr/lib/$i $R/usr/lib/
+done
+for i in sh mount umount sync ls cat reboot ps cp mv
+do
+	(cd $B ; ln -s busybox $i )
+done
+(cd $R/usr/sbin/ ; ln -s ../../bin/busybox chroot )
+(cd $R/sbin/ ; ln -s ../bin/busybox pivot_root )
+
+cp /usr/sbin/update-after-pivot.sh $R/bin
+cp /tmp/update-after-pivot.sh $R/bin
+cat /etc/passwd >$R/etc/passwd
+mount -t proc proc ${R}proc 
+mount -t sysfs sys ${R}sys 
+cp -av /dev ${R}/
+#not nice, but works.
+for i in /tmp/services/* ; do echo `basename $i .start` ; done | grep -v "httpd" | while read service ; do stopservice $service ; done
+killall process_monitor
+killall mstpd
+killall resetbutton
+if [ -f /tmp/${UPDATEFILE} ]
+then
+	mv /tmp/${UPDATEFILE} ${R}/tmp/
+fi
+cd ${R}
+pivot_root . oldroot
+exec chroot . /bin/sh /bin/update-after-pivot.sh ${FIFO} ${MTDPART} ${USEFILE} <dev/console >dev/console 2>&1
