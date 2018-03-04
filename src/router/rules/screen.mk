39f40448a05ecc1312ebaa16934120897eb1b202
BrainSlayer <s.gottschall@dd-wrt.com>
2018-03-04 00:30:30 +0000

    add some optimization
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@35210 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/screen.mk
+++ src/router/rules/screen.mk
@@ -1,5 +1,5 @@
 screen-configure:
-	cd screen && ./configure --host=$(ARCH)-linux --enable-colors256 CFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -I$(TOP)/ncurses/include" LDFLAGS="-L$(TOP)/ncurses/lib" --prefix=/usr ac_cv_safe_to_define___extensions__=no
+	cd screen && ./configure --host=$(ARCH)-linux --enable-colors256 CFLAGS="$(COPTS) $(MIPS16_OPT)  -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF -I$(TOP)/ncurses/include" LDFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/ncurses/lib" --prefix=/usr ac_cv_safe_to_define___extensions__=no
 
 screen: ncurses
 	make   -C screen
