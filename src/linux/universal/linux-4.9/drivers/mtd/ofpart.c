c521fc778b192963bf0499f1cbd8e10279620ebc
BrainSlayer <s.gottschall@dd-wrt.com>
2018-03-06 15:37:11 +0000

    backup space
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@35263 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: dc1a7ea85b6c64a5223333e738b4bc57be98439f

--- src/linux/universal/linux-4.9/drivers/mtd/ofpart.c
+++ src/linux/universal/linux-4.9/drivers/mtd/ofpart.c
@@ -82,7 +82,7 @@ static int parse_ofpart_partitions(struct mtd_info *master,
 		nr_parts++;
 	}
 	#ifdef CONFIG_SOC_IMX6
-		nr_parts++; // for nvram
+		nr_parts+=2; // for nvram
 	#endif
 
 	if (nr_parts == 0)
@@ -150,9 +150,14 @@ static int parse_ofpart_partitions(struct mtd_info *master,
 			parts[i].size -= 0x80000;
 			i++;
 			parts[i].offset = parts[i-1].offset + parts[i-1].size;
-			parts[i].size = 0x80000;	
+			parts[i].size = 0x40000;	
 			parts[i].mask_flags = 0;	
 			parts[i].name = "nvram";
+			i++;
+			parts[i].offset = parts[i-1].offset + parts[i-1].size;
+			parts[i].size = 0x40000;	
+			parts[i].mask_flags = 0;	
+			parts[i].name = "mampf";
 		}    
 		#endif
 		i++;
