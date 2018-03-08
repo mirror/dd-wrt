ac1672948cf4bc139f2e88c3f50a1e1db2158939
BrainSlayer <s.gottschall@dd-wrt.com>
2018-03-08 16:31:39 +0000

    space api
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@35281 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/tools/reset_defaults.c
+++ src/router/services/tools/reset_defaults.c
@@ -34,22 +34,13 @@ extern struct nvram_param *srouter_defaults;
 extern void load_defaults(void);
 extern void free_defaults(void);
 
-#ifdef NVRAM_SPACE_256
-#define NVRAMSPACE NVRAM_SPACE_256
-#elif HAVE_NVRAM_128
-#define NVRAMSPACE 0x20000
-#elif HAVE_MVEBU
-#define NVRAMSPACE 0x10000
-#elif HAVE_IPQ806X
-#define NVRAMSPACE 0x10000
-#else
-#define NVRAMSPACE NVRAM_SPACE
-#endif
 
 extern int nvram_critical(char *name);
 
+int nvram_size(void);
 void start_defaults(void)
 {
+	int NVRAMSPACE = nvram_size();
 	fprintf(stderr, "restore nvram to defaults\n");
 	char *buf = (char *)malloc(NVRAMSPACE);
 	int i;
