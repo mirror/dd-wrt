39c48e5f3d1b07a71fba57aefbcd7afad8b48a6b
BrainSlayer <s.gottschall@dd-wrt.com>
2018-03-08 17:29:08 +0000

    use api for size
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@35290 52c4871e-980c-0410-b1e0-e73912ce01f8
    
    Former-commit-id: 385ca5d41538dfc88b4e51bd483f6599e99051c4

--- src/router/services/sysinit/tools/recover.c
+++ src/router/services/sysinit/tools/recover.c
@@ -10,16 +10,17 @@ void start_backup(void)
 #endif
 	//backup nvram
 	fprintf(stderr, "backup nvram\n");
+	int size = nvram_size();
 	FILE *in = fopen("/usr/local/nvram/nvram.bin", "rb");
 	if (in) {
-		char *mem = malloc(65536 * 2);
-		fread(mem, 65536 * 2, 1, in);
+		char *mem = malloc(size);
+		fread(mem, size, 1, in);
 		fclose(in);
 		in = fopen(drive, "r+b");
 		fseeko(in, 0, SEEK_END);
 		off_t mtdlen = ftello(in);
-		fseeko(in, mtdlen - (65536 * 3), SEEK_SET);
-		fwrite(mem, 65536 * 2, 1, in);
+		fseeko(in, mtdlen - (size + 65536), SEEK_SET);
+		fwrite(mem, size, 1, in);
 		fflush(in);
 		fsync(fileno(in));
 		fclose(in);
@@ -40,12 +41,13 @@ void start_recover(void)
 #else
 	sprintf(dev, "/dev/%s", getdisc());
 #endif
+	int size = nvram_size();
 	in = fopen(dev, "rb");
 	fseeko(in, 0, SEEK_END);
 	off_t mtdlen = ftello64(in);
-	fseeko(in, mtdlen - (65536 * 3), SEEK_SET);
+	fseeko(in, mtdlen - (size + 65536), SEEK_SET);
 
-	unsigned char *mem = malloc(65536 * 2);
+	unsigned char *mem = malloc(size);
 	fread(mem, 65536 * 2, 1, in);
 	if (mem[0] != 0x46 || mem[1] != 0x4c || mem[2] != 0x53 || mem[3] != 0x48) {
 		fseeko(in, mtdlen - (65536 * 2), SEEK_SET);
@@ -56,7 +58,7 @@ void start_recover(void)
 		fprintf(stderr, "found recovery\n");
 		in = fopen("/usr/local/nvram/nvram.bin", "wb");
 		if (in != NULL) {
-			fwrite(mem, 65536 * 2, 1, in);
+			fwrite(mem, size, 1, in);
 			fclose(in);
 		}
 	}
