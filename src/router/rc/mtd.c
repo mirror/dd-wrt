355877b4359bc8abc528e9d57244064fd9d80aea
kong <USER@DOMAIN>
2018-03-27 18:49:10 +0000

    warn user about deprecated command and redirect to proper command
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@35548 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/mtd.c
+++ src/router/rc/mtd.c
@@ -155,6 +155,13 @@ int mtd_erase(const char *mtd)
 	int mtd_fd;
 	struct mtd_info_user mtd_info;
 	struct erase_info_user erase_info;
+	
+	if(!strcmp(mtd, "nvram"))
+	{
+		fprintf(stderr, "erase nvram command is deprecated, please use nvram erase\n");
+		sysprintf("nvram erase");
+		return 0;
+	}
 
 	/* 
 	 * char *et0; char *et1;
