78816d6230441667f936085ed916d2c2c367ec78
BrainSlayer <s.gottschall@dd-wrt.com>
2018-03-28 10:59:57 +0000

    change copyright dates
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@35569 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/validate/wepkey.c
+++ src/router/httpd/validate/wepkey.c
@@ -1,7 +1,23 @@
 /*
- * 
- * Arguments: genstr - a null terminated string wep_key - a 2d array that is
- * filled with the wep keys Returns: nothing 
+ * wepkey.c
+ *
+ * Copyright (C) 2005 - 2018 Sebastian Gottschall <sebastian.gottschall@newmedia-net.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id:
  */
 
 #include <string.h>
