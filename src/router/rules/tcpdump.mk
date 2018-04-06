205c9612f80d147fa2d2acb0b9c348ef872d4b62
BrainSlayer <s.gottschall@dd-wrt.com>
2018-04-06 21:27:43 +0000

    make it unique
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@35689 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/tcpdump.mk
+++ src/router/rules/tcpdump.mk
@@ -1,11 +1,11 @@
 ifeq ($(CONFIG_IPV6),y)
-export EXTRA:=--enable-ipv6
+export TCPDUMPEXTRA:=--enable-ipv6
 endif
 
 
 
 tcpdump-configure: libpcap
-	cd tcpdump && ./configure --host=$(ARCH)-linux --enable-shared --libdir=/lib --disable-static --without-crypto $(EXTRA) CC="$(CC)" ac_cv_linux_vers=2 ac_cv_ssleay_path=no td_cv_buggygetaddrinfo=no CPPFLAGS="-I../libpcap $(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE -DNEED_PRINTF" CFLAGS="-I../libpcap $(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -DHAVE_BPF_DUMP" LDFLAGS="-L../libpcap"
+	cd tcpdump && ./configure --host=$(ARCH)-linux --enable-shared --libdir=/lib --disable-static --without-crypto $(TCPDUMPEXTRA) CC="$(CC)" ac_cv_linux_vers=2 ac_cv_ssleay_path=no td_cv_buggygetaddrinfo=no CPPFLAGS="-I../libpcap $(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE -DNEED_PRINTF" CFLAGS="-I../libpcap $(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -DHAVE_BPF_DUMP" LDFLAGS="-L../libpcap"
 
 tcpdump:
 	make -j 4 -C tcpdump
