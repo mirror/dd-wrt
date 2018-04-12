b232decdea21701f6b117e8e26f8e198f388f460
BrainSlayer <s.gottschall@dd-wrt.com>
2018-04-12 08:00:35 +0000

    fix arm big endian support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@35710 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/icu/i18n/double-conversion-utils.h
+++ src/router/icu/i18n/double-conversion-utils.h
@@ -77,7 +77,7 @@ inline void abort_noreturn() { abort(); }
 // On Linux,x86 89255e-22 != Div_double(89255.0/1e22)
 // ICU PATCH: Enable ARM builds for Windows with 'defined(_M_ARM)'.
 #if defined(_M_X64) || defined(__x86_64__) || \
-    defined(__ARMEL__) || defined(__avr32__) || defined(_M_ARM) || \
+    defined(__ARMEL__) || defined(__ARMEB__) || defined(__avr32__) || defined(_M_ARM) || \
     defined(__hppa__) || defined(__ia64__) || \
     defined(__mips__) || \
     defined(__powerpc__) || defined(__ppc__) || defined(__ppc64__) || \
