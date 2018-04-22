e4a7d8c2a84a7b147e709274f39501e5979d1cca
BrainSlayer <s.gottschall@dd-wrt.com>
2018-04-22 11:31:04 +0000

    update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@35788 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.14/include/linux/platform_data/phy-at803x.h
+++ src/linux/universal/linux-4.14/include/linux/platform_data/phy-at803x.h
@@ -6,6 +6,8 @@ struct at803x_platform_data {
 	int enable_rgmii_tx_delay:1;
 	int enable_rgmii_rx_delay:1;
 	int fixup_rgmii_tx_delay:1;
+	int has_reset_gpio:1;
+	int reset_gpio;
 };
 
 #endif /* _PHY_AT803X_PDATA_H */
