9111ddcaaa115408a5500f71c08b5cf73416a645
BrainSlayer <s.gottschall@dd-wrt.com>
2018-05-13 15:25:09 +0000

    fix mc build
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@35944 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/mc.mk
+++ src/router/rules/mc.mk
@@ -33,7 +33,7 @@ mc-configure: ncurses
 
 	cd mc2 && ./configure --host=$(ARCH)-uclibc-linux AWK="awk" \
 		CFLAGS="$(COPTS)  $(MIPS16_OPT) -DNEED_PRINTF -DSTAT_STATVFS -I$(TOP)/mc2/slang/src -I$(TOP)/ncurses/include" \
-		LDFLAGS="-L$(TOP)/ncurses/lib -L$(TOP)/mc2/slang/src/elf$(ARCH)objs -lncurses" \
+		LDFLAGS="-L$(TOP)/ncurses/lib -L$(TOP)/mc2/intl -lintl -L$(TOP)/mc2/slang/src/elf$(ARCH)objs -lncurses" \
 		GLIB_CFLAGS="-I$(TOP)/glib20/libglib/glib -I$(TOP)/glib20/libglib -L$(INSTALLDIR)/util-linux/usr/lib" \
 		GLIB_LIBS="-L$(TOP)/glib20/libglib/glib/.libs -lglib-2.0" \
 		GMODULE_CFLAGS="-pthread -I$(TOP)/glib20/libglib/gmodule -I$(TOP)/glib20/libglib/glib -I$(TOP)/glib20/libglib" \
