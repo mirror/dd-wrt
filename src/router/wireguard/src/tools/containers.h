92072871833e79c9533e2b9420a7bcc94879d5f5
BrainSlayer <s.gottschall@dd-wrt.com>
2018-05-20 12:15:32 +0000

    update tools
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@35974 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/wireguard/src/tools/containers.h
+++ src/router/wireguard/src/tools/containers.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0
  *
- * Copyright (C) 2015-2017 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
+ * Copyright (C) 2015-2018 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
  */
 
 #ifndef CONTAINERS_H
@@ -78,7 +78,6 @@ struct wgdevice {
 
 #define for_each_wgpeer(__dev, __peer) for ((__peer) = (__dev)->first_peer; (__peer); (__peer) = (__peer)->next_peer)
 #define for_each_wgallowedip(__peer, __allowedip) for ((__allowedip) = (__peer)->first_allowedip; (__allowedip); (__allowedip) = (__allowedip)->next_allowedip)
-#define max(a, b) ((a) > (b) ? (a) : (b))
 
 static inline void free_wgdevice(struct wgdevice *dev)
 {
