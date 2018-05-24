ff576295ecabb9808246b4a7fefddb5760c05420
BrainSlayer <s.gottschall@dd-wrt.com>
2018-05-24 14:35:51 +0000

    update kernels
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36020 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4/net/core/sock.c
+++ src/linux/universal/linux-4.4/net/core/sock.c
@@ -1475,7 +1475,7 @@ void sk_destruct(struct sock *sk)
 static void __sk_free(struct sock *sk)
 {
 #ifdef CONFIG_SOCK_DIAG
-	if (unlikely(sock_diag_has_destroy_listeners(sk) && sk->sk_net_refcnt))
+	if (unlikely(sk->sk_net_refcnt && sock_diag_has_destroy_listeners(sk)))
 		sock_diag_broadcast_destroy(sk);
 	else
 #endif
