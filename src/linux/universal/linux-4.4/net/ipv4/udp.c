63c5a77f47d2e67198345dc2ae3d59ecd4edb7d8
BrainSlayer <s.gottschall@dd-wrt.com>
2018-05-24 14:35:51 +0000

    update kernels
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36020 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4/net/ipv4/udp.c
+++ src/linux/universal/linux-4.4/net/ipv4/udp.c
@@ -1001,8 +1001,10 @@ int udp_sendmsg(struct sock *sk, struct msghdr *msg, size_t len)
 	ipc.addr = faddr = daddr;
 
 	if (ipc.opt && ipc.opt->opt.srr) {
-		if (!daddr)
-			return -EINVAL;
+		if (!daddr) {
+			err = -EINVAL;
+			goto out_free;
+		}
 		faddr = ipc.opt->opt.faddr;
 		connected = 0;
 	}
@@ -1115,6 +1117,7 @@ do_append_data:
 
 out:
 	ip_rt_put(rt);
+out_free:
 	if (free)
 		kfree(ipc.opt);
 	if (!err)
