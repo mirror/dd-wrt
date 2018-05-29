a53af3225792441ee4bdcc6beacc63aae423c1e9
BrainSlayer <s.gottschall@dd-wrt.com>
2018-05-29 08:14:59 +0000

    fix northstar build. was broken. 36056 removed
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36066 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4/arch/arm/include/asm/assembler.h
+++ src/linux/universal/linux-4.4/arch/arm/include/asm/assembler.h
@@ -529,4 +529,13 @@ THUMB(	orr	\reg , \reg , #PSR_T_BIT	)
 	.popsection
 #endif
 	.endm
+#ifdef CONFIG_KPROBES
+#define _ASM_NOKPROBE(entry)				\
+	.pushsection "_kprobe_blacklist", "aw" ;	\
+	.balign 4 ;					\
+	.long entry;					\
+	.popsection
+#else
+#define _ASM_NOKPROBE(entry)
+#endif
 #endif /* __ASM_ASSEMBLER_H__ */
