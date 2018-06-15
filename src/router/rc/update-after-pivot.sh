6100ca28a839b5fcb79b4e02e7ac6a04c79656b3
BrainSlayer <s.gottschall@dd-wrt.com>
2018-06-15 08:14:58 +0000

    fork flash process, otherwise flashing may not work
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36148 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rc/update-after-pivot.sh
@@ -0,0 +1,35 @@
+#!/bin/sh -x
+if [ x$1 = x ]
+then
+	echo "No file/fifo given, exit"
+	exit
+fi
+if [ x$2 = x ]
+then
+	echo "No mtd partition given, exit"
+	exit
+fi
+
+FIFO=$1
+MTDPART=$2
+
+/bin/busybox mount -o remount,ro /oldroot/
+umount /oldroot/sys/kernel/debug/
+umount /oldroot/sys
+umount /oldroot/dev/pts
+umount /oldroot/proc
+umount -l /oldroot
+cd /tmp
+if [ x$4 = x1 ]
+then
+	dd if=${FIFO} of=${MTDPART} bs=65536 conv=fsync
+else
+	write ${FIFO} ${MTDPART}
+fi
+busybox sync
+busybox sync
+busybox sync
+if [ x$3 = x1 ]
+then
+	busybox reboot
+fi
