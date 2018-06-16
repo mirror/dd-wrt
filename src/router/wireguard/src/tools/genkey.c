b48e1e9084b253cf8a742f125a89633db029715a
BrainSlayer <s.gottschall@dd-wrt.com>
2018-06-16 09:30:00 +0000

    sync wireguard with latest git
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36157 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/wireguard/src/tools/genkey.c
+++ src/router/wireguard/src/tools/genkey.c
@@ -13,6 +13,15 @@
 #ifdef __linux__
 #include <sys/syscall.h>
 #endif
+#ifdef __APPLE__
+#include <AvailabilityMacros.h>
+#ifndef MAC_OS_X_VERSION_10_12
+#define MAC_OS_X_VERSION_10_12 101200
+#endif
+#if MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_12
+#include <sys/random.h>
+#endif
+#endif
 
 #include "curve25519.h"
 #include "encoding.h"
@@ -22,11 +31,19 @@ static inline ssize_t get_random_bytes(uint8_t *out, size_t len)
 {
 	ssize_t ret;
 	int fd;
+
+#if defined(__OpenBSD__) || (defined(__APPLE__) && MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_12) || (defined(__GLIBC__) && (__GLIBC__ > 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 25)))
+	ret = getentropy(out, len);
+	if (!ret)
+		return len;
+#endif
+
 #if defined(__NR_getrandom) && defined(__linux__)
 	ret = syscall(__NR_getrandom, out, len, 0);
 	if (ret >= 0)
 		return ret;
 #endif
+
 	fd = open("/dev/urandom", O_RDONLY);
 	if (fd < 0)
 		return fd;
