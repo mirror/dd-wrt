5b522c8bc8379c7e3cd3351ccd3cd05948e35a90
BrainSlayer <s.gottschall@dd-wrt.com>
2018-06-24 22:22:12 +0000

    must provide pid file
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36226 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/minidlna.c
+++ src/router/services/services/minidlna.c
@@ -125,7 +125,7 @@ void start_dlna(void)
 void start_dlna_rescan(void)
 {
 	if (nvram_match("dlna_enable", "1") && nvram_match("dlna_rescan", "1"))
-		eval("minidlna", "-U");
+		eval("minidlna", "-P", "/var/run/minidlna/minidlna.pid", "-U");
 }
 
 void stop_dlna(void)
