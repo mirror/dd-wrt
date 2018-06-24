5d52bedc1310a85482dceaa7d3e7bdaebe51a47e
BrainSlayer <s.gottschall@dd-wrt.com>
2018-06-24 22:05:29 +0000

    rescan in a defined interval
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36218 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/dd-wrt/lang_pack/german.js
+++ src/router/kromo/dd-wrt/lang_pack/german.js
@@ -759,7 +759,7 @@ nas.dlna_subtitles="Verwende Untertitel";
 nas.dlna_dir="DLNA Verzeichnis";
 nas.dlna_cleandb="Datenbank jetzt leeren";
 nas.dlna_metadata="Metadatan Behalten";
-
+nas.dlna_rescan="Zyklisches neu Scannen";
 //** Privoxy.asp **//
 privoxy.titl="Werbefilter";
 privoxy.server="Privoxy";
