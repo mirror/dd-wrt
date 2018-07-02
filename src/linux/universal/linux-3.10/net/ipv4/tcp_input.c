152f5083fcf8df7760f128a5b31204b4aafd673d
BrainSlayer <s.gottschall@dd-wrt.com>
2018-07-02 13:40:28 +0000

    update kernels
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36252 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.10/net/ipv4/tcp_input.c
+++ src/linux/universal/linux-3.10/net/ipv4/tcp_input.c
@@ -563,7 +563,7 @@ void tcp_rcv_space_adjust(struct sock *sk)
 				sk->sk_rcvbuf = space;
 
 				/* Make the window clamp follow along.  */
-				tp->window_clamp = new_clamp;
+				tp->window_clamp = tcp_win_from_space(space);
 			}
 		}
 	}
