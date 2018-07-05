da0ffbe229267e9c09852ff7f270266647a6a3cf
BrainSlayer <s.gottschall@dd-wrt.com>
2018-07-05 07:45:51 +0000

    backport getrawmonotonic64
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36266 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/include/linux/timekeeping.h
+++ src/linux/universal/linux-3.18/include/linux/timekeeping.h
@@ -26,7 +26,7 @@ struct timespec __current_kernel_time(void);
  * timespec based interfaces
  */
 struct timespec get_monotonic_coarse(void);
-extern void getrawmonotonic(struct timespec *ts);
+extern void getrawmonotonic64(struct timespec64 *ts);
 extern void ktime_get_ts64(struct timespec64 *ts);
 
 extern int __getnstimeofday64(struct timespec64 *tv);
@@ -51,6 +51,10 @@ static inline void ktime_get_ts(struct timespec *ts)
 static inline void ktime_get_real_ts(struct timespec *ts)
 {
 	getnstimeofday64(ts);
+}
+static inline void getrawmonotonic(struct timespec *ts)
+{
+	getrawmonotonic64(ts);
 }
 
 #else
@@ -84,6 +88,13 @@ static inline void ktime_get_real_ts(struct timespec *ts)
 	struct timespec64 ts64;
 
 	getnstimeofday64(&ts64);
+	*ts = timespec64_to_timespec(ts64);
+}
+static inline void getrawmonotonic(struct timespec *ts)
+{
+	struct timespec64 ts64;
+
+	getrawmonotonic64(&ts64);
 	*ts = timespec64_to_timespec(ts64);
 }
 #endif
