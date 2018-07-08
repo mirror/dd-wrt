ae9de43ec60e593623ff939f0c3077d964593ef6
BrainSlayer <s.gottschall@dd-wrt.com>
2018-07-08 18:33:39 +0000

    fix out of bound
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36278 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/java/classpath/native/jni/java-lang/java_lang_VMProcess.c
+++ src/router/java/classpath/native/jni/java-lang/java_lang_VMProcess.c
@@ -209,7 +209,7 @@ Java_java_lang_VMProcess_nativeSpawn (JNIEnv * env, jobject this,
   err = cpproc_forkAndExec(strings, newEnviron, fds, pipe_count, &pid, dir);
   if (err != 0)
     {
-      strncpy(errbuf, cpnative_getErrorString (err), sizeof(errbuf));
+      strncpy(errbuf, cpnative_getErrorString (err), sizeof(errbuf) - 1);
       goto system_error;
     }
 
