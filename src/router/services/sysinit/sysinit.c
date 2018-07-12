8f61bc763d74f07c5dfb1e3e4fab6914267a9efc
BrainSlayer <s.gottschall@dd-wrt.com>
2018-07-12 07:05:37 +0000

    fix usb leds for wrt32x
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36304 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit.c
+++ src/router/services/sysinit/sysinit.c
@@ -3185,9 +3185,9 @@ void start_drivers(void)
 	}
 
 	if (brand == ROUTER_WRT_32X) {
-		set_led_usbport("pca963x\\:venom\\:white\\:usb3_2", "usb2-port1");
-		set_led_usbport("pca963x\\:venom\\:white\\:usb3_1", "usb2-port1 usb1-port1");
-		set_led_usbport("pca963x\\:venom\\:white\\:usb2", "usb3-port1");
+		set_led_usbport("pca963x\\:venom\\:blue\\:usb3_2", "usb2-port1");
+		set_led_usbport("pca963x\\:venom\\:blue\\:usb3_1", "usb2-port1 usb1-port1");
+		set_led_usbport("pca963x\\:venom\\:blue\\:usb2", "usb3-port1");
 
 	}
 #endif
