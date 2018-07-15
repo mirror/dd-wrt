0e937a7d0c4136f157088ee409528b30e99e9f2f
BrainSlayer <s.gottschall@dd-wrt.com>
2018-07-15 12:07:15 +0000

    update nmap
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36316 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/nmap/nselib/http.lua
+++ src/router/nmap/nselib/http.lua
@@ -102,6 +102,10 @@
 -- that should be pipelined. Defaults to <code>http.pipeline</code> (if set), or to what
 -- <code>getPipelineMax</code> function returns.
 --
+-- @args http.host The value to use in the Host header of all requests unless
+-- otherwise set. By default, the Host header uses the output of
+-- <code>stdnse.get_hostname()</code>.
+
 -- TODO
 -- Implement cache system for http pipelines
 --
@@ -130,6 +134,7 @@ _ENV = stdnse.module("http", stdnse.seeall)
 local have_ssl, openssl = pcall(require,'openssl')
 
 USER_AGENT = stdnse.get_script_args('http.useragent') or "Mozilla/5.0 (compatible; Nmap Scripting Engine; https://nmap.org/book/nse.html)"
+local host_header = stdnse.get_script_args('http.host')
 local MAX_REDIRECT_COUNT = 5
 
 -- Recursively copy a table.
@@ -166,9 +171,10 @@ local get_default_port = url.get_default_port
 --- Get a value suitable for the Host header field.
 -- See RFC 2616 sections 14.23 and 5.2.
 local function get_host_field(host, port)
+  if host_header then return host_header end
   if not host then return nil end
   if type(port) == "number" then
-    port = {number=port, protocol="tcp", state="open", version={}}
+    port = {number=port, protocol="tcp", state="open"}
   end
   local scheme = shortport.ssl(host, port) and "https" or "http"
   if port.number == get_default_port(scheme) then
@@ -657,7 +663,7 @@ end
 local function parse_status_line(status_line, response)
   response["status-line"] = status_line
   local version, status, reason_phrase = string.match(status_line,
-    "^HTTP/(%d+%.%d+) +(%d+) +(.-)\r?\n$")
+    "^HTTP/(%d+%.%d+) +(%d+)%f[ \r\n] *(.-)\r?\n$")
   if not version then
     return nil, string.format("Error parsing status-line %q.", status_line)
   end
@@ -2819,15 +2825,17 @@ test_suite = unittest.TestSuite:new()
 
 do
   local cookie_tests = {
-    { "#1198: conflicting attribute name",
-      "JSESSIONID=aaa; name=bbb; value=ccc; attr=ddd", {
+    { name = "#1198: conflicting attribute name",
+      cookie = "JSESSIONID=aaa; name=bbb; value=ccc; attr=ddd",
+      parsed = {
         name = "JSESSIONID",
         value = "aaa",
         attr = "ddd",
       }
     },
-    { "#1171: empty attribute value",
-      "JSESSIONID=aaa; attr1; attr2=; attr3=", {
+    { name = "#1171: empty attribute value",
+      cookie = "JSESSIONID=aaa; attr1; attr2=; attr3=",
+      parsed = {
         name = "JSESSIONID",
         value = "aaa",
         attr1 = "",
@@ -2835,44 +2843,50 @@ do
         attr3 = "",
       }
     },
-    { "#1170: quotes present",
-      "aaa=\"b\\\"bb\"; pATH = \"ddd eee\" fff", {
+    { name = "#1170: quotes present",
+      cookie = "aaa=\"b\\\"bb\"; pATH = \"ddd eee\" fff",
+      parsed = {
         name = "aaa",
         value = "\"b\\\"bb\"",
         path = "\"ddd eee\" fff"
       }
     },
-    { "#1169: empty attributes",
-      "JSESSIONID=aaa; ; Path=/;;Secure;", {
+    { name = "#1169: empty attributes",
+      cookie = "JSESSIONID=aaa; ; Path=/;;Secure;",
+      parsed = {
         name = "JSESSIONID",
         value = "aaa",
         path = "/",
         secure = ""
       }
     },
-    { "#844: space in a cookie value",
-      " SESSIONID = IgAAABjN8b3xxxNsLRIiSpHLPn1lE=&IgAAAxxxMT6Bw==&Huawei USG6320&langfrombrows=en-US&copyright=2014 ;secure", {
+    { name = "#844: space in a cookie value",
+      cookie = " SESSIONID = IgAAABjN8b3xxxNsLRIiSpHLPn1lE=&IgAAAxxxMT6Bw==&Huawei USG6320&langfrombrows=en-US&copyright=2014 ;secure",
+      parsed = {
         name = "SESSIONID",
         value = "IgAAABjN8b3xxxNsLRIiSpHLPn1lE=&IgAAAxxxMT6Bw==&Huawei USG6320&langfrombrows=en-US&copyright=2014",
         secure = ""
       }
     },
-    { "#866: unexpected attribute",
-      " SID=c98fefa3ad659caa20b89582419bb14f; Max-Age=1200; Version=1", {
+    { name = "#866: unexpected attribute",
+      cookie = " SID=c98fefa3ad659caa20b89582419bb14f; Max-Age=1200; Version=1",
+      parsed = {
         name = "SID",
         value = "c98fefa3ad659caa20b89582419bb14f",
         ["max-age"] = "1200",
         version = "1"
       }
     },
-    { "#731: trailing semicolon",
-      "session_id=76ca8bc8c19;", {
+    { name = "#731: trailing semicolon",
+      cookie = "session_id=76ca8bc8c19;",
+      parsed = {
         name = "session_id",
         value = "76ca8bc8c19"
       }
     },
-    { "#229: comma is not a delimiter",
-      "c1=aaa; path=/bbb/ccc,ddd/eee", {
+    { name = "#229: comma is not a delimiter",
+      cookie = "c1=aaa; path=/bbb/ccc,ddd/eee",
+      parsed = {
         name = "c1",
         value = "aaa",
         path = "/bbb/ccc,ddd/eee"
@@ -2881,12 +2895,64 @@ do
   }
 
   for _, test in ipairs(cookie_tests) do
-    local parsed = parse_set_cookie(test[2])
-    test_suite:add_test(unittest.not_nil(parsed), test[1])
+    local parsed = parse_set_cookie(test.cookie)
+    test_suite:add_test(unittest.not_nil(parsed), test.name)
     if parsed then
-      test_suite:add_test(unittest.keys_equal(parsed, test[3]), test[1])
+      test_suite:add_test(unittest.keys_equal(parsed, test.parsed), test.name)
     end
   end
+
+  local status_line_tests = {
+    { name = "valid status line",
+      line = "HTTP/1.0 200 OK\r\n",
+      result = true,
+      parsed = {
+        version = "1.0",
+        status = 200,
+      }
+    },
+    { name = "malformed version in status line",
+      line = "HTTP/1. 200 OK\r\n",
+      result = false,
+      parsed = {
+        version = nil,
+        status = nil,
+      }
+    },
+    { name = "non-integer status code in status line",
+      line = "HTTP/1.0 20A OK\r\n",
+      result = false,
+      parsed = {
+        version = "1.0",
+        status = nil,
+      }
+    },
+    { name = "missing reason phrase in status line",
+      line = "HTTP/1.0 200\r\n",
+      result = true,
+      parsed = {
+        version = "1.0",
+        status = 200,
+      }
+    },
+  }
+
+  for _, test in ipairs(status_line_tests) do
+    local response = {}
+    local result, error = parse_status_line(test.line, response)
+    if test.result then
+      test_suite:add_test(unittest.not_nil(result), test.name)
+    else
+      test_suite:add_test(unittest.is_nil(result), test.name)
+      test_suite:add_test(unittest.not_nil(error), test.name)
+    end
+    test_suite:add_test(unittest.equal(response["status-line"], test.line), test.name)
+    if result then
+      test_suite:add_test(unittest.equal(response.status, test.parsed.status), test.name)
+      test_suite:add_test(unittest.equal(response.version, test.parsed.version), test.name)
+    end
+  end
+
 end
 
 return _ENV;
