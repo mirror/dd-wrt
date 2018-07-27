289cbee4145b438bbbcf4792ae4a2bfaf97531e3
BrainSlayer <s.gottschall@dd-wrt.com>
2018-07-27 07:12:31 +0000

    this is obsolete. do not use it anymore
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36405 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/quagga.mk
+++ src/router/rules/quagga.mk
@@ -1,29 +1,15 @@
 quagga-configure: ncurses
-ifeq ($(CONFIG_QUAGGA_STABLE),y)
-	cd quagga-stable/readline && ./configure --host=$(ARCH)-uclibc-linux --prefix=/usr CFLAGS="$(COPTS) $(MIPS16_OPT) -fPIC -fno-strict-aliasing"
-	$(MAKE) -C quagga-stable/readline clean all
-	cd quagga-stable && ./configure --host=$(ARCH)-uclibc-linux $(CONFIG_QUAGGA_EXTRA) --libdir=/usr/lib --enable-opaque-lsa --enable-ospf-te --disable-ospfclient --enable-multipath=32  --enable-ipv6 --prefix=/usr --disable-ospf6d  --enable-vtysh --enable-user=root --enable-group=root --disable-ospfapi --with-libreadline=$(TOP)/quagga-stable/readline CFLAGS="-fno-strict-aliasing -I$(TOP)/quagga-stable -Drpl_malloc=malloc -Drpl_realloc=realloc $(COPTS)  $(MIPS16_OPT)" LDFLAGS="-L$(TOP)/quagga-stable/readline -L$(TOP)/ncurses/lib -lncurses" 
-else
 	cd quagga && autoreconf --force --install
 	cd quagga/readline && ./configure --host=$(ARCH)-uclibc-linux --prefix=/usr  CFLAGS="$(COPTS) $(MIPS16_OPT) -fPIC -fno-strict-aliasing"
 	$(MAKE) -C quagga/readline clean all
 	cd quagga && ./configure --host=$(ARCH)-uclibc-linux $(CONFIG_QUAGGA_EXTRA) --localstatedir=/var/run  --libdir=/usr/tmp --enable-opaque-lsa --disable-nhrpd --enable-ospf-te --disable-ospfclient --enable-multipath=32  --enable-ipv6 --prefix=/usr --sysconfdir=/tmp --disable-ospf6d  --enable-vtysh --enable-user=root --enable-group=root --disable-ospfapi --disable-isisd --disable-pimd --disable-nhrpd --enable-pie=no --with-libreadline=$(TOP)/quagga/readline CFLAGS="-fno-strict-aliasing -I$(TOP)/quagga -Drpl_malloc=malloc -Drpl_realloc=realloc $(COPTS)  $(MIPS16_OPT)" LDFLAGS="-L$(TOP)/quagga/readline -L$(TOP)/ncurses/lib -lncurses" 
 	cd quagga && touch *
-endif
 
 quagga: ncurses
-ifeq ($(CONFIG_QUAGGA_STABLE),y)
-	$(MAKE) -j 4 -C quagga-stable
-else
 	$(MAKE) -j 4 -C quagga
-endif
 
 quagga-install:
-ifeq ($(CONFIG_QUAGGA_STABLE),y)
-	make -C quagga-stable DESTDIR=$(INSTALLDIR)/quagga install
-else
 	make -C quagga DESTDIR=$(INSTALLDIR)/quagga install
-endif
 	mkdir -p $(INSTALLDIR)/quagga/usr/lib
 	-cp -urv $(INSTALLDIR)/quagga/usr/tmp/* $(INSTALLDIR)/quagga/usr/lib
 	rm -rf $(INSTALLDIR)/quagga/usr/tmp 
@@ -51,8 +37,4 @@ endif
 	chmod 755 $(INSTALLDIR)/quagga/usr/bin/vtysh_init.sh
 
 quagga-clean:
-ifeq ($(CONFIG_QUAGGA_STABLE),y)
-	-if test -e "quagga-stable/Makefile"; then $(MAKE) -C quagga-stable clean; fi
-else
 	-if test -e "quagga/Makefile"; then $(MAKE) -C quagga clean; fi
-endif
