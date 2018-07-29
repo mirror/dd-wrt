f3518e40590501546e1198b9bfa5d6628e345c14
BrainSlayer <s.gottschall@dd-wrt.com>
2018-07-29 11:46:57 +0000

    fix iptraf configure
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36430 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/iptraf.mk
+++ src/router/rules/iptraf.mk
@@ -1,4 +1,5 @@
 iptraf-configure:
+	cd iptraf && ./autoconf
 	cd iptraf && ./configure --prefix=/usr --host=$(ARCH)-linux CFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE -I$(TOP)/ncurses/include -L$(TOP)/ncurses/lib" CC="ccache $(ARCH)-linux-uclibc-gcc"
 
 iptraf:
