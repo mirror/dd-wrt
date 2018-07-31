8221f21b581a8934c46debf90b650a97d669d9c6
BrainSlayer <s.gottschall@dd-wrt.com>
2018-07-31 07:49:57 +0000

    increase vap limit to 16 for ath9k
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36443 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/dd-wrt.c
+++ src/router/httpd/visuals/dd-wrt.c
@@ -2565,7 +2565,7 @@ static int show_virtualssid(webs_t wp, char *prefix)
 #ifndef HAVE_GUESTPORT
 	websWrite(wp, "<div class=\"center\">\n");
 #ifdef HAVE_MADWIFI
-	if (!has_ad(prefix) && count < 8)
+	if ((is_ath10k(prefix) && !has_ad(prefix) && count < 8) || (is_ath9k(prefix) && !is_ath10k(prefix) && !has_ad(prefix) && count < 16))
 #elif HAVE_RT2880
 	if (count < 7)
 #else
