f5b3a3d25816a60264ccc70555059025bb199106
BrainSlayer <s.gottschall@dd-wrt.com>
2018-08-05 11:16:00 +0000

    support channel quality in site survey
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36481 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/tools/site_survey_mac80211.c
+++ src/router/services/tools/site_survey_mac80211.c
@@ -1545,7 +1545,7 @@ void mac80211_site_survey(char *interface)
 		}
 
 		fprintf(stderr,
-			"[%2d] SSID[%20s] BSSID[%s] channel[%2d/%4d] frequency[%4d] rssi[%d] noise[%d] active[%llu] busy[%llu] beacon[%d] cap[%x] dtim[%d] rate[%d] enc[%s]\n",
+			"[%2d] SSID[%20s] BSSID[%s] channel[%2d/%4d] frequency[%4d] rssi[%d] noise[%d] active[%llu] busy[%llu] quality[%llu] beacon[%d] cap[%x] dtim[%d] rate[%d] enc[%s]\n",
 			i, site_survey_lists[i].SSID,
 			site_survey_lists[i].BSSID,
 			site_survey_lists[i].channel & 0xff,
@@ -1554,7 +1554,13 @@ void mac80211_site_survey(char *interface)
 			site_survey_lists[i].RSSI,
 			site_survey_lists[i].phy_noise,
 			site_survey_lists[i].active,
-			site_survey_lists[i].busy, site_survey_lists[i].beacon_period, site_survey_lists[i].capability, site_survey_lists[i].dtim_period, site_survey_lists[i].rate_count, site_survey_lists[i].ENCINFO);
+			site_survey_lists[i].busy, 
+			site_survey_lists[i].active ? (100 - (site_survey_lists[i].busy * 100 / site_survey_lists[i].active)) : 100,
+			site_survey_lists[i].beacon_period, 
+			site_survey_lists[i].capability, 
+			site_survey_lists[i].dtim_period, 
+			site_survey_lists[i].rate_count, 
+			site_survey_lists[i].ENCINFO);
 	}
 	free(site_survey_lists);
 	unl_free(&unl);
