eba4fff834c00a38c5b6129825b169392b1c803a
BrainSlayer <s.gottschall@dd-wrt.com>
2018-08-06 10:17:57 +0000

    fix long long data type for websWrite, otherwise big endian systems would show zero here
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36504 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/callvalidate.c
+++ src/router/httpd/modules/callvalidate.c
@@ -69,8 +69,8 @@
 
 #define SERVICEALT_MODULE "/jffs/usr/lib/validate.so"
 #define VISSERVICEALT_MODULE "/jffs/usr/lib/visuals.so"
-#define SERVICEALT_MODULE "/tmp/validate.so"
-#define VISSERVICEALT_MODULE "/tmp/visuals.so"
+//#define SERVICEALT_MODULE "/tmp/validate.so"
+//#define VISSERVICEALT_MODULE "/tmp/visuals.so"
 
 //#define SERVICE_MODULE "/tmp/validate.so"
 //#define VISSERVICE_MODULE "/tmp/visuals.so"
