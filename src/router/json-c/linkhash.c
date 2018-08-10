21b80b39ac27ddf4860d8b053a1847a4cf699cc0
BrainSlayer <s.gottschall@dd-wrt.com>
2018-08-10 19:23:40 +0000

    fix for older compilers
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36547 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/json-c/linkhash.c
+++ src/router/json-c/linkhash.c
@@ -405,7 +405,7 @@ unsigned long lh_char_hash(const void *k)
 		int seed;
 		/* we can't use -1 as it is the unitialized sentinel */
 		while ((seed = json_c_get_random_seed()) == -1);
-#if defined __GNUC__
+#if __GNUC__ >= 4 && __GNUC_MINOR__ >= 4
 		__sync_val_compare_and_swap(&random_seed, -1, seed);
 #elif defined _MSC_VER
 		InterlockedCompareExchange(&random_seed, seed, -1);
