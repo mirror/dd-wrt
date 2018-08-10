b2110d8c73f7bbccd2caf6a07e43011d58886cf0
BrainSlayer <s.gottschall@dd-wrt.com>
2018-08-10 20:51:42 +0000

    split dir
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36555 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rules/readline.mk
@@ -0,0 +1,22 @@
+readline-configure: 
+	cd readline && ./configure --host=$(ARCH)-uclibc-linux --prefix=/usr --disable-static CFLAGS="$(COPTS) $(MIPS16_OPT) -fPIC -fno-strict-aliasing"
+	$(MAKE) -C readline clean all
+	cd readline/shlib && rm -f libreadline.so
+
+readline:
+	$(MAKE) -C readline
+	cd readline/shlib && rm -f libreadline.so
+	cd readline/shlib && ln -s libreadline.so.5.0 libreadline.so
+
+readline-install:
+	rm -rf $(INSTALLDIR)/readline/usr/lib
+	$(MAKE) -C readline DESTDIR=$(INSTALLDIR)/readline install
+	rm -rf $(INSTALLDIR)/readline/usr/include
+	rm -rf $(INSTALLDIR)/readline/usr/info
+	rm -rf $(INSTALLDIR)/readline/usr/man
+	rm -f $(INSTALLDIR)/readline/usr/lib/*.la
+	rm -f $(INSTALLDIR)/readline/usr/lib/*.a
+	rm -f $(INSTALLDIR)/readline/usr/lib/libhistory*
+
+readline-clean:
+	-if test -e "readline/Makefile"; then $(MAKE) -C readline clean; fi
