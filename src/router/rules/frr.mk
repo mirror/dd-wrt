441c5b28a38bc5b64d19944b5bbafbfd284ade9c
BrainSlayer <s.gottschall@dd-wrt.com>
2018-08-11 09:31:39 +0000

    doesnt seem to like it
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36569 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/frr.mk
+++ src/router/rules/frr.mk
@@ -3,13 +3,13 @@ frr-configure: ncurses json-c readline
 	rm -rf frr/build
 	-mkdir -p frr/build
 	cd frr/build && ../configure CC="" CFLAGS="" LDFLAGS="" --with-vtysh-pager=less --disable-eigrpd --disable-ldpd --disable-pbrd --disable-rfptest --disable-ssd  --disable-doc --enable-clippy-only --disable-zeromq --enable-opaque-lsa --disable-nhrpd --enable-ospf-te --disable-ospfclient --enable-multipath=32  --enable-ipv6 --prefix=/usr --sysconfdir=/tmp --disable-ospf6d  --enable-vtysh --enable-user=root --enable-group=root --disable-ospfapi --disable-isisd --disable-pimd --disable-nhrpd --enable-pie=no
-	make -C frr/build lib/clippy PYTHONHOME=/usr/lib64/python2.7
+	make -C frr/build lib/clippy
 	-mkdir -p frr/build/hosttools/lib
 	cd frr/build && cp -vR lib/* hosttools/lib
 	cd frr/build && ../configure --host=$(ARCH)-uclibc-linux --localstatedir=/var/run  --libdir=/usr/tmp --with-vtysh-pager=less  --disable-eigrpd --disable-pbrd --disable-ldpd  --disable-rfptest --disable-ssd --disable-doc --disable-zeromq --enable-opaque-lsa --disable-nhrpd --enable-ospf-te --disable-ospfclient --enable-multipath=32  --enable-ipv6 --prefix=/usr --sysconfdir=/tmp --disable-ospf6d  --enable-vtysh --enable-user=root --enable-group=root --disable-ospfapi --disable-isisd --disable-pimd --disable-nhrpd --enable-pie=no --with-libreadline=$(TOP)/readline CFLAGS="-fno-strict-aliasing -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/ -Drpl_malloc=malloc -Drpl_realloc=realloc -I$(TOP)/_staging/usr/include -I$(TOP)/frr/build" CPPFLAGS="-fno-strict-aliasing -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/ -Drpl_malloc=malloc -Drpl_realloc=realloc -I$(TOP)/_staging/usr/include -I$(TOP)/frr/build" LDFLAGS="-L$(TOP)/readline/shlib -L$(TOP)/ncurses/lib -lncurses -L$(TOP)/json-c/.libs -ljson-c" 
 
 frr: ncurses json-c
-	make -C frr/build PYTHONHOME=/usr/lib64/python2.7
+	make -C frr/build
 
 frr-install:
 	make -C frr/build DESTDIR=$(INSTALLDIR)/frr install
