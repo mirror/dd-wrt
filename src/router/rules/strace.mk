a62ac71181f0ccd606ad5637c6011bf0a3b9e2d1
BrainSlayer <s.gottschall@dd-wrt.com>
2018-08-19 14:38:34 +0000

    add strace
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36675 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rules/strace.mk
@@ -0,0 +1,19 @@
+strace-configure:
+	cd strace && ./configure \
+		--prefix=/usr \
+		--libdir=/usr/lib \
+		--sysconfdir=/etc \
+		--host=$(ARCH)-linux \
+		CC="$(CC)" \
+		CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/openssl -DNEED_PRINTF" \
+		LDFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/openssl"
+
+strace: 
+	$(MAKE) -C strace
+
+strace-clean: 
+	if test -e "strace/Makefile"; then $(MAKE) -C strace clean ; fi
+
+strace-install: 
+	$(MAKE) -C strace install DESTDIR=$(INSTALLDIR)/strace
+	
