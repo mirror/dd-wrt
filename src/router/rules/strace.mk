a41d048790bba92cb560e72a780b7a636a46ea31
BrainSlayer <s.gottschall@dd-wrt.com>
2018-08-19 19:32:59 +0000

    for x86, but consider that musl lacks setcontext implementation. so it cannot be compiled on x86
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36687 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/strace.mk
+++ src/router/rules/strace.mk
@@ -1,28 +1,36 @@
 ifeq ($(ARCH),armeb)
+export LIBUNWIND_OPT = --with-libunwind
 export LIBUNWIND_LIB = -lunwind-arm
 endif
 ifeq ($(ARCH),arm)
+export LIBUNWIND_OPT = --with-libunwind
 export LIBUNWIND_LIB = -lunwind-arm
 endif
 ifeq ($(ARCH),mips)
+export LIBUNWIND_OPT = --with-libunwind
 export LIBUNWIND_LIB = -lunwind-mips
 endif
 ifeq ($(ARCH),mips64)
+export LIBUNWIND_OPT = --with-libunwind
 export LIBUNWIND_LIB = -lunwind-mips
 endif
 ifeq ($(ARCH),mipsel)
+export LIBUNWIND_OPT = --with-libunwind
 export LIBUNWIND_LIB = -lunwind-mips
 endif
 ifeq ($(ARCH),powerpc)
+export LIBUNWIND_OPT = --with-libunwind
 export LIBUNWIND_LIB = -lunwind-ppc32
 endif
 ifeq ($(ARCH),i386)
 export LIBUNWIND_LIB = -lunwind -lunwind-x86
 endif
 ifeq ($(ARCH),x86_64)
+export LIBUNWIND_OPT = --with-libunwind
 export LIBUNWIND_LIB = -lunwind -lunwind-x86_64
 endif
 ifeq ($(ARCH),aarch64)
+export LIBUNWIND_OPT = --with-libunwind
 export LIBUNWIND_LIB = -lunwind-aarch64
 endif
 
@@ -33,7 +41,7 @@ strace-configure: libunwind
 		--libdir=/usr/lib \
 		--sysconfdir=/etc \
 		--host=$(ARCH)-linux \
-		--with-libunwind \
+		$(LIBUNWIND_OPT) \
 		CC="$(CC)" \
 		CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/openssl -DNEED_PRINTF -I$(TOP)/libunwind/include" \
 		LDFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/openssl -L$(TOP)/libunwind/src/.libs $(LIBUNWIND_LIB)"
