a07a531ef2f4520e29b78988a92549b324109eec
BrainSlayer <s.gottschall@dd-wrt.com>
2018-08-19 20:01:36 +0000

    same applies for powerpc. libunwind cannot be compiled with musl here
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36691 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libunwind.mk
+++ src/router/rules/libunwind.mk
@@ -15,16 +15,21 @@ libunwind-configure:
 	sed -i 's/need_relink=yes/need_relink=no/g' $(TOP)/libunwind/libtool
 
 libunwind: 
+ifneq ($(ARCH),powerpc)
 ifneq ($(ARCH),i386)
 	$(MAKE) -C libunwind
+endif
 endif
 
 libunwind-clean: 
 ifneq ($(ARCH),i386)
+ifneq ($(ARCH),powerpc)
 	if test -e "libunwind/Makefile"; then $(MAKE) -C libunwind clean ; fi
+endif
 endif
 
 libunwind-install: 
+ifneq ($(ARCH),powerpc)
 ifneq ($(ARCH),i386)
 	$(MAKE) -C libunwind install DESTDIR=$(INSTALLDIR)/libunwind
 	rm -rf $(INSTALLDIR)/libunwind/usr/man
@@ -32,4 +37,5 @@ ifneq ($(ARCH),i386)
 	rm -f $(INSTALLDIR)/libunwind/usr/lib/*.a
 	rm -f $(INSTALLDIR)/libunwind/usr/lib/*.la
 	rm -rf $(INSTALLDIR)/libunwind/usr/lib/pkgconfig
-endif	
+endif
+endif
