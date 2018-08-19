f71dca5c0ab699de9c5fbd4d45bb571c85faec21
BrainSlayer <s.gottschall@dd-wrt.com>
2018-08-19 22:45:31 +0000

    do autoget
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36693 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/glib.mk
+++ src/router/rules/glib.mk
@@ -30,9 +30,9 @@ glib20-configure: libffi zlib util-linux
 	cd glib20/libiconv && touch *
 	$(MAKE) -C glib20/libiconv clean all
 
-	-cd glib20/gettext && ./autogen.sh
 	cd glib20/gettext && ./configure --disable-libmount --enable-shared --disable-static --disable-openmp --host=$(ARCH)-linux  LDFLAGS="$(COPTS) -std=gnu89 $(MIPS16_OPT) -D_GNU_SOURCE -fPIC -Drpl_malloc=malloc " CFLAGS="$(COPTS)  $(MIPS16_OPT)  -D_GNU_SOURCE -fPIC -Drpl_malloc=malloc -I$(TOP)/glib20/libiconv/include -L$(TOP)/glib20/libiconv/lib/.libs" CXXFLAGS="$(COPTS)  $(MIPS16_OPT) -D_GNU_SOURCE -fPIC -Drpl_malloc=malloc -I$(TOP)/glib20/libiconv/include -L$(TOP)/glib20/libiconv/lib/.libs"
 	cd glib20/gettext && touch *
+	cd glib20/gettext/gettext-tools/examples && touch *
 	$(MAKE) -C glib20/gettext clean all
 
 	-cd glib20/libglib && ./autogen.sh
