628da10b65c1fb66e705b2f2c5bb6666022727c1
BrainSlayer <s.gottschall@dd-wrt.com>
2018-08-19 18:57:35 +0000

    use libunwind
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36681 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/strace.mk
+++ src/router/rules/strace.mk
@@ -1,12 +1,42 @@
+ifeq ($(ARCH),armeb)
+export LIBUNWIND_LIB = -lunwind-arm
+endif
+ifeq ($(ARCH),arm)
+export LIBUNWIND_LIB = -lunwind-arm
+endif
+ifeq ($(ARCH),mips)
+export LIBUNWIND_LIB = -lunwind-mips
+endif
+ifeq ($(ARCH),mips64)
+export LIBUNWIND_LIB = -lunwind-mips
+endif
+ifeq ($(ARCH),mipsel)
+export LIBUNWIND_LIB = -lunwind-mips
+endif
+ifeq ($(ARCH),powerpc)
+export LIBUNWIND_LIB = -lunwind-ppc
+endif
+ifeq ($(ARCH),i386)
+export LIBUNWIND_LIB = -lunwind-ppc
+endif
+ifeq ($(ARCH),x86_64)
+export LIBUNWIND_LIB = -lunwind-ppc
+endif
+ifeq ($(ARCH),aarch64)
+export LIBUNWIND_LIB = -lunwind-ppc
+endif
+
+
 strace-configure:
 	cd strace && ./configure \
 		--prefix=/usr \
 		--libdir=/usr/lib \
 		--sysconfdir=/etc \
 		--host=$(ARCH)-linux \
+		--with-libunwind \
 		CC="$(CC)" \
-		CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/openssl -DNEED_PRINTF" \
-		LDFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/openssl"
+		CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/openssl -DNEED_PRINTF -I$(TOP)/libunwind/include" \
+		LDFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/openssl -L$(TOP)/libunwind/src/.libs $(LIBUNWIND_LIB)"
 
 strace: 
 	$(MAKE) -C strace
@@ -16,5 +46,5 @@ strace-clean:
 
 strace-install: 
 	$(MAKE) -C strace install DESTDIR=$(INSTALLDIR)/strace
-	rm -rf $(INSTALLDIR)/strace/usr/man
+	rm -rf $(INSTALLDIR)/strace/usr/share
 	
