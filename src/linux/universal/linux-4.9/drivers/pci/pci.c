377da3c5e17a7845a367cfb1bf1daf705f7a6906
BrainSlayer <s.gottschall@dd-wrt.com>
2018-08-23 08:34:06 +0000

    update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36704 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.9/drivers/pci/pci.c
+++ src/linux/universal/linux-4.9/drivers/pci/pci.c
@@ -3436,6 +3436,44 @@ char * __weak __init pcibios_setup(char *str)
 	return str;
 }
 
+static void devm_pci_unmap_iospace(struct device *dev, void *ptr)
+{
+	struct resource **res = ptr;
+
+	pci_unmap_iospace(*res);
+}
+
+/**
+ * devm_pci_remap_iospace - Managed pci_remap_iospace()
+ * @dev: Generic device to remap IO address for
+ * @res: Resource describing the I/O space
+ * @phys_addr: physical address of range to be mapped
+ *
+ * Managed pci_remap_iospace().  Map is automatically unmapped on driver
+ * detach.
+ */
+int devm_pci_remap_iospace(struct device *dev, const struct resource *res,
+			   phys_addr_t phys_addr)
+{
+	const struct resource **ptr;
+	int error;
+
+	ptr = devres_alloc(devm_pci_unmap_iospace, sizeof(*ptr), GFP_KERNEL);
+	if (!ptr)
+		return -ENOMEM;
+
+	error = pci_remap_iospace(res, phys_addr);
+	if (error) {
+		devres_free(ptr);
+	} else	{
+		*ptr = res;
+		devres_add(dev, ptr);
+	}
+
+	return error;
+}
+EXPORT_SYMBOL(devm_pci_remap_iospace);
+
 /**
  * pcibios_set_master - enable PCI bus-mastering for device dev
  * @dev: the PCI device to enable
