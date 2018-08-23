7e35e8e04b3d753ac317568eb0118d10af81cdf3
BrainSlayer <s.gottschall@dd-wrt.com>
2018-08-23 10:20:42 +0000

    caramola2, reconfigure lan devices
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36711 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit.c
+++ src/router/services/sysinit/sysinit.c
@@ -1371,11 +1371,11 @@ void start_restore_defaults(void)
 #elif HAVE_CARAMBOLA
 	struct nvram_param generic[] = {
 		{"lan_ifname", "br0"},
-		{"lan_ifnames", "vlan1 vlan2 ath0"},
-		{"wan_ifname", "vlan2"},
-		{"wan_ifname2", "vlan2"},
-		{"wan_ifnames", "vlan2"},
-		{"wan_default", "vlan2"},
+		{"lan_ifnames", "eth0 ath0"},
+		{"wan_ifname", "eth1"},
+		{"wan_ifname2", "eth1"},
+		{"wan_ifnames", "eth1"},
+		{"wan_default", "eth1"},
 		{0, 0}
 	};
 #elif HAVE_WR710
