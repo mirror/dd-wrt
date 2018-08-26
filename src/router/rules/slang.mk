888bfda22f03ba42ceba0e5218573f84d72680c2
BrainSlayer <s.gottschall@dd-wrt.com>
2018-08-26 18:41:37 +0000

    fixes for slang
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36786 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/slang.mk
+++ src/router/rules/slang.mk
@@ -1,8 +1,9 @@
 slang-configure:
 	cd slang && ./configure --host=$(ARCH)-uclibc-linux CFLAGS="$(COPTS)  $(MIPS16_OPT) -I$(TOP)/zlib -L$(TOP)/zlib -L$(INSTALLDIR)/util-linux/usr/lib" --enable-shared \
-		--enable-static \
+		--disable-static \
 		--without-png \
 		--libdir=/usr/lib \
+		--prefix=/usr \
 		--enable-debug=no 
 	make -C slang clean
 	make -C slang
@@ -10,5 +11,14 @@ slang-configure:
 slang:
 	make -C slang
 
-slang-clean
+slang-clean:
 	if test -e "slang/Makefile"; then $(MAKE) -C slang clean; fi
+
+slang-install:
+	make -C slang install DESTDIR=$(INSTALLDIR)/slang
+	rm -rf $(INSTALLDIR)/slang/usr/bin
+	rm -rf $(INSTALLDIR)/slang/usr/etc
+	rm -rf $(INSTALLDIR)/slang/usr/include
+	rm -rf $(INSTALLDIR)/slang/usr/share
+	rm -rf $(INSTALLDIR)/slang/usr/lib/pkgconfig
+	rm -rf $(INSTALLDIR)/slang/usr/lib/slang
