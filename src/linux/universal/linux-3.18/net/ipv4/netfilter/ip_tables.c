9c5b6a9bc59556f0b9e4024be8901a704fcbb635
BrainSlayer <s.gottschall@dd-wrt.com>
2018-08-26 09:34:27 +0000

    update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36748 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/net/ipv4/netfilter/ip_tables.c
+++ src/linux/universal/linux-3.18/net/ipv4/netfilter/ip_tables.c
@@ -2143,6 +2143,7 @@ static struct xt_match ipt_builtin_mt[] __read_mostly = {
 		.checkentry = icmp_checkentry,
 		.proto      = IPPROTO_ICMP,
 		.family     = NFPROTO_IPV4,
+		.me	    = THIS_MODULE,
 	},
 };
 
