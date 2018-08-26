23f0fa7872f41a0b80cd1d109b6d1422f2b49310
BrainSlayer <s.gottschall@dd-wrt.com>
2018-08-26 11:29:33 +0000

    fix dep
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36754 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/iftop.mk
+++ src/router/rules/iftop.mk
@@ -1,4 +1,4 @@
-iftop-configure:
+iftop-configure: ncurses
 	cd iftop && ./configure --host=$(ARCH)-linux --with-libpcap="$(TOP)/libpcap" CFLAGS="$(COPTS) -I$(TOP)/libpcap -I$(TOP)/ncurses/include" LDFLAGS="-L$(TOP)/ncurses/lib -L$(TOP)/libpcap" CPPFLAGS="$(COPTS) -I$(TOP)/libpcap -I$(TOP)/ncurses/include"
 
 iftop: ncurses
