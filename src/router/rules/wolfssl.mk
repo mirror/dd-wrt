ed72fd59e5887b045315c99e9f02a58f7e7f0525
BrainSlayer <s.gottschall@dd-wrt.com>
2018-09-09 09:39:34 +0000

    do autogen
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36850 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/wolfssl.mk
+++ src/router/rules/wolfssl.mk
@@ -1,4 +1,5 @@
 wolfssl-configure:
+	cd wolfssl && ./autogen.sh
 	cd wolfssl && ./configure --prefix=/usr --host=$(ARCH)-linux --enable-opensslextra --disable-shared --enable-static -disable-errorstrings --disable-oldtls --disable-poly1305 --disable-chacha --enable-ecc --disable-sslv3 CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections"
 
 wolfssl:
