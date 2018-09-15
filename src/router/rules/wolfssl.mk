4bfcddf8a045a5c8c4d3b6b0c03e727e94da0cde
BrainSlayer <s.gottschall@dd-wrt.com>
2018-09-15 08:44:36 +0000

    experimental for implementing wpa3 support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@36908 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/wolfssl.mk
+++ src/router/rules/wolfssl.mk
@@ -1,6 +1,6 @@
 wolfssl-configure:
 	cd wolfssl && ./autogen.sh
-	cd wolfssl && ./configure --prefix=/usr --host=$(ARCH)-linux --enable-opensslextra --disable-shared --enable-static -disable-errorstrings --disable-oldtls --disable-poly1305 --disable-chacha --enable-ecc --disable-sslv3 CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections"
+	cd wolfssl && ./configure --prefix=/usr --host=$(ARCH)-linux --enable-opensslextra --disable-shared --enable-static -disable-errorstrings --disable-oldtls --disable-poly1305 --disable-chacha --enable-ecc --disable-sslv3 --enable-des3 --enable-md4 --enable-stunnel --enable-session-ticket --enable-wpas --enable-cmac CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections"
 
 wolfssl:
 	$(MAKE) -j 4 -C wolfssl
