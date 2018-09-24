2553531c8e630f2ac831afa79455a3b690979a10
BrainSlayer <s.gottschall@dd-wrt.com>
2018-09-24 18:08:56 +0000

    memcached support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@37065 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/opendpi/opendpi/lib/ndpi_main.c
+++ src/router/opendpi/opendpi/lib/ndpi_main.c
@@ -829,6 +829,8 @@ static void ndpi_init_protocol_defaults(struct ndpi_detection_module_struct *ndp
 				ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0) /* UDP */ );
 	ndpi_set_proto_defaults(ndpi_mod, NDPI_PROTOCOL_ACCEPTABLE, NDPI_PROTOCOL_SMBV23, no_master, no_master, "SMBv23", ndpi_build_default_ports(ports_a, 445, 0, 0, 0, 0) /* TCP */ ,
 				ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0) /* UDP */ );
+	ndpi_set_proto_defaults(ndpi_mod, NDPI_PROTOCOL_ACCEPTABLE, NDPI_PROTOCOL_MEMCACHED, no_master, no_master, "Memcached", ndpi_build_default_ports(ports_a, 11211, 0, 0, 0, 0) /* TCP */ ,
+				ndpi_build_default_ports(ports_b, 11211, 0, 0, 0, 0) /* UDP */ );
 	ndpi_set_proto_defaults(ndpi_mod, NDPI_PROTOCOL_ACCEPTABLE, NDPI_PROTOCOL_MINING, no_master, no_master, "Mining", ndpi_build_default_ports(ports_a, 8333, 0, 0, 0, 0) /* TCP */ ,
 				ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0) /* UDP */ );
 	ndpi_set_proto_defaults(ndpi_mod, NDPI_PROTOCOL_ACCEPTABLE, NDPI_PROTOCOL_SYSLOG, no_master, no_master, "Syslog", ndpi_build_default_ports(ports_a, 514, 0, 0, 0, 0) /* TCP */ ,
@@ -2317,6 +2319,10 @@ static void ndpi_set_protocol_detection_bitmask2(struct ndpi_detection_module_st
 
 	/* AJP */
 	init_ajp_dissector(ndpi_struct, &a, detection_bitmask);
+
+	/* Memcached */
+	init_memcached_dissector(ndpi_struct, &a, detection_bitmask);
+
 
 	/* ----------------------------------------------------------------- */
 
