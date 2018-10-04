d5f9b6a072e90f685c08eddcefb09194db96ca51
BrainSlayer <s.gottschall@dd-wrt.com>
2018-10-04 23:30:39 +0000

    backport zstd feature
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@37156 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.9/security/apparmor/apparmorfs.c
+++ src/linux/universal/linux-4.9/security/apparmor/apparmorfs.c
@@ -100,7 +100,7 @@ static char *aa_simple_write_to_buffer(int op, const char __user *userbuf,
 		return ERR_PTR(-EACCES);
 
 	/* freed by caller to simple_write_to_buffer */
-	data = kvmalloc(alloc_size);
+	data = kvmalloc(alloc_size, GFP_KERNEL);
 	if (data == NULL)
 		return ERR_PTR(-ENOMEM);
 
