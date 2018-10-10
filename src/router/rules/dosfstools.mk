9c661a5c49fecbbe93b2bbc6b3e5a8abd8a304ae
BrainSlayer <s.gottschall@dd-wrt.com>
2018-10-10 21:07:52 +0000

    dosfstools
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@37320 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rules/dosfstools.mk
@@ -0,0 +1,19 @@
+dosfstools-configure:
+	cd dosfstools && autoconf
+	cd dosfstools && ./configure --host=$(ARCH)-linux --prefix=/usr --enable-compat-symlinks \
+	    CC="$(CC) $(COPTS) $(MIPS16_OPT) -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections -D_GNU_SOURCE  -DNEED_PRINTF -std=gnu89" \
+	    LDFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections"
+
+dosfstools:
+	make -C dosfstools
+
+dosfstools-clean:
+	make -C dosfstools clean
+
+dosfstools-install:
+	make -C dosfstools install DESTDIR=$(INSTALLDIR)/dosfstools
+	rm -rf $(INSTALLDIR)/dosfstools/usr/share
+	rm -f $(INSTALLDIR)/dosfstools/usr/sbin/dumpexfat
+	rm -f $(INSTALLDIR)/dosfstools/usr/sbin/fatlabel
+	rm -f $(INSTALLDIR)/dosfstools/usr/sbin/dosfslabel
+
