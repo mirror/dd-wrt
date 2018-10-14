d1aea9e930c38c7ce866a7e9361fbafe678bd0a6
BrainSlayer <s.gottschall@dd-wrt.com>
2018-10-14 20:49:19 +0000

    autogen
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@37393 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/wifidog.mk
+++ src/router/rules/wifidog.mk
@@ -1,4 +1,5 @@
 wifidog-configure:
+	cd wifidog && ./autogen.sh
 	cd wifidog && ./configure --disable-nls --prefix=/usr --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections"
 
 wifidog:
