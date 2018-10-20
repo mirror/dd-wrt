7153c3206129d194155bb40d3b4dc5072d5f34e6
BrainSlayer <s.gottschall@dd-wrt.com>
2018-10-20 12:00:17 +0000

    use linux sort instead of freebsd qsort variant
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@37469 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/zfsnew/module/zstd/zstd.c
+++ src/router/zfsnew/module/zstd/zstd.c
@@ -34,142 +34,19 @@
 #include <sys/zstd/zstd.h>
 #include <sys/zstd/zstd_errors.h>
 
+
 #define	ZSTD_KMEM_MAGIC		0x20160831
 
 /* for BSD compat */
 #define	__unused			__attribute__((unused))
-typedef int		 cmp_t(const void *, const void *);
-static inline char	*med3(char *, char *, char *, cmp_t *, void *);
-#ifndef MIN
-#define	MIN(a, b)	((a) < (b) ? a : b)
+
+
+#ifdef _KERNEL
+#include <linux/sort.h>
+#define	zstd_qsort(a, n, es, cmp) sort(a, n, es, cmp, NULL)
+#else
+#define	zstd_qsort qsort
 #endif
-static inline void
-swapfunc(char *a, char *b, size_t es)
-{
-	char t;
-
-	do {
-		t = *a;
-		*a++ = *b;
-		*b++ = t;
-	} while (--es > 0);
-}
-
-#define	vecswap(a, b, n)				\
-	if ((n) > 0) swapfunc(a, b, n)
-#define	CMP(t, x, y) (cmp((x), (y)))
-
-static inline char *
-med3(char *a, char *b, char *c, cmp_t *cmp, void *thunk __unused)
-{
-	return CMP(thunk, a, b) < 0 ?
-	    (CMP(thunk, b, c) < 0 ? b : (CMP(thunk, a, c) < 0 ? c : a))
-	    :(CMP(thunk, b, c) > 0 ? b : (CMP(thunk, a, c) < 0 ? a : c));
-}
-
-#define	thunk NULL
-void
-zstd_qsort(void *a, size_t n, size_t es, cmp_t *cmp)
-{
-	char *pa, *pb, *pc, *pd, *pl, *pm, *pn;
-	size_t d1, d2;
-	int cmp_result;
-	int swap_cnt;
-
-loop:
-	swap_cnt = 0;
-	if (n < 7) {
-		for (pm = (char *)a + es; pm < (char *)a + n * es; pm += es)
-			for (pl = pm;
-			    pl > (char *)a && CMP(thunk, pl - es, pl) > 0;
-			    pl -= es)
-				swapfunc(pl, pl - es, es);
-		return;
-	}
-	pm = (char *)a + (n / 2) * es;
-	if (n > 7) {
-		pl = a;
-		pn = (char *)a + (n - 1) * es;
-		if (n > 40) {
-			size_t d = (n / 8) * es;
-
-			pl = med3(pl, pl + d, pl + 2 * d, cmp, thunk);
-			pm = med3(pm - d, pm, pm + d, cmp, thunk);
-			pn = med3(pn - 2 * d, pn - d, pn, cmp, thunk);
-		}
-		pm = med3(pl, pm, pn, cmp, thunk);
-	}
-	swapfunc(a, pm, es);
-	pa = pb = (char *)a + es;
-
-	pc = pd = (char *)a + (n - 1) * es;
-	for (;;) {
-		while (pb <= pc && (cmp_result = CMP(thunk, pb, a)) <= 0) {
-			if (cmp_result == 0) {
-				swap_cnt = 1;
-				swapfunc(pa, pb, es);
-				pa += es;
-			}
-			pb += es;
-		}
-		while (pb <= pc && (cmp_result = CMP(thunk, pc, a)) >= 0) {
-			if (cmp_result == 0) {
-				swap_cnt = 1;
-				swapfunc(pc, pd, es);
-				pd -= es;
-			}
-			pc -= es;
-		}
-		if (pb > pc)
-			break;
-		swapfunc(pb, pc, es);
-		swap_cnt = 1;
-		pb += es;
-		pc -= es;
-	}
-	if (swap_cnt == 0) {  /* Switch to insertion sort */
-		for (pm = (char *)a + es; pm < (char *)a + n * es; pm += es)
-			for (pl = pm;
-			    pl > (char *)a && CMP(thunk, pl - es, pl) > 0;
-			    pl -= es)
-				swapfunc(pl, pl - es, es);
-		return;
-	}
-
-	pn = (char *)a + n * es;
-	d1 = MIN(pa - (char *)a, pb - pa);
-	vecswap(a, pb - d1, d1);
-	d1 = MIN(pd - pc, pn - pd - es);
-	vecswap(pb, pn - d1, d1);
-
-	d1 = pb - pa;
-	d2 = pd - pc;
-	if (d1 <= d2) {
-		/* Recurse on left partition, then iterate on right partition */
-		if (d1 > es) {
-			zstd_qsort(a, d1 / es, es, cmp);
-		}
-		if (d2 > es) {
-			/* Iterate rather than recurse to save stack space */
-			/* qsort(pn - d2, d2 / es, es, cmp); */
-			a = pn - d2;
-			n = d2 / es;
-			goto loop;
-		}
-	} else {
-		/* Recurse on right partition, then iterate on left partition */
-		if (d2 > es) {
-			zstd_qsort(pn - d2, d2 / es, es, cmp);
-		}
-		if (d1 > es) {
-			/* Iterate rather than recurse to save stack space */
-			/* qsort(a, d1 / es, es, cmp); */
-			n = d1 / es;
-			goto loop;
-		}
-	}
-}
-
 
 static size_t real_zstd_compress(const char *source, char *dest, int isize,
     int osize, int level);
@@ -250,7 +127,7 @@ zstd_compare(const void *a, const void *b)
 	ASSERT3U(x->kmem_magic, ==, ZSTD_KMEM_MAGIC);
 	ASSERT3U(y->kmem_magic, ==, ZSTD_KMEM_MAGIC);
 
-	return AVL_CMP(x->kmem_size, y->kmem_size);
+	return (AVL_CMP(x->kmem_size, y->kmem_size));
 }
 
 static enum zio_zstd_levels
@@ -550,8 +427,8 @@ zstd_free(void *opaque __unused, void *ptr)
 	}
 }
 #ifndef _KERNEL
-#define __init
-#define __exit
+#define	__init
+#define	__exit
 #endif
 
 extern int __init
@@ -597,7 +474,7 @@ zstd_init(void)
 	/* Sort the kmem caches for later searching */
 	zstd_qsort(zstd_cache_size, ZSTD_KMEM_COUNT, sizeof (struct zstd_kmem),
 	    zstd_compare);
-	return 0;
+	return (0);
 }
 
 extern void __exit
@@ -628,5 +505,3 @@ MODULE_AUTHOR(ZFS_META_AUTHOR);
 MODULE_LICENSE(ZFS_META_LICENSE);
 MODULE_VERSION(ZFS_META_VERSION "-" ZFS_META_RELEASE);
 #endif
-
-
