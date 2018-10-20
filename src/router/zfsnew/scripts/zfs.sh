17d143d3ca26b1346c3da3eac7c892230547708f
BrainSlayer <s.gottschall@dd-wrt.com>
2018-10-20 10:06:08 +0000

    refactoring work for zstd
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@37461 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/zfsnew/scripts/zfs.sh
+++ src/router/zfsnew/scripts/zfs.sh
@@ -28,6 +28,7 @@ KMOD_ZCOMMON=${KMOD_ZCOMMON:-zcommon}
 KMOD_ZLUA=${KMOD_ZLUA:-zlua}
 KMOD_ICP=${KMOD_ICP:-icp}
 KMOD_ZFS=${KMOD_ZFS:-zfs}
+KMOD_ZZSTD=${KMOD_ZZSTD:-zzstd}
 
 
 usage() {
@@ -76,7 +77,7 @@ check_modules() {
 	MISSING_MODULES=""
 
 	for KMOD in $KMOD_SPL $KMOD_ZAVL $KMOD_ZNVPAIR \
-	    $KMOD_ZUNICODE $KMOD_ZCOMMON $KMOD_ZLUA $KMOD_ICP $KMOD_ZFS; do
+	    $KMOD_ZUNICODE $KMOD_ZCOMMON $KMOD_ZLUA $KMOD_ZZSTD $KMOD_ICP $KMOD_ZFS; do
 		NAME=$(basename "$KMOD" .ko)
 
 		if lsmod | grep -E -q "^${NAME}"; then
@@ -135,7 +136,7 @@ load_modules() {
 	fi
 
 	for KMOD in $KMOD_SPL $KMOD_ZAVL $KMOD_ZNVPAIR \
-	    $KMOD_ZUNICODE $KMOD_ZCOMMON $KMOD_ZLUA $KMOD_ICP $KMOD_ZFS; do
+	    $KMOD_ZUNICODE $KMOD_ZCOMMON $KMOD_ZLUA $KMOD_ZZSTD $KMOD_ICP $KMOD_ZFS; do
 		load_module "$KMOD" || return 1
 	done
 
@@ -163,7 +164,7 @@ unload_module() {
 }
 
 unload_modules() {
-	for KMOD in $KMOD_ZFS $KMOD_ICP $KMOD_ZLUA $KMOD_ZCOMMON $KMOD_ZUNICODE \
+	for KMOD in $KMOD_ZFS $KMOD_ICP $KMOD_ZZSTD $KMOD_ZLUA $KMOD_ZCOMMON $KMOD_ZUNICODE \
 	    $KMOD_ZNVPAIR  $KMOD_ZAVL $KMOD_SPL; do
 		NAME=$(basename "$KMOD" .ko)
 		USE_COUNT=$(lsmod | grep -E "^${NAME} " | awk '{print $3}')
