2306e18ebad2d29ab8a4689dc44f40557d463ede
BrainSlayer <s.gottschall@dd-wrt.com>
2018-11-05 10:10:29 +0000

    loginprompt for custom
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@37588 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/init.c
+++ src/router/services/sysinit/init.c
@@ -178,6 +178,8 @@ void start_post_sysinit(void)
 #endif
 #elif HAVE_RAYTRONIK
 	fprintf(fp, "RAYTRONIK %s (c) 2018 NewMedia-NET GmbH\nRelease: " BUILD_DATE " (SVN revision: %s)\n", DIST, SVN_REVISION);
+#elif HAVE_ANTAIRA
+	fprintf(fp, "Antaira %s (c) 2018 NewMedia-NET GmbH\nRelease: " BUILD_DATE " (SVN revision: %s)\n", DIST, SVN_REVISION);
 #elif HAVE_HDWIFI
 	fprintf(fp, "HDWIFI %s (c) 2018 NewMedia-NET GmbH\nRelease: " BUILD_DATE " (SVN revision: %s)\n", DIST, SVN_REVISION);
 #else
