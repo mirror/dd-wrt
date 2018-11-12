4f73bd78f5db113e4a8faa448009353d83006768
BrainSlayer <s.gottschall@dd-wrt.com>
2018-11-12 05:29:36 +0000

    update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@37645 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4/fs/btrfs/disk-io.c
+++ src/linux/universal/linux-4.4/fs/btrfs/disk-io.c
@@ -1820,7 +1820,7 @@ static int cleaner_kthread(void *arg)
 		 */
 		btrfs_delete_unused_bgs(root->fs_info);
 sleep:
-		if (!again) {
+		if (!try_to_freeze() && !again) {
 			set_current_state(TASK_INTERRUPTIBLE);
 			if (!kthread_should_stop())
 				schedule();
