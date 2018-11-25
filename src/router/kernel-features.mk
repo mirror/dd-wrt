9693ba5d2c98c10d5958151623dbdadd387cd595
BrainSlayer <s.gottschall@dd-wrt.com>
2018-11-25 13:52:18 +0000

    do it as module
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@37847 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kernel-features.mk
+++ src/router/kernel-features.mk
@@ -20,8 +20,8 @@ define kernelfeatures
 		echo "# CONFIG_CUSE is not set" >> $(LINUXDIR)/.config; \
 	fi
 	if [ "$(CONFIG_WPA3)" = "y" ]; then \
-		sed -i 's/\# CONFIG_CRYPTO_GCM is not set/CONFIG_CRYPTO_GCM=y/g' $(LINUXDIR)/.config; \
-		sed -i 's/\CONFIG_CRYPTO_GCM=m/CONFIG_CRYPTO_GCM=y/g' $(LINUXDIR)/.config; \
+		sed -i 's/\# CONFIG_CRYPTO_GCM is not set/CONFIG_CRYPTO_GCM=m/g' $(LINUXDIR)/.config; \
+		echo "# CONFIG_CRYPTO_DRBG_CTR is not set" >> $(LINUXDIR)/.config; \
 	fi
 	if [ "$(CONFIG_EOP_TUNNEL)" = "y" ]; then \
 		echo CONFIG_NET_ETHERIP=m >> $(LINUXDIR)/.config; \
