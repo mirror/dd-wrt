811b4926902988495b639381501b5c3fd26a8b19
BrainSlayer <s.gottschall@dd-wrt.com>
2018-12-02 13:53:41 +0000

    this is very performance intense
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@37900 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.9/arch/x86/kernel/cpu/bugs.c
+++ src/linux/universal/linux-4.9/arch/x86/kernel/cpu/bugs.c
@@ -331,8 +331,8 @@ static void __init spectre_v2_select_mitigation(void)
 	 * If the CPU is not affected and the command line mode is NONE or AUTO
 	 * then nothing to do.
 	 */
-	if (!boot_cpu_has_bug(X86_BUG_SPECTRE_V2) &&
-	    (cmd == SPECTRE_V2_CMD_NONE || cmd == SPECTRE_V2_CMD_AUTO))
+//	if (!boot_cpu_has_bug(X86_BUG_SPECTRE_V2) &&
+//	    (cmd == SPECTRE_V2_CMD_NONE || cmd == SPECTRE_V2_CMD_AUTO))
 		return;
 
 	switch (cmd) {
@@ -460,7 +460,7 @@ static enum ssb_mitigation_cmd __init ssb_parse_cmdline(void)
 	enum ssb_mitigation_cmd cmd = SPEC_STORE_BYPASS_CMD_AUTO;
 	char arg[20];
 	int ret, i;
-
+	return SPEC_STORE_BYPASS_CMD_NONE;
 	if (cmdline_find_option_bool(boot_command_line, "nospec_store_bypass_disable")) {
 		return SPEC_STORE_BYPASS_CMD_AUTO;
 	} else {
@@ -491,6 +491,7 @@ static enum ssb_mitigation __init __ssb_select_mitigation(void)
 	enum ssb_mitigation mode = SPEC_STORE_BYPASS_NONE;
 	enum ssb_mitigation_cmd cmd;
 
+	return mode;
 	if (!boot_cpu_has(X86_FEATURE_SSBD))
 		return mode;
 
