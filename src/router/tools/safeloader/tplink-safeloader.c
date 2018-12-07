ea056174cb02ff977cf692c29db1856e569846c5
BrainSlayer <s.gottschall@dd-wrt.com>
2018-12-07 13:04:35 +0000

    seem to be identical with c7 v5 but has different layout
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@37925 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/tools/safeloader/tplink-safeloader.c
+++ src/router/tools/safeloader/tplink-safeloader.c
@@ -859,6 +859,55 @@ static struct device_info boards[] = {
 		.last_sysupgrade_partition = "file-system"
 	},
 	{
+		.id     = "ARCHER-A7-V5",
+		.vendor = "",
+		.support_list =
+			"SupportList:\n"
+			"{product_name:ARCHER A7,product_ver:5.0.0,special_id:00000000}\n" // UNIVERSAL
+			"{product_name:ARCHER A7,product_ver:5.0.0,special_id:55530000}\n" //US 
+			"{product_name:ARCHER A7,product_ver:5.0.0,special_id:45550000}\n" //EU
+			"{product_name:ARCHER A7,product_ver:5.0.0,special_id:4B520000}\n" //KR
+			"{product_name:ARCHER A7,product_ver:5.0.0,special_id:42520000}\n" //BR
+			"{product_name:ARCHER A7,product_ver:5.0.0,special_id:4A500000}\n" //JP
+			"{product_name:ARCHER A7,product_ver:5.0.0,special_id:43410000}\n" //CA
+			"{product_name:ARCHER A7,product_ver:5.0.0,special_id:41550000}\n" //AU
+			"{product_name:ARCHER A7,product_ver:5.0.0,special_id:52550000}\n" //RU
+			"{product_name:ARCHER A7,product_ver:5.0.0,special_id:54570000}\n", // TW
+		.support_trail = '\x00',
+		.soft_ver = "soft_ver:1.0.0\n",
+
+		/**
+		    We use a bigger os-image partition than the stock images (and thus
+		    smaller file-system), as our kernel doesn't fit in the stock firmware's
+		    1MB os-image.
+		*/
+		
+		.partitions = {
+			{"factory-boot", 0x00000, 0x20000},
+			{"fs-uboot", 0x20000, 0x20000},
+			{"os-image", 0x40000, 0x120000},	/* Stock: base 0x30000 size 0x100000 */
+			{"file-system", 0x160000, 0xda0000},	/* Stock: base 0x130000 size 0x6a0000 */
+			{"default-mac", 0xf40000, 0x00200},
+			{"pin", 0xf40200, 0x00200},
+			{"device-id", 0xf40400, 0x00100},
+			{"product-info", 0xf40500, 0x0fb00},
+			{"soft-version", 0xf50000, 0x01000},
+			{"extra-para", 0xf51000, 0x01000},
+			{"support-list", 0xf52000, 0x0a000},
+			{"profile", 0xf5c000, 0x04000},
+			{"default-config", 0xf60000, 0x10000},
+			{"user-config", 0xf70000, 0x40000},
+			{"certificate", 0xfb0000, 0x10000},
+			{"partition-table", 0xfc0000, 0x10000},
+			{"log", 0xfd0000, 0x20000},
+			{"radio", 0xff0000, 0x10000},
+			{NULL, 0, 0}
+		},
+
+		.first_sysupgrade_partition = "os-image",
+		.last_sysupgrade_partition = "file-system"
+	},
+	{
 		.id     = "ARCHER-C7-V5",
 		.vendor = "",
 		.support_list =
@@ -1459,6 +1508,11 @@ static void build_image(const char *output,
 	}
 
 	if (strcasecmp(info->id, "ARCHER-C7-V5") == 0) {
+		const char mdat[11] = {0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00};
+		parts[5] = put_data("extra-para", mdat, 11);
+	}
+
+	if (strcasecmp(info->id, "ARCHER-A7-V5") == 0) {
 		const char mdat[11] = {0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00};
 		parts[5] = put_data("extra-para", mdat, 11);
 	}
