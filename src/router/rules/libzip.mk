2125e95f882490eee1898d6c28a96add5a994383
BrainSlayer <s.gottschall@dd-wrt.com>
2018-12-13 10:49:13 +0000

    libzip add
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@37976 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rules/libzip.mk
@@ -0,0 +1,33 @@
+PKG_INSTALL:=1
+
+MAKE_FLAGS+=VERBOSE=0
+
+LIBZIP_PKG_BUILD_DIR=$(TOP)/libzip
+LIBZIP_CMAKE_OPTIONS=-DZLIB_LIBRARY=$(TOP)/zlib \
+		    -DZLIB_INCLUDE_DIR=$(TOP)/zlib/include \
+		    -DENABLE_GNUTLS=OFF \
+		    -DENABLE_OPENSSL=ON \
+		    -DENABLE_COMMONCRYPTO=OFF \
+		    -DOPENSSL_CRYPTO_LIBRARY=$(TOP)/openssl/libcrypto.so \
+		    -DOPENSSL_SSL_LIBRARY=$(TOP)/openssl/libssl.so \
+		    -DOPENSSL_INCLUDE_DIR=$(TOP)/openssl/include
+
+LIBZIP_STAGING_DIR=$(TOP)/_staging/usr
+LIBZIP_EXTRA_CFLAGS=-I$(TOP)/_staging/usr/include $(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib/include -I$(TOP)/openssl/include
+LIBZIP_EXTRA_LDFLAGS=-L$(TOP)/_staging/usr/lib -L$(TOP)/zlib -L$(TOP)/openssl -lz -lssl -lcrypto
+
+
+libzip-configure: gzip openssl
+	$(call CMakeClean,$(LIBZIP_PKG_BUILD_DIR))
+	$(call CMakeConfigure,$(LIBZIP_PKG_BUILD_DIR),$(LIBZIP_STAGING_DIR),$(LIBZIP_CMAKE_OPTIONS),$(LIBZIP_EXTRA_CFLAGS),$(LIBZIP_EXTRA_LDFLAGS)) 
+
+libzip: gzip openssl
+	$(MAKE) -C libzip
+
+libzip-install:
+	install -D libzip/lib/libzip.so.5.0 $(INSTALLDIR)/libzip/usr/lib/libzip.so.5.0
+	cd $(INSTALLDIR)/libzip/usr/lib ; ln -s libzip.so.5.0 libzip.so.5  ; true
+	cd $(INSTALLDIR)/libzip/usr/lib ; ln -s libzip.so.5.0 libzip.so  ; true
+
+libzip-clean:
+	if [ -e "$(LIBZIP_PKG_BUILD_DIR)/Makefile" ]; then $(MAKE) -C libzip clean ; fi
