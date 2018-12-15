994ec9a71b2dcd29d330bfc90008eb58b240426d
BrainSlayer <s.gottschall@dd-wrt.com>
2018-12-15 05:15:33 +0000

    just some dirty math, since its multiplied by 450 later
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@37987 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/libwireless/mac80211info.c
+++ src/router/libutils/libwireless/mac80211info.c
@@ -440,7 +440,8 @@ int mac80211_get_coverageclass(char *interface)
 			char state[32];
 			fscanf(fp, "%d %d %s", &rawack, &ack, state);
 			fclose(fp);
-			ack /= 3;
+			ack = (ack + 1) / 2; // make single strip
+			ack = (ack + 2) / 3; // do the coverage class
 			unlock();
 			return ack;
 		}
