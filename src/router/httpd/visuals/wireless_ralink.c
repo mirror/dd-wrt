f24d231a8bde22c71b50954607a2d98e0768b54a
BrainSlayer <s.gottschall@dd-wrt.com>
2018-12-20 08:50:36 +0000

    if not available. keep it empty
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38062 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/wireless_ralink.c
+++ src/router/httpd/visuals/wireless_ralink.c
@@ -332,7 +332,7 @@ int ej_active_wireless_if(webs_t wp, int argc, char_t ** argv, char *ifname, int
 				char str[64] = { 0 };
 
 				websWrite(wp,
-					  "'%s','/N/A','%s','%s','%s','%s','%s','%d','%d','%d','%d'", mac, getRADev(ifname), UPTIME(table.Entry[i].ConnectedTime, str), tx, rx, info, table.Entry[i].AvgRssi0, -95,
+					  "'%s','','%s','%s','%s','%s','%s','%d','%d','%d','%d'", mac, getRADev(ifname), UPTIME(table.Entry[i].ConnectedTime, str), tx, rx, info, table.Entry[i].AvgRssi0, -95,
 					  (table.Entry[i].AvgRssi0 - (-95)), qual);
 			}
 		}
