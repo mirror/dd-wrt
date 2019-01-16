56f44469777bc6a3625dce0800ef0a7d65de728a
BrainSlayer <s.gottschall@dd-wrt.com>
2019-01-16 11:56:37 +0000

    quirk
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38312 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/drivers/mtd/devices/ar7240_flash.c
+++ src/linux/universal/linux-3.18/drivers/mtd/devices/ar7240_flash.c
@@ -98,11 +98,11 @@ int guessbootsize(void *offset, unsigned int maxscan)
 			printk(KERN_EMERG "alpha SEAMA found\n");
 			return i * 4;	// redboot, lzma image
 		}
-		if (ofs[i + 0x8000] == 0x27051956) {
-			printk(KERN_EMERG "uboot detected (MMS344 Quirk)\n");
-			return (i * 4) + 0x20000;	// uboot, lzma image		
-		}
 		if (ofs[i] == 0x27051956) {
+			if (ofs[i + 0x8000] == 0x27051956) {
+				printk(KERN_EMERG "uboot detected (MMS344 Quirk)\n");
+				return (i * 4) + 0x20000;	// uboot, lzma image		
+			}
 			printk(KERN_EMERG "uboot detected\n");
 			return i * 4;	// uboot, lzma image
 		}
