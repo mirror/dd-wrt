26a77f11947c5ecb695e60b872f961e1dd436bea
BrainSlayer <s.gottschall@dd-wrt.com>
2019-01-16 15:54:51 +0000

    add site-survey link to wireless for cust
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38318 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/dd-wrt.c
+++ src/router/httpd/visuals/dd-wrt.c
@@ -4355,6 +4355,13 @@ void ej_gen_timer_fields(webs_t wp, int argc, char_t ** argv)
 
 void ej_show_wireless(webs_t wp, int argc, char_t ** argv)
 {
+#ifdef HAVE_ANTAIRA
+        websWrite(wp, "<div class=\"center\">");
+        websWrite(wp, "<input title=\"Site survey\" class=\"button\" type=\"button\" name=\"site_survey\" value=\"Wireless site survey\" onclick=\"openWindow('Site_Survey.asp', 760, 700)\"/>");
+        websWrite(wp, "<br></br>");
+        websWrite(wp, "</div>");
+#endif
+
 #ifndef HAVE_MADWIFI
 #if defined(HAVE_NORTHSTAR) || defined(HAVE_80211AC) && !defined(HAVE_BUFFALO)
 	if (!nvram_matchi("nocountrysel", 1)) {
