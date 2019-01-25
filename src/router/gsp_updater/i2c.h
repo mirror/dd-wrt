19e3cb65d9472450775520d6095fcac693730f54
BrainSlayer <s.gottschall@dd-wrt.com>
2019-01-25 21:10:01 +0000

    update gsc
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38398 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/gsp_updater/i2c.h
+++ src/router/gsp_updater/i2c.h
@@ -37,7 +37,7 @@
  */
 struct i2c_msg {
 	__u16 addr;	/* slave address			*/
-	unsigned short flags;		
+	unsigned short flags;
 #define I2C_M_TEN	0x10	/* we have a ten bit chip address	*/
 #define I2C_M_RD	0x01
 #define I2C_M_NOSTART	0x4000
@@ -55,16 +55,16 @@ struct i2c_msg {
 #define I2C_FUNC_PROTOCOL_MANGLING	0x00000004 /* I2C_M_{REV_DIR_ADDR,NOSTART,..} */
 #define I2C_FUNC_SMBUS_PEC		0x00000008
 #define I2C_FUNC_SMBUS_BLOCK_PROC_CALL	0x00008000 /* SMBus 2.0 */
-#define I2C_FUNC_SMBUS_QUICK		0x00010000 
-#define I2C_FUNC_SMBUS_READ_BYTE	0x00020000 
-#define I2C_FUNC_SMBUS_WRITE_BYTE	0x00040000 
-#define I2C_FUNC_SMBUS_READ_BYTE_DATA	0x00080000 
-#define I2C_FUNC_SMBUS_WRITE_BYTE_DATA	0x00100000 
-#define I2C_FUNC_SMBUS_READ_WORD_DATA	0x00200000 
-#define I2C_FUNC_SMBUS_WRITE_WORD_DATA	0x00400000 
-#define I2C_FUNC_SMBUS_PROC_CALL	0x00800000 
-#define I2C_FUNC_SMBUS_READ_BLOCK_DATA	0x01000000 
-#define I2C_FUNC_SMBUS_WRITE_BLOCK_DATA 0x02000000 
+#define I2C_FUNC_SMBUS_QUICK		0x00010000
+#define I2C_FUNC_SMBUS_READ_BYTE	0x00020000
+#define I2C_FUNC_SMBUS_WRITE_BYTE	0x00040000
+#define I2C_FUNC_SMBUS_READ_BYTE_DATA	0x00080000
+#define I2C_FUNC_SMBUS_WRITE_BYTE_DATA	0x00100000
+#define I2C_FUNC_SMBUS_READ_WORD_DATA	0x00200000
+#define I2C_FUNC_SMBUS_WRITE_WORD_DATA	0x00400000
+#define I2C_FUNC_SMBUS_PROC_CALL	0x00800000
+#define I2C_FUNC_SMBUS_READ_BLOCK_DATA	0x01000000
+#define I2C_FUNC_SMBUS_WRITE_BLOCK_DATA 0x02000000
 #define I2C_FUNC_SMBUS_READ_I2C_BLOCK	0x04000000 /* I2C-like block xfer  */
 #define I2C_FUNC_SMBUS_WRITE_I2C_BLOCK	0x08000000 /* w/ 1-byte reg. addr. */
 #define I2C_FUNC_SMBUS_READ_I2C_BLOCK_2	 0x10000000 /* I2C-like block xfer  */
@@ -86,10 +86,10 @@ struct i2c_msg {
 /* Old name, for compatibility */
 #define I2C_FUNC_SMBUS_HWPEC_CALC	I2C_FUNC_SMBUS_PEC
 
-/* 
- * Data for SMBus Messages 
+/*
+ * Data for SMBus Messages
  */
-#define I2C_SMBUS_BLOCK_MAX	32	/* As specified in SMBus standard */	
+#define I2C_SMBUS_BLOCK_MAX	32	/* As specified in SMBus standard */
 #define I2C_SMBUS_I2C_BLOCK_MAX	32	/* Not specified but we use same structure */
 union i2c_smbus_data {
 	__u8 byte;
@@ -102,11 +102,11 @@ union i2c_smbus_data {
 #define I2C_SMBUS_READ	1
 #define I2C_SMBUS_WRITE	0
 
-/* SMBus transaction types (size parameter in the above functions) 
+/* SMBus transaction types (size parameter in the above functions)
    Note: these no longer correspond to the (arbitrary) PIIX4 internal codes! */
 #define I2C_SMBUS_QUICK		    0
 #define I2C_SMBUS_BYTE		    1
-#define I2C_SMBUS_BYTE_DATA	    2 
+#define I2C_SMBUS_BYTE_DATA	    2
 #define I2C_SMBUS_WORD_DATA	    3
 #define I2C_SMBUS_PROC_CALL	    4
 #define I2C_SMBUS_BLOCK_DATA	    5
@@ -116,8 +116,8 @@ union i2c_smbus_data {
 
 
 /* ----- commands for the ioctl like i2c_command call:
- * note that additional calls are defined in the algorithm and hw 
- *	dependent layers - these can be listed here, or see the 
+ * note that additional calls are defined in the algorithm and hw
+ *	dependent layers - these can be listed here, or see the
  *	corresponding header files.
  */
 				/* -> bit-adapter specific ioctls	*/
@@ -162,7 +162,7 @@ struct i2c_rdwr_ioctl_data {
 };
 
 
-static inline __s32 i2c_smbus_access(int file, char read_write, __u8 command, 
+static inline __s32 i2c_smbus_access(int file, char read_write, __u8 command,
                                      int size, union i2c_smbus_data *data)
 {
 	struct i2c_smbus_ioctl_data args;
@@ -179,7 +179,7 @@ static inline __s32 i2c_smbus_write_quick(int file, __u8 value)
 {
 	return i2c_smbus_access(file,value,0,I2C_SMBUS_QUICK,NULL);
 }
-	
+
 static inline __s32 i2c_smbus_read_byte(int file)
 {
 	union i2c_smbus_data data;
@@ -205,7 +205,7 @@ static inline __s32 i2c_smbus_read_byte_data(int file, __u8 command)
 		return 0x0FF & data.byte;
 }
 
-static inline __s32 i2c_smbus_write_byte_data(int file, __u8 command, 
+static inline __s32 i2c_smbus_write_byte_data(int file, __u8 command,
                                               __u8 value)
 {
 	union i2c_smbus_data data;
@@ -224,7 +224,7 @@ static inline __s32 i2c_smbus_read_word_data(int file, __u8 command)
 		return 0x0FFFF & data.word;
 }
 
-static inline __s32 i2c_smbus_write_word_data(int file, __u8 command, 
+static inline __s32 i2c_smbus_write_word_data(int file, __u8 command,
                                               __u16 value)
 {
 	union i2c_smbus_data data;
@@ -246,7 +246,7 @@ static inline __s32 i2c_smbus_process_call(int file, __u8 command, __u16 value)
 
 
 /* Returns the number of read bytes */
-static inline __s32 i2c_smbus_read_block_data(int file, __u8 command, 
+static inline __s32 i2c_smbus_read_block_data(int file, __u8 command,
                                               __u8 *values)
 {
 	union i2c_smbus_data data;
@@ -261,7 +261,7 @@ static inline __s32 i2c_smbus_read_block_data(int file, __u8 command,
 	}
 }
 
-static inline __s32 i2c_smbus_write_block_data(int file, __u8 command, 
+static inline __s32 i2c_smbus_write_block_data(int file, __u8 command,
                                                __u8 length, __u8 *values)
 {
 	union i2c_smbus_data data;
