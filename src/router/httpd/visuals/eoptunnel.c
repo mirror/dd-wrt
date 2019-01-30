ed8278c3c4d078f9a86d77e4c145ca791ec4e75f
BrainSlayer <s.gottschall@dd-wrt.com>
2019-01-30 23:15:43 +0000

    length shouldnt be restricted
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38535 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/eoptunnel.c
+++ src/router/httpd/visuals/eoptunnel.c
@@ -195,7 +195,7 @@ void ej_show_eop_tunnels(webs_t wp, int argc, char_t ** argv)
 						websWrite(wp, "<div class=\"setting\">\n");
 						{
 							show_caption(wp, "label", "eoip.wireguard_allowedips", NULL);
-							websWrite(wp, "<input size=\"18\" maxlength=\"18\" name=\"%s\" value=\"%s\" />\n", temp, nvram_default_get(temp, "0.0.0.0/0"));
+							websWrite(wp, "<input size=\"18\" maxlength=\"1024\" name=\"%s\" value=\"%s\" />\n", temp, nvram_default_get(temp, "0.0.0.0/0"));
 						}
 						websWrite(wp, "</div>\n");
 						snprintf(temp, sizeof(temp), "oet%d_ka%d", tun, peer);
