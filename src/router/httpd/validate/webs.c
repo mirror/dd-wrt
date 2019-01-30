72836872d729d7f4e5fa80c96e1b91f03d8a38d9
BrainSlayer <s.gottschall@dd-wrt.com>
2019-01-30 10:44:01 +0000

    fix check
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38529 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/validate/webs.c
+++ src/router/httpd/validate/webs.c
@@ -724,9 +724,10 @@ int copytonv_check(webs_t wp, const char *fmt, ...)
 	char *wl = websGetVar(wp, varbuf, NULL);
 	dd_debug(DEBUG_HTTPD, "save %s with value %s\n", varbuf, wl ? wl : "");
 	char *oldval = nvram_safe_get(varbuf);
+	int ret = strcmp(wl ? wl : "", oldval);
 	if (wl)
 		nvram_set(varbuf, wl);
-	return strcmp(wl ? wl : "", oldval);
+	return ret;
 }
 
 void copymergetonv(webs_t wp, const char *fmt, ...)
