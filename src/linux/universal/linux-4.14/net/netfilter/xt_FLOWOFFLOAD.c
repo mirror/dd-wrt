5fb5b038c4d4409f24534710ff5bd84ec4f5ea5a
BrainSlayer <s.gottschall@dd-wrt.com>
2019-02-03 13:17:18 +0000

    revert
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38566 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.14/net/netfilter/xt_FLOWOFFLOAD.c
+++ src/linux/universal/linux-4.14/net/netfilter/xt_FLOWOFFLOAD.c
@@ -8,7 +8,6 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/netfilter.h>
-#include <linux/netfilter/xt_FLOWOFFLOAD.h>
 #include <net/ip.h>
 #include <net/netfilter/nf_conntrack.h>
 #include <net/netfilter/nf_flow_table.h>
@@ -21,7 +20,6 @@ static struct delayed_work hook_work;
 struct xt_flowoffload_hook {
 	struct hlist_node list;
 	struct nf_hook_ops ops;
-	struct net *net;
 	bool registered;
 	bool used;
 };
@@ -102,9 +100,8 @@ restart:
 			continue;
 
 		hook->registered = true;
-		hook->net = dev_net(hook->ops.dev);
 		spin_unlock_bh(&hooks_lock);
-		nf_register_net_hook(hook->net, &hook->ops);
+		nf_register_net_hook(dev_net(hook->ops.dev), &hook->ops);
 		spin_lock_bh(&hooks_lock);
 		goto restart;
 	}
@@ -123,7 +120,7 @@ restart:
 
 		hlist_del(&hook->list);
 		spin_unlock_bh(&hooks_lock);
-		nf_unregister_net_hook(hook->net, &hook->ops);
+		nf_unregister_net_hook(dev_net(hook->ops.dev), &hook->ops);
 		kfree(hook);
 		spin_lock_bh(&hooks_lock);
 		goto restart;
@@ -190,43 +187,28 @@ xt_flowoffload_skip(struct sk_buff *skb)
 	return false;
 }
 
-static struct dst_entry *
-xt_flowoffload_dst(const struct nf_conn *ct, enum ip_conntrack_dir dir,
-		   const struct xt_action_param *par)
-{
-	struct dst_entry *dst = NULL;
-	struct flowi fl;
-
-	memset(&fl, 0, sizeof(fl));
-	switch (xt_family(par)) {
-	case NFPROTO_IPV4:
-		fl.u.ip4.daddr = ct->tuplehash[dir].tuple.src.u3.ip;
-		break;
-	case NFPROTO_IPV6:
-		fl.u.ip6.saddr = ct->tuplehash[dir].tuple.dst.u3.in6;
-		fl.u.ip6.daddr = ct->tuplehash[dir].tuple.src.u3.in6;
-		break;
-	}
-
-	nf_route(xt_net(par), &dst, &fl, false, xt_family(par));
-
-	return dst;
-}
-
 static int
 xt_flowoffload_route(struct sk_buff *skb, const struct nf_conn *ct,
 		   const struct xt_action_param *par,
 		   struct nf_flow_route *route, enum ip_conntrack_dir dir)
 {
-	struct dst_entry *this_dst, *other_dst;
+	struct dst_entry *this_dst = skb_dst(skb);
+	struct dst_entry *other_dst = NULL;
+	struct flowi fl;
 
-	this_dst = xt_flowoffload_dst(ct, dir, par);
-	other_dst = xt_flowoffload_dst(ct, !dir, par);
-	if (!this_dst || !other_dst)
+	memset(&fl, 0, sizeof(fl));
+	switch (xt_family(par)) {
+	case NFPROTO_IPV4:
+		fl.u.ip4.daddr = ct->tuplehash[!dir].tuple.dst.u3.ip;
+		break;
+	case NFPROTO_IPV6:
+		fl.u.ip6.daddr = ct->tuplehash[!dir].tuple.dst.u3.in6;
+		break;
+	}
+
+	nf_route(xt_net(par), &other_dst, &fl, false, xt_family(par));
+	if (!other_dst)
 		return -ENOENT;
-
-	if (dst_xfrm(this_dst) || dst_xfrm(other_dst))
-		return -EINVAL;
 
 	route->tuple[dir].dst		= this_dst;
 	route->tuple[dir].ifindex	= xt_in(par)->ifindex;
@@ -239,7 +221,6 @@ xt_flowoffload_route(struct sk_buff *skb, const struct nf_conn *ct,
 static unsigned int
 flowoffload_tg(struct sk_buff *skb, const struct xt_action_param *par)
 {
-	const struct xt_flowoffload_target_info *info = par->targinfo;
 	enum ip_conntrack_info ctinfo;
 	enum ip_conntrack_dir dir;
 	struct nf_flow_route route;
@@ -255,9 +236,6 @@ flowoffload_tg(struct sk_buff *skb, const struct xt_action_param *par)
 
 	switch (ct->tuplehash[IP_CT_DIR_ORIGINAL].tuple.dst.protonum) {
 	case IPPROTO_TCP:
-		if (ct->proto.tcp.state != TCP_CONNTRACK_ESTABLISHED)
-			return XT_CONTINUE;
-		break;
 	case IPPROTO_UDP:
 		break;
 	default:
@@ -292,9 +270,6 @@ flowoffload_tg(struct sk_buff *skb, const struct xt_action_param *par)
 	xt_flowoffload_check_device(xt_in(par));
 	xt_flowoffload_check_device(xt_out(par));
 
-	if (info->flags & XT_FLOWOFFLOAD_HW)
-		nf_flow_offload_hw_add(xt_net(par), flow, ct);
-
 	return XT_CONTINUE;
 
 err_flow_add:
@@ -309,11 +284,6 @@ err_flow_route:
 
 static int flowoffload_chk(const struct xt_tgchk_param *par)
 {
-	struct xt_flowoffload_target_info *info = par->targinfo;
-
-	if (info->flags & ~XT_FLOWOFFLOAD_MASK)
-		return -EINVAL;
-
 	return 0;
 }
 
@@ -321,8 +291,6 @@ static struct xt_target offload_tg_reg __read_mostly = {
 	.family		= NFPROTO_UNSPEC,
 	.name		= "FLOWOFFLOAD",
 	.revision	= 0,
-	.targetsize	= sizeof(struct xt_flowoffload_target_info),
-	.usersize	= sizeof(struct xt_flowoffload_target_info),
 	.checkentry	= flowoffload_chk,
 	.target		= flowoffload_tg,
 	.me		= THIS_MODULE,
@@ -330,7 +298,6 @@ static struct xt_target offload_tg_reg __read_mostly = {
 
 static int xt_flowoffload_table_init(struct nf_flowtable *table)
 {
-	table->flags = NF_FLOWTABLE_F_HW;
 	nf_flow_table_init(table);
 	return 0;
 }
@@ -340,40 +307,9 @@ static void xt_flowoffload_table_cleanup(struct nf_flowtable *table)
 	nf_flow_table_free(table);
 }
 
-static int flow_offload_netdev_event(struct notifier_block *this,
-				     unsigned long event, void *ptr)
-{
-	struct xt_flowoffload_hook *hook = NULL;
-	struct net_device *dev = netdev_notifier_info_to_dev(ptr);
-
-	if (event != NETDEV_UNREGISTER)
-		return NOTIFY_DONE;
-
-	spin_lock_bh(&hooks_lock);
-	hook = flow_offload_lookup_hook(dev);
-	if (hook) {
-		hlist_del(&hook->list);
-	}
-	spin_unlock_bh(&hooks_lock);
-	if (hook) {
-		nf_unregister_net_hook(hook->net, &hook->ops);
-		kfree(hook);
-	}
-
-	nf_flow_table_cleanup(dev_net(dev), dev);
-
-	return NOTIFY_DONE;
-}
-
-static struct notifier_block flow_offload_netdev_notifier = {
-	.notifier_call	= flow_offload_netdev_event,
-};
-
 static int __init xt_flowoffload_tg_init(void)
 {
 	int ret;
-
-	register_netdevice_notifier(&flow_offload_netdev_notifier);
 
 	INIT_DELAYED_WORK(&hook_work, xt_flowoffload_hook_work);
 
@@ -392,7 +328,6 @@ static void __exit xt_flowoffload_tg_exit(void)
 {
 	xt_unregister_target(&offload_tg_reg);
 	xt_flowoffload_table_cleanup(&nf_flowtable);
-	unregister_netdevice_notifier(&flow_offload_netdev_notifier);
 }
 
 MODULE_LICENSE("GPL");
