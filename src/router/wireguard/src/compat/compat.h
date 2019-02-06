701abe9b0f11fe8e70773f433ad58c880308ff91
BrainSlayer <s.gottschall@dd-wrt.com>
2019-02-06 05:12:49 +0000

    missing gfp highmem flag
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38582 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/wireguard/src/compat/compat.h
+++ src/router/wireguard/src/compat/compat.h
@@ -475,7 +475,7 @@ static inline void *kvmalloc_ours(size_t size, gfp_t flags)
 	ret = kmalloc(size, kmalloc_flags);
 	if (ret || size <= PAGE_SIZE)
 		return ret;
-	return __vmalloc(size, flags, PAGE_KERNEL);
+	return __vmalloc(size, flags | __GFP_HIGHMEM, PAGE_KERNEL);
 }
 static inline void *kvzalloc_ours(size_t size, gfp_t flags)
 {
