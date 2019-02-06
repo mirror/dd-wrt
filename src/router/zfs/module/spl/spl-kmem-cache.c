72a133b3d96c0625e92d23dc12a1d749e13cc821
BrainSlayer <s.gottschall@dd-wrt.com>
2019-02-06 10:50:18 +0000

    remove debug
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38587 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/zfs/module/spl/spl-kmem-cache.c
+++ src/router/zfs/module/spl/spl-kmem-cache.c
@@ -203,11 +203,7 @@ kv_alloc(spl_kmem_cache_t *skc, int size, int flags)
 		ASSERT(ISP2(size));
 		ptr = (void *)__get_free_pages(lflags, get_order(size));
 	} else if (skc->skc_flags & KMC_KVMEM) {
-		    printk(KERN_INFO "kvmalloc try %d\n",size);
 		ptr = kvmalloc(size, lflags);
-		if (!ptr) {
-		    printk(KERN_INFO "kvmalloc fail\n");
-		}
 	} else {
 		ptr = __vmalloc(size, lflags | __GFP_HIGHMEM, PAGE_KERNEL);
 	}
