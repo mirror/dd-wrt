36f3eb830f121ecd4bbdd5c41c0d4b2a22cb5760
BrainSlayer <s.gottschall@dd-wrt.com>
2019-02-10 23:15:47 +0000

    fix wrong size
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38678 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/dhcpv6/auth.c
+++ src/router/dhcpv6/auth.c
@@ -222,7 +222,7 @@ hmacmd5_invalidate(hmacmd5_t *ctx)
 {
 	md5_invalidate(&ctx->md5ctx);
 	memset(ctx->key, 0, sizeof(ctx->key));
-	memset(ctx, 0, sizeof(ctx));
+	memset(ctx, 0, sizeof(*ctx));
 }
 
 /*
