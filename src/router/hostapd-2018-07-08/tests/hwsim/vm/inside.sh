e09c5fcfdd6373a0478a00bba1e07c5c1262d876
BrainSlayer <s.gottschall@dd-wrt.com>
2019-02-10 21:35:27 +0000

    update hostapd: add new files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38676 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/hostapd-2018-07-08/tests/hwsim/vm/inside.sh
+++ src/router/hostapd-2018-07-08/tests/hwsim/vm/inside.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
+# keep old /etc
+mount tmpfs -t tmpfs /tmp
+mkdir /tmp/etc
+mount --bind /etc /tmp/etc
 # mount all kinds of things
 mount tmpfs -t tmpfs /etc
 # we need our own /dev/rfkill, and don't want device access
 mount tmpfs -t tmpfs /dev
-mount tmpfs -t tmpfs /tmp
 # some sockets go into /var/run, and / is read-only
 mount tmpfs -t tmpfs /var/run
 mount proc -t proc /proc
 mount sysfs -t sysfs /sys
 # needed for tracing
 mount debugfs -t debugfs /sys/kernel/debug
+
+# for inside telnet
+mkdir /dev/pts
+mount devpts -t devpts /dev/pts
 
 export PATH=/usr/sbin:$PATH
 
@@ -22,10 +29,12 @@ sysctl kernel.panic=1
 TESTDIR=$(sed 's/.*testdir=\([^ ]*\) .*/\1/' /proc/cmdline)
 TIMEWARP=$(sed 's/.*timewarp=\([^ ]*\) .*/\1/' /proc/cmdline)
 EPATH=$(sed 's/.*EPATH=\([^ ]*\) .*/\1/' /proc/cmdline)
+TELNET=$(sed 's/.*TELNET=\([^ ]*\) .*/\1/' /proc/cmdline)
 ARGS=$(sed 's/.*ARGS=\([^ ]*\)\( \|$\).*/\1/' /proc/cmdline)
 
 # create /dev entries we need
 mknod -m 660 /dev/ttyS0 c 4 64
+mknod -m 666 /dev/ptmx c 5 2
 mknod -m 660 /dev/random c 1 8
 mknod -m 660 /dev/urandom c 1 9
 mknod -m 666 /dev/null c 1 3
@@ -35,6 +44,8 @@ test -f /sys/class/misc/rfkill/dev && \
 ln -s /proc/self/fd/0 /dev/stdin
 ln -s /proc/self/fd/1 /dev/stdout
 ln -s /proc/self/fd/2 /dev/stderr
+
+echo "VM has started up" > /dev/ttyS0
 
 # create dummy sudo - everything runs as uid 0
 mkdir /tmp/bin
@@ -71,6 +82,9 @@ udp     17      UDP
 ipv6-icmp 58	IPv6-ICMP
 EOF
 
+# we may need /etc/alternatives, at least on Debian-based systems
+ln -s /tmp/etc/alternatives /etc/
+
 # local network is needed for some tests
 ip link set lo up
 
@@ -90,7 +104,26 @@ if [ "$TIMEWARP" = "1" ] ; then
     ) &
 fi
 
+echo hwsimvm > /proc/sys/kernel/hostname
 echo 8 8 8 8 > /proc/sys/kernel/printk
+
+cat > /tmp/bin/login <<EOF
+#!/bin/sh
+
+export PS1='\h:\w\$ '
+exec bash
+EOF
+chmod +x /tmp/bin/login
+
+if [ "$TELNET" = "1" ] ; then
+  ip link set eth0 up
+  ip addr add 172.16.0.15/24 dev eth0
+  which in.telnetd >/dev/null && (
+    while true ; do
+      in.telnetd -debug 23 -L /tmp/bin/login
+    done
+  ) &
+fi
 
 # check if we're rebooting due to a kernel panic ...
 if grep -q 'Kernel panic' /tmp/logs/console ; then
