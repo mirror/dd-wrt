850ea7f13ca298e15bb7537cf1e537949ec8daae
BrainSlayer <s.gottschall@dd-wrt.com>
2019-02-12 14:45:13 +0000

    add wan-port to bridge
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38729 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/network.c
+++ src/router/services/networking/network.c
@@ -1168,7 +1168,7 @@ void start_lan(void)
 		nvram_set("et0macaddr", get_hwaddr("eth1", macaddr));
 	strcpy(mac, nvram_safe_get("et0macaddr"));
 #elif HAVE_CPE880
-	nvram_setz(lan_ifnames, "vlan1 ath0");
+	nvram_setz(lan_ifnames, "vlan1 vlan2 ath0");
 	if (getSTA() || getWET() || CANBRIDGE()) {
 		PORTSETUPWAN("");
 	} else {
