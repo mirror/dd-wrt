f7ff2928f63a0d00d5e8effc0e4ff18ddcd265eb
BrainSlayer <s.gottschall@dd-wrt.com>
2019-02-12 14:59:28 +0000

    hack for lazy vendors
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38734 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/libutils/detect.c
+++ src/router/libutils/libutils/detect.c
@@ -1072,7 +1072,10 @@ int internal_getRouterBrand()
 	}
 	fclose(fp);
 	name[len] = 0;
-	setRouter(name);
+	if (!strncmp(name, "To be filled",12))
+	    setRouter("Generic X86");
+	else 
+	    setRouter(name);
 	return ROUTER_BOARD_X86;
       generic:;
 	setRouter("Generic X86");
