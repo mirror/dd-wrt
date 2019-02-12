8f6c7eec28e27dd775191418895ebb303c0c6ac5
BrainSlayer <s.gottschall@dd-wrt.com>
2019-02-12 05:20:25 +0000

    build only static and remove orphan files after install
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38703 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/transmission.mk
+++ src/router/rules/transmission.mk
@@ -13,6 +13,8 @@ transmission-install:
 	$(MAKE) -C transmission install DESTDIR=$(INSTALLDIR)/transmission
 	mv $(INSTALLDIR)/transmission/usr/bin/transmission-daemon $(INSTALLDIR)/transmission/usr/bin/transmissiond
 	rm -rf $(INSTALLDIR)/transmission/usr/share/man
+	rm -f $(INSTALLDIR)/transmission/usr/lib/*.la
+	rm -f $(INSTALLDIR)/transmission/usr/lib/*.a
 	install -D transmission/configs/transmission.nvramconfig $(INSTALLDIR)/transmission/etc/config/transmission.nvramconfig
 	install -D transmission/configs/transmission.webnas $(INSTALLDIR)/transmission/etc/config/transmission.webnas
 
@@ -22,7 +24,7 @@ transmission-clean:
 transmission-configure: libevent-configure curl-configure
 	-cd transmission && ./autogen.sh
 	cd transmission && ./configure  --prefix=/usr ac_cv_host=$(ARCH)-uclibc-linux --target=$(ARCH)-linux --host=$(ARCH) CC="ccache $(ARCH)-linux-uclibc-gcc" \
-	--enable-daemon --disable-nls --without-systemd-daemon \
+	--enable-daemon --disable-nls --without-systemd-daemon --libdir=/usr/lib --disable-static \
 	CFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE -DNO_SYS_QUEUE_H -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib   -I$(TOP)/curl/include -I$(TOP)/openssl/include -I$(TOP)/libevent/include" \
 	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE -DNO_SYS_QUEUE_H -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib  -I$(TOP)/curl/include  -I$(TOP)/openssl/include  -I$(TOP)/libevent/include" \
 	LDFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE -DNO_SYS_QUEUE_H  -L$(TOP)/zlib   -L$(TOP)/openssl -lssl -lcrypto -L$(TOP)/libevent/.libs  -L$(TOP)/curl/lib/.libs -ldl" \
