09447baaa42b38c37f7cbe0870173bab5fff1853
kong <USER@DOMAIN>
2019-02-17 14:02:59 +0000

    fix compile
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38815 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libnetfilter_log.mk
+++ src/router/rules/libnetfilter_log.mk
@@ -1,7 +1,7 @@
 libnetfilter_log-configure: libnfnetlink
 	cd libnetfilter_log && \
 	LIBNFNETLINK_CFLAGS="-I$(TOP)/libnfnetlink/include" \
-	LIBNFNETLINK_LIBS="-L$(TOP)/libnfnetlink/src/.libs" \
+	LIBNFNETLINK_LIBS="-L$(TOP)/libnfnetlink/src/.libs -lnfnetlink" \
 	./configure \
 		--build=$(ARCH)-linux \
 		--host=$(ARCH)-linux-gnu \
@@ -10,7 +10,7 @@ libnetfilter_log-configure: libnfnetlink
 
 libnetfilter_log: libnfnetlink
 	$(MAKE) -C libnetfilter_log CFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE"
-	$(MAKE) -C libnetfilter_log/utils nfulnl_test CFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE"
+	$(MAKE) -C libnetfilter_log/utils nfulnl_test CFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE" LDFLAGS="-L$(TOP)/libnfnetlink/src/.libs -lnfnetlink"
 	#$(MAKE) -C libnetfilter_log/utils CFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE"
 
 libnetfilter_log-install:
