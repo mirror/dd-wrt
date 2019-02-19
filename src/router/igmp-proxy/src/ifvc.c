32aa78bd7f1860968d9fab31c21ad2207599a1c2
BrainSlayer <s.gottschall@dd-wrt.com>
2019-02-19 13:20:16 +0000

    format code
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38844 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/igmp-proxy/src/ifvc.c
+++ src/router/igmp-proxy/src/ifvc.c
@@ -33,130 +33,126 @@
 
 #include "igmpproxy.h"
 
-struct IfDesc IfDescVc[ MAX_IF ], *IfDescEp = IfDescVc;
+struct IfDesc IfDescVc[MAX_IF], *IfDescEp = IfDescVc;
 
 /*
 ** Builds up a vector with the interface of the machine. Calls to the other functions of 
 ** the module will fail if they are called before the vector is build.
 **          
 */
-void buildIfVc() {
-    struct ifreq IfVc[ sizeof( IfDescVc ) / sizeof( IfDescVc[ 0 ] )  ];
-    struct ifreq *IfEp;
+void buildIfVc()
+{
+	struct ifreq IfVc[sizeof(IfDescVc) / sizeof(IfDescVc[0])];
+	struct ifreq *IfEp;
 
-    int Sock;
+	int Sock;
 
-    if ( (Sock = socket( AF_INET, SOCK_DGRAM, 0 )) < 0 )
-        my_log( LOG_ERR, errno, "RAW socket open" );
+	if ((Sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+		my_log(LOG_ERR, errno, "RAW socket open");
 
-    /* get If vector
-     */
-    {
-        struct ifconf IoCtlReq;
+	/* get If vector
+	 */
+	{
+		struct ifconf IoCtlReq;
 
-        IoCtlReq.ifc_buf = (void *)IfVc;
-        IoCtlReq.ifc_len = sizeof( IfVc );
+		IoCtlReq.ifc_buf = (void *)IfVc;
+		IoCtlReq.ifc_len = sizeof(IfVc);
 
-        if ( ioctl( Sock, SIOCGIFCONF, &IoCtlReq ) < 0 )
-            my_log( LOG_ERR, errno, "ioctl SIOCGIFCONF" );
+		if (ioctl(Sock, SIOCGIFCONF, &IoCtlReq) < 0)
+			my_log(LOG_ERR, errno, "ioctl SIOCGIFCONF");
 
-        IfEp = (void *)((char *)IfVc + IoCtlReq.ifc_len);
-    }
+		IfEp = (void *)((char *)IfVc + IoCtlReq.ifc_len);
+	}
 
-    /* loop over interfaces and copy interface info to IfDescVc
-     */
-    {
-        struct ifreq  *IfPt, *IfNext;
+	/* loop over interfaces and copy interface info to IfDescVc
+	 */
+	{
+		struct ifreq *IfPt, *IfNext;
 
-        // Temp keepers of interface params...
-        uint32_t addr, subnet, mask;
+		// Temp keepers of interface params...
+		uint32_t addr, subnet, mask;
 
-        for ( IfPt = IfVc; IfPt < IfEp; IfPt = IfNext ) {
-            struct ifreq IfReq;
-            char FmtBu[ 32 ];
+		for (IfPt = IfVc; IfPt < IfEp; IfPt = IfNext) {
+			struct ifreq IfReq;
+			char FmtBu[32];
 
-	    IfNext = (struct ifreq *)((char *)&IfPt->ifr_addr +
+			IfNext = (struct ifreq *)((char *)&IfPt->ifr_addr +
 #ifdef HAVE_STRUCT_SOCKADDR_SA_LEN
-				    IfPt->ifr_addr.sa_len
+						  IfPt->ifr_addr.sa_len
 #else
-				    sizeof(struct sockaddr_in)
+						  sizeof(struct sockaddr_in)
 #endif
-		    );
-	    if (IfNext < IfPt + 1)
-		    IfNext = IfPt + 1;
+			    );
+			if (IfNext < IfPt + 1)
+				IfNext = IfPt + 1;
 
-            strncpy( IfDescEp->Name, IfPt->ifr_name, sizeof( IfDescEp->Name ) );
+			strncpy(IfDescEp->Name, IfPt->ifr_name, sizeof(IfDescEp->Name));
 
-            // Currently don't set any allowed nets...
-            //IfDescEp->allowednets = NULL;
+			// Currently don't set any allowed nets...
+			//IfDescEp->allowednets = NULL;
 
-            // Set the index to -1 by default.
-            IfDescEp->index = -1;
+			// Set the index to -1 by default.
+			IfDescEp->index = -1;
 
-            /* don't retrieve more info for non-IP interfaces
-             */
-            if ( IfPt->ifr_addr.sa_family != AF_INET ) {
-                IfDescEp->InAdr.s_addr = 0;  /* mark as non-IP interface */
-                IfDescEp++;
-                continue;
-            }
+			/* don't retrieve more info for non-IP interfaces
+			 */
+			if (IfPt->ifr_addr.sa_family != AF_INET) {
+				IfDescEp->InAdr.s_addr = 0;	/* mark as non-IP interface */
+				IfDescEp++;
+				continue;
+			}
+			// Get the interface adress...
+			IfDescEp->InAdr = ((struct sockaddr_in *)&IfPt->ifr_addr)->sin_addr;
+			addr = IfDescEp->InAdr.s_addr;
 
-            // Get the interface adress...
-            IfDescEp->InAdr = ((struct sockaddr_in *)&IfPt->ifr_addr)->sin_addr;
-            addr = IfDescEp->InAdr.s_addr;
+			memcpy(IfReq.ifr_name, IfDescEp->Name, sizeof(IfReq.ifr_name));
+			IfReq.ifr_addr.sa_family = AF_INET;
+			((struct sockaddr_in *)&IfReq.ifr_addr)->sin_addr.s_addr = addr;
 
-            memcpy( IfReq.ifr_name, IfDescEp->Name, sizeof( IfReq.ifr_name ) );
-            IfReq.ifr_addr.sa_family = AF_INET;
-            ((struct sockaddr_in *)&IfReq.ifr_addr)->sin_addr.s_addr = addr;
+			// Get the subnet mask...
+			if (ioctl(Sock, SIOCGIFNETMASK, &IfReq) < 0)
+				my_log(LOG_ERR, errno, "ioctl SIOCGIFNETMASK for %s", IfReq.ifr_name);
+			mask = ((struct sockaddr_in *)&IfReq.ifr_addr)->sin_addr.s_addr;
+			subnet = addr & mask;
 
-            // Get the subnet mask...
-            if (ioctl(Sock, SIOCGIFNETMASK, &IfReq ) < 0)
-                my_log(LOG_ERR, errno, "ioctl SIOCGIFNETMASK for %s", IfReq.ifr_name);
-            mask = ((struct sockaddr_in *)&IfReq.ifr_addr)->sin_addr.s_addr;
-            subnet = addr & mask;
+			/* get if flags
+			 **
+			 ** typical flags:
+			 ** lo    0x0049 -> Running, Loopback, Up
+			 ** ethx  0x1043 -> Multicast, Running, Broadcast, Up
+			 ** ipppx 0x0091 -> NoArp, PointToPoint, Up 
+			 ** grex  0x00C1 -> NoArp, Running, Up
+			 ** ipipx 0x00C1 -> NoArp, Running, Up
+			 */
+			if (ioctl(Sock, SIOCGIFFLAGS, &IfReq) < 0)
+				my_log(LOG_ERR, errno, "ioctl SIOCGIFFLAGS");
 
-            /* get if flags
-            **
-            ** typical flags:
-            ** lo    0x0049 -> Running, Loopback, Up
-            ** ethx  0x1043 -> Multicast, Running, Broadcast, Up
-            ** ipppx 0x0091 -> NoArp, PointToPoint, Up 
-            ** grex  0x00C1 -> NoArp, Running, Up
-            ** ipipx 0x00C1 -> NoArp, Running, Up
-            */
-            if ( ioctl( Sock, SIOCGIFFLAGS, &IfReq ) < 0 )
-                my_log( LOG_ERR, errno, "ioctl SIOCGIFFLAGS" );
+			IfDescEp->Flags = IfReq.ifr_flags;
 
-            IfDescEp->Flags = IfReq.ifr_flags;
+			// Insert the verified subnet as an allowed net...
+			IfDescEp->allowednets = (struct SubnetList *)malloc(sizeof(struct SubnetList));
+			if (IfDescEp->allowednets == NULL)
+				my_log(LOG_ERR, 0, "Out of memory !");
 
-            // Insert the verified subnet as an allowed net...
-            IfDescEp->allowednets = (struct SubnetList *)malloc(sizeof(struct SubnetList));
-            if(IfDescEp->allowednets == NULL) my_log(LOG_ERR, 0, "Out of memory !");
-            
-            // Create the network address for the IF..
-            IfDescEp->allowednets->next = NULL;
-            IfDescEp->allowednets->subnet_mask = mask;
-            IfDescEp->allowednets->subnet_addr = subnet;
+			// Create the network address for the IF..
+			IfDescEp->allowednets->next = NULL;
+			IfDescEp->allowednets->subnet_mask = mask;
+			IfDescEp->allowednets->subnet_addr = subnet;
 
-            // Set the default params for the IF...
-            IfDescEp->state         = IF_STATE_DISABLED;
-            IfDescEp->robustness    = DEFAULT_ROBUSTNESS;
-            IfDescEp->threshold     = DEFAULT_THRESHOLD;   /* ttl limit */
-            IfDescEp->ratelimit     = DEFAULT_RATELIMIT; 
-            
+			// Set the default params for the IF...
+			IfDescEp->state = IF_STATE_DISABLED;
+			IfDescEp->robustness = DEFAULT_ROBUSTNESS;
+			IfDescEp->threshold = DEFAULT_THRESHOLD;	/* ttl limit */
+			IfDescEp->ratelimit = DEFAULT_RATELIMIT;
 
-            // Debug log the result...
-            my_log( LOG_DEBUG, 0, "buildIfVc: Interface %s Addr: %s, Flags: 0x%04x, Network: %s",
-                 IfDescEp->Name,
-                 fmtInAdr( FmtBu, IfDescEp->InAdr ),
-                 IfDescEp->Flags,
-                 inetFmts(subnet,mask, s1));
+			// Debug log the result...
+			my_log(LOG_DEBUG, 0, "buildIfVc: Interface %s Addr: %s, Flags: 0x%04x, Network: %s", IfDescEp->Name, fmtInAdr(FmtBu, IfDescEp->InAdr), IfDescEp->Flags, inetFmts(subnet, mask, s1));
 
-            IfDescEp++;
-        } 
-    }
+			IfDescEp++;
+		}
+	}
 
-    close( Sock );
+	close(Sock);
 }
 
 /*
@@ -166,14 +162,15 @@ void buildIfVc() {
 **          - NULL if no interface 'IfName' exists
 **          
 */
-struct IfDesc *getIfByName( const char *IfName ) {
-    struct IfDesc *Dp;
+struct IfDesc *getIfByName(const char *IfName)
+{
+	struct IfDesc *Dp;
 
-    for ( Dp = IfDescVc; Dp < IfDescEp; Dp++ )
-        if ( ! strcmp( IfName, Dp->Name ) )
-            return Dp;
+	for (Dp = IfDescVc; Dp < IfDescEp; Dp++)
+		if (!strcmp(IfName, Dp->Name))
+			return Dp;
 
-    return NULL;
+	return NULL;
 }
 
 /*
@@ -183,9 +180,10 @@ struct IfDesc *getIfByName( const char *IfName ) {
 **          - NULL if no interface 'Ix' exists
 **          
 */
-struct IfDesc *getIfByIx( unsigned Ix ) {
-    struct IfDesc *Dp = &IfDescVc[ Ix ];
-    return Dp < IfDescEp ? Dp : NULL;
+struct IfDesc *getIfByIx(unsigned Ix)
+{
+	struct IfDesc *Dp = &IfDescVc[Ix];
+	return Dp < IfDescEp ? Dp : NULL;
 }
 
 /**
@@ -193,63 +191,62 @@ struct IfDesc *getIfByIx( unsigned Ix ) {
 *   the supplied IP adress. The IP must match a interfaces
 *   subnet, or any configured allowed subnet on a interface.
 */
-struct IfDesc *getIfByAddress( uint32_t ipaddr ) {
+struct IfDesc *getIfByAddress(uint32_t ipaddr)
+{
 
-    struct IfDesc       *Dp;
-    struct SubnetList   *currsubnet;
-    struct IfDesc       *res = NULL;
-    uint32_t            last_subnet_mask = 0;
+	struct IfDesc *Dp;
+	struct SubnetList *currsubnet;
+	struct IfDesc *res = NULL;
+	uint32_t last_subnet_mask = 0;
 
-    for ( Dp = IfDescVc; Dp < IfDescEp; Dp++ ) {
-        // Loop through all registered allowed nets of the VIF...
-        for(currsubnet = Dp->allowednets; currsubnet != NULL; currsubnet = currsubnet->next) {
-            // Check if the ip falls in under the subnet....
-            if(currsubnet->subnet_mask > last_subnet_mask && (ipaddr & currsubnet->subnet_mask) == currsubnet->subnet_addr) {
-                res = Dp;
-                last_subnet_mask = currsubnet->subnet_mask;
-            }
-        }
-    }
-    return res;
+	for (Dp = IfDescVc; Dp < IfDescEp; Dp++) {
+		// Loop through all registered allowed nets of the VIF...
+		for (currsubnet = Dp->allowednets; currsubnet != NULL; currsubnet = currsubnet->next) {
+			// Check if the ip falls in under the subnet....
+			if (currsubnet->subnet_mask > last_subnet_mask && (ipaddr & currsubnet->subnet_mask) == currsubnet->subnet_addr) {
+				res = Dp;
+				last_subnet_mask = currsubnet->subnet_mask;
+			}
+		}
+	}
+	return res;
 }
-
 
 /**
 *   Returns a pointer to the IfDesc whose subnet matches
 *   the supplied IP adress. The IP must match a interfaces
 *   subnet, or any configured allowed subnet on a interface.
 */
-struct IfDesc *getIfByVifIndex( unsigned vifindex ) {
-    struct IfDesc       *Dp;
-    if(vifindex>0) {
-        for ( Dp = IfDescVc; Dp < IfDescEp; Dp++ ) {
-            if(Dp->index == vifindex) {
-                return Dp;
-            }
-        }
-    }
-    return NULL;
+struct IfDesc *getIfByVifIndex(unsigned vifindex)
+{
+	struct IfDesc *Dp;
+	if (vifindex > 0) {
+		for (Dp = IfDescVc; Dp < IfDescEp; Dp++) {
+			if (Dp->index == vifindex) {
+				return Dp;
+			}
+		}
+	}
+	return NULL;
 }
-
 
 /**
 *   Function that checks if a given ipaddress is a valid
 *   address for the supplied VIF.
 */
-int isAdressValidForIf( struct IfDesc* intrface, uint32_t ipaddr ) {
-    struct SubnetList   *currsubnet;
-    
-    if(intrface == NULL) {
-        return 0;
-    }
-    // Loop through all registered allowed nets of the VIF...
-    for(currsubnet = intrface->allowednets; currsubnet != NULL; currsubnet = currsubnet->next) {
-        // Check if the ip falls in under the subnet....
-        if((ipaddr & currsubnet->subnet_mask) == currsubnet->subnet_addr) {
-            return 1;
-        }
-    }
-    return 0;
+int isAdressValidForIf(struct IfDesc *intrface, uint32_t ipaddr)
+{
+	struct SubnetList *currsubnet;
+
+	if (intrface == NULL) {
+		return 0;
+	}
+	// Loop through all registered allowed nets of the VIF...
+	for (currsubnet = intrface->allowednets; currsubnet != NULL; currsubnet = currsubnet->next) {
+		// Check if the ip falls in under the subnet....
+		if ((ipaddr & currsubnet->subnet_mask) == currsubnet->subnet_addr) {
+			return 1;
+		}
+	}
+	return 0;
 }
-
-
--- src/router/igmp-proxy/src/lib.c
+++ src/router/igmp-proxy/src/lib.c
@@ -36,62 +36,63 @@
 /*
  * Exported variables.
  */
-char s1[19];        /* buffers to hold the string representations  */
-char s2[19];        /* of IP addresses, to be passed to inet_fmt() */
-char s3[19];        /* or inet_fmts().                             */
+char s1[19];			/* buffers to hold the string representations  */
+char s2[19];			/* of IP addresses, to be passed to inet_fmt() */
+char s3[19];			/* or inet_fmts().                             */
 char s4[19];
-            
+
 /*
 ** Formats 'InAdr' into a dotted decimal string. 
 **
 ** returns: - pointer to 'St'
 **          
 */
-char *fmtInAdr( char *St, struct in_addr InAdr ) {
-    sprintf( St, "%u.%u.%u.%u", 
-             ((uint8_t *)&InAdr.s_addr)[ 0 ],
-             ((uint8_t *)&InAdr.s_addr)[ 1 ],
-             ((uint8_t *)&InAdr.s_addr)[ 2 ],
-             ((uint8_t *)&InAdr.s_addr)[ 3 ] );
+char *fmtInAdr(char *St, struct in_addr InAdr)
+{
+	sprintf(St, "%u.%u.%u.%u", ((uint8_t *) & InAdr.s_addr)[0], ((uint8_t *) & InAdr.s_addr)[1], ((uint8_t *) & InAdr.s_addr)[2], ((uint8_t *) & InAdr.s_addr)[3]);
 
-    return St;
+	return St;
 }
 
 /*
  * Convert an IP address in u_long (network) format into a printable string.
  */
-char *inetFmt(uint32_t addr, char *s) {
-    register u_char *a;
+char *inetFmt(uint32_t addr, char *s)
+{
+	register u_char *a;
 
-    a = (u_char *)&addr;
-    sprintf(s, "%u.%u.%u.%u", a[0], a[1], a[2], a[3]);
-    return(s);
+	a = (u_char *) & addr;
+	sprintf(s, "%u.%u.%u.%u", a[0], a[1], a[2], a[3]);
+	return (s);
 }
-
 
 /*
  * Convert an IP subnet number in u_long (network) format into a printable
  * string including the netmask as a number of bits.
  */
-char *inetFmts(uint32_t addr, uint32_t mask, char *s) {
-    register u_char *a, *m;
-    int bits;
+char *inetFmts(uint32_t addr, uint32_t mask, char *s)
+{
+	register u_char *a, *m;
+	int bits;
 
-    if ((addr == 0) && (mask == 0)) {
-        sprintf(s, "default");
-        return(s);
-    }
-    a = (u_char *)&addr;
-    m = (u_char *)&mask;
-    bits = 33 - ffs(ntohl(mask));
+	if ((addr == 0) && (mask == 0)) {
+		sprintf(s, "default");
+		return (s);
+	}
+	a = (u_char *) & addr;
+	m = (u_char *) & mask;
+	bits = 33 - ffs(ntohl(mask));
 
-    if (m[3] != 0) sprintf(s, "%u.%u.%u.%u/%d", a[0], a[1], a[2], a[3],
-                           bits);
-    else if (m[2] != 0) sprintf(s, "%u.%u.%u/%d",    a[0], a[1], a[2], bits);
-    else if (m[1] != 0) sprintf(s, "%u.%u/%d",       a[0], a[1], bits);
-    else                sprintf(s, "%u/%d",          a[0], bits);
+	if (m[3] != 0)
+		sprintf(s, "%u.%u.%u.%u/%d", a[0], a[1], a[2], a[3], bits);
+	else if (m[2] != 0)
+		sprintf(s, "%u.%u.%u/%d", a[0], a[1], a[2], bits);
+	else if (m[1] != 0)
+		sprintf(s, "%u.%u/%d", a[0], a[1], bits);
+	else
+		sprintf(s, "%u/%d", a[0], bits);
 
-    return(s);
+	return (s);
 }
 
 /*
@@ -112,37 +113,35 @@ char *inetFmts(uint32_t addr, uint32_t mask, char *s) {
  * Checksum routine for Internet Protocol family headers (C Version)
  *
  */
-uint16_t inetChksum(uint16_t *addr, int len) {
-    register int nleft = len;
-    register uint16_t *w = addr;
-    uint16_t answer = 0;
-    register int32_t sum = 0;
+uint16_t inetChksum(uint16_t * addr, int len)
+{
+	register int nleft = len;
+	register uint16_t *w = addr;
+	uint16_t answer = 0;
+	register int32_t sum = 0;
 
-    /*
-     *  Our algorithm is simple, using a 32 bit accumulator (sum),
-     *  we add sequential 16 bit words to it, and at the end, fold
-     *  back all the carry bits from the top 16 bits into the lower
-     *  16 bits.
-     */
-    while (nleft > 1) {
-        sum += *w++;
-        nleft -= 2;
-    }
+	/*
+	 *  Our algorithm is simple, using a 32 bit accumulator (sum),
+	 *  we add sequential 16 bit words to it, and at the end, fold
+	 *  back all the carry bits from the top 16 bits into the lower
+	 *  16 bits.
+	 */
+	while (nleft > 1) {
+		sum += *w++;
+		nleft -= 2;
+	}
 
-    /* mop up an odd byte, if necessary */
-    if (nleft == 1) {
-        *(uint8_t *) (&answer) = *(uint8_t *)w ;
-        sum += answer;
-    }
+	/* mop up an odd byte, if necessary */
+	if (nleft == 1) {
+		*(uint8_t *) (&answer) = *(uint8_t *) w;
+		sum += answer;
+	}
 
-    /*
-     * add back carry outs from top 16 bits to low 16 bits
-     */
-    sum = (sum >> 16) + (sum & 0xffff); /* add hi 16 to low 16 */
-    sum += (sum >> 16);         /* add carry */
-    answer = ~sum;              /* truncate to 16 bits */
-    return(answer);
+	/*
+	 * add back carry outs from top 16 bits to low 16 bits
+	 */
+	sum = (sum >> 16) + (sum & 0xffff);	/* add hi 16 to low 16 */
+	sum += (sum >> 16);	/* add carry */
+	answer = ~sum;		/* truncate to 16 bits */
+	return (answer);
 }
-
-
-
