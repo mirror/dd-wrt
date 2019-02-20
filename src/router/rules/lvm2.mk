11e7526e3d7c8728ca24798cb4c6610cd542fb06
BrainSlayer <s.gottschall@dd-wrt.com>
2019-02-20 07:51:03 +0000

    split file
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38874 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rules/lvm2.mk
@@ -0,0 +1,16 @@
+lvm2-configure:
+	cd lvm2 && ./configure --prefix=/usr --libdir=/usr/lib --host=$(ARCH)-linux \
+		CFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF" \
+		BLKID_CFLAGS="-L$(TOP)/e2fsprogs/lib/blkid" \
+		BLKID_LIBS="-L$(TOP)/e2fsprogs/lib/blkid -lblkid"
+
+lvm2:
+	make -C lvm2 device-mapper
+
+lvm2-clean:
+	make -C lvm2 clean
+
+lvm2-install:
+	make -C lvm2 install_device-mapper DESTDIR=$(INSTALLDIR)/lvm2
+	rm -rf $(INSTALLDIR)/lvm2/usr/share
+	rm -rf $(INSTALLDIR)/lvm2/usr/include
