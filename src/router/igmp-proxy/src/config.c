f5903238106feb8ecaa68f5d3cfec95f2cf55082
BrainSlayer <s.gottschall@dd-wrt.com>
2019-02-20 12:05:42 +0000

    fix typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38885 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/igmp-proxy/src/config.c
+++ src/router/igmp-proxy/src/config.c
@@ -357,7 +357,7 @@ struct SubnetList *parseSubnetAddress(char *addrstr) {
     tmpStr = strtok(NULL, "/");
     if(tmpStr != NULL) {
         int bitcnt = atoi(tmpStr);
-        if(bitcnt <= 0 || bitcnt > 32) {
+        if(bitcnt < 0 || bitcnt > 32) {
             my_log(LOG_WARNING, 0, "The bits part of the address is invalid : %d.",tmpStr);
             return NULL;
         }
