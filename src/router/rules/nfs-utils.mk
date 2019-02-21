306a6f5946134d380ed9434279fd4cd43d2a7e1f
BrainSlayer <s.gottschall@dd-wrt.com>
2019-02-21 11:56:51 +0000

    require _GNU_SOURCE
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38901 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/nfs-utils.mk
+++ src/router/rules/nfs-utils.mk
@@ -1,7 +1,7 @@
 nfs-utils-configure: libtirpc lvm2 keyutils
 	cd nfs-utils && ./autogen.sh
 	cd nfs-utils && ./configure --enable-fast-install --with-sysroot=yes --libdir=/usr/lib --with-tirpcinclude=$(TOP)/libtirpc/tirpc --host=$(ARCH)-linux \
-		CFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -I$(TOP)/libevent -I$(TOP)/libevent/include -I$(TOP)/libtirpc -I$(TOP)/libtirpc/tirpc -I$(TOP)/lvm2/libdm  -I$(TOP)/keyutils" \
+		CFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -I$(TOP)/libevent -I$(TOP)/libevent/include -I$(TOP)/libtirpc -I$(TOP)/libtirpc/tirpc -I$(TOP)/lvm2/libdm  -I$(TOP)/keyutils -D_GNU_SOURCE" \
 		LDFLAGS="-L$(TOP)/libtirpc/src/.libs  -L$(TOP)/libevent/.libs -L$(TOP)/lvm2/libdm/ioctl -L$(TOP)/keyutils" \
 		--with-rpcgen=internal --disable-uuid --disable-gssapi --disable-static --prefix=/usr \
 		--disable-gss --disable-nfsdcltrack
