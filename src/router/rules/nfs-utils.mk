61dfa5591f60b8e0c4f8c00e32d2bb3b38e201bb
BrainSlayer <s.gottschall@dd-wrt.com>
2019-02-23 00:35:27 +0000

    fix dep
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38937 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/nfs-utils.mk
+++ src/router/rules/nfs-utils.mk
@@ -1,4 +1,4 @@
-nfs-utils-configure: libtirpc lvm2 keyutils
+nfs-utils-configure: libtirpc lvm2 keyutils krb5
 	cd nfs-utils && ./autogen.sh
 	cd nfs-utils && ./configure --enable-fast-install --with-sysroot=yes --libdir=/usr/lib --with-tirpcinclude=$(TOP)/libtirpc/tirpc --host=$(ARCH)-linux \
 		--with-rpcgen=internal --disable-uuid --disable-gssapi --disable-static --prefix=/usr \
@@ -11,7 +11,7 @@ nfs-utils-configure: libtirpc lvm2 keyutils
 		CFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -I$(TOP)/libevent -I$(TOP)/libevent/include -I$(TOP)/libtirpc -I$(TOP)/libtirpc/tirpc -I$(TOP)/lvm2/libdm  -I$(TOP)/keyutils -D_GNU_SOURCE" \
 		LDFLAGS="-L$(TOP)/libtirpc/src/.libs  -L$(TOP)/libevent/.libs -L$(TOP)/lvm2/libdm/ioctl -L$(TOP)/keyutils -L$(TOP)/krb5/src/lib" \
 
-nfs-utils: libtirpc lvm2 keyutils
+nfs-utils: libtirpc lvm2 keyutils krb5
 	make -C nfs-utils
 
 nfs-utils-clean:
