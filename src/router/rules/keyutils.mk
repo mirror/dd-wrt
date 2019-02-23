01595c9c0fe145f7fda0bae9f63f81270555b01f
BrainSlayer <s.gottschall@dd-wrt.com>
2019-02-23 00:01:48 +0000

    fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38935 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/keyutils.mk
+++ src/router/rules/keyutils.mk
@@ -6,11 +6,3 @@ keyutils-clean:
 
 keyutils-install:
 	make -C keyutils install DESTDIR=$(INSTALLDIR)/keyutils
-
-nfs-utils-configure: libtirpc lvm2
-	cd nfs-utils && ./autogen.sh
-	cd nfs-utils && ./configure --enable-fast-install --with-sysroot=yes --libdir=/usr/lib --with-tirpcinclude=$(TOP)/libtirpc/tirpc --host=$(ARCH)-linux \
-		CFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -I$(TOP)/libevent -I$(TOP)/libevent/include -I$(TOP)/libtirpc -I$(TOP)/libtirpc/tirpc -I$(TOP)/lvm2/libdm  -I$(TOP)/keyutils" \
-		LDFLAGS="-L$(TOP)/libtirpc/src/.libs  -L$(TOP)/libevent/.libs -L$(TOP)/lvm2/libdm/ioctl -L$(TOP)/keyutils" \
-		--with-rpcgen=internal --disable-uuid --disable-gssapi --disable-static --prefix=/usr \
-		--disable-gss --disable-nfsdcltrack
