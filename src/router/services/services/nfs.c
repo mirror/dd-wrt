696e45c42d8fad433b29b2d04e49ae059fa2f359
BrainSlayer <s.gottschall@dd-wrt.com>
2019-02-24 08:02:16 +0000

    remove code errors
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38941 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/nfs.c
+++ src/router/services/services/nfs.c
@@ -39,9 +39,7 @@
 void start_nfs(void)
 {
 	struct samba3_share *cs, *csnext;
-	struct samba3_shareuser *csu, *csunext;
-	struct samba3_user *samba3users, *cu, *cunext;
-	struct samba3_share *samba3shares;
+	struct samba3_share *nfsshares;
 #ifdef _SC_NPROCESSORS_ONLN
 	int cpucount = sysconf(_SC_NPROCESSORS_ONLN);
 #else
@@ -51,30 +49,44 @@ void start_nfs(void)
 		return;
 	FILE *fp = fopen("/tmp/exports", "wb");
 	if (fp) {
-		samba3shares = getsamba3shares();
-		for (cs = samba3shares; cs; cs = csnext) {
-		fprintf(fp, "%s/%s\t%s/%d\n", cs->mp, cs->sd, nvram_safe_get("lan_ipaddr"), getmask(nvram_safe_get("lan_netmask")); csnext = cs->next; free(cs);}
-			fclose(fp);}
-			mkdir("/var/lib", 0777);
-			mkdir("/var/lib/nfs", 0777);
-			insmod("ipv6 tunnel4 ip_tunnel sit xfrm_algo esp6 ah6 mip6 tunnel6 ip6_tunnel xfrm6_mode_beet xfrm6_mode_ro xfrm6_mode_transport xfrm6_mode_tunnel xfrm6_tunnel xfrm_ipcomp ipcomp6");
-			insmod("oid_registry");
-			insmod("sunrpc");
-			insmod("auth_rpcgss");
-			insmod("rpcsec_gss_krb5");
-			insmod("grace");
-			insmod("lockd");
-			insmod("exportfs");
-			insmod("nfsd");
-			eval("rpcbind");
-			dd_loginfo("NFSD", "rpcbind successfully started\n");
-			eval("rpc.mountd");
-			dd_loginfo("NFSD", "NFS Mount Daemon successfully started\n");
-			char threads[32];
-			sprintf(threads, "%d", cpucount);
-			eval("rpc.nfsd", threads);
-			dd_loginfo("NFSD", "rpc.nfsd successfully started\n"); eval("rpc.statd"); dd_loginfo("NFSD", "NSM Service Daemon successfully started\n"); eval("exportfs", "-a", "-r"); return;}
+		nfsshares = getsamba3shares();
+		for (cs = nfsshares; cs; cs = csnext) {
+			fprintf(fp, "%s/%s\t%s/%d\n", cs->mp, cs->sd, nvram_safe_get("lan_ipaddr"), getmask(nvram_safe_get("lan_netmask")));
+			csnext = cs->next;
+			free(cs);
+		}
+		fclose(fp);
+	}
+	mkdir("/var/lib", 0777);
+	mkdir("/var/lib/nfs", 0777);
+	insmod("ipv6 tunnel4 ip_tunnel sit xfrm_algo esp6 ah6 mip6 tunnel6 ip6_tunnel xfrm6_mode_beet xfrm6_mode_ro xfrm6_mode_transport xfrm6_mode_tunnel xfrm6_tunnel xfrm_ipcomp ipcomp6");
+	insmod("oid_registry");
+	insmod("sunrpc");
+	insmod("auth_rpcgss");
+	insmod("rpcsec_gss_krb5");
+	insmod("grace");
+	insmod("lockd");
+	insmod("exportfs");
+	insmod("nfsd");
+	eval("rpcbind");
+	dd_loginfo("NFSD", "rpcbind successfully started\n");
+	eval("rpc.mountd");
+	dd_loginfo("NFSD", "NFS Mount Daemon successfully started\n");
+	char threads[32];
+	sprintf(threads, "%d", cpucount);
+	eval("rpc.nfsd", threads);
+	dd_loginfo("NFSD", "rpc.nfsd successfully started\n");
+	eval("rpc.statd");
+	dd_loginfo("NFSD", "NSM Service Daemon successfully started\n");
+	eval("exportfs", "-a", "-r");
+	return;
+}
 
-			void stop_nfs(void) {
-			eval("exportfs", "-a", "-u"); eval("rpc.nfsd", "0"); stop_process("rpc.statd", "NSM Service Daemon"); stop_process("rpc.mountd", "NFS Mount Daemon");}
+void stop_nfs(void)
+{
+	eval("exportfs", "-a", "-u");
+	eval("rpc.nfsd", "0");
+	stop_process("rpc.statd", "NSM Service Daemon");
+	stop_process("rpc.mountd", "NFS Mount Daemon");
+}
 #endif
