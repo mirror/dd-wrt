5e6bf770f4c3784d5baf28cc136e5cb8d2d66df2
BrainSlayer <s.gottschall@dd-wrt.com>
2019-02-28 10:00:14 +0000

    update e2fsprogs: add new files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@38997 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/e2fsprogs/asm_types.h
@@ -0,0 +1,8 @@
+#define __S8_TYPEDEF __signed__ char
+#define __U8_TYPEDEF unsigned char
+#define __S16_TYPEDEF __signed__ short
+#define __U16_TYPEDEF unsigned short
+#define __S32_TYPEDEF __signed__ int
+#define __U32_TYPEDEF unsigned int
+#define __S64_TYPEDEF __signed__ long long
+#define __U64_TYPEDEF unsigned long long
