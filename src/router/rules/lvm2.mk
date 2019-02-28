4843874f3255600c08dfbaee20f4841bebc77dde
BrainSlayer <s.gottschall@dd-wrt.com>
2019-02-28 19:46:09 +0000

    fix dep
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@39005 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/lvm2.mk
+++ src/router/rules/lvm2.mk
@@ -1,10 +1,10 @@
-lvm2-configure:
+lvm2-configure: e2fsprogs
 	cd lvm2 && ./configure --prefix=/usr --libdir=/usr/lib --host=$(ARCH)-linux \
 		CFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF" \
 		BLKID_CFLAGS="-L$(TOP)/e2fsprogs/lib/blkid" \
-		BLKID_LIBS="-L$(TOP)/e2fsprogs/lib/blkid -lblkid"
+		BLKID_LIBS="-L$(TOP)/e2fsprogs/lib/blkid -le2fsblkid"
 
-lvm2:
+lvm2: e2fsprogs
 	make -C lvm2 device-mapper
 
 lvm2-clean:
