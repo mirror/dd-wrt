2d1015b9cc53bb10292415ae24ae66f60f8f92af
BrainSlayer <s.gottschall@dd-wrt.com>
2019-03-08 00:15:40 +0000

    add rsync
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@39112 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rules/rsync.mk
@@ -0,0 +1,15 @@
+rsync-configure:
+	cd rsync && autoconf
+	cd rsync && ./configure --host=$(ARCH)-linux --prefix=/usr --disable-locate --disable-iconv CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_malloc=malloc" 
+
+rsync:
+	$(MAKE) -C rsync
+
+rsync-clean:
+	$(MAKE) -C rsync clean
+
+rsync-install:
+	$(MAKE) -C rsync install DESTDIR=$(INSTALLDIR)/rsync
+	rm -rf $(INSTALLDIR)/rsync/usr/share
+	mkdir -p $(INSTALLDIR)/rsync/usr/sbin
+	cd $(INSTALLDIR)/rsync/usr/sbin && ln -s ../bin/rsync rsyncd
