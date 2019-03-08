b97198fda16b6a7d8a01b83c0f9e1eb0cff2517c
BrainSlayer <s.gottschall@dd-wrt.com>
2019-03-08 00:50:28 +0000

    add inbound mangeling
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@39114 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/dhcp6.c
+++ src/router/services/services/dhcp6.c
@@ -180,6 +180,11 @@ void start_dhcp6c(void)
 		eval("vconfig", "set_egress_map", wan_ifname, "0", "6");
 		eval("vconfig", "set_egress_map", wan_ifname, "1", "0");
 		insmod("nf_defrag_ipv6 nf_log_ipv6 ip6_tables nf_conntrack_ipv6 ip6table_filter ip6table_mangle xt_DSCP xt_CLASSIFY");
+		eval("iptables", "-t", "mangle", "-D", "PREROUTING", "-i", wan_ifname, "-j", "CLASSIFY", "--set-class", "0:1");
+		eval("iptables", "-t", "mangle", "-A", "PREROUTING", "-i", wan_ifname, "-j", "CLASSIFY", "--set-class", "0:1");
+		eval("ip6tables", "-t", "mangle", "-D", "PREROUTING", "-i", wan_ifname, "-j", "CLASSIFY", "--set-class", "0:1");
+		eval("ip6tables", "-t", "mangle", "-A", "PREROUTING", "-i", wan_ifname, "-j", "CLASSIFY", "--set-class", "0:1");
+
 		eval("iptables", "-t", "mangle", "-A", "POSTROUTING", "-o", wan_ifname, "-j", "CLASSIFY", "--set-class", "0:1");
 		eval("iptables", "-t", "mangle", "-A", "POSTROUTING", "-o", wan_ifname, "-p", "udp", "--dport", "67", "-j", "CLASSIFY", "--set-class", "0:0");
 		eval("ip6tables", "-t", "mangle", "-D", "POSTROUTING", "-o", wan_ifname, "-j", "CLASSIFY", "--set-class", "0:1");
