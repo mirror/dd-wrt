36add6f098af198414a700020089b94158b169f9
BrainSlayer <s.gottschall@dd-wrt.com>
2019-03-16 03:02:15 +0000

    headers
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@39188 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/shared/nfs.h
@@ -0,0 +1,14 @@
+#ifndef _nfs_h_
+#define _nfs_h_
+#include <jansson.h>
+
+struct nfs_share {
+	char mp[32];
+	char sd[64];
+	struct nfs_share *next;
+};
+
+struct nfs_share *getnfsshares(void);
+struct nfs_share *getnfsshare(char *mp, char *sd, int types);
+
+#endif
---
+++ src/router/shared/rsync.h
@@ -0,0 +1,14 @@
+#ifndef _rsync_h_
+#define _rsync_h_
+#include <jansson.h>
+
+struct rsync_share {
+	char mp[32];
+	char sd[64];
+	struct rsync_share *next;
+};
+
+struct rsync_share *getrsyncshares(void);
+struct rsync_share *getrsyncshare(char *mp, char *sd, int types);
+
+#endif
