e90420ce6c0aaf71f91d910f135bec39d5cdd32a
BrainSlayer <s.gottschall@dd-wrt.com>
2019-03-17 14:49:31 +0000

    add rsync config
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@39205 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/rsync.mk
+++ src/router/rules/rsync.mk
@@ -13,3 +13,5 @@ rsync-install:
 	rm -rf $(INSTALLDIR)/rsync/usr/share
 	mkdir -p $(INSTALLDIR)/rsync/usr/sbin
 	cd $(INSTALLDIR)/rsync/usr/sbin && ln -s ../bin/rsync rsyncd
+	install -D rsync/config/rsync.webnas $(INSTALLDIR)/rsync/etc/config/05rsync.webnas
+	install -D rsync/config/rsync.nvramconfig $(INSTALLDIR)/rsync/etc/config/rsync.nvramconfig
