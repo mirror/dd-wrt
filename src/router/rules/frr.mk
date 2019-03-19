2e9cbe9ea2e8c2306d6e3e3bd87a2b2eaa64048c
BrainSlayer <s.gottschall@dd-wrt.com>
2019-03-19 21:49:58 +0000

    new frr rules
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@39238 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/frr.mk
+++ src/router/rules/frr.mk
@@ -1,14 +1,35 @@
-frr-configure: ncurses json-c readline
+frr-configure: ncurses json-c readline libyang
 	cd frr && autoreconf --force --install
 	rm -rf frr/build
 	-mkdir -p frr/build
-	cd frr/build && ../configure CC="" CFLAGS="" LDFLAGS="" --with-vtysh-pager=less --disable-eigrpd --disable-ldpd --disable-pbrd --disable-rfptest --disable-ssd  --disable-doc --enable-clippy-only --disable-zeromq --enable-opaque-lsa --disable-nhrpd --enable-ospf-te --disable-ospfclient --enable-multipath=32  --enable-ipv6 --prefix=/usr --sysconfdir=/tmp --disable-ospf6d  --enable-vtysh --enable-user=root --enable-group=root --disable-ospfapi --disable-isisd --disable-pimd --disable-nhrpd --disable-staticd --disable-bfdd --disable-babeld --enable-pie=no
+	cd frr/build && ../configure CC="" CFLAGS="" LDFLAGS="" LD="" --with-vtysh-pager=less --disable-eigrpd --disable-ldpd --disable-pbrd --disable-rfptest --disable-ssd  --disable-doc --enable-clippy-only --disable-zeromq --enable-opaque-lsa --disable-nhrpd --enable-ospf-te --disable-ospfclient --enable-multipath=32  --enable-ipv6 --prefix=/usr --sysconfdir=/tmp --disable-ospf6d  --enable-vtysh --enable-user=root --enable-group=root --disable-ospfapi --disable-isisd --disable-pimd --disable-nhrpd --disable-staticd --disable-bfdd --disable-babeld --enable-pie=no
 	make -C frr/build lib/clippy
 	-mkdir -p frr/build/hosttools/lib
 	cd frr/build && cp -vR lib/* hosttools/lib
-	cd frr/build && ../configure --host=$(ARCH)-uclibc-linux --localstatedir=/var/run  --libdir=/usr/tmp --with-vtysh-pager=less  --disable-eigrpd --disable-pbrd --disable-ldpd  --disable-rfptest --disable-ssd --disable-doc --disable-zeromq --enable-opaque-lsa --disable-nhrpd --enable-ospf-te --disable-ospfclient --enable-multipath=32  --enable-ipv6 --prefix=/usr --sysconfdir=/tmp --disable-ospf6d  --enable-vtysh --enable-user=root --enable-group=root --disable-ospfapi --disable-isisd --disable-pimd --disable-nhrpd --disable-staticd --disable-bfdd --disable-babeld --enable-pie=no --with-libreadline=$(TOP)/readline CFLAGS="-fno-strict-aliasing -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/ -Drpl_malloc=malloc -Drpl_realloc=realloc -I$(TOP)/_staging/usr/include -I$(TOP)/frr/build" CPPFLAGS="-fno-strict-aliasing -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/ -Drpl_malloc=malloc -Drpl_realloc=realloc -I$(TOP)/_staging/usr/include -I$(TOP)/frr/build" LDFLAGS="-L$(TOP)/readline/shlib -L$(TOP)/ncurses/lib -lncurses -L$(TOP)/json-c/.libs -ljson-c" 
+	cd frr/build && ../configure \
+		--host=$(ARCH)-uclibc-linux \
+		--localstatedir=/var/run \
+		--libdir=/usr/tmp \
+		--with-vtysh-pager=less  \
+		--disable-eigrpd \
+		--disable-pbrd \
+		--disable-ldpd  \
+		--disable-rfptest \
+		--disable-ssd \
+		--disable-doc \
+		--disable-zeromq \
+		--enable-opaque-lsa \
+		--disable-nhrpd \
+		--enable-ospf-te --disable-ospfclient --enable-multipath=32  --enable-ipv6 --prefix=/usr --sysconfdir=/tmp --disable-ospf6d \
+		--enable-vtysh --enable-user=root --enable-group=root --disable-ospfapi --disable-isisd --disable-pimd --disable-nhrpd \
+		--disable-staticd --disable-bfdd --disable-babeld --enable-pie=no --with-libreadline=$(TOP)/readline \
+		CFLAGS="-fno-strict-aliasing -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/ -Drpl_malloc=malloc -Drpl_realloc=realloc -I$(TOP)/_staging/usr/include -I$(TOP)/frr/build" \
+		CPPFLAGS="-fno-strict-aliasing -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/ -Drpl_malloc=malloc -Drpl_realloc=realloc -I$(TOP)/_staging/usr/include -I$(TOP)/frr/build" \
+		LDFLAGS="-L$(TOP)/readline/shlib -L$(TOP)/ncurses/lib -lncurses -L$(TOP)/json-c/.libs -ljson-c" \
+		LIBYANG_CFLAGS="-I$(TOP)/libyang/src" \
+		LIBYANG_LIBS="-L$(TOP)/libyang -lyang -L$(TOP)/pcre/.libs -lpcre"
 
-frr: ncurses json-c
+frr: ncurses json-c libyang
 	make -C frr/build
 
 frr-install:
