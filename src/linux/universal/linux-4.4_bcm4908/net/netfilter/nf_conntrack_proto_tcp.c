b10d84687a42ac635d175fcfc34f46c1c701fe27
BrainSlayer <s.gottschall@dd-wrt.com>
2019-03-24 13:54:43 +0000

    update kernels
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@39271 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4_bcm4908/net/netfilter/nf_conntrack_proto_tcp.c
+++ src/linux/universal/linux-4.4_bcm4908/net/netfilter/nf_conntrack_proto_tcp.c
@@ -414,6 +414,8 @@ static void tcp_options(const struct sk_buff *skb,
 			length--;
 			continue;
 		default:
+			if (length < 2)
+				return;
 			opsize=*ptr++;
 			if (opsize < 2) /* "silly options" */
 				return;
@@ -474,6 +476,8 @@ static void tcp_sack(const struct sk_buff *skb, unsigned int dataoff,
 			length--;
 			continue;
 		default:
+			if (length < 2)
+				return;
 			opsize = *ptr++;
 			if (opsize < 2) /* "silly options" */
 				return;
