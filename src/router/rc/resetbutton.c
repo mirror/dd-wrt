d405cece923e4f9c0f672ae18e52b1e9b12d8f07
BrainSlayer <s.gottschall@dd-wrt.com>
2019-04-08 20:06:51 +0000

    add wps button
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@39451 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/resetbutton.c
+++ src/router/rc/resetbutton.c
@@ -1211,6 +1211,9 @@ static void resetbtn_period_check(int sig)
 	sesgpio = 0x10c;
 	val |= get_gpio(15) << 15;	//aoss pushbutton
 	val |= get_gpio(12) << 12;	//aoss pushbutton
+#elif defined(HAVE_ARCHERC7V5)
+	sesgpio = 0x102;
+	val |= get_gpio(2) << 2;	//aoss pushbutton
 #elif defined(HAVE_WR1043V4)
 	sesgpio = 0x101;
 	val |= get_gpio(1) << 1;	//aoss pushbutton
