a9cfba03c52f569afddb22ec383840a2375f9924
BrainSlayer <s.gottschall@dd-wrt.com>
2019-04-10 23:43:19 +0000

    fix some typos
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@39477 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/minidlna.c
+++ src/router/httpd/visuals/minidlna.c
@@ -139,7 +139,7 @@ void ej_dlna_sharepaths(webs_t wp, int argc, char_t ** argv)
 
 		websWrite(wp, "				<td style=\"width: 50px; text-align: center;\">\n");
 		websWrite(wp,
-			  "					<script type=\"text/javascript\">document.write(\"<input type=\\\"button\\\" class=\\\"button\\\" name=\\\"dlnashare_del%s\\\" value=\\\"\"+nas.sharedel+\"\\\"  style=\\\"width: 100%%;\\\" onclick=\\\"removeDlnaShare(this);\\\"i />\")</script>\n",
+			  "					<script type=\"text/javascript\">document.write(\"<input type=\\\"button\\\" class=\\\"button\\\" name=\\\"dlnashare_del%s\\\" value=\\\"\"+nas.sharedel+\"\\\"  style=\\\"width: 100%%;\\\" onclick=\\\"removeDlnaShare(this);\\\" />\")</script>\n",
 			  number);
 		websWrite(wp, "				</td>\n");
 		websWrite(wp, "			</tr>\n");
