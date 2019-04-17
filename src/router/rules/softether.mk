916bf9f5302e84b41e202b4998ef54cacee74d98
BrainSlayer <s.gottschall@dd-wrt.com>
2019-04-17 05:36:37 +0000

    fix rules
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@39552 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/softether.mk
+++ src/router/rules/softether.mk
@@ -25,8 +25,8 @@ softether-configure: zlib readline ncurses
 	sed -i 's/\SHARED/STATIC/g' $(TOP)/softether/host/src/Mayaqua/CMakeLists.txt
 	sed -i 's/\SHARED/STATIC/g' $(TOP)/softether/host/src/Cedar/CMakeLists.txt
 	sed -i 's/\readline/libreadline.a/g' $(TOP)/softether/host/src/Cedar/CMakeLists.txt
-	cd $(TOP)/softether/host && export CC=gcc && export LD=ld && cmake -DCMAKE_BUILD_TYPE=release .
-	cd $(TOP)/softether/host && export CC=gcc && export LD=ld && make
+	cd $(TOP)/softether/host && export CC=gcc && export LD=ld && export CFLAGS=-O2 && cmake -DCMAKE_BUILD_TYPE=release .
+	cd $(TOP)/softether/host && export CC=gcc && export LD=ld && export CFLAGS=-O2 && make
 	cp $(TOP)/softether/host/tmp/hamcorebuilder /usr/local/bin
 	$(call CMakeClean,$(SOFTETHER_PKG_BUILD_DIR))
 	$(call CMakeConfigure,$(SOFTETHER_PKG_BUILD_DIR),$(SOFTETHER_STAGING_DIR),$(SOFTETHER_CMAKE_OPTIONS),$(SOFTETHER_EXTRA_CFLAGS),$(SOFTETHER_EXTRA_LDFLAGS)) 
