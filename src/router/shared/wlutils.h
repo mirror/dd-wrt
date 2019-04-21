97d341403fa7342603f0a9af175454e414f409a5
BrainSlayer <s.gottschall@dd-wrt.com>
2019-04-21 09:49:07 +0000

    for mesh support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@39589 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/wlutils.h
+++ src/router/shared/wlutils.h
@@ -201,6 +201,8 @@ static inline int has_ad(const char *prefix)
 #endif
 
 #ifdef HAVE_ATH9K
+extern int has_mesh(const char *prefix);
+extern int has_tdma(const char *prefix);
 extern int has_gcmp(const char *prefix);
 extern int has_cmac(const char *prefix);
 extern int has_gcmp_128(const char *prefix);
@@ -210,6 +212,14 @@ extern int has_gmac_128(const char *prefix);
 extern int has_gmac_256(const char *prefix);
 extern int has_cmac_256(const char *prefix);
 #else
+static inline int has_mesh(const char *prefix)
+{
+	return 0;
+}
+static inline int has_tdma(const char *prefix)
+{
+	return 0;
+}
 static inline int has_gcmp(const char *prefix)
 {
 	return 0;
