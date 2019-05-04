048b29eee8cbc88b95945211417a590cf7a41485
BrainSlayer <s.gottschall@dd-wrt.com>
2019-05-04 09:09:59 +0000

    update libimobiledevice: add new files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@39727 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/ipeth/libimobiledevice/common/utils.c
+++ src/router/ipeth/libimobiledevice/common/utils.c
@@ -266,6 +266,7 @@ int plist_read_from_filename(plist_t *plist, const char *filename)
 
 	return 1;
 }
+
 int plist_write_to_filename(plist_t plist, const char *filename, enum plist_format_t format)
 {
 	char *buffer = NULL;
@@ -287,6 +288,7 @@ int plist_write_to_filename(plist_t plist, const char *filename, enum plist_form
 
 	return 1;
 }
+
 static const char base64_str[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 static const char base64_pad = '=';
 
