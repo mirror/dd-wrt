11f5f99784d7e9402cb1cfa41497b34cf3cde559
BrainSlayer <s.gottschall@dd-wrt.com>
2019-05-15 12:08:22 +0000

    update kernel
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@39803 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4_bcm4908/include/linux/cpu.h
+++ src/linux/universal/linux-4.4_bcm4908/include/linux/cpu.h
@@ -50,6 +50,8 @@ extern ssize_t cpu_show_spec_store_bypass(struct device *dev,
 					  struct device_attribute *attr, char *buf);
 extern ssize_t cpu_show_l1tf(struct device *dev,
 			     struct device_attribute *attr, char *buf);
+extern ssize_t cpu_show_mds(struct device *dev,
+			    struct device_attribute *attr, char *buf);
 
 extern __printf(4, 5)
 struct device *cpu_device_create(struct device *parent, void *drvdata,
@@ -302,5 +304,22 @@ void idle_notifier_register(struct notifier_block *n);
 void idle_notifier_unregister(struct notifier_block *n);
 void idle_notifier_call_chain(unsigned long val);
 #endif
+
+/*
+ * These are used for a global "mitigations=" cmdline option for toggling
+ * optional CPU mitigations.
+ */
+enum cpu_mitigations {
+	CPU_MITIGATIONS_OFF,
+	CPU_MITIGATIONS_AUTO,
+};
+
+extern enum cpu_mitigations cpu_mitigations;
+
+/* mitigations=off */
+static inline bool cpu_mitigations_off(void)
+{
+	return cpu_mitigations == CPU_MITIGATIONS_OFF;
+}
 
 #endif /* _LINUX_CPU_H_ */
