61f6d9c1431949ecac7319633222929308f3a88e
BrainSlayer <s.gottschall@dd-wrt.com>
2019-05-21 03:39:13 +0000

    wip
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@39839 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.9/net/ipv4/tcp_input.c
+++ src/linux/universal/linux-4.9/net/ipv4/tcp_input.c
@@ -5424,6 +5424,7 @@ void tcp_rcv_established(struct sock *sk, struct sk_buff *skb,
 {
 	struct tcp_sock *tp = tcp_sk(sk);
 
+	skb_mstamp_get(&tp->tcp_mstamp);
 	if (unlikely(!sk->sk_rx_dst))
 		inet_csk(sk)->icsk_af_ops->sk_rx_dst_set(sk, skb);
 	/*
@@ -5986,6 +5987,7 @@ int tcp_rcv_state_process(struct sock *sk, struct sk_buff *skb)
 
 	case TCP_SYN_SENT:
 		tp->rx_opt.saw_tstamp = 0;
+		skb_mstamp_get(&tp->tcp_mstamp);
 		queued = tcp_rcv_synsent_state_process(sk, skb, th);
 		if (queued >= 0)
 			return queued;
@@ -5997,6 +5999,7 @@ int tcp_rcv_state_process(struct sock *sk, struct sk_buff *skb)
 		return 0;
 	}
 
+	skb_mstamp_get(&tp->tcp_mstamp);
 	tp->rx_opt.saw_tstamp = 0;
 	req = tp->fastopen_rsk;
 	if (req) {
