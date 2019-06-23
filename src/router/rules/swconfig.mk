5c5f7c765c3e3490cd3409f77e42c2943e72de65
BrainSlayer <s.gottschall@dd-wrt.com>
2019-06-23 17:31:56 +0000

    install libswitch
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@40091 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/swconfig.mk
+++ src/router/rules/swconfig.mk
@@ -2,11 +2,12 @@ swconfig-configure:
 	@true
 
 swconfig: libnltiny
-	$(MAKE) -C swconfig clean CFLAGS="$(COPTS) -I$(TOP)/libnl-tiny/include -DCONFIG_LIBNL20 -D_GNU_SOURCE  -DNEED_PRINTF" LDFLAGS="-L$(TOP)/libnl-tiny/ -lnl-tiny" LIBS="-lm -lnl-tiny"
-	$(MAKE) -C swconfig CFLAGS="$(COPTS) -I$(TOP)/kernel_headers/$(KERNELRELEASE)/include -I$(LINUXDIR)/include -I$(TOP)/libnl-tiny/include -DCONFIG_LIBNL20 -D_GNU_SOURCE  -DNEED_PRINTF" LDFLAGS="-L$(TOP)/libnl-tiny/ -lnl-tiny" LIBS="-lm -lnl-tiny"
+	$(MAKE) -C swconfig clean CFLAGS="$(COPTS) -I$(TOP)/kernel_headers/$(KERNELRELEASE)/include -I$(TOP)/libnl-tiny/include -DCONFIG_LIBNL20 -D_GNU_SOURCE  -DNEED_PRINTF" LDFLAGS="-L$(TOP)/libnl-tiny/ -lnl-tiny" LIBS="-lm -lnl-tiny"
+	$(MAKE) -C swconfig CFLAGS="$(COPTS) -I$(TOP)/kernel_headers/$(KERNELRELEASE)/include -I$(TOP)/kernel_headers/$(KERNELRELEASE)/include -I$(LINUXDIR)/include -I$(TOP)/libnl-tiny/include -DCONFIG_LIBNL20 -D_GNU_SOURCE  -DNEED_PRINTF" LDFLAGS="-L$(TOP)/libnl-tiny/ -lnl-tiny" LIBS="-lm -lnl-tiny"
 
 swconfig-clean:
-	$(MAKE) -C swconfig clean CFLAGS="$(COPTS) -I$(TOP)/libnl-tiny/include -DCONFIG_LIBNL20 -D_GNU_SOURCE  -DNEED_PRINTF" LDFLAGS="-L$(TOP)/libnl-tiny/ -lnl-tiny" LIBS="-lm -lnl-tiny"
+	$(MAKE) -C swconfig clean CFLAGS="$(COPTS) -I$(TOP)/kernel_headers/$(KERNELRELEASE)/include -I$(TOP)/libnl-tiny/include -DCONFIG_LIBNL20 -D_GNU_SOURCE  -DNEED_PRINTF" LDFLAGS="-L$(TOP)/libnl-tiny/ -lnl-tiny" LIBS="-lm -lnl-tiny"
 
 swconfig-install:
 	install -D swconfig/swconfig $(INSTALLDIR)/swconfig/usr/sbin/swconfig
+	install -D swconfig/libswitch.so $(INSTALLDIR)/swconfig/usr/lib
