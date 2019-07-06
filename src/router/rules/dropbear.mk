2763e47801462200716994f135dd9a8e78ec139f
BrainSlayer <s.gottschall@dd-wrt.com>
2019-07-06 11:46:13 +0000

    blocklist support for ssh
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@40209 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/dropbear.mk
+++ src/router/rules/dropbear.mk
@@ -2,7 +2,7 @@ DROPBEAR_OPTS = $(MIPS16_OPT) -DDISABLE_X11FWD
 
 dropbear-configure:
 	cd dropbear && autoconf
-	cd dropbear && ./configure --host=$(ARCH)-linux --disable-pam --disable-harden --disable-lastlog --disable-utmp --disable-zlib --disable-utmpx --disable-wtmp --disable-wtmpx --enable-bundled-libtom --disable-pututxline CC="$(CC)" CPPFLAGS="-DNEED_PRINTF -I../zlib $(COPTS) $(MIPS16_OPT) $(DROPBEAR_OPTS) -DARGTYPE=3 -DXFREE=free -L../zlib -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections" host_alias=$(ARCH)-linux ac_cv_func_getpass=yes
+	cd dropbear && ./configure --host=$(ARCH)-linux --disable-pam --disable-harden --disable-lastlog --disable-utmp --disable-zlib --disable-utmpx --disable-wtmp --disable-wtmpx --enable-bundled-libtom --disable-pututxline CC="$(CC)" CPPFLAGS="-DNEED_PRINTF -I../zlib $(COPTS) $(MIPS16_OPT) $(DROPBEAR_OPTS) -DARGTYPE=3 -DXFREE=free -L../zlib -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections  -L$(TOP)/libutils -L$(TOP)/nvram -lshutils -lnvram" host_alias=$(ARCH)-linux ac_cv_func_getpass=yes
 
 dropbear:
 	$(MAKE) -j 4 -C dropbear PROGRAMS="dropbear dbclient dropbearkey dropbearconvert scp" SCPPROGRESS=1 MULTI=1
