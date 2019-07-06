22df6f7f0b078cf35712889a927c6fecc15371b5
BrainSlayer <s.gottschall@dd-wrt.com>
2019-07-06 11:46:05 +0000

    blocklist support for ssh
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@40208 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/dropbear/svr-main.c
+++ src/router/dropbear/svr-main.c
@@ -31,7 +31,7 @@
 #include "dbrandom.h"
 #include "crypto_desc.h"
 
-void check_blocklist(const char *service, char *ip);
+int check_blocklist(const char *service, char *ip);
 
 static size_t listensockets(int *sock, size_t sockcount, int *maxfd);
 static void sigchld_handler(int dummy);
@@ -248,7 +248,7 @@ static void main_noinetd() {
 
 			/* Limit the number of unauthenticated connections per IP */
 			getaddrstring(&remoteaddr, &remote_host, NULL, 0);
-			if ((check_blocklist("dropbear", remote_host))
+			if (check_blocklist("dropbear", remote_host))
 				goto out;
 
 			num_unauthed_for_addr = 0;
