44e30feda1400dc24b3550f590a51f16f8c1fa01
BrainSlayer <s.gottschall@dd-wrt.com>
2019-07-17 06:36:16 +0000

    change name
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@40364 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/zlib.mk
+++ src/router/rules/zlib.mk
@@ -38,7 +38,7 @@ zlib:
 	rm -f zlib/libz.a
 
 zlib-install:
-	install -D zlib/libz.so.1.2.11.zlib-ng $(INSTALLDIR)/zlib/usr/lib/libz.so.1.2.11
+	install -D zlib/libz.so.1.2.11 $(INSTALLDIR)/zlib/usr/lib/libz.so.1.2.11
 	cd $(INSTALLDIR)/zlib/usr/lib ; ln -s libz.so.1.2.11 libz.so.1  ; true
 	cd $(INSTALLDIR)/zlib/usr/lib ; ln -s libz.so.1.2.11 libz.so  ; true
 
