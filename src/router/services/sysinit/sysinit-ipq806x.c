34e89d047eda66d1ae65dff48bd69b919be6747e
BrainSlayer <s.gottschall@dd-wrt.com>
2019-08-08 16:01:12 +0000

    prepare for 8devices habanero ipx40xx device
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@40574 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-ipq806x.c
+++ src/router/services/sysinit/sysinit-ipq806x.c
@@ -514,12 +514,16 @@ void start_sysinit(void)
 		eval("swconfig", "dev", "switch0", "set", "enable_vlan", "1");
 		eval("swconfig", "dev", "switch0", "set", "igmp_snooping", "0");
 		eval("swconfig", "dev", "switch0", "set", "igmp_v3", "1");
-		eval("swconfig", "dev", "switch0", "vlan", "1", "set", "ports", "0 1");
+		eval("swconfig", "dev", "switch0", "vlan", "1", "set", "ports", "0t 1 2 3 4");
+		eval("swconfig", "dev", "switch0", "vlan", "2", "set", "ports", "0t 1");
 		eval("swconfig", "dev", "switch0", "set", "apply");
 		eval("ifconfig", "eth0", "up");
 		eval("ifconfig", "eth1", "up");
+		eval("vconfig", "set_name_type", "VLAN_PLUS_VID_NO_PAD");
+		eval("vconfig", "add", "eth0", "1");
+		eval("vconfig", "add", "eth0", "2");
 
-		nvram_set("sw_wancpuport", "0");
+		nvram_set("sw_cpuport", "0");
 		nvram_set("sw_wan", "1");
 		nvram_set("sw_lan1", "2");
 		nvram_set("sw_lan2", "3");
