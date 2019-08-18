d5c068b0279c2b834e4889083a4b66e80b603dd2
BrainSlayer <s.gottschall@dd-wrt.com>
2019-08-18 08:18:09 +0000

    support all scheduler types for individual shaping
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@40675 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/libqos/qos.c
+++ src/router/libutils/libqos/qos.c
@@ -24,6 +24,10 @@
 #include <shutils.h>
 
 #ifdef HAVE_SVQOS
+
+#define TYPE_HTB 0x0
+#define TYPE_HFSC 0x1
+
 /* NF Mark/Mask
  *
  * since multiple services needs a NF packet mark,
@@ -369,40 +373,137 @@ char *get_tcfmark(uint32 mark)
 #if defined(ARCH_broadcom) && !defined(HAVE_BCMMODERN)
 static void add_tc_class(char *dev, int pref, int handle, int classid)
 {
-	sysprintf("tc filter add dev %s protocol ip pref %d handle 0x%x fw classid 1:%d", dev, pref, handle, classid);
+	char h[32];
+	char c[32];
+	char p[32];
+	sprintf(h, "0x%02x", handle);
+	sprintf(c, "1:", classid);
+	sprintf(p, "%d", pref);
+	eval("tc", "filter", "add", "dev", dev, "protocol", "ip", "pref", p, "handle", h, "fw", "classid", c);
 }
 #else
 static void add_tc_mark(char *dev, char *mark, int flow)
 {
-	sysprintf("tc filter add dev %s protocol ip parent 1: u32 match mark %s flowid 1:%d", dev, mark, flow);
+	char f[32];
+	sprintf(f, "1:%d", flow);
+	eval("tc", "filter", "add", "dev", dev, "protocol", "ip", "parent", "1:", "u32", "match", "mark", mark, "flowid", f);
 }
 #endif
-static void add_tc_sfq(char *dev, int parent, int handle, int quantum)
+
+static const char *math(char *buf, int val, const char *ext)
 {
-	sysprintf("tc qdisc add dev %s parent 1:%d handle %d: sfq quantum %d perturb 10", dev, parent, handle, quantum);
+	sprintf(buf, "%d%s", val, ext);
+	return buf;
 }
 
-#if defined(HAVE_CODEL) || defined(HAVE_FQ_CODEL)
-
-static void add_tc_codel(char *dev, int parent, int handle, char *aqd, char *target)
+static void add_codel(const char *dev, int handle, const char *aqd, int rtt, int noecn)
 {
-	if (target)
-		sysprintf("tc qdisc add dev %s parent 1:%d handle %d: %s %s", dev, parent, handle, aqd, target);
+	char p[32];
+	char h[32];
+	char r[32];
+	sprintf(p, "1:%d", handle);
+	sprintf(h, "%d:", handle);
+	sprintf(r, "%dms", rtt);
+	char *ECN = NULL;
+	if (noecn == 1)
+		ECN = "noecn";
+	if (noecn == 0)
+		ECN = "ecn";
+
+	if (rtt != -1)
+		eval("tc", "qdisc", "add", "dev", dev, "parent", p, "handle", h, aqd, "target", r, ECN);
 	else
-		sysprintf("tc qdisc add dev %s parent 1:%d handle %d: %s", dev, parent, handle, aqd);
+		eval("tc", "qdisc", "add", "dev", dev, "parent", p, "handle", h, aqd, ECN);
 }
-#endif
+
+static void add_sfq(const char *dev, int handle, int mtu)
+{
+	char qmtu[32];
+	sprintf(qmtu, "%d", mtu + 14);
+	char p[32];
+	char h[32];
+	sprintf(p, "1:%d", handle);
+	sprintf(h, "%d:", handle);
+	eval("tc", "qdisc", "add", "dev", dev, "parent", p, "handle", h, "sfq", "quantum", qmtu, "perturb", "10");
+
+}
+
+static void add_fq_codel(const char *dev, int handle, const char *aqd)
+{
+	char p[32];
+	char h[32];
+	sprintf(p, "1:%d", handle);
+	sprintf(h, "%d:", handle);
+	eval("tc", "qdisc", "add", "dev", dev, "parent", p, "handle", h, aqd);
+}
+
+static void add_cake(const char *dev, int handle, const char *aqd, int rtt)
+{
+	char p[32];
+	char h[32];
+	char r[32];
+	sprintf(p, "1:%d", handle);
+	sprintf(h, "%d:", handle);
+	sprintf(r, "%dms", rtt);
+	if (rtt != -1)
+		eval("tc", "qdisc", "add", "dev", dev, "parent", p, "handle", h, aqd, "unlimited", "ethernet", "besteffort", "noatm", "raw", "internet", "dual-srchost", "ack-filter", "nat", "rtt", r);
+	else
+		eval("tc", "qdisc", "add", "dev", dev, "parent", p, "handle", h, aqd, "unlimited", "ethernet", "besteffort", "noatm", "raw", "internet", "dual-srchost", "ack-filter", "nat");
+}
+
+static void add_pie(const char *dev, int handle, const char *aqd, int ms5, int noecn)
+{
+	char p[32];
+	char h[32];
+	sprintf(p, "1:%d", handle);
+	sprintf(h, "%d:", handle);
+	char *ECN = NULL;
+	if (noecn == 1)
+		ECN = "noecn";
+	if (noecn == 0)
+		ECN = "ecn";
+
+	if (ms5)
+		eval("tc", "qdisc", "add", "dev", dev, "parent", p, "handle", h, aqd, "target", "5ms", ECN);
+	else
+		eval("tc", "qdisc", "add", "dev", dev, "parent", p, "handle", h, aqd, ECN);
+}
+
+static void add_htb_class(const char *dev, const char *parent, const char *classid, int rate, int limit, int mtu, int p)
+{
+	char buf[32];
+	char qmtu[32];
+	char prio[32];
+	sprintf(qmtu, "%d", mtu + 14);
+	sprintf(prio, "%d", p);
+	if (p != -1)
+		eval("tc", "class", "add", "dev", dev, "parent", parent, "classid", classid, "htb", "rate", math(buf, rate, "kbit"), "ceil", math(buf, limit, "kbit"), "prio", prio, "quantum", qmtu);
+	else
+		eval("tc", "class", "add", "dev", dev, "parent", parent, "classid", classid, "htb", "rate", math(buf, rate, "kbit"), "ceil", math(buf, limit, "kbit"), "quantum", qmtu);
+}
+
 static void add_tc_htb(char *dev, int parent, int class, int rate, int ceil, int quantum, int prio)
 {
-	if (prio > -1)
-		sysprintf("tc class add dev %s parent 1:%d classid 1:%d htb rate %dkbit ceil %dkbit quantum %d prio 0", dev, parent, class, rate, ceil, quantum, prio);
-	else
-		sysprintf("tc class add dev %s parent 1:%d classid 1:%d htb rate %dkbit ceil %dkbit quantum %d", dev, parent, class, rate, ceil, quantum);
+	char classid[32];
+	char parentid[32];
+	sprintf(classid, "1:%d", class);
+	sprintf(parentid, "1:%d", parent);
+	add_htb_class(dev, parentid, classid, rate, ceil, quantum, prio);
+}
+
+static void add_hfsc_class(const char *dev, const char *parent, const char *classid, int rate, int limit)
+{
+	char buf[32];
+	eval("tc", "class", "add", "dev", dev, "parent", parent, "classid", classid, "hfsc", "sc", "rate", math(buf, rate, "kbit"), "ul", "rate", math(buf, limit, "kbit"));
 }
 
 static void add_tc_hfsc(char *dev, int parent, int class, int uprate, int uplimit)
 {
-	sysprintf("tc class add dev %s parent 1:%d classid 1:%d hfsc sc rate %dkbit ul rate %dkbit", dev, parent, class, uprate, uplimit);
+	char classid[32];
+	char parentid[32];
+	sprintf(classid, "1:%d", class);
+	sprintf(parentid, "1:%d", parent);
+	add_hfsc_class(dev, classid, parentid, uprate, uplimit);
 }
 
 void add_client_classes(unsigned int base, unsigned int uprate, unsigned int downrate, unsigned int lanrate, unsigned int level)
@@ -414,16 +515,20 @@ void add_client_classes(unsigned int base, unsigned int uprate, unsigned int dow
 	unsigned int lanlimit = 1000000;
 	unsigned int prio;
 	unsigned int parent;
-	char target[64] = "";
-
+	int rtt = -1;
+	int noecn = -1;
 	int max = 50;
-	int sec = 0;
+	int rtt_cake = -1;
+	if (!nvram_matchi("qos_type", 0)) {
+		rtt = 5;
+	}
 	if (uprate <= 2000) {
-		sec = max - (uprate / 50);
-		sprintf(target, "target %dms noecn", sec);
+		rtt = max - (uprate / 50);
+		rtt_cake = rtt;
+		noecn = 1;
 	}
 
-	unsigned int quantum = get_mtu_val() + 14;
+	unsigned int mtu = get_mtu_val();
 
 	if (lanrate < 1)
 		lanrate = lanlimit;
@@ -483,21 +588,22 @@ void add_client_classes(unsigned int base, unsigned int uprate, unsigned int dow
 	if (nvram_matchi("qos_type", 0)) {
 		char prios[5] = { 0, prio, prio + 1, prio + 1, 7 };
 
-		add_tc_htb(wan_dev, parent, base, uprate, uplimit, quantum, -1);
-		add_tc_htb("imq0", parent, base, downrate, downlimit, quantum, -1);
+		add_tc_htb(wan_dev, parent, base, uprate, uplimit, mtu, -1);
+		add_tc_htb("imq0", parent, base, downrate, downlimit, mtu, -1);
 		if (nvram_match("wshaper_dev", "LAN")) {
-			add_tc_htb("imq1", parent, base, lanrate, lanlimit, quantum, -1);
+			add_tc_htb("imq1", parent, base, lanrate, lanlimit, mtu, -1);
 		}
 		int i;
 		for (i = 0; i < 5; i++) {
-			add_tc_htb(wan_dev, base, base + 1 + i, uprates[i], uplimit, quantum, prios[i]);
-			add_tc_htb("imq0", base, base + 1 + i, downrates[i], downlimit, quantum, prios[i]);
+			add_tc_htb(wan_dev, base, base + 1 + i, uprates[i], uplimit, mtu, prios[i]);
+			add_tc_htb("imq0", base, base + 1 + i, downrates[i], downlimit, mtu, prios[i]);
 			if (nvram_match("wshaper_dev", "LAN")) {
-				add_tc_htb("imq1", base, base + 1 + i, lanrates[i], lanlimit, quantum, prios[i]);
+				add_tc_htb("imq1", base, base + 1 + i, lanrates[i], lanlimit, mtu, prios[i]);
 			}
 		}
 
 	} else {
+
 		add_tc_hfsc(wan_dev, 1, base, uprate, uplimit);
 		add_tc_hfsc("imq0", 1, base, downrate, downlimit);
 		add_tc_hfsc("imq1", 1, base, lanrate, lanlimit);
@@ -526,11 +632,8 @@ void add_client_classes(unsigned int base, unsigned int uprate, unsigned int dow
 #else
 	int i;
 	for (i = 0; i < 5; i++) {
-
 		add_tc_mark(wan_dev, get_tcfmark(base + i), base + 1 + i);
-
 		add_tc_mark("imq0", get_tcfmark(base + i), base + 1 + i);
-
 		if (nvram_match("wshaper_dev", "LAN")) {
 			add_tc_mark("imq1", get_tcfmark(base + i), base + 1 + i);
 		}
@@ -541,30 +644,61 @@ void add_client_classes(unsigned int base, unsigned int uprate, unsigned int dow
 	if (!strcmp(aqd, "sfq")) {
 		int i;
 		for (i = 1; i < 6; i++) {
-			add_tc_sfq(wan_dev, base + i, base + i, quantum);
-			add_tc_sfq("imq0", base + i, base + i, quantum);
+			add_sfq(wan_dev, base + i, mtu);
+			add_sfq("imq0", base + i, mtu);
 			if (nvram_match("wshaper_dev", "LAN")) {
-				add_tc_sfq("imq1", base + i, base + i, quantum);
+				add_sfq("imq1", base + i, mtu);
 			}
 		}
 
 	}
 #if defined(HAVE_CODEL) || defined(HAVE_FQ_CODEL)
-	if (!strcmp(aqd, "codel")
-	    || !strcmp(aqd, "fq_codel")
-	    || !strcmp(aqd, "cake")
-	    || !strcmp(aqd, "pie")) {
-
+	if (!strcmp(aqd, "codel")) {
 		int i;
 		for (i = 1; i < 6; i++) {
-			add_tc_codel(wan_dev, base + i, base + i, aqd, target);
-			add_tc_codel("imq0", base + i, base + i, aqd, NULL);
+			add_codel(wan_dev, base + i, aqd, rtt, noecn);
+			add_codel("imq0", base + i, aqd, -1, -1);
 			if (nvram_match("wshaper_dev", "LAN")) {
-				add_tc_codel("imq1", base + i, base + i, aqd, NULL);
+				add_codel("imq1", base + i, aqd, -1, -1);
+			}
+		}
+	}
+	if (!strcmp(aqd, "fq_codel")) {
+		int i;
+		for (i = 1; i < 6; i++) {
+			add_fq_codel(wan_dev, base + i, aqd);
+			add_fq_codel("imq0", base + i, aqd);
+			if (nvram_match("wshaper_dev", "LAN")) {
+				add_fq_codel("imq1", base + i, aqd);
 			}
 		}
 	}
 #endif
+	if (!strcmp(aqd, "cake")) {
+		int i;
+		for (i = 1; i < 6; i++) {
+			add_cake(wan_dev, base + i, aqd, rtt_cake);
+			add_cake("imq0", base + i, aqd, -1);
+			if (nvram_match("wshaper_dev", "LAN")) {
+				add_cake("imq1", base + i, aqd, -1);
+			}
+		}
+	}
+	if (!strcmp(aqd, "pie")) {
+		if (!nvram_matchi("qos_type", 0))
+			noecn = 0;
+		int i;
+		for (i = 1; i < 6; i++) {
+			add_pie(wan_dev, base + i, aqd, 0, noecn);
+			if (nvram_matchi("qos_type", 0))
+				add_pie("imq0", base + i, aqd, 1, 0);
+			else
+				add_pie("imq0", base + i, aqd, 0, noecn);
+			if (nvram_match("wshaper_dev", "LAN")) {
+				add_pie("imq1", base + i, aqd, 0, noecn);
+			}
+		}
+	}
 }
 
 void add_usermac(char *mac, int base, int uprate, int downrate, int lanrate)
@@ -609,7 +743,7 @@ void add_userip(char *ip, int base, int uprate, int downrate, int lanrate)
 	eval("iptables", "-t", "mangle", "-D", "FILTER_IN", "-j", "CONNMARK", "--save");
 	eval("iptables", "-t", "mangle", "-D", "FILTER_IN", "-j", "RETURN");
 
-//	eval("iptables", "-t", "mangle", "-D", "FILTER_OUT", "-j", "VPN_DSCP");
+//      eval("iptables", "-t", "mangle", "-D", "FILTER_OUT", "-j", "VPN_DSCP");
 	eval("iptables", "-t", "mangle", "-D", "FILTER_OUT", "-j", "CONNMARK", "--save");
 	eval("iptables", "-t", "mangle", "-D", "FILTER_OUT", "-j", "RETURN");
 
@@ -630,7 +764,7 @@ void add_userip(char *ip, int base, int uprate, int downrate, int lanrate)
 	eval("iptables", "-t", "mangle", "-A", "FILTER_IN", "-j", "CONNMARK", "--save");
 	eval("iptables", "-t", "mangle", "-A", "FILTER_IN", "-j", "RETURN");
 
-//	eval("iptables", "-t", "mangle", "-A", "FILTER_OUT", "-j", "VPN_DSCP");
+//      eval("iptables", "-t", "mangle", "-A", "FILTER_OUT", "-j", "VPN_DSCP");
 	eval("iptables", "-t", "mangle", "-A", "FILTER_OUT", "-j", "CONNMARK", "--save");
 	eval("iptables", "-t", "mangle", "-A", "FILTER_OUT", "-j", "RETURN");
 }
@@ -642,31 +776,6 @@ void deinit_qos(const char *wandev, const char *imq_wan, const char *imq_lan)
 	eval("tc", "qdisc", "del", "dev", imq_lan, "root");
 	eval("ip", "link", "set", imq_wan, "down");
 	eval("ip", "link", "set", imq_lan, "down");
-}
-
-static const char *math(char *buf, int val, const char *ext)
-{
-	sprintf(buf, "%d%s", val, ext);
-	return buf;
-}
-
-static void add_hfsc_class(const char *dev, const char *parent, const char *classid, int rate, int limit)
-{
-	char buf[32];
-	eval("tc", "class", "add", "dev", dev, "parent", parent, "classid", classid, "hfsc", "sc", "rate", math(buf, rate, "kbit"), "ul", "rate", math(buf, limit, "kbit"));
-}
-
-static void add_htb_class(const char *dev, const char *parent, const char *classid, int rate, int limit, int mtu, int p)
-{
-	char buf[32];
-	char qmtu[32];
-	char prio[32];
-	sprintf(qmtu, "%d", mtu + 14);
-	sprintf(prio, "%d", p);
-	if (p != -1)
-		eval("tc", "class", "add", "dev", dev, "parent", parent, "classid", classid, "htb", "rate", math(buf, rate, "kbit"), "ceil", math(buf, limit, "kbit"), "prio", prio, "quantum", qmtu);
-	else
-		eval("tc", "class", "add", "dev", dev, "parent", parent, "classid", classid, "htb", "rate", math(buf, rate, "kbit"), "ceil", math(buf, limit, "kbit"), "quantum", qmtu);
 }
 
 static void init_htb_class(const char *dev, int rate, int mtu)
@@ -700,85 +809,12 @@ static void init_hfsc_class(const char *dev, int rate)
 
 }
 
-static void add_sfq(const char *dev, int handle, int mtu)
-{
-	char qmtu[32];
-	sprintf(qmtu, "%d", mtu + 14);
-	char p[32];
-	char h[32];
-	sprintf(p, "1:%d", handle);
-	sprintf(h, "%d:", handle);
-	eval("tc", "qdisc", "add", "dev", dev, "parent", p, "handle", h, "sfq", "quantum", qmtu, "perturb", "10");
-
-}
-
-static void add_codel(const char *dev, int handle, const char *aqd, int rtt, int noecn)
-{
-	char p[32];
-	char h[32];
-	char r[32];
-	sprintf(p, "1:%d", handle);
-	sprintf(h, "%d:", handle);
-	sprintf(r, "%dms", rtt);
-	char *ECN = NULL;
-	if (noecn == 1)
-		ECN = "noecn";
-	if (noecn == 0)
-		ECN = "ecn";
-
-	if (rtt != -1)
-		eval("tc", "qdisc", "add", "dev", dev, "parent", p, "handle", h, aqd, "target", r, ECN);
-	else
-		eval("tc", "qdisc", "add", "dev", dev, "parent", p, "handle", h, aqd, ECN);
-}
-
-static void add_fq_codel(const char *dev, int handle, const char *aqd)
-{
-	char p[32];
-	char h[32];
-	sprintf(p, "1:%d", handle);
-	sprintf(h, "%d:", handle);
-	eval("tc", "qdisc", "add", "dev", dev, "parent", p, "handle", h, aqd);
-}
-
-static void add_cake(const char *dev, int handle, const char *aqd, int rtt)
-{
-	char p[32];
-	char h[32];
-	char r[32];
-	sprintf(p, "1:%d", handle);
-	sprintf(h, "%d:", handle);
-	sprintf(r, "%dms", rtt);
-	if (rtt != -1)
-		eval("tc", "qdisc", "add", "dev", dev, "parent", p, "handle", h, aqd, "unlimited", "ethernet", "besteffort", "noatm", "raw", "internet", "dual-srchost", "ack-filter", "nat", "rtt", r);
-	else
-		eval("tc", "qdisc", "add", "dev", dev, "parent", p, "handle", h, aqd, "unlimited", "ethernet", "besteffort", "noatm", "raw", "internet", "dual-srchost", "ack-filter", "nat");
-}
-
-static void add_pie(const char *dev, int handle, const char *aqd, int ms5, int noecn)
-{
-	char p[32];
-	char h[32];
-	sprintf(p, "1:%d", handle);
-	sprintf(h, "%d:", handle);
-	char *ECN = NULL;
-	if (noecn == 1)
-		ECN = "noecn";
-	if (noecn == 0)
-		ECN = "ecn";
-
-	if (ms5)
-		eval("tc", "qdisc", "add", "dev", dev, "parent", p, "handle", h, aqd, "target", "5ms", ECN);
-	else
-		eval("tc", "qdisc", "add", "dev", dev, "parent", p, "handle", h, aqd, ECN);
-}
-
-static void init_qdisc(const char *type, const char *dev, const char *wandev, const char *aqd, int mtu, int up, int ms5)
+static void init_qdisc(int type, const char *dev, const char *wandev, const char *aqd, int mtu, int up, int ms5)
 {
 	int noecn = -1;
 	int rtt = -1;
 	int rtt_cake = -1;
-	if (!strcmp(type, "hfsc")) {
+	if (type == TYPE_HFSC) {
 		rtt = 5;
 	}
 
@@ -818,7 +854,7 @@ static void init_qdisc(const char *type, const char *dev, const char *wandev, co
 	}
 	if (!strcmp(aqd, "pie")) {
 		/* for imq_wan and htb only 5ms is enforced. i dont know why. i just took it from the original script */
-		if (ms5 || !strcmp(type, "hfsc"))
+		if (ms5 || type == TYPE_HFSC)
 			noecn = 0;
 		add_pie(dev, 100, aqd, ms5, noecn);
 		add_pie(dev, 10, aqd, ms5, noecn);
@@ -851,36 +887,43 @@ static void init_filter(const char *dev)
 	add_filter(dev, 9, 0x28, 40);
 }
 
-void init_qos(const char *type, int up, int down, const char *wandev, int mtu, const char *imq_wan, const char *aqd, const char *imq_lan)
+void init_qos(const char *strtype, int up, int down, const char *wandev, int mtu, const char *imq_wan, const char *aqd, const char *imq_lan)
 {
+	int type;
 	deinit_qos(wandev, imq_wan, imq_lan);
+
+	if (!strcmp(strtype, "htb"))
+		type = TYPE_HTB;
+	else
+		type = TYPE_HFSC;
 
 	int ll = 1000000;
 
-	if (!strcmp(type, "htb") && strcmp(wandev, "xx")) {
-		eval("tc", "qdisc", "add", "dev", wandev, "root", "handle", "1:", "htb", "default", "30");
-		init_htb_class(wandev, up, mtu);
-		init_qdisc(type, wandev, wandev, aqd, mtu, up, 0);
-		init_filter(wandev);
-	}
-	if (!strcmp(type, "hfsc") && strcmp(wandev, "xx")) {
+	if (strcmp(wandev, "xx")) {
+		if (type == TYPE_HTB) {
+			eval("tc", "qdisc", "add", "dev", wandev, "root", "handle", "1:", "htb", "default", "30");
+			init_htb_class(wandev, up, mtu);
+			init_qdisc(type, wandev, wandev, aqd, mtu, up, 0);
+			init_filter(wandev);
+		} else {
 
-		eval("tc", "qdisc", "add", "dev", wandev, "root", "handle", "1:", "hfsc", "default", "30");
-		init_hfsc_class(wandev, up);
-		init_qdisc(type, wandev, wandev, aqd, mtu, up, 0);
-		init_filter(wandev);
+			eval("tc", "qdisc", "add", "dev", wandev, "root", "handle", "1:", "hfsc", "default", "30");
+			init_hfsc_class(wandev, up);
+			init_qdisc(type, wandev, wandev, aqd, mtu, up, 0);
+			init_filter(wandev);
+		}
 	}
 
 	if (down != 0) {
 		eval("ip", "link", "set", imq_wan, "up");
 
-		if (!strcmp(type, "htb")) {
+		if (type == TYPE_HTB) {
 			eval("tc", "qdisc", "add", "dev", imq_wan, "root", "handle", "1:", "htb", "default", "30");
 			init_htb_class(imq_wan, down, mtu);
 			init_qdisc(type, imq_wan, wandev, aqd, mtu, up, 1);	// force 5ms for PIE on imq_wan
 
 		}
-		if (!strcmp(type, "hfsc")) {
+		{
 			eval("tc", "qdisc", "add", "dev", imq_wan, "root", "handle", "1:", "hfsc", "default", "30");
 			init_hfsc_class(imq_wan, down);
 			init_qdisc(type, imq_wan, wandev, aqd, mtu, up, 0);
@@ -891,11 +934,10 @@ void init_qos(const char *type, int up, int down, const char *wandev, int mtu, c
 	if (strcmp(imq_lan, "0")) {
 		eval("ip", "link", "set", imq_lan, "up");
 
-		if (!strcmp(type, "htb")) {
+		if (type == TYPE_HTB) {
 			eval("tc", "qdisc", "add", "dev", imq_lan, "root", "handle", "1:", "htb", "default", "30");
 			init_htb_class(imq_lan, ll, mtu);
-		}
-		if (!strcmp(type, "hfsc")) {
+		} else {
 			eval("tc", "qdisc", "add", "dev", imq_lan, "root", "handle", "1:", "hfsc", "default", "30");
 			init_hfsc_class(imq_lan, ll);
 		}
