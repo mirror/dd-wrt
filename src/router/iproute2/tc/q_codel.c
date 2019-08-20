81817914b2e76560cfeb00830ccd72582bf5c4fe
BrainSlayer <s.gottschall@dd-wrt.com>
2019-08-20 17:34:40 +0000

    fix wrong argument
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@40715 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/iproute2/tc/q_codel.c
+++ src/router/iproute2/tc/q_codel.c
@@ -58,7 +58,7 @@ static void explain(void)
 }
 
 static int codel_parse_opt(struct qdisc_util *qu, int argc, char **argv,
-			   struct nlmsghdr *n, const char *dev)
+			   struct nlmsghdr *n)
 {
 	unsigned int limit = 0;
 	unsigned int target = 0;
