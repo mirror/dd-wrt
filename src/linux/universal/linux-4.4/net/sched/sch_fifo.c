cbe274e04b6c361285bf44f4281bf27910dc2a33
BrainSlayer <s.gottschall@dd-wrt.com>
2019-08-22 15:49:12 +0000

    reduce memory pressure
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@40739 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4/net/sched/sch_fifo.c
+++ src/linux/universal/linux-4.4/net/sched/sch_fifo.c
@@ -21,16 +21,21 @@
 
 static int bfifo_enqueue(struct sk_buff *skb, struct Qdisc *sch)
 {
-	if (likely(sch->qstats.backlog + qdisc_pkt_len(skb) <= sch->limit))
+	if (likely(sch->qstats.backlog + qdisc_pkt_len(skb) <= sch->limit)) {
+		if (skb_queue_len(&sch->q) > 128)
+			skb = skb_reduce_truesize(skb);
 		return qdisc_enqueue_tail(skb, sch);
-
+	}
 	return qdisc_reshape_fail(skb, sch);
 }
 
 static int pfifo_enqueue(struct sk_buff *skb, struct Qdisc *sch)
 {
-	if (likely(skb_queue_len(&sch->q) < sch->limit))
+	if (likely(skb_queue_len(&sch->q) < sch->limit)) {
+		if (skb_queue_len(&sch->q) > 128)
+			skb = skb_reduce_truesize(skb);
 		return qdisc_enqueue_tail(skb, sch);
+	}
 
 	return qdisc_reshape_fail(skb, sch);
 }
