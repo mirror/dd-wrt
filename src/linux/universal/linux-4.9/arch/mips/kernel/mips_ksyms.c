7c053c850c6bc02c13010c48101a80f016696836
BrainSlayer <s.gottschall@dd-wrt.com>
2019-08-23 22:37:34 +0000

    fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@40772 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.9/arch/mips/kernel/mips_ksyms.c
+++ src/linux/universal/linux-4.9/arch/mips/kernel/mips_ksyms.c
@@ -19,20 +19,6 @@
 
 extern void *__bzero_kernel(void *__s, size_t __count);
 extern void *__bzero(void *__s, size_t __count);
-extern long __strncpy_from_kernel_nocheck_asm(char *__to,
-					      const char *__from, long __len);
-extern long __strncpy_from_kernel_asm(char *__to, const char *__from,
-				      long __len);
-extern long __strncpy_from_user_nocheck_asm(char *__to,
-					    const char *__from, long __len);
-extern long __strncpy_from_user_asm(char *__to, const char *__from,
-				    long __len);
-extern long __strlen_kernel_asm(const char *s);
-extern long __strlen_user_asm(const char *s);
-extern long __strnlen_kernel_nocheck_asm(const char *s);
-extern long __strnlen_kernel_asm(const char *s);
-extern long __strnlen_user_nocheck_asm(const char *s);
-extern long __strnlen_user_asm(const char *s);
 
 /*
  * Core architecture code
