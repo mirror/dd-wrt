25052907b43f0166ed0d8ceadd6bc2696f13f88f
BrainSlayer <s.gottschall@dd-wrt.com>
2019-08-29 07:26:03 +0000

    revert
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@40855 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.10/brcm/mipsel/include/bcmcrypto/des.h
+++ src/linux/universal/linux-3.10/brcm/mipsel/include/bcmcrypto/des.h
@@ -16,7 +16,7 @@
  * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
  * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
- * $Id: des.h 467210 2014-04-02 20:16:21Z $
+ * $Id: des.h 467211 2014-04-02 20:17:11Z $
  */
 
 #ifndef _DES_H_
@@ -24,8 +24,8 @@
 
 typedef unsigned int DES_KS[16][2];	/* Single-key DES key schedule */
 
-void BCMROMFN(deskey)(DES_KS, unsigned char *, int);
+void deskey(DES_KS, unsigned char *, int);
 
-void BCMROMFN(des)(DES_KS, unsigned char *);
+void des(DES_KS, unsigned char *);
 
 #endif /* _DES_H_ */
