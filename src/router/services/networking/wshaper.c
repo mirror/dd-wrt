5f1903dafdc7530d3dd87fafb0d338e6e2c69612
BrainSlayer <s.gottschall@dd-wrt.com>
2019-09-02 10:35:34 +0000

    send all non matching traffic to default class
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@40879 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/wshaper.c
+++ src/router/services/networking/wshaper.c
@@ -863,6 +863,8 @@ static int svqos_iptables(void)
 		}
 	} while ((qos_svcs = strpbrk(++qos_svcs, "|")) && qos_svcs++);
 
+
+
 	// close mark-tables 
 	eval("iptables", "-t", "mangle", "-A", "FILTER_IN", "-m", "mark", "--mark", nullmask, "-j", "SVQOS_SVCS");
 	eval("iptables", "-t", "mangle", "-A", "FILTER_IN", "-j", "CONNMARK", "--save-mark");
@@ -891,6 +893,9 @@ static int svqos_iptables(void)
 #endif
 	eval("iptables", "-t", "mangle", "-A", "FILTER_OUT", "-j", "CONNMARK", "--save-mark");
 	eval("iptables", "-t", "mangle", "-A", "FILTER_OUT", "-j", "RETURN");
+
+	/* anything which doesnt match should get default class */
+	eval("iptables", "-t", "mangle", "-A", "SVQOS_SVCS", "-j", "MARK", "--set-mark", qos_nfmark(30));
 
 	eval("iptables", "-t", "mangle", "-A", "SVQOS_SVCS", "-j", "RETURN");
 
