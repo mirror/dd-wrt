bf98ede6cae635f78d2fdb1dd9fe0d88b92575e5
BrainSlayer <s.gottschall@dd-wrt.com>
2019-09-02 07:34:56 +0000

    fix header conflict
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@40873 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/httpd/visuals/packets.c
@@ -0,0 +1,26 @@
+#include <libiptc/libip4tc.c>
+void getpacketcounts(unsigned long long *counts, int len)
+{
+	int c = 0;
+	const char *this;
+	if (!len)
+		return;
+	iptc_handle_t handle = iptc_init("mangle");
+
+	for (this = iptc_first_chain(&handle); this; this = iptc_next_chain(&handle)) {
+		const struct ipt_entry *i;
+		if (strcmp("SVQOS_SVCS", this) != 0)
+			continue;
+		i = iptc_first_rule(this, &handle);
+		while (i) {
+			counts[c++] = i->counters.pcnt;
+			if (c == len) {
+				iptc_free(&handle);
+				return;
+			}
+			i = iptc_next_rule(i, &handle);
+		}
+	}
+	iptc_free(&handle);
+}
+
