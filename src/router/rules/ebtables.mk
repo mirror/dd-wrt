ec5655bf703266f5e7dc0968270451c099646eec
BrainSlayer <s.gottschall@dd-wrt.com>
2019-09-04 09:13:32 +0000

    need atomic for powerpc
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@40909 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/ebtables.mk
+++ src/router/rules/ebtables.mk
@@ -4,7 +4,7 @@ ifeq ($(CONFIG_HOTPLUG2),y)
 	$(MAKE) -C ebtables-2.0.9 static BINDIR=/usr/sbin LIBDIR=/usr/lib KERNEL_DIR=$(LINUXDIR)
 else
 	-cp -f ebtables/ebtables-standalone.c.use ebtables/ebtables-standalone.c
-	$(MAKE) -C ebtables static BINDIR=/usr/sbin LIBDIR=/usr/lib KERNEL_DIR=$(LINUXDIR) KERNEL_INCLUDES=$(LINUXDIR)/include
+	$(MAKE) -C ebtables static BINDIR=/usr/sbin LIBDIR=/usr/lib KERNEL_DIR=$(LINUXDIR) KERNEL_INCLUDES=$(TOP)/kernel_headers/$(KERNELRELEASE)/include
 endif
 
 ebtables-install:
