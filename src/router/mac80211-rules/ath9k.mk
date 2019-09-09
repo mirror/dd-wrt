293d565679c1b7b6f2ef942ddbd53d375899f698
BrainSlayer <s.gottschall@dd-wrt.com>
2019-09-09 19:23:49 +0000

    change paths
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@40985 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/mac80211-rules/ath9k.mk
+++ src/router/mac80211-rules/ath9k.mk
@@ -3,7 +3,7 @@ include $(TOP)/.config
 MAC80211_PATH=compat-wireless
 
 ifeq ($(wildcard $(TOP)/$(MAC80211_PATH)/config.mk),) 
-	include $(TOP)/private/ath9k-rules/ath9k-kconfig.mk	
+	include $(TOP)/mac80211-rules/ath9k-kconfig.mk	
 else
-	include $(TOP)/private/ath9k-rules/ath9k-nokconfig.mk	
+	include $(TOP)/mac80211-rules/ath9k-nokconfig.mk	
 endif
