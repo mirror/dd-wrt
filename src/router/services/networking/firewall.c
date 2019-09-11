ab8d0e814eb014bcb36dd79932740d44a89f487e
BrainSlayer <s.gottschall@dd-wrt.com>
2019-09-11 11:30:24 +0000

    pando can be udp and tcp
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41015 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/firewall.c
+++ src/router/services/networking/firewall.c
@@ -1471,7 +1471,7 @@ static void advgrp_chain(int seq, int urlenable, char *ifname)
 		save2file_A("advgrp_%d -p tcp -m ndpi --gnutella -j %s", seq, log_drop);
 		save2file_A("advgrp_%d -m ndpi --imesh -j %s", seq, log_drop);
 		save2file_A("advgrp_%d -p tcp -m ndpi --openft -j %s", seq, log_drop);
-		save2file_A("advgrp_%d -p tcp -m ndpi --pando_media_booster -j %s", seq, log_drop);
+		save2file_A("advgrp_%d -m ndpi --pando_media_booster -j %s", seq, log_drop);
 		save2file_A("advgrp_%d -p tcp -m ndpi --soulseek -j %s", seq, log_drop);
 		save2file_A("advgrp_%d -p tcp -m ndpi --winmx -j %s", seq, log_drop);
 #else
