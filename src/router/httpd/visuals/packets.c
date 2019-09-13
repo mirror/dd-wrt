fe373fa8fb525f678e88f635f29eefbbc8dfa236
BrainSlayer <s.gottschall@dd-wrt.com>
2019-09-13 10:02:10 +0000

    to get total count from a chain
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41032 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/packets.c
+++ src/router/httpd/visuals/packets.c
@@ -27,3 +27,27 @@ void getpacketcounts(unsigned long long *counts, int len)
 	}
 	iptc_free(&handle);
 }
+unsigned long long getpackettotal(char *table, char *chain)
+{
+	int c = 0;
+	unsigned long long count;
+	const char *this;
+	iptc_handle_t handle = iptc_init(table);
+
+	for (this = iptc_first_chain(&handle); this; this = iptc_next_chain(&handle)) {
+		const struct ipt_entry *i;
+		if (strcmp(chain, this))
+			continue;
+		i = iptc_first_rule(this, &handle);
+		while (i) {
+			const char *target = iptc_get_target(i, handle);
+			if (target && !strcmp(target, "RETURN"))
+			    goto skip;
+			count += i->counters.pcnt;
+			skip:;
+			i = iptc_next_rule(i, &handle);
+		}
+	}
+	iptc_free(&handle);
+	return count;
+}
