e02d4d2b007584e864e950c516f85255d4402084
BrainSlayer <s.gottschall@dd-wrt.com>
2019-09-18 10:18:22 +0000

    fix pointer
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41066 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/net/sched/sch_generic.c
+++ src/linux/universal/linux-3.18/net/sched/sch_generic.c
@@ -741,7 +741,7 @@ static void attach_one_default_qdisc(struct net_device *dev,
 
 	if (dev->tx_queue_len) {
 		qdisc = qdisc_create_dflt(dev_queue,
-					  &default_qdisc_ops, TC_H_ROOT);
+					  default_qdisc_ops, TC_H_ROOT);
 		if (!qdisc) {
 			netdev_info(dev, "activation failed\n");
 			return;
