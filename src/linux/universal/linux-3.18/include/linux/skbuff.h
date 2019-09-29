9bd34e5f122c9f4bc2f3c3293383a26482035064
BrainSlayer <s.gottschall@dd-wrt.com>
2019-09-29 13:30:58 +0000

    backport
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41198 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/include/linux/skbuff.h
+++ src/linux/universal/linux-3.18/include/linux/skbuff.h
@@ -785,13 +785,28 @@ static inline bool skb_unref(struct sk_buff *skb)
 {
 	if (unlikely(!skb))
 		return false;
-	if (likely(refcount_read(&skb->users) == 1))
+	if (likely(atomic_read(&skb->users) == 1))
 		smp_rmb();
-	else if (likely(!refcount_dec_and_test(&skb->users)))
+	else if (likely(!atomic_dec_and_test(&skb->users)))
 		return false;
 
 	return true;
 }
+
+
+/* decrement the reference count and return true if we can free the skb */
+static inline bool skb_unref(struct sk_buff *skb)
+{
+	if (unlikely(!skb))
+		return false;
+	if (likely(atomic_read(&skb->users) == 1))
+		smp_rmb();
+	else if (likely(!atomic_dec_and_test(&skb->users)))
+		return false;
+
+	return true;
+}
+
 
 void kfree_skb(struct sk_buff *skb);
 void kfree_skb_list(struct sk_buff *segs);
