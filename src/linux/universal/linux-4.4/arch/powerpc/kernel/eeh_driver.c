6dc73ee744141dbacacfb54e9317e2237e4c81fc
BrainSlayer <s.gottschall@dd-wrt.com>
2019-10-06 18:56:48 +0000

    update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41240 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4/arch/powerpc/kernel/eeh_driver.c
+++ src/linux/universal/linux-4.4/arch/powerpc/kernel/eeh_driver.c
@@ -675,6 +675,10 @@ static bool eeh_handle_normal_event(struct eeh_pe *pe)
 	pr_warn("EEH: This PCI device has failed %d times in the last hour\n",
 		pe->freeze_count);
 
+	eeh_for_each_pe(pe, tmp_pe)
+		eeh_pe_for_each_dev(tmp_pe, edev, tmp)
+			edev->mode &= ~EEH_DEV_NO_HANDLER;
+
 	/* Walk the various device drivers attached to this slot through
 	 * a reset sequence, giving each an opportunity to do what it needs
 	 * to accomplish the reset.  Each child gets a report of the
@@ -840,7 +844,8 @@ perm_error:
 
 static void eeh_handle_special_event(void)
 {
-	struct eeh_pe *pe, *phb_pe;
+	struct eeh_pe *pe, *phb_pe, *tmp_pe;
+	struct eeh_dev *edev, *tmp_edev;
 	struct pci_bus *bus;
 	struct pci_controller *hose;
 	unsigned long flags;
@@ -918,6 +923,10 @@ static void eeh_handle_special_event(void)
 				    !(phb_pe->state & EEH_PE_ISOLATED) ||
 				    (phb_pe->state & EEH_PE_RECOVERING))
 					continue;
+
+				eeh_for_each_pe(pe, tmp_pe)
+					eeh_pe_for_each_dev(tmp_pe, edev, tmp_edev)
+						edev->mode &= ~EEH_DEV_NO_HANDLER;
 
 				/* Notify all devices to be down */
 				eeh_pe_state_clear(pe, EEH_PE_PRI_BUS);
