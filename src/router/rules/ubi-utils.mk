672bfb5261ffac92bf11713793df71f0f5ad0b33
BrainSlayer <s.gottschall@dd-wrt.com>
2019-10-08 22:18:31 +0000

    update ubi-utils
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41252 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/ubi-utils.mk
+++ src/router/rules/ubi-utils.mk
@@ -1,4 +1,15 @@
-
+ubi-utils-configure:
+	cd ubi-utils && ./autogen.sh
+	cd ubi-utils && ./configure --prefix=/usr --host=$(ARCH)-linux \
+		CC="$(CC)" \
+		CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/lzo/include -L$(TOP)/lzo/src/.libs -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+		LZO_CFLAGS="-I$(TOP)/lzo/include" \
+		LZO_LIBS="-L$(TOP)/lzo/src/.libs -llzo2" \
+		ZLIB_CFLAGS="-I$(TOP)/zlib/include" \
+		ZLIB_LIBS="-L$(TOP)/zlib -lz" \
+		ZSTD_CFLAGS="-I$(TOP)/zstd/lib" \
+		ZSTD_LIBS="-L$(TOP)/zstd/lib -lzstd"
+		
 ubi-utils:
 	$(MAKE) -C ubi-utils
 
