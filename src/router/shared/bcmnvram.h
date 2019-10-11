e2801b34e78094f3f7fa19013593fac7b6de932a
BrainSlayer <s.gottschall@dd-wrt.com>
2019-10-11 05:21:32 +0000

    new function
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41277 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/bcmnvram.h
+++ src/router/shared/bcmnvram.h
@@ -132,6 +132,7 @@ extern void nvram_seti(const char *name, const int value);
 
 extern int nvram_states(char *list);
 extern int nvram_state(char *name);
+extern int nvram_state_change(char *name);
 extern int nvram_delstates(char *list);
 
 int nvram_ngeti(const char *fmt, ...);
