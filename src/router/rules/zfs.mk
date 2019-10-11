727d0b04c20abb90a5b73aec5f42a7eebb0d7e74
BrainSlayer <s.gottschall@dd-wrt.com>
2019-10-11 20:04:35 +0000

    adjust flags
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41293 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/zfs.mk
+++ src/router/rules/zfs.mk
@@ -6,8 +6,8 @@ zfs-configure: libtirpc-configure libtirpc libudev openssl zlib
 		--libdir=/usr/lib \
 		--host=$(ARCH)-linux \
 		CC="$(CC) -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-		CFLAGS="-I$(TOP)/zlib -I$(TOP)/e2fsprogs/lib  -I$(TOP)/libtirpc -I$(TOP)/libtirpc/tirpc -I$(TOP)/openssl/include  -I$(TOP)/libudev/src/libudev" \
-		LDFLAGS="-L$(TOP)/zlib  -L$(TOP)/e2fsprogs/lib/blkid -L$(TOP)/e2fsprogs/lib/uuid -L$(TOP)/libtirpc/src/.libs -L$(TOP)/zfs/lib/libuutil/.libs -L$(TOP)/openssl -L$(TOP)/libudev/src/libudev/.libs -ludev" \
+		CFLAGS="-I$(TOP)/zlib -I$(TOP)/util-linux/include  -I$(TOP)/util-linux/libblkid/src -I$(TOP)/util-linux/libuuid/src    -I$(TOP)/libtirpc -I$(TOP)/libtirpc/tirpc -I$(TOP)/openssl/include  -I$(TOP)/libudev/src/libudev -D_GNU_SOURCE" \
+		LDFLAGS="-L$(TOP)/zlib  -L$(TOP)/util-linux/.libs -L$(TOP)/libtirpc/src/.libs -L$(TOP)/zfs/lib/libuutil/.libs -L$(TOP)/openssl -L$(TOP)/libudev/src/libudev/.libs" \
 		--with-linux=$(LINUXDIR)
 	cd zfs && find . -name *.la -exec sed -i 's/relink_command/# relink_command/g' {} +
 	cd zfs && find . -name *.la -exec touch {} +
