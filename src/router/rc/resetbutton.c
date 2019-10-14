9113b0a0efcd1c5c59d0dd6a8f062ab226d3f1e2
BrainSlayer <s.gottschall@dd-wrt.com>
2019-10-14 12:55:24 +0000

    invert
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41327 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/resetbutton.c
+++ src/router/rc/resetbutton.c
@@ -1136,7 +1136,7 @@ static void resetbtn_period_check(int sig)
 	case ROUTER_BOARD_GW2388:
 	case ROUTER_BOARD_GW6400:
 #ifdef HAVE_LAGUNA
-		val = get_gpio(100);
+		val = !get_gpio(100);
 #else
 		val = get_gpio(240);
 #endif
