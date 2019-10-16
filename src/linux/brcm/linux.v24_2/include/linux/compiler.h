ef31d20c201784eac14e442ce790c308022bb705
BrainSlayer <s.gottschall@dd-wrt.com>
2019-10-16 19:39:19 +0000

    detect and handle 64k nvram
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41334 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/brcm/linux.v24_2/include/linux/compiler.h
+++ src/linux/brcm/linux.v24_2/include/linux/compiler.h
@@ -41,6 +41,7 @@
 #endif
 #endif
 
+#ifdef __KERNEL__
 #if __GNUC__ > 5 || __GNUC__ == 5 && __GNUC_MINOR__ >= 2
 #error "GCC >= 4.2 miscompiles kernel 2.4, do not use it!"
 #error "While the resulting kernel may boot, you will encounter random bugs"
@@ -49,7 +50,7 @@
 #error "   make bzImage CC=gcc-3.3 "
 #endif
 
-#ifdef __KERNEL__
+
 #if __GNUC__ > 4 || __GNUC__ == 4 && __GNUC_MINOR__ >= 2
 #error "GCC >= 4.2 miscompiles kernel 2.4, do not use it!"
 #error "While the resulting kernel may boot, you will encounter random bugs"
