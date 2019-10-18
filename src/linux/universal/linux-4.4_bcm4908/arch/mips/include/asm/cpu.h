0f39b01cf53ce36bd350f4f7f8c3de8858432503
BrainSlayer <s.gottschall@dd-wrt.com>
2019-10-18 00:02:51 +0000

    remove double definition
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41350 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4_bcm4908/arch/mips/include/asm/cpu.h
+++ src/linux/universal/linux-4.4_bcm4908/arch/mips/include/asm/cpu.h
@@ -348,20 +348,6 @@ enum cpu_type_enum {
 /*
  * CPU Option encodings
  */
-#ifdef __ASSEMBLY__
-#define _AC(X,Y)	X
-#define _AT(T,X)	X
-#else
-#define __AC(X,Y)	(X##Y)
-#define _AC(X,Y)	__AC(X,Y)
-#define _AT(T,X)	((T)(X))
-#endif
-
-#define _UL(x)		(_AC(x, UL))
-#define _ULL(x)		(_AC(x, ULL))
-
-#define ULL(x)		(_ULL(x))
-#define BIT_ULL(nr)		(ULL(1) << (nr))
 
 #define MIPS_CPU_TLB		BIT_ULL( 0)	/* CPU has TLB */
 #define MIPS_CPU_4KEX		BIT_ULL( 1)	/* "R4K" exception model */
