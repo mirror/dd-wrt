a77983823cf943af33730694eda2bb948eaa00d4
BrainSlayer <s.gottschall@dd-wrt.com>
2019-10-31 16:01:22 +0000

    add gnutls and final makefile for samba4
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41428 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/samba4.mk
+++ src/router/rules/samba4.mk
@@ -1,12 +1,42 @@
-samba4-configure:
+samba4-configure: gnutls
 	cp $(TOP)/samba4/waf-cross-answers/$(ARCH).txt $(TOP)/samba4/cross-answers.txt
 	echo 'Checking uname machine type: "$(ARCH)"' >> $(TOP)/samba4/cross-answers.txt
 	echo 'Checking uname release type: "$(LINUX_VERSION)"' >> $(TOP)/samba4/cross-answers.txt
 	echo 'Checking uname version type: "$(VERSION_DIST) Linux-$(LINUX_VERSION) $(shell date +%Y-%m-%d)"' >> $(TOP)/samba4/cross-answers.txt
 	# NOTE: For some unknown reason this answer is not needed on some hosts/distros, yet needed on others?
 	echo 'Checking whether POSIX capabilities are available: OK' >> $(TOP)/samba4/cross-answers.txt
-
-	cd samba4 && ./configure -v \
+	cd samba4 && ./configure --hostcc=gcc \
+		--disable-cups \
+		--disable-iprint \
+		--disable-cephfs \
+		--disable-fault-handling \
+		--disable-glusterfs \
+		--disable-rpath \
+		--disable-rpath-install \
+		--disable-rpath-private-install \
+		--enable-fhs \
+		--without-automount \
+		--without-iconv \
+		--without-lttng \
+		--without-ntvfs-fileserver \
+		--without-pam \
+		--without-systemd \
+		--without-utmp \
+		--without-json \
+		--without-dmapi \
+		--without-fam \
+		--without-gettext \
+		--without-regedit \
+		--without-gpgme \
+		--builtin-libraries=replace --nonshared-binary=asn1_compile \
+		--disable-avahi \
+		--without-quotas --without-acl-support --without-winbind \
+		--without-ad-dc --without-libarchive --disable-python --nopyc --nopyo \
+		--without-dnsupdate --without-ads --without-ldap python_LDFLAGS="" python_LIBDIR="" CC=gcc LD=ld CFLAGS="-O2"
+	cd samba4 && ./buildtools/bin/waf build --targets=asn1_compile,compile_et python_LDFLAGS="" python_LIBDIR="" CC=gcc LD=ld CFLAGS="-O2"
+		install $(TOP)/samba4/bin/asn1_compile $(TOP)/samba4/bin/asn1_compile_host 
+		install $(TOP)/samba4/bin/compile_et $(TOP)/samba4/bin/compile_et_host
+	cd samba4 && ./configure \
 		--hostcc="$(ARCH)-linux-uclibc-gcc" \
 		--cross-compile \
 		--cross-answers=cross-answers.txt \
@@ -35,8 +65,8 @@ samba4-configure:
 		--with-logfilebase=/var/log \
 		--with-piddir=/var/run \
 		--with-privatedir=/etc/samba \
-		--disable-avahi \
-		--without-quotas \
+		--enable-avahi \
+		--with-quotas \
 		--without-acl-support \
 		--without-ad-dc \
 		--without-libarchive \
@@ -46,14 +76,23 @@ samba4-configure:
 		--without-dnsupdate \
 		--without-ads \
 		--without-ldap \
-		--without-winbind \
-		--private-libraries=talloc,tevent,tevent-util,texpect,tdb,ldb,tdr,cmocka,replace CFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF"
+		--with-winbind \
+		--without-json \
+		--prefix=/usr \
+		--with-lockdir=/var \
+		--destdir=$(INSTALLDIR)/samba4 \
+		--private-libraries=talloc,tevent,tevent-util,texpect,tdb,ldb,tdr,cmocka,replace \
+		CFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -I$(TOP)/gnutls/lib/includes -ffunction-sections -fdata-sections" \
+		LDFLAGS="-Wl,--gc-sections -L$(TOP)/gnutls/lib/.libs -lgnutls -L$(TOP)/nettle -lnettle -lhogweed -L$(TOP)/gmp/.libs -lgmp"
 
-samba4:
-	@true
+
+samba4: gnutls
+	make -C samba4
 
 samba4-install:
-	@true
+	cd samba4 && ./buildtools/bin/waf install --destdir=$(INSTALLDIR)/samba4
+	rm -rf $(INSTALLDIR)/samba4/usr/include
+	rm -rf $(INSTALLDIR)/samba4/usr/lib/pkgconfig
 
 samba4-clean:
-	@true
+	make -C samba4 clean
