e990cbe0caf647a2e167d80bfb83fda2eced1831
BrainSlayer <s.gottschall@dd-wrt.com>
2019-11-12 23:23:03 +0000

    disable mips16, gcc will raise a internal compiler error here
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41526 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/transmission.mk
+++ src/router/rules/transmission.mk
@@ -1,8 +1,8 @@
 transmission: libevent curl
 	CC="ccache $(ARCH)-linux-uclibc-gcc" \
-	CFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib  -I$(TOP)/curl/include -I$(TOP)/libevent/include" \
-	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib   -I$(TOP)/curl/include -I$(TOP)/libevent/include" \
-	LDFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE -L$(TOP)/zlib -L$(TOP)/libevent/.libs -levent -L$(TOP)/curl/lib/.libs -lcurl -ldl" \
+	CFLAGS="$(COPTS) -D_GNU_SOURCE -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib  -I$(TOP)/curl/include -I$(TOP)/libevent/include" \
+	CPPFLAGS="$(COPTS) -D_GNU_SOURCE -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib   -I$(TOP)/curl/include -I$(TOP)/libevent/include" \
+	LDFLAGS="$(COPTS) -D_GNU_SOURCE -L$(TOP)/zlib -L$(TOP)/libevent/.libs -levent -L$(TOP)/curl/lib/.libs -lcurl -ldl" \
 	ZLIB_CFLAGS="-I$(TOP)/zlib" \
 	ZLIB_LIBS="-L$(TOP)/zlib -lz" \
 	OPENSSL_CFLAGS="-I$(TOP)/openssl/include" \
@@ -28,9 +28,9 @@ transmission-configure: libevent-configure curl-configure
 	-cd transmission && ./autogen.sh
 	cd transmission && ./configure  --prefix=/usr ac_cv_host=$(ARCH)-uclibc-linux --target=$(ARCH)-linux --host=$(ARCH) CC="ccache $(ARCH)-linux-uclibc-gcc" \
 	--enable-daemon --disable-nls --without-systemd-daemon --libdir=/usr/lib --disable-static --disable-dependency-tracking \
-	CFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE -DNO_SYS_QUEUE_H -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib   -I$(TOP)/curl/include -I$(TOP)/openssl/include -I$(TOP)/libevent/include -flto" \
-	CPPFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE -DNO_SYS_QUEUE_H -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib  -I$(TOP)/curl/include  -I$(TOP)/openssl/include  -I$(TOP)/libevent/include -flto" \
-	LDFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE -DNO_SYS_QUEUE_H  -L$(TOP)/zlib   -L$(TOP)/openssl -lssl -lcrypto -L$(TOP)/libevent/.libs  -L$(TOP)/curl/lib/.libs -ldl -flto" \
+	CFLAGS="$(COPTS) -D_GNU_SOURCE -DNO_SYS_QUEUE_H -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib   -I$(TOP)/curl/include -I$(TOP)/openssl/include -I$(TOP)/libevent/include -flto" \
+	CPPFLAGS="$(COPTS) -D_GNU_SOURCE -DNO_SYS_QUEUE_H -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib  -I$(TOP)/curl/include  -I$(TOP)/openssl/include  -I$(TOP)/libevent/include -flto" \
+	LDFLAGS="$(COPTS) -D_GNU_SOURCE -DNO_SYS_QUEUE_H  -L$(TOP)/zlib   -L$(TOP)/openssl -lssl -lcrypto -L$(TOP)/libevent/.libs  -L$(TOP)/curl/lib/.libs -ldl -flto" \
 	ZLIB_LIBS="-L$(TOP)/zlib -lz" \
 	ZLIB_CFLAGS="-I$(TOP)/zlib" \
 	LIBCURL_CFLAGS="-I$(TOP)/curl/include" \
