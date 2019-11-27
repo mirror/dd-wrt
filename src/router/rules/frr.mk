31ab1bbde59002a1f39213df383d9333e23f5596
BrainSlayer <s.gottschall@dd-wrt.com>
2019-11-27 01:09:14 +0000

    update frr
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41609 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/frr.mk
+++ src/router/rules/frr.mk
@@ -1,8 +1,20 @@
-frr-configure: ncurses json-c readline libyang
+libcap:
+	make -C libcap
+
+libcap-clean:
+	make -C libcap clean
+
+libcap-install:
+	make -C libcap install DESTDIR=$(INSTALLDIR)/libcap
+
+
+
+
+frr-configure: ncurses json-c readline libyang libcap
 	cd frr && autoreconf --force --install
 	rm -rf frr/build
 	-mkdir -p frr/build
-	cd frr/build && ../configure CC="" CFLAGS="" LDFLAGS="" LD="" --with-vtysh-pager=less --disable-eigrpd --disable-ldpd --disable-pbrd --disable-rfptest --disable-ssd  --disable-doc --enable-clippy-only --disable-zeromq --enable-opaque-lsa --disable-nhrpd --enable-ospf-te --disable-ospfclient --enable-multipath=32  --enable-ipv6 --prefix=/usr --sysconfdir=/tmp --disable-ospf6d  --enable-vtysh --enable-user=root --enable-group=root --disable-ospfapi --disable-isisd --disable-pimd --disable-nhrpd --disable-staticd --disable-bfdd --disable-babeld --enable-pie=no
+	cd frr/build && ../configure CC="" CFLAGS="" LDFLAGS="" LD="" --with-vtysh-pager=less --disable-eigrpd --disable-ldpd --enable-shared --disable-pbrd --disable-rfptest --disable-ssd  --disable-doc --enable-clippy-only --enable-shared --disable-zeromq --enable-opaque-lsa --disable-nhrpd --enable-ospf-te --disable-ospfclient --enable-multipath=32  --enable-ipv6 --prefix=/usr --sysconfdir=/tmp --disable-ospf6d  --enable-vtysh --enable-user=root --enable-group=root --disable-ospfapi --disable-isisd --disable-pimd --disable-nhrpd --disable-staticd --disable-bfdd --disable-babeld --enable-pie=no
 	make -C frr/build lib/clippy
 	-mkdir -p frr/build/hosttools/lib
 	cd frr/build && cp -vR lib/* hosttools/lib
@@ -16,6 +28,7 @@ frr-configure: ncurses json-c readline libyang
 		--disable-ldpd  \
 		--disable-rfptest \
 		--disable-ssd \
+		--enable-shared \
 		--disable-doc \
 		--disable-zeromq \
 		--disable-backtrace \
@@ -24,13 +37,13 @@ frr-configure: ncurses json-c readline libyang
 		--enable-ospf-te --disable-ospfclient --enable-multipath=32  --enable-ipv6 --prefix=/usr --sysconfdir=/tmp --disable-ospf6d \
 		--enable-vtysh --enable-user=root --enable-group=root --disable-ospfapi --disable-isisd --disable-pimd --disable-nhrpd \
 		--disable-staticd --disable-bfdd --disable-babeld --enable-pie=no --with-libreadline=$(TOP)/readline \
-		CFLAGS="-fno-strict-aliasing -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/ -Drpl_malloc=malloc -Drpl_realloc=realloc -I$(TOP)/_staging/usr/include -I$(TOP)/frr/build -I$(TOP)/libyang/src" \
-		CPPFLAGS="-fno-strict-aliasing -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/ -Drpl_malloc=malloc -Drpl_realloc=realloc -I$(TOP)/_staging/usr/include -I$(TOP)/frr/build -I$(TOP)/libyang/src" \
-		LDFLAGS="-L$(TOP)/readline/shlib -L$(TOP)/ncurses/lib -lncurses -L$(TOP)/json-c/.libs -ljson-c -L$(TOP)/libyang -lyang -L$(TOP)/pcre/.libs -lpcre" \
+		CFLAGS="-fno-strict-aliasing -I$(TOP)/libcap/libcap/include -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/ -Drpl_malloc=malloc -Drpl_realloc=realloc -I$(TOP)/_staging/usr/include -I$(TOP)/frr/build -I$(TOP)/libyang/src" \
+		CPPFLAGS="-fno-strict-aliasing -I$(TOP)/libcap/libcap/include -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/ -Drpl_malloc=malloc -Drpl_realloc=realloc -I$(TOP)/_staging/usr/include -I$(TOP)/frr/build -I$(TOP)/libyang/src" \
+		LDFLAGS="-L$(TOP)/readline/shlib -L$(TOP)/ncurses/lib -lncurses -L$(TOP)/json-c/.libs -ljson-c -L$(TOP)/libyang -lyang -L$(TOP)/pcre/.libs -lpcre -L$(TOP)/libcap/libcap -lcap" \
 		LIBYANG_CFLAGS="-I$(TOP)/libyang/src" \
 		LIBYANG_LIBS="-L$(TOP)/libyang -lyang -L$(TOP)/pcre/.libs -lpcre"
 
-frr: ncurses json-c libyang
+frr: ncurses json-c libyang libcap
 	make -C frr/build
 
 frr-install:
