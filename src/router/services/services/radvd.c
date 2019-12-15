b31260be2f97150cb8379442c94cf1fb129397f2
BrainSlayer <s.gottschall@dd-wrt.com>
2019-12-15 22:12:53 +0000

    allow to adjust prefix len
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41728 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/radvd.c
+++ src/router/services/services/radvd.c
@@ -146,14 +146,14 @@ void start_radvd(void)
 			" AdvManagedFlag %s;\n"
 			" AdvOtherConfigFlag on;\n"
 			" AdvLinkMTU %d;\n"
-			" prefix %s/64 \n"
+			" prefix %s/%d \n"
 			" {\n"
 			"  AdvOnLink on;\n"
 			"  AdvAutonomous %s;\n"
 			"  AdvValidLifetime 30;\n"
 			"  AdvPreferredLifetime 20;\n"
 			"%s%s%s"
-			" };\n", nvram_safe_get("lan_ifname"), manual ? "on" : "off", mtu, prefix, manual ? "off" : "on", do_6to4 ? "  Base6to4Interface " : "", do_6to4 ? get_wan_face() : "", do_6to4 ? ";\n" : "");
+			" };\n", nvram_safe_get("lan_ifname"), manual ? "on" : "off", mtu, prefix, nvram_geti("ipv6_pf_len"), manual ? "off" : "on", do_6to4 ? "  Base6to4Interface " : "", do_6to4 ? get_wan_face() : "", do_6to4 ? ";\n" : "");
 
 		char ipv6_dns_str[1024] = "";
 		char nvname[sizeof("ipv6_dnsXXX")];
