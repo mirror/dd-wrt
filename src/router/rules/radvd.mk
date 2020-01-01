fe50afc091983986b708148c4984ad8b9a3014af
BrainSlayer <s.gottschall@dd-wrt.com>
2020-01-01 23:48:55 +0000

    doesnt bring much, but who knows
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41860 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/radvd.mk
+++ src/router/rules/radvd.mk
@@ -1,9 +1,13 @@
 radvd-configure: nvram libutils
 	cd radvd && ./autogen.sh
-	cd radvd/libdaemon && ./configure --disable-nls --disable-shared --enable-static --disable-lynx --prefix=/usr --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS) -ffunction-sections -fdata-sections -Wl,--gc-sections "  ac_cv_func_setpgrp_void=yes ; make
+	cd radvd/libdaemon && ./configure --disable-nls --disable-shared --enable-static --disable-lynx --prefix=/usr --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" AR_FLAGS="cru $(LTOPLUGIN)" RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)" ac_cv_func_setpgrp_void=yes ; make
 	-cd radvd && aclocal && autoconf && automake -a && cd .. 
-	cd radvd/flex && ./configure --disable-nls --prefix=/usr --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS) -ffunction-sections -fdata-sections -Wl,--gc-sections"
-	cd radvd && ./configure --host=$(ARCH)-linux CHECK_CFLAGS=no CHECK_LIBS=no CFLAGS="$(COPTS) -DNEED_PRINTF -I$(TOP)/radvd/flex -ffunction-sections -fdata-sections -Wl,--gc-sections " DAEMON_CFLAGS="-I$(TOP)/radvd/libdaemon" DAEMON_LIBS="-L$(TOP)/radvd/libdaemon/libdaemon/.libs  -ldaemon" LDFLAGS="-L$(TOP)/radvd/flex -L$(TOP)/libutils -lutils -lshutils -L$(TOP)/nvram -lnvram -ffunction-sections -fdata-sections -Wl,--gc-sections"  --with-flex=$(TOP)/radvd/flex; \
+	cd radvd/flex && ./configure --disable-nls --prefix=/usr --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	AR_FLAGS="cru $(LTOPLUGIN)" \
+	RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
+	cd radvd && ./configure --host=$(ARCH)-linux CHECK_CFLAGS=no CHECK_LIBS=no LDFLAGS="$(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections"  CFLAGS="$(COPTS) $(LTO) -DNEED_PRINTF -I$(TOP)/radvd/flex -ffunction-sections -fdata-sections -Wl,--gc-sections " DAEMON_CFLAGS="-I$(TOP)/radvd/libdaemon" DAEMON_LIBS="-L$(TOP)/radvd/libdaemon/libdaemon/.libs  -ldaemon" LDFLAGS="-L$(TOP)/radvd/flex -L$(TOP)/libutils -lutils -lshutils -L$(TOP)/nvram -lnvram -ffunction-sections -fdata-sections -Wl,--gc-sections"  --with-flex=$(TOP)/radvd/flex \
+	AR_FLAGS="cru $(LTOPLUGIN)" \
+	RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
 	
 radvd-clean:
 	if test -e "radvd/Makefile"; then make -C radvd/flex clean; make -C radvd clean; make -C radvd/libdaemon clean; fi
