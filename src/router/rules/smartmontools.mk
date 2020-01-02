80ee1f2a54cf4131744672f31306f2dcb3109314
BrainSlayer <s.gottschall@dd-wrt.com>
2020-01-02 11:49:42 +0000

    enable lto
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41867 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/smartmontools.mk
+++ src/router/rules/smartmontools.mk
@@ -1,13 +1,14 @@
 smartmontools-configure:
 	cd smartmontools && ./autogen.sh
 	cd smartmontools && ./configure --host=$(ARCH)-linux-uclibc \
-		CPPFLAGS="$(COPTS)  $(MIPS16_OPT) -fPIC -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-		CXXFLAGS="$(COPTS)  $(MIPS16_OPT) -fPIC -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-		CFLAGS="$(COPTS)  $(MIPS16_OPT) -fPIC -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-		LDFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections" \
+		CPPFLAGS="$(LTO) $(COPTS)  $(MIPS16_OPT) -fPIC -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+		CXXFLAGS="$(LTO) $(COPTS)  $(MIPS16_OPT) -fPIC -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+		CFLAGS="$(LTO) $(COPTS)  $(MIPS16_OPT) -fPIC -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+		LDFLAGS="$(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 		--prefix=/usr --libdir=/usr/lib
 
 smartmontools:
+	$(MAKE) -C smartmontools svnversion.h
 	$(MAKE) -C smartmontools
 
 smartmontools-clean:
