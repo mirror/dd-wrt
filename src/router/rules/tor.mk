d75341a823a3091499fdc117d5249d6cef35d1a8
BrainSlayer <s.gottschall@dd-wrt.com>
2020-01-03 21:28:31 +0000

    fix up arflags
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41882 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/tor.mk
+++ src/router/rules/tor.mk
@@ -21,24 +21,27 @@ tor-configure: openssl xz zstd libevent
 	--disable-asciidoc \
 	--enable-lzma \
 	--enable-zstd \
+	--disable-unittests \
 	--disable-tool-name-check \
 	--disable-gcc-hardening \
 	CFLAGS="$(LTO) $(COPTS) $(MIPS16_OPT) -std=gnu99 -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib -I$(TOP) -I$(TOP)/openssl/include -I$(TOP)/libevent -I$(TOP)/libevent/include -I$(TOP)/xz/src/liblzma/api -I$(TOP)/zstd/lib" \
 	CPPFLAGS="$(LTO) $(COPTS) $(MIPS16_OPT) -std=gnu99 -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib -I$(TOP) -I$(TOP)/openssl/include -I$(TOP)/libevent  -I$(TOP)/libevent/include -I$(TOP)/xz/src/liblzma/api -I$(TOP)/zstd/lib" \
 	LDFLAGS="$(LDLTO) $(COPTS) $(MIPS16_OPT) -std=gnu99  -L$(TOP)/zlib   -L$(TOP)/openssl -L$(TOP)/libevent/.libs -L$(TOP)/xz/src/liblzma/.libs -L$(TOP)/zstd/lib" \
 	AR_FLAGS="cru $(LTOPLUGIN)" \
+	ARFLAGS="cru $(LTOPLUGIN)" \
 	RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)" \
 	ZSTD_CFLAGS="-I$(TOP)/zstd/lib" \
 	ZSTD_LIBS="-I$(TOP)/zstd/lib -lzstd"
 
 tor: openssl libevent
-	make -C tor
+	make -C tor ARFLAGS="cru $(LTOPLUGIN)"  all
 
 tor-clean:
 	make -C tor clean
 
 tor-install:
 	make -C tor DESTDIR=$(INSTALLDIR)/tor install
+
 	mkdir -p $(INSTALLDIR)/tor/etc/config
 	install -D tor/config/tor.webservices $(INSTALLDIR)/tor/etc/config
 	install -D tor/config/tor.nvramconfig $(INSTALLDIR)/tor/etc/config
