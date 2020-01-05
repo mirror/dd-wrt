be9e76d653d22511348920dc2d54df7e05d72cd2
BrainSlayer <s.gottschall@dd-wrt.com>
2020-01-05 20:17:08 +0000

    some openwrt patches
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41897 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/pppd/pppd/chap-new.c
+++ src/router/pppd/pppd/chap-new.c
@@ -58,6 +58,7 @@ int (*chap_verify_hook)(char *name, char *ourname, int id,
 int chap_timeout_time = 3;
 int chap_max_transmits = 10;
 int chap_rechallenge_time = 0;
+int chapms_strip_domain = 0;
 
 /*
  * Command-line options.
@@ -69,6 +70,8 @@ static option_t chap_option_list[] = {
 	  "Set max #xmits for challenge", OPT_PRIO },
 	{ "chap-interval", o_int, &chap_rechallenge_time,
 	  "Set interval for rechallenge", OPT_PRIO },
+	{ "chapms-strip-domain", o_bool, &chapms_strip_domain,
+	  "Strip the domain prefix before the Username", 1 },
 	{ NULL }
 };
 
@@ -336,6 +339,14 @@ chap_handle_response(struct chap_server_state *ss, int id,
 			/* Null terminate and clean remote name. */
 			slprintf(rname, sizeof(rname), "%.*v", len, name);
 			name = rname;
+
+			/* strip the MS domain name */
+			if (chapms_strip_domain && strrchr(rname, '\\')) {
+				char tmp[MAXNAMELEN+1];
+
+				strcpy(tmp, strrchr(rname, '\\') + 1);
+				strcpy(rname, tmp);
+			}
 		}
 
 		if (chap_verify_hook)
