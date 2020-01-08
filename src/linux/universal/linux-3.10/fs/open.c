04e471e7b27cc82f7a77afb0097a76c7058b9f34
BrainSlayer <s.gottschall@dd-wrt.com>
2020-01-08 01:26:52 +0000

    same backport
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41938 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.10/fs/open.c
+++ src/linux/universal/linux-3.10/fs/open.c
@@ -221,7 +221,7 @@ SYSCALL_DEFINE2(ftruncate64, unsigned int, fd, loff_t, length)
 #endif /* BITS_PER_LONG == 32 */
 
 
-int do_fallocate(struct file *file, int mode, loff_t offset, loff_t len)
+int vfs_fallocate(struct file *file, int mode, loff_t offset, loff_t len)
 {
 	struct inode *inode = file_inode(file);
 	long ret;
@@ -278,6 +278,7 @@ int do_fallocate(struct file *file, int mode, loff_t offset, loff_t len)
 	sb_end_write(inode->i_sb);
 	return ret;
 }
+EXPORT_SYMBOL(vfs_fallocate);
 
 SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 {
@@ -285,7 +286,7 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 	int error = -EBADF;
 
 	if (f.file) {
-		error = do_fallocate(f.file, mode, offset, len);
+		error = vfs_fallocate(f.file, mode, offset, len);
 		fdput(f);
 	}
 	return error;
