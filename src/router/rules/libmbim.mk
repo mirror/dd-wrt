543098249f92403a6f7c1539f9eee309d42a1b26
BrainSlayer <s.gottschall@dd-wrt.com>
2020-01-09 21:39:01 +0000

    we dont need this anymore
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41962 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libmbim.mk
+++ src/router/rules/libmbim.mk
@@ -2,7 +2,7 @@ GLIB_GIO=-lgio-2.0
 libmbim-configure: glib20
 	cd libmbim && ./autogen.sh
 	cd libmbim && ./configure --enable-more-warnings=no --enable-static --disable-shared --libexecdir=/usr/lib --host=$(ARCH)-linux CFLAGS="$(COPTS) $(MIPS16_OPT) -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_realloc=realloc -Drpl_malloc=malloc -I$(TOP)/glib20/libglib/glib -I$(TOP)/glib20/libglib/gobject -I$(TOP)/_staging/usr/include -liconv -lgnuintl -lpthread -DMESSAGE_ENABLE_TRACE" \
-	LDFLAGS="-L$(TOP)/glib20/gettext/gettext-runtime/intl/.libs -L$(TOP)/glib20/libiconv/lib/.libs -L$(TOP)/glib20/libglib/gobject/.libs -L$(TOP)/glib20/libglib/gthread/.libs -L$(TOP)/glib20/libglib/gio/.libs -L$(TOP)/_staging/usr/lib" \
+	LDFLAGS="-L$(TOP)/glib20/gettext/gettext-runtime/intl/.libs -L$(TOP)/glib20/libglib/gobject/.libs -L$(TOP)/glib20/libglib/gthread/.libs -L$(TOP)/glib20/libglib/gio/.libs -L$(TOP)/_staging/usr/lib" \
 	LIBMBIM_GLIB_CFLAGS="$(COPTS) -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_realloc=realloc -Drpl_malloc=malloc -I$(TOP)/glib20/libglib/glib -I$(TOP)/glib20/libglib -I$(TOP)/glib20/libglib/gmodule -pthread" \
 	LIBMBIM_GLIB_LIBS="-pthread -lpthread -L$(TOP)/glib20/libglib/glib/.libs -L$(TOP)/glib20/libglib/gmodule/.libs -L$(TOP)/glib20/libglib/gthread/.libs $(GLIB_GIO) -lgobject-2.0 -lglib-2.0" \
 	MBIMCLI_CFLAGS="$(COPTS) -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_realloc=realloc -Drpl_malloc=malloc -I$(TOP)/glib20/libglib/glib -I$(TOP)/glib20/libglib -I$(TOP)/glib20/libglib/gmodule -I$(TOP)/_staging/usr/include -pthread" \
