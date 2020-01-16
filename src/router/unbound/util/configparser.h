bd39a6be66e44cb056a78a54ff7f9074282017bb
BrainSlayer <s.gottschall@dd-wrt.com>
2020-01-16 14:05:58 +0000

    update unbound: add new files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@41998 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/unbound/util/configparser.h
+++ src/router/unbound/util/configparser.h
@@ -1,8 +1,8 @@
-/* A Bison parser, made by GNU Bison 3.0.5.  */
+/* A Bison parser, made by GNU Bison 3.0.4.  */
 
 /* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015, 2018 Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -588,11 +588,11 @@ extern int yydebug;
 
 union YYSTYPE
 {
-#line 66 "util/configparser.y" /* yacc.c:1910  */
+#line 66 "util/configparser.y" /* yacc.c:1909  */
 
 	char*	str;
 
-#line 596 "util/configparser.h" /* yacc.c:1910  */
+#line 596 "util/configparser.h" /* yacc.c:1909  */
 };
 
 typedef union YYSTYPE YYSTYPE;
