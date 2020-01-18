bf6a214f1afe30ddae410b5bfd009f1430212334
BrainSlayer <s.gottschall@dd-wrt.com>
2020-01-18 19:04:44 +0000

    use lto for wolfssl
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42030 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/wolfssl.mk
+++ src/router/rules/wolfssl.mk
@@ -1,6 +1,8 @@
 wolfssl-configure:
 	cd wolfssl && ./autogen.sh
-	cd wolfssl && ./configure --prefix=/usr --host=$(ARCH)-linux --enable-opensslextra --disable-shared --enable-static -disable-errorstrings --disable-oldtls --disable-poly1305 --disable-chacha --enable-ecc --disable-sslv3 --enable-des3 --enable-md4 --enable-stunnel --enable-session-ticket --enable-wpas --enable-cmac CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections"
+	cd wolfssl && ./configure --prefix=/usr --host=$(ARCH)-linux --enable-opensslextra --disable-shared --enable-static -disable-errorstrings --disable-oldtls --disable-poly1305 --disable-chacha --enable-ecc --disable-sslv3 --enable-des3 --enable-md4 --enable-stunnel --enable-session-ticket --enable-wpas --enable-cmac CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	    AR_FLAGS="cru $(LTOPLUGIN)" \
+	    RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
 
 wolfssl:
 	$(MAKE) -j 4 -C wolfssl
