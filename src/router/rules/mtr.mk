0867357a2f39a875871aeb034e09247ad5689df6
BrainSlayer <s.gottschall@dd-wrt.com>
2020-01-19 00:31:54 +0000

    reconf
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42036 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/mtr.mk
+++ src/router/rules/mtr.mk
@@ -1,4 +1,5 @@
 mtr-configure: ncurses-configure ncurses
+	cd mtr && autoreconf -fi
 	cd mtr && ./configure --disable-nls --without-glib --without-gtk --prefix=/usr --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT) -D__GLIBC__ -I$(TOP)/ncurses/include -L$(TOP)/ncurses/lib  -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF  -lncurses" --without-gtk ac_cv_lib_resolv_res_mkquery=yes
 	cd mtr && touch *
 
