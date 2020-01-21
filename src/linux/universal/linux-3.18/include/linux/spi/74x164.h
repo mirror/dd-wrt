7751380a59af20d799e46dff5e1a462eba89bd2d
BrainSlayer <s.gottschall@dd-wrt.com>
2020-01-21 19:19:16 +0000

    archerc25 support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42062 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/linux/universal/linux-3.18/include/linux/spi/74x164.h
@@ -0,0 +1,13 @@
+#ifndef LINUX_SPI_74X164_H
+#define LINUX_SPI_74X164_H
+
+struct gen_74x164_chip_platform_data {
+	/* number assigned to the first GPIO */
+	unsigned        base;
+	/* number of chained registers */
+	unsigned	num_registers;
+	/* address of a buffer containing initial data */
+	u8		*init_data;
+};
+
+#endif
