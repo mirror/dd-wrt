cd98b28b6ebec6091de8c05c6c1bf728ba1b8b22
BrainSlayer <s.gottschall@dd-wrt.com>
2020-01-21 19:18:44 +0000

    archerc25 support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42060 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/arch/mips/ar7240/dev-ap91-pci.c
+++ src/linux/universal/linux-3.18/arch/mips/ar7240/dev-ap91-pci.c
@@ -76,6 +76,7 @@ void ap91_pci_init(u8 *cal_data, u8 *mac_addr)
 #ifndef CONFIG_LIMA
 #ifndef CONFIG_MMS344
 #ifndef CONFIG_RAMBUTAN
+#ifndef CONFIG_ARCHERC25
 	if (cal_data)
 		memcpy(wmac_data.eeprom_data, cal_data, sizeof(wmac_data.eeprom_data));
 
@@ -86,6 +87,7 @@ void ap91_pci_init(u8 *cal_data, u8 *mac_addr)
 
 	ar71xx_pci_plat_dev_init = ap91_pci_plat_dev_init;
 	pci_enable_ath9k_fixup(0, wmac_data.eeprom_data);
+#endif
 #endif
 #endif
 #endif
