20c1bfb56292d7442002d5c49150775c06497c3f
BrainSlayer <s.gottschall@dd-wrt.com>
2020-01-21 21:01:43 +0000

    update wireguard
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42068 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/wireguard/src/crypto/zinc/curve25519/curve25519-x86_64-glue.c
+++ src/router/wireguard/src/crypto/zinc/curve25519/curve25519-x86_64-glue.c
@@ -8,29 +8,25 @@
 
 #include "curve25519-x86_64.c"
 
-static bool curve25519_use_bmi2 __ro_after_init;
-static bool curve25519_use_adx __ro_after_init;
+static bool curve25519_use_bmi2_adx __ro_after_init;
 static bool *const curve25519_nobs[] __initconst = {
-	&curve25519_use_bmi2, &curve25519_use_adx };
+	&curve25519_use_bmi2_adx };
 
 static void __init curve25519_fpu_init(void)
 {
-	curve25519_use_bmi2 = IS_ENABLED(CONFIG_AS_BMI2) &&
-			      boot_cpu_has(X86_FEATURE_BMI2);
-	curve25519_use_adx = IS_ENABLED(CONFIG_AS_ADX) &&
-			     boot_cpu_has(X86_FEATURE_BMI2) &&
-			     boot_cpu_has(X86_FEATURE_ADX);
+	curve25519_use_bmi2_adx = IS_ENABLED(CONFIG_AS_BMI2) &&
+				  IS_ENABLED(CONFIG_AS_ADX) &&
+				  boot_cpu_has(X86_FEATURE_BMI2) &&
+				  boot_cpu_has(X86_FEATURE_ADX);
 }
 
 static inline bool curve25519_arch(u8 mypublic[CURVE25519_KEY_SIZE],
 				   const u8 secret[CURVE25519_KEY_SIZE],
 				   const u8 basepoint[CURVE25519_KEY_SIZE])
 {
-	if (IS_ENABLED(CONFIG_AS_ADX) && curve25519_use_adx) {
-		curve25519_adx(mypublic, secret, basepoint);
-		return true;
-	} else if (IS_ENABLED(CONFIG_AS_BMI2) && curve25519_use_bmi2) {
-		curve25519_bmi2(mypublic, secret, basepoint);
+	if (IS_ENABLED(CONFIG_AS_ADX) && IS_ENABLED(CONFIG_AS_BMI2) &&
+	    curve25519_use_bmi2_adx) {
+		curve25519_ever64(mypublic, secret, basepoint);
 		return true;
 	}
 	return false;
@@ -39,11 +35,9 @@ static inline bool curve25519_arch(u8 mypublic[CURVE25519_KEY_SIZE],
 static inline bool curve25519_base_arch(u8 pub[CURVE25519_KEY_SIZE],
 					const u8 secret[CURVE25519_KEY_SIZE])
 {
-	if (IS_ENABLED(CONFIG_AS_ADX) && curve25519_use_adx) {
-		curve25519_adx_base(pub, secret);
-		return true;
-	} else if (IS_ENABLED(CONFIG_AS_BMI2) && curve25519_use_bmi2) {
-		curve25519_bmi2_base(pub, secret);
+	if (IS_ENABLED(CONFIG_AS_ADX) && IS_ENABLED(CONFIG_AS_BMI2) &&
+	    curve25519_use_bmi2_adx) {
+		curve25519_ever64_base(pub, secret);
 		return true;
 	}
 	return false;
