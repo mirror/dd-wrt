6181209d619bfa6cdcd6126383c823bea714aa57
BrainSlayer <s.gottschall@dd-wrt.com>
2020-01-21 08:42:54 +0000

    install screenrc
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42055 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/screen.mk
+++ src/router/rules/screen.mk
@@ -1,6 +1,6 @@
 screen-configure: ncurses
 	cd screen && autoreconf --force --install --symlink
-	cd screen && ./configure --host=$(ARCH)-linux --enable-colors256 CFLAGS="$(COPTS) $(MIPS16_OPT)  -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF -I$(TOP)/ncurses/include" LDFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/ncurses/lib" --prefix=/usr ac_cv_safe_to_define___extensions__=no
+	cd screen && ./configure --host=$(ARCH)-linux --enable-colors256 --with-sys-screenrc=/etc/screenrc CFLAGS="$(COPTS) $(MIPS16_OPT)  -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF -I$(TOP)/ncurses/include" LDFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/ncurses/lib" --prefix=/usr ac_cv_safe_to_define___extensions__=no
 
 screen: ncurses
 	make   -C screen
@@ -13,4 +13,5 @@ screen-install:
 	rm -rf $(INSTALLDIR)/screen/usr/share/man
 	rm -rf $(INSTALLDIR)/screen/usr/share/info
 	rm -rf $(INSTALLDIR)/screen/usr/lib
-
+	install -Dm644 etc/etcscreenrc $(INSTALLDIR)/screen/etc/screenrc
+	install -Dm644 etc/screenrc $(INSTALLDIR)/screen/etc/skel/.screenrc
