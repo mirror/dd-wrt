1fb3528d04ed497500a0ddaf77906f627a684178
BrainSlayer <s.gottschall@dd-wrt.com>
2020-01-22 16:56:55 +0000

    add missing files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42076 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/smbd/tools/include/usmbdtools.h
+++ src/router/smbd/tools/include/usmbdtools.h
@@ -1,12 +1,12 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
  *   Copyright (C) 2018 Samsung Electronics Co., Ltd.
  *
  *   linux-cifsd-devel@lists.sourceforge.net
  */
 
-#ifndef __SMBDTOOLS_H__
-#define __SMBDTOOLS_H__
+#ifndef __USMBDTOOLS_H__
+#define __USMBDTOOLS_H__
 
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE 1
@@ -54,38 +54,38 @@ struct smbconf_global {
 	unsigned int		smb2_max_trans;
 };
 
-#define SMBD_LOCK_FILE		"/tmp/smbd.lock"
+#define USMBD_LOCK_FILE		"/tmp/usmbd.lock"
 
-#define SMBD_RESTRICT_ANON_TYPE_1	1
-#define SMBD_RESTRICT_ANON_TYPE_2	2
+#define USMBD_RESTRICT_ANON_TYPE_1	1
+#define USMBD_RESTRICT_ANON_TYPE_2	2
 
 extern struct smbconf_global global_conf;
 
-#define SMBD_CONF_MAP_TO_GUEST_NEVER		(0)
-#define SMBD_CONF_MAP_TO_GUEST_BAD_USER	(1 << 0)
-#define SMBD_CONF_MAP_TO_GUEST_BAD_PASSWORD	(1 << 1)
-#define SMBD_CONF_MAP_TO_GUEST_BAD_UID		(1 << 2)
+#define USMBD_CONF_MAP_TO_GUEST_NEVER		(0)
+#define USMBD_CONF_MAP_TO_GUEST_BAD_USER	(1 << 0)
+#define USMBD_CONF_MAP_TO_GUEST_BAD_PASSWORD	(1 << 1)
+#define USMBD_CONF_MAP_TO_GUEST_BAD_UID		(1 << 2)
 
-#define SMBD_CONF_DEFAULT_NETBIOS_NAME	"SMBD SERVER"
-#define SMBD_CONF_DEFAULT_SERVER_STRING	"SMBD"
-#define SMBD_CONF_DEFAULT_WORK_GROUP		"WORKGROUP"
+#define USMBD_CONF_DEFAULT_NETBIOS_NAME	"USMBD SERVER"
+#define USMBD_CONF_DEFAULT_SERVER_STRING	"SMB SERVER"
+#define USMBD_CONF_DEFAULT_WORK_GROUP		"WORKGROUP"
 
-#define SMBD_CONF_DEFAULT_GUEST_ACCOUNT	"nobody"
-#define SMBD_CONF_FALLBACK_GUEST_ACCOUNT	"ftp"
+#define USMBD_CONF_DEFAULT_GUEST_ACCOUNT	"nobody"
+#define USMBD_CONF_FALLBACK_GUEST_ACCOUNT	"ftp"
 
-#define SMBD_CONF_DEFAULT_SESS_CAP	1024
-#define SMBD_CONF_DEFAULT_TPC_PORT	445
+#define USMBD_CONF_DEFAULT_SESS_CAP	1024
+#define USMBD_CONF_DEFAULT_TPC_PORT	445
 
-#define SMBD_CONF_FILE_MAX		10000
+#define USMBD_CONF_FILE_MAX		10000
 
-#define PATH_PWDDB	"/etc/smbd/smbdpwd.db"
-#define PATH_SMBCONF	"/etc/smbd/smb.conf"
+#define PATH_PWDDB	"/etc/ksmbd/ksmbdpwd.db"
+#define PATH_SMBCONF	"/etc/ksmbd/smb.conf"
 
-#define SMBD_HEALTH_START		(0)
-#define SMBD_HEALTH_RUNNING		(1 << 0)
-#define SMBD_SHOULD_RELOAD_CONFIG	(1 << 1)
+#define USMBD_HEALTH_START		(0)
+#define USMBD_HEALTH_RUNNING		(1 << 0)
+#define USMBD_SHOULD_RELOAD_CONFIG	(1 << 1)
 
-extern int smbd_health_status;
+extern int usmbd_health_status;
 
 #define TRACING_DUMP_NL_MSG	0
 
@@ -114,7 +114,7 @@ static int log_level = PR_DEBUG;
 char *strerr(int err);
 
 __attribute__ ((format (printf, 2, 3)))
-extern void __pr_log(int level, const char *fmt,...);
+extern void __pr_log(int level, const char *fmt, ...);
 extern void set_logger_app_name(const char *an);
 extern const char *get_logger_app_name(void);
 extern void pr_logger_init(int flags);
@@ -127,11 +127,11 @@ extern void pr_logger_init(int flags);
 					##__VA_ARGS__);			\
 	} while (0)
 
-#define pr_debug(f,...)	\
+#define pr_debug(f, ...)	\
 	pr_log(PR_DEBUG, PRDEBUG f, ##__VA_ARGS__)
-#define pr_info(f,...)	\
+#define pr_info(f, ...)	\
 	pr_log(PR_INFO, PRINF f, ##__VA_ARGS__)
-#define pr_err(f,...)	\
+#define pr_err(f, ...)	\
 	pr_log(PR_ERROR, PRERR f, ##__VA_ARGS__)
 
 //---------------------------------------------------------------//
@@ -141,7 +141,7 @@ void pr_hex_dump(const void *mem, size_t sz);
 char *base64_encode(unsigned char *src, size_t srclen);
 unsigned char *base64_decode(char const *src, size_t *dstlen);
 
-gchar *smbd_gconvert(const gchar *str,
+gchar *usmbd_gconvert(const gchar *str,
 		      gssize       str_len,
 		      int          to_codeset,
 		      int          from_codeset,
@@ -149,19 +149,19 @@ gchar *smbd_gconvert(const gchar *str,
 		      gsize       *bytes_written);
 
 enum charset_idx {
-	SMBD_CHARSET_UTF8		= 0,
-	SMBD_CHARSET_UTF16LE,
-	SMBD_CHARSET_UCS2LE,
-	SMBD_CHARSET_UTF16BE,
-	SMBD_CHARSET_UCS2BE,
-	SMBD_CHARSET_MAX		= 5,
+	USMBD_CHARSET_UTF8		= 0,
+	USMBD_CHARSET_UTF16LE,
+	USMBD_CHARSET_UCS2LE,
+	USMBD_CHARSET_UTF16BE,
+	USMBD_CHARSET_UCS2BE,
+	USMBD_CHARSET_MAX		= 5,
 };
 
-#define SMBD_CHARSET_DEFAULT		SMBD_CHARSET_UTF8
+#define USMBD_CHARSET_DEFAULT		USMBD_CHARSET_UTF8
 
-extern char *smbd_conv_charsets[SMBD_CHARSET_MAX + 1];
+extern char *usmbd_conv_charsets[USMBD_CHARSET_MAX + 1];
 
-void notify_smbd_daemon(void);
+void notify_usmbd_daemon(void);
 int test_file_access(char *conf);
 
-#endif /* __SMBDTOOLS_H__ */
+#endif /* __USMBDTOOLS_H__ */
