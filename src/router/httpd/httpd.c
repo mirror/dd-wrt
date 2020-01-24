d93b8da4ee264d948bc9d56dfe99db0c7059e034
BrainSlayer <s.gottschall@dd-wrt.com>
2020-01-24 21:13:11 +0000

    in case thread fails, release semaphore
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42099 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/httpd.c
+++ src/router/httpd/httpd.c
@@ -1891,8 +1891,10 @@ int main(int argc, char **argv)
 		pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
 //              pthread_attr_setstacksize(&attr, 4096);
 		pthread_t thread;
-		if (pthread_create(&thread, &attr, handle_request, conn_fp) != 0)
+		if (pthread_create(&thread, &attr, handle_request, conn_fp) != 0) {
+			SEM_POST(&semaphore);
 			fprintf(stderr, "Failed to create thread\n");
+		}
 		pthread_attr_destroy(&attr);
 
 //              conn_fp->do_ssl = do_ssl;
