2b856c6b101720016ef5a6d876154dcba730ac55
BrainSlayer <s.gottschall@dd-wrt.com>
2020-01-26 11:39:46 +0000

    revert
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42119 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/smbd/smbd/server.c
+++ src/router/smbd/smbd/server.c
@@ -434,7 +434,7 @@ static ssize_t kill_server_store(struct class *class,
 	if (!sysfs_streq(buf, "hard"))
 		return len;
 
-	ksmbd_err("kill command received\n");
+	ksmbd_info("shutting down ksmbd\n");
 	mutex_lock(&ctrl_lock);
 	WRITE_ONCE(server_conf.state, SERVER_STATE_RESETTING);
 	__module_get(THIS_MODULE);
