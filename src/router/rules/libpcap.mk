d995b9c04c88ffe1667f01258125b5187924390a
BrainSlayer <s.gottschall@dd-wrt.com>
2020-01-30 15:25:20 +0000

    do not use static anymore
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42181 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libpcap.mk
+++ src/router/rules/libpcap.mk
@@ -6,30 +6,22 @@ libpcap-configure:
 		--exec-prefix=/usr --bindir=/usr/bin --sbindir=/usr/sbin \
 		--libexecdir=/usr/lib --sysconfdir=/etc --datadir=/usr/share \
 		--localstatedir=/var --mandir=/usr/man --infodir=/usr/info \
-		--disable-nls --enable-shared --enable-static \
+		--disable-nls --enable-shared --disable-static \
 		--disable-yydebug --enable-ipv6 --with-build-cc=gcc \
 		--with-pcap=linux --without-septel --without-dag \
 		--disable-canusb --disable-can --disable-bluetooth \
-		CC="$(CC)" CFLAGS="$(COPTS) -fPIC"
+		CC="$(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT) -fPIC"
 libpcap:
-#	$(MAKE) -C libpcap CC="$(CC)" AR=$(AR) RANLIB=$(RANLIB) libpcap.so 
 	$(MAKE) -C libpcap CC="$(CC)" AR=$(AR) RANLIB=$(RANLIB)
-ifeq ($(CONFIG_LIBPCAP_SHARED),y)
 	$(MAKE) -C libpcap CC="$(CC)" AR=$(AR) RANLIB=$(RANLIB) libpcap.so 
 	-cd libpcap ; ln -s libpcap.so.1.9.1 libpcap.so
 	-cd libpcap ; ln -s libpcap.so libpcap.so.1
-else
-	$(MAKE) -C libpcap CC="$(CC)" AR=$(AR) RANLIB=$(RANLIB) libpcap.a 
-endif
 
 libpcap-install:
-	@true
-ifeq ($(CONFIG_LIBPCAP_SHARED),y)
 	install -d $(INSTALLDIR)/libpcap/usr/lib
 	install libpcap/libpcap.so $(INSTALLDIR)/libpcap/usr/lib
 	-cd $(INSTALLDIR)/libpcap/usr/lib ; ln -s libpcap.so libpcap.so.1 
 	-cd $(INSTALLDIR)/libpcap/usr/lib ; ln -s libpcap.so libpcap.so.1.0.0
-endif
 
 libpcap-clean:
 	$(MAKE) -C libpcap clean
