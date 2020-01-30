103dfe5048099bfa46539ab07ffd9ea5fbe80c44
BrainSlayer <s.gottschall@dd-wrt.com>
2020-01-30 23:50:08 +0000

    revert
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42197 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/fs/cifs/connect.c
+++ src/linux/universal/linux-3.18/fs/cifs/connect.c
@@ -876,7 +876,6 @@ cifs_demultiplex_thread(void *p)
 				GFP_KERNEL);
 
 	set_freezable();
-	allow_kernel_signal(SIGKILL);
 	while (server->tcpStatus != CifsExiting) {
 		if (try_to_freeze())
 			continue;
@@ -2147,7 +2146,7 @@ cifs_put_tcp_session(struct TCP_Server_Info *server, int from_reconnect)
 
 	task = xchg(&server->tsk, NULL);
 	if (task)
-		send_sig(SIGKILL, task, 1);
+		force_sig(SIGKILL, task);
 }
 
 static struct TCP_Server_Info *
