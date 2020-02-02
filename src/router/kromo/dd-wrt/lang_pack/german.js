4b0394536fc999016e1788db871b94340a342b92
BrainSlayer <s.gottschall@dd-wrt.com>
2020-02-02 22:00:23 +0000

    change naming
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42242 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/dd-wrt/lang_pack/german.js
+++ src/router/kromo/dd-wrt/lang_pack/german.js
@@ -2468,8 +2468,8 @@ wl_basic.add="Virtuellen AP Hinzuf&uuml;gen";
 wl_basic.airtime_policy="Airtime Stategie";
 wl_basic.airtime_dynamic="Dynamischer Modus";
 wl_basic.airtime_limit="Limit Modus";
-wl_basic.airtime_weight="Wichtung";
-wl_basic.airtime_dolimit="Limitiere";
+wl_basic.airtime_weight="Airtime Wichtung";
+wl_basic.airtime_dolimit="Airtime Limitiere";
 
 
 //help container
