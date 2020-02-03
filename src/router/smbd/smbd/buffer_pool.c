4ee31db8dc4a30062c07444b21f23169b2083556
BrainSlayer <s.gottschall@dd-wrt.com>
2020-02-03 10:54:23 +0000

    for debugging
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42254 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/smbd/smbd/buffer_pool.c
+++ src/router/smbd/smbd/buffer_pool.c
@@ -310,9 +310,12 @@ void ksmbd_release_buffer(void *buffer)
 
 	wm = container_of(buffer, struct wm, buffer);
 	wm_list = match_wm_list(wm->realsize);
-	//WARN_ON(!wm_list);
+	WARN_ON(!wm_list);
 	if (wm_list)
 		release_wm(wm, wm_list);
+	else {
+		ksmbd_info("wm list empty sz=%zu realsize %zu\n", wm->sz, wm->realsize);
+	}
 }
 
 void *ksmbd_realloc_response(void *ptr, size_t old_sz, size_t new_sz)
