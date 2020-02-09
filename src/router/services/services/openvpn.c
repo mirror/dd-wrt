dcf61cffb332b39e2c1914bada83f740ff5b9d04
BrainSlayer <s.gottschall@dd-wrt.com>
2020-02-09 10:03:16 +0000

    fix delete of fw rules
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42315 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/openvpn.c
+++ src/router/services/services/openvpn.c
@@ -531,11 +531,14 @@ void start_openvpn(void)
 		fprintf(fp, "startservice wshaper -f\n");
 		fprintf(fp, "sleep 5\n");
 	}
-	if (nvram_matchi("openvpncl_nat", 1))
-		fprintf(fp, "iptables -D POSTROUTING -t nat -o %s -j MASQUERADE\n" "iptables -I POSTROUTING -t nat -o %s -j MASQUERADE\n", ovpniface, ovpniface);
-	if (nvram_matchi("openvpncl_sec", 0))
-		fprintf(fp, "iptables -D INPUT -i %s -j ACCEPT\n" "iptables -I INPUT -i %s -j ACCEPT\n", ovpniface, ovpniface);
-	else {
+	if (nvram_matchi("openvpncl_nat", 1)) {
+		fprintf(fp, "iptables -D POSTROUTING -t nat -o %s -j MASQUERADE\n" //
+			    "iptables -I POSTROUTING -t nat -o %s -j MASQUERADE\n", ovpniface, ovpniface);
+	}
+	if (nvram_matchi("openvpncl_sec", 0)) {
+		fprintf(fp, "iptables -D INPUT -i %s -j ACCEPT\n" //
+			    "iptables -I INPUT -i %s -j ACCEPT\n", ovpniface, ovpniface);
+	} else {
 		if (nvram_match("openvpncl_tuntap", "tun"))	//only needed with tun
 			fprintf(fp, "iptables -D INPUT -i %s -j ACCEPT\n"	//
 				"iptables -D FORWARD -i %s -j ACCEPT\n"	//
@@ -600,10 +603,19 @@ void start_openvpn(void)
 	else if (nvram_match("openvpncl_tuntap", "tap")
 		 && *(nvram_safe_get("openvpncl_ip")))
 		fprintf(fp, "ifconfig %s down\n", ovpniface);
-	if (nvram_matchi("openvpncl_nat", 1))
-		fprintf(fp, "iptables -D INPUT -i %s -j ACCEPT\n" "iptables -D POSTROUTING -t nat -o %s -j MASQUERADE\n", ovpniface, ovpniface);
-	else {
-		fprintf(fp, "iptables -D INPUT -i %s -j ACCEPT\n" "iptables -D FORWARD -i %s -j ACCEPT\n" "iptables -D FORWARD -o %s -j ACCEPT\n", ovpniface, ovpniface, ovpniface);
+
+	if (nvram_matchi("openvpncl_nat", 1)) {
+		fprintf(fp, "iptables -D INPUT -i %s -j ACCEPT\n"
+			    "iptables -D POSTROUTING -t nat -o %s -j MASQUERADE\n", ovpniface, ovpniface);
+	}
+	if (nvram_matchi("openvpncl_sec", 0)) {
+		fprintf(fp, "iptables -D INPUT -i %s -j ACCEPT\n", ovpniface);
+	}
+	if (nvram_match("openvpncl_tuntap", "tun")
+	{
+		fprintf(fp, "iptables -D INPUT -i %s -j ACCEPT\n"
+			    "iptables -D FORWARD -i %s -j ACCEPT\n"
+			    "iptables -D FORWARD -o %s -j ACCEPT\n", ovpniface, ovpniface, ovpniface);
 	}
 	if (nvram_default_matchi("openvpncl_fw", 1, 0)) {
 		fprintf(fp, "iptables -D INPUT -i %s -m state --state NEW -j DROP\n", ovpniface);
