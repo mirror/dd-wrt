058fe2fe53d93adca828cc9c6f5072aa5512c89f
BrainSlayer <s.gottschall@dd-wrt.com>
2020-02-10 16:05:03 +0000

    fix arm again
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42336 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/wireguard.mk
+++ src/router/rules/wireguard.mk
@@ -1,6 +1,6 @@
 wireguard: libmnl
 	cd wireguard/src ; $(MAKE) -C $(LINUXDIR) M="$(TOP)/wireguard/src" KERNELDIR=$(LINUX_DIR)
-	cd wireguard/src ; $(MAKE) LIBMNL_CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections -I$(TOP)/libmnl/include" LIBMNL_LDLIBS="$(LDLTO) -L$(TOP)/libmnl/src/.libs -lmnl" RUNSTATEDIR=/var/run tools 
+	cd wireguard/src ; $(MAKE) LIBMNL_CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections -I$(TOP)/libmnl/include" LIBMNL_LDLIBS="$(LDLTO) $(COPTS) $(MIPS16_OPT) -L$(TOP)/libmnl/src/.libs -lmnl" RUNSTATEDIR=/var/run tools 
 
 wireguard-install:
 	install -D wireguard/src/wireguard.ko $(INSTALLDIR)/wireguard/lib/modules/$(KERNELRELEASE)/wireguard.ko
