f49388137a3184b242979ac9902921f38d19fd4b
BrainSlayer <s.gottschall@dd-wrt.com>
2020-02-14 23:53:55 +0000

    since mac80211 does not support multiple country settings (individual per card) we sync all settings
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42386 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/validate/webs.c
+++ src/router/httpd/validate/webs.c
@@ -2752,7 +2752,7 @@ static char *vapsettings[] = {
 	"wds6_if", "wds6_ipaddr", "wds6_netmask", "wds6_ospf", "wds7", "wds7_desc", "wds7_enable", "wds7_hwaddr", "wds7_if", "wds7_ipaddr", "wds7_netmask", "wds7_ospf", "wds8", "wds8_desc", "wds8_enable",
 	"wds8_hwaddr", "wds8_if", "wds8_ipaddr", "wds8_netmask", "wds8_ospf", "wds9", "wds9_desc", "wds9_enable", "wds9_hwaddr", "wds9_if", "wds9_ipaddr", "wds9_netmask", "wds9_ospf", "web_filter", "wep", "wep_bit",
 	"wep_buf", "wme", "wme_ap_be", "wme_ap_bk", "wme_ap_vi", "wme_ap_vo", "wme_apsd", "wme_no_ack", "wme_sta_be", "wme_sta_bk", "wme_sta_vi", "wme_sta_vo", "wme_txp_be", "wme_txp_bk", "wme_txp_vi", "wme_txp_vo",
-	"wmf_bss_enable", "wmf_igmpq_filter", "wmf_mdata_sendup", "wmf_ucast_upnp", "wmf_ucigmp_query", "wmm", "wpa", "wpa2", "wpa2-sha256", "wpa3", "wpa3-128", "wpa3-192", "wpa_gtk_rekey", "wpa_psk", "xr", "at_policy", 
+	"wmf_bss_enable", "wmf_igmpq_filter", "wmf_mdata_sendup", "wmf_ucast_upnp", "wmf_ucigmp_query", "wmm", "wpa", "wpa2", "wpa2-sha256", "wpa3", "wpa3-128", "wpa3-192", "wpa_gtk_rekey", "wpa_psk", "xr", "at_policy",
 	"at_weight", "at_limit"
 };
 
@@ -4183,7 +4183,12 @@ static void save_prefix(webs_t wp, char *prefix)
 #ifdef HAVE_MAKSAT
 	copytonv(wp, "ath_specialmode");
 #endif
+#ifdef HAVE_ATH9K
+	if (!strcmp(prefix, "ath0"))
+		copytonv_prefix(wp, "regdomain", prefix);
+#else
 	copytonv_prefix(wp, "regdomain", prefix);
+#endif
 
 	copytonv_prefix(wp, "rts", prefix);
 	if (nvram_nmatch("1", "%s_rts", prefix)) {
