54829d196bd8b22e42062cc286d8e911debfa886
BrainSlayer <s.gottschall@dd-wrt.com>
2020-02-21 09:34:29 +0000

    just in case something went strange, we exit here after 30 seconds max
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42470 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/madwifi.c
+++ src/router/services/networking/madwifi.c
@@ -2908,7 +2908,8 @@ void configure_wifi(void)	// madwifi implementation for atheros based
 	}
 
 #if 1
-	while (1) {
+	int dead=10*30; // after 30 seconds, we can assume that something is hanging
+	while (dead--) {
 		int cnf = 0;
 		for (i = 0; i < c; i++) {
 			char path[42];
