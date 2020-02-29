c7d37b0c35340698eb84238a2ce621cbcb87fea7
BrainSlayer <s.gottschall@dd-wrt.com>
2020-02-29 09:51:47 +0000

    missing commit
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42574 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/gpsd.mk
+++ src/router/rules/gpsd.mk
@@ -1,5 +1,5 @@
 gpsd-configure: ncurses
-	cd gpsd && ./configure --host=$(ARCH)-linux --disable-fv18 --disable-sirf --disable-python --disable-tsip --disable-garmin --disable-ntrip --disable-evermore --disable-rtcm104 --disable-tripmate --disable-earthmate --disable-shared --without-x CFLAGS="$(COPTS) -DNEED_PRINTF -I../ncurses/include -L../ncurses/lib " --prefix=/usr 
+	cd gpsd && ./configure --host=$(ARCH)-linux --disable-fv18 --disable-sirf --disable-python --disable-tsip --disable-garmin --disable-ntrip --disable-evermore --disable-rtcm104 --disable-tripmate --disable-earthmate --disable-shared --without-x CFLAGS="$(COPTS) -DNEED_PRINTF -I../ncurses/include -L../ncurses/lib " --prefix=/usr --enable-ntpshm --enable-pps
 
 gpsd:
 	make  -C gpsd
