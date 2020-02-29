74658c27e89789ea6db68d4060a6294123b9d3fc
BrainSlayer <s.gottschall@dd-wrt.com>
2020-02-29 09:51:39 +0000

    remove useless shared lib
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42573 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/sispmctl.mk
+++ src/router/rules/sispmctl.mk
@@ -1,7 +1,7 @@
 sispmctl-configure: comgt
 	cd sispmctl && aclocal
 	cd sispmctl && automake
-	cd sispmctl && ./configure --prefix=/usr --host=$(ARCH)-linux CC="$(CC)" \
+	cd sispmctl && ./configure --prefix=/usr --disable-shared --enable-static --host=$(ARCH)-linux CC="$(CC)" \
 		CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/usb_modeswitch/libusb-compat/libusb -L$(TOP)/usb_modeswitch/libusb-compat/libusb/.libs -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF  -Drpl_malloc=malloc" \
 		LIBUSB_CFLAGS="-I$(TOP)/usb_modeswitch/libusb-compat/libusb" \
 		LIBUSB_LIBS="-L$(TOP)/usb_modeswitch/libusb-compat/libusb/.libs -lusb"
