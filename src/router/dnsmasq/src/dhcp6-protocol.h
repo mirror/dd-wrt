2ce5a2f3e123cb774fd339d82c9babdbef189b9b
BrainSlayer <s.gottschall@dd-wrt.com>
2020-03-06 08:45:22 +0000

    update dnsmasq
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42630 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/dnsmasq/src/dhcp6-protocol.h
+++ src/router/dnsmasq/src/dhcp6-protocol.h
@@ -1,4 +1,4 @@
-/* dnsmasq is Copyright (c) 2000-2018 Simon Kelley
+/* dnsmasq is Copyright (c) 2000-2020 Simon Kelley
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -60,10 +60,6 @@
 #define OPTION6_SUBSCRIBER_ID   38
 #define OPTION6_FQDN            39
 #define OPTION6_CLIENT_MAC      79
-
-/* replace this with the real number when allocated.
-   defining this also enables the relevant code. */ 
-/* #define OPTION6_PREFIX_CLASS    99 */
 
 
 #define DHCP6SUCCESS     0
