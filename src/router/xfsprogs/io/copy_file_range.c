730dcdeb25ad623c8657d764c5303776934d3bbf
BrainSlayer <s.gottschall@dd-wrt.com>
2020-03-08 20:09:35 +0000

    collides with musl implementation
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42646 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/xfsprogs/io/copy_file_range.c
+++ src/router/xfsprogs/io/copy_file_range.c
@@ -43,7 +43,7 @@ copy_range_help(void)
 }
 
 static loff_t
-copy_file_range(int fd, loff_t *src, loff_t *dst, size_t len)
+copy_file_range2(int fd, loff_t *src, loff_t *dst, size_t len)
 {
 	loff_t ret;
 
@@ -130,7 +130,7 @@ copy_range_f(int argc, char **argv)
 		copy_dst_truncate();
 	}
 
-	ret = copy_file_range(fd, &src, &dst, len);
+	ret = copy_file_range2(fd, &src, &dst, len);
 	close(fd);
 	return ret;
 }
