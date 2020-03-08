9616be5a3c6f75b8ba82f07c9a5c2dc1e91b4289
BrainSlayer <s.gottschall@dd-wrt.com>
2020-03-08 20:48:08 +0000

    fix compiler warning
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42647 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/java/classpath/native/jni/native-lib/cpio.c
+++ src/router/java/classpath/native/jni/native-lib/cpio.c
@@ -578,7 +578,7 @@ int cpio_readDir (void *handle, char *filename)
       return errno;
     }
 
-  strncpy (filename, dBuf->d_name, FILENAME_MAX - 1);
+  strlcpy (filename, dBuf->d_name, FILENAME_MAX);
   return 0;
 }
 
