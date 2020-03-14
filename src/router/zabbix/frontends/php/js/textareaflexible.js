effc0b9228d66ec94cbc440fd2e6cf3ad7b8651a
BrainSlayer <s.gottschall@dd-wrt.com>
2020-03-14 23:02:51 +0000

    update zabbix: add new files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42691 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/zabbix/frontends/php/js/textareaflexible.js
@@ -0,0 +1,80 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2020 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+
+(function($) {
+	'use strict';
+
+	function update(e) {
+		var $textarea = $(this);
+
+		if (e.which === 13) {
+			// Simulate input behavior by submitting form on enter key.
+
+			var $form = $(this).closest('form'),
+				$submit = $form.find('button:submit:first');
+
+			if ($submit.length) {
+				$submit.click();
+			}
+			else {
+				$form.submit();
+			}
+
+			return false;
+		}
+
+		var old_value = $textarea.val(),
+			new_value = old_value.replace(/\r?\n/gi, ''),
+			scroll_pos = $(window).scrollTop();
+
+		if (old_value.length !== new_value.length) {
+			$textarea.val(new_value);
+		}
+
+		$textarea.height(0).innerHeight($textarea[0].scrollHeight);
+		$(window).scrollTop(scroll_pos);
+	}
+
+	var methods = {
+		init: function(options) {
+			var settings = $.extend({}, options);
+
+			return this.each(function() {
+				var $textarea = $(this);
+
+				$textarea
+					.off('input keydown paste', update)
+					.on('input keydown paste', update)
+					.trigger('input');
+			});
+		}
+	};
+
+	/**
+	 * Flexible textarea helper.
+	 */
+	$.fn.textareaFlexible = function(method) {
+		if (methods[method]) {
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		}
+
+		return methods.init.apply(this, arguments);
+	};
+})(jQuery);
---
+++ src/router/zabbix/src/go/plugins/net/netif/netif.go
@@ -0,0 +1,50 @@
+/*
+** Zabbix
+** Copyright (C) 2001-2020 Zabbix SIA
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
+
+package netif
+
+import (
+	"zabbix.com/pkg/plugin"
+)
+
+const (
+	errorInvalidSecondParam   = "Invalid second parameter."
+	errorEmptyIfName          = "Network interface name cannot be empty."
+	errorTooManyParams        = "Too many parameters."
+	errorUnsupportedMetric    = "Unsupported metric."
+	errorParametersNotAllowed = "Item does not allow parameters."
+)
+
+// Plugin -
+type Plugin struct {
+	plugin.Base
+}
+
+var impl Plugin
+
+type dirFlag uint8
+
+const (
+	dirIn dirFlag = 1 << iota
+	dirOut
+)
+
+type msgIfDiscovery struct {
+	Ifname string `json:"{#IFNAME}"`
+}
