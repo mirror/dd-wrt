a5ab86b52c63431934178a429d98eb09c27232a2
BrainSlayer <s.gottschall@dd-wrt.com>
2020-03-22 14:41:28 +0000

    fix wrong check
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42766 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/smartdns/src/include/atomic.h
+++ src/router/smartdns/src/include/atomic.h
@@ -22,7 +22,7 @@
 
 
 /* Check GCC version, just to be safe */
-#if !defined(__GNUC__) || (__GNUC__ < 4) || (__GNUC_MINOR__ < 1)
+#if !defined(__GNUC__) || ((__GNUC__ < 4) || (__GNUC_MINOR__ < 1)) && (__GNUC__ < 5)
 # error atomic.h works only with GCC newer than version 4.1
 #endif /* GNUC >= 4.1 */
 
