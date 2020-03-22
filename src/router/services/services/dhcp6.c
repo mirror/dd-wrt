701b71cc0f10aab67c229af05d30bbeb1e2091a6
BrainSlayer <s.gottschall@dd-wrt.com>
2020-03-22 21:05:18 +0000

    must use link local
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42776 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/dhcp6.c
+++ src/router/services/services/dhcp6.c
@@ -260,7 +260,7 @@ void start_dhcp6s(void)
 
 		fprintf(fp, "option refreshtime %d;\n", 900);	/* 15 minutes for now */
 		if (nvram_invmatchi("dnsmasq_enable", 1)) {
-			fprintf(fp, "option domain-name-servers %s", getifaddr(nvram_safe_get("lan_ifname"), AF_INET6, 0));
+			fprintf(fp, "option domain-name-servers %s", getifaddr(nvram_safe_get("lan_ifname"), AF_INET6, GIF_LINKLOCAL));
 		} else {
 			fprintf(fp, "option domain-name-servers %s", nvram_safe_get("ipv6_get_dns"));
 			/* dhcp6s won't start if there are duplicate dns ips */
