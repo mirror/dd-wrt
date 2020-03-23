72e912a5a8eb0b9734db72d421fa7580d7544f30
BrainSlayer <s.gottschall@dd-wrt.com>
2020-03-23 11:25:38 +0000

    reduce size
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42790 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/smartdns.mk
+++ src/router/rules/smartdns.mk
@@ -5,7 +5,7 @@ smartdns-clean:
 	$(MAKE) -C smartdns/src clean 
 
 smartdns: openssl
-	$(MAKE) -C smartdns/src CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections -I$(TOP)/openssl/include" LDFLAGS="$(COPTS) $(MIPS16_OPT) $(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections  -L$(TOP)/openssl"
+	$(MAKE) -C smartdns/src CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections -I$(TOP)/openssl/include" LDFLAGS="$(COPTS) $(MIPS16_OPT) $(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections  -L$(TOP)/openssl"
 
 smartdns-install:
 	install -D smartdns/src/smartdns $(INSTALLDIR)/smartdns/usr/sbin/smartdns
