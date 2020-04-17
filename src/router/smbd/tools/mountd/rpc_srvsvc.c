0b9a1eba05709c471a5ccb60fb9d9564631ce662
BrainSlayer <s.gottschall@dd-wrt.com>
2020-04-17 13:13:17 +0000

    update smbd
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42917 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/smbd/tools/mountd/rpc_srvsvc.c
+++ src/router/smbd/tools/mountd/rpc_srvsvc.c
@@ -332,7 +332,7 @@ static int srvsvc_clear_headers(struct ksmbd_rpc_pipe *pipe, int status)
 static int srvsvc_share_info_return(struct ksmbd_rpc_pipe *pipe)
 {
 	struct ksmbd_dcerpc *dce = pipe->dce;
-	int ret = KSMBD_RPC_OK, status;
+	int status = KSMBD_RPC_ENOTIMPLEMENTED;
 
 	/*
 	 * Reserve space for response NDR header. We don't know yet if
@@ -372,14 +372,11 @@ static int srvsvc_share_info_return(struct ksmbd_rpc_pipe *pipe)
 	/*
 	 * [out] DWORD Return value/code
 	 */
-	if (ret != KSMBD_RPC_OK)
-		status = ret;
-
 	ndr_write_int32(dce, status);
 	dcerpc_write_headers(dce, status);
 
 	dce->rpc_resp->payload_sz = dce->offset;
-	return ret;
+	return KSMBD_RPC_OK;
 }
 
 static int srvsvc_invoke(struct ksmbd_rpc_pipe *pipe)
--- src/router/smbd/tools/mountd/rpc_wkssvc.c
+++ src/router/smbd/tools/mountd/rpc_wkssvc.c
@@ -91,7 +91,7 @@ static int wkssvc_netwksta_get_info_return(struct ksmbd_rpc_pipe *pipe)
 static int wkssvc_netwksta_info_return(struct ksmbd_rpc_pipe *pipe)
 {
 	struct ksmbd_dcerpc *dce = pipe->dce;
-	int ret = KSMBD_RPC_OK, status = KSMBD_RPC_ENOTIMPLEMENTED;
+	int status = KSMBD_RPC_ENOTIMPLEMENTED;
 
 	/*
 	 * Reserve space for response NDR header. We don't know yet if
@@ -123,14 +123,11 @@ static int wkssvc_netwksta_info_return(struct ksmbd_rpc_pipe *pipe)
 	/*
 	 * [out] DWORD Return value/code
 	 */
-	if (ret != KSMBD_RPC_OK)
-		status = ret;
-
 	ndr_write_int32(dce, status);
 	dcerpc_write_headers(dce, status);
 
 	dce->rpc_resp->payload_sz = dce->offset;
-	return ret;
+	return KSMBD_RPC_OK;
 }
 
 static int
