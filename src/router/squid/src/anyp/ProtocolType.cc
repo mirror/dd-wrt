1e09215c98552248eb432db05f05cfb49a688f0c
BrainSlayer <s.gottschall@dd-wrt.com>
2020-04-20 18:02:22 +0000

    missing files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42963 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/squid/src/anyp/ProtocolType.cc
@@ -0,0 +1,32 @@
+/*
+ * Auto-Generated File. Changes will be destroyed.
+ */
+#include "squid.h"
+#include "anyp/ProtocolType.h"
+namespace AnyP
+{
+
+const char * ProtocolType_str[] = {
+	"NONE",
+	"HTTP",
+	"FTP",
+	"HTTPS",
+	"COAP",
+	"COAPS",
+	"GOPHER",
+	"WAIS",
+	"CACHE_OBJECT",
+	"ICP",
+#if USE_HTCP
+	"HTCP",
+#endif
+	"URN",
+	"WHOIS",
+	"ICY",
+	"TLS",
+	"SSL",
+	"AUTHORITY_FORM",
+	"UNKNOWN",
+	"MAX"
+};
+}; // namespace AnyP
---
+++ src/router/squid/src/auth/CredentialState.cc
@@ -0,0 +1,16 @@
+/*
+ * Auto-Generated File. Changes will be destroyed.
+ */
+#include "squid.h"
+#include "auth/CredentialState.h"
+namespace Auth
+{
+
+const char * CredentialState_str[] = {
+	"Unchecked",
+	"Ok",
+	"Pending",
+	"Handshake",
+	"Failed"
+};
+}; // namespace Auth
