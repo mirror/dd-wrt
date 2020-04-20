c544abde46ea4f8f1a19151a0418eba3cc2ea373
BrainSlayer <s.gottschall@dd-wrt.com>
2020-04-20 18:01:34 +0000

    missing files: add new files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@42962 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/squid/test-suite/stub_fatal.cc
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 1996-2020 The Squid Software Foundation and contributors
+ *
+ * Squid software is distributed under GPLv2+ license and includes
+ * contributions from numerous individuals and organizations.
+ * Please see the COPYING and CONTRIBUTORS files for details.
+ */
+
+#include "squid.h"
+
+#define STUB_API "fatal.cc"
+#include "tests/STUB.h"
+
+void fatal(const char *) STUB
+void fatal_common(const char *) STUB
+void fatalf(const char *, ...) STUB
+void fatalvf(const char *, va_list) STUB
+void fatal_dump(const char *) STUB
+
