3b6758d1d855c1c610a584007ce57254a2b9b9c3
BrainSlayer <s.gottschall@dd-wrt.com>
2020-05-27 09:50:23 +0000

    egc's patch for wireguard enhancements
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@43271 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/wireguard/wireguard-state.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#parameter passing with oet number and peerkey
+i=$1
+p=$2
+
+#VAR=$(/usr/bin/wg | sed "/oet${i}/,/interface/!d;/interface/d" | sed "/${p//\//\\\/}/,/peer/!d;/peer/d;/preshared/d;/endpoint/d;/allowed/d;/persistent/d")
+VAR=$(/usr/bin/wg | sed "/${p//\//\\\/}/,/peer/!d" | grep -e 'latest' -e 'transfer' )
+if [[ ! -z "$VAR" ]]; then
+	/bin/echo "${VAR//$'\n'/\\n}" | tr '\n' ' '
+fi
+
