d56dd83f088493d7c1938d460185d90f8c4a6f5c
BrainSlayer <s.gottschall@dd-wrt.com>
2020-06-04 10:08:40 +0000

    for next try
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@43336 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/smbd.mk
+++ src/router/rules/smbd.mk
@@ -17,8 +17,8 @@ libnl-install:
 	@true
 
 smbd-configure: libnl
-	cd smbd/tools && ./autogen.sh
-	cd smbd/tools && ./configure --prefix=/usr --host=$(ARCH)-linux --disable-shared --enable-static  --libdir=/usr/lib \
+	cd smbd-next/tools && ./autogen.sh
+	cd smbd-next/tools && ./configure --prefix=/usr --host=$(ARCH)-linux --disable-shared --enable-static  --libdir=/usr/lib \
 	    CFLAGS="$(COPTS) $(LTO) $(MIPS16_OPT) -D_GNU_SOURCE -DNEED_PRINTF -Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	    LDFLAGS="$(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	    LIBNL_CFLAGS="-I$(TOP)/libnl/include" \
@@ -27,21 +27,21 @@ smbd-configure: libnl
 	    RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
 
 smbd: libnl
-	$(MAKE) -C smbd/smbd all
-	$(MAKE) -C smbd/tools all
+	$(MAKE) -C smbd-next/smbd all
+	$(MAKE) -C smbd-next/tools all
 
 smbd-install:
-	$(MAKE) -C smbd/smbd install
-	$(MAKE) -C smbd/tools install DESTDIR=$(INSTALLDIR)/smbd
-	rm -rf $(INSTALLDIR)/smbd/usr/lib
-	cd $(INSTALLDIR)/smbd/usr/sbin && ln -sf smbd_multicall ksmbd.mountd
-	cd $(INSTALLDIR)/smbd/usr/sbin && ln -sf smbd_multicall ksmbd.adduser
-	cd $(INSTALLDIR)/smbd/usr/sbin && ln -sf smbd_multicall ksmbd.addshare
+	$(MAKE) -C smbd-next/smbd install
+	$(MAKE) -C smbd-next/tools install DESTDIR=$(INSTALLDIR)/smbd
+	rm -rf $(INSTALLDIR)/smbd-next/usr/lib
+	cd $(INSTALLDIR)/smbd-next/usr/sbin && ln -sf smbd_multicall ksmbd.mountd
+	cd $(INSTALLDIR)/smbd-next/usr/sbin && ln -sf smbd_multicall ksmbd.adduser
+	cd $(INSTALLDIR)/smbd-next/usr/sbin && ln -sf smbd_multicall ksmbd.addshare
 
-	install -D smbd/config/samba_ksmbd.webnas $(INSTALLDIR)/smbd/etc/config/02samba_ksmbd.webnas
-	install -D smbd/config/samba_ksmbd.nvramconfig $(INSTALLDIR)/smbd/etc/config/samba_ksmbd.nvramconfig
-	install -D filesharing/config/zfilesharing.webnas $(INSTALLDIR)/smbd/etc/config/03zfilesharing.webnas
+	install -D smbd-next/config/samba_ksmbd.webnas $(INSTALLDIR)/smbd-next/etc/config/02samba_ksmbd.webnas
+	install -D smbd-next/config/samba_ksmbd.nvramconfig $(INSTALLDIR)/smbd-next/etc/config/samba_ksmbd.nvramconfig
+	install -D filesharing/config/zfilesharing.webnas $(INSTALLDIR)/smbd-next/etc/config/03zfilesharing.webnas
 
 smbd-clean:
-	$(MAKE) -C smbd/smbd clean
-	$(MAKE) -C smbd/tools clean
+	$(MAKE) -C smbd-next/smbd clean
+	$(MAKE) -C smbd-next/tools clean
