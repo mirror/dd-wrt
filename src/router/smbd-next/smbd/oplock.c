abe42107cf847835bd0305f02c41f88a20dc2d64
BrainSlayer <s.gottschall@dd-wrt.com>
2020-06-05 06:09:29 +0000

    testing code
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@43338 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/smbd-next/smbd/oplock.c
+++ src/router/smbd-next/smbd/oplock.c
@@ -670,13 +670,23 @@ static void wait_for_break_ack(struct oplock_info *opinfo)
 static int oplock_break_pending(struct oplock_info *opinfo)
 {
 	int prev_op_level = opinfo->level;
+	int ret = 0;
 
 	while  (test_and_set_bit(0, &opinfo->pending_break)) {
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,17,0)
 		wait_on_bit(&opinfo->pending_break, 0, bit_wait, TASK_UNINTERRUPTIBLE);
-#else
+#elif LINUX_VERSION_CODE < KERNEL_VERSION(4,4,0)
 		wait_on_bit(&opinfo->pending_break, 0, TASK_UNINTERRUPTIBLE);
+#else
+		ret = wait_on_bit_timeout(&opinfo->pending_break, 0,
+				TASK_UNINTERRUPTIBLE, 60*HZ);
 #endif
+
+		/* Oplock break timeout */
+		if (ret && ret != -EINTR) {
+			ksmbd_err("oplock break pending timeout !!!!!, opinfo level : 0x%x, prev_op_level 0x%x\n", opinfo->level,prev_op_level);
+			return 1;
+		}
 
 		/* Not immediately break to none. */
 		opinfo->open_trunc = 0;
