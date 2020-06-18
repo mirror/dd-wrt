c522db13283585368a218ca68c0f7f66f665c4b2
BrainSlayer <s.gottschall@dd-wrt.com>
2020-06-18 09:29:30 +0000

    no printk
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@43460 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/mac80211-rules/ath9k-kconfig.mk
+++ src/router/mac80211-rules/ath9k-kconfig.mk
@@ -213,6 +213,8 @@ ifeq ($(CPTCFG_ATH5K_PCI),y)
 endif
 ifeq ($(CPTCFG_ATH5K_AHB),y)
 	echo "CPTCFG_ATH5K_AHB=y" >>$(MAC80211_PATH)/.config_temp
+	BUILDFLAGS += -DCONFIG_NOPRINTK
+	
 endif
 endif
 ifeq ($(CONFIG_ATH10KUSB),y)
