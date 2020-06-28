1073acb351b413cc7d63612e86d770d102fb7326
BrainSlayer <s.gottschall@dd-wrt.com>
2020-06-28 11:55:02 +0000

    add bootconfig
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@43550 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/services/services/bootconfig.c
@@ -0,0 +1,83 @@
+/*
+ * bootconfig.c
+ *
+ * Copyright (C) 2013 Sebastian Gottschall <gottschall@dd-wrt.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id:
+ */
+#include <stdlib.h>
+#include <bcmnvram.h>
+#include <shutils.h>
+#include <utils.h>
+#include <syslog.h>
+#include <signal.h>
+#include <services.h>
+
+void start_bootconfig(void)
+{
+	char *disc = getdisc();
+	char args[512] = { 0 };
+	char dev[64];
+
+	insmod("crc16 crc32c_generic crc32_generic mbcache ext2 jbd jbd2 ext3 ext4");
+
+	if (strlen(disc) == 7)	//mmcblk0
+		sprintf(dev, "/dev/%sp1", disc);
+	else
+		sprintf(dev, "/dev/%s1", disc);
+	eval("mount", "-t", "ext2", dev, "/boot");
+	if (nvram_match("boot_disable_msi", "1"))
+		strcat(args, " pci=nomsi");
+	if (nvram_match("boot_noaer", "1"))
+		strcat(args, " pci=noaer");
+	if (nvram_match("boot_noari", "1"))
+		strcat(args, " pci=noari");
+	if (nvram_match("boot_pcie_tune", "pcie_bus_tune_off"))
+		strcat(args, " pci=pcie_bus_tune_off");
+	if (nvram_match("boot_pcie_tune", "pcie_bus_safe"))
+		strcat(args, " pci=pcie_bus_safe");
+	if (nvram_match("boot_pcie_tune", "pcie_bus_perf"))
+		strcat(args, " pci=pcie_bus_perf");
+	if (nvram_match("boot_pcie_tune", "pcie_bus_peer2peer"))
+		strcat(args, " pci=pcie_bus_peer2peer");
+	FILE *in = fopen("/boot/boot/grub/menu.lst", "rb");
+	char serial[64];
+	fscanf(in, "%s", serial);
+	fclose(in);
+	FILE *out = fopen("/boot/boot/grub/menu.lst", "wb");
+	if (!strcmp(serial, "serial")) {
+		fprintf(out, "serial --unit=0 --speed=115200 --word=8 --parity=no --stop=1\n");
+		fprintf(out, "terminal --timeout=10 serial\n");
+		fprintf(out, "\n");
+	}
+	fprintf(out, "default 0\n");
+	fprintf(out, "timeout 3\n");
+	fprintf(out, "\n");
+	fprintf(out, "title   DD-WRT\n");
+	fprintf(out, "root    (hd0,0)\n");
+	fprintf(out, "kernel  /boot/vmlinuz root=/dev/hda2 rootfstype=squashfs noinitrd console=ttyS0,115200n8 reboot=bios rootdelay=5 %s\n", args);
+	fprintf(out, "boot\n");
+	fprintf(out, "title   DD-WRT (default)\n");
+	fprintf(out, "root    (hd0,0)\n");
+	fprintf(out, "kernel  /boot/vmlinuz root=/dev/hda2 rootfstype=squashfs noinitrd console=ttyS0,115200n8 reboot=bios rootdelay=5\n");
+	fprintf(out, "boot\n");
+	fprintf(out, "\n");
+	fclose(out);
+	eval("umount", "/boot");
+
+	return;
+}
