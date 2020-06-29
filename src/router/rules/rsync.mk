b607439598852d63b9a48539bdb36d027979e33f
BrainSlayer <s.gottschall@dd-wrt.com>
2020-06-29 21:59:43 +0000

    add new rsync
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@43596 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/rsync.mk
+++ src/router/rules/rsync.mk
@@ -1,7 +1,9 @@
 rsync-configure:
 	cd rsync && autoconf
-	cd rsync && ./configure --host=$(ARCH)-linux --prefix=/usr --disable-locate --disable-iconv CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_malloc=malloc" 
+	cd rsync && ./configure --host=$(ARCH)-linux --prefix=/usr --disable-locate --disable-simd --disable-iconv --disable-xxhash --disable-lz4 \
+		CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/openssl/include -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_malloc=malloc -L$(TOP)/openssl -L$(TOP)/zstd/lib -I$(TOP)/zstd/lib"
 
+	$(MAKE) -C rsync reconfigure
 rsync:
 	$(MAKE) -C rsync
 
@@ -9,6 +11,7 @@ rsync-clean:
 	$(MAKE) -C rsync clean
 
 rsync-install:
+	rm -rf $(INSTALLDIR)/rsync
 	$(MAKE) -C rsync install DESTDIR=$(INSTALLDIR)/rsync
 	rm -rf $(INSTALLDIR)/rsync/usr/share
 	mkdir -p $(INSTALLDIR)/rsync/usr/sbin
