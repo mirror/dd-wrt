81001b8a4f438c573a64e51bb98d5f21358a1805
BrainSlayer <s.gottschall@dd-wrt.com>
2020-06-30 17:31:17 +0000

    revert, new version is broken
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@43613 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/nfs-utils/utils/mount/mount.c
+++ src/router/nfs-utils/utils/mount/mount.c
@@ -393,6 +393,11 @@ int main(int argc, char *argv[])
 	if(!strncmp(progname, "umount", strlen("umount")))
 		exit(nfsumount(argc, argv));
 
+	if ((argc < 3)) {
+		mount_usage();
+		exit(EX_USAGE);
+	}
+
 	mount_config_init(progname);
 
 	while ((c = getopt_long(argc, argv, "rvVwfno:hs",
@@ -430,11 +435,6 @@ int main(int argc, char *argv[])
 			mount_usage();
 			goto out_usage;
 		}
-	}
-
-	if ((argc < 3)) {
-		mount_usage();
-		exit(EX_USAGE);
 	}
 
 	/*
