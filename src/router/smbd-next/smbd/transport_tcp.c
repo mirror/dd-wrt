38d8fa8d98e804ea313b486f34c42477807a96b1
BrainSlayer <s.gottschall@dd-wrt.com>
2020-06-30 19:02:32 +0000

    upstream test
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@43620 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/smbd-next/smbd/transport_tcp.c
+++ src/router/smbd-next/smbd/transport_tcp.c
@@ -5,6 +5,7 @@
  */
 
 #include <linux/freezer.h>
+#include <linux/etherdevice.h>
 
 #include "smb_common.h"
 #include "server.h"
@@ -435,6 +436,8 @@ static void tcp_destroy_socket(struct socket *ksmbd_socket)
 		sock_release(ksmbd_socket);
 }
 
+static const struct in6_addr my_in6addr_any = IN6ADDR_ANY_INIT;
+
 /**
  * create_socket - create socket for ksmbd/0
  *
@@ -450,7 +453,7 @@ static int create_socket(struct interface *iface)
 
 	ret = sock_create(PF_INET6, SOCK_STREAM, IPPROTO_TCP, &ksmbd_socket);
 	if (ret) {
-		ksmbd_err("Can't create socket for ipv6, try ipv4: %d\n", ret);
+		ksmbd_debug(ALL, "Can't create socket for ipv6, try ipv4: %d\n", ret);
 		ret = sock_create(PF_INET, SOCK_STREAM, IPPROTO_TCP,
 				&ksmbd_socket);
 		if (ret) {
@@ -464,7 +467,7 @@ static int create_socket(struct interface *iface)
 		ipv4 = true;
 	} else {
 		sin6.sin6_family = PF_INET6;
-		sin6.sin6_addr = in6addr_any;
+		sin6.sin6_addr = my_in6addr_any;
 		sin6.sin6_port = htons(server_conf.tcp_port);
 	}
 
@@ -488,7 +491,6 @@ static int create_socket(struct interface *iface)
 		ksmbd_err("Failed to set SO_BINDTODEVICE: %d\n", ret);
 		goto out_error;
 	}
-
 	if (ipv4)
 		ret = kernel_bind(ksmbd_socket, (struct sockaddr *)&sin,
 				sizeof(sin));
@@ -496,7 +498,7 @@ static int create_socket(struct interface *iface)
 		ret = kernel_bind(ksmbd_socket, (struct sockaddr *)&sin6,
 				sizeof(sin6));
 	if (ret) {
-		ksmbd_err("Failed to bind socket: %d\n", ret);
+		ksmbd_err("Failed to bind socket: %d (%s)\n", ret, iface->name);
 		goto out_error;
 	}
 
@@ -528,7 +530,7 @@ int ksmbd_tcp_init(void)
 {
 	struct interface *iface;
 	struct list_head *tmp;
-	int ret;
+	int ret = 0;
 
 	if (list_empty(&iface_list))
 		return 0;
@@ -536,8 +538,6 @@ int ksmbd_tcp_init(void)
 	list_for_each(tmp, &iface_list) {
 		iface = list_entry(tmp, struct interface, entry);
 		ret = create_socket(iface);
-		if (ret)
-			break;
 	}
 
 	return ret;
@@ -617,6 +617,9 @@ int ksmbd_tcp_set_interfaces(char *ifc_list, int ifc_list_sz)
 		rtnl_lock();
 		for_each_netdev(&init_net, netdev) {
 			if (netdev->priv_flags & IFF_BRIDGE_PORT)
+				continue;
+			if (netdev->type != ARPHRD_ETHER || netdev->addr_len != ETH_ALEN ||
+			    !is_valid_ether_addr(netdev->dev_addr))
 				continue;
 			if (alloc_iface(kstrdup(netdev->name, GFP_KERNEL)))
 				return -ENOMEM;
