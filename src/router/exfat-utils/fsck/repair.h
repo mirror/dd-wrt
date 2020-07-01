0b8f70cf700795e7881f59b4338a01694fead323
BrainSlayer <s.gottschall@dd-wrt.com>
2020-07-01 07:41:23 +0000

    next
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@43636 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/exfat-utils/fsck/repair.h
+++ src/router/exfat-utils/fsck/repair.h
@@ -6,17 +6,17 @@
 #define _REPAIR_H
 
 #define ER_BS_CHECKSUM			0x00000001
+#define ER_DE_CHECKSUM			0x00001001
+#define ER_FILE_VALID_SIZE		0x00002001
+#define ER_FILE_INVALID_CLUS		0x00002002
+#define ER_FILE_FIRST_CLUS		0x00002003
+#define ER_FILE_SMALLER_SIZE		0x00002004
+#define ER_FILE_LARGER_SIZE		0x00002005
+#define ER_FILE_DUPLICATED_CLUS		0x00002006
 
 typedef unsigned int er_problem_code_t;
 
-union exfat_repair_context {
-	struct {
-		__le32		checksum;
-		void		*checksum_sect;
-	} bs_checksum;
-};
-
-bool exfat_repair(struct exfat *exfat, er_problem_code_t prcode,
-			union exfat_repair_context *rctx);
+bool exfat_repair_ask(struct exfat *exfat, er_problem_code_t prcode,
+		const char *fmt, ...);
 
 #endif
