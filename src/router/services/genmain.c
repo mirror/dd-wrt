dce8f142092329464912f16d4fd112559740b124
BrainSlayer <s.gottschall@dd-wrt.com>
2020-07-12 15:28:04 +0000

    generate service main executable
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@43765 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/services/genmain.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char **syms;
+void readsymbols(void)
+{
+	char addr[64];
+	char type[64];
+	char sym[256];
+	syms = (char **)malloc(sizeof(char *) * 10000);
+	FILE *fp = fopen("symbols", "rb");
+	int cnt = 0;
+	int valid = 0;
+	while (!feof(fp)) {
+		if (!valid) {
+		    fscanf(fp, "%s", type);
+		    if (!strcmp(type,"T"))
+			valid++;
+		}
+		if (valid) {
+		    fscanf(fp,"%s", sym);
+			syms[cnt++] = strdup(sym);
+			fprintf(stdout, "%s\n", sym);
+			valid=0;
+		}
+	}
+	syms[cnt] = NULL;
+}
+
+int sym(char *name, char *prefix, char *postfix)
+{
+	int i = 0;
+	char check[256];
+	sprintf(check, "%s%s%s%s%s", prefix ? prefix : "", prefix ? "_" : "", name, postfix ? "_" : "", postfix ? postfix : "");
+	while (syms[i]) {
+		if (!strcmp(syms[i++], check))
+			return 1;
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	FILE *out = fopen("main.c", "wb");
+	int i=0;
+	readsymbols();
+	while (syms[i]) {
+		if (!strncmp(syms[i],"start_", 6)) {
+			fprintf(stdout, "process %s\n",syms[i]);
+			char *name = syms[i] + 6;
+			int deps = sym(name, NULL, "deps");
+			int proc = sym(name, NULL, "proc");
+			fprintf(out,"void start_%s(void);\n",name);
+			if (deps)
+			fprintf(out,"char *%s_deps(void);\n",name);
+			if (proc)
+			fprintf(out,"char *%s_proc(void);\n",name);
+			
+		} else if (!strncmp(syms[i],"stop_", 5)) {
+			fprintf(out,"void stop_%s(void);\n",syms[i]+5);
+		} else {
+			char copy[256];
+			strcpy(copy, syms[i]);
+			char *p = strstr(copy, "_main");
+			if (p) {
+				*p = 0;
+				if (sym(copy, NULL, "main"))
+					fprintf(out,"int %s_main(int argc,char *argv[]);\n", copy);
+			}
+		}
+		i++;
+	}
+	fprintf(out, "#include <string.h>\n");
+	fprintf(out, "#include \"genmain.h\"\n");
+	fprintf(out, "int main(int argc,char *argv[]){\n");
+	fprintf(out, "int force;\n");
+	fprintf(out, "if (argc>3 && !strcmp(argv[3],\"-f\")) force = 1;\n");
+	
+	i=0;
+	fprintf(out, "if (!strcmp(argv[2],\"start\")) {\n");
+	while (syms[i]) {
+		if (!strncmp(syms[i],"start_", 6)) {
+			fprintf(stdout, "process %s\n",syms[i]);
+			char *name = syms[i] + 6;
+			int deps = sym(name, NULL, "deps");
+			int proc = sym(name, NULL, "proc");
+			if (deps && proc)
+				fprintf(out, "HANDLE_START_DEPS_PROC(\"%s\",%s);\n",name,name);
+			else if (deps)
+				fprintf(out, "HANDLE_START_DEPS(\"%s\",%s);\n",name,name);
+			else if (proc)
+				fprintf(out, "HANDLE_START_PROC(\"%s\",%s);\n",name,name);
+			else
+				fprintf(out, "HANDLE_START(\"%s\",%s);\n",name,name);
+		}
+		i++;
+	}
+	i=0;
+	fprintf(out, "}\n");
+	fprintf(out, "if (!strcmp(argv[2],\"stop\")) {\n");
+	while (syms[i]) {
+		if (!strncmp(syms[i],"stop_", 5)) {
+			fprintf(out, "HANDLE_STOP(\"%s\",%s);\n",syms[i]+5,syms[i]+5);
+		} 
+		i++;
+	}
+	i=0;
+	fprintf(out, "}\n");
+	fprintf(out, "if (!strcmp(argv[2],\"main\")) {\n");
+	fprintf(out, "char **args = buildargs(argc,argv);\n");
+	while (syms[i]) {
+			char copy[256];
+			strcpy(copy, syms[i]);
+			char *p = strstr(copy, "_main");
+			if (p) {
+				*p = 0;
+				printf("check %s\n",copy);
+				if (sym(copy, NULL, "main"))
+					fprintf(out, "HANDLE_MAIN(\"%s\", %s);\n", copy, copy);
+			}
+		i++;
+	}
+	fprintf(out, "}\n");
+	fprintf(out, "}\n");
+	fclose(out);
+}
---
+++ src/router/services/genmain.h
@@ -0,0 +1,78 @@
+#include <bcmnvram.h>
+#include <shutils.h>
+#include <utils.h>
+
+static void handle_procdeps(char *name, void (*start)(void), char * (*deps_func)(void), char * (*proc_func)(void), int force)
+{
+	char *deps = NULL;
+	int state;
+	if (deps_func) {
+		deps = deps_func();
+		dd_debug(DEBUG_SERVICE, "%s_deps exists, check nvram params %s\n", name, deps);
+		state = nvram_states(deps);
+	}
+
+	if (!state) {
+		if (proc_func) {
+			dd_debug(DEBUG_SERVICE, "%s_proc exists, check process\n", name);
+			char *proc = proc_func();
+			int pid = pidof(proc);
+			dd_debug(DEBUG_SERVICE, "process name is %s, pid is %d\n", proc, pidof(proc));
+			if (pid == -1)
+				state = 1;
+		}
+	}
+	if (force || state) {
+		start();
+		if (deps)
+			nvram_states(deps);
+	}
+
+}
+
+#define HANDLE_START(name, sym) \
+	if (!strcmp(argv[1],name)) \
+		start_##sym();
+
+#define HANDLE_START_DEPS(name, sym) \
+	if (!strcmp(argv[1],name)) { \
+		char * (*deps)(void) = sym##_deps; \
+		void (*start)(void) = start_##sym; \
+		handle_procdeps(name,start, deps,  NULL, force); \
+	}
+
+#define HANDLE_START_PROC(name, sym) \
+	if (!strcmp(argv[1],name)) { \
+		char * (*proc)(void) = sym##_proc; \
+		void (*start)(void) = start_##sym; \
+		handle_procdeps(name,start,  NULL, proc, force); \
+	}
+
+#define HANDLE_START_DEPS_PROC(name, sym) \
+	if (!strcmp(argv[1],name)) { \
+		char * (*deps)(void) = sym##_deps; \
+		char * (*proc)(void) = sym##_proc; \
+		void (*start)(void) = start_##sym; \
+		handle_procdeps(name,start, deps, proc, force); \
+	}
+
+#define HANDLE_STOP(name, sym) \
+	if (!strcmp(argv[1],name)) { \
+		stop_##sym(); \
+	}
+
+#define HANDLE_MAIN(name, sym) \
+	if (!strcmp(argv[1],name)) { \
+		sym##_main(argc-2, args); \
+	}
+
+#include <stdlib.h>
+static char **buildargs(int argc, char *argv[])
+{
+	char **args = (char **)malloc(sizeof(char **) * argc);
+	int i;
+	for (i = 0; i < argc - 3; i++)
+		args[i + 1] = argv[i + 3];
+	args[0] = argv[1];
+	return args;
+}
