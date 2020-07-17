81138ef99f5fcaf6df2bebcd4a7c10f7acaca530
BrainSlayer <s.gottschall@dd-wrt.com>
2020-07-17 15:41:37 +0000

    fix missing symbols
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@43834 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/ndpi-netfilter/src/lib/ndpi_main.c
+++ src/router/ndpi-netfilter/src/lib/ndpi_main.c
@@ -6472,3 +6472,134 @@ int ndpi_flowv6_flow_hash(u_int8_t l4_proto, struct ndpi_in6_addr *src_ip, struc
 
   return(0); /* OK */
 }
+
+
+static int enough(int a, int b) {
+  u_int8_t percentage = 20;
+
+  if(b == 0) return(0);
+  if(a == 0) return(1);
+
+  if(b > (((a+1)*percentage)/100)) return(1);
+
+  return(0);
+}
+
+static char *strtok_r(char *s, const char *sep, char **p)
+{
+	if (!s && !(s = *p)) return NULL;
+	s += strspn(s, sep);
+	if (!*s) return *p = 0;
+	*p = s + strcspn(s, sep);
+	if (**p) *(*p)++ = 0;
+	else *p = 0;
+	return s;
+}
+
+int ndpi_check_dga_name(struct ndpi_detection_module_struct *ndpi_str,
+			struct ndpi_flow_struct *flow,
+			char *name) {
+  int len, rc = 0;
+
+  len = strlen(name);
+
+  if(len >= 5) {
+    int i, j, num_found = 0, num_impossible = 0, num_bigram_checks = 0, num_digits = 0, num_vowels = 0, num_words = 0;
+    char tmp[128], *word, *tok_tmp;
+
+    len = snprintf(tmp, sizeof(tmp)-1, "%s", name);
+    if(len < 0) return(0);
+
+    for(i=0, j=0; (i<len) && (j<(sizeof(tmp)-1)); i++) {
+	tmp[j++] = tolower(name[i]);
+    }
+
+    tmp[j] = '\0';
+    len = j;
+
+    for(word = strtok_r(tmp, ".", &tok_tmp); ; word = strtok_r(NULL, ".", &tok_tmp)) {
+      if(!word) break;
+
+      num_words++;
+
+      if(strlen(word) < 3) continue;
+
+#ifdef DGA_DEBUG
+      printf("-> %s [%s][len: %u]\n", word, name, (unsigned int)strlen(word));
+#endif
+
+      for(i = 0; word[i+1] != '\0'; i++) {
+	if(isdigit(word[i])) {
+	  num_digits++;
+
+	  // if(!isdigit(word[i+1])) num_impossible++;
+
+	  continue;
+	}
+
+	switch(word[i]) {
+	case '_':
+	case '-':
+	case ':':
+	  continue;
+	  break;
+
+	case '.':
+	  continue;
+	  break;
+	}
+
+	switch(word[i]) {
+	case 'a':
+	case 'e':
+	case 'i':
+	case 'o':
+	case 'u':
+	  num_vowels++;
+	  break;
+	}
+
+	if(isdigit(word[i+1])) {
+	  num_digits++;
+	  // num_impossible++;
+	  continue;
+	}
+
+	num_bigram_checks++;
+
+	if(ndpi_match_bigram(ndpi_str, &ndpi_str->bigrams_automa, &word[i])) {
+	  num_found++;
+	} else {
+	  if(ndpi_match_bigram(ndpi_str,
+			       &ndpi_str->impossible_bigrams_automa,
+			       &word[i])) {
+#ifdef DGA_DEBUG
+	    printf("IMPOSSIBLE %s\n", &word[i]);
+#endif
+	    num_impossible++;
+	  }
+	}
+      } /* for */
+    } /* for */
+
+#ifdef DGA_DEBUG
+    printf("[num_found: %u][num_impossible: %u][num_digits: %u][num_bigram_checks: %u][num_vowels: %u/%u]\n",
+	   num_found, num_impossible, num_digits, num_bigram_checks, num_vowels, j-num_vowels);
+#endif
+
+    if(num_bigram_checks
+       && ((num_found == 0) || ((num_digits > 5) && (num_words <= 3)) || enough(num_found, num_impossible)))
+      rc = 1;
+
+//    if(rc && flow)
+//      NDPI_SET_BIT(flow->risk, NDPI_SUSPICIOUS_DGA_DOMAIN);
+
+#ifdef DGA_DEBUG
+    if(rc)
+      printf("DGA %s [num_found: %u][num_impossible: %u]\n",
+	     name, num_found, num_impossible);
+#endif
+  }
+
+  return(rc);
+}
