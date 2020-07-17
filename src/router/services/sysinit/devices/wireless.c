b3cb61a499e4f8d7af6d695cd68a8e31882e7e0e
BrainSlayer <s.gottschall@dd-wrt.com>
2020-07-17 10:59:28 +0000

    new drivers
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@43831 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/devices/wireless.c
+++ src/router/services/sysinit/devices/wireless.c
@@ -396,11 +396,18 @@ static void detect_wireless_devices(int mask)
 		int total = 0;
 		insmod("mt76");
 		insmod("mt76-usb");
+		insmod("mt76-sdio");
 		insmod("mt7615-common");
+		insmod("mt76-usb-sdio-common");
 		wificnt += detectchange("mt7615e");
+		wificnt += detectchange("mt7663u");
+		wificnt += detectchange("mt7663s");
 		if (!wificnt) {
 			rmmod("mt7615_common");
 			rmmod("mt7615-common");
+			rmmod("mt76-usb-sdio-common");
+			rmmod("mt76_usb_sdio_common");
+			rmmod("mt76-sdio");
 		}
 		total += wificnt;
 		wificnt = 0;
