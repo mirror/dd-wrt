76570a76b392b8f74eed25839e95448136abde45
BrainSlayer <s.gottschall@dd-wrt.com>
2020-07-20 11:06:06 +0000

    update dnsmasq
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@43875 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/dnsmasq/src/netlink.c
+++ src/router/dnsmasq/src/netlink.c
@@ -49,7 +49,7 @@ static u32 netlink_pid;
 
 static void nl_async(struct nlmsghdr *h);
 
-void netlink_init(void)
+char *netlink_init(void)
 {
   struct sockaddr_nl addr;
   socklen_t slen = sizeof(addr);
@@ -86,8 +86,6 @@ void netlink_init(void)
     die(_("cannot create netlink socket: %s"), NULL, EC_MISC);
 
    if (daemon->netlinkfd == -1 || 
-      (daemon->kernel_version >= KERNEL_VERSION(2,6,30) &&
-       setsockopt(daemon->netlinkfd, SOL_NETLINK, NETLINK_NO_ENOBUFS, &opt, sizeof(opt)) == -1) ||
        getsockname(daemon->netlinkfd, (struct sockaddr *)&addr, &slen) == -1)
      die(_("cannot create netlink socket: %s"), NULL, EC_MISC);
    
@@ -96,6 +94,12 @@ void netlink_init(void)
   
   iov.iov_len = 100;
   iov.iov_base = safe_malloc(iov.iov_len);
+
+  if (daemon->kernel_version >= KERNEL_VERSION(2,6,30) &&
+      setsockopt(daemon->netlinkfd, SOL_NETLINK, NETLINK_NO_ENOBUFS, &opt, sizeof(opt)) == -1)
+    return _("warning: failed to set NETLINK_NO_ENOBUFS on netlink socket");
+  
+  return NULL;
 }
 
 static ssize_t netlink_recv(void)
