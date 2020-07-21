4ae751b85d05f8a8cf780342a2190e3db630ddb8
BrainSlayer <s.gottschall@dd-wrt.com>
2020-07-21 21:39:22 +0000

    update wireguard
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@43894 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/wireguard/src/receive.c
+++ src/router/wireguard/src/receive.c
@@ -392,7 +392,7 @@ static void wg_packet_consume_data_done(struct wg_peer *peer,
 #ifndef COMPAT_CANNOT_USE_CSUM_LEVEL
 	skb->csum_level = ~0; /* All levels */
 #endif
-	skb->protocol = wg_examine_packet_protocol(skb);
+	skb->protocol = ip_tunnel_parse_protocol(skb);
 	if (skb->protocol == htons(ETH_P_IP)) {
 		len = ntohs(ip_hdr(skb)->tot_len);
 		if (unlikely(len < sizeof(struct iphdr)))
