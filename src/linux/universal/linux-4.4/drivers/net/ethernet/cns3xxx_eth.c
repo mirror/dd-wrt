ae95eb813106e2292d5989197ad2be118736dc95
BrainSlayer <s.gottschall@dd-wrt.com>
2020-07-24 21:42:20 +0000

    enhance api for thread naming
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@43946 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4/drivers/net/ethernet/cns3xxx_eth.c
+++ src/linux/universal/linux-4.4/drivers/net/ethernet/cns3xxx_eth.c
@@ -1225,7 +1225,7 @@ static int eth_init_one(struct platform_device *pdev)
 	platform_set_drvdata(pdev, napi_dev);
 
 	netif_napi_add(napi_dev, &sw->napi, eth_poll, NAPI_WEIGHT);
-
+	napi_set_threaded_named(&sw->napi, true, "switch-napi");
 	for (i = 0; i < 3; i++) {
 		if (!(plat->ports & (1 << i))) {
 			continue;
