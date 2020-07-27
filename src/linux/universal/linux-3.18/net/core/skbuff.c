33e61cd6d25004ec0f3f9cd521fae7608907713e
BrainSlayer <s.gottschall@dd-wrt.com>
2020-07-27 07:10:43 +0000

    fix typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@43970 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/net/core/skbuff.c
+++ src/linux/universal/linux-3.18/net/core/skbuff.c
@@ -3239,7 +3239,7 @@ err:
 }
 EXPORT_SYMBOL_GPL(skb_segment);
 
-int skb_gro_receive(struct sk_buff *head, struct sk_buff *skb)
+int skb_gro_receive(struct sk_buff *p, struct sk_buff *skb)
 {
 	struct skb_shared_info *pinfo, *skbinfo = skb_shinfo(skb);
 	unsigned int offset = skb_gro_offset(skb);
