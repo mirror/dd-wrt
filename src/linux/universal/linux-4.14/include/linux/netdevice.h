aa0283e91adbf0212b537f1dd06cf13ff8843e67
BrainSlayer <s.gottschall@dd-wrt.com>
2020-07-27 09:48:08 +0000

    bacport
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@43981 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.14/include/linux/netdevice.h
+++ src/linux/universal/linux-4.14/include/linux/netdevice.h
@@ -3345,6 +3345,7 @@ int do_xdp_generic(struct bpf_prog *xdp_prog, struct sk_buff *skb);
 int netif_rx(struct sk_buff *skb);
 int netif_rx_ni(struct sk_buff *skb);
 int netif_receive_skb(struct sk_buff *skb);
+void netif_receive_skb_list(struct list_head *head);
 gro_result_t napi_gro_receive(struct napi_struct *napi, struct sk_buff *skb);
 void napi_gro_flush(struct napi_struct *napi, bool flush_old);
 struct sk_buff *napi_get_frags(struct napi_struct *napi);
