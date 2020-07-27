4c3aca82c034d98a8310101dab03ef829c1e1411
BrainSlayer <s.gottschall@dd-wrt.com>
2020-07-27 07:24:46 +0000

    not required
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@43972 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/net/core/dev.c
+++ src/linux/universal/linux-3.18/net/core/dev.c
@@ -4473,7 +4473,6 @@ EXPORT_SYMBOL(__napi_schedule);
 void __napi_complete(struct napi_struct *n)
 {
 	BUG_ON(!test_bit(NAPI_STATE_SCHED, &n->state));
-	BUG_ON(n->gro_list);
 
 	list_del(&n->poll_list);
 	smp_mb__before_atomic();
