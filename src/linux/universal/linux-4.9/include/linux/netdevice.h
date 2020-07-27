087ce5b1f910990e73869db9dff057f828cc8214
BrainSlayer <s.gottschall@dd-wrt.com>
2020-07-27 01:00:53 +0000

    backport
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@43961 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.9/include/linux/netdevice.h
+++ src/linux/universal/linux-4.9/include/linux/netdevice.h
@@ -3383,6 +3383,7 @@ static inline void dev_consume_skb_any(struct sk_buff *skb)
 int netif_rx(struct sk_buff *skb);
 int netif_rx_ni(struct sk_buff *skb);
 int netif_receive_skb(struct sk_buff *skb);
+void netif_receive_skb_list(struct list_head *head);
 gro_result_t napi_gro_receive(struct napi_struct *napi, struct sk_buff *skb);
 void napi_gro_flush(struct napi_struct *napi, bool flush_old);
 struct sk_buff *napi_get_frags(struct napi_struct *napi);
