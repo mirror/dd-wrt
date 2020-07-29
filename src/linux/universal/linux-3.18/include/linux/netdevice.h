25ea6591169d99b9e80c9aaddc2f30ce90bb07d2
BrainSlayer <s.gottschall@dd-wrt.com>
2020-07-29 20:49:49 +0000

    limit each hash list length to MAX_GRO_SKBS
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@44027 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/include/linux/netdevice.h
+++ src/linux/universal/linux-3.18/include/linux/netdevice.h
@@ -291,6 +291,11 @@ struct netdev_boot_setup {
 
 int __init netdev_boot_setup(char *str);
 
+struct gro_list {
+	struct list_head	list;
+	int			count;
+};
+
 /*
  * Structure for NAPI scheduling similar to tasklet but with weighting
  */
@@ -313,7 +318,7 @@ struct napi_struct {
 	int			poll_owner;
 #endif
 	struct net_device	*dev;
-	struct list_head	gro_hash[GRO_HASH_BUCKETS];
+	struct gro_list		gro_hash[GRO_HASH_BUCKETS];
 	struct sk_buff		*skb;
 	struct list_head	dev_list;
 	struct hlist_node	napi_hash_node;
