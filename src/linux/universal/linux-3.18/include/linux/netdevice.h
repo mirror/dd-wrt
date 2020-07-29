a53fb0542a08fc4f7c887f9af8fa1b248883c50b
BrainSlayer <s.gottschall@dd-wrt.com>
2020-07-29 20:29:35 +0000

    convert to hash
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@44023 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/include/linux/netdevice.h
+++ src/linux/universal/linux-3.18/include/linux/netdevice.h
@@ -294,6 +294,7 @@ int __init netdev_boot_setup(char *str);
 /*
  * Structure for NAPI scheduling similar to tasklet but with weighting
  */
+#define GRO_HASH_BUCKETS	8
 struct napi_struct {
 	/* The poll_list must only be managed by the entity which
 	 * changes the state of the NAPI_STATE_SCHED bit.  This means
@@ -312,7 +313,7 @@ struct napi_struct {
 	int			poll_owner;
 #endif
 	struct net_device	*dev;
-	struct list_head	gro_list;
+	struct list_head	gro_hash[GRO_HASH_BUCKETS];
 	struct sk_buff		*skb;
 	struct list_head	dev_list;
 	struct hlist_node	napi_hash_node;
