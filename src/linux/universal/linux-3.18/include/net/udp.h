7f7b7a3a4c58cdf5adfbc444bc8f449ecef3e72b
BrainSlayer <s.gottschall@dd-wrt.com>
2020-07-30 15:50:09 +0000

    revert
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@44040 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/include/net/udp.h
+++ src/linux/universal/linux-3.18/include/net/udp.h
@@ -158,7 +158,7 @@ static inline __sum16 udp_v4_check(int len, __be32 saddr,
 void udp_set_csum(bool nocheck, struct sk_buff *skb,
 		  __be32 saddr, __be32 daddr, int len);
 
-struct sk_buff *udp_gro_receive(struct list_head *head, struct sk_buff *skb,
+struct sk_buff **udp_gro_receive(struct sk_buff **head, struct sk_buff *skb,
 				 struct udphdr *uh);
 int udp_gro_complete(struct sk_buff *skb, int nhoff);
 
--- src/linux/universal/linux-3.18/kernel/kthread.c
+++ src/linux/universal/linux-3.18/kernel/kthread.c
@@ -97,7 +97,6 @@ bool kthread_should_park(void)
 {
 	return test_bit(KTHREAD_SHOULD_PARK, &to_kthread(current)->flags);
 }
-EXPORT_SYMBOL_GPL(kthread_should_park);
 
 /**
  * kthread_freezable_should_stop - should this freezable kthread return now?
@@ -173,7 +172,6 @@ void kthread_parkme(void)
 {
 	__kthread_parkme(to_kthread(current));
 }
-EXPORT_SYMBOL_GPL(kthread_parkme);
 
 static int kthread(void *_create)
 {
@@ -420,7 +418,6 @@ void kthread_unpark(struct task_struct *k)
 	if (kthread)
 		__kthread_unpark(k, kthread);
 }
-EXPORT_SYMBOL_GPL(kthread_unpark);
 
 /**
  * kthread_park - park a thread created by kthread_create().
@@ -451,7 +448,6 @@ int kthread_park(struct task_struct *k)
 	}
 	return ret;
 }
-EXPORT_SYMBOL_GPL(kthread_park);
 
 /**
  * kthread_stop - stop a thread created by kthread_create().
