08968cdd51be84e3406e2732958beda8392eb3fc
BrainSlayer <s.gottschall@dd-wrt.com>
2020-07-31 09:12:52 +0000

    fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@44045 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.9/drivers/net/ethernet/intel/ixgbe/ixgbe.h
+++ src/linux/universal/linux-4.9/drivers/net/ethernet/intel/ixgbe/ixgbe.h
@@ -439,7 +439,7 @@ static inline void ixgbe_qv_unlock_napi(struct ixgbe_q_vector *q_vector)
 	WARN_ON(atomic_read(&q_vector->state) != IXGBE_QV_STATE_NAPI);
 
 	/* flush any outstanding Rx frames */
-	if (q_vector->napi.gro_count)
+	if (q_vector->napi.gro_bitmask)
 		napi_gro_flush(&q_vector->napi, false);
 
 	/* reset state to idle */
