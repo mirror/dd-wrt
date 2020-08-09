d74c412b4fa9ae136c4281f83d818978779e3438
BrainSlayer <s.gottschall@dd-wrt.com>
2020-08-09 16:43:17 +0000

    add lto
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@44139 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/mrp.mk
+++ src/router/rules/mrp.mk
@@ -6,13 +6,18 @@ MRP_PKG_BUILD_DIR=$(TOP)/mrp
 MRP_CMAKE_OPTIONS=-DCMAKE_BUILD_TYPE=release -DLibNL_INCLUDE_DIR="$(TOP)/libnl-tiny/include" -DLibNL_LIBRARY="$(TOP)/libnl-tiny/libnl-tiny.so" -DLibEV_LIBRARY="$(TOP)/mrp/ev/.libs/libev.a"
 
 MRP_STAGING_DIR=$(TOP)/_staging/usr
-MRP_EXTRA_CFLAGS=$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -D_GNU_SOURCE 
-#MRP_EXTRA_LDFLAGS=-lev
+MRP_EXTRA_CFLAGS=$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -D_GNU_SOURCE $(LTO)
+MRP_EXTRA_LDFLAGS=$(LDLTO)
 
 
 mrp-configure:
+	-make -C mrp/ev clean
 	cd mrp/ev && . ./autogen.sh
-	cd mrp/ev && ./configure --prefix=/usr --disable-shared --enable-static --host=$(ARCH)-linux
+	cd mrp/ev && ./configure --prefix=/usr --disable-shared --enable-static --host=$(ARCH)-linux \
+		CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO)" \
+		AR_FLAGS="cru $(LTOPLUGIN)" \
+		RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
+
 	make -C mrp/ev
 	rm -f $(TOP)/mrp/CMakeCache.txt
 	$(call CMakeClean,$(MRP_PKG_BUILD_DIR))
