e37978dca3eb3e6eb0e7ec4f48e5452fa3ca1223
BrainSlayer <s.gottschall@dd-wrt.com>
2020-09-08 08:29:31 +0000

    add missing file
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@44322 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rules/libnl.mk
@@ -0,0 +1,19 @@
+libnl-configure:
+	cd libnl && autoreconf --install --verbose
+	cd libnl && ./configure --prefix=/usr --host=$(ARCH)-linux --disable-shared --enable-static --disable-debug --enable-cli=no \
+	    CFLAGS="$(COPTS) $(LTO) $(MIPS16_OPT) -D_GNU_SOURCE -Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	    LDFLAGS="$(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	    AR_FLAGS="cru $(LTOPLUGIN)" \
+	    RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
+	$(MAKE) -C libnl
+
+libnl:
+	$(MAKE) -C libnl
+
+libnl-clean:
+	$(MAKE) -C libnl clean
+
+libnl-install:
+	@true
+
+
