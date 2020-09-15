0c2ec2ef2fdb5d0eade8a47860a36ef166cf2acd
BrainSlayer <s.gottschall@dd-wrt.com>
2020-09-15 11:19:58 +0000

    add ipset
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@44363 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/configs.mk
+++ src/router/rules/configs.mk
@@ -362,6 +362,8 @@ obj-$(CONFIG_NGINX) += pcre openssl zlib nginx
 obj-$(CONFIG_X86) += yukon bootconfig
 obj-$(CONFIG_MRP) += mrp
 obj-$(CONFIG_HTOP) += ncurses libnl htop
+obj-$(CONFIG_IPSET) += libmnl ipset
+
 
 obj-y+=configs
 
