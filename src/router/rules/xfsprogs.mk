bf447a4e1cf3cfdf60e1c09b5ec304cfe3e8e199
BrainSlayer <s.gottschall@dd-wrt.com>
2020-09-17 08:24:51 +0000

    avoid static libs
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@44383 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/xfsprogs.mk
+++ src/router/rules/xfsprogs.mk
@@ -1,9 +1,21 @@
 xfsprogs-configure:
-	cd xfsprogs && ./configure --host=$(ARCH)-linux CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections  -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -I$(TOP)/e2fsprogs/lib -DNEED_PRINTF" LDFLAGS="-L$(TOP)/e2fsprogs/lib/uuid -luuid -ffunction-sections -fdata-sections -Wl,--gc-sections" CC="$(CC) $(COPTS) $(MIPS16_OPT)" --with-gnu-ld --disable-rpath --enable-gettext=no --disable-blkid --enable-lib64=no root_prefix=$(INSTALLDIR)/xfsprogs
+	cd xfsprogs && ./configure --host=$(ARCH)-linux \
+		CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -I$(TOP)/e2fsprogs/lib -DNEED_PRINTF" \
+		LDFLAGS="$(LDLTO) -L$(TOP)/e2fsprogs/lib/uuid -luuid -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+		CC="$(CC) $(COPTS) $(MIPS16_OPT)" \
+		--with-gnu-ld  \
+		--disable-rpath \
+		--enable-gettext=no \
+		--disable-blkid \
+		--enable-shared=yes \
+		--enable-static=no \
+		--enable-lib64=no \
+		root_prefix=$(INSTALLDIR)/xfsprogs \
+		AR_FLAGS="cru $(LTOPLUGIN)" \
+		RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
 
 xfsprogs:
 	make -C xfsprogs DEBUG= Q= 
-
 
 xfsprogs-clean:
 	make -C xfsprogs clean
@@ -17,6 +29,6 @@ xfsprogs-install:
 	rm -rf $(INSTALLDIR)/xfsprogs/usr/man
 	rm -rf $(INSTALLDIR)/xfsprogs/usr/include
 	rm -rf $(INSTALLDIR)/xfsprogs/usr
-	rm -rf $(INSTALLDIR)/xfsprogs/lib
-	rm -f $(INSTALLDIR)/xfsprogs/sbin/fsck.xfs
-	rm -f $(INSTALLDIR)/xfsprogs/sbin/xfs_repair
+#	rm -rf $(INSTALLDIR)/xfsprogs/lib
+#	rm -f $(INSTALLDIR)/xfsprogs/sbin/fsck.xfs
+#	rm -f $(INSTALLDIR)/xfsprogs/sbin/xfs_repair
