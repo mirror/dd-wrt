59532cc410068bea53db6e8dc5922f7645c46872
BrainSlayer <s.gottschall@dd-wrt.com>
2020-09-20 16:19:44 +0000

    add 2.5 cap
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@44435 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/utils.h
+++ src/router/shared/utils.h
@@ -1171,6 +1171,9 @@ extern int has_no_apmode(const char *prefix);
 extern int has_qboost_tdma(const char *prefix);
 extern int has_beacon_limit(const char *prefix);
 extern int has_spectralscanning(const char *prefix);
+extern int has_half(const char *prefix);
+extern int has_quarter(const char *prefix);
+extern int has_subquarter(const char *prefix);
 extern int getath9kdevicecount(void);
 #else
 static inline int is_mac80211(const char *prefix)
@@ -1214,6 +1217,21 @@ static inline int has_beacon_limit(char *prefix)
 }
 
 static inline int has_channelsurvey(char *prefix)
+{
+	return 0;
+}
+
+static inline int has_half(char *prefix)
+{
+	return 0;
+}
+
+static inline int has_quarter(char *prefix)
+{
+	return 0;
+}
+
+static inline int has_subquarter(char *prefix)
 {
 	return 0;
 }
