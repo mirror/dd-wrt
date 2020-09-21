09717b043c9be8a3a7b3416a4b2569eccce213d2
BrainSlayer <s.gottschall@dd-wrt.com>
2020-09-21 09:08:40 +0000

    extra rule. return only true if all wan relevant services are started
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@44444 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/check_ps.c
+++ src/router/rc/check_ps.c
@@ -92,17 +92,17 @@ struct mon mons[] = {
 #endif
 	{ "dnsmasq", M_LAN, "dnsmasq_enable", "1", NULL, NULL, NULL },
 #ifdef HAVE_SMARTDNS
-	{ "smartdns", M_LAN, "smartdns", "1", NULL, NULL, NULL },
+	{ "smartdns", M_WAN, "smartdns", "1", NULL, NULL, NULL },
 #endif
 	{ "dhcpfwd", M_WAN, "dhcpfwd_enable", "1", NULL, NULL, NULL },
 #ifdef HAVE_PRIVOXY
-	{ "privoxy", M_LAN, "privoxy_enable", "1", NULL, NULL, NULL },
+	{ "privoxy", M_WAN, "privoxy_enable", "1", NULL, NULL, NULL },
 #endif
 #ifdef HAVE_NOCAT
-	{ "splashd", M_LAN, "NC_enable", "1", NULL, NULL, NULL },
+	{ "splashd", M_WAN, "NC_enable", "1", NULL, NULL, NULL },
 #endif
 #ifdef HAVE_CHILLI
-	{ "chilli", M_LAN, "chilli_enable", "1", NULL, NULL, NULL },
+	{ "chilli", M_WAN, "chilli_enable", "1", NULL, NULL, NULL },
 #endif
 #ifdef HAVE_WIFIDOG
 	{ "wifidog", M_WAN, "wd_enable", "1", NULL, NULL, NULL },
@@ -117,14 +117,14 @@ struct mon mons[] = {
 	{ "igmprt", M_WAN, "block_multicast", "0", NULL, NULL, &check_igmprt },
 #endif
 #ifdef HAVE_TRANSMISSION
-	{ "transmission", M_LAN, "transmission_enable", "1", NULL, NULL, &check_transmission },
+	{ "transmission", M_WAN, "transmission_enable", "1", NULL, NULL, &check_transmission },
 #endif
 #ifdef HAVE_ERC
 #ifdef HAVE_OPENVPN
-	{ "openvpn", M_LAN, "openvpncl_enable", "1", NULL, NULL, NULL },
+	{ "openvpn", M_WAN, "openvpncl_enable", "1", NULL, NULL, NULL },
 #endif
 #endif
-	{ "ddns", M_LAN, "ddns_enable", "1", NULL, NULL, &check_ddns },
+	{ "ddns", M_WAN, "ddns_enable", "1", NULL, NULL, &check_ddns },
 	{ NULL, 0, NULL, NULL, NULL, NULL }
 };
 
