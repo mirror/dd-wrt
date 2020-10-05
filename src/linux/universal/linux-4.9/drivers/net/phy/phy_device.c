75a46ca9f21cce210ffe823fe4aa972b282be9e7
BrainSlayer <s.gottschall@dd-wrt.com>
2020-10-05 13:54:23 +0000

    kernel update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@44499 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.9/drivers/net/phy/phy_device.c
+++ src/linux/universal/linux-4.9/drivers/net/phy/phy_device.c
@@ -1049,7 +1049,8 @@ void phy_detach(struct phy_device *phydev)
 	phy_suspend(phydev);
 	phydev->phylink = NULL;
 
-	module_put(phydev->mdio.dev.driver->owner);
+	if (phydev->mdio.dev.driver)
+		module_put(phydev->mdio.dev.driver->owner);
 
 	/* If the device had no specific driver before (i.e. - it
 	 * was using the generic driver), we unbind the device
