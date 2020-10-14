b941c3b5be30ceebac7be098b37fcedc5c0535ad
BrainSlayer <s.gottschall@dd-wrt.com>
2020-10-14 07:38:42 +0000

    fix scan list
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@44541 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/mac80211.c
+++ src/router/services/networking/mac80211.c
@@ -287,7 +287,7 @@ void configure_single_ath9k(int count)
 		else
 			sysprintf("echo 20 > /sys/kernel/debug/ieee80211/%s/ath5k/bwmode", wif);
 	}
-	if (isath10k) {
+	else if (isath10k) {
 		if (nvram_matchi(bw, 2))
 			sysprintf("echo 2 > /sys/kernel/debug/ieee80211/%s/ath10k/chanbw", wif);
 		else if (nvram_matchi(bw, 5))
@@ -297,7 +297,7 @@ void configure_single_ath9k(int count)
 		else
 			sysprintf("echo 20 > /sys/kernel/debug/ieee80211/%s/ath10k/chanbw", wif);
 	}
-	if (ismt7615) {
+	else if (ismt7615) {
 		if (nvram_matchi(bw, 5))
 			sysprintf("echo 5 > /sys/kernel/debug/ieee80211/%s/mt76/chanbw", wif);
 		else if (nvram_matchi(bw, 10))
