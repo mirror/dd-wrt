4baa2189e63ee12ab274a7505ece11e46db62feb
BrainSlayer <s.gottschall@dd-wrt.com>
2020-10-21 09:15:51 +0000

    fix frr and yang
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@44618 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/frr.mk
+++ src/router/rules/frr.mk
@@ -41,11 +41,11 @@ frr-configure: ncurses json-c readline libyang libcap
 		--enable-ospf-te --disable-ospfclient --enable-multipath=64  --enable-ipv6 --prefix=/usr --sysconfdir=/tmp --disable-ospf6d \
 		--enable-vtysh --enable-user=root --enable-group=root --disable-ospfapi --disable-isisd --disable-pimd --disable-nhrpd \
 		--disable-staticd --enable-bfdd --disable-babeld --enable-pie=no --with-libreadline=$(TOP)/readline \
-		CFLAGS="-fno-strict-aliasing -I$(TOP)/libcap/libcap/include -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/ -Drpl_malloc=malloc -Drpl_realloc=realloc -I$(TOP)/_staging/usr/include -I$(TOP)/frr/build -I$(TOP)/libyang/src" \
-		CPPFLAGS="-fno-strict-aliasing -I$(TOP)/libcap/libcap/include -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/ -Drpl_malloc=malloc -Drpl_realloc=realloc -I$(TOP)/_staging/usr/include -I$(TOP)/frr/build -I$(TOP)/libyang/src" \
-		LDFLAGS="-L$(TOP)/readline/shlib -L$(TOP)/ncurses/lib -lncurses -L$(TOP)/json-c/.libs -ljson-c -L$(TOP)/libyang -lyang -L$(TOP)/pcre/.libs -lpcre -L$(TOP)/libcap/libcap -lcap" \
-		LIBYANG_CFLAGS="-I$(TOP)/libyang/src" \
-		LIBYANG_LIBS="-L$(TOP)/libyang -lyang -L$(TOP)/pcre/.libs -lpcre" \
+		CFLAGS="-fno-strict-aliasing -I$(TOP)/libcap/libcap/include -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/ -Drpl_malloc=malloc -Drpl_realloc=realloc -I$(TOP)/_staging/usr/include -I$(TOP)/frr/build -I$(TOP)/libyang/build" \
+		CPPFLAGS="-fno-strict-aliasing -I$(TOP)/libcap/libcap/include -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -I$(TOP)/ -Drpl_malloc=malloc -Drpl_realloc=realloc -I$(TOP)/_staging/usr/include -I$(TOP)/frr/build -I$(TOP)/libyang/build" \
+		LDFLAGS="-L$(TOP)/readline/shlib -L$(TOP)/ncurses/lib -lncurses -L$(TOP)/json-c/.libs -ljson-c -L$(TOP)/libyang/build -lyang -L$(TOP)/pcre/.libs -lpcre -L$(TOP)/libcap/libcap -lcap" \
+		LIBYANG_CFLAGS="-I$(TOP)/libyang/build" \
+		LIBYANG_LIBS="-L$(TOP)/libyang/build -lyang -L$(TOP)/pcre/.libs -lpcre" \
 		PYTHON=/usr/bin/python3
 
 frr: ncurses json-c libyang libcap
