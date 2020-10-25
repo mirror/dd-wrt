560ba491fea8f3414415f749d0cfa64677e4d3b4
BrainSlayer <s.gottschall@dd-wrt.com>
2020-10-25 12:55:32 +0000

    add sdparm
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@44638 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rules/sdparm.mk
@@ -0,0 +1,13 @@
+sdparm-configure:
+	cd sdparm && ./autogen.sh && ./configure --host=$(ARCH)-uclibc-linux \
+	CFLAGS="$(COPTS) $(LTO) $(MIPS16_OPT) -D_GNU_SOURCE -DNEED_PRINTF -Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	LDFLAGS="$(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+
+sdparm:
+	$(MAKE) -C sdparm
+
+sdparm-install:
+	install -D sdparm/src/sdparm $(INSTALLDIR)/sdparm/usr/sbin/sdparm
+	
+sdparm-clean:
+	$(MAKE) -C sdparm clean
