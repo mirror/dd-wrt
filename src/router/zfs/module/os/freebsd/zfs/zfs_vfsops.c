7ebbf0e0deb0aaa9d1fd413430914be94455246c
BrainSlayer <s.gottschall@dd-wrt.com>
2020-10-31 01:10:01 +0000

    update zfs, make some more small optimizations on zstd
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@44704 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/zfs/module/os/freebsd/zfs/zfs_vfsops.c
+++ src/router/zfs/module/os/freebsd/zfs/zfs_vfsops.c
@@ -433,7 +433,7 @@ zfs_sync(vfs_t *vfsp, int waitfor)
 	} else {
 		/*
 		 * Sync all ZFS filesystems.  This is what happens when you
-		 * run sync(1M).  Unlike other filesystems, ZFS honors the
+		 * run sync(8).  Unlike other filesystems, ZFS honors the
 		 * request by waiting for all pools to commit all dirty data.
 		 */
 		spa_sync_allpools();
@@ -1118,21 +1118,10 @@ zfsvfs_setup(zfsvfs_t *zfsvfs, boolean_t mounting)
 	return (0);
 }
 
-extern krwlock_t zfsvfs_lock; /* in zfs_znode.c */
-
 void
 zfsvfs_free(zfsvfs_t *zfsvfs)
 {
 	int i;
-
-	/*
-	 * This is a barrier to prevent the filesystem from going away in
-	 * zfs_znode_move() until we can safely ensure that the filesystem is
-	 * not unmounted. We consider the filesystem valid before the barrier
-	 * and invalid after the barrier.
-	 */
-	rw_enter(&zfsvfs_lock, RW_READER);
-	rw_exit(&zfsvfs_lock);
 
 	zfs_fuid_destroy(zfsvfs);
 
