532a5cc47e2fa5921a9430f7b5348fdfcf6170b8
BrainSlayer <s.gottschall@dd-wrt.com>
2020-11-20 22:51:43 +0000

    fix brcm26
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@44853 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/radvd.mk
+++ src/router/rules/radvd.mk
@@ -2,10 +2,10 @@ radvd-configure: nvram libutils
 	cd radvd && ./autogen.sh
 	cd radvd/libdaemon && ./configure --disable-nls --disable-shared --enable-static --disable-lynx --prefix=/usr --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" AR_FLAGS="cru $(LTOPLUGIN)" RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)" ac_cv_func_setpgrp_void=yes ; make
 	-cd radvd && aclocal && autoconf && automake -a && cd .. 
-	cd radvd/flex && ./configure --disable-nls --prefix=/usr --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	cd radvd/flex && ./configure --disable-nls --without-stack-protector --prefix=/usr --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	AR_FLAGS="cru $(LTOPLUGIN)" \
 	RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
-	cd radvd && ./configure --host=$(ARCH)-linux CHECK_CFLAGS=no CHECK_LIBS=no LDFLAGS="$(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections"  CFLAGS="$(COPTS) $(LTO) -DNEED_PRINTF -I$(TOP)/radvd/flex -ffunction-sections -fdata-sections -Wl,--gc-sections " DAEMON_CFLAGS="-I$(TOP)/radvd/libdaemon" DAEMON_LIBS="-L$(TOP)/radvd/libdaemon/libdaemon/.libs  -ldaemon" LDFLAGS="-L$(TOP)/radvd/flex -L$(TOP)/libutils -lutils -lshutils -L$(TOP)/nvram -lnvram -ffunction-sections -fdata-sections -Wl,--gc-sections"  --with-flex=$(TOP)/radvd/flex \
+	cd radvd && ./configure --host=$(ARCH)-linux --without-stack-protector CHECK_CFLAGS=no CHECK_LIBS=no LDFLAGS="$(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections"  CFLAGS="$(COPTS) $(LTO) -DNEED_PRINTF -I$(TOP)/radvd/flex -ffunction-sections -fdata-sections -Wl,--gc-sections " DAEMON_CFLAGS="-I$(TOP)/radvd/libdaemon" DAEMON_LIBS="-L$(TOP)/radvd/libdaemon/libdaemon/.libs  -ldaemon" LDFLAGS="-L$(TOP)/radvd/flex -L$(TOP)/libutils -lutils -lshutils -L$(TOP)/nvram -lnvram -ffunction-sections -fdata-sections -Wl,--gc-sections"  --with-flex=$(TOP)/radvd/flex \
 	AR_FLAGS="cru $(LTOPLUGIN)" \
 	RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
 	
