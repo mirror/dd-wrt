8b10d2feb457faa3106c768a5bc5cd8a3e000c00
BrainSlayer <s.gottschall@dd-wrt.com>
2020-12-07 07:16:58 +0000

    missed this hardcoded shit here
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@44929 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/dd-wrt/lang_pack/german.js
+++ src/router/kromo/dd-wrt/lang_pack/german.js
@@ -426,10 +426,10 @@ bmenu.wirelessAdvanced="Erweiterte Einstellungen";
 bmenu.wirelessAdvancedwl0="WL0-Erweitert";
 bmenu.wirelessAdvancedwl1="WL1-Erweitert";
 bmenu.wirelessWds="WDS";
-bmenu.wirelessWds0="Ath0-WDS";
-bmenu.wirelessWds1="Ath1-WDS";
-bmenu.wirelessWds2="Ath2-WDS";
-bmenu.wirelessWds3="Ath3-WDS";
+bmenu.wirelessWds0="Wlan0-WDS";
+bmenu.wirelessWds1="Wlan1-WDS";
+bmenu.wirelessWds2="Wlan2-WDS";
+bmenu.wirelessWds3="Wlan3-WDS";
 bmenu.wirelessWdswl0="WL0-WDS";
 bmenu.wirelessWdswl1="WL1-WDS";
 
