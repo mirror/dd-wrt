56db5d3888519f893fa5339d2c3f5746e8980035
BrainSlayer <s.gottschall@dd-wrt.com>
2020-12-08 05:37:13 +0000

    fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@44950 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/frr/lib/northbound.h
+++ src/router/frr/lib/northbound.h
@@ -562,11 +562,7 @@ struct frr_yang_module_info {
 
 		/* Priority - lower priorities are processed first. */
 		uint32_t priority;
-#if defined(__GNUC__) && ((__GNUC__ - 0) < 5) && !defined(__clang__)
 	} nodes[YANG_MODULE_MAX_NODES + 1];
-#else
-	} nodes[];
-#endif
 };
 
 /* Northbound error codes. */
