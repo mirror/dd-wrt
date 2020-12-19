baf9a97e4b67e9062fa2d156a35dbe9cd1e7b105
BrainSlayer <s.gottschall@dd-wrt.com>
2020-12-19 07:10:13 +0000

    new params
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@45047 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/mac80211.c
+++ src/router/services/networking/mac80211.c
@@ -244,6 +244,8 @@ void set_mesh_params(char *dev)
 	mesh_param("mesh_awake_window", "10");
 	mesh_param("mesh_plink_timeout", "0");
 	mesh_param("mesh_nolearn", "0");
+	mesh_param("mesh_connected_to_gate", "0");
+	mesh_param("mesh_connected_to_as", "0");
 }
 
 void mesh_params_main(int argc, char *argv[])
