5774b4623bbb6b23c26e9a8a8ae8a8b360153121
BrainSlayer <s.gottschall@dd-wrt.com>
2020-12-20 16:00:31 +0000

    shorten string
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@45061 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/dd-wrt/lang_pack/english.js
+++ src/router/kromo/dd-wrt/lang_pack/english.js
@@ -3026,7 +3026,7 @@ wl_basic.mesh_deep="Deep Sleep";
 wl_basic.mesh_light="Light Sleep";
 wl_basic.mesh_nolearn="Avoid Multi-Hop Path Discovery";
 wl_basic.mesh_connected_to_gate="Announce Connected To Gate";
-wl_basic.mesh_connected_to_as="Announce Connected To Authentication Server";
+wl_basic.mesh_connected_to_as="Announce Connected To Auth-Server";
 
 //help container
 var hwl_basic=new Object();
