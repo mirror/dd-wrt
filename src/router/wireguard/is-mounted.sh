dfcfac42f08353b61a9a2a5a0844f0bd971f1631
BrainSlayer <s.gottschall@dd-wrt.com>
2021-01-03 12:49:12 +0000

    wg fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@45281 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/wireguard/is-mounted.sh
+++ src/router/wireguard/is-mounted.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # wait until directory is mounted and writable
 # usage: is-mounted /name-of directory
 # default is /jffs
@@ -6,15 +7,15 @@ DIR=$1
 [[ -z "$DIR" ]] && DIR="/jffs"
 i=0
 # grep -q "$DIR.*rw" /proc/mounts && echo "$DIR is ready" || echo "$DIR is not ready"
-until [[ -f $DIR/is-mounted ]]; do
+until [[ -f $DIR/mounted ]]; do
 	sleep 1
 	i=$(($i+1))
-	touch "$DIR/is-mounted" >/dev/null 2>&1
+	touch "$DIR/mounted" >/dev/null 2>&1
 	if [[ $i -gt 35 ]]; then
 		echo "$DIR not mounted after 35 sec"
 		exit
 	fi
 done 
-rm $DIR/is-mounted
+rm $DIR/mounted >/dev/null 2>&1
 echo "$DIR mounted after $i seconds"
 )2>&1 | logger -t "$(basename $0): "
