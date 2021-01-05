6afc674d4f38d53ae4d37052ab3662be5abb8865
BrainSlayer <s.gottschall@dd-wrt.com>
2021-01-05 20:31:47 +0000

    missing commit
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@45349 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.5/include/linux/compiler-intel.h
+++ src/linux/universal/linux-3.5/include/linux/compiler-intel.h
@@ -15,6 +15,7 @@
  */
 #undef barrier
 #undef RELOC_HIDE
+#undef OPTIMIZER_HIDE_VAR
 
 #define barrier() __memory_barrier()
 
@@ -23,7 +24,20 @@
      __ptr = (unsigned long) (ptr);				\
     (typeof(ptr)) (__ptr + (off)); })
 
+/* This should act as an optimization barrier on var.
+ * Given that this compiler does not have inline assembly, a compiler barrier
+ * is the best we can do.
+ */
+#define OPTIMIZER_HIDE_VAR(var) barrier()
+
 /* Intel ECC compiler doesn't support __builtin_types_compatible_p() */
 #define __must_be_array(a) 0
 
 #endif
+
+#ifndef __HAVE_BUILTIN_BSWAP16__
+/* icc has this, but it's called _bswap16 */
+#define __HAVE_BUILTIN_BSWAP16__
+#define __builtin_bswap16 _bswap16
+#endif
+
