f19220f071642eb2c43f68599b60afc3dbeac7d0
BrainSlayer <s.gottschall@dd-wrt.com>
2021-01-13 20:43:20 +0000

    disable vht forcefully if turboqam is not enabled
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@45437 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/mac80211.c
+++ src/router/services/networking/mac80211.c
@@ -369,6 +369,11 @@ void configure_single_ath9k(int count)
 	sprintf(wl_sifs_trigger_time, "%s_sifs_trigger_time", dev);
 #ifdef HAVE_ATH10K
 	if (is_ath10k(dev)) {
+		if (nvram_nmatch("1", "%s_turbo_qam", dev))
+			sysprintf("echo 1 > /sys/kernel/debug/ieee80211/%s/ath10k/turboqam");
+		else
+			sysprintf("echo 0 > /sys/kernel/debug/ieee80211/%s/ath10k/turboqam");
+		
 		if (has_qboost(dev)) {
 			sysprintf("echo %s > /sys/kernel/debug/ieee80211/%s/ath10k/qboost_enable", nvram_default_get(wl_qboost, "0"), wif);
 			if (has_qboost_tdma(dev)) {
