dcda88e291ff9b76f273f72a4b89bb78e5917969
BrainSlayer <s.gottschall@dd-wrt.com>
2021-01-20 07:33:03 +0000

    2.83 is now official. so we can drop the prerelease version
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@45503 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/dnsmasq/src/config.h
+++ src/router/dnsmasq/src/config.h
@@ -120,6 +120,9 @@ HAVE_AUTH
    define this to include the facility to act as an authoritative DNS
    server for one or more zones.
 
+HAVE_NETTLEHASH
+   include just hash function from nettle, but no DNSSEC.
+
 HAVE_DNSSEC
    include DNSSEC validator.
 
@@ -195,6 +198,7 @@ RESOLVFILE
 /* #define HAVE_IDN */
 /* #define HAVE_LIBIDN2 */
 /* #define HAVE_CONNTRACK */
+/* #define HAVE_NETTLEHASH */
 /* #define HAVE_DNSSEC */
 
 
@@ -424,6 +428,10 @@ static char *compile_opts =
 "no-"
 #endif
 "auth "
+#if !defined(HAVE_NETTLEHASH) && !defined(HAVE_DNSSEC)
+"no-"
+#endif
+"nettlehash "
 #ifndef HAVE_DNSSEC
 "no-"
 #endif
