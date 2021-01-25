d8fff0167d437b7fe7c6c339af15cde96de363a0
BrainSlayer <s.gottschall@dd-wrt.com>
2021-01-25 09:25:20 +0000

    already defined
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@45566 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.14/include/linux/skbuff.h
+++ src/linux/universal/linux-4.14/include/linux/skbuff.h
@@ -1373,12 +1373,6 @@ static inline void skb_mark_not_on_list(struct sk_buff *skb)
 	for ((skb) = (first), (next_skb) = (skb) ? (skb)->next : NULL; (skb);  \
 	     (skb) = (next_skb), (next_skb) = (skb) ? (skb)->next : NULL)
 
-static inline void skb_list_del_init(struct sk_buff *skb)
-{
-	__list_del_entry(&skb->list);
-	skb_mark_not_on_list(skb);
-}
-
 /**
  *	skb_queue_empty - check if a queue is empty
  *	@list: queue head
