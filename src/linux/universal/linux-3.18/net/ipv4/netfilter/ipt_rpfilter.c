1206ae655c5de358b13c242bb2239da85997c72b
BrainSlayer <s.gottschall@dd-wrt.com>
2021-02-01 10:36:41 +0000

    backport
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@45648 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/net/ipv4/netfilter/ipt_rpfilter.c
+++ src/linux/universal/linux-3.18/net/ipv4/netfilter/ipt_rpfilter.c
@@ -95,7 +95,7 @@ static bool rpfilter_mt(const struct sk_buff *skb, struct xt_action_param *par)
 	flow.daddr = iph->saddr;
 	flow.saddr = rpfilter_get_saddr(iph->daddr);
 	flow.flowi4_mark = info->flags & XT_RPFILTER_VALID_MARK ? skb->mark : 0;
-	flow.flowi4_tos = RT_TOS(iph->tos);
+	flow.flowi4_tos = iph->tos & IPTOS_RT_MASK;
 	flow.flowi4_scope = RT_SCOPE_UNIVERSE;
 
 	return rpfilter_lookup_reverse(&flow, par->in, info->flags) ^ invert;
