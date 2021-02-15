9dac88220e6fa478654e6f59d8bd5dcabb33d92d
BrainSlayer <s.gottschall@dd-wrt.com>
2021-02-15 19:11:38 +0000

    avoid null pointer
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@45763 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/libwireless/mac80211autochannel.c
+++ src/router/libutils/libwireless/mac80211autochannel.c
@@ -488,7 +488,8 @@ struct mac80211_ac *mac80211autochannel(const char *interface, char *freq_range,
 			if (!nvram_nmatch("sta", "%s_mode", dev) && !nvram_nmatch("wdssta", "%s_mode", dev)) {
 				if (nvram_nmatch("0", "%s_channel", dev)) {
 					struct wifi_interface *interface = wifi_getfreq(dev);
-					freq = interface->freq;
+					if (interface)
+						freq = interface->freq;
 				} else {
 					freq = atoi(nvram_nget("%s_channel", dev));
 				}
