10173d2e54775cb83b2f2af0eb61a1d96df0e838
BrainSlayer <s.gottschall@dd-wrt.com>
2021-03-01 18:33:04 +0000

    increase maxlength for endpoint
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@45874 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/eoptunnel.c
+++ src/router/httpd/visuals/eoptunnel.c
@@ -315,7 +315,7 @@ EJ_VISIBLE void ej_show_eop_tunnels(webs_t wp, int argc, char_t ** argv)
 						{
 							show_caption(wp, "label", "eoip.wireguard_peer", NULL);
 							websWrite(wp,
-								  "<input size=\"20\" maxlength=\"20\" name=\"%s\" value=\"%s\" />:<input size=\"5\" maxlength=\"5\" name=\"%s\" class=\"num\" value=\"%s\" />\n\n",
+								  "<input size=\"20\" maxlength=\"48\" name=\"%s\" value=\"%s\" />:<input size=\"5\" maxlength=\"5\" name=\"%s\" class=\"num\" value=\"%s\" />\n\n",
 								  temp, nvram_safe_get(temp), temp2, nvram_safe_get(temp2));
 						}
 						websWrite(wp, "</div>\n");
