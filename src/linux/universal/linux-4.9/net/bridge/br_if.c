3490997865b88a2ac5d35c020e2ad4de5ad09731
BrainSlayer <s.gottschall@dd-wrt.com>
2021-03-06 08:01:30 +0000

    backport bridge cfm
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@45929 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.9/net/bridge/br_if.c
+++ src/linux/universal/linux-4.9/net/bridge/br_if.c
@@ -271,6 +271,8 @@ static void del_nbp(struct net_bridge_port *p)
 	spin_unlock_bh(&br->lock);
 
 	br_mrp_port_del(br, p);
+	br_cfm_port_del(br, p);
+
 	br_ifinfo_notify(RTM_DELLINK, p);
 
 	list_del_rcu(&p->list);
