fc7a70861b7e994a3802546d7e44970f6f6fb02d
BrainSlayer <s.gottschall@dd-wrt.com>
2021-03-12 17:21:07 +0000

    for testing, need to discuss with plex maintainers if they agree about integration
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@45999 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/plex/x64/usr/lib/plexmediaserver/Resources/Graphics/sugar.cmake
@@ -0,0 +1,11 @@
+# This file generated automatically by:
+#   generate_sugar_files.py
+# see wiki for more info:
+#   https://github.com/ruslo/sugar/wiki/Collecting-sources
+
+if(DEFINED GRAPHICS_SUGAR_CMAKE_)
+  return()
+else()
+  set(GRAPHICS_SUGAR_CMAKE_ 1)
+endif()
+
