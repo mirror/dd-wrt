641fe35a153361c5b095038aa6c763083e1ce543
BrainSlayer <s.gottschall@dd-wrt.com>
2021-03-13 23:16:11 +0000

    add plex
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46022 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/services/services/plex.c
@@ -0,0 +1,63 @@
+/*
+ * telnet.c
+ *
+ * Copyright (C) 2007 Sebastian Gottschall <gottschall@dd-wrt.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id:
+ */
+#ifdef HAVE_PLEX
+#include <stdlib.h>
+#include <bcmnvram.h>
+#include <shutils.h>
+#include <utils.h>
+#include <syslog.h>
+#include <signal.h>
+#include <services.h>
+
+char *plex_deps(void)
+{
+	return "plex_enable";
+}
+
+char *plex_proc(void)
+{
+	return "Plex Media Server";
+}
+
+void start_plex(void)
+{
+	char *plex_argv[] = { "/usr/lib/plexmediaserver/lib/run.sh", NULL };
+	stop_plex();
+
+	if (!nvram_invmatchi("plex_enable", 0))
+		return;
+
+	_evalpid(plex_argv, NULL, 0, NULL);
+	dd_loginfo("plex", "daemon successfully started\n");
+
+	cprintf("done\n");
+	return;
+}
+
+void stop_plex(void)
+{
+	stop_process("Plex Media Server", "daemon");
+	nvram_delstates(plex_deps());
+	cprintf("done\n");
+	return;
+}
+#endif
