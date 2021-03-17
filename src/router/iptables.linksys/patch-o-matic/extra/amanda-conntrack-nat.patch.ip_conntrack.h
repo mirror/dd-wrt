ad72c6e5483a7be76881de0dafcb866b16d456e2
BrainSlayer <s.gottschall@dd-wrt.com>
2021-03-17 09:35:36 +0000

    obsolete
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46081 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/iptables.linksys/patch-o-matic/extra/amanda-conntrack-nat.patch.ip_conntrack.h
+++
@@ -1,3 +0,0 @@
-/* Add protocol helper include file here */
-#include <linux/netfilter_ipv4/ip_conntrack_amanda.h>
-
--- src/router/iptables.linksys/patch-o-matic/extra/h323-conntrack-nat.patch.ip_conntrack.h
+++
@@ -1,3 +0,0 @@
-/* Add protocol helper include file here */
-#include <linux/netfilter_ipv4/ip_conntrack_h323.h>
-
