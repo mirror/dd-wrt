5a5858490bd600538ba3cb228117688555366961
BrainSlayer <s.gottschall@dd-wrt.com>
2021-03-18 08:10:20 +0000

    block jar files too
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46089 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/brcm/linux-2.6.23/net/ipv4/netfilter/ipt_webstr.c
+++ src/linux/brcm/linux-2.6.23/net/ipv4/netfilter/ipt_webstr.c
@@ -356,6 +356,12 @@ match(const struct sk_buff *skb,
 		    found = 1;
 		    goto match_ret;
 		}
+		vicelen = sizeof(".jar") - 1;
+		if (strnicmp(htinfo.url + htinfo.urllen - vicelen, ".jar", vicelen) == 0) {
+		    SPARQ_LOG("%s: MATCH....java\n", __FUNCTION__);
+		    found = 1;
+		    goto match_ret;
+		}
 	    }
 	    if (opt & BLK_ACTIVE){
 		vicelen = sizeof(".ocx") - 1;
