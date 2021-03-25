46a2d53bca3a1e3cd06908a19284c42e067bb9a6
BrainSlayer <s.gottschall@dd-wrt.com>
2021-03-25 08:54:58 +0000

    update kernels
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46171 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.9/include/linux/thread_info.h
+++ src/linux/universal/linux-4.9/include/linux/thread_info.h
@@ -60,6 +60,18 @@ extern long do_no_restart_syscall(struct restart_block *parm);
 
 #ifdef __KERNEL__
 
+#ifndef arch_set_restart_data
+#define arch_set_restart_data(restart) do { } while (0)
+#endif
+
+static inline long set_restart_fn(struct restart_block *restart,
+					long (*fn)(struct restart_block *))
+{
+	restart->fn = fn;
+	arch_set_restart_data(restart);
+	return -ERESTART_RESTARTBLOCK;
+}
+
 #define THREADINFO_GFP	(GFP_KERNEL_ACCOUNT | __GFP_NOTRACK | __GFP_ZERO)
 
 /*
