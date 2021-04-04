faa907057e074f1f1311d6829099cd167db12478
BrainSlayer <s.gottschall@dd-wrt.com>
2021-04-04 04:55:22 +0000

    move back to header. inlining cannot works accross file boundaries
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46270 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/smbd-next/smbd/netmisc.c
+++ src/router/smbd-next/smbd/netmisc.c
@@ -643,33 +643,3 @@ struct timespec64 ksmbd_NTtimeToUnix(__le64 ntutc)
 
 	return ts;
 }
-
-/* Convert the Unix UTC into NT UTC. */
-#if LINUX_VERSION_CODE <= KERNEL_VERSION(4, 18, 0)
-inline u64 ksmbd_UnixTimeToNT(struct timespec t)
-#else
-inline u64 ksmbd_UnixTimeToNT(struct timespec64 t)
-#endif
-{
-	/* Convert to 100ns intervals and then add the NTFS time offset. */
-	return (u64)t.tv_sec * 10000000 + t.tv_nsec / 100 + NTFS_TIME_OFFSET;
-}
-
-inline long long ksmbd_systime(void)
-{
-#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 18, 0)
-	struct timespec		ts;
-
-	getnstimeofday(&ts);
-	return ksmbd_UnixTimeToNT(ts);
-#else
-	struct timespec64	ts;
-
-	ktime_get_real_ts64(&ts);
-#if LINUX_VERSION_CODE <= KERNEL_VERSION(4, 18, 0)
-	return ksmbd_UnixTimeToNT(timespec64_to_timespec(ts));
-#else
-	return ksmbd_UnixTimeToNT(ts);
-#endif
-#endif
-}
--- src/router/smbd-next/smbd/smb_common.h
+++ src/router/smbd-next/smbd/smb_common.h
@@ -556,10 +556,33 @@ struct timespec64 ksmbd_NTtimeToUnix(__le64 ntutc);
 #define KSMBD_TIME_TO_TM	time_to_tm
 #endif
 
+/* Convert the Unix UTC into NT UTC. */
 #if LINUX_VERSION_CODE <= KERNEL_VERSION(4, 18, 0)
-inline u64 ksmbd_UnixTimeToNT(struct timespec t);
+static inline u64 ksmbd_UnixTimeToNT(struct timespec t)
 #else
-inline u64 ksmbd_UnixTimeToNT(struct timespec64 t);
+static inline u64 ksmbd_UnixTimeToNT(struct timespec64 t)
 #endif
-inline long long ksmbd_systime(void);
+{
+	/* Convert to 100ns intervals and then add the NTFS time offset. */
+	return (u64)t.tv_sec * 10000000 + t.tv_nsec / 100 + NTFS_TIME_OFFSET;
+}
+
+static inline long long ksmbd_systime(void)
+{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 18, 0)
+	struct timespec		ts;
+
+	getnstimeofday(&ts);
+	return ksmbd_UnixTimeToNT(ts);
+#else
+	struct timespec64	ts;
+
+	ktime_get_real_ts64(&ts);
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(4, 18, 0)
+	return ksmbd_UnixTimeToNT(timespec64_to_timespec(ts));
+#else
+	return ksmbd_UnixTimeToNT(ts);
+#endif
+#endif
+}
 #endif /* __SMB_COMMON_H__ */
