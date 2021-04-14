c588eb3772e266501baa163f75e17343ef32f89e
BrainSlayer <s.gottschall@dd-wrt.com>
2021-04-14 08:13:47 +0000

    ignore this
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46336 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/squid/src/main.cc
+++ src/router/squid/src/main.cc
@@ -1070,13 +1070,13 @@ setEffectiveUser(void)
     return;
 #endif
 
-    if (geteuid() == 0) {
+/*    if (geteuid() == 0) {
         debugs(0, DBG_CRITICAL, "Squid is not safe to run as root!  If you must");
         debugs(0, DBG_CRITICAL, "start Squid as root, then you must configure");
         debugs(0, DBG_CRITICAL, "it to run as a non-priveledged user with the");
         debugs(0, DBG_CRITICAL, "'cache_effective_user' option in the config file.");
         fatal("Don't run Squid as root, set 'cache_effective_user'!");
-    }
+    }*/
 }
 
 /// changes working directory, providing error reporting
