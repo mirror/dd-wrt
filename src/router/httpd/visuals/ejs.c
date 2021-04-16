5f35dbda0a1c67a429481f7863cbd37bdeeec2c5
BrainSlayer <s.gottschall@dd-wrt.com>
2021-04-16 12:02:21 +0000

    fix style in case of x86/x66 fw upgrades
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46375 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/ejs.c
+++ src/router/httpd/visuals/ejs.c
@@ -2118,6 +2118,8 @@ EJ_VISIBLE void ej_do_pagehead(webs_t wp, int argc, char_t ** argv)	// Eko
 	char *style = nvram_safe_get("router_style");
 	char *style_dark = nvram_safe_get("router_style_dark");
 	static char *charset = NULL;
+	if (!*style)
+		style = "elegant";
 	if (!charset)
 		charset = strdup(live_translate(wp, "lang_charset.set"));
 
