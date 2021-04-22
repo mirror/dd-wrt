0b12efe78acd88bc293c1b999f8ee0a7c6952e36
BrainSlayer <s.gottschall@dd-wrt.com>
2021-04-22 14:36:07 +0000

    workaround hack to fix bad configure
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46441 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/tcpdump.mk
+++ src/router/rules/tcpdump.mk
@@ -1,8 +1,10 @@
 tcpdump-configure: libpcap
-	cd tcpdump && ./configure --host=$(ARCH)-linux --enable-shared --enable-ipv6 --libdir=/lib --disable-static CC="$(CC)" ac_cv_linux_vers=2 ac_cv_ssleay_path=no td_cv_buggygetaddrinfo=no CPPFLAGS="-I../libpcap $(COPTS) $(MIPS16_OPT) $(LTO)  -ffunction-sections -fdata-sections -Wl,--gc-sections -D_GNU_SOURCE -DNEED_PRINTF" CFLAGS="-I../libpcap $(COPTS) $(LTO) $(MIPS16_OPT) -D_GNU_SOURCE  -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF -DHAVE_BPF_DUMP" LDFLAGS="-L$(TOP)/libpcap $(LDLTO)" \
+	cd tcpdump && ./configure --host=$(ARCH)-linux --enable-shared --enable-ipv6 --libdir=/lib --disable-static CC="$(CC)" ac_cv_linux_vers=2 ac_cv_ssleay_path=no td_cv_buggygetaddrinfo=no CPPFLAGS="-I../libpcap $(COPTS) $(MIPS16_OPT) $(LTO)  -ffunction-sections -fdata-sections -Wl,--gc-sections -D_GNU_SOURCE -DNEED_PRINTF" CFLAGS="-I../libpcap $(COPTS) $(LTO) $(MIPS16_OPT) -D_GNU_SOURCE  -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF -DHAVE_BPF_DUMP  -D_GNU_SOURCE" LDFLAGS="-L$(TOP)/libpcap $(LDLTO)" \
 		AR="$(ARCH)-linux-ar cru $(LTOPLUGIN)" \
 		RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
-
+	sed -i 's/-I\/usr\/include/ /g' $(TOP)/tcpdump/Makefile
+	sed -i 's/-Wl,-rpath,\/usr\/lib64/ /g' $(TOP)/tcpdump/Makefile
+	sed -i 's/-L\/usr\/lib64/ /g' $(TOP)/tcpdump/Makefile
 tcpdump:
 	make -j 4 -C tcpdump
 
