b18adf7a93b1f9b7a1118159b4f83de7d60c3187
BrainSlayer <s.gottschall@dd-wrt.com>
2021-04-28 15:08:56 +0000

    update kernels
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46452 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4/include/linux/compiler-gcc.h
+++ src/linux/universal/linux-4.4/include/linux/compiler-gcc.h
@@ -326,6 +326,11 @@
  */
 #define uninitialized_var(x) x = x
 
+
+#if GCC_VERSION >= 50100
+#define COMPILER_HAS_GENERIC_BUILTIN_OVERFLOW 1
+#endif
+
 /*
  * Turn individual warnings and errors on and off locally, depending
  * on version.
