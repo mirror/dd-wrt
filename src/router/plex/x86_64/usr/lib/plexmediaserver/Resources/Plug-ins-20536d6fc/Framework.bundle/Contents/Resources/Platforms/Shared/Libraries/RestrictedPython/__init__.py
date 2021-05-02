04feceb3df90e67bb4c1bbfd77c77b068c073079
BrainSlayer <s.gottschall@dd-wrt.com>
2021-05-02 06:44:47 +0000

    update plex: add new files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46495 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/plex/x86_64/usr/lib/plexmediaserver/Resources/Plug-ins-20536d6fc/Framework.bundle/Contents/Resources/Platforms/Shared/Libraries/RestrictedPython/__init__.py
@@ -0,0 +1,19 @@
+##############################################################################
+#
+# Copyright (c) 2002 Zope Corporation and Contributors. All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+'''
+RestrictedPython package.
+$Id: __init__.py 76322 2007-06-04 17:40:03Z philikon $
+'''
+
+from SelectCompiler import *
+from PrintCollector import PrintCollector
