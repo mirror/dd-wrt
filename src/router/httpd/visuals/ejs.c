a08ceb4273b19c3b5d92cb500e1b8e543a1ce435
BrainSlayer <s.gottschall@dd-wrt.com>
2021-05-02 08:02:37 +0000

    fix typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46507 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/ejs.c
+++ src/router/httpd/visuals/ejs.c
@@ -539,7 +539,7 @@ static int rule_wanvlan(char *name)
 static int rule_fa(char *name)
 {
 	char *fa = nvram_safe_get("ctf_fa_mode");
-	return *ctf ? 1 : 0;
+	return *fa ? 1 : 0;
 }
 
 static int rule_afterburner(char *name)
