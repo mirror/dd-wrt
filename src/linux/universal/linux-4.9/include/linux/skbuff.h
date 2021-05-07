11c8b349ffdd7068f3452b57b7eef5a76c38d784
BrainSlayer <s.gottschall@dd-wrt.com>
2021-05-07 07:37:34 +0000

    increase skb pad
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46567 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.9/include/linux/skbuff.h
+++ src/linux/universal/linux-4.9/include/linux/skbuff.h
@@ -2339,7 +2339,7 @@ static inline int pskb_network_may_pull(struct sk_buff *skb, unsigned int len)
  * NET_IP_ALIGN(2) + ethernet_header(14) + IP_header(20/40) + ports(8)
  */
 #ifndef NET_SKB_PAD
-#define NET_SKB_PAD	max(32, L1_CACHE_BYTES)
+#define NET_SKB_PAD	max(64, L1_CACHE_BYTES)
 #endif
 
 int ___pskb_trim(struct sk_buff *skb, unsigned int len);
