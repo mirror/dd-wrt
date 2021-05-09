e8b33be49e1d2ad14ff7857fdca36a59de2d3716
BrainSlayer <s.gottschall@dd-wrt.com>
2021-05-09 16:33:27 +0000

    fix compile bugs
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46609 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4/tools/perf/util/cache.h
+++ src/linux/universal/linux-4.4/tools/perf/util/cache.h
@@ -73,7 +73,7 @@ extern char *perf_pathdup(const char *fmt, ...)
 
 #ifndef __UCLIBC__
 /* Matches the libc/libbsd function attribute so we declare this unconditionally: */
-extern size_t strlcpy(char *dest, const char *src, size_t size);
+//extern size_t strlcpy(char *dest, const char *src, size_t size);
 #endif
 
 #endif /* __PERF_CACHE_H */
