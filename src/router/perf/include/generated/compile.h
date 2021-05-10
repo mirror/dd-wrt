92390f7c12279580221c9e2574fc021cb3861b88
BrainSlayer <s.gottschall@dd-wrt.com>
2021-05-10 11:04:00 +0000

    use more recent perf version
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46616 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/perf/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 33 */
+/* SMP */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#33 SMP Mon Apr 26 14:40:33 +07 2021"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "linux"
+#define LINUX_COMPILER "gcc version 11.1.0 (OpenWrt GCC 11.1.0 r16592-aabc632023)"
