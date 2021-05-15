9204e8175b0006217353812a4b5967e1629d4d09
BrainSlayer <s.gottschall@dd-wrt.com>
2021-05-15 06:34:37 +0000

    no reboot required for changing sfe/ctf mode
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46676 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/base.c
+++ src/router/httpd/modules/base.c
@@ -1556,8 +1556,6 @@ static int apply_cgi(webs_t wp, char_t * urlPrefix, char_t * webDir, int arg, ch
 			if (!strcmp(wshaper_enable, "1"))
 				need_reboot = 1;
 		}
-		if (sfe && nvram_geti("sfe") != atoi(sfe))
-			need_reboot = 1;
 		if (fa && nvram_geti("ctf_fa_mode") != atoi(fa))
 			need_reboot = 1;
 		if (need_reboot)
