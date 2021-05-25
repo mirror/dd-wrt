e2c5e921d57c655ba07c7a309fbb2f44b1e49a15
BrainSlayer <s.gottschall@dd-wrt.com>
2021-05-25 06:58:15 +0000

    new wds watchdog mode
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46754 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/wdswatchdog/wdswatchdog.sh
+++ src/router/wdswatchdog/wdswatchdog.sh
@@ -2,28 +2,70 @@
 
 WDS_WATCHDOG_INTERVAL_SEC=$(nvram get wds_watchdog_interval_sec)
 WDS_WATCHDOG_IPS=$(nvram get wds_watchdog_ips)
+WDS_WATCHDOG_MODE=$(nvram get wds_watchdog_mode)
+TAG="WDS_Watchdog[$$]"
 
+logger -t "$TAG" "Started"
 while sleep $WDS_WATCHDOG_INTERVAL_SEC
 do
-  for ip in $WDS_WATCHDOG_IPS
-  do
-    if ping -c 1 $ip > /tmp/null
-    then
-      echo "$ip ok"
-    else
-      echo "$ip dropped one"
-      sleep 10
-      if ! ping -c 1 $ip > /tmp/null
+  if [ $WDS_WATCHDOG_MODE = "0" ]
+  then
+    logger -t "$TAG" "Test cycle in mode of any dropped IPs for reboot"
+    for ip in $WDS_WATCHDOG_IPS
+    do
+      if ping -c 1 $ip > /tmp/null
       then
-        echo "$ip dropped two"
+        logger -t "$TAG" "$ip ok"
+      else
+        logger -t "$TAG" "$ip dropped (1/3)"
         sleep 10
         if ! ping -c 1 $ip > /tmp/null
         then
-    	    echo "$ip dropped three, Restarting Router"
-    	    /usr/sbin/nvram commit
-    	    /sbin/reboot &
+          logger -t "$TAG" "$ip dropped (2/3)"
+          sleep 10
+          if ! ping -c 1 $ip > /tmp/null
+          then
+              logger -t "$TAG" "$ip dropped (3/3), Restarting Router"
+              /usr/sbin/nvram commit
+              /sbin/reboot &
+          fi
         fi
       fi
+    done
+  else
+    logger -t "$TAG" "Test cycle in mode of all dropped IPs for reboot"
+    WDS_WATCHDOG_PING_OK=false
+    for ip in $WDS_WATCHDOG_IPS
+    do
+      if ping -c 1 $ip > /tmp/null
+      then
+        WDS_WATCHDOG_PING_OK=true
+        logger -t "$TAG" "$ip ok"
+      else
+        logger -t "$TAG" "$ip dropped (1/3)"
+        sleep 10
+        if ping -c 1 $ip > /tmp/null
+        then
+          WDS_WATCHDOG_PING_OK=true
+          logger -t "$TAG" "$ip ok"
+        else
+          logger -t "$TAG" "$ip dropped (2/3)"
+          sleep 10
+          if ping -c 1 $ip > /tmp/null
+          then
+            WDS_WATCHDOG_PING_OK=true
+            logger -t "$TAG" "$ip ok"
+          else
+            logger -t "$TAG" "$ip dropped (3/3)"          
+          fi
+        fi
+      fi
+    done
+    if ! $WDS_WATCHDOG_PING_OK
+    then
+      logger -t "$TAG" "None of the IPs $WDS_WATCHDOG_IPS responded ping, Restarting Router"
+      /usr/sbin/nvram commit
+      /sbin/reboot &
     fi
-  done
+  fi
 done 2>&1
