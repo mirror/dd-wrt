4d7759a2283c95f15f58ffa75a76e4f41fd667c2
BrainSlayer <s.gottschall@dd-wrt.com>
2021-05-27 06:41:52 +0000

    we have to run vlan config here, otherwise the ip config of such interfaces remains unhandled
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46784 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/services.c
+++ src/router/rc/services.c
@@ -449,6 +449,7 @@ static void handle_index(void)
 	start_service("bridging");
 #endif
 	start_service_force("lan");
+	start_service_force("vlan_config");
 #ifdef HAVE_IPVS
 	start_service("ipvs");
 #endif
