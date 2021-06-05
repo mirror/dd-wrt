9a93f756638ebebd698da43de1fa2d00ad9cdafc
BrainSlayer <s.gottschall@dd-wrt.com>
2021-06-05 16:29:38 +0000

    experimental
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46887 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4/scripts/mod/modpost.c
+++ src/linux/universal/linux-4.4/scripts/mod/modpost.c
@@ -605,7 +605,7 @@ static int ignore_undef_symbol(struct elf_info *info, const char *symname)
 #define KSYMTAB_PFX VMLINUX_SYMBOL_STR(__ksymtab_)
 
 static void handle_modversions(struct module *mod, struct elf_info *info,
-			       Elf_Sym *sym, const char *symname)
+			       Elf_Sym *sym, const char *symname, int hasinit, int hasexit)
 {
 	unsigned int crc;
 	enum export export;
@@ -677,6 +677,10 @@ static void handle_modversions(struct module *mod, struct elf_info *info,
 			mod->has_init = 1;
 		if (strcmp(symname, VMLINUX_SYMBOL_STR(cleanup_module)) == 0)
 			mod->has_cleanup = 1;
+		if (hasinit)
+			mod->has_init=1;
+		if (hasexit)
+			mod->has_cleanup=1;
 		break;
 	}
 }
@@ -1948,7 +1952,31 @@ static void read_symbols(char *modname)
 
 	if (!parse_elf(&info, modname))
 		return;
-
+	int hasinit=0;
+	int hasexit=0;
+	char modn[512];
+	sprintf(modn,"nm %s|grep \"T init_module\"", modname);
+	FILE *p = popen(modn, "r");
+	if (p) {
+	    char line[512];
+	    memset(line,0,sizeof(line));
+	    fgets(line, sizeof(line), p);
+	    if (strstr(line, "init_module")) {
+		hasinit=1;
+	    }
+	    pclose(p);
+	}
+	sprintf(modn,"nm %s|grep \"T cleanup_module\"", modname);
+	p = popen(modn, "r");
+	if (p) {
+	    char line[512];
+	    memset(line,0,sizeof(line));
+	    fgets(line, sizeof(line), p);
+	    if (strstr(line, "cleanup_module")) {
+		hasexit=1;
+	    }
+	    pclose(p);
+	}
 	mod = new_module(modname);
 
 	/* When there's no vmlinux, don't print warnings about
@@ -1977,7 +2005,7 @@ static void read_symbols(char *modname)
 	for (sym = info.symtab_start; sym < info.symtab_stop; sym++) {
 		symname = remove_dot(info.strtab + sym->st_name);
 
-		handle_modversions(mod, &info, sym, symname);
+		handle_modversions(mod, &info, sym, symname, hasinit, hasexit);
 #ifndef CONFIG_MODULE_STRIPPED
 		handle_moddevtable(mod, &info, sym, symname);
 #endif
