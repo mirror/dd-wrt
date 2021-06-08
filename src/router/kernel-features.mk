bfc7f53a2674730c44128f9f440737f1f321f8b7
BrainSlayer <s.gottschall@dd-wrt.com>
2021-06-08 06:35:10 +0000

    lto can create massive oversized images, so we use it only for size optimizations right now
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46915 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kernel-features.mk
+++ src/router/kernel-features.mk
@@ -144,6 +144,7 @@ define kernelfeatures
 		sed -i 's/\CONFIG_CC_OPTIMIZE_FOR_PERFORMANCE_O3=y/# CONFIG_CC_OPTIMIZE_FOR_PERFORMANCE_O3 is not set/g' $(LINUXDIR)/.config; \
 		sed -i 's/\CONFIG_CC_OPTIMIZE_FOR_PERFORMANCE=y/# CONFIG_CC_OPTIMIZE_FOR_PERFORMANCE is not set/g' $(LINUXDIR)/.config; \
 		sed -i 's/\# CONFIG_CC_OPTIMIZE_FOR_SIZE is not set/CONFIG_CC_OPTIMIZE_FOR_SIZE=y/g' $(LINUXDIR)/.config; \
+		sed -i 's/\CONFIG_OPTIMIZE_INLINING=y/# CONFIG_OPTIMIZE_INLINING is not set/g' $(LINUXDIR)/.config; \
 	else \
 		echo "# CONFIG_LTO_MENU is not set" >> $(LINUXDIR)/.config; \
 	fi
