92b9d4435115f7585580e3dedde3c28a4c87c31c
BrainSlayer <s.gottschall@dd-wrt.com>
2021-06-13 14:51:27 +0000

    update smbd
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46952 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/smbd-next/smbd/asn1.c
+++ src/router/smbd-next/smbd/asn1.c
@@ -18,8 +18,9 @@
 #include "asn1.h"
 #include "connection.h"
 #include "auth.h"
-#include "spnego_negtokeninit-asn1.h"
-#include "spnego_negtokentarg-asn1.h"
+#include "ksmbd_spnego_negtokeninit-asn1.h"
+#include "ksmbd_spnego_negtokentarg-asn1.h"
+ 
 
 #define SPNEGO_OID_LEN 7
 #define NTLMSSP_OID_LEN  10
@@ -66,7 +67,7 @@ static bool asn1_oid_decode(const unsigned char *value, size_t vlen,
 
 	vlen += 1;
 	if (vlen < 2 || vlen > UINT_MAX / sizeof(unsigned long))
-		return false;
+		goto fail_nullify;
 
 	*oid = kmalloc(vlen * sizeof(unsigned long), GFP_KERNEL);
 	if (!*oid)
@@ -102,6 +103,7 @@ static bool asn1_oid_decode(const unsigned char *value, size_t vlen,
 
 fail:
 	kfree(*oid);
+fail_nullify:
 	*oid = NULL;
 	return false;
 }
@@ -119,7 +121,7 @@ int
 ksmbd_decode_negTokenInit(unsigned char *security_blob, int length,
 			  struct ksmbd_conn *conn)
 {
-	return asn1_ber_decoder(&spnego_negtokeninit_decoder, conn,
+	return asn1_ber_decoder(&ksmbd_spnego_negtokeninit_decoder, conn,
 				security_blob, length);
 }
 
@@ -127,7 +129,7 @@ int
 ksmbd_decode_negTokenTarg(unsigned char *security_blob, int length,
 			  struct ksmbd_conn *conn)
 {
-	return asn1_ber_decoder(&spnego_negtokentarg_decoder, conn,
+	return asn1_ber_decoder(&ksmbd_spnego_negtokentarg_decoder, conn,
 				security_blob, length);
 }
 
@@ -299,8 +301,8 @@ bad:
 	return -EBADMSG;
 }
 
-int gssapi_this_mech(void *context, size_t hdrlen, unsigned char tag,
-		     const void *value, size_t vlen)
+int ksmbd_gssapi_this_mech(void *context, size_t hdrlen, unsigned char tag,
+			   const void *value, size_t vlen)
 {
 	unsigned long *oid;
 	size_t oidlen;
@@ -324,8 +326,9 @@ out:
 	return err;
 }
 
-int neg_token_init_mech_type(void *context, size_t hdrlen, unsigned char tag,
-			     const void *value, size_t vlen)
+int ksmbd_neg_token_init_mech_type(void *context, size_t hdrlen,
+				   unsigned char tag, const void *value,
+				   size_t vlen)
 {
 	struct ksmbd_conn *conn = context;
 	unsigned long *oid;
@@ -361,8 +364,9 @@ fail:
 	return -EBADMSG;
 }
 
-int neg_token_init_mech_token(void *context, size_t hdrlen, unsigned char tag,
-			      const void *value, size_t vlen)
+int ksmbd_neg_token_init_mech_token(void *context, size_t hdrlen,
+				    unsigned char tag, const void *value,
+				    size_t vlen)
 {
 	struct ksmbd_conn *conn = context;
 
@@ -375,8 +379,9 @@ int neg_token_init_mech_token(void *context, size_t hdrlen, unsigned char tag,
 	return 0;
 }
 
-int neg_token_targ_resp_token(void *context, size_t hdrlen, unsigned char tag,
-			      const void *value, size_t vlen)
+int ksmbd_neg_token_targ_resp_token(void *context, size_t hdrlen,
+				    unsigned char tag, const void *value,
+				    size_t vlen)
 {
 	struct ksmbd_conn *conn = context;
 
