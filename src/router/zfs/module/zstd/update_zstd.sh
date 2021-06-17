d588a6529eecd7f67ea3ae3121792133abb4748b
BrainSlayer <s.gottschall@dd-wrt.com>
2021-06-17 06:04:21 +0000

    update zfs, add zstd 1.5.0 support: add new files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@46969 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/zfs/module/zstd/update_zstd.sh
+++ src/router/zfs/module/zstd/update_zstd.sh
@@ -7,6 +7,6 @@ wget -O /tmp/zstd.tar.gz \
 tar -C /tmp -xzf /tmp/zstd.tar.gz
 
 cp ${zstd}/lib/zstd.h module/zstd/lib/
-cp ${zstd}/lib/common/zstd_errors.h module/zstd/lib/
-${zstd}/contrib/single_file_libs/combine.sh \
+cp ${zstd}/lib/zstd_errors.h module/zstd/lib/
+scripts/single_file_libs/combine.sh \
     -r ${zstd}/lib -o module/zstd/lib/zstd.c module/zstd/zstd-in.c
