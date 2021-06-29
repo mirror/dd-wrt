7e5333e8d8cfa7260af5bc6daeb1d499c8694c62
BrainSlayer <s.gottschall@dd-wrt.com>
2021-06-29 08:45:37 +0000

    lto fun with older kernels
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@47003 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.2/lib/dump_stack.c
+++ src/linux/universal/linux-3.2/lib/dump_stack.c
@@ -6,10 +6,11 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
-void dump_stack(void)
+/*asmlinkage __visible void dump_stack(void)
 {
 	printk(KERN_NOTICE
 		"This architecture does not implement dump_stack()\n");
 }
 
 EXPORT_SYMBOL(dump_stack);
+*/
