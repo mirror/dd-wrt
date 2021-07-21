0b5399960bdeda0581660499edcaa18f47832d6f
BrainSlayer <s.gottschall@dd-wrt.com>
2021-07-21 12:45:05 +0000

    update kernel
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@47080 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.14/drivers/net/virtio_net.c
+++ src/linux/universal/linux-4.14/drivers/net/virtio_net.c
@@ -1262,7 +1262,7 @@ static int xmit_skb(struct send_queue *sq, struct sk_buff *skb)
 	if (virtio_net_hdr_from_skb(skb, &hdr->hdr,
 				    virtio_is_little_endian(vi->vdev), false,
 				    0))
-		BUG();
+		return -EPROTO;
 
 	if (vi->mergeable_rx_bufs)
 		hdr->num_buffers = 0;
@@ -2765,8 +2765,11 @@ static __maybe_unused int virtnet_restore(struct virtio_device *vdev)
 	virtnet_set_queues(vi, vi->curr_queue_pairs);
 
 	err = virtnet_cpu_notif_add(vi);
-	if (err)
+	if (err) {
+		virtnet_freeze_down(vdev);
+		remove_vq_common(vi);
 		return err;
+	}
 
 	return 0;
 }
