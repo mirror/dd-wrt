6201d4ad23915368e664d1b365f39300cd4baef9
BrainSlayer <s.gottschall@dd-wrt.com>
2021-08-13 14:16:35 +0000

    add async commit action
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@47154 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/libshutils/shutils.c
+++ src/router/libutils/libshutils/shutils.c
@@ -1273,6 +1273,20 @@ int nvram_commit(void)
 	return _nvram_commit();
 }
 
+int nvram_async_commit(void)
+{
+#if defined(HAVE_WZRHPG300NH) || defined(HAVE_WHRHPGN) || defined(HAVE_WZRHPAG300NH) || defined(HAVE_DIR825) || defined(HAVE_TEW632BRP) || defined(HAVE_TG2521) || defined(HAVE_WR1043)  || defined(HAVE_WRT400) || defined(HAVE_WZRHPAG300NH) || defined(HAVE_WZRG450) || defined(HAVE_DANUBE) || defined(HAVE_WR741) || defined(HAVE_NORTHSTAR) || defined(HAVE_DIR615I) || defined(HAVE_WDR4900) || defined(HAVE_VENTANA) || defined(HAVE_UBNTM)
+	eval("ledtool", "1");
+#elif HAVE_LSX
+	//nothing
+#elif HAVE_XSCALE
+	//nothing
+#else
+	eval("ledtool", "1");
+#endif
+	eval("async_commit");
+}
+
 int writeproc(char *path, char *value)
 {
 	int fd;
