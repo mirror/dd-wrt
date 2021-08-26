6af8ecd4bb934bc3fb7d80c2d593e26f80fa8979
BrainSlayer <s.gottschall@dd-wrt.com>
2021-08-26 15:49:06 +0000

    wg fixes by egc
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@47259 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/eop-tunnel/eop-tunnel-raip.sh
+++ src/router/eop-tunnel/eop-tunnel-raip.sh
@@ -96,7 +96,7 @@ fi
 if [ ! -z "$($nv get oet${i}_pbr | sed '/^[[:blank:]]*#/d')" ]; then
 	#add default routes
 	ip route add 0.0.0.0/1 dev oet${i} table $TID >/dev/null 2>&1
-	ip route add 128.0.0.0/1 dev oet${i} table $TID>/dev/null 2>&1
+	ip route add 128.0.0.0/1 dev oet${i} table $TID >/dev/null 2>&1
 fi
 # check how many tunnels, for last active tunnel copy local routes to all existing PBR tables
 #tunnels=$(nvram get oet_tunnels)
--- src/router/wireguard/wireguard-state.sh
+++ src/router/wireguard/wireguard-state.sh
@@ -4,7 +4,9 @@ i=$1
 p=$2
 
 #VAR=$(/usr/bin/wg | sed "/oet${i}/,/interface/!d;/interface/d" | sed "/${p//\//\\\/}/,/peer/!d;/peer/d;/preshared/d;/endpoint/d;/allowed/d;/persistent/d")
-VAR=$(/usr/bin/wg | sed "/${p//\//\\\/}/,/peer/!d" | grep -e 'latest' -e 'transfer' -e 'endpoint' )
+# this rule is for users who use peers with the same peerkey so we have to parse the tunnel interface
+VAR=$(/usr/bin/wg | sed "/oet${i}/,/interface/!d;/interface/d" | sed "/${p//\//\\\/}/,/peer/!d;/peer/d;/preshared/d;/allowed/d;/persistent/d")
+#VAR=$(/usr/bin/wg | sed "/${p//\//\\\/}/,/peer/!d" | grep -e 'latest' -e 'transfer' -e 'endpoint' )
 if [[ ! -z "$VAR" ]]; then
 	/bin/echo "${VAR//$'\n'/\\n}" | tr '\n' ' '
 fi
