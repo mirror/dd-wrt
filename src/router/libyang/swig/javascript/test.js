66d3144cbd1df4e4907d9458a55552cdc667ef80
BrainSlayer <s.gottschall@dd-wrt.com>
2021-08-31 09:36:35 +0000

    update libyang: remove old files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@47295 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libyang/swig/javascript/test.js
+++
@@ -1,6 +0,0 @@
-var yang = require("./index")
-
-const ctx = new yang.Context("./files", 0);
-var module = ctx.parse_module_path("./files/b.yang", yang.LYS_IN_YANG);
-
-console.log(module.name());
--- src/router/libyang/swig/swig_base/javascript_base.i
+++
@@ -1,4 +0,0 @@
-%module javascript_base
-
-%include "../swig_base/base.i"
-%include "../swig_base/libyangEnums.i"
