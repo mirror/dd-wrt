fe6d60932f4baf267ed5279513bc3ed5e9c4e81b
BrainSlayer <s.gottschall@dd-wrt.com>
2021-09-04 07:13:43 +0000

    ubuntu
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@47315 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/ejs.c
+++ src/router/httpd/visuals/ejs.c
@@ -1032,6 +1032,22 @@ EJ_VISIBLE void ej_show_styles(webs_t wp, int argc, char_t ** argv)
 	return;
 }
 
+EJ_VISIBLE void ej_show_stylus(webs_t wp, int argc, char_t ** argv)
+{
+	/* todo, read dir content and generate this */
+	char buf[128];
+
+	websWrite(wp, "<option value=\"off\" %s>Off</option>\n", nvram_match("stylus", "off") ? "selected=\"selected\"" : "", tran_string(buf, "share.off"));
+	websWrite(wp, "<option value=\"dracula\" %s>Dracula</option>\n", off, nvram_match("stylus", "dracula") ? "selected=\"selected\"" : "");
+	websWrite(wp, "<option value=\"material\" %s>Material</option>\n", off, nvram_match("stylus", "material") ? "selected=\"selected\"" : "");
+	websWrite(wp, "<option value=\"material_dark\" %s>Material Dark</option>\n", off, nvram_match("stylus", "material_dark") ? "selected=\"selected\"" : "");
+	websWrite(wp, "<option value=\"solarized\" %s>Solarized</option>\n", off, nvram_match("stylus", "solarized") ? "selected=\"selected\"" : "");
+	websWrite(wp, "<option value=\"the_matrix\" %s>The Matrix</option>\n", off, nvram_match("stylus", "the_matrix") ? "selected=\"selected\"" : "");
+	websWrite(wp, "<option value=\"twilight\" %s>Twilight</option>\n", off, nvram_match("stylus", "twilight") ? "selected=\"selected\"" : "");
+	websWrite(wp, "<option value=\"ubuntu\" %s>Ubuntu</option>\n", off, nvram_match("stylus", "ubuntu") ? "selected=\"selected\"" : "");
+	return;
+}
+
 #include "../webs.h"
 extern const websRomPageIndexType websRomPageIndex[];
 
