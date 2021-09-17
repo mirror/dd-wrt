7dbc1f13fba8400fd1bf5a03678bc5741a7cb5c3
BrainSlayer <s.gottschall@dd-wrt.com>
2021-09-17 03:11:44 +0000

    another trivial fix. i was maybe too tired yesterday
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@47461 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/openssl.mk
+++ src/router/rules/openssl.mk
@@ -74,8 +74,8 @@ openssl-apps-static:
 
 openssl-install:
 #ifeq ($(CONFIG_MADWIFI),y)
-	-install -D openssl/libcrypto.so.1 $(INSTALLDIR)/openssl/usr/lib/libcrypto.so.1
-	-install -D openssl/libssl.so.1 $(INSTALLDIR)/openssl/usr/lib/libssl.so.1
+	-install -D openssl/libcrypto.so.1.1 $(INSTALLDIR)/openssl/usr/lib/libcrypto.so.1.1
+	-install -D openssl/libssl.so.1.1 $(INSTALLDIR)/openssl/usr/lib/libssl.so.1.1
 #endif
 #ifneq ($(ARCH),mipsel)
 #	-install -D openssl/apps/openssl $(INSTALLDIR)/openssl/usr/sbin/openssl
