5f0164c6b2d34a8fc5a7cdc1ec96463f8a5932c4
BrainSlayer <s.gottschall@dd-wrt.com>
2021-09-21 07:50:10 +0000

    remove obsolete title (old wrt devices only)
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@47477 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/switch.c
+++ src/router/httpd/visuals/switch.c
@@ -167,7 +167,7 @@ EJ_VISIBLE void ej_port_vlan_table(webs_t wp, int argc, char_t ** argv)
 	websWrite(wp, "<tr>\n");
 	websWrite(wp, "<th rowspan=\"2\"><script type=\"text/javascript\">Capture(vlan.legend)</script></th>\n");
 	websWrite(wp, "<th colspan=\"5\"><script type=\"text/javascript\">Capture(share.port)</script></th>\n");
-	websWrite(wp, "<th rowspan=\"2\"><script type=\"text/javascript\">Capture(vlan.bridge)</script></th>\n");
+	websWrite(wp, "<th rowspan=\"2\">&nbsp;</th>\n");
 	websWrite(wp, "</tr>\n");
 	websWrite(wp, "<tr>\n");
 	websWrite(wp, "<th>W</th>\n");
