0c48272f01b13441a4d3135a2e89c98d61f20b3b
BrainSlayer <s.gottschall@dd-wrt.com>
2021-10-13 09:35:03 +0000

    keep style_ie, its identical
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@47540 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/base.c
+++ src/router/httpd/modules/base.c
@@ -2806,8 +2806,8 @@ static struct mime_handler mime_handlers[] = {
 	{ "**.html", "text/html", no_cache, NULL, do_ej, NULL, NO_HEADER, IGNORE_OPTIONS },
 
 #endif
-/*#ifdef HAVE_ROUTERSTYLE
-	{ "style/blue/style.css", "text/css", do_cache, NULL, do_stylecss, NULL, SEND_HEADER, IGNORE_OPTIONS },
+#ifdef HAVE_ROUTERSTYLE
+/*	{ "style/blue/style.css", "text/css", do_cache, NULL, do_stylecss, NULL, SEND_HEADER, IGNORE_OPTIONS },
 	{ "style/cyan/style.css", "text/css", do_cache, NULL, do_stylecss, NULL, SEND_HEADER, IGNORE_OPTIONS },
 	{ "style/elegant/style.css", "text/css", do_cache, NULL, do_stylecss, NULL, SEND_HEADER, IGNORE_OPTIONS },
 	{ "style/elegant/fresh.css", "text/css", do_cache, NULL, do_ej, NULL, NO_HEADER, IGNORE_OPTIONS },
@@ -2816,6 +2816,7 @@ static struct mime_handler mime_handlers[] = {
 	{ "style/orange/style.css", "text/css", do_cache, NULL, do_stylecss, NULL, SEND_HEADER, IGNORE_OPTIONS },
 	{ "style/red/style.css", "text/css", do_cache, NULL, do_stylecss, NULL, SEND_HEADER, IGNORE_OPTIONS },
 	{ "style/yellow/style.css", "text/css", do_cache, NULL, do_stylecss, NULL, SEND_HEADER, IGNORE_OPTIONS },
+*/
 	{ "style/blue/style_ie.css", "text/css", do_cache, NULL, do_stylecss_ie, NULL, SEND_HEADER, IGNORE_OPTIONS },
 	{ "style/cyan/style_ie.css", "text/css", do_cache, NULL, do_stylecss_ie, NULL, SEND_HEADER, IGNORE_OPTIONS },
 	{ "style/elegant/style_ie.css", "text/css", do_cache, NULL, do_stylecss_ie, NULL, SEND_HEADER, IGNORE_OPTIONS },
@@ -2823,7 +2824,7 @@ static struct mime_handler mime_handlers[] = {
 	{ "style/orange/style_ie.css", "text/css", do_cache, NULL, do_stylecss_ie, NULL, SEND_HEADER, IGNORE_OPTIONS },
 	{ "style/red/style_ie.css", "text/css", do_cache, NULL, do_stylecss_ie, NULL, SEND_HEADER, IGNORE_OPTIONS },
 	{ "style/yellow/style_ie.css", "text/css", do_cache, NULL, do_stylecss_ie, NULL, SEND_HEADER, IGNORE_OPTIONS },
-#endif*/
+#endif
 #ifdef HAVE_REGISTER
 	{ "style/logo.png", "image/png", NULL, NULL, do_trial_logo, NULL, NO_HEADER, IGNORE_OPTIONS },
 #endif
