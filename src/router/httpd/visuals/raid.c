40421ff38bb76fd50b1993c1b677e55911f13497
BrainSlayer <s.gottschall@dd-wrt.com>
2021-10-19 06:30:30 +0000

    fix bad html code
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@47570 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/raid.c
+++ src/router/httpd/visuals/raid.c
@@ -608,10 +608,10 @@ EJ_VISIBLE void ej_show_raid(webs_t wp, int argc, char_t ** argv)
 			char *fs = getfsname(drive);
 			if (!fs)
 				continue;
-			websWrite(wp, "<input type=\"hidden\" name=\"drivename%d\" value=\"%s\"\n", idx, &drive[5]);
+			websWrite(wp, "<input type=\"hidden\" name=\"drivename%d\" value=\"%s\">\n", idx, &drive[5]);
 		}
 	}
-	websWrite(wp, "<input type=\"hidden\" name=\"drivecount\" id=\"drivecount\" value=\"%d\"\n", idx);
+	websWrite(wp, "<input type=\"hidden\" name=\"drivecount\" id=\"drivecount\" value=\"%d\">\n", idx);
 	websWrite(wp, "</fieldset>\n");
 }
 
