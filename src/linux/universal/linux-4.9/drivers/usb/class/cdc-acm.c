f0b503df098d670deb2cfa1ba78ea5e389dc7332
BrainSlayer <s.gottschall@dd-wrt.com>
2021-10-19 08:14:58 +0000

    update kernel
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@47574 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.9/drivers/usb/class/cdc-acm.c
+++ src/linux/universal/linux-4.9/drivers/usb/class/cdc-acm.c
@@ -372,6 +372,9 @@ static void acm_ctrl_irq(struct urb *urb)
 			acm->iocount.overrun++;
 		spin_unlock(&acm->read_lock);
 
+		if (newctrl & ACM_CTRL_BRK)
+			tty_flip_buffer_push(&acm->port);
+
 		if (difference)
 			wake_up_all(&acm->wioctl);
 
@@ -431,11 +434,16 @@ static int acm_submit_read_urbs(struct acm *acm, gfp_t mem_flags)
 
 static void acm_process_read_urb(struct acm *acm, struct urb *urb)
 {
+	unsigned long flags;
+
 	if (!urb->actual_length)
 		return;
 
+	spin_lock_irqsave(&acm->read_lock, flags);
 	tty_insert_flip_string(&acm->port, urb->transfer_buffer,
 			urb->actual_length);
+	spin_unlock_irqrestore(&acm->read_lock, flags);
+
 	tty_flip_buffer_push(&acm->port);
 }
 
