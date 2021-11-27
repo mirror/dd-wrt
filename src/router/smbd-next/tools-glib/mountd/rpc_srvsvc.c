188f6242618738cafc84fb1db587174f350bd102
BrainSlayer <s.gottschall@dd-wrt.com>
2021-11-27 09:17:18 +0000

    fix for file transfer stuck
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@47688 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/smbd-next/tools-glib/mountd/rpc_srvsvc.c
+++ src/router/smbd-next/tools-glib/mountd/rpc_srvsvc.c
@@ -169,8 +169,11 @@ static int srvsvc_share_get_info_invoke(struct ksmbd_rpc_pipe *pipe,
 {
 	struct ksmbd_share *share;
 	int ret;
+	gchar *share_name;
 
-	share = shm_lookup_share(STR_VAL(hdr->share_name));
+	share_name = g_ascii_strdown(STR_VAL(hdr->share_name),
+			strlen(STR_VAL(hdr->share_name)));
+	share = shm_lookup_share(share_name);
 	if (!share)
 		return 0;
 
@@ -188,9 +191,12 @@ static int srvsvc_share_get_info_invoke(struct ksmbd_rpc_pipe *pipe,
 	}
 
 	if (ret != 0) {
+		gchar *server_name = g_ascii_strdown(STR_VAL(hdr->server_name),
+				strlen(STR_VAL(hdr->server_name)));
+
 		ret = shm_lookup_hosts_map(share,
 					   KSMBD_SHARE_HOSTS_DENY_MAP,
-					   STR_VAL(hdr->server_name));
+					   server_name);
 		if (ret == 0) {
 			put_ksmbd_share(share);
 			return 0;
