b9be2b998c0051652c1768071af224fca3191e0a
BrainSlayer <s.gottschall@dd-wrt.com>
2021-12-14 07:51:36 +0000

    public config for speedchecker
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@47842 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/speedchecker.mk
+++ src/router/rules/speedchecker.mk
@@ -6,8 +6,8 @@ speedchecker-clean:
 	-make DDWRT=1 -C speedchecker clean
 
 speedchecker-install:
-	install -D speedchecker/configs/speedchecker.nvramconfig $(INSTALLDIR)/speedchecker/etc/config/speedchecker.nvramconfig
+	install -D speedchecker-configs/configs/speedchecker.nvramconfig $(INSTALLDIR)/speedchecker/etc/config/speedchecker.nvramconfig
 	install -D speedchecker/scc $(INSTALLDIR)/speedchecker/usr/sbin/scc
 	install -D speedchecker/scripts/ping $(INSTALLDIR)/speedchecker/usr/bin/speedchecker/ping
 	install -D speedchecker/scripts/traceroute $(INSTALLDIR)/speedchecker/usr/bin/speedchecker/traceroute
-	install -D speedchecker/configs/01-sc.shownf $(INSTALLDIR)/speedchecker/etc/config/01-sc.shownf
+	install -D speedchecker-configs/configs/01-sc.shownf $(INSTALLDIR)/speedchecker/etc/config/01-sc.shownf
