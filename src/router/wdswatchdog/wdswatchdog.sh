ce0d31d5344bbc4e233ca78dd2f38185c1c59ffe
BrainSlayer <s.gottschall@dd-wrt.com>
2021-12-19 18:40:25 +0000

    do not show successfull events
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@47896 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/wdswatchdog/wdswatchdog.sh
+++ src/router/wdswatchdog/wdswatchdog.sh
@@ -17,7 +17,7 @@ do
 		do
 			if ping -c 1 -W $WDS_WATCHDOG_TIMEOUT $ip > /tmp/null
 			then
-				logger -t "$TAG" "$ip ok"
+				#logger -t "$TAG" "$ip ok"
 			else
 				logger -t "$TAG" "$ip dropped (1/3)"
 				DROPPED=true
@@ -31,7 +31,7 @@ do
 			do
 				if ping -c 1 -W $WDS_WATCHDOG_TIMEOUT $ip > /tmp/null
 				then
-					logger -t "$TAG" "$ip ok"
+					#logger -t "$TAG" "$ip ok"
 				else
 					logger -t "$TAG" "$ip dropped (2/3)"
 					DROPPED=true
@@ -46,7 +46,7 @@ do
 			do
 				if ping -c 1 -W $WDS_WATCHDOG_TIMEOUT $ip > /tmp/null
 				then
-					logger -t "$TAG" "$ip ok"
+					#logger -t "$TAG" "$ip ok"
 				else
 					logger -t "$TAG" "$ip dropped (3/3), Restarting Router"
 					/sbin/reboot &
@@ -64,7 +64,7 @@ do
 			if ping -c 1 -W $WDS_WATCHDOG_TIMEOUT $ip > /tmp/null
 			then
 				DROPPED=false
-				logger -t "$TAG" "$ip ok"
+				#logger -t "$TAG" "$ip ok"
 			else
 				logger -t "$TAG" "$ip dropped (1/3)"
 			fi 
@@ -78,7 +78,7 @@ do
 				if ping -c 1 -W $WDS_WATCHDOG_TIMEOUT $ip > /tmp/null
 				then
 					DROPPED=false
-					logger -t "$TAG" "$ip ok"
+					#logger -t "$TAG" "$ip ok"
 				else
 					logger -t "$TAG" "$ip dropped (2/3)"
 				fi 
@@ -93,7 +93,7 @@ do
 				if ping -c 1 -W $WDS_WATCHDOG_TIMEOUT $ip > /tmp/null
 				then
 					DROPPED=false
-					logger -t "$TAG" "$ip ok"
+					#logger -t "$TAG" "$ip ok"
 				else
 					logger -t "$TAG" "$ip dropped (3/3)"          
 				fi 
