a6ed2acb0bed9c22e4f4b1e269d974e64dca77c9
BrainSlayer <s.gottschall@dd-wrt.com>
2021-12-30 11:48:12 +0000

    try to exclude c1 from that path
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@47947 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4/arch/arm/mach-brcm-hnd/gpio.c
+++ src/linux/universal/linux-4.4/arch/arm/mach-brcm-hnd/gpio.c
@@ -220,7 +220,7 @@ static int __init gpio_init(void)
 		isac66 = 1;
 	}
 
-	if (nvram_match("model","RT-AC68U")) {
+	if (nvram_match("model","RT-AC68U") && !nvram_match("boardrev", "0x1103")) {
 		printk(KERN_EMERG "Asus-RT-AC68U init\n");
 		isac68 = 1;
 	} else if ((boardnum != 24) && nvram_match("boardtype", "0x0646") && (nvram_match("boardrev", "0x1100"))) {
