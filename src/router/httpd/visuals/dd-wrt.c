4824b509c2e02b6c436e2ada865216d251e43c54
BrainSlayer <s.gottschall@dd-wrt.com>
2022-01-07 04:47:30 +0000

    add delete button
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@47987 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/dd-wrt.c
+++ src/router/httpd/visuals/dd-wrt.c
@@ -800,6 +800,7 @@ EJ_VISIBLE void ej_show_staticleases(webs_t wp, int argc, char_t ** argv)
 		websWrite(wp,
 			  "<td><input name=\"lease%d_time\" value=\"%s\" size=\"10\" maxlength=\"10\" class=\"num\" onblur=\"valid_name(this,share.time,SPACE_NO)\" /><script type=\"text/javascript\">Capture(share.minutes)</script></td></tr>\n",
 			  i, sep != NULL ? sep : "");
+		websWrite(wp,"<td><script type=\"text/javascript\">\n//<![CDATA[\n document.write(\"<input class=\\\"button\\\" type=\\\"button\\\" value=\\\"\" + sbutton.del + \"\\\" onclick=\\\"lease_del_submit(this.form,%d)\\\" />\");\n//]]>\n</script>\n</td>", i);
 	}
 	debug_free(originalpointer);
 	return;
