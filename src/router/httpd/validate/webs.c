47ae85a9c0a7ec383c7a67534fe207d972dd0044
BrainSlayer <s.gottschall@dd-wrt.com>
2022-01-07 05:01:22 +0000

    add del function
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@47991 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/validate/webs.c
+++ src/router/httpd/validate/webs.c
@@ -2690,9 +2690,27 @@ void filter_add(webs_t wp)
 	nvram_set(filter, num);
 }
 
-void lease_remove(webs_t wp)
+void lease_del(webs_t wp)
 {
-	macro_rem("static_leasenum", "static_leases");
+	int todel = websGetVari(wp, "lease_del_value", 0);
+	char *wordlist = nvram_safe_get("static_leases");
+	char *next;
+	char word[256];
+	char *target = malloc(strlen(wordlist) + 1);
+	memset(target, 0, strlen(wordlist) + 1);
+	int idx = 0;
+	foreach(word, wordlist, next) {
+		if (idx)
+			strcat(target, " ");
+		if (idx == todel)
+			continue;
+		idx++;
+		strcat(target, word);
+	}
+	int num = nvram_geti("static_leasenum");
+	if (num)
+		num--;
+	nvram_seti("static_leasenum", num);
 }
 
 void lease_add(webs_t wp)
@@ -3166,7 +3184,8 @@ static char *vapsettings[] = {
 	"radius_retry", "radmacpassword", "radmactype", "rate", "rateset", "reg_mode", "regdomain", "regulatory", "rifs_advert", "rts", "rts rtsvalue", "rtsvalue", "rx_amsdu_in_ampdu", "rxantenna", "rxchain",
 	"rxchain_pwrsave_enable", "sae_key", "scanlist", "security_mode", "security_mode_akm", "shortgi", "shortslot", "smps", "ssid", "sta_retry_time", "stay", "stbc_rx", "stbc_tx", "strikes", "subf", "tkip", "tls",
 	"tls8021xaddopt", "tls8021xanon", "tls8021xca", "tls8021xkeyxchng", "tls8021xpasswd", "tls8021xpem", "tls8021xphase2", "tls8021xprv", "tls8021xuser", "tpc_db", "ttls", "ttls8021xaddopt", "ttls8021xanon",
-	"ttls8021xca", "ttls8021xpasswd", "ttls8021xphase2", "ttls8021xuser", "turbo_qam", "nitro_qam", "fwtype", "txant", "txantenna", "txbf", "txbf_bfe_cap", "txbf_bfr_cap", "txbf_imp", "txchain", "txchain_pwrsave_enable", "txpwr",
+	"ttls8021xca", "ttls8021xpasswd", "ttls8021xphase2", "ttls8021xuser", "turbo_qam", "nitro_qam", "fwtype", "txant", "txantenna", "txbf", "txbf_bfe_cap", "txbf_bfr_cap", "txbf_imp", "txchain",
+	"txchain_pwrsave_enable", "txpwr",
 	"txpwrdbm", "txpwrusr", "txq", "uapsd", "d_lowack", "ldpc", "unit", "vifs", "vlan_prio_mode", "wchannel", "wds", "wds0", "wds0_if", "wds1", "wds10_desc", "wds10_enable", "wds10_hwaddr", "wds10_if",
 	"wds10_ipaddr",
 	"wds10_netmask", "wds10_ospf", "wds1_desc", "wds1_enable", "wds1_hwaddr", "wds1_if", "wds1_ipaddr", "wds1_netmask", "wds1_ospf", "wds2", "wds2_desc", "wds2_enable", "wds2_hwaddr", "wds2_if", "wds2_ipaddr",
