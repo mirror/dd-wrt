4bafd8baa9393c8f3c0d61019556de3d70056bf6
BrainSlayer <s.gottschall@dd-wrt.com>
2022-01-12 09:29:12 +0000

    update softether: remove old files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@48099 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/softether/.ci/memory-leak-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -eux
-
-echo -n | ./build/vpntest s | grep -Fq 'NO MEMORY LEAKS'
-echo -n | ./build/vpntest c | grep -Fq 'NO MEMORY LEAKS'
-echo -n | ./build/vpntest b | grep -Fq 'NO MEMORY LEAKS'
