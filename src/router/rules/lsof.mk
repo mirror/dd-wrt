51b792c5c24225be1427679bf2addc12b4e78398
BrainSlayer <s.gottschall@dd-wrt.com>
2022-01-13 02:54:51 +0000

    fix ranlib/ar
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@48118 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/lsof.mk
+++ src/router/rules/lsof.mk
@@ -3,10 +3,10 @@ lsof-configure:
 		LINUX_CLIB="-DGLIBCV=2" \
 		LSOF_CC="$(CC)" \
 		LSOF_VSTR="$(KERNELRELEASE)" \
-		LSOF_CFGF="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF -D_GNU_SOURCE -I$(TOP)/lsof/librpc" \
-		LSOF_CFGL="-ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF -L$(TOP)/lsof/librpc -lrpc" \
-		LSOF_AR="$(CROSS_COMPILE)ar cr" \
-		LSOF_RANLIB="$(CROSS_COMPILE)ranlib" \
+		LSOF_CFGF="$(COPTS) $(MIPS16_OPT) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF -D_GNU_SOURCE -I$(TOP)/lsof/librpc" \
+		LSOF_CFGL="$(COPTS) $(MIPS16_OPT) $(LTO) $(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF $(TOP)/lsof/librpc/librpc.a" \
+		LSOF_AR="$(CROSS_COMPILE)gcc-ar cr $(LTOPLUGIN)" \
+		LSOF_RANLIB="$(CROSS_COMPILE)gcc-ranlib $(LTOPLUGIN)" \
 		./Configure -n linux
 
 #	cd lsof && ./configure --prefix=/usr --libdir=/usr/lib --host=$(ARCH)-linux CC="$(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF"
