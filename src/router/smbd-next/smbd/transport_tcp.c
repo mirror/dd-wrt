5f845de6c3544a6fd428b773238295ada35763e3
BrainSlayer <s.gottschall@dd-wrt.com>
2022-01-16 18:01:38 +0000

    merge from git
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@48136 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/smbd-next/smbd/transport_tcp.c
+++ src/router/smbd-next/smbd/transport_tcp.c
@@ -474,7 +474,7 @@ static int create_socket(struct interface *iface)
 				  &ksmbd_socket);
 		if (ret) {
 			pr_err("Can't create socket for ipv4: %d\n", ret);
-			goto out_error;
+			goto out_clear;
 		}
 
 		sin.sin_family = PF_INET;
@@ -543,6 +543,7 @@ static int create_socket(struct interface *iface)
 
 out_error:
 	tcp_destroy_socket(ksmbd_socket);
+out_clear:
 	iface->ksmbd_socket = NULL;
 	return ret;
 }
