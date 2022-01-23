6bb24dd259adc4229c7d6c5cc73fe5a7d91d0c44
BrainSlayer <s.gottschall@dd-wrt.com>
2022-01-23 19:26:51 +0000

    works only in ap or sta mode
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@48148 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/dd-wrt.c
+++ src/router/httpd/visuals/dd-wrt.c
@@ -3778,6 +3778,7 @@ static void internal_ej_show_wireless_single(webs_t wp, char *prefix)
 		showRadio(wp, "wl_basic.turboqam", wl_turboqam);
 	}
 #ifdef HAVE_BCMMODERN
+	if (nvram_match(wl_mode,"ap") || nvram_match(wl_mode,"sta"))
 	{
 		char wl_dwds;
 		sprintf(wl_dwds,"%s_dwds", prefix);
