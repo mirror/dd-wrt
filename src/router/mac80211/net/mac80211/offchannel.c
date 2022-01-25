6e1787e66de9529c4eb7141ec3d0d70e34cd99c7
BrainSlayer <s.gottschall@dd-wrt.com>
2022-01-25 08:02:06 +0000

    modify hostapds resp frames for stanum support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@48195 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/mac80211/net/mac80211/offchannel.c
+++ src/router/mac80211/net/mac80211/offchannel.c
@@ -761,6 +761,32 @@ int ieee80211_cancel_remain_on_channel(struct wiphy *wiphy,
 	return ieee80211_cancel_roc(local, cookie, false);
 }
 
+void ieee80211_mgmt_mod_params(struct wiphy *wiphy, struct wireless_dev *wdev,
+		      struct cfg80211_mgmt_tx_params *params, u64 *cookie) {
+
+	struct ieee80211_sub_if_data *sdata = IEEE80211_WDEV_TO_SUB_IF(wdev);
+	const struct ieee80211_mgmt *mgmt = (void *)params->buf;
+	int extraie = 0;
+	if (ieee80211_is_probe_resp(mgmt->frame_control)) {
+		extraie = sizeof(struct ieee80211_brcm_ie) + sizeof(struct ieee80211_mtik_ie);
+	}
+	if (extraie) {
+		u8 *oldbuf = params->buf;
+		params->buf = kmalloc(params->len + extraie, GFP_KERNEL);
+		memcpy(params->buf, oldbuf, params->len);
+		ieee80211_add_brcm_ie(params->buf + params->len, sta_count(sdata));
+		ieee80211_add_mtik_ie(params->buf + params->len + sizeof(struct ieee80211_brcm_ie), 0);
+		params->len += extraie;
+	}
+}
+void ieee80211_mgmt_mod_params_release(struct wiphy *wiphy, struct wireless_dev *wdev,
+		      struct cfg80211_mgmt_tx_params *params, u64 *cookie) {
+
+	const struct ieee80211_mgmt *mgmt = (void *)params->buf;
+	if (ieee80211_is_probe_resp(mgmt->frame_control)) {
+		kfree(params->buf);
+	}
+}
 int ieee80211_mgmt_tx(struct wiphy *wiphy, struct wireless_dev *wdev,
 		      struct cfg80211_mgmt_tx_params *params, u64 *cookie)
 {
@@ -865,7 +891,7 @@ int ieee80211_mgmt_tx(struct wiphy *wiphy, struct wireless_dev *wdev,
 		ret = -EBUSY;
 		goto out_unlock;
 	}
-
+	
 	skb = dev_alloc_skb(local->hw.extra_tx_headroom + params->len);
 	if (!skb) {
 		ret = -ENOMEM;
@@ -874,7 +900,6 @@ int ieee80211_mgmt_tx(struct wiphy *wiphy, struct wireless_dev *wdev,
 	skb_reserve(skb, local->hw.extra_tx_headroom);
 
 	data = skb_put_data(skb, params->buf, params->len);
-
 	/* Update CSA counters */
 	if (sdata->vif.csa_active &&
 	    (sdata->vif.type == NL80211_IFTYPE_AP ||
