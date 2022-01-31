821a356a978b871f829d12097ece697e5970cdfb
BrainSlayer <s.gottschall@dd-wrt.com>
2022-01-31 09:00:56 +0000

    update plex: add new files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@48247 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/plex/aarch64/usr/lib/plexmediaserver/Resources/Plug-ins-989df2310/Framework.bundle/Contents/Resources/Versions/2/Python/Framework/components/__init__.py
@@ -0,0 +1,19 @@
+#
+#  Plex Extension Framework
+#  Copyright (C) 2008-2012 Plex, Inc. (James Clarke, Elan Feingold). All Rights Reserved.
+#
+
+import Framework
+
+from base			import BaseComponent
+from data           import Data
+from networking     import Networking
+from runtime        import Runtime
+from storage        import Storage
+from caching        import Caching
+from localization   import Localization
+from messaging      import Messaging
+from debugging      import Debugging
+from services       import Services
+from myplex         import MyPlex
+from notifications  import Notifications
