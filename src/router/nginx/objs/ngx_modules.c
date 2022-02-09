d9af0d17bd1940cd0e944e92cb05266623265d0d
BrainSlayer <s.gottschall@dd-wrt.com>
2022-02-09 15:08:04 +0000

    update nginx: add new files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@48307 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/nginx/objs/ngx_modules.c
@@ -0,0 +1,240 @@
+
+#include <ngx_config.h>
+#include <ngx_core.h>
+
+
+
+extern ngx_module_t  ngx_core_module;
+extern ngx_module_t  ngx_errlog_module;
+extern ngx_module_t  ngx_conf_module;
+extern ngx_module_t  ngx_openssl_module;
+extern ngx_module_t  ngx_regex_module;
+extern ngx_module_t  ngx_events_module;
+extern ngx_module_t  ngx_event_core_module;
+extern ngx_module_t  ngx_epoll_module;
+extern ngx_module_t  ngx_rtmp_module;
+extern ngx_module_t  ngx_rtmp_core_module;
+extern ngx_module_t  ngx_rtmp_cmd_module;
+extern ngx_module_t  ngx_rtmp_codec_module;
+extern ngx_module_t  ngx_rtmp_access_module;
+extern ngx_module_t  ngx_rtmp_record_module;
+extern ngx_module_t  ngx_rtmp_live_module;
+extern ngx_module_t  ngx_rtmp_play_module;
+extern ngx_module_t  ngx_rtmp_flv_module;
+extern ngx_module_t  ngx_rtmp_mp4_module;
+extern ngx_module_t  ngx_rtmp_netcall_module;
+extern ngx_module_t  ngx_rtmp_relay_module;
+extern ngx_module_t  ngx_rtmp_exec_module;
+extern ngx_module_t  ngx_rtmp_auto_push_module;
+extern ngx_module_t  ngx_rtmp_auto_push_index_module;
+extern ngx_module_t  ngx_rtmp_notify_module;
+extern ngx_module_t  ngx_rtmp_log_module;
+extern ngx_module_t  ngx_rtmp_limit_module;
+extern ngx_module_t  ngx_rtmp_hls_module;
+extern ngx_module_t  ngx_rtmp_dash_module;
+extern ngx_module_t  ngx_http_module;
+extern ngx_module_t  ngx_http_core_module;
+extern ngx_module_t  ngx_http_log_module;
+extern ngx_module_t  ngx_http_upstream_module;
+extern ngx_module_t  ngx_http_static_module;
+extern ngx_module_t  ngx_http_autoindex_module;
+extern ngx_module_t  ngx_http_index_module;
+extern ngx_module_t  ngx_http_mirror_module;
+extern ngx_module_t  ngx_http_try_files_module;
+extern ngx_module_t  ngx_http_auth_basic_module;
+extern ngx_module_t  ngx_http_access_module;
+extern ngx_module_t  ngx_http_limit_conn_module;
+extern ngx_module_t  ngx_http_limit_req_module;
+extern ngx_module_t  ngx_http_geo_module;
+extern ngx_module_t  ngx_http_map_module;
+extern ngx_module_t  ngx_http_split_clients_module;
+extern ngx_module_t  ngx_http_referer_module;
+extern ngx_module_t  ngx_http_rewrite_module;
+extern ngx_module_t  ngx_http_ssl_module;
+extern ngx_module_t  ngx_http_proxy_module;
+extern ngx_module_t  ngx_http_fastcgi_module;
+extern ngx_module_t  ngx_http_uwsgi_module;
+extern ngx_module_t  ngx_http_scgi_module;
+extern ngx_module_t  ngx_http_memcached_module;
+extern ngx_module_t  ngx_http_empty_gif_module;
+extern ngx_module_t  ngx_http_browser_module;
+extern ngx_module_t  ngx_http_upstream_hash_module;
+extern ngx_module_t  ngx_http_upstream_ip_hash_module;
+extern ngx_module_t  ngx_http_upstream_least_conn_module;
+extern ngx_module_t  ngx_http_upstream_random_module;
+extern ngx_module_t  ngx_http_upstream_keepalive_module;
+extern ngx_module_t  ngx_http_upstream_zone_module;
+extern ngx_module_t  ngx_rtmp_stat_module;
+extern ngx_module_t  ngx_rtmp_control_module;
+extern ngx_module_t  ngx_http_write_filter_module;
+extern ngx_module_t  ngx_http_header_filter_module;
+extern ngx_module_t  ngx_http_chunked_filter_module;
+extern ngx_module_t  ngx_http_range_header_filter_module;
+extern ngx_module_t  ngx_http_gzip_filter_module;
+extern ngx_module_t  ngx_http_postpone_filter_module;
+extern ngx_module_t  ngx_http_ssi_filter_module;
+extern ngx_module_t  ngx_http_charset_filter_module;
+extern ngx_module_t  ngx_http_userid_filter_module;
+extern ngx_module_t  ngx_http_headers_filter_module;
+extern ngx_module_t  ngx_http_copy_filter_module;
+extern ngx_module_t  ngx_http_range_body_filter_module;
+extern ngx_module_t  ngx_http_not_modified_filter_module;
+
+ngx_module_t *ngx_modules[] = {
+    &ngx_core_module,
+    &ngx_errlog_module,
+    &ngx_conf_module,
+    &ngx_openssl_module,
+    &ngx_regex_module,
+    &ngx_events_module,
+    &ngx_event_core_module,
+    &ngx_epoll_module,
+    &ngx_rtmp_module,
+    &ngx_rtmp_core_module,
+    &ngx_rtmp_cmd_module,
+    &ngx_rtmp_codec_module,
+    &ngx_rtmp_access_module,
+    &ngx_rtmp_record_module,
+    &ngx_rtmp_live_module,
+    &ngx_rtmp_play_module,
+    &ngx_rtmp_flv_module,
+    &ngx_rtmp_mp4_module,
+    &ngx_rtmp_netcall_module,
+    &ngx_rtmp_relay_module,
+    &ngx_rtmp_exec_module,
+    &ngx_rtmp_auto_push_module,
+    &ngx_rtmp_auto_push_index_module,
+    &ngx_rtmp_notify_module,
+    &ngx_rtmp_log_module,
+    &ngx_rtmp_limit_module,
+    &ngx_rtmp_hls_module,
+    &ngx_rtmp_dash_module,
+    &ngx_http_module,
+    &ngx_http_core_module,
+    &ngx_http_log_module,
+    &ngx_http_upstream_module,
+    &ngx_http_static_module,
+    &ngx_http_autoindex_module,
+    &ngx_http_index_module,
+    &ngx_http_mirror_module,
+    &ngx_http_try_files_module,
+    &ngx_http_auth_basic_module,
+    &ngx_http_access_module,
+    &ngx_http_limit_conn_module,
+    &ngx_http_limit_req_module,
+    &ngx_http_geo_module,
+    &ngx_http_map_module,
+    &ngx_http_split_clients_module,
+    &ngx_http_referer_module,
+    &ngx_http_rewrite_module,
+    &ngx_http_ssl_module,
+    &ngx_http_proxy_module,
+    &ngx_http_fastcgi_module,
+    &ngx_http_uwsgi_module,
+    &ngx_http_scgi_module,
+    &ngx_http_memcached_module,
+    &ngx_http_empty_gif_module,
+    &ngx_http_browser_module,
+    &ngx_http_upstream_hash_module,
+    &ngx_http_upstream_ip_hash_module,
+    &ngx_http_upstream_least_conn_module,
+    &ngx_http_upstream_random_module,
+    &ngx_http_upstream_keepalive_module,
+    &ngx_http_upstream_zone_module,
+    &ngx_rtmp_stat_module,
+    &ngx_rtmp_control_module,
+    &ngx_http_write_filter_module,
+    &ngx_http_header_filter_module,
+    &ngx_http_chunked_filter_module,
+    &ngx_http_range_header_filter_module,
+    &ngx_http_gzip_filter_module,
+    &ngx_http_postpone_filter_module,
+    &ngx_http_ssi_filter_module,
+    &ngx_http_charset_filter_module,
+    &ngx_http_userid_filter_module,
+    &ngx_http_headers_filter_module,
+    &ngx_http_copy_filter_module,
+    &ngx_http_range_body_filter_module,
+    &ngx_http_not_modified_filter_module,
+    NULL
+};
+
+char *ngx_module_names[] = {
+    "ngx_core_module",
+    "ngx_errlog_module",
+    "ngx_conf_module",
+    "ngx_openssl_module",
+    "ngx_regex_module",
+    "ngx_events_module",
+    "ngx_event_core_module",
+    "ngx_epoll_module",
+    "ngx_rtmp_module",
+    "ngx_rtmp_core_module",
+    "ngx_rtmp_cmd_module",
+    "ngx_rtmp_codec_module",
+    "ngx_rtmp_access_module",
+    "ngx_rtmp_record_module",
+    "ngx_rtmp_live_module",
+    "ngx_rtmp_play_module",
+    "ngx_rtmp_flv_module",
+    "ngx_rtmp_mp4_module",
+    "ngx_rtmp_netcall_module",
+    "ngx_rtmp_relay_module",
+    "ngx_rtmp_exec_module",
+    "ngx_rtmp_auto_push_module",
+    "ngx_rtmp_auto_push_index_module",
+    "ngx_rtmp_notify_module",
+    "ngx_rtmp_log_module",
+    "ngx_rtmp_limit_module",
+    "ngx_rtmp_hls_module",
+    "ngx_rtmp_dash_module",
+    "ngx_http_module",
+    "ngx_http_core_module",
+    "ngx_http_log_module",
+    "ngx_http_upstream_module",
+    "ngx_http_static_module",
+    "ngx_http_autoindex_module",
+    "ngx_http_index_module",
+    "ngx_http_mirror_module",
+    "ngx_http_try_files_module",
+    "ngx_http_auth_basic_module",
+    "ngx_http_access_module",
+    "ngx_http_limit_conn_module",
+    "ngx_http_limit_req_module",
+    "ngx_http_geo_module",
+    "ngx_http_map_module",
+    "ngx_http_split_clients_module",
+    "ngx_http_referer_module",
+    "ngx_http_rewrite_module",
+    "ngx_http_ssl_module",
+    "ngx_http_proxy_module",
+    "ngx_http_fastcgi_module",
+    "ngx_http_uwsgi_module",
+    "ngx_http_scgi_module",
+    "ngx_http_memcached_module",
+    "ngx_http_empty_gif_module",
+    "ngx_http_browser_module",
+    "ngx_http_upstream_hash_module",
+    "ngx_http_upstream_ip_hash_module",
+    "ngx_http_upstream_least_conn_module",
+    "ngx_http_upstream_random_module",
+    "ngx_http_upstream_keepalive_module",
+    "ngx_http_upstream_zone_module",
+    "ngx_rtmp_stat_module",
+    "ngx_rtmp_control_module",
+    "ngx_http_write_filter_module",
+    "ngx_http_header_filter_module",
+    "ngx_http_chunked_filter_module",
+    "ngx_http_range_header_filter_module",
+    "ngx_http_gzip_filter_module",
+    "ngx_http_postpone_filter_module",
+    "ngx_http_ssi_filter_module",
+    "ngx_http_charset_filter_module",
+    "ngx_http_userid_filter_module",
+    "ngx_http_headers_filter_module",
+    "ngx_http_copy_filter_module",
+    "ngx_http_range_body_filter_module",
+    "ngx_http_not_modified_filter_module",
+    NULL
+};
+
