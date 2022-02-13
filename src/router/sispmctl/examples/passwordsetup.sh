b558649593df3574e08c064ac526efda893b0f9b
BrainSlayer <s.gottschall@dd-wrt.com>
2022-02-13 07:05:57 +0000

    fix wrong path: add new files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@48333 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/sispmctl/examples/passwordsetup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+echo 'SiS PM Control - Password Setup'
+echo
+FILE=/etc/sispmctl/password
+which base64 > /dev/null || (echo base64 is missing; false)
+id | grep '^uid=0(' > /dev/null || \
+(echo This scripts must be run as root; false)
+echo -n 'User name: '
+read UNAME
+while true
+do
+  echo -n 'Password: '
+  read -s PASSWD
+  echo
+  echo -n 'Repeat password: '
+  read -s PASSWD2
+  echo
+  if [[ "x$PASSWD" == "x$PASSWD2" ]]; then
+    break;
+  fi
+  echo
+  echo The password inputs did not match.
+done
+SECRET=$(echo -n "$UNAME:$PASSWD" | base64)
+rm -f $FILE
+echo $SECRET > $FILE
+chmod 400 $FILE
+chown sispmctl $FILE
+echo
+echo To enable the new password restart the sispmctl services.
