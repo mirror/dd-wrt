179614afb900dca1822ef1f8635942324b47e2da
BrainSlayer <s.gottschall@dd-wrt.com>
2022-02-13 09:51:31 +0000

    for fallback with old configs
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@48341 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/eop-tunnel/eop-tunnel-raip.sh
+++ src/router/eop-tunnel/eop-tunnel-raip.sh
@@ -198,7 +198,9 @@ if [[ $($nv get oet${i}_failstate) -eq 2 ]]; then
 		ps | grep "[w]ireguard-fwatchdog\.sh $i" | awk '{print $1}' | xargs kill -9 >/dev/null 2>&1
 	fi
 	# send tunnelnumber, sleeptime (sec), ping address, reset (1=Yes)
-	sh /usr/bin/wireguard-fwatchdog.sh $i 29 $($nv get oet${i}_failip) $fset &
+	PINGIP=$($nv get oet${i}_failip)
+	[[ -z "$PINGIP" ]] && PINGIP="8.8.8.8"
+	sh /usr/bin/wireguard-fwatchdog.sh $i 29 $PINGIP $fset &
 fi
 #restart dnsmasq when the last tunnel has been setup to reread resolv.dnsmasq, due to a bug this does not happen on change of resolv.dnsmasq
 # for now disabled waiting for DNSMasq to be repaired 2.86 works but 287test4 is buggy and does not want to replace a DNS resolver which is already in memory
