795356c81a12ee1c8744c2a52ae6960b72097150
BrainSlayer <s.gottschall@dd-wrt.com>
2022-02-23 20:24:21 +0000

    update plex: remove old files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@48417 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/plex/aarch64/usr/lib/plexmediaserver/Resources/Plug-ins-989df2310/Scanners.bundle/Contents/Resources/Photos/Plex Photo Scanner.py
+++
@@ -1,5 +0,0 @@
-import PhotoFiles
-
-# Scans through files, and add to the media list.
-def Scan(path, files, mediaList, subdirs, language=None, root=None, **kwargs):
-  PhotoFiles.Scan(path, files, mediaList, subdirs, language, root);
