f90d5415e2c5fef6c317e0fb431e16976f2a63cd
BrainSlayer <s.gottschall@dd-wrt.com>
2022-02-23 20:24:21 +0000

    update plex: remove old files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@48417 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/plex/arm/usr/lib/plexmediaserver/Resources/Plug-ins-989df2310/Framework.bundle/Contents/Resources/Versions/0/Python/PMS/__init__.py
+++
@@ -1,9 +0,0 @@
-#
-#  Plex Media Framework
-#  Copyright (C) 2008-2009 Plex, Inc. (James Clarke, Elan Feingold). All Rights Reserved.
-#
-
-import __error
-Error = __error.StandardErrors
-
-from threading import Timer as Timer
