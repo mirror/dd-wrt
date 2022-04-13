e7c8ebdbc0456bff1d857d42f5e4c6888b3c5d20
BrainSlayer <s.gottschall@dd-wrt.com>
2022-04-13 07:56:51 +0000

    build libdaemon static
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@48649 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libdaemon.mk
+++ src/router/rules/libdaemon.mk
@@ -1,18 +1,16 @@
 libdaemon-configure:
 	cd libdaemon && autoreconf -fsi
-	CFLAGS="$(LTO) $(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -D_GNU_SOURCE -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_malloc=malloc"
-	LDFLAGS="$(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections"
-	cd libdaemon && ./configure --prefix=/usr --host=$(ARCH)-linux --disable-dependency-tracking --disable-examples ac_cv_func_setpgrp_void=yes
+	cd libdaemon && ./configure --prefix=/usr --host=$(ARCH)-linux --disable-dependency-tracking --disable-shared --enable-static --disable-examples ac_cv_func_setpgrp_void=yes \
+	CFLAGS="$(LTO) $(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_malloc=malloc" \
+	LDFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	AR_FLAGS="cru $(LTOPLUGIN)" \
+	RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
 
 libdaemon:
 	$(MAKE) -C libdaemon
-	#$(MAKE) -C libdaemon DESTDIR=$(TOP)/libdaemon/staged install
 
 libdaemon-install:
-	install -D libdaemon/libdaemon/.libs/libdaemon.so.0.5.0 $(INSTALLDIR)/libdaemon/usr/lib/libdaemon.so.0.5.0
-	cd $(INSTALLDIR)/libdaemon/usr/lib && \
-		ln -sf libdaemon.so.0.5.0 libdaemon.so && \
-		ln -sf libdaemon.so.0.5.0 libdaemon.so.0
+	 @true
 
 libdaemon-clean:
 	-$(MAKE) -C libdaemon clean
