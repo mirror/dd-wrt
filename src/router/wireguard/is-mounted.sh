153dba1bf964d720cbecff4757d36ed2ca0b2c71
BrainSlayer <s.gottschall@dd-wrt.com>
2022-04-20 18:07:08 +0000

    patches by egc (to be tested)
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@48689 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/wireguard/is-mounted.sh
+++ src/router/wireguard/is-mounted.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
-# wait until directory is mounted and writable
-# usage: is-mounted.sh /name-of directory
-# default is /jffs
-(
+# Wait until directory is mounted and writable
+# Usage: is-mounted path-of-directory  max-wait-time
+# default is /jffs 35
+# Can also be used to test drive readiness e.g.:
+# if is-mounted /jffs 5; then echo "drive ready"; else echo "drive not ready"; fi
 DIR=$1
 [[ -z "$DIR" ]] && DIR="/jffs"
+MAXT=$2
+[[ -z "$MAXT" ]] && MAXT=35
 i=0
 # grep -q "$DIR.*rw" /proc/mounts && echo "$DIR is ready" || echo "$DIR is not ready"
 until [[ -f $DIR/mounted ]]; do
 	sleep 1
 	i=$(($i+1))
 	touch "$DIR/mounted" >/dev/null 2>&1
-	if [[ $i -gt 35 ]]; then
-		echo "$DIR not mounted after 35 sec"
-		exit
+	if [[ $i -gt $MAXT ]]; then
+		logger -p user.err -t "$(basename $0)" "$DIR *not* mounted after trying $MAXT sec"
+		exit 1
 	fi
 done 
 rm $DIR/mounted >/dev/null 2>&1
-echo "$DIR mounted after $i seconds"
-)2>&1 | logger -t "$(basename $0): "
+logger -p user.info -t "$(basename $0)" "$DIR mounted after $i seconds"
+exit 0
