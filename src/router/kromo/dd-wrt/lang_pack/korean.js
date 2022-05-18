c4a36bfab3b91aabe71804f8f173d2665e51dd81
BrainSlayer <s.gottschall@dd-wrt.com>
2022-05-18 09:32:34 +0000

    merge by the-joker
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@48898 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/dd-wrt/lang_pack/korean.js
+++ src/router/kromo/dd-wrt/lang_pack/korean.js
@@ -806,7 +806,7 @@ hnas.right2="In order to share resources add new shares by supplying path and sh
 
 //help page
 hnas.page1="<dd>FTP Server enables you to share files : <br /><ul><li>Over the Internet - WAN</li><li>On local network</li></ul></dd>";
-hnas.page2="<dd>DLNA Server enables you to share media : <br /><ul><li>You need a dlna capable client e.g. TV to view files served by the router.</li></ul></dd>";
+hnas.page2="<dd>DLNA Server enables you to share media : <br /><ul><li>You need a DLNA capable client e.g. TV to view files served by the router.</li></ul></dd>";
 hnas.page3="<dd>SAMBA Server enables you to acces files : <br /><ul><li>On router from local network through file explorer</li></ul></dd>";
 hnas.page4="<dd><br /><ul><li>Path: Path to mounted partition. See currently mounted disks under <a href=\"../USB.asp\">Disk Info</a> </li><li>Subdir: Directory name on given partition e.g. public or folder/sub_folder/sub_sub_folder </li><li>Name: Share name displayed when browsing the network shares e.g. \\router\name </li><li>Public: Everyone can access this share. No user account required.</li></ul></dd><dt>Note</dt><dd><br />For Windows users: a connected USB drive shows up in Windows under D: and contains two directories <i>public, secret</i>.<br />You want to share <i>D:\public</i>. To do this connect the usb drive and lookup or specify a mountpoint under <a href=\"../USB.asp\">USB Settings</a>.<br />D: equals a mount point under linux. In order to share <i>D:\public</i>, select current mount point and specify subdir <i>public</i><br />give it a name e.g. Guest and setup access permissions.</dd>";
 
