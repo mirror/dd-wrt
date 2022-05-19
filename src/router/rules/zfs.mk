496e4ed538e826c5b5ed9684afe4c753177e85f3
BrainSlayer <s.gottschall@dd-wrt.com>
2022-05-19 04:32:20 +0000

    fix build
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@48905 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/zfs.mk
+++ src/router/rules/zfs.mk
@@ -10,8 +10,8 @@ zfs-configure: libtirpc-configure libtirpc libudev openssl zlib curl
 		CFLAGS="-I$(TOP)/zlib -I$(TOP)/util-linux/include  -I$(TOP)/util-linux/libblkid/src -I$(TOP)/util-linux/libuuid/src -I$(TOP)/curl/include -I$(TOP)/libtirpc -I$(TOP)/libtirpc/tirpc -I$(TOP)/openssl/include  -I$(TOP)/libudev/src/libudev -D_GNU_SOURCE" \
 		LDFLAGS="-L$(TOP)/zlib  -L$(TOP)/util-linux/.libs -L$(TOP)/libtirpc/src/.libs -L$(TOP)/zfs/lib/libuutil/.libs -L$(TOP)/openssl -L$(TOP)/libudev/src/libudev/.libs" \
 		--with-linux=$(LINUXDIR)
-	cd zfs && find . -name *.la -exec sed -i 's/relink_command/# relink_command/g' {} +
-	cd zfs && find . -name *.la -exec touch {} +
+	cd zfs && find .libs -name "*.la" -exec sed -i 's/relink_command/# relink_command/g' {} +
+	cd zfs && find .libs -name "*.la" -exec touch {} +
 
 zfs: libtirpc libudev openssl zlib
 	$(MAKE) -j 4 -C zfs
@@ -24,8 +24,8 @@ zfs-distclean:
 	
 
 zfs-install:
-	cd zfs && find . -name *.la -exec sed -i 's/relink_command/# relink_command/g' {} +
-	cd zfs && find . -name *.la -exec touch {} +
+	cd zfs && find .libs -name "*.la" -exec sed -i 's/relink_command/# relink_command/g' {} +
+	cd zfs && find .libs -name "*.la" -exec touch {} +
 	make -C zfs install DESTDIR=$(INSTALLDIR)/zfs
 	rm -rf $(INSTALLDIR)/zfs/usr/include
 	rm -rf $(INSTALLDIR)/zfs/usr/lib/pkgconfig
