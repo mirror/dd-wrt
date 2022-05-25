00206286d865581afa4a0e4d5023c833c547a507
BrainSlayer <s.gottschall@dd-wrt.com>
2022-05-25 07:09:40 +0000

    we dont need it for any pb42 target
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@48959 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/wireless-tools.mk
+++ src/router/rules/wireless-tools.mk
@@ -6,7 +6,9 @@ wireless-tools:
 
 wireless-tools-install:
 ifneq ($(CONFIG_JUSTLIBIW),y)
+ifneq ($(CONFIG_LSX),y)
 	make -C wireless-tools INSTALL_DIR=$(INSTALLDIR)/wireless-tools/usr/sbin install-iwmulticall
+endif
 endif
 	make -C wireless-tools INSTALL_LIB=$(INSTALLDIR)/wireless-tools/lib install-dynamic
 
