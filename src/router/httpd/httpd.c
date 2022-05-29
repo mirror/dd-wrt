16173fa66a4d1d9be8a8035c897f8ad3976277e8
BrainSlayer <s.gottschall@dd-wrt.com>
2022-05-29 11:34:31 +0000

    fix typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@48988 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/httpd.c
+++ src/router/httpd/httpd.c
@@ -542,7 +542,7 @@ static void send_error(webs_t conn_fp, int noheader, int status, char *title, ch
 	websWrite(conn_fp, "<html%s>\n",translate);
 	do_error_style(conn_fp, status, title, text);
 
-	char *charset = live_translate(wp, "lang_charset.set");
+	char *charset = live_translate(conn_fp, "lang_charset.set");
 	websWrite(conn_fp, "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n" //
 			   "<head>\n" //
 			   "<meta http-equiv=\"Content-Type\" content=\"application/xhtml+xml; charset=%s\" />\n", charset);
