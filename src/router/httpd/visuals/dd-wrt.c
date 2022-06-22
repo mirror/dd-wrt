6707fc009a70b1fc97835fa1071597e6a824d3ea
BrainSlayer <s.gottschall@dd-wrt.com>
2022-06-22 08:02:37 +0000

    fixes for the jokers merge
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@49297 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/dd-wrt.c
+++ src/router/httpd/visuals/dd-wrt.c
@@ -2986,7 +2986,7 @@ EJ_VISIBLE void ej_show_countrylist(webs_t wp, int argc, char_t ** argv)
 	if (nvram_matchi("nocountrysel", 1))
 		return;
 	char *list = getCountryList(NULL);
-	showOptionsChoose(wp, argv[0], list, nvram_safe_get(argv[0]));
+	showOptionsChoose(wp, argv[0], list, NULL, nvram_safe_get(argv[0]));
 }
 
 static void mesh_num(webs_t wp, char *prefix, char *name, int len, int def)
