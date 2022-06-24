a48eef02b348b9ecf146609bffb3f1e8e4ed8817
BrainSlayer <s.gottschall@dd-wrt.com>
2022-06-24 08:09:26 +0000

    working no debug patch now
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@49351 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/zfs/lib/libspl/include/assert.h
+++ src/router/zfs/lib/libspl/include/assert.h
@@ -51,7 +51,39 @@ libspl_assert(const char *buf, const char *file, const char *func, int line)
 #ifdef verify
 #undef verify
 #endif
+#ifdef NDEBUG
+#define	VERIFY(cond) (void) ((!(cond)))
+#define	verify(cond) (void) ((!(cond)))
 
+#define	VERIFY3B(LEFT, OP, RIGHT)					\
+do {									\
+	const boolean_t __left = (boolean_t)(LEFT);			\
+	const boolean_t __right = (boolean_t)(RIGHT);			\
+} while (0)
+
+#define	VERIFY3S(LEFT, OP, RIGHT)					\
+do {									\
+	const int64_t __left = (int64_t)(LEFT);				\
+	const int64_t __right = (int64_t)(RIGHT);			\
+} while (0)
+
+#define	VERIFY3U(LEFT, OP, RIGHT)					\
+do {									\
+	const uint64_t __left = (uint64_t)(LEFT);			\
+	const uint64_t __right = (uint64_t)(RIGHT);			\
+} while (0)
+
+#define	VERIFY3P(LEFT, OP, RIGHT)					\
+do {									\
+	const uintptr_t __left = (uintptr_t)(LEFT);			\
+	const uintptr_t __right = (uintptr_t)(RIGHT);			\
+} while (0)
+
+#define	VERIFY0(LEFT)							\
+do {									\
+	const uint64_t __left = (uint64_t)(LEFT);			\
+} while (0)
+#else
 #define	VERIFY(cond)							\
 	(void) ((!(cond)) &&						\
 	    libspl_assert(#cond, __FILE__, __FUNCTION__, __LINE__))
@@ -107,7 +139,7 @@ do {									\
 		    "%s == 0 (0x%llx == 0)", #LEFT,			\
 		    (u_longlong_t)__left);				\
 } while (0)
-
+#endif
 #ifdef assert
 #undef assert
 #endif
