f205db0c6e325268eb38d6aa68b9776a9a744ec7
BrainSlayer <s.gottschall@dd-wrt.com>
2022-07-08 14:38:46 +0000

    compat fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@49466 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/wireguard/src/compat/compat.h
+++ src/router/wireguard/src/compat/compat.h
@@ -282,7 +282,7 @@ static const struct in6_addr __compat_in6addr_any = IN6ADDR_ANY_INIT;
 #define in6addr_any __compat_in6addr_any
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 13, 0) && LINUX_VERSION_CODE >= KERNEL_VERSION(4, 2, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 9, 0) && LINUX_VERSION_CODE >= KERNEL_VERSION(4, 2, 0)
 #include <linux/completion.h>
 #include <linux/random.h>
 #include <linux/errno.h>
@@ -378,7 +378,7 @@ static inline bool rng_is_initialized(void)
 }
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 13, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 9, 0)
 static inline int get_random_bytes_wait(void *buf, int nbytes)
 {
 	int ret = wait_for_random_bytes();
