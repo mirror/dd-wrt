8978c711f2750f73c80848854c80267a1873f82f
BrainSlayer <s.gottschall@dd-wrt.com>
2022-08-03 14:04:49 +0000

    update rules file
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@49640 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/antaira-agent.mk
+++ src/router/rules/antaira-agent.mk
@@ -3,9 +3,15 @@ antaira-agent-configure:
 
 antaira-agent:
 	make -C antaira-agent
+	install -D antaira-agent/config/antaira-agent.webvpn $(TOP)/httpd/ej_temp/antaira-agent.webvpn
 
 antaira-agent-clean:
 	make -C antaira-agent clean
 
 antaira-agent-install:
 	make -C antaira-agent install
+	install -D antaira-agent/antaira-quick-vpn-agent $(INSTALLDIR)/antaira-agent/usr/sbin/antaira-quick-vpn-agent
+	cd $(INSTALLDIR)/openvpn/usr/sbin && ln -sf openvpn antaira-agent-vpn
+	install -D antaira-agent/config/antaira-agent.nvramconfig $(INSTALLDIR)/antaira-agent/etc/config/antaira-agent.nvramconfig
+	install -D antaira-agent/config/antaira-agent.webvpn $(INSTALLDIR)/antaira-agent/etc/config/antaira-agent.webvpn
+
