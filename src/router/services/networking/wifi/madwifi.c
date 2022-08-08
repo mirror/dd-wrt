88c086ff3f0ef5f688441a73be392e3d3497d2fe
BrainSlayer <s.gottschall@dd-wrt.com>
2022-08-08 10:48:37 +0000

    needed
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@49666 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/wifi/madwifi.c
+++ src/router/services/networking/wifi/madwifi.c
@@ -588,7 +588,7 @@ void check_cryptomod(char *prefix)
 		int w = nvram_default_geti(mfp, 0);
 
 		if (w == 1 || w == -1 || nvhas(akm, "psk3") || nvhas(akm, "owe") || nvhas(akm, "wpa3") || nvhas(akm, "wpa3-192") || nvhas(akm, "wpa3-128") || nvhas(akm, "wpa2-sha256") || nvhas(akm, "psk2-sha256"))
-			insmod("crypto_hash crypto_null aead gf128mul ctr ghash-generic gcm");
+			insmod("crypto_hash crypto_null aead gf128mul ctr ghash-generic gcm ccm");
 	}
 }
 
