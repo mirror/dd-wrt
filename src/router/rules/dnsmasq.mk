384707e25a0108b7d113af1e8985d03f212c0717
BrainSlayer <s.gottschall@dd-wrt.com>
2022-08-12 05:19:36 +0000

    fix for k26 problem
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@49680 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/dnsmasq.mk
+++ src/router/rules/dnsmasq.mk
@@ -28,10 +28,8 @@ ifeq ($(ARCH),aarch64)
 DNSMASQ_COPTS += -DNEED_PRINTF
 endif
 ifeq ($(ARCHITECTURE),broadcom)
-ifneq ($(CONFIG_BCMMODERN),y)
 ifneq ($(CONFIG_80211AC),y)
 DNSMASQ_COPTS += -DNO_INOTIFY
-endif
 endif
 endif
 ifeq ($(CONFIG_DNSSEC),y)
