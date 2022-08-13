b740117f892466e10318676ee1c5f9b85c8d5921
BrainSlayer <s.gottschall@dd-wrt.com>
2022-08-13 13:18:44 +0000

    basics are up now
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@49689 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-northstar.c
+++ src/router/services/sysinit/sysinit-northstar.c
@@ -5549,9 +5549,9 @@ void start_sysinit(void)
 			nvram_commit();
 			nvram_seti("0:ddwrt", 1);
 		}
-		nvram_set("1:sw_txchain_mask","0xf");
-		nvram_set("1:sw_rxchain_mask","0xf");
-		nvram_set("1:disband5grp","0");
+		nvram_set("1:sw_txchain_mask", "0xf");
+		nvram_set("1:sw_rxchain_mask", "0xf");
+		nvram_set("1:disband5grp", "0");
 		nvram_unset("et1macaddr");
 		nvram_unset("et2macaddr");
 		set_gpio(0, 1);	//USB
@@ -6498,7 +6498,7 @@ void start_sysinit(void)
 	}
       next:;
 
-	nvram_set("has_ctf","1");
+	nvram_set("has_ctf", "1");
 	if (nvram_match("sfe", "2"))
 		nvram_set("ctf_disable", "0");
 	else
