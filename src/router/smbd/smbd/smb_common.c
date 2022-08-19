ee7fff56256b407821b1066e6b64fa701e9f3eaa
BrainSlayer <s.gottschall@dd-wrt.com>
2022-08-19 15:28:42 +0000

    merge more code with upstream
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@49781 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/smbd/smbd/smb_common.c
+++ src/router/smbd/smbd/smb_common.c
@@ -167,6 +167,7 @@ int ksmbd_verify_smb_message(struct ksmbd_work *work)
 		return ksmbd_smb2_check_message(work);
 	}
 
+	work->conn->outstanding_credits++;
 	return ksmbd_smb1_check_message(work);
 #else
 	struct smb_hdr *hdr;
@@ -176,8 +177,10 @@ int ksmbd_verify_smb_message(struct ksmbd_work *work)
 
 	hdr = work->request_buf;
 	if (*(__le32 *)hdr->Protocol == SMB1_PROTO_NUMBER &&
-	    hdr->Command == SMB_COM_NEGOTIATE)
+	    hdr->Command == SMB_COM_NEGOTIATE) {
+		work->conn->outstanding_credits++;
 		return 0;
+	}
 
 	return -EINVAL;
 #endif
