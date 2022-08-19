0621fc7777f17a33b32ccc29b333939f480cbc5f
BrainSlayer <s.gottschall@dd-wrt.com>
2022-08-19 06:38:23 +0000

    update kernel
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@49775 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4/drivers/nfc/nxp-nci/i2c.c
+++ src/linux/universal/linux-4.4/drivers/nfc/nxp-nci/i2c.c
@@ -140,7 +140,9 @@ static int nxp_nci_i2c_fw_read(struct nxp_nci_i2c_phy *phy,
 	memcpy(skb_put(*skb, NXP_NCI_FW_HDR_LEN), &header, NXP_NCI_FW_HDR_LEN);
 
 	r = i2c_master_recv(client, skb_put(*skb, frame_len), frame_len);
-	if (r != frame_len) {
+	if (r < 0) {
+		goto fw_read_exit_free_skb;
+	} else if (r != frame_len) {
 		nfc_err(&client->dev,
 			"Invalid frame length: %u (expected %zu)\n",
 			r, frame_len);
@@ -181,8 +183,13 @@ static int nxp_nci_i2c_nci_read(struct nxp_nci_i2c_phy *phy,
 	memcpy(skb_put(*skb, NCI_CTRL_HDR_SIZE), (void *) &header,
 	       NCI_CTRL_HDR_SIZE);
 
+	if (!header.plen)
+		return 0;
+
 	r = i2c_master_recv(client, skb_put(*skb, header.plen), header.plen);
-	if (r != header.plen) {
+	if (r < 0) {
+		goto nci_read_exit_free_skb;
+	} else if (r != header.plen) {
 		nfc_err(&client->dev,
 			"Invalid frame payload length: %u (expected %u)\n",
 			r, header.plen);
