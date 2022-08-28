9651ebfbc05f80d4008f542d3b8659d357eb0b0d
BrainSlayer <s.gottschall@dd-wrt.com>
2022-08-28 05:15:08 +0000

    for size reduction
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@49937 52c4871e-980c-0410-b1e0-e73912ce01f8

--- tools/removewhitespace.sh
+++ tools/removewhitespace.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in $2/*.asp $2/*.svg $2/*.htm $2/*.html $2/*.js $2/*.css $2/*.webservices $2/*.webconfig  $2/*.webconfig_release $2/*.webhotspot $2/*.webalive $2/*.websecurity $2/*.webvpn $2/*.ipv6config $2/*.p2pwebconfig $2/*.webproxy $2/*.webnas $2/*.webserver $2/*.webusb; do
+for i in $2/*.asp $2/*.svg $2/*.stylus $2/*.htm $2/*.html $2/*.js $2/*.css $2/*.webservices $2/*.webconfig  $2/*.webconfig_release $2/*.webhotspot $2/*.webalive $2/*.websecurity $2/*.webvpn $2/*.ipv6config $2/*.p2pwebconfig $2/*.webproxy $2/*.webnas $2/*.webserver $2/*.webusb; do
 	if test -e $i; then
 		echo $i
 		$1/removewhitespace $i
