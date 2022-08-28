f6eb25ee820011aedf1ad8321cda663fe857ad2b
BrainSlayer <s.gottschall@dd-wrt.com>
2022-08-28 15:33:19 +0000

    center buttons
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@49967 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/bridging.c
+++ src/router/httpd/visuals/bridging.c
@@ -219,7 +219,7 @@ EJ_VISIBLE void ej_show_bridgenames(webs_t wp, int argc, char_t ** argv)
 	websWrite(wp, "<td>&nbsp;</td>\n");
 	websWrite(wp, "<td>&nbsp;</td>\n");
 	websWrite(wp, "<td>&nbsp;</td>\n");
-	websWrite(wp, "<td>");
+	websWrite(wp, "<td align=\"center\">\n");
 
 	websWrite(wp,
 		  "<script type=\"text/javascript\">\n//<![CDATA[\n document.write(\"<input class=\\\"button\\\" type=\\\"button\\\" value=\\\"\" + sbutton.add + \"\\\" onclick=\\\"bridge_add_submit(this.form)\\\" />\");\n//]]>\n</script>\n");
@@ -394,7 +394,7 @@ EJ_VISIBLE void ej_show_bridgeifnames(webs_t wp, int argc, char_t ** argv)
 	websWrite(wp, "<td>&nbsp;</td>\n");
 	websWrite(wp, "<td>&nbsp;</td>\n");
 	websWrite(wp, "<td>&nbsp;</td>\n");
-	websWrite(wp, "<td>\n");
+	websWrite(wp, "<td align=\"center\">\n");
 	websWrite(wp,
 		  "<script type=\"text/javascript\">\n//<![CDATA[\n document.write(\"<input class=\\\"button\\\" type=\\\"button\\\" value=\\\"\" + sbutton.add + \"\\\" onclick=\\\"bridgeif_add_submit(this.form)\\\" />\");\n//]]>\n</script>\n");
 	websWrite(wp, "</td>\n");
