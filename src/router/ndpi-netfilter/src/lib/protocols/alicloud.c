aa8a38d4cfe9a0b7b9d97b6536308735191f1df1
BrainSlayer <s.gottschall@dd-wrt.com>
2022-08-30 12:24:13 +0000

    fix api
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@50000 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/ndpi-netfilter/src/lib/protocols/alicloud.c
+++ src/router/ndpi-netfilter/src/lib/protocols/alicloud.c
@@ -37,7 +37,7 @@ static void ndpi_int_alicloud_add_connection(struct ndpi_detection_module_struct
 void ndpi_search_alicloud(struct ndpi_detection_module_struct *ndpi_struct,
                           struct ndpi_flow_struct *flow)
 {
-  struct ndpi_packet_struct const * const packet = &ndpi_struct->packet;
+  struct ndpi_packet_struct *packet = ndpi_get_packet_struct(ndpi_struct);
 
   NDPI_LOG_DBG(ndpi_struct, "search alicloud\n");
 
