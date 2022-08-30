e6ee6290dba24522a6f9ff185c1c6bfc2ca3d968
BrainSlayer <s.gottschall@dd-wrt.com>
2022-08-30 12:24:13 +0000

    fix api
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@50000 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/ndpi-netfilter/src/lib/protocols/discord.c
+++ src/router/ndpi-netfilter/src/lib/protocols/discord.c
@@ -38,7 +38,7 @@ static void ndpi_int_discord_add_connection(struct ndpi_detection_module_struct 
 void ndpi_search_discord(struct ndpi_detection_module_struct *ndpi_struct,
                          struct ndpi_flow_struct *flow)
 {
-  struct ndpi_packet_struct const * const packet = &ndpi_struct->packet;
+  struct ndpi_packet_struct *packet = ndpi_get_packet_struct(ndpi_struct);
 
   NDPI_LOG_DBG(ndpi_struct, "search discord\n");
 
