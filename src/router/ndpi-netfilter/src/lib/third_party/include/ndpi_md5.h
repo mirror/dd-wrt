5789c2148c2a90efca7a6425e84f9ac38f285156
BrainSlayer <s.gottschall@dd-wrt.com>
2022-08-30 08:17:18 +0000

    merge to single file compile
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@49986 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/ndpi-netfilter/src/lib/third_party/include/ndpi_md5.h
+++ src/router/ndpi-netfilter/src/lib/third_party/include/ndpi_md5.h
@@ -1,3 +1,5 @@
+#ifndef NDPI_MD5_H
+#define NDPI_MD5_H
 /*
   Contributed code pasted here to make nDPI self-contained with no
   external dependencies
@@ -16,3 +18,4 @@ typedef struct ndpi_MD5Context {
 extern void ndpi_MD5Init(ndpi_MD5_CTX *ctx);
 extern void ndpi_MD5Update(ndpi_MD5_CTX *ctx, unsigned char const *buf, unsigned len);
 extern void ndpi_MD5Final(unsigned char digest[16], ndpi_MD5_CTX *ctx);
+#endif
