dbf3279c90fe7d6272a550be890bdaf7c58c29e2
BrainSlayer <s.gottschall@dd-wrt.com>
2022-08-30 07:24:18 +0000

    fix email addresses
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@49983 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/generic/qos.c
+++ src/router/services/networking/generic/qos.c
@@ -1,5 +1,5 @@
 /*
- * wshaper.c Copyright (C) 2006 Sebastian Gottschall <gottschall@dd-wrt.com>
+ * wshaper.c Copyright (C) 2006 Sebastian Gottschall <s.gottschall@dd-wrt.com>
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2 of the License, or (at your option)
