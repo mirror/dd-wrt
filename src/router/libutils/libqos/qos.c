15aa4c8fed3ff6fc12020742c77e4f16af795ced
BrainSlayer <s.gottschall@dd-wrt.com>
2022-09-01 08:20:40 +0000

    dont know if it works. lets try
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@50031 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/libqos/qos.c
+++ src/router/libutils/libqos/qos.c
@@ -283,7 +283,7 @@ void add_client_mac_srvfilter(char *name, char *type, char *data, int level, int
 				eval("iptables", "-t", "mangle", "-A", "FILTER_IN", "-m", "mac", "--mac-source", client, "-m", "layer7", "--l7proto", "bt2", "-j", "MARK", "--set-mark", qos_nfmark(base + idx));
 
 				evalip6("ip6tables", "-t", "mangle", "-A", "FILTER_IN", "-m", "mac", "--mac-source", client, "-m", "length", "--length", "0:550", "-m", "layer7", "--l7proto", "bt", "-j", "MARK",
-				     "--set-mark", qos_nfmark(base + idx));
+					"--set-mark", qos_nfmark(base + idx));
 				evalip6("ip6tables", "-t", "mangle", "-A", "FILTER_IN", "-m", "mac", "--mac-source", client, "-m", "layer7", "--l7proto", "bt1", "-j", "MARK", "--set-mark", qos_nfmark(base + idx));
 				evalip6("ip6tables", "-t", "mangle", "-A", "FILTER_IN", "-m", "mac", "--mac-source", client, "-m", "layer7", "--l7proto", "bt2", "-j", "MARK", "--set-mark", qos_nfmark(base + idx));
 			}
@@ -420,7 +420,8 @@ static void add_tc_class(char *dev, int pref, int handle, int classid)
 	sprintf(c, "1:", classid);
 	sprintf(p, "%d", pref);
 	eval("tc", "filter", "add", "dev", dev, "protocol", "ip", "pref", p, "handle", h, "fw", "classid", c);
-	eval("tc", "filter", "add", "dev", dev, "protocol", "ipv6", "pref", p, "handle", h, "fw", "classid", c);
+	if (nvram_match("ipv6_enable", "1"))
+		eval("tc", "filter", "add", "dev", dev, "protocol", "ipv6", "pref", p, "handle", h, "fw", "classid", c);
 }
 #else
 static void add_tc_mark(char *dev, int pref, char *mark, char *mark2, int flow)
@@ -430,7 +431,8 @@ static void add_tc_mark(char *dev, int pref, char *mark, char *mark2, int flow)
 	char f[32];
 	sprintf(f, "1:%d", flow);
 	eval("tc", "filter", "add", "dev", dev, "protocol", "ip", "pref", p, "parent", "1:", "u32", "match", "mark", mark, mark2, "flowid", f);
-	eval("tc", "filter", "add", "dev", dev, "protocol", "ipv6", "pref", p, "parent", "1:", "u32", "match", "mark", mark, mark2, "flowid", f);
+	if (nvram_match("ipv6_enable", "1"))
+		eval("tc", "filter", "add", "dev", dev, "protocol", "ipv6", "pref", p, "parent", "1:", "u32", "match", "mark", mark, mark2, "flowid", f);
 }
 #endif
 
@@ -1035,7 +1037,8 @@ static void add_filter(const char *dev, int pref, int handle, int classid)
 	sprintf(h, "0x%02X", handle);
 	sprintf(c, "1:%d", classid);
 	eval("tc", "filter", "add", "dev", dev, "protocol", "ip", "pref", p, "handle", h, "fw", "classid", c);
-	eval("tc", "filter", "add", "dev", dev, "protocol", "ipv6", "pref", p, "handle", h, "fw", "classid", c);
+	if (nvram_match("ipv6_enable", "1"))
+		eval("tc", "filter", "add", "dev", dev, "protocol", "ipv6", "pref", p, "handle", h, "fw", "classid", c);
 
 }
 
