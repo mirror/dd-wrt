2da0970b8e4e449bc62398bc186ab2cd79aae861
BrainSlayer <s.gottschall@dd-wrt.com>
2022-10-05 15:29:40 +0000

    merge patch by the joker with some mods
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@50371 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/openvpn/config/openvpnstate.sh
+++ src/router/openvpn/config/openvpnstate.sh
@@ -10,10 +10,10 @@ echo -e "Server: "
 echo -e "`/bin/echo "state" | /usr/bin/nc 127.0.0.1 ${PORT} |awk -F"," '{ print $2}'` `/bin/echo "state" | /usr/bin/nc 127.0.0.1 ${PORT} |awk -F"," '{print $3}'` <hr/>"
 if [ "$(nvram get openvpn_tuntap)" = "tun" ]; then
 	echo -e "Local Address: `/bin/echo "state" | /usr/bin/nc 127.0.0.1 ${PORT} |awk -F"," '{print $4}'` <br>"
-	echo -e "Remote Address: `ifconfig tun2 |awk '{print $3}'|grep P-t-P|awk -F ":" '{print $2}'` <br><br><br>"
+	echo -e "Remote Address: `ifconfig tun2 |awk '{print $3}'|grep P-t-P|awk -F ":" '{print $2}'` <br>"
 else # TAP
-	echo -e "TAP Mode: Bridged <br>"
-	echo -e "MAC Address: `ifconfig tap2 |grep HWaddr|awk '{print $5}'` <br><br><br>"
+	echo -e "TAP Mode: Bridged<br>"
+	echo -e "MAC Address: `ifconfig tap2 |grep HWaddr|awk '{print $5}'` <br><br>"
 fi
 fi
 
@@ -37,10 +37,10 @@ else
 	echo -e "MAC Address: `ifconfig tap1 |grep HWaddr|awk '{print $5}'` <br>"
 	echo -e "Local IP: `ifconfig tap1 |awk -F ":" '/inet addr/ {print $2}' |awk -F " " '{print $1}'` <br>"
 	if [ "$(nvram get openvpncl_nat)" = "1" ]; then
-		echo -e "NAT: On <br>"
+		echo -e "NAT: On<br>"
 	fi
 	if [ "$(nvram get openvpncl_sec)" = "1" ]; then
-		echo -e "Firewall Protection: On <br>"
+		echo -e "Firewall Protection: On<br>"
 	fi
 fi
 fi
