35fd3b2f4973e54679f6e08ff34622bf2c7c92b1
BrainSlayer <s.gottschall@dd-wrt.com>
2022-10-05 15:40:07 +0000

    fix typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@50376 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/base.c
+++ src/router/httpd/modules/base.c
@@ -2529,7 +2529,7 @@ static int do_syslog(unsigned char method, struct mime_handler *handler, char *u
 		  "<script type=\"text/javascript\" src=\"common.js\"></script>\n"	//
 		  "<script type=\"text/javascript\" src=\"lang_pack/english.js\"></script>\n"
 #ifdef HAVE_LANGUAGE
-		  if (!nvram_match("language", "english.js"))
+		  if (!nvram_match("language", "english"))
 		  "<script type=\"text/javascript\" src=\"lang_pack/language.js\"></script>\n"
 #endif
 		  , charset) ;
