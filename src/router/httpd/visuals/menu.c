4c69caff52518b7cb4ed3d9ad6add307dac3db62
BrainSlayer <s.gottschall@dd-wrt.com>
2022-10-06 13:58:43 +0000

    add logout button by the-joker
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@50404 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/menu.c
+++ src/router/httpd/visuals/menu.c
@@ -326,6 +326,10 @@ EJ_VISIBLE void ej_do_menu(webs_t wp, int argc, char_t ** argv)
 //fprintf(stderr,"%s->%d\n",__func__,__LINE__);
 			websWrite(wp, "<div id=\"menuSub\">\n");
 			websWrite(wp, "<ul id=\"menuSubList\">\n");
+			websWrite(wp, "<a id=\"logout\" href=\"Logout.asp\">\n");
+			websWrite(wp, "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"logout\" viewBox=\"0 0 16 16\">\n");
+			websWrite(wp, "<path fill-rule=\"evenodd\" d=\"M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z\"></path>\n");
+			websWrite(wp, "<path fill-rule=\"evenodd\" d=\"M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z\"></path></svg></a>");
 
 			for (j = 0; j < MAXSUBMENU; j++) {
 //fprintf(stderr,"%s->%d %d %d\n",__func__,__LINE__,i, j);
