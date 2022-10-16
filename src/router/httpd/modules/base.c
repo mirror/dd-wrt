a5757a45375eeb039bc574c3c093c25f6d4a219e
BrainSlayer <s.gottschall@dd-wrt.com>
2022-10-16 14:06:58 +0000

    merge patch from the-joker with own mods
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@50517 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/base.c
+++ src/router/httpd/modules/base.c
@@ -2524,8 +2524,8 @@ static int do_syslog(unsigned char method, struct mime_handler *handler, char *u
 	if (handler && !handler->send_headers)
 		send_headers(stream, 200, "OK", handler->extra_header, handler->mime_type, -1, NULL, 1);
 
-	websWrite(stream, "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n"	//
-		  "<html>\n" "<head>\n" "<meta http-equiv=\"Content-Type\" content=\"application/xhtml+xml; charset=%s\" />\n"	//
+	websWrite(stream, "<!DOCTYPE html>\n"	//
+		  "<html>\n" "<head>\n" "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=%s\" />\n"	//
 		  "<script type=\"text/javascript\" src=\"common.js\"></script>\n"	//
 		  "<script type=\"text/javascript\" src=\"lang_pack/english.js\"></script>\n", charset);
 #ifdef HAVE_LANGUAGE
@@ -2671,7 +2671,7 @@ static int do_ttgraph(unsigned char method, struct mime_handler *handler, char *
 		send_headers(stream, 200, "OK", handler->extra_header, handler->mime_type, -1, NULL, 1);
 
 	websWrite(stream, "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n"	//
-		  "<html>\n" "<head>\n" "<meta http-equiv=\"Content-Type\" content=\"application/xhtml+xml; charset=%s\" />\n"	//
+		  "<html>\n" "<head>\n" "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=%s\" />\n"	//
 		  "<title>dd-wrt traffic graph</title>\n"	//
 		  "<script type=\"text/javascript\">\n"	//
 		  "//<![CDATA[\n"	//
