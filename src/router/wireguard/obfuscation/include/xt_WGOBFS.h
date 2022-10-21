9fe2b806689f4e0c3a3c8b354ae219b374c97b7a
BrainSlayer <s.gottschall@dd-wrt.com>
2022-10-21 07:17:11 +0000

    update obfuscation code
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@50582 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/wireguard/obfuscation/include/xt_WGOBFS.h
+++ src/router/wireguard/obfuscation/include/xt_WGOBFS.h
@@ -1,14 +1,15 @@
 #ifndef _XT_WGOBFS_H
 #define _XT_WGOBFS_H
 
-#define XT_WGOBFS_MAX_KEY_SIZE 63
+#define XT_WGOBFS_MAX_KEY_SIZE 32
+#define XT_CHACHA_KEY_SIZE 32
 #define XT_MODE_OBFS   0
 #define XT_MODE_UNOBFS 1
 
 struct xt_wg_obfs_info {
     unsigned char mode;
     char key[XT_WGOBFS_MAX_KEY_SIZE + 1];
-    unsigned char hash128[16];  /* siphash secret */
+    unsigned char chacha_key[XT_CHACHA_KEY_SIZE];  /* 256 bits chacha key */
 };
 
 #endif
