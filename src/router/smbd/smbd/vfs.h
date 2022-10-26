6c61836f1e028c31e3d2e2c331d90923f6cdb2a9
BrainSlayer <s.gottschall@dd-wrt.com>
2022-10-26 07:20:09 +0000

    cannot be const here
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@50674 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/smbd/smbd/vfs.h
+++ src/router/smbd/smbd/vfs.h
@@ -238,7 +238,7 @@ static int ksmbd_vfs_fsync(struct ksmbd_work *work, u64 fid, u64 p_id);
 static int ksmbd_vfs_remove_file(struct ksmbd_work *work, char *name);
 static int ksmbd_vfs_link(struct ksmbd_work *work,
 		const char *oldname, const char *newname);
-static int ksmbd_vfs_getattr(const struct path *path, struct kstat *stat);
+static int ksmbd_vfs_getattr(struct path *path, struct kstat *stat);
 #ifdef CONFIG_SMB_INSECURE_SERVER
 static int ksmbd_vfs_setattr(struct ksmbd_work *work, const char *name,
 		u64 fid, struct iattr *attrs);
