2c89417de4ac740e62ce562ffe701ce8cbd2eb91
BrainSlayer <s.gottschall@dd-wrt.com>
2022-11-07 08:00:10 +0000

    enable openssl support
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@50821 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/privoxy.mk
+++ src/router/rules/privoxy.mk
@@ -2,9 +2,9 @@
 privoxy: pcre zlib
 	install -D privoxy/configs/privoxy.webproxy httpd/ej_temp/privoxy.webproxy
 	CC="$(ARCH)-linux-uclibc-gcc" \
-	CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -I$(TOP)/zlib -I$(TOP)/pcre -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	CPPFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -I$(TOP)/zlib -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	LDFLAGS="$(COPTS) $(MIPS16_OPT) $(LDLTO) -L$(TOP)/zlib -L$(TOP)/pcre/.libs -ffunction-sections -fdata-sections -Wl,--gc-sections -lz -fPIC" \
+	CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -I$(TOP)/openssl/include -I$(TOP)/zlib -I$(TOP)/pcre -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	CPPFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -I$(TOP)/openssl/include -I$(TOP)/zlib -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	LDFLAGS="$(COPTS) $(MIPS16_OPT) $(LDLTO) -L$(TOP)/openssl -L$(TOP)/zlib -L$(TOP)/pcre/.libs -ffunction-sections -fdata-sections -Wl,--gc-sections -lz -fPIC" \
 	$(MAKE) -C privoxy
 
 privoxy-install:
@@ -26,4 +26,14 @@ privoxy-clean:
 privoxy-configure: pcre-configure pcre zlib
 	cd privoxy && rm -rf config.{cache,status} \
 	&& autoheader && autoconf \
-	&& ./configure ac_cv_func_setpgrp_void=yes --prefix=/usr --enable-compression --sysconfdir=/etc/privoxy --target=$(ARCH)-linux --host=$(ARCH) CC=$(ARCH)-linux-uclibc-gcc CFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib -I$(TOP)/pcre -ffunction-sections -fdata-sections -Wl,--gc-sections" CPPFLAGS="$(COPTS) $(MIPS16_OPT) -I$(TOP)/zlib -I$(TOP)/pcre -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="$(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/zlib -L$(TOP)/pcre/.libs -lz -fPIC" 
+	&& ./configure ac_cv_func_setpgrp_void=yes \
+		--prefix=/usr \
+		--enable-compression \
+		--sysconfdir=/etc/privoxy \
+		--target=$(ARCH)-linux \
+		--host=$(ARCH) \
+		--with-openssl \
+		CC=$(ARCH)-linux-uclibc-gcc \
+		CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -I$(TOP)/openssl/include -I$(TOP)/zlib -I$(TOP)/pcre -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+		CPPFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -I$(TOP)/openssl/include -I$(TOP)/zlib -I$(TOP)/pcre -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+		LDFLAGS="$(COPTS) $(MIPS16_OPT) $(LDLTO) -L$(TOP)/openssl -ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/zlib -L$(TOP)/pcre/.libs -lz -fPIC"
