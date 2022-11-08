3c8629e97cb0fba686258773323806f6b883bfe3
BrainSlayer <s.gottschall@dd-wrt.com>
2022-11-08 12:09:36 +0000

    some debug
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@50839 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/inadynv2/src/tcp.c
+++ src/router/inadynv2/src/tcp.c
@@ -152,9 +152,8 @@ int tcp_init(tcp_sock_t *tcp, char *msg)
 		if (!servinfo)
 			s = getaddrinfo(tcp->remote_host, port, &hints, &servinfo);
 
-
 		if (!servinfo) {
-			logit(LOG_WARNING, "Failed resolving hostname %s: %s", tcp->remote_host, gai_strerror(s));
+			logit(LOG_WARNING, "Failed resolving hostname (port %d) %s: %s", port, tcp->remote_host, gai_strerror(s));
 			rc = RC_TCP_INVALID_REMOTE_ADDR;
 			break;
 		}
