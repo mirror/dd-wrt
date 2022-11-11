427d6c6ccf4eae57efb9f9cc4e90d44f73cd673d
BrainSlayer <s.gottschall@dd-wrt.com>
2022-11-11 09:42:42 +0000

    enforce ath5k
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@50849 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/devices/wireless.c
+++ src/router/services/sysinit/devices/wireless.c
@@ -243,9 +243,9 @@ static void detect_wireless_devices(int mask)
 	}
 #endif
 #ifdef HAVE_ATH5K
-#ifndef HAVE_ATH5KONLY
-	if (nvram_match("use_ath5k", "1"))
-#endif
+//#ifndef HAVE_ATH5KONLY
+//	if (nvram_match("use_ath5k", "1"))
+//#endif
 	{
 		loadath5k = loadlegacy;
 		loadlegacy = 0;
