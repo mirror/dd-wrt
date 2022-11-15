84fee718b160bf4cb58b5a99e52a8962f5f5f643
BrainSlayer <s.gottschall@dd-wrt.com>
2022-11-15 21:03:26 +0000

    fix link
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@50900 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/boinc.mk
+++ src/router/rules/boinc.mk
@@ -37,6 +37,8 @@ boinc-install:
 
 #	$(MAKE) -C boinc install DESTDIR=$(INSTALLDIR)/boinc
 	rm -rf $(INSTALLDIR)/boinc/usr/include
+	rm -f $(INSTALLDIR)/boinc/usr/bin/boinc
+	cd $(INSTALLDIR)/boinc/usr/bin/boinc && ln -s boinc_clien boinct
 #	rm -rf $(INSTALLDIR)/boinc/usr/bin
 #	rm -rf $(INSTALLDIR)/boinc/usr/share
 	rm -rf $(INSTALLDIR)/boinc/usr/lib/pkgconfig
