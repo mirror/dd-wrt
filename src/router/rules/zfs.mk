f149fdc347c038aa132a0bb2b9a0b610a30a8c2c
BrainSlayer <s.gottschall@dd-wrt.com>
2022-12-11 09:41:03 +0000

    fix zlib header path
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@50997 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/zfs.mk
+++ src/router/rules/zfs.mk
@@ -7,7 +7,7 @@ zfs-configure: libtirpc-configure libtirpc libudev openssl zlib curl ncurses uti
 		--host=$(ARCH)-linux \
 		--disable-pyzfs \
 		CC="$(CC) -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-		CFLAGS="-I$(TOP)/zlib -I$(TOP)/util-linux/include  -I$(TOP)/util-linux/libblkid/src -I$(TOP)/util-linux/libuuid/src -I$(TOP)/curl/include -I$(TOP)/libtirpc -I$(TOP)/libtirpc/tirpc -I$(TOP)/openssl/include  -I$(TOP)/libudev/src/libudev -D_GNU_SOURCE" \
+		CFLAGS="-I$(TOP)/zlib/include -I$(TOP)/util-linux/include  -I$(TOP)/util-linux/libblkid/src -I$(TOP)/util-linux/libuuid/src -I$(TOP)/curl/include -I$(TOP)/libtirpc -I$(TOP)/libtirpc/tirpc -I$(TOP)/openssl/include  -I$(TOP)/libudev/src/libudev -D_GNU_SOURCE" \
 		LDFLAGS="-L$(TOP)/zlib  -L$(TOP)/util-linux/.libs -L$(TOP)/libtirpc/src/.libs -L$(TOP)/zfs/lib/libuutil/.libs -L$(TOP)/openssl -L$(TOP)/libudev/src/libudev/.libs" \
 		--with-linux=$(LINUXDIR)
 	cd zfs && find . -name "*.la" -exec sed -i 's/relink_command/# relink_command/g' {} +
