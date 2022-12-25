71dc2599ef20bc561f092af058b58db52012d4aa
BrainSlayer <s.gottschall@dd-wrt.com>
2022-12-25 13:19:52 +0000

    revert to old version. new version does not support cross compiling anymore: remove old files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@51086 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libtalloc/buildtools/devel_env.sh
+++
@@ -1,7 +0,0 @@
-# This file can be sourced using
-#
-# source buildtools/devel_env.sh
-
-# Setup python path for lsp server
-PYTHONPATH="$(pwd)/third_party/waf:$(pwd)/python:$(pwd)/bin/python:$(pwd)/selftest:${PYTHONPATH}"
-export PYTHONPATH
