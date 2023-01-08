8e1436706e2a4f7b7f17b78ad1835e5f8877a2be
BrainSlayer <s.gottschall@dd-wrt.com>
2023-01-08 09:59:33 +0000

    typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@51225 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/inadynv2/plugins/easydns.c
+++ src/router/inadynv2/plugins/easydns.c
@@ -92,7 +92,7 @@ static int response(http_trans_t *trans, ddns_info_t *info, ddns_alias_t *alias)
 PLUGIN_INIT(plugin_init)
 {
 	plugin_register(&plugin);
-	plugin_register(&plugin_v6);
+	plugin_register(&plugin);
 }
 
 PLUGIN_EXIT(plugin_exit)
