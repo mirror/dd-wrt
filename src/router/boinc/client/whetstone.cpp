c4718b7554025248bf32678bed672386cf4b201f
BrainSlayer <s.gottschall@dd-wrt.com>
2023-01-08 17:03:44 +0000

    fix compiling bug
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@51262 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/boinc/client/whetstone.cpp
+++ src/router/boinc/client/whetstone.cpp
@@ -290,6 +290,8 @@ int whetstone(double& flops, double& cpu_time, double min_cpu_time) {
     return 0;
 }
 
+#ifdef ANDROID
 #if defined(ANDROID_NEON) || defined(ANDROID_VFP)
   }
 #endif // namespace closure
+#endif
