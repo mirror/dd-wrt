38139d607cad270751e3738548360766fa441474
BrainSlayer <s.gottschall@dd-wrt.com>
2023-01-15 18:24:15 +0000

    add schokokeks dyndns
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@51354 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/validate/webs.c
+++ src/router/httpd/validate/webs.c
@@ -5706,12 +5706,12 @@ void ddns_save_value(webs_t wp)
 	char _ipv6_only[] = "ddns_ipv6_only_XX";
 
 	int enable = websGetVari(wp, "ddns_enable", -1);
-	if (enable > 43 || enable < 0) {
+	if (enable > 44 || enable < 0) {
 		return;
 	}
 
 	int i;
-	for (i = 1; i < 44; i++) {
+	for (i = 1; i < 45; i++) {
 		if (i == enable)
 			continue;
 		snprintf(_username, sizeof(_username), "ddns_username_%d", i);
