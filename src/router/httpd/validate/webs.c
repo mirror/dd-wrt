5e7352bfdb4013e27a1f1719a3c48e3704934888
BrainSlayer <s.gottschall@dd-wrt.com>
2023-01-15 13:57:21 +0000

    new ddns provider
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@51326 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/validate/webs.c
+++ src/router/httpd/validate/webs.c
@@ -5706,12 +5706,12 @@ void ddns_save_value(webs_t wp)
 	char _ipv6_only[] = "ddns_ipv6_only_XX";
 
 	int enable = websGetVari(wp, "ddns_enable", -1);
-	if (enable > 33 || enable < 0) {
+	if (enable > 34 || enable < 0) {
 		return;
 	}
 
 	int i;
-	for (i = 1; i < 34; i++) {
+	for (i = 1; i < 35; i++) {
 		if (i == enable)
 			continue;
 		snprintf(_username, sizeof(_username), "ddns_username_%d", i);
