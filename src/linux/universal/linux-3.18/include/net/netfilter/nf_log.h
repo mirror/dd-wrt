3fa2b3f1814e3b5b8dd1718b516f98714436bafe
BrainSlayer <s.gottschall@dd-wrt.com>
2023-01-27 18:01:04 +0000

    another hack
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@51523 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/include/net/netfilter/nf_log.h
+++ src/linux/universal/linux-3.18/include/net/netfilter/nf_log.h
@@ -50,6 +50,7 @@ struct nf_logger {
 };
 
 /* Function to register/unregister log function. */
+#if defined(CONFIG_PRINTK)  && !defined(CONFIG_NOPRINTK)
 int nf_log_register(u_int8_t pf, struct nf_logger *logger);
 void nf_log_unregister(struct nf_logger *logger);
 
@@ -64,7 +65,44 @@ void nf_log_unbind_pf(struct net *net, u_int8_t pf);
 int nf_logger_find_get(int pf, enum nf_log_type type);
 void nf_logger_put(int pf, enum nf_log_type type);
 void nf_logger_request_module(int pf, enum nf_log_type type);
+#else
 
+static inline int nf_log_register(u_int8_t pf, struct nf_logger *logger) {
+	return 0;
+}
+static inline void nf_log_unregister(struct nf_logger *logger)
+{
+}
+
+static inline void nf_log_set(struct net *net, u_int8_t pf,
+		const struct nf_logger *logger)
+{
+}
+static inline void nf_log_unset(struct net *net, const struct nf_logger *logger)
+{
+}
+
+static inline int nf_log_bind_pf(struct net *net, u_int8_t pf,
+		   const struct nf_logger *logger)
+{
+return 0;
+}
+static inline void nf_log_unbind_pf(struct net *net, u_int8_t pf);
+{
+}
+
+static inline int nf_logger_find_get(int pf, enum nf_log_type type)
+{
+return 0;
+}
+static inline void nf_logger_put(int pf, enum nf_log_type type)
+{
+}
+static inline void nf_logger_request_module(int pf, enum nf_log_type type)
+{
+}
+
+#endif
 #define MODULE_ALIAS_NF_LOGGER(family, type) \
 	MODULE_ALIAS("nf-logger-" __stringify(family) "-" __stringify(type))
 
