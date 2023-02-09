c73c4b67a73ea6eafee7a9766b6710e45c1362f6
BrainSlayer <s.gottschall@dd-wrt.com>
2023-02-09 08:55:50 +0000

    update transmission rules
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@51636 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/transmission.mk
+++ src/router/rules/transmission.mk
@@ -1,52 +1,38 @@
+TRANSMISSION_PKG_BUILD_DIR=$(TOP)/transmission
+TRANSMISSION_CMAKE_OPTIONS=-DOPENSSL_CRYPTO_LIBRARY=$(TOP)/openssl/libcrypto.so \
+		    -DOPENSSL_SSL_LIBRARY=$(TOP)/openssl/libssl.so \
+		    -DOPENSSL_INCLUDE_DIR=$(TOP)/openssl/include \
+		    -DCURL_INCLUDE_DIR=$(TOP)/curl/include \
+		    -DCURL_LIBRARY=$(TOP)/curl/lib/.libs/libcurl.so \
+		    -DCMAKE_BUILD_TYPE=release
+
+TRANSMISSION_STAGING_DIR=$(TOP)/_staging/usr
+TRANSMISSION_EXTRA_CFLAGS=$(COPTS) $(MIPS16_OPT) -I$(TOP) -I $(TOP)/openssl/include -L $(TOP)/openssl -lcrypto -DNEED_PRINTF
+TRANSMISSION_EXTRA_LDFLAGS=-L$(TOP)/openssl -lcrypto -lssl -L$(TOP)/ncurses/lib -L$(TOP)/zlib -lz
+
+
 transmission: libevent curl zlib
 	install -D transmission/configs/transmission.webnas httpd/ej_temp/06transmission.webnas
-	CC="ccache $(ARCH)-linux-uclibc-gcc" \
-	CFLAGS="$(COPTS) -D_GNU_SOURCE -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib  -I$(TOP)/curl/include -I$(TOP)/libevent/include  $(LTO)" \
-	CPPFLAGS="$(COPTS) -D_GNU_SOURCE -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib   -I$(TOP)/curl/include -I$(TOP)/libevent/include  $(LTO)" \
-	LDFLAGS="$(COPTS) -D_GNU_SOURCE -L$(TOP)/zlib -L$(TOP)/libevent/.libs -levent -L$(TOP)/curl/lib/.libs -lcurl -ldl  $(LDLTO)" \
-	ZLIB_CFLAGS="-I$(TOP)/zlib" \
-	ZLIB_LIBS="-L$(TOP)/zlib -lz" \
-	OPENSSL_CFLAGS="-I$(TOP)/openssl/include" \
-	OPENSSL_LIBS="-L$(TOP)/openssl -lssl -lcrypto" \
-	AR_FLAGS="cru $(LTOPLUGIN)" \
-	RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
-	$(MAKE) -C transmission
+	$(MAKE) -C transmission/build
 
 transmission-install:
-	$(MAKE) -C transmission install DESTDIR=$(INSTALLDIR)/transmission
-	rm -f $(INSTALLDIR)/transmission/usr/bin/transmission-edit
-	rm -f $(INSTALLDIR)/transmission/usr/bin/transmission-show
-	rm -f $(INSTALLDIR)/transmission/usr/bin/transmission-remote
-	rm -f $(INSTALLDIR)/transmission/usr/bin/transmission-create
+	$(MAKE) -C transmission/build install DESTDIR=$(INSTALLDIR)/transmission
 	mv $(INSTALLDIR)/transmission/usr/bin/transmission-daemon $(INSTALLDIR)/transmission/usr/bin/transmissiond
 	rm -rf $(INSTALLDIR)/transmission/usr/share/man
 	rm -f $(INSTALLDIR)/transmission/usr/lib/*.la
 	rm -f $(INSTALLDIR)/transmission/usr/lib/*.a
 	install -D transmission/configs/transmission.nvramconfig $(INSTALLDIR)/transmission/etc/config/transmission.nvramconfig
 	install -D transmission/configs/transmission.webnas $(INSTALLDIR)/transmission/etc/config/06transmission.webnas
-	mv -f $(INSTALLDIR)/transmission/usr/share/transmission/web $(INSTALLDIR)/transmission/usr/share/transmission/default
+	mv -f $(INSTALLDIR)/transmission/usr/share/transmission/public_html $(INSTALLDIR)/transmission/usr/share/transmission/default
 	cp -r transmission/combustion/ $(INSTALLDIR)/transmission/usr/share/transmission/combustion
 	cp -r transmission/transmission-web-control/ $(INSTALLDIR)/transmission/usr/share/transmission/transmission-web-control
 
 transmission-clean:
-	$(MAKE) -C transmission clean
+	$(MAKE) -C transmission/build clean
 
 transmission-configure: libevent-configure curl-configure zlib
-	-cd transmission && ./autogen.sh
-	cd transmission && ./configure  --prefix=/usr ac_cv_host=$(ARCH)-uclibc-linux --target=$(ARCH)-linux --host=$(ARCH) CC="ccache $(ARCH)-linux-uclibc-gcc" \
-	--enable-daemon --enable-lightweight --disable-nls --without-systemd-daemon --libdir=/usr/lib --disable-static --disable-dependency-tracking \
-	CFLAGS="$(COPTS) -D_GNU_SOURCE -DNO_SYS_QUEUE_H -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib   -I$(TOP)/curl/include -I$(TOP)/openssl/include -I$(TOP)/libevent/include $(LTO)" \
-	CPPFLAGS="$(COPTS) -D_GNU_SOURCE -DNO_SYS_QUEUE_H -ffunction-sections -fdata-sections -Wl,--gc-sections  -I$(TOP)/zlib  -I$(TOP)/curl/include  -I$(TOP)/openssl/include  -I$(TOP)/libevent/include  $(LTO)" \
-	LDFLAGS="$(COPTS) -D_GNU_SOURCE -DNO_SYS_QUEUE_H  -L$(TOP)/zlib   -L$(TOP)/openssl -lssl -lcrypto -L$(TOP)/libevent/.libs  -L$(TOP)/curl/lib/.libs -ldl $(LDLTO)" \
-	ZLIB_LIBS="-L$(TOP)/zlib -lz" \
-	ZLIB_CFLAGS="-I$(TOP)/zlib" \
-	LIBCURL_CFLAGS="-I$(TOP)/curl/include" \
-	LIBCURL_LIBS="-L$(TOP)/curl/lib/.libs -lcurl" \
-	LIBEVENT_CFLAGS="-I$(TOP)/libevent/include" \
-	LIBEVENT_LIBS="-L$(TOP)/libevent/.libs -levent" \
-	OPENSSL_CFLAGS="-I$(TOP)/openssl/include" \
-	OPENSSL_LIBS="-L$(TOP)/openssl -lssl -lcrypto" \
-	AR_FLAGS="cru $(LTOPLUGIN)" \
-	RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
-
-	$(MAKE) -C transmission clean
+	rm -f $(TOP)/transmission/CMakeCache.txt
+	rm -rf $(TOP)/transmission/build
+	mkdir -p $(TOP)/transmission/build
+	$(call CMakeClean,$(TRANSMISSION_PKG_BUILD_DIR))
+	$(call CMakeConfigure,$(TRANSMISSION_PKG_BUILD_DIR),$(TRANSMISSION_STAGING_DIR),$(TRANSMISSION_CMAKE_OPTIONS),$(TRANSMISSION_EXTRA_CFLAGS),$(TRANSMISSION_EXTRA_LDFLAGS),-S $(TOP)/transmission,-B $(TOP)/transmission/build) 
