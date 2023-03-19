e1a141262a742b359eff420082727a4822b64460
BrainSlayer <s.gottschall@dd-wrt.com>
2023-03-19 15:12:31 +0000

    update plex: remove old files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52060 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/plex/arm/usr/lib/plexmediaserver/Resources/Plug-ins-bc0674160/Framework.bundle/Contents/Resources/Platforms/Shared/Libraries/crypto/entropy/__init__.py
+++
@@ -1,8 +0,0 @@
-""" crypto.entropy
-
-    Entropy package of CryptoPy
-
-    Copyright (c) 2002 by Paul A. Lambert
-    Read LICENSE.txt for license information.
-"""
-
--- src/router/plex/i386/usr/lib/plexmediaserver/Resources/Plug-ins-bc0674160/PlexMovieNew.bundle/Contents/Code/__init__.py
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (c) 2020 Plex Development Team. All rights reserved.
-#
-
-class PlexMovieNewAgent(Agent.Movies):
-  name = 'Plex Movie'
-  primary_provider = True
-  languages = []
-
