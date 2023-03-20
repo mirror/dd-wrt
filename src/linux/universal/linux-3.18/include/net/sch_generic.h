fe029d3960140d42e150c65341d38b241e9ca656
BrainSlayer <s.gottschall@dd-wrt.com>
2023-03-20 16:17:47 +0000

    fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52071 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/include/net/sch_generic.h
+++ src/linux/universal/linux-3.18/include/net/sch_generic.h
@@ -340,6 +340,7 @@ static inline void sch_tree_unlock(const struct Qdisc *q)
 extern struct Qdisc noop_qdisc;
 extern struct Qdisc_ops noop_qdisc_ops;
 extern struct Qdisc_ops pfifo_fast_ops;
+extern struct Qdisc_ops noqueue_qdisc_ops;
 extern struct Qdisc_ops fq_codel_qdisc_ops;
 extern struct Qdisc_ops mq_qdisc_ops;
 extern const struct Qdisc_ops *default_qdisc_ops;
