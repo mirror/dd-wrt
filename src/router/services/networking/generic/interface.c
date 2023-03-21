772b97f53209264a767abffbcd7150d1fc66fe68
BrainSlayer <s.gottschall@dd-wrt.com>
2023-03-21 18:06:01 +0000

    always tagged here
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52088 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/generic/interface.c
+++ src/router/services/networking/generic/interface.c
@@ -223,7 +223,7 @@ void start_setup_vlans(void)
 						char vl[32];
 						sprintf(vl, "%d", vlanlist[vlan_number]);
 						char pl[32];
-						sprintf(pl, "%st %s%s", nvram_safe_get("sw_wancpuport"), nvram_safe_get("sw_wan"), tagged[i] ? "t" : "");
+						sprintf(pl, "%st %st", nvram_safe_get("sw_wancpuport"), nvram_safe_get("sw_wan"));
 						eval("swconfig", "dev", "switch0", "vlan", vl, "set", "ports", pl);
 					}
 					break;
