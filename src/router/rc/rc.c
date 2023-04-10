73e0057cf9a1769b8be9d3bb740087b559a4a4e1
BrainSlayer <s.gottschall@dd-wrt.com>
2023-04-10 07:52:42 +0000

    more of this
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52272 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/rc.c
+++ src/router/rc/rc.c
@@ -52,7 +52,6 @@
 #include "check_ps.c"
 //#include "resetbutton.c"
 #include "listen.c"
-#include "radio_timer.c"
 #include "watchdog.c"
 #ifdef HAVE_WOL
 #include "wol.c"
@@ -90,166 +89,6 @@ static int softwarerevision_main(int argc, char **argv)
 /* 
  * Call when keepalive mode
  */
-static int redial_main(int argc, char **argv)
-{
-	switch (fork()) {
-	case -1:
-		// can't fork
-		exit(0);
-		break;
-	case 0:
-		/* 
-		 * child process 
-		 */
-		// fork ok
-		(void)setsid();
-		break;
-	default:
-		/* 
-		 * parent process should just die 
-		 */
-		_exit(0);
-	}
-	int need_redial = 0;
-	int status;
-	pid_t pid;
-	int _count = 1;
-	int num;
-
-	while (1) {
-#if defined(HAVE_3G)
-#if defined(HAVE_LIBMBIM) || defined(HAVE_UMBIM)
-		if (nvram_match("wan_proto", "3g")
-		    && nvram_match("3gdata", "mbim")) {
-			start_service_force("check_mbim");
-		}
-#endif
-		if (nvram_match("wan_proto", "3g")
-		    && nvram_match("3gdata", "sierradirectip")) {
-			start_service_force("check_sierradirectip");
-		} else if (nvram_match("wan_proto", "3g")
-			   && nvram_match("3gnmvariant", "1")) {
-			start_service_force("check_sierrappp");
-		}
-#endif
-#if defined(HAVE_UQMI) || defined(HAVE_LIBQMI)
-		if (nvram_match("wan_proto", "3g")
-		    && nvram_match("3gdata", "qmi") && _count == 1) {
-			start_service_force("check_qmi");
-		}
-#endif
-		if (argc < 2)
-			sleep(30);
-		else
-			sleep(atoi(argv[1]));
-		num = 0;
-		_count++;
-
-//               fprintf(stderr, "check PPPoE %d\n", num);
-		if (!check_wan_link(num)) {
-//                       fprintf(stderr, "PPPoE %d need to redial\n", num);
-			need_redial = 1;
-		} else {
-//                       fprintf(stderr, "PPPoE %d not need to redial\n", num);
-			continue;
-		}
-
-		if (need_redial) {
-			pid = fork();
-			switch (pid) {
-			case -1:
-				perror("fork failed");
-				exit(1);
-			case 0:
-#ifdef HAVE_PPPOE
-				if (nvram_match("wan_proto", "pppoe")) {
-					sleep(1);
-					start_service_force("wan_redial");
-				}
-#if defined(HAVE_PPTP) || defined(HAVE_L2TP) || defined(HAVE_HEARTBEAT) || defined(HAVE_PPPOATM) || defined(HAVE_PPPOEDUAL)
-				else
-#endif
-#endif
-
-#ifdef HAVE_PPPOEDUAL
-				if (nvram_match("wan_proto", "pppoe_dual")) {
-					sleep(1);
-					start_service_force("wan_redial");
-				}
-#if defined(HAVE_PPTP) || defined(HAVE_L2TP) || defined(HAVE_HEARTBEAT) || defined(HAVE_PPPOATM)
-				else
-#endif
-#endif
-
-#ifdef HAVE_PPPOATM
-				if (nvram_match("wan_proto", "pppoa")) {
-					sleep(1);
-					start_service_force("wan_redial");
-				}
-#if defined(HAVE_PPTP) || defined(HAVE_L2TP) || defined(HAVE_HEARTBEAT)
-				else
-#endif
-#endif
-
-#ifdef HAVE_PPTP
-				if (nvram_match("wan_proto", "pptp")) {
-					stop_service_force("pptp");
-					unlink("/tmp/services/pptp.stop");
-					sleep(1);
-					start_service_force("wan_redial");
-				}
-#if defined(HAVE_L2TP) || defined(HAVE_HEARTBEAT)
-				else
-#endif
-#endif
-#ifdef HAVE_L2TP
-				if (nvram_match("wan_proto", "l2tp")) {
-					stop_service_force("l2tp");
-					unlink("/tmp/services/l2tp.stop");
-					sleep(1);
-					start_service_force("wan_redial");
-				}
-#ifdef HAVE_HEARTBEAT
-				else
-#endif
-#endif
-					// Moded by Boris Bakchiev
-					// We dont need this at all.
-					// But if this code is executed by any of pppX programs
-					// we might have to do this.
-
-#ifdef HAVE_HEARTBEAT
-				if (nvram_match("wan_proto", "heartbeat")) {
-					if (pidof("bpalogin") == -1) {
-						stop_service_force("heartbeat_redial");
-						sleep(1);
-						start_service_force("heartbeat_redial");
-					}
-
-				}
-#endif
-#ifdef HAVE_3G
-				else if (nvram_match("wan_proto", "3g")) {
-					sleep(1);
-					start_service_force("wan_redial");
-				}
-#endif
-#ifdef HAVE_IPETH
-				else if (nvram_match("wan_proto", "iphone")) {
-					sleep(1);
-					start_service_force("wan_redial");
-				}
-#endif
-				exit(0);
-				break;
-			default:
-				waitpid(pid, &status, 0);
-				// dprintf("parent\n");
-				break;
-			}
-		}
-	}
-}
 
 static int service_main(int argc, char *argv[])
 {
@@ -357,7 +196,6 @@ static struct MAIN maincalls[] = {
 	{ "ledtool", NULL, ledtool_main },
 	{ "check_ps", NULL, check_ps_main },
 	{ "listen", NULL, listen_main },
-	{ "radio_timer", NULL, radio_timer_main },
 #ifdef HAVE_MADWIFI
 	{ "roaming_daemon", NULL, roaming_daemon_main },
 #endif
@@ -370,7 +208,7 @@ static struct MAIN maincalls[] = {
 #ifdef HAVE_PPTPD
 	{ "poptop", "pptpd_main", NULL },
 #endif
-	{ "redial", NULL, redial_main },
+//	{ "redial", NULL, redial_main },
 #ifndef HAVE_RB500
 	// {"resetbutton", NULL, &resetbutton_main},
 #endif
