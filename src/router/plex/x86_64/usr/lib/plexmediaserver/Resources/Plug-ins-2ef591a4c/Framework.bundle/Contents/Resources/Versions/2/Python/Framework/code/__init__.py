138a11b65c07f92ee43467a817d482e9641ca679
BrainSlayer <s.gottschall@dd-wrt.com>
2023-04-11 18:31:44 +0000

    update plex: remove old files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52314 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/plex/x86_64/usr/lib/plexmediaserver/Resources/Plug-ins-2ef591a4c/Framework.bundle/Contents/Resources/Versions/2/Python/Framework/code/__init__.py
+++
@@ -1,9 +0,0 @@
-#
-#  Plex Extension Framework
-#  Copyright (C) 2008-2012 Plex, Inc. (James Clarke, Elan Feingold). All Rights Reserved.
-#
-
-from loader import Loader, RestrictedModule
-from context import ExecutionContext
-from sandbox import Sandbox
-
