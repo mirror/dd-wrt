1dcaaf16a648e00d08e0a253662d2a83be06be10
BrainSlayer <s.gottschall@dd-wrt.com>
2023-04-21 11:12:05 +0000

    change custom defaults
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52371 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/defaults.c
+++ src/router/services/sysinit/defaults.c
@@ -778,8 +778,6 @@ struct nvram_param srouter_defaults[] = {
 	{ "http_passwd", "$1$HHwZAUaN$Xj8iAs4882IkDVfBzO9GI1" },	/* Password */
 #elif HAVE_RAYTRONIK
 	{ "http_passwd", "$1$iuIXI3we$ZM4nJ4QVrzGySwLd/9PDF0" },	/* Password */
-#elif HAVE_NDTRADE
-	{ "http_passwd", "$1$u236h/PW$dCTOBdqPsZsNU8t2lH2b0/" },	/* Password */
 #else
 	{ "http_passwd", DEFAULT_PASS },	/* Password */
 #endif
