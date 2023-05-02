9a1109c13d8776a03cefe481c806cf9ba94cdd1b
BrainSlayer <s.gottschall@dd-wrt.com>
2023-05-02 11:10:10 +0000

    fix libmbim
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52399 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libmbim.mk
+++ src/router/rules/libmbim.mk
@@ -1,13 +1,13 @@
 GLIB_GIO=-lgio-2.0 -lgmodule-2.0
 libmbim-configure: glib20
 	#cd libmbim && ./autogen.sh
-	cd libmbim && ./configure --enable-more-warnings=no --enable-static --disable-shared --libexecdir=/usr/lib --host=$(ARCH)-linux CFLAGS="$(COPTS) $(MIPS16_OPT) -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_realloc=realloc -Drpl_malloc=malloc  -I$(TOP)/_staging/usr/lib/glib-2.0/include -I$(TOP)/glib20/libglib/glib -I$(TOP)/glib20/libglib/gobject -I$(TOP)/_staging/usr/include -lpthread -DMESSAGE_ENABLE_TRACE" \
-	LDFLAGS="-L$(TOP)/glib20/gettext/.libs -L$(TOP)/_staging/usr/lib" \
-	LIBMBIM_GLIB_CFLAGS="$(COPTS) -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_realloc=realloc -Drpl_malloc=malloc -I$(TOP)/_staging/usr/lib/glib-2.0/include -I$(TOP)/_staging/usr/include/glib-2.0 -I$(TOP)/glib20/libglib/glib -I$(TOP)/glib20/libglib -I$(TOP)/glib20/libglib/gmodule -pthread" \
+	cd libmbim && ./configure --enable-more-warnings=no --enable-static --disable-shared --libexecdir=/usr/lib --host=$(ARCH)-linux CFLAGS="$(COPTS) $(MIPS16_OPT) -L$(TOP)/_staging/usr/lib -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_realloc=realloc -Drpl_malloc=malloc  -I$(TOP)/_staging/usr/lib/glib-2.0/include -I$(TOP)/glib20/libglib/glib -I$(TOP)/glib20/libglib/gobject -I$(TOP)/_staging/usr/include -lpthread -DMESSAGE_ENABLE_TRACE" \
+	LDFLAGS="-L$(TOP)/glib20/gettext/.libs -L$(TOP)/_staging/usr/lib -lpcre2-8" \
+	LIBMBIM_GLIB_CFLAGS="$(COPTS) -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_realloc=realloc -Drpl_malloc=malloc  -L$(TOP)/_staging/usr/lib -I$(TOP)/_staging/usr/lib/glib-2.0/include -I$(TOP)/_staging/usr/include/glib-2.0 -I$(TOP)/glib20/libglib/glib -I$(TOP)/glib20/libglib -I$(TOP)/glib20/libglib/gmodule -pthread" \
 	LIBMBIM_GLIB_LIBS="-pthread -lpthread -L$(TOP)/_staging/usr/lib $(GLIB_GIO) -lgobject-2.0 -lglib-2.0 -L$(TOP)/libffi/$(ARCH)-$(SUBARCH)-linux-gnu/.libs -lffi -L$(TOP)/zlib -lz" \
 	MBIMCLI_CFLAGS="$(COPTS) -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_realloc=realloc -Drpl_malloc=malloc -I$(TOP)/_staging/usr/lib/glib-2.0/include -I$(TOP)/_staging/usr/include/glib-2.0 -I$(TOP)/glib20/libglib/glib -I$(TOP)/glib20/libglib -I$(TOP)/glib20/libglib/gmodule -I$(TOP)/_staging/usr/include -pthread" \
 	MBIMCLI_LIBS="-pthread -lpthread -L$(TOP)/_staging/usr/lib $(GLIB_GIO) -lgobject-2.0 -lglib-2.0 -ljson-c -lm -L$(TOP)/libffi/$(ARCH)-$(SUBARCH)-linux-gnu/.libs -lffi -L$(TOP)/zlib -lz" \
-	MBIMPROXY_CFLAGS="$(COPTS) -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_realloc=realloc -Drpl_malloc=malloc -I$(TOP)/_staging/usr/lib/glib-2.0/include -I$(TOP)/_staging/usr/include/glib-2.0 -I$(TOP)/glib20/libglib/glib -I$(TOP)/glib20/libglib -I$(TOP)/glib20/libglib/gmodule -pthread" \
+	MBIMPROXY_CFLAGS="$(COPTS) -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_realloc=realloc -Drpl_malloc=malloc -L$(TOP)/_staging/usr/lib -I$(TOP)/_staging/usr/lib/glib-2.0/include -I$(TOP)/_staging/usr/include/glib-2.0 -I$(TOP)/glib20/libglib/glib -I$(TOP)/glib20/libglib -I$(TOP)/glib20/libglib/gmodule -pthread" \
 	MBIMPROXY_LIBS="-pthread -lpthread -L$(TOP)/_staging/usr/lib $(GLIB_GIO) -lgobject-2.0 -lglib-2.0 -L$(TOP)/libffi/$(ARCH)-$(SUBARCH)-linux-gnu/.libs -lffi -L$(TOP)/zlib -lz"
 
 libmbim: glib20
