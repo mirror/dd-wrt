e9e4a83b2d57f794373a0ea74fd766f5904d7092
BrainSlayer <s.gottschall@dd-wrt.com>
2023-05-03 07:30:00 +0000

    fix warning
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52407 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.9/include/linux/mmzone.h
+++ src/linux/universal/linux-4.9/include/linux/mmzone.h
@@ -1129,8 +1129,10 @@ extern struct mem_section mem_section[NR_SECTION_ROOTS][SECTIONS_PER_ROOT];
 
 static inline struct mem_section *__nr_to_section(unsigned long nr)
 {
+#ifdef CONFIG_SPARSEMEM_EXTREME
 	if (!mem_section[SECTION_NR_TO_ROOT(nr)])
 		return NULL;
+#endif
 	return &mem_section[SECTION_NR_TO_ROOT(nr)][nr & SECTION_ROOT_MASK];
 }
 extern int __section_nr(struct mem_section* ms);
