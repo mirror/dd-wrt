7f6341317118b869ac0bdb893ad1db80678c0906
BrainSlayer <s.gottschall@dd-wrt.com>
2023-05-06 10:51:31 +0000

    fix for newer musl
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52441 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/transmission.mk
+++ src/router/rules/transmission.mk
@@ -7,7 +7,7 @@ TRANSMISSION_CMAKE_OPTIONS=-DOPENSSL_CRYPTO_LIBRARY=$(TOP)/openssl/libcrypto.so 
 		    -DCMAKE_BUILD_TYPE=release
 
 TRANSMISSION_STAGING_DIR=$(TOP)/_staging/usr
-TRANSMISSION_EXTRA_CFLAGS=$(COPTS) $(MIPS16_OPT) -I$(TOP) -I $(TOP)/openssl/include -L $(TOP)/openssl -lcrypto -DNEED_PRINTF
+TRANSMISSION_EXTRA_CFLAGS=$(COPTS) $(MIPS16_OPT) -I$(TOP) -I $(TOP)/openssl/include -L $(TOP)/openssl -lcrypto -DNEED_PRINTF -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 
 TRANSMISSION_EXTRA_LDFLAGS=-L$(TOP)/openssl -lcrypto -lssl -L$(TOP)/ncurses/lib -L$(TOP)/zlib -lz -latomic
 
 
