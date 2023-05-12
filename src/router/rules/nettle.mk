f6f3041ffc86564d2e440f20de92727554dfc05f
BrainSlayer <s.gottschall@dd-wrt.com>
2023-05-12 23:43:02 +0000

    update nettle
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52505 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/nettle.mk
+++ src/router/rules/nettle.mk
@@ -1,12 +1,25 @@
 NETTLE_CONFIGURE_ARGS+= \
 	--disable-documentation \
-	--with-include-path="$(TOP)/openssl/include" \
-	--with-lib-path="$(TOP)/openssl , $(TOP)/gmp"
+	--disable-openssl
+
 ifeq ($(ARCH),armeb)
 NETTLE_CONFIGURE_ARGS+= --disable-assembler
 endif
-nettle-configure: pcre zlib openssl gmp
-	cd nettle && ./configure --host=$(ARCH)-linux --disable-shared --enable-static --disable-pic --prefix=/usr --libdir=/usr/lib $(NETTLE_CONFIGURE_ARGS) CFLAGS="$(COPTS) $(LTO) $(MIPS16_OPT) -fPIC -ffunction-sections -fdata-sections -I$(TOP)/pcre -I$(TOP)/gmp -I$(TOP)/zlib  -fPIC" CPPFLAGS="$(COPTS) $(MIPS16_OPT) -fPIC" LDFLAGS="-L$(TOP)/pcre/.libs -L$(TOP)/gmp/.libs -lpthread -lpcre -L$(TOP)/zlib $(LDFLAGS) $(LDLTO) -lz" \
+
+ifeq ($(ARCHITECTURE),ipq806x)
+NETTLE_CONFIGURE_ARGS+= --enable-arm-neon
+endif
+
+ifeq ($(ARCHITECTURE),alpine)
+NETTLE_CONFIGURE_ARGS+= --enable-arm-neon
+endif
+
+ifeq ($(ARCHITECTURE),ventana)
+NETTLE_CONFIGURE_ARGS+= --enable-arm-neon
+endif
+
+nettle-configure: pcre zlib gmp
+	cd nettle && ./configure --host=$(ARCH)-linux --disable-shared --enable-static --disable-pic --enable-fat --prefix=/usr --libdir=/usr/lib $(NETTLE_CONFIGURE_ARGS) CFLAGS="$(COPTS) $(LTO) $(MIPS16_OPT) -fPIC -ffunction-sections -fdata-sections -I$(TOP)/pcre -I$(TOP)/gmp -I$(TOP)/zlib  -fPIC" CPPFLAGS="$(COPTS) -fPIC" LDFLAGS="-L$(TOP)/pcre/.libs -L$(TOP)/gmp/.libs -lpthread -lpcre -L$(TOP)/zlib $(LDFLAGS) $(LDLTO) -lz" \
 	AR_FLAGS="cru $(LTOPLUGIN)" \
 	RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
 
