348527176aaa3f8feefe03a6709e1eb14859c2e3
BrainSlayer <s.gottschall@dd-wrt.com>
2023-05-13 09:11:10 +0000

    wolfssl support for inadyn
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52512 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/inadyn.mk
+++ src/router/rules/inadyn.mk
@@ -16,6 +16,7 @@ inadyn-configure:
 
 	mkdir -p $(TOP)/inadynv2/build
 	mkdir -p $(TOP)/inadynv2/build_ssl
+	mkdir -p $(TOP)/inadynv2/build_wolfssl
 	
 	cd inadynv2/build && ../configure --prefix=/usr \
 		--enable-reduced \
@@ -46,25 +47,50 @@ inadyn-configure:
 		AR_FLAGS="cru $(LTOPLUGIN)" \
 		RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
 
+
+	-cd inadynv2/build_wolfssl && ../configure --prefix=/usr \
+		--disable-reduced \
+		--enable-ssl \
+		--enable-openssl \
+		--disable-shared \
+		--enable-static \
+		--localstatedir=/tmp \
+		--host=$(ARCH)-linux \
+		CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+		OpenSSL_CFLAGS="-DOPENSSL_EXTRA -I$(WOLFSSL_SSLPATH) -I$(WOLFSSL_SSLPATH)/standard -I$(WOLFSSL_SSLPATH)/standard/wolfssl  -I$(WOLFSSL_SSLPATH)/wolfssl" \
+		OpenSSL_LIBS="-L$(WOLFSSL_SSLPATH)/standard/src/.libs -lwolfssl" \
+		confuse_CFLAGS="-I$(TOP)/inadynv2/libconfuse/src" \
+		confuse_LIBS="-L$(TOP)/inadynv2/libconfuse/src/.libs -lconfuse" \
+		AR_FLAGS="cru $(LTOPLUGIN)" \
+		RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
+
 inadyn:
 	$(MAKE) -C inadynv2/libconfuse
+ifeq ($(CONFIG_WOLFSSL),y)
+	$(MAKE) -C inadynv2/build_wolfssl
+else
 ifeq ($(CONFIG_OPENSSL),y)
 	$(MAKE) -C inadynv2/build_ssl
 else
 	$(MAKE) -C inadynv2/build
 endif
+endif
 
 inadyn-install:
+ifeq ($(CONFIG_WOLFSSL),y)
+	install -D inadynv2/build_wolfssl/src/inadyn $(INSTALLDIR)/inadyn/usr/sbin/inadyn
+else
 ifeq ($(CONFIG_OPENSSL),y)
 	install -D inadynv2/build_ssl/src/inadyn $(INSTALLDIR)/inadyn/usr/sbin/inadyn
 else
 	install -D inadynv2/build/src/inadyn $(INSTALLDIR)/inadyn/usr/sbin/inadyn
 endif
-
+endif
 inadyn-clean:
 	$(MAKE) -C inadynv2/libconfuse clean
-	$(MAKE) -C inadynv2/build_ssl clean
-	$(MAKE) -C inadynv2/build clean
+	-$(MAKE) -C inadynv2/build_wolfssl clean
+	-$(MAKE) -C inadynv2/build_ssl clean
+	-$(MAKE) -C inadynv2/build clean
 
 
 
