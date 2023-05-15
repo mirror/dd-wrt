ab1da9d13682b1b05e9a484e1e46812369d60455
BrainSlayer <s.gottschall@dd-wrt.com>
2023-05-15 07:28:38 +0000

    skip them here. they are not available for qos (now)
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52579 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/libqos/qos.c
+++ src/router/libutils/libqos/qos.c
@@ -158,6 +158,8 @@ void add_client_dev_srvfilter(char *name, char *type, char *data, int level, int
 	if (idx == 10)
 		idx = 0;
 
+	if (name && !strcmp(name, "windows-telemetry") || !strcmp(name, "ubnt-telemetry"))
+		return;
 	if (strstr(type, "udp") || strstr(type, "both")) {
 		eval("iptables", "-t", "mangle", "-A", chain, "-p", "udp", "-m", "udp", "--dport", data, "-j", "MARK", "--set-mark", qos_nfmark(base + idx));
 		eval("iptables", "-t", "mangle", "-A", chain, "-p", "udp", "-m", "udp", "--sport", data, "-j", "MARK", "--set-mark", qos_nfmark(base + idx));
@@ -236,6 +238,8 @@ void add_client_mac_srvfilter(char *name, char *type, char *data, int level, int
 
 	if (idx == 10)
 		idx = 0;
+	if (name && !strcmp(name, "windows-telemetry") || !strcmp(name, "ubnt-telemetry"))
+		return;
 
 	if (strstr(type, "udp") || strstr(type, "both")) {
 		eval("iptables", "-t", "mangle", "-A", "FILTER_IN", "-p", "udp", "-m", "udp", "--dport", data, "-m", "mac", "--mac-source", client, "-j", "MARK", "--set-mark", qos_nfmark(base + idx));
@@ -319,6 +323,9 @@ void add_client_ip_srvfilter(char *name, char *type, char *data, int level, int 
 
 	if (idx == 10)
 		idx = 0;
+
+	if (name && !strcmp(name, "windows-telemetry") || !strcmp(name, "ubnt-telemetry"))
+		return;
 
 	if (strstr(type, "udp") || strstr(type, "both")) {
 		eval("iptables", "-t", "mangle", "-A", "FILTER_OUT", "-p", "udp", "-m", "udp", "--dport", data, "-s", client, "-j", "MARK", "--set-mark", qos_nfmark(base + idx));
