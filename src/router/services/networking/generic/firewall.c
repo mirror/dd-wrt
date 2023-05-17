23ebdc8521335b49c58d5c6f9b1c500ffbcb3c2f
BrainSlayer <s.gottschall@dd-wrt.com>
2023-05-17 08:11:35 +0000

    obsolete
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52587 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/generic/firewall.c
+++ src/router/services/networking/generic/firewall.c
@@ -1788,7 +1788,7 @@ static void advgrp_chain(int seq, int urlenable, char *ifname)
 		save2file_A("advgrp_%d -m ndpi --proto bittorrent -j %s", seq, log_drop);
 		save2file_A("advgrp_%d -m ndpi --proto edonkey -j %s", seq, log_drop);
 		/*atm rarly used protocols */
-		save2file_A("advgrp_%d -p tcp -m ndpi --proto applejuice -j %s", seq, log_drop);
+//		save2file_A("advgrp_%d -p tcp -m ndpi --proto applejuice -j %s", seq, log_drop);
 		save2file_A("advgrp_%d -p tcp -m ndpi --proto directconnect -j %s", seq, log_drop);
 		save2file_A("advgrp_%d -m ndpi --proto fasttrack -j %s", seq, log_drop);
 //              save2file_A("advgrp_%d -p tcp -m ndpi --proto filetopia -j %s", seq, log_drop);
