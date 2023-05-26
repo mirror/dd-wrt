1c39470cf7e135c11c750fb4f7aeea834e505917
BrainSlayer <s.gottschall@dd-wrt.com>
2023-05-26 07:24:30 +0000

    check more often. this can cause a massive delay
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52711 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.14/drivers/net/ethernet/mediatek/mt7530.c
+++ src/linux/universal/linux-4.14/drivers/net/ethernet/mediatek/mt7530.c
@@ -346,13 +346,13 @@ mt7530_vtcr(struct mt7530_priv *priv, u32 cmd, u32 val)
 
 	mt7530_w32(priv, REG_ESW_VLAN_VTCR, BIT(31) | (cmd << 12) | val);
 
-	for (i = 0; i < 20; i++) {
+	for (i = 0; i < 2000; i++) {
 		u32 val = mt7530_r32(priv, REG_ESW_VLAN_VTCR);
 
 		if ((val & BIT(31)) == 0)
 			break;
 
-		udelay(1000);
+		udelay(10);
 	}
 	if (i == 20)
 		printk("mt7530: vtcr timeout\n");
