4042958ee3098d8dba1c193c3df8ad03a07e9c23
BrainSlayer <s.gottschall@dd-wrt.com>
2023-05-26 09:02:37 +0000

    update inadyn
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52714 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/inadynv2/plugins/desec.c
+++ src/router/inadynv2/plugins/desec.c
@@ -25,17 +25,17 @@
 #define DESEC_UPDATE_IP_REQUEST						\
 	"GET %s?"							\
 	"username=%s&"							\
-	"passwword=%s&"							\
+	"password=%s&"							\
 	"hostname=%s&"							\
 	"myipv4=%s "							\
 	"HTTP/1.0\r\n"							\
 	"Host: %s\r\n"							\
 	"User-Agent: %s\r\n\r\n"
 
-#define DESEC_UPDATE_IP6_REQUEST						\
+#define DESEC_UPDATE_IP6_REQUEST					\
 	"GET %s?"							\
 	"username=%s&"							\
-	"passwword=%s&"							\
+	"password=%s&"							\
 	"hostname=%s&"							\
 	"myipv6=%s "							\
 	"HTTP/1.0\r\n"							\
@@ -56,7 +56,7 @@ static ddns_system_t plugin = {
 	.checkip_ssl  = DYNDNS_MY_IP_SSL,
 
 	.server_name  = "update.dedyn.io",
-	.server_url   =  "/update.php"
+	.server_url   =  "/"
 };
 
 static ddns_system_t plugin_v6 = {
@@ -70,15 +70,14 @@ static ddns_system_t plugin_v6 = {
 	.checkip_ssl  = DDNS_CHECKIP_SSL_SUPPORTED,
 
 	.server_name  = "update6.dedyn.io",
-	.server_url   =  "/update.php"
+	.server_url   =  "/"
 };
 
 
 static int request(ddns_t *ctx, ddns_info_t *info, ddns_alias_t *alias)
 {
-	if (strstr(info->system->name, "ipv6")) {
-		return snprintf(ctx->request_buf, ctx->request_buflen,
-			DESEC_UPDATE_IP6_REQUEST,
+	return snprintf(ctx->request_buf, ctx->request_buflen,
+			info->system->server_req,
 			info->server_url,
 			info->creds.username,
 			info->creds.password,
@@ -86,38 +85,27 @@ static int request(ddns_t *ctx, ddns_info_t *info, ddns_alias_t *alias)
 			alias->address,
 			info->server_name.name,
 			info->user_agent);
-	} else {
-		return snprintf(ctx->request_buf, ctx->request_buflen,
-			DESEC_UPDATE_IP_REQUEST,
-			info->server_url,
-			info->creds.username,
-			info->creds.password,
-			alias->name,
-			alias->address,
-			info->server_name.name,
-			info->user_agent);
-	}
 }
 
 static int response(http_trans_t *trans, ddns_info_t *info, ddns_alias_t *alias)
 {
-	char *rsp = trans->rsp_body;
+	char *body = trans->rsp_body;
 
 	(void)info;
 	(void)alias;
 
 	DO(http_status_valid(trans->status));
 
-	if (strstr(rsp, alias->address))
-		return 0;
+	if (strstr(body, "good") || strstr(body, alias->address))
+		return RC_OK;
 
 	return RC_DDNS_RSP_NOTOK;
 }
 
 PLUGIN_INIT(plugin_init)
 {
-	plugin_register(&plugin);
-	plugin_register(&plugin_v6);
+	plugin_register(&plugin, DESEC_UPDATE_IP_REQUEST);
+	plugin_register(&plugin_v6, DESEC_UPDATE_IP6_REQUEST);
 }
 
 PLUGIN_EXIT(plugin_exit)
