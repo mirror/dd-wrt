0c91b68235752791a8d3bf48e9ec8c1ecc146370
BrainSlayer <s.gottschall@dd-wrt.com>
2023-05-26 05:08:06 +0000

    fix disabled state
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52708 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.10/drivers/net/phy/ar8216.c
+++ src/linux/universal/linux-3.10/drivers/net/phy/ar8216.c
@@ -1688,9 +1688,7 @@ ar8xxx_sw_get_disable(struct switch_dev *dev,
 		return -EOPNOTSUPP;
 
 	t = ar8xxx_read(priv, AR8216_REG_PORT_STATUS(port));
-	if (!(t & AR8216_PORT_STATUS_LINK_AUTO))
-		val->value.i = 1;
-	else	if (!(t & (AR8216_PORT_SPEED_10M << AR8216_PORT_STATUS_SPEED_S)) && !(t & (AR8216_PORT_SPEED_100M << AR8216_PORT_STATUS_SPEED_S)) && !(t & (AR8216_PORT_SPEED_1000M << AR8216_PORT_STATUS_SPEED_S)))
+	if (!(t & AR8216_PORT_STATUS_LINK_AUTO) && !(t & (AR8216_PORT_SPEED_10M << AR8216_PORT_STATUS_SPEED_S)) && !(t & (AR8216_PORT_SPEED_100M << AR8216_PORT_STATUS_SPEED_S)) && !(t & (AR8216_PORT_SPEED_1000M << AR8216_PORT_STATUS_SPEED_S)))
 		val->value.i = 1;
 	else
 		val->value.i = 0;
