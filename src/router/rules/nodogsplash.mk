119835678dc02fd693c1e341278cc65fb34dd2c8
BrainSlayer <s.gottschall@dd-wrt.com>
2023-05-28 11:20:49 +0000

    fix install
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52756 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/nodogsplash.mk
+++ src/router/rules/nodogsplash.mk
@@ -10,7 +10,7 @@ nodogsplash:
 
 
 nodogsplash-install:
-	$(MAKE) -C nodogsplash install DESTDIR=$(INSTALLDIR)/nodog
+	$(MAKE) -C nodogsplash install DESTDIR=$(INSTALLDIR)/nodogsplash
 	install -D nodogsplash/config/nodog.webhotspot $(INSTALLDIR)/nodogsplash/etc/config/nodog.webhotspot
 	install -D nodogsplash/config/nodog.nvramconfig $(INSTALLDIR)/nodogsplash/etc/config/nodog.nvramconfig
 	install -D nodogsplash/config/nodog.startup $(INSTALLDIR)/nodogsplash/etc/config/nodog.startup
