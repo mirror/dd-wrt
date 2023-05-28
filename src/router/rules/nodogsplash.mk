f3477f1bdb2f859f1bd03e50b2b0a6f0374041cd
BrainSlayer <s.gottschall@dd-wrt.com>
2023-05-28 10:34:25 +0000

    fix paths
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52745 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/nodogsplash.mk
+++ src/router/rules/nodogsplash.mk
@@ -3,10 +3,15 @@ nodogsplash-configure:
 	@true
 
 nodogsplash:
+	install -D nodogsplash/config/nodog.webhotspot httpd/ej_temp/nodog.webhotspot
 	$(MAKE) -C nodogsplash \
-	    CFLAGS="$(COPTS) $(MIPS16_OPT)  -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF $(LTO) -I$(TOP)/libmicrohttpd/src/include" \
-	    LDFLAGS="$(COPTS) $(MIPS16_OPT)  -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF $(LTO) -L$(TOP)/libmicrohttpd/src/microhttpd/.libs"
+	    CFLAGS="$(COPTS) $(MIPS16_OPT)  -ffunction-sections -fdata-sections -Wl,--gc-sections $(LTO) -I$(TOP)/libmicrohttpd/src/include" \
+	    LDFLAGS="$(COPTS) $(MIPS16_OPT)  -ffunction-sections -fdata-sections -Wl,--gc-sections $(LTO) -L$(TOP)/libmicrohttpd/src/microhttpd/.libs"
 
 
 nodogsplash-install:
-	@true
+	$(MAKE) -C nodogsplash install DESTDIR=$(INSTALLDIR)/nodog
+	install -D nodogsplash/config/nodog.webhotspot $(INSTALLDIR)/nodogsplash/etc/config/nodog.webhotspot
+	install -D nodogsplash/config/nodog.nvramconfig $(INSTALLDIR)/nodogsplash/etc/config/nodog.nvramconfig
+	install -D nodogsplash/config/nodog.startup $(INSTALLDIR)/nodogsplash/etc/config/nodog.startup
+
