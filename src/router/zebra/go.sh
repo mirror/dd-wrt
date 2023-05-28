7018dde0e09051f3ad524f06cad765f520e30c2a
BrainSlayer <s.gottschall@dd-wrt.com>
2023-05-28 07:40:07 +0000

    obsolete
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52727 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/zebra/go.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-./configure --disable-ipv6 --disable-ripngd --disable-ospfd \
-            --disable-ospf6d --disable-bgpd --disable-bgpd-announce \
-	    --prefix=/usr/local/zebra
-
-    
