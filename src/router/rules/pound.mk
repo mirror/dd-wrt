531645f2dc26c14225d2bacf712f35b077e59c61
BrainSlayer <s.gottschall@dd-wrt.com>
2023-05-28 19:38:13 +0000

    change configure arguments
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52787 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/pound.mk
+++ src/router/rules/pound.mk
@@ -2,7 +2,7 @@
 
 pound-configure:
 	cd pound && ./bootstrap
-	cd pound && ./configure --prefix=/usr --with-ssl=$(TOP)/openssl --host=$(ARCH)-linux CC="$(ARCH)-linux-uclibc-gcc" LDFLAGS="-Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/openssl -lpthread" CFLAGS="-Drpl_malloc=malloc -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections $(COPTS) $(MIPS16_OPT) -I$(TOP)/openssl/include"
+	cd pound && ./configure --prefix=/usr --disable-pcreposix --with-ssl=$(TOP)/openssl --host=$(ARCH)-linux CC="$(ARCH)-linux-uclibc-gcc" LDFLAGS="-Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/openssl -lpthread" CFLAGS="-Drpl_malloc=malloc -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections $(COPTS) $(MIPS16_OPT) -I$(TOP)/openssl/include"
 
 pound-clean:
 	make -C pound clean
