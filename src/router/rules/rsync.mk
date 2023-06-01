21c11074f8db991cf6516bcb90bfe0b5e53b484a
BrainSlayer <s.gottschall@dd-wrt.com>
2023-06-01 23:49:26 +0000

    some deps
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52840 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/rsync.mk
+++ src/router/rules/rsync.mk
@@ -1,10 +1,10 @@
-rsync-configure:
+rsync-configure: zstd openssl libucontext
 	cd rsync && autoconf
 	cd rsync && ./configure --host=$(ARCH)-linux --prefix=/usr --disable-locate --disable-md2man --disable-roll-simd --disable-simd --disable-iconv --disable-xxhash --disable-lz4 \
 		CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -I$(TOP)/openssl/include -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_malloc=malloc -L$(TOP)/openssl -L$(TOP)/zstd/lib -I$(TOP)/zstd/lib"
 
 	$(MAKE) -C rsync reconfigure
-rsync:
+rsync: zstd openssl libucontext
 	install -D rsync/config/rsync.webnas httpd/ej_temp/05rsync.webnas
 	$(MAKE) -C rsync
 
