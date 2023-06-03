27b667717fe345adc0e08d458cea20ad0232ca51
BrainSlayer <s.gottschall@dd-wrt.com>
2023-06-03 04:36:57 +0000

    adjust flags
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52875 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/microhttpd.mk
+++ src/router/rules/microhttpd.mk
@@ -5,7 +5,21 @@ libmicrohttpd-configure:
 	cd libmicrohttpd && autoconf
 	cd libmicrohttpd && autoheader
 	cd libmicrohttpd && autoreconf -vfi
-	cd libmicrohttpd && ./configure  --host=$(ARCH)-linux  --disable-messages --without-openssl --without-gnutls --disable-spdy --disable-https --prefix=/usr --libdir=/usr/lib --disable-shared --enable-static \
+	cd libmicrohttpd && ./configure \
+		--host=$(ARCH)-linux \
+		--disable-doc \
+		--without-gnutls \
+		--disable-https \
+		--prefix=/usr \
+		--libdir=/usr/lib \
+		--disable-shared \
+		--enable-static \
+		--disable-curl \
+		--disable-rpath \
+		--disable-examples \
+		--disable-poll \
+		--enable-epoll \
+		--with-pic \
 		CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -ffunction-sections -fdata-sections" LDFLAGS="$(LDLTO)" \
 		AR_FLAGS="cru $(LTOPLUGIN)" \
 		RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
