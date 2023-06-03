a3197691fb69dc6abd6a1413f617b58993f2af65
BrainSlayer <s.gottschall@dd-wrt.com>
2023-06-03 05:39:14 +0000

    use redirect
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52877 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/nodogsplash/src/main.c
+++ src/router/nodogsplash/src/main.c
@@ -274,7 +274,7 @@ main_loop(void)
 
 	/* Initializes the web server */
 	if ((webserver = MHD_start_daemon(
-						MHD_USE_EPOLL_INTERNALLY | MHD_USE_TCP_FASTOPEN,
+						MHD_USE_EPOLL_INTERNALLY,
 						config->gw_port,
 						NULL, NULL,
 						libmicrohttpd_cb, NULL,
