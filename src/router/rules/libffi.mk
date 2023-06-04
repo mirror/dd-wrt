305f3de946ce8039d8dad86fcb9a62fb16ad7486
BrainSlayer <s.gottschall@dd-wrt.com>
2023-06-04 11:40:05 +0000

    fix libdir
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52885 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libffi.mk
+++ src/router/rules/libffi.mk
@@ -1,5 +1,5 @@
 libffi-configure:
-	cd libffi && ./configure --host=$(ARCH)-linux --build=$(ARCH) --prefix=/usr --libdir=/usr/lib CFLAGS="$(COPTS) -std=gnu89 -D_GNU_SOURCE -fPIC -Drpl_malloc=malloc"
+	cd libffi && ./configure --host=$(ARCH)-linux --libexecdir=/usr/lib --build=$(ARCH) --prefix=/usr --libdir=/usr/lib CFLAGS="$(COPTS) -std=gnu89 -D_GNU_SOURCE -fPIC -Drpl_malloc=malloc"
 
 libffi:
 	make -C libffi
@@ -11,5 +11,7 @@ ifneq ($(CONFIG_PYTHON),y)
 	-rm -rf $(INSTALLDIR)/libffi/usr/lib/libffi-3.2.1
 endif
 	-rm -rf $(INSTALLDIR)/libffi/usr/lib/pkgconfig
+	-mv $(INSTALLDIR)/libffi/usr/lib64/* $(INSTALLDIR)/libffi/usr/lib
+	-rm -rf $(INSTALLDIR)/libffi/usr/lib64
 	-rm -f $(INSTALLDIR)/libffi/usr/lib/*.a
 	-rm -f $(INSTALLDIR)/libffi/usr/lib/*.la
