96fd3d7a9f65a929fd5d74c047ac96c904a251fb
BrainSlayer <s.gottschall@dd-wrt.com>
2023-06-08 05:31:32 +0000

    generate automake/autoconf files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52922 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/dhcp-forwarder.mk
+++ src/router/rules/dhcp-forwarder.mk
@@ -1,4 +1,10 @@
 dhcpforwarder-configure:
+	cd dhcpforwarder && rm -f config.cache
+	cd dhcpforwarder && libtoolize
+	cd dhcpforwarder && aclocal
+	cd dhcpforwarder && autoconf
+	cd dhcpforwarder && autoheader
+	cd dhcpforwarder && autoreconf -vfi
 	cd dhcpforwarder && ./configure ac_cv_host=$(ARCH)-uclibc-linux --target=$(ARCH)-linux --host=$(ARCH) --sysconfdir=/tmp/dhcp-fwd CC="$(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections -DHAVE_MALLOC=1 -Drpl_malloc=malloc -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections -DNDEBUG" LDFLAGS="$(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections -DNDEBUG"
 
 dhcpforwarder:
