06a7284df8b19fe2c34dbda54266723ac5be87c1
BrainSlayer <s.gottschall@dd-wrt.com>
2023-06-11 20:25:28 +0000

    reduce size for transmission
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52951 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/transmission.mk
+++ src/router/rules/transmission.mk
@@ -7,8 +7,8 @@ TRANSMISSION_CMAKE_OPTIONS=-DOPENSSL_CRYPTO_LIBRARY=$(TOP)/openssl/libcrypto.so 
 		    -DCMAKE_BUILD_TYPE=release
 
 TRANSMISSION_STAGING_DIR=$(TOP)/_staging/usr
-TRANSMISSION_EXTRA_CFLAGS=$(COPTS) $(MIPS16_OPT) -I$(TOP) -I $(TOP)/openssl/include -L $(TOP)/openssl -lcrypto -DNEED_PRINTF -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 
-TRANSMISSION_EXTRA_LDFLAGS=-L$(TOP)/openssl -lcrypto -lssl -L$(TOP)/ncurses/lib -L$(TOP)/zlib -lz -latomic
+TRANSMISSION_EXTRA_CFLAGS=$(COPTS) $(MIPS16_OPT) -I$(TOP) -I $(TOP)/openssl/include -L $(TOP)/openssl -lcrypto -DNEED_PRINTF -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -ffunction-sections -fdata-sections -Wl,--gc-sections 
+TRANSMISSION_EXTRA_LDFLAGS=-L$(TOP)/openssl -lcrypto -lssl -L$(TOP)/ncurses/lib -L$(TOP)/zlib -lz -latomic -ffunction-sections -fdata-sections -Wl,--gc-sections
 
 
 transmission: libevent curl zlib
