050e01e9debd93e13944f79c3a68aac7c0d1d952
BrainSlayer <s.gottschall@dd-wrt.com>
2023-06-13 00:45:43 +0000

    add speedtest
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@52962 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/httpd/visuals/speedtest.c
@@ -0,0 +1,58 @@
+/*
+ * speedtest.c
+ *
+ * Copyright (C) 2005 - 2023 Sebastian Gottschall <s.gottschall@dd-wrt.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id:
+ */
+#ifdef HAVE_SPEEDTEST_CLI
+#define VISUALSOURCE 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+#include <broadcom.h>
+
+
+EJ_VISIBLE void ej_speed_up(webs_t wp, int argc, char_t ** argv)
+{
+FILE *in = fopen("/tmp/speedtest_upload_result","rb");
+if (!in) {
+	return;
+}
+double f;
+fscanf(in,"%.2f", &f);
+fclose(in);
+websWrite(wp,"%.2f",f);
+}
+
+EJ_VISIBLE void ej_speed_down(webs_t wp, int argc, char_t ** argv)
+{
+FILE *in = fopen("/tmp/speedtest_download_result","rb");
+if (!in) {
+	return;
+}
+double f;
+fscanf(in,"%.2f", &f);
+fclose(in);
+websWrite(wp,"%.2f",f);
+}
+
+#endif
