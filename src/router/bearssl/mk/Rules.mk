b22e5a4a4022b6ec6c5b18bef550045be7bb2762
BrainSlayer <s.gottschall@dd-wrt.com>
2023-06-19 18:27:14 +0000

    build only static
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@53009 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/bearssl/mk/Rules.mk
+++ src/router/bearssl/mk/Rules.mk
@@ -325,7 +325,7 @@ HEADERSTOOLS = $(HEADERSPUB) tools$Pbrssl.h
 T0SRC = T0$PBlobWriter.cs T0$PCPU.cs T0$PCodeElement.cs T0$PCodeElementJump.cs T0$PCodeElementUInt.cs T0$PCodeElementUIntExpr.cs T0$PCodeElementUIntInt.cs T0$PCodeElementUIntUInt.cs T0$PConstData.cs T0$POpcode.cs T0$POpcodeCall.cs T0$POpcodeConst.cs T0$POpcodeGetLocal.cs T0$POpcodeJump.cs T0$POpcodeJumpIf.cs T0$POpcodeJumpIfNot.cs T0$POpcodeJumpUncond.cs T0$POpcodePutLocal.cs T0$POpcodeRet.cs T0$PSType.cs T0$PT0Comp.cs T0$PTPointerBase.cs T0$PTPointerBlob.cs T0$PTPointerExpr.cs T0$PTPointerNull.cs T0$PTPointerXT.cs T0$PTValue.cs T0$PWord.cs T0$PWordBuilder.cs T0$PWordData.cs T0$PWordInterpreted.cs T0$PWordNative.cs
 T0KERN =
 
-all: $(STATICLIB) $(DLL) $(TOOLS) $(TESTS)
+all: $(STATICLIB) $(TOOLS) $(TESTS)
 
 no:
 
