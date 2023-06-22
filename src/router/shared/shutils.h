539fe81da501b01a91fb77bdc88f380f36bed08a
BrainSlayer <s.gottschall@dd-wrt.com>
2023-06-22 06:08:10 +0000

    use snprintf if output string is a array
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@53047 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/shutils.h
+++ src/router/shared/shutils.h
@@ -178,7 +178,8 @@ extern int dd_snprintf(char *str, int len, const char *fmt, ...);
 extern void *dd_malloc(size_t len);
 
 #define malloc(len) dd_malloc(len)
-#define sprintf(output,format,args...) dd_sprintf(output, format, ## args)
+
+#define sprintf(output,format,args...) sizeof(output) > sizeof(size_t) ? dd_snprintf(output, sizeof(output), format, ## args) : dd_sprintf(output, format, ## args)
 #define snprintf(output,len,format,args...) dd_snprintf(output, len,format, ## args)
 #define system(cmd) dd_system(cmd)
 #endif
