9453f9ed8162c5044b8aa37f42f6be949e2eca77
BrainSlayer <s.gottschall@dd-wrt.com>
2023-07-04 15:56:50 +0000

    included in 6.1 already
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@53190 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/smbd.mk
+++ src/router/rules/smbd.mk
@@ -20,12 +20,16 @@ smbd-configure: libnl
 smbd: libnl
 	install -D smbd/config/samba_ksmbd.webnas httpd/ej_temp/02samba_ksmbd.webnas
 	install -D filesharing/config/zfilesharing.webnas httpd/ej_temp/03zfilesharing.webnas
+ifneq ($(KERNELVERSION),6.1)
 	$(MAKE) -C smbd/smbd all
+endif
 	$(MAKE) -C smbd/tools all
 	$(MAKE) -C smbd/tools-glib all
 
 smbd-install:
+ifneq ($(KERNELVERSION),6.1)
 	$(MAKE) -C smbd/smbd install
+endif
 	$(MAKE) -C smbd/tools-glib install DESTDIR=$(INSTALLDIR)/smbd
 	rm -rf $(INSTALLDIR)/smbd/usr/lib
 	cd $(INSTALLDIR)/smbd/usr/sbin && ln -sf smbd_multicall ksmbd.mountd
@@ -37,6 +41,8 @@ smbd-install:
 	install -D filesharing/config/zfilesharing.webnas $(INSTALLDIR)/smbd/etc/config/03zfilesharing.webnas
 
 smbd-clean:
+ifneq ($(KERNELVERSION),6.1)
 	$(MAKE) -C smbd/smbd clean
+endif
 	$(MAKE) -C smbd/tools clean
 	$(MAKE) -C smbd/tools-glib clean
