e4de13a62184603f5b5e25b209a81745341834c7
BrainSlayer <s.gottschall@dd-wrt.com>
2023-07-13 21:47:42 +0000

    use rcu here
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@53302 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.9/net/core/dev.c
+++ src/linux/universal/linux-4.9/net/core/dev.c
@@ -4203,7 +4203,6 @@ static int __netif_receive_skb_core(struct sk_buff *skb, bool pfmemalloc)
 	skb_reset_mac_len(skb);
 
 	pt_prev = NULL;
-	fast_recv = rcu_dereference(fast_nat_recv);
 
 another_round:
 	skb->skb_iif = skb->dev->ifindex;
@@ -4217,6 +4216,7 @@ another_round:
 			goto out;
 	}
 
+	fast_recv = rcu_dereference(fast_nat_recv);
 	if (fast_recv && fast_recv(skb)) {
 		ret = NET_RX_SUCCESS;
 		goto out;
