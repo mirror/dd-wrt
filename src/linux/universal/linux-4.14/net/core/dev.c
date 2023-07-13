a980689c4b81634b4117e17459233c4b0e9d8e48
BrainSlayer <s.gottschall@dd-wrt.com>
2023-07-13 21:37:46 +0000

    relocate code
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@53300 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.14/net/core/dev.c
+++ src/linux/universal/linux-4.14/net/core/dev.c
@@ -4439,6 +4439,7 @@ static int __netif_receive_skb_core(struct sk_buff *skb, bool pfmemalloc)
 	skb_reset_mac_len(skb);
 
 	pt_prev = NULL;
+	fast_recv = rcu_dereference(fast_nat_recv);
 
 another_round:
 	skb->skb_iif = skb->dev->ifindex;
@@ -4452,7 +4453,6 @@ another_round:
 			goto out;
 	}
 
-	fast_recv = rcu_dereference(fast_nat_recv);
 	if (fast_recv && fast_recv(skb)) {
 		ret = NET_RX_SUCCESS;
 		goto out;
