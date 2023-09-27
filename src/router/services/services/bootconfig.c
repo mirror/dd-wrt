8921b440a24638cc6046dd1d54fa03b53f54064f
BrainSlayer <s.gottschall@dd-wrt.com>
2023-09-27 10:57:50 +0000

    bootconfig for efi
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@53544 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/bootconfig.c
+++ src/router/services/services/bootconfig.c
@@ -32,14 +32,8 @@ char *bootconfig_deps(void)
 	return "boot_disable_msi boot_noaer boot_noari boot_noacpi boot_pcie_tune boot_mds boot_tsx_async_abort boot_srbds boot_nospectre_v1 boot_nospectre_v2 boot_l1tf boot_nospec_store_bypass_disable boot_nopti";
 }
 
-void start_bootconfig(void)
+static void makeargs(char *args)
 {
-	char *disc = getdisc();
-	char args[512] = { 0 };
-	char dev[64];
-
-	insmod("crc16 crc32c_generic crc32_generic mbcache ext2 jbd jbd2 ext3 ext4");
-
 	if (nvram_match("boot_disable_msi", "1"))
 		strcat(args, " pci=nomsi");
 	if (nvram_match("boot_noaer", "1"))
@@ -74,6 +68,17 @@ void start_bootconfig(void)
 		strcat(args, " initcall_blacklist=acpi_cpufreq_init amd_pstate.shared_mem=1 amd_pstate=passive");
 	if (nvram_match("boot_nospec_store_bypass_disable", "1"))
 		strcat(args, " spec_store_bypass_disable=off");
+
+}
+
+void start_bootconfig_legacy(void)
+{
+	char *disc = getdisc();
+	char args[512] = { 0 };
+	char dev[64];
+
+	insmod("crc16 crc32c_generic crc32_generic mbcache ext2 jbd jbd2 ext3 ext4");
+	makeargs(args);
 	if (strlen(disc) == 7)	//mmcblk0 / nvme0n1
 		sprintf(dev, "/dev/%sp1", disc);
 	else
@@ -118,4 +123,74 @@ void start_bootconfig(void)
 	eval("umount", "/boot");
 
 	return;
+}
+
+void start_bootconfig_efi(void)
+{
+	char *disc = getdisc();
+	char args[512] = { 0 };
+	char dev[64];
+
+	insmod("nls_base nls_cp932 nls_cp936 nls_cp950 nls_cp437 nls_iso8859-1 nls_iso8859-2 nls_utf8");
+	insmod("fat");
+	insmod("vfat");
+	insmod("msdos");
+	makeargs(args);
+
+	if (strlen(disc) == 7)	//mmcblk0 / nvme0n1
+		sprintf(dev, "/dev/%sp1", disc);
+	else
+		sprintf(dev, "/dev/%s1", disc);
+	free(disc);
+	eval("mount", "-t", "vfat", dev, "/boot");
+	FILE *in = fopen("/boot/boot/grub/grub.cfg", "rb");
+	if (!in)
+		return;
+	char serial[64];
+	fscanf(in, "%s", serial);
+	fclose(in);
+	FILE *out = fopen("/boot/boot/grub/grub.cfg", "wb");
+	if (!out)
+		return;
+	char *vga = " fbcon=nodefer vga=0x305 video=efifb:1024x768x32";
+	if (!strncmp(serial, "serial", 6)) {
+		fprintf(out, "serial --unit=0 --speed=115200 --word=8 --parity=no --stop=1\n");
+		fprintf(out, "terminal_input console serial; terminal_output console serial\n");
+		fprintf(out, "\n");
+		vga = " video=vga16fb:off nofb console=ttyS0,115200n8";
+	}
+
+	fprintf(out, "set default=\"0\"\n");
+	fprintf(out, "set timeout=\"5\"\n");
+	fprintf(out, "search -l kernel -s root\n");
+	fprintf(out, "\n");
+
+	if (strlen(args)) {
+
+		fprintf(out, " menuentry \"DD-WRT\" {\n");
+		fprintf(out, " 	set gfxpayload=keep\n");
+		fprintf(out, " 	linux /boot/vmlinuz root=/dev/hda2 rootfstype=squashfs noinitrd%sinitcall_blacklist=acpi_cpufreq_init reboot=bios rootdelay=5%s\n", vga, args);
+		fprintf(out, " }\n");
+	}
+
+	fprintf(out, " menuentry \"default\" {\n");
+	fprintf(out, " 	set gfxpayload=keep\n");
+	fprintf(out, " 	linux /boot/vmlinuz root=/dev/hda2 rootfstype=squashfs noinitrd%sinitcall_blacklist=acpi_cpufreq_init reboot=bios rootdelay=5\n", vga);
+	fprintf(out, " }\n");
+
+	fprintf(out, "menuentry \"MEMTEST86\" {\n");
+	fprintf(out, "	set root='hd0,gpt1'\n");
+	fprintf(out, "	chainloader ($root)/efi/memtest86/memtest64.efi\n");
+	fprintf(out, "}\n");
+
+	fclose(out);
+	eval("umount", "/boot");
+
+	return;
+}
+
+void start_bootconfig(void)
+{
+	start_bootconfig_legacy();
+	start_bootconfig_efi();
 }
