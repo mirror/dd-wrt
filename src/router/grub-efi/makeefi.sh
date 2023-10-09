8c1a917d7bad16724a5394692dcd4309d82cd27c
BrainSlayer <s.gottschall@dd-wrt.com>
2023-10-09 16:35:33 +0000

    increase efi partition
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@53589 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/grub-efi/makeefi.sh
+++ src/router/grub-efi/makeefi.sh
@@ -23,7 +23,7 @@ echo "menuentry \"MEMTEST86\" {" >> tmp/boot/grub/grub.cfg
 echo "	set root='hd0,gpt1'" >> tmp/boot/grub/grub.cfg
 echo "	chainloader (\$root)/efi/memtest86/memtest64.efi" >> tmp/boot/grub/grub.cfg
 echo "}" >> tmp/boot/grub/grub.cfg
-PADDING="1" SIGNATURE="$IMG_PART_SIGNATURE" GUID="$IMG_PART_DISKGUID" ./scripts/gen_image_generic.sh efi.img 16 tmp 320 ../x86_64-uclibc/root.fs 256 128
+PADDING="1" SIGNATURE="$IMG_PART_SIGNATURE" GUID="$IMG_PART_DISKGUID" ./scripts/gen_image_generic.sh efi.img 32 tmp 320 ../x86_64-uclibc/root.fs 256 128
 
 rm -rf imp
 mkdir -p img
@@ -38,7 +38,7 @@ grub-tools/grub-bios-setup -m "img/device.map" -d "img" \
 ./trunc efi.img efi.tmp
 mv -f efi.tmp efi.img 
 
-PADDING="1" SIGNATURE="$IMG_PART_SIGNATURE" GUID="$IMG_PART_DISKGUID" ./scripts/gen_image_generic.sh efi-big.img 16 tmp 320 ../x86_64-uclibc/root.fs 256 1780
+PADDING="1" SIGNATURE="$IMG_PART_SIGNATURE" GUID="$IMG_PART_DISKGUID" ./scripts/gen_image_generic.sh efi-big.img 32 tmp 320 ../x86_64-uclibc/root.fs 256 1780
 
 rm -rf imp
 mkdir -p img
@@ -70,7 +70,7 @@ echo "	set root='hd0,gpt1'" >> tmp/boot/grub/grub.cfg
 echo "	chainloader (\$root)/efi/memtest86/memtest64.efi" >> tmp/boot/grub/grub.cfg
 echo "}" >> tmp/boot/grub/grub.cfg
 
-PADDING="1" SIGNATURE="$IMG_PART_SIGNATURE" GUID="$IMG_PART_DISKGUID" ./scripts/gen_image_generic.sh efi-vga.img 16 tmp 320 ../x86_64-uclibc/root.fs 256 128
+PADDING="1" SIGNATURE="$IMG_PART_SIGNATURE" GUID="$IMG_PART_DISKGUID" ./scripts/gen_image_generic.sh efi-vga.img 32 tmp 320 ../x86_64-uclibc/root.fs 256 128
 rm -rf imp
 mkdir -p img
 cp boot/grub/boot.img img
@@ -85,7 +85,7 @@ grub-tools/grub-bios-setup -m "img/device.map" -d "img" \
 mv -f efi-vga.tmp efi-vga.img 
 
 
-PADDING="1" SIGNATURE="$IMG_PART_SIGNATURE" GUID="$IMG_PART_DISKGUID" ./scripts/gen_image_generic.sh efi-vga-big.img 16 tmp 320 ../x86_64-uclibc/root.fs 256 1780
+PADDING="1" SIGNATURE="$IMG_PART_SIGNATURE" GUID="$IMG_PART_DISKGUID" ./scripts/gen_image_generic.sh efi-vga-big.img 32 tmp 320 ../x86_64-uclibc/root.fs 256 1780
 rm -rf imp
 mkdir -p img
 cp boot/grub/boot.img img
