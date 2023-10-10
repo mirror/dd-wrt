862c672b92fa8391c8863319775ea1c21b36935c
BrainSlayer <s.gottschall@dd-wrt.com>
2023-10-10 16:25:31 +0000

    fix for 6.1
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@53602 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/netconf/netconf_linux.c
+++ src/router/netconf/netconf_linux.c
@@ -765,6 +765,36 @@ insert_entry(const char *chain, struct ipt_entry *entry, iptc_handle_t *handle)
 		return iptc_append_entry(chain, entry, handle);
 }
 
+#ifndef NFC_IP_SRC
+/* IP Cache bits. */
+/* Src IP address. */
+#define NFC_IP_SRC		0x0001
+/* Dest IP address. */
+#define NFC_IP_DST		0x0002
+/* Input device. */
+#define NFC_IP_IF_IN		0x0004
+/* Output device. */
+#define NFC_IP_IF_OUT		0x0008
+/* TOS. */
+#define NFC_IP_TOS		0x0010
+/* Protocol. */
+#define NFC_IP_PROTO		0x0020
+/* IP options. */
+#define NFC_IP_OPTIONS		0x0040
+/* Frag & flags. */
+#define NFC_IP_FRAG		0x0080
+
+/* Per-protocol information: only matters if proto match. */
+/* TCP flags. */
+#define NFC_IP_TCPFLAGS		0x0100
+/* Source port. */
+#define NFC_IP_SRC_PT		0x0200
+/* Dest port. */
+#define NFC_IP_DST_PT		0x0400
+/* Something else about the proto */
+#define NFC_IP_PROTO_UNKNOWN	0x2000
+#endif
+
 /*
  * Add a firewall entry
  * @param	fw	firewall entry
