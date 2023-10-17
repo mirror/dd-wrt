b66b6df8cec0244d200acecccdb9ccf547de2db6
BrainSlayer <s.gottschall@dd-wrt.com>
2023-10-17 02:35:34 +0000

    allow gpio sharing by sysfs. so we can control the power of the usbports for wrt1900 and such devices
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@53682 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.1/drivers/pinctrl/pinmux.c
+++ src/linux/universal/linux-6.1/drivers/pinctrl/pinmux.c
@@ -124,7 +124,7 @@ static int pin_request(struct pinctrl_dev *pctldev,
 	dev_dbg(pctldev->dev, "request pin %d (%s) for %s\n",
 		pin, desc->name, owner);
 
-	if ((!gpio_range || ops->strict) &&
+/*	if ((!gpio_range || ops->strict) &&
 	    desc->mux_usecount && strcmp(desc->mux_owner, owner)) {
 		dev_err(pctldev->dev,
 			"pin %s already requested by %s; cannot claim for %s\n",
@@ -137,7 +137,7 @@ static int pin_request(struct pinctrl_dev *pctldev,
 			"pin %s already requested by %s; cannot claim for %s\n",
 			desc->name, desc->gpio_owner, owner);
 		goto out;
-	}
+	}*/
 
 	if (gpio_range) {
 		desc->gpio_owner = owner;
