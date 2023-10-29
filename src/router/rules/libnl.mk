9e93d28ee49d32a4ef59b329fcd5d30df7a70c2a
BrainSlayer <s.gottschall@dd-wrt.com>
2023-10-29 10:09:59 +0000

    hack for newer libtool
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@53786 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libnl.mk
+++ src/router/rules/libnl.mk
@@ -3,7 +3,7 @@ libnl-configure:
 	cd libnl && ./configure --prefix=/usr --host=$(ARCH)-linux --disable-shared --enable-static --disable-debug --enable-cli=no \
 	    CFLAGS="$(COPTS) $(LTO) $(MIPS16_OPT) -D_GNU_SOURCE -Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	    LDFLAGS="$(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	    AR_FLAGS="cru $(LTOPLUGIN)" \
+	    AR_FLAGS="\"cru $(LTOPLUGIN)\"" \
 	    RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
 	$(MAKE) -C libnl
 
