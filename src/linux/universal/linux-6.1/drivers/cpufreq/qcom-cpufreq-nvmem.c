6a371455e54a30d61e631144289ba3642fc751b0
BrainSlayer <s.gottschall@dd-wrt.com>
2023-10-31 15:53:44 +0000

    revert
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@53838 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.1/drivers/cpufreq/qcom-cpufreq-nvmem.c
+++ src/linux/universal/linux-6.1/drivers/cpufreq/qcom-cpufreq-nvmem.c
@@ -34,14 +34,6 @@
 #define IPQ8074_HAWKEYE_VERSION		BIT(0)
 #define IPQ8074_ACORN_VERSION		BIT(1)
 
-enum ipq806x_versions {
-	IPQ8062_VERSION = 0,
-	IPQ8064_VERSION,
-	IPQ8065_VERSION,
-};
-
-#define IPQ6000_VERSION	BIT(2)
-
 struct qcom_cpufreq_drv;
 
 struct qcom_cpufreq_match_data {
@@ -60,26 +52,8 @@ struct qcom_cpufreq_drv {
 
 static struct platform_device *cpufreq_dt_pdev, *cpufreq_pdev;
 
-static int qcom_cpufreq_simple_get_version(struct device *cpu_dev,
-					   struct nvmem_cell *speedbin_nvmem,
-					   char **pvs_name,
-					   struct qcom_cpufreq_drv *drv)
-{
-	u8 *speedbin;
-
-	*pvs_name = NULL;
-	speedbin = nvmem_cell_read(speedbin_nvmem, NULL);
-	if (IS_ERR(speedbin))
-		return PTR_ERR(speedbin);
-
-	dev_dbg(cpu_dev, "speedbin: %d\n", *speedbin);
-	drv->versions = 1 << *speedbin;
-	kfree(speedbin);
-	return 0;
-}
-
 static void get_krait_bin_format_a(struct device *cpu_dev,
-					  int *speed, int *pvs,
+					  int *speed, int *pvs, int *pvs_ver,
 					  u8 *buf)
 {
 	u32 pte_efuse;
@@ -210,7 +184,8 @@ static int qcom_cpufreq_krait_name_version(struct device *cpu_dev,
 
 	switch (len) {
 	case 4:
-		get_krait_bin_format_a(cpu_dev, &speed, &pvs, speedbin);
+		get_krait_bin_format_a(cpu_dev, &speed, &pvs, &pvs_ver,
+				       speedbin);
 		break;
 	case 8:
 		get_krait_bin_format_b(cpu_dev, &speed, &pvs, &pvs_ver,
@@ -267,125 +242,12 @@ static int qcom_cpufreq_ipq8074_name_version(struct device *cpu_dev,
 	return 0;
 }
 
-static int qcom_cpufreq_ipq8064_name_version(struct device *cpu_dev,
-					     struct nvmem_cell *speedbin_nvmem,
-					     char **pvs_name,
-					     struct qcom_cpufreq_drv *drv)
-{
-	int speed = 0, pvs = 0;
-	int msm_id, ret = 0;
-	u8 *speedbin;
-	size_t len;
-
-	speedbin = nvmem_cell_read(speedbin_nvmem, &len);
-	if (IS_ERR(speedbin))
-		return PTR_ERR(speedbin);
-
-	if (len != 4) {
-		dev_err(cpu_dev, "Unable to read nvmem data. Defaulting to 0!\n");
-		ret = -ENODEV;
-		goto exit;
-	}
-
-	get_krait_bin_format_a(cpu_dev, &speed, &pvs, speedbin);
-
-	ret = qcom_smem_get_soc_id(&msm_id);
-	if (ret)
-		goto exit;
-
-	switch (msm_id) {
-	case QCOM_ID_IPQ8062:
-		drv->versions = BIT(IPQ8062_VERSION);
-		break;
-	case QCOM_ID_IPQ8064:
-	case QCOM_ID_IPQ8066:
-	case QCOM_ID_IPQ8068:
-		drv->versions = BIT(IPQ8064_VERSION);
-		break;
-	case QCOM_ID_IPQ8065:
-	case QCOM_ID_IPQ8069:
-		drv->versions = BIT(IPQ8065_VERSION);
-		break;
-	default:
-		dev_err(cpu_dev,
-			"SoC ID %u is not part of IPQ8064 family, limiting to 1.0GHz!\n",
-			msm_id);
-		drv->versions = BIT(IPQ8062_VERSION);
-		break;
-	}
-
-	/* IPQ8064 speed is never fused. Only pvs values are fused. */
-	snprintf(*pvs_name, sizeof("speed0-pvsXX"), "speed0-pvs%d", pvs);
-
-exit:
-	kfree(speedbin);
-	return ret;
-}
-
-static int qcom_cpufreq_ipq6018_name_version(struct device *cpu_dev,
-					     struct nvmem_cell *speedbin_nvmem,
-					     char **pvs_name,
-					     struct qcom_cpufreq_drv *drv)
-{
-	u32 msm_id;
-	int ret;
-	u8 *speedbin;
-	*pvs_name = NULL;
-
-	ret = qcom_smem_get_soc_id(&msm_id);
-	if (ret)
-		return ret;
-
-	speedbin = nvmem_cell_read(speedbin_nvmem, NULL);
-	if (IS_ERR(speedbin))
-		return PTR_ERR(speedbin);
-
-	switch (msm_id) {
-	case QCOM_ID_IPQ6005:
-	case QCOM_ID_IPQ6010:
-	case QCOM_ID_IPQ6018:
-	case QCOM_ID_IPQ6028:
-		/* Fuse Value    Freq    BIT to set
-		 * ---------------------------------
-		 *   2’b0     No Limit     BIT(0)
-		 *   2’b1     1.5 GHz      BIT(1)
-		 */
-		drv->versions = 1 << (unsigned int)(*speedbin);
-		break;
-	case QCOM_ID_IPQ6000:
-		/*
-		 * IPQ6018 family only has one bit to advertise the CPU
-		 * speed-bin, but that is not enough for IPQ6000 which
-		 * is only rated up to 1.2GHz.
-		 * So for IPQ6000 manually set BIT(2) based on SMEM ID.
-		 */
-		drv->versions = IPQ6000_VERSION;
-		break;
-	default:
-		dev_err(cpu_dev,
-			"SoC ID %u is not part of IPQ6018 family, limiting to 1.2GHz!\n",
-			msm_id);
-		drv->versions = IPQ6000_VERSION;
-		break;
-	}
-
-	kfree(speedbin);
-	return 0;
-}
-
-static const char *generic_genpd_names[] = { "perf", NULL };
-
 static const struct qcom_cpufreq_match_data match_data_kryo = {
 	.get_version = qcom_cpufreq_kryo_name_version,
 };
 
 static const struct qcom_cpufreq_match_data match_data_krait = {
 	.get_version = qcom_cpufreq_krait_name_version,
-};
-
-static const struct qcom_cpufreq_match_data match_data_msm8909 = {
-	.get_version = qcom_cpufreq_simple_get_version,
-	.genpd_names = generic_genpd_names,
 };
 
 static const char *qcs404_genpd_names[] = { "cpr", NULL };
@@ -396,14 +258,6 @@ static const struct qcom_cpufreq_match_data match_data_qcs404 = {
 
 static const struct qcom_cpufreq_match_data match_data_ipq8074 = {
 	.get_version = qcom_cpufreq_ipq8074_name_version,
-};
-
-static const struct qcom_cpufreq_match_data match_data_ipq6018 = {
-	.get_version = qcom_cpufreq_ipq6018_name_version,
-};
-
-static const struct qcom_cpufreq_match_data match_data_ipq8064 = {
-	.get_version = qcom_cpufreq_ipq8064_name_version,
 };
 
 static int qcom_cpufreq_probe(struct platform_device *pdev)
@@ -426,8 +280,7 @@ static int qcom_cpufreq_probe(struct platform_device *pdev)
 	if (!np)
 		return -ENOENT;
 
-	ret = of_device_is_compatible(np, "operating-points-v2-kryo-cpu") ||
-	      of_device_is_compatible(np, "operating-points-v2-krait-cpu");
+	ret = of_device_is_compatible(np, "operating-points-v2-kryo-cpu");
 	if (!ret) {
 		of_node_put(np);
 		return -ENOENT;
@@ -549,11 +402,9 @@ static struct platform_driver qcom_cpufreq_driver = {
 
 static const struct of_device_id qcom_cpufreq_match_list[] __initconst = {
 	{ .compatible = "qcom,apq8096", .data = &match_data_kryo },
-	{ .compatible = "qcom,msm8909", .data = &match_data_msm8909 },
 	{ .compatible = "qcom,msm8996", .data = &match_data_kryo },
 	{ .compatible = "qcom,qcs404", .data = &match_data_qcs404 },
-	{ .compatible = "qcom,ipq6018", .data = &match_data_ipq6018 },
-	{ .compatible = "qcom,ipq8064", .data = &match_data_ipq8064 },
+	{ .compatible = "qcom,ipq8064", .data = &match_data_krait },
 	{ .compatible = "qcom,ipq8074", .data = &match_data_ipq8074 },
 	{ .compatible = "qcom,apq8064", .data = &match_data_krait },
 	{ .compatible = "qcom,msm8974", .data = &match_data_krait },
