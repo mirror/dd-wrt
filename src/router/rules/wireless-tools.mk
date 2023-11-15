3a89776f238e5ee2a13de314dcb819df3ee351e0
BrainSlayer <s.gottschall@dd-wrt.com>
2023-11-15 05:06:05 +0000

    just s small fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@54041 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/wireless-tools.mk
+++ src/router/rules/wireless-tools.mk
@@ -7,8 +7,8 @@ wireless-tools:
 wireless-tools-install:
 ifneq ($(CONFIG_JUSTLIBIW),y)
 ifneq ($(CONFIG_LSX),y)
-	make -C wireless-tools INSTALL_DIR=$(INSTALLDIR)/wireless-tools/usr/sbin install-iwmulticall
+	make -C wireless-tools CC="$(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT)" INSTALL_DIR=$(INSTALLDIR)/wireless-tools/usr/sbin install-iwmulticall
 endif
 endif
-	make -C wireless-tools INSTALL_LIB=$(INSTALLDIR)/wireless-tools/lib install-dynamic
+	make -C wireless-tools CC="$(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT)" INSTALL_LIB=$(INSTALLDIR)/wireless-tools/lib install-dynamic
 
