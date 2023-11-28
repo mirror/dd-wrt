c5dd20d6789e78d40696828b5c893dd648d03a08
BrainSlayer <s.gottschall@dd-wrt.com>
2023-11-28 08:20:58 +0000

    fix typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@54236 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/wifi/madwifi.c
+++ src/router/services/networking/wifi/madwifi.c
@@ -1502,11 +1502,11 @@ void setupHostAPPSK(FILE * fp, char *prefix, int isfirst)
 		char dl[32];
 		fprintf(fp, "nas_identifier=%s\n", nvram_nget("%s_nas", prefix));
 		fprintf(fp, "mobility_domain=%s\n", nvram_nget("%s_domain", prefix));
-		sprintf("%s_ft_over_ds", prefix);
+		sprintf(dl, "%s_ft_over_ds", prefix);
 		fprintf(fp, "ft_over_ds=%s\n", nvram_default_geti(dl, 0));
 		fprintf(fp, "ft_psk_generate_local=1\n");
 		fprintf(fp, "pmk_r1_push=1\n");
-		sprintf("%s_deadline", prefix);
+		sprintf(dl, "%s_deadline", prefix);
 		fprintf(fp, "reassociation_deadline=%s\n", nvram_default_geti(dl, 1000));
 		// todo. add key holders
 	}
