868795ed2469750aff37be6b70f09cfe4cd06623
BrainSlayer <s.gottschall@dd-wrt.com>
2023-11-28 03:47:16 +0000

    spelling
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@54214 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/dd-wrt/lang_pack/english.js
+++ src/router/kromo/dd-wrt/lang_pack/english.js
@@ -2724,7 +2724,7 @@ wpa.rrm_beacon_report="Beacon report via radio";
 wpa.proxy_arp="Proxy ARP";
 wpa.time_advertisement="Time advertisement";
 wpa.mbo_cell_data_conn_pref="Cellular Connection Preference";
-wpa.reassociation_deadline="Reassociation deadline";
+wpa.reassociation_deadline="Reassociation Deadline";
 
 var aoss=new Object();
 aoss.titl="AOSS Security";
