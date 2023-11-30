08d50c1439aa29c53539810eceb29cb65c696695
BrainSlayer <s.gottschall@dd-wrt.com>
2023-11-30 06:06:08 +0000

    fix usteer install
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@54285 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/usteer.mk
+++ src/router/rules/usteer.mk
@@ -10,11 +10,11 @@ USTEER_CMAKE_OPTIONS+=VERBOSE=0 -DBUILD_LUA=OFF \
 USTEER_EXTRA_CFLAGS=-I$(TOP) -I$(TOP)/libpcap -I$(STAGING_DIR)/usr/include -L$(STAGING_DIR)/usr/lib  $(MIPS16_OPT) -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections -flto=auto -fno-fat-lto-objects
 USTEER_EXTRA_LDFLAGS=-L$(TOP)/libpcap -ljson-c -L$(TOP)/libubox/  -ffunction-sections -fdata-sections -Wl,--gc-sections -fuse-ld=bfd -flto=auto -fuse-linker-plugin
 
-usteer-configure: 
+usteer-configure: libubox ubus
 	$(call CMakeClean,$(USTEER_PKG_BUILD_DIR))
 	$(call CMakeConfigure,$(USTEER_PKG_BUILD_DIR),$(USTEER_STAGING_DIR),$(USTEER_CMAKE_OPTIONS),$(USTEER_EXTRA_CFLAGS),$(USTEER_EXTRA_LDFLAGS),.) 
 
-usteer: json-c libubox usteer-configure
+usteer: json-c libubox ubus
 	$(MAKE) -C usteer
 #	-install -D usteer/libusteer.so $(STAGING_DIR)/usr/lib/libusteer.so
 #	-cp usteer/usteermsg.h $(STAGING_DIR)/usr/include/
@@ -22,9 +22,9 @@ usteer: json-c libubox usteer-configure
 #	-cp usteer/libusteer.h $(STAGING_DIR)/usr/include/
 
 usteer-install:
-#	install -D usteer/libusteer.so $(INSTALLDIR)/usteer/usr/lib/libusteer.so
-#	install -D usteer/usteer $(INSTALLDIR)/usteer/usr/sbin/usteer
-#	install -D usteer/usteerd $(INSTALLDIR)/usteer/usr/sbin/usteerd
+	install -D usteer/ap-monitor $(INSTALLDIR)/usteer/usr/lib/ap-monitor
+	install -D usteer/fakeap $(INSTALLDIR)/usteer/usr/sbin/fakeap
+	install -D usteer/usteerd $(INSTALLDIR)/usteer/usr/sbin/usteerd
 
 usteer-clean:
 	if [ -e "$(USTEER_PKG_BUILD_DIR)/Makefile" ]; then $(MAKE) -C usteer clean ; fi
