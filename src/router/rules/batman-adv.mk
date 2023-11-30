b8ba7975600c4c602cd144c533ae987be877ab6b
BrainSlayer <s.gottschall@dd-wrt.com>
2023-11-30 03:44:46 +0000

    build fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@54265 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/batman-adv.mk
+++ src/router/rules/batman-adv.mk
@@ -1,26 +1,14 @@
 batman-adv:
-ifneq ($(KERNELVERSION),6.1)
 	$(MAKE) -C batman-adv KERNELPATH="$(LINUXDIR)" all
-	$(MAKE) -C batctl all CC="$(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="$(COPTS) -ffunction-sections -fdata-sections -Wl,--gc-sections -lm"
-else
-	@true
-endif
+	$(MAKE) -C batctl all CC="$(CC)" CFLAGS="$(COPTS) $(LTO) $(MIPS16_OPT) -I$(TOP)/libnl-tiny/include -D_GNU_SOURCE -DNEED_PRINTF -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="$(COPTS) $(LDLTO) -L$(TOP)/libnl-tiny -lnl-tiny -ffunction-sections -fdata-sections -Wl,--gc-sections -lm"
 
 batman-adv-clean:
-ifneq ($(KERNELVERSION),6.1)
 	$(MAKE) -C batman-adv KERNELPATH="$(LINUXDIR)" clean
-	$(MAKE) -C batctl CC="$(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT) -D_GNU_SOURCE -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="$(COPTS) -ffunction-sections -fdata-sections -Wl,--gc-sections -lm" clean
+	$(MAKE) -C batctl CC="$(CC)" CFLAGS="$(COPTS) $(LTO) $(MIPS16_OPT) -I$(TOP)/libnl-tiny/include -D_GNU_SOURCE -ffunction-sections -fdata-sections -Wl,--gc-sections" LDFLAGS="$(COPTS) $(LDLTO) -L$(TOP)/libnl-tiny -lnl-tiny -ffunction-sections -fdata-sections -Wl,--gc-sections -lm" clean
 	@true
-else
-	@true
-endif
 
 batman-adv-install:
-ifneq ($(KERNELVERSION),6.1)
-	install -D batman-adv/batman-adv.ko $(INSTALLDIR)/batman-adv/lib/batman-adv/batman-adv.ko
+	install -D batman-adv/net/batman-adv/batman-adv.ko $(INSTALLDIR)/batman-adv/lib/batman-adv/batman-adv.ko
 	$(ARCH)-linux-strip --strip-unneeded --remove-section=.comment --remove-section=.pdr --remove-section=.mdebug.abi32 $(INSTALLDIR)/batman-adv/lib/batman-adv/*
 	install -D batctl/batctl $(INSTALLDIR)/batman-adv/usr/sbin/batctl
 	install -D batctl/config/batstart.sh $(INSTALLDIR)/batman-adv/usr/sbin/batstart.sh
-else
-	@true
-endif
