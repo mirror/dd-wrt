b35c0b55a1d6725940931a790356afe1f2e096eb
BrainSlayer <s.gottschall@dd-wrt.com>
2023-12-01 04:42:56 +0000

    set default config
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@54318 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/usteer.c
+++ src/router/services/services/usteer.c
@@ -33,7 +33,66 @@ void start_usteer(void)
 	// Only start if enabled
 	if (!nvram_invmatchi("usteer_enable", 0))
 		return;
+	char *config;
+
+	asprintf(&config, "{"	//
+		 "\"syslog\": true,"	//
+		 "\"debug_level\": 1,"	//
+		 "\"ipv6\": false,"	//
+		 "\"local_mode\": false,"	//
+		 "\"sta_block_timeout\": 30000,"	//
+		 "\"local_sta_timeout\": 120000,"	//
+		 "\"local_sta_update\": 1000,"	//
+		 "\"max_neighbor_reports\": 8,"	//
+		 "\"max_retry_band\": 5,"	//
+		 "\"seen_policy_timeout\": 30000,"	//
+		 "\"measurement_report_timeout\": 120000,"	//
+		 "\"load_balancing_threshold\": 0,"	//
+		 "\"band_steering_threshold\": 5,"	//
+		 "\"remote_update_interval\": 1000,"	//
+		 "\"remote_node_timeout\": 10,"	//
+		 "\"assoc_steering\": false,"	//
+		 "\"min_connect_snr\": 0,"	//
+		 "\"min_snr\": 0,"	//
+		 "\"min_snr_kick_delay\": 5000,"	//
+		 "\"steer_reject_timeout\": 60000,"	//
+		 "\"roam_process_timeout\": 5000,"	//
+		 "\"roam_scan_snr\": 0,"	//
+		 "\"roam_scan_tries\": 3,"	//
+		 "\"roam_scan_timeout\": 0,"	//
+		 "\"roam_scan_interval\": 10000,"	//
+		 "\"roam_trigger_snr\": 0,"	//
+		 "\"roam_trigger_interval\": 60000,"	//
+		 "\"roam_kick_delay\": 10000,"	//
+		 "\"signal_diff_threshold\": 0,"	//
+		 "\"initial_connect_delay\": 0,"	//
+		 "\"load_kick_enabled\": false,"	//
+		 "\"load_kick_threshold\": 75,"	//
+		 "\"load_kick_delay\": 10000,"	//
+		 "\"load_kick_min_clients\": 10,"	//
+		 "\"load_kick_reason_code\": 5,"	//
+		 "\"band_steering_interval\": 120000,"	//
+		 "\"band_steering_min_snr\": -60,"	//
+		 "\"link_measurement_interval\": 30000,"	//
+		 "\"interfaces\": [ "	//
+		 "\"br0\" "	//
+		 "],"		//
+		 "\"event_log_types\": ["	//
+		 "\"auth_req_deny\","	//
+		 "\"assoc_req_deny\","	//
+		 "\"load_kick_client\","	//
+		 "\"signal_kick\" "	//
+		 "] "		//
+		 "} ");
+	char *cmdline;
+	asprintf(&cmdline, "ubus call usteer set_config \"%s\"", config);
 	sysprintf("usteerd -i br0 -s -v 1&");
+	// wait until usteer started
+	eval("ubus", "-t", "10", "wait_for", "usteer");
+	system(cmdline);
+	free(cmdline);
+	free(config);
+
 	cprintf("done\n");
 	return;
 }
