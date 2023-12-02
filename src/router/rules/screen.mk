4c35f6626107160c834712cf728e1b1456c4cbe6
BrainSlayer <s.gottschall@dd-wrt.com>
2023-12-02 09:03:14 +0000

    add lto
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@54386 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/screen.mk
+++ src/router/rules/screen.mk
@@ -1,6 +1,6 @@
 screen-configure: ncurses
 	cd screen && autoreconf --force --install --symlink
-	cd screen && ./configure --host=$(ARCH)-linux --enable-colors256 --with-sys-screenrc=/etc/screenrc CFLAGS="$(COPTS) $(MIPS16_OPT)  -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF -I$(TOP)/ncurses/include" LDFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections -L$(TOP)/ncurses/lib" --prefix=/usr ac_cv_safe_to_define___extensions__=no
+	cd screen && ./configure --host=$(ARCH)-linux --enable-colors256 --with-sys-screenrc=/etc/screenrc CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF -I$(TOP)/ncurses/include" LDFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections $(LDLTO) -L$(TOP)/ncurses/lib" --prefix=/usr ac_cv_safe_to_define___extensions__=no
 
 screen: ncurses
 	make   -C screen
