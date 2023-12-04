6506bf12a69ac08627d8262a78b4a630de220986
BrainSlayer <s.gottschall@dd-wrt.com>
2023-12-04 08:30:06 +0000

    these values are snr not rssi
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@54423 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/usteer.c
+++ src/router/services/services/usteer.c
@@ -159,15 +159,15 @@ void start_usteer(void)
 		 nvram_default_geti("usteer_remote_node_timeout", 50), //
 		 nvram_default_geti("usteer_assoc_steering", 0) ? "true" : "false", //
 		 nvram_default_geti("usteer_min_connect_snr", 0), //
-		 nvram_default_geti("usteer_min_snr", -82), //
+		 nvram_default_geti("usteer_min_snr", 15), //
 		 nvram_default_geti("usteer_min_snr_kick_delay", 5000), //
 		 nvram_default_geti("usteer_steer_reject_timeout", 60000), //
 		 nvram_default_geti("usteer_roam_process_timeout", 5000), //
-		 nvram_default_geti("usteer_roam_scan_snr", -70), //
+		 nvram_default_geti("usteer_roam_scan_snr", 25), //
 		 nvram_default_geti("usteer_roam_scan_tries", 6), //
 		 nvram_default_geti("usteer_roam_scan_timeout", 60000), //
 		 nvram_default_geti("usteer_roam_scan_interval", 15000), //
-		 nvram_default_geti("usteer_roam_trigger_snr", -75), //
+		 nvram_default_geti("usteer_roam_trigger_snr", 20), //
 		 nvram_default_geti("usteer_roam_trigger_interval", 180000), //
 		 nvram_default_geti("usteer_roam_kick_delay", 100), //
 		 nvram_default_geti("usteer_signal_diff_threshold", 12), //
@@ -178,7 +178,7 @@ void start_usteer(void)
 		 nvram_default_geti("usteer_load_kick_min_clients", 10), //
 		 nvram_default_geti("usteer_load_kick_reason_code", 5), //
 		 nvram_default_geti("usteer_band_steering_interval", 120000), //
-		 nvram_default_geti("usteer_band_steering_min_snr", -60), //
+		 nvram_default_geti("usteer_band_steering_min_snr", 25), //
 		 nvram_default_geti("usteer_link_measurement_interval", 30000), //
 		 ssid_list);
 	char *cmdline;
