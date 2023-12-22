33955ec613a960e16bdf50f9b7e51befbce23420
BrainSlayer <s.gottschall@dd-wrt.com>
2023-12-22 02:39:56 +0000

    use standard structures
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@54579 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/openssl/include/openssl/des.h
+++ src/router/openssl/include/openssl/des.h
@@ -46,9 +46,6 @@ typedef struct DES_ks {
          */
         DES_LONG deslong[2];
     } ks[16];
-#ifdef OCTEON_OPENSSL
-    uint64_t cvmkey;
-#endif
 } DES_key_schedule;
 
 # define DES_KEY_SZ      (sizeof(DES_cblock))
