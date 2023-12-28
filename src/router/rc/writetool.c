0b295d4b0a7d8c56f76a094ba1fcac9c08f8d00f
BrainSlayer <s.gottschall@dd-wrt.com>
2023-12-28 15:11:45 +0000

    fix missing macros
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@54630 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/writetool.c
+++ src/router/rc/writetool.c
@@ -15,10 +15,16 @@
 #define cpu_to_le16(x) bswap_16(x)
 #define cpu_to_le32(x) bswap_32(x)
 #define cpu_to_le64(x) bswap_64(x)
+#define le16_to_cpu(x) bswap_16(x)
+#define le32_to_cpu(x) bswap_32(x)
+#define le64_to_cpu(x) bswap_64(x)
 #elif __BYTE_ORDER == __LITTLE_ENDIAN
 #define cpu_to_le16(x) (x)
 #define cpu_to_le32(x) (x)
 #define cpu_to_le64(x) (x)
+#define le16_to_cpu(x) (x)
+#define le32_to_cpu(x) (x)
+#define le64_to_cpu(x) (x)
 #else
 #error unknown endianness!
 #endif
