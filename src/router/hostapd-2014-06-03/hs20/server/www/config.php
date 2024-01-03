4879f2f6e4b37852aa42569c32debc3730bafb56
BrainSlayer <s.gottschall@dd-wrt.com>
2024-01-03 16:13:05 +0000

    cleanup old unused trees
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@54702 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/hostapd-2014-06-03/hs20/server/www/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-$osu_root = "/home/user/hs20-server";
-$osu_db = "sqlite:$osu_root/AS/DB/eap_user.db";
-?>
--- src/router/hostapd-2014-10-25/tests/hwsim/vm/uevent.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# assume this was a call for CRDA,
-# if not then it won't find a COUNTRY
-# environment variable and exit
-exec crda
