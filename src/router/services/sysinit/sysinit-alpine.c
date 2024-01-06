8e245286639b8ea2854355c78b20dd4770982884
BrainSlayer <s.gottschall@dd-wrt.com>
2024-01-06 02:42:54 +0000

    correct led color
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@54744 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-alpine.c
+++ src/router/services/sysinit/sysinit-alpine.c
@@ -243,12 +243,12 @@ void start_sysinit(void)
 	eval("ssdk_sh_id", "1", "vlan", "entry", "create", "2");
 
 	eval("ssdk_sh_id", "0", "debug", "reg", "set", "0x50", "0xcc35cc35", "4");
-	eval("ssdk_sh_id", "0", "debug", "reg", "set", "0x54", "0xca35cc35", "4");
+	eval("ssdk_sh_id", "0", "debug", "reg", "set", "0x54", "0xca35ca35", "4");
 	eval("ssdk_sh_id", "0", "debug", "reg", "set", "0x58", "0xc935c935", "4");
 	eval("ssdk_sh_id", "0", "debug", "reg", "set", "0x5c", "0x03ffff00", "4");
 
 	eval("ssdk_sh_id", "1", "debug", "reg", "set", "0x50", "0xcc35cc35", "4");
-	eval("ssdk_sh_id", "1", "debug", "reg", "set", "0x54", "0xca35cc35", "4");
+	eval("ssdk_sh_id", "1", "debug", "reg", "set", "0x54", "0xca35ca35", "4");
 	eval("ssdk_sh_id", "1", "debug", "reg", "set", "0x58", "0xc935c935", "4");
 	eval("ssdk_sh_id", "1", "debug", "reg", "set", "0x5c", "0x03ffff00", "4");
 
