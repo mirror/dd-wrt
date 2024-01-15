d1c4ea20ab8a7814c7e64193f2627aee396cb84f
BrainSlayer <s.gottschall@dd-wrt.com>
2024-01-15 20:54:11 +0000

    inlining this is way faster if size is static
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@54947 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.9/include/crypto/algapi.h
+++ src/linux/universal/linux-4.9/include/crypto/algapi.h
@@ -191,7 +191,36 @@ static inline unsigned int crypto_queue_len(struct crypto_queue *queue)
 	return queue->qlen;
 }
 
-void crypto_inc(u8 *a, unsigned int size);
+static inline void crypto_inc_byte(u8 *a, unsigned int size)
+{
+	u8 *b = (a + size);
+	u8 c;
+
+	for (; size; size--) {
+		c = *--b + 1;
+		*b = c;
+		if (c)
+			break;
+	}
+}
+
+static inline void crypto_inc(u8 *a, unsigned int size)
+{
+	__be32 *b = (__be32 *)(a + size);
+	u32 c;
+
+	if (IS_ENABLED(CONFIG_HAVE_EFFICIENT_UNALIGNED_ACCESS) ||
+	    !((unsigned long)b & (__alignof__(*b) - 1)))
+		for (; size >= 4; size -= 4) {
+			c = be32_to_cpu(*--b) + 1;
+			*b = cpu_to_be32(c);
+			if (c)
+				return;
+		}
+
+	crypto_inc_byte(a, size);
+}
+
 void __crypto_xor(u8 *dst, const u8 *src1, const u8 *src2, unsigned int size);
 
 static inline void crypto_xor(u8 *dst, const u8 *src, unsigned int size)
