8b13d1c58eeda82ef5be1e72273c242422601468
BrainSlayer <s.gottschall@dd-wrt.com>
2024-02-05 21:40:53 +0000

    new xfsprogs
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55082 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/xfsprogs.mk
+++ src/router/rules/xfsprogs.mk
@@ -1,7 +1,36 @@
-xfsprogs-configure:
+liburcu-configure:
+	cd liburcu && libtoolize
+	cd liburcu && aclocal
+	cd liburcu && autoconf
+	cd liburcu && autoheader
+	cd liburcu && autoreconf -vfi
+	cd liburcu && ./configure --host=$(ARCH)-linux \
+		CFLAGS="$(COPTS) $(MIPS16_OPT) -DNDEBUG -ffunction-sections -fdata-sections -Wl,--gc-sections -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -I$(TOP)/util-linux/libuuid/src -DNEED_PRINTF" \
+		LDFLAGS="-L$(TOP)/util-linux/.libs -luuid -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+		CC="$(CC) $(COPTS) $(MIPS16_OPT)" \
+		--disable-static \
+		--enable-shared \
+		--prefix=/usr \
+		--libdir=/usr/lib
+
+liburcu:
+	make -C liburcu
+
+liburcu-clean:
+	make -C liburcu clean
+
+liburcu-install:
+	make -C liburcu install DESTDIR=$(INSTALLDIR)/liburcu
+	rm -rf $(INSTALLDIR)/liburcu/usr/share
+	rm -rf $(INSTALLDIR)/liburcu/usr/include
+	rm -rf $(INSTALLDIR)/liburcu/usr/lib/pkgconfig
+	rm -f $(INSTALLDIR)/liburcu/usr/lib/*.la
+
+
+xfsprogs-configure: liburcu-configure liburcu
 	cd xfsprogs && ./configure --host=$(ARCH)-linux \
-		CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -I$(TOP)/util-linux/libuuid/src -DNEED_PRINTF" \
-		LDFLAGS="$(LDLTO) -L$(TOP)/util-linux/.libs -luuid -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+		CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -I$(TOP)/util-linux/libuuid/src -I$(TOP)/liburcu/include -DNEED_PRINTF" \
+		LDFLAGS="$(LDLTO) -L$(TOP)/util-linux/.libs -luuid -L$(TOP)/liburcu/src/.libs -lurcu -lurcu-memb -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 		CC="$(CC) $(COPTS) $(MIPS16_OPT)" \
 		--with-gnu-ld  \
 		--disable-rpath \
@@ -15,7 +44,7 @@ xfsprogs-configure:
 		AR_FLAGS="cru $(LTOPLUGIN)" \
 		RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
 
-xfsprogs:
+xfsprogs: liburcu
 	make -C xfsprogs DEBUG= Q= 
 
 xfsprogs-clean:
