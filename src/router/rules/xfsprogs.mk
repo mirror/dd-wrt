4a09597647a3e94b920343b1c924e0db8ac859f7
BrainSlayer <s.gottschall@dd-wrt.com>
2024-02-06 23:16:17 +0000

    use default install method
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55097 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/xfsprogs.mk
+++ src/router/rules/xfsprogs.mk
@@ -39,8 +39,8 @@ xfsprogs-configure: liburcu-configure liburcu
 		--enable-shared=yes \
 		--enable-static=no \
 		--enable-lib64=no \
+		--prefix=/usr \
 		--libdir=/usr/lib \
-		root_prefix=$(INSTALLDIR)/xfsprogs \
 		AR_FLAGS="cru $(LTOPLUGIN)" \
 		RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
 
@@ -51,19 +51,7 @@ xfsprogs-clean:
 	make -C xfsprogs clean
 
 xfsprogs-install:
-	mkdir -p $(INSTALLDIR)/xfsprogs/lib
-	mkdir -p $(INSTALLDIR)/xfsprogs/usr/sbin
-	cp -a $(TOP)/xfsprogs/fsck/xfs_fsck.sh $(INSTALLDIR)/xfsprogs/usr/sbin/fsck.xfs
-	cp -a $(TOP)/xfsprogs/mkfs/mkfs.xfs $(INSTALLDIR)/xfsprogs/usr/sbin/mkfs.xfs
-	cp -a $(TOP)/xfsprogs/repair/xfs_repair $(INSTALLDIR)/xfsprogs/usr/sbin/xfs_repair
-	cp -a $(TOP)/xfsprogs/libxcmd/.libs/*.so $(INSTALLDIR)/xfsprogs/lib
-	cp -a $(TOP)/xfsprogs/libxcmd/.libs/*.so.* $(INSTALLDIR)/xfsprogs/lib
-	cp -a $(TOP)/xfsprogs/libfrog/.libs/*.so $(INSTALLDIR)/xfsprogs/lib
-	cp -a $(TOP)/xfsprogs/libfrog/.libs/*.so.* $(INSTALLDIR)/xfsprogs/lib
-	cp -a $(TOP)/xfsprogs/libhandle/.libs/*.so $(INSTALLDIR)/xfsprogs/lib
-	cp -a $(TOP)/xfsprogs/libhandle/.libs/*.so.* $(INSTALLDIR)/xfsprogs/lib
-	cp -a $(TOP)/xfsprogs/libxcmd/.libs/*.so $(INSTALLDIR)/xfsprogs/lib
-	cp -a $(TOP)/xfsprogs/libxcmd/.libs/*.so.* $(INSTALLDIR)/xfsprogs/lib
-	cp -a $(TOP)/xfsprogs/libxfs/.libs/*.so $(INSTALLDIR)/xfsprogs/lib
-	cp -a $(TOP)/xfsprogs/libxfs/.libs/*.so.* $(INSTALLDIR)/xfsprogs/lib
+	make -C xfsprogs install DESTDIR=$(INSTALLDIR)/xfsprogs
+	rm -rf $(INSTALLDIR)/xfsprogs/usr
+	rm -f $(INSTALLDIR)/xfsprogs/lib/libhandle*
 
