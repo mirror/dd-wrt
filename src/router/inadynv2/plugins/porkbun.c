66baf8d5f3cfbca7bb591721556a39c941d2b258
BrainSlayer <s.gottschall@dd-wrt.com>
2024-02-20 10:19:20 +0000

    fix plugin. upstream pull request is broken
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55154 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/inadynv2/plugins/porkbun.c
+++ src/router/inadynv2/plugins/porkbun.c
@@ -167,7 +167,7 @@ static int get_result_value(const char *json, const char *key, jsmntok_t *out_re
 		goto cleanup;
 	}
 
-	if (check_status(json, tokens, num_tokens) == -1) {
+	if (check_success(json, tokens, num_tokens) == -1) {
 		logit(LOG_ERR, "Request was unsuccessful.");
 		goto cleanup;
 	}
