1798e9a6652c699610e1b409d9431421573bdbe7
BrainSlayer <s.gottschall@dd-wrt.com>
2024-02-20 10:24:27 +0000

    add porkbun
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55158 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/validate/webs.c
+++ src/router/httpd/validate/webs.c
@@ -6260,12 +6260,12 @@ void ddns_save_value(webs_t wp)
 	char _ipv6_only[] = "ddns_ipv6_only_XX";
 
 	int enable = websGetVari(wp, "ddns_enable", -1);
-	if (enable > 63 || enable < 0) {
+	if (enable > 64 || enable < 0) {
 		return;
 	}
 
 	int i;
-	for (i = 1; i < 64; i++) {
+	for (i = 1; i < 65; i++) {
 		if (i == enable)
 			continue;
 		snprintf(_username, sizeof(_username), "ddns_username_%d", i);
