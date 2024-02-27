542f0752ef7dfa1423238b38cfd5c34ee8af806b
BrainSlayer <s.gottschall@dd-wrt.com>
2024-02-27 16:35:44 +0000

    revert. doesnt compile. no time to fix all that mess now: remove old files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55219 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/softether/src/Mayaqua/Encoding.h
+++
@@ -1,9 +0,0 @@
-#ifndef ENCODING_H
-#define ENCODING_H
-
-#include "MayaType.h"
-
-UINT Base64Decode(void *dst, const void *src, const UINT size);
-UINT Base64Encode(void *dst, const void *src, const UINT size);
-
-#endif
