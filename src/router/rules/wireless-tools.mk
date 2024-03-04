43ecaaa05aeaccbce9b21513839698032fa86caf
BrainSlayer <s.gottschall@dd-wrt.com>
2024-03-04 06:54:10 +0000

    fix for jobserver
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55287 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/wireless-tools.mk
+++ src/router/rules/wireless-tools.mk
@@ -2,13 +2,13 @@ wireless-tools-clean:
 	make -C wireless-tools clean
 
 wireless-tools:
-	make -C wireless-tools CC="$(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT)" all iwmulticall
+	make -C wireless-tools MAKE="make" CC="$(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT)" all iwmulticall
 
 wireless-tools-install:
 ifneq ($(CONFIG_JUSTLIBIW),y)
 ifneq ($(CONFIG_LSX),y)
-	make -C wireless-tools CC="$(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT)" INSTALL_DIR=$(INSTALLDIR)/wireless-tools/usr/sbin install-iwmulticall
+	make -C wireless-tools MAKE="make" CC="$(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT)" INSTALL_DIR=$(INSTALLDIR)/wireless-tools/usr/sbin install-iwmulticall
 endif
 endif
-	make -C wireless-tools CC="$(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT)" INSTALL_LIB=$(INSTALLDIR)/wireless-tools/lib install-dynamic
+	make -C wireless-tools MAKE="make" CC="$(CC)" CFLAGS="$(COPTS) $(MIPS16_OPT)" INSTALL_LIB=$(INSTALLDIR)/wireless-tools/lib install-dynamic
 
