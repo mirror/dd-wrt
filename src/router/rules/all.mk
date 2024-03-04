7458eb2124340d822b488547aa81040a3db9c486
BrainSlayer <s.gottschall@dd-wrt.com>
2024-03-04 08:18:32 +0000

    fix for install
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55290 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/all.mk
+++ src/router/rules/all.mk
@@ -380,7 +380,7 @@ include rules/btop.mk
 	[ ! -d $* ] || $(MAKE) -C $* clean
 
 %-install:
-	[ ! -d $* ] || $(MAKE) -C $* install INSTALLDIR=$(INSTALLDIR)/$*
+	[ ! -d $* ] || make -C $* install INSTALLDIR=$(INSTALLDIR)/$*
 
 
 $(obj-y) $(obj-m) $(obj-n) $(obj-clean) $(obj-install): dummy
