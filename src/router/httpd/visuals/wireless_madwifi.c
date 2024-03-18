bac655830470020fd52f77378ddce68d776c8f05
BrainSlayer <s.gottschall@dd-wrt.com>
2024-03-18 19:35:25 +0000

    might be the limit for ipq4019. at least on habanero
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55413 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/wireless_madwifi.c
+++ src/router/httpd/visuals/wireless_madwifi.c
@@ -587,8 +587,12 @@ EJ_VISIBLE void ej_get_high_2ghz(webs_t wp, int argc, char_t **argv)
 
 EJ_VISIBLE void ej_get_high_5ghz(webs_t wp, int argc, char_t **argv)
 {
+	if (is_ipq4019("wlan0") || is_ipq4019("wlan1")) {
+		websWrite(wp, "6115"); // tested on habanero
+		return;
+	}
 	if ((is_ath10k("wlan0") && has_wave2("wlan0")) || (is_ath10k("wlan1") && has_wave2("wlan1"))) {
-		websWrite(wp, "6995"); // need to find out the real maximum which is way higher than 7 ghz
+		websWrite(wp, "6995"); // need to find out the real maximum which is way higher than 7 ghz 
 		return;
 	}
 	if ((is_ath10k("wlan0")) || (is_ath10k("wlan1"))) {
