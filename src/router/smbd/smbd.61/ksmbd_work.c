bc1697d44642919b6db8d2035fbaea4409feeb74
BrainSlayer <s.gottschall@dd-wrt.com>
2024-03-19 18:37:56 +0000

    fix
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55418 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/smbd/smbd.61/ksmbd_work.c
+++ src/router/smbd/smbd.61/ksmbd_work.c
@@ -51,7 +51,7 @@ void ksmbd_free_work_struct(struct ksmbd_work *work)
 #endif
 
 	list_for_each_entry_safe(ar, tmp, &work->aux_read_list, entry) {
-		ksmbd_release_buffer(ar->buf);
+		kvfree(ar->buf);
 		list_del(&ar->entry);
 		kfree(ar);
 	}
