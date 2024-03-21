a4e3abd9d1ca2e77b9de827eb038d4dd2e234f61
BrainSlayer <s.gottschall@dd-wrt.com>
2024-03-21 05:50:09 +0000

    rename it
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55428 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/wireless_madwifi.c
+++ src/router/httpd/visuals/wireless_madwifi.c
@@ -581,7 +581,7 @@ EJ_VISIBLE void ej_get_low_2ghz(webs_t wp, int argc, char_t **argv)
 
 EJ_VISIBLE void ej_get_high_2ghz(webs_t wp, int argc, char_t **argv)
 {
-	if (has_ipq4019("wlan0") || has_ipq4019("wlan1")) {
+	if (has_ar900b("wlan0") || has_ar900b("wlan1")) {
 		websWrite(wp, "2492"); // tested on habanero
 		return;
 	}
@@ -591,7 +591,7 @@ EJ_VISIBLE void ej_get_high_2ghz(webs_t wp, int argc, char_t **argv)
 
 EJ_VISIBLE void ej_get_high_5ghz(webs_t wp, int argc, char_t **argv)
 {
-	if (has_ipq4019("wlan0") || has_ipq4019("wlan1")) {
+	if (has_ar900b("wlan0") || has_ar900b("wlan1")) {
 		websWrite(wp, "6115"); // tested on habanero
 		return;
 	}
