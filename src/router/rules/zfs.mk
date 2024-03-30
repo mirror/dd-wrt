6f6641494ede5ba407abbd0de420e7566c2163b3
BrainSlayer <s.gottschall@dd-wrt.com>
2024-03-30 14:44:49 +0000

    if we build with different kernels in the same tree we run into a problem. so use separate build dirs
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55534 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/zfs.mk
+++ src/router/rules/zfs.mk
@@ -2,7 +2,8 @@ zfs-configure: libtirpc libudev openssl zlib curl ncurses util-linux
 	$(MAKE) -C libtirpc
 	cd zfs && ./autogen.sh
 	cd zfs && autoreconf
-	cd zfs && ./configure \
+	mkdir -p zfs/$(KERNELVERSION)
+	cd zfs/$(KERNELVERSION) && ../configure \
 		--prefix=/usr \
 		--libdir=/usr/lib \
 		--host=$(ARCH)-linux \
@@ -11,29 +12,29 @@ zfs-configure: libtirpc libudev openssl zlib curl ncurses util-linux
 		CFLAGS="-I$(TOP)/zlib/include -I$(TOP)/util-linux/include  -I$(TOP)/util-linux/libblkid/src -I$(TOP)/util-linux/libuuid/src -I$(TOP)/curl/include -I$(TOP)/libtirpc -I$(TOP)/libtirpc/tirpc -I$(TOP)/openssl/include  -I$(TOP)/libudev/src/libudev -D_GNU_SOURCE" \
 		LDFLAGS="-L$(TOP)/zlib  -L$(TOP)/util-linux/.libs -L$(TOP)/libtirpc/src/.libs -L$(TOP)/zfs/lib/libuutil/.libs -L$(TOP)/openssl -L$(TOP)/libudev/src/libudev/.libs" \
 		--with-linux=$(LINUXDIR)
-	cd zfs && find . -name "*.la" -exec sed -i 's/relink_command/# relink_command/g' {} +
-	cd zfs && find . -name "*.la" -exec touch {} +
+	cd zfs/$(KERNELVERSION) && find . -name "*.la" -exec sed -i 's/relink_command/# relink_command/g' {} +
+	cd zfs/$(KERNELVERSION) && find . -name "*.la" -exec touch {} +
 	touch $(TOP)/util-linux/libblkid/src/blkid.h
 	touch $(TOP)/openssl/include/openssl/opensslconf.h
 
 zfs: libtirpc libudev openssl zlib ncurses util-linux
-	cd zfs && find . -name "*.la" -exec sed -i 's/relink_command/# relink_command/g' {} +
-	cd zfs && find . -name "*.la" -exec touch {} +
+	cd zfs/$(KERNELVERSION) && find . -name "*.la" -exec sed -i 's/relink_command/# relink_command/g' {} +
+	cd zfs/$(KERNELVERSION) && find . -name "*.la" -exec touch {} +
 	touch $(TOP)/util-linux/libblkid/src/blkid.h
 	touch $(TOP)/openssl/include/openssl/opensslconf.h
-	$(MAKE) -C zfs
+	$(MAKE) -C zfs/$(KERNELVERSION)
 
 zfs-clean:
-	if test -e "zfs/Makefile"; then make -C zfs clean; fi
+	if test -e "zfs/$(KERNELVERSION)/Makefile"; then make -C zfs/$(KERNELVERSION) clean; fi
 
 zfs-distclean:
-	if test -e "zfs/Makefile"; then make -C zfs distclean; fi
+	if test -e "zfs/$(KERNELVERSION)/Makefile"; then make -C zfs/$(KERNELVERSION) distclean; fi
 	
 
 zfs-install:
-	cd zfs && find . -name "*.la" -exec sed -i 's/relink_command/# relink_command/g' {} +
-	cd zfs && find . -name "*.la" -exec touch {} +
-	make -C zfs install DESTDIR=$(INSTALLDIR)/zfs
+	cd zfs/$(KERNELVERSION) && find . -name "*.la" -exec sed -i 's/relink_command/# relink_command/g' {} +
+	cd zfs/$(KERNELVERSION) && find . -name "*.la" -exec touch {} +
+	make -C zfs/$(KERNELVERSION) install DESTDIR=$(INSTALLDIR)/zfs
 	rm -rf $(INSTALLDIR)/zfs/usr/include
 	rm -rf $(INSTALLDIR)/zfs/usr/lib/pkgconfig
 	rm -rf $(INSTALLDIR)/zfs/usr/share
