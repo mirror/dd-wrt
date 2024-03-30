12818097779d1a51e57b717a6941a8b9b856bf07
BrainSlayer <s.gottschall@dd-wrt.com>
2024-03-30 05:30:31 +0000

    fallback syscalls for 2.4 kernel. fixes compile error
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55525 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libmicrohttpd/src/microhttpd/daemon.c
+++ src/router/libmicrohttpd/src/microhttpd/daemon.c
@@ -7355,6 +7355,40 @@ parse_options_va (struct MHD_Daemon *daemon,
 
 
 #ifdef EPOLL_SUPPORT
+#include <sys/syscall.h>
+
+
+int
+epoll_create(int size)
+{
+#if !defined(__NR_epoll_create) && defined(__NR_epoll_create1)
+	if (size <= 0) {
+		errno = EINVAL;
+		return -1;
+	}
+	return (syscall(__NR_epoll_create1, 0));
+#else
+	return (syscall(__NR_epoll_create, size));
+#endif
+}
+
+int
+epoll_ctl(int epfd, int op, int fd, struct epoll_event *event)
+{
+
+	return (syscall(__NR_epoll_ctl, epfd, op, fd, event));
+}
+
+int
+epoll_wait(int epfd, struct epoll_event *events, int maxevents, int timeout)
+{
+#if !defined(__NR_epoll_wait) && defined(__NR_epoll_pwait)
+	return (syscall(__NR_epoll_pwait, epfd, events, maxevents, timeout, NULL, 0));
+#else
+	return (syscall(__NR_epoll_wait, epfd, events, maxevents, timeout));
+#endif
+}
+
 static int
 setup_epoll_fd (struct MHD_Daemon *daemon)
 {
