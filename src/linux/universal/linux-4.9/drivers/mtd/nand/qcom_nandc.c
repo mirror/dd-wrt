a0bc92a94206b22f5646e09f3328de257a0bf61e
BrainSlayer <s.gottschall@dd-wrt.com>
2024-03-31 15:59:09 +0000

    fix nand driver
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55558 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.9/drivers/mtd/nand/qcom_nandc.c
+++ src/linux/universal/linux-4.9/drivers/mtd/nand/qcom_nandc.c
@@ -2687,7 +2687,6 @@ static int qcom_nandc_probe(struct platform_device *pdev)
 	struct resource *res;
 	int ret;
 	struct qcom_nand_driver_data *driver_data;
-
 	nandc = devm_kzalloc(&pdev->dev, sizeof(*nandc), GFP_KERNEL);
 	if (!nandc)
 		return -ENOMEM;
@@ -2742,22 +2741,19 @@ static int qcom_nandc_probe(struct platform_device *pdev)
 		goto err_setup;
 
 	for_each_available_child_of_node(dn, child) {
-		if (of_device_is_compatible(child, "qcom,nandcs")) {
-			host = devm_kzalloc(dev, sizeof(*host), GFP_KERNEL);
-			if (!host) {
-				of_node_put(child);
-				ret = -ENOMEM;
-				goto err_cs_init;
-			}
-
-			ret = qcom_nand_host_init(nandc, host, child);
-			if (ret) {
-				devm_kfree(dev, host);
-				continue;
-			}
-
-			list_add_tail(&host->node, &nandc->host_list);
+		host = devm_kzalloc(dev, sizeof(*host), GFP_KERNEL);
+		if (!host) {
+			of_node_put(child);
+			ret = -ENOMEM;
+			goto err_cs_init;
 		}
+		ret = qcom_nand_host_init(nandc, host, child);
+		if (ret) {
+			devm_kfree(dev, host);
+			continue;
+ 		}
+
+		list_add_tail(&host->node, &nandc->host_list);
 	}
 
 	if (list_empty(&nandc->host_list)) {
