f8d19f248e4d97fa480c384ad26c40a4f47f0520
BrainSlayer <s.gottschall@dd-wrt.com>
2024-04-08 22:20:53 +0000

    even if this is implemented by linux in a non standard way, some applications make use if it in this way
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55723 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/musl/src/sched/sched_setscheduler.c
+++ src/router/musl/src/sched/sched_setscheduler.c
@@ -1,8 +1,8 @@
 #include <sched.h>
-#include <errno.h>
 #include "syscall.h"
 
 int sched_setscheduler(pid_t pid, int sched, const struct sched_param *param)
 {
-	return __syscall_ret(-ENOSYS);
+	static const struct sched_param def;
+	return syscall(SYS_sched_setscheduler, pid, 0, &def);
 }
