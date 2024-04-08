0d300af5b366b145fa14ec7f130ea4591271fbfd
BrainSlayer <s.gottschall@dd-wrt.com>
2024-04-08 08:01:31 +0000

    remove debug noise
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55702 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/hostapd-2023-09-08/src/common/ieee802_11_common.c
+++ src/router/hostapd-2023-09-08/src/common/ieee802_11_common.c
@@ -475,11 +475,11 @@ static ParseRes __ieee802_11_parse_elems(void *ctx, const u8 *start, size_t len,
 							     elems,
 							     show_errors)) {
 				unknown++;
-			   if (ctx) {
-			   	hostapd_logger(ctx, NULL, HOSTAPD_MODULE_IEEE80211,
-			       HOSTAPD_LEVEL_INFO,"parse vendor failed (id=%d elen=%d)",
-				   id, elen);
-			    }
+//			   if (ctx) {
+//			   	hostapd_logger(ctx, NULL, HOSTAPD_MODULE_IEEE80211,
+//			       HOSTAPD_LEVEL_INFO,"parse vendor failed (id=%d elen=%d)",
+//				   id, elen);
+//			    }
 			}
 			break;
 		case WLAN_EID_RSN:
