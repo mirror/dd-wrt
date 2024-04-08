0be2733a2e8af4b16e349575c4440bcc7f97811a
BrainSlayer <s.gottschall@dd-wrt.com>
2024-04-08 08:07:05 +0000

    add envtools
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55703 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rules/envtools.mk
@@ -0,0 +1,17 @@
+
+envtools:
+	make -C uboot defconfig	
+	make -C uboot envtools ARCH="sandbox" TARGET_CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -DNEED_PRINTF" 
+
+envtools-install:
+	install -d $(INSTALLDIR)/envtools/usr/sbin
+	install -d $(INSTALLDIR)/envtools/etc
+	install uboot/tools/env/fw_printenv $(INSTALLDIR)/envtools/usr/sbin
+	cd $(INSTALLDIR)/envtools/usr/sbin && ln -sf fw_printenv fw_setenv
+	cd $(INSTALLDIR)/envtools/etc && ln -sf /tmp/fw_env.config fw_env.config
+
+envtools-clean:
+	make -C uboot clean
+
+
+
