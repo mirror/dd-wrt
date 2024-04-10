5d2faeb412bb4c1b48cbbfdf77b72b2fdc46d068
BrainSlayer <s.gottschall@dd-wrt.com>
2024-04-10 00:32:48 +0000

    for new tools
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55734 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/smbd.mk
+++ src/router/rules/smbd.mk
@@ -17,28 +17,41 @@ smbd-configure: libnl
 	    AR_FLAGS="cru $(LTOPLUGIN)" \
 	    RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
 
+	cd smbd/tools-glib.61 && ./autogen.sh
+	cd smbd/tools-glib.61 && ./configure --prefix=/usr --host=$(ARCH)-linux --disable-shared --enable-static  --libdir=/usr/lib \
+	    CFLAGS="$(COPTS) $(LTO) $(MIPS16_OPT) -D_GNU_SOURCE -DNEED_PRINTF -I$(TOP)/_staging_static/usr/include/glib-2.0 -I$(TOP)/_staging_static/usr/lib/glib-2.0/include -Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	    LDFLAGS="-L$(TOP)/_staging_static/usr/lib $(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	    LIBNL_CFLAGS="-I$(TOP)/libnl/include" \
+	    LIBNL_LIBS="-L$(TOP)/libnl/lib/.libs -lnl-3 -lnl-genl-3" \
+	    AR_FLAGS="cru $(LTOPLUGIN)" \
+	    RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
+
 smbd: libnl
 	install -D smbd/config/samba_ksmbd.webnas httpd/ej_temp/02samba_ksmbd.webnas
 	install -D filesharing/config/zfilesharing.webnas httpd/ej_temp/03zfilesharing.webnas
 ifneq ($(KERNELVERSION),6.1)
 	$(MAKE) -C smbd/smbd all
+	$(MAKE) -C smbd/tools all
 else
 	$(MAKE) -C smbd/smbd.61 all
+	$(MAKE) -C smbd/tools-glib.61 all
 endif
-	$(MAKE) -C smbd/tools all
-	$(MAKE) -C smbd/tools-glib all
 
 smbd-install:
 ifneq ($(KERNELVERSION),6.1)
 	$(MAKE) -C smbd/smbd install
-else
-	$(MAKE) -C smbd/smbd.61 install
-endif
 	$(MAKE) -C smbd/tools-glib install DESTDIR=$(INSTALLDIR)/smbd
 	rm -rf $(INSTALLDIR)/smbd/usr/lib
-	cd $(INSTALLDIR)/smbd/usr/sbin && ln -sf smbd_multicall ksmbd.mountd
-	cd $(INSTALLDIR)/smbd/usr/sbin && ln -sf smbd_multicall ksmbd.adduser
-	cd $(INSTALLDIR)/smbd/usr/sbin && ln -sf smbd_multicall ksmbd.addshare
+else
+	$(MAKE) -C smbd/smbd.61 install
+	$(MAKE) -C smbd/tools-glib.61 install DESTDIR=$(INSTALLDIR)/smbd
+endif
+	rm -rf $(INSTALLDIR)/smbd/usr/share
+	rm -rf $(INSTALLDIR)/smbd/usr/etc
+	rm -rf $(INSTALLDIR)/smbd/usr/lib/systemd
+#	cd $(INSTALLDIR)/smbd/usr/sbin && ln -sf smbd_multicall ksmbd.mountd
+#	cd $(INSTALLDIR)/smbd/usr/sbin && ln -sf smbd_multicall ksmbd.adduser
+#	cd $(INSTALLDIR)/smbd/usr/sbin && ln -sf smbd_multicall ksmbd.addshare
 
 	install -D smbd/config/samba_ksmbd.webnas $(INSTALLDIR)/smbd/etc/config/02samba_ksmbd.webnas
 	install -D smbd/config/samba_ksmbd.nvramconfig $(INSTALLDIR)/smbd/etc/config/samba_ksmbd.nvramconfig
@@ -47,8 +60,9 @@ endif
 smbd-clean:
 ifneq ($(KERNELVERSION),6.1)
 	$(MAKE) -C smbd/smbd clean
-else
-	$(MAKE) -C smbd/smbd.61 clean
-endif
 	$(MAKE) -C smbd/tools clean
 	$(MAKE) -C smbd/tools-glib clean
+else
+	$(MAKE) -C smbd/smbd.61 clean
+	$(MAKE) -C smbd/tools-glib.61 clean
+endif
