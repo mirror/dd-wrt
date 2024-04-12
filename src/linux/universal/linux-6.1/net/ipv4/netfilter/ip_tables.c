c711b2cd6b86b6a3effa77e36c1b545a7e0dc10c
BrainSlayer <s.gottschall@dd-wrt.com>
2024-04-12 07:32:41 +0000

    update kernel
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55780 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.1/net/ipv4/netfilter/ip_tables.c
+++ src/linux/universal/linux-6.1/net/ipv4/netfilter/ip_tables.c
@@ -1184,6 +1184,8 @@ do_replace(struct net *net, sockptr_t arg, unsigned int len)
 	void *loc_cpu_entry;
 	struct ipt_entry *iter;
 
+	if (len < sizeof(tmp))
+		return -EINVAL;
 	if (copy_from_sockptr(&tmp, arg, sizeof(tmp)) != 0)
 		return -EFAULT;
 
@@ -1571,6 +1573,8 @@ compat_do_replace(struct net *net, sockptr_t arg, unsigned int len)
 	void *loc_cpu_entry;
 	struct ipt_entry *iter;
 
+	if (len < sizeof(tmp))
+		return -EINVAL;
 	if (copy_from_sockptr(&tmp, arg, sizeof(tmp)) != 0)
 		return -EFAULT;
 
