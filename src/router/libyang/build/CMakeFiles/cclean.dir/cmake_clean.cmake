878faa023470fd2bdae3678c69956393822c923c
BrainSlayer <s.gottschall@dd-wrt.com>
2024-04-20 05:50:09 +0000

    remove bin files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55860 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libyang/build/CMakeFiles/cclean.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cclean"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cclean.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- src/router/libyang/build/tools/lint/CMakeFiles/yanglint.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for yanglint.
-# This may be replaced when dependencies are built.
