f0df0acc37a0700de9feb6ae969734aa378d901c
BrainSlayer <s.gottschall@dd-wrt.com>
2024-04-20 05:49:14 +0000

    use 2.1.148: remove old files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@55859 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libyang/tools/upstream-version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# get latest upstream libyang version from github
-
-RLS_URL=https://api.github.com/repos/CESNET/libyang/releases
-VERSION=$(curl -s $RLS_URL | grep tag_name | cut -d '"' -f 4 | sort --version-sort | tail -n 1)
-VERSION=${VERSION#v}
-echo $VERSION
