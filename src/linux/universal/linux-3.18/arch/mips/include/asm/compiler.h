73cc1076e1cb7facbdb9403f1d5a1d8e282b961c
BrainSlayer <s.gottschall@dd-wrt.com>
2024-04-29 04:48:16 +0000

    fix xchg
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@56074 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/arch/mips/include/asm/compiler.h
+++ src/linux/universal/linux-3.18/arch/mips/include/asm/compiler.h
@@ -30,5 +30,13 @@
 #define MIPS_ISA_ARCH_LEVEL_RAW MIPS_ISA_LEVEL_RAW
 #endif /* CONFIG_CPU_MIPSR6 */
 
+#ifndef CONFIG_CPU_MICROMIPS
+#define GCC_OFF12_ASM() "R"
+#elif __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 9)
+#define GCC_OFF12_ASM() "ZC"
+#else
+#error "microMIPS compilation unsupported with GCC older than 4.9"
+#endif
+
 #endif /* _ASM_COMPILER_H */
 
