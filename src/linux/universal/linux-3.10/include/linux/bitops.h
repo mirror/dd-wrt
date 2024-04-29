e6e5233371314ffcf051e91ec71672c6a9a143a9
BrainSlayer <s.gottschall@dd-wrt.com>
2024-04-29 04:53:35 +0000

    fix some errors
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@56075 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.10/include/linux/bitops.h
+++ src/linux/universal/linux-3.10/include/linux/bitops.h
@@ -10,6 +10,17 @@
 #define BITS_TO_LONGS(nr)	DIV_ROUND_UP(nr, BITS_PER_BYTE * sizeof(long))
 #endif
 
+/*
+ * Create a contiguous bitmask starting at bit position @l and ending at
+ * position @h. For example
+ * GENMASK_ULL(39, 21) gives us the 64bit vector 0x000000ffffe00000.
+ */
+#define GENMASK(h, l) \
+	(((~0UL) << (l)) & (~0UL >> (BITS_PER_LONG - 1 - (h))))
+
+#define GENMASK_ULL(h, l) \
+	(((~0ULL) << (l)) & (~0ULL >> (BITS_PER_LONG_LONG - 1 - (h))))
+
 extern unsigned int __sw_hweight8(unsigned int w);
 extern unsigned int __sw_hweight16(unsigned int w);
 extern unsigned int __sw_hweight32(unsigned int w);
