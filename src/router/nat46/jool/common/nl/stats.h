f816a00b4dd8ae77baaa3cae45cbde9295fd5a11
BrainSlayer <s.gottschall@dd-wrt.com>
2024-05-06 16:40:53 +0000

    add jool for later work: remove old files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@56230 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/nat46/jool/common/nl/stats.h
+++
@@ -1,8 +0,0 @@
-#ifndef SRC_MOD_COMMON_NL_STATS_H_
-#define SRC_MOD_COMMON_NL_STATS_H_
-
-#include <net/genetlink.h>
-
-int handle_stats_foreach(struct sk_buff *jool, struct genl_info *info);
-
-#endif /* SRC_MOD_COMMON_NL_STATS_H_ */
--- src/router/nat46/jool/common/steps/handling_hairpinning_siit.h
+++
@@ -1,9 +0,0 @@
-#ifndef SRC_MOD_COMMON_STEPS_HANDLING_HAIRPINNING_H_
-#define SRC_MOD_COMMON_STEPS_HANDLING_HAIRPINNING_H_
-
-#include "mod/common/translation_state.h"
-
-bool is_hairpin_siit(struct xlation *state);
-verdict handling_hairpinning_siit(struct xlation *old);
-
-#endif /* SRC_MOD_COMMON_STEPS_HANDLING_HAIRPINNING_H_ */
