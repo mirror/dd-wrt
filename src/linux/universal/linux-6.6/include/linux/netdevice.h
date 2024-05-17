90b74393ce1a0437a21d745434e4bbebc432bad6
BrainSlayer <s.gottschall@dd-wrt.com>
2024-05-17 00:10:37 +0000

    port from 6.1
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@56394 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.6/include/linux/netdevice.h
+++ src/linux/universal/linux-6.6/include/linux/netdevice.h
@@ -2724,6 +2724,16 @@ netif_napi_add_tx_weight(struct net_device *dev,
 	netif_napi_add_weight(dev, napi, poll, weight);
 }
 
+static inline void
+netif_threaded_napi_add_tx_weight(struct net_device *dev,
+			 struct napi_struct *napi,
+			 int (*poll)(struct napi_struct *, int),
+			 int weight)
+{
+	set_bit(NAPI_STATE_NO_BUSY_POLL, &napi->state);
+	netif_threaded_napi_add_weight(dev, napi, poll, weight);
+}
+
 /**
  * netif_napi_add_tx() - initialize a NAPI context to be used for Tx only
  * @dev:  network device
@@ -2739,6 +2749,13 @@ static inline void netif_napi_add_tx(struct net_device *dev,
 				     int (*poll)(struct napi_struct *, int))
 {
 	netif_napi_add_tx_weight(dev, napi, poll, NAPI_POLL_WEIGHT);
+}
+
+static inline void netif_threaded_napi_add_tx(struct net_device *dev,
+				     struct napi_struct *napi,
+				     int (*poll)(struct napi_struct *, int))
+{
+	netif_threaded_napi_add_tx_weight(dev, napi, poll, NAPI_POLL_WEIGHT);
 }
 
 /**
