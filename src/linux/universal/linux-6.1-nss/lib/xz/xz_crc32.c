07cd4085051450669a95aecca2c0177acd7a23a0
BrainSlayer <s.gottschall@dd-wrt.com>
2024-05-26 03:46:45 +0000

    update xz
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@56510 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.1-nss/lib/xz/xz_crc32.c
+++ src/linux/universal/linux-6.1-nss/lib/xz/xz_crc32.c
@@ -1,11 +1,10 @@
+// SPDX-License-Identifier: 0BSD
+
 /*
  * CRC32 using the polynomial from IEEE-802.3
  *
  * Authors: Lasse Collin <lasse.collin@tukaani.org>
  *          Igor Pavlov <https://7-zip.org/>
- *
- * This file has been put into the public domain.
- * You can do whatever you want with this file.
  */
 
 /*
@@ -29,7 +28,7 @@ STATIC_RW_DATA uint32_t xz_crc32_table[256];
 
 XZ_EXTERN void xz_crc32_init(void)
 {
-	const uint32_t poly = CRC32_POLY_LE;
+	const uint32_t poly = 0xEDB88320;
 
 	uint32_t i;
 	uint32_t j;
