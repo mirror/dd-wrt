f06cb2936e0a7f680718f432c18fd94dfe924d1d
BrainSlayer <s.gottschall@dd-wrt.com>
2024-05-27 16:59:55 +0000

    add various new filters including delta
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@56532 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/lib/xz/xz_dec_syms.c
+++ src/linux/universal/linux-3.18/lib/xz/xz_dec_syms.c
@@ -1,10 +1,9 @@
+// SPDX-License-Identifier: 0BSD
+
 /*
  * XZ decoder module information
  *
  * Author: Lasse Collin <lasse.collin@tukaani.org>
- *
- * This file has been put into the public domain.
- * You can do whatever you want with this file.
  */
 
 #include <linux/module.h>
@@ -15,12 +14,14 @@ EXPORT_SYMBOL(xz_dec_reset);
 EXPORT_SYMBOL(xz_dec_run);
 EXPORT_SYMBOL(xz_dec_end);
 
-MODULE_DESCRIPTION("XZ decompressor");
-MODULE_VERSION("1.0");
-MODULE_AUTHOR("Lasse Collin <lasse.collin@tukaani.org> and Igor Pavlov");
+#ifdef CONFIG_XZ_DEC_MICROLZMA
+EXPORT_SYMBOL(xz_dec_microlzma_alloc);
+EXPORT_SYMBOL(xz_dec_microlzma_reset);
+EXPORT_SYMBOL(xz_dec_microlzma_run);
+EXPORT_SYMBOL(xz_dec_microlzma_end);
+#endif
 
-/*
- * This code is in the public domain, but in Linux it's simplest to just
- * say it's GPL and consider the authors as the copyright holders.
- */
-MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("XZ decompressor");
+MODULE_VERSION("1.2");
+MODULE_AUTHOR("Lasse Collin <lasse.collin@tukaani.org> and Igor Pavlov");
+MODULE_LICENSE("Dual BSD/GPL");
--- src/linux/universal/linux-3.18/lib/xz/xz_lzma2.h
+++ src/linux/universal/linux-3.18/lib/xz/xz_lzma2.h
@@ -1,11 +1,10 @@
+/* SPDX-License-Identifier: 0BSD */
+
 /*
  * LZMA2 definitions
  *
  * Authors: Lasse Collin <lasse.collin@tukaani.org>
- *          Igor Pavlov <http://7-zip.org/>
- *
- * This file has been put into the public domain.
- * You can do whatever you want with this file.
+ *          Igor Pavlov <https://7-zip.org/>
  */
 
 #ifndef XZ_LZMA2_H
