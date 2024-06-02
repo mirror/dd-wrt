0c3e4cd713fb9bda590ee7bd5a414e88ac72aeef
BrainSlayer <s.gottschall@dd-wrt.com>
2024-06-02 17:24:24 +0000

    revert this
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@56615 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.1/net/core/dev.c
+++ src/linux/universal/linux-6.1/net/core/dev.c
@@ -4716,7 +4716,7 @@ static int napi_schedule_rps(struct softnet_data *sd)
 	struct softnet_data *mysd = this_cpu_ptr(&softnet_data);
 
 #ifdef CONFIG_RPS
-	if (sd != mysd && !test_bit(NAPI_STATE_THREADED, &sd->backlog.state)) {
+	if (sd != mysd) {
 		sd->rps_ipi_next = mysd->rps_ipi_list;
 		mysd->rps_ipi_list = sd;
 
@@ -5910,8 +5910,6 @@ static DEFINE_PER_CPU(struct work_struct, flush_works);
 /* Network device is going away, flush any packets still pending */
 static void flush_backlog(struct work_struct *work)
 {
-	unsigned int process_queue_empty;
-	bool threaded, flush_processq;
 	struct sk_buff *skb, *tmp;
 	struct softnet_data *sd;
 
@@ -5926,16 +5924,7 @@ static void flush_backlog(struct work_struct *work)
 			input_queue_head_incr(sd);
 		}
 	}
-
-	threaded = test_bit(NAPI_STATE_THREADED, &sd->backlog.state);
-	flush_processq = threaded &&
-			 !skb_queue_empty_lockless(&sd->process_queue);
-	if (flush_processq)
-		process_queue_empty = sd->process_queue_empty;
 	rps_unlock_irq_enable(sd);
-
-	if (threaded)
-		goto out;
 
 	skb_queue_walk_safe(&sd->process_queue, skb, tmp) {
 		if (skb->dev->reg_state == NETREG_UNREGISTERING) {
@@ -5944,16 +5933,7 @@ static void flush_backlog(struct work_struct *work)
 			input_queue_head_incr(sd);
 		}
 	}
-
-out:
 	local_bh_enable();
-
-	while (flush_processq) {
-		msleep(1);
-		rps_lock_irq_disable(sd);
-		flush_processq = process_queue_empty == sd->process_queue_empty;
-		rps_unlock_irq_enable(sd);
-	}
 }
 
 static bool flush_required(int cpu)
@@ -6085,7 +6065,6 @@ static int process_backlog(struct napi_struct *napi, int quota)
 		}
 
 		rps_lock_irq_disable(sd);
-		sd->process_queue_empty++;
 		if (skb_queue_empty(&sd->input_pkt_queue)) {
 			/*
 			 * Inline a custom version of __napi_complete().
@@ -6095,8 +6074,7 @@ static int process_backlog(struct napi_struct *napi, int quota)
 			 * We can use a plain write instead of clear_bit(),
 			 * and we dont need an smp_mb() memory barrier.
 			 */
-			napi->state &= ~(NAPIF_STATE_SCHED |
-					 NAPIF_STATE_SCHED_THREADED);
+			napi->state = 0;
 			again = false;
 		} else {
 			skb_queue_splice_tail_init(&sd->input_pkt_queue,
@@ -6511,55 +6489,6 @@ int dev_set_threaded(struct net_device *dev, bool threaded)
 	return err;
 }
 EXPORT_SYMBOL(dev_set_threaded);
-
-int backlog_set_threaded(bool threaded)
-{
-	static bool backlog_threaded;
-	int err = 0;
-	int i;
-
-	if (backlog_threaded == threaded)
-		return 0;
-
-	for_each_possible_cpu(i) {
-		struct softnet_data *sd = &per_cpu(softnet_data, i);
-		struct napi_struct *n = &sd->backlog;
-
-		if (n->thread)
-			continue;
-		n->thread = kthread_run(napi_threaded_poll, n, "napi/backlog-%d", i);
-		if (IS_ERR(n->thread)) {
-			err = PTR_ERR(n->thread);
-			pr_err("kthread_run failed with err %d\n", err);
-			n->thread = NULL;
-			threaded = false;
-			break;
-		}
-
-	}
-
-	backlog_threaded = threaded;
-
-	/* Make sure kthread is created before THREADED bit
-	 * is set.
-	 */
-	smp_mb__before_atomic();
-
-	for_each_possible_cpu(i) {
-		struct softnet_data *sd = &per_cpu(softnet_data, i);
-		struct napi_struct *n = &sd->backlog;
-		unsigned long flags;
-
-		rps_lock_irqsave(sd, &flags);
-		if (threaded)
-			n->state |= NAPIF_STATE_THREADED;
-		else
-			n->state &= ~NAPIF_STATE_THREADED;
-		rps_unlock_irq_restore(sd, &flags);
-	}
-
-	return err;
-}
 
 void netif_napi_add_weight(struct net_device *dev, struct napi_struct *napi,
 			   int (*poll)(struct napi_struct *, int), int weight)
@@ -11323,9 +11252,6 @@ static int dev_cpu_dead(unsigned int oldcpu)
 	raise_softirq_irqoff(NET_TX_SOFTIRQ);
 	local_irq_enable();
 
-	if (test_bit(NAPI_STATE_THREADED, &oldsd->backlog.state))
-		return 0;
-
 #ifdef CONFIG_RPS
 	remsd = oldsd->rps_ipi_list;
 	oldsd->rps_ipi_list = NULL;
@@ -11642,7 +11568,6 @@ static int __init net_dev_init(void)
 #endif
 		spin_lock_init(&sd->defer_lock);
 
-		INIT_LIST_HEAD(&sd->backlog.poll_list);
 		init_gro_hash(&sd->backlog);
 		sd->backlog.poll = process_backlog;
 		sd->backlog.weight = weight_p;
