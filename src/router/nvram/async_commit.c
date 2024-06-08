8a47dac5c9cc6d1e71654f40c9417e6625742a9e
BrainSlayer <s.gottschall@dd-wrt.com>
2024-06-08 06:44:11 +0000

    some diag activity on nvram commit
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@56737 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/nvram/async_commit.c
+++ src/router/nvram/async_commit.c
@@ -7,7 +7,7 @@
 
 int main(int argc, char *argv[])
 {
-#if defined(HAVE_WZRHPG300NH) || defined(HAVE_WHRHPGN) || defined(HAVE_WZRHPAG300NH) || defined(HAVE_DIR825) || defined(HAVE_TEW632BRP) || defined(HAVE_TG2521) || defined(HAVE_WR1043)  || defined(HAVE_WRT400) || defined(HAVE_WZRHPAG300NH) || defined(HAVE_WZRG450) || defined(HAVE_DANUBE) || defined(HAVE_WR741) || defined(HAVE_NORTHSTAR) || defined(HAVE_DIR615I) || defined(HAVE_WDR4900) || defined(HAVE_VENTANA) || defined(HAVE_UBNTM) || defined(DHAVE_IPQ806X)
+#if defined(HAVE_WZRHPG300NH) || defined(HAVE_WHRHPGN) || defined(HAVE_WZRHPAG300NH) || defined(HAVE_DIR825) || defined(HAVE_TEW632BRP) || defined(HAVE_TG2521) || defined(HAVE_WR1043)  || defined(HAVE_WRT400) || defined(HAVE_WZRHPAG300NH) || defined(HAVE_WZRG450) || defined(HAVE_DANUBE) || defined(HAVE_WR741) || defined(HAVE_NORTHSTAR) || defined(HAVE_DIR615I) || defined(HAVE_WDR4900) || defined(HAVE_VENTANA) || defined(HAVE_UBNTM) || defined(HAVE_IPQ806X) || defined(HAVE_IPQ6018)
 	system("ledtool 1");
 #elif HAVE_LSX
 	//nothing
