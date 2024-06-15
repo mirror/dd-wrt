ff21437383f0033f9fc006b92e00682016cf0c37
BrainSlayer <s.gottschall@dd-wrt.com>
2024-06-15 06:45:22 +0000

    format code
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@56885 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/libwireless/mac80211info.c
+++ src/router/libutils/libwireless/mac80211info.c
@@ -2297,14 +2297,16 @@ void mac80211_set_antennas(char *prefix, uint32_t tx_ant, uint32_t rx_ant)
 {
 	int maxrxchain = mac80211_get_antennas(prefix, 0, 1);
 	int maxtxchain = mac80211_get_antennas(prefix, 0, 0);
-	if (has_vht160_2by2(prefix) && (nvram_nmatch("160","%s_channelbw",prefix) || nvram_nmatch("8080","%s_channelbw",prefix) && tx_ant==0xf && rx_ant==0xf) {
+	if (has_vht160_2by2(prefix) &&
+	    (nvram_nmatch("160", "%s_channelbw", prefix) || nvram_nmatch("8080", "%s_channelbw", prefix)) && tx_ant == 0xf &&
+	    rx_ant == 0xf) {
 		rx_ant = 0x3;
 		tx_ant = 0x3;
 	}
 	if (maxrxchain > 15 && (maxrxchain & 0xf) == 0)
-	    rx_ant <<= 4;
+		rx_ant <<= 4;
 	if (maxtxchain > 15 && (maxtxchain & 0xf) == 0)
-	    tx_ant <<= 4;
+		tx_ant <<= 4;
 
 	int phy = get_ath9k_phy_ifname(prefix);
 	mac80211_init();
