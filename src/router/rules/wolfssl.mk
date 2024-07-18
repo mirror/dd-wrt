d270d89dd98ec3f181b9d605ef1f6a06f8a886c8
BrainSlayer <s.gottschall@dd-wrt.com>
2024-07-18 03:46:54 +0000

    fix for old targets
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@57465 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/wolfssl.mk
+++ src/router/rules/wolfssl.mk
@@ -1,12 +1,19 @@
+ifeq ($(ARCHITECTURE),broadcom)
+ifneq ($(CONFIG_BCMMODERN),y)
+NO_TLS=ac_cv_tls=none
+endif
+endif
+
+
 wolfssl-configure:
 	cd wolfssl && ./autogen.sh
 	mkdir -p wolfssl/minimal
 	mkdir -p wolfssl/standard
-	cd wolfssl/standard && ../configure --prefix=/usr --libdir=/usr/lib --host=$(ARCH)-linux --disable-examples --enable-opensslextra --enable-opensslall --enable-shared --enable-fastmath --disable-static -disable-errorstrings --enable-lowresource --disable-oldtls --enable-aesgcm --enable-poly1305 --enable-chacha --enable-ecc --disable-sslv3 --enable-des3 --enable-md4 --enable-stunnel --enable-tls13 --enable-session-ticket --enable-wpas --enable-cmac CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	cd wolfssl/standard && ../configure $(NO_TLS)  --prefix=/usr --libdir=/usr/lib --host=$(ARCH)-linux --disable-examples --enable-opensslextra --enable-opensslall --enable-shared --enable-fastmath --disable-static -disable-errorstrings --enable-lowresource --disable-oldtls --enable-aesgcm --enable-poly1305 --enable-chacha --enable-ecc --disable-sslv3 --enable-des3 --enable-md4 --enable-stunnel --enable-tls13 --enable-session-ticket --enable-wpas --enable-cmac CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	    AR_FLAGS="cru $(LTOPLUGIN)" \
 	    RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
 
-	cd wolfssl/minimal && ../configure --prefix=/usr --libdir=/usr/lib --host=$(ARCH)-linux --enable-opensslextra --disable-shared --enable-fastmath --enable-static -disable-errorstrings --disable-oldtls --disable-sha --enable-lowresource --disable-md5 --disable-arc4 --disable-poly1305 --disable-chacha --enable-ecc --disable-sslv3 --disable-tls13 --disable-des3 --enable-md4 --enable-stunnel --enable-session-ticket --enable-cmac CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	cd wolfssl/minimal && ../configure $(NO_TLS) --prefix=/usr --libdir=/usr/lib --host=$(ARCH)-linux --enable-opensslextra --disable-shared --enable-fastmath --enable-static -disable-errorstrings --disable-oldtls --disable-sha --enable-lowresource --disable-md5 --disable-arc4 --disable-poly1305 --disable-chacha --enable-ecc --disable-sslv3 --disable-tls13 --disable-des3 --enable-md4 --enable-stunnel --enable-session-ticket --enable-cmac CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	    AR_FLAGS="cru $(LTOPLUGIN)" \
 	    RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
 	$(MAKE) -C wolfssl/minimal
