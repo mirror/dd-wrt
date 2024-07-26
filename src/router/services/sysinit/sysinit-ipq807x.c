17c25dccaabf227cc43c3c36247b7849237583ac
BrainSlayer <s.gottschall@dd-wrt.com>
2024-07-26 04:26:42 +0000

    add mesh mgr
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@57575 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-ipq807x.c
+++ src/router/services/sysinit/sysinit-ipq807x.c
@@ -443,7 +443,7 @@ static void load_nss_ipq60xx(int profile)
 		insmod("nss-ifb");
 		insmod("qca-nss-netlink-ipq60xx");
 		insmod("qca-nss-bridge-mgr-ipq60xx");
-
+		insmod("qca-nss-wifi-meshmgr");
 	}
 }
 
@@ -486,6 +486,7 @@ static void load_nss_ipq50xx(int profile)
 		insmod("qca-mcs");
 		insmod("nss-ifb");
 		insmod("qca-nss-bridge-mgr-ipq50xx");
+		insmod("qca-nss-wifi-meshmgr");	
 	}
 }
 static void load_nss_ipq807x(int profile)
@@ -534,6 +535,7 @@ static void load_nss_ipq807x(int profile)
 		insmod("nss-ifb");
 		insmod("qca-nss-netlink-ipq807x");
 		insmod("qca-nss-bridge-mgr-ipq807x");
+		insmod("qca-nss-wifi-meshmgr");	
 	}
 }
 
