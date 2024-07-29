c2ceb9def4636119d8cf00e0be93d63c0d912b59
BrainSlayer <s.gottschall@dd-wrt.com>
2024-07-29 02:31:27 +0000

    fix flags
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@57612 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/ubi-utils.mk
+++ src/router/rules/ubi-utils.mk
@@ -9,10 +9,73 @@ ubi-utils-configure: zlib
 		ZLIB_LIBS=" $(LDLTO) -L$(TOP)/zlib -lz" \
 		ZSTD_CFLAGS="$(LTO) -I$(TOP)/zstd/lib" \
 		ZSTD_LIBS="$(LDLTO) -L$(TOP)/zstd/lib -lzstd" \
+		OPENSSL_CFLAGS="$(LTO) -I$(TOP)/openssl/include" \
+		OPENSSL_LIBS="$(LDLTO) -L$(TOP)/openssl -lssl -lcrypto" \
+		UUID_CFLAGS="-I$(TOP)/$(ARCH)-uclibc/install/util-linux/usr/include/uuid" \
+		UUID_LIBS="-L$(TOP)/$(ARCH)-uclibc/install/util-linux/usr/lib -luuid" \
 		AR_FLAGS="cru $(LTOPLUGIN)" \
 		RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
+
 ubi-utils: zlib
+	$(MAKE) -C util-linux
+	$(MAKE) -C util-linux install DESTDIR=$(INSTALLDIR)/util-linux
+	mkdir -p $(INSTALLDIR)/util-linux/usr/lib
+	-cp -urv $(INSTALLDIR)/util-linux/usr/tmp/* $(INSTALLDIR)/util-linux/usr/lib
+	rm -rf $(INSTALLDIR)/util-linux/usr/tmp 
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libuuid.a
+	rm -rf $(INSTALLDIR)/util-linux/usr/sbin
+	rm -rf $(INSTALLDIR)/util-linux/usr/bin
+	rm -rf $(INSTALLDIR)/util-linux/bin
+	rm -rf $(INSTALLDIR)/util-linux/sbin
+ifneq ($(CONFIG_ASTERISK),y)
+ifneq ($(CONFIG_ZABBIX),y)
+ifneq ($(CONFIG_MC),y)
+ifneq ($(CONFIG_LIBQMI),y)
+ifneq ($(CONFIG_ZFS),y)
+ifneq ($(CONFIG_WEBSERVER),y)
+ifneq ($(CONFIG_E2FSPROGS),y)
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libblkid.so*
+endif
+endif
+endif
+endif
+endif
+endif
+endif
 	$(MAKE) -C ubi-utils
+	rm -rf $(INSTALLDIR)/util-linux/usr/sbin
+	rm -rf $(INSTALLDIR)/util-linux/usr/bin
+	rm -rf $(INSTALLDIR)/util-linux/bin
+	rm -rf $(INSTALLDIR)/util-linux/sbin
+	rm -rf $(INSTALLDIR)/util-linux/usr/share
+	rm -rf $(INSTALLDIR)/util-linux/usr/include
+	rm -rf $(INSTALLDIR)/util-linux/usr/lib/pkgconfig
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libmount.a
+ifneq ($(CONFIG_NFS),y)
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libmount.so*
+endif
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libmount.la
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libfdisk*
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libsmartcols*
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libblkid.a
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libblkid.la
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libuuid.a
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libuuid.la
+ifneq ($(CONFIG_ASTERISK),y)
+ifneq ($(CONFIG_ZABBIX),y)
+ifneq ($(CONFIG_MC),y)
+ifneq ($(CONFIG_LIBQMI),y)
+ifneq ($(CONFIG_WEBSERVER),y)
+ifneq ($(CONFIG_ZFS),y)
+	rm -f $(INSTALLDIR)/util-linux/usr/lib/libblkid.so*
+endif
+endif
+endif
+endif
+endif
+endif
+	rm -f $(INSTALLDIR)/util-linux/lib/libfdisk.so*
+	rm -f $(INSTALLDIR)/util-linux/lib/libsmartcols.so*
 
 ubi-utils-clean:
 	$(MAKE) -C ubi-utils clean
