fbaabe4775057006f894126fd598f2d2f870afc2
BrainSlayer <s.gottschall@dd-wrt.com>
2024-07-30 05:09:33 +0000

    fixup page size
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@57646 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/dosfstools.mk
+++ src/router/rules/dosfstools.mk
@@ -5,7 +5,7 @@ dosfstools-configure:
 	cd dosfstools && automake --add-missing
 	cd dosfstools && ./configure --host=$(ARCH)-linux --prefix=/usr --enable-compat-symlinks --without-udev \
 	    CC="$(CC) $(COPTS) $(MIPS16_OPT) $(THUMB) -fPIC -ffunction-sections -fdata-sections -Wl,--gc-sections -D_GNU_SOURCE  -DNEED_PRINTF -std=gnu89" \
-	    LDFLAGS="-ffunction-sections -fdata-sections -Wl,--gc-sections"
+	    LDFLAGS="$(COPTS) -ffunction-sections -fdata-sections -Wl,--gc-sections"
 
 dosfstools:
 	make -C dosfstools
