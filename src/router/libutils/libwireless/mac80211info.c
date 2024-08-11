94ea9bd151fcd291d851f527e8aabb4e0dc5c605
BrainSlayer <s.gottschall@dd-wrt.com>
2024-08-11 05:50:44 +0000

    remove noise
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@57861 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/libwireless/mac80211info.c
+++ src/router/libutils/libwireless/mac80211info.c
@@ -119,8 +119,6 @@ static void __attribute__((constructor)) mac80211_init(void)
 		int ret = unl_genl_init(&unl, "nl80211");
 		if (!ret) {
 			bunl = 1;
-		} else {
-			dd_loginfo("mac80211info.c", "cannot connect to nl80211 (yet)");
 		}
 	}
 }
