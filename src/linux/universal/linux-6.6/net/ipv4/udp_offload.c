1521aca821a146d12fb3a280806628f73784f897
BrainSlayer <s.gottschall@dd-wrt.com>
2024-08-23 15:09:18 +0000

    fix broken udp
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@58134 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.6/net/ipv4/udp_offload.c
+++ src/linux/universal/linux-6.6/net/ipv4/udp_offload.c
@@ -285,7 +285,8 @@ struct sk_buff *__udp_gso_segment(struct sk_buff *gso_skb,
 		features &= ~(NETIF_F_GSO_UDP_L4 | NETIF_F_CSUM_MASK);
 
 	if (unlikely(skb_checksum_start(gso_skb) !=
-		     skb_transport_header(gso_skb)))
+		     skb_transport_header(gso_skb) &&
+		     !(skb_shinfo(gso_skb)->gso_type & SKB_GSO_FRAGLIST)))
 		return ERR_PTR(-EINVAL);
 
 	if (skb_gso_ok(gso_skb, features | NETIF_F_GSO_ROBUST)) {
