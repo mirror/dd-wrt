b277ecbe2ea659aa8b526535c69ece852a2e1989
BrainSlayer <s.gottschall@dd-wrt.com>
2024-08-25 13:43:13 +0000

    update some notices
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@58172 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/tools/gpio.c
+++ src/router/services/tools/gpio.c
@@ -1,7 +1,7 @@
 /*
  * gpio.c
  *
- * Copyright (C) 2006,2008 Sebastian Gottschall <s.gottschall@dd-wrt.com>
+ * Copyright (C) 2006 - 2024 Sebastian Gottschall <s.gottschall@dd-wrt.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
