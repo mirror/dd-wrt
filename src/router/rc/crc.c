87beb1dec451d2eb90dfc1b28ac4e41165bc3807
BrainSlayer <s.gottschall@dd-wrt.com>
2024-08-26 06:22:01 +0000

    for older builds
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@58180 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/crc.c
+++ src/router/rc/crc.c
@@ -6,11 +6,12 @@ static uint32 crc32(void *vdata, int size, uint32 crc)
 {
 	unsigned char *data = (unsigned char *)vdata;
 	unsigned char *end = data + size;
+	int i;
 
 	while (data < end) {
 		crc ^= *data++;
 
-		for (int i = 0; i < 8; i++) {
+		for (i = 0; i < 8; i++) {
 			uint t = ~((crc & 1) - 1);
 			crc = (crc >> 1) ^ (0xEDB88320 & t);
 		}
