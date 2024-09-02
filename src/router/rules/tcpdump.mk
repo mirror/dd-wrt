75f3974cb120a619075d352b29b67ec5ec6e5454
BrainSlayer <s.gottschall@dd-wrt.com>
2024-09-02 07:13:05 +0000

    fix ssl miscompile
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@58288 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/tcpdump.mk
+++ src/router/rules/tcpdump.mk
@@ -1,5 +1,5 @@
 tcpdump-configure: libpcap
-	cd tcpdump && ./configure --host=$(ARCH)-linux --enable-shared --enable-ipv6 --libdir=/lib --disable-static CC="$(CC)" ac_cv_linux_vers=2 ac_cv_ssleay_path=no td_cv_buggygetaddrinfo=no CPPFLAGS="-I../libpcap $(COPTS) $(MIPS16_OPT) $(THUMB) $(LTO)  -ffunction-sections -fdata-sections -Wl,--gc-sections -D_GNU_SOURCE -DNEED_PRINTF" CFLAGS="-I../libpcap $(COPTS) $(LTO) $(MIPS16_OPT) $(THUMB) -D_GNU_SOURCE  -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF -DHAVE_BPF_DUMP  -D_GNU_SOURCE" LDFLAGS="-L$(TOP)/libpcap $(LDLTO)" \
+	cd tcpdump && ./configure --host=$(ARCH)-linux --enable-shared --enable-ipv6 --with-crypto=$(SSLPATH) --libdir=/lib --disable-static CC="$(CC)" ac_cv_linux_vers=2 ac_cv_ssleay_path=no td_cv_buggygetaddrinfo=no CPPFLAGS="-I$(TOP)/libpcap -I$(SSLPATH) $(COPTS) $(MIPS16_OPT) $(THUMB) $(LTO)  -ffunction-sections -fdata-sections -Wl,--gc-sections -D_GNU_SOURCE -DNEED_PRINTF" CFLAGS=" -I$(TOP)/tcpdump -I$(TOP)/libpcap $(COPTS) $(LTO) $(MIPS16_OPT) $(THUMB) -D_GNU_SOURCE  -ffunction-sections -fdata-sections -Wl,--gc-sections -DNEED_PRINTF -DHAVE_BPF_DUMP  -D_GNU_SOURCE" LDFLAGS="-L$(TOP)/libpcap -L$(SSLPATH) $(LDLTO)" \
 		AR="$(ARCH)-linux-ar cru $(LTOPLUGIN)" \
 		RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
 	sed -i 's/-I\/usr\/include/ /g' $(TOP)/tcpdump/Makefile
