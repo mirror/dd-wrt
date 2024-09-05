2fb65e0b642051e543c0799253af4774b1eae15f
BrainSlayer <s.gottschall@dd-wrt.com>
2024-09-05 08:45:43 +0000

    for testing airbag crashlog
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@58329 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/services/services/crashtest.c
@@ -0,0 +1,63 @@
+/*
+ * wland.c
+ *
+ * Copyright (C) 2009 - 2024 Sebastian Gottschall <s.gottschall@dd-wrt.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id:
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h> /* AhMan March 18 2005 */
+#include <sys/socket.h>
+#include <sys/mount.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <net/route.h> /* AhMan March 18 2005 */
+#include <sys/types.h>
+#include <signal.h>
+
+#include <bcmnvram.h>
+#include <bcmconfig.h>
+#include <netconf.h>
+#include <shutils.h>
+#include <utils.h>
+#include <cy_conf.h>
+#include <code_pattern.h>
+#include <rc.h>
+#include <wlutils.h>
+#include <nvparse.h>
+#include <syslog.h>
+#include <services.h>
+
+
+void start_crashtest(void)
+{
+char *dest = malloc(-1);
+strcpy(dest, "test");
+fprintf(stderr, "%s", dest);
+}
