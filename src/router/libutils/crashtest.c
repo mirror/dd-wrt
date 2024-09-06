24f7e9b3fae0941d09336d372e1fbefe1c72bbbf
BrainSlayer <s.gottschall@dd-wrt.com>
2024-09-06 08:04:46 +0000

    should work now for all targets. its not included in the firmware anyway. just a testbin
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@58341 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/crashtest.c
+++ src/router/libutils/crashtest.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <malloc.h>
+#include <airbag.h>
 
 
 int main(int argc, char *argv[])
 {
-void airbag_init(void);
 airbag_init();
 fprintf(stderr, "test\n");
 char *dest = malloc(-1);
