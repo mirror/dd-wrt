9ccbd2250720b5ddbe58ac3f58e06c11cda722fd
BrainSlayer <s.gottschall@dd-wrt.com>
2024-09-12 04:44:29 +0000

    reformat code
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@58380 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/eop-tunnel/mtik/eoipcr.c
+++ src/router/eop-tunnel/mtik/eoipcr.c
@@ -60,7 +60,8 @@ static int print_link(const struct sockaddr_nl *who, struct nlmsghdr *n, void *a
 	if (!strcmp(kind, "eoip") && ifinfo[IFLA_INFO_DATA]) {
 		char ts[IFNAMSIZ], td[IFNAMSIZ];
 
-		parse_rtattr(ifgreo, IFLA_GRE_MAX, (void *)rta_getattr_str(ifinfo[IFLA_INFO_DATA]), ifinfo[IFLA_INFO_DATA]->rta_len);
+		parse_rtattr(ifgreo, IFLA_GRE_MAX, (void *)rta_getattr_str(ifinfo[IFLA_INFO_DATA]),
+			     ifinfo[IFLA_INFO_DATA]->rta_len);
 		if (ifgreo[IFLA_GRE_LINK])
 			link = rta_getattr_u32(ifgreo[IFLA_GRE_LINK]);
 		if (ifgreo[IFLA_GRE_TOS])
@@ -75,7 +76,8 @@ static int print_link(const struct sockaddr_nl *who, struct nlmsghdr *n, void *a
 			tunid = rta_getattr_u32(ifgreo[IFLA_GRE_IKEY]);
 		strcpy(ts, inet_ntoa(sip));
 		strcpy(td, inet_ntoa(dip));
-		printf("%d: %s@%d: link/%s %s remote %s tunnel-id %d ttl %d tos %d\n", ifi->ifi_index, ifname, link, kind, ts, td, tunid, ttl, tos);
+		printf("%d: %s@%d: link/%s %s remote %s tunnel-id %d ttl %d tos %d\n", ifi->ifi_index, ifname, link, kind, ts, td,
+		       tunid, ttl, tos);
 	}
 
 	return 0;
@@ -235,28 +237,76 @@ typedef struct {
 } s_cmd;
 
 static s_cmd cmds[] = {
-	{.cmd = "list",.cid = C_LIST,},
-	{.cmd = "show",.cid = C_LIST,},
-	{.cmd = "add",.cid = C_ADD,},
-	{.cmd = "new",.cid = C_ADD,},
-	{.cmd = "set",.cid = C_SET,},
-	{.cmd = "change",.cid = C_SET,},
-	{.cmd = NULL,.cid = 0,},
+	{
+		.cmd = "list",
+		.cid = C_LIST,
+	},
+	{
+		.cmd = "show",
+		.cid = C_LIST,
+	},
+	{
+		.cmd = "add",
+		.cid = C_ADD,
+	},
+	{
+		.cmd = "new",
+		.cid = C_ADD,
+	},
+	{
+		.cmd = "set",
+		.cid = C_SET,
+	},
+	{
+		.cmd = "change",
+		.cid = C_SET,
+	},
+	{
+		.cmd = NULL,
+		.cid = 0,
+	},
 };
 
 static s_cmd prms[] = {
-	{.cmd = "local",.cid = P_LOCAL,},
-	{.cmd = "remote",.cid = P_REMOTE,},
-	{.cmd = "tunnel-id",.cid = P_TUNNELID,},
-	{.cmd = "tid",.cid = P_TUNNELID,},
-	{.cmd = "ttl",.cid = P_TTL,},
-	{.cmd = "tos",.cid = P_TOS,},
-	{.cmd = "link",.cid = P_LINK,},
-	{.cmd = "name",.cid = P_NAME,},
-	{.cmd = NULL,.cid = 0,},
+	{
+		.cmd = "local",
+		.cid = P_LOCAL,
+	},
+	{
+		.cmd = "remote",
+		.cid = P_REMOTE,
+	},
+	{
+		.cmd = "tunnel-id",
+		.cid = P_TUNNELID,
+	},
+	{
+		.cmd = "tid",
+		.cid = P_TUNNELID,
+	},
+	{
+		.cmd = "ttl",
+		.cid = P_TTL,
+	},
+	{
+		.cmd = "tos",
+		.cid = P_TOS,
+	},
+	{
+		.cmd = "link",
+		.cid = P_LINK,
+	},
+	{
+		.cmd = "name",
+		.cid = P_NAME,
+	},
+	{
+		.cmd = NULL,
+		.cid = 0,
+	},
 };
 
-static e_cmd find_cmd(s_cmd * l, char *pcmd)
+static e_cmd find_cmd(s_cmd *l, char *pcmd)
 {
 	int cnt = 0;
 	int len = 0;
@@ -290,7 +340,9 @@ static void usage(char *me)
 {
 	printf("usage:\n"
 	       "\t%s add [name <name>] tunnel-id <id> [local <ip>] remote <ip> [ttl <ttl>] [tos <tos>] [link <ifindex|ifname>]\n"
-	       "\t%s set  name <name>  tunnel-id <id> [local <ip>] remote <ip> [ttl <ttl>] [tos <tos>] [link <ifindex|ifname>]\n" "\t%s list\n", me, me, me);
+	       "\t%s set  name <name>  tunnel-id <id> [local <ip>] remote <ip> [ttl <ttl>] [tos <tos>] [link <ifindex|ifname>]\n"
+	       "\t%s list\n",
+	       me, me, me);
 }
 
 int main(int argc, char **argv)
@@ -300,7 +352,7 @@ int main(int argc, char **argv)
 		int excl = 1;
 
 		switch (c) {
-		      dafeutl:
+dafeutl:
 		default:
 			usage(argv[0]);
 			return 0;
@@ -311,90 +363,90 @@ int main(int argc, char **argv)
 			return 0;
 		case C_SET:
 			excl = 0;
-		case C_ADD:{
-				char ifname[IFNAMSIZ + 1] = "";
-				uint32_t sip = htonl(0), dip = htonl(0);
-				uint32_t tid = ~0;
-				uint8_t ttl = 0, tos = 0;
-				uint32_t link = 0;
-				int i = 2;
+		case C_ADD: {
+			char ifname[IFNAMSIZ + 1] = "";
+			uint32_t sip = htonl(0), dip = htonl(0);
+			uint32_t tid = ~0;
+			uint8_t ttl = 0, tos = 0;
+			uint32_t link = 0;
+			int i = 2;
 
-				while (i < argc) {
-					e_cmd p = find_cmd(prms, argv[i]);
-					int noarg = !((i + 1) < argc);
-					struct in_addr iad;
+			while (i < argc) {
+				e_cmd p = find_cmd(prms, argv[i]);
+				int noarg = !((i + 1) < argc);
+				struct in_addr iad;
 
-					switch (p) {
-					default:
-						break;
-					case E_NONE:
-						printf("unknown option: %s\n", argv[i]);
-						return 0;
-					case E_AMBIGUOUS:
-						printf("option is ambiguous: %s\n", argv[i]);
-						return 0;
-					}
-
-					if (noarg) {
-						printf("option: %s requires an argument\n", argv[i]);
-						return 0;
-					}
-
-					switch (p) {
-					default:
-						printf("BUG!\n");
-						return 0;
-					case P_NAME:
-						ifname[(sizeof ifname) - 1] = 0;
-						strncpy(ifname, argv[i + 1], IFNAMSIZ);
-						break;
-					case P_TTL:
-						ttl = atoi(argv[i + 1]);
-						break;
-					case P_TOS:
-						tos = atoi(argv[i + 1]);
-						break;
-					case P_LINK:
-						// convert ifname to ifinidex, also support numeric arg
-						link = if_nametoindex(argv[i + 1]);
-						if (!link)
-							link = atoi(argv[i + 1]);
-						if (!link) {
-							printf("invald interface name/index: %s\n", argv[i + 1]);
-							return 0;
-						}
-						break;
-					case P_TUNNELID:
-						tid = atoi(argv[i + 1]);
-						break;
-					case P_LOCAL:
-						if (!inet_aton(argv[i + 1], &iad)) {
-							printf("invald ip address: %s\n", argv[i + 1]);
-							return 0;
-						}
-						sip = iad.s_addr;
-						break;
-					case P_REMOTE:
-						if (!inet_aton(argv[i + 1], &iad)) {
-							printf("invald ip address: %s\n", argv[i + 1]);
-							return 0;
-						}
-						dip = iad.s_addr;
-						break;
-					}
-					i += 2;
-				}
-				if (tid > 0xffff) {
-					if (tid == ~0)
-						printf("tunnel-id is mandatory parameter\n");
-					else
-						printf("invalid tunnel-id value: %d\n", tid);
+				switch (p) {
+				default:
+					break;
+				case E_NONE:
+					printf("unknown option: %s\n", argv[i]);
+					return 0;
+				case E_AMBIGUOUS:
+					printf("option is ambiguous: %s\n", argv[i]);
 					return 0;
 				}
-				// tunnel id is in host byte order, addresses are in net byte order
-				eoip_add(excl, ifname, tid, sip, dip, link, ttl, tos);
+
+				if (noarg) {
+					printf("option: %s requires an argument\n", argv[i]);
+					return 0;
+				}
+
+				switch (p) {
+				default:
+					printf("BUG!\n");
+					return 0;
+				case P_NAME:
+					ifname[(sizeof ifname) - 1] = 0;
+					strncpy(ifname, argv[i + 1], IFNAMSIZ);
+					break;
+				case P_TTL:
+					ttl = atoi(argv[i + 1]);
+					break;
+				case P_TOS:
+					tos = atoi(argv[i + 1]);
+					break;
+				case P_LINK:
+					// convert ifname to ifinidex, also support numeric arg
+					link = if_nametoindex(argv[i + 1]);
+					if (!link)
+						link = atoi(argv[i + 1]);
+					if (!link) {
+						printf("invald interface name/index: %s\n", argv[i + 1]);
+						return 0;
+					}
+					break;
+				case P_TUNNELID:
+					tid = atoi(argv[i + 1]);
+					break;
+				case P_LOCAL:
+					if (!inet_aton(argv[i + 1], &iad)) {
+						printf("invald ip address: %s\n", argv[i + 1]);
+						return 0;
+					}
+					sip = iad.s_addr;
+					break;
+				case P_REMOTE:
+					if (!inet_aton(argv[i + 1], &iad)) {
+						printf("invald ip address: %s\n", argv[i + 1]);
+						return 0;
+					}
+					dip = iad.s_addr;
+					break;
+				}
+				i += 2;
+			}
+			if (tid > 0xffff) {
+				if (tid == ~0)
+					printf("tunnel-id is mandatory parameter\n");
+				else
+					printf("invalid tunnel-id value: %d\n", tid);
 				return 0;
 			}
+			// tunnel id is in host byte order, addresses are in net byte order
+			eoip_add(excl, ifname, tid, sip, dip, link, ttl, tos);
+			return 0;
+		}
 		}
 	}
 	return 0;
