99832d55f26c81d4530b4fe3eed384749c9a4b41
BrainSlayer <s.gottschall@dd-wrt.com>
2024-10-01 04:34:22 +0000

    for other variants
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@58413 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/libutils/detect.c
+++ src/router/libutils/libutils/detect.c
@@ -1437,6 +1437,18 @@ generic:;
 		setRouter("Buffalo WXR-5950AX12");
 		return ROUTER_BUFFALO_WXR5950AX12;
 	}
+	if (!strcmp(modelstr, "WXR-6000AX12S")) {
+		setRouter("Buffalo WXR-6000AX12S");
+		return ROUTER_BUFFALO_WXR5950AX12;
+	}
+	if (!strcmp(modelstr, "WXR-6000AX12B")) {
+		setRouter("Buffalo WXR-6000AX12B");
+		return ROUTER_BUFFALO_WXR5950AX12;
+	}
+	if (!strcmp(modelstr, "WXR-6000AX12P")) {
+		setRouter("Buffalo WXR-6000AX12P");
+		return ROUTER_BUFFALO_WXR5950AX12;
+	}
 	setRouter("Qualcomm IPQ6018");
 	return ROUTER_IPQ6018;
 #elif HAVE_IPQ806X
