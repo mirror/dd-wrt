82f97e3bb49974ba5e1d642145359d5c0b53931d
BrainSlayer <s.gottschall@dd-wrt.com>
2024-11-10 17:36:44 +0000

    fix name for iphone tethering
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@58658 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.14/drivers/net/usb/ipheth.c
+++ src/linux/universal/linux-4.14/drivers/net/usb/ipheth.c
@@ -619,7 +619,7 @@ static int ipheth_probe(struct usb_interface *intf,
 
 	netdev->netdev_ops = &ipheth_netdev_ops;
 	netdev->watchdog_timeo = IPHETH_TX_TIMEOUT;
-	strscpy(netdev->name, "eth%d", sizeof(netdev->name));
+	strscpy(netdev->name, "iph%d", sizeof(netdev->name));
 
 	dev = netdev_priv(netdev);
 	dev->udev = udev;
