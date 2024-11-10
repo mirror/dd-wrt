6f2d745a6b8725c37db7eabcba94cb2e324bde1e
BrainSlayer <s.gottschall@dd-wrt.com>
2024-11-10 14:20:18 +0000

    use dynamic lib
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@58653 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/nut.mk
+++ src/router/rules/nut.mk
@@ -7,7 +7,7 @@ nut-configure: usb_modeswitch
 	cd nut && autoreconf -vfi
 	cd nut && ./configure --disable-nls --disable-static --enable-shared --prefix=/usr --host=$(ARCH)-linux --with-usb \
 	--with-usb-includes="-I$(TOP)/usb_modeswitch/libusb/libusb" \
-	--with-usb-libs="$(TOP)/usb_modeswitch/libusb/libusb/.libs/libusb-1.0.a" \
+	--with-usb-libs="-L$(TOP)/usb_modeswitch/libusb/libusb/.libs -lusb-1.0" \
 	--libdir=/usr/lib \
 	--without-python \
 	--without-python2 \
@@ -16,7 +16,7 @@ nut-configure: usb_modeswitch
 	CFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -D_GNU_SOURCE -Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	CXXFLAGS="$(COPTS) $(MIPS16_OPT) -DNEED_PRINTF -D_GNU_SOURCE -Drpl_malloc=malloc -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	LIBUSB_CFLAGS="-I$(TOP)/usb_modeswitch/libusb/libusb" \
-	LIBUSB_LIBS="$(TOP)/usb_modeswitch/libusb/libusb/.libs/libusb-1.0.a"
+	LIBUSB_LIBS="-L$(TOP)/usb_modeswitch/libusb/libusb/.libs/ -lusb-1.0"
 
 nut: usb_modeswitch
 	$(MAKE) -C nut
@@ -26,9 +26,12 @@ nut-clean:
 	@true
 
 nut-install:
+	$(MAKE) -C usb_modeswitch/libusb install DESTDIR=$(INSTALLDIR)/nut
 	$(MAKE) -C nut install DESTDIR=$(INSTALLDIR)/nut
 	rm -rf $(INSTALLDIR)/nut/usr/share
 	rm -rf $(INSTALLDIR)/nut/usr/local
 	rm -f $(INSTALLDIR)/nut/usr/lib/*.la
+	rm -f $(INSTALLDIR)/nut/usr/lib/*.a
 	rm -rf $(INSTALLDIR)/nut/usr/lib/systemd
+	rm -rf $(INSTALLDIR)/nut/usr/lib/pkgconfig
 	rm -rf $(INSTALLDIR)/nut/usr/lib/tmpfiles.d
