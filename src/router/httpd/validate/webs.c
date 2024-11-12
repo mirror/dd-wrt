71c688b59f376414fc007cb0551208bfcf240cf3
BrainSlayer <s.gottschall@dd-wrt.com>
2024-11-12 07:30:03 +0000

    add domene.shop
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@58677 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/validate/webs.c
+++ src/router/httpd/validate/webs.c
@@ -6318,12 +6318,12 @@ void ddns_save_value(webs_t wp)
 	char _ipv6_only[] = "ddns_ipv6_only_XX";
 
 	int enable = websGetVari(wp, "ddns_enable", -1);
-	if (enable > 64 || enable < 0) {
+	if (enable > 65 || enable < 0) {
 		return;
 	}
 
 	int i;
-	for (i = 1; i < 65; i++) {
+	for (i = 1; i < 66; i++) {
 		if (i == enable)
 			continue;
 		snprintf(_username, sizeof(_username), "ddns_username_%d", i);
