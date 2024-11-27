14e007f5b931c5b41c662ac44eb1ae29c439d693
BrainSlayer <s.gottschall@dd-wrt.com>
2024-11-27 10:00:07 +0000

    fix compile bug
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@58726 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-4.4/kernel/time/posix-clock.c
+++ src/linux/universal/linux-4.4/kernel/time/posix-clock.c
@@ -342,7 +342,7 @@ static int pc_clock_settime(clockid_t id, const struct timespec *ts)
 	struct posix_clock_desc cd;
 	int err;
 
-	if (!timespec64_valid_strict(ts))
+	if (!timespec64_valid_strict(&ts64))
 		return -EINVAL;
 
 	err = get_clock_desc(id, &cd);
