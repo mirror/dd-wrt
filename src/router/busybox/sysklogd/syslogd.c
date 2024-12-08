9fb30730cd002880c3f68e4fe92f6858b14bab8c
BrainSlayer <s.gottschall@dd-wrt.com>
2024-12-08 19:44:24 +0000

    fix timezone adjustment
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@58787 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/busybox/sysklogd/syslogd.c
+++ src/router/busybox/sysklogd/syslogd.c
@@ -1055,6 +1055,9 @@ static int NOINLINE syslogd_init(char **argv)
 	//if (opts & OPT_outfile) // -O
 	if (opts & OPT_loglevel) // -l
 		G.logLevel = xatou_range(opt_l, 1, 8);
+	if (opts & OPT_adjustz) // -Z
+		G.adjustTimezone = 1;
+
 	//if (opts & OPT_small) // -S
 #if ENABLE_FEATURE_ROTATE_LOGFILE
 	if (opts & OPT_filesize) // -s
