0854abd3bdac4b2c02b542e8622a967fe8e50df2
BrainSlayer <s.gottschall@dd-wrt.com>
2024-12-28 16:10:59 +0000

    show only for compatible modes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@58929 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/dd-wrt.c
+++ src/router/httpd/visuals/dd-wrt.c
@@ -3199,9 +3199,15 @@ static void internal_ej_show_wireless_single(webs_t wp, char *prefix)
 	showOptionsNames(wp, "wl_basic.cell_density", cell_density, "0 1 2 3",
 			 (char *[]){ "share.disabled", "share.normal", "share.high", "share.veryhigh" },
 			 nvram_default_get("cell_density", "0"));
-	sprintf(legacy, "%s_legacy", prefix);
-	nvram_default_get(legacy, "1");
-	showRadio(wp, "wl_basic.legacy", legacy);
+
+	if ((nvram_nmatch("n-only", "%s_net_mode", prefix) || nvram_nmatch("ng-only", "%s_net_mode", prefix) ||
+	     nvram_nmatch("g-only", "%s_net_mode", prefix) || nvram_nmatch("n2-only", "%s_net_mode", prefix) ||
+	     nvram_nmatch("mixed", "%s_net_mode", prefix) || nvram_nmatch("bg-mixed", "%s_net_mode", prefix) ||
+	     nvram_nmatch("axg-only", "%s_net_mode", prefix))) {
+		sprintf(legacy, "%s_legacy", prefix);
+		nvram_default_get(legacy, "1");
+		showRadio(wp, "wl_basic.legacy", legacy);
+	}
 	if (has_ax(prefix)) {
 		char *netmode = nvram_nget("%s_net_mode", prefix);
 		if (!strcmp(netmode, "mixed") || !strcmp(netmode, "xacn-mixed") || !strcmp(netmode, "ax-only") ||
@@ -5051,9 +5057,14 @@ static void internal_ej_show_wireless_single(webs_t wp, char *prefix)
 	showOptionsNames(wp, "wl_basic.cell_density", cell_density, "0 1 2 3",
 			 (char *[]){ "share.disabled", "share.normal", "share.high", "share.veryhigh" },
 			 nvram_default_get("cell_density", "0"));
-	sprintf(legacy, "%s_legacy", prefix);
-	nvram_default_get(legacy, "1");
-	showRadio(wp, "wl_basic.legacy", legacy);
+	if ((nvram_nmatch("n-only", "%s_net_mode", prefix) || nvram_nmatch("ng-only", "%s_net_mode", prefix) ||
+	     nvram_nmatch("g-only", "%s_net_mode", prefix) || nvram_nmatch("n2-only", "%s_net_mode", prefix) ||
+	     nvram_nmatch("mixed", "%s_net_mode", prefix) || nvram_nmatch("bg-mixed", "%s_net_mode", prefix) ||
+	     nvram_nmatch("axg-only", "%s_net_mode", prefix))) {
+		sprintf(legacy, "%s_legacy", prefix);
+		nvram_default_get(legacy, "1");
+		showRadio(wp, "wl_basic.legacy", legacy);
+	}
 	if (has_ax(prefix)) {
 		char *netmode = nvram_nget("%s_net_mode", prefix);
 		if (!strcmp(netmode, "mixed") || !strcmp(netmode, "xacn-mixed") || !strcmp(netmode, "ax-only") ||
