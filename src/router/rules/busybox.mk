65a5acd287a2ddb4bac83615af5fc468e2a53fb8
BrainSlayer <s.gottschall@dd-wrt.com>
2024-12-30 09:03:47 +0000

    save history enabled
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@58955 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/busybox.mk
+++ src/router/rules/busybox.mk
@@ -700,6 +700,7 @@ endif
 	echo "# CONFIG_TIME64 is not set" >> busybox/.config
 	sed -i 's/\CONFIG_FEATURE_EDITING_HISTORY=15/CONFIG_FEATURE_EDITING_HISTORY=30/g' busybox/.config
 	sed -i 's/\# CONFIG_FEATURE_EDITING_SAVEHISTORY is not set/CONFIG_FEATURE_EDITING_SAVEHISTORY=y/g' busybox/.config
+	echo "CONFIG_FEATURE_EDITING_SAVE_ON_EXIT=y" >> busybox/.config
 
 	cd busybox && make oldconfig
 	
