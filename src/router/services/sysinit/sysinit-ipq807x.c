387969fbea38c1766044684ef6e920738d30b316
BrainSlayer <s.gottschall@dd-wrt.com>
2025-01-02 12:15:05 +0000

    for mr5500/mx5500 fix broadcast packet problem
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@58958 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-ipq807x.c
+++ src/router/services/sysinit/sysinit-ipq807x.c
@@ -1440,9 +1440,10 @@ static void load_ath11k(int profile, int pci, int nss)
 			eval_silence("insmod", driver_ath11k, overdrive);
 	} else {
 		eval_silence("insmod", "mac80211", "nss_redirect=0");
-		eval_silence(
-			"insmod", driver_ath11k, "nss_offload=0", "frame_mode=1",
-			overdrive); // the only working nss firmware for qca5018 on mx5500/mr5500 does not work with nss offload for ath11k
+		if (nvram_match("ath11k_frame_mode", "1"))
+			eval_silence("insmod", driver_ath11k, "nss_offload=0", "frame_mode=1", overdrive);
+		else
+			eval_silence("insmod", driver_ath11k, "nss_offload=0", overdrive); // the only working nss firmware for qca5018 on mx5500/mr5500 does not work with nss offload for ath11k
 		sysprintf("echo 0 > /proc/sys/dev/nss/general/redirect"); // required if nss_redirect is enabled
 	}
 	insmod(driver_ath11k_ahb);
