c069fa8797fcc7d2b22a11e09ff2a387518e3c01
BrainSlayer <s.gottschall@dd-wrt.com>
2025-01-09 07:45:28 +0000

    fix argument declaration
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@59085 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/wiviz2/wiviz.c
+++ src/router/wiviz2/wiviz.c
@@ -53,8 +53,8 @@ To add:
 #define INFO_RXRATE 3
 #define INFO_TXRATE 4
 
-int getWifiInfo_ath9k(char *ifname, unsigned char *mac, int field);	// only used internal
-int getWifiInfo(char *ifname, unsigned char *mac, int field);
+int getWifiInfo_ath9k(const char *ifname, unsigned char *mac, int field);	// only used internal
+int getWifiInfo(const char *ifname, unsigned char *mac, int field);
 
 #define getNoise(ifname, mac) getWifiInfo(ifname, mac, INFO_NOISE)
 #define getRssi(ifname, mac) getWifiInfo(ifname, mac, INFO_RSSI)
