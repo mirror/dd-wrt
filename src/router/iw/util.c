d148cd69c4fb536072f6ed26624b01c37819790f
BrainSlayer <s.gottschall@dd-wrt.com>
2025-01-16 07:58:47 +0000

    fix frequencies
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@59202 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/iw/util.c
+++ src/router/iw/util.c
@@ -213,9 +213,6 @@ int ieee80211_frequency_to_channel(int freq)
 	/* see 802.11-2007 17.3.8.3.2 and Annex J */
 	if (freq == 2484)
 		return 14;
-	/* see 802.11ax D6.1 27.3.23.2 and Annex E */
-	else if (freq == 5935)
-		return 2;
 	// boes hack...
 	else if (freq == 2407)
 		return 0;
@@ -223,22 +220,28 @@ int ieee80211_frequency_to_channel(int freq)
 		return (freq - 2407) / 5 + 256;
 	else if (freq < 2484)
 		return (freq - 2407) / 5;
+	else if (freq < 2502 && freq > 2484)
+		return 14;
+	else if (freq < 2512 && freq > 2484)
+		return 15;
 	else if (freq > 2484 && freq < 4000 )
-		return (freq - 2414) / 5;
+		return (15 + ((freq - 2512) / 20)) & 0xff;
 	else if (freq < 4990 && freq > 4940)
 		return ((freq * 10) + (((freq % 5) == 2) ? 5 : 0) - 49400) / 5;
-	else if (freq >= 4800 && freq < 5005)
+	else if (freq > 4800 && freq < 5005)
 		return (freq - 4000) / 5;
-	else if (freq < 5945)
-		return (freq - 5000) / 5;
+	else if (freq == 5935)
+		return (freq - 5925) / 5;
+	else if (freq > 5950 && freq <= 7115)
+		return (freq - 5950) / 5;
 	else if (freq <= 45000) /* DMG band lower limit */
-		/* see 802.11ax D4.1 27.3.22.2 */
-		return (freq - 5940) / 5;
-	else if (freq >= 58320 && freq <= 70200)
+		return ((freq - 5000) / 5) & 0xff;
+	else if (freq >= 58320 && freq <= 64800)
 		return (freq - 56160) / 2160;
 	else
 		return 0;
 }
+
 
 void print_ssid_escaped(const uint8_t len, const uint8_t *data)
 {
