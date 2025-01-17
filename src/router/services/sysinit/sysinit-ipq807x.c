888c6d5ca70c0f3dd468866ac119983b06390c64
BrainSlayer <s.gottschall@dd-wrt.com>
2025-01-17 08:12:04 +0000

    not required anymore, we initialize the phy direct
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@59223 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-ipq807x.c
+++ src/router/services/sysinit/sysinit-ipq807x.c
@@ -1436,8 +1436,8 @@ void start_sysinit(void)
 		setscaling(1800000);
 		//	disableportlearn();
 		sysprintf("echo 1 > /proc/sys/dev/nss/clock/auto_scale");
-		eval("fw_setenv", "bootcmd",
-		     "aq_load_fw; if test $auto_recovery = no; then bootipq; elif test $boot_part = 1; then run bootpart1; else run bootpart2; fi");
+//		eval("fw_setenv", "bootcmd",
+//		     "aq_load_fw; if test $auto_recovery = no; then bootipq; elif test $boot_part = 1; then run bootpart1; else run bootpart2; fi");
 
 		//reload firmware
 		eval("ssdk_sh", "debug", "phy", "set", "0x8", "0x401e2680", "0x1");
