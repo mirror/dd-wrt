59507a29267df2d1cfe2c273c6088fe43dccc409
BrainSlayer <s.gottschall@dd-wrt.com>
2025-01-17 07:20:48 +0000

    refactor a little bit
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@59220 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/devices/wireless.c
+++ src/router/services/sysinit/devices/wireless.c
@@ -64,7 +64,7 @@ static char *has_device(char *dev)
 static void wait_for_wifi(int min)
 {
 	int cnt = 0;
-	while ((cnt++) < 6 && getdevicecount()<min) {
+	while ((cnt++) < 6 && getdevicecount() < min) {
 		sleep(1);
 	}
 }
@@ -142,6 +142,260 @@ int load_mac80211(void)
 	return 0;
 }
 
+int load_iwlwifi(void)
+{
+	insmod("libipw");
+	if (!detectchange("ipw2100") && !detectchange("ipw2200"))
+		rmmod("libipw");
+	insmod("iwlegacy");
+	if (!detectchange("iwl3945") && !detectchange("iwl4965")) {
+		rmmod("iwlegacy");
+	}
+	insmod("iwlwifi");
+	sleep(3); // neet to wait a little bit
+	if (!detectchange("iwldvm") && !detectchange("iwlmvm")) {
+		rmmod("iwlwifi");
+	}
+	return 0;
+}
+
+int load_ath11k(void)
+{
+	insmod("thermal_sys");
+	insmod("hwmon");
+	if (!nvram_match("noath11k", "1")) {
+		insmod("ath11k");
+		insmod("qrtr");
+		insmod("mhi");
+		insmod("qrtr-mhi");
+		insmod("ath11k_pci");
+	}
+	return 0;
+}
+
+int load_mt76(void)
+{
+	insmod("thermal_sys");
+	insmod("hwmon");
+	dd_loginfo("sysinit", "load Medatek MT76 Driver");
+	insmod("nls_base usb-common usbcore ehci-hcd ehci-platform ehci-fsl ehci-pci usb-uhci uhci-hcd usb-ohci ohci-hcd ohci-pci xhci-hcd xhci-pci xhci-plat-hcd xhci-mtk dwc_otg usb-libusual fsl-mph-dr-of phy-mxs-usb extcon-core extcon ci_hdrc ci13xxx_imx usbmisc_imx ci_hdrc_imx phy-qcom-dwc3 dwc3-of-simple dwc3 dwc3-qcom phy-qcom-hsusb phy-qcom-ssusb phy-qcom-ipq806x-usb phy-qcom-ipq806x-sata phy-qcom-ipq4019-usb");
+	insmod("mt76");
+	insmod("mt76-usb");
+	insmod("mt76-sdio");
+	insmod("mt76-connac-lib");
+	insmod("mt7615-common");
+	insmod("mt7663-usb-sdio-common");
+	insmod("mt7615e");
+	insmod("mt7663u");
+	insmod("mt7663s");
+	insmod("mt76x02-lib");
+	insmod("mt76x02-usb");
+	insmod("mt76x2-common");
+	insmod("mt76x2e");
+	insmod("mt76x2u");
+	insmod("mt76x0-common");
+	insmod("mt76x0e");
+	insmod("mt76x0u");
+	insmod("mt7603e");
+	insmod("mt7915e");
+	insmod("mt7921-common");
+	insmod("mt76-sdio");
+	insmod("mt7921e");
+	insmod("mt7921s");
+	insmod("mt7996e");
+	insmod("rt2x00lib");
+	insmod("rt2x00mmio");
+	insmod("rt2x00pci");
+	insmod("rt2800lib");
+	insmod("rt2800mmio");
+
+	return 0;
+}
+int load_ath10k(void)
+{
+	insmod("thermal_sys");
+	insmod("hwmon");
+	nvram_default_get("ath10k_encap", "0");
+	if (!nvram_match("noath10k", "1")) {
+		insmod("ath");
+		nvram_set("wlan0_fwtype_use", "ddwrt");
+		nvram_set("wlan1_fwtype_use", "ddwrt");
+		nvram_set("wlan2_fwtype_use", "ddwrt");
+		nvram_set("wlan3_fwtype_use", "ddwrt");
+		nvram_set("wlan4_fwtype_use", "ddwrt");
+		nvram_set("wlan5_fwtype_use", "ddwrt");
+		nvram_set("wlan6_fwtype_use", "ddwrt");
+		nvram_set("wlan7_fwtype_use", "ddwrt");
+		nvram_set("wlan0_dualband_use", nvram_default_get("wlan0_dualband", "0"));
+		nvram_set("wlan1_dualband_use", nvram_default_get("wlan1_dualband", "0"));
+		nvram_set("wlan2_dualband_use", nvram_default_get("wlan2_dualband", "0"));
+		nvram_set("wlan3_dualband_use", nvram_default_get("wlan3_dualband", "0"));
+		unsigned int dual = 0;
+		int v = nvram_geti("wlan0_dualband");
+		if (v == 2 || v == 5)
+			dual |= v;
+		v = nvram_geti("wlan1_dualband");
+		if (v == 2 || v == 5)
+			dual |= v << 8;
+		v = nvram_geti("wlan2_dualband");
+		if (v == 2 || v == 5)
+			dual |= v << 16;
+		v = nvram_geti("wlan3_dualband");
+		if (v == 2 || v == 5)
+			dual |= v << 24;
+		if (!dual)
+			dual = 0x05050505;
+		char dualband[32];
+		sprintf(dualband, "dual_band=0x%08x", dual);
+		//			sprintf(dualband, "dual_band=0x02020202");
+		if (nvram_match("ath10k_encap", "1"))
+			eval("insmod", "ath10k", "ethernetmode=1", dualband);
+		else
+			eval("insmod", "ath10k", dualband);
+		if (!detectchange(NULL)) {
+			rmmod("ath10k");
+			rmmod("ath");
+		}
+	}
+	return 0;
+}
+
+int load_rtlwifi(void)
+{
+	dd_loginfo("sysinit", "load Realtek RTLWIFI Driver");
+	int total = 0;
+	insmod("nls_base usb-common usbcore ehci-hcd ehci-platform ehci-fsl ehci-pci usb-uhci uhci-hcd usb-ohci ohci-hcd ohci-pci xhci-hcd xhci-pci xhci-plat-hcd xhci-mtk dwc_otg usb-libusual fsl-mph-dr-of phy-mxs-usb extcon-core extcon ci_hdrc ci13xxx_imx usbmisc_imx ci_hdrc_imx phy-qcom-dwc3 dwc3-of-simple dwc3 dwc3-qcom phy-qcom-hsusb phy-qcom-ssusb phy-qcom-ipq806x-usb phy-qcom-ipq806x-sata phy-qcom-ipq4019-usb");
+	insmod("rtlwifi");
+	insmod("rtl_pci");
+	insmod("rtl_usb");
+	insmod("btcoexist");
+	int wificnt = 0;
+	wificnt += detectchange("rtl8188ee");
+	insmod("rtl8192c-common");
+	wificnt += detectchange("rtl8192ce");
+	wificnt += detectchange("rtl8192de");
+	wificnt += detectchange("rtl8192se");
+	wificnt += detectchange("rtl8821ae");
+	wificnt += detectchange("rtl8192cu");
+	wificnt += detectchange("rtl8192ee");
+	if (!wificnt)
+		rmmod("rtl8192c-common");
+	total += wificnt;
+	wificnt = 0;
+
+	insmod("rtl8723-common");
+	wificnt += detectchange("rtl8723be");
+	wificnt += detectchange("rtl8723ae");
+	if (!wificnt)
+		rmmod("rtl8723-common");
+	total += wificnt;
+	wificnt = 0;
+	if (!total) {
+		rmmod("btcoexist");
+		rmmod("rtl_usb");
+		rmmod("rtl_pci");
+		rmmod("rtlwifi");
+	}
+
+	insmod("rtw88_core");
+	insmod("rtw88_pci");
+	insmod("rtw88_8822b");
+	insmod("rtw88_8822c");
+	insmod("rtw88_8821c");
+	insmod("rtw88_8723d");
+	wificnt += detectchange("rtw88_8822be");
+	wificnt += detectchange("rtw88_8822ce");
+	wificnt += detectchange("rtw88_8821ce");
+	wificnt += detectchange("rtw88_8723de");
+	if (!wificnt) {
+		rmmod("rtw88_8723d");
+		rmmod("rtw88_8821c");
+		rmmod("rtw88_8822c");
+		rmmod("rtw88_8822b");
+		rmmod("rtw88_pci");
+		rmmod("rtw88_core");
+	}
+	return 0;
+}
+
+int load_brcmfmac(void)
+{
+	dd_loginfo("sysinit", "load Broadcom FMAC Driver");
+	insmod("brcmutil");
+	insmod("brcmfmac");
+	if (!detectchange("brcmfmac")) {
+		rmmod("brcmutil");
+	}
+	return 0;
+}
+
+int load_wil6210(void)
+{
+	eval("insmod", "wil6210", "led_id=2");
+	if (!detectchange(NULL))
+		rmmod("wil6210");
+	return 0;
+}
+
+int load_ath9k(void)
+{
+	dd_loginfo("sysinit", "load ATH9K 802.11n Driver");
+	load_mac80211();
+	// some are just for future use and not (yet) there
+	insmod("ath");
+#ifdef HAVE_ATH5K
+	if (loadath5k && (mask & RADIO_LEGACY)) {
+		dd_loginfo("sysinit", "load ATH5K 802.11 Driver");
+		insmod("ath5k");
+		if (!detectchange(NULL))
+			rmmod("ath5k");
+	}
+#endif
+	if (!nvram_match("no_ath9k", "1") && (mask & RADIO_ATH9K)) {
+		int od = nvram_default_geti("power_overdrive", 0);
+		char overdrive[32];
+		sprintf(overdrive, "overdrive=%d", od);
+
+#ifdef HAVE_WZRG450
+		eval("insmod", "ath9k", overdrive, "blink=1");
+#elif HAVE_PERU
+		char *dis = getUEnv("rndis");
+		if (dis && !strcmp(dis, "1"))
+			eval("insmod", "ath9k", overdrive, "no_ahb=1");
+		else
+			eval("insmod", "ath9k", overdrive);
+
+#else
+		eval("insmod", "ath9k", overdrive);
+#endif
+		if (!detectchange(NULL))
+			rmmod("ath9k");
+
+		eval("insmod", "ath9k_htc");
+		if (!detectchange(NULL))
+			rmmod("ath9k_htc");
+	}
+	if (!totalwifi)
+		rmmod("ath");
+	delete_ath9k_devices(NULL);
+}
+
+int load_madwifi(void)
+{
+	dd_loginfo("sysinit", "load ATH 802.11 a/b/g Driver");
+	insmod("ath_hal");
+	if (nvram_exists("rate_control")) {
+		char rate[64];
+
+		sprintf(rate, "ratectl=%s", nvram_safe_get("rate_control"));
+		eval("insmod", "ath_pci", rate);
+		eval("insmod", "ath_ahb", rate);
+	} else {
+		insmod("ath_pci");
+		insmod("ath_ahb");
+	}
+	return 0;
+}
 #define RADIO_ALL 0xffff
 #define RADIO_ATH9K 0x1
 #define RADIO_ATH10K 0x2
@@ -267,252 +521,50 @@ static int detect_wireless_devices(int mask)
 	nvram_default_get("rate_control", "minstrel");
 #ifdef HAVE_MADWIFI
 	if (loadlegacy && (mask & RADIO_LEGACY)) {
-		dd_loginfo("sysinit","load ATH 802.11 a/b/g Driver");
-		insmod("ath_hal");
-		if (nvram_exists("rate_control")) {
-			char rate[64];
-
-			sprintf(rate, "ratectl=%s", nvram_safe_get("rate_control"));
-			eval("insmod", "ath_pci", rate);
-			eval("insmod", "ath_ahb", rate);
-		} else {
-			insmod("ath_pci");
-			insmod("ath_ahb");
-		}
+		load_madwifi();
 	}
 #ifdef HAVE_ATH9K
 	{
 		if (loadath9k || loadath5k) {
-			dd_loginfo("sysinit","load ATH9K 802.11n Driver");
-			load_mac80211();
-			// some are just for future use and not (yet) there
-			insmod("ath");
-#ifdef HAVE_ATH5K
-			if (loadath5k && (mask & RADIO_LEGACY)) {
-				dd_loginfo("sysinit","load ATH5K 802.11 Driver");
-				insmod("ath5k");
-				if (!detectchange(NULL))
-					rmmod("ath5k");
-			}
-#endif
-			if (!nvram_match("no_ath9k", "1") && (mask & RADIO_ATH9K)) {
-				int od = nvram_default_geti("power_overdrive", 0);
-				char overdrive[32];
-				sprintf(overdrive, "overdrive=%d", od);
-
-#ifdef HAVE_WZRG450
-				eval("insmod", "ath9k", overdrive, "blink=1");
-#elif HAVE_PERU
-				char *dis = getUEnv("rndis");
-				if (dis && !strcmp(dis, "1"))
-					eval("insmod", "ath9k", overdrive, "no_ahb=1");
-				else
-					eval("insmod", "ath9k", overdrive);
-
-#else
-				eval("insmod", "ath9k", overdrive);
-#endif
-				if (!detectchange(NULL))
-					rmmod("ath9k");
-
-				eval("insmod", "ath9k_htc");
-				if (!detectchange(NULL))
-					rmmod("ath9k_htc");
-			}
-			if (!totalwifi)
-				rmmod("ath");
-			delete_ath9k_devices(NULL);
+			load_ath9k();
 		}
 	}
 #endif
 
 #ifdef HAVE_ATH10K
-	dd_loginfo("sysinit","load ATH/QCA 802.11ac Driver");
+	dd_loginfo("sysinit", "load ATH/QCA 802.11ac Driver");
 	if ((mask & RADIO_ATH10K)) {
-		insmod("thermal_sys");
-		insmod("hwmon");
-		nvram_default_get("ath10k_encap", "0");
-		if (!nvram_match("noath10k", "1")) {
-			insmod("ath");
-			nvram_set("wlan0_fwtype_use", "ddwrt");
-			nvram_set("wlan1_fwtype_use", "ddwrt");
-			nvram_set("wlan2_fwtype_use", "ddwrt");
-			nvram_set("wlan3_fwtype_use", "ddwrt");
-			nvram_set("wlan4_fwtype_use", "ddwrt");
-			nvram_set("wlan5_fwtype_use", "ddwrt");
-			nvram_set("wlan6_fwtype_use", "ddwrt");
-			nvram_set("wlan7_fwtype_use", "ddwrt");
-			nvram_set("wlan0_dualband_use", nvram_default_get("wlan0_dualband", "0"));
-			nvram_set("wlan1_dualband_use", nvram_default_get("wlan1_dualband", "0"));
-			nvram_set("wlan2_dualband_use", nvram_default_get("wlan2_dualband", "0"));
-			nvram_set("wlan3_dualband_use", nvram_default_get("wlan3_dualband", "0"));
-			unsigned int dual = 0;
-			int v = nvram_geti("wlan0_dualband");
-			if (v == 2 || v == 5)
-				dual |= v;
-			v = nvram_geti("wlan1_dualband");
-			if (v == 2 || v == 5)
-				dual |= v << 8;
-			v = nvram_geti("wlan2_dualband");
-			if (v == 2 || v == 5)
-				dual |= v << 16;
-			v = nvram_geti("wlan3_dualband");
-			if (v == 2 || v == 5)
-				dual |= v << 24;
-			if (!dual)
-				dual = 0x05050505;
-			char dualband[32];
-			sprintf(dualband, "dual_band=0x%08x", dual);
-			//			sprintf(dualband, "dual_band=0x02020202");
-			if (nvram_match("ath10k_encap", "1"))
-				eval("insmod", "ath10k", "ethernetmode=1", dualband);
-			else
-				eval("insmod", "ath10k", dualband);
-			if (!detectchange(NULL)) {
-				rmmod("ath10k");
-				rmmod("ath");
-			}
-		}
+		load_ath10k();
 	}
 #endif
 #ifdef HAVE_WIL6210
 	if ((mask & RADIO_WIL6210)) {
-		eval("insmod", "wil6210", "led_id=2");
-		if (!detectchange(NULL))
-			rmmod("wil6210");
+		load_wil6210();
 	}
 #endif
 #ifdef HAVE_BRCMFMAC
 	if ((mask & RADIO_BRCMFMAC)) {
-		dd_loginfo("sysinit","load Broadcom FMAC Driver");
-		insmod("brcmutil");
-		insmod("brcmfmac");
-		if (!detectchange("brcmfmac")) {
-			rmmod("brcmutil");
-		}
+		load_brcmfmac();
 	}
 #endif
 #ifdef HAVE_RTLWIFI
 	if ((mask & RADIO_RTLWIFI)) {
-		dd_loginfo("sysinit","load Realtek RTLWIFI Driver");
-		int total = 0;
-		insmod("nls_base usb-common usbcore ehci-hcd ehci-platform ehci-fsl ehci-pci usb-uhci uhci-hcd usb-ohci ohci-hcd ohci-pci xhci-hcd xhci-pci xhci-plat-hcd xhci-mtk dwc_otg usb-libusual fsl-mph-dr-of phy-mxs-usb extcon-core extcon ci_hdrc ci13xxx_imx usbmisc_imx ci_hdrc_imx phy-qcom-dwc3 dwc3-of-simple dwc3 dwc3-qcom phy-qcom-hsusb phy-qcom-ssusb phy-qcom-ipq806x-usb phy-qcom-ipq806x-sata phy-qcom-ipq4019-usb");
-		insmod("rtlwifi");
-		insmod("rtl_pci");
-		insmod("rtl_usb");
-		insmod("btcoexist");
-		int wificnt = 0;
-		wificnt += detectchange("rtl8188ee");
-		insmod("rtl8192c-common");
-		wificnt += detectchange("rtl8192ce");
-		wificnt += detectchange("rtl8192de");
-		wificnt += detectchange("rtl8192se");
-		wificnt += detectchange("rtl8821ae");
-		wificnt += detectchange("rtl8192cu");
-		wificnt += detectchange("rtl8192ee");
-		if (!wificnt)
-			rmmod("rtl8192c-common");
-		total += wificnt;
-		wificnt = 0;
-
-		insmod("rtl8723-common");
-		wificnt += detectchange("rtl8723be");
-		wificnt += detectchange("rtl8723ae");
-		if (!wificnt)
-			rmmod("rtl8723-common");
-		total += wificnt;
-		wificnt = 0;
-		if (!total) {
-			rmmod("btcoexist");
-			rmmod("rtl_usb");
-			rmmod("rtl_pci");
-			rmmod("rtlwifi");
-		}
-
-		insmod("rtw88_core");
-		insmod("rtw88_pci");
-		insmod("rtw88_8822b");
-		insmod("rtw88_8822c");
-		insmod("rtw88_8821c");
-		insmod("rtw88_8723d");
-		wificnt += detectchange("rtw88_8822be");
-		wificnt += detectchange("rtw88_8822ce");
-		wificnt += detectchange("rtw88_8821ce");
-		wificnt += detectchange("rtw88_8723de");
-		if (!wificnt) {
-			rmmod("rtw88_8723d");
-			rmmod("rtw88_8821c");
-			rmmod("rtw88_8822c");
-			rmmod("rtw88_8822b");
-			rmmod("rtw88_pci");
-			rmmod("rtw88_core");
-		}
+		load_rtlwifi();
 	}
 #endif
 #if 1 //def HAVE_ATH11K
 	if ((mask & RADIO_ATH11K)) {
-		insmod("thermal_sys");
-		insmod("hwmon");
-		if (!nvram_match("noath11k", "1")) {
-			insmod("ath11k");
-			insmod("qrtr");
-			insmod("mhi");
-			insmod("qrtr-mhi");
-			insmod("ath11k_pci");
-		}
+		load_ath11k();
 	}
 #endif
 #ifdef HAVE_MT7615
 	if ((mask & RADIO_MT76) && !nvram_match("nomt76", "1")) {
-		insmod("thermal_sys");
-		insmod("hwmon");
-		dd_loginfo("sysinit","load Medatek MT76 Driver");
-		insmod("nls_base usb-common usbcore ehci-hcd ehci-platform ehci-fsl ehci-pci usb-uhci uhci-hcd usb-ohci ohci-hcd ohci-pci xhci-hcd xhci-pci xhci-plat-hcd xhci-mtk dwc_otg usb-libusual fsl-mph-dr-of phy-mxs-usb extcon-core extcon ci_hdrc ci13xxx_imx usbmisc_imx ci_hdrc_imx phy-qcom-dwc3 dwc3-of-simple dwc3 dwc3-qcom phy-qcom-hsusb phy-qcom-ssusb phy-qcom-ipq806x-usb phy-qcom-ipq806x-sata phy-qcom-ipq4019-usb");
-		insmod("mt76");
-		insmod("mt76-usb");
-		insmod("mt76-sdio");
-		insmod("mt76-connac-lib");
-		insmod("mt7615-common");
-		insmod("mt7663-usb-sdio-common");
-		insmod("mt7615e");
-		insmod("mt7663u");
-		insmod("mt7663s");
-		insmod("mt76x02-lib");
-		insmod("mt76x02-usb");
-		insmod("mt76x2-common");
-		insmod("mt76x2e");
-		insmod("mt76x2u");
-		insmod("mt76x0-common");
-		insmod("mt76x0e");
-		insmod("mt76x0u");
-		insmod("mt7603e");
-		insmod("mt7915e");
-		insmod("mt7921-common");
-		insmod("mt76-sdio");
-		insmod("mt7921e");
-		insmod("mt7921s");
-		insmod("mt7996e");
-		insmod("rt2x00lib");
-		insmod("rt2x00mmio");
-		insmod("rt2x00pci");
-		insmod("rt2800lib");
-		insmod("rt2800mmio");
+		load_mt76();
 	}
 #endif
 #ifdef HAVE_IWLWIFI
 	if ((mask & RADIO_IWLWIFI)) {
-		insmod("libipw");
-		if (!detectchange("ipw2100") && !detectchange("ipw2200"))
-			rmmod("libipw");
-		insmod("iwlegacy");
-		if (!detectchange("iwl3945") && !detectchange("iwl4965")) {
-			rmmod("iwlegacy");
-		}
-		insmod("iwlwifi");
-		sleep(3); // neet to wait a little bit
-		if (!detectchange("iwldvm") && !detectchange("iwlmvm")) {
-			rmmod("iwlwifi");
-		}
+		load_iwlwifi();
 	}
 #endif
 /*	if (!totalwifi) {
