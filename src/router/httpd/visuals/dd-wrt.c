5fea8faa66e18ee2c9969fbe1feb70320ce6600f
BrainSlayer <s.gottschall@dd-wrt.com>
2025-01-20 03:46:02 +0000

    both mustbe considered
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@59252 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/dd-wrt.c
+++ src/router/httpd/visuals/dd-wrt.c
@@ -2040,7 +2040,8 @@ static void show_netmode(webs_t wp, char *prefix)
 			websWrite(wp, "document.write(\"<option value=\\\"ac-only\\\" %s>\" + wl_basic.ac + \"</option>\");\n",
 				  nvram_match(wl_net_mode, "ac-only") ? "selected=\\\"selected\\\"" : "");
 		}
-		if (is_ath11k(prefix) && has_5ghz(prefix) && !is_ath10k(prefix)) {
+		if (is_ath11k(prefix) && (has_5ghz(prefix) || has_6ghz(prefix)) && !is_ath10k(prefix)) {
+			if (has_5ghz(prefix))
 			websWrite(wp, "document.write(\"<option value=\\\"xacn-mixed\\\" %s>\" + wl_basic.xacn + \"</option>\");\n",
 				  nvram_match(wl_net_mode, "xacn-mixed") ? "selected=\\\"selected\\\"" : "");
 			websWrite(wp, "document.write(\"<option value=\\\"ax-only\\\" %s>\" + wl_basic.ax + \"</option>\");\n",
