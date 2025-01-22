7efa29ab2818db2728c9f9104357c0b95d248c38
BrainSlayer <s.gottschall@dd-wrt.com>
2025-01-22 15:03:31 +0000

    update plex: remove old files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@59316 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/plex/aarch64/usr/lib/plexmediaserver/Resources/Plug-ins-c6bbc1b53/Framework.bundle/Contents/Resources/Versions/2/Python/Framework/handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-from base import BaseHandler, InternalRequestHandler, ManagementRequestHandler, PrefixRequestHandler, generate_prefix_handler
-from plugin import PluginRequestHandler
-from runtime import RuntimeRequestHandler
-from debugging import DebugRequestHandler
-from resources import ResourceRequestHandler
-from messaging import MessagingRequestHandler
-from services import ServiceRequestHandler
