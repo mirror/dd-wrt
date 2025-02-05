107c8103472919ab8afb06d9f1e1f81ab1fa51fd
BrainSlayer <s.gottschall@dd-wrt.com>
2025-02-05 22:41:58 +0000

    static wolfssl
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@59531 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/wolfssl.mk
+++ src/router/rules/wolfssl.mk
@@ -9,7 +9,16 @@ wolfssl-configure:
 	cd wolfssl && ./autogen.sh
 	mkdir -p wolfssl/minimal
 	mkdir -p wolfssl/standard
-	cd wolfssl/standard && ../configure $(NO_TLS)  --prefix=/usr --libdir=/usr/lib --host=$(ARCH)-linux --disable-crypttests --disable-examples --disable-benchmark --disable-examples --enable-opensslextra --enable-opensslall --enable-shared --enable-fastmath --disable-static -disable-errorstrings --enable-lowresource --disable-oldtls --enable-aesgcm --enable-poly1305 --enable-chacha --enable-ecc --disable-sslv3 --enable-des3 --enable-md4 --enable-stunnel --enable-tls13 --enable-session-ticket --enable-wpas --enable-cmac CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	mkdir -p wolfssl/standard_static
+	cd wolfssl/standard && ../configure $(NO_TLS)  --prefix=/usr --libdir=/usr/lib --host=$(ARCH)-linux --disable-crypttests --disable-examples --disable-benchmark --disable-examples --enable-opensslextra --enable-opensslall --enable-shared --enable-fastmath --disable-static -disable-errorstrings   --enable-ocsp --enable-lowresource --disable-oldtls --enable-aesgcm  --enable-aesccm --enable-poly1305 --enable-chacha --enable-ecc --disable-sslv3 --enable-des3 --enable-md4 --enable-stunnel --enable-tls13 --enable-session-ticket --enable-wpas --enable-cmac --enable-dh CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	    AR_FLAGS="cru $(LTOPLUGIN)" \
+	    RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
+
+#	cd wolfssl/standard_static && ../configure $(NO_TLS)  --prefix=/usr --libdir=/usr/lib --host=$(ARCH)-linux --disable-crypttests --disable-examples --disable-benchmark --disable-examples --enable-opensslextra --enable-opensslall --disable-shared --enable-fastmath --enable-static -disable-errorstrings   --enable-ocsp --enable-lowresource --disable-oldtls --enable-aesgcm  --enable-aesccm --enable-poly1305 --enable-chacha --enable-ecc --disable-sslv3 --enable-des3 --enable-md4 --enable-stunnel --enable-tls13 --enable-session-ticket --enable-wpas --enable-cmac --enable-dh CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+#	    AR_FLAGS="cru $(LTOPLUGIN)" \
+#	    RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
+
+	cd wolfssl/standard_static && ../configure $(NO_TLS)  --prefix=/usr --libdir=/usr/lib --host=$(ARCH)-linux --disable-shared --enable-static --enable-lighty --enable-opensslall --enable-opensslextra --enable-sni --enable-stunnel --enable-altcertchains --disable-crypttests --disable-examples --enable-ipv6 --enable-aesccm --enable-certgen --enable-chacha --enable-poly1305 --enable-dh --enable-arc4 --enable-tlsv10 --enable-tls13 --enable-session-ticket --disable-dtls --enable-curve25519 --disable-curve448 --enable-openvpn --disable-afalg --enable-devcrypto=no --enable-ocsp --enable-ocspstapling --enable-ocspstapling2 --enable-wpas --enable-fortress --enable-fastmath CFLAGS="$(COPTS) $(MIPS16_OPT) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	    AR_FLAGS="cru $(LTOPLUGIN)" \
 	    RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
 
@@ -18,18 +27,21 @@ wolfssl-configure:
 	    RANLIB="$(ARCH)-linux-ranlib $(LTOPLUGIN)"
 	$(MAKE) -C wolfssl/minimal
 	$(MAKE) -C wolfssl/standard
+	$(MAKE) -C wolfssl/standard_static
 
 wolfssl:
 ifeq ($(CONFIG_WOLFSSLMIN),y)
 	$(MAKE) -C wolfssl/minimal
 else
 	$(MAKE) -C wolfssl/standard
+	$(MAKE) -C wolfssl/standard_static
 endif
 	-rm wolfssl/wolfssl/options.h
 
 wolfssl-clean:
 	-make -C wolfssl/minimal clean
 	-make -C wolfssl/standard clean
+	-make -C wolfssl/standard_static clean
 	@true
 
 wolfssl-install:
