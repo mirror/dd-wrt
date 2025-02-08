087a06cbd643a1c7b95bd571bc4ded08100aa9e0
BrainSlayer <s.gottschall@dd-wrt.com>
2025-02-08 09:28:40 +0000

    fix tor
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@59580 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libevent.mk
+++ src/router/rules/libevent.mk
@@ -1,13 +1,13 @@
+
 libevent:
-	$(MAKE) -C libevent/openssl
-	$(MAKE) -C libevent/wolfssl
+	CC="ccache $(ARCH)-linux-uclibc-gcc" \
+	CFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) -I$(SSLPATH)/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	CPPFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) -I$(SSLPATH)/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	LDFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) -L$(SSLPATH)" \
+	$(MAKE) -C libevent
 
 libevent-install:
-ifneq ($(CONFIG_WOLFSSL),y)
-	$(MAKE) -C libevent/openssl install DESTDIR=$(INSTALLDIR)/libevent
-else
-	$(MAKE) -C libevent/wolfssl install DESTDIR=$(INSTALLDIR)/libevent
-endif
+	$(MAKE) -C libevent install DESTDIR=$(INSTALLDIR)/libevent
 	rm -rf $(INSTALLDIR)/libevent/usr/bin
 	rm -rf $(INSTALLDIR)/libevent/usr/include
 	rm -rf $(INSTALLDIR)/libevent/usr/lib/pkgconfig
@@ -19,23 +19,11 @@ libevent-clean:
 	$(MAKE) -C libevent clean
 
 libevent-configure: openssl
-	cd libevent && ./autogen.sh
-	mkdir -p libevent/wolfssl
-	mkdir -p libevent/openssl
-	
-	cd libevent/openssl && ../configure  --disable-debug-mode --prefix=/usr ac_cv_host=$(ARCH)-uclibc-linux --libdir=/usr/lib --target=$(ARCH)-linux --host=$(ARCH) CC="ccache $(ARCH)-linux-uclibc-gcc" \
+	cd libevent && ./configure  --disable-debug-mode --prefix=/usr ac_cv_host=$(ARCH)-uclibc-linux --libdir=/usr/lib --target=$(ARCH)-linux --host=$(ARCH) CC="ccache $(ARCH)-linux-uclibc-gcc" \
+	CFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) -I$(SSLPATH)/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	LDFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) -L$(SSLPATH)" 
 	CC="ccache $(ARCH)-linux-uclibc-gcc" \
 	CFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) -I$(SSLPATH)/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	CPPFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) -I$(SSLPATH)/include -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	LDFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) -L$(SSLPATH) -lssl -lcrypto"
-	$(MAKE) -C libevent/openssl
-
-
-	cd libevent/wolfssl && ../configure  --disable-debug-mode --prefix=/usr ac_cv_host=$(ARCH)-uclibc-linux --libdir=/usr/lib --target=$(ARCH)-linux --host=$(ARCH) CC="ccache $(ARCH)-linux-uclibc-gcc" \
-	CFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) -DWOLFSSL_USE_OPTIONS_H -I$(TOP)/wolfssl/standard -I$(TOP)/wolfssl/ -I$(TOP)/wolfssl/wolfssl -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	CPPFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) -DWOLFSSL_USE_OPTIONS_H -I$(TOP)/wolfssl/standard -I$(TOP)/wolfssl/ -I$(TOP)/wolfssl/wolfssl -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	LDFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) -L$(TOP)/wolfssl/standard/src/.libs -lwolfssl" \
-	CC="ccache $(ARCH)-linux-uclibc-gcc" \
-	OPENSSL_LIBS="-L$(TOP)/wolfssl/standard/src/.libs -lwolfssl" \
-	OPENSSL_CFLAGS="-DWOLFSSL_USE_OPTIONS_H -I$(TOP)/wolfssl/standard -I$(TOP)/wolfssl/ -I$(TOP)/wolfssl/wolfssl"
-	$(MAKE) -C libevent/wolfssl
+	LDFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) -L$(SSLPATH)" \
+	$(MAKE) -C libevent
