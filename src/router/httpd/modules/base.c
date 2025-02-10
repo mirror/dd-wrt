0831b3424748fb34ecde8c60cdeed05ed0260b2e
BrainSlayer <s.gottschall@dd-wrt.com>
2025-02-10 12:02:18 +0000

    use 256 again
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@59610 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/modules/base.c
+++ src/router/httpd/modules/base.c
@@ -904,8 +904,8 @@ static int do_spectral_scan(unsigned char method, struct mime_handler *handler, 
 		writestr(dest, "trigger");
 	} else if (is_ath11k(ifname)) {
 		sprintf(dest, "%s/spectral_bins", path);
-		writestr(dest, "512");
-		writestr(dest, "1024");
+		writestr(dest, "256");
+//		writestr(dest, "1024");
 		//		writestr(dest, "1024"); // QCN90xx supports 1024. but i dont think we need that
 		sprintf(dest, "%s/spectral_scan_ctl", path);
 		if (pidof("disable_fft") < 0) {
