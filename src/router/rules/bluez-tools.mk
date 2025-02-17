a9a192fa78f8839efbae7f8f65a9697c60cbf44c
BrainSlayer <s.gottschall@dd-wrt.com>
2025-02-17 10:31:02 +0000

    fix flags
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@59721 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/bluez-tools.mk
+++ src/router/rules/bluez-tools.mk
@@ -7,6 +7,8 @@ bluez-tools-configure:
 	--localstatedir=/tmp/var \
 	GLIB_CFLAGS="-I$(TOP)/glib20/libglib/glib -I$(TOP)/glib20/libglib -I$(TOP)/glib20/libglib/build -I$(TOP)/glib20/libglib/build/glib" \
 	GLIB_LIBS="-L$(TOP)/glib20/libglib -L$(TOP)/glib20/libglib/glib -L$(TOP)/glib20/libglib/build/glib -L$(TOP)/glib20/libglib/build/gthread -L$(TOP)/glib20/libglib/build/gio -L$(TOP)/glib20/libglib/build/gobject" \
+	GIO_CFLAGS="-I$(TOP)/glib20/libglib -I$(TOP)/glib20/libglib/build -I$(TOP)/glib20/libglib/build/glib -I$(TOP)/glib20/libglib/build/gio -I$(TOP)/glib20/libglib/build/gmodule -I$(TOP)/glib20/libglib/gmodule" \
+	GIO_LIBS="-L$(TOP)/glib20/libglib/build/gio -lgio-2.0 -L$(TOP)/glib20/libglib/build/glib -lglib-2.0 -L$(TOP)/glib20/libglib/build/gobject -lgobject-2.0" \
 	CFLAGS="$(LTO) $(COPTS) $(MIPS16_OPT) -I$(TOP) -I$(TOP)/zlib  -I$(TOP)/kernel_headers/$(KERNELRELEASE)/include -UHAVE_SELINUX -DNEED_PRINTF -D_GNU_SOURCE -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_malloc=malloc" \
 	LDFLAGS="$(LDLTO) -L$(TOP)/dbus/dbus/.libs -L$(TOP)/readline/shlib -L$(TOP)/ncurses/lib -lncurses -L$(TOP)/libffi/$(ARCH)-$(SUBARCH)-linux-gnu/.libs -lffi  -L$(TOP)/zlib -lz -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	ac_cv_lib_rt_clock_gettime=yes
