d1096efeb599c5deae9607c8104786ae6ae131a2
BrainSlayer <s.gottschall@dd-wrt.com>
2025-03-01 08:40:35 +0000

    must be enabled for ss uility
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@59993 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kernel-features.mk
+++ src/router/kernel-features.mk
@@ -560,6 +560,14 @@ define kernelfeatures
 		sed -i 's/CONFIG_MSDOS_FS=m/\# CONFIG_MSDOS_FS is not set/g' $(LINUXDIR)/.config; \
 		sed -i 's/CONFIG_VFAT_FS=m/\# CONFIG_VFAT_FS is not set/g' $(LINUXDIR)/.config; \
 	fi	
+	if [ "$(CONFIG_IPTOOLS)" != "y" ]; then \
+		sed -i 's/\# CONFIG_NETLINK_DIAG is not set/CONFIG_NETLINK_DIAG=y/g' $(LINUXDIR)/.config; \
+		sed -i 's/\# CONFIG_PACKET_DIAG is not set/CONFIG_PACKET_DIAG=y/g' $(LINUXDIR)/.config; \
+		sed -i 's/\# CONFIG_UNIX_DIAG is not set/CONFIG_UNIX_DIAG=y/g' $(LINUXDIR)/.config; \
+		sed -i 's/\# CONFIG_INET_DIAG is not set/CONFIG_INET_DIAG=y/g' $(LINUXDIR)/.config; \
+		sed -i 's/\# CONFIG_MPTCP_DIAG is not set/CONFIG_MPTCP_DIAG=y/g' $(LINUXDIR)/.config; \
+		sed -i 's/\# CONFIG_SCTP_DIAG is not set/CONFIG_SCTP_DIAG=y/g' $(LINUXDIR)/.config; \
+	fi	
 	if [ "$(CONFIG_EXFAT)" != "y" ]; then \
 		sed -i 's/CONFIG_EXFAT_FS=m/\# CONFIG_EXFAT_FS is not set/g' $(LINUXDIR)/.config; \
 		echo "# CONFIG_EXFAT_FS is not set" >> $(LINUXDIR)/.config; \
