06bcf838ad2b53beff2aade02c0758f24f27750b
BrainSlayer <s.gottschall@dd-wrt.com>
2025-03-01 07:08:34 +0000

    for maybe as new webgui option at the networking page
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@59979 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/services/services/arpd.c
@@ -0,0 +1,78 @@
+/*
+ * arpd.c
+ *
+ * Copyright (C) 2009 - 2025 Sebastian Gottschall <s.gottschall@dd-wrt.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * $Id:
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
+#include <signal.h>
+#include <utils.h>
+#include <ddnvram.h>
+#include <shutils.h>
+#include <services.h>
+
+void start_arpd(void)
+{
+		char vifs[512];
+		char *ifnames[128] = {"arpd","-b",NULL,"-a","3", "-k"};
+		int cnt=0;
+		getIfLists(vifs, sizeof(vifs));
+		char var[32], *wordlist;
+		const char *next;
+		char *dbname;
+	if (!jffs_mounted() && (freediskSpace("/jffs") < 8 * 1024 * 1024))
+		ifnames[2] = "/tmp/arpd.db";
+	else
+		ifnames[2] = "/jffs/arpd.db";
+		cnt = 6;
+		int active = 0;
+		foreach(var, vifs, next)
+		{
+		    if (cnt == ARRAY_SIZE(ifnames))
+			    break;
+		    if (nvram_nmatch("1", "%s_arpd", var)) 
+			    ifnames[cnt++] = strdup(var);
+		    active = 1;
+		}
+	_log_evalpid(ifnames, NULL, 0, NULL);
+
+	
+	return;
+}
+
+void restart_arpd(void)
+{
+	stop_arpd();
+	start_arpd();
+}
+
+void stop_arpd(void)
+{
+	stop_process("arpd", "daemon");
+
+	return;
+}
