6533c43c93641f481ec4b143ef09ee23805b6269
BrainSlayer <s.gottschall@dd-wrt.com>
2025-03-03 08:11:22 +0000

    dont start if hostapd isnt running
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@60076 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/usteer.c
+++ src/router/services/services/usteer.c
@@ -39,6 +39,8 @@ void start_usteer(void)
 	const char *next;
 	int i;
 	char *ssid_list = NULL;
+	if (pidof("hostapd")<=0)
+		return;
 	for (i = 0; i < c; i++) {
 		sprintf(dev, "wlan%d", i);
 		char busname[64];
