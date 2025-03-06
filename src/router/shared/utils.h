83822964e5103c6571f96b6f1ec33034de060494
BrainSlayer <s.gottschall@dd-wrt.com>
2025-03-06 06:27:00 +0000

    fix typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@60111 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/utils.h
+++ src/router/shared/utils.h
@@ -1212,13 +1212,6 @@ int route_del(char *name, int metric, char *dst, char *gateway, char *genmask);
 #define DEFAULT_ETH_LIST "eth ixp vlan wlan ra rb apcli wds wl br oet lan wan 10g-copper 10g-sfp vxlan ofdm"
 #endif
 
-#if defined(HAVE_MVEBU) || defined(HAVE_IPQ6018)
-	getIfList(eths, size, "ixp vlan wlan ra rb apcli wds wl br oet lan wan 10g-copper 10g-sfp vxlan ofdm");
-#else
-	getIfList(eths, size, "ixp eth vlan wlan ra rb apcli wds wl br oet lan wan 10g-copper 10g-sfp vxlan ofdm");
-#endif
-
-
 extern void waitfordead(const char *procname, int maxtime);
 extern int pidof(const char *name);
 extern int killall(const char *name, int sig);
