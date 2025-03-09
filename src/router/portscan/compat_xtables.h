41de2754e0a15bbc9178f136e9f5a54a5b7ec5db
BrainSlayer <s.gottschall@dd-wrt.com>
2025-03-09 23:59:22 +0000

    tarpit port
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@60168 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/portscan/compat_xtables.h
+++ src/router/portscan/compat_xtables.h
@@ -6,10 +6,6 @@
 
 #define DEBUGP Use__pr_debug__instead
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 16, 0)
-#	warning Kernels below 4.16 not supported.
-#endif
-
 #if IS_ENABLED(CONFIG_NF_CONNTRACK)
 #	if !defined(CONFIG_NF_CONNTRACK_MARK)
 #		warning You have CONFIG_NF_CONNTRACK enabled, but CONFIG_NF_CONNTRACK_MARK is not (please enable).
@@ -24,9 +20,18 @@
     LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 78) && LINUX_VERSION_CODE < KERNEL_VERSION(5, 5, 0) || \
     LINUX_VERSION_CODE >= KERNEL_VERSION(4, 19, 158) && LINUX_VERSION_CODE < KERNEL_VERSION(4, 20, 0)
 #else
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 4, 0)
+#	define ip6_local_out(xnet, xsk, xskb) ip6_local_out(xskb)
+#	define ip6_route_me_harder(xnet, xsk, xskb) ip6_route_me_harder(xskb)
+#	define ip_local_out(xnet, xsk, xskb) ip_local_out(xskb)
+#	define ip_route_me_harder(xnet, xsk, xskb, xaddrtype) ip_route_me_harder((xskb), (xaddrtype))
+#else
 #	define ip_route_me_harder(xnet, xsk, xskb, xaddrtype) ip_route_me_harder((xnet), (xskb), (xaddrtype))
 #	define ip6_route_me_harder(xnet, xsk, xskb) ip6_route_me_harder((xnet), (xskb))
 #endif
+#endif
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 17, 0)
 #	define pde_data(inode) PDE_DATA(inode)
 #endif
@@ -44,9 +49,27 @@
 #	define proc_lseek llseek
 #	define proc_release release
 #endif
-
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 18, 0)
+static inline u32 prandom_u32_max(u32 ep_ro)
+{
+	return (u32)(((u64) prandom_u32() * ep_ro) >> 32);
+}
+#endif
 #if LINUX_VERSION_CODE < KERNEL_VERSION(6, 2, 0)
 #	define get_random_u32_below prandom_u32_max
 #endif
 
 extern void *HX_memmem(const void *, size_t, const void *, size_t);
+
+static inline struct net *par_net(const struct xt_action_param *par)
+{
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0)
+	return par->state->net;
+#else
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 4, 0)
+	return par->net;
+#else
+	return dev_net((par->in != NULL) ? par->in : par->out);
+#endif
+#endif
+}
