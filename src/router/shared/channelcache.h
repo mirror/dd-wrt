83b08754f247d00bb56602ea7bc6b817efd68d1c
BrainSlayer <s.gottschall@dd-wrt.com>
2025-03-10 08:22:59 +0000

    second try with new code. it was just a bool problem
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@60191 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/channelcache.h
+++ src/router/shared/channelcache.h
@@ -76,26 +76,26 @@ static void _invalidate_channelcache(void)
 	}
 }
 
-#define INITVALUECACHEi(prefix)                                  \
-	static int devs[8] = { -1, -1, -1, -1, -1, -1, -1, -1 }; \
-	int dn, ret = 0;                                         \
-	sscanf(prefix, "wlan%d", &dn);                   \
-	if (dn > -1 && (dn > 7 || devs[dn] == -1)) {
-#define INITVALUECACHE()                                         \
-	static int devs[8] = { -1, -1, -1, -1, -1, -1, -1, -1 }; \
-	int dn, ret = 0;                                         \
-	sscanf(prefix, "wlan%d", &dn);                   \
-	if (dn > -1 && (dn > 7 || devs[dn] == -1)) {
-#define EXITVALUECACHE()               \
-	}                              \
-	else                           \
-	{                              \
-		return (devs[dn] > 0); \
-	}                              \
-out_cache:;                            \
-	if (dn < 8 && dn > -1)         \
-		devs[dn] = ret;
+#define INITVALUECACHEi(prefix)         \
+	static unsigned int __set = 0; \
+	static unsigned int __val = 0; \
+	int dn, ret = 0;                \
+	dn = prefix[4] - '0';                \
+	if (dn >= 0 && dn <= 9 && !(__set & (1 << dn))) {
 
+#define INITVALUECACHE() INITVALUECACHEi(prefix)
+
+#define EXITVALUECACHE()                    \
+	}                                   \
+	else                                \
+	{                                   \
+		return (__val & (1 << dn)); \
+	}                                   \
+out_cache:;                                 \
+	if (dn <= 9 && dn >= 0) {            \
+		__set |= 1 << dn;           \
+		__val |= (!!ret) << dn;     \
+	}
 #define RETURNVALUE(val)        \
 	{                       \
 		ret = val;      \
