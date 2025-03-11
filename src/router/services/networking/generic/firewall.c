493338ae3f97318820c4986e72b3e41157b0d5f4
BrainSlayer <s.gottschall@dd-wrt.com>
2025-03-11 14:32:28 +0000

    keep it that way. its more effective
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@60236 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/generic/firewall.c
+++ src/router/services/networking/generic/firewall.c
@@ -2683,13 +2683,12 @@ static void filter_forward(char *wanface, char *lanface, char *lan_cclass, int d
 		if (nvram_matchi("block_portscan", 1)) {
 #ifdef HAVE_PORTSCAN
 			save2file_A_forward("-p tcp -m lscan --synscan --cnscan --mirai -j %s", log_drop);
-#else
+#endif
 			save2file_A_forward("-m recent --name portscan --rcheck --seconds 86400 -j %s", log_drop);
 			save2file_A_forward("-m recent --name portscan --remove");
 			save2file_A_forward("-p tcp -m tcp --dport 139 -m recent --name portscan --set -j %s", "portscan");
-#endif
 #ifdef HAVE_PORTSCAN
-			save2file_A_forward("-m psd --psd-weight-threshold 15 --psd-hi-ports-weight 3 -j %s", "portscan");
+			save2file_A_forward("-m psd --psd-weight-threshold 15 --psd-hi-ports-weight 3 -j %s", log_drop);
 #endif
 		}
 		save2file_A_forward("-j SECURITY");
@@ -3122,13 +3121,12 @@ static void filter_table(char *wanface, char *lanface, char *wanaddr, char *lan_
 			if (nvram_matchi("block_portscan", 1)) {
 #ifdef HAVE_PORTSCAN
 				save2file_A_input("-p tcp -m lscan --synscan --cnscan --mirai -j %s", log_drop);
-#else
+#endif
 				save2file_A_input("-m recent --name portscan --rcheck --seconds 86400 -j %s", log_drop);
 				save2file_A_input("-m recent --name portscan --remove");
 				save2file_A_input("-p tcp -m tcp --dport 139 -m recent --name portscan --set -j %s", "portscan");
-#endif
 #ifdef HAVE_PORTSCAN
-				save2file_A_input("-m psd --psd-weight-threshold 15 --psd-hi-ports-weight 3 -j %s", "portscan");
+				save2file_A_input("-m psd --psd-weight-threshold 15 --psd-hi-ports-weight 3 -j %s", log_drop);
 #endif
 			}
 			//save2file_A_input("-j SECURITY");
@@ -3368,7 +3366,7 @@ static void run_firewall6(char *vifs)
 #ifdef HAVE_PORTSCAN
 		eval("ip6tables", "-A", "INPUT", "-p", "tcp", "-m", "lscan", "--synscan", "--cnscan", "--mirai", "-j", log_drop);
 		eval("ip6tables", "-A", "FORWARD", "-p", "tcp", "-m", "lscan", "--synscan", "--cnscan", "--mirai", "-j", log_drop);
-#else
+#endif
 		eval("ip6tables", "-A", "INPUT", "-m", "recent", "--name", "portscan", "--rcheck", "--seconds", "86400", "-j",
 		     "tarpit");
 		eval("ip6tables", "-A", "FORWARD", "-m", "recent", "--name", "portscan", "--rcheck", "--seconds", "86400", "-j",
@@ -3380,12 +3378,11 @@ static void run_firewall6(char *vifs)
 		     "--set", "-j", "portscan");
 		eval("ip6tables", "-A", "FORWARD", "-p", "tcp", "-m", "tcp", "--dport", "139", "-m", "recent", "--name", "portscan",
 		     "--set", "-j", "portscan");
-#endif
 #ifdef HAVE_PORTSCAN
 		eval("ip6tables", "-A", "INPUT", "-m", "psd", "--psd-weight-threshold", "15", "--psd-hi-ports-weight", "3", "-j",
-		     "portscan");
+		     log_drop);
 		eval("ip6tables", "-A", "FORWARD", "-m", "psd", "--psd-weight-threshold", "15", "--psd-hi-ports-weight", "3", "-j",
-		     "portscan");
+		     log_drop);
 #endif
 	}
 	/* Filter all packets that have RH0 headers */
