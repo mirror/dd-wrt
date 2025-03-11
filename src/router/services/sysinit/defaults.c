20ab0ffd94df43131bc816796af2b5888fa7c197
BrainSlayer <s.gottschall@dd-wrt.com>
2025-03-11 15:02:14 +0000

    we have to refine the udpflood rules usually. it fucks up wireguard
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@60237 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/defaults.c
+++ src/router/services/sysinit/defaults.c
@@ -2033,9 +2033,9 @@ struct nvram_param srouter_defaults[] = {
 	{ "block_wan", "1" },	/* Block WAN Request [1|0] */
 	{ "block_portscan", "1" },	/* Detect and Block port scanning [1|0] */
 	{ "block_tarpit", "0" },	/* tarpit bad connections [1|0] */
-	{ "block_syncflood", "1" },	/* Block sycnflood [1|0] */
-	{ "block_udpflood", "1" },	/* Block udpflood [1|0] */
-	{ "block_pod", "1" },	/* Block Ping of Death [1|0] */
+	{ "block_syncflood", "0" },	/* Block sycnflood [1|0] */
+	{ "block_udpflood", "0" },	/* Block udpflood [1|0] */
+	{ "block_pod", "0" },	/* Block Ping of Death [1|0] */
 	{ "block_ident", "1" },	/* Block IDENT passthrough [1|0] */
 	{ "block_proxy", "0" },	/* Block Proxy [1|0] */
 	{ "block_java", "0" },	/* Block Java [1|0] */
