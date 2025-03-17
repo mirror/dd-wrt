cd6c383b4b44fd9529d976dfa358b31acd6120de
BrainSlayer <s.gottschall@dd-wrt.com>
2025-03-17 23:18:29 +0000

    try mimalloc
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@60325 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/musl.mk
+++ src/router/rules/musl.mk
@@ -12,3 +12,21 @@ musl:
 musl-install:
 	@true
 
+
+
+
+musl-mimalloc-configure:
+	-make -C musl-mimalloc clean
+	cd musl-mimalloc && ./configure --host=$(ARCH)-linux CFLAGS="$(COPTS) -DCRYPT_SIZE_HACK" --enable-optimize=size --disable-gcc-wrapper --with-malloc=external EXTRA_OBJ=$(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o
+	make -C musl-mimalloc EXTRA_OBJ=$(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o
+
+musl-mimalloc-clean:
+	make -C musl-mimalloc clean
+
+musl-mimalloc:
+	rm -f $(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o
+	make -C musl-mimalloc EXTRA_OBJ=$(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o $(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o
+
+musl-mimalloc-install:
+	@true
+
