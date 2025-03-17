d9fb873aacd8e0ca03f748c248893f0f96af5765
BrainSlayer <s.gottschall@dd-wrt.com>
2025-03-17 18:39:21 +0000

    len must be size_t
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@60320 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/shared/shutils.h
+++ src/router/shared/shutils.h
@@ -177,14 +177,14 @@ extern int strhas(char *list, char *key);
  */
 char *strcat_r(const char *s1, const char *s2, char *buf);
 char *strlcat_r(const char *s1, const char *s2, char *buf, size_t len);
-char *dd_strncat(char *dst, size_t len, const char *src);
+char *dd_strncat(char *dst, const char *src, size_t len);
 
 #define strcat_r(s1, s2, buf) (sizeof(buf) == sizeof(void *) ? strcat_r(s1, s2, buf) : strlcat_r(s1, s2, buf, sizeof(buf)))
-#define strcat(buf, s1) sizeof(buf) == sizeof(void *) ? strcat(buf, s1) : dd_strncat(buf, sizeof(buf), s1)
+#define strcat(buf, s1) sizeof(buf) == sizeof(void *) ? strcat(buf, s1) : dd_strncat(buf, s1, sizeof(buf))
 
 #ifndef FROM_NVRAM
 extern int dd_sprintf(char *str, const char *fmt, ...);
-extern int dd_snprintf(char *str, int len, const char *fmt, ...);
+extern int dd_snprintf(char *str, size_t len, const char *fmt, ...);
 
 extern void *dd_malloc(size_t len);
 
