a01b94ef264722cf927f6ff269e8eda900401c81
BrainSlayer <s.gottschall@dd-wrt.com>
2025-03-18 15:44:58 +0000

    compile both variants
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@60357 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/musl.mk
+++ src/router/rules/musl.mk
@@ -32,34 +32,21 @@ musl-configure:
 	-make -C musl clean
 	cd musl && ./configure --host=$(ARCH)-linux CFLAGS="$(COPTS) -DCRYPT_SIZE_HACK" --enable-optimize=size --disable-gcc-wrapper
 	make -C musl
-
-musl-clean:
-	make -C musl clean
-
-musl:
-	make -C musl
-
-musl-install:
-	@true
-
-
-
-
-musl-mimalloc-configure:
 	-make -C musl-mimalloc clean
 	cd musl-mimalloc && ./configure --host=$(ARCH)-linux CFLAGS="$(COPTS) -DCRYPT_SIZE_HACK" --enable-optimize=size --disable-gcc-wrapper --with-malloc=external EXTRA_OBJ=$(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o
 	rm -f $(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o
 	make -C musl-mimalloc EXTRA_OBJ=$(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o MIMALLOC_OPT=$(MIMALLOC_OPT) MIMALLOC_LD=$(MIMALLOC_LD) $(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o
 	make -C musl-mimalloc EXTRA_OBJ=$(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o MIMALLOC_OPT=$(MIMALLOC_OPT) MIMALLOC_LD=$(MIMALLOC_LD)
 
-musl-mimalloc-clean:
+musl-clean:
+	make -C musl clean
 	make -C musl-mimalloc clean
 
-musl-mimalloc:
+musl:
+	make -C musl
 	rm -f $(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o
 	make -C musl-mimalloc EXTRA_OBJ=$(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o MIMALLOC_OPT=$(MIMALLOC_OPT) MIMALLOC_LD=$(MIMALLOC_LD) $(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o
 	make -C musl-mimalloc EXTRA_OBJ=$(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o MIMALLOC_OPT=$(MIMALLOC_OPT) MIMALLOC_LD=$(MIMALLOC_LD)
 
-musl-mimalloc-install:
+musl-install:
 	@true
-
