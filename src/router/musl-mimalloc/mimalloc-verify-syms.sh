16044e5a93275c5d4002db36c90a979ad0509484
BrainSlayer <s.gottschall@dd-wrt.com>
2025-03-18 08:17:51 +0000

    fix new symbols
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@60348 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/musl-mimalloc/mimalloc-verify-syms.sh
+++ src/router/musl-mimalloc/mimalloc-verify-syms.sh
@@ -6,8 +6,11 @@ nm "$1" | grep '[0-9A-Za-z] [A-Z] ' | while read -r addr type name; do
         __libc_*|__malloc_*) ;;
         # compiler-generated
         .L*) ;;
+        __x86.*) ;;
+        #atomic
+	__atomic_load_8|__atomic_fetch_add_8|__atomic_compare_exchange_8|__atomic_store_8) ;;
         # directly provided api
-        aligned_alloc|malloc_usable_size|__atomic_load_8|__atomic_fetch_add_8|__atomic_compare_exchange_8|__atomic_store_8) ;;
+        aligned_alloc|malloc_usable_size) ;;
         # mimalloc heaps
         _mi_heap_empty|_mi_heap_main) ;;
         *)
