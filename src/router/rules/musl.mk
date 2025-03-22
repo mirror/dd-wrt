a1f15b5721903fd55180a314f11f2974181e5dfc
BrainSlayer <s.gottschall@dd-wrt.com>
2025-03-22 01:56:08 +0000

    use secure mode as secondary option, since its slower than standard
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@60436 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/musl.mk
+++ src/router/rules/musl.mk
@@ -34,19 +34,28 @@ musl-configure:
 	make -C musl
 	-make -C musl-mimalloc clean
 	cd musl-mimalloc && ./configure --host=$(ARCH)-linux CFLAGS="$(COPTS) -DCRYPT_SIZE_HACK" --enable-optimize=size --disable-gcc-wrapper --with-malloc=external EXTRA_OBJ=$(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o
+	-make -C musl-mimalloc-secure clean
+	cd musl-mimalloc-secure && ./configure --host=$(ARCH)-linux CFLAGS="$(COPTS) -DCRYPT_SIZE_HACK" --enable-optimize=size --disable-gcc-wrapper --with-malloc=external EXTRA_OBJ=$(TOP)/musl-mimalloc-secure/src/malloc/external/mimalloc.o
 	rm -f $(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o
+	rm -f $(TOP)/musl-mimalloc-secure/src/malloc/external/mimalloc.o
 	-make -C musl-mimalloc EXTRA_OBJ=$(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o MIMALLOC_OPT=$(MIMALLOC_OPT) MIMALLOC_LD=$(MIMALLOC_LD) $(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o
 	-make -C musl-mimalloc EXTRA_OBJ=$(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o MIMALLOC_OPT=$(MIMALLOC_OPT) MIMALLOC_LD=$(MIMALLOC_LD)
+	-make -C musl-mimalloc-secure EXTRA_OBJ=$(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o MIMALLOC_OPT=$(MIMALLOC_OPT) MIMALLOC_LD=$(MIMALLOC_LD) $(TOP)/musl-mimalloc-secure/src/malloc/external/mimalloc.o
+	-make -C musl-mimalloc-secure EXTRA_OBJ=$(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o MIMALLOC_OPT=$(MIMALLOC_OPT) MIMALLOC_LD=$(MIMALLOC_LD)
 
 musl-clean:
 	make -C musl clean
 	make -C musl-mimalloc clean
+	make -C musl-mimalloc-secure clean
 
 musl:
 	make -C musl
 	rm -f $(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o
+	rm -f $(TOP)/musl-mimalloc-secure/src/malloc/external/mimalloc-secure.o
 	-make -C musl-mimalloc EXTRA_OBJ=$(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o MIMALLOC_OPT=$(MIMALLOC_OPT) MIMALLOC_LD=$(MIMALLOC_LD) $(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o
 	-make -C musl-mimalloc EXTRA_OBJ=$(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o MIMALLOC_OPT=$(MIMALLOC_OPT) MIMALLOC_LD=$(MIMALLOC_LD)
+	-make -C musl-mimalloc-secure EXTRA_OBJ=$(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o MIMALLOC_OPT=$(MIMALLOC_OPT) MIMALLOC_LD=$(MIMALLOC_LD) $(TOP)/musl-mimalloc-secure/src/malloc/external/mimalloc.o
+	-make -C musl-mimalloc-secure EXTRA_OBJ=$(TOP)/musl-mimalloc/src/malloc/external/mimalloc.o MIMALLOC_OPT=$(MIMALLOC_OPT) MIMALLOC_LD=$(MIMALLOC_LD)
 
 musl-install:
 	@true
