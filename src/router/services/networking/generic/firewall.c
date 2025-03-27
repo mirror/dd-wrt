4bcca321671ff81be5bf705df950a6f27de4677e
BrainSlayer <s.gottschall@dd-wrt.com>
2025-03-27 20:59:55 +0000

    if modprobe is always included, we dont need this condition anymore
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@60510 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/generic/firewall.c
+++ src/router/services/networking/generic/firewall.c
@@ -3677,7 +3677,7 @@ void start_loadfwmodules(void)
 	modprobe("iptable_raw iptable_mangle nf_conntrack_h323 xt_NFLOG" //
 		 " xt_length xt_REDIRECT xt_CT xt_limit xt_TCPMSS" //
 		 " xt_connbytes xt_connlimit" //
-		 " xt_CLASSIFY xt_recent ipv6 xt_TARPIT xt_lscan xt_psd ipt_recent" //
+		 " xt_CLASSIFY xt_recent xt_TARPIT xt_lscan xt_psd ipt_recent" //
 		 " xt_conntrack xt_state" //
 		 " xt_string xt_LOG xt_iprange xt_tcpmss" //
 		 " xt_NETMAP compat_xtables" //
