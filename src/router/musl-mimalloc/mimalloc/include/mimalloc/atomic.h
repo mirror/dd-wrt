a4192aa1e7999bade598a6d55c49df61fabd19e8
BrainSlayer <s.gottschall@dd-wrt.com>
2025-04-04 08:27:10 +0000

    fix xscale
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@60589 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/musl-mimalloc/mimalloc/include/mimalloc/atomic.h
+++ src/router/musl-mimalloc/mimalloc/include/mimalloc/atomic.h
@@ -363,7 +363,7 @@ static inline void mi_atomic_yield(void) {
   _mm_pause();
 }
 #elif (defined(__GNUC__) || defined(__clang__)) && \
-      (defined(__x86_64__) || defined(__i386__) || defined(__arm__) || defined(__armel__) || defined(__ARMEL__) || \
+      (defined(__x86_64__) || defined(__i386__) || defined(__arm__) || defined(__armel__) || defined(__ARMEL__) || defined(__armbe__) || defined(__ARMBE__) || \
        defined(__aarch64__) || defined(__powerpc__) || defined(__ppc__) || defined(__PPC__)) || defined(__POWERPC__)
 #if defined(__x86_64__) || defined(__i386__)
 static inline void mi_atomic_yield(void) {
@@ -388,6 +388,10 @@ static inline void mi_atomic_yield(void) {
 }
 #endif
 #elif defined(__armel__) || defined(__ARMEL__)
+static inline void mi_atomic_yield(void) {
+  __asm__ volatile ("nop" ::: "memory");
+}
+#elif defined(__armbe__) || defined(__ARMBE__)
 static inline void mi_atomic_yield(void) {
   __asm__ volatile ("nop" ::: "memory");
 }
