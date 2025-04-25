f68ca6d8021476ed14f1e52c8a7232f3539bcd75
BrainSlayer <s.gottschall@dd-wrt.com>
2025-04-25 22:37:55 +0000

    fix configure
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@60775 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/bluez.mk
+++ src/router/rules/bluez.mk
@@ -27,7 +27,9 @@ bluez-configure: dbus
 	--enable-deprecated \
 	DBUS_CFLAGS="-I$(TOP)/dbus/staged/usr/include/dbus-1.0 -I$(TOP)/dbus/staged/usr/lib/dbus-1.0/include" \
 	GLIB_CFLAGS="-I$(TOP)/glib20/libglib/glib -I$(TOP)/glib20/libglib -I$(TOP)/glib20/libglib/build -I$(TOP)/glib20/libglib/build/glib" \
-	GLIB_LIBS="-L$(TOP)/glib20/libglib -L$(TOP)/glib20/libglib/glib -L$(TOP)/glib20/libglib/build/glib -L$(TOP)/glib20/libglib/build/gthread -L$(TOP)/glib20/libglib/build/gio -L$(TOP)/glib20/libglib/build/gobject" \
+	GLIB_LIBS="-L$(TOP)/glib20/libglib -L$(TOP)/glib20/libglib/glib -L$(TOP)/glib20/libglib/build/glib -L$(TOP)/glib20/libglib/build/gthread -L$(TOP)/glib20/libglib/build/gio -L$(TOP)/glib20/libglib/build/gobject -lglib-2.0" \
+	GTHREAD_CFLAGS="-I$(TOP)/glib20/libglib/glib" \
+	GTHREAD_LIBS="-L$(TOP)/glib20/libglib/build/gthread" \
 	CFLAGS="$(LTO) $(COPTS) $(MIPS16_OPT) -I$(TOP) -I$(TOP)/kernel_headers/$(KERNELRELEASE)/include -UHAVE_SELINUX -DNEED_PRINTF -D_GNU_SOURCE -ffunction-sections -fdata-sections -Wl,--gc-sections -Drpl_malloc=malloc" \
 	LDFLAGS="$(LDLTO) -L$(TOP)/dbus/dbus/.libs -L$(TOP)/readline/shlib -L$(TOP)/json-c/.libs -L$(TOP)/ncurses/lib -lncurses -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	ac_cv_lib_rt_clock_gettime=yes
