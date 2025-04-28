1f712066e159c946a6711d29e28ceb0a0a817405
BrainSlayer <s.gottschall@dd-wrt.com>
2025-04-28 07:44:02 +0000

    ipq806x devices make use of 2 cpu ports, so we should wan exlusive on eth0 and eth1 for all the lan ports. otherwise all gets just routed on eth0 which decreases performance. this requires some patches for busybox iproute (see recent commits)
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@60798 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/sysinit/sysinit-ipq806x.c
+++ src/router/services/sysinit/sysinit-ipq806x.c
@@ -914,6 +914,11 @@ void start_sysinit(void)
 		nvram_unset("sw_lan2");
 		nvram_unset("sw_lan3");
 		nvram_unset("sw_lan4");
+		eval("ip", "link", "set", "wan", "type", "dsa", "master", "eth0");
+		eval("ip", "link", "set", "lan1", "type", "dsa", "master", "eth1");
+		eval("ip", "link", "set", "lan2", "type", "dsa", "master", "eth1");
+		eval("ip", "link", "set", "lan3", "type", "dsa", "master", "eth1");
+		eval("ip", "link", "set", "lan4", "type", "dsa", "master", "eth1");
 		writeproc("/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor", "performance");
 		writeproc("/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq", "600000");
 		writeproc("/sys/devices/system/cpu/cpu1/cpufreq/scaling_governor", "performance");
@@ -932,6 +937,11 @@ void start_sysinit(void)
 		nvram_unset("sw_lan2");
 		nvram_unset("sw_lan3");
 		nvram_unset("sw_lan4");
+		eval("ip", "link", "set", "wan", "type", "dsa", "master", "eth0");
+		eval("ip", "link", "set", "lan1", "type", "dsa", "master", "eth1");
+		eval("ip", "link", "set", "lan2", "type", "dsa", "master", "eth1");
+		eval("ip", "link", "set", "lan3", "type", "dsa", "master", "eth1");
+		eval("ip", "link", "set", "lan4", "type", "dsa", "master", "eth1");
 		writeproc("/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor", "performance");
 		writeproc("/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq", "600000");
 		writeproc("/sys/devices/system/cpu/cpu1/cpufreq/scaling_governor", "performance");
@@ -942,6 +952,11 @@ void start_sysinit(void)
 		set_named_smp_affinity("eth1", 1, 1);
 		break;
 	case ROUTER_LINKSYS_EA8500:
+		eval("ip", "link", "set", "wan", "type", "dsa", "master", "eth0");
+		eval("ip", "link", "set", "lan1", "type", "dsa", "master", "eth1");
+		eval("ip", "link", "set", "lan2", "type", "dsa", "master", "eth1");
+		eval("ip", "link", "set", "lan3", "type", "dsa", "master", "eth1");
+		eval("ip", "link", "set", "lan4", "type", "dsa", "master", "eth1");
 	default:
 		nvram_unset("sw_cpuport");
 		nvram_unset("sw_wancpuport");
