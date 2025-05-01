84f4c151e0d90a4ec37ef30b27526fee664371ae
BrainSlayer <s.gottschall@dd-wrt.com>
2025-05-01 18:14:09 +0000

    save routines
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@60864 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/vlanfiltering.c
+++ src/router/httpd/visuals/vlanfiltering.c
@@ -19,7 +19,7 @@
  *
  * $Id:
  */
- 
+
 #ifdef HAVE_VLAN_FILTERING
 EJ_VISIBLE void ej_show_vlanfiltering(webs_t wp, int argc, char_t **argv)
 {
@@ -27,113 +27,115 @@ EJ_VISIBLE void ej_show_vlanfiltering(webs_t wp, int argc, char_t **argv)
 	int count = 0;
 	char word[256];
 	const char *next, *wordlist;
+	int has_vlan = br_has_vlan_filtering();
+	if (has_vlan) {
+		websWrite(wp, "<h2><script type=\"text/javascript\">Capture(networking.hfiltering)</script></h2>");
+		websWrite(wp,
+			  "<fieldset>\n<legend><script type=\"text/javascript\">Capture(networking.flegend)</script></legend>\n");
 
-	websWrite(wp, "<h2><script type=\"text/javascript\">Capture(networking.hfiltering)</script></h2>");
-	websWrite(wp, "<fieldset>\n<legend><script type=\"text/javascript\">Capture(networking.flegend)</script></legend>\n");
+		getIfList(buffer, sizeof(buffer), NULL);
+		int totalcount = 0;
+		int realcount = nvram_default_geti("vlan_filtercount", 0);
+		websWrite(wp, "<table cellspacing=\"4\" summary=\"filters\" id=\"filters_table\" class=\"table\"><thead><tr>\n");
+		show_caption_pp(wp, NULL, "networking.iface", "<th>", "</th>\n");
+		show_caption_pp(wp, NULL, "networking.tg_number", "<th>", "</th>\n");
+		show_caption_pp(wp, NULL, "networking.pvid", "<th>", "</th>\n");
+		show_caption_pp(wp, NULL, "networking.untagged", "<th>", "</th>\n");
+		show_caption_pp(wp, NULL, "share.actiontbl", "<th class=\"center\" width=\"10%%\">", "</th></thead><tbody>\n");
 
-	getIfList(buffer, sizeof(buffer), NULL);
-	int totalcount = 0;
-	int realcount = nvram_default_geti("vlan_filtercount", 0);
-	websWrite(wp, "<table cellspacing=\"4\" summary=\"filters\" id=\"filters_table\" class=\"table\"><thead><tr>\n");
-	show_caption_pp(wp, NULL, "networking.iface", "<th>", "</th>\n");
-	show_caption_pp(wp, NULL, "networking.tg_number", "<th>", "</th>\n");
-	show_caption_pp(wp, NULL, "networking.pvid", "<th>", "</th>\n");
-	show_caption_pp(wp, NULL, "networking.untagged", "<th>", "</th>\n");
-	show_caption_pp(wp, NULL, "share.actiontbl", "<th class=\"center\" width=\"10%%\">", "</th></thead><tbody>\n");
+		wordlist = nvram_safe_get("vlan_filters");
+		foreach(word, wordlist, next) {
+			GETENTRYBYIDX(ifname, word, 0);
+			GETENTRYBYIDX(vlan, word, 1);
+			GETENTRYBYIDX(pvid, word, 2);
+			GETENTRYBYIDX(untagged, word, 3);
+			if (!tag || !port)
+				break;
+			char vlan_name[32];
+			websWrite(wp, "<tr>\n");
+			websWrite(wp, "<td>");
+			sprintf(vlan_name, "filterifname%d", count);
+			showIfOptions(wp, vlan_name, buffer, tag);
+			websWrite(wp, "</td>\n");
+			//tag number
+			sprintf(vlan_name, "filtertag%d", count);
+			websWrite(wp, "<td>");
+			websWrite(wp, "<input class=\"num\" name=\"%s\" size=\"5\" value=\"%s\" />\n", vlan_name, vlan);
+			websWrite(wp, "</td>\n");
+			//pvid
+			sprintf(vlan_name, "filterpvid%d", count);
+			websWrite(wp, "<td>");
+			websWrite(wp, "<input type=\"checkbox\" name=\"%s\" value=\"1\" %s>\n", vlan_name,
+				  !strcmp(pvid, "1") ? "checked" : "");
+			websWrite(wp, "</td>\n");
+			//untagged
+			sprintf(vlan_name, "filteruntagged%d", count);
+			websWrite(wp, "<td>");
+			websWrite(wp, "<input type=\"checkbox\" name=\"%s\" value=\"1\" %s>\n", vlan_name,
+				  !strcmp(untagged, "1") ? "checked" : "");
+			websWrite(wp, "</td>\n");
+			websWrite(
+				wp,
+				"<script type=\"text/javascript\">\n//<![CDATA[\n document.write(\"<td class=\\\"center\\\" title=\\\"\" + sbutton.del + \"\\\"><input class=\\\"remove\\\" aria-label=\\\"\" + sbutton.del + \"\\\" type=\\\"button\\\" onclick=\\\"vlan_del_submit(this.form,%d)\\\" />\");\n//]]>\n</script>\n",
+				count);
+			websWrite(wp, "</td>\n");
+			websWrite(wp, "</tr>\n");
 
-	wordlist = nvram_safe_get("vlan_filters");
-	foreach(word, wordlist, next) {
-		GETENTRYBYIDX(ifname, word, 0);
-		GETENTRYBYIDX(vlan, word, 1);
-		GETENTRYBYIDX(pvid, word, 2);
-		GETENTRYBYIDX(untagged, word, 3);
-		if (!tag || !port)
-			break;
-		char vlan_name[32];
+			count++;
+		}
+		totalcount = count;
+		int i;
+
+		for (i = count; i < realcount; i++) {
+			char vlan_name[32];
+			websWrite(wp, "<tr>\n");
+			websWrite(wp, "<td>");
+
+			sprintf(vlan_name, "filterifname%d", i);
+			showIfOptions(wp, vlan_name, buffer, "");
+			websWrite(wp, "</td>\n");
+
+			//tag number
+			sprintf(vlan_name, "filtertag%d", count);
+			websWrite(wp, "<td>");
+			websWrite(wp, "<input class=\"num\" name=\"%s\" size=\"5\" value=\"0\" />\n", vlan_name);
+			websWrite(wp, "</td>\n");
+
+			//pvid
+			sprintf(vlan_name, "filterpvid%d", count);
+			websWrite(wp, "<td>");
+			websWrite(wp, "<input type=\"checkbox\" name=\"%s\" value=\"1\">\n", vlan_name);
+			websWrite(wp, "</td>\n");
+
+			//untagged
+			sprintf(vlan_name, "filteruntagged%d", count);
+			websWrite(wp, "<td>");
+			websWrite(wp, "<input type=\"checkbox\" name=\"%s\" value=\"1\">\n", vlan_name);
+			websWrite(wp, "</td>\n");
+
+			websWrite(
+				wp,
+				"<script type=\"text/javascript\">\n//<![CDATA[\n document.write(\"<td class=\\\"center\\\" title=\\\"\" + sbutton.del + \"\\\"><input class=\\\"remove\\\" aria-label=\\\"\" + sbutton.del + \"\\\" type=\\\"button\\\" onclick=\\\"filter_del_submit(this.form,%d)\\\" />\");\n//]]>\n</script>\n",
+				i);
+			websWrite(wp, "</td>\n");
+			websWrite(wp, "</tr>\n");
+			totalcount++;
+		}
+		char var[32];
+
+		sprintf(var, "%d", totalcount);
+		nvram_set("vlan_filtercount", var);
 		websWrite(wp, "<tr>\n");
-		websWrite(wp, "<td>");
-		sprintf(vlan_name, "filterifname%d", count);
-		showIfOptions(wp, vlan_name, buffer, tag);
-		websWrite(wp, "</td>\n");
-		//tag number
-		sprintf(vlan_name, "filtertag%d", count);
-		websWrite(wp, "<td>");
-		websWrite(wp, "<input class=\"num\" name=\"%s\" size=\"5\" value=\"%s\" />\n", vlan_name, vlan);
-		websWrite(wp, "</td>\n");
-		//pvid
-		sprintf(vlan_name, "filterpvid%d", count);
-		websWrite(wp, "<td>");
-		websWrite(wp, "<input type=\"checkbox\" name=\"%s\" value=\"1\" %s>\n", vlan_name, !strcmp(pvid,"1") ? "checked" : "");
-		websWrite(wp, "</td>\n");
-		//untagged
-		sprintf(vlan_name, "filteruntagged%d", count);
-		websWrite(wp, "<td>");
-		websWrite(wp, "<input type=\"checkbox\" name=\"%s\" value=\"1\" %s>\n", vlan_name, !strcmp(untagged,"1") ? "checked" : "");
-		websWrite(wp, "</td>\n");
+		websWrite(wp, "<td colspan=\"4\">&nbsp;</td>\n");
+		websWrite(wp, "<td class=\"center\">\n");
 		websWrite(
 			wp,
-			"<script type=\"text/javascript\">\n//<![CDATA[\n document.write(\"<td class=\\\"center\\\" title=\\\"\" + sbutton.del + \"\\\"><input class=\\\"remove\\\" aria-label=\\\"\" + sbutton.del + \"\\\" type=\\\"button\\\" onclick=\\\"vlan_del_submit(this.form,%d)\\\" />\");\n//]]>\n</script>\n",
-			count);
+			"<script type=\"text/javascript\">\n//<![CDATA[\n document.write(\"<input class=\\\"add\\\" type=\\\"button\\\" aria-label=\\\"\" + sbutton.add + \"\\\" onclick=\\\"filter_add_submit(this.form)\\\" />\");\n//]]>\n</script>\n");
 		websWrite(wp, "</td>\n");
 		websWrite(wp, "</tr>\n");
+		websWrite(wp, "</tbody></table>\n");
 
-		count++;
+		websWrite(wp, "</fieldset><br />\n");
 	}
-	totalcount = count;
-	int i;
-
-	for (i = count; i < realcount; i++) {
-		char vlan_name[32];
-		websWrite(wp, "<tr>\n");
-		websWrite(wp, "<td>");
-
-		sprintf(vlan_name, "filterifname%d", i);
-		showIfOptions(wp, vlan_name, buffer, "");
-		websWrite(wp, "</td>\n");
-
-
-		//tag number
-		sprintf(vlan_name, "filtertag%d", count);
-		websWrite(wp, "<td>");
-		websWrite(wp, "<input class=\"num\" name=\"%s\" size=\"5\" value=\"0\" />\n", vlan_name);
-		websWrite(wp, "</td>\n");
-
-		//pvid
-		sprintf(vlan_name, "filterpvid%d", count);
-		websWrite(wp, "<td>");
-		websWrite(wp, "<input type=\"checkbox\" name=\"%s\" value=\"1\">\n", vlan_name);
-		websWrite(wp, "</td>\n");
-
-		//untagged
-		sprintf(vlan_name, "filteruntagged%d", count);
-		websWrite(wp, "<td>");
-		websWrite(wp, "<input type=\"checkbox\" name=\"%s\" value=\"1\">\n", vlan_name);
-		websWrite(wp, "</td>\n");
-
-		websWrite(
-			wp,
-			"<script type=\"text/javascript\">\n//<![CDATA[\n document.write(\"<td class=\\\"center\\\" title=\\\"\" + sbutton.del + \"\\\"><input class=\\\"remove\\\" aria-label=\\\"\" + sbutton.del + \"\\\" type=\\\"button\\\" onclick=\\\"filter_del_submit(this.form,%d)\\\" />\");\n//]]>\n</script>\n",
-			i);
-		websWrite(wp, "</td>\n");
-		websWrite(wp, "</tr>\n");
-		totalcount++;
-	}
-	char var[32];
-
-	sprintf(var, "%d", totalcount);
-	nvram_set("vlan_filtercount", var);
-	websWrite(wp, "<tr>\n");
-	websWrite(wp, "<td colspan=\"4\">&nbsp;</td>\n");
-	websWrite(wp, "<td class=\"center\">\n");
-	websWrite(
-		wp,
-		"<script type=\"text/javascript\">\n//<![CDATA[\n document.write(\"<input class=\\\"add\\\" type=\\\"button\\\" aria-label=\\\"\" + sbutton.add + \"\\\" onclick=\\\"filter_add_submit(this.form)\\\" />\");\n//]]>\n</script>\n");
-	websWrite(wp, "</td>\n");
-	websWrite(wp, "</tr>\n");
-	websWrite(wp, "</tbody></table>\n");
-
-	websWrite(wp, "</fieldset><br />\n");
-
-
 }
 #endif
