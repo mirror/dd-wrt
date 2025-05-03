e2d8cfa236f828f1e67954e999e83256160c2704
BrainSlayer <s.gottschall@dd-wrt.com>
2025-05-03 13:18:24 +0000

    link local learn option
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@60932 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/validate/webs.c
+++ src/router/httpd/validate/webs.c
@@ -4919,6 +4919,14 @@ void save_networking(webs_t wp)
 			} else {
 				nvram_seti(n, 1);
 			}
+			sprintf(var, "bridgenoll%d", i);
+			vlan = websGetVar(wp, var, NULL);
+			sprintf(n, "%s_noll", ifname);
+			if (!vlan || !*vlan) {
+				nvram_seti(n, 0);
+			} else {
+				nvram_seti(n, 1);
+			}
 		}
 		sprintf(var, "bridgemcastbr%d", i);
 		mcast = websGetVar(wp, var, NULL);
