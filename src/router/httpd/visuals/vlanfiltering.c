a3a9f67a4aa9ecacf7fe1b3b25edf72af993cda3
BrainSlayer <s.gottschall@dd-wrt.com>
2025-05-04 13:46:27 +0000

    missing tr tag
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@60971 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/vlanfiltering.c
+++ src/router/httpd/visuals/vlanfiltering.c
@@ -41,7 +41,7 @@ EJ_VISIBLE void ej_show_vlanfiltering(webs_t wp, int argc, char_t **argv)
 		show_caption_pp(wp, NULL, "networking.tg_number", "<th>", "</th>\n");
 		show_caption_pp(wp, NULL, "networking.pvid", "<th>", "</th>\n");
 		show_caption_pp(wp, NULL, "networking.untagged", "<th>", "</th>\n");
-		show_caption_pp(wp, NULL, "share.actiontbl", "<th class=\"center\" width=\"10%%\">", "</th></thead><tbody>\n");
+		show_caption_pp(wp, NULL, "share.actiontbl", "<th class=\"center\" width=\"10%%\">", "</th></tr></thead><tbody>\n");
 
 		wordlist = nvram_safe_get("vlan_filters");
 		foreach(word, wordlist, next) {
