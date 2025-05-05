d82631f97442c2da9846fe53be44c96d87daf32e
BrainSlayer <s.gottschall@dd-wrt.com>
2025-05-05 20:46:09 +0000

    fix mips boot hang
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@60995 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.6/lib/string.c
+++ src/linux/universal/linux-6.6/lib/string.c
@@ -797,61 +797,24 @@ char *strnstr(const char *s1, const char *s2, size_t len)
 EXPORT_SYMBOL(strnstr);
 #endif
 
-#if defined(CONFIG_ARCH_HAS_FAST_MULTIPLIER) && BITS_PER_LONG == 64
-
-#define MEMCHR_MASK_GEN(mask) (mask *= 0x0101010101010101ULL)
-
-#elif defined(CONFIG_ARCH_HAS_FAST_MULTIPLIER)
-
-#define MEMCHR_MASK_GEN(mask)                                                  \
-	do {                                                                   \
-		mask *= 0x01010101;                                            \
-		mask |= mask << 32;                                            \
-	} while (0)
-
-#else
-
-#define MEMCHR_MASK_GEN(mask)                                                  \
-	do {                                                                   \
-		mask |= mask << 8;                                             \
-		mask |= mask << 16;                                            \
-		mask |= mask << 32;                                            \
-	} while (0)
-
-#endif
-
 #ifndef __HAVE_ARCH_MEMCHR
 /**
  * memchr - Find a character in an area of memory.
- * @p: The memory area
+ * @s: The memory area
  * @c: The byte to search for
- * @length: The size of the area.
+ * @n: The size of the area.
  *
  * returns the address of the first occurrence of @c, or %NULL
  * if @c is not found
  */
-void *memchr(const void *p, int c, size_t length)
+void *memchr(const void *s, int c, size_t n)
 {
-	u64 mask, val;
-	const void *end = p + length;
-
-	c &= 0xff;
-	if (p <= end - 8) {
-		mask = c;
-		MEMCHR_MASK_GEN(mask);
-
-		for (; p <= end - 8; p += 8) {
-			val = *(u64 *)p ^ mask;
-			if ((val + 0xfefefefefefefeffull) &
-			    (~val & 0x8080808080808080ull))
-				break;
+	const unsigned char *p = s;
+	while (n-- != 0) {
+        	if ((unsigned char)c == *p++) {
+			return (void *)(p - 1);
 		}
 	}
-
-	for (; p < end; p++)
-		if (*(unsigned char *)p == c)
-			return (void *)p;
-
 	return NULL;
 }
 EXPORT_SYMBOL(memchr);
@@ -887,7 +850,16 @@ void *memchr_inv(const void *start, int c, size_t bytes)
 		return check_bytes8(start, value, bytes);
 
 	value64 = value;
-	MEMCHR_MASK_GEN(value64);
+#if defined(CONFIG_ARCH_HAS_FAST_MULTIPLIER) && BITS_PER_LONG == 64
+	value64 *= 0x0101010101010101ULL;
+#elif defined(CONFIG_ARCH_HAS_FAST_MULTIPLIER)
+	value64 *= 0x01010101;
+	value64 |= value64 << 32;
+#else
+	value64 |= value64 << 8;
+	value64 |= value64 << 16;
+	value64 |= value64 << 32;
+#endif
 
 	prefix = (unsigned long)start % 8;
 	if (prefix) {
