c4cf04d8b3fd562674982212fbb85fb7b4f98706
BrainSlayer <s.gottschall@dd-wrt.com>
2025-05-09 01:38:53 +0000

    wolfssl support for smartdns
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@61130 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/smartdns.mk
+++ src/router/rules/smartdns.mk
@@ -9,10 +9,14 @@ ifeq ($(CONFIG_OPENSSL),y)
 	$(MAKE) -C smartdns/src CFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections -I$(SSLPATH)/include" LDFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) $(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections  -L$(SSLPATH)" clean
 	$(MAKE) -C smartdns/src CFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections -I$(SSLPATH)/include" LDFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) $(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections  -L$(SSLPATH)" SSL=yes
 else
+ifeq ($(CONFIG_WOLFSSL),y)
+	$(MAKE) -C smartdns/src CFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections -DOPENSSL_EXTRA -I$(WOLFSSL_SSLPATH) -I$(WOLFSSL_SSLPATH)/standard -I$(WOLFSSL_SSLPATH)/standard/wolfssl  -I$(WOLFSSL_SSLPATH)/wolfssl" LDFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) $(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections  -L$(WOLFSSL_SSLPATH)/standard/src/.libs -lwolfssl" clean
+	$(MAKE) -C smartdns/src CFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections -DOPENSSL_EXTRA -I$(WOLFSSL_SSLPATH) -I$(WOLFSSL_SSLPATH)/standard -I$(WOLFSSL_SSLPATH)/standard/wolfssl  -I$(WOLFSSL_SSLPATH)/wolfssl" LDFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) $(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections  -L$(WOLFSSL_SSLPATH)/standard/src/.libs -lwolfssl" WOLFSSL=yes
+else
 	$(MAKE) -C smartdns/src CFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections -I$(SSLPATH)/include" LDFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) $(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections  -L$(SSLPATH)" clean
 	$(MAKE) -C smartdns/src CFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) $(LTO) -ffunction-sections -fdata-sections -Wl,--gc-sections -I$(SSLPATH)/include" LDFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) $(LDLTO) -ffunction-sections -fdata-sections -Wl,--gc-sections  -L$(SSLPATH)"
 endif
-
+endif
 smartdns-install:
 	install -D smartdns/src/smartdns $(INSTALLDIR)/smartdns/usr/sbin/smartdns
 
