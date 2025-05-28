00f4791a526b9130d8df89ff847932a79b16535f
BrainSlayer <s.gottschall@dd-wrt.com>
2025-05-28 07:41:39 +0000

    fix zabbix for qca devices
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@61473 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/zabbix.c
+++ src/router/services/services/zabbix.c
@@ -43,7 +43,56 @@ void start_zabbix(void)
 	if (pidof("zabbix_agentd") > 0) {
 		//syslog(LOG_INFO, "dlna : minidlna already running\n");
 	} else {
-		dd_logstart("zabbix", eval("sh", "/etc/config/zabbix.startup"));
+		sysprintf("grep -q zabbix /etc/passwd || echo \"zabbix:*:65533:65533:zabbix:/var:/bin/false\" >> /etc/passwd");
+		char *HOST;
+		if (!*nvram_safe_get("wan_hostname"))
+			HOST = nvram_safe_get("DD_BOARD");
+		else
+			HOST = nvram_safe_get("wan_hostname");
+		char *SRVIP = nvram_safe_get("zabbix_serverip");
+		char *IP = nvram_safe_get("lan_ipaddr");
+		FILE *fp = fopen("/tmp/zabbix.conf", "wb");
+		fprintf(fp, "LogType=file\n");
+
+		fprintf(fp, "LogType=file\n");
+		fprintf(fp, "LogFile=/var/log/zabbix.log\n");
+		fprintf(fp, "LogFileSize=1\n");
+		fprintf(fp, "Server=%s\n", SRVIP);
+		fprintf(fp, "Hostname=%s\n", HOST);
+		fprintf(fp, "ListenIP=%s\n", IP);
+		fprintf(fp, "StartAgents=5\n");
+		fprintf(fp, "AllowRoot=1\n");
+#ifndef HAVE_MADWIFI
+		fprintf(fp, "UserParameter=temperature.wl0, /usr/sbin/temps eth1 2> /dev/null\n");
+		fprintf(fp, "UserParameter=temperature.wl1, /usr/sbin/temps eth2 2> /dev/null\n");
+		fprintf(fp, "UserParameter=temperature.wl2, /usr/sbin/temps eth3 2> /dev/null\n");
+		fprintf(fp, "UserParameter=clients.wl0, /usr/sbin/wclients eth1\n");
+		fprintf(fp, "UserParameter=clients.wl1, /usr/sbin/wclients eth2\n");
+		fprintf(fp, "UserParameter=clients.wl2, /usr/sbin/wclients eth3\n");
+#else
+		fprintf(fp, "UserParameter=temperature.wlan0, /usr/sbin/temps wlan0 2> /dev/null\n");
+		fprintf(fp, "UserParameter=temperature.wlan1, /usr/sbin/temps wlan1 2> /dev/null\n");
+		fprintf(fp, "UserParameter=temperature.wlan2, /usr/sbin/temps wlan2 2> /dev/null\n");
+		fprintf(fp, "UserParameter=clients.wlan0, /usr/sbin/wclients wlan0\n");
+		fprintf(fp, "UserParameter=clients.wlan1, /usr/sbin/wclients wlan1\n");
+		fprintf(fp, "UserParameter=clients.wlan2, /usr/sbin/wclients wlan2\n");
+#endif
+		fprintf(fp, "UserParameter=clients.wired, /usr/sbin/clients\n");
+		fprintf(fp, "UserParameter=system.topcpu[*], /usr/sbin/topcpu $1 $2\n");
+		fprintf(fp, "UserParameter=listenport[*], netstat -ln 2> /dev/null  | grep -c ':'\n");
+		fprintf(fp,
+			"UserParameter=net.iptables.cksum, iptables-save | grep -v '^[#:]' | md5sum | tr -cd 0-9 | cut -b1-10\n");
+		fprintf(fp, "UserParameter=net.ipv4.cksum,ifconfig | grep -B1 ' inet ' | md5sum | tr -cd 0-9 | cut -b1-10\n");
+		fprintf(fp,
+			"UserParameter=net.ipv4,ifconfig | grep -B1 ' inet ' | grep -o -e 'addr:[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*' -e '^[a-z0-9:]*'\n");
+		fprintf(fp,
+			"UserParameter=nvram.cksum, /usr/sbin/nvram show 2>/dev/null | grep -vE '^(forward_|traff|dnsmasq_lease_|http_client_)' | sort | md5sum | tr -cd '0-9' | cut -b1-10\n");
+		fprintf(fp, "UserParameter=nvram.free, /usr/sbin/nvram show 2>&1 1>/dev/null | grep -o '(.*' | tr -cd '0-9'\n");
+		if (*nvram_safe_get("zabbix_usrpara"))
+			fprintf(fp, "\n%s\n", nvram_safe_get("zabbix_usrpara"));
+		fclose(fp);
+
+		dd_logstart("zabbix", eval("zabbix_agentd", "-c", "/tmp/zabbix.conf"));
 	}
 
 	return;
