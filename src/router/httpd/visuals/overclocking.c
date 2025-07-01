f5af7458022d1c3d13e5ec556557b48548a1d20a
BrainSlayer <s.gottschall@dd-wrt.com>
2025-07-01 11:32:19 +0000

    it seems that 1512 is valid here too
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@61875 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/overclocking.c
+++ src/router/httpd/visuals/overclocking.c
@@ -80,7 +80,7 @@ static unsigned int ns_type7_clocks[4] = { 600, 800, 1000, 0 };
 #endif
 #endif
 
-static unsigned int ipq6018_clocks_op1[] = { 864000, 1056000, 1320000, 1440000, 1608000, 1800000, 0 };
+//static unsigned int ipq6018_clocks_op1[] = { 864000, 1056000, 1320000, 1440000, 1608000, 1800000, 0 };
 static unsigned int ipq6018_clocks_op2[] = { 864000, 1200000, 1056000, 1320000, 1440000, 1512000, 1608000, 1800000, 0 };
 static unsigned int ipq807x_clocks[] = { 1017600, 1382400, 1651200, 1843200, 1920000, 2208000, 0 };
 static unsigned int ipq5018_clocks[] = { 800000, 1008000, 0 };
@@ -127,7 +127,7 @@ EJ_VISIBLE void ej_show_clocks(webs_t wp, int argc, char_t **argv)
 		break;
 	case ROUTER_GLINET_AX1800:
 		defclock = "1608000";
-		c = ipq6018_clocks_op1;
+		c = ipq6018_clocks_op2;
 		break;
 	}
 	if (!*oclk) {
