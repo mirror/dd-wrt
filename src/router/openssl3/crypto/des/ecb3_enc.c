e908a710fcf921a5fbdb51602977eec573734ea6
BrainSlayer <s.gottschall@dd-wrt.com>
2025-07-07 06:28:34 +0000

    octeon specific hacks
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@61908 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/openssl3/crypto/des/ecb3_enc.c
+++ src/router/openssl3/crypto/des/ecb3_enc.c
@@ -14,9 +14,9 @@ void DES_ecb3_encrypt(const_DES_cblock *input, DES_cblock *output,
                       DES_key_schedule *ks3, int enc)
 {
 #ifdef OCTEON_OPENSSL
-  uint64_t *rdkey1 = &ks1->ks->cblock[0];
-  uint64_t *rdkey2 = &ks2->ks->cblock[0];
-  uint64_t *rdkey3 = &ks3->ks->cblock[0];
+  uint64_t *rdkey1 = (uint64_t *)&ks1->ks->cblock[0];
+  uint64_t *rdkey2 = (uint64_t *)&ks2->ks->cblock[0];
+  uint64_t *rdkey3 = (uint64_t *)&ks3->ks->cblock[0];
 
   CVMX_MT_3DES_KEY (*rdkey1, 0);
   CVMX_MT_3DES_KEY (*rdkey2, 1);
