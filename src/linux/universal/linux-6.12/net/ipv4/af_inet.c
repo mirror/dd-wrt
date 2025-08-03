613baba1336e7237698df99501c4951aac52ded0
BrainSlayer <s.gottschall@dd-wrt.com>
2025-08-03 05:55:46 +0000

    improve pppoe performance
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@62023 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.12/net/ipv4/af_inet.c
+++ src/linux/universal/linux-6.12/net/ipv4/af_inet.c
@@ -1546,6 +1546,7 @@ out:
 
 	return pp;
 }
+EXPORT_INDIRECT_CALLABLE(inet_gro_receive);
 
 static struct sk_buff *ipip_gro_receive(struct list_head *head,
 					struct sk_buff *skb)
@@ -1631,6 +1632,7 @@ int inet_gro_complete(struct sk_buff *skb, int nhoff)
 out:
 	return err;
 }
+EXPORT_INDIRECT_CALLABLE(inet_gro_complete);
 
 static int ipip_gro_complete(struct sk_buff *skb, int nhoff)
 {
