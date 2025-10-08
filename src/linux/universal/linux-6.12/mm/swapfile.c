6b054e6663f695814f2bae18cd1891850a6f8c32
BrainSlayer <s.gottschall@dd-wrt.com>
2025-10-08 10:36:51 +0000

    kernel update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@62266 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.12/mm/swapfile.c
+++ src/linux/universal/linux-6.12/mm/swapfile.c
@@ -2337,6 +2337,8 @@ static int unuse_mm(struct mm_struct *mm, unsigned int type)
 	VMA_ITERATOR(vmi, mm, 0);
 
 	mmap_read_lock(mm);
+	if (check_stable_address_space(mm))
+		goto unlock;
 	for_each_vma(vmi, vma) {
 		if (vma->anon_vma && !is_vm_hugetlb_page(vma)) {
 			ret = unuse_vma(vma, type);
@@ -2346,6 +2348,7 @@ static int unuse_mm(struct mm_struct *mm, unsigned int type)
 
 		cond_resched();
 	}
+unlock:
 	mmap_read_unlock(mm);
 	return ret;
 }
