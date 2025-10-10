8bcda9a566a619fe9e2a8652e11b6678eb2f2240
BrainSlayer <s.gottschall@dd-wrt.com>
2025-10-10 08:10:36 +0000

    not sure
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@62294 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/libutils/utils.c
+++ src/router/libutils/libutils/utils.c
@@ -2676,9 +2676,14 @@ float get_temperature(float celsius)
 	return nvram_match("fahrenheit", "1") ? celsius_to_fahrenheit(celsius) : celsius;
 }
 
-float get_lengths(float meter)
+float get_length(float meter)
 {
 	return nvram_match("feet", "1") ? meter_to_feet(meter) : meter;
+}
+
+float get_length_unit(float meter)
+{
+	return nvram_match("fahrenheit", "1") ? "ft" : "m";
 }
 
 char *get_temperature_unit(void)
