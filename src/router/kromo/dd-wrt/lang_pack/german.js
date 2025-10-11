b50fdeed383148a1b156be515ee8c350bc7d9b8b
BrainSlayer <s.gottschall@dd-wrt.com>
2025-10-11 04:37:02 +0000

    for ack timing / distance
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@62303 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/kromo/dd-wrt/lang_pack/german.js
+++ src/router/kromo/dd-wrt/lang_pack/german.js
@@ -207,6 +207,7 @@ share.styl_dark="Dunkles Farbschema";
 share.err="Fehler";
 share.errs="Fehler";
 share.meters="Meter";
+share.feet="Fuß";
 share.vht80plus80="80+80 MHz";
 share.vht160="160 MHz";
 share.vht80="80 MHz";
