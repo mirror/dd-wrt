f0394a13b4bc44d01bc6efc1c33cd1d07757dc76
BrainSlayer <s.gottschall@dd-wrt.com>
2025-10-13 08:40:34 +0000

    only free if available
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@62337 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.12/net/ipv4/etherip.c
+++ src/linux/universal/linux-6.12/net/ipv4/etherip.c
@@ -66,7 +66,8 @@ struct etherip_tunnel {
 
 static void etherip_dev_free(struct net_device *dev)
 {
-	free_percpu(dev->tstats);
+	if (dev->tstats)
+		free_percpu(dev->tstats);
 	free_netdev(dev);
 }
 
