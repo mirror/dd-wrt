20253b77cff9f9320fe9f5179f6175e6870aac76
BrainSlayer <s.gottschall@dd-wrt.com>
2025-10-30 06:56:54 +0000

    need to recalculate crc
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@62466 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/tools/realtek/crc.c
@@ -0,0 +1,19 @@
+
+#include "crc.h"
+
+static uint32 crc32(void *vdata, int size, uint32 crc)
+{
+	unsigned char *data = (unsigned char *)vdata;
+	unsigned char *end = data + size;
+	int i;
+
+	while (data < end) {
+		crc ^= *data++;
+
+		for (i = 0; i < 8; i++) {
+			uint t = ~((crc & 1) - 1);
+			crc = (crc >> 1) ^ (0xEDB88320 & t);
+		}
+	}
+	return crc;
+}
