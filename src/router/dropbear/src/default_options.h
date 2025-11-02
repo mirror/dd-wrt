dbd0250d1e3c9075d89facddd46cf4d5dcb3b67b
BrainSlayer <s.gottschall@dd-wrt.com>
2025-11-02 17:51:57 +0000

    include post quantum crypto for bigger builds
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@62516 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/dropbear/src/default_options.h
+++ src/router/dropbear/src/default_options.h
@@ -201,8 +201,13 @@ IMPORTANT: Some options will require "make clean" after changes */
 #define DROPBEAR_DH_GROUP14_SHA256 1
 #define DROPBEAR_DH_GROUP16 0
 #define DROPBEAR_CURVE25519 1
+#if defined(PQC)
 #define DROPBEAR_SNTRUP761 1
+#define DROPBEAR_MLKEM768 1
+#else
+#define DROPBEAR_SNTRUP761 0
 #define DROPBEAR_MLKEM768 0
+#endif
 #define DROPBEAR_ECDH 0
 #define DROPBEAR_DH_GROUP1 0
 
