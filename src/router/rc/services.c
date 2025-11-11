877d5b096f963441609a38044302b2debaab1e90
BrainSlayer <s.gottschall@dd-wrt.com>
2025-11-11 09:06:02 +0000

    according to tests we need at leaste 16 threads to saturate fiber connections with 1 gbit. this test is based on ipq6018 which is a 4 core arm64 cpu. on smaller devices i dont think it has any benefit here
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@62579 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/services.c
+++ src/router/rc/services.c
@@ -1072,7 +1072,7 @@ static void handle_pppoe(void)
 
 static void handle_speedtest(void)
 {
-	int cpucount = 4;
+	int cpucount = getlogicalcores() * 4;
 	char nr[32];
 	sprintf(nr, "%d", cpucount);
 	char *args[] = { "speedtest_cli", "1", nr, "1", nr, NULL };
