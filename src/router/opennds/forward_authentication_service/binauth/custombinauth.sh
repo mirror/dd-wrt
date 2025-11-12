7f5b8a48ef8e0e1afecb87e7ae3193de46f6d7e7
BrainSlayer <s.gottschall@dd-wrt.com>
2025-11-12 17:07:51 +0000

    update: add new files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@62617 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/opennds/forward_authentication_service/binauth/custombinauth.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#Copyright (C) The openNDS Contributors 2004-2022
+#Copyright (C) BlueWave Projects and Services 2015-2025
+#This software is released under the GNU GPL license.
+
+# This is a stub for a custom binauth script
+# It is included by the default binauth_log.sh script when it runs
+#
+# This included script can override:
+# exitlevel, session length, upload rate, download rate, upload quota and download quota.
+
+# Add custom code after this line:
+
