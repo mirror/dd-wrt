4ff649ba4b7e67362d17d4885d13da9142fb2006
BrainSlayer <s.gottschall@dd-wrt.com>
2025-11-14 08:10:45 +0000

    testcode for nftables
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@62695 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/proxywatchdog/proxywatchdog.sh
+++ src/router/proxywatchdog/proxywatchdog.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+IPTABLES=$(nvram get IPTABLES)
 
 SQUID_WATCHDOG_INTERVAL_SEC=$(nvram get squid_watchdog_interval_sec)
 ROUTER_IP=$(nvram get lan_ipaddr)
@@ -13,13 +14,13 @@ do
     if ping -c 1 $ip > /dev/null
     then
       if [ -z "$(nvram get transparent_proxy)" ]; then
-        /usr/sbin/iptables -t nat -A PREROUTING -i br0 -s $ROUTER_IP/$ROUTER_NETMASK \
+        $IPTABLES -t nat -A PREROUTING -i br0 -s $ROUTER_IP/$ROUTER_NETMASK \
           -d $ROUTER_IP/$ROUTER_NETMASK -p tcp --dport 80 -j ACCEPT
-        /usr/sbin/iptables -t nat -A PREROUTING -i br0 ! -s $PROXY_SERVER -p tcp --dport 80 \
+        $IPTABLES -t nat -A PREROUTING -i br0 ! -s $PROXY_SERVER -p tcp --dport 80 \
           -j DNAT --to $PROXY_SERVER:$PROXY_PORT
-        /usr/sbin/iptables -t nat -A POSTROUTING -o br0 -s $ROUTER_IP/$ROUTER_NETMASK -p tcp -d \
+        $IPTABLES -t nat -A POSTROUTING -o br0 -s $ROUTER_IP/$ROUTER_NETMASK -p tcp -d \
           $PROXY_SERVER -j SNAT --to $ROUTER_IP
-        /usr/sbin/iptables -t filter -I FORWARD -s $ROUTER_IP/$ROUTER_NETMASK -d $PROXY_SERVER -i br0 \
+        $IPTABLES -t filter -I FORWARD -s $ROUTER_IP/$ROUTER_NETMASK -d $PROXY_SERVER -i br0 \
           -o br0 -p tcp --dport $PROXY_PORT -j ACCEPT
         /usr/sbin/nvram set transparent_proxy="1"
       else
@@ -27,13 +28,13 @@ do
       fi
     else
       if [ "$(/usr/sbin/nvram get transparent_proxy)" = "1" ]; then
-        /usr/sbin/iptables -t nat -D PREROUTING -i br0 -s $ROUTER_IP/$ROUTER_NETMASK \
+        $IPTABLES -t nat -D PREROUTING -i br0 -s $ROUTER_IP/$ROUTER_NETMASK \
           -d $ROUTER_IP/$ROUTER_NETMASK -p tcp --dport 80 -j ACCEPT
-        /usr/sbin/iptables -t nat -D PREROUTING -i br0 ! -s $PROXY_SERVER -p tcp --dport 80 \
+        $IPTABLES -t nat -D PREROUTING -i br0 ! -s $PROXY_SERVER -p tcp --dport 80 \
           -j DNAT --to $PROXY_SERVER:$PROXY_PORT
-        /usr/sbin/iptables -t nat -D POSTROUTING -o br0 -s $ROUTER_IP/$ROUTER_NETMASK -p tcp -d \
+        $IPTABLES -t nat -D POSTROUTING -o br0 -s $ROUTER_IP/$ROUTER_NETMASK -p tcp -d \
           $PROXY_SERVER -j SNAT --to $ROUTER_IP
-        /usr/sbin/iptables -t filter -D FORWARD -s $ROUTER_IP/$ROUTER_NETMASK -d $PROXY_SERVER -i br0 \
+        $IPTABLES -t filter -D FORWARD -s $ROUTER_IP/$ROUTER_NETMASK -d $PROXY_SERVER -i br0 \
           -o br0 -p tcp --dport $PROXY_PORT -j ACCEPT
         /usr/sbin/nvram unset transparent_proxy
       else
