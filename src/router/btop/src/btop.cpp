2acc455e6958b9f4f5e429ee8049e675f258a39d
BrainSlayer <s.gottschall@dd-wrt.com>
2025-11-22 04:44:48 +0000

    remove nonsense check
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@62788 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/btop/src/btop.cpp
+++ src/router/btop/src/btop.cpp
@@ -935,15 +935,19 @@ int main(const int argc, const char** argv) {
 			}
 		}
 	#else
+#if 0
 		if (found.empty() and cli.force_utf) {
 			Logger::warning("No UTF-8 locale detected! Forcing start with --force-utf argument.");
 		} else if (found.empty()) {
 			Global::exit_error_msg = "No UTF-8 locale detected!\nUse --force-utf argument to force start if you're sure your terminal can handle it.";
 			clean_quit(1);
 		}
+#endif
 	#endif
+#if 0
 		else if (not set_failure)
 			Logger::debug("Setting LC_ALL=" + found);
+#endif
 	}
 
 	//? Initialize terminal and set options
