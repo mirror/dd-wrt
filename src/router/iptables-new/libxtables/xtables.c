380d6f46309121746ad2abb62c394ea7d6267c27
BrainSlayer <s.gottschall@dd-wrt.com>
2025-12-05 22:07:22 +0000

    this message is more than just bogus
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@62963 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/iptables-new/libxtables/xtables.c
+++ src/router/iptables-new/libxtables/xtables.c
@@ -814,10 +814,10 @@ xtables_find_match(const char *name, enum xtables_tryload tryload,
 		dptr = &((*dptr)->next);
 	}
 
-	if (seen && !found)
-		fprintf(stderr,
-			"Warning: Extension %s is not supported, missing kernel module?\n",
-			name);
+//	if (seen && !found)
+//		fprintf(stderr,
+//			"Warning: Extension %s is not supported, missing kernel module?\n",
+//			name);
 
 	for (ptr = xtables_matches; ptr; ptr = ptr->next) {
 		if (extension_cmp(name, ptr->name, ptr->family)) {
@@ -944,10 +944,10 @@ xtables_find_target(const char *name, enum xtables_tryload tryload)
 		dptr = &((*dptr)->next);
 	}
 
-	if (seen && !found)
-		fprintf(stderr,
-			"Warning: Extension %s is not supported, missing kernel module?\n",
-			name);
+//	if (seen && !found)
+//		fprintf(stderr,
+//			"Warning: Extension %s is not supported, missing kernel module?\n",
+//			name);
 
 	for (ptr = xtables_targets; ptr; ptr = ptr->next) {
 		if (extension_cmp(name, ptr->name, ptr->family)) {
