b1b57da451a5ad2762f0398ecf22f00453c65ca1
BrainSlayer <s.gottschall@dd-wrt.com>
2025-12-10 17:05:27 +0000

    let compiler decide
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@63033 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.12/arch/arm/include/asm/jump_label.h
+++ src/linux/universal/linux-6.12/arch/arm/include/asm/jump_label.h
@@ -9,7 +9,7 @@
 
 #define JUMP_LABEL_NOP_SIZE 4
 
-static __always_inline bool arch_static_branch(struct static_key *key, bool branch)
+static inline __attribute__((__always_inline__)) bool arch_static_branch(struct static_key *key, bool branch)
 {
 	asm goto("1:\n\t"
 		 WASM(nop) "\n\t"
@@ -23,7 +23,7 @@ l_yes:
 	return true;
 }
 
-static __always_inline bool arch_static_branch_jump(struct static_key *key, bool branch)
+static inline __attribute__((__always_inline__)) bool arch_static_branch_jump(struct static_key *key, bool branch)
 {
 	asm goto("1:\n\t"
 		 WASM(b) " %l[l_yes]\n\t"
