7bd68395dd9bb3b0066829e82fc98de46c8dd0ae
BrainSlayer <s.gottschall@dd-wrt.com>
2025-12-15 18:44:44 +0000

    update kernel
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@63083 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.12/arch/loongarch/kernel/machine_kexec.c
+++ src/linux/universal/linux-6.12/arch/loongarch/kernel/machine_kexec.c
@@ -249,6 +249,7 @@ void machine_crash_shutdown(struct pt_regs *regs)
 #ifdef CONFIG_SMP
 	crash_smp_send_stop();
 #endif
+	machine_kexec_mask_interrupts();
 	cpumask_set_cpu(crashing_cpu, &cpus_in_crash);
 
 	pr_info("Starting crashdump kernel...\n");
@@ -286,6 +287,7 @@ void machine_kexec(struct kimage *image)
 
 	/* We do not want to be bothered. */
 	local_irq_disable();
+	machine_kexec_mask_interrupts();
 
 	pr_notice("EFI boot flag 0x%lx\n", efi_boot);
 	pr_notice("Command line at 0x%lx\n", cmdline_ptr);
