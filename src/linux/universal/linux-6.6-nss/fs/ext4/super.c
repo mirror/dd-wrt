74e5a7cec6f9dbb7cd9540625ea3c659695d504d
BrainSlayer <s.gottschall@dd-wrt.com>
2026-01-12 21:23:06 +0000

    update kernel
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@63341 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.6-nss/fs/ext4/super.c
+++ src/linux/universal/linux-6.6-nss/fs/ext4/super.c
@@ -3632,14 +3632,6 @@ int ext4_feature_set_ok(struct super_block *sb, int readonly)
 	}
 #endif
 
-	if (EXT4_SB(sb)->s_es->s_def_hash_version == DX_HASH_SIPHASH &&
-	    !ext4_has_feature_casefold(sb)) {
-		ext4_msg(sb, KERN_ERR,
-			 "Filesystem without casefold feature cannot be "
-			 "mounted with siphash");
-		return 0;
-	}
-
 	if (readonly)
 		return 1;
 
