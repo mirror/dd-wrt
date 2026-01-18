68523025636ecc6dfc8e5daace80e185863c1da3
BrainSlayer <s.gottschall@dd-wrt.com>
2026-01-18 22:19:33 +0000

    fixes: add new files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@63405 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/transmission/libtransmission/announcer-http.cc
+++ src/router/transmission/libtransmission/announcer-http.cc
@@ -158,22 +158,17 @@ void onAnnounceDone(tr_web::FetchResponse const& web_response)
         {
             data->on_response(response);
         }
-        else if (got_all_responses)
-        {
-            // All requests have been answered, but none were successful.
-            // Choose the one that went further to report.
-            if (data->previous_response)
-            {
-                data->on_response(response.did_connect || response.did_timeout ? response : *data->previous_response);
-            }
-        }
         else
         {
-            // There is still one request pending that might succeed, so store
-            // the response for later. There is only room for 1 previous response,
-            // because there can be at most 2 requests.
-            TR_ASSERT(!data->previous_response);
-            data->previous_response = std::move(response);
+            if (!data->failed_response || tr_announce_response::compare_failed(*data->failed_response, response) < 0)
+            {
+                data->failed_response = std::move(response);
+            }
+
+            if (got_all_responses)
+            {
+                data->on_response(*data->failed_response);
+            }
         }
     }
 
