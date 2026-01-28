d9bd3d9afadeeb9d827cf67487914f786fc74e2e
BrainSlayer <s.gottschall@dd-wrt.com>
2026-01-28 19:02:54 +0000

    fix warnings
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@63536 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/writetool.c
+++ src/router/rc/writetool.c
@@ -19,7 +19,6 @@
  *
  * $Id:
  */
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
@@ -98,17 +97,17 @@ struct pte {
 };
 
 typedef struct {
-	uint64_t magic;
+	unsigned long long magic;
 	uint32_t revision;
 	uint32_t hdr_size;
 	uint32_t hdr_crc32;
 	uint32_t reserved;
-	uint64_t current_lba;
-	uint64_t backup_lba;
-	uint64_t first_usable_lba;
-	uint64_t last_usable_lba;
+	unsigned long long current_lba;
+	unsigned long long backup_lba;
+	unsigned long long first_usable_lba;
+	unsigned long long last_usable_lba;
 	uint8_t disk_guid[GUID_LEN];
-	uint64_t first_part_lba;
+	unsigned long long first_part_lba;
 	uint32_t n_parts;
 	uint32_t part_entry_len;
 	uint32_t part_array_crc32;
@@ -117,9 +116,9 @@ typedef struct {
 typedef struct {
 	uint8_t type_guid[GUID_LEN];
 	uint8_t part_guid[GUID_LEN];
-	uint64_t lba_start;
-	uint64_t lba_end;
-	uint64_t flags;
+	unsigned long long lba_start;
+	unsigned long long lba_end;
+	unsigned long long flags;
 	uint16_t name36[36];
 } gpt_partition;
 
@@ -151,17 +150,17 @@ static void copy(FILE *out, size_t inoff, size_t outoff, int len)
 	if (inoff >= outoff) {
 		for (i = 0; i < (len / 65536); i++) {
 			fprintf(stderr, "copy from %zu to %zu\r", inoff + (i * 65536), outoff + (i * 65536));
-			fseek(out, inoff + (i * 65536), SEEK_SET);
+			fseeko(out, inoff + (i * 65536), SEEK_SET);
 			fread(mem, 65536, 1, out);
-			fseek(out, outoff + (i * 65536), SEEK_SET);
+			fseeko(out, outoff + (i * 65536), SEEK_SET);
 			fwrite(mem, 65536, 1, out);
 			fflush(out);
 			fsync(fileno(out));
 		}
 		if (len % 65536) {
-			fseek(out, inoff + (i * 65536), SEEK_SET);
+			fseeko(out, inoff + (i * 65536), SEEK_SET);
 			fread(mem, len % 65536, 1, out);
-			fseek(out, outoff + (i * 65536), SEEK_SET);
+			fseeko(out, outoff + (i * 65536), SEEK_SET);
 			fwrite(mem, len % 65536, 1, out);
 			fflush(out);
 			fsync(fileno(out));
@@ -170,9 +169,9 @@ static void copy(FILE *out, size_t inoff, size_t outoff, int len)
 		long o = len - 65536;
 		for (i = 0; i < (len / 65536); i++) {
 			fprintf(stderr, "copy from %lu to %lu\r", inoff + o, outoff + o);
-			fseek(out, inoff + o, SEEK_SET);
+			fseeko(out, inoff + o, SEEK_SET);
 			fread(mem, 65536, 1, out);
-			fseek(out, outoff + o, SEEK_SET);
+			fseeko(out, outoff + o, SEEK_SET);
 			fwrite(mem, 65536, 1, out);
 			fflush(out);
 			fsync(fileno(out));
@@ -181,9 +180,9 @@ static void copy(FILE *out, size_t inoff, size_t outoff, int len)
 				break;
 		}
 		if (len % 65536) {
-			fseek(out, inoff, SEEK_SET);
+			fseeko(out, inoff, SEEK_SET);
 			fread(mem, len % 65536, 1, out);
-			fseek(out, outoff, SEEK_SET);
+			fseeko(out, outoff, SEEK_SET);
 			fwrite(mem, len % 65536, 1, out);
 			fflush(out);
 			fsync(fileno(out));
@@ -197,8 +196,8 @@ int main_mbr(int argc, char *argv[])
 {
 	FILE *in = fopen(argv[1], "r+b");
 	FILE *out = fopen(argv[2], "r+b");
-	fseek(out, MBR_PARTITION_ENTRY_OFFSET, SEEK_SET);
-	fseek(in, MBR_PARTITION_ENTRY_OFFSET, SEEK_SET);
+	fseeko(out, MBR_PARTITION_ENTRY_OFFSET, SEEK_SET);
+	fseeko(in, MBR_PARTITION_ENTRY_OFFSET, SEEK_SET);
 	// read old mbr
 	struct pte presentlayout[4];
 	struct pte newlayout[4];
@@ -219,15 +218,15 @@ int main_mbr(int argc, char *argv[])
 			newlayout[i].type);
 	}
 	struct pte *nvram = &presentlayout[2];
-	fseek(out, le32_to_cpu(nvram->start) * 512, SEEK_SET);
+	fseeko(out, le32_to_cpu(nvram->start) * 512, SEEK_SET);
 	uint32_t nvlen = nvram->length * 512;
 	if (nvlen) {
 		if (le32_to_cpu(nvram->start) == le32_to_cpu(newlayout[2].start)) {
 			if (le32_to_cpu(nvram->length) > le32_to_cpu(newlayout[2].length)) {
 				// if old nvram partition size is bigger than the new partition to be written, we keep the old partition entry as is
 				memcpy(&newlayout[2], &nvram, sizeof(struct pte));
-				fseek(out, MBR_PARTITION_ENTRY_OFFSET, SEEK_SET);
-				fseek(in, MBR_PARTITION_ENTRY_OFFSET, SEEK_SET);
+				fseeko(out, MBR_PARTITION_ENTRY_OFFSET, SEEK_SET);
+				fseeko(in, MBR_PARTITION_ENTRY_OFFSET, SEEK_SET);
 				fwrite(&newlayout, sizeof(struct pte), MBR_ENTRY_MAX, in);
 				fwrite(&newlayout, sizeof(struct pte), MBR_ENTRY_MAX, out);
 				fflush(in);
@@ -236,18 +235,18 @@ int main_mbr(int argc, char *argv[])
 				fsync(fileno(out));
 			}
 		} else {
-			fprintf(stderr, "read nvram from old offset %lld with len %lld and write to offset %lld\n",
-				le32_to_cpu(nvram->start) * 512, nvlen, le32_to_cpu(newlayout[2].start) * 512);
+			fprintf(stderr, "read nvram from old offset %lld with len %d and write to offset %lld\n",
+				(unsigned long long)(le32_to_cpu(nvram->start) * 512), nvlen, (unsigned long long)le32_to_cpu(newlayout[2].start) * 512);
 			copy(out, le32_to_cpu(nvram->start) * 512, le32_to_cpu(newlayout[2].start) * 512, nvlen);
 		}
 	}
-	fseek(in, 0, SEEK_END);
-	uint32_t len = ftell(in);
-	fseek(in, 0, SEEK_SET);
-	fseek(out, 0, SEEK_SET);
+	fseeko(in, 0, SEEK_END);
+	off_t len = ftello(in);
+	fseeko(in, 0, SEEK_SET);
+	fseeko(out, 0, SEEK_SET);
 	char *buf = malloc(65536);
 	int count = len / 65536;
-	fprintf(stderr, "write image len = %d, count %d\n", len, count);
+	fprintf(stderr, "write image len = %ld, count %d\n", len, count);
 	for (i = 0; i < count; i++) {
 		fprintf(stderr, "write %d\r", i * 65536);
 		fread(buf, 65536, 1, in);
@@ -277,12 +276,12 @@ int main_gpt(int argc, char *argv[])
 	unsigned char *buf2;
 	FILE *in = fopen(argv[1], "r+b");
 	FILE *out = fopen(argv[2], "r+b");
-	fseek(in, 512, SEEK_SET);
-	fseek(out, 512, SEEK_SET);
+	fseeko(in, 512, SEEK_SET);
+	fseeko(out, 512, SEEK_SET);
 	fread(&header, sizeof(header), 1, in);
 	fread(&header2, sizeof(header), 1, out);
-	fseek(in, 512 * le64_to_cpu(header.first_part_lba), SEEK_SET);
-	fseek(out, 512 * le64_to_cpu(header2.first_part_lba), SEEK_SET);
+	fseeko(in, 512 * le64_to_cpu(header.first_part_lba), SEEK_SET);
+	fseeko(out, 512 * le64_to_cpu(header2.first_part_lba), SEEK_SET);
 	uint32_t n_parts;
 	uint32_t part_entry_len;
 
@@ -297,25 +296,25 @@ int main_gpt(int argc, char *argv[])
 	fprintf(stderr, "old layout\n");
 	for (i = 0; i < 4; i++) {
 		part = (gpt_partition *)&buf2[i * le32_to_cpu(header2.part_entry_len)];
-		fprintf(stderr, "p[%d]: start %lld end %lld flags %llX name %s\n", i, le64_to_cpu(part->lba_start),
-			le64_to_cpu(part->lba_end), le64_to_cpu(part->flags), part->name36);
+		fprintf(stderr, "p[%d]: start %llu end %llu flags %llX name %s\n", i, le64_to_cpu(part->lba_start),
+			le64_to_cpu(part->lba_end), le64_to_cpu(part->flags), (char*)part->name36);
 	}
 	fprintf(stderr, "new layout\n");
 	for (i = 0; i < 4; i++) {
 		part = (gpt_partition *)&buf[i * le32_to_cpu(header.part_entry_len)];
 		fprintf(stderr, "p[%d]: start %lld end %lld flags %llX name %s\n", i, le64_to_cpu(part->lba_start),
-			le64_to_cpu(part->lba_end), le64_to_cpu(part->flags), part->name36);
+			le64_to_cpu(part->lba_end), le64_to_cpu(part->flags), (char*)part->name36);
 	}
 	gpt_partition *nvram = (gpt_partition *)&buf2[2 * le32_to_cpu(header2.part_entry_len)];
 	gpt_partition *newnvram = (gpt_partition *)&buf[2 * le32_to_cpu(header.part_entry_len)];
-	fseek(out, le64_to_cpu(nvram->lba_start) * 512, SEEK_SET);
-	int64_t nvlen = (le64_to_cpu(nvram->lba_end) - le64_to_cpu(nvram->lba_start)) * 512;
+	fseeko(out, le64_to_cpu(nvram->lba_start) * 512, SEEK_SET);
+	unsigned long long nvlen = (le64_to_cpu(nvram->lba_end) - le64_to_cpu(nvram->lba_start)) * 512;
 	if (nvlen > 0) {
 		if (nvram->lba_start == newnvram->lba_start) {
 			if (le64_to_cpu(nvram->lba_end) > le64_to_cpu(newnvram->lba_end)) {
 				// if old nvram partition size is bigger than the new partition to be written, we keep the old partition entry as is
 				memcpy(newnvram, nvram, le32_to_cpu(header.part_entry_len));
-				fseek(out, le32_to_cpu(header.part_entry_len) * 2, SEEK_SET);
+				fseeko(out, le32_to_cpu(header.part_entry_len) * 2, SEEK_SET);
 				fwrite(newnvram, le32_to_cpu(header.part_entry_len), 1, out);
 				fflush(out);
 				fsync(fileno(out));
@@ -326,13 +325,13 @@ int main_gpt(int argc, char *argv[])
 			copy(out, le64_to_cpu(nvram->lba_start) * 512, le64_to_cpu(newnvram->lba_start) * 512, nvlen);
 		}
 	}
-	fseek(in, 0, SEEK_END);
-	uint32_t len = ftell(in);
-	fseek(in, 0, SEEK_SET);
-	fseek(out, 0, SEEK_SET);
+	fseeko(in, 0, SEEK_END);
+	off_t len = ftello(in);
+	fseeko(in, 0, SEEK_SET);
+	fseeko(out, 0, SEEK_SET);
 	char *sbuf = malloc(65536);
 	int count = len / 65536;
-	fprintf(stderr, "write image len = %d, count %d\n", len, count);
+	fprintf(stderr, "write image len = %ld, count %d\n", len, count);
 	for (i = 0; i < count; i++) {
 		fprintf(stderr, "write %d\r", i * 65536);
 		fread(sbuf, 65536, 1, in);
@@ -353,7 +352,7 @@ int main(int argc, char *argv[])
 {
 	gpt_header header;
 	FILE *in = fopen(argv[1], "r+b");
-	fseek(in, 512, SEEK_SET);
+	fseeko(in, 512, SEEK_SET);
 	fread(&header, sizeof(header), 1, in);
 	fclose(in);
 	if (header.magic == le64_to_cpu(GPT_MAGIC))
