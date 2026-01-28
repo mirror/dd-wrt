0befbbfe00874a776d5d694b5ee24ca5185260dc
BrainSlayer <s.gottschall@dd-wrt.com>
2026-01-28 21:45:17 +0000

    fix path
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@63553 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/libgd.mk
+++ src/router/rules/libgd.mk
@@ -1,7 +1,7 @@
 libgd: libpng minidlna zlib
 	CC="ccache $(ARCH)-linux-uclibc-gcc" \
-	CFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB)   -I$(TOP)/minidlna/libjpeg-turbo-2.1.4 -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	CPPFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) -I$(TOP)/minidlna/libjpeg-turbo-2.1.4 -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	CFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB)   -I$(TOP)/minidlna/libjpeg-turbo-3.1.3 -I$(TOP)/minidlna/libjpeg-turbo-3.1.3/src -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	CPPFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) -I$(TOP)/minidlna/libjpeg-turbo-3.1.3 -I$(TOP)/minidlna/libjpeg-turbo-3.1.3/src -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	LDFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB)  -lm -L$(TOP)/zlib -L$(TOP)/libpng/.libs -lpng16 -L$(TOP)/minidlna/lib -ljpeg -fPIC -v -Wl,--verbose" \
 	$(MAKE) -C libgd
 
@@ -23,7 +23,7 @@ libgd-configure: minidlna-configure minidlna libpng-configure libpng zlib-config
 	--with-zlib \
 	enable_werror=no \
 	CC="ccache $(ARCH)-linux-uclibc-gcc" \
-	CFLAGS="-fPIC -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) $(THUMB) -I$(TOP)/minidlna/libjpeg-turbo-2.1.4 -I$(TOP)/zlib" \
+	CFLAGS="-fPIC -DNEED_PRINTF $(COPTS) $(MIPS16_OPT) $(THUMB) -I$(TOP)/minidlna/libjpeg-turbo-3.1.3 -I$(TOP)/minidlna/libjpeg-turbo-3.1.3/src -I$(TOP)/zlib" \
 	LDFLAGS="-L$(TOP)/minidlna/lib -L$(TOP)/zlib -L$(TOP)/libpng/.libs -lpng16" \
 	LIBZ_CFLAGS="-I$(TOP)/zlib" \
 	LIBZ_LIBS="-L$(TOP)/zlib -lz" \
@@ -31,8 +31,8 @@ libgd-configure: minidlna-configure minidlna libpng-configure libpng zlib-config
 	LIBPNG_LIBS="-L$(TOP)/libpng/.libs -lpng16"
 
 	CC="ccache $(ARCH)-linux-uclibc-gcc" \
-	CFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB)   -I$(TOP)/minidlna/libjpeg-turbo-2.1.4 -ffunction-sections -fdata-sections -Wl,--gc-sections" \
-	CPPFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) -I$(TOP)/minidlna/libjpeg-turbo-2.1.4 -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	CFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB)   -I$(TOP)/minidlna/libjpeg-turbo-3.1.3 -I$(TOP)/minidlna/libjpeg-turbo-3.1.3/src -ffunction-sections -fdata-sections -Wl,--gc-sections" \
+	CPPFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) -I$(TOP)/minidlna/libjpeg-turbo-3.1.3 -I$(TOP)/minidlna/libjpeg-turbo-3.1.3/src -ffunction-sections -fdata-sections -Wl,--gc-sections" \
 	LDFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB)  -lm -L$(TOP)/zlib -L$(TOP)/libpng/.libs -lpng16 -L$(TOP)/minidlna/lib -ljpeg -fPIC -v -Wl,--verbose" \
 	LIBZ_CFLAGS="-I$(TOP)/zlib" \
 	LIBZ_LIBS="-L$(TOP)/zlib -lz" \
