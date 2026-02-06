790f50cc24b0b096a93be9a884f832b33c506def
BrainSlayer <s.gottschall@dd-wrt.com>
2026-02-06 03:31:41 +0000

    fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@63684 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.12/fs/ntfsplus/runlist.c
+++ src/linux/universal/linux-6.12/fs/ntfsplus/runlist.c
@@ -742,8 +742,7 @@ struct runlist_element *ntfs_mapping_pairs_decompress(const struct ntfs_volume *
 
 #ifdef DEBUG
 	/* Make sure attr exists and is non-resident. */
-	if (!attr || !attr->non_resident ||
-	    le64_to_cpu(attr->data.non_resident.lowest_vcn) < 0) {
+	if (!attr || !attr->non_resident) {
 		ntfs_error(vol->sb, "Invalid arguments.");
 		return ERR_PTR(-EINVAL);
 	}
@@ -866,8 +865,10 @@ struct runlist_element *ntfs_mapping_pairs_decompress(const struct ntfs_volume *
 			}
 
 			/* chkdsk accepts zero-sized runs only for holes */
-			if ((lcn != -1) && !rl[rlpos].length) {
-				ntfs_error(vol->sb, "Invalid zero-sized data run.\n");
+			if ((lcn != LCN_HOLE) && !rl[rlpos].length) {
+				ntfs_error(vol->sb,
+					   "Invalid zero-sized data run(lcn : %lld).\n",
+					   lcn);
 				goto err_out;
 			}
 
