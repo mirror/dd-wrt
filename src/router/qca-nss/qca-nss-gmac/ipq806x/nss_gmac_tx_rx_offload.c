73ce5558ae271fd01ac1aa614d117c6fe8033279
BrainSlayer <s.gottschall@dd-wrt.com>
2026-02-22 18:39:11 +0000

    disable napi threading. may lead to corruption. under investigation
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@63762 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/qca-nss/qca-nss-gmac/ipq806x/nss_gmac_tx_rx_offload.c
+++ src/router/qca-nss/qca-nss-gmac/ipq806x/nss_gmac_tx_rx_offload.c
@@ -945,7 +945,7 @@ int nss_gmac_open(struct net_device *netdev)
 		netdev_info(netdev, "%s: offload is not enabled, bring up gmac with slowpath\n",
 								__func__);
 
-		netif_threaded_napi_add_weight(netdev, &gmacdev->napi, nss_gmac_poll,
+		netif_napi_add_weight(netdev, &gmacdev->napi, nss_gmac_poll,
 							NSS_GMAC_NAPI_BUDGET);
 
 		/* Initial the RX/TX ring */
