184011c055f9eca1690d6540586f3de5e28d6050
BrainSlayer <s.gottschall@dd-wrt.com>
2026-03-02 07:57:03 +0000

    fixes
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@63891 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/httpd/visuals/dd-wrt.c
+++ src/router/httpd/visuals/dd-wrt.c
@@ -1271,7 +1271,7 @@ static void show_channel(webs_t wp, char *dev, char *prefix, int type)
 						websWrite(
 							wp,
 							"document.write(\"<option value=\\\"%s\\\" %s>%s - %d.%d \"+wl_basic.khz+\"%s</option>\");\n",
-							fr, !strcmp(wlc, fr) ? "selected=\\\"selected\\\"" : "", ieee80211_mhz2ieee(chan[i].mapped_freq),
+							fr, !strcmp(wlc, fr) ? "selected=\\\"selected\\\"" : "", ieee80211_mhz2ieee(prefix, chan[i].mapped_freq),
 							chan[i].mapped_freq / 1000, (chan[i].mapped_freq % 1000) / 100, eirp);
 
 					} else if (is_mac80211(prefix) && !is_ath5k(prefix)) {
