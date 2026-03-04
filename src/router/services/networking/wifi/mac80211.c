5dfa3405d8a470dae6fabe474dcf888169a09081
BrainSlayer <s.gottschall@dd-wrt.com>
2026-03-04 02:29:57 +0000

    define global opclass
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@63919 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/wifi/mac80211.c
+++ src/router/services/networking/wifi/mac80211.c
@@ -1510,8 +1510,7 @@ void setupHostAP_generic_ath9k(const char *prefix, FILE *fp, int isrepeater, int
 		fprintf(fp, "channel=acs_survey\n");
 		char *country = getRegionCode(nvram_default_get("wlan0_regdomain", "UNITED_STATES"));
 		fprintf(fp, "country_code=%s\n", country);
-		fprintf(fp, "op_class=67\n");
-
+		fprintf(fp, "op_class=%d\n", morse_opclass(nvram_safe_geti("%s_channel", prefix)));
 		char shortgi[32];
 		sprintf(shortgi, "%s_shortgi", prefix);
 		if (nvram_default_matchi(shortgi, 1, 1))
