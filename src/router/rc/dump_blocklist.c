960fb07c1528a4745c0913a918921e733af1c166
BrainSlayer <s.gottschall@dd-wrt.com>
2026-03-29 07:18:44 +0000

    small tool to show content of our blocklist
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@64155 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/rc/dump_blocklist.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <utils.h>
+#include <time.h>
+#include <arpa/inet.h>
+
+struct blocklist {
+	char ip[INET6_ADDRSTRLEN];
+	time_t end;
+	int count;
+	int attempts;
+	int blocked;
+	int ver; // future
+	struct blocklist *next;
+} __attribute__((packed));
+
+static struct blocklist blocklist_root;
+
+static void init_blocklist(void)
+{
+	struct blocklist *entry = blocklist_root.next;
+	struct blocklist *last = &blocklist_root;
+	if (entry) {
+		while (entry) {
+			last = entry;
+			entry = entry->next;
+			free(last);
+		}
+		blocklist_root.next = NULL;
+		entry = blocklist_root.next;
+		last = &blocklist_root;
+	}
+
+	FILE *fp = NULL;
+	if (jffs_mounted()) {
+		fp = fopen("/jffs/blocklist", "rb");
+	}
+	if (!fp)
+		fp = fopen("/tmp/blocklist", "rb");
+	if (fp) {
+		while (!feof(fp)) {
+			last->next = malloc(sizeof(*entry));
+			int elems = fread(last->next, sizeof(struct blocklist) - sizeof(void *), 1, fp);
+			if (elems < 1) {
+				free(last->next);
+				last->next = NULL;
+				break;
+			}
+			last = last->next;
+		}
+		fclose(fp);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	init_blocklist();
+
+	struct blocklist *entry = blocklist_root.next;
+	FILE *fp = NULL;
+	while (entry) {
+		fprintf(stdout, "blocklist entry [%s]\t\tAttempts %d\tState(%d) %s\tBlocked Until %s\n", entry->ip, entry->attempts,
+			entry->blocked, entry->blocked == 1 ? "Blocked" : "Open", ctime(&entry->end));
+		entry = entry->next;
+	}
+	fclose(fp);
+}
