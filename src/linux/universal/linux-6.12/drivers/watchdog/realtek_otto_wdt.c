21add0610926fcd667bc18c9d50394b22e2405f7
BrainSlayer <s.gottschall@dd-wrt.com>
2026-04-01 01:39:13 +0000

    initial watchdog trigger
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@64221 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.12/drivers/watchdog/realtek_otto_wdt.c
+++ src/linux/universal/linux-6.12/drivers/watchdog/realtek_otto_wdt.c
@@ -345,7 +345,8 @@ static int otto_wdt_probe(struct platform_device *pdev)
 	ret = otto_wdt_determine_timeouts(&ctrl->wdev, ctrl->wdev.timeout, 1);
 	if (ret)
 		return dev_err_probe(dev, ret, "Failed to set timeout\n");
-
+	otto_wdt_start(&ctrl->wdev);
+	otto_wdt_ping(&ctrl->wdev);
 	return devm_watchdog_register_device(dev, &ctrl->wdev);
 }
 
