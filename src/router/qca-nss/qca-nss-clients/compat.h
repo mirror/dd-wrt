4d73230cecf8f1512a720c5b2a1c5fc577183f83
BrainSlayer <s.gottschall@dd-wrt.com>
2026-04-12 05:00:29 +0000

    add missing file
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@64410 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/qca-nss/qca-nss-clients/compat.h
@@ -0,0 +1,15 @@
+// compat.h
+#ifndef _COMPAT_H
+#define _COMPAT_H
+
+#include <linux/version.h>
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 10, 0)
+#include <linux/vmalloc.h>
+#define compat_const const
+#define strlcpy strscpy
+#else
+#define compat_const
+#endif
+
+#endif /* _COMPAT_H */
