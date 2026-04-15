e7da34a20581cc0652962a34e7a0157019ad7f20
BrainSlayer <s.gottschall@dd-wrt.com>
2026-04-15 11:39:35 +0000

    fix polarity
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@64477 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-3.18/arch/mips/ar7240/gpio_driver.c
+++ src/linux/universal/linux-3.18/arch/mips/ar7240/gpio_driver.c
@@ -480,7 +480,7 @@ static struct gpio_led generic_leds_gpio[] __initdata = {
 	 .name = "generic_19",
 	 .gpio = 19,
 #if defined(CONFIG_RUCKUSR500)
-	 .active_low = 0,
+	 .active_low = 1,
 #elif defined(CONFIG_WR841V8) || defined(CONFIG_DAP3310)
 	 .active_low = 1,
 #elif defined(CONFIG_XD3200)
@@ -495,7 +495,9 @@ static struct gpio_led generic_leds_gpio[] __initdata = {
 	{
 	 .name = "generic_20",
 	 .gpio = 20,
-#if defined(CONFIG_WR841V8) || defined(CONFIG_WR1043V4)
+#if defined(CONFIG_RUCKUSR500)
+	 .active_low = 1,
+#elif defined(CONFIG_WR841V8) || defined(CONFIG_WR1043V4)
 	 .active_low = 1,
 #elif defined(CONFIG_CPE880)
 	 .active_low = 1,
@@ -508,7 +510,7 @@ static struct gpio_led generic_leds_gpio[] __initdata = {
 	 .name = "generic_21",
 	 .gpio = 21,
 #if defined(CONFIG_RUCKUSR500)
-	 .active_low = 0,
+	 .active_low = 1,
 #elif defined(CONFIG_WR841V8) || defined(CONFIG_WR1043V4) || defined(CONFIG_ARCHERC7V5)
 	 .active_low = 1,
 #elif defined(CONFIG_CPE880)
@@ -522,7 +524,9 @@ static struct gpio_led generic_leds_gpio[] __initdata = {
 	{
 	 .name = "generic_22",
 	 .gpio = 22,
-#if defined(CONFIG_CPE880)
+#if defined(CONFIG_RUCKUSR500)
+	 .active_low = 1,
+#elif defined(CONFIG_CPE880)
 	 .active_low = 1,
 #else
 	 .active_low = 0,
