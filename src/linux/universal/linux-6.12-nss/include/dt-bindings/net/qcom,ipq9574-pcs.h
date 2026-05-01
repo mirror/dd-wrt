8b67453d8bcc6fc2a9756de315a2cb2d4468540e
BrainSlayer <s.gottschall@dd-wrt.com>
2026-05-01 14:06:47 +0000

    update for 9574 support: add new files
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@64631 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/linux/universal/linux-6.12-nss/include/dt-bindings/net/qcom,ipq9574-pcs.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Device Tree constants for the Qualcomm IPQ9574 PCS
+ */
+
+#ifndef _DT_BINDINGS_PCS_QCOM_IPQ9574_H
+#define _DT_BINDINGS_PCS_QCOM_IPQ9574_H
+
+/* The RX and TX clocks which are provided from the SerDes to NSSCC. */
+#define PCS_RX_CLK		0
+#define PCS_TX_CLK		1
+
+#endif /* _DT_BINDINGS_PCS_QCOM_IPQ9574_H */
---
+++ src/linux/universal/linux-6.12-nss/include/dt-bindings/net/qcom,qca808x.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Device Tree constants for the Qualcomm QCA808X PHYs
+ */
+
+#ifndef _DT_BINDINGS_QCOM_QCA808X_H
+#define _DT_BINDINGS_QCOM_QCA808X_H
+
+/* PHY package modes of QCA8084 to apply the interface modes of two PCSes. */
+#define QCA808X_PCS1_10G_QXGMII_PCS0_UNUNSED		0
+#define QCA808X_PCS1_SGMII_MAC_PCS0_SGMII_MAC		1
+#define QCA808X_PCS1_SGMII_MAC_PCS0_SGMII_PHY		2
+
+#endif
