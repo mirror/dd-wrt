57c42a7b0d3ed44a3824dbd73c137ef7c7fe91f1
BrainSlayer <s.gottschall@dd-wrt.com>
2026-05-03 21:25:31 +0000

    needed for emmc
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@64661 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/f2fs-tools.mk
+++ src/router/rules/f2fs-tools.mk
@@ -1,11 +1,17 @@
 f2fs-tools-configure: util-linux
 	cd f2fs-tools && ./autogen.sh 
-	cd f2fs-tools && ./configure \
+	cd f2fs-tools && ./configure ac_cv_func_lseek64=yes \
+	    --libdir=/usr/lib \
+	    --disable-shared \
+	    --enable-static \
 	    --prefix=/usr \
+	    --without-blkid \
+	    --without-lzo2 \
+	    --without-lz4 \
 	    --without-selinux \
 	    --host=$(ARCH)-linux \
-	    CFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) -I$(TOP)/util-linux/libuuid/src -ffunction-sections -fdata-sections -Wl,--gc-sections  -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -DNEED_PRINTF" \
-	    LDFLAGS="-L$(TOP)/util-linux/.libs -ffunction-sections -fdata-sections -Wl,--gc-sections" CC="$(CC) $(COPTS)"
+	    CFLAGS="$(COPTS) $(MIPS16_OPT) $(THUMB) $(LTO) -I$(TOP)/util-linux/libuuid/src -ffunction-sections -fdata-sections -Wl,--gc-sections  -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -DNEED_PRINTF" \
+	    LDFLAGS="-L$(TOP)/util-linux/.libs -ffunction-sections -fdata-sections -Wl,--gc-sections" CC="$(CC) $(COPTS) $(LTO)"
 
 f2fs-tools:
 	make -C f2fs-tools DEBUG= Q= 
@@ -16,6 +22,7 @@ f2fs-tools-clean:
 
 f2fs-tools-install:
 	make -C f2fs-tools install DESTDIR=$(INSTALLDIR)/f2fs-tools
-	rm -rf $(INSTALLDIR)/f2fs-tools/usr/share 
+	rm -rf $(INSTALLDIR)/f2fs-tools/usr/share
+	rm -rf $(INSTALLDIR)/f2fs-tools/usr/include
 	-rm -f $(INSTALLDIR)/f2fs-tools/usr/lib/*.a
 	-rm -f $(INSTALLDIR)/f2fs-tools/usr/lib/*.la
