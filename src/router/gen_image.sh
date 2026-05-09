d724a40e7006e881d84b037533831ff16d6b8341
BrainSlayer <s.gottschall@dd-wrt.com>
2026-05-09 13:34:02 +0000

    try different variant
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@64843 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/gen_image.sh
+++ src/router/gen_image.sh
@@ -36,7 +36,7 @@ BLOCKS="$((($KERNELSIZE / 2) - 1))"
 
 genext2fs -d "$part1d" -b "$BLOCKS" "$file.kernel"
 dd if="$file.kernel" of="$file" bs=512 seek="${KERNELOFFSET}" conv=notrunc
-rm -f "$file.kernel"
+#rm -f "$file.kernel"
 
 dd if="$part2f" of="$file" bs=512 seek="${ROOTFSOFFSET}" conv=notrunc
 
