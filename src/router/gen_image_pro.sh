8584029b50a20ad63fdf78eef5f2948fb0bc4461
BrainSlayer <s.gottschall@dd-wrt.com>
2026-05-10 06:36:55 +0000

    do not delete kern part
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@64870 52c4871e-980c-0410-b1e0-e73912ce01f8

---
+++ src/router/gen_image_pro.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+# Copyright (C) 2006 OpenWrt.org
+
+[ $# == 5 ] || {
+	echo "SYNTAX: $0 <file> <kernel size> <kernel directory> <rootfs size> <rootfs image>"
+	exit 1
+}
+
+file="$1"
+part1s="$2"
+part1d="$3"
+part2s="$4"
+part2f="$5"
+#part3s="$6"
+#part4s=1
+head=16
+sect=63
+cyl=$(( ($part1s + $part2s ) * 1024 * 1024 / ($head * $sect * 512)))
+
+#dd if=/dev/zero of="$file" bs=1M count=$(($part1s + $part2s - 1))  2>/dev/null || exit
+# create partition table
+rm -f $file
+
+which ptgen
+set `ptgen -o "$file" -h $head -s $sect -p ${part1s}m -p ${part2s}m  -p 3072m`
+echo $1 $2 $3 $4
+KERNELOFFSET="$(($1 / 512))"
+KERNELSIZE="$(($2 / 512))"
+ROOTFSOFFSET="$(($3 / 512))"
+ROOTFSSIZE="$(($4 / 512))"
+#ROOTFSOFFSET="$(($KERNELOFFSET+$KERNELSIZE))"
+BLOCKS="$((($KERNELSIZE / 2) - 1))"
+
+#[ $# == 8 ] || {
+#	echo "gen_image.sh: wrong/old ptgen version ABORT"
+#	exit 1
+#}
+
+genfatfs -d "$part1d" -b "$BLOCKS" "$file.kernel"
+dd if="$file.kernel" of="$file" bs=512 seek="${KERNELOFFSET}" conv=notrunc
+#rm -f "$file.kernel"
+
+dd if="$part2f" of="$file" bs=512 seek="${ROOTFSOFFSET}" conv=notrunc
+
+#which chpax >/dev/null && chpax -zp $(which grub)
+#grub --device-map=/dev/null <<EOF
+#device (hd0) $file
+#geometry (hd0) $cyl $head $sect
+#root (hd0,0)
+#setup (hd0)
+#EOF
+
