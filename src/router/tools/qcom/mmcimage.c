cc12893d67ad47e2c014ea7ef8ef284ecf8c5f71
BrainSlayer <s.gottschall@dd-wrt.com>
2026-05-10 05:05:19 +0000

    store partition count and names, even if not in use right now. so we can use it more flexible in future
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@64857 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/tools/qcom/mmcimage.c
+++ src/router/tools/qcom/mmcimage.c
@@ -1,35 +1,44 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
 	FILE *in, *in2, *out;
 	in = fopen(argv[1], "rb");
 	if (!in) {
-	    fprintf(stderr, "%s not found\n",argv[1]);
-	    return -1;
+		fprintf(stderr, "%s not found\n", argv[1]);
+		return -1;
 	}
 	in2 = fopen(argv[2], "rb");
 	if (!in2) {
-	    fprintf(stderr, "%s not found\n", argv[2]);
-	    return -1;
+		fprintf(stderr, "%s not found\n", argv[2]);
+		return -1;
 	}
 	out = fopen(argv[3], "wb");
 	if (!out) {
-	    fprintf(stderr, "%s not found\n", argv[3]);
-	    return -1;
+		fprintf(stderr, "%s not found\n", argv[3]);
+		return -1;
 	}
+	char name[32];
+	putc(2, out); // num of partitions
 	fseek(in, 0, SEEK_END);
-	unsigned int len, len2;
+	unsigned long long len, len2;
 	len = ftell(in);
 	int i;
 	rewind(in);
-	fwrite(&len, 4, 1, out);
+	memset(name, 0, sizeof(name));
+	sprintf(name, "kernel");
+	fwrite(name, sizeof(name), 1, out);
+	fwrite(&len, 8, 1, out);
 
 	fseek(in2, 0, SEEK_END);
 	len2 = ftell(in2);
 	rewind(in2);
-	fwrite(&len2, 4, 1, out);
+	memset(name, 0, sizeof(name));
+	sprintf(name, "rootfs");
+	fwrite(name, sizeof(name), 1, out);
 
+	fwrite(&len2, 8, 1, out);
 
 	for (i = 0; i < len; i++)
 		putc(getc(in), out);
@@ -38,5 +47,5 @@ int main(int argc, char *argv[])
 	fclose(in);
 	fclose(in2);
 	fclose(out);
-	    return 0;
+	return 0;
 }
