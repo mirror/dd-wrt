9f3c815dca5168da81694451b90b98abd7be192b
BrainSlayer <s.gottschall@dd-wrt.com>
2026-05-14 20:33:38 +0000

    update kernel
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@64996 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.1-nss/net/ipv4/ip_output.c
+++ src/linux/universal/linux-6.1-nss/net/ipv4/ip_output.c
@@ -1463,6 +1463,8 @@ ssize_t	ip_append_page(struct sock *sk, struct flowi4 *fl4, struct page *page,
 			goto error;
 		}
 
+		skb_shinfo(skb)->flags |= SKBFL_SHARED_FRAG;
+
 		if (skb->ip_summed == CHECKSUM_NONE) {
 			__wsum csum;
 			csum = csum_page(page, offset, len);
