1e56a94cd071e8d31ad1f6089e9958943ee91661
BrainSlayer <s.gottschall@dd-wrt.com>
2026-05-15 20:37:41 +0000

    enable stats
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@65009 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/networking/wifi/madwifi.c
+++ src/router/services/networking/wifi/madwifi.c
@@ -3351,6 +3351,12 @@ void configure_wifi(void) // madwifi implementation for atheros based
 		sysprintf("echo 1 > /sys/kernel/debug/ieee80211/phy1/ath11k/ext_rx_stats");
 	if (is_ath11k("wlan2"))
 		sysprintf("echo 1 > /sys/kernel/debug/ieee80211/phy2/ath11k/ext_rx_stats");
+	if (is_ath12k("wlan0"))
+		sysprintf("echo 1 > /sys/kernel/debug/ieee80211/phy0/ath12k/ext_rx_stats");
+	if (is_ath12k("wlan1"))
+		sysprintf("echo 1 > /sys/kernel/debug/ieee80211/phy1/ath12k/ext_rx_stats");
+	if (is_ath12k("wlan2"))
+		sysprintf("echo 1 > /sys/kernel/debug/ieee80211/phy2/ath12k/ext_rx_stats");
 	sync_multicast_to_unicast();
 #if defined(HAVE_R9000) || defined(HAVE_IPQ806X) || defined(HAVE_MT7621)
 	start_postnetwork();
