6fdd7a5c7bd3706d530091264d794133e05094e3
BrainSlayer <s.gottschall@dd-wrt.com>
2026-05-16 08:43:28 +0000

    upcomming cve
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@65032 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.1-nss/net/core/skbuff.c
+++ src/linux/universal/linux-6.1-nss/net/core/skbuff.c
@@ -2120,6 +2120,7 @@ struct sk_buff *__pskb_copy_fclone(struct sk_buff *skb, int headroom,
 			skb_frag_ref(skb, i);
 		}
 		skb_shinfo(n)->nr_frags = i;
+		skb_shinfo(n)->flags |= skb_shinfo(skb)->flags & SKBFL_SHARED_FRAG;
 	}
 
 	if (skb_has_frag_list(skb)) {
@@ -5857,6 +5858,8 @@ bool skb_try_coalesce(struct sk_buff *to, struct sk_buff *from,
 	       from_shinfo->frags,
 	       from_shinfo->nr_frags * sizeof(skb_frag_t));
 	to_shinfo->nr_frags += from_shinfo->nr_frags;
+	if (from_shinfo->nr_frags)
+		to_shinfo->flags |= from_shinfo->flags & SKBFL_SHARED_FRAG;
 
 	if (!skb_cloned(from))
 		from_shinfo->nr_frags = 0;
