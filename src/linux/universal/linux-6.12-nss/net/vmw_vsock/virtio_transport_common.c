9980af1d1af963d0d60f08c3d6dc599f54de5a9a
BrainSlayer <s.gottschall@dd-wrt.com>
2026-05-18 06:07:19 +0000

    update
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@65051 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.12-nss/net/vmw_vsock/virtio_transport_common.c
+++ src/linux/universal/linux-6.12-nss/net/vmw_vsock/virtio_transport_common.c
@@ -430,9 +430,7 @@ static int virtio_transport_send_pkt_info(struct vsock_sock *vsk,
 static bool virtio_transport_inc_rx_pkt(struct virtio_vsock_sock *vvs,
 					u32 len)
 {
-	u64 skb_overhead = (skb_queue_len(&vvs->rx_queue) + 1) * SKB_TRUESIZE(0);
-
-	if (skb_overhead + vvs->buf_used + len > vvs->buf_alloc)
+	if (vvs->buf_used + len > vvs->buf_alloc)
 		return false;
 
 	vvs->rx_bytes += len;
