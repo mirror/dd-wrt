090ae61b63c10577b6ce40e2b5e33f4fdc2bea2b
BrainSlayer <s.gottschall@dd-wrt.com>
2026-05-27 18:09:12 +0000

    revert custom name
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@65231 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/libutils/detect.c
+++ src/router/libutils/libutils/detect.c
@@ -190,9 +190,7 @@ static void setRouter(const char *name)
 	nvram_set(NVROUTER_ALT, "Industrial Router");
 #endif /*HAVE_ANTAIRA_MINI */
 #elif HAVE_NDTRADE
-#ifdef HAVE_RUCKUSH500
-	nvram_set(NVROUTER_ALT, "ND Trade");
-#else
+#ifdef HAVE_DW02_412H
 	nvram_set(NVROUTER_ALT, "KT412H-8000");
 #endif
 #endif
