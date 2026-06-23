002a34d427eedfbd031f94f8c06b7ab7021e42be
BrainSlayer <s.gottschall@dd-wrt.com>
2026-06-23 12:33:02 +0000

    fix api
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@65395 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/qca-nss/ssdk-shell/make/tools.mk
+++ src/router/qca-nss/ssdk-shell/make/tools.mk
@@ -1,9 +1,5 @@
 
 ifeq (linux, $(OS))
-  CC=$(TOOL_PATH)/$(TOOLPREFIX)gcc
-  AR=$(TOOL_PATH)/$(TOOLPREFIX)gcc-ar
-  LD=$(TOOL_PATH)/$(TOOLPREFIX)ld
-  STRIP=$(TOOL_PATH)/$(TOOLPREFIX)strip
   CP=cp
   MKDIR=mkdir
   RM=rm
