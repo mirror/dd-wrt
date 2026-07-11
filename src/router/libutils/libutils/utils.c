2fd1f3022ba0aafd769feb1a9ed58d8e2c5581eb
BrainSlayer <s.gottschall@dd-wrt.com>
2026-07-11 10:47:25 +0000

    fix typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@65606 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/libutils/libutils/utils.c
+++ src/router/libutils/libutils/utils.c
@@ -1093,9 +1093,9 @@ static int ifcompare(const void *a, const void *b)
 	const char *src = (const char *)a;
 	const char *dst = (const char *)b;
 	if (!strncmp(src, "lan", 3) && !strncmp(dst, "lan", 3)) {
-		if (strlen(src) == 3 && strlen(dst) > 3)
+		if (strlen(src) == 4 && strlen(dst) > 4)
 			return -1;
-		if (strlen(dst) == 3 && strlen(src) > 3)
+		if (strlen(dst) == 4 && strlen(src) > 4)
 			return 1;
 	}
 	return strcmp(*(const char **)a, *(const char **)b);
