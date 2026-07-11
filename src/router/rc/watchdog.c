4b9253169a89cce85f04c85caa9fd10bed2ac3f6
BrainSlayer <s.gottschall@dd-wrt.com>
2026-07-11 00:37:35 +0000

    fix typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@65586 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/watchdog.c
+++ src/router/rc/watchdog.c
@@ -83,7 +83,7 @@ static void check_fan(int brand)
 			    psu -= 50000;
 			else
 			    psu = 0;
-			sysprintf("/bin/echo %s > /sys/class/hwmon/hwmon8/pwm1", psu / 59);
+			sysprintf("/bin/echo %d > /sys/class/hwmon/hwmon8/pwm1", psu / 59);
 		}
 	}
 	if (nvram_match("DD_BOARD", "Zyxel XGS1250-12 B1")) {
