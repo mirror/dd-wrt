71afc8f29a86901da73827e184146a54f039c9b8
BrainSlayer <s.gottschall@dd-wrt.com>
2026-07-24 21:34:50 +0000

    fix typo
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@65770 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.1-nss/net/sched/sch_fq_codel.c
+++ src/linux/universal/linux-6.1-nss/net/sched/sch_fq_codel.c
@@ -501,17 +501,17 @@ static int fq_codel_init(struct Qdisc *sch, struct nlattr *opt,
 	sch->limit = 10*1024;
 	q->flows_cnt = 1024;
 #if defined(CONFIG_X86) || defined(CONFIG_ALPINE) || defined(CONFIG_ARCH_QCOM) || defined(CONFIG_SOC_IMX6) || defined(CONFIG_ARM64)
-	fq->memory_limit = 32 << 20; /* 32 MBytes */
+	q->memory_limit = 32 << 20; /* 32 MBytes */
 #elif defined(CONFIG_UBNTXW)
-	fq->memory_limit = 4 << 20; /* 16 MBytes */
+	q->memory_limit = 4 << 20; /* 16 MBytes */
 #elif defined(CONFIG_ARCH_CNS3XXX)
-	fq->memory_limit = 16 << 20; /* 16 MBytes */
+	q->memory_limit = 16 << 20; /* 16 MBytes */
 #elif defined(CONFIG_ARCH_IXP4XX)
-	fq->memory_limit = 4 << 20; /* 4 MBytes */
+	q->memory_limit = 4 << 20; /* 4 MBytes */
 #elif (defined(CONFIG_MIPS) && !defined(CONFIG_64BIT))
-	fq->memory_limit = 1 << 18; /* 256kb */
+	q->memory_limit = 1 << 18; /* 256kb */
 #else
-	fq->memory_limit = 4 << 20; /* 4 MBytes */
+	q->memory_limit = 4 << 20; /* 4 MBytes */
 #endif
 	q->drop_batch_size = 64;
 	q->quantum = psched_mtu(qdisc_dev(sch));
