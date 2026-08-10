2d87c3d130b41b3563121c96ce0822348e18945f
BrainSlayer <s.gottschall@dd-wrt.com>
2026-08-10 06:24:46 +0000

    fix number
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@65915 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rc/nvram.c
+++ src/router/rc/nvram.c
@@ -117,7 +117,7 @@ static int nvram_main(int argc, char **argv)
 				puts(name);
 			int space;
 			size = nvram_used(&space);
-			fprintf(stdout, "size: %d bytes (%d total, %s left)\n", size, space, space - size);
+			fprintf(stdout, "size: %d bytes (%d total, %d left)\n", size, space, space - size);
 		} else if (!strncmp(*argv, "backup", 6)) {
 			if (*++argv) {
 				int ret = nvram_backup(*argv);
