0f4bd0e5a6caf288ddde8a6639c5e83438ef81d0
BrainSlayer <s.gottschall@dd-wrt.com>
2026-08-28 09:16:26 +0000

    not for now
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@66064 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/mac80211-rules/ath9k-kconfig.mk
+++ src/router/mac80211-rules/ath9k-kconfig.mk
@@ -554,6 +554,10 @@ endif
 ifeq ($(CONFIG_ATH12K),y)
 	-mkdir -p $(INSTALLDIR)/ath9k/lib/firmware/ath12k/
 	-cp -av $(TOP)/firmwares/wireless/ath12k/* $(INSTALLDIR)/ath9k/lib/firmware/ath12k/
+	rm -rf $(INSTALLDIR)/ath9k/lib/firmware/ath12k/IPQ5332
+	rm -rf $(INSTALLDIR)/ath9k/lib/firmware/ath12k/IPQ5424
+	rm -rf $(INSTALLDIR)/ath9k/lib/firmware/ath12k/QCN9589
+	rm -rf $(INSTALLDIR)/ath9k/lib/firmware/ath12k/QCN9625
 ifeq ($(CONFIG_IPQ95XX),y)
 	rm -rf $(INSTALLDIR)/ath9k/lib/firmware/ath12k/QCC2072
 	rm -rf $(INSTALLDIR)/ath9k/lib/firmware/ath12k/WCN7850
