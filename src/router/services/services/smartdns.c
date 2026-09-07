07dcb0139ee884d4f93344a1619f346ab18775c2
BrainSlayer <s.gottschall@dd-wrt.com>
2026-09-07 19:35:55 +0000

    option to use only custom options field
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@66110 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/smartdns.c
+++ src/router/services/services/smartdns.c
@@ -41,75 +41,77 @@ void start_smartdns(void)
 	if (!nvram_matchi("smartdns", 1))
 		return;
 	FILE *fp = fopencreate("/tmp/smartdns/smartdns.conf", "wb");
-	int port = 6053;
-	if (!nvram_matchi("dns_dnsmasq", 1))
-		port = 53;
-	int ipv6 = nvram_matchi("ipv6_enable", 1);
-	int tcp_server = nvram_matchi("smartdns_tcp", 1);
-	char *hostname = nvram_safe_get("wan_hostname");
-	if (*hostname)
-		fprintf(fp, "server-name %s\n", hostname);
-	if (ipv6)
-		fprintf(fp, "bind [::]:%d\n", port);
-	else
-		fprintf(fp, "bind :%d\n", port);
-	if (tcp_server) {
+	if (!nvram_match("smartdns_custom", "1")) {
+		int port = 6053;
+		if (!nvram_matchi("dns_dnsmasq", 1))
+			port = 53;
+		int ipv6 = nvram_matchi("ipv6_enable", 1);
+		int tcp_server = nvram_matchi("smartdns_tcp", 1);
+		char *hostname = nvram_safe_get("wan_hostname");
+		if (*hostname)
+			fprintf(fp, "server-name %s\n", hostname);
 		if (ipv6)
-			fprintf(fp, "bind-tcp [::]:%d\n", port);
+			fprintf(fp, "bind [::]:%d\n", port);
 		else
-			fprintf(fp, "bind-tcp :%d\n", port);
-	}
-
-	if (nvram_matchi("smartdns_dualstack_ip_selection", 1))
-		fprintf(fp, "dualstack-ip-selection yes\n");
-	if (nvram_matchi("smartdns_prefetch_domain", 1))
-		fprintf(fp, "prefetch-domain yes\n");
-	if (nvram_matchi("smartdns_serve_expired", 1))
-		fprintf(fp, "serve-expired yes\n");
-	//fprintf(fp, "log-size 64K\n");
-	fprintf(fp, "log-size 32K\n");
-	fprintf(fp, "log-num 1\n");
-	fprintf(fp, "log-level warn\n");
-	fprintf(fp, "cache-size %s\n", nvram_default_get("smartdns_cache_size", "3000"));
-	fprintf(fp, "log-file /tmp/smartdns.log\n");
-	if (!nvram_matchi("dns_dnsmasq", 1) && !nvram_matchi("dhcpd_usenvram", 1)) {
-		int usejffs = 0;
-		if (jffs_mounted() && nvram_matchi("dhcpd_usejffs", 1)) {
-			FILE *fpcheck;
-			if (!(fpcheck = fopen("/jffs/dnsmasq.leases", "a"))) {
-				usejffs = 0;
-			} else {
-				fclose(fpcheck);
-				usejffs = 1;
-			}
+			fprintf(fp, "bind :%d\n", port);
+		if (tcp_server) {
+			if (ipv6)
+				fprintf(fp, "bind-tcp [::]:%d\n", port);
+			else
+				fprintf(fp, "bind-tcp :%d\n", port);
 		}
-		if (usejffs)
-			fprintf(fp, "dnsmasq-lease-file /jffs/dnsmasq.leases\n");
-		else
-			fprintf(fp, "dnsmasq-lease-file /tmp/dnsmasq.leases\n");
-	}
+
+		if (nvram_matchi("smartdns_dualstack_ip_selection", 1))
+			fprintf(fp, "dualstack-ip-selection yes\n");
+		if (nvram_matchi("smartdns_prefetch_domain", 1))
+			fprintf(fp, "prefetch-domain yes\n");
+		if (nvram_matchi("smartdns_serve_expired", 1))
+			fprintf(fp, "serve-expired yes\n");
+		//fprintf(fp, "log-size 64K\n");
+		fprintf(fp, "log-size 32K\n");
+		fprintf(fp, "log-num 1\n");
+		fprintf(fp, "log-level warn\n");
+		fprintf(fp, "cache-size %s\n", nvram_default_get("smartdns_cache_size", "3000"));
+		fprintf(fp, "log-file /tmp/smartdns.log\n");
+		if (!nvram_matchi("dns_dnsmasq", 1) && !nvram_matchi("dhcpd_usenvram", 1)) {
+			int usejffs = 0;
+			if (jffs_mounted() && nvram_matchi("dhcpd_usejffs", 1)) {
+				FILE *fpcheck;
+				if (!(fpcheck = fopen("/jffs/dnsmasq.leases", "a"))) {
+					usejffs = 0;
+				} else {
+					fclose(fpcheck);
+					usejffs = 1;
+				}
+			}
+			if (usejffs)
+				fprintf(fp, "dnsmasq-lease-file /jffs/dnsmasq.leases\n");
+			else
+				fprintf(fp, "dnsmasq-lease-file /tmp/dnsmasq.leases\n");
+		}
 	//egc: do we really need these certificates and if so are these the right ones?
 	#ifdef HAVE_HTTPS
-	fprintf(fp, "ca-file /etc/ssl/certs/ca-certificates.crt\n");
-	fprintf(fp, "ca-path /etc/ssl/certs/\n");
+		fprintf(fp, "ca-file /etc/ssl/certs/ca-certificates.crt\n");
+		fprintf(fp, "ca-path /etc/ssl/certs/\n");
 	#endif
 	#ifdef HAVE_TOR
-	if (nvram_match("tor_enable", "1")) {
-		fprintf(fp, "server %s:5353\n", get_lan_ipaddr());
-	}
-	#endif
-	struct dns_lists *dns_list = NULL;
-	if (nvram_matchi("recursive_dns", 1)) {
-		fprintf(fp, "server 127.0.0.1:7053\n");
-	} else if (nvram_matchi("smartdns_use_dns", 0)) {
-		dns_list = get_dns_list(1);
-		if (dns_list && dns_list->num_servers > 0) {
-			int i;
-			for (i = 0; i < dns_list->num_servers; i++)
-				fprintf(fp, "server %s\n", dns_list->dns_server[i].ip);
+		if (nvram_match("tor_enable", "1")) {
+			fprintf(fp, "server %s:5353\n", get_lan_ipaddr());
 		}
-		if (dns_list)
-			free_dns_list(dns_list);
+	#endif
+		struct dns_lists *dns_list = NULL;
+		if (nvram_matchi("recursive_dns", 1)) {
+			fprintf(fp, "server 127.0.0.1:7053\n");
+		} else if (nvram_matchi("smartdns_use_dns", 0)) {
+			dns_list = get_dns_list(1);
+			if (dns_list && dns_list->num_servers > 0) {
+				int i;
+				for (i = 0; i < dns_list->num_servers; i++)
+					fprintf(fp, "server %s\n", dns_list->dns_server[i].ip);
+			}
+			if (dns_list)
+				free_dns_list(dns_list);
+		}
 	}
 	fwritenvram("smartdns_options", fp);
 	fclose(fp);
