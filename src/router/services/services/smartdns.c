aa5dd67c3138e1308051eecc4c239592a3c20dd8
BrainSlayer <s.gottschall@dd-wrt.com>
2026-09-09 08:40:27 +0000

    cannot be used without accurate system time
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@66132 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/services/services/smartdns.c
+++ src/router/services/services/smartdns.c
@@ -99,14 +99,14 @@ void start_smartdns(void)
 			fprintf(fp, "server 127.0.0.1:7053\n");
 		} else if (nvram_matchi("smartdns_use_dns", 0)) {
 			if (nvram_match("dns_adblock", "1")) {
-	#ifdef HAVE_USE_OPENSSL
+/*	#ifdef HAVE_USE_OPENSSL
 				fprintf(fp,
 					"server-tls 188.34.161.210:853 -host-name root.hagezi.org -tls-host-verify root.hagezi.org\n");
 				fprintf(fp,
 					"server-tls 159.69.155.94:853 -host-name wurzn.hagezi.org -tls-host-verify wurzn.hagezi.org\n");
 				fprintf(fp,
 					"server-tls 95.217.163.17:853 -host-name juuri.hagezi.org -tls-host-verify juuri.hagezi.org\n");
-	#endif
+	#endif*/
 				fprintf(fp, "server 188.34.161.210\n");
 				fprintf(fp, "server 159.69.155.94\n");
 				fprintf(fp, "server 95.217.163.17\n");
