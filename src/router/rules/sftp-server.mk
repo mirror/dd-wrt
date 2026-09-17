1c389c8a714a4acde74b615bb4213ef59596c068
BrainSlayer <s.gottschall@dd-wrt.com>
2026-09-17 15:19:55 +0000

    use openssh as build source
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@66257 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/router/rules/sftp-server.mk
+++ src/router/rules/sftp-server.mk
@@ -1,9 +1,38 @@
-sftp-server-clean:
-	make -C sftp-server clean
+sftp-server-configure:
+	cd openssh && ./configure \
+		--host=$(ARCH)-uclibc-linux \
+		--prefix=/usr \
+		--libdir=/usr/lib \
+		--sysconfdir=/etc/ssh \
+		--with-privsep-user=sshd \
+		--with-privsep-path=/var/empty \
+		--disable-strip \
+		--disable-etc-default-login \
+		--disable-lastlog \
+		--disable-utmp \
+		--disable-utmpx \
+		--disable-wtmp \
+		--disable-wtmpx \
+		--without-bsd-auth \
+		--without-pam \
+		--without-bsd-out \
+		--without-libedit \
+		--without-ldns \
+		--without-security-key-bsd \
+		--disable-strip \
+		--without-openssl \
+		--without-zlib \
+		--without-kerberos5 \
+		--with-stackprotect \
+		--with-cflags-after="-fzero-call-used-regs=skip $(LTO) $(MIPS16_OPT) -ffunction-sections -fdata-sections -Wl,--gc-sections"
 
-sftp-server: nvram shared
-	make -C sftp-server
+
+sftp-server-clean:
+	make -C openssh clean
+
+sftp-server: 
+	make -C openssh
 
 sftp-server-install:
-	make -C sftp-server install INSTALLDIR=$(INSTALLDIR)/sftp-server
+	install -D openssh/sftp-server $(INSTALLDIR)/sftp-server/usr/libexec/sftp-server	
 
