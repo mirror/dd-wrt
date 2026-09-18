110005e7895cccbfd5c4677b9ecff761a1e1f04f
BrainSlayer <s.gottschall@dd-wrt.com>
2026-09-18 06:19:43 +0000

    merge from upstream
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@66263 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.12/drivers/net/dsa/rtl83xx/l3.h
+++ src/linux/universal/linux-6.12/drivers/net/dsa/rtl83xx/l3.h
@@ -60,11 +60,11 @@ struct otto_l3_nexthop {
 	u16 id;		/* ID: L3_NEXT_HOP table-index or route-index set in L2_NEXT_HOP */
 	u32 dev_id;
 	u16 port;
-	u16 vid;	/* VLAN-ID for L2 table entry (saved from L2-UC entry) */
 	u16 rvid;	/* Relay VID/FID for the L2 table entry */
 	u64 mac;	/* The MAC address of the entry in the L2_NEXT_HOP table */
 	u16 mac_id;
 	u16 l2_id;	/* Index of this next hop forwarding entry in L2 FIB table */
+	u64 l2_seed;	/* Seed the entry at l2_id was claimed on */
 	u64 gw;		/* The gateway MAC address packets are forwarded to */
 	int if_id;	/* Interface (into L3_EGR_INTF_IDX) */
 	bool l2_installed;	/* Entry written to the L2 table */
