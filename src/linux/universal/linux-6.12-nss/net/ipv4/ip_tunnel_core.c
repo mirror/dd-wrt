06ea803203d4f4fd622b3cd1004563469729ffd9
BrainSlayer <s.gottschall@dd-wrt.com>
2026-09-23 08:09:20 +0000

    update kernel
    git-svn-id: svn://svn.dd-wrt.com/DD-WRT@66283 52c4871e-980c-0410-b1e0-e73912ce01f8

--- src/linux/universal/linux-6.12-nss/net/ipv4/ip_tunnel_core.c
+++ src/linux/universal/linux-6.12-nss/net/ipv4/ip_tunnel_core.c
@@ -281,6 +281,9 @@ static int iptunnel_pmtud_build_icmp(struct sk_buff *skb, int mtu)
 	eth_header(skb, skb->dev, ntohs(eh.h_proto), eh.h_source, eh.h_dest, 0);
 	skb_reset_mac_header(skb);
 
+	if (skb_valid_dst(skb))
+		skb_dst_drop(skb);
+
 	return skb->len;
 }
 
@@ -383,6 +386,9 @@ static int iptunnel_pmtud_build_icmpv6(struct sk_buff *skb, int mtu)
 
 	eth_header(skb, skb->dev, ntohs(eh.h_proto), eh.h_source, eh.h_dest, 0);
 	skb_reset_mac_header(skb);
+
+	if (skb_valid_dst(skb))
+		skb_dst_drop(skb);
 
 	return skb->len;
 }
